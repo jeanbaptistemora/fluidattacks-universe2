eb17ca4135d18910f81e1577af7db36c9c7731aa
jrestrepoatfluid <jrestrepo@fluid.la>
2016-10-18 20:58:03 +0000

    solución problemas de hosts

--- smoke.sh
+++ smoke.sh
@@ -4,7 +4,7 @@
 export ANSIBLE_HOSTS="$PROJECT_DIR"/config/hosts
 
 # Probar configuracion ansible 
-ansible container -m ping
+ansible container -m ping -i config/hosts
 
 # Ejecutar playbook
-ansible-playbook main.yml
+ansible-playbook main.yml -i config/hosts
