9bdf14de76e995144776ee1c0e38963dbb5a1f08
jrestrepoatfluid <jrestrepo@fluid.la>
2016-10-19 00:45:53 +0000

    habilitar modo verbose.

--- smoke.sh
+++ smoke.sh
@@ -4,7 +4,7 @@
 export ANSIBLE_HOSTS="$PROJECT_DIR"/config/hosts
 
 # Probar configuracion ansible 
-ansible container -m ping -i config/hosts
+ansible container -m ping -i config/hosts -vvvv
 
 # Ejecutar playbook
-ansible-playbook main.yml -i config/hosts
+ansible-playbook main.yml -i config/hosts -vvvv
