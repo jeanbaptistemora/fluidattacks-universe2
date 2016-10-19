34f7c900d3ad1973adebcbc8f2563c31de20e851
STATIC\jrestrepo <jrestrepo@fluid.la>
2016-10-18 21:10:06 -0500

    habilitar verbose

--- start.sh
+++ start.sh
@@ -17,7 +17,7 @@ export IP=127.0.0.1
 echo "---### Compilando contenedor."
 
 # construir la imagen
-docker build -t alg "$PROJECT_DIR"
+docker build -t alg .
 
 # Mensaje de inicio
 echo "---### Iniciando contenedor."
@@ -27,26 +27,26 @@ if [ -z $(docker ps -q -f name="$SERVICE") ]; then
 
 #  echo "Contenedor no ha iniciado, iniciando contenedor..."	 
 
-  # Crear dinamicamente claves de acceso al contenedor
-  # La ruta de configuración SSH tambien esta parametrizado en test/setup/hosts
-  mkdir -p ~/.ssh/
-  cp "$PROJECT_DIR"/config/ssh_config ~/.ssh/config.facont
-  echo -e "y\n" | ssh-keygen -b 2048 -t rsa -f ~/.ssh/facont_id_rsa -q -N ""
+# Crear dinamicamente claves de acceso al contenedor
+# La ruta de configuración SSH tambien esta parametrizado en test/setup/hosts
+mkdir -p ~/.ssh/
+cp config/ssh_config ~/.ssh/config.facont
+echo -e "y\n" | ssh-keygen -b 2048 -t rsa -f ~/.ssh/facont_id_rsa -q -N ""
 
-  docker run \
+docker run \
 		--detach \
 		--name="$SERVICE" \
 		--hostname="$SERVICE" \
-        -p 22000:22 \
-        -p 443:443 \
-        -p 80:80 \
+    -p 22000:22 \
+    -p 443:443 \
+    -p 80:80 \
 		-e SSH_KEY="$(cat ~/.ssh/facont_id_rsa.pub)" \
 		alg
 
-  echo "Esperando que el puerto 22000 de SSH este abierto."
-  until nc -z $IP 22000; do : sleep 5; done
-  echo "Puerto SSH (22000) abierto en contenedor."
+echo "Esperando que el puerto 22000 de SSH este abierto."
+until nc -z $IP 22000; do : sleep 5; done
+echo "Puerto SSH (22000) abierto en contenedor."
 else
-  echo "Contenedor ya inicio, reutilizando contenedor."
+echo "Contenedor ya inicio, reutilizando contenedor."
 fi
 
