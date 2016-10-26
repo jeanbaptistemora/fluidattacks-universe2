96e5c46b8a364db55978f7d309e0f2827ac88192
jrestrepoatfluid <jrestrepo@fluid.la>
2016-10-25 22:02:44 -0500

    Agregando procedimiento de instalación moodle y mysql.
    Pendiente importar datos de la base de datos.

--- exams.sh
+++ exams.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 exams/container/build.sh
 exams/container/start.sh
-#ansible-playbook exams/main.yml -i exams/provision/hosts --vault-password-file ~/.vault.txt
+ansible-playbook exams/main.yml -i exams/provision/hosts --vault-password-file ~/.vault.txt
 
