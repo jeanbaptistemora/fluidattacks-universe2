85b87415955ffac7af87fe0b8b5273a64cdd284b
jrestrepoatfluid <jrestrepo@fluid.la>
2016-11-02 18:52:08 -0500

    Eliminación error conexión ssh.

--- exams/serverspec/spec/spec_helper.rb
+++ exams/serverspec/spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'bundler/setup'
 
 RSpec.configure do |config|
   set :host,  '127.0.0.1'
-  set :ssh_options, :user => 'root', :port => 22001, :verbose => :warn, :host_key => 'ssh-rsa', :keys => '~/.ssh/exams_facont_id_rsa'
+  set :ssh_options, :user => 'root', :port => 22001, :host_key => 'ssh-rsa', :keys => '~/.ssh/exams_facont_id_rsa'
   set :backend, :ssh
   set :request_pty, true
 end
