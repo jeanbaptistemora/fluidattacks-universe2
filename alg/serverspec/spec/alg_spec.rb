b4414692cd01e82d0a4dfdd026df5618f68e5df2
jrestrepoatfluid <jrestrepo@fluid.la>
2016-11-03 16:28:29 -0500

    Agregando pruebas a serverspec.

--- alg/serverspec/spec/alg_spec.rb
+++ alg/serverspec/spec/alg_spec.rb
@@ -12,14 +12,18 @@ describe command('ls /etc/apache2/sites-enabled') do
   its(:stdout) { should eq "000-default.conf  default-ssl  fluid.la\n"}
 end
 
-#describe command("lsb_release -d") do
-#  it { should return_stdout /jessie/ }
-#end
+describe command("lsb_release -d") do
+  its(:stdout) { should contain /jessie/ }
+end
 
-#describe package(apache) do
-#  it { should be_installed }
-#end
+describe package('apache2') do
+  it { should be_installed }
+end
 
-#describe service(apache) do
-#  it { should be_enabled }
-#end
+describe service('apache2') do
+  it { should be_enabled }
+end
+
+describe user('root') do 
+  it {should exist }
+end
