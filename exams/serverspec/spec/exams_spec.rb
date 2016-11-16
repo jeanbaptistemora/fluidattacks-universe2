6b2040cb9099f3254f1787020746c9f2a8b06c6e
jrestrepoatfluid <jrestrepo@fluid.la>
2016-11-16 09:01:51 -0500

    Corregir salida 0. Agregar nuevos tests.

--- exams/serverspec/spec/exams_spec.rb
+++ exams/serverspec/spec/exams_spec.rb
@@ -16,23 +16,27 @@ describe file('/var/lib/mysql/moodle') do
   it { should be_directory }
 end
 
+describe file('/var/www/moodledata') do
+  it { should be_directory }
+end
+
+describe file('/var/www/html') do
+  it { should be_mode 755 }
+end
+
 describe command('mysql -e "select version();"') do
   its(:stdout) { should contain "Access denied" }
 end
 
-describe user('exams') do
-  it {should exist }
-end
-
 describe cron do
-  it { should have_entry '"/usr/bin/php /var/www/html/admin/cli/cron.php >/dev/null"' }
+  it { should have_entry '* * * * * /usr/bin/php /var/www/html/admin/cli/cron.php >/dev/null' }
 end
 
 describe service('apache2') do
   it { should be_enabled }
 end
 
-describe service('mysql-server') do
+describe service('mysql') do
   it { should be_enabled }
 end
 
