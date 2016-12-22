450c35c81a98f3fd4143b9f650cc6610250f23f0
jrestrepoatfluid <jrestrepo@fluid.la>
2016-12-22 15:08:25 -0500

    Serverspec test

--- alg/serverspec/spec/alg_spec.rb
+++ alg/serverspec/spec/alg_spec.rb
@@ -12,7 +12,7 @@ describe command('ls /etc/apache2/sites-enabled') do
   its(:stdout) { should eq "000-default.conf  default-ssl  fluid.la  www.fluid.la  www.fluidsignal.com\n"}
 end
 
-describe command('curl -s https://localhost/forms/compras | grep "<title>"') do
+describe command('curl -sk https://localhost/forms/compras | grep "<title>"') do
   its(:stdout) { should eq "    <title>Compras - Formstack</title>\n"}
 end
 
