3f8361222cb748342fd197d9cf62c37fbfb3835d
jrestrepoatfluid <jrestrepo@fluid.la>
2017-02-28 14:29:41 -0500

    Disable TLSv1

---
+++ servers/alg/scripts/asserts.py
@@ -0,0 +1,11 @@
+from fluidasserts.service import http
+from fluidasserts.service import http_ssl
+from fluidasserts.service import moddns
+
+server = '127.0.0.1'
+http.is_version_visible(server)
+http_ssl.is_cert_inactive(server)
+http_ssl.is_cert_validity_lifespan_unsafe(server)
+http_ssl.is_pfs_disabled(server)
+http_ssl.is_sslv3_enabled(server)
+http_ssl.is_tlsv1_enabled(server)
