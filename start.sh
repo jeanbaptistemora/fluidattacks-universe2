351a9feaf1399bc4ebd7b61815bab0c401c197ef
Jonathan Armas <jarmas@fluid.la>
2017-04-04 13:29:44 -0500

    Test without sh

--- start.sh
+++ start.sh
@@ -12,6 +12,6 @@ done
 
 # Construye los servers
 for server in ${SERVERS}; do
-    sh /root/fluid-serves/servers/${server}/scripts/start.sh
+    /root/fluid-serves/servers/${server}/scripts/start.sh
     ansible-playbook /root/fluid-serves/servers/${server}/main.yml -i /root/fluid-serves/servers/${server}/hosts --vault-password-file ~/.vault.txt
 done
