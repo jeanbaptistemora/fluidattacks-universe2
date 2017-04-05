33b306f4ef6264e05bb1b4f2abe9a46c77146c57
Jonathan Armas <jarmas@fluid.la>
2017-04-05 13:48:35 -0500

    Fixed cron backup

--- start.sh
+++ start.sh
@@ -15,3 +15,8 @@ for server in ${SERVERS}; do
     servers/${server}/scripts/start.sh
     ansible-playbook servers/${server}/main.yml -i servers/${server}/hosts --vault-password-file ~/.vault.txt
 done
+
+#Crea cron para backup de exams
+command="ansible-playbook servers/exams/scripts/backup.yml --vault-password-file ~/.vault.txt"
+job="0 1 * * * $command"
+cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
