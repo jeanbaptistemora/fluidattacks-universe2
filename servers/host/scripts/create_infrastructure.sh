327bda989d3c90fd2e4022c04c1ff52ce78a68a6
Jonathan Armas <jarmas@fluid.la>
2017-04-24 16:00:10 -0500

    Return defaultssl

--- servers/host/scripts/create_infrastructure.sh
+++ servers/host/scripts/create_infrastructure.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Realiza verificaciones de emails y domains de AmazonSES
-python servers/host/scripts/amazonses_creator.py
+# # Realiza verificaciones de emails y domains de AmazonSES
+# python servers/host/scripts/amazonses_creator.py
 
 # Crea las instancias de EC2, VPC, ACL y Security Groups
 python servers/host/scripts/cf_ec2creator.py
 
-# Crea el DNS de Route53 y los registros de DNS
-python servers/host/scripts/cf_r53creator.py
-
-# Crea los Buckets S3
-python servers/host/scripts/cf_s3creator.py
+# # Crea el DNS de Route53 y los registros de DNS
+# python servers/host/scripts/cf_r53creator.py
+#
+# # Crea los Buckets S3
+# python servers/host/scripts/cf_s3creator.py
