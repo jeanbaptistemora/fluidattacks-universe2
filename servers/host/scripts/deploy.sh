aaff844b28a323e46437a79e7a2c66fab8e08fad
Jonathan Armas <jarmas@fluid.la>
2017-05-10 16:05:56 -0500

    Change order

--- servers/host/scripts/deploy.sh
+++ servers/host/scripts/deploy.sh
@@ -10,9 +10,9 @@ IP=$(cat ${IP_FILE})
 
 sed -e "s/__ip_address__/${IP}/g" ${HOST_TEMPLATE} > ${HOST_FILE}
 
-ssh-add ${KEY_FILE}
 echo "Esperando que el puerto 22 de SSH este abierto."
 until nc -z $IP 22; do : sleep 0.2; done
+ssh-add ${KEY_FILE}
 sudo scp "~/.vault.txt" "${USERNAME}@${IP}:/home/${USERNAME}/"
 scp "servers/host/scripts/install_python.sh" "${USERNAME}@${IP}:/home/${USERNAME}/"
 python servers/host/scripts/install_python.py
