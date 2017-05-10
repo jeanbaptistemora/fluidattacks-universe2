32b35d45c750d6aa9b3d397dbeccb28102106e7f
Jonathan Armas <jarmas@fluid.la>
2017-05-10 16:22:37 -0500

    Using root

--- servers/host/scripts/deploy.sh
+++ servers/host/scripts/deploy.sh
@@ -13,6 +13,6 @@ sed -e "s/__ip_address__/${IP}/g" ${HOST_TEMPLATE} > ${HOST_FILE}
 echo "Esperando que el puerto 22 de SSH este abierto."
 until nc -z $IP 22; do : sleep 0.2; done
 ssh-add ${KEY_FILE}
-sudo scp -i ${KEY_FILE} "~/.vault.txt" "${USERNAME}@${IP}:/home/${USERNAME}/"
+sudo scp -i ${KEY_FILE} "/root/.vault.txt" "${USERNAME}@${IP}:/home/${USERNAME}/"
 scp -i ${KEY_FILE} "servers/host/scripts/install_python.sh" "${USERNAME}@${IP}:/home/${USERNAME}/"
 python servers/host/scripts/install_python.py
