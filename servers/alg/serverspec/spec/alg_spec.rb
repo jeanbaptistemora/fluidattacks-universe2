9c9f2396909e18c8fee93cc39c96baa121555870
Jonathan Armas <jarmas@fluid.la>
2017-05-11 09:54:10 -0500

    Dockerspec

--- servers/alg/serverspec/spec/alg_spec.rb
+++ servers/alg/serverspec/spec/alg_spec.rb
@@ -1,43 +1,45 @@
 require 'spec_helper'
-require 'dockerspec/serverspec'
-require 'dockerspec/infrataster'
+
 
 describe docker_run('alg') do
-  describe command('ls /etc/apache2/sites-enabled') do
-    its(:stdout) { should eq "000-default.conf  default-ssl  fluid.la  www.fluid.la  www.fluidsignal.com\n"}
-  end
+  case os[:family]
+  when 'debian'
 
-  describe command("lsb_release -d") do
-    its(:stdout) { should contain /jessie/ }
-  end
+    describe command('ls /etc/apache2/sites-enabled') do
+      its(:stdout) { should eq "000-default.conf  default-ssl  fluid.la  www.fluid.la  www.fluidsignal.com\n"}
+    end
 
-  describe package('apache2') do
-    it { should be_installed }
-  end
+    describe command("lsb_release -d") do
+      its(:stdout) { should contain /jessie/ }
+    end
 
-  describe service('apache2') do
-    it { should be_enabled }
-  end
+    describe package('apache2') do
+      it { should be_installed }
+    end
 
-  describe user('root') do
-    it {should exist }
-  end
+    describe service('apache2') do
+      it { should be_enabled }
+    end
 
-  describe file ('/etc/apache2/sites-enabled/default-ssl') do
-    it {should contain 'ProxyPassReverse /blog'}
-    it {should contain 'ProxyPassReverse /sitemap.xml'}
-    it {should contain 'ProxyPassReverse /courses'}
-    it {should contain 'ProxyPassReverse /forms'}
-    it {should contain 'ProxyPassReverse /kb'}
-    it {should contain 'SSLProxyEngine on'}
-  end
+    describe user('root') do
+      it {should exist }
+    end
 
-  describe file('/var/www/html/index.html') do
-    it { should_not exist }
-  end
+    describe file ('/etc/apache2/sites-enabled/default-ssl') do
+      it {should contain 'ProxyPassReverse /blog'}
+      it {should contain 'ProxyPassReverse /sitemap.xml'}
+      it {should contain 'ProxyPassReverse /courses'}
+      it {should contain 'ProxyPassReverse /forms'}
+      it {should contain 'ProxyPassReverse /kb'}
+      it {should contain 'SSLProxyEngine on'}
+    end
 
-  describe file ('/etc/apache2/apache2.conf') do
-    it {should contain 'ServerTokens ProductOnly'}
-    it {should contain 'ServerSignature Off'}
-  end
+    describe file('/var/www/html/index.html') do
+      it { should_not exist }
+    end
+
+    describe file ('/etc/apache2/apache2.conf') do
+      it {should contain 'ServerTokens ProductOnly'}
+      it {should contain 'ServerSignature Off'}
+    end
 end
--- servers/alg/serverspec/spec/spec_helper.rb
+++ servers/alg/serverspec/spec/spec_helper.rb
@@ -1,4 +1,10 @@
 require 'serverspec'
+require 'net/ssh'
+require 'pathname'
+require 'bundler/setup'
+require 'serverspec_extended_types'
+require 'dockerspec/serverspec'
 
-set :backend, :exec
 
+set :env, :LANG => 'C', :LC_MESSAGES => 'C'
+set :path, '/sbin:/usr/local/sbin:$PATH'
