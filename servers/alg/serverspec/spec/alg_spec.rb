8bdb77afbf5f2a6583eac510d6be257d306fd5b7
Jonathan Armas <jarmas@fluid.la>
2017-05-11 10:44:30 -0500

    Added docker_build and boto3 script to update r53

--- servers/alg/serverspec/spec/alg_spec.rb
+++ servers/alg/serverspec/spec/alg_spec.rb
@@ -1,47 +1,48 @@
 require 'spec_helper'
 
+describe docker_build('.', tag: 'fluidsignal/fluidservesalg:latest') do
+  describe docker_run('alg') do
+    case os[:family]
+    when 'debian'
 
-describe docker_run('alg') do
-  case os[:family]
-  when 'debian'
+      describe command('ls /etc/apache2/sites-enabled') do
+        its(:stdout) { should eq "000-default.conf  default-ssl  fluid.la  www.fluid.la  www.fluidsignal.com\n"}
+      end
 
-    describe command('ls /etc/apache2/sites-enabled') do
-      its(:stdout) { should eq "000-default.conf  default-ssl  fluid.la  www.fluid.la  www.fluidsignal.com\n"}
+      describe command("lsb_release -d") do
+        its(:stdout) { should   contain /jessie/ }
+      end
+
+      describe package('apache2') do
+        it { should be_installed }
+      end
+
+      describe service('apache2') do
+        it { should be_enabled }
+      end
+
+      describe user('root') do
+        it {should exist }
+      end
+
+      describe file ('/etc/apache2/sites-enabled/default-ssl') do
+        it {should contain 'ProxyPassReverse /blog'}
+        it {should contain 'ProxyPassReverse /sitemap.xml'}
+        it {should contain 'ProxyPassReverse /courses'}
+        it {should contain 'ProxyPassReverse /forms'}
+        it {should contain 'ProxyPassReverse /kb'}
+        it {should contain 'SSLProxyEngine on'}
+      end
+
+      describe file('/var/www/html/index.html') do
+        it { should_not exist }
+      end
+
+      describe file ('/etc/apache2/apache2.conf') do
+        it {should contain 'ServerTokens ProductOnly'}
+        it {should contain 'ServerSignature Off'}
+      end
+
     end
-
-    describe command("lsb_release -d") do
-      its(:stdout) { should   contain /jessie/ }
-    end
-
-    describe package('apache2') do
-      it { should be_installed }
-    end
-
-    describe service('apache2') do
-      it { should be_enabled }
-    end
-
-    describe user('root') do
-      it {should exist }
-    end
-
-    describe file ('/etc/apache2/sites-enabled/default-ssl') do
-      it {should contain 'ProxyPassReverse /blog'}
-      it {should contain 'ProxyPassReverse /sitemap.xml'}
-      it {should contain 'ProxyPassReverse /courses'}
-      it {should contain 'ProxyPassReverse /forms'}
-      it {should contain 'ProxyPassReverse /kb'}
-      it {should contain 'SSLProxyEngine on'}
-    end
-
-    describe file('/var/www/html/index.html') do
-      it { should_not exist }
-    end
-
-    describe file ('/etc/apache2/apache2.conf') do
-      it {should contain 'ServerTokens ProductOnly'}
-      it {should contain 'ServerSignature Off'}
-    end
-
   end
 end
