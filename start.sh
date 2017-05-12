21d3bd41f4a81081adf98340aa8c9d5120af950e
Jonathan Armas <jarmas@fluid.la>
2017-05-12 18:01:05 -0500

    Previous version of start

--- start.sh
+++ start.sh
@@ -4,8 +4,12 @@
 
 SERVERS="alg exams integrates"
 
-# Crea los contenedores y construye los servers
+# Crea los contenedores dependiendo el OS
 for os in ${SERVERS}; do
     containers/${os}/build.sh
+done
+
+# Construye los servers
+for server in ${SERVERS}; do
     servers/${server}/scripts/start.sh
 done
