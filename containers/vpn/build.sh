427ad33c7adf55259973642023ffe100d262bf7f
jrestrepoatfluid <jrestrepo@fluid.la>
2017-07-06 11:53:13 -0500

    Add openvpn server

---
+++ containers/vpn/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# habilitar depuración
+if [ -n "$VERBOSE" ]; then
+  set -x
+fi
+
+# Salir inmediatamente si algun comando retorna diferente de cero.
+set -e
+
+SERVER="vpn"
+
+# Mensaje de inicio
+echo "---### [${SERVER}] Compilando contenedor."
+
+# construir la imagen
+cp ~/.vault.txt containers/${SERVER}/
+docker build -t 205810638802.dkr.ecr.us-east-1.amazonaws.com/serves:vpn containers/${SERVER}
+rm containers/${SERVER}/.vault.txt
