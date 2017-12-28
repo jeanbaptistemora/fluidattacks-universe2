1924362a1bec2f9ddb59671f49b12e97fd1be309
acuberosatfluid <acuberos@fluid.la>
2017-12-28 16:07:30 -0500

    Remove unused sharing template
    Former-commit-id: 87cff6864b7cdf3e2a243246b60d3c3e21289f27

--- pelicanconf.py
+++ pelicanconf.py
@@ -96,7 +96,3 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
          ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
