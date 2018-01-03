7166b9015b3931dcc5312a2034ef5efbe798f06a
acuberosatfluid <acuberos@fluid.la>
2018-01-03 16:27:45 -0500

    Update localtest.sh and create localtest_docker.sh
    Former-commit-id: f3b728c4fa96bdbff357cdb6dd5ed93923ef1a79

--- localtest_docker.sh
+++ localtest_docker.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -e
 
-echo "Activating Virtual Environment"
-source pelican/bin/activate
 echo "Deploying FLUID Website (local environment)"
-
 echo "Verifying content (1/5) . . ."
-cd ./content
+cd /web/content
 if egrep -r 'Fluid|Fluidsignal\ Group|fluidsignal'; then echo "El único nombre aceptado es FLUID"; exit 1; fi
 cd ..
 
@@ -15,7 +12,6 @@ rm -rf ./output
 
 echo "Generating build (3/5) . . ."
 sed -i 's/https:\/\/fluid.la/http:\/\/localhost:8000/' pelicanconf.py
-sed -i 's/\/app\///' pelicanconf.py
 pelican --fatal errors content/
 mv output/web/en/blog-en output/web/en/blog && mv output/web/es/blog-es output/web/es/blog
 
@@ -23,10 +19,8 @@ echo "Updating sitemap and setting redirect (4/5) . . ."
 ./xmlcombine.sh
 mv output/web/en/redirect/index.html output/web/ && rmdir output/web/en/redirect/
 git checkout -- pelicanconf.py
+chmod -R a+rwX {output/,pelicanconf.py,pelicanconf.pyc}
 
 echo "Starting local HTTP server on port 8000 (5/5) . . ."
 cd ./output
 python -m SimpleHTTPServer
-cd ..
-echo "Deactivating Virtual Environment"
-deactivate
