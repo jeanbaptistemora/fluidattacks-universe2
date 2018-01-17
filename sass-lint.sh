adfc10da2563467fd1e5bef63ec4bf471cfca343
acuberosatfluid <acuberos@fluid.la>
2018-01-17 10:32:58 -0500

    Fix Sass linter to include files with extensions .scss
    Former-commit-id: 19a7f967055678eea0105c7f4db64ba952f5cccd

--- sass-lint.sh
+++ sass-lint.sh
@@ -3,7 +3,7 @@ NC='\033[0m'
 
 ERRORS=0
 
-for FILE in $(find theme/2018 -iname '*.sass');do
+for FILE in $(find theme/2018 -type f \( -name '*.sass' -or -name '*.scss' \));do
   echo -e ${GC}$FILE${NC};
   if sass-lint.js -q -v --max-warnings 0 $FILE;then
     ERRORS=1;
