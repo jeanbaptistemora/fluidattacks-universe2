2bbfd23298589906d9ece136083ddad95419f893
acuberosatflluid <acuberos@fluid.la>
2018-01-23 09:10:39 -0500

    Fix KB articles
    Former-commit-id: 04551acdd618fbb0c84ffffae3a1ee725f35fc50

--- local-test.sh
+++ local-test.sh
@@ -17,8 +17,8 @@ mv output/web/en/blog-en output/web/en/blog && mv output/web/es/blog-es output/w
 
 echo "Updating sitemap, setting redirect and pages images (4/5) . . ."
 ./xmlcombine.sh
-mv output/web/es/pages-es/* output/web/es/ && rm -r output/web/es/pages-es
-mv output/web/en/pages-en/* output/web/en/ && rm -r output/web/en/pages-en
+cp -r output/web/es/pages-es/* output/web/es/ && rm -rf output/web/es/pages-es
+cp -r output/web/en/pages-en/* output/web/en/ && rm -rf output/web/en/pages-en
 mv output/web/en/redirect/index.html output/web/ && rmdir output/web/en/redirect/
 # Undo changes made to the file and change permissions of the files used by the container as root
 git checkout -- pelicanconf.py
