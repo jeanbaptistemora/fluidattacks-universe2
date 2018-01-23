fbace3c5c9e8b328460bd7bf6a725c564a181423
acuberosatflluid <acuberos@fluid.la>
2018-01-23 17:08:32 -0500

    Patch anchors for Google Chrome
    Former-commit-id: 8ebc62f845150477158bf7aebaa03d2efaf892a1

--- theme/2014/static/js/id.js
+++ theme/2014/static/js/id.js
@@ -25,3 +25,14 @@
 (function($) {
   $(window).on('resize', $(document).idtag);
 })(jQuery);
+
+$(document).ready(function () {
+        var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+        if (window.location.hash && isChrome) {
+            setTimeout(function () {
+                var hash = window.location.hash;
+                window.location.hash = "";
+                window.location.hash = hash;
+            }, 300);
+        }
+    });
