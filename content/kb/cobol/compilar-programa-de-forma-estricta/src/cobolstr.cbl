269b4deea83da4ac9091b6567b715e9f6f58a99d
Luis Stiven Arteaga Solano <lsarteaga@unicauca.edu.co>
2018-02-05 08:45:01 -0500

    fix: kb/cobol/compilar-programa-de-forma-estricta
    Former-commit-id: 9a4c5683e379a3a223206e710b09a82abca16197

---
+++ content/kb/cobol/compilar-programa-de-forma-estricta/src/cobolstr.cbl
@@ -0,0 +1,8 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. COBOLSTR.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+       MAIN.
+           DISPLAY "Programa estricto".
+           STOP RUN.
