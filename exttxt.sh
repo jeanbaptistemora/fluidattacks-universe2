82adc3ce465e7a2f6d8a65bed48adf8750c09f3a
Edprado4 <34edprado@gmail.com>
2018-02-06 08:31:40 -0500

    Updated exttxt script
    Excluded links, anchors and cites from word counting.
    
    
    Former-commit-id: b0294d0a07ed2684c07d21052e913afb0da7225a

--- exttxt.sh
+++ exttxt.sh
@@ -2,4 +2,4 @@
 
 : "${1?"Extract text from asciidoc file. Usage: $0 file"}"
 
-(pcregrep -M -v '^\[source.*\n^----((.|\n)*?)^----' | pcregrep -v '^[+:]') < "$1"
+(pcregrep -M -v '^\[source.*\n^----((.|\n)*?)^----' | sed -e 's/link:.*\[//g'| sed -e 's/<<.*>>//g' | sed -e 's/\[\[.*\]]//g' | pcregrep -v '^[+:]') < "$1"
