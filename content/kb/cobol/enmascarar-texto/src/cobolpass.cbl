4216864cdfc4e667c9e117528de5ecbc9a2c55a8
Luis Stiven Arteaga Solano <lsarteaga@unicauca.edu.co>
2018-02-06 07:53:14 -0500

    fix: kb/cobol/enmascarar-texto
    Former-commit-id: 0395821e6eb13362aa84ff3e10bd1e22aa722fe1

--- content/kb/cobol/enmascarar-texto/src/cobolpass.cbl
+++ content/kb/cobol/enmascarar-texto/src/cobolpass.cbl
@@ -1,18 +1,12 @@
        IDENTIFICATION DIVISION.
-      ******************
-      * Identification *
-      ******************
+
        PROGRAM-ID. COBOLPASS.
-      ********
-      * Data *
-      ********
+
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        01 W01-USERNAME PIC X(20).
        01 W02-PASSWORD PIC X(20).
-      ********
-      * Main *
-      ********
+
        PROCEDURE DIVISION.
        MAIN.
            DISPLAY "Username: ".
