1d24eb2c415d1ad6e9db7da797f7539a7813b1ed
Luis Stiven Arteaga Solano <lsarteaga@unicauca.edu.co>
2018-02-06 14:39:30 -0500

    fix: kb/cobol/generar-llaves
    Former-commit-id: 5842786567431f057e5193bb09c458ed5d00b1ce

--- content/kb/cobol/generar-llaves/src/cobolrnds.cbl
+++ content/kb/cobol/generar-llaves/src/cobolrnds.cbl
@@ -1,30 +1,21 @@
        PROCESS NOMONOPRC.
        IDENTIFICATION DIVISION.
-      ******************
-      * Identification *
-      ******************
        PROGRAM-ID. COBOLRNDS.
-      ***************
-      * Environment *
-      ***************
+
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
          SOURCE-COMPUTER. IBM-ISERIES.
          OBJECT-COMPUTER. IBM-ISERIES.
          SPECIAL-NAMES.
          LINKAGE TYPE PROCEDURE FOR "Qc3GenPRNs".
-      ********
-      * Data *
-      ********
+
        DATA DIVISION.
 
        WORKING-STORAGE SECTION.
        COPY QC3CCI OF QSYSINC-QCBLLESRC.
        COPY QUSEC OF QSYSINC-QCBLLESRC.
        01 WS-RAND-BYTES PIC X(32).
-      ********
-      * Main *
-      ********
+
        PROCEDURE DIVISION.
        MAIN.
            CALL "Qc3GenPRNs" USING
