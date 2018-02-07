e29699c42749dd1a767e4a03f4b32ea6eb177393
acuberosatfluid <acuberos@fluid.la>
2018-02-07 16:30:44 -0500

    Fix issue #331
    Former-commit-id: 135d9e6347c6f38180bff2253d1145c0b7e41678

---
+++ theme/2014/static/js/linenum.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  var lines = document.getElementsByClassName("linenodiv");
+  if (lines.length) {
+    for (i = 1; i < lines.length; i++) {
+      var linenum = lines[i].innerText.split("\n").length - 1;
+      var newlinenum = "";
+      for (j = 1; j <= linenum; j++) {
+        var num = parseInt(lines[i-1].innerText.split("\n")[lines[i-1].innerText.split("\n").length - 2]) + j;
+        newlinenum = newlinenum + num.toString() + "\n";
+      }
+      lines[i].innerHTML = "<pre>" + newlinenum + "</pre>";
+    }
+  }
+});
