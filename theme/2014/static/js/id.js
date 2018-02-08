28727ea43ee0ce4bddf20ef2bb5aec6e6b2035e9
acuberosatfluid <acuberos@fluid.la>
2018-02-08 11:50:38 -0500

    Fix issue #243
    Former-commit-id: 77e16af15d964d52dfde6286714d24dcd40c7540

--- theme/2014/static/js/id.js
+++ theme/2014/static/js/id.js
@@ -4,15 +4,19 @@
       var titles = $(document).find("h"+ i +":visible");
       for (j = 0; j < titles.length; j++) {
         if (!($(titles[j]).children("span").length)) {
-          var str = titles[j].innerText;
           var replaceChars={ "á":"a" ,"é":"e", "í":"i", "ó":"o", "ú":"u", "ñ":"n" };
+          var str = titles[j].innerText.toLowerCase();
           str = str.replace(/^[0-9].*\.\s+/, '');
           str = str.replace(/[¡!¿?,':\.]/g, '');
-          str = str.replace(/\s+/g, '-').toLowerCase();
+          str = str.replace(/\s+/g, '-');
           str = str.replace(/[áéíóúñ]/g, function(match) {return replaceChars[match];});
           $(titles[j]).prepend('<a href="#'+ str +'" class="anchor-sign"></a>');
           $(titles[j]).prepend('<span id="'+ str +'" class="anchor"></span>');
           $(titles[j]).removeAttr("id");
+          if ( titles.length ) {
+            str = titles[j].textContent;
+            titles[j].childNodes[2].data = str.slice(0,str.toLowerCase().search(/[a-z]/)) + str[str.toLowerCase().search(/[a-z]/)].toUpperCase() + str.slice(str.toLowerCase().search(/[a-z]/) + 1); 
+          }
         }
       }
     }
