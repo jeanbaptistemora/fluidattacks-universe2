481f2ede0514095028aa948d01dc81a57bdc211a
acuberosatfluid <acuberos@fluid.la>
2018-02-12 16:09:21 -0500

    Add support for responsiveness in vertically distributed tables
    Former-commit-id: 2c4d48d428a0f6a374e54512bb0687b92b9f80e8

--- theme/2014/static/js/table-responsive.js
+++ theme/2014/static/js/table-responsive.js
@@ -1,25 +1,65 @@
-(function ($) {
-  $.fn.responsive = function () {
-    table = $(document).find(".tb-row");
-    $(table).each(function () {
-      if ($(this)[0].rows[0].cells[0].offsetWidth < 100) {
-        if (!$(this).hasClass("tb-row-responsive")) {
-          $(this).toggleClass("tb-row-responsive");         
-        }
+(function($) {
+  $.fn.transpose = function() {
+    var diff = $(this)[0].rows[0].cells.length - $(this)[0].rows.length;
+    if (diff > 0) {
+      for (i = 0; i < diff; i++) {
+        this[0].insertRow(-1);
       }
-      else if ($(this)[0].rows[0].cells[0].offsetWidth > 100 * $(this)[0].rows[0].cells.length) {
-        if ($(this).hasClass("tb-row-responsive")) {
-          $(this).toggleClass("tb-row-responsive");          
-        }
+    }
+    var newrows = [];
+    for (j = 0; j < $(this)[0].rows[0].cells.length; j++) {
+      newrows[j] = "";
+      for (z = 0 ; z < $(this)[0].rows.length; z++) {
+        newrows[j] += $(this)[0].rows[z].innerHTML.split("\n")[j + 1] + "\n";
       }
-    });
+    }
+    for (i = 0; i < $(this)[0].rows.length; i++) {
+      $(this)[0].rows[i].innerHTML = "\n" + newrows[i] + "\n";
+    }
+    if (diff < 0) {
+      for (i = 0; i < Math.abs(diff); i++) {
+        this[0].deleteRow(-1);
+      }
+    }
+    else if (diff > 0) {
+      for (i = 0; i < $(this)[0].rows.length; i++) {
+        this[0].rows[i].innerHTML = this[0].rows[i].innerHTML.replace(/undefined\n/g, '');
+      }
+    }
   };
 })(jQuery);
 
-(function ($) {
-  $(window).on('resize', $(document).responsive);
+function responsive(tableclass, minsize){
+  table = $(document).find(tableclass);
+  $(table).each(function () {
+    if ($(this)[0].rows[0].cells[0].offsetWidth < minsize && !$(this).hasClass("tb-responsive")) {
+      if (tableclass == ".tb-col") {
+        $(this).transpose();
+      }
+      $(this).toggleClass("tb-responsive");
+    }
+    else {
+      if (tableclass == ".tb-col") {
+        if ($(this)[0].rows[0].cells[0].offsetWidth > minsize * $(this)[0].rows.length && $(this).hasClass("tb-responsive")) {
+          $(this).transpose();
+          $(this).toggleClass("tb-responsive");
+        }
+      }
+      else if ($(this)[0].rows[0].cells[0].offsetWidth > minsize * $(this)[0].rows[0].cells.length && $(this).hasClass("tb-responsive")) {
+        $(this).toggleClass("tb-responsive");
+      }
+    }
+  });
+}
+
+(function($) {
+  $(window).on('resize', function(){
+    responsive(".tb-row", 100);
+    responsive(".tb-col", 100);
+  });
 })(jQuery);
 
-$(document).ready(function (){
-  $(this).responsive();
+$(document).ready(function() {
+  responsive(".tb-row", 100);
+  responsive(".tb-col", 100);  
 });
