b2a1f6ea86fcc5fb4ffbb2a5da63a5b663eda9e0
jvillaatfluid <jvilla@fluid.la>
2018-02-19 11:40:25 -0500

    fix: /kb/java/comprobar-contrasena-diccionario/
    Former-commit-id: 625c94426406ada297aad4aaaf4c8d0efa885b77

---
+++ content/kb/java/comprobar-contrasena-diccionario/src/dictionary.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class Dictionary {
+    public static void main(String[] args) {
+        try {
+            String diccionario = "C:\\passwords.txt";
+            System.out.println(isPasswordInDictionary(diccionario, "123"));
+            System.out.println(isPasswordInDictionary(diccionario, "hello"));
+            System.out.println(isPasswordInDictionary(diccionario, "admin"));
+            System.out.println(isPasswordInDictionary(diccionario, "SystEM"));
+            System.out.println(isPasswordInDictionary(diccionario, "My-Str0n6_P4assw0rd!"));
+            
+        } catch (FileNotFoundException e) {
+            System.err.println("Diccionario no encontrado");
+        } catch (IOException e) {
+            System.err.println("Error de entrada/salida de datos");
+        }
+    }
+    
+    public static boolean isPasswordInDictionary(String dictionaryFilePath,
+                                                        String password)
+                                                        throws FileNotFoundException, IOException {
+        
+        BufferedReader dictionary = new BufferedReader(new FileReader(dictionaryFilePath));
+        
+        String word = "";
+        
+        while ((word = dictionary.readLine()) != null) {
+            if (password.equalsIgnoreCase(word)) {
+                return true;
+            }
+        }
+        
+        return false;        
+    }
+}
