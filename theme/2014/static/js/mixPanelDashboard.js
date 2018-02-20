a787d4c4d0c92c15536f6c83db48b1851ef8693f
acuberosatfluid <acuberos@fluid.la>
2018-02-20 18:54:37 -0500

    Add Mixpanel with "Page View" event
    Former-commit-id: ff7d29a2365e271ab2a7af78c2dc46c875956329

---
+++ theme/2014/static/js/mixPanelDashboard.js
@@ -0,0 +1,31 @@
+/**
+ * @file mixPanelDashboard.js
+ * @author engineering@fluid.la
+ */
+/*
+ * Object to control mixpanel calls
+ */
+mixPanelDashboard = {}; 
+/*
+ * MixPanel localhost Fixer
+ */
+mixPanelDashboard.isProduction = function(){
+    result = false;
+    try{
+        result = location.toString().indexOf("localhost:8000") != -1;
+    }catch(e){
+        result = false;
+    }finally{
+        return result;
+    }
+};
+
+mixPanelDashboard.trackPageView = function(trackName, pageName, pageUrl){
+    if(mixPanelDashboard.isProduction()) return false;
+    mixpanel.track(
+        trackName, {
+            "Page": pageName,
+            "URL": pageUrl
+        }
+    );
+};
