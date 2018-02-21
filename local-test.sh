79b1a1b9a1706f61888a59c450155bdcbb391310
acuberosatfluid <acuberos@fluid.la>
2018-02-21 16:05:54 -0500

    Fix robots and sitemap location
    Former-commit-id: c098711c82bd4d4c7d0d472b58ebedc573d5d462

--- local-test.sh
+++ local-test.sh
@@ -27,7 +27,7 @@ echo "Updating sitemap, setting redirect and pages images (4/5) . . ."
 cp -r output/web/es/pages-es*/* output/web/es/ && rm -rf output/web/es/pages-es*
 cp -r output/web/en/pages-en*/* output/web/en/ && rm -rf output/web/en/pages-en*
 mv output/web/en/redirect/index.html output/web/ && rmdir output/web/en/redirect/
-cp robots.txt output/
+cp robots.txt output/web/
 # Undo changes made to the file and change permissions of the files used by the container as root
 git checkout -- pelicanconf.py pelicanconf-2018.py
 chmod -R a+rwX {output/,pelicanconf.py,pelicanconf-2018.py}
