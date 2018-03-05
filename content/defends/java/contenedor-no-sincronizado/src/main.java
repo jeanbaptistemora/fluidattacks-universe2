9d3de8006334342365dd9f4e6e26337ba3b4335e
jvillaatfluid <jvilla@fluid.la>
2018-03-05 11:55:28 -0500

    migrated: /defends/java/contenedor-no-sincronizado/
    Former-commit-id: 54dd29967f7e8eb7b01f1295f7581b265aca7467

---
+++ content/defends/java/contenedor-no-sincronizado/src/main.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+  public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("FLUID");
+    sb.append("SIGNAL");
+    System.out.println(sb);
+    List<String> lista = new ArrayList<String>();
+    lista.add("FLUID");
+    lista.add("SIGNAL");
+    System.out.println(lista);
+  }
+}
