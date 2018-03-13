b69bcc43c1bd915fc0277d94ba14524e897b0ec6
Edprado4 <34edprado@gmail.com>
2018-03-13 15:57:47 -0500

    Fixes
    - Removed non-printable caracters from article defends/csharp/aplicar-seguridad-codigo
    - Increased scope of category linter to defends articles
    
    
    Former-commit-id: 883269cccb2e1bd8417acbcb5406eb1aec317c10

--- check-articles.sh
+++ check-articles.sh
@@ -4,9 +4,9 @@ GC='\033[0;32m'
 NC='\033[0m'
 ERRORS=0
 
-for FILE in $(find content/blog-es content/blog-en -iname '*.adoc'); do
+for FILE in $(find content/blog-es content/blog-en content/defends -iname '*.adoc'); do
 
-# Check that every article in blogs has a valid category	
+# Check that every article in blogs and defends has a valid category	
   ARTCAT=$(cat $FILE | pcregrep --color -o '(?<=^:category:).*');
   if ! cat categories.lst categorias.lst | pcregrep -q $ARTCAT ; then
  	echo -e "${RD} $ARTCAT";
