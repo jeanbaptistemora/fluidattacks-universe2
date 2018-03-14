c7f384f433a9f94fc2a13fa6fe12af4a592809e5
Edprado4 <34edprado@gmail.com>
2018-03-14 06:31:28 -0500

    Fix issue #683
    - Created tags/etiquetas list with current valid tags for blog articles.
    - Added tags validation feature to check-articles linter
    - Updated some articles tags to match the current list.
    
    
    Former-commit-id: 6c85876e95b0057344b7cc5eefea7c1a7892a29b

--- check-articles.sh
+++ check-articles.sh
@@ -5,7 +5,7 @@ NC='\033[0m'
 ERRORS=0
 
 for FILE in $(find content/blog-es content/blog-en content/defends -iname '*.adoc'); do
-
+  
 # Check that every article in blogs and defends has a valid category	
   ARTCAT=$(cat $FILE | pcregrep --color -o '(?<=^:category:).*');
   if ! cat categories.lst categorias.lst | pcregrep -q $ARTCAT ; then
@@ -13,6 +13,19 @@ for FILE in $(find content/blog-es content/blog-en content/defends -iname '*.ado
 	echo -e "${GC} The article does not match any valid category. Please correct the file \"$FILE\"${NC}";
 	ERRORS=1;
   fi
+
+# Check that every article in blogs has valid tags
+  if ! pcregrep -q ':defends:' $FILE ; then 
+  	ARTTAGS=$(cat $FILE | pcregrep -o '(?<=^:tags:).*'| tr , \\n)
+  	for TAG in $( echo $ARTTAGS ); do
+  	  if ! cat etiquetas.lst tags.lst | pcregrep -q $TAG ; then
+  	  	echo -e "${RD} $TAG";
+  		echo -e "${GC} The previous tag is not valid. Please correct the file \"$FILE\" or add the new tag in the list. ${NC}";
+	    ERRORS=1;  	
+  	  fi
+  	done
+  fi 
+
 done
 
 exit $ERRORS
