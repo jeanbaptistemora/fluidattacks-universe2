a0f205e9d267a97799ca8795ef49da92cacc9cf2
acuberosatfluid <acuberos@fluid.la>
2018-03-15 10:35:22 -0500

    Change permissions of uploaded files in S3
    Former-commit-id: 7be5891c4339d5cf1a72022fc40197410fd06efc

--- amz-redirect.sh
+++ amz-redirect.sh
@@ -5,7 +5,7 @@ for FILE in $(find output -iname '*.html'); do
 	STRING=${FILE/$PATTERN/}
 	NAME=${STRING/$PATTERN2/}
 	if [[ ! $NAME = *".html" ]]; then
-		aws s3api put-object --acl public-read-write \
+		aws s3api put-object --acl public-read \
 		--bucket $S3_BUCKET_NAME --key $NAME --content-type text/html \
 		--website-redirect-location "/$NAME/";
 	fi;	
