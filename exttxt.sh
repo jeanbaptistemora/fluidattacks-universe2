7dc10c60ce057d5d872363e9affc08580e3acd7e
Edprado4 <34edprado@gmail.com>
2018-03-15 08:09:47 -0500

    Added feature to exttxt script
    Added new feature to ignore raw text input between hypens (-) in text extraction script
    
    
    Former-commit-id: 4bf5c5b77b7cfdd32b54b0dc948c5ebfbc74b722

--- exttxt.sh
+++ exttxt.sh
@@ -2,4 +2,4 @@
 
 : "${1?"Extract text from asciidoc file. Usage: $0 file"}"
 
-(pcregrep -M -v -e '^\[(source|"graphviz"|"plantuml").*\n^----((.|\n)*?)^----' | sed -e 's/link:.*\[//g'| sed -e 's/<<.*>>//g' | sed -e 's/\[\[.*\]]//g' | pcregrep -v '^[+:]') < "$1"
+(pcregrep -M -v -e '^\[(source|"graphviz"|"plantuml").*\n^----((.|\n)*?)^----' | pcregrep -M -v -e '^----((.|\n)*?)^----' | sed -e 's/link:.*\[//g'| sed -e 's/<<.*>>//g' | sed -e 's/\[\[.*\]]//g' | pcregrep -v '^[+:]') < "$1"
