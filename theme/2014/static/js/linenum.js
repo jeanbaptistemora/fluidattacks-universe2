d0144b908c0ce9ecd03e1a4da3fb3c1eb0d227d3
acuberosatfluid <acuberos@fluid.la>
2018-04-02 10:51:52 -0500

    Fix issue #650
    Former-commit-id: 27ac228b9ce4911176c8e77170f93d0ce6d5e0db

--- theme/2014/static/js/linenum.js
+++ theme/2014/static/js/linenum.js
@@ -8,17 +8,20 @@ $(document).ready(function () {
   var lines = document.getElementsByClassName("linenodiv");
   if (lines.length) {
     for (i = 1; i < lines.length; i++) {
-      var linenum = lines[i].innerHTML.split('\n').length;
-      var newlinenum = "";
-      var lastnum = parseInt(lines[i-1].innerHTML.split('\n')[lines[i-1].innerHTML.split('\n').length - 1].replace(/\D/g, ''));
-      for (j = 1; j <= linenum; j++) {
-        var num = lastnum + j;
-        newlinenum += num.toString();
-        if (j != linenum) {
-          newlinenum += '\n';
+      if ($(lines[i]).parents(".content").prev().length) {}
+      else {
+        var linenum = lines[i].innerHTML.split('\n').length;
+        var newlinenum = "";
+        var lastnum = parseInt(lines[i-1].innerHTML.split('\n')[lines[i-1].innerHTML.split('\n').length - 1].replace(/\D/g, ''));
+        for (j = 1; j <= linenum; j++) {
+          var num = lastnum + j;
+          newlinenum += num.toString();
+          if (j != linenum) {
+            newlinenum += '\n';
+          }
         }
+        lines[i].innerHTML = "<pre>" + newlinenum + "</pre>";
       }
-      lines[i].innerHTML = "<pre>" + newlinenum + "</pre>";
     }
   }
 });
