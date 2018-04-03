ff4ff032fe6e4edc5d889bbf10296c298d42b95c
acuberosatfluid <acuberos@fluid.la>
2018-04-03 16:06:26 -0500

    Add line to remove titles in exttxt.sh and erase unused extsrc.sh
    Former-commit-id: 3d075d71a8fdc67c20ec148398123bddb8ff862b

--- exttxt.sh
+++ exttxt.sh
@@ -6,4 +6,11 @@
 
 : "${1?"Extract text from asciidoc file. Usage: $0 file"}"
 
-(pcregrep -M -v -e '^\[(source|"graphviz"|"plantuml").*\n^----((.|\n)*?)^----' | pcregrep -M -v -e '^----((.|\n)*?)^----' | sed -e 's/link:.*\[//g'| sed -e 's/<<.*>>//g' | sed -e 's/\[\[.*\]]//g' | pcregrep -v '^[+:]') < "$1"
+(pcregrep -M -v -e '^\[(source|"graphviz"|"plantuml").*\n^----((.|\n)*?)^----' \
+  -e '^----((.|\n)*?)^----' \
+  | pcregrep -v -e '^\.[a-zA-Z0-9].*' \
+  | sed -e 's/link:.*\[//g' \
+    -e 's/<<.*>>//g' \
+    -e 's/\[\[.*\]]//g' \
+    -e 's/image:*.*\[.*\]//g' \
+  | pcregrep -v '^[+:]') < "$1"
