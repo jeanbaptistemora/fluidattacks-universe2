57fa8329a03a49fa98d0c5c439bad643a9e50489
acuberosatfluid <acuberos@fluid.la>
2018-04-04 20:29:30 -0500

    Fix: Wrapping in all Defends pages
    Former-commit-id: 1291cfe4fbb98d5da853580af2e5af1f48ea0be8

--- content/defends/cobol/registrar-excepciones-ile/exttxt.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-: "${1?"Extract text from asciidoc file. Usage: $0 file"}"
-
-(pcregrep -M -v '^\[source.*\n^----((.|\n)*?)^----' | sed -e 's/link:.*\[//g'| sed -e 's/<<.*>>//g' | sed -e 's/\[\[.*\]]//g' | pcregrep -v '^[+:]') < "$1"
