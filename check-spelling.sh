86721b4fdd7c12a4ebf1a35445f623566c76ae16
Edprado4 <34edprado@gmail.com>
2018-04-25 12:22:36 -0500

    Fix issue #640
    Created linter for strict spelling checking in certain words.
    
    
    Former-commit-id: 77450e1734417454f21bad2d87f3dc0b0bfb3b72

---
+++ check-spelling.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# This script set a strict spelling for certain words and validates
+# the correct spelling  in all content folders.
+# To add new rules simply add the correct spelling
+# in the file strict-words.lst
+
+GC='\033[0;32m'
+NC='\033[0m'
+ERRORS=0
+
+while IFS= read -r CORRECT; do
+  while IFS= read -r FILE; do
+    # Check if strict spelling has 2 words
+    if [[ "$CORRECT" =~ \   ]]; then
+      SRCH=${CORRECT// /(\\s)?}
+    else
+      SRCH=$CORRECT
+    fi
+
+    if  ./exttxt.sh "$FILE" | pcregrep -ioqM "(\\s|^)[\\*|\\+|\\(]?$SRCH[\\*\\+\\)]?[\\.\\:\\;\\,]?(\\s|\\n|\\])"; then
+
+      CURRENT=$(./exttxt.sh "$FILE" | pcregrep -ioM "(\\s|^)[\\*|\\+|\\(]?$SRCH[\\*\\+\\)]?[\\.\\:\\;\\,]?(\\s|\\n|\\])" | pcregrep -ioM "$SRCH" )
+
+      while IFS=$'\n' read -r WORD; do
+        if  [[ "$WORD" != "$CORRECT" ]]; then
+          VALIDATE=$(./exttxt.sh "$FILE" | pcregrep "$WORD")
+            if [[ "$VALIDATE" =~ $WORD ]]; then
+              echo -e "$VALIDATE" | grep -E --color "$WORD"
+              echo -e "${GC}The spelling $WORD is invalid, the only admitted spelling is $CORRECT. Please correct the file \"$FILE\"${NC}"
+              ERRORS=1;
+            fi
+        fi
+      done < <(echo -e "$CURRENT\\n"|grep -v "^$")
+    fi
+
+  done < <(find content -iname '*.adoc')
+done < strict-words.lst
+
+exit $ERRORS
