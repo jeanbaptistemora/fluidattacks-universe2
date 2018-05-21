e1ac057c6de0ad9fae41907bb2d8de3a9f74c7d9
Andres Cuberos <acuberos@fluidattacks.com>
2018-05-21 17:01:29 -0500

    Fix issue #69
    Former-commit-id: af40b5b3c83e94ea8c58b37f8adf4c6708c8e116

--- infrastructure/iam/roles/cloudwatchagent.tf
+++ infrastructure/iam/roles/cloudwatchagent.tf
@@ -24,10 +24,27 @@ data "aws_iam_policy_document" "fs-cloudwatchagent-doc" {
   }
 }
 
+data "aws_iam_policy_document" "fs-cloudwatchagent-assume-role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_iam_role" "fs-cloudwatchagent" {
   name        = "FS_CloudWatch_Agent"
   path        = "/"
   description = "Role to enable custom metrics in EC2 instances"
   
-  assume_role_policy = "${data.aws_iam_policy_document.fs-cloudwatchagent-doc.json}"
+  assume_role_policy = "${data.aws_iam_policy_document.fs-cloudwatchagent-assume-role.json}"
+}
+
+resource "aws_iam_role_policy" "fs-cloudwatchagent-policy" {
+  name   = "FS_CloudWatch_Agent_Policy"
+  policy = "${data.aws_iam_policy_document.fs-cloudwatchagent-doc.json}"
+  role   = "${aws_iam_role.fs-cloudwatchagent.name}"
 }
