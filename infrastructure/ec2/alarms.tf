1dd9e44ee87155344fa8a1cd26f1b7ab2794178e
Andres Cuberos <acuberos@fluidattacks.com>
2018-05-21 20:00:34 -0500

    Fix CPU alarm definition
    Former-commit-id: df432a03cbbdc6463141c96851390b87452e941a

--- infrastructure/ec2/alarms.tf
+++ infrastructure/ec2/alarms.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_metric_alarm" "fs-cpu-usage" {
   alarm_name                = "FS_CPU_USAGE"
   comparison_operator       = "GreaterThanThreshold"
-  evaluation_periods        = "2"
+  evaluation_periods        = "3"
   metric_name               = "CPUUtilization"
   namespace                 = "AWS/EC2"
   period                    = "120"
@@ -18,14 +18,14 @@ resource "aws_cloudwatch_metric_alarm" "fs-cpu-usage" {
 resource "aws_cloudwatch_metric_alarm" "fs-disk-usage" {
   alarm_name                = "FS_DISK_USAGE"
   comparison_operator       = "GreaterThanThreshold"
-  evaluation_periods        = "2"
+  evaluation_periods        = "3"
   metric_name               = "disk_used_percent"
   namespace                 = "CWAgent"
   period                    = "120"
   statistic                 = "Average"
   threshold                 = "80"
   alarm_description         = "This metric monitors ec2 disk utilization"
-  datapoints_to_alarm       = "2"
+  datapoints_to_alarm       = "3"
   treat_missing_data        = "notBreaching"
   dimensions {
     InstanceId = "${aws_instance.fluidserves.id}"
