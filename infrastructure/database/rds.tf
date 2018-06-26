20a3b3f317ebc144ed3ef625b320283f854c8b26
Andres Cuberos <acuberos@fluidattacks.com>
2018-06-26 13:21:07 -0500

    Define backup_retention_period for RDS
    Former-commit-id: ee67a4c892ceb2bf35652aec87b622f5e667c53e

--- infrastructure/database/rds.tf
+++ infrastructure/database/rds.tf
@@ -40,6 +40,7 @@ resource "aws_db_instance" "fluiddb" {
   skip_final_snapshot  = true
   publicly_accessible = true
   maintenance_window = "sun:05:00-sun:05:30"
+  backup_retention_period = 7
   backup_window = "06:00-06:30"
   lifecycle {
     ignore_changes = ["snapshot_identifier"]
