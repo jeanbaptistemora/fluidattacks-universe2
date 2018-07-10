74d978ba36d68a52eb26aa38d67baacf789d49ef
Andres Cuberos <acuberos@fluidattacks.com>
2018-07-10 13:31:57 -0500

    Automatically add nodes to cluster and setup Helm-Tiller
    Former-commit-id: 89929e3bfe6d6468ced802a747210812820eccd4

---
+++ infrastructure/eks/k8s-conf.tf
@@ -0,0 +1,92 @@
+locals {
+  kubeconfig = <<KUBECONFIG
+
+
+apiVersion: v1
+clusters:
+- cluster:
+    server: ${aws_eks_cluster.k8s_cluster.endpoint}
+    certificate-authority-data: ${aws_eks_cluster.k8s_cluster.certificate_authority.0.data}
+  name: kubernetes
+contexts:
+- context:
+    cluster: kubernetes
+    user: aws
+  name: aws
+current-context: aws
+kind: Config
+preferences: {}
+users:
+- name: aws
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1alpha1
+      command: heptio-authenticator-aws
+      args:
+        - "token"
+        - "-i"
+        - "${var.cluster_name}"
+KUBECONFIG
+}
+
+resource "null_resource" "k8s_config" {
+  provisioner "local-exec" {
+    command = "echo \"${local.kubeconfig}\" > ~/.kube/config"
+  }
+
+  depends_on = ["aws_eks_cluster.k8s_cluster"]
+}
+
+provider "kubernetes" {
+  host                   = "${aws_eks_cluster.k8s_cluster.endpoint}"
+  cluster_ca_certificate = "${base64decode(aws_eks_cluster.k8s_cluster.certificate_authority.0.data)}"
+  token                  = "${data.external.aws_auth.result.token}"
+  load_config_file       = false
+}
+
+data "external" "aws_auth" {
+  program = ["bash", "${path.module}/aws-auth.sh"]
+
+  query {
+    cluster_name = "${var.cluster_name}"
+  }
+}
+
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data {
+    mapRoles = <<YAML
+- rolearn: ${aws_iam_role.k8s_nodes_role.arn}
+  username: system:node:{{EC2PrivateDNSName}}
+  groups:
+    - system:bootstrappers
+    - system:nodes
+YAML
+  }
+  depends_on = ["null_resource.k8s_config"]
+
+  provisioner "local-exec" {
+    command = "sleep 60"
+  }
+}
+
+resource "kubernetes_service_account" "helm" {
+  metadata {
+    name      = "tiller"
+    namespace = "kube-system"
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl create clusterrolebinding tiller-rule --clusterrole=cluster-admin --serviceaccount=${kubernetes_service_account.helm.metadata.0.namespace}:${kubernetes_service_account.helm.metadata.0.name}"
+  }
+
+  provisioner "local-exec" {
+    command = "helm init --upgrade --service-account ${kubernetes_service_account.helm.metadata.0.name} --wait"
+  }
+
+  depends_on = ["kubernetes_config_map.aws_auth"]
+}
