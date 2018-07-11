ae2edafcdbb4004bc35ec38db57c9b20b9c6fe2b
Andres Cuberos <acuberos@fluidattacks.com>
2018-07-11 10:14:05 -0500

    Add corrections to Bash scripts
    Former-commit-id: 619b8e2f2b94522c87ad92d26a1d254b749f89b9

--- infrastructure/eks/k8s-conf.tf
+++ infrastructure/eks/k8s-conf.tf
@@ -37,19 +37,19 @@ resource "null_resource" "k8s_config" {
   depends_on = ["aws_eks_cluster.k8s_cluster"]
 }
 
-provider "kubernetes" {
-  host                   = "${aws_eks_cluster.k8s_cluster.endpoint}"
-  cluster_ca_certificate = "${base64decode(aws_eks_cluster.k8s_cluster.certificate_authority.0.data)}"
-  token                  = "${data.external.aws_auth.result.token}"
-  load_config_file       = false
-}
-
 data "external" "aws_auth" {
   program = ["bash", "${path.module}/aws-auth.sh"]
 
   query {
     cluster_name = "${var.cluster_name}"
   }
+}
+
+provider "kubernetes" {
+  host                   = "${aws_eks_cluster.k8s_cluster.endpoint}"
+  cluster_ca_certificate = "${base64decode(aws_eks_cluster.k8s_cluster.certificate_authority.0.data)}"
+  token                  = "${data.external.aws_auth.result.token}"
+  load_config_file       = false
 }
 
 resource "kubernetes_config_map" "aws_auth" {
