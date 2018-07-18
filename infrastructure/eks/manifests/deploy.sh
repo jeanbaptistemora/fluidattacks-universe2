ec0347c0b5f262c49b07e1c7c14515a83ace8761
Andres Cuberos <acuberos@fluidattacks.com>
2018-07-18 16:38:29 -0500

    Add configuration for domain fluid.la
    Former-commit-id: 8c4234ed517d8be52777a14cccc8909ed3aa1bc7

--- infrastructure/eks/manifests/deploy.sh
+++ infrastructure/eks/manifests/deploy.sh
@@ -11,7 +11,8 @@ fi
 
 # Set TLS certificates in the NGINX server
 sed -i 's/$TLS_KEY/'"$TLS_KEY"'/;
-  s/$TLS_CERT/'"$TLS_CERT"'/' \
+  s/$FA_TLS_CERT/'"$FA_TLS_CERT"'/;
+  s/$FLA_TLS_CERT/'"$FLA_TLS_CERT"'/' \
   eks/manifests/ingress-tls.yaml
 kubectl apply -f eks/manifests/ingress-tls.yaml
 
