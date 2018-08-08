8fa109a83234a8c72e03f7e954b611737f380504
Andres Cuberos <acuberos@fluidattacks.com>
2018-08-08 16:09:34 -0500

    Fix IAM account password policies
    Former-commit-id: b146101fc2c1991fa7e7e7a1834e637b58fec00f

--- infrastructure/iam/main.tf
+++ infrastructure/iam/main.tf
@@ -1,5 +1,17 @@
 variable "fwBucket" {}
 
+resource "aws_iam_account_password_policy" "strict" {
+  allow_users_to_change_password = true
+  hard_expiry                    = false
+  max_password_age               = 30
+  minimum_password_length        = 14
+  password_reuse_prevention      = 24
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_symbols                = true
+  require_uppercase_characters   = true
+}
+
 module "users" {
   source = "./users"
 }
