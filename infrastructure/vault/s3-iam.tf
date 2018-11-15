f835682679b624b6de5a55130ed7a3e5bc8f6865
Andres Cuberos <acuberos@fluidattacks.com>
2018-11-15 16:32:58 -0500

    Create KMS key and its permissions, to encrypt Vault unseal key
    Former-commit-id: 364f06287fe44d3c6bc80eb618446737594e1abf

--- infrastructure/vault/s3-iam.tf
+++ infrastructure/vault/s3-iam.tf
@@ -1,11 +1,32 @@
 variable "vaultBucket" {}
 
-resource "aws_s3_bucket" "vault_bucket" {
+data "aws_iam_policy_document" "key_access_policy" {
+  statement {
+    sid     = "Allow Access to EKSNode role"
+    actions = ["kms:Encrypt", "kms:Decrypt"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${aws_iam_role.k8s_nodes_role.arn}"]
+    }
+  }
+}
+
+resource "aws_kms_key" "vault_encryption_key" {
+  description             = "Key used to encrypt and store Vault unseal key"
+  deletion_window_in_days = 7
+  policy = "${data.aws_iam_policy_document.key_access_policy.json}"
+
+  tags {
+    Name = "Vault_Encryption_Key"
+    App  = "Vault"
+  }
+}
+
+resource "aws_s3_bucket" "vault" {
   bucket = "${var.vaultBucket}"
   acl    = "private"
-  tags {
-    Name = "Vault Backup Bucket"
-  }
+
   lifecycle_rule {
     id      = "cleanup"
     prefix  = "vault.etcd"
@@ -13,5 +34,10 @@ resource "aws_s3_bucket" "vault_bucket" {
     expiration {
       days  = 30
     }
+  }
+
+  tags {
+    Name = "Vault_Bucket"
+    App  = "Vault"
   }
 }
