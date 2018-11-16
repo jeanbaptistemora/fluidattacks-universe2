6cc909bef641fb124a5ff449f222fa24432d8245
Andres Cuberos <acuberos@fluidattacks.com>
2018-11-16 17:52:24 -0500

    Improve function to install Charts with Helm
    Former-commit-id: 49a7376d93f64a204454a302343402a8069310cc

--- infrastructure/eks/manifests/deploy.sh
+++ infrastructure/eks/manifests/deploy.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-declare -A chart_names_map
-declare -A release_names_map
-declare -A chart_namespace_map
-
-chart_names_map=( ["nginx"]="stable/nginx-ingress"
-  ["runner"]="gitlab/gitlab-runner"
-  ["cert-manager"]="stable/cert-manager" )
-release_names_map=( ["nginx"]="controller"
-  ["runner"]="gitlab-runner"
-  ["cert-manager"]="cert-manager" )
-chart_namespace_map=( ["nginx"]="serves"
-  ["runner"]="operations"
-  ["cert-manager"]="operations" )
-
 function echo-blue() {
   echo -e '\033[1;34m'"${1}"'\033[0m'
 }
@@ -53,19 +39,6 @@ function get_changed_files() {
     | grep -Po '(?<=(M|A)\t).*'
 }
 
-# Get which of the changed files are related to the Helm Charts deployments
-function get_changed_helm_files() {
-  mapfile -t changed_files < <(get_changed_files)
-  local changed_helm_files=()
-  for file in "${changed_files[@]}"; do
-    if [[ "${file}" =~ 'helm_values/' ]]; then
-      replace_env_variables "${file}"
-      changed_helm_files+=( "${file##*/}" )
-    fi
-  done
-  echo "${changed_helm_files[*]}"
-}
-
 function get_vault_approle_token() {
   local role_id="${1}"
   local secret_id="${2}"
@@ -74,25 +47,24 @@ function get_vault_approle_token() {
   curl --request POST --data "${data}" "${url}" | jq -r '.auth.client_token'
 }
 
-# Use the modified file to extract chart information from the maps
-# and install or upgrade it, if it exists
 function install_helm_chart() {
-  local helm_values=( "$@" )
-  local files_dir="eks/manifests/helm_values"
-  for file in "${helm_values[@]}"; do
-    local file_no_extension="$(echo "${file}" | cut -d. -f1)"
-    local chart="${chart_names_map[${file_no_extension}]}"
-    local release="${release_names_map[${file_no_extension}]}"
-    local namespace="${chart_namespace_map[${file_no_extension}]}"
-    if helm list --tls | grep -q "${release}"; then
+  local chart="${1}"
+  local name="${2}"
+  local namespace="${3}"
+  local values="helm/${4}"
+  if helm list --tls | grep -q "${name}"; then
+    mapfile -t changed_files < <(get_changed_files)
+    if echo "${changed_files[@]}" | grep -o "${values}"; then
       echo-blue "Upgrading chart ${chart}..."
-      helm upgrade "${release}" "${chart}" -f "${files_dir}/${file}" --tls
+      helm upgrade "${name}" "${chart}" --values "${values}" --wait --tls
     else
-      echo-blue "Installing chart ${chart}..."
-      helm install "${chart}" --name "${release}" -f "${files_dir}/${file}" \
-        --namespace "${namespace}" --tls
+      echo-blue "Chart ${chart} is up to date!"
     fi
-  done
+  else
+    echo-blue "Installing chart ${chart}..."
+    helm install "${chart}" --name "${name}" --namespace "${namespace}" \
+      --values "${values}" --wait --tls
+  fi
 }
 
 # Automate the generation and renewal of TLS certificates for secondary domains
@@ -133,16 +105,6 @@ issue_secondary_domain_certificates ingress/secondary-domains.yaml \
   secondary-domains-cert secondary-domains 
 
 
-IFS=" " read -r -a helm_files <<< "$(get_changed_helm_files)"
-if [ -z "${helm_files}" ]; then
-  echo-blue "Helm values were not updated in this deployment."
-else
-  helm init --client-only
-  helm repo add gitlab https://charts.gitlab.io
-  helm repo update
-  install_helm_chart "${helm_files[@]}"
-fi
-
 # Provide information to access Gitlab Container Registry and pull images
 if ! kubectl get secret gitlab-reg; then
   echo "Creating secret to access Gitlab Registry..."
@@ -173,6 +135,11 @@ deploy_application deployments/alg.yaml
 deploy_application deployments/exams.yaml
 deploy_application deployments/integrates.yaml
 deploy_application deployments/vpn.yaml
+
+
+install_helm_chart stable/nginx-ingress controller serves nginx.yaml
+install_helm_chart gitlab/gitlab-runner runner operations runner.yaml
+install_helm_chart stable/cert-manager cert-manager operations cert-manager.yaml
 
 # Wait until the initialization of the Load Balancer is complete
 sleep 5
