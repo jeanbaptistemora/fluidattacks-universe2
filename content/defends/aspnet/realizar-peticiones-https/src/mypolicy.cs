12ce3115901e40e4ba5c541781731f11968cebee
Oscar Prado <oprado@fluidattacks.com>
2018-12-06 12:05:29 -0500

    migrate: defends/aspnet/realizar-peticiones-https/
    - Added words
    - Lix: 47,5 = school year 8
    - 453 words
    - Created src files
    
    Cambios renderizados localmente:
    
    
    Former-commit-id: 0eb72f8919184a77dc75ee313004a00f99bda3c3

---
+++ content/defends/aspnet/realizar-peticiones-https/src/mypolicy.cs
@@ -0,0 +1,10 @@
+using System.Security.Cryptography.X509Certificates;
+using System.Net;
+
+public class MyPolicy : ICertificatePolicy {
+  public bool CheckValidationResult(ServicePoint srvPoint,
+    X509Certificate certificate, WebRequest request,
+    int certificateProblem) {
+      return true;
+    }
+}
