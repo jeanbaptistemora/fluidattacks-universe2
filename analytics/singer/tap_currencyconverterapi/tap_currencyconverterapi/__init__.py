52c90c82b5101fedc76f61942e76c0e1ab62fe32
kamadoatfluid <kamado@fluidattacks.com>
2018-12-17 08:33:29 -0500

    analytics: tap to get the market exchange rates
    Former-commit-id: 196b942beab9ba83b1ae2d69ac70566b8ef54230

---
+++ analytics/singer/tap_currencyconverterapi/tap_currencyconverterapi/__init__.py
@@ -0,0 +1,69 @@
+"""
+Singer.io tap for the https://www.currencyconverterapi.com/ API
+"""
+
+## python3 -m pylint (default configuration)
+# Your code has been rated at 10.00/10
+
+import json
+import urllib.request
+
+def get_exchange_rates():
+    """ make the request and returns a json object with the response """
+    resource = "https://free.currencyconverterapi.com/api/v6/convert"
+    resource += "?q=USD_COP"
+    resource += ",COP_USD"
+    resource += "&compact=ultra"
+    request = urllib.request.Request(resource)
+    response = urllib.request.urlopen(request).read()
+    json_obj = json.loads(response.decode('utf-8'))
+    return json_obj
+
+def write_schema():
+    """ write the SCHEMA message to stdout """
+
+    stdout_json_obj = {
+        "type": "SCHEMA",
+        "stream": "currencies",
+        "key_properties": ["at"],
+        "schema": {
+            "properties": {
+                "at": {
+                    "type": "string"
+                },
+                "USD_COP": {
+                    "type": "number"
+                },
+                "COP_USD": {
+                    "type": "number"
+                }
+            }
+        }
+    }
+
+    print(json.dumps(stdout_json_obj))
+
+def write_records(json_obj):
+    """ write the RECORD message to stdout """
+
+    stdout_json_obj = {
+        "type": "RECORD",
+        "stream": "currencies",
+        "record": {
+            "at": "last sync",
+            "USD_COP": float(json_obj["USD_COP"]),
+            "COP_USD": float(json_obj["COP_USD"])
+        }
+    }
+
+    print(json.dumps(stdout_json_obj))
+
+def main():
+    """ usual entry point """
+    json_obj = get_exchange_rates()
+
+    write_schema()
+    write_records(json_obj)
+
+if __name__ == "__main__":
+    main()
