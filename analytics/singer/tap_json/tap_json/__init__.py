dde98b79beb57513ede1a5136e613c369f50cf89
Kevin Amado <kamado@fluidattacks.com>
2019-01-30 11:28:29 -0500

    refac(job): #139 towards prospector and mypy
    - refactors code to make it prospector (veryhigh) compliant
    - includes optional mypy static typing
    
    
    Former-commit-id: 36e549d70963f82bedf9b26810ef18edcd6f9fc2

--- analytics/singer/tap_json/tap_json/__init__.py
+++ analytics/singer/tap_json/tap_json/__init__.py
@@ -1,30 +1,64 @@
-""" Singer tap for a JSON stream """
+"""Singer tap for a JSON stream.
+"""
 
 import io
 import re
 import os
 import sys
+import datetime
 import argparse
 
 from json import load, loads, dumps
 from json.decoder import JSONDecodeError
 
-from datetime import datetime
+from typing import Callable, Tuple, Any
 
-class ID():
-    """ class to generate light weight unique ids """
-    # pylint: disable=too-few-public-methods
-    def __init__(self):
-        self.identifier = 0
-    def get(self):
-        """ generates a unique id """
-        self.identifier += 1
+# stru_type aliases that improve clarity
+JSON = Any
+STRU = Any
+
+# Module control pannel
+FIELD_SEP: str = "__"
+TABLE_SEP: str = "____"
+SCHEMAS_DIR: str = "____schemas"
+RECORDS_DIR: str = "____records"
+
+ENABLE_TIMESTAMPS: bool = False
+
+DATE_FORMATS: Tuple[str, str, str, str, str, str, str] = (
+    "%Y %m %d %H %M %S %f",
+    "%Y %m %d %H %M %S",
+    "%Y %m %d",
+    "%d %m %Y",
+    "%b %d %Y",
+    "%b %Y",
+    "%H %M",
+)
+
+class GenID():
+    """Class to generate light weight unique ids.
+    """
+
+    def __init__(self) -> None:
+        self.identifier: int = 0
+
+    def __repr__(self) -> str:
         return format(self.identifier, "X")
 
-_FSEP = "__"           # field separator
-_TSEP = "____"         # table separator
-_SDIR = "____schemas"  # schemas directory
-_RDIR = "____records"  # records directory
+    def seed(self, seed: int) -> None:
+        """Seeds the generator.
+        """
+
+        self.identifier = seed
+
+    def gen(self) -> str:
+        """Generates a unique id.
+        """
+
+        self.identifier += 1
+        return repr(self)
+
+ID: GenID = GenID()
 
 IS_NONE = lambda stru: stru is None
 IS_STR = lambda stru: isinstance(stru, str)
@@ -40,93 +74,106 @@ CLEANIT = lambda string: re.sub(r"[^ _a-zA-Z0-9]", "", string)
 TYPE = lambda stru: type(stru).__name__ if IS_NONE(to_date(stru)) else "datetime"
 CAST = lambda stru: stru if IS_NONE(to_date(stru)) else to_date(stru)
 
-ID_GEN = ID()
 
-ENABLE_TS = False
-DATE_FORMATS = (
-    "%Y %m %d %H %M %S %f",
-    "%Y %m %d %H %M %S",
-    "%Y %m %d",
-)
+def to_date(date_time: Any) -> Any:
+    """Manipulates a date to provide a RFC339 compatible date.
+    """
 
-def to_date(date_time):
-    """ tries to match the date with the provided DATE_FORMATS
-        returns a RFC3339 date if it finds a match """
-
-    if ENABLE_TS and IS_TIME(date_time):
+    if ENABLE_TIMESTAMPS and IS_TIME(date_time):
         try:
-            date_obj = datetime.utcfromtimestamp(date_time)
-            date_time = date_obj.strftime("%Y-%m-%dT%H:%M:%SZ")
+            date_stru = datetime.datetime.utcfromtimestamp(date_time)
+            date_time = date_stru.strftime("%Y-%m-%dT%H:%M:%SZ")
             return date_time
         except ValueError:
             pass
 
     if IS_STR(date_time):
-        date_time = re.sub(r"[^\d]", " ", date_time)
-        date_time = re.sub(r"\s+", " ", date_time)
+        date_time = re.sub(r"[^\d]", r" ", date_time)
+        date_time = re.sub(r"\s+", r" ", date_time)
 
         for date_format in DATE_FORMATS:
             try:
-                date_obj = datetime.strptime(date_time, date_format)
-                date_time = date_obj.strftime("%Y-%m-%dT%H:%M:%SZ")
+                date_stru = datetime.datetime.strptime(date_time, date_format)
+                date_time = date_stru.strftime("%Y-%m-%dT%H:%M:%SZ")
                 return date_time
             except ValueError:
                 pass
 
-    return None
+    return False
 
-def pt2st(ptype):
-    """ returns a singer type for the python type """
-    if ptype == "bool":
-        return {"type": "boolean"}
-    if ptype == "float":
-        return {"type": "number"}
-    if ptype == "int":
-        return {"type": "integer"}
-    if ptype == "str":
-        return {"type": "string"}
-    if ptype == "datetime":
-        return {"type": "string", "format": "date-time"}
-    raise Exception(f"pt2st(ptype): ptype={ptype} not matched")
+
+def pt2st(pstru_type: str) -> JSON:
+    """Returns a singer stru_type for the python stru_type.
+    """
+
+    if pstru_type == "bool":
+        return {"stru_type": "boolean"}
+    if pstru_type == "float":
+        return {"stru_type": "number"}
+    if pstru_type == "int":
+        return {"stru_type": "integer"}
+    if pstru_type == "str":
+        return {"stru_type": "string"}
+    if pstru_type == "datetime":
+        return {"stru_type": "string", "format": "date-time"}
+    raise Exception(f"pt2st(pstru_type): pstru_type={pstru_type} not matched")
+
 
 def prepare_env():
-    """ creates/restablish the target folder """
+    """Creates/restablish the target folders.
+    """
 
-    for _dir in (_RDIR, _SDIR):
+    for _dir in (RECORDS_DIR, SCHEMAS_DIR):
         if not os.path.exists(_dir):
             os.makedirs(_dir)
         else:
             for file in os.listdir(_dir):
                 os.remove(f"{_dir}/{file}")
 
+
 def clean_env():
-    """ cleans the used paths on exit """
-    for _dir in (_SDIR, _RDIR):
+    """Cleans the used paths on exit.
+    """
+
+    for _dir in (SCHEMAS_DIR, RECORDS_DIR):
         for file in os.listdir(_dir):
             os.remove(f"{_dir}/{file}")
         os.removedirs(f"{_dir}")
 
-def write(directory, table_name, obj, func=lambda x: x):
-    """ writes func(obj) to a file"""
+
+def write(
+        directory: str,
+        table_name: str,
+        stru: STRU,
+        func: Callable[[STRU], STRU] = lambda x: x) -> None:
+    """Writes func(stru) to a file.
+    """
 
     with open(f"{directory}/{table_name}", "a") as file:
-        file.write(func(obj))
+        file.write(func(stru))
         file.write("\n")
 
-def read(directory, table_name, func=lambda x: x):
-    """ iterator to func(line) of file """
+
+def read(
+        directory: str,
+        table_name: str,
+        func: Callable[[STRU], STRU] = lambda x: x) -> Any:
+    """Yields func(line) per every line of a file.
+    """
 
     with open(f"{directory}/{table_name}", "r") as file:
-        line = file.readline()
-        while line:
+        for line in file:
             yield func(line)
-            line = file.readline()
 
-def json_from_file(file_path):
-    """ loads a json from a file """
+
+def json_from_file(file_path: str) -> JSON:
+    """Loads a json from a file.
+    """
+
     with open(file_path, "r") as json_file:
-        json_obj = load(json_file)
-    return json_obj
+        json_stru = load(json_file)
+    return json_stru
+
 
 def linearize(table_name, structura):
     """ breaks a structura into flat records.
@@ -147,7 +194,7 @@ def linearize(table_name, structura):
                 if IS_DICT(val):
                     for nkey, nval in val.items():
                         if IS_STRU(nval):
-                            new_stru[f"{CLEANIT(key)}{_FSEP}{CLEANIT(nkey)}"] = nval
+                            new_stru[f"{CLEANIT(key)}{FIELD_SEP}{CLEANIT(nkey)}"] = nval
                     new_stru = simplify(new_stru)
                 elif IS_STRU(val):
                     new_stru[CLEANIT(key)] = simplify(val)
@@ -171,21 +218,22 @@ def linearize(table_name, structura):
                 if IS_PRIM(nstru):
                     record[nkey] = nstru
                 elif IS_LIST(nstru):
-                    nid = ID_GEN.get()
-                    ntable = f"{table}{_TSEP}{nkey}"
+                    nid = ID.gen()
+                    ntable = f"{table}{TABLE_SEP}{nkey}"
                     ntable_ids = [nid] if ids is None else ids + [nid]
                     record[ntable] = nid
                     deconstruct(table=ntable, stru=nstru, ids=ntable_ids)
-            write(_RDIR, table, record, func=dumps)
+            write(RECORDS_DIR, table, record, func=dumps)
 
     deconstruct(table=table_name, stru=simplify(structura))
+
 
 def catalog():
     """ deduce the schema of the generated tables """
 
-    for table_name in os.listdir(_RDIR):
+    for table_name in os.listdir(RECORDS_DIR):
         schema = {}
-        for structura in read(_RDIR, table_name, loads):
+        for structura in read(RECORDS_DIR, table_name, loads):
             for key, val in structura.items():
                 vtype = TYPE(val)
                 try:
@@ -193,7 +241,8 @@ def catalog():
                         schema[key].append(vtype)
                 except KeyError:
                     schema[key] = [vtype]
-        write(_SDIR, table_name, schema, dumps)
+        write(SCHEMAS_DIR, table_name, schema, dumps)
+
 
 def main():
     """ usual entry point """
@@ -212,8 +261,8 @@ def main():
     # we need some way to tell the entire module that timestamps should be recognized as dates
     # this is an on-demand feature,
     # because may yield ambiguity with numbers that are not timestamps
-    global ENABLE_TS # pylint: disable=global-statement
-    ENABLE_TS = args.enable_timestamps
+    global ENABLE_TIMESTAMPS # pylint: disable=global-statement
+    ENABLE_TIMESTAMPS = args.enable_timestamps
 
     input_messages = io.TextIOWrapper(sys.stdin.buffer, encoding="utf-8")
 
@@ -229,8 +278,8 @@ def main():
     catalog()
 
     # Parse everything to singer
-    for table in os.listdir(_SDIR):
-        pschema = json_from_file(f"{_SDIR}/{table}")
+    for table in os.listdir(SCHEMAS_DIR):
+        pschema = json_from_file(f"{SCHEMAS_DIR}/{table}")
         sschema = {
             "type": "SCHEMA",
             "stream": table,
@@ -241,7 +290,7 @@ def main():
         }
         print(dumps(sschema))
 
-        for precord in read(_RDIR, table, loads):
+        for precord in read(RECORDS_DIR, table, loads):
             srecord = {
                 "type": "RECORD",
                 "stream": table,
@@ -250,6 +299,7 @@ def main():
             print(dumps(srecord))
 
     clean_env()
+
 
 if __name__ == "__main__":
     main()
