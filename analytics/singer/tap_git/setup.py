910c4d26c9b0ace94962168d31e05a0ae1fd8830
Kevin Amado <kamado@fluidattacks.com>
2019-01-30 08:37:51 -0500

    refac(job): #139 towards prospector and mypy
    - refactors code to make it prospector (veryhigh) compliant
    - includes optional mypy static typing
    
    
    Former-commit-id: 3089bb27ad192c1cc055f98145237cdd94f1b969

--- analytics/singer/tap_git/setup.py
+++ analytics/singer/tap_git/setup.py
@@ -1,30 +1,33 @@
-"""
-package setup file
+"""Package setup file.
 """
 
 import setuptools
 
 setuptools.setup(
-    name='tap_git',
-    version='1.0.0',
-    description='Singer tap for a git repository',
-    classifiers=['Programming Language :: Python :: 3 :: Only'],
+    name="tap_git",
+    version="1.0.0",
+    description="Singer tap for a git repository",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
 
-    author='Fluid Attacks, We hack your software.',
-    url='https://fluidattacks.com/',
+    author="Fluid Attacks, We hack your software.",
+    url="https://fluidattacks.com/",
 
-    py_modules=[],
+    py_modules=[
+    ],
 
     install_requires=[
         "gitpython"
     ],
 
-    entry_points='''
+    entry_points="""
         [console_scripts]
         tap-git=tap_git:main
-    ''',
+    """,
 
-    packages=['tap_git'],
+    packages=[
+        "tap_git"
+    ],
+
     package_data={
         "tap_git": [
             "commits.schema.json",
--- analytics/singer/tap_timedoctor/tap_timedoctor/api_timedoctor.py
+++ analytics/singer/tap_timedoctor/tap_timedoctor/api_timedoctor.py
@@ -1,23 +1,25 @@
+"""TimeDoctor API wrapper.
 """
-Time Doctor API wrapper
-    The access_token must have admin privileges
-"""
-
-## python3 -m pylint (default configuration)
-# Your code has been rated at 10.00/10
 
 import time
+import urllib.error
 import urllib.request
 
+
 def current_timestamp(offset=0.0):
-    """ returns the current time stamp """
+    """Returns the current timestamp.
+    """
+
     return time.time() + offset
 
+
 class Worker():
-    """ class to represent a worker who make request to the API without exceeding the rate limit """
+    """Class to represent a worker who make request to the API.
+
+    It takes care of making the requests without exceeding the rate limit.
+    """
 
     def __init__(self, access_token):
-        """ constructor """
         self.access_token = access_token
         self.url = "https://webapi.timedoctor.com"
 
@@ -25,24 +27,32 @@ class Worker():
         self.last_request_timestamp = current_timestamp()
 
     def sslr(self):
-        """ seconds since last request """
+        """Seconds since last request.
+        """
+
         return current_timestamp() - self.last_request_timestamp
 
     def wait(self):
-        """ wait until we can make another request to the API """
+        """Wait until we can make another request to the API.
+        """
+
         time.sleep(max(self.min_sslr - self.sslr(), 0.0))
         self.last_request_timestamp = current_timestamp()
 
     def request(self, resource):
-        """ Makes a request to the API """
+        """Makes a request to the API.
+        """
+
         response = None
         status_code = None
 
         self.wait()
 
         try:
-            headers = {}
-            headers["Authorization"] = "Bearer " + self.access_token
+            headers = {
+                "Authorization": f"Bearer {self.access_token}"
+            }
+
             request = urllib.request.Request(resource, headers=headers)
             response = urllib.request.urlopen(request).read().decode('utf-8')
             status_code = 200
@@ -60,90 +70,46 @@ class Worker():
         return (status_code, response)
 
     def get_companies(self):
-        """ return the account info of the access_token owner """
-        resource = self.url + "/v1.1/companies"
+        """Return the account info of the access_token owner.
+        """
+
+        resource = f"{self.url}/v1.1/companies"
         return self.request(resource)
 
     def get_users(self, company_id):
-        """ return a collection of user(s) under the given company_id """
-        resource = self.url + "/v1.1/companies/" + company_id + "/users"
+        """Return a collection of user(s) under the given company_id.
+        """
+
+        resource = f"{self.url}/v1.1/companies/{company_id}/users"
         return self.request(resource)
 
     def get_worklogs(self, company_id, limit, offset):
-        """ return a collection of users worklogs under the given company id """
-        resource = self.url + "/v1.1/companies/" + company_id + "/worklogs"
+        """Return a collection of users worklogs under the given company id.
+        """
 
-        # fetch historical
-        resource += "?start_date=2017-01-01&end_date=2999-12-31"
-        resource += "&limit=" + str(limit) + "&offset=" + str(offset)
+        resource = (
+            f"{self.url}/v1.1/companies/{company_id}/worklogs"
 
-        # fetch working time, not breaks
-        resource += "&breaks_only=0"
+            # fetch historical
+            f"?start_date=2017-01-01&end_date=2999-12-31"
+            f"&limit={limit}&offset={offset}"
 
-        # don't consolidate records to make information richer
-        resource += "&consolidated=0"
+            # fetch working time, not breaks
+            f"&breaks_only=0"
+
+            # don't consolidate records to make information richer
+            f"&consolidated=0"
+        )
 
         return self.request(resource)
 
     def get_computer_activity(self, company_id, user_id):
-        """ returns screenshots, keystrokes, mouse activities for the specified user_id """
-        resource = self.url + "/v1.1/companies/" + company_id + "/screenshots"
-        resource += "?start_date=2017-01-01&end_date=2999-12-31"
-        resource += "&user_id=" + user_id
-        return self.request(resource)
+        """Returns screenshots, keystrokes, mouse activities for a user_id.
+        """
 
-    # the next four functions:
-    #   - were not tested/implemented due to service availability (v.g. poortime)
-    #   - contribute negligible entropy to our use case (v.g. we don't have payrolls/webandapp)
-
-    def get_poortime(self, company_id, limit, offset):
-        """ return a collection of poortime usage under the given company_id """
-        resource = self.url + "/v1.1/companies/" + company_id + "/poortime"
-        resource += "?start_date=1900-01-01&end_date=2999-12-31"
-        resource += "&limit=" + limit + "&offset=" + offset
-        return self.request(resource)
-
-    def get_absent_and_late(self, company_id):
-        """ return a collection of absent and late reason under the given company_id """
-        resource = self.url + "/v1.1/companies/" + company_id + "/absent-and-late"
-        return self.request(resource)
-
-    def get_payrolls(self, company_id):
-        """ return a collection of payrolls under the given company Id """
-        resource = self.url + "/v1.1/companies/" + company_id + "/payrolls"
-        return self.request(resource)
-
-    def get_webandapp(self, company_id):
-        """ return a collection of web and apps used under the specified userID & date range """
-        resource = self.url + "/v1.1/companies/" + company_id + "/webandapp"
-        return self.request(resource)
-
-    # the next five functions are automatically included in get_worklogs()
-
-    def get_projects(self, company_id, user_id):
-        """ return a collection of poortime usage under the given company_id """
-        resource = self.url + "/v1.1/companies/" + company_id + "/users/" + user_id + "/projects"
-        resource += "/?all=1"
-        return self.request(resource)
-
-    def get_project_by_id(self, company_id, user_id, project_id):
-        """ return a collection of poortime usage under the given company_id """
-        resource = self.url + "/v1.1/companies/" + company_id + "/users/" + user_id
-        resource += "/projects/" + project_id
-        return self.request(resource)
-
-    def get_tasks(self, company_id, user_id):
-        """ return a collection of poortime usage under the given company_id """
-        resource = self.url + "/v1.1/companies/" + company_id + "/users/" + user_id + "/tasks"
-        return self.request(resource)
-
-    def get_tasks_by_id(self, company_id, user_id, task_id):
-        """ return a collection of poortime usage under the given company_id """
-        resource = self.url + "/v1.1/companies/" + company_id + "/users/" + user_id
-        resource += "/tasks/" + task_id
-        return self.request(resource)
-
-    def get_users_by_id(self, company_id, user_id):
-        """ return general info about a specific user under the given company_id """
-        resource = self.url + "/v1.1/companies/" + company_id + "/users/" + user_id + ""
+        resource = (
+            f"{self.url}/v1.1/companies/{company_id}/screenshots"
+            f"?start_date=2017-01-01&end_date=2999-12-31"
+            f"&user_id={user_id}"
+        )
         return self.request(resource)
