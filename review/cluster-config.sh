345a4afdfa8f91cca32d63e2657cce5cdc368d5f
Andres Cuberos <acuberos@fluidattacks.com>
2019-02-13 11:02:58 -0500

    fix(cross): #1627 improve pipeline
    - Build all containers using Kaniko to remove the privileges of
      docker:dind.
    - All containers that are used in the cluster are now stored
      in a private repository to avoid malicious override in the
      public registry.
    - Use the cache from the container used in the Review Apps instead
      of storing it as an artifact. This due to a breaking change
      that ocurred in Gitlab API and artifacts could not be downloaded.
    - Redistribute content check and generation so they are performed
      in parallel
    
    
    Former-commit-id: 0fed3db539fbb0af7f0f316b2ba834dcfae8a461

--- review/cluster-config.sh
+++ review/cluster-config.sh
@@ -14,29 +14,29 @@ envsubst < review/deploy-web.yaml > deploy-web.yaml \
 # Set namespace preference for kubectl commands
 echo "Setting namespace preferences..."
 kubectl config set-context \
-  "$(kubectl config current-context)" --namespace="$CI_PROJECT_NAME"
+  "$(kubectl config current-context)" --namespace="${CI_PROJECT_NAME}"
 
 # Check secret to pull images from Gitlab Registry and set if not present
-if ! kubectl get secret 'gitlab-reg'; then
+if ! kubectl get secret 'jfrog-reg'; then
   echo "Creating secret to access Gitlab Registry..."
-  kubectl create secret docker-registry 'gitlab-reg' \
-  --docker-server="$CI_REGISTRY" --docker-username="$DOCKER_USER" \
-  --docker-password="$DOCKER_PASS" --docker-email="$DOCKER_EMAIL"
+  kubectl create secret docker-registry 'jfrog-reg' \
+  --docker-server="${REGISTRY}" --docker-username="${REGISTRY_USER}" \
+  --docker-password="${REGISTRY_PASS}" --docker-email="${REGISTRY_EMAIL}"
 fi
 
 # Delete previous deployments and services of the same branch, if present
-if kubectl get deployments | grep -q "$CI_COMMIT_REF_SLUG"; then
+if kubectl get deployments | grep -q "${CI_COMMIT_REF_SLUG}"; then
   echo "Erasing previous deployments..."
-  kubectl delete deployment "review-$CI_COMMIT_REF_SLUG"
-  kubectl delete service "service-$CI_COMMIT_REF_SLUG";
-  kubectl get ingress "ingress-$CI_PROJECT_NAME" -o yaml | tac | sed '/path:\ \/'"$CI_COMMIT_REF_SLUG"'/,+3d' | tac > current-ingress.yaml
+  kubectl delete deployment "review-${CI_COMMIT_REF_SLUG}"
+  kubectl delete service "service-${CI_COMMIT_REF_SLUG}";
+  kubectl get ingress "ingress-${CI_PROJECT_NAME}" -o yaml | tac | sed '/path:\ \/'"${CI_COMMIT_REF_SLUG}"'/,+3d' | tac > current-ingress.yaml
 fi
 
 # Update current ingress resource if it exists, otherwise create it from zero.
-if kubectl get ingress "ingress-$CI_PROJECT_NAME"; then
+if kubectl get ingress "ingress-${CI_PROJECT_NAME}"; then
   if [ ! -f current-ingress.yaml ]; then
     echo "Getting current ingress manifest..."
-    kubectl get ingress "ingress-$CI_PROJECT_NAME" -o yaml > current-ingress.yaml;
+    kubectl get ingress "ingress-${CI_PROJECT_NAME}" -o yaml > current-ingress.yaml;
   fi
   echo "Updating ingress manifest..."
   sed -n '/spec:/,/tls:/p' current-ingress.yaml | tail -n +6 | head -n -1 >> review/ingress.yaml
@@ -51,4 +51,4 @@ kubectl apply -f review/tls.yaml
 # Deploy pod and service
 echo "Deploying latest image..."
 kubectl create -f review/deploy-web.yaml
-kubectl rollout status "deploy/review-$CI_COMMIT_REF_SLUG"
+kubectl rollout status "deploy/review-${CI_COMMIT_REF_SLUG}"
