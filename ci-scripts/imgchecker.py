bf4b8fd608ce88e4d2d43f72a5af0124d549fc29
Oscar Prado <oprado@fluidattacks.com>
2019-02-26 08:56:17 -0500

    feat(build): #1628.6 improved image linting
    - Now image linting runs only over changes
    - Created separated script for image linting
    - Created new rules for images
    
    
    Former-commit-id: 39d4a341106a8cbf4f118938816b2ff80c53feb9

---
+++ ci-scripts/imgchecker.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+"""
+Script to check if images inside the repo are properly optimized
+in size, extension and dimensions
+Author: Oscar Eduardo Prado oprado@fluidattacks.com
+Version 1.0
+"""
+import os
+import sys
+import print_helper
+
+EXIT_CODE = 0
+
+#Define Rules
+
+print_helper.print_success("Checking images ...\n")
+
+#Image size must not exceed 300kb
+OUT = os.popen("find content/ -iname '*.png' -size +300k").read()
+if len(OUT) > 0:
+  print_helper.print_failure("Issue found in the following file(s):\n")
+  print_helper.print_failure(OUT)
+  print_helper.print_warning("Image file size must not exceed 300Kb. "\
+                             "Please correct the file(s) and try again\n\n")
+  EXIT_CODE = 1
+
+#Only valid image extension is PNG
+OUT = os.popen("find . -iname '*.jpeg' -o -iname '*.jpg' \
+                -o -iname '*.bmp' -o -iname '*.tiff'").read()
+if len(OUT) > 0:
+  print_helper.print_failure("Issue found in the following file(s):\n")
+  print_helper.print_failure(OUT)
+  print_helper.print_warning("The only allowed extension for images is PNG. "\
+                             "Please correct the file(s) and try again\n\n")
+  EXIT_CODE = 1
+
+#All blog entries must have a cover.png file
+ARTS = os.popen("find content/blog-* -mindepth 1 -type d").read()
+ARTS = ARTS.split()
+for ART in ARTS:
+  if not os.path.exists(ART+'/cover.png'):
+    print_helper.print_failure("Issue found in the following article(s):\n")
+    print_helper.print_failure(ART+"\n")
+    print_helper.print_warning("All blog entries must have a cover.png file. "\
+                               "If you already have a cover image, "  \
+                               "please rename it to cover.png. \n\n")
+
+    EXIT_CODE = 1
+  # Check that cover.png has the proper dimensions
+  else:
+      OUT = os.popen('identify -format "%wx%h" '+ART+'/cover.png').read()
+      if not OUT == "600x280":
+        print_helper.print_failure("Issue found in the following article(s):\n")
+        print_helper.print_failure(ART+"\n")
+        print_helper.print_warning("All cover images must have the size NxN. "\
+                                   "Please modify the cover size. \n\n")
+
+        EXIT_CODE = 1
+
+#Check that new images are optimized
+print_helper.print_warning("Checking image optimization ...\n")
+#get changes in the repo:
+BRANCH = os.popen('git rev-parse --abbrev-ref HEAD').read()
+BRANCH = BRANCH.split()
+REMOTE = os.popen('git rev-list --count --no-merges origin/master').read()
+LOCAL = os.popen('git rev-list --count --no-merges '+BRANCH[0]).read()
+NCOMMITS = str(int(LOCAL) - int(REMOTE))
+CHANGES = os.popen('git diff HEAD~'+NCOMMITS+' --name-only').read()
+CHANGES = CHANGES.split()
+
+for FILE in CHANGES:
+  if ".png" in FILE:
+    COMMAND ='optipng '+FILE+' |& pcregrep "already"'
+    OUT = os.popen("bash -c '{}'".format(COMMAND)).read()
+    if not "already optimized" in OUT:
+      print_helper.print_failure("Issue found in the following image(s):\n")
+      print_helper.print_failure(FILE+"\n")
+      print_helper.print_warning("All png files must be optimized. "\
+                                 "Run \"optipng\" or visit https://compresspng.com/es/"\
+                                 " to optimize. \n\n")
+
+      EXIT_CODE = 1
+    else:
+      print_helper.print_success(FILE+" is optimized :) \n")
+
+print_helper.print_success("\nDone!\n")
+
+sys.exit(EXIT_CODE)
