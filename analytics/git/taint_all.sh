c839ee963129b3fabcc7220a04fe1f6c57a67cb3
kamadoatfluid <kamado@fluidattacks.com>
2019-02-27 09:59:03 -0500

    perf(job): #117 faster taints pipeline
    - separates the push and extract steps
    - so no lines.csv is parsed twice
    - name nested subscriptions "a-b" as "b"
    - so data can be incorportated seamlessly in chartio
    
    
    Former-commit-id: db8249eb3e09cbe9c10645af9e2266c5121cc314

--- analytics/git/taint_all.sh
+++ analytics/git/taint_all.sh
@@ -8,17 +8,25 @@ output=$(cat /config.json \
   | jq -r ".[]|.location" \
     | sed -E 's/(\/git)\/(.*?)\/(.*)/\2 \1\/\2\/\3/g')
 
-./analytics/git/taint.py set username "lines.csv"
+# push every repo to the database
 echo "$output" | while read line
 do
   subs=$(echo "$line" | grep -o -E '^[a-zA-Z0-9_-]+')
+  subs=${subs#*-}
   path=$(echo "$line" | grep -o -E ' .*$')
   echo "subscription: $subs"
   echo "path: $path"
-
   ./analytics/git/taint.py set subscription $subs
   ./analytics/git/taint.py database push $path
+  echo ""
+done
+
+# extract information about clean lines from every lines.csv
+./analytics/git/taint.py set username "lines.csv"
+echo "$output" | grep -o -E '^[a-zA-Z0-9_-]+' | uniq | while read subs
+do
   lines_csv_path="$continuous_path/subscriptions/${subs/-//}/lines.csv"
+  echo "path: $lines_csv_path"
   if [ -f "$lines_csv_path" ]; then
     ./analytics/git/taint.py database wring "$lines_csv_path"
   fi
