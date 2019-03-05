d3f3871731d9a21c122da5da79cd21832b5f2ff9
kamadoatfluid <kamado@fluidattacks.com>
2019-03-05 10:19:23 -0500

    feat(job): #133 pcap increase metadata
    - it now parse the packet.show2() method from scapy
    - which increase a lot the extraction of packet metadata
    - and allow us to see everything that is happening in there
    - output is first parsed to a YAML, then to a JSON for simplicity
    - no-deltas-check
    
    
    Former-commit-id: 02c9a167902c102b6f066c014cd75133704684bc

--- analytics/singer/streamer_pcap/streamer_pcap/__init__.py
+++ analytics/singer/streamer_pcap/streamer_pcap/__init__.py
@@ -1,17 +1,63 @@
 #!/usr/bin/env python3
 """Streamer for a Packet Capture (.pcap) file."""
 
+import re
 import json
 import argparse
-import itertools
+import datetime
 
-from typing import Any
+from typing import Iterator, Any
 
+import yaml
 import scapy.all
 
 # type aliases
 JSON = Any
+YAML = Any
 PACKET = Any
+
+
+def indent(level: int):
+    """Return an indentation string."""
+    return "  " * level
+
+
+def date_from_timestamp(timestamp: float) -> str:
+    """Return a RFC 3339 date from the timestamp."""
+    date_obj = datetime.datetime.utcfromtimestamp(
+        timestamp)
+    date_str = date_obj.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    return date_str
+
+
+def string_escape(string: str) -> str:
+    """Escape a string to HEX.
+
+    Packet data comes with not safe chars for YAML or JSON.
+    They must be escaped and are recognized for beeing surrounded by ' or ".
+    """
+    # Return if not surrounded with ' or "
+    if len(string) <= 2:
+        return string
+    for char in ("'", '"'):
+        if string[0] == char and string[-1] == char:
+            break
+    else:
+        return string
+
+    new_string = ""
+    reading_byte = 0
+    for char in string[1:-2]:
+        if reading_byte == 0:
+            new_string += char.encode("ascii").hex().upper()
+        elif reading_byte == 1 and char == "\\":
+            reading_byte = 2
+        elif reading_byte == 2 and char == "x":
+            reading_byte = 3
+        elif reading_byte == 3:
+            reading_byte = 0
+            new_string += char.upper()
+    return new_string
 
 
 def stream_it(name: str, json_obj: JSON) -> JSON:
@@ -32,51 +78,124 @@ def stream_packets(file: str) -> None:
         stream_it("packets", packet_encoded)
 
 
-def stream_packets__encode(packet: PACKET) -> str:
+def stream_packets__encode(packet: PACKET) -> str: # noqa
     """Encode a packet object into a JSON."""
-    packet_object: JSON = {}
-    packet_decoded = repr(packet)
-    packet_decoded_iter = iter(packet_decoded)
+    # Control flow
+    level: int = 0
+    level_flag: int = 1
+    level_level_flag: int = 1
+    reading_list: bool = False
+    reading_list_list: bool = False
+
+    # Packet model
+    packet_document: YAML = ""
+    packet_iterlines: Iterator[str] = iter(
+        packet.show2(dump=True).splitlines())
+
+    # Packet parsing regexps
+    re_base_header = re.compile(r"^###\[(.*)\]###$")
+    re_base_body = re.compile(r"^([^=]*)=(.*)$")
+    re_list_header = re.compile(r"^\|###\[(.*)\]###$")
+    re_list_flag = re.compile(r"^\\(.*)\\$")
+    re_list_body = re.compile(r"^\|([^=]*)=(.*)$")
+    re_list_list_header = re.compile(r"^\|\|###\[(.*)\]###$")
+    re_list_list_flag = re.compile(r"^\|\\(.*)\\$")
+    re_list_list_body = re.compile(r"^\|\|([^=]*)=(.*)$")
+
+    packet_document += f'packet_time: "{date_from_timestamp(packet.time)}"'
+
     try:
         while True:
-            name = stream_packets__encode__get_token(packet_decoded_iter)
-            next(packet_decoded_iter)
-            while True:
-                packet_var = stream_packets__encode__get_token(
-                    packet_decoded_iter)
-                packet_val = stream_packets__encode__get_token(
-                    packet_decoded_iter)
-                packet_object[f"{name}__{packet_var}"] = packet_val
-                packet_decoded_iter, copy = itertools.tee(packet_decoded_iter)
-                if next(copy) == "|":
-                    next(packet_decoded_iter)
-                    break
+            statement = stream_packets__encode__get(packet_iterlines)
+            packet_document += "\n"
+
+            # List List flag
+            match = re_list_list_flag.match(statement)
+            if match:
+                if not reading_list_list:
+                    level_level_flag = level
+                    reading_list_list = True
+                else:
+                    level = level_level_flag
+                packet_document += f"{indent(level+1)}{match.groups()[0]}:"
+                continue
+
+            # List List header
+            match = re_list_list_header.match(statement)
+            if match:
+                packet_document += f"{indent(level+2)}- {match.groups()[0]}:"
+                continue
+
+            # List List body
+            match = re_list_list_body.match(statement)
+            if match:
+                key, val = match.groups()
+                val = string_escape(val)
+                packet_document += f"{indent(level+4)}{key}: {val}"
+                continue
+
+            # List flag
+            match = re_list_flag.match(statement)
+            if match:
+                if not reading_list:
+                    level_flag = level
+                    reading_list = True
+                else:
+                    level = level_flag
+                packet_document += f"{indent(level+1)}{match.groups()[0]}:"
+                continue
+
+            # List header
+            match = re_list_header.match(statement)
+            if match:
+                packet_document += f"{indent(level+2)}- {match.groups()[0]}:"
+                continue
+
+            # List body
+            match = re_list_body.match(statement)
+            if match:
+                key, val = match.groups()
+                val = string_escape(val)
+                packet_document += f"{indent(level+4)}{key}: {val}"
+                continue
+
+            # Base header
+            match = re_base_header.match(statement)
+            if match:
+                level = 0
+                level_flag = 1
+                packet_document += f"{indent(level)}{match.groups()[0]}:"
+                reading_list = False
+                continue
+
+            # Base body
+            match = re_base_body.match(statement)
+            if match:
+                key, val = match.groups()
+                val = string_escape(val)
+                packet_document += f"{indent(level+1)}{key}: {val}"
+                reading_list = False
+                continue
     except StopIteration:
         pass
-    return packet_object
+    return yaml.load(packet_document)
 
 
-def stream_packets__encode__get_token(packet_decoded_iter) -> str:
-    """Get a token from the decoded packet."""
-    token: str = ""
-    last_char: str = ""
-    reading_bracket: bool = False
-    reading_quote: bool = False
-    for char in packet_decoded_iter:
-        if last_char != "\\":
-            if char in ("'", "'"):
-                reading_quote = not reading_quote
-                continue
-            if char in ("[", "]"):
-                reading_bracket = not reading_bracket
-                continue
-            if char in ("<", ">"):
-                continue
-        if not reading_quote and not reading_bracket and char in (" ", "="):
-            break
-        last_char = char
-        token += char
-    return token
+def stream_packets__encode__get(
+        packet_iterlines: Iterator[str]) -> str:
+    """Return a formated statement from the iterable."""
+    char_last: str = ""
+    statement: str = ""
+    inside_single_quote: bool = False
+    for char in next(packet_iterlines):
+        if char_last != "\\":
+            if char == "'":
+                inside_single_quote = not inside_single_quote
+        if not inside_single_quote and char in (" ",):
+            continue
+        statement += char
+        char_last = char
+    return statement
 
 
 def main():
@@ -88,6 +207,7 @@ def main():
         help="Packet Capture file.")
     args = parser.parse_args()
 
+    # stream the packets in the pcap file
     stream_packets(args.file)
 
 
