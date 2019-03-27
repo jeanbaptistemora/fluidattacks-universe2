cedd1d1728a1a3d52d19b5beec4eb1306800a6c0
Andres Cuberos <acuberos@fluidattacks.com>
2019-03-27 15:52:46 -0500

    fix(build): #171 upgrade moodle to v3.6
    - Change DB parameters and update tables to upgrade Moodle to
      the newest version (3.6.3).
    - Import updated DB Instance and Parameter Groups to Terraform
      files.
    - Use Debian Buster as the base image for the Exams and VPN
      containers since it is the stable distribution.
    
    
    Former-commit-id: f81242e46359fd8eff65cfc6492cff6875e497d7

--- infrastructure/database/rds.tf
+++ infrastructure/database/rds.tf
@@ -24,30 +24,58 @@ resource "aws_db_parameter_group" "default" {
   }
 }
 
-resource "aws_db_instance" "fluiddb" {
-  apply_immediately = true
-  allocated_storage    = 10
-  storage_type         = "${var.storageType}"
-  engine               = "${var.engine}"
-  engine_version       = "${var.engineVersion}"
-  instance_class       = "${var.instanceClass}"
-  name                 = "${var.dbName}"
-  username             = "${var.dbUser}"
-  password             = "${var.dbPass}"
-  db_subnet_group_name = "${aws_db_subnet_group.default.id}"
-  parameter_group_name = "${aws_db_parameter_group.default.id}"
-  snapshot_identifier = "${var.dbSnapId}"
-  skip_final_snapshot  = true
-  publicly_accessible = true
-  maintenance_window = "sun:05:00-sun:05:30"
-  backup_retention_period = 7
-  backup_window = "06:00-06:30"
-  lifecycle {
-    ignore_changes = ["snapshot_identifier"]
+resource "aws_db_parameter_group" "moodle-v36" {
+  name   = "moodle-v36"
+  family = "mysql5.6"
+  description = "Parameter Group to upgrade Moodle tp 3.6"
+
+  parameter {
+    name  = "character_set_server"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "collation_server"
+    value = "utf8mb4_unicode_ci"
+  }
+
+  parameter {
+    name  = "innodb_file_format"
+    value = "barracuda"
+  }
+
+  parameter {
+    name  = "innodb_large_prefix"
+    value = 1
+  }
+
+  parameter {
+    name  = "skip-character-set-client-handshake"
+    value = 1
   }
 }
 
+resource "aws_db_instance" "fluid-database" {
+  allocated_storage         = 10
+  apply_immediately         = true
+  backup_retention_period   = 7
+  backup_window             = "06:00-06:30"
+  db_subnet_group_name      = "${aws_db_subnet_group.default.id}"
+  engine                    = "${var.engine}"
+  engine_version            = "${var.engineVersion}"
+  final_snapshot_identifier = "fluid-database-last-snapshot"
+  identifier                = "fluid-database"
+  instance_class            = "${var.instanceClass}"
+  maintenance_window        = "sun:05:00-sun:05:30"
+  name                      = "${var.dbName}"
+  parameter_group_name      = "${aws_db_parameter_group.moodle-v36.id}"
+  password                  = "${var.dbPass}"
+  publicly_accessible       = true
+  skip_final_snapshot       = false
+  username                  = "${var.dbUser}"
+}
+
 output "endpoint" {
-  value = "${aws_db_instance.fluiddb.address}"
+  value = "${aws_db_instance.fluid-database.address}"
 
 }
