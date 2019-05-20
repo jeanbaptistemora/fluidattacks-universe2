100f21faac6e35cae7d20997622d4d546834c6d0
Oscar Prado <oprado@fluidattacks.com>
2019-05-20 18:04:17 -0500

    fix(front): #1930.8 fix crawling issues
    - fixed missing closure tags in sitemap
    
    
    Former-commit-id: b5f89adde0060853642f515f2ca797a905fbebc6

--- xmlcombine.sh
+++ xmlcombine.sh
@@ -7,7 +7,7 @@
 cp sitemap.xml output/sitemap.xml
 
 # Append all the URLs of the english subsite to the main file without the XML headers and the closing tag
-tail -n +6 output/web/en/sitemap.xml | head -n -1 >> output/sitemap.xml
+tail -n +6 output/web/en/sitemap.xml >> output/sitemap.xml
 
 # With the main file complete, remove the incomplete files
 rm output/web/en/sitemap.xml
