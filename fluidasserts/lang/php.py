dce60c5cae6256273c0f0a659ae9b15b1bf44612
kamadoatfluid <kamado@fluidattacks.com>
2019-08-05 10:36:58 -0500

    feat(back): #649 migrate php checks

--- fluidasserts/lang/php.py
+++ fluidasserts/lang/php.py
@@ -6,14 +6,14 @@
 # None
 
 # 3rd party imports
-from pyparsing import (CaselessKeyword, Literal, oneOf, Regex)
+from pyparsing import Keyword, oneOf, Regex
+
 # local imports
+from fluidasserts import Result
+from fluidasserts import HIGH
+from fluidasserts import OPEN, CLOSED
 from fluidasserts.helper import lang
-from fluidasserts import show_close
-from fluidasserts import show_open
-from fluidasserts import show_unknown
-from fluidasserts.utils.generic import get_sha256
-from fluidasserts.utils.decorators import track, level, notify
+from fluidasserts.utils.decorators import api
 
 
 LANGUAGE_SPECS = {
@@ -24,38 +24,24 @@ LANGUAGE_SPECS = {
 }  # type: dict
 
 
-@notify
-@level('low')
-@track
-def has_preg_ce(php_dest: str, exclude: list = None) -> bool:
+@api(risk=HIGH)
+def has_preg_ce(php_dest: str, exclude: list = None) -> Result:
     """
     Search for preg_replace calls with '/e'.
 
     :param php_dest: Path to a PHP script or package.
     :param exclude: Paths that contains any string from this list are ignored.
     """
-    tk_preg_func = CaselessKeyword('preg_replace')
-    tk_quotes = oneOf(["'", '"'])
-    tk_regex = Regex(r'.*/e\b')
+    quote = oneOf(["'", '"'])
+    grammar = Keyword('preg_replace') + '(' + quote + Regex(r'.*/e\b') + quote
 
-    tk_preg_rce = tk_preg_func + Literal('(') + tk_quotes + tk_regex + \
-        tk_quotes
-
-    result = False
-    try:
-        matches = lang.check_grammar(tk_preg_rce, php_dest,
-                                     LANGUAGE_SPECS, exclude)
-        if not matches:
-            show_close('Code does not allow RCE using preg_replace()',
-                       details=dict(code_dest=php_dest,
-                                    fingerprint=get_sha256(php_dest)))
-            return False
-    except FileNotFoundError:
-        show_unknown('File does not exist', details=dict(code_dest=php_dest))
-        return False
-    else:
-        result = True
-        show_open('Code may allow RCE using preg_replace()',
-                  details=dict(matched=matches,
-                               total_vulns=len(matches)))
-    return result
+    return lang.generic_method(
+        path=php_dest,
+        gmmr=grammar,
+        func=lang.path_contains_grammar2,
+        msgs={
+            OPEN: 'Code may allow RCE using preg_replace()',
+            CLOSED: 'Code does not allow RCE using preg_replace()',
+        },
+        spec=LANGUAGE_SPECS,
+        excl=exclude)
--- test/test_lang_php.py
+++ test/test_lang_php.py
@@ -17,7 +17,6 @@ CODE_DIR = 'test/static/lang/php/'
 SECURE_CODE = CODE_DIR + 'safe_code.php'
 INSECURE_CODE = CODE_DIR + 'vuln_code.php'
 NON_EXISTANT_CODE = CODE_DIR + 'not_exists.php'
-LINES_FORMAT = 'lines: '
 
 
 #
@@ -26,12 +25,9 @@ LINES_FORMAT = 'lines: '
 
 def test_has_preg_rce_open():
     """Code uses unsafe preg_replace."""
-    assert php.has_preg_ce(INSECURE_CODE)
+    assert php.has_preg_ce(INSECURE_CODE).is_open()
+    assert php.has_preg_ce(CODE_DIR).is_open()
 
-
-def test_has_preg_rce_in_dir_open():
-    """Code uses unsafe preg_replace."""
-    assert php.has_preg_ce(CODE_DIR)
 
 #
 # Closing tests
@@ -40,6 +36,15 @@ def test_has_preg_rce_in_dir_open():
 
 def test_has_preg_rce_close():
     """Code uses unsafe preg_replace."""
-    assert not php.has_preg_ce(SECURE_CODE)
-    assert not php.has_preg_ce(NON_EXISTANT_CODE)
-    assert not php.has_preg_ce(CODE_DIR, exclude=['test'])
+    assert php.has_preg_ce(SECURE_CODE).is_closed()
+    assert php.has_preg_ce(CODE_DIR, exclude=['test']).is_closed()
+
+
+#
+# Unknown tests
+#
+
+
+def test_has_preg_rce_close():
+    """Code uses unsafe preg_replace."""
+    assert php.has_preg_ce(NON_EXISTANT_CODE).is_unknown()
