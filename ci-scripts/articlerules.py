9b5445b1c784e672c55cc78c6d2c313b0f96ddd9
Oscar Prado <oprado@fluidattacks.com>
2019-08-14 09:04:50 -0500

    refac(build): #2245.4 pylint articlerules.py
    Modified articlerules.py to comply with pylint
    
    
    Former-commit-id: 6d3757991a95ea4a8088bc49ea3c5c95819fa85b

--- ci-scripts/articlerules.py
+++ ci-scripts/articlerules.py
@@ -4,74 +4,77 @@
 Script that checks structure and rules in blog entries
 Replaces check-articles.sh
 Author: Oscar Eduardo Prado oprado@fluidattacks.com
-Version 1.2
-Patch notes 1.2:
-- Removed spanish related linters
+Version 1.3
+Patch notes 1.3:
+- Comply with pylint
 """
 
 import os
 import print_helper
 
-def artchecker(PATH, EXIT_CODE):
+def artchecker(path, exit_code):
+    """
+    Define checks for blog articles
+    """
 
-  #check that every article has valid category
-  ARTCAT = os.popen("pcregrep -o '(?<=^:category:\s).*$' "+PATH).read()
-  ARTCAT = ARTCAT.replace('\n', '')
-  if "blog" in PATH:
-    OUT = os.popen("pcregrep  '"+ARTCAT+"' categories.lst ").read()
-  if not len(OUT):
-    print_helper.print_failure("Issue found in "+PATH+" \n")
-    print_helper.print_warning(ARTCAT+" does not match any valid category " \
-                               "in categories list file.\n\n")
-    EXIT_CODE = 1
+    #check that every article has valid category
+    art_cat = os.popen("pcregrep -o '(?<=^:category: ).*$' "+path).read()
+    art_cat = art_cat.replace('\n', '')
+    if "blog" in path:
+        out = os.popen("pcregrep  '"+art_cat+"' categories.lst ").read()
+    if not out:
+        print_helper.print_failure("Issue found in "+path+" \n")
+        print_helper.print_warning(art_cat+" does not match any valid category " \
+                                   "in categories list file.\n\n")
+        exit_code = 1
 
-  #check that every article has valid tags
-  ARTTAGS = os.popen("pcregrep -o '(?<=^:tags:\s).*$' "+PATH).read()
-  ARTTAGS = ARTTAGS.replace('\n', '')
-  ARTTAGS = ARTTAGS.split(", ")
-  for TAG in ARTTAGS:
-    if "blog/" in PATH:
-      OUT = os.popen("pcregrep  '"+TAG+"' tags.lst ").read()
-    if not len(OUT):
-      print_helper.print_failure("Issue found in "+PATH+" \n")
-      print_helper.print_warning(TAG+" does not match any valid tag " \
-                               "in tags list file.\n\n")
-      EXIT_CODE = 1
+    #check that every article has valid tags
+    art_tags = os.popen("pcregrep -o '(?<=^:tags: ).*$' "+path).read()
+    art_tags = art_tags.replace('\n', '')
+    art_tags = art_tags.split(", ")
+    for tag in art_tags:
+        if "blog/" in path:
+            out = os.popen("pcregrep  '"+tag+"' tags.lst ").read()
+        if not out:
+            print_helper.print_failure("Issue found in "+path+" \n")
+            print_helper.print_warning(tag+" does not match any valid tag " \
+                                      "in tags list file.\n\n")
+            exit_code = 1
 
-  #Check that every article in blog has a valid title lenght
-  OUT = os.popen("pcregrep -no '(?<=^=\s).{37,}' "+PATH).read()
-  if len(OUT) > 0:
-    print_helper.print_failure("Issue found in "+PATH+"\n")
-    print_helper.print_failure(OUT)
-    print_helper.print_warning("The title lenght exceeds 35 characters. "\
-                               "Please correct the file and try again.\n\n")
-    EXIT_CODE = 1
+    #Check that every article in blog has a valid title lenght
+    out = os.popen("pcregrep -no '(?<=^= ).{37,}' "+path).read()
+    if out:
+        print_helper.print_failure("Issue found in "+path+"\n")
+        print_helper.print_failure(out)
+        print_helper.print_warning("The title lenght exceeds 35 characters. "\
+                                   "Please correct the file and try again.\n\n")
+        exit_code = 1
 
-  #Check that every article in blog has a subtitle defined
-  OUT = os.popen("pcregrep -L '^:subtitle:' "+PATH).read()
-  if len(OUT) > 0:
-    print_helper.print_failure("Issue found in "+PATH+"\n")
-    print_helper.print_warning("The attributte \"subtitle\" must be defined. "\
-                               "Please correct the file and try again.\n\n")
-    EXIT_CODE = 1
+    #Check that every article in blog has a subtitle defined
+    out = os.popen("pcregrep -L '^:subtitle:' "+path).read()
+    if out:
+        print_helper.print_failure("Issue found in "+path+"\n")
+        print_helper.print_warning("The attributte \"subtitle\" must be defined. "\
+                                   "Please correct the file and try again.\n\n")
+        exit_code = 1
 
-  #Check that every article in blog has a valid subtitle lenght
-  OUT = os.popen("pcregrep -o '(?<=^:subtitle: ).{56,}' "+PATH).read()
-  if len(OUT) > 0:
-    print_helper.print_failure("Issue found in "+PATH+"\n")
-    print_helper.print_failure(OUT)
-    print_helper.print_warning("The subtitle lenght exceeds 55 characters. "\
-                               "Please correct the file and try again.\n\n")
-    EXIT_CODE = 1
+    #Check that every article in blog has a valid subtitle lenght
+    out = os.popen("pcregrep -o '(?<=^:subtitle: ).{56,}' "+path).read()
+    if out:
+        print_helper.print_failure("Issue found in "+path+"\n")
+        print_helper.print_failure(out)
+        print_helper.print_warning("The subtitle lenght exceeds 55 characters. "\
+                                   "Please correct the file and try again.\n\n")
+        exit_code = 1
 
-  # Check that articles have alt description for their featured images
-  OUT = os.popen("pcregrep -L  '^:alt:' "+PATH).read()
-  if len(OUT) > 0:
-    print_helper.print_failure("Issue found in "+PATH+"\n")
-    print_helper.print_failure(OUT)
-    print_helper.print_warning("The articles must have the \"alt\" metadata "\
-                               "set for their representative image. "\
-                               "Please correct the file and try again.\n\n")
-    EXIT_CODE = 1
+    # Check that articles have alt description for their featured images
+    out = os.popen("pcregrep -L  '^:alt:' "+path).read()
+    if out:
+        print_helper.print_failure("Issue found in "+path+"\n")
+        print_helper.print_failure(out)
+        print_helper.print_warning("The articles must have the \"alt\" metadata "\
+                                   "set for their representative image. "\
+                                   "Please correct the file and try again.\n\n")
+        exit_code = 1
 
-  return EXIT_CODE
+    return exit_code
