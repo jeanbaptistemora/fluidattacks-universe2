27b00114594e3373ad84913c08f82d7423b08f47
kamadoatfluid <kamado@fluidattacks.com>
2019-08-14 10:06:54 -0500

    feat(back): #0 easier aws import

--- fluidasserts/cloud/aws/__init__.py
+++ fluidasserts/cloud/aws/__init__.py
@@ -1,3 +1,15 @@
 # -*- coding: utf-8 -*-
 
 """Fluid Asserts AWS cloud package."""
+
+# Imported but unused
+# flake8: noqa
+
+from fluidasserts.cloud.aws import cloudfront
+from fluidasserts.cloud.aws import cloudtrail
+from fluidasserts.cloud.aws import ec2
+from fluidasserts.cloud.aws import generic
+from fluidasserts.cloud.aws import iam
+from fluidasserts.cloud.aws import rds
+from fluidasserts.cloud.aws import redshift
+from fluidasserts.cloud.aws import s3
