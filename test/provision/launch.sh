7f560dc06015b1069b63f3f8a737d62a10d4bc15
kamadoatfluid <kamado@fluidattacks.com>
2019-08-26 14:50:45 -0500

    feat(back): #716 remove unneeded files
    - Remove some unused files and generate others on-the-fly
    - no-deltas-check
    
    Closes #716
    
    
    Former-commit-id: 0cfd697c42c41876c5a68099b397318978c534d1

---
+++ test/provision/launch.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+
+module="${1}"
+context="${2}"
+exposed_port="${3}"
+
+tag_name="registry.gitlab.com/fluidattacks/asserts/mocks/${module}:${context}"
+context_path="./test/provision/${module}/${context}/"
+
+{
+  test "${module}" && test "${context}" && test "${exposed_port}"
+} || {
+  echo "Use example: ${0} [module] [context] [port]";
+  echo "Use example: ${0} postgresql hard 5432";
+  echo "Use example: ${0} postgresql weak 5432";
+  echo "Use example: ${0} ssl hard 443";
+  exit 1;
+}
+
+docker build --tag "${tag_name}" "${context_path}" \
+&& docker run \
+    --rm \
+    --tty \
+    --interactive  \
+    --publish ${exposed_port}:${exposed_port} \
+    --name ${module} \
+  "${tag_name}"
