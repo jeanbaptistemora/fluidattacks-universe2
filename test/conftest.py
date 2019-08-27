4f470167328d3070f304a811646e6809387a142d
kamadoatfluid <kamado@fluidattacks.com>
2019-08-27 10:07:31 -0500

    feat(job): #718 smart cache image and container
    - Smart cache the image if not changed
    - Smart cache the container if exist and not changed
    - Add a lot of static typing
    
    Closes #718
    
    
    Former-commit-id: c48bd0ab66cf7a31bbabbc8aea4d2a03271bd5df

--- test/conftest.py
+++ test/conftest.py
@@ -5,8 +5,8 @@
 # standard imports
 from __future__ import print_function
 import os
-import sys
 import time
+from typing import Optional
 from multiprocessing import Process, cpu_count
 from multiprocessing.pool import Pool
 
@@ -14,6 +14,7 @@ from multiprocessing.pool import Pool
 import docker
 import pytest
 import wait
+from docker.models.containers import Image, Container
 
 # local imports
 from test.mock import graphqlserver
@@ -21,7 +22,7 @@ from test.mock import httpserver
 from test.mock import sip
 
 # Constants
-NETWORK_NAME = 'bridge'
+CLIENT = docker.from_env()
 
 MOCKS = [
     # These need to be built first
@@ -57,65 +58,109 @@ MOCKS = [
 ]
 
 
-def get_docker_client():
-    """Get docker client."""
-    client = docker.from_env()
-    client.login(registry='registry.gitlab.com',
-                 username=os.environ['DOCKER_USER'],
-                 password=os.environ['DOCKER_PASS'])
-    return client
-
-
-def get_mock_name(mock):
-    """Get mock IP."""
-    try:
-        mock_name = '{}_{}'.format(mock.replace(':', '_'),
-                                   os.environ['CI_COMMIT_REF_NAME'])
-    except KeyError:
-        print('CI_COMMIT_REF_NAME not in environ')
-        sys.exit(-1)
+def get_mock_name(mock: str) -> str:
+    """Get mock name."""
+    branch = os.environ.get('CI_COMMIT_REF_NAME', 'test')
+    mock_name = f'{mock.replace(":", "_")}_{branch}'
     return mock_name
 
 
-def get_ip(con):
+def get_container_ip(cont: Container) -> str:
     """Get mock IP."""
-    return con.attrs['NetworkSettings']['Networks']\
-        ['bridge']['IPAddress']
+    return cont.attrs['NetworkSettings']['Networks']['bridge']['IPAddress']
 
 
-def start_container(mock) -> None:
-    """Run a container and build if necessary."""
-    client = get_docker_client()
+def image_to_digest(image: Image) -> int:
+    """Return an image digest from the Image object."""
+    # Repositories have the digest, but local images don't, let's compute one
+    #   https://windsock.io/explaining-docker-image-ids/
+    entries: tuple = tuple(image.attrs.get('RootFS', {}).get('Layers', []))
+    return hash(entries)
+
+
+def get_existing_image(image: str) -> Optional[int]:
+    """Return the image digest or None if the image does not exist."""
+    try:
+        print(f'Get image: {image}')
+        img_obj = CLIENT.images.get(image)
+    except docker.errors.NotFound:
+        print(f'Image not found: {image}')
+        return None
+    return image_to_digest(img_obj)
+
+
+def build_image(image: str, context: str) -> Optional[int]:
+    """Build an image and return its digest or None if the build fails."""
+    try:
+        print(f'Build image: {image}')
+        img_obj, _ = CLIENT.images.build(tag=image, path=context)
+    except docker.errors.BuildError as exc:
+        print(f'Build image failed: {image} {exc}')
+        return None
+    return image_to_digest(img_obj)
+
+
+def start_container(image: str, mock_name: str) -> None:
+    """Start an existing container or create a new one from the given image."""
+    try:
+        cont = CLIENT.containers.get(mock_name)
+        print(f'Start existing container: {mock_name}')
+        cont.start()
+    except docker.errors.NotFound:
+        try:
+            print(f'Start fresh container: {mock_name}')
+            CLIENT.containers.run(image, name=mock_name, tty=True, detach=True)
+        except docker.errors.APIError as exc:
+            print(f'Start container failed: {mock_name}')
+            pytest.fail(str(exc))
+
+
+def smart_create_container(mock: str) -> None:
+    """Build mocks caching imgs/containers and building/running when needed."""
     image = f'registry.gitlab.com/fluidattacks/asserts/mocks/{mock}'
-    mock_dir = f'test/provision/{mock.replace(":", "/")}'
+    context = f'test/provision/{mock.replace(":", "/")}'
 
     mock_name = get_mock_name(mock)
 
-    try:
-        cont = client.containers.get(mock_name)
-        cont.start()
-    except docker.errors.NotFound:
-        client.images.build(path=mock_dir, tag=image)
-        try:
-            client.containers.run(image, name=mock_name, tty=True, detach=True)
-        except docker.errors.APIError:
-            cont = client.containers.get(mock_name)
-            cont.start()
+    # Get the current image digest in the registry
+    img_digest = get_existing_image(image)
+
+    # Build the image to get the current hash in the repository
+    # if not changes has been made, then it'll be entirely get from cache
+    new_img_digest = build_image(image, context)
+
+    if not new_img_digest:
+        # Build failed
+        pytest.fail()
+
+    # If the image in the registry is different from the one in the repository
+    if not img_digest == new_img_digest:
+        # Invalidate the existing container because it comes from an old image
+        stop_container(mock_name, remove=True)
+
+    # Start a container, create it if not exists
+    start_container(image, mock_name)
 
 
-def open_ports(mock, port_mapping) -> None:
+def open_ports(mock: str, port_mapping) -> None:
     """Open the TCP ports needed for a container."""
-    client = get_docker_client()
-    ip = get_ip(client.containers.get(get_mock_name(mock)))
+    ip = get_container_ip(CLIENT.containers.get(get_mock_name(mock)))
     for value in port_mapping.values():
         wait.tcp.open(value, ip, timeout=30)
 
 
-def stop_container(mock) -> None:
-    """Stop a container."""
-    client = get_docker_client()
-    cont = client.containers.get(get_mock_name(mock))
-    cont.stop(timeout=0)
+def stop_container(mock_name: str, remove: bool = False) -> None:
+    """Stop a container removing it optionally."""
+    try:
+        cont = CLIENT.containers.get(mock_name)
+    except docker.errors.NotFound:
+        print(f'Container not found: {mock_name}')
+    else:
+        print(f'Stop container: {mock_name}')
+        cont.stop(timeout=0)
+        if remove:
+            print(f'Remove container: {mock_name}')
+            cont.remove(force=True)
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -150,7 +195,7 @@ def run_mocks(request):
     """Run mock with given parameters."""
     with Pool(processes=cpu_count()) as workers:
         for mocks in MOCKS:
-            workers.map(start_container, tuple(mocks.keys()), 1)
+            workers.map(smart_create_container, tuple(mocks.keys()), 1)
             workers.starmap(open_ports, tuple(mocks.items()), 1)
 
 
@@ -159,15 +204,15 @@ def stop_mocks(request):
     """Stop mocks mock with given parameters."""
     with Pool(processes=cpu_count()) as workers:
         for mocks in MOCKS:
-            workers.map(stop_container, tuple(mocks.keys()), 1)
+            mock_names: tuple = tuple(map(get_mock_name, mocks.keys()))
+            workers.map(stop_container, mock_names, 1)
 
 
 @pytest.fixture(scope='function')
 def get_mock_ip(request):
     """Run mock with given parameters."""
     mock = request.param
-    client = docker.from_env()
-    con = client.containers.get(get_mock_name(mock))
+    con = CLIENT.containers.get(get_mock_name(mock))
     if con.status != 'running':
         con.start()
-    yield get_ip(con)
+    yield get_container_ip(con)
