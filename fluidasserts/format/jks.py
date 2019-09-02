d668937221b55d835e6efc871437cc0375cd5ebc
kamadoatfluid <kamado@fluidattacks.com>
2019-09-02 12:38:46 -0500

    feat(back): #649 migrate jks checks
    Former-commit-id: dd7b328af1ca6158710b088696860170fc0eb4f7

--- fluidasserts/format/jks.py
+++ fluidasserts/format/jks.py
@@ -5,55 +5,52 @@
 
 # standard imports
 import os
+from typing import List
 
 # 3rd party imports
 import jks
 
 # local imports
-from fluidasserts import show_open
-from fluidasserts import show_close
-from fluidasserts import show_unknown
-from fluidasserts.utils.generic import get_sha256
-from fluidasserts.utils.generic import get_paths
-from fluidasserts.utils.decorators import track, level, notify
+from fluidasserts import Unit, SAST, HIGH, CLOSED, OPEN, UNKNOWN
+from fluidasserts.utils.generic import get_paths, get_sha256
+from fluidasserts.utils.decorators import api
 
 
-@notify
-@level('high')
-@track
-def has_no_password_protection(path: str) -> bool:
+@api(risk=HIGH, kind=SAST)
+def has_no_password_protection(path: str) -> tuple:
     """
     Check if .jks files are password protected.
 
     :param path: path to check
+    :rtype: :class:`fluidasserts.Result`
     """
     if not os.path.exists(path):
-        show_unknown('Path does not exist',
-                     details=dict(path=path))
-        return False
-    jks_with_password: list = []
-    jks_without_password: list = []
+        return UNKNOWN, 'Path does not exist'
+
+    msg_open: str = 'JKS is not password protected'
+    msg_closed: str = 'JKS is password protected'
+
+    safes: List[Unit] = []
+    vulns: List[Unit] = []
     for full_path in get_paths(path, endswith=('.jks',)):
+        vulnerable: bool = True
         try:
             jks.KeyStore.load(full_path, '')
         except jks.util.KeystoreSignatureException:
-            # has password
-            jks_with_password.append(dict(path=full_path,
-                                          sha256=get_sha256(full_path)))
-        else:
-            # has not password
-            jks_without_password.append(dict(path=full_path,
-                                             sha256=get_sha256(full_path)))
-    if jks_without_password:
-        show_open('JKS is/are not password protected',
-                  details=dict(jks_without_password=jks_without_password))
-        return True
-    show_close('JKS is/are password protected',
-               details=dict(jks_with_password=jks_with_password))
-    return False
+            vulnerable = False
+
+        (vulns if vulnerable else safes).append(
+            Unit(where=path,
+                 source='JKS/Password',
+                 specific=[msg_open if vulnerable else msg_closed],
+                 fingerprint=get_sha256(path)))
+
+    if vulns:
+        return OPEN, msg_open, vulns, safes
+    return CLOSED, msg_closed, vulns, safes
 
 
-def _use_passwords(path: str, passwords: list) -> bool:
+def _use_passwords(path: str, passwords: list) -> tuple:
     """
     Check if a JKS file has been protected by any of ``passwords``.
 
@@ -61,16 +58,18 @@ def _use_passwords(path: str, passwords: list) -> bool:
     :param passwords: passwords to test
     """
     if not os.path.exists(path):
-        show_unknown('Path does not exist',
-                     details=dict(path=path))
-        return False
+        return UNKNOWN, 'Path does not exist'
 
-    opened_jks: list = []
-    closed_jks: list = []
+    msg_open: str = 'JKS is protected by a weak password'
+    msg_closed: str = 'JKS is protected by a strong password'
+
+    safes: List[Unit] = []
+    vulns: List[Unit] = []
+
     passwords = ['', *(p for p in set(passwords))]
 
     for full_path in get_paths(path, endswith=('.jks',)):
-        success: bool = False
+        vulnerable: bool = False
         for password in passwords:
             try:
                 jks.KeyStore.load(full_path, password)
@@ -79,47 +78,39 @@ def _use_passwords(path: str, passwords: list) -> bool:
                 continue
             else:
                 # correct password
-                success = True
+                vulnerable = True
                 break
-        if success:
-            opened_jks.append(dict(path=full_path,
-                                   password=password,
-                                   sha256=get_sha256(full_path)))
-        else:
-            closed_jks.append(dict(path=full_path,
-                                   sha256=get_sha256(full_path)))
-    if opened_jks:
-        show_open('JKS is/are protected by a password from the list',
-                  details=dict(opened_jks=opened_jks,
-                               tested_passwords=passwords))
-        return True
-    show_close('JKS is/are protected by a password from the list',
-               details=dict(closed_jks=closed_jks,
-                            tested_passwords=passwords))
-    return False
+
+        (vulns if vulnerable else safes).append(
+            Unit(where=path,
+                 source='JKS/Password',
+                 specific=[msg_open if vulnerable else msg_closed],
+                 fingerprint=get_sha256(path)))
+
+    if vulns:
+        return OPEN, msg_open, vulns, safes
+    return CLOSED, msg_closed, vulns, safes
 
 
-@notify
-@level('high')
-@track
-def use_password(path: str, password: str) -> bool:
+@api(risk=HIGH, kind=SAST)
+def use_password(path: str, password: str) -> tuple:
     """
     Check if a JKS file has been protected by ``password``.
 
     :param path: path to check
     :param password: password to test
+    :rtype: :class:`fluidasserts.Result`
     """
     return _use_passwords(path, [password])
 
 
-@notify
-@level('high')
-@track
-def use_passwords(path: str, passwords: list) -> bool:
+@api(risk=HIGH, kind=SAST)
+def use_passwords(path: str, passwords: list) -> tuple:
     """
     Check if a JKS file has been protected by any of ``passwords``.
 
     :param path: path to check
     :param passwords: passwords to test
+    :rtype: :class:`fluidasserts.Result`
     """
     return _use_passwords(path, passwords)
