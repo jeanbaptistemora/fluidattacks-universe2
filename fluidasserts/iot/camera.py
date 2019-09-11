34831b0c4fbeb41bab947a490439b56fd10114dd
Andres Roldan <aroldan@fluidattacks.com>
2019-09-11 14:30:31 -0500

    refac(back): #649 migrated camera checks
    Former-commit-id: b0e9db94ea3eabda30c66d63ec64f80581018cbf

--- fluidasserts/iot/camera.py
+++ fluidasserts/iot/camera.py
@@ -9,36 +9,36 @@
 # None
 
 # local imports
-from fluidasserts import show_close
-from fluidasserts import show_open
-from fluidasserts import show_unknown
+from fluidasserts import HIGH, DAST
 from fluidasserts.helper import http
-from fluidasserts.utils.decorators import track, level, notify
+from fluidasserts.utils.decorators import api, unknown_if
+
+# pylint: disable=protected-access
 
 
-@notify
-@level('high')
-@track
-def axis_has_rce(url: str) -> bool:
+@api(risk=HIGH, kind=DAST)
+@unknown_if(http.ConnError)
+def axis_has_rce(url: str) -> tuple:
     """
     Check if Axis Communications MPQT/PACS Server has RCE.
 
     Taken from https://www.exploit-db.com/exploits/40125
 
-    :param cam_ip: IP or host of Axis camera.
+    :param url: URL of Axis camera.
+    :returns: - ``OPEN`` if camera is vulnerable to RCE.
+              - ``UNKNOWN`` on errors.
+              - ``CLOSED`` otherwise.
+    :rtype: :class:`fluidasserts.Result`
     """
-    try:
-        url = '{}/httpDisabled.shtml?&http_user=%p|%p'.format(url)
-        sess = http.HTTPSession(url)
-    except http.ConnError as exc:
-        show_unknown('Could not connect',
-                     details=dict(url=url,
-                                  reason=str(exc).replace(':', ',')))
-        return False
+    url = f'{url}/httpDisabled.shtml?&http_user=%p|%p'
+    sess = http.HTTPSession(url)
 
-    if sess.response.status_code == 500:
-        show_open('Axis camera vulnerable to RCE', details=dict(url=url))
-        return True
-    show_close('Axis camera not vulnerable to RCE',
-               details=dict(url=url))
-    return False
+    sess._set_messages(
+        source='Camera/HTTPServer',
+        msg_open='Axis camera vulnerable to RCE',
+        msg_closed='Axis camera not vulnerable to RCE'
+    )
+
+    sess._add_unit(is_vulnerable=sess.response.status_code == 500)
+
+    return sess._get_tuple_result()
