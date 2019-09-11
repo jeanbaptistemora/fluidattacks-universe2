844d181a33e8148b9f88f00822fca81eb7d54fbc
kamadoatfluid <kamado@fluidattacks.com>
2019-09-11 07:56:45 -0500

    feat(job): #228 use one table and add typing
    - Now int fields will be integers and date fields will be dates
    - Fusion all lines.csv in one table and add a column for the
      subscription name
    
    
    Former-commit-id: 5aa17f8837bdd07d4663ab7f7714db6dc0251173

--- analytics/continuous/streamer_toe.py
+++ analytics/continuous/streamer_toe.py
@@ -6,15 +6,40 @@ import re
 import csv
 import json
 import glob
+from datetime import datetime
+from typing import Any, Callable, List, Tuple
+
+
+def normalize_date(date_str: str) -> str:
+    """Return a RFC 3339 date."""
+    lines_format, rfc3339_format = '%Y-%m-%d', '%Y-%m-%dT%H:%M:%SZ'
+    return datetime.strptime(date_str, lines_format).strftime(rfc3339_format)
 
 
 def stream_lines_csv(subs: str, lines_csv_path: str) -> None:
     """Streams the lines.csv file to stdout."""
+    lines_csv_fields: List[Tuple[str, Callable, Any]] = [
+        # field_name, field_type, field_default_value
+        ('filename', str, ''),
+        ('comments', str, ''),
+        ('modified-commit', str, ''),
+        ('loc', int, 0),
+        ('tested-lines', int, 0),
+        ('modified-date', normalize_date, '2000-01-01'),
+        ('tested-date', normalize_date, '2000-01-01'),
+    ]
+
     with open(lines_csv_path) as csv_file:
         for row in csv.DictReader(csv_file):
             print(json.dumps({
-                "stream": f'lines_{subs}',
-                "record": dict(row)
+                "stream": 'lines',
+                "record": {
+                    'subs': subs,
+                    **{
+                        field: function(row.get(field, default))
+                        for field, function, default in lines_csv_fields
+                    }
+                }
             }))
 
 
