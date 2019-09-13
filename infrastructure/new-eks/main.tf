06e7ddfd2e22d9d7e195fc3e28b966f3d43a8423
dsalazaratfluid <dsalazar@fluidattacks.com>
2019-09-13 12:35:23 -0500

    refac(back): #216.8 temporarily remove new eks
    - Temporarily remove new eks in order to
    modularize it
    
    
    Former-commit-id: d93c62d305196259e466f2907fc21af9a79983cb

--- infrastructure/new-eks/main.tf
+++ infrastructure/new-eks/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   version = ">= 2.11"
-  region  = var.region
+  region  = "us-east-1"
 }
 
 provider "random" {
@@ -17,40 +17,4 @@ provider "null" {
 
 provider "template" {
   version = "~> 2.1"
-}
-
-locals {
-  cluster_name = "eks-${random_string.suffix.result}"
-}
-
-resource "random_string" "suffix" {
-  length  = 4
-  special = false
-}
-
-module "eks" {
-  source       = "terraform-aws-modules/eks/aws"
-  cluster_name = local.cluster_name
-  subnets      = module.vpc.private_subnets
-  vpc_id       = module.vpc.vpc_id
-
-  worker_groups = [
-    {
-      name                          = "conventional-workers"
-      instance_type                 = "t3.nano"
-      asg_max_size                  = 2
-      asg_desired_capacity          = 2
-      tags = [{
-        key                 = "Name"
-        value               = "eks-conventional-worker"
-        propagate_at_launch = true
-      }]
-    }
-  ]
-
-  tags = {
-    Name       = local.cluster_name
-    GithubRepo = "terraform-aws-eks"
-    GithubOrg  = "terraform-aws-modules"
-  }
 }
--- infrastructure/new-eks/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "region" {
-  default = "us-east-1"
-}
-
-variable "availability-zone-names" {
-  type    = list(string)
-  default = ["us-east-1a", "us-east-1b", "us-east-1d"]
-}
