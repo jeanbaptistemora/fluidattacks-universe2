5ce574f667880118d723ae9e783a416a61e026a7
kamadoatfluid <kamado@fluidattacks.com>
2019-09-16 10:26:00 -0500

    feat(back): #649 migrate aws.s3
    Former-commit-id: 42044ded9c961c8e9ea3acea9c466f6e4662dda2

--- fluidasserts/cloud/aws/s3.py
+++ fluidasserts/cloud/aws/s3.py
@@ -2,108 +2,99 @@
 
 """AWS cloud checks (S3)."""
 
-# standard imports
-# None
-
 # 3rd party imports
-# None
+from botocore.exceptions import BotoCoreError
+from botocore.vendored.requests.exceptions import RequestException
 
 # local imports
-from fluidasserts import show_close
-from fluidasserts import show_open
-from fluidasserts import show_unknown
-from fluidasserts.utils.decorators import track, level, notify
+from fluidasserts import DAST, LOW, HIGH
 from fluidasserts.helper import aws
+from fluidasserts.cloud.aws import _get_result_as_tuple
+from fluidasserts.utils.decorators import api, unknown_if
 
 
-@notify
-@level('low')
-@track
+@api(risk=LOW, kind=DAST)
+@unknown_if(BotoCoreError, RequestException)
 def has_server_access_logging_disabled(
-        key_id: str, secret: str, retry: bool = True) -> bool:
+        key_id: str, secret: str, retry: bool = True) -> tuple:
     """
     Check if S3 buckets have server access logging enabled.
 
     :param key_id: AWS Key Id
     :param secret: AWS Key Secret
     """
-    try:
-        buckets = aws.run_boto3_func(key_id, secret, 's3',
-                                     'list_buckets',
-                                     param='Buckets',
-                                     retry=retry)
-    except aws.ConnError as exc:
-        show_unknown('Could not connect',
-                     details=dict(error=str(exc).replace(':', '')))
-        return False
-    except aws.ClientErr as exc:
-        show_unknown('Error retrieving info. Check credentials.',
-                     details=dict(error=str(exc).replace(':', '')))
-        return False
-    if not buckets:
-        show_close('Not S3 buckets were found')
-        return False
+    buckets = aws.run_boto3_func(key_id=key_id,
+                                 secret=secret,
+                                 service='s3',
+                                 func='list_buckets',
+                                 param='Buckets',
+                                 retry=retry)
 
-    result = []
-    for bucket in buckets:
-        logging = aws.run_boto3_func(key_id, secret, 's3',
-                                     'get_bucket_logging',
-                                     retry=retry,
-                                     Bucket=bucket['Name'])
-        if 'LoggingEnabled' not in logging:
-            result.append(bucket['Name'])
+    msg_open: str = 'Logging is disabled on buckets'
+    msg_closed: str = 'Logging is enabled on buckets'
 
-    if result:
-        show_open('Logging not enabled on buckets',
-                  details=dict(bucket=result))
-        return True
-    show_close('Logging enabled on buckets')
-    return False
+    vulns, safes = [], []
+
+    if buckets:
+        for bucket in buckets:
+            bucket_name = bucket['Name']
+            bucket_logging = aws.run_boto3_func(key_id=key_id,
+                                                secret=secret,
+                                                service='s3',
+                                                func='get_bucket_logging',
+                                                Bucket=bucket_name,
+                                                retry=retry)
+
+            bucket_logging_enabled = bool(bucket_logging.get('LoggingEnabled'))
+
+            (vulns if not bucket_logging_enabled else safes).append(
+                (bucket_name, 'must have logging enabled'))
+
+    return _get_result_as_tuple(
+        service='S3', objects='buckets',
+        msg_open=msg_open, msg_closed=msg_closed,
+        vulns=vulns, safes=safes)
 
 
-@notify
-@level('high')
-@track
+@api(risk=HIGH, kind=DAST)
+@unknown_if(BotoCoreError, RequestException)
 def has_public_buckets(
-        key_id: str, secret: str, retry: bool = True) -> bool:
+        key_id: str, secret: str, retry: bool = True) -> tuple:
     """
     Check if S3 buckets have public read access.
 
     :param key_id: AWS Key Id
     :param secret: AWS Key Secret
     """
-    try:
-        buckets = aws.run_boto3_func(key_id, secret, 's3',
-                                     'list_buckets',
-                                     param='Buckets',
-                                     retry=retry)
-    except aws.ConnError as exc:
-        show_unknown('Could not connect',
-                     details=dict(error=str(exc).replace(':', '')))
-        return False
-    except aws.ClientErr as exc:
-        show_unknown('Error retrieving info. Check credentials.',
-                     details=dict(error=str(exc).replace(':', '')))
-        return False
-    if not buckets:
-        show_close('Not S3 buckets were found')
-        return False
+    buckets = aws.run_boto3_func(key_id=key_id,
+                                 secret=secret,
+                                 service='s3',
+                                 func='list_buckets',
+                                 param='Buckets',
+                                 retry=retry)
 
-    public_buckets = []
+    msg_open: str = 'There are public buckets'
+    msg_closed: str = 'There are not public buckets'
 
-    for bucket in buckets:
-        grants = aws.run_boto3_func(key_id, secret, 's3',
-                                    'get_bucket_acl',
-                                    param='Grants',
-                                    retry=retry,
-                                    Bucket=bucket['Name'])
-        result = aws.get_bucket_public_grants(bucket['Name'], grants)
-        if result:
-            public_buckets.append(result)
+    vulns, safes = [], []
 
-    if public_buckets:
-        show_open('There are public buckets',
-                  details=dict(public_buckets=public_buckets))
-        return True
-    show_close('There are not public buckets')
-    return False
+    if buckets:
+        for bucket in buckets:
+            bucket_name = bucket['Name']
+            bucket_grants = aws.run_boto3_func(key_id=key_id,
+                                               secret=secret,
+                                               service='s3',
+                                               func='get_bucket_acl',
+                                               Bucket=bucket_name,
+                                               param='Grants',
+                                               retry=retry)
+
+            result = aws.get_bucket_public_grants(bucket_name, bucket_grants)
+
+            (vulns if result else safes).append(
+                (bucket_name, 'must not be publicly accessible'))
+
+    return _get_result_as_tuple(
+        service='S3', objects='buckets',
+        msg_open=msg_open, msg_closed=msg_closed,
+        vulns=vulns, safes=safes)
