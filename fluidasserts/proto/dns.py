d49c1e0bf0c1855083e022a1f473a0cb1cabec93
kamadoatfluid <kamado@fluidattacks.com>
2019-09-17 13:56:12 -0500

    feat(back): #649 migrate dns module
    Former-commit-id: 9385dd4dbaf4830ea0092035d8f721d421e8f59d

--- fluidasserts/proto/dns.py
+++ fluidasserts/proto/dns.py
@@ -3,7 +3,6 @@
 """This module allows to check vulnerabilities in DNS systems."""
 
 # standard imports
-from __future__ import absolute_import
 from typing import Set, Tuple, List
 from contextlib import suppress
 
@@ -21,11 +20,8 @@ from dns.zone import from_xfr
 
 
 # local imports
-from fluidasserts import Unit, DAST, MEDIUM, OPEN, CLOSED, UNKNOWN
-from fluidasserts import show_close
-from fluidasserts import show_open
-from fluidasserts import show_unknown
-from fluidasserts.utils.decorators import track, level, notify, api
+from fluidasserts import Unit, DAST, LOW, MEDIUM, HIGH, OPEN, CLOSED
+from fluidasserts.utils.decorators import api, unknown_if
 
 
 DOMAIN = str
@@ -61,7 +57,30 @@ def _recursive_query_dns(
     results.update((domain, str(target)) for target in answers)
 
 
+def _get_result_as_tuple(*,
+                         nameserver: str, domain: str,
+                         msg_open: str, msg_closed: str,
+                         open_if: bool = None,
+                         vulns: List[str] = None) -> tuple:
+    """Return the tuple version of the Result object."""
+    domain = domain.strip('.')
+
+    units: List[Unit] = [
+        Unit(where=nameserver,
+             specific=vulns or [
+                 f'{domain} {msg_open if open_if or vulns else msg_closed}'])]
+
+    if open_if or vulns:
+        return OPEN, msg_open, units, []
+    return CLOSED, msg_closed, [], units
+
+
 @api(risk=MEDIUM, kind=DAST)
+@unknown_if(socket.error,
+            dns.exception.Timeout,
+            dns.exception.FormError,
+            dns.resolver.NoNameservers,
+            dns.resolver.NXDOMAIN)
 def has_subdomain_takeover(
         domain: DOMAIN,
         nameserver: NAMESERVER,
@@ -81,15 +100,8 @@ def has_subdomain_takeover(
     """
     origin_to_target: Set[Tuple[DOMAIN, DOMAIN]] = set()
 
-    try:
-        # Fill the results
-        _recursive_query_dns(domain, nameserver, origin_to_target)
-    except (socket.error,
-            dns.exception.Timeout,
-            dns.exception.FormError,
-            dns.resolver.NoNameservers,
-            dns.resolver.NXDOMAIN) as exc:
-        return UNKNOWN, f'An error occurred: {exc}'
+    # Fill the results
+    _recursive_query_dns(domain, nameserver, origin_to_target)
 
     vuln_records: List[DOMAIN] = list(
         f'Record: {origin} -> {target}'
@@ -97,99 +109,76 @@ def has_subdomain_takeover(
         for controlled_domain in attacker_controlled_domains
         if controlled_domain in target)
 
-    units: List[Unit] = [Unit(where=domain,
-                              source='DNS/Records',
-                              specific=vuln_records,
-                              fingerprint=hex(hash(tuple(vuln_records))))]
-
-    if vuln_records:
-        return OPEN, (f'{domain} is vulnerable to a sub-domain takeover'
-                      'because one of the records point to a site that '
-                      'can be controlled by an attacker'), units
-    return CLOSED, f'{domain} is safe against a sub-domain takeover', [], units
+    return _get_result_as_tuple(
+        nameserver=nameserver, domain=domain,
+        msg_open=('is vulnerable to a sub-domain takeover because one of the '
+                  'records point to a site that can be controlled by an '
+                  'attacker'),
+        msg_closed='is safe against a sub-domain takeover',
+        vulns=vuln_records)
 
 
-@notify
-@level('medium')
-@track
-def is_xfr_enabled(domain: str, nameserver: str) -> bool:
+@api(risk=MEDIUM, kind=DAST)
+@unknown_if(socket.error, dns.exception.Timeout, dns.exception.FormError)
+def is_xfr_enabled(domain: str, nameserver: str) -> tuple:
     """
     Check if zone transfer is enabled.
 
     :param domain: Name of the zone to transfer.
     :param nameserver: IPv4 or 6 to test.
     """
+    is_zone_transfer_enabled: bool = False
+
     axfr_query = dns.query.xfr(nameserver, domain, timeout=5,
                                relativize=False, lifetime=10)
 
-    result = True
-    try:
+    with suppress(NoSOA, NoNS, BadZone, dns.query.BadResponse,
+                  dns.query.TransferError):
+
         zone = from_xfr(axfr_query, relativize=False)
-        if not str(zone.origin).rstrip('.'):
-            show_close('Zone transfer not enabled on server',
-                       details=dict(domain=domain, nameserver=nameserver))
-            result = False
-        result = True
-        show_open('Zone transfer enabled on server',
-                  details=dict(domain=domain, nameserver=nameserver))
-    except (NoSOA, NoNS, BadZone,
-            dns.query.BadResponse, dns.query.TransferError):
-        show_close('Zone transfer not enabled on server',
-                   details=dict(domain=domain, nameserver=nameserver))
-        result = False
-    except (socket.error,
-            dns.exception.Timeout, dns.exception.FormError) as exc:
-        show_unknown('Could not connect',
-                     details=dict(domain=domain, nameserver=nameserver,
-                                  error=str(exc).replace(':', ',')))
-        result = False
 
-    return result
+        if str(zone.origin).rstrip('.'):
+            is_zone_transfer_enabled = True
+
+    return _get_result_as_tuple(
+        nameserver=nameserver, domain=domain,
+        msg_open='zone transfer is enabled on name server',
+        msg_closed='zone transfer is disabled on name server',
+        open_if=is_zone_transfer_enabled)
 
 
-@notify
-@level('high')
-@track
-def is_dynupdate_enabled(domain: str, nameserver: str) -> bool:
+@api(risk=HIGH, kind=DAST)
+@unknown_if(socket.error, dns.exception.Timeout)
+def is_dynupdate_enabled(domain: str, nameserver: str) -> tuple:
     """
     Check if zone updating is enabled.
 
     :param domain: Name of the zone to transfer.
     :param nameserver: IPv4 or 6 to test.
     """
-    newrecord = 'newrecord'
+    is_zone_update_enabled: bool = False
 
-    try:
+    with suppress(dns.query.BadResponse):
         update = dns.update.Update(domain)
-        update.add(newrecord, 3600, dns.rdatatype.A, '10.10.10.10')
+        update.add('newrecord', 3600, dns.rdatatype.A, '10.10.10.10')
+
         response = dns.query.tcp(update, nameserver, timeout=5)
 
-        result = True
+        if response.rcode() <= 0:
+            is_zone_update_enabled = True
 
-        if response.rcode() > 0:
-            show_close('Zone update not enabled on server',
-                       details=dict(domain=domain, nameserver=nameserver))
-            result = False
-        else:
-            show_open('Zone update enabled on server',
-                      details=dict(domain=domain, nameserver=nameserver))
-            result = True
-    except dns.query.BadResponse:
-        show_close('Zone update not enabled on server',
-                   details=dict(domain=domain, nameserver=nameserver))
-        result = False
-    except (socket.error, dns.exception.Timeout) as exc:
-        show_unknown('Could not connect',
-                     details=dict(domain=domain, nameserver=nameserver,
-                                  error=str(exc).replace(':', ',')))
-        result = False
-    return result
+    return _get_result_as_tuple(
+        nameserver=nameserver, domain=domain,
+        msg_open='zone update is enabled on name server',
+        msg_closed='zone update is disabled on name server',
+        open_if=is_zone_update_enabled)
 
 
-@notify
-@level('medium')
-@track
-def has_cache_poison(domain: str, nameserver: str) -> bool:
+@api(risk=MEDIUM, kind=DAST)
+@unknown_if(dns.exception.Timeout,
+            dns.exception.SyntaxError,
+            dns.resolver.NoNameservers)
+def has_cache_poison(domain: str, nameserver: str) -> tuple:
     """
     Check if cache poisoning is possible.
 
@@ -198,169 +187,131 @@ def has_cache_poison(domain: str, nameserver: str) -> bool:
     :param domain: Name of the zone to transfer.
     :param nameserver: IPv4 or 6 to test.
     """
-    myresolver = _get_resolver(nameserver)
+    is_vulnerable: bool = False
 
     name = dns.name.from_text(domain)
 
-    result = True
     try:
-        response = myresolver.query(name, 'DNSKEY')
-    except (dns.exception.Timeout, dns.exception.SyntaxError,
-            dns.resolver.NoNameservers) as exc:
-        show_unknown('Could not connect',
-                     details=dict(domain=domain, nameserver=nameserver,
-                                  error=str(exc).replace(':', ',')))
-        return False
+        response = _get_resolver(nameserver).query(name, 'DNSKEY')
     except dns.resolver.NoAnswer:
-        show_open('Cache poisoning possible on server',
-                  details=dict(domain=domain, nameserver=nameserver))
-        return True
-    if response.response.rcode() != 0:
-        show_open('Cache poisoning is possible on server',
-                  details=dict(domain=domain, nameserver=nameserver))
-        result = True
+        is_vulnerable = True
     else:
-        answer = response.rrset
-        if len(answer) != 2:
-            show_open('Cache poisoning possible on server',
-                      details=dict(domain=domain, nameserver=nameserver))
-            result = True
-        else:
-            show_close('Cache poisoning not possible on server',
-                       details=dict(domain=domain, nameserver=nameserver))
-            result = False
+        if response.response.rcode() != 0 or len(response.rrset) != 2:
+            is_vulnerable = True
 
-    return result
+    return _get_result_as_tuple(
+        nameserver=nameserver, domain=domain,
+        msg_open='cache poisoning is possible on name server',
+        msg_closed='cache poisoning is not possible on name server',
+        open_if=is_vulnerable)
 
 
-@notify
-@level('low')
-@track
-def has_cache_snooping(nameserver: str) -> bool:
+@api(risk=LOW, kind=DAST)
+@unknown_if(socket.error, dns.exception.Timeout)
+def has_cache_snooping(nameserver: str, domain: str = 'google.com.') -> tuple:
     """
     Check if nameserver has cache snooping.
 
     (supports non recursive queries)
     :param nameserver: IPv4 or 6 to test.
     """
-    domain = 'google.com.'
+    is_vulnerable: bool = False
+
     name = dns.name.from_text(domain)
 
-    try:
+    with suppress(dns.exception.SyntaxError):
         # Make a recursive request to fill out the cache
-        request = dns.message.make_query(name, dns.rdatatype.A,
+        request = dns.message.make_query(name,
+                                         dns.rdatatype.A,
                                          dns.rdataclass.IN)
 
         dns.query.udp(request, nameserver, timeout=5)
 
         # Make a non-recursive request
-        request = dns.message.make_query(name, dns.rdatatype.A,
+        request = dns.message.make_query(name,
+                                         dns.rdatatype.A,
                                          dns.rdataclass.IN)
+
         request.flags ^= dns.flags.RD
 
         response = dns.query.udp(request, nameserver, timeout=5)
 
-        result = True
         if response.rcode() == 0:
-            show_open('Cache snooping possible on server',
-                      details=dict(domain=domain, nameserver=nameserver))
-            result = True
-        else:
-            show_close('Cache snooping not possible on server',
-                       details=dict(domain=domain, nameserver=nameserver))
-            result = False
-    except dns.exception.SyntaxError:
-        show_close('Cache snooping not possible on server',
-                   details=dict(domain=domain, nameserver=nameserver))
-        result = False
-    except (socket.error, dns.exception.Timeout) as exc:
-        show_unknown('Could not connect',
-                     details=dict(domain=domain, nameserver=nameserver,
-                                  error=str(exc).replace(':', ',')))
-        result = False
-    return result
+            is_vulnerable = True
+
+    return _get_result_as_tuple(
+        nameserver=nameserver, domain=domain,
+        msg_open='cache snooping is possible on name server',
+        msg_closed='cache snooping is not possible on name server',
+        open_if=is_vulnerable)
 
 
-@notify
-@level('low')
-@track
-def has_recursion(nameserver: str) -> bool:
+@api(risk=LOW, kind=DAST)
+@unknown_if(socket.error, dns.exception.Timeout)
+def has_recursion(nameserver: str, domain: str = 'google.com.') -> tuple:
     """
     Check if nameserver has recursion enabled.
 
     :param nameserver: IPv4 or 6 to test.
     """
-    domain = 'google.com.'
+    is_vulnerable: bool = False
+
     name = dns.name.from_text(domain)
 
-    try:
+    with suppress(dns.exception.SyntaxError):
         # Make a recursive request
-        request = dns.message.make_query(name, dns.rdatatype.A,
+        request = dns.message.make_query(name,
+                                         dns.rdatatype.A,
                                          dns.rdataclass.IN)
 
         response = dns.query.udp(request, nameserver, timeout=5)
-        result = True
+
         if response.rcode() == 0:
-            show_open('Recursion possible on server',
-                      details=dict(domain=domain, nameserver=nameserver))
-            result = True
-        else:
-            show_close('Recursion not possible on server',
-                       details=dict(domain=domain, nameserver=nameserver))
-            result = False
-    except dns.exception.SyntaxError:
-        show_close('Recursion not possible on server',
-                   details=dict(domain=domain, nameserver=nameserver))
-        result = False
-    except (socket.error, dns.exception.Timeout) as exc:
-        show_unknown('Could not connect',
-                     details=dict(domain=domain, nameserver=nameserver,
-                                  error=str(exc).replace(':', ',')))
-        result = False
-    return result
+            is_vulnerable = True
+
+    return _get_result_as_tuple(
+        nameserver=nameserver, domain=domain,
+        msg_open='recursion is possible on name server',
+        msg_closed='recursion is not possible on name server',
+        open_if=is_vulnerable)
 
 
-@notify
-@level('medium')
-@track
-def can_amplify(nameserver: str) -> bool:
+@api(risk=MEDIUM, kind=DAST)
+@unknown_if(socket.error, dns.exception.Timeout)
+def can_amplify(nameserver: str, domain: str = 'google.com.') -> tuple:
     """
     Check if nameserver allows amplification attacks.
 
     :param nameserver: IPv4 or 6 to test.
     """
-    domain = 'google.com.'
+    is_vulnerable: bool = False
+
     name = dns.name.from_text(domain)
 
-    try:
+    with suppress(dns.exception.SyntaxError):
         # Make a recursive request
-        request = dns.message.make_query(name, dns.rdatatype.A,
+        request = dns.message.make_query(name,
+                                         dns.rdatatype.A,
                                          dns.rdataclass.IN)
+
         response = dns.query.udp(request, nameserver, timeout=5)
+
         if response.rcode() == 0:
             request = dns.message.make_query(name, dns.rdatatype.ANY)
             request.flags |= dns.flags.AD
             request.find_rrset(request.additional, dns.name.root, 65535,
                                dns.rdatatype.OPT, create=True,
                                force_unique=True)
+            request_len = len(request.to_text())
+
             response = dns.query.udp(request, nameserver, timeout=5)
-            resp_len = sum(len(x.to_text()) for x in response.answer)
-            req_len = len(request.to_text())
-            if req_len < resp_len:
-                show_open('Amplification attack is possible on server',
-                          details=dict(nameserver=nameserver,
-                                       request_len=req_len,
-                                       response_len=resp_len))
-                return True
-        show_close('Amplification attack is not possible on server',
-                   details=dict(nameserver=nameserver))
-        return False
-    except dns.exception.SyntaxError:
-        show_close('Amplification attack is not possible on server',
-                   details=dict(nameserver=nameserver))
-        return False
-    except (socket.error, dns.exception.Timeout) as exc:
-        show_unknown('Could not connect',
-                     details=dict(domain=domain, nameserver=nameserver,
-                                  error=str(exc).replace(':', ',')))
-        return False
+            response_len = sum(len(x.to_text()) for x in response.answer)
+
+            if response_len > request_len:
+                is_vulnerable = True
+
+    return _get_result_as_tuple(
+        nameserver=nameserver, domain=domain,
+        msg_open='amplification attack is possible on name server',
+        msg_closed='amplification attack  is not possible on name server',
+        open_if=is_vulnerable)
