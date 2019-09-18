2342a139d6de465fdbff356b6627238c047985ae
kamadoatfluid <kamado@fluidattacks.com>
2019-09-18 12:28:35 -0500

    feat(back): #649 migrate proto.ftp
    Former-commit-id: f1df0fe5c9d89930dbaaaa2835944291af70161c

--- fluidasserts/proto/ftp.py
+++ fluidasserts/proto/ftp.py
@@ -3,104 +3,101 @@
 """This module allows to check FTP-specific vulnerabilities."""
 
 # standard imports
-from ftplib import error_perm
-from ftplib import FTP
-
-# 3rd party imports
-# None
+import ftplib
+from contextlib import suppress
 
 # local imports
-from fluidasserts import show_close
-from fluidasserts import show_open
-from fluidasserts import show_unknown
-from fluidasserts.utils.decorators import track, level, notify
+from fluidasserts import DAST, OPEN, CLOSED, Unit, HIGH
+from fluidasserts.utils.decorators import unknown_if, api
 
-PORT = 21
-NULL_PASSWORD = ''
-ADMIN_USERNAME = 'root'
-ANONYMOUS_USERNAME = 'anonymous'
-ANONYMOUS_PASS = 'anonymous'
+# Constants
+PORT: int = 21
+NULL_PASSWORD: str = str()
+ADMIN_USERNAME: str = 'root'
+ANONYMOUS_USERNAME: str = 'anonymous'
+ANONYMOUS_PASS: str = 'anonymous'
 
 
-def _ftp_do_auth(ip_address: str, username: str,
-                 password: str, port: int = PORT) -> bool:
+@unknown_if(OSError)
+def _ftp_do_auth(*,
+                 ip_address: str, port: int,
+                 username: str, password: str,
+                 msg_open: str, msg_closed: str) -> tuple:
     """
     Perform FTP auth.
 
     :param ip_address: IP address to connect to.
     :param username: Username to check.
     :param password: Password to check.
-    :param port: If necessary, specifiy port to connect to.
+    :param port: If necessary, specify port to connect to.
     """
-    result = False
-    try:
-        with FTP() as ftp:
+    could_connect: bool = False
+
+    with ftplib.FTP() as ftp:
+        with suppress(ftplib.error_perm):
             ftp.connect(ip_address, port)
             ftp.login(username, password)
             ftp.makepasv()
-            result = True
-            show_open('FTP Authentication {}:{}'.format(ip_address, port),
-                      details=dict(username=username, password=password))
-    except error_perm:
-        show_close('FTP Authentication {}:{}'.format(ip_address, port),
-                   details=dict(username=username, password=password))
-        result = False
-    except OSError as exc:
-        show_unknown('Could not connect',
-                     details=dict(ip=ip_address, username=username,
-                                  password=password,
-                                  error=str(exc)))
-        result = False
-    return result
+
+            could_connect = True
+
+    unit: Unit = Unit(where=f'FTP://{username}:{password}@{ip_address}:{port}',
+                      specific=[msg_open if could_connect else msg_closed])
+
+    if could_connect:
+        return OPEN, msg_open, [unit], []
+    return CLOSED, msg_closed, [], [unit]
 
 
-@notify
-@level('high')
-@track
+@api(risk=HIGH, kind=DAST)
 def is_a_valid_user(ip_address: str, username: str,
-                    password: str, port: int = PORT) -> bool:
+                    password: str, port: int = PORT) -> tuple:
     """
     Check if given credentials are valid in FTP service.
 
     :param ip_address: IP address to connect to.
     :param username: Username to check.
     """
-    return _ftp_do_auth(ip_address, username, password, port)
+    return _ftp_do_auth(
+        ip_address=ip_address, port=port,
+        username=username, password=password,
+        msg_open='Credentials are valid', msg_closed='Credentials are invalid')
 
 
-@notify
-@level('high')
-@track
+@api(risk=HIGH, kind=DAST)
 def user_without_password(ip_address: str, username: str,
-                          port: int = PORT) -> bool:
+                          port: int = PORT) -> tuple:
     """
     Check if a user can login without password.
 
     :param ip_address: IP address to connect to.
     :param username: Username to check.
     """
-    return _ftp_do_auth(ip_address, username, password=NULL_PASSWORD,
-                        port=port)
+    return _ftp_do_auth(
+        ip_address=ip_address, port=port,
+        username=username, password=NULL_PASSWORD,
+        msg_open='Password is not required', msg_closed='Password is required')
 
 
-@notify
-@level('high')
-@track
-def is_anonymous_enabled(ip_address: str, port: int = PORT) -> bool:
+@api(risk=HIGH, kind=DAST)
+def is_anonymous_enabled(ip_address: str, port: int = PORT) -> tuple:
     """
     Check if FTP service allows anonymous login.
 
     :param ip_address: IP address to connect to.
     """
-    return _ftp_do_auth(ip_address, ANONYMOUS_USERNAME, ANONYMOUS_PASS,
-                        port=port)
+    return _ftp_do_auth(
+        ip_address=ip_address, port=port,
+        username=ANONYMOUS_USERNAME, password=ANONYMOUS_PASS,
+        msg_open='Anonymous user is enabled',
+        msg_closed='Anonymous user is disabled')
 
 
-@notify
-@level('high')
-@track
-def is_admin_enabled(ip_address: str, password: str,
-                     username: str = ADMIN_USERNAME, port: int = PORT) -> bool:
+@api(risk=HIGH, kind=DAST)
+def is_admin_enabled(ip_address: str,
+                     password: str,
+                     username: str = ADMIN_USERNAME,
+                     port: int = PORT) -> tuple:
     """
     Check if FTP service allows admin login.
 
@@ -108,4 +105,8 @@ def is_admin_enabled(ip_address: str, password: str,
     :param username: Username to check.
     :param password: Password to check.
     """
-    return _ftp_do_auth(ip_address, username, password, port=port)
+    return _ftp_do_auth(
+        ip_address=ip_address, port=port,
+        username=username, password=password,
+        msg_open='Admin credentials are valid',
+        msg_closed='Admin credentials are invalid')
