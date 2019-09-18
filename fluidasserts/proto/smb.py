9ba9c2a9072ff1fed42c62311fd05d18e1ba9f17
kamadoatfluid <kamado@fluidattacks.com>
2019-09-18 13:33:33 -0500

    feat(back): #649 migrate proto.smb
    Former-commit-id: 225f580ea41dbe8f05241b33a71b46821b8c4d9d

--- fluidasserts/proto/smb.py
+++ fluidasserts/proto/smb.py
@@ -1,32 +1,29 @@
 # -*- coding: utf-8 -*-
 
-"""This module allows to check SMB vulnerabilites."""
+"""This module allows to check SMB vulnerabilities."""
 
 # standard imports
-from __future__ import absolute_import
 from typing import Optional
+from contextlib import suppress
 
 # 3rd party imports
 from smb import SMBConnection
 from smb import smb_structs
 
 # local imports
-from fluidasserts import show_close
-from fluidasserts import show_open
-from fluidasserts import show_unknown
-from fluidasserts.utils.decorators import track, level, notify
+from fluidasserts import DAST, MEDIUM, HIGH, _get_result_as_tuple_host_port
+from fluidasserts.utils.decorators import unknown_if, api
 
-
+# Constants
 CLIENT_MACHINE_NAME = 'assertspc'
 
 
-@notify
-@level('medium')
-@track
+@api(risk=MEDIUM, kind=DAST)
+@unknown_if(AssertionError, OSError)
 def has_dirlisting(server: str, share: str,
                    user: Optional[str] = None,
                    password: Optional[str] = None,
-                   domain: str = 'WORKGROUP') -> bool:
+                   domain: str = 'WORKGROUP') -> tuple:
     r"""
     Check if an SMB share has dirlisting.
 
@@ -34,75 +31,59 @@ def has_dirlisting(server: str, share: str,
     :param \*args: Optional arguments for SMB connect.
     :param \*\*kwargs: Optional arguments for SMB connection.
     """
-    try:
+    success: bool = False
+    with suppress(smb_structs.OperationFailure):
         with SMBConnection.SMBConnection(user, password,
                                          CLIENT_MACHINE_NAME, server,
-                                         domain=domain, use_ntlm_v2=True,
+                                         domain=domain,
+                                         use_ntlm_v2=True,
                                          is_direct_tcp=True) as conn:
+            if not conn.connect(server, port=445):
+                raise AssertionError('There was an error connecting to SMB')
 
-            ret = conn.connect(server, port=445)
-            if not ret:
-                show_unknown('There was an error connecting to SMB',
-                             details=dict(server=server, domain=domain))
-                return False
+            # Attempt to list directories
             conn.listPath(share, '/')
-            show_open('Directory listing is possible',
-                      details=dict(domain=domain,
-                                   user=user,
-                                   server=server,
-                                   share=share))
-            return True
-    except OSError as exc:
-        show_unknown('There was an error connecting to SMB',
-                     details=dict(server=server, domain=domain,
-                                  error=str(exc)))
-        return False
-    except smb_structs.OperationFailure:
-        show_close('Directory listing not possible',
-                   details=dict(domain=domain,
-                                user=user,
-                                server=server,
-                                share=share))
-        return False
+            success = True
+
+    return _get_result_as_tuple_host_port(
+        protocol='SMB', host=server, port=445, extra=share,
+        msg_open='Directory listing is possible',
+        msg_closed='Directory listing is not possible',
+        open_if=success,
+        auth=(user, password))
 
 
-@notify
-@level('high')
-@track
+@api(risk=HIGH, kind=DAST)
+@unknown_if(OSError)
 def is_anonymous_enabled(server: str,
-                         domain: str = 'WORKGROUP') -> bool:
+                         domain: str = 'WORKGROUP') -> tuple:
     """
     Check if anonymous login is possible over SMB.
 
     :param server: The NetBIOS machine name of the remote server.
     :param domain: The network domain/workgroup. Defaults to 'WORKGROUP'
     """
-    user = 'anonymous'
-    password = ''
-    try:
-        with SMBConnection.SMBConnection(user, password,
-                                         CLIENT_MACHINE_NAME, server,
-                                         domain=domain, use_ntlm_v2=True,
-                                         is_direct_tcp=True) as conn:
-            ret = conn.connect(server, port=445)
-            if not ret:
-                show_close('Anonymous login not possible',
-                           details=dict(domain=domain, user=user,
-                                        server=server))
-                return False
-            show_open('Anonymous login enabled',
-                      details=dict(domain=domain, user=user, server=server))
-            return True
-    except OSError as exc:
-        show_unknown('There was an error connecting to SMB',
-                     details=dict(server=server, domain=domain,
-                                  error=str(exc)))
-        return False
+    success: bool
+    username: str = 'anonymous'
+    password: str = str()
+
+    with SMBConnection.SMBConnection(username, password,
+                                     CLIENT_MACHINE_NAME, server,
+                                     domain=domain,
+                                     use_ntlm_v2=True,
+                                     is_direct_tcp=True) as conn:
+        success = bool(conn.connect(server, port=445))
+
+    return _get_result_as_tuple_host_port(
+        protocol='SMB', host=server, port=445,
+        msg_open='Anonymous login is possible',
+        msg_closed='Anonymous login is not possible',
+        open_if=success,
+        auth=(username, password))
 
 
-@notify
-@level('low')
-@track
+@api(risk=MEDIUM, kind=DAST)
+@unknown_if(AssertionError, OSError)
 def is_signing_disabled(server, user, password, domain='WORKGROUP'):
     """
     Check if SMB connection uses signing.
@@ -112,26 +93,20 @@ def is_signing_disabled(server, user, password, domain='WORKGROUP'):
     :param password: Password for given user.
     :param domain: The network domain/workgroup. Defaults to 'WORKGROUP'
     """
-    try:
-        with SMBConnection.SMBConnection(user, password,
-                                         CLIENT_MACHINE_NAME, server,
-                                         domain=domain, use_ntlm_v2=True,
-                                         is_direct_tcp=True) as conn:
-            ret = conn.connect(server, port=445)
-            if not ret:
-                show_unknown('There was an error connecting to SMB',
-                             details=dict(server=server, domain=domain))
-                return False
-            if conn.is_signing_active:
-                show_close('SMB has signing active',
-                           details=dict(domain=domain, server=server,
-                                        user=user))
-                return False
-            show_open('SMB has signing disabled',
-                      details=dict(domain=domain, server=server, user=user))
-            return True
-    except OSError as exc:
-        show_unknown('There was an error connecting to SMB',
-                     details=dict(server=server, domain=domain,
-                                  error=str(exc)))
-        return False
+    signs: bool
+    with SMBConnection.SMBConnection(user, password,
+                                     CLIENT_MACHINE_NAME, server,
+                                     domain=domain,
+                                     use_ntlm_v2=True,
+                                     is_direct_tcp=True) as conn:
+        if not conn.connect(server, port=445):
+            raise AssertionError('There was an error connecting to SMB')
+
+        signs = conn.is_signing_active
+
+    return _get_result_as_tuple_host_port(
+        protocol='SMB', host=server, port=445,
+        msg_open='Signing is not active',
+        msg_closed='Signing is active',
+        open_if=not signs,
+        auth=(user, password))
