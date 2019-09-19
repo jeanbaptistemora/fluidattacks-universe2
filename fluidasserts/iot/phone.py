30c62604e3a2a753be84e3fc3b44607ba545c929
kamadoatfluid <kamado@fluidattacks.com>
2019-09-18 22:29:57 -0500

    feat(back): #649 migrate iot.phone
    Former-commit-id: 906f6255ad0419d1d902a4fa7bea6d74d6259f2e

--- fluidasserts/iot/phone.py
+++ fluidasserts/iot/phone.py
@@ -3,21 +3,18 @@
 """This module allows to check SIP vulnerabilities."""
 
 # standard imports
-import base64
-import email
 import io
 import re
+import email
+import base64
 import socket
-
-# third party imports
-# None
+import textwrap
+from typing import Dict
 
 # local imports
-from fluidasserts import show_close
-from fluidasserts import show_open
-from fluidasserts import show_unknown
+from fluidasserts import DAST, LOW, HIGH, _get_result_as_tuple_host_port
 from fluidasserts.helper import http
-from fluidasserts.utils.decorators import track, level, notify
+from fluidasserts.utils.decorators import unknown_if, api
 
 
 def _make_udp_request(server: str, port: int, data: str):
@@ -42,171 +39,135 @@ def _make_tcp_request(server: str, port: int, data: str):
     return buff.decode()
 
 
-@notify
-@level('low')
-@track
+@api(risk=LOW, kind=DAST)
+@unknown_if(socket.error)
 def is_version_visible(server: str, port: int = 5060,
                        source_ip: str = '127.0.0.1',
-                       source_port: int = 5060) -> bool:
+                       source_port: int = 5060) -> tuple:
     """
     Check if SIP server version is visible.
 
-    :param ipaddress: IP address to test.
+    :param server: IP address to test.
     :param port: Port to connect to.
     """
-    request = f"""OPTIONS sip:100@{server} SIP/2.0
-Via: SIP/2.0/UDP {source_ip}:{source_port};rport
-Content-Length: 0
-From: "fake" <sip:fake@{source_ip}>
-Accept: application/sdp
-User-Agent: Fluid Asserts
-To: <sip:100@1.1.1.1>
-Contact: sip:fake@{source_ip}:{source_port}
-CSeq: 1 OPTIONS
-Call-ID: fake-id@{source_ip}
-Max-Forwards: 70
+    is_vulnerable: bool = False
+    fingerprint: Dict[str, str] = {}
+    request: str = textwrap.dedent(f"""
+        OPTIONS sip:100@{server} SIP/2.0
+        Via: SIP/2.0/UDP {source_ip}:{source_port};rport
+        Content-Length: 0
+        From: "fake" <sip:fake@{source_ip}>
+        Accept: application/sdp
+        User-Agent: Fluid Asserts
+        To: <sip:100@1.1.1.1>
+        Contact: sip:fake@{source_ip}:{source_port}
+        CSeq: 1 OPTIONS
+        Call-ID: fake-id@{source_ip}
+        Max-Forwards: 70
 
-"""
-    request = request.replace('\n', '\r\n')
+        """)[1:].replace('\n', '\r\n')
 
-    proto = None
     try:
         recv_data = _make_udp_request(server, port, request)
-        proto = 'UDP'
     except socket.error:
-        try:
-            recv_data = _make_tcp_request(server, port, request)
-            proto = 'TCP'
-        except socket.error as exc:
-            show_unknown('Could not connect',
-                         details=dict(server=server, port=port, proto=proto,
-                                      error=str(exc).replace(':', ',')))
-            return False
+        recv_data = _make_tcp_request(server, port, request)
+
     _, headers_alone = recv_data.split('\r\n', 1)
     message = email.message_from_file(io.StringIO(headers_alone))
     headers = dict(message.items())
-    if 'Server' not in headers or 'User-Agent' not in headers:
-        show_close('Server or User-Agent header were not returned',
-                   details=dict(server=server, port=port, proto=proto))
-        return False
-    regex_match = re.search(r'([a-z-A-Z]+)[^a-zA-Z0-9](.*)',
-                            headers['Server'])
-    if regex_match:
-        show_open('SIP server version visible',
-                  details=dict(server=server, port=port, proto=proto,
-                               product=regex_match.group(1),
-                               version=regex_match.group(2)))
-        result = True
-    else:
-        show_close('SIP server version not visible',
-                   details=dict(server=server, port=port, proto=proto))
-        result = False
-    return result
+
+    if 'Server' in headers and 'User-Agent' in headers:
+        is_vulnerable = re.search(
+            r'([a-z-A-Z]+)[^a-zA-Z0-9](.*)', headers['Server'])
+
+        if is_vulnerable:
+            fingerprint.update({
+                'product': is_vulnerable.group(1),
+                'version': is_vulnerable.group(2)
+            })
+
+    return _get_result_as_tuple_host_port(
+        protocol='SIP', host=server, port=port,
+        msg_open='Version is visible',
+        msg_closed='Version is not visible',
+        open_if=is_vulnerable,
+        fingerprint=fingerprint)
 
 
-@notify
-@level('high')
-@track
+@api(risk=HIGH, kind=DAST)
+@unknown_if(AssertionError, http.ConnError)
 def unify_has_default_credentials(hostname: str,
                                   proto: str = 'https',
                                   port: int = '443',
-                                  password: str = '123456') -> bool:
+                                  password: str = '123456') -> tuple:
     """
     Check if Unify OpenScape Desk Phone IP 55G has default credentials.
 
     :param hostname: IP or host of phone.
     :param password: Default password.
     """
-    try:
-        url = '{}://{}:{}/index.cmd?user=Admin'.format(proto, hostname, port)
-        sess = http.HTTPSession(url)
+    session = http.HTTPSession(
+        f'{proto}://{hostname}:{port}/index.cmd?user=Admin')
 
-        if 'OpenScape Desk Phone IP Admin' not in sess.response.text:
-            show_unknown('Resources not found. Is it a valid phone version?',
-                         details=dict(host=hostname, url=url,
-                                      status_code=sess.response.status_code))
-            return False
+    if 'OpenScape Desk Phone IP Admin' not in session.response.text:
+        raise AssertionError(
+            'Resource not found. Is it a valid phone version?')
 
-        sess.data = 'page_submit=WEBMp_Admin_Login&lang=es&AdminPassword={}'\
-            .format(password)
-        sess.url = '{}://{}:{}/page.cmd'.format(proto, hostname, port)
-        sess.do_request()
-    except http.ConnError as exc:
-        show_unknown('Could not connect',
-                     details=dict(hostname=hostname, url=url,
-                                  reason=str(exc).replace(':', ',')))
-        return False
+    session.url = f'{proto}://{hostname}:{port}/page.cmd'
+    session.data: str = \
+        f'page_submit=WEBMp_Admin_Login&lang=es&AdminPassword={password}'
+    session.do_request()
 
-    failed = "action='./page.cmd'"
+    if session.response.status_code > 400:
+        raise AssertionError(
+            'Resources not found. Is it a valid phone version?')
 
-    if sess.response.status_code > 400:
-        show_unknown('Resources not found. Is it a valid phone version?',
-                     details=dict(host=hostname, url=url,
-                                  status_code=sess.response.status_code))
-        return False
-    if failed not in sess.response.text:
-        show_open('Phone has default credentials',
-                  details=dict(host=hostname, username='Admin',
-                               password=password))
-        result = True
-    else:
-        show_close('Phone has not default credentials',
-                   details=dict(host=hostname, username='Admin',
-                                password=password))
-        result = False
-    return result
+    session.set_messages(
+        source='Unify OpenScape Desk Phone IP 55G/Credentials',
+        msg_open='Phone has default credentials',
+        msg_closed='Phone has not default credentials')
+    session.add_unit(
+        is_vulnerable="action='./page.cmd'" not in session.response.text)
+    return session.get_tuple_result()
 
 
-@notify
-@level('high')
-@track
+@api(risk=HIGH, kind=DAST)
+@unknown_if(AssertionError, http.ConnError)
 def polycom_has_default_credentials(hostname: str,
                                     proto: str = 'https',
                                     port: int = '443',
-                                    password: str = '456') -> bool:
+                                    password: str = '456') -> tuple:
     """
     Check if Polycom SoundStation IP 6000 has default credentials.
 
     :param hostname: IP or host of phone.
     :param password: Default password.
     """
-    try:
-        url = '{}://{}:{}/login.htm'.format(proto, hostname, port)
-        sess = http.HTTPSession(url)
-        if 'Polycom Web Configuration Utility' not in sess.response.text:
-            show_unknown('Resources not found. Is it a valid phone version?',
-                         details=dict(host=hostname, url=url,
-                                      status_code=sess.response.status_code))
-            return False
-        creds = 'Polycom:{}'.format(password)
-        encoded = base64.b64encode(creds.encode())
+    url: str = f'{proto}://{hostname}:{port}/login.htm'
+    encoded: str = base64.b64encode(f'Polycom:{password}'.encode())
 
-        sess.headers.update({'X-Requested-With': 'XMLHttpRequest'})
-        sess.headers.update({'Authorization': 'Basic {}'
-                                              .format(encoded.decode())})
-        sess.url = '{}://{}:{}/auth.htm?\
-t=Tue,%2020%20Nov%202018%2019:48:43%20GMT'.format(proto, hostname, port)
-        sess.do_request()
-    except http.ConnError as exc:
-        show_unknown('Could not connect',
-                     details=dict(hostname=hostname, url=url,
-                                  reason=str(exc).replace(':', ',')))
-        return False
-    expected = "SoundStation IP 6000"
+    session = http.HTTPSession(url)
 
-    if sess.response.status_code > 401:
-        show_unknown('Resources not found. Is it a valid phone version?',
-                     details=dict(host=hostname, url=url,
-                                  status_code=sess.response.status_code))
-        return False
-    if expected in sess.response.text:
-        show_open('Phone has default credentials',
-                  details=dict(host=hostname, username='Admin',
-                               password=password))
-        result = True
-    else:
-        show_close('Phone has not default credentials',
-                   details=dict(host=hostname, username='Admin',
-                                password=password))
-        result = False
-    return result
+    if 'Polycom Web Configuration Utility' not in session.response.text:
+        raise AssertionError(
+            'Resources not found. Is it a valid phone version?')
+
+    session.url = (f'{proto}://{hostname}:{port}/auth.htm'
+                   '?t=Tue,%2020%20Nov%202018%2019:48:43%20GMT')
+    session.headers.update({
+        'X-Requested-With': 'XMLHttpRequest',
+        'Authorization': f'Basic {encoded.decode()}',
+    })
+    session.do_request()
+
+    if session.response.status_code > 401:
+        raise AssertionError(
+            'Resources not found. Is it a valid phone version?')
+
+    session.set_messages(
+        source='Unify OpenScape Desk Phone IP 55G/Credentials',
+        msg_open='Phone has default credentials',
+        msg_closed='Phone has not default credentials')
+    session.add_unit(
+        is_vulnerable='SoundStation IP 6000' in session.response.text)
+    return session.get_tuple_result()
