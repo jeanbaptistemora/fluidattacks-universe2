e9779dafd97a64860fa8580bd503a305519623f2
Oscar Prado <oprado@fluidattacks.com>
2019-09-23 16:30:34 -0500

    refac(front): #1596.39 tachyons blockquotes
    - Migrated blockquotes styles to tachyons
    using js
    
    - Also migrated preformatted text style
    to tachyon
    
    locally rendered changes:
    
    
    Former-commit-id: a6fd8d6ce8521d631b23ae254db1a3d32fa28267

--- theme/2014/static/js/dynclasses.js
+++ theme/2014/static/js/dynclasses.js
@@ -12,5 +12,11 @@ Tachyons classes on dynamically generated content
     //common links
     $( "a").addClass("no-underline");
 
+    //quoteblocks
+    $( ".quoteblock" ).addClass("overflow-auto bl b--dark-blue bw2 i tc pv4 ph2 bg-fluid-lightgray");
+
+    //pre-formatted text
+    $( "pre" ).addClass("overflow-auto bl b--dark-blue bw2 i tl f5 pa3 bg-fluid-lightgray ma0");
+
   });
 })(jQuery);
