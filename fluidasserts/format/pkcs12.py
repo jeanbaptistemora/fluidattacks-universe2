8d6114e2782a07a53eeda2f64e6d6182f094d2bd
drestrepoatfluid <drestrepo@fluidattacks.com>
2019-09-25 15:34:53 -0500

    feat(cross): #756.2 open .p12 with x passwords
    Former-commit-id: edb1b65e6daeb320c5a4d0ecd24053bf429c2519

--- fluidasserts/format/pkcs12.py
+++ fluidasserts/format/pkcs12.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 """This module allows to check ``PKCS12`` vulnerabilities."""
 
 # standard imports
@@ -39,6 +38,51 @@ def has_no_password_protection(path: str) -> tuple:
                 crypto.load_pkcs12(p12_file_handle.read())
         except crypto.Error:
             vulnerable = False
+
+        (vulns if vulnerable else safes).append(
+            Unit(
+                where=full_path,
+                source='PKCS 12/Password',
+                specific=[msg_open if vulnerable else msg_closed],
+                fingerprint=get_sha256(path)))
+    if vulns:
+        return OPEN, msg_open, vulns, safes
+    return CLOSED, msg_closed, vulns, safes
+
+
+@api(risk=HIGH, kind=SAST)
+@unknown_if(FileNotFoundError)
+def use_passwords(path: str, passwords: List):
+    """
+    Check if a PKCS 12 file has been protected by any of ``passwords``.
+
+    :param path: path to check
+    :param passwords: passwords to test
+    :rtype: :class:`fluidasserts.Result`
+    """
+    msg_open: str = 'PKCS is protected by a weak password'
+    msg_closed: str = 'PKCS is protected by a strong password'
+
+    safes: List[Unit] = []
+    vulns: List[Unit] = []
+
+    passwords = ['', *(p for p in set(passwords))]
+
+    for full_path in get_paths(
+            path, endswith=(
+                '.p12',
+                '.pfx',
+            )):
+        vulnerable = False
+        for password in passwords:
+            try:
+                with open(full_path, 'rb') as p12_file_handle:
+                    crypto.load_pkcs12(p12_file_handle.read(), password)
+            except crypto.Error:
+                continue
+            else:
+                vulnerable = True
+                break
 
         (vulns if vulnerable else safes).append(
             Unit(
--- test/test_format_pkcs12.py
+++ test/test_format_pkcs12.py
@@ -16,6 +16,11 @@ from fluidasserts.format import pkcs12
 NO_PWD_FILE = 'test/static/format/pkcs12/no_pwd.p12'
 NON_EXISTING_FILE = 'test/static/format/pkcs12/does_not_exist.p12'
 PWD_FILE = 'test/static/format/pkcs12/pwd.p12'
+# Password = 123456
+PWD_FILE_1 = 'test/static/format/pkcs12/open/pkijs_pkcs12_1.p12'
+# Password = 1234567
+PWD_FILE_2 = 'test/static/format/pkcs12/open/pkijs_pkcs12_2.p12'
+FOLDER_OPEN = 'test/static/format/pkcs12/open'
 
 #
 # Open tests
@@ -23,8 +28,14 @@ PWD_FILE = 'test/static/format/pkcs12/pwd.p12'
 
 
 def test_has_no_password_protection_open():
-    """p12 file is not password protected."""
+    """PKCS 12 file is not password protected."""
     assert pkcs12.has_no_password_protection(NO_PWD_FILE).is_open()
+
+
+def test_use_passwords_open():
+    """PKCS 12 file has been protected by any of passwords."""
+    assert pkcs12.use_passwords(PWD_FILE_1, ['', '123', '123456']).is_open()
+    assert pkcs12.use_passwords(FOLDER_OPEN, ['', '123', '123456']).is_open()
 
 
 #
@@ -37,6 +48,13 @@ def test_has_no_password_protection_close():
     assert pkcs12.has_no_password_protection(PWD_FILE).is_closed()
 
 
+def test_use_passwords_close():
+    """PKCS 12 file has been protected by a strong password."""
+    assert pkcs12.use_passwords(PWD_FILE_1, ['', '123', '12345']).is_closed()
+    assert pkcs12.use_passwords(FOLDER_OPEN,
+                                ['', '123', '12345699']).is_closed()
+
+
 #
 # Unknown tests
 #
@@ -45,3 +63,8 @@ def test_has_no_password_protection_close():
 def test_has_no_password_protection_unknown():
     """Test if PKCS 12 file does not exist."""
     assert pkcs12.has_no_password_protection(NON_EXISTING_FILE).is_unknown()
+
+
+def test__use_passwords_open_unknowm():
+    """Test if PKCS 12 file does not exist."""
+    assert pkcs12.use_passwords(NON_EXISTING_FILE, ['', '1233']).is_unknown()
