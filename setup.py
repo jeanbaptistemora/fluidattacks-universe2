8fdab424e49be68abcb2d86f890464baa9c9bc56
Andres Roldan <aroldan@fluidattacks.com>
2019-09-27 11:03:54 -0500

    fix(back): #784 add cython dep
    Former-commit-id: 8658b6ac8a70c2f11858be1bc790016c35292ae9

--- setup.py
+++ setup.py
@@ -97,7 +97,8 @@ setup(
         'pygments==2.4.2',             # fluidasserts
         'pyjks==19.0.0',               # fluidasserts.format.jks
         'pyjwt==1.7.1',                # fluidasserts.format.jwt
-        'pymssql==2.1.4',              # fluidasserts
+        'pymssql==2.1.4',              # fluidasserts.db.mssql
+        'Cython==0.29.13',             # fluidasserts.db.mssql
         'pynacl==1.3.0',               # fluidasserts.proto.ssl
         'pyparsing==2.4.2',            # fluidasserts.lang
         'pypdf2==1.26.0',              # fluidasserts.format.pdf
