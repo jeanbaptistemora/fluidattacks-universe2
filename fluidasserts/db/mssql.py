31714e8992a0a020534f02115fe4565bfadff507
drestrepoatfluid <drestrepo@fluidattacks.com>
2019-09-30 08:09:20 -0500

    feat(back): #758.4 user can execute os commands
    - Function to check if current user can execute OS commandas
    
    
    Former-commit-id: 862860f05404b862fdf3c043df8dcca05fab7809

--- fluidasserts/db/mssql.py
+++ fluidasserts/db/mssql.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
-
 """``Dynamic Application Security Testing`` Suite of Microsoft SQL Server."""
 
 # standard imports
 from contextlib import contextmanager
-from typing import List, NamedTuple, Tuple
+from typing import Any, Dict, Iterable, List, NamedTuple, Optional, Tuple
 
 # 3rd party imports
 import pymssql
-from pymssql import Connection
+from pymssql import Connection, Cursor
 # local imports
-from fluidasserts import DAST, LOW
+from fluidasserts import DAST, HIGH, LOW
 from fluidasserts.db import _get_result_as_tuple
 from fluidasserts.utils.decorators import api, unknown_if
 
@@ -28,8 +27,35 @@ def _is_auth_error(exception: pymssql.Error) -> bool:
     return 'Login failed' in str(exception)
 
 
+def _is_xp_cmdshell_error(exception: pymssql.Error) -> bool:
+    """Return True if the exception is an xp_cmdshell exception."""
+    return exception.args[0] == 15281
+
+
+def _is_permission_error(exception: pymssql.Error) -> bool:
+    """Return True if the exception is an permission exception."""
+    return exception.args[0] == 229
+
+
+def _is_compatibility_error(exception: pymssql.Error) -> bool:
+    """Return True if the exception is an pecompatibility exception."""
+    return exception.args[0] == 16202
+
+
+def _execute(connection_string: ConnectionString, query: str,
+             variables: Optional[Dict[str, Any]]):
+    with databse(connection_string) as (conn, cursor):
+        if not variables:
+            cursor.execute(query)
+        else:
+            cursor.execute(query, variables)
+        conn.commit()
+        return cursor
+
+
 @contextmanager
-def databse(connection_string: ConnectionString) -> Connection:
+def databse(connection_string: ConnectionString
+            ) -> Iterable[Tuple[Connection, Cursor]]:
     """
     Context manager to get a safe connection and a cursor.
 
@@ -81,6 +107,52 @@ def have_access(dbname: str, user: str, password: str, host: str,
             success = True
     except pymssql.Error as exc:
         if not _is_auth_error(exc):
+            raise exc
+
+    vulns: List[str] = []
+    safes: List[str] = []
+
+    (vulns if success else safes).append(msg_open if success else msg_closed)
+
+    return _get_result_as_tuple(
+        host=host,
+        port=port,
+        msg_open=msg_open,
+        msg_closed=msg_closed,
+        vulns=vulns,
+        safes=safes)
+
+
+@api(risk=HIGH, kind=DAST)
+@unknown_if(pymssql.Error)
+def can_execute_commands(dbname: str, user: str, password: str, host: str,
+                         port: int) -> Tuple:
+    """
+    Check if the user can execute OS commands.
+
+    :param dbname: database name.
+    :param user: username with access permissions to the database.
+    :param password: database password.
+    :param host: database ip.
+    :param port: database port.
+    :returns: - ``OPEN`` if we were able execute OS commands.
+              - ``UNKNOWN`` on errors.
+              - ``CLOSED`` otherwise.
+    :rtype: :class:`fluidasserts.Result`
+    """
+    connection_string: ConnectionString = ConnectionString(
+        dbname, user, password, host, port)
+    success: bool = False
+    msg_open: str = 'The user can execute OS commands'
+    msg_closed: str = 'The user can\'t execute OS commands'
+
+    try:
+        with databse(connection_string) as database:
+            database[1].execute('EXEC xp_cmdshell \'mkdir test, NO_OUTPUT\'')
+            success = True
+    except pymssql.Error as exc:
+        if not _is_compatibility_error and not _is_permission_error(
+                exc) and not _is_xp_cmdshell_error(exc):
             raise exc
 
     vulns: List[str] = []
