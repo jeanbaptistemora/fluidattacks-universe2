ebbe915c09845e1f0646d9e4713e5329427967f9
dsalazaratfluid <dsalazar@fluidattacks.com>
2019-10-04 18:16:33 -0500

    feat(back): #216.37 serves admin
    - Add policies and groups for a serves admin,
    this is necessary for later giving this specific
    role access to the kms key.
    
    
    Former-commit-id: 4d6d12227b14f095c16175fd0e021d52aea2eb1d

---
+++ services/sops-serves/terraform/serves-admin.tf
@@ -0,0 +1,46 @@
+data "aws_iam_policy_document" "serves-admin-data" {
+
+  statement {
+    sid = "General admin"
+    effect = "Allow"
+    actions = [
+      "sns:*",
+      "rds:*",
+      "s3:*",
+      "dynamodb:Scan",
+      "elasticloadbalancing:*",
+      "autoscaling:*",
+      "iam:*",
+      "secretsmanager:*",
+      "cloudwatch:*",
+      "kms:*",
+      "route53:*",
+      "ecr:*",
+      "ec2:*",
+      "eks:*",
+      "elasticache:*"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "serves-admin" {
+  name        = "serves-admin"
+  path        = "/serves/"
+  description = "Policy for serves administration"
+
+  policy = data.aws_iam_policy_document.serves-admin-data.json
+}
+
+resource "aws_iam_group" "serves-admin" {
+  name = "serves-admin"
+  path = "/serves/"
+}
+
+resource "aws_iam_policy_attachment" "serves-admin" {
+  name       = "serves-admin"
+  groups     = aws_iam_group.serves-admin.name
+  policy_arn = aws_iam_policy.serves-admin-policy.arn
+}
