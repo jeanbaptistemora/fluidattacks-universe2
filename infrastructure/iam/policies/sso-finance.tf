20d7af5b8367ca4c45eb308dd93732a0095ed8fd
dsalazaratfluid <dsalazar@fluidattacks.com>
2019-10-09 08:18:41 -0500

    refac(back): #255.1 sso finance account id
    - Remove account id from sso finance file
    
    
    Former-commit-id: b7e840abb0f0a82702a8181e4c5d5f521147c4a5

--- infrastructure/iam/policies/sso-finance.tf
+++ infrastructure/iam/policies/sso-finance.tf
@@ -7,7 +7,7 @@ data "aws_iam_policy_document" "ssofinance-policy" {
       "ec2:StopInstances"
     ]
     resources = [
-      "arn:aws:ec2:us-east-1:205810638802:instance/i-0ba149836e9bb8e7c"
+      "arn:aws:ec2:us-east-1:${data.aws_caller_identity.current.account_id}:instance/i-0ba149836e9bb8e7c"
     ]
   }
 }
