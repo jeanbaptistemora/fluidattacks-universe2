638ec51054a13480d5fab475e4778562fb529d4a
Oscar Prado <oprado@fluidattacks.com>
2019-10-11 14:20:04 -0500

    refac(front): #1596.52 tachyons monokai
    Source blocks are now customized
    entirely by tachyons
    
    locally rendered changes:
    
    
    Former-commit-id: e333c162524b5826c44152ed787ed9336c93ba79

--- theme/2014/static/js/dynclasses.js
+++ theme/2014/static/js/dynclasses.js
@@ -43,7 +43,25 @@ Tachyons classes on dynamically generated content
     $( ".qlist > ol > li" ).addClass("mb0 pb3");
 
     //red inner links
-    $( "span.inner > a" ).addClass("c-fluid-dkred b hover-bg-light-gray")
+    $( "span.inner > a" ).addClass("c-fluid-dkred b hover-bg-light-gray");
+
+    //monokai theme
+    $( ".highlight > pre" ).addClass("bg-code-bk ma0 br0 bn tl c-codegray");
+    $( ".highlight > pre > span.c, span.cm, span.cp, span.c1, span.cs, span.gu" ).addClass("c-codebrown");
+    $( ".highlight > pre > span.err" ).addClass("bg-code-dkred c-codepink");
+    $( ".highlight > pre > span.k, span.kc, span.kd, span.kp, span.kr, span.kt, span.no" ).addClass("c-codelightblue");
+    $( ".highlight > pre > span.l, span.m, span.mh, span.mf, span.mi, span.mo ,span.se span.il" ).addClass("c-codepurple");
+    $( ".highlight > pre > span.n, span.p, span.nb, span.ni, span.nl, span.py ,span.nv span.w span.bp span.vc span.vg span.vi" ).addClass("c-codegray");
+    $( ".highlight > pre > span.o, span.kn, span.nt, span.ow, span.gd" ).addClass("c-codelightred");
+    $( ".highlight > pre > span.ge" ).addClass("i");
+    $( ".highlight > pre > span.gs" ).addClass("b");
+    $( ".highlight > pre > span.ld, span.s, span.sb, span.sc, span.sd, span.s2 ,span.sh span.si span.sx span.sr span.s1 span.ss" ).addClass("c-codeyellow");
+    $( ".highlight > pre > span.na, span.nc, span.nd, span.ne, span.nf, span.nx, span.gi" ).addClass("c-codegreen");
+    $( ".highlighttable" ).addClass("collapse");
+    $( ".highlighttable > pre" ).addClass("fs-normal pl3 tl");
+    $( ".linenos" ).addClass("bn pa0 v-top");
+    $( ".linenos > .linenodiv > pre" ).addClass("bg-code-bk bn br0 ma0 c-codegray");
+    $( ".code" ).addClass("bn pa0 w-100");
 
   });
 })(jQuery);
