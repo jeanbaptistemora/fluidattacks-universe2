5cc37c846ed90e24cf69ef57a45c055d95a426b3
Oscar Prado <oprado@fluidattacks.com>
2019-10-16 17:06:46 -0500

    feat(front): #2469.4 small details
    - Added function to open the menu
    when the url contains the anchor of a category
    
    - Removed empty class in rules htmls
    (seems it does not damage anything)
    
    - Fixed rules descriptions to meet
    the previous layout style
    (italic, centered, some horizontal padding)
    
    locally rendered changes:
    
    
    Former-commit-id: 83d3df5462b57e35b4bcb2c50798c61a149b514f

--- theme/2014/static/js/rules.js
+++ theme/2014/static/js/rules.js
@@ -10,3 +10,16 @@ for (var i = 0; i < rulescat.length; i++ ){
     ruleslist.classList.toggle("dn");
   });
 }
+
+//Activate the list when the category is referenced through the url
+$(document).ready(function () {
+  var anchor = window.location.hash;
+  if (anchor != "") {
+    var span = document.getElementById(anchor.replace('#',''));
+    var category = span.parentNode.parentNode;
+    category.classList.toggle("bg-fluid-lightgray");
+    category.children[1].classList.toggle("rotate-90");
+    var list = category.nextElementSibling;
+    list.classList.toggle("dn");
+  }
+});
