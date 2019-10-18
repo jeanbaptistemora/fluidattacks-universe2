b32059ff3e2c9a1f2019d0c966e876c51f300b62
Andres Roldan <aroldan@fluidattacks.com>
2019-10-18 10:25:50 -0500

    fix(build): #782 use upstream tlslite-ng
    - Upstream tlslite-ng project now supports ECDSA certs
    
    
    Former-commit-id: 841671f840f807757a1575146a5406b341148131

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ setup(
         'requests==2.22.0',            # fluidasserts.proto.http
         'requirements-detector==0.6',  # fluidasserts.sca
         'selenium==3.141.0',           # fluidasserts.helper.http
-        'tlslite-ng-ecdsa==0.0.1',     # fluidasserts.proto.ssl
+        'tlslite-ng==0.8.0-alpha28',   # fluidasserts.proto.ssl
         'typed-ast==1.4.0',            # fluidasserts
         'viewstate==0.4.3',            # fluidasserts.proto.http
     ],
