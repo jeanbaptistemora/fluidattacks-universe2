51cdf473c5c54fa630aaf753a3870874b79bee72
John Perez <jperez@fluidattacks.com>
2019-11-18 16:48:22 -0500

    refac(build): #277 integrates user policies
    - Refactor integrates-user policies
    - Add region variable
    
    
    Former-commit-id: 066f7e6894a7f8a0fd07a5b39cbb2c8e4f9b145f

--- services/user-provision/integrates/terraform/integrates-dev.tf
+++ services/user-provision/integrates/terraform/integrates-dev.tf
@@ -1,3 +1,14 @@
+data "aws_iam_policy_document" "integrates-dev-policy-data" {
+    statement {
+        effect = "Allow"
+        actions = ["kms:*"]
+        resources = [
+            "arn:aws:kms:${var.region}:${data.aws_caller_identity.current.account_id}:alias/integrates-dev-*"
+            ]
+    }
+
+}
+
 resource "aws_iam_user" "integrates-dev" {
  name = "integrates-dev"
  path = "/user-provision/"
@@ -10,24 +21,5 @@ resource "aws_iam_access_key" "integrates-dev-key" {
 resource "aws_iam_user_policy" "integrates-user-policies" {
     name = "user-provision-policy-${aws_iam_user.integrates-dev.name}"
     user = aws_iam_user.integrates-dev.name
-
-    policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "kms:*"
-            ],
-            "Effect": "Allow",
-            "Resource": "*",
-            "Condition" : {
-                "StringEquals" : {
-                    "aws:username" : "${aws_iam_user.integrates-dev.name}"
-                    }
-                }
-        }
-    ]
-}
-EOF
+    policy = data.aws_iam_policy_document.integrates-dev-policy-data.json
 }
--- services/user-provision/integrates/terraform/main.tf
+++ services/user-provision/integrates/terraform/main.tf
@@ -11,5 +11,5 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   version = ">= 2.11"
-  region  = "us-east-1"
+  region  = var.region
 }
