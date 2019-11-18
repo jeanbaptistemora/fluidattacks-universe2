4d517766093201c9c37af8dd08ec042285f2f553
John Perez <jperez@fluidattacks.com>
2019-11-18 15:27:37 -0500

    feat(build): #277 added output
    - Added key id output
    
    
    Former-commit-id: c39e4f994a1c0c7cb6c53a46d5acbf68f0b43fa9

--- services/user-provision/integrates/terraform/outputs.tf
+++ services/user-provision/integrates/terraform/outputs.tf
@@ -1,3 +1,7 @@
-output "integrates-dev-key" {
+output "integrates-dev-secret-key-id" {
+  value = aws_iam_access_key.integrates-dev-key.id
+}
+
+output "integrates-dev-secret-key" {
   value = aws_iam_access_key.integrates-dev-key.secret
 }
