358f2d39999dd30f457fa6aec16ef34c677fdf5e
Kevin Amado <kamado@fluidattacks.com>
2019-11-29 11:20:14 -0500

    fix(job): #856 fix default paths
    - 'default' was moved to 'public' repository
    
    Closes #856
    
    
    Former-commit-id: c1e2a1db59b629e610338763fb039c4a0cf041f4

--- ci-scripts/commitlint-conf.sh
+++ ci-scripts/commitlint-conf.sh
@@ -4,7 +4,7 @@
 
 COMMITLINT_RULES_NAME='commitlint.config.js'
 COMMITLINT_PARSER_NAME='parser-preset.js'
-COMMITLINT_BASE_URL='https://gitlab.com/fluidattacks/default/raw/master/commitlint-configs/others'
+COMMITLINT_BASE_URL='https://gitlab.com/fluidattacks/public/raw/master/commitlint-configs/others'
 COMMITLINT_RULES_URL="$COMMITLINT_BASE_URL/$COMMITLINT_RULES_NAME"
 COMMITLINT_PARSER_URL="$COMMITLINT_BASE_URL/$COMMITLINT_PARSER_NAME"
 
