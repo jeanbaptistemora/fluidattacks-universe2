9ea19fcb3fa59edbfdb021553fae6767c295afda
dsalazaratfluid <dsalazar@fluidattacks.com>
2019-12-04 12:25:47 -0500

    feat(build): #300.1 build-builder
    - Add build_container helper
    - Add build-builder sh script
    - Refactor builder dockerfile
    - Other minor changes
    
    
    Former-commit-id: 7a417e2d63b7d07c688c86b905421897d40401ea

---
+++ ci-scripts/jobs/build-builder.sh
@@ -0,0 +1,15 @@
+build_builder() {
+
+  # Build serves builder
+
+  set -euov pipefail
+
+  # Import functions
+  . ci-scripts/helpers/others.sh
+
+  build_container \
+    "registry.gitlab.com/fluidattacks/serves/builder:$CI_COMMIT_REF_NAME" \
+    containers/builder
+}
+
+build_builder
