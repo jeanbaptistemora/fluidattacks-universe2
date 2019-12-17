39089ce5231583c17ca625c5644f575fc3c462d3
Daniel Salazar <dsalazar@fluidattacks.com>
2019-12-17 18:41:48 -0500

    feat(back): #313.2 add gateway
    - Add gateway to vpc
    - Change vpc ip to a valid private ip
    - Add route to vpc default table for internet access through gateway
    
    
    Former-commit-id: 18ff54b6087b7e0638ffb858c468d37a7f32076a

--- services/fluid-vpc/terraform/subnet-autoscaling-ci.tf
+++ services/fluid-vpc/terraform/subnet-autoscaling-ci.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "subnet-autoscaling-ci" {
   vpc_id     = aws_vpc.fluid-vpc.id
-  cidr_block = "10.0.0.0/22"
+  cidr_block = "192.168.0.0/22"
   availability_zone = "us-east-1a"
 
   tags = {
--- services/fluid-vpc/terraform/vpc.tf
+++ services/fluid-vpc/terraform/vpc.tf
@@ -1,6 +1,20 @@
 resource "aws_vpc" "fluid-vpc" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = "192.168.0.0/16"
   tags = {
     Name = "fluid-vpc"
   }
+}
+
+resource "aws_internet_gateway" "fluid-vpc" {
+  vpc_id = aws_vpc.fluid-vpc.id
+
+  tags = {
+    Name = "fluid-vpc"
+  }
+}
+
+resource "aws_route" "fluid-vpc-internet" {
+  route_table_id         = aws_vpc.fluid-vpc.default_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.fluid-vpc.id
 }
