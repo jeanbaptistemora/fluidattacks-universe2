8b90383da1c3630599adce8d3d781737bc48104d
Diego Restrepo <drestrepo@fluidattacks.com>
2019-12-20 10:28:00 -0500

    feat(back): #863 has os disk encryption disabled
    - Check if Virtual Machines OS Disks has encryption disabled.
    
    
    Former-commit-id: 1ea3c92fa9e781114c164d578c92f38a0c874f80

---
+++ fluidasserts/cloud/azure/virtual_machines.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+"""Fluid Asserts Azure Virtual Machines package."""
+
+# standar imports
+from typing import Tuple
+
+# 3rd party imports
+from msrest.exceptions import AuthenticationError, ClientException
+from azure.mgmt.compute import ComputeManagementClient
+
+# local imports
+from fluidasserts import DAST, MEDIUM
+from fluidasserts.utils.decorators import api, unknown_if
+from fluidasserts.cloud.azure import (_get_result_as_tuple, _get_credentials)
+
+
+@api(risk=MEDIUM, kind=DAST)
+@unknown_if(ClientException, AuthenticationError)
+def has_os_disk_encryption_disabled(client_id: str, secret: str, tenant: str,
+                                    subscription_id: str) -> Tuple:
+    """
+    Check if Virtual Machines OS Disks has encryption disabled.
+
+    :param client_id: Azure service client_id.
+    :param secret: Azure service secret.
+    :param tenant: Azure service tenant.
+    :param subscription_id: Azure subscription ID.
+
+    :returns: - ``OPEN`` if there are OS disk that do not have
+                encryption enabled.
+              - ``UNKNOWN`` on errors.
+              - ``CLOSED`` otherwise.
+
+    :rtype: :class:`fluidasserts.Result`
+    """
+    msg_open: str = 'OS disks has encryption disabled.'
+    msg_closed: str = 'OS disks has encryption enabled.'
+    vulns, safes = [], []
+
+    credentials = _get_credentials(client_id, secret, tenant)
+
+    disks = ComputeManagementClient(credentials, subscription_id).disks.list()
+
+    for disk in filter(lambda d: d.os_type is not None, disks):
+        (vulns if not disk.encryption_settings_collection else safes).append(
+            (disk.id, 'must enable encryption.'))
+
+    return _get_result_as_tuple(
+        objects='Disks',
+        msg_open=msg_open,
+        msg_closed=msg_closed,
+        vulns=vulns,
+        safes=safes)
