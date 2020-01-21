9fc568f6d8d63c73284eaa3d4c24ecaad1c7d02e
Daniel Salazar <dsalazar@fluidattacks.com>
2020-01-21 13:30:10 -0500

    feat(back): #326.2 continuous-prod rotation
    - Add script and job for rotating continuous aws credentials
    - Remove vault user rotation as it is no longer being used
    - Other minor changes
    
    
    Former-commit-id: a3ffea3330126e7bedbec2c200a5a754aa0878d9

--- ci-scripts/jobs/user-provision-integrates-prod.sh
+++ ci-scripts/jobs/user-provision-integrates-prod.sh
@@ -43,3 +43,41 @@ user_provision_integrates_prod_terraform_plan() {
     fluidattacks-terraform-states-prod \
     plan
 }
+
+user_provision_integrates_prod_rotate_aws() {
+
+  # Script to rotate integrates-prod access key
+
+  set -Eeuo pipefail
+
+  local INTEGRATES_REPO_ID
+  local TERRAFORM_DIR
+  local BUCKET
+
+  BUCKET='fluidattacks-terraform-states-prod'
+  TERRAFORM_DIR='services/user-provision-integrates/integrates-prod/terraform'
+  INTEGRATES_REPO_ID='4620828'
+
+  # Import functions
+  . toolbox/terraform.sh
+  . ci-scripts/helpers/others.sh
+  . <(curl -s https://gitlab.com/fluidattacks/public/raw/master/shared-scripts/gitlab-variables.sh)
+
+  taint_terraform \
+    "$TERRAFORM_DIR" \
+    "$BUCKET" \
+    aws_iam_access_key.integrates-prod-key
+
+  run_terraform \
+    "$TERRAFORM_DIR" \
+    "$BUCKET" \
+    apply
+
+  VAR_KEY="$(output_terraform $TERRAFORM_DIR $BUCKET integrates-prod-secret-key-id)"
+  VAR_SECRET="$(output_terraform $TERRAFORM_DIR $BUCKET integrates-prod-secret-key)"
+
+  set_project_variable "$GITLAB_API_TOKEN" "$INTEGRATES_REPO_ID" PROD_AWS_ACCESS_KEY_ID "$VAR_KEY" true false
+  set_project_variable "$GITLAB_API_TOKEN" "$INTEGRATES_REPO_ID" PROD_AWS_SECRET_ACCESS_KEY "$VAR_SECRET" true false
+
+  deploy_integrates
+}
