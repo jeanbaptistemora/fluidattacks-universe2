1b73f774e3cf773fe13c03f04f155c72b20e7502
Daniel Salazar <dsalazar@fluidattacks.com>
2020-01-21 14:15:15 -0500

    feat(back): #326.3 integrates-dev rotation
    - Add script and job for rotation integrates dev aws
    credentials
    
    
    Former-commit-id: 42f7e8482f42e336eef644517402d673903ea0cc

--- ci-scripts/jobs/user-provision-integrates-dev.sh
+++ ci-scripts/jobs/user-provision-integrates-dev.sh
@@ -43,3 +43,38 @@ user_provision_integrates_dev_terraform_lint() {
     services/user-provision-integrates/integrates-dev/terraform \
     fluidattacks-terraform-states-prod
 }
+
+user_provision_integrates_dev_rotate_aws() {
+
+  # Script to rotate integrates-dev access key
+
+  set -Eeuo pipefail
+
+  local INTEGRATES_REPO_ID
+  local TERRAFORM_DIR
+  local BUCKET
+
+  BUCKET='fluidattacks-terraform-states-prod'
+  TERRAFORM_DIR='services/user-provision-integrates/integrates-dev/terraform'
+  INTEGRATES_REPO_ID='4620828'
+
+  # Import functions
+  . toolbox/terraform.sh
+  . <(curl -s https://gitlab.com/fluidattacks/public/raw/master/shared-scripts/gitlab-variables.sh)
+
+  taint_terraform \
+    "$TERRAFORM_DIR" \
+    "$BUCKET" \
+    aws_iam_access_key.integrates-dev-key
+
+  run_terraform \
+    "$TERRAFORM_DIR" \
+    "$BUCKET" \
+    apply
+
+  VAR_KEY="$(output_terraform $TERRAFORM_DIR $BUCKET integrates-dev-secret-key-id)"
+  VAR_SECRET="$(output_terraform $TERRAFORM_DIR $BUCKET integrates-dev-secret-key)"
+
+  set_project_variable "$GITLAB_API_TOKEN" "$INTEGRATES_REPO_ID" DEV_AWS_ACCESS_KEY_ID "$VAR_KEY" false false
+  set_project_variable "$GITLAB_API_TOKEN" "$INTEGRATES_REPO_ID" DEV_AWS_SECRET_ACCESS_KEY "$VAR_SECRET" false false
+}
