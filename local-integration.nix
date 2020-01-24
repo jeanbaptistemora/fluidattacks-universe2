d5533ba323276965c7042493d455a7fc3f65ae4b
Kevin Amado <kamado@fluidattacks.com>
2020-01-24 14:57:40 -0500

    refac(job): #862 remove unused code
    Former-commit-id: 5b715d0081f413b1b813c369336430ac738d17e2

--- local-integration.nix
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash -p docker git gitlab-runner
-
-DOCKER_PASS="set"
-DOCKER_USER="set"
-
-gitlab-ci-multi-runner exec docker --docker-privileged --docker-volumes /var/run/docker.sock:/var/run/docker.sock --env "DOCKER_USER=$DOCKER_USER" --env "DOCKER_PASS=$DOCKER_PASS" --env "PYPI_USER=$PYPI_USER" --env "PYPI_PASS=$PYPI_PASS" build
-gitlab-ci-multi-runner exec docker --docker-privileged --docker-volumes /var/run/docker.sock:/var/run/docker.sock --env "DOCKER_USER=$DOCKER_USER" --env "DOCKER_PASS=$DOCKER_PASS" --env "PYPI_USER=$PYPI_USER" --env "PYPI_PASS=$PYPI_PASS" test
