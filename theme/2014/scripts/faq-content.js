e2f1ea952acaa675730f409562eafab19d4904d6
John Perez <jperez@fluidattacks.com>
2020-02-04 17:24:47 -0500

    refac(cross): #921.2 typescript configuration
    - Moved js files to a propper location in preparation for TypeScript
    - Move tsconfig.json to a propper location
    - Update to latest version of TypeScript
    - Added rules needed to tsconfig.json file
    - Added tslint with some basic rules
    - Added first TypeScript code to test project behavior
    
    
    Former-commit-id: 8aab99452fc762dd0f3fd70f9affe54bfd353645

--- theme/2014/scripts/faq-content.js
+++ theme/2014/scripts/faq-content.js
@@ -1,9 +1,4 @@
 //Add basic styles for active tabs
-$('.accordion-item-title').on('click', function() {
-    $(this).addClass('red');
-    $(this).parent('.accordion-item').siblings().find('.accordion-item-title').removeClass('red');
-  });
-
 (function($){
     $(document).ready(function(){
         // Add acordion item class
