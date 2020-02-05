380a241676da1f3628e9c06a53ff3d3517497b55
Simon Gomez <sgomez@fluidattacks.com>
2020-02-05 07:41:58 -0500

    feat(back): #835 has_not_point_in_time_recovery
    Former-commit-id: 25940a2b6234117f895ddc4fbaaedc3a8f375122

--- test/test_cloud_aws_terraform_dynamodb.py
+++ test/test_cloud_aws_terraform_dynamodb.py
@@ -1,22 +1,22 @@
-"""Test methods of fluidasserts.cloud.cloudformation.dynamodb module."""
+"""Test methods of fluidasserts.cloud.terraform.dynamodb module."""
 
 # local imports
-from fluidasserts.cloud.aws.cloudformation import dynamodb
+from fluidasserts.cloud.aws.terraform import dynamodb
 
 # 3rd party imports
 import pytest
-pytestmark = pytest.mark.asserts_module('cloud_aws_cloudformation')
+pytestmark = pytest.mark.asserts_module('cloud_aws_terraform')
 
 # Constants
-SAFE: str = 'test/static/cloudformation/safe'
-VULN: str = 'test/static/cloudformation/vulnerable'
-NOT_EXISTS: str = 'test/static/cloudformation/not-exists'
+SAFE: str = 'test/static/terraform/safe'
+VULN: str = 'test/static/terraform/vulnerable'
+NOT_EXISTS: str = 'test/static/terraform/not-exists'
 
 
 def test_has_not_point_in_time_recovery():
     """test dynamodb.has_not_point_in_time_recovery."""
     result = dynamodb.has_not_point_in_time_recovery(VULN)
     assert result.is_open()
-    assert result.get_vulns_number() == 2 * 2
+    assert result.get_vulns_number() == 1
     assert dynamodb.has_not_point_in_time_recovery(SAFE).is_closed()
     assert dynamodb.has_not_point_in_time_recovery(NOT_EXISTS).is_unknown()
