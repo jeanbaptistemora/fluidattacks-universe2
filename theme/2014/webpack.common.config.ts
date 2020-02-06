bd94d9d902e8fb9501417061ad8f8d061446db70
John Perez <jperez@fluidattacks.com>
2020-02-06 16:04:16 -0500

    refac(front): #921.5 install webpack
    - Installed webpack with necessary dependencies
    - Updated gitignore for future files
    - Added terser package to replace uglify js because it cant support es6+
    and uglify-es is no longer maintained
    - Added webpack configuration
    
    
    Former-commit-id: 9db25a905f9f3a12cdf658de599101d5e682db9c

---
+++ theme/2014/webpack.common.config.ts
@@ -0,0 +1,37 @@
+import path from "path";
+import webpack from "webpack";
+
+export const commonConfig: webpack.Configuration = {
+  entry: {
+    faqContent: "./static/js/faqContent.ts",
+    jquery: "./static/js/jquery.ts",
+  },
+  module: {
+    rules: [
+      {
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              configFile: "tsconfig.webpack.json",
+              transpileOnly: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    filename: "[name].min.js",
+    futureEmitAssets: true,
+    path: path.resolve(__dirname, "./static/js/"),
+  },
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+  stats: {
+    children: false,
+    colors: true,
+    modules: false,
+  },
+};
