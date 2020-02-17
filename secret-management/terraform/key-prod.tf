310b94f570ea0939ef0d30965d08fb37710d39e4
Daniel Salazar <dsalazar@fluidattacks.com>
2020-02-17 15:25:11 -0500

    feat(cross): #2952 secret-management
    - Add secret-management infra service
    - Add test job (Still need to install terraform
    in order to activate it)
    
    
    Former-commit-id: d4c6f71488d83276cf2f90a2cbd3f78c053e81d2

---
+++ secret-management/terraform/key-prod.tf
@@ -0,0 +1,115 @@
+data "aws_iam_policy_document" "key-web-production" {
+
+  statement {
+    sid    = "Enable IAM User Permissions"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions = [
+      "kms:*"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid    = "Key Administrators"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/FLUIDServes_TF",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/serves-admin",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/web-admin",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user-provision/web-prod"
+      ]
+    }
+    actions = [
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:TagResource",
+      "kms:UntagResource",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion",
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid    = "Key Users"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/FLUIDServes_TF",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/serves-admin",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/web-admin",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user-provision/web-prod"
+      ]
+    }
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid    = "Attachment Of Persistent Resources"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/FLUIDServes_TF",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/serves-admin",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/web-admin",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user-provision/web-prod"
+      ]
+    }
+    actions = [
+      "kms:CreateGrant",
+      "kms:ListGrants",
+      "kms:RevokeGrant",
+    ]
+    resources = [
+      "*"
+    ]
+    condition {
+      test     = "Bool"
+      variable = "kms:GrantIsForAWSResource"
+      values   = [
+        "true",
+      ]
+    }
+  }
+}
+
+resource "aws_kms_key" "key-web-prod" {
+  description             = "production kms key for web."
+  policy                  = data.aws_iam_policy_document.key-web-production.json
+  deletion_window_in_days = 30
+  is_enabled              = true
+}
+
+resource "aws_kms_alias" "key-web-prod" {
+  name          = "alias/web-prod"
+  target_key_id = aws_kms_key.key-web-prod.key_id
+}
---
+++ secret-management/terraform/role-continuous-admin.tf
@@ -0,0 +1,9 @@
+resource "aws_iam_role" "web-admin" {
+  name               = "web-admin"
+  assume_role_policy = data.aws_iam_policy_document.okta-assume-role-policy-data.json
+}
+
+resource "aws_iam_role_policy_attachment" "attach-break-build-audit-policy" {
+  role       = aws_iam_role.web-admin.name
+  policy_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/user-provision/web-prod-policy"
+}
