fdef657d2f2f9e56b965dda825e427dbcceb0d12
Daniel Salazar <dsalazar@fluidattacks.com>
2020-02-25 10:31:24 -0500

    feat(back): #363 add second key
    - Add second aws key to integrates-dev
    
    
    Former-commit-id: 54e6ffb79b171408b11e8519be5501c5373dfaf9

--- services/user-provision/integrates/dev/terraform/integrates-dev.tf
+++ services/user-provision/integrates/dev/terraform/integrates-dev.tf
@@ -3,6 +3,10 @@ resource "aws_iam_user" "integrates-dev" {
   path = "/user-provision/"
 }
 
-resource "aws_iam_access_key" "integrates-dev-key" {
+resource "aws_iam_access_key" "integrates-dev-key-1" {
+  user = var.user-name
+}
+
+resource "aws_iam_access_key" "integrates-dev-key-2" {
   user = var.user-name
 }
--- services/user-provision/integrates/dev/terraform/outputs.tf
+++ services/user-provision/integrates/dev/terraform/outputs.tf
@@ -1,9 +1,19 @@
-output "integrates-dev-secret-key-id" {
+output "integrates-dev-secret-key-id-1" {
   sensitive = true
-  value     = aws_iam_access_key.integrates-dev-key.id
+  value     = aws_iam_access_key.integrates-dev-key-1.id
 }
 
-output "integrates-dev-secret-key" {
+output "integrates-dev-secret-key-1" {
   sensitive = true
-  value     = aws_iam_access_key.integrates-dev-key.secret
+  value     = aws_iam_access_key.integrates-dev-key-1.secret
+}
+
+output "integrates-dev-secret-key-id-2" {
+  sensitive = true
+  value     = aws_iam_access_key.integrates-dev-key-2.id
+}
+
+output "integrates-dev-secret-key-2" {
+  sensitive = true
+  value     = aws_iam_access_key.integrates-dev-key-2.secret
 }
