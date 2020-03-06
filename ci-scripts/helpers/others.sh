d17de17abbecb90b29a8bd9713d0566819cb2521
Daniel Salazar <dsalazar@fluidattacks.com>
2020-03-06 13:54:22 -0500

    feat(cross): #2966 eph env on buckets
    - Add deploy function for ephemeral envs
    in buckets
    
    
    Former-commit-id: 86b43aed432856e1bcaac1414c6771f6fb6f2da6

--- ci-scripts/helpers/others.sh
+++ ci-scripts/helpers/others.sh
@@ -21,3 +21,43 @@ terraform_login() {
   TF_VAR_aws_access_key="$AWS_ACCESS_KEY_ID"
   TF_VAR_aws_secret_key="$AWS_SECRET_ACCESS_KEY"
 }
+
+function sync_s3 {
+  # Synchronize <source_code> in <bucket>
+
+  local source_code="${1}"
+  local bucket_path="${2}"
+  local extensions=("html" "css" "js" "png")
+
+  #aws_login
+
+  # Compress all HTML, CSS and JS files and remove the .gz extension
+  while IFS= read -r file; do
+    gzip -9 "${file}"
+    mv "${file}.gz" "${file}";
+  done < <(find "${source_code}" -type f -name '*.html' -o -name '*.css' -o -name '*.js')
+
+  # Set correct metadata according to the compressed file and upload them
+  for extension in "${extensions[@]}"; do
+    aws s3 sync                          \
+      "${source_code}/web"               \
+      "s3://${bucket_path}/web"          \
+      --acl public-read                  \
+      --exclude "*"                      \
+      --include "*.${extension}"         \
+      --metadata-directive REPLACE       \
+      --content-type "text/${extension}" \
+      --content-encoding gzip            \
+      --delete
+  done
+
+  # Upload remaining files
+  aws s3 sync            \
+  "${source_code}/"      \
+  "s3://${bucket_path}/" \
+  --exclude "*.html"     \
+  --exclude "*.css"      \
+  --exclude "*.js"       \
+  --exclude "*.png"      \
+  --delete
+}
