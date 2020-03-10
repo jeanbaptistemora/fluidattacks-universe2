f189e50a833f4d8e0e994953897ccd036e9b10d9
Daniel Salazar <dsalazar@fluidattacks.com>
2020-03-10 05:38:44 -0500

    feat(infra): #2966 etags
    - Add etags to bucket object resources
    in order to trigger re-uploads in case
    file changes
    - Other minor changes
    
    
    Former-commit-id: a2d7795ec31c41addf084a92908f41a94afef1c1

--- deploy/ephemeral/terraform/bucket.tf
+++ deploy/ephemeral/terraform/bucket.tf
@@ -5,7 +5,7 @@ resource "aws_s3_bucket" "web-ephemeral-bucket" {
 
   website {
      index_document = "index.html"
-     error_document = "index-error.html"
+     error_document = "error-index.html"
    }
    tags = {
     Pry = "General"
@@ -16,12 +16,14 @@ resource "aws_s3_bucket_object" "home-index" {
   bucket       = aws_s3_bucket.web-ephemeral-bucket.id
   key          = "index.html"
   source       = "index.html"
+  etag         = filemd5("index.html")
   content_type = "text/html"
 }
 
 resource "aws_s3_bucket_object" "error-index" {
   bucket       = aws_s3_bucket.web-ephemeral-bucket.id
-  key          = "index-error.html"
-  source       = "index-error.html"
+  key          = "error-index.html"
+  source       = "error-index.html"
+  etag         = filemd5("error-index.html")
   content_type = "text/html"
 }
