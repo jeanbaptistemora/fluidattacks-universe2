f9f671600ed23041021a7fe955654d9101e9aecc
John Perez <jperez@fluidattacks.com>
2020-03-18 13:34:10 -0500

    feat(front): #2961.5 carousel for continuous page
    - added carousel component for continuous page
    
    
    Former-commit-id: be1c69e2299ad7a06131348416daf50fa5ab957c

--- new/theme/2020/webpack.common.config.ts
+++ new/theme/2020/webpack.common.config.ts
@@ -3,6 +3,7 @@ import webpack from "webpack";
 
 export const commonConfig: webpack.Configuration = {
   entry: {
+    carousel: "./static/js/carousel.ts",
     contentHome: "./static/js/contentHome.ts",
     dynClasses: "./static/js/dynClasses.ts",
   },
