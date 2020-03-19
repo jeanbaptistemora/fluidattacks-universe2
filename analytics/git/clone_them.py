64394ed8523dc33b3466aa17d2b7941a1c085ef3
Michael Rivera <mrivera@fluidattacks.com>
2020-03-19 17:41:45 -0500

    rever(job): #386.6 revert provisional code
    - gets rid of test code so pipeline can run properly
    
    
    Former-commit-id: 7086dd094720533fa4b22dac840d85492c7e9847

--- analytics/git/clone_them.py
+++ analytics/git/clone_them.py
@@ -5,10 +5,8 @@ import os
 import glob
 import textwrap
 import subprocess
-import time
 
 from typing import Tuple
-from multiprocessing import Process
 
 
 def run_command(cmd: str, cwd: str) -> Tuple[int, str]:
@@ -54,17 +52,6 @@ def main() -> None:
     subs_paths = glob.glob(f'/git/fluidattacks/continuous/subscriptions/*')
     for subs_path in subs_paths:
         clone(subs_path)
-    vpn_req = ['/git/fluidattacks/continuous/subscriptions/tabbasa',
-               '/git/fluidattacks/continuous/subscriptions/targon',
-               '/git/fluidattacks/continuous/subscriptions/triana',
-               '/git/fluidattacks/continuous/subscriptions/troitsk',
-               '/git/fluidattacks/continuous/subscriptions/turtwig', ]
-    process = Process(target=open_vpn, args=(vpn_req[0],))
-    process.start()
-    time.sleep(60)
-    for prot_paths in vpn_req:
-        clone(prot_paths)
-    process.terminate()
 
 
 if __name__ == '__main__':
