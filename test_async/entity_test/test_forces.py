99e64540c439ef91fdc11a9a0052db7a59781311
Andres Roldan <aroldan@fluidattacks.com>
2020-03-20 07:29:01 -0500

    test(back): #1635 migrated forces test
    Former-commit-id: 7101cc9482a51f2b9396e9831f2de450010fd4ab

--- test_async/entity_test/test_forces.py
+++ test_async/entity_test/test_forces.py
@@ -37,7 +37,7 @@ class ForcesExecutionsTests(TestCase):
         return result
 
     async def test_project_name(self):
-        """Check for project_name field"""
+        """Check for project_name field."""
         query = '''{
           forcesExecutions(projectName: "unittesting"){
             projectName
@@ -48,3 +48,137 @@ class ForcesExecutionsTests(TestCase):
         assert 'errors' not in result
         assert result['data']['forcesExecutions']['projectName'] \
             == 'unittesting'
+
+    async def test_executions(self):
+        """Check for executions field."""
+        query = """
+          query {
+            forcesExecutions(
+                projectName: "unittesting",
+                fromDate: "2020-02-01T00:00:00Z",
+                toDate: "2020-02-28T23:59:59Z"
+            ) {
+              executions {
+                projectName
+                identifier
+                date
+                exitCode
+                gitBranch
+                gitCommit
+                gitOrigin
+                gitRepo
+                kind
+                log
+                strictness
+                vulnerabilities {
+                  exploits {
+                    kind
+                    who
+                    where
+                  }
+                  mockedExploits {
+                    kind
+                    who
+                    where
+                  }
+                  acceptedExploits {
+                    kind
+                    who
+                    where
+                  }
+                  numOfVulnerabilitiesInExploits
+                  numOfVulnerabilitiesInMockedExploits
+                  numOfVulnerabilitiesInAcceptedExploits
+                }
+              }
+            }
+          }
+        """
+        data = {'query': query}
+        result = await self._get_result(data)
+        executions = result['data']['forcesExecutions']['executions']
+        assert 'errors' not in result
+        assert executions[0]['projectName'] == project_name
+        assert executions[0]['identifier'] == '33e5d863252940edbfb144ede56d56cf'
+        assert executions[0]['date'] == '2020-02-19T19:31:18+00:00'
+        assert executions[0]['exitCode'] == '1'
+        assert executions[0]['gitBranch'] == 'master'
+        assert executions[0]['gitCommit'] == '6e7b34c1358db2ff4123c3c76e7fe3bf9f2838f6'
+        assert executions[0]['gitOrigin'] == 'http://test.com'
+        assert executions[0]['gitRepo'] == 'Repository'
+        assert executions[0]['kind'] == 'dynamic'
+        assert len(executions[0]['log']) > 100
+        assert isinstance(executions[0]['log'], str)
+        assert executions[0]['strictness'] == 'strict'
+        assert executions[0]['vulnerabilities'] == {
+            "exploits": [
+                {
+                    "kind": "DAST",
+                    "who": "https://test.com/test",
+                    "where": "HTTP/Implementation"
+                }
+            ],
+            "mockedExploits": [
+                {
+                    "kind": "DAST",
+                    "who": "https://test.com/test",
+                    "where": "HTTP/Implementation"
+                }
+            ],
+            "acceptedExploits": [
+                {
+                    "kind": "DAST",
+                    "who": "https://test.com/test/looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong",
+                    "where": "HTTP/Implementatioooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooon"
+                }
+            ],
+            "numOfVulnerabilitiesInExploits": 1,
+            "numOfVulnerabilitiesInMockedExploits": 1,
+            "numOfVulnerabilitiesInAcceptedExploits": 1
+        }
+        assert executions[1]['projectName'] == project_name
+        assert executions[1]['identifier'] == 'a125217504d447ada2b81da3e4bdab0e'
+        assert executions[1]['date'] == '2020-02-19T19:04:33+00:00'
+        assert executions[1]['exitCode'] == '0'
+        assert executions[1]['gitBranch'] == 'awesomeFeature'
+        assert executions[1]['gitCommit'] == '23c3c76e7fe3bf9f2838f66e7b34c1358db2ff41'
+        assert executions[1]['gitOrigin'] == 'https://test.com/test'
+        assert executions[1]['gitRepo'] == 'Repository'
+        assert executions[1]['kind'] == 'static'
+        assert len(executions[1]['log']) > 100
+        assert isinstance(executions[1]['log'], str)
+        assert executions[1]['strictness'] == 'strict'
+        assert executions[1]['vulnerabilities'] == {
+            "exploits": [],
+            "mockedExploits": [],
+            "acceptedExploits": [
+                {
+                    "kind": "SAST",
+                    "who": "Repository/folder/folder/file.cs",
+                    "where": "55"
+                },
+                {
+                    "kind": "SAST",
+                    "who": "Repository/folder/folder/file2.cs",
+                    "where": "3"
+                },
+                {
+                    "kind": "SAST",
+                    "who": "Repository/folder/folder/file3.cs",
+                    "where": "8"
+                },
+                {
+                    "kind": "SAST",
+                    "who": "Repository/folder/folder/file3.cs",
+                    "where": "9"
+                },
+                {
+                    "kind": "SAST",
+                    "who": "Repository/folder/folder/file4.cs",
+                    "where": "10"
+                }
+            ],
+            "numOfVulnerabilitiesInExploits": 0,
+            "numOfVulnerabilitiesInMockedExploits": 0,
+            "numOfVulnerabilitiesInAcceptedExploits": 5
+        }
