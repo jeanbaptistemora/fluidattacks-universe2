c169fd4acd5c37814efa60e48e213aa4f9af39fb
John Perez <jperez@fluidattacks.com>
2020-03-30 14:40:11 -0500

    feat(front): #2972.1 products page
    - Added Products page with its content
    
    
    Former-commit-id: af03cab1f590ba3c87854cd10a3d5dbbbc5ebdea

--- new/theme/2020/static/js/dynClasses.ts
+++ new/theme/2020/static/js/dynClasses.ts
@@ -9,6 +9,10 @@ const dynClasses: (() => void) = (): void => {
     addClasses(".use-cases > .sect1 > .sectionbody",
                "tl fw1 w40-l w-50-m w90-s center f4-ns f5 lh-2");
 
+    addClasses(".products > .sect1 > h2", "f3 tc w6-ns center w-90");
+
+    addClasses(".products > .sect1 > .sectionbody",
+               "fw1 w-40-l w-50-m w90-s center f5 lh-2");
   });
 };
 
