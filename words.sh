1e59bd8d8ae061939be9f1a6b6164888b2408864
Daniel Salazar <dsalazar@fluidattacks.com>
2020-03-31 12:49:48 -0500

    refac(job): #2974 clean da house
    - Remove all legacy code that has already been
    replaced
    - no-deltas-check
    
    
    Former-commit-id: 610034dc404a1092958588b6cdc5ec997cbc6a83

--- words.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# This script extracts the text from a document and counts the words in it.
-# It allows the setting of limits to keep documents in a certain length
-# that better suits the target audience.
-
-: "${1?"Number of words on the text. Usage: $0 file"}"
-
-echo -e "$(./exttxt.sh "$1" | style | pcregrep -o1 '(\d*) words,' || echo 0)\\t$1"
