bb4be3358cda8d4f9d3fc42b69f14b5ace84a33d
David Acevedo <dacevedo@fluidattacks.com>
2020-04-01 16:00:25 -0500

    refac(front): #1705.30 remove redux
    - remove dashboard reducer
    - remove hackish wrapper that was needed
    - during the times of the great migration
    
    
    Former-commit-id: 07c1583e2dd0da7b79e6711ec29d32e4d5164732

--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -1,37 +1,6 @@
-import { applyMiddleware, createStore, Store } from "redux";
-import thunk from "redux-thunk";
-import { getEnvironment } from "../utils/context";
+import { createStore, Store } from "redux";
 import rootReducer from "./rootReducer";
 
-/* tslint:disable:no-any
- *
- * NO-ANY: Disabling this rule is necessary because TypeScript doesn't
- * recognize custom properties attached to the global window object,
- * which is required to bind redux dev tools browser extension
- */
-declare var window: any;
-const reduxDevTools: any = (): any =>
-  (
-    /* tslint:disable-next-line:strict-type-predicates
-     * Disabling this rule is necessary because the following expression is
-     * misbelived to be always true by the linter while it is necessary for
-     * avoiding errors in the npm test environment where the object window
-     * doesn't exist.
-     */
-    typeof window === "undefined"
-    ? undefined
-    : window.__REDUX_DEVTOOLS_EXTENSION__
-      ? window.__REDUX_DEVTOOLS_EXTENSION__()
-      : undefined
-  );
-/* tslint:enable:no-any */
-
-const store: Store = createStore(
-  rootReducer,
-  getEnvironment() === "production"
-  ? undefined
-  : reduxDevTools(),
-  applyMiddleware(thunk),
-);
+const store: Store = createStore(rootReducer);
 
 export = store;
--- front/src/store/rootReducer.ts
+++ front/src/store/rootReducer.ts
@@ -1,10 +1,6 @@
 import { combineReducers, Reducer } from "redux";
 import { reducer as formReducer } from "redux-form";
-import { dashboard } from "../scenes/Dashboard/reducer";
 
-const rootReducer: Reducer = combineReducers({
-  dashboard,
-  form: formReducer,
-});
+const rootReducer: Reducer = combineReducers({ form: formReducer });
 
 export = rootReducer;
