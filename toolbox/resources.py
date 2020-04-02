9fa72f3811884702c0470bd2e1ac3080c85c43d8
Mateo Marulanda <mmarulanda@fluidattacks.com>
2020-04-02 17:47:05 -0500

    refac(conf): #400 add permissions to s3
    - Add permissions to download repositories from s3
    - Enable quiet mode in aws cli
    - Rename secret-management folder to user-provision

--- toolbox/resources.py
+++ toolbox/resources.py
@@ -636,12 +636,13 @@ def sync_active_repo_to_fusion(subs: str):
     if not os.path.exists(local_path):
         os.mkdir(local_path)
     sync_command = ["aws", "s3", "sync", bucket_path, local_path,
-                    "--sse", "AES256"]
+                    "--sse", "AES256", "--quiet"]
     subprocess.run(sync_command, check=True)
 
 
 def sync_s3_to_fusion(subs: str) -> bool:
     if not does_project_exist(subs):
         return False
+    logger.info(f"Dowloading {subs} repositories")
     sync_active_repo_to_fusion(subs)
     return True
