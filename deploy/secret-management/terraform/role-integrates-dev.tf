883e5430e9391ca72c1e19fe0f40419e54e1844d
Daniel Salazar <dsalazar@fluidattacks.com>
2020-04-02 16:10:34 -0500

    feat(back): #1899 increase session time
    - Increase integrates-dev session time to 9 hours
    
    Closes #1899
    
    
    Former-commit-id: d1600ca0f84fe363961f606cc53aa2d7a867111e

--- deploy/secret-management/terraform/role-integrates-dev.tf
+++ deploy/secret-management/terraform/role-integrates-dev.tf
@@ -1,6 +1,7 @@
 resource "aws_iam_role" "integrates-dev" {
   name = "integrates-dev"
   assume_role_policy = data.aws_iam_policy_document.okta-assume-role-policy-data.json
+  max_session_duration = "32400"
 }
 
 resource "aws_iam_role_policy_attachment" "integrates-dev-push-cloudwatch" {
