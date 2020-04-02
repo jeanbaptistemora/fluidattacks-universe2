7a7d236cfed21ccf172711632ccd6e6426a98b40
Andres Roldan <aroldan@fluidattacks.com>
2020-04-02 16:47:16 -0500

    perf(back): #1635 async event mutations
    - Event mutations are now async
    
    
    Former-commit-id: daf2bfd3f03480a05b50f0e5290b5d63b3986cfe

--- django-apps/integrates-back-async/backend/api/resolvers/event.py
+++ django-apps/integrates-back-async/backend/api/resolvers/event.py
@@ -1,6 +1,10 @@
 # pylint: disable=import-error
 
 from time import time
+import sys
+
+from asgiref.sync import sync_to_async
+
 from backend.api.dataloaders.event import EventLoader
 from backend.decorators import (
     get_cached, require_login, require_event_access, rename_kwargs,
@@ -11,6 +15,14 @@ from backend.domain import project as project_domain
 from backend import util
 
 from ariadne import convert_kwargs_to_snake_case
+
+
+@convert_kwargs_to_snake_case
+def resolve_event_mutation(obj, info, **parameters):
+    """Resolve update_severity mutation."""
+    field = util.camelcase_to_snakecase(info.field_name)
+    resolver_func = getattr(sys.modules[__name__], f'_do_{field}')
+    return util.run_async(resolver_func, obj, info, **parameters)
 
 
 @require_login
@@ -45,15 +57,17 @@ def resolve_events(_, info, project_name):
     return util.run_async(_resolve_events_async, event_ids)
 
 
-@convert_kwargs_to_snake_case
 @require_login
 @enforce_group_level_auth_async
 @require_event_access
-def resolve_update_event(_, info, event_id, **kwargs):
+async def _do_update_event(_, info, event_id, **kwargs):
     """Resolve update_event mutation."""
-    success = event_domain.update_event(event_id, **kwargs)
+    success = await \
+        sync_to_async(event_domain.update_event)(event_id, **kwargs)
     if success:
-        project_name = event_domain.get_event(event_id).get('project_name')
+        event = await \
+            sync_to_async(event_domain.get_event)(event_id)
+        project_name = event.get('project_name')
         util.invalidate_cache(event_id)
         util.invalidate_cache(project_name)
         util.cloudwatch_log(
@@ -62,14 +76,15 @@ def resolve_update_event(_, info, event_id, **kwargs):
     return dict(success=success)
 
 
-@convert_kwargs_to_snake_case
 @require_login
 @enforce_group_level_auth_async
 @require_project_access
-def resolve_create_event(_, info, project_name, image=None, file=None, **kwa):
+async def _do_create_event(
+    _, info, project_name, image=None, file=None, **kwa
+):
     """Resolve create_event mutation."""
     analyst_email = util.get_jwt_content(info.context)['user_email']
-    success = event_domain.create_event(
+    success = await sync_to_async(event_domain.create_event)(
         analyst_email, project_name.lower(), file, image, **kwa)
     if success:
         util.cloudwatch_log(info.context, 'Security: Created event in '
@@ -78,17 +93,18 @@ def resolve_create_event(_, info, project_name, image=None, file=None, **kwa):
     return dict(success=success)
 
 
-@convert_kwargs_to_snake_case
 @require_login
 @enforce_group_level_auth_async
 @require_event_access
-def resolve_solve_event(_, info, event_id, affectation, date):
+async def _do_solve_event(_, info, event_id, affectation, date):
     """Resolve solve_event mutation."""
     analyst_email = util.get_jwt_content(info.context)['user_email']
-    success = event_domain.solve_event(
+    success = await sync_to_async(event_domain.solve_event)(
         event_id, affectation, analyst_email, date)
     if success:
-        project_name = event_domain.get_event(event_id).get('project_name')
+        event = await \
+            sync_to_async(event_domain.get_event)(event_id)
+        project_name = event.get('project_name')
         util.invalidate_cache(event_id)
         util.invalidate_cache(project_name)
         util.cloudwatch_log(
@@ -99,16 +115,16 @@ def resolve_solve_event(_, info, event_id, affectation, date):
     return dict(success=success)
 
 
-@convert_kwargs_to_snake_case
 @require_login
 @enforce_group_level_auth_async
 @require_event_access
-def resolve_add_event_comment(_, info, content, event_id, parent):
+async def _do_add_event_comment(_, info, content, event_id, parent):
     """Resolve add_event_comment mutation."""
     comment_id = int(round(time() * 1000))
     user_info = util.get_jwt_content(info.context)
-    comment_id, success = event_domain.add_comment(
-        comment_id, content, event_id, parent, user_info)
+    comment_id, success = await \
+        sync_to_async(event_domain.add_comment)(
+            comment_id, content, event_id, parent, user_info)
     if success:
         util.invalidate_cache(event_id)
         util.cloudwatch_log(
@@ -121,15 +137,15 @@ def resolve_add_event_comment(_, info, content, event_id, parent):
     return dict(success=success, comment_id=comment_id)
 
 
-@convert_kwargs_to_snake_case
 @require_login
 @enforce_group_level_auth_async
 @require_event_access
-def resolve_update_event_evidence(_, info, event_id, evidence_type, file):
+async def _do_update_event_evidence(_, info, event_id, evidence_type, file):
     """Resolve update_event_evidence mutation."""
     success = False
-    if event_domain.validate_evidence(evidence_type, file):
-        success = event_domain.update_evidence(
+    if await \
+            sync_to_async(event_domain.validate_evidence)(evidence_type, file):
+        success = await sync_to_async(event_domain.update_evidence)(
             event_id, evidence_type, file)
     if success:
         util.invalidate_cache(event_id)
@@ -143,14 +159,14 @@ def resolve_update_event_evidence(_, info, event_id, evidence_type, file):
     return dict(success=success)
 
 
-@convert_kwargs_to_snake_case
 @require_login
 @enforce_group_level_auth_async
 @require_event_access
-def resolve_download_event_file(_, info, event_id, file_name):
+async def _do_download_event_file(_, info, event_id, file_name):
     """Resolve download_event_file mutation."""
     success = False
-    signed_url = event_domain.get_evidence_link(event_id, file_name)
+    signed_url = await \
+        sync_to_async(event_domain.get_evidence_link)(event_id, file_name)
     if signed_url:
         util.cloudwatch_log(
             info.context,
@@ -163,13 +179,13 @@ def resolve_download_event_file(_, info, event_id, file_name):
     return dict(success=success, url=signed_url)
 
 
-@convert_kwargs_to_snake_case
 @require_login
 @enforce_group_level_auth_async
 @require_event_access
-def resolve_remove_event_evidence(_, info, event_id, evidence_type):
+async def _do_remove_event_evidence(_, info, event_id, evidence_type):
     """Resolve remove_event_evidence mutation."""
-    success = event_domain.remove_evidence(evidence_type, event_id)
+    success = await \
+        sync_to_async(event_domain.remove_evidence)(evidence_type, event_id)
     if success:
         util.cloudwatch_log(
             info.context,
