bbbeb8020f78f31740c1391138b819be019d9a86
Andres Roldan <aroldan@fluidattacks.com>
2020-04-06 11:11:12 -0500

    test(back): #1635 ws subscription test
    Former-commit-id: e8e4905b73f8cfa91d789e0532355d0837b883b7

--- test_async/entity_test/test_subscription.py
+++ test_async/entity_test/test_subscription.py
@@ -3,11 +3,23 @@ import json
 import os
 
 from ariadne import graphql
+from ariadne.asgi import (
+    GraphQL,
+    GQL_CONNECTION_INIT,
+    GQL_CONNECTION_ACK,
+    GQL_START,
+    GQL_DATA,
+    GQL_STOP,
+    GQL_COMPLETE,
+    GQL_CONNECTION_TERMINATE,
+)
+
 from django.test import TestCase
 from django.test.client import RequestFactory
 from django.contrib.sessions.middleware import SessionMiddleware
 from django.conf import settings
 from jose import jwt
+from starlette.testclient import TestClient
 from backend.api.schema import SCHEMA
 
 
@@ -47,3 +59,26 @@ class SubscriptionTest(TestCase):
         result = await self._get_result(data)
         assert 'errors' not in result
         assert result['data']['postBroadcastMessage']['success']
+
+    def test_websocket_connection(self):
+        """Test websocket consumer."""
+        client = TestClient(GraphQL(SCHEMA))
+        with client.websocket_connect("/api", "graphql-ws") as ws:
+            ws.send_json({"type": GQL_CONNECTION_INIT})
+            ws.send_json(
+                {
+                    "type": GQL_START,
+                    "id": "test1",
+                    "payload": {"query": "subscription { broadcast }"},
+                }
+            )
+            response = ws.receive_json()
+            assert response["type"] == GQL_CONNECTION_ACK
+            response = ws.receive_json()
+            assert response["type"] == GQL_DATA
+            assert response["id"] == "test1"
+            ws.send_json({"type": GQL_STOP, "id": "test1"})
+            response = ws.receive_json()
+            assert response["type"] == GQL_COMPLETE
+            assert response["id"] == "test1"
+            ws.send_json({"type": GQL_CONNECTION_TERMINATE})
