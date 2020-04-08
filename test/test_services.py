e51164c1e912a6b71283401ec39b05c301f18446
Neil Sanchez <nsanchez@fluidattacks.com>
2020-04-08 13:55:21 -0500

    test(back): #1372 test registered user
    - Added test for registered user
    
    
    Former-commit-id: 98c6906e54316f93fffa27d7bb21fd6a635f8c69

--- test/test_services.py
+++ test/test_services.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
 from backend.services import (
+    is_registered,
     has_valid_access_token
 )
 
 class ServicesTests(TestCase):
+
+    def test_is_registered(self):
+        assert is_registered('unittest')
 
     def test_has_valid_access_token(self):
         jti = 'ff6273146a0e4ed82715cdb4db7f5915b30dfa4bccc54c0d2cda17a61a44a5f6'
