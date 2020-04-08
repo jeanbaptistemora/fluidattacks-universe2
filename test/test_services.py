038be6c54a427fafbc659a78bc5dceb6c37eb32a
Neil Sanchez <nsanchez@fluidattacks.com>
2020-04-08 17:42:24 -0500

    test(back): #1372 test service
    - Added test for access to finding
    - Added test for acces to event
    
    
    Former-commit-id: 9570b6959269382a40f9911d35537e4fc8fa48de

--- test/test_services.py
+++ test/test_services.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from backend.services import (
     is_registered,
+    has_access_to_finding,
+    has_access_to_event,
     has_valid_access_token
 )
 
@@ -8,6 +10,12 @@ class ServicesTests(TestCase):
 
     def test_is_registered(self):
         assert is_registered('unittest')
+
+    def test_has_access_to_finding(self):
+        assert has_access_to_finding('unittest@fluidattacks.com', '560175507')
+
+    def test_has_access_to_event(self):
+        assert has_access_to_event('unittest@fluidattacks.com', '418900971')
 
     def test_has_valid_access_token(self):
         jti = 'ff6273146a0e4ed82715cdb4db7f5915b30dfa4bccc54c0d2cda17a61a44a5f6'
