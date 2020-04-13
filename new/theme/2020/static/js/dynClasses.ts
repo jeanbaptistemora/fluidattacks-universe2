1faf7cea58615dc36a3262bec006c934efc6b14c
John Perez <jperez@fluidattacks.com>
2020-04-13 10:49:16 -0500

    fix(front): #3071 products fixes
    -  Added margin in the top and bottom of the main navbar
    -  Fixed margin of the content in products page
    -  Fix banner position for the products page
    - Fix the size of images in the products page
    -  Fix font sizes for products pages and subpages
    - Fix the layout of the carousel for each product
    
    
    Former-commit-id: d3ba19e6b1dd1a6b46a4318fa9583b1bfc2f7bd9

--- new/theme/2020/static/js/dynClasses.ts
+++ new/theme/2020/static/js/dynClasses.ts
@@ -9,10 +9,10 @@ const dynClasses: (() => void) = (): void => {
     addClasses(".use-cases > .sect1 > .sectionbody",
                "tl fw1 w40-l w-50-m w90-s center f4-ns f5 lh-2");
 
-    addClasses(".products > .sect1 > h2", "f1 tc w6-ns center w-90");
+    addClasses(".products > .sect1 > h2", "f1-ns f3 tc w6-ns center w-90");
 
     addClasses(".products > .sect1 > .sectionbody",
-               "fw1 w-40-l w-50-m w90-s center f3 lh-2");
+               "fw1 w-40-l w-50-m w90-s center f3-ns f5 lh-2");
 
     addClasses(".forces-feature > h3 > a", "c-lightblack no-underline");
 
