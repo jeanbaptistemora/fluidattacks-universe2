8ba877e4babc78f7acd8f7add5925d57b4651064
Neil Sanchez <nsanchez@fluidattacks.com>
2020-04-16 17:13:20 -0500

    test(back): #1372 test services
    - Added services tests
    
    
    Former-commit-id: 086819befa9e8cfc7e8934db436c18a6e17e4357

--- test/test_services.py
+++ test/test_services.py
@@ -1,4 +1,8 @@
 from django.test import TestCase
+
+from backend.exceptions import (
+    FindingNotFound
+)
 from backend.services import (
     is_registered,
     has_access_to_finding,
@@ -9,10 +13,18 @@ from backend.services import (
 class ServicesTests(TestCase):
 
     def test_is_registered(self):
-        assert is_registered('unittest')
+        wrong_user = ['unregistered', 'user0000', '1111000','user>**']
+        right_user = 'unittest'
+        for user in wrong_user:
+            assert not is_registered(user)
+        assert is_registered(right_user)
 
     def test_has_access_to_finding(self):
-        assert has_access_to_finding('unittest@fluidattacks.com', '560175507')
+        wrong_data = ['unittest@fluidattacks.com', '000000000']
+        right_data = ['unittest@fluidattacks.com', '560175507']
+        with self.assertRaises(FindingNotFound):
+            has_access_to_finding(wrong_data[0], wrong_data[1])
+        assert has_access_to_finding(right_data[0], right_data[1])
 
     def test_has_access_to_event(self):
         assert has_access_to_event('unittest@fluidattacks.com', '418900971')
