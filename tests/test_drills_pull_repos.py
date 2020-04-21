28add6826930d4449c33edcbcb597393793832f9
Daniel Salazar <dsalazar@fluidattacks.com>
2020-04-20 22:47:26 -0500

    refac(back): #521.9 rmtree
    - Make pull repos tests to delete continuous2

--- tests/test_drills_pull_repos.py
+++ tests/test_drills_pull_repos.py
@@ -1,3 +1,6 @@
+# Standard libraries
+from shutil import rmtree
+
 # Local libraries
 from toolbox.drills import pull_repos
 
@@ -6,7 +9,11 @@ EXISTING_REPO_NO_PERMISSIONS: str = 'daimon'
 NON_EXISTING_REPO: str = 'sodjfoisajfdoiasjfdoia'
 LOCAL_PATH = 'continuoustest2'
 
+
 def test_drills_pull_repos():
-    assert pull_repos.pull_repos_s3_to_fusion(EXISTING_REPO, LOCAL_PATH)
-    assert not pull_repos.main(EXISTING_REPO_NO_PERMISSIONS)
-    assert not pull_repos.main(NON_EXISTING_REPO)
+    try:
+        assert pull_repos.pull_repos_s3_to_fusion(EXISTING_REPO, LOCAL_PATH)
+        assert not pull_repos.main(EXISTING_REPO_NO_PERMISSIONS)
+        assert not pull_repos.main(NON_EXISTING_REPO)
+    finally:
+        rmtree(LOCAL_PATH)
