d0ef79696baa57b220dfe997e154119201553c01
Neil Sanchez <nsanchez@fluidattacks.com>
2020-04-22 13:28:12 -0500

    test(back): #1372 increase coverage
    - Added tests in domain alert
    
    
    Former-commit-id: 504b0f187bcc6555bab1b2eded85a2fa076ae4d7

---
+++ test_async/domain_test/test_alert.py
@@ -0,0 +1,12 @@
+import pytest
+
+from django.test import TestCase
+
+from backend.domain.alert import set_company_alert
+
+class AlertTests(TestCase):
+
+    def test_set_company_alert(self):
+        assert set_company_alert('fluid', 'DEACTIVATE', 'unittesting')
+        assert set_company_alert('fluid', 'DEACTIVATE', 'all')
+        assert set_company_alert('fluid', 'ACTIVATE', 'unittesting')
