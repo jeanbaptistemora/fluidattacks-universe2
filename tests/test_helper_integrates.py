09a1a8486392a0f245489b73f7e67cfe74e6dc06
Kevin Amado <kamado@fluidattacks.com>
2020-04-23 11:47:18 -0500

    refac(back): #526.8 standard import
    - This is the convention we are using now
    - no-deltas-check

--- tests/test_helper_integrates.py
+++ tests/test_helper_integrates.py
@@ -2,7 +2,24 @@
 from typing import Tuple
 
 # Local imports
-from toolbox.helper import integrates
+from toolbox.helper.integrates import (
+    delete_pending_vulns,
+    does_finding_exist,
+    get_finding_attack_vector,
+    get_finding_cvss_score,
+    get_finding_description,
+    get_finding_recommendation,
+    get_finding_repos,
+    get_finding_static_repos_states,
+    get_finding_static_repos_vulns,
+    get_finding_threat,
+    get_finding_title,
+    get_finding_type,
+    get_project_findings,
+    is_finding_accepted,
+    is_finding_open,
+    is_finding_released,
+)
 from toolbox.constants import SAST, DAST
 
 # Constants
@@ -15,52 +32,52 @@ FINDING_OPEN: str = FINDING_ACCEPTED
 FINDING_CLOSED: str = FINDING
 
 
-def test_helper_does_finding_exist():
+def test_does_finding_exist():
     """Test helper.does_finding_exist."""
-    assert integrates.does_finding_exist(FINDING)
-    assert not integrates.does_finding_exist(FINDING_BAD)
+    assert does_finding_exist(FINDING)
+    assert not does_finding_exist(FINDING_BAD)
 
 
-def test_helper_is_finding_accepted():
+def test_is_finding_accepted():
     """Test helper.is_finding_accepted."""
-    assert not integrates.is_finding_accepted(FINDING)
-    assert integrates.is_finding_accepted(FINDING_ACCEPTED)
+    assert not is_finding_accepted(FINDING)
+    assert is_finding_accepted(FINDING_ACCEPTED)
 
 
-def test_helper_is_finding_released():
+def test_is_finding_released():
     """Test helper.is_finding_released."""
-    assert integrates.is_finding_released(FINDING)
-    assert integrates.is_finding_released(FINDING_ACCEPTED)
-    assert not integrates.is_finding_released(FINDING_DRAFT)
+    assert is_finding_released(FINDING)
+    assert is_finding_released(FINDING_ACCEPTED)
+    assert not is_finding_released(FINDING_DRAFT)
 
 
-def test_helper_is_finding_open():
+def test_is_finding_open():
     """Test helper.is_finding_open."""
-    assert integrates.is_finding_open(FINDING_OPEN, SAST)
-    assert integrates.is_finding_open(FINDING_OPEN, DAST)
-    assert not integrates.is_finding_open(FINDING_CLOSED, SAST)
-    assert not integrates.is_finding_open(FINDING_CLOSED, DAST)
+    assert is_finding_open(FINDING_OPEN, SAST)
+    assert is_finding_open(FINDING_OPEN, DAST)
+    assert not is_finding_open(FINDING_CLOSED, SAST)
+    assert not is_finding_open(FINDING_CLOSED, DAST)
 
 
-def test_helper_get_finding_title():
+def test_get_finding_title():
     """Test helper.get_finding_title."""
     finding_title: str = \
         'FIN.S.0002. PLEASE DO NOT EDIT THIS FINDING I USE THIS IN UNIT TESTS'
-    assert integrates.get_finding_title(FINDING) == finding_title
+    assert get_finding_title(FINDING) == finding_title
 
 
-def test_helper_get_finding_cvss_score():
+def test_get_finding_cvss_score():
     """Test helper.get_finding_cvss_score."""
-    assert integrates.get_finding_cvss_score(FINDING) == 5.3
+    assert get_finding_cvss_score(FINDING) == 5.3
 
 
-def test_helper_get_finding_description():
+def test_get_finding_description():
     """Test helper.get_finding_description."""
-    assert integrates.get_finding_description(FINDING) == '.'
+    assert get_finding_description(FINDING) == '.'
 
 
 def test_get_finding_static_repos_states_closed():
-    result = integrates.get_finding_static_repos_states(FINDING_CLOSED)
+    result = get_finding_static_repos_states(FINDING_CLOSED)
     expected = {
         'NoRepo': False,
         'Test': False,
@@ -73,7 +90,7 @@ def test_get_finding_static_repos_states_closed():
 
 
 def test_get_finding_static_repos_states_open():
-    result = integrates.get_finding_static_repos_states(FINDING_OPEN)
+    result = get_finding_static_repos_states(FINDING_OPEN)
     expected = {
         'Test': True,
         'continuous': True,
@@ -86,7 +103,7 @@ def test_get_finding_static_repos_states_open():
 
 
 def test_get_finding_static_repos_vulns_closed():
-    result = integrates.get_finding_static_repos_vulns(FINDING_CLOSED)
+    result = get_finding_static_repos_vulns(FINDING_CLOSED)
     expected = {
         'NoRepo': 0,
         'Test': 0,
@@ -99,7 +116,7 @@ def test_get_finding_static_repos_vulns_closed():
 
 
 def test_get_finding_static_repos_vulns_open():
-    result = integrates.get_finding_static_repos_vulns(FINDING_OPEN)
+    result = get_finding_static_repos_vulns(FINDING_OPEN)
     expected = {
         'Test': 1,
         'continuous': 1,
@@ -111,44 +128,44 @@ def test_get_finding_static_repos_vulns_open():
         assert result[repo] == expected[repo], repo
 
 
-def test_helper_get_finding_threat():
+def test_get_finding_threat():
     """Test helper.get_finding_threat."""
-    assert integrates.get_finding_threat(FINDING) == '.'
+    assert get_finding_threat(FINDING) == '.'
 
 
-def test_helper_get_finding_attack_vector():
+def test_get_finding_attack_vector():
     """Test helper.get_finding_attack_vector."""
-    assert integrates.get_finding_attack_vector(FINDING) == '.'
+    assert get_finding_attack_vector(FINDING) == '.'
 
 
-def test_helper_get_finding_recommendation():
+def test_get_finding_recommendation():
     """Test helper.get_finding_recommendation."""
-    assert integrates.get_finding_recommendation(FINDING) == '.'
+    assert get_finding_recommendation(FINDING) == '.'
 
 
-def test_helper_get_project_findings():
+def test_get_project_findings():
     """Test helper.get_project_findings."""
-    finding_id__title = integrates.get_project_findings(SUBS)
+    finding_id__title = get_project_findings(SUBS)
     finding_ids = tuple(map(lambda x: x[0], finding_id__title))
     assert FINDING in finding_ids
     assert FINDING_ACCEPTED in finding_ids
     assert FINDING_DRAFT not in finding_ids
 
 
-def test_helper_get_finding_type():
+def test_get_finding_type():
     """Test helper.get_finding_type."""
-    assert integrates.get_finding_type(FINDING) == (True, True)
-    assert integrates.get_finding_type(FINDING_ACCEPTED) == (True, True)
+    assert get_finding_type(FINDING) == (True, True)
+    assert get_finding_type(FINDING_ACCEPTED) == (True, True)
 
 
-def test_helper_delete_pending_vulns():
+def test_delete_pending_vulns():
     """Test helper.delete_pending_vulns."""
-    assert integrates.delete_pending_vulns(FINDING)
+    assert delete_pending_vulns(FINDING)
 
 
 def test_get_finding_repos():
     """Test helper.get_finding_repos."""
-    result = integrates.get_finding_repos(FINDING)
+    result = get_finding_repos(FINDING)
     expected = ('Test', 'NoRepo')
     result, expected = tuple(sorted(result)), tuple(sorted(expected))
     assert result == expected
