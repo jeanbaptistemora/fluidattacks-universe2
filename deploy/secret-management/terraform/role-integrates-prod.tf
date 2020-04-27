5c9cee07fbd3f0621ca881c2f164659f0349100f
Andres Cuberos <acuberos@fluidattacks.com>
2020-04-27 12:36:40 -0500

    feat(infra): #1943 create production role in tf
    - Create IAM role to access production settings thorugh Okta
    
    
    Former-commit-id: c0a35d67b25d718acf3e7b85f9ef1bac2810ed71

---
+++ deploy/secret-management/terraform/role-integrates-prod.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_role" "integrates-prod" {
+  name = "integrates-prod"
+  assume_role_policy = data.aws_iam_policy_document.okta-assume-role-policy-data.json
+  max_session_duration = "32400"
+}
+
+resource "aws_iam_role_policy_attachment" "integrates-prod-push-cloudwatch" {
+  role       = aws_iam_role.integrates-prod.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
+}
+
+resource "aws_iam_role_policy_attachment" "integrates-prod-dynamo-full-access" {
+  role       = aws_iam_role.integrates-prod.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "integrates-prod" {
+  role       = aws_iam_role.integrates-prod.name
+  policy_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/user-provision/integrates-prod-policy"
+}
