b1bb655d0e96601b6c9adf9f80d9c669a58df0c5
Daniel Salazar <dsalazar@fluidattacks.com>
2020-04-28 11:33:39 -0500

    refac(back): #438.1 increase cluster nodes
    - Move from 4 to 5 nodes
    
    
    Former-commit-id: 8ff32936857e10cea0a185fb747c7514668be556

--- infrastructure/eks/cluster.tf
+++ infrastructure/eks/cluster.tf
@@ -59,10 +59,10 @@ resource "aws_launch_configuration" "k8s_nodes_launch_config" {
 }
 
 resource "aws_autoscaling_group" "k8s_nodes_autoscaling" {
-  desired_capacity     = 4
+  desired_capacity     = 5
   launch_configuration = aws_launch_configuration.k8s_nodes_launch_config.id
-  max_size             = 5
-  min_size             = 4
+  max_size             = 6
+  min_size             = 5
   name                 = "EKSWorkerNodes"
   vpc_zone_identifier  = aws_subnet.k8s_subnets_secondary.*.id
 
