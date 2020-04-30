f5b184159377e21df00b9d9e3ef0d5b572ddc32e
Daniel Salazar <dsalazar@fluidattacks.com>
2020-04-30 11:39:25 -0500

    feat(build): #1921.7 infra_cache
    - Add provisioners for both tests and deploy
    - Make build.sh to use them
    
    
    Former-commit-id: 43ceb31edc39786c00f24305730de83e8cd339e5

--- build.sh
+++ build.sh
@@ -28,6 +28,8 @@ function decide_and_call_provisioner {
         deploy_k8s*      ) provisioner='infra';;
         infra_backup_test) provisioner='infra_backup_test';;
         infra_backup_deploy) provisioner='infra_backup_deploy';;
+        infra_cache_db_test) provisioner='infra_cache_db_test';;
+        infra_cache_db_deploy) provisioner='infra_cache_db_deploy';;
         infra*           ) provisioner='infra';;
         functional_tests*) provisioner='selenium';;
         test*            ) provisioner='test';;
