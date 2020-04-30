934b2ee5a5dd8b8f1a8a7e008146af177dd831e4
Andres Aguirre <aaguirre@fluidattacks.com>
2020-04-30 15:28:23 -0500

    test(back): #1372 test update all pending vulns
    Former-commit-id: ed074805a7a22414bb016640be50e672368153cb

--- test_async/entity_test/test_project.py
+++ test_async/entity_test/test_project.py
@@ -107,7 +107,7 @@ class ProjectTests(TestCase):
         assert result['data']['project']['hasDrills']
         assert result['data']['project']['hasForces']
         assert len(result['data']['project']['findings']) == 6
-        assert result['data']['project']['openVulnerabilities'] == 31
+        assert result['data']['project']['openVulnerabilities'] == 32
         assert result['data']['project']['closedVulnerabilities'] == 8
         assert 'lastClosingVuln' in result['data']['project']
         assert result['data']['project']['pendingClosingCheck'] == 2
