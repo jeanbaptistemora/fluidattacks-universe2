bd60311cc82445acbf5284a6957da7e74f605401
Andres Cuberos <acuberos@fluidattacks.com>
2020-05-07 14:54:18 -0500

    fix(infra): #1943.2 allow prod role to use key
    - Set permissions in key resource so production role can
      use it for encryption/decryption
    
    
    Former-commit-id: 2c5e51db82dd5203dc1debcb7ccc3935be34a60a

--- deploy/secret-management/terraform/key-prod.tf
+++ deploy/secret-management/terraform/key-prod.tf
@@ -55,7 +55,8 @@ data "aws_iam_policy_document" "integrates-prod-key" {
       identifiers = [
         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/FLUIDServes_TF",
         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/serves-admin",
-        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user-provision/integrates-prod"
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user-provision/integrates-prod",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/integrates-prod"
       ]
     }
     actions = [
@@ -78,7 +79,8 @@ data "aws_iam_policy_document" "integrates-prod-key" {
       identifiers = [
         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/FLUIDServes_TF",
         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/serves-admin",
-        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user-provision/integrates-prod"
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user-provision/integrates-prod",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/integrates-prod"
       ]
     }
     actions = [
