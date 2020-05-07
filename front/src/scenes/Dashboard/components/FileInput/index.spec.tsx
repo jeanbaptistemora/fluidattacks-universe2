fdd7302e0bba41ac03fe6de7c219373fe7525cc2
Andres Aguirre <aaguirre@fluidattacks.com>
2020-05-07 14:36:41 -0500

    test(front): #1372 increase coverage
    Former-commit-id: 7c2f00a84ba5fffc667794eebf8740e18eb22685

--- front/src/scenes/Dashboard/components/FileInput/index.spec.tsx
+++ front/src/scenes/Dashboard/components/FileInput/index.spec.tsx
@@ -1,4 +1,4 @@
-import { shallow, ShallowWrapper } from "enzyme";
+import { mount, ReactWrapper, shallow, ShallowWrapper } from "enzyme";
 import React from "react";
 import { FileInput } from "./index";
 
@@ -19,5 +19,26 @@ describe("File input", () => {
     );
     expect(wrapper)
       .toHaveLength(1);
+  });
+
+  it("should render selected file name", () => {
+    const wrapper: ReactWrapper = mount(
+      <FileInput
+        icon="search"
+        id="test"
+        type=".exp"
+        visible={true}
+      />,
+    );
+    const input: ReactWrapper = wrapper.find("input");
+    const fileName: ReactWrapper = wrapper.find("label")
+                                          .find("span")
+                                          .at(0);
+    expect("")
+      .toEqual(fileName.text());
+
+    input.simulate("change", { target: { files: [ { name: "Test file name"} ] } });
+    expect("Test file name")
+      .toEqual(fileName.text());
   });
 });
