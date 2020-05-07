f22f459660b01098dae4cccf7c492b0e245616b0
Mateo Marulanda <mmarulanda@fluidattacks.com>
2020-05-07 16:37:37 -0500

    test(front): #1372 increase coverage
    - Add test for input field and add button
    
    
    Former-commit-id: b6963e17cfaabe12d9c696c77c1fac948f274c95

--- front/src/scenes/Dashboard/components/AddTagsModal/index.spec.tsx
+++ front/src/scenes/Dashboard/components/AddTagsModal/index.spec.tsx
@@ -1,14 +1,13 @@
-import { shallow, ShallowWrapper } from "enzyme";
+import { mount, ReactWrapper, shallow, ShallowWrapper } from "enzyme";
 import * as React from "react";
 import { Provider } from "react-redux";
-import { Action, createStore, Store } from "redux";
+import store from "../../../../store";
 import { AddTagsModal } from "./index";
 
 const functionMock: (() => void) = (): void => undefined;
 
 describe("Add Tags modal", () => {
 
-  const store: Store<{}, Action<{}>> = createStore(() => ({}));
   const wrapper: ShallowWrapper = shallow(
     <Provider store={store}>
       <AddTagsModal
@@ -27,5 +26,57 @@ describe("Add Tags modal", () => {
   it("should render", () => {
     expect(wrapper)
       .toHaveLength(1);
+  });
+
+  it("should render input field and add button", () => {
+
+    const modal: ReactWrapper = mount(
+      <Provider store={store}>
+        <AddTagsModal
+          isOpen={true}
+          onClose={functionMock}
+          onSubmit={functionMock}
+        />
+      </Provider>,
+    );
+
+    const addButton: ReactWrapper = modal.find("renderTagsFields")
+      .find("Glyphicon");
+
+    const inputField: ReactWrapper = modal.find("renderTagsFields")
+      .find("input");
+
+    expect(addButton)
+      .toHaveLength(1);
+
+    expect(inputField)
+      .toHaveLength(1);
+  });
+
+  it("should add and remove a input field", () => {
+    const modal: ReactWrapper = mount(
+      <Provider store={store}>
+        <AddTagsModal
+          isOpen={true}
+          onClose={functionMock}
+          onSubmit={functionMock}
+        />
+      </Provider>,
+    );
+    const addButton: ReactWrapper = modal.find("renderTagsFields")
+      .find(".glyphicon-plus");
+
+    addButton.simulate("click");
+    expect(modal.find("renderTagsFields")
+    .find("input"))
+    .toHaveLength(2);
+
+    const deleteButton: ReactWrapper = modal.find("renderTagsFields")
+      .find(".glyphicon-trash");
+
+    deleteButton.simulate("click");
+    expect(modal.find("renderTagsFields")
+    .find("input"))
+    .toHaveLength(1);
   });
 });
