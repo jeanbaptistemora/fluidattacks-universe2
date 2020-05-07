6b09b12bf40a805d7c0363d2acf71f397d960ec7
Andres Aguirre <aaguirre@fluidattacks.com>
2020-05-07 11:28:04 -0500

    test(front): #1372 increase coverage
    Former-commit-id: fc08c63859102fa390302d4a47196f4fba7712d2

--- front/src/scenes/Dashboard/components/AddRepositoriesModal/index.spec.tsx
+++ front/src/scenes/Dashboard/components/AddRepositoriesModal/index.spec.tsx
@@ -1,23 +1,12 @@
-import { shallow, ShallowWrapper } from "enzyme";
+import { mount, ReactWrapper, shallow, ShallowWrapper } from "enzyme";
 import * as React from "react";
 import { Provider } from "react-redux";
-import { Action, createStore, Store } from "redux";
+import store from "../../../../store";
 import { AddRepositoriesModal } from "./index";
 
 const functionMock: (() => void) = (): void => undefined;
 
 describe("Add Repositories modal", () => {
-
-  const store: Store<{}, Action<{}>> = createStore(() => ({}));
-  const wrapper: ShallowWrapper = shallow(
-    <Provider store={store}>
-      <AddRepositoriesModal
-        isOpen={true}
-        onClose={functionMock}
-        onSubmit={functionMock}
-      />
-    </Provider>,
-  );
 
   it("should return a function", () => {
     expect(typeof (AddRepositoriesModal))
@@ -25,7 +14,37 @@ describe("Add Repositories modal", () => {
   });
 
   it("should render", () => {
+    const wrapper: ShallowWrapper = shallow(
+      <Provider store={store}>
+        <AddRepositoriesModal
+          isOpen={true}
+          onClose={functionMock}
+          onSubmit={functionMock}
+        />
+      </Provider>,
+    );
     expect(wrapper)
+      .toHaveLength(1);
+  });
+
+  it("should render input fields and add button", (): void => {
+    const wrapper: ReactWrapper = mount(
+      <Provider store={store}>
+        <AddRepositoriesModal
+          isOpen={true}
+          onClose={functionMock}
+          onSubmit={functionMock}
+        />
+      </Provider>,
+    );
+    expect(wrapper.find("renderReposFields")
+      .find("select"))
+      .toHaveLength(1);
+    expect(wrapper.find("renderReposFields")
+      .find("input"))
+      .toHaveLength(2);
+    expect(wrapper.find("renderReposFields")
+      .find(".glyphicon-plus"))
       .toHaveLength(1);
   });
 });
