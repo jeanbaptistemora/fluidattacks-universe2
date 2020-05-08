bf35cb37018377868e2d853df20ed2ab43492192
Danilo Vasquez <dvasquez@fluidattacks.com>
2020-05-07 19:55:06 -0500

    feat(front): #2967.1 add toggle all option
    - Added a toggle-all element for rules categories
    
    
    Former-commit-id: 5f47fece3abcc7f23bbd8e112e551c3eed892370

--- theme/2014/static/js/rules.ts
+++ theme/2014/static/js/rules.ts
@@ -2,16 +2,35 @@ import { default as $ } from "jquery";
 /* This script creates a dropdown menu in rules index
  similar to Defends */
 
+// Function to toggle a category
+
+ let togglecat: function; togglecat = function(categ: HTMLElement){
+   categ.classList.toggle("bg-fluid-lightgray");
+   categ.children[1].classList.toggle("rotate-90");
+   const ruleslist: HTMLElement  = categ.nextElementSibling as HTMLElement;
+   ruleslist.classList.toggle("dn");
+ };
+
 let rulescat: HTMLCollection; rulescat = document.getElementsByClassName("rulescat");
 let i: number;
 for (i = 0; i < rulescat.length; i += 1) {
   rulescat[i].addEventListener("click", (event: Event) => {
-    (event.currentTarget as HTMLElement).classList.toggle("bg-fluid-lightgray");
-    (event.currentTarget as HTMLElement).children[1].classList.toggle("rotate-90");
-    const ruleslist: HTMLElement  = (event.currentTarget as HTMLElement).nextElementSibling as HTMLElement;
-    ruleslist.classList.toggle("dn");
+    let categ: HTMLElement; categ = event.currentTarget as HTMLElement;
+    togglecat(categ);
   });
 }
+
+// This section is for the expand/collapse all button
+
+let categlist: HTMLElement; categlist = document.getElementsByClassName("categlist")[0];
+categlist.addEventListener("click", (event: Event) => {
+  (event.currentTarget as HTMLElement).classList.toggle("bg-fluid-lightgray");
+  (event.currentTarget as HTMLElement).children[1].classList.toggle("rotate-90");
+  let i: number;
+  for (i = 0; i < rulescat.length; i += 1) {
+      togglecat(rulescat[i]);
+  }
+});
 
 // Activate the list when the category is referenced through the url
 $(() => {
