944ccb91fedf43dfc0d4d5000e87b7e2f982ce06
Mateo Marulanda <mmarulanda@fluidattacks.com>
2020-05-08 09:53:24 -0500

    test(front): #1372 increase coverage
    - Test for form fields
    
    
    Former-commit-id: ba3e180cf860bafc6fa76a57903b311fd3435720

--- front/src/scenes/Dashboard/components/AddProjectModal/index.spec.tsx
+++ front/src/scenes/Dashboard/components/AddProjectModal/index.spec.tsx
@@ -8,17 +8,21 @@ import { PROJECTS_NAME_QUERY } from "./queries";
 import { IProjectName } from "./types";
 
 describe("AddProjectModal component", () => {
+
+  const projectName: IProjectName = { internalProjectNames : { projectName: "" } };
+
+  const mocksMutation: MockedResponse[] = [{
+    request: {
+      query: PROJECTS_NAME_QUERY,
+    },
+    result: {
+      data: { projectName },
+    },
+  }];
+
+  const handleOnClose: jest.Mock = jest.fn();
+
   it("should render add project modal", () => {
-    const handleOnClose: jest.Mock = jest.fn();
-    const projectName: IProjectName = { internalProjectNames : { projectName: "" } };
-    const mocksMutation: MockedResponse[] = [{
-        request: {
-          query: PROJECTS_NAME_QUERY,
-        },
-        result: {
-          data: { projectName },
-        },
-      }];
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
         <MockedProvider mocks={mocksMutation} addTypename={false}>
@@ -29,6 +33,7 @@ describe("AddProjectModal component", () => {
         </MockedProvider>
       </Provider>,
     );
+
     const cancelButton: ReactWrapper = wrapper
       .find("Button")
       .filterWhere((element: ReactWrapper) => element.contains("Cancel"));
@@ -37,5 +42,56 @@ describe("AddProjectModal component", () => {
       .toHaveLength(1);
     expect(handleOnClose.mock.calls.length)
       .toEqual(1);
+  });
+
+  it("should render form fields", () => {
+    const wrapper: ReactWrapper = mount(
+      <Provider store={store}>
+        <MockedProvider mocks={mocksMutation} addTypename={false}>
+          <AddProjectModal
+            isOpen={true}
+            onClose={handleOnClose}
+          />
+        </MockedProvider>
+      </Provider>,
+    );
+
+    const companyField: ReactWrapper = wrapper
+      .find({ name: "company" })
+      .find("input");
+
+    const projectNameField: ReactWrapper = wrapper
+      .find({ name: "name" })
+      .find("input");
+
+    const descriptionField: ReactWrapper = wrapper
+      .find({ name: "description" })
+      .find("input");
+
+    const typeField: ReactWrapper = wrapper
+      .find({ name: "type" })
+      .find("select");
+
+    const switchButtons: ReactWrapper = wrapper
+      .find({ checked: true })
+      .find(".switch-group");
+
+    const submitButton: ReactWrapper = wrapper
+    .findWhere((element: ReactWrapper) => element.contains("Proceed"))
+    .at(0);
+
+    expect(companyField)
+      .toHaveLength(1);
+    expect(projectNameField)
+      .toHaveLength(1);
+    expect(descriptionField)
+      .toHaveLength(1);
+    expect(typeField)
+      .toHaveLength(1);
+    expect(switchButtons)
+      .toHaveLength(3);
+    expect(submitButton)
+      .toHaveLength(1);
+
   });
 });
