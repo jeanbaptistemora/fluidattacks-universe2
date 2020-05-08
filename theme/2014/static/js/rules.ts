a2b7f98f0576b5084e29f04d24f0b4d5d23801cd
Danilo Vasquez <dvasquez@fluidattacks.com>
2020-05-08 14:18:03 -0500

    feat(front): #2967.2 extend toggle option to fins
    - Adjusted the toggle option because it was simply toggling the
    current states and not expanding/collapsing all categories
    - Added the toggle option to the findings
    
    
    Former-commit-id: a0b792ef79a1f7e1320eff743fdebf0340d262c1

--- theme/2014/static/js/rules.ts
+++ theme/2014/static/js/rules.ts
@@ -4,12 +4,12 @@ import { default as $ } from "jquery";
 
 // Function to toggle a category
 
- let togglecat: function; togglecat = function(categ: HTMLElement){
-   categ.classList.toggle("bg-fluid-lightgray");
-   categ.children[1].classList.toggle("rotate-90");
-   const ruleslist: HTMLElement  = categ.nextElementSibling as HTMLElement;
-   ruleslist.classList.toggle("dn");
- };
+let togglecat: function; togglecat = function(categ: HTMLElement){
+  categ.classList.toggle("bg-fluid-lightgray");
+  categ.children[1].classList.toggle("rotate-90");
+  const ruleslist: HTMLElement  = categ.nextElementSibling as HTMLElement;
+  ruleslist.classList.toggle("dn");
+};
 
 let rulescat: HTMLCollection; rulescat = document.getElementsByClassName("rulescat");
 let i: number;
@@ -20,19 +20,45 @@ for (i = 0; i < rulescat.length; i += 1) {
   });
 }
 
-// This section is for the expand/collapse all button
+// This section toggles visibility for the expand/collapse all buttons
 
-let categlist: HTMLElement; categlist = document.getElementsByClassName("categlist")[0];
-categlist.addEventListener("click", (event: Event) => {
-  (event.currentTarget as HTMLElement).classList.toggle("bg-fluid-lightgray");
-  (event.currentTarget as HTMLElement).children[1].classList.toggle("rotate-90");
+let toggleswitch: function; toggleswitch = function(switched: HTMLElement){
+  switched.children[0].classList.toggle("dn");
+  switched.children[1].classList.toggle("rotate-90");
+  switched.children[1].classList.toggle("dn");
+}
+
+let expander: HTMLElement; expander = document.getElementsByClassName("expall")[0];
+let collapser: HTMLElement; collapser = document.getElementsByClassName("collall")[0];
+
+// This section expands all categories
+
+expander.addEventListener("click", (event: Event) => {
+  let collapsed: HTMLCollection; collapsed = document.querySelectorAll(".rulescat:not(.bg-fluid-lightgray)");
+  toggleswitch(expander);
+  toggleswitch(collapser);
+
   let i: number;
-  for (i = 0; i < rulescat.length; i += 1) {
-      togglecat(rulescat[i]);
+  for (i = 0; i < collapsed.length; i += 1) {
+      togglecat(collapsed[i]);
+  }
+});
+
+// This section collapses all categories
+
+collapser.addEventListener("click", (event: Event) => {
+  let expanded: HTMLCollection; expanded = document.querySelectorAll(".rulescat.bg-fluid-lightgray");
+  toggleswitch(expander);
+  toggleswitch(collapser);
+
+  let i: number;
+  for (i = 0; i < expanded.length; i += 1) {
+      togglecat(expanded[i]);
   }
 });
 
 // Activate the list when the category is referenced through the url
+
 $(() => {
     let anchor: string; anchor = window.location.hash;
     if (anchor !== "") {
