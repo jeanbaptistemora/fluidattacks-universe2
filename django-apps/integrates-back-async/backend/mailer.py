96efa24af4734da3d7120229941ea9d470e0bb4d
Mario Varas <mvaras@fluidattacks.com>
2020-05-13 17:54:54 -0500

    refac(back): #1963 remove vuln update mail
    Former-commit-id: e4ccd42bfb51e58d553764d79e9b2e4852061e42

--- django-apps/integrates-back-async/backend/mailer.py
+++ django-apps/integrates-back-async/backend/mailer.py
@@ -286,10 +286,6 @@ def send_mail_accepted_finding(email_to: List[str], context: Dict[str, Union[str
     _send_mail('acceptedfinding', email_to, context=context, tags=GENERAL_TAG)
 
 
-def send_mail_updated_vulns(email_to: List[str], context: Dict[str, Union[str, int]]):
-    _send_mail('updated-vulns', email_to, context=context, tags=GENERAL_TAG)
-
-
 def send_mail_project_deletion(email_to: List[str], context: Dict[str, Union[str, int]]):
     _send_mail('projectdeletion', email_to, context=context, tags=GENERAL_TAG)
 
