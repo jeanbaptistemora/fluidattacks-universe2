1d2254adecced0ea8ecb0eb2a6584701d73b4d9c
Andres Aguirre <aaguirre@fluidattacks.com>
2020-05-18 09:20:57 -0500

    test(front): #1372 increase coverage
    - Test handle errors in grantUserAccess function from ProjectUsersView
    
    
    Former-commit-id: 69217af1155e378612e036b4a365e2190072e49f

--- front/src/scenes/Dashboard/containers/ProjectUsersView/index.spec.tsx
+++ front/src/scenes/Dashboard/containers/ProjectUsersView/index.spec.tsx
@@ -9,7 +9,7 @@ import { Provider } from "react-redux";
 import wait from "waait";
 import store from "../../../../store/index";
 import { authzContext } from "../../../../utils/authz/config";
-import { msgSuccess } from "../../../../utils/notifications";
+import { msgError, msgSuccess } from "../../../../utils/notifications";
 import { ProjectUsersView } from "./index";
 import { ADD_USER_MUTATION, EDIT_USER_MUTATION, GET_USERS, REMOVE_USER_MUTATION } from "./queries";
 import { IProjectUsersViewProps } from "./types";
@@ -17,6 +17,7 @@ import { IProjectUsersViewProps } from "./types";
 jest.mock("../../../../utils/notifications", () => {
   const mockedNotifications: Dictionary = jest.requireActual("../../../../utils/notifications");
   mockedNotifications.msgSuccess = jest.fn();
+  mockedNotifications.msgError = jest.fn();
 
   return mockedNotifications;
 });
@@ -422,5 +423,87 @@ describe("Project users view", () => {
       .toHaveLength(0);
     expect(msgSuccess)
       .toHaveBeenCalled();
+  });
+
+  it("should handle errors when add user to the project", async () => {
+    const mocksMutation: ReadonlyArray<MockedResponse> = [{
+      request: {
+        query: ADD_USER_MUTATION,
+        variables: {
+          email: "unittest@test.com",
+          organization: "unittesting",
+          phoneNumber: "+573123210123",
+          projectName: "TEST",
+          responsibility: "Project Manager",
+          role: "ANALYST",
+        },
+      },
+      result: { errors: [
+        new GraphQLError("Access denied"),
+        new GraphQLError("Exception - Email is not valid"),
+        new GraphQLError("Exception - Invalid field in form"),
+        new GraphQLError("Exception - Invalid phone number in form"),
+        new GraphQLError("Exception - Invalid email address in form"),
+      ]},
+    }];
+    const mockedPermissions: PureAbility<string> = new PureAbility([
+      { action: "backend_api_resolvers_user__do_grant_user_access" },
+      { action: "grant_group_level_role:analyst" },
+    ]);
+    const wrapper: ReactWrapper = mount(
+      <Provider store={store}>
+        <MockedProvider mocks={mocks.concat(mocksMutation)} addTypename={false}>
+          <authzContext.Provider value={mockedPermissions}>
+            <ProjectUsersView {...mockProps} />
+          </authzContext.Provider>
+        </MockedProvider>
+      </Provider>,
+    );
+    await act(async () => { await wait(0); wrapper.update(); });
+    const addButton: ReactWrapper = wrapper.find("button")
+      .findWhere((element: ReactWrapper) => element.contains("Add"))
+      .at(0);
+    addButton.simulate("click");
+    let addUserModal: ReactWrapper = wrapper
+      .find("modal")
+      .find({open: true, headerTitle: "Add user to this project"});
+    expect(addUserModal)
+      .toHaveLength(1);
+    const emailInput: ReactWrapper = addUserModal
+      .find({name: "email", type: "text"})
+      .at(0)
+      .find("input");
+    emailInput.simulate("change", { target: { value: "unittest@test.com" } });
+    const organizationInput: ReactWrapper = addUserModal
+      .find({name: "organization", type: "text"})
+      .at(0)
+      .find("input");
+    organizationInput.simulate("change", { target: { value: "unittesting" } });
+    const phoneNumberInput: ReactWrapper = addUserModal
+      .find({name: "phoneNumber", type: "text"})
+      .at(0)
+      .find("input");
+    phoneNumberInput.simulate("change", { target: { value: "+573123210123" } });
+    const responsibilityInput: ReactWrapper = addUserModal
+      .find({name: "responsibility", type: "text"})
+      .at(0)
+      .find("input");
+    responsibilityInput.simulate("change", { target: { value: "Project Manager" } });
+    const select: ReactWrapper = addUserModal.find("select")
+      .findWhere((element: ReactWrapper) => element.contains("Analyst"))
+      .at(0);
+    select.simulate("change", { target: { value: "ANALYST" } });
+    const form: ReactWrapper = addUserModal
+      .find("genericForm")
+      .at(0);
+    form.simulate("submit");
+    await act(async () => { await wait(0); wrapper.update(); });
+    addUserModal = wrapper
+      .find("modal")
+      .find({open: true, headerTitle: "Add user to this project"});
+    expect(addUserModal)
+      .toHaveLength(0);
+    expect(msgError)
+      .toHaveBeenCalledTimes(5);
   });
 });
