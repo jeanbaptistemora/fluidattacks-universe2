fc6b48178d17aeb087ce6e618b3f220453d20812
David Acevedo <dacevedo@fluidattacks.com>
2020-05-19 17:44:44 -0500

    test(front): #1372 increase coverage
    Former-commit-id: 7c14ef39ec1d4c3971f6f738a645e3c4c7cdfe82

--- mobile/src/containers/LoginView/index.spec.tsx
+++ mobile/src/containers/LoginView/index.spec.tsx
@@ -4,6 +4,7 @@ import React from "react";
 // tslint:disable-next-line: no-submodule-imports
 import { act } from "react-dom/test-utils";
 import { I18nextProvider } from "react-i18next";
+import { Alert } from "react-native";
 import { Button, Provider as PaperProvider } from "react-native-paper";
 import { NativeRouter } from "react-router-native";
 
@@ -17,6 +18,13 @@ jest.mock("expo-google-app-auth", (): Dictionary => {
   mockedGoogleAuth.logInAsync = jest.fn();
 
   return mockedGoogleAuth;
+});
+
+jest.mock("react-native", (): Dictionary => {
+  const mockedRN: Dictionary = jest.requireActual("react-native");
+  Object.assign(mockedRN.Alert, { alert: jest.fn() });
+
+  return mockedRN;
 });
 
 const mockHistoryReplace: jest.Mock = jest.fn();
@@ -141,5 +149,35 @@ describe("LoginView", (): void => {
       .find("preloader")
       .prop("visible"))
       .toEqual(false);
+  });
+
+  it("should handle errors", async (): Promise<void> => {
+    (checkVersion as jest.Mock).mockImplementation((): Promise<boolean> => Promise.reject("Oops :("));
+    (Google.logInAsync as jest.Mock).mockImplementation((): Promise<Google.LogInResult> => Promise.reject("Oops :("));
+
+    const wrapper: ReactWrapper = mount(
+      <PaperProvider>
+        <I18nextProvider i18n={i18next}>
+          <NativeRouter initialEntries={["/"]}>
+            <LoginView />
+          </NativeRouter>
+        </I18nextProvider>
+      </PaperProvider>,
+    );
+
+    expect(wrapper)
+      .toHaveLength(1);
+
+    const googleBtn: ReactWrapper<React.ComponentProps<typeof Button>> = wrapper
+      .find<React.ComponentProps<typeof Button>>(Button);
+    expect(googleBtn)
+      .toHaveLength(1);
+
+    await act(async (): Promise<void> => {
+      await (googleBtn.invoke("onPress") as () => Promise<void>)();
+      wrapper.update();
+    });
+    expect(Alert.alert)
+      .toHaveBeenCalled();
   });
 });
