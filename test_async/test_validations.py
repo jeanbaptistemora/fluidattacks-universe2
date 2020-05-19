c8686b8bdb747ea823d0b4e7f3825b16167a8681
Mario Varas <mvaras@fluidattacks.com>
2020-05-19 07:38:49 -0500

    test(back): #1965 validate field length test
    Former-commit-id: ece4ce21c3bd439d7f10a88ba78fb1404ff5c9bd

--- test_async/test_validations.py
+++ test_async/test_validations.py
@@ -4,28 +4,33 @@ import pytest
 from django.test import TestCase
 
 from backend.utils.validations import (
-    validate_email_address, validate_fields, validate_project_name,
-    validate_alphanumeric_field, validate_phone_field
+    validate_email_address, validate_fields, validate_field_length,
+    validate_project_name, validate_alphanumeric_field, validate_phone_field
 )
-from backend.exceptions import InvalidChar, InvalidField
+from backend.exceptions import InvalidChar, InvalidField, InvalidFieldLength
 
 
 class ValidationsTests(TestCase):
 
     def test_validate_fields(self):
-        assert validate_fields(['testfield', 'testfield2']) == None
+        assert not bool(validate_fields(['testfield', 'testfield2']))
         with pytest.raises(InvalidChar):
             assert validate_fields(['=testfield', 'testfield2'])
             assert validate_fields(['testfield', 'testfiel\'d'])
             assert validate_fields(['testfield', '<testfield2'])
+    
+    def test_validate_field_length(self):
+        with pytest.raises(InvalidFieldLength):
+            assert validate_field_length('testlength', limit=9)
 
     def test_validate_email_address(self):
         assert validate_email_address('test@unittesting.com')
         with pytest.raises(InvalidField):
             assert validate_email_address('testunittesting.com')
+            assert validate_email_address('test+1@unittesting.com')
 
     def test_validate_project_name(self):
-        assert validate_project_name('test') == None
+        assert not bool(validate_project_name('test'))
         with pytest.raises(InvalidField):
             assert validate_project_name('=test2@')
 
