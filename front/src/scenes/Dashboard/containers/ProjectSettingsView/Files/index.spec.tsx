364ef994852d36153d4081e0baa87f2efc1b88c7
Andres Aguirre <aaguirre@fluidattacks.com>
2020-05-20 08:27:19 -0500

    test(front): #1372 increase coverage
    - Test handle errors in uploadFile function from files
    
    
    Former-commit-id: 9ee84446fe22274913d0a2a9958a99a347f2f5aa

--- front/src/scenes/Dashboard/containers/ProjectSettingsView/Files/index.spec.tsx
+++ front/src/scenes/Dashboard/containers/ProjectSettingsView/Files/index.spec.tsx
@@ -1,6 +1,7 @@
 import { MockedProvider, MockedResponse } from "@apollo/react-testing";
 import { PureAbility } from "@casl/ability";
 import { mount, ReactWrapper } from "enzyme";
+import { GraphQLError } from "graphql";
 import * as React from "react";
 // tslint:disable-next-line: no-submodule-imports
 import { act } from "react-dom/test-utils";
@@ -8,12 +9,13 @@ import { Provider } from "react-redux";
 import wait from "waait";
 import store from "../../../../../store/index";
 import { authzContext } from "../../../../../utils/authz/config";
-import { msgSuccess } from "../../../../../utils/notifications";
+import { msgError, msgSuccess } from "../../../../../utils/notifications";
 import { DOWNLOAD_FILE_MUTATION, GET_FILES, REMOVE_FILE_MUTATION, UPLOAD_FILE_MUTATION } from "../queries";
 import { Files, IFilesProps } from "./index";
 
 jest.mock("../../../../../utils/notifications", () => {
   const mockedNotifications: Dictionary = jest.requireActual("../../../../../utils/notifications");
+  mockedNotifications.msgError = jest.fn();
   mockedNotifications.msgSuccess = jest.fn();
 
   return mockedNotifications;
@@ -252,5 +254,62 @@ describe("Files", () => {
     await act(async () => { await wait(0); wrapper.update(); });
     expect(open)
       .toBeCalledWith("https://test.com/file");
+  });
+
+  it("should handle errors when add a file", async () => {
+    const mocksMutation: ReadonlyArray<MockedResponse> = [{
+      request: {
+        query: UPLOAD_FILE_MUTATION,
+        variables:  {
+          file: {},
+          filesData: JSON.stringify([{
+            description: "Test description",
+            fileName: "image.png",
+          }]),
+          projectName: "TEST",
+        },
+      },
+      result: { errors: [
+        new GraphQLError("Access denied"),
+        new GraphQLError("Exception - Invalid field in form"),
+        new GraphQLError("Exception - Invalid characters"),
+      ]},
+    }];
+    const mockedPermissions: PureAbility<string> = new PureAbility([
+      { action: "backend_api_resolvers_resource__do_add_files" },
+    ]);
+    const wrapper: ReactWrapper = mount(
+      <Provider store={store}>
+        <MockedProvider mocks={mocksFiles.concat(mocksMutation)} addTypename={false}>
+          <authzContext.Provider value={mockedPermissions}>
+            <Files {...mockProps} />
+          </authzContext.Provider>
+        </MockedProvider>
+      </Provider>,
+    );
+    await act(async () => { await wait(0); wrapper.update(); });
+    const addButton: ReactWrapper = wrapper.find("button")
+      .findWhere((element: ReactWrapper) => element.contains("Add"))
+      .at(0);
+    addButton.simulate("click");
+    const addFilesModal: ReactWrapper = wrapper.find("addFilesModal");
+    const file: File = new File([""], "image.png", { type: "image/png" });
+    const fileInput: ReactWrapper = addFilesModal
+      .find({name: "file"})
+      .at(0)
+      .find("input");
+    fileInput.simulate("change", { target: { files: [ file ] } });
+    const descriptionInput: ReactWrapper = addFilesModal
+      .find({name: "description", type: "text"})
+      .at(0)
+      .find("textarea");
+    descriptionInput.simulate("change", { target: { value: "Test description" } });
+    const form: ReactWrapper = addFilesModal
+      .find("genericForm")
+      .at(0);
+    form.simulate("submit");
+    await act(async () => { await wait(0); wrapper.update(); });
+    expect(msgError)
+      .toHaveBeenCalledTimes(3);
   });
 });
