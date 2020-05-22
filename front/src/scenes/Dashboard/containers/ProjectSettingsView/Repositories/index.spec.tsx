bd63282d36077c20077d41a5541a29e51f641b3c
Andres Aguirre <aaguirre@fluidattacks.com>
2020-05-22 13:44:43 -0500

    test(front): #1372 increase coverage
    - Test handle errors in addRepositories function from Repositories
    
    
    Former-commit-id: 990bd9b50ff1b9953b376af6ba85da49dc8b12a2

--- front/src/scenes/Dashboard/containers/ProjectSettingsView/Repositories/index.spec.tsx
+++ front/src/scenes/Dashboard/containers/ProjectSettingsView/Repositories/index.spec.tsx
@@ -1,6 +1,7 @@
 import { MockedProvider, MockedResponse } from "@apollo/react-testing";
 import { PureAbility } from "@casl/ability";
 import { mount, ReactWrapper } from "enzyme";
+import { GraphQLError } from "graphql";
 import * as React from "react";
 // tslint:disable-next-line: no-submodule-imports
 import { act } from "react-dom/test-utils";
@@ -284,5 +285,69 @@ describe("Repositories", () => {
       .at(0);
     expect(firstRowInfo.text())
       .toEqual("SSHpruebarepo/git2masterActive");
+  });
+
+  it("should handle errors when add a repository", async () => {
+    const mocksMutation: ReadonlyArray<MockedResponse> = [{
+      request: {
+        query: ADD_REPOSITORIES_MUTATION,
+        variables: {
+          projectName: "TEST",
+          repos: [
+            {
+              branch: "master",
+              protocol : "HTTPS",
+              urlRepo: "gitlab.com/test/test.git",
+            },
+          ],
+        },
+      },
+      result: { errors: [
+        new GraphQLError("Access denied"),
+        new GraphQLError("Exception - Invalid field in form"),
+        new GraphQLError("Exception - One or more values already exist"),
+        new GraphQLError("Exception - Invalid characters"),
+      ]},
+    }];
+    const mockedPermissions: PureAbility<string> = new PureAbility([
+      { action: "backend_api_resolvers_resource__do_add_repositories" },
+    ]);
+    const wrapper: ReactWrapper = mount(
+      <Provider store={store}>
+        <MockedProvider mocks={mocksRepositories.concat(mocksMutation)} addTypename={false}>
+          <authzContext.Provider value={mockedPermissions}>
+            <Repositories {...mockProps} />
+          </authzContext.Provider>
+        </MockedProvider>
+      </Provider>,
+    );
+    await act(async () => { await wait(0); wrapper.update(); });
+    const addButton: ReactWrapper = wrapper.find("button")
+      .findWhere((element: ReactWrapper) => element.contains("Add"))
+      .at(0);
+    addButton.simulate("click");
+    const addRepositoriesModal: ReactWrapper = wrapper.find("addRepositoriesModal");
+    const repositoryInput: ReactWrapper = addRepositoriesModal
+    .find({name: "resources[0].urlRepo", type: "text"})
+    .at(0)
+    .find("input");
+    repositoryInput.simulate("change", { target: { value: "gitlab.com/test/test.git" } });
+    const protocolSelect: ReactWrapper = addRepositoriesModal
+      .find("select")
+      .findWhere((element: ReactWrapper) => element.contains("HTTPS"))
+      .at(0);
+    protocolSelect.simulate("change", { target: { value: "HTTPS" } });
+    const branchInput: ReactWrapper = addRepositoriesModal
+    .find({name: "resources[0].branch", type: "text"})
+    .at(0)
+    .find("input");
+    branchInput.simulate("change", { target: { value: "master" } });
+    const form: ReactWrapper = addRepositoriesModal
+      .find("genericForm")
+      .at(0);
+    form.simulate("submit");
+    await act(async () => { await wait(0); wrapper.update(); });
+    expect(msgError)
+      .toHaveBeenCalledTimes(4);
   });
 });
