62051a11cdec51dfe8f8589a97df20452ceba4c0
Andres Aguirre <aaguirre@fluidattacks.com>
2020-06-01 12:10:59 -0500

    perf(back): #2066 concurrent code
    - Load findings, drafts and attrs concurrently to load project
    
    
    Former-commit-id: c8e4a669e1e13622dcf690033a48f2a22ed4a84f

--- django-apps/integrates-back-async/backend/api/dataloaders/project.py
+++ django-apps/integrates-back-async/backend/api/dataloaders/project.py
@@ -1,5 +1,6 @@
 # pylint: disable=method-hidden
 
+import asyncio
 from typing import Dict, List
 from asgiref.sync import sync_to_async
 from backend.domain import project as project_domain
@@ -9,16 +10,33 @@ from aiodataloader import DataLoader
 async def _batch_load_fn(projects: List[str]):
     """Batch the data load requests within the same execution fragment."""
     projects_data: Dict = dict()
+    list_findings_tasks = [
+        asyncio.create_task(
+            sync_to_async(project_domain.list_findings)(project.lower())
+        )
+        for project in projects
+    ]
+    list_drafts_tasks = [
+        asyncio.create_task(
+            sync_to_async(project_domain.list_drafts)(project.lower())
+        )
+        for project in projects
+    ]
+    get_attributes_tasks = [
+        asyncio.create_task(
+            sync_to_async(project_domain.get_attributes)(project.lower(), [])
+        )
+        for project in projects
+    ]
+    findings = await asyncio.gather(*list_findings_tasks)
+    drafts = await asyncio.gather(*list_drafts_tasks)
+    attrs = await asyncio.gather(*get_attributes_tasks)
 
     for project in projects:
-        project = project.lower()
-        findings = await sync_to_async(project_domain.list_findings)(project)
-        drafts = await sync_to_async(project_domain.list_drafts)(project)
-        attrs = await sync_to_async(project_domain.get_attributes)(project, [])
         projects_data[project] = dict(
-            findings=findings,
-            drafts=drafts,
-            attrs=attrs
+            findings=findings.pop(0),
+            drafts=drafts.pop(0),
+            attrs=attrs.pop(0)
         )
 
     return [projects_data.get(project, dict()) for project in projects]
