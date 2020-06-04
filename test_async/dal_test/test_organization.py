6a7079c7917a9dfe63a1edd7b569b1f93cceedf8
Mateo Norena <mnorena@fluidattacks.com>
2020-06-04 08:49:45 -0500

    test(back): #2038.6 organization dal
    Former-commit-id: 3faa9e019acd8b2701d315afa1f7072d2fbe33ea

---
+++ test_async/dal_test/test_organization.py
@@ -0,0 +1,84 @@
+import pytest
+
+import backend.dal.organization as org_dal
+from backend.exceptions import InvalidOrganization
+
+def test__map_keys_to_domain():
+    test_dict = {
+        'pk': 'primary-key',
+        'sk': 'secondary-key',
+        'attr1': 'attribute_1',
+        'attr2': 'attribute_2'
+    }
+    mapped_dict = org_dal._map_keys_to_domain(test_dict)
+    assert mapped_dict['id'] == test_dict['pk']
+    assert mapped_dict['name'] == test_dict['sk']
+    assert mapped_dict['attr1'] == test_dict['attr1']
+    assert mapped_dict['attr2'] == test_dict['attr2']
+    assert 'pk' not in mapped_dict
+    assert 'sk' not in mapped_dict
+
+def test__map_keys_to_dal():
+    test_dict = {
+        'id': 'primary-key',
+        'name': 'secondary-key',
+        'attr1': 'attribute_1',
+        'attr2': 'attribute_2'
+    }
+    mapped_dict = org_dal._map_keys_to_dal(test_dict)
+    assert mapped_dict['pk'] == test_dict['id']
+    assert mapped_dict['sk'] == test_dict['name']
+    assert mapped_dict['attr1'] == test_dict['attr1']
+    assert mapped_dict['attr2'] == test_dict['attr2']
+    assert 'id' not in mapped_dict
+    assert 'name' not in mapped_dict
+
+def test__map_attributes_to_dal():
+    test_list = ['id', 'name', 'attr1', 'attr2']
+    mapped_list = org_dal._map_attributes_to_dal(test_list)
+    assert 'pk' in mapped_list
+    assert 'sk' in mapped_list
+    assert 'attr1' in mapped_list
+    assert 'attr2' in mapped_list
+    assert 'id' not in mapped_list
+    assert 'name' not in mapped_list
+
+@pytest.mark.changes_db
+def test_create():
+    org_name = 'test-org-creating'
+    new_org = org_dal.create(org_name)
+    assert isinstance(new_org, dict)
+    assert 'id' in new_org
+    assert new_org['name'] == org_name
+    with pytest.raises(InvalidOrganization):
+        org_dal.create(org_name)
+
+def test_exists():
+    existing_group = org_dal.exists('test-org')
+    assert existing_group
+    non_existent_group = org_dal.exists('no-exists')
+    assert not non_existent_group
+
+def test_get():
+    ex_org_name = 'test-org'
+    not_ex_org_name = 'no-exists'
+    existing_org = org_dal.get(ex_org_name)
+    assert isinstance(existing_org, dict)
+    assert 'id' in existing_org
+    assert existing_org['name'] == ex_org_name
+    not_existent_org = org_dal.get(not_ex_org_name)
+    assert not not_existent_org
+
+@pytest.mark.changes_db
+def test_get_or_create():
+    ex_org_name = 'test-org'
+    not_ex_org_name = 'new-org'
+    existing_org = org_dal.get_or_create(ex_org_name)
+    assert isinstance(existing_org, dict)
+    assert existing_org['id'] == '38eb8f25-7945-4173-ab6e-0af4ad8b7ef3'
+    assert existing_org['name'] == ex_org_name
+    
+    not_existent_org = org_dal.get_or_create(not_ex_org_name)
+    assert isinstance(not_existent_org, dict)
+    assert 'id' in not_existent_org
+    assert not_existent_org['name'] == not_ex_org_name
