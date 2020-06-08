f6bc41398205c9f939f1176400dca46a62adca51
Richard Almanza <rvega@fluidattacks.com>
2020-06-08 08:44:56 -0500

    test(back): #1372 add dal/tag test
    Former-commit-id: 22a0126ccc35095d45a3e37506d113f9e2e10f62

---
+++ test_async/dal_test/test_tag.py
@@ -0,0 +1,29 @@
+import pytest
+
+from django.test import TestCase
+from decimal import Decimal
+from backend.dal.tag import (
+    update, get_attributes
+)
+
+
+class TagTest(TestCase):
+
+    @pytest.mark.changes_db
+    def test_update(self):
+        # company, tag, data
+        test_1 = ('fluid', 'test-updates', {
+            'mean_remediate_critical_severity' : None,
+            'mean_remediate' : None,
+            'max_severity' : Decimal('3.3')
+        })
+        original = {
+            'mean_remediate_critical_severity' : Decimal('0'),
+            'mean_remediate' : Decimal('132'),
+            'max_severity' : Decimal('6.0')
+        }
+        attributes = [attr for attr in original]
+        assert original == get_attributes(test_1[0], test_1[1], attributes)
+        assert update(*test_1)
+        assert {'max_severity' : Decimal('3.3')} == \
+            get_attributes(test_1[0], test_1[1], attributes)
