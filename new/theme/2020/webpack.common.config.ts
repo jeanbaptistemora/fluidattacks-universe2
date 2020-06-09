2491ca0eb5e2b3a0c9feac13f72e958d2d28035e
John Perez <jperez@fluidattacks.com>
2020-06-09 11:36:52 -0500

    feat(front): #3331 add defends page
    Former-commit-id: f0e146f490f29edbe69297ae851bc9dca14c0587

--- new/theme/2020/webpack.common.config.ts
+++ new/theme/2020/webpack.common.config.ts
@@ -7,6 +7,7 @@ export const commonConfig: webpack.Configuration = {
     carousel: "./static/js/carousel.ts",
     contactSlides: "./static/js/contactSlides.ts",
     contentHome: "./static/js/contentHome.ts",
+    defends: "./static/js/defends.ts",
     dynClasses: "./static/js/dynClasses.ts",
     form: "./static/js/form.ts",
     progressBar: "./static/js/progressBar.ts",
