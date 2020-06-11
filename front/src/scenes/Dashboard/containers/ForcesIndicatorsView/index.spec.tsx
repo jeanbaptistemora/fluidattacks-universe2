6e09ce02a73547751cc55a2e43626f2a4cf7e64e
Andres Lemus <alemus@fluidattacks.com>
2020-06-11 17:00:04 -0500

    test(front): #1372.151 edit forces_indicators test
    Former-commit-id: aa12702a4b513171858eaa7ba5b22879e477f0cd

--- front/src/scenes/Dashboard/containers/ForcesIndicatorsView/index.spec.tsx
+++ front/src/scenes/Dashboard/containers/ForcesIndicatorsView/index.spec.tsx
@@ -2,46 +2,21 @@ import { MockedProvider, MockedResponse } from "@apollo/react-testing";
 import { mount, ReactWrapper } from "enzyme";
 import { GraphQLError } from "graphql";
 import * as React from "react";
+// tslint:disable-next-line: no-submodule-imports
+import { act } from "react-dom/test-utils";
 import { Provider } from "react-redux";
 import { MemoryRouter } from "react-router-dom";
 import wait from "waait";
 import store from "../../../../store/index";
 import { ForcesIndicatorsView } from "./index";
 import { GET_INDICATORS } from "./queries";
-import { IForcesIndicatorsViewBaseProps } from "./types";
+import {
+  IForcesExecution, IForcesIndicatorsProps, IForcesIndicatorsViewBaseProps, IForcesVulnerabilities,
+} from "./types";
 
 describe("ForcesIndicatorsView", () => {
 
   const mockProps: IForcesIndicatorsViewBaseProps = { projectName: "TEST" };
-
-  const mocks: ReadonlyArray<MockedResponse> = [
-    {
-      request: {
-        query: GET_INDICATORS,
-        variables: {
-          projectName: "TEST",
-        },
-      },
-      result: {
-        data: {
-          forcesExecutions: {
-            executions: [
-              {
-                strictness: "strict",
-                vulnerabilities: {
-                  numOfVulnerabilitiesInAcceptedExploits: 1,
-                  numOfVulnerabilitiesInExploits: 2,
-                  numOfVulnerabilitiesInIntegratesExploits: 3,
-                },
-              },
-            ],
-          },
-          project: {
-            hasForces: true,
-          },
-        },
-      },
-  }];
 
   const mockError: ReadonlyArray<MockedResponse> = [
     {
@@ -54,7 +29,7 @@ describe("ForcesIndicatorsView", () => {
       result: {
         errors: [new GraphQLError("Access denied")],
       },
-  }];
+    }];
 
   it("should return an function", () => {
     expect(typeof (ForcesIndicatorsView))
@@ -76,18 +51,97 @@ describe("ForcesIndicatorsView", () => {
       .toHaveLength(1);
   });
 
-  it("should render a component", async () => {
+  it("should render Forces Indicators", async () => {
+    const forcesVulnerabilities: IForcesVulnerabilities = {
+      numOfVulnerabilitiesInAcceptedExploits: 1,
+      numOfVulnerabilitiesInExploits: 2,
+      numOfVulnerabilitiesInIntegratesExploits: 3,
+    };
+
+    const forcesExecution: IForcesExecution = {
+      strictness: "strict",
+      vulnerabilities: forcesVulnerabilities,
+    };
+
+    const forcesIndicators: IForcesIndicatorsProps = {
+      forcesExecutions: {
+        executions: [forcesExecution],
+      },
+      project: {
+        hasForces: true,
+      },
+    };
+
+    const mocks: ReadonlyArray<MockedResponse> = [
+      {
+        request: {
+          query: GET_INDICATORS,
+          variables: {
+            projectName: "TEST",
+          },
+        },
+        result: {
+          data: forcesIndicators,
+        },
+      }];
+
     const wrapper: ReactWrapper = mount(
       <MemoryRouter initialEntries={["/project/TEST/forces"]}>
         <Provider store={store}>
-          <MockedProvider mocks={mocks} addTypename={true}>
+          <MockedProvider mocks={mocks} addTypename={false}>
             <ForcesIndicatorsView {...mockProps} />
           </MockedProvider>
         </Provider>
       </MemoryRouter>,
     );
-    await wait(0);
+
     expect(wrapper)
+      .toHaveLength(1);
+
+    await act(async () => { await wait(0); wrapper.update(); });
+
+    const forcesIndicatorsTitle: ReactWrapper = wrapper.find("h1")
+      .filterWhere((element: ReactWrapper) => element.contains("Forces Indicators"));
+    const systemStatusWidget: ReactWrapper = wrapper
+      .find("div")
+      .filterWhere((element: ReactWrapper) => element.contains("System status"))
+      .first();
+    const ratioOfStrictBuildsWidget: ReactWrapper = wrapper
+      .find("div")
+      .filterWhere((element: ReactWrapper) => element.contains("Ratio of builds in Strict mode"))
+      .first();
+    const serviceUsageWidget: ReactWrapper = wrapper
+      .find("div")
+      .filterWhere((element: ReactWrapper) => element.contains("Service usage"))
+      .first();
+    const vulnerableBuildsWidget: ReactWrapper = wrapper
+      .find("div")
+      .filterWhere((element: ReactWrapper) => element.contains("Vulnerable builds"))
+      .first();
+    const protectedBuildsWidget: ReactWrapper = wrapper
+      .find("div")
+      .filterWhere((element: ReactWrapper) => element.contains("Protected builds"))
+      .first();
+    const buildsWithAcceptedRiskWidget: ReactWrapper = wrapper
+      .find("div")
+      .filterWhere((element: ReactWrapper) => element.contains("Builds with accepted risk"))
+      .first();
+
+    expect(wrapper)
+      .toHaveLength(1);
+    expect(forcesIndicatorsTitle)
+      .toHaveLength(1);
+    expect(systemStatusWidget)
+      .toHaveLength(1);
+    expect(ratioOfStrictBuildsWidget)
+      .toHaveLength(1);
+    expect(serviceUsageWidget)
+      .toHaveLength(1);
+    expect(protectedBuildsWidget)
+      .toHaveLength(1);
+    expect(vulnerableBuildsWidget)
+      .toHaveLength(1);
+    expect(buildsWithAcceptedRiskWidget)
       .toHaveLength(1);
   });
 });
