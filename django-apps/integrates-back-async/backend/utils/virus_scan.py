e01d842db414b489536e405d70389e3801e46c79
Mario Varas <mvaras@fluidattacks.com>
2020-06-11 16:50:04 -0500

    feat(back): #2102 consume api only in prod
    Former-commit-id: 3a2c02811cfe9aec2439a057d4c6a5715b7fab67

--- django-apps/integrates-back-async/backend/utils/virus_scan.py
+++ django-apps/integrates-back-async/backend/utils/virus_scan.py
@@ -4,7 +4,7 @@ import cloudmersive_virus_api_client
 import rollbar
 from cloudmersive_virus_api_client.rest import ApiException
 from backend.exceptions import FileInfected
-from __init__ import FI_CLOUDMERSIVE_API_KEY
+from __init__ import FI_CLOUDMERSIVE_API_KEY, FI_ENVIRONMENT
 
 
 API_CLIENT = cloudmersive_virus_api_client.ScanApi()
@@ -13,13 +13,15 @@ API_CLIENT.api_client.configuration.api_key['Apikey'] = FI_CLOUDMERSIVE_API_KEY
 
 
 def scan_file(file_object: IO):
-    try:
-        tmp_file = tempfile.NamedTemporaryFile()
-        tmp_file.write(file_object.read())
-        api_response = API_CLIENT.scan_file(tmp_file.name)
-        tmp_file.close()
-        file_object.seek(0)
-        if not api_response.clean_result:
-            raise FileInfected()
-    except ApiException as api_error:
-        rollbar.report_message('Error: Cloudmersive VirusScan API error: %s' % api_error)
+    if FI_ENVIRONMENT == 'production':
+        try:
+            tmp_file = tempfile.NamedTemporaryFile()
+            tmp_file.write(file_object.read())
+            tmp_file.flush()
+            api_response = API_CLIENT.scan_file(tmp_file.name)
+            tmp_file.close()
+            file_object.seek(0)
+            if not api_response.clean_result:
+                raise FileInfected()
+        except ApiException as api_error:
+            rollbar.report_message('Error: Cloudmersive VirusScan API error: %s' % api_error)
