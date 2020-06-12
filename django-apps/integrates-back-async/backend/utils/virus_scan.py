71cc697115ff5dd44d14d1fd297102fff9440567
Mario Varas <mvaras@fluidattacks.com>
2020-06-12 14:09:42 -0500

    feat(back): #2102 virus scan extra options
    - customized configuration
    
    
    Former-commit-id: eeb64eaf0c485603fcbd13cbcd7acc196e47c813

--- django-apps/integrates-back-async/backend/utils/virus_scan.py
+++ django-apps/integrates-back-async/backend/utils/virus_scan.py
@@ -7,9 +7,10 @@ from backend.exceptions import FileInfected
 from __init__ import FI_CLOUDMERSIVE_API_KEY, FI_ENVIRONMENT
 
 
-API_CLIENT = cloudmersive_virus_api_client.ScanApi()
-API_CLIENT.api_client.configuration.api_key = {}
-API_CLIENT.api_client.configuration.api_key['Apikey'] = FI_CLOUDMERSIVE_API_KEY
+API_CONFIGURATION = cloudmersive_virus_api_client.Configuration()
+API_CONFIGURATION.api_key['Apikey'] = FI_CLOUDMERSIVE_API_KEY
+API_CLIENT = cloudmersive_virus_api_client.ScanApi(
+    cloudmersive_virus_api_client.ApiClient(API_CONFIGURATION))
 
 
 def scan_file(file_object: IO):
@@ -18,7 +19,9 @@ def scan_file(file_object: IO):
             tmp_file = tempfile.NamedTemporaryFile()
             tmp_file.write(file_object.read())
             tmp_file.flush()
-            api_response = API_CLIENT.scan_file(tmp_file.name)
+            api_response = API_CLIENT.scan_file_advanced(tmp_file.name,
+                                                         allow_executables=True,
+                                                         allow_scripts=True)
             tmp_file.close()
             file_object.seek(0)
             if not api_response.clean_result:
