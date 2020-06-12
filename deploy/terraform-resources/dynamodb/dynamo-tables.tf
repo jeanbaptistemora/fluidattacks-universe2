219e8cbabee8fdd8b571566f79aaa474796c15a2
Andres Cuberos <acuberos@fluidattacks.com>
2020-06-12 15:37:57 -0500

    feat(infra): #2151.1 create organizations table
    - It will have two columns `pk` and `sk`. The rest of the columns
      will depend on the type of record.
    - `sk` can take the form `user#userID`, `group#groupID` to easily
      access different kind of information depending on the access
      pattern identified.
    
    
    Former-commit-id: f518a621c2db2be40e738511428c4adee1e3322c

--- deploy/terraform-resources/dynamodb/dynamo-tables.tf
+++ deploy/terraform-resources/dynamodb/dynamo-tables.tf
@@ -282,3 +282,31 @@ resource "aws_dynamodb_table" "portfolios" {
     enabled = true
   }
 }
+
+resource "aws_dynamodb_table" "organizations" {
+  name = "fi_organizations"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "pk"
+  range_key = "sk"
+
+  attribute {
+    name = "pk"
+    type = "S"
+  }
+
+  attribute {
+    name = "sk"
+    type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  global_secondary_index {
+    name = "gsi-1"
+    hash_key =  "sk"
+    range_key = "pk"
+    projection_type = "ALL"
+  }
+}
