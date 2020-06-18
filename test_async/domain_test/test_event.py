1b4ee7eeb43d5a348571b2a914aea1e7b3a4a515
Richard Almanza <rvega@fluidattacks.com>
2020-06-18 13:11:19 -0500

    test(back): #1372 change domain_test/test_event
    - change tests methods as functions
    
    
    Former-commit-id: e7670aa197b58e18fb9cb5f4b008742e3639143f

--- test_async/domain_test/test_event.py
+++ test_async/domain_test/test_event.py
@@ -13,60 +13,58 @@ from backend.exceptions import (
 )
 
 
-class EventTests(TestCase):
+def test_get_event():
+    event_id = '418900971'
+    test_data = event_domain.get_event(event_id)
+    expected_output = 'unittesting'
+    assert test_data.get('project_name') == expected_output
+    with pytest.raises(EventNotFound):
+        event_domain.get_event('000001111')
 
-    def test_get_event(self):
-        event_id = '418900971'
-        test_data = event_domain.get_event(event_id)
-        expected_output = 'unittesting'
-        assert test_data.get('project_name') == expected_output
-        with pytest.raises(EventNotFound):
-            event_domain.get_event('000001111')
+@pytest.mark.changes_db
+def test_create_event():
+    attrs = {
+        'action_after_blocking': 'TRAINING',
+        'action_before_blocking': 'DOCUMENT_PROJECT',
+        'accessibility': 'REPOSITORY',
+        'analyst_email': 'unittesting@fluidattacks.com',
+        'context': 'OTHER',
+        'detail': 'Something happened.',
+        'event_date': parse_datetime('2019-12-09T05:00:00.000Z'),
+        'event_type': 'CLIENT_DETECTS_ATTACK',
+        'project_name': 'unittesting'
+    }
+    assert event_domain.create_event(**attrs)
 
-    @pytest.mark.changes_db
-    def test_create_event(self):
-        attrs = {
-            'action_after_blocking': 'TRAINING',
-            'action_before_blocking': 'DOCUMENT_PROJECT',
-            'accessibility': 'REPOSITORY',
-            'analyst_email': 'unittesting@fluidattacks.com',
-            'context': 'OTHER',
-            'detail': 'Something happened.',
-            'event_date': parse_datetime('2019-12-09T05:00:00.000Z'),
-            'event_type': 'CLIENT_DETECTS_ATTACK',
-            'project_name': 'unittesting'
-        }
-        assert event_domain.create_event(**attrs)
-
-    @pytest.mark.changes_db
-    def test_create_event_file_image(self):
-        attrs = {
-            'action_after_blocking': 'TRAINING',
-            'action_before_blocking': 'DOCUMENT_PROJECT',
-            'accessibility': 'REPOSITORY',
-            'analyst_email': 'unittesting@fluidattacks.com',
-            'context': 'OTHER',
-            'detail': 'Something happened.',
-            'event_date': parse_datetime('2019-12-09T05:00:00.000Z'),
-            'event_type': 'CLIENT_DETECTS_ATTACK',
-            'project_name': 'unittesting'
-        }
-        filename = os.path.dirname(os.path.abspath(__file__))
-        filename = os.path.join(filename, '../mock/test-file-records.csv')
-        with open(filename, 'rb') as test_file:
-            uploaded_file = SimpleUploadedFile(name=test_file.name,
-                                                content=test_file.read(),
-                                                content_type='text/csv')
-        imagename = os.path.dirname(os.path.abspath(__file__))
-        imagename = os.path.join(imagename, '../mock/test-anim.gif')
-        with open(imagename, 'rb') as image_test:
-            uploaded_image = SimpleUploadedFile(name=image_test.name,
-                                                content=image_test.read(),
-                                                content_type='image/gif')
-        test_data = event_domain.create_event(**attrs, file=uploaded_file, image=uploaded_image)
-        expected_output = True
-        assert isinstance(test_data, bool)
-        assert test_data == expected_output
+@pytest.mark.changes_db
+def test_create_event_file_image():
+    attrs = {
+        'action_after_blocking': 'TRAINING',
+        'action_before_blocking': 'DOCUMENT_PROJECT',
+        'accessibility': 'REPOSITORY',
+        'analyst_email': 'unittesting@fluidattacks.com',
+        'context': 'OTHER',
+        'detail': 'Something happened.',
+        'event_date': parse_datetime('2019-12-09T05:00:00.000Z'),
+        'event_type': 'CLIENT_DETECTS_ATTACK',
+        'project_name': 'unittesting'
+    }
+    filename = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(filename, '../mock/test-file-records.csv')
+    with open(filename, 'rb') as test_file:
+        uploaded_file = SimpleUploadedFile(name=test_file.name,
+                                            content=test_file.read(),
+                                            content_type='text/csv')
+    imagename = os.path.dirname(os.path.abspath(__file__))
+    imagename = os.path.join(imagename, '../mock/test-anim.gif')
+    with open(imagename, 'rb') as image_test:
+        uploaded_image = SimpleUploadedFile(name=image_test.name,
+                                            content=image_test.read(),
+                                            content_type='image/gif')
+    test_data = event_domain.create_event(**attrs, file=uploaded_file, image=uploaded_image)
+    expected_output = True
+    assert isinstance(test_data, bool)
+    assert test_data == expected_output
 
 @pytest.mark.changes_db
 def test_solve_event():
