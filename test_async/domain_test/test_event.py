9a9ee1d0123471d46b7eeb917b8b45892d27d257
Richard Almanza <rvega@fluidattacks.com>
2020-06-18 11:48:47 -0500

    test(back): #1372 change domain_test/test_event
    - change tests methods as functions
    
    
    Former-commit-id: 20e4a2a53548c3a6408e592460ec34e865c97dff

--- test_async/domain_test/test_event.py
+++ test_async/domain_test/test_event.py
@@ -68,86 +68,86 @@ class EventTests(TestCase):
         assert isinstance(test_data, bool)
         assert test_data == expected_output
 
-    @pytest.mark.changes_db
-    def test_solve_event(self):
+@pytest.mark.changes_db
+def test_solve_event():
+    assert event_domain.solve_event(
+        event_id='538745942',
+        affectation=1,
+        analyst_email='unittesting@fluidattacks.com',
+        date=parse_datetime('2019-12-09T05:00:00.000Z'))
+    event = event_domain.get_event('538745942')
+    assert event['historic_state'][-1]['state'] == 'SOLVED'
+    with pytest.raises(EventAlreadyClosed):
         assert event_domain.solve_event(
             event_id='538745942',
             affectation=1,
             analyst_email='unittesting@fluidattacks.com',
             date=parse_datetime('2019-12-09T05:00:00.000Z'))
-        event = event_domain.get_event('538745942')
-        assert event['historic_state'][-1]['state'] == 'SOLVED'
-        with pytest.raises(EventAlreadyClosed):
-            assert event_domain.solve_event(
-                event_id='538745942',
-                affectation=1,
-                analyst_email='unittesting@fluidattacks.com',
-                date=parse_datetime('2019-12-09T05:00:00.000Z'))
 
-    @pytest.mark.changes_db
-    def test_add_comment(self):
-        comment_id = int(round(time() * 1000))
-        comment_id, success = event_domain.add_comment(
-            comment_id=comment_id,
-            content='comment test',
-            event_id='538745942',
-            parent='0',
-            user_info={
-                'user_email': 'unittesting@fluidattacks.com',
-                'first_name': 'Unit',
-                'last_name': 'test'
-            })
-        assert success
-        assert comment_id
+@pytest.mark.changes_db
+def test_add_comment():
+    comment_id = int(round(time() * 1000))
+    comment_id, success = event_domain.add_comment(
+        comment_id=comment_id,
+        content='comment test',
+        event_id='538745942',
+        parent='0',
+        user_info={
+            'user_email': 'unittesting@fluidattacks.com',
+            'first_name': 'Unit',
+            'last_name': 'test'
+        })
+    assert success
+    assert comment_id
 
-        comment_id, success = event_domain.add_comment(
+    comment_id, success = event_domain.add_comment(
+        comment_id=int(round(time() * 1000)),
+        content='comment test 2',
+        event_id='538745942',
+        parent=str(comment_id),
+        user_info={
+            'user_email': 'unittesting@fluidattacks.com',
+            'first_name': 'Unit',
+            'last_name': 'test'
+        })
+    assert success
+    assert comment_id
+
+    with pytest.raises(InvalidCommentParent):
+        assert event_domain.add_comment(
             comment_id=int(round(time() * 1000)),
             content='comment test 2',
             event_id='538745942',
-            parent=str(comment_id),
+            parent=str(comment_id + 1),
             user_info={
                 'user_email': 'unittesting@fluidattacks.com',
                 'first_name': 'Unit',
                 'last_name': 'test'
             })
-        assert success
-        assert comment_id
 
-        with pytest.raises(InvalidCommentParent):
-            assert event_domain.add_comment(
-                comment_id=int(round(time() * 1000)),
-                content='comment test 2',
-                event_id='538745942',
-                parent=str(comment_id + 1),
-                user_info={
-                    'user_email': 'unittesting@fluidattacks.com',
-                    'first_name': 'Unit',
-                    'last_name': 'test'
-                })
+@pytest.mark.changes_db
+def test_update_evidence():
+    event_id = '418900978'
+    evidence_type = 'records'
+    filename = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(filename, '../mock/test-file-records.csv')
+    with open(filename, 'rb') as test_file:
+        uploaded_file = SimpleUploadedFile(name=test_file.name,
+                                            content=test_file.read(),
+                                            content_type='text/csv')
+    test_data = event_domain.update_evidence(event_id, evidence_type, uploaded_file)
+    expected_output = True
+    assert isinstance(test_data, bool)
+    assert test_data == expected_output
 
-    @pytest.mark.changes_db
-    def test_update_evidence(self):
-        event_id = '418900978'
-        evidence_type = 'records'
-        filename = os.path.dirname(os.path.abspath(__file__))
-        filename = os.path.join(filename, '../mock/test-file-records.csv')
-        with open(filename, 'rb') as test_file:
-            uploaded_file = SimpleUploadedFile(name=test_file.name,
-                                                content=test_file.read(),
-                                                content_type='text/csv')
-        test_data = event_domain.update_evidence(event_id, evidence_type, uploaded_file)
-        expected_output = True
-        assert isinstance(test_data, bool)
-        assert test_data == expected_output
-
-    @pytest.mark.changes_db
-    def test_update_evidence_attribute_error(self):
-        event_id = '418900978'
-        evidence_type = 'records'
-        test_data = event_domain.update_evidence(event_id, evidence_type, '')
-        expected_output = False
-        assert isinstance(test_data, bool)
-        assert test_data == expected_output
+@pytest.mark.changes_db
+def test_update_evidence_attribute_error():
+    event_id = '418900978'
+    evidence_type = 'records'
+    test_data = event_domain.update_evidence(event_id, evidence_type, '')
+    expected_output = False
+    assert isinstance(test_data, bool)
+    assert test_data == expected_output
 
 def test_validate_evidence_invalid_image_type():
     evidence_type = 'evidence'
