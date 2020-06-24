14b420bf8d6c9f2d34916b7b9bb55b6df335a89d
Mateo Norena <mnorena@fluidattacks.com>
2020-06-24 15:18:46 -0500

    fix(back): #2182 allow closer get vulns
    Former-commit-id: 3f9f9c4c33de79d90255d5b26262eaf1352afd60

--- django-apps/integrates-back-async/backend/authz/model.py
+++ django-apps/integrates-back-async/backend/authz/model.py
@@ -149,6 +149,7 @@ GROUP_LEVEL_ROLES: Dict[str, Dict[str, Set[str]]] = dict(
             'backend_api_resolvers_finding__do_update_evidence',
             'backend_api_resolvers_finding__get_analyst',
             'backend_api_resolvers_finding__get_historic_state',
+            'backend_api_resolvers_finding__get_pending_vulns',
             'backend_api_resolvers_finding_resolve_finding',
             'backend_api_resolvers_forces_resolve_forces_executions',
             'backend_api_resolvers_project__get_comments',
