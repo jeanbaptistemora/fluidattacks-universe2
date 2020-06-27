e1568f2d408229a2940f00d1f8369e557dec6517
Richard Almanza <rvega@fluidattacks.com>
2020-06-26 19:35:35 -0500

    style(back): #1985.19 restyle utils/virus_scan
    - Applied requirement REQ-7
    
    
    Former-commit-id: f1f7caf0b719ddd1179a0e53449b605d0c19c0de

--- django-apps/integrates-back-async/backend/utils/virus_scan.py
+++ django-apps/integrates-back-async/backend/utils/virus_scan.py
@@ -4,7 +4,10 @@ import rollbar
 from cloudmersive_virus_api_client.rest import ApiException
 from backend.exceptions import FileInfected
 from django.core.files.uploadedfile import InMemoryUploadedFile
-from __init__ import FI_CLOUDMERSIVE_API_KEY, FI_ENVIRONMENT
+from __init__ import (
+    FI_CLOUDMERSIVE_API_KEY,
+    FI_ENVIRONMENT
+)
 
 
 API_CONFIGURATION = cloudmersive_virus_api_client.Configuration()
@@ -13,7 +16,10 @@ API_CLIENT = cloudmersive_virus_api_client.ScanApi(
     cloudmersive_virus_api_client.ApiClient(API_CONFIGURATION))
 
 
-def scan_file(target_file: InMemoryUploadedFile, user_email: str, project_name: str):
+def scan_file(
+        target_file: InMemoryUploadedFile,
+        user_email: str,
+        project_name: str):
     if FI_ENVIRONMENT == 'production':
         try:
             file_object = target_file.file
@@ -24,16 +30,19 @@ def scan_file(target_file: InMemoryUploadedFile, user_email: str, project_name: 
                 tmp_file.name,
                 allow_executables=True,
                 allow_scripts=True,
-                allow_invalid_files=True)
+                allow_invalid_files=True
+            )
             tmp_file.close()
             file_object.seek(0)
             if not api_response.clean_result:
                 rollbar.report_message(
                     f'Report: Cloudmersive VirusScan file infected for user '
                     f'{user_email} in project {project_name}: '
-                    f'{target_file.name}\n{str(api_response)}')
+                    f'{target_file.name}\n{str(api_response)}'
+                )
                 raise FileInfected()
         except ApiException as api_error:
             rollbar.report_message(
                 f'Error: Cloudmersive VirusScan API error for user '
-                f'{user_email} in project {project_name}: {api_error}')
+                f'{user_email} in project {project_name}: {api_error}'
+            )
