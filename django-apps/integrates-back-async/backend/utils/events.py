7b092b5346ad0400a3f8030f50a933682205dc64
Richard Almanza <rvega@fluidattacks.com>
2020-06-26 19:25:32 -0500

    style(back): #1985.19 restyle utils/event
    - Applied requirement REQ-7
    
    
    Former-commit-id: 3cbc6a205a96ba6c23d86be8ddc01268af4a9852

--- django-apps/integrates-back-async/backend/utils/events.py
+++ django-apps/integrates-back-async/backend/utils/events.py
@@ -3,7 +3,10 @@ from backend.typing import Event as EventType
 
 
 def format_data(event: EventType) -> EventType:
-    historic_state = cast(List[Dict[str, str]], event.get('historic_state', [{}, {}]))
+    historic_state = cast(
+        List[Dict[str, str]],
+        event.get('historic_state', [{}, {}])
+    )
     event['closing_date'] = '-'
     if historic_state[-1].get('state') == 'SOLVED':
         event['closing_date'] = historic_state[-2].get('date', '')
