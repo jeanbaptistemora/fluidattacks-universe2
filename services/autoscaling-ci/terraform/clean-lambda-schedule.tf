994cb048cb42f3473a33b255e75ab0b6fc28859c
Daniel Salazar <dsalazar@fluidattacks.com>
2020-07-01 15:21:20 -0500

    feat(back): #406.2 cloudwatch schedule
    - Add cloudwatch schedule for running
    clean-autoscaling lambda every hour
    
    
    Former-commit-id: b0bb4bc06db6dafc7eab0f78ab6828acff86bb9f

---
+++ services/autoscaling-ci/terraform/clean-lambda-schedule.tf
@@ -0,0 +1,27 @@
+resource "aws_cloudwatch_event_rule" "every-hour" {
+  name                = "every--hour"
+  description         = "Fires every one hours"
+  schedule_expression = "rate(1 hour)"
+}
+
+resource "aws_cloudwatch_event_target" "autoscaling-clean" {
+  rule      = aws_cloudwatch_event_rule.every-hour.name
+  target_id = "lambda"
+  arn       = aws_lambda_function.autoscaling-clean.arn
+
+  input = <<EOF
+{
+  "region": [
+    "us-east-1"
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_permission" "autoscaling-clean-every-hour" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.autoscaling-clean.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.every-hour.arn
+}
--- services/autoscaling-ci/terraform/clean-lambda.tf
+++ services/autoscaling-ci/terraform/clean-lambda.tf
@@ -40,26 +40,26 @@ resource "aws_iam_role" "autoscaling-clean" {
   assume_role_policy = data.aws_iam_policy_document.lambda-assume-role-policy.json
 }
 
-resource "aws_iam_role_policy_attachment" "policy-clean-role" {
+resource "aws_iam_role_policy_attachment" "autoscaling-clean" {
   role       = aws_iam_role.autoscaling-clean.name
   policy_arn = aws_iam_policy.autoscaling-clean.arn
 }
 
-data "archive_file" "archive-clean-role" {
+data "archive_file" "autoscaling-clean" {
   type        = "zip"
   source_file = "index.js"
-  output_path = "clean-role.zip"
+  output_path = "autoscaling-clean.zip"
 }
 
-resource "aws_lambda_function" "lambda-clean-role" {
-  filename      = "clean-role.zip"
-  function_name = "autoscaling-clean-lambda"
+resource "aws_lambda_function" "autoscaling-clean" {
+  filename      = "autoscaling-clean.zip"
+  function_name = "autoscaling-clean"
   role          = aws_iam_role.autoscaling-clean.arn
   handler       = "index.handler"
   publish       = true
   timeout       = 10
 
-  source_code_hash = data.archive_file.archive-clean-role.output_base64sha256
+  source_code_hash = data.archive_file.autoscaling-clean.output_base64sha256
 
   runtime = "nodejs12.x"
 }
