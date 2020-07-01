83432795a0c7f8bd5800901184044d1376a4394f
Diego Restrepo <drestrepo@fluidattacks.com>
2020-07-01 14:58:39 -0500

    feat(back): #941.21 replace old check
    make that serves_content_over_http use graphs
    make that serves_content_over_insecure_protocols use graphs
    
    
    Former-commit-id: 6295b926f26bf46bed3929cf08cb8500166a6018

--- fluidasserts/cloud/aws/cloudformation/cloudfront.py
+++ fluidasserts/cloud/aws/cloudformation/cloudfront.py
@@ -1,7 +1,11 @@
 """AWS CloudFormation checks for ``CloudFront`` (Content Delivery Network)."""
 
 # Standard imports
-from typing import List, Optional
+from typing import List, Optional, Tuple, Dict
+
+# Treed imports
+from networkx import DiGraph
+from networkx.algorithms import dfs_preorder_nodes
 
 # Local imports
 from fluidasserts import SAST, MEDIUM, LOW
@@ -12,12 +16,16 @@ from fluidasserts.cloud.aws.cloudformation import (
     _get_result_as_tuple,
 )
 from fluidasserts.utils.decorators import api, unknown_if
+from fluidasserts.cloud.aws.cloudformation import get_templates
+from fluidasserts.cloud.aws.cloudformation import get_graph
+from fluidasserts.cloud.aws.cloudformation import get_resources
+from fluidasserts.cloud.aws.cloudformation import get_ref_nodes
 
 
 @api(risk=MEDIUM, kind=SAST)
 @unknown_if(FileNotFoundError)
-def serves_content_over_http(
-        path: str, exclude: Optional[List[str]] = None) -> tuple:
+def serves_content_over_http(path: str,
+                             exclude: Optional[List[str]] = None) -> tuple:
     """
     Check if ``Distributions`` are serving content over HTTP.
 
@@ -29,47 +37,49 @@ def serves_content_over_http(
               - ``CLOSED`` otherwise.
     :rtype: :class:`fluidasserts.Result`
     """
-    vulnerabilities: list = []
-    vulnerable_protocol_policy = ('allow-all',)
-    for yaml_path, res_name, res_props in helper.iterate_rsrcs_in_cfn_template(
-            starting_path=path,
-            resource_types=[
-                'AWS::CloudFront::Distribution',
-            ],
-            exclude=exclude):
+    vulnerable_protocol_policy = ('allow-all', )
+    vulnerabilities: List[Vulnerability] = []
+    graph: DiGraph = get_graph(path, exclude)
+    templates: List[Tuple[int, Dict]] = get_templates(graph, path, exclude)
+    distributions: List[int] = get_resources(
+        graph,
+        map(lambda x: x[0], templates), {'AWS', 'CloudFront', 'Distribution'},
+        info=True)
+    for dist, resource, template in distributions:
+        default = get_resources(graph, dist, 'DefaultCacheBehavior', 3)
+        view_policy = helper.get_index(
+            get_resources(graph, default, 'ViewerProtocolPolicy'), 0)
         found_issues: list = []
+        if view_policy:
+            view_policy_node = get_ref_nodes(graph, view_policy,
+                                             lambda x: isinstance(x, str))[0]
+            value = graph.nodes[view_policy_node]
+            if any(map(value['value'].__eq__, vulnerable_protocol_policy)):
+                found_issues.append(('DefaultCacheBehavior', value['value'],
+                                     value['line']))
 
-        protocol_policy = _index(
-            dictionary=res_props,
-            indexes=(
-                'DistributionConfig',
-                'DefaultCacheBehavior',
-                'ViewerProtocolPolicy'))
-        if any(map(protocol_policy.__eq__, vulnerable_protocol_policy)):
-            found_issues.append(('DefaultCacheBehavior', protocol_policy))
+        behaviors = get_resources(graph, dist, 'CacheBehaviors', 3)
+        view_policies = get_resources(graph, behaviors, 'ViewerProtocolPolicy',
+                                      4)
+        for policy in view_policies:
+            policy_node = get_ref_nodes(graph, policy,
+                                        lambda x: isinstance(x, str))[0]
+            value = graph.nodes[policy_node]
+            if any(map(value['value'].__eq__, vulnerable_protocol_policy)):
+                found_issues.append(('CacheBehaviors', value['value'],
+                                     value['line']))
 
-        for cache_behavior in _index(
-                dictionary=res_props,
-                indexes=(
-                    'DistributionConfig',
-                    'CacheBehaviors',
-                ),
-                default=[]):
-            protocol_policy = cache_behavior.get('ViewerProtocolPolicy')
-            if any(map(protocol_policy.__eq__, vulnerable_protocol_policy)):
-                found_issues.append(('CacheBehaviors', protocol_policy))
-
-        for cache, policy in found_issues:
+        for cache, policy, line in found_issues:
             vulnerabilities.append(
                 Vulnerability(
-                    path=yaml_path,
+                    path=template['path'],
                     entity=(f'AWS::CloudFront::Distribution'
                             f'/DistributionConfig'
                             f'/{cache}'
                             f'/ViewerProtocolPolicy'
                             f'/{policy}'),
-                    identifier=res_name,
-                    line=helper.get_line(res_props),
+                    identifier=resource['name'],
+                    line=line,
                     reason='allows HTTP traffic'))
 
     return _get_result_as_tuple(
@@ -106,64 +116,57 @@ def serves_content_over_insecure_protocols(
     #    SSLv3 TLSv1
     #          TLSv1.1
     #          TLSv1.2 (best available)
-    vulnerabilities: list = []
-    vulnerable_origin_ssl_protocols = \
-        ('SSLv3', 'TLSv1', 'TLSv1.1')
-    vulnerable_min_prot_versions = \
-        ('SSLv3', 'TLSv1', 'TLSv1_2016', 'TLSv1.1_2016')
-    for yaml_path, res_name, res_props in helper.iterate_rsrcs_in_cfn_template(
-            starting_path=path,
-            resource_types=[
-                'AWS::CloudFront::Distribution',
-            ],
-            exclude=exclude):
-        minimum_protocol_version = _index(
-            dictionary=res_props,
-            indexes=(
-                'DistributionConfig',
-                'ViewerCertificate',
-                'MinimumProtocolVersion',
-            ),
-            default=None)
-        if minimum_protocol_version in vulnerable_min_prot_versions:
-            vulnerabilities.append(
-                Vulnerability(
-                    path=yaml_path,
-                    entity=(f'AWS::CloudFront::Distribution'
-                            f'/DistributionConfig'
-                            f'/ViewerCertificate'
-                            f'/MinimumProtocolVersion'
-                            f'/{minimum_protocol_version}'),
-                    identifier=res_name,
-                    line=helper.get_line(res_props),
-                    reason='is not the strongest protocol provided by AWS'))
+    vulnerabilities: List[Vulnerability] = []
+    vulnerable_origin_ssl_protocols = ('SSLv3', 'TLSv1', 'TLSv1.1')
+    vulnerable_min_prot_versions = ('SSLv3', 'TLSv1', 'TLSv1_2016',
+                                    'TLSv1.1_2016')
+    graph: DiGraph = get_graph(path, exclude)
+    templates: List[Tuple[int, Dict]] = get_templates(graph, path, exclude)
+    distributions: List[int] = get_resources(
+        graph,
+        map(lambda x: x[0], templates), {'AWS', 'CloudFront', 'Distribution'},
+        info=True)
+    for dist, resource, template in distributions:
+        version = helper.get_index(
+            get_resources(graph, dist, 'MinimumProtocolVersion', depth=4), 0)
+        if version:
+            version_node = helper.get_index(
+                get_ref_nodes(graph, version,
+                              lambda x: x in vulnerable_min_prot_versions), 0)
+            if version_node:
+                value = graph.nodes[version_node]
+                vulnerabilities.append(
+                    Vulnerability(
+                        path=template['path'],
+                        entity=(f'AWS::CloudFront::Distribution'
+                                f'/DistributionConfig'
+                                f'/ViewerCertificate'
+                                f'/MinimumProtocolVersion'
+                                f'/{value["value"]}'),
+                        identifier=resource['line'],
+                        line=value['line'],
+                        reason='is not the strongest protocol provided by AWS')
+                )
 
-        for origin in _index(
-                dictionary=res_props,
-                indexes=(
-                    'DistributionConfig',
-                    'Origins',
-                ),
-                default=[]):
-            for origin_ssl_protocol in _index(
-                    dictionary=origin,
-                    indexes=(
-                        'CustomOriginConfig',
-                        'OriginSSLProtocols',
-                    ),
-                    default=[]):
-                if origin_ssl_protocol in vulnerable_origin_ssl_protocols:
+        origins_nodes = helper.get_index(
+            get_resources(graph, dist, 'OriginSSLProtocols', 7), 0)
+        if origins_nodes:
+            for origin_ssl_protocol in filter(
+                    lambda x: 'value' in graph.nodes[x],
+                    dfs_preorder_nodes(graph, origins_nodes, 4)):
+                value = graph.nodes[origin_ssl_protocol]
+                if value['value'] in vulnerable_origin_ssl_protocols:
                     vulnerabilities.append(
                         Vulnerability(
-                            path=yaml_path,
+                            path=template['path'],
                             entity=(f'AWS::CloudFront::Distribution'
                                     f'/DistributionConfig'
                                     f'/Origins'
                                     f'/CustomOriginConfig'
                                     f'/OriginSSLProtocols'
-                                    f'/{origin_ssl_protocol}'),
-                            identifier=res_name,
-                            line=helper.get_line(res_props),
+                                    f'/{value["value"]}'),
+                            identifier=resource['name'],
+                            line=value['line'],
                             reason=('is not the strongest protocol '
                                     'provided by AWS')))
 
