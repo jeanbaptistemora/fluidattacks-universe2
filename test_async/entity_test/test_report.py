19b52ff400fbf35c3709a8d4389707fa0e370cbc
Mario Varas <mvaras@fluidattacks.com>
2020-07-07 15:42:00 -0500

    test(back): #2018.25 reports tests
    - Splitted by report type
    
    
    Former-commit-id: d12474ad5b817cbd79657b2b64cf56d19db3078f

--- test_async/entity_test/test_report.py
+++ test_async/entity_test/test_report.py
@@ -15,41 +15,92 @@ from backend.api.dataloaders.project import ProjectLoader
 from backend.api.schema import SCHEMA
 from test_async.utils import create_dummy_session
 
-pytestmark = pytest.mark.asyncio
 
+pytestmark = pytest.mark.asyncio
 
 class ReportTests(TestCase):
 
-    @pytest.mark.changes_db
-    async def test_request_report(self):
+    async def test_finding_report(self):
         query_pdf = '''
-            mutation {
-                requestProjectReport(
+            query test {
+                report(
                     projectName: "oneshottest",
                     reportType: PDF,
                     lang: EN) {
-                    success
+                    url
                 }
             }
         '''
         query_xls = '''
-            mutation {
-                requestProjectReport(
+            query test {
+                report(
                     projectName: "oneshottest",
                     reportType: XLS) {
-                    success
+                    url
+                }
+            }
+        '''
+        query_data = '''
+            query test {
+                report(
+                    projectName: "oneshottest",
+                    reportType: DATA) {
+                    url
                 }
             }
         '''
         data_pdf = {'query': query_pdf}
         data_xls = {'query': query_xls}
+        data_data = {'query': query_data}
         request = create_dummy_session('integratesmanager@gmail.com')
         request.loaders = {
             'project': ProjectLoader(),
         }
         _, result_pdf = await graphql(SCHEMA, data_pdf, context_value=request)
         _, result_xls = await graphql(SCHEMA, data_xls, context_value=request)
-        assert any('errors' not in result for result in [result_pdf, result_xls])
-        assert all('success' in result['data']['requestProjectReport'] and
-            result['data']['requestProjectReport']['success']
-            for result in [result_pdf, result_xls])
+        _, result_data = await graphql(SCHEMA, data_data, context_value=request)
+        assert all('url' in result['data']['report'] and
+            result['data']['report']['url']
+            for result in [result_xls, result_data, result_pdf])
+
+    async def test_all_vulns_report(self):
+        query_all_vulns = '''
+            query test {
+                report(reportType: ALL_VULNS) {
+                    url
+                }
+            }
+        '''
+        data_all_vulns = {'query': query_all_vulns}
+        request = create_dummy_session('integratesmanager@gmail.com')
+        request.loaders = {
+            'project': ProjectLoader(),
+        }
+        _, result_all_vulns = await graphql(
+            SCHEMA,
+            data_all_vulns,
+            context_value=request
+        )
+        assert ('url' in result_all_vulns['data']['report']
+            and result_all_vulns['data']['report']['url'])
+
+    async def test_all_users_report(self):
+        query_all_users = '''
+            query test {
+                report(reportType: ALL_USERS) {
+                    url
+                }
+            }
+        '''
+        data_all_users = {'query': query_all_users}
+        request = create_dummy_session('integratesmanager@gmail.com')
+        request.loaders = {
+            'project': ProjectLoader(),
+        }
+        _, result_all_users = await graphql(
+            SCHEMA,
+            data_all_users,
+            context_value=request
+        )
+        assert ('url' in result_all_users['data']['report']
+            and result_all_users['data']['report']['url'])
