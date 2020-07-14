d57278004df7443b015a9c1e308d4e7d6a23d75a
Richard Almanza <rvega@fluidattacks.com>
2020-07-14 17:10:06 -0500

    integrates\style(back): #2263.5 app/middleware
    - Added type definition to functions and parameters
    - Applied style requirements
    
    
    Former-commit-id: 71cfc06ba42fbd17d1939b51ac06cc47a650850d

--- app/middleware.py
+++ app/middleware.py
@@ -1,19 +1,27 @@
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect
 from social_core import exceptions as social_exceptions
 from social_django.middleware import SocialAuthExceptionMiddleware
 
 
-class SocialAuthException(SocialAuthExceptionMiddleware):
-    def process_exception(self, request, exception):
+class SocialAuthException(SocialAuthExceptionMiddleware):  # type: ignore
+    def process_exception(
+        self,
+        request: HttpRequest,
+        exception: social_exceptions.SocialAuthBaseException
+    ) -> HttpResponse:
         if hasattr(social_exceptions, exception.__class__.__name__):
             exception_type = exception.__class__.__name__
 
-            # An already logged in user attempted to access with a different account
+            # An already logged in user attempted to
+            # access with a different account
             if exception_type == 'AuthAlreadyAssociated':
-                return HttpResponse('<script> \
-                    localStorage.setItem("showAlreadyLoggedin","1"); \
-                    location.assign("/integrates/registration");</script>')
-            return redirect("/integrates/index")
-        return super(SocialAuthException, self).process_exception(request,
-                                                                  exception)
+                return HttpResponse(
+                    '<script> '
+                    'localStorage.setItem("showAlreadyLoggedin","1"); '
+                    'location.assign("/integrates/registration");</script>'
+                )
+            return redirect('/integrates/index')
+        return super(SocialAuthException, self).process_exception(
+            request, exception
+        )
