d31c284aa3542e66217c459cc15a891095815e16
Kevin Amado <kamado@fluidattacks.com>
2020-07-14 09:57:59 -0500

    test(back): #2220.25 subscriptions
    Former-commit-id: 838a132b723294527242b1ac3030bcdaa52df51f

---
+++ test_async/dal_test/test_subscription.py
@@ -0,0 +1,87 @@
+# Standard library
+from decimal import Decimal
+
+# Third party libraries
+import pytest
+
+# Local libraries
+from backend.dal.subscriptions import (
+    get_subscriptions_to_entity_report,
+    get_user_subscriptions,
+    subscribe_user_to_entity_report,
+
+)
+
+pytestmark = [
+    pytest.mark.changes_db,
+    pytest.mark.asyncio,
+]
+
+
+@pytest.mark.changes_db
+async def test_update():
+    await subscribe_user_to_entity_report(
+        period=86400,
+        report_entity='test_report_entity',
+        report_subject='test_report_subject',
+        user_email='test_user_email',
+    )
+    await subscribe_user_to_entity_report(
+        period=3600.0,
+        report_entity='test_report_entity',
+        report_subject='test_report_subject2',
+        user_email='test_user_email2',
+    )
+
+    assert await get_subscriptions_to_entity_report(
+        audience='user',
+    ) == [
+        {
+            'period': Decimal('86400'),
+            'pk': {'email': 'test_user_email', 'meta': 'user'},
+            'pk_meta': 'user',
+            'sk': {
+                'entity': 'test_report_entity',
+                'meta': 'entity_report',
+                'subject': 'test_report_subject',
+            },
+            'sk_meta': 'entity_report',
+        },
+        {
+            'period': Decimal('3600'),
+            'pk': {'email': 'test_user_email2', 'meta': 'user'},
+            'pk_meta': 'user',
+            'sk': {
+                'entity': 'test_report_entity',
+                'meta': 'entity_report',
+                'subject': 'test_report_subject2',
+            },
+            'sk_meta': 'entity_report',
+        },
+    ]
+    assert await get_user_subscriptions(
+        user_email='test_user_email',
+    ) == [{
+        'period': Decimal('86400'),
+        'pk': {'email': 'test_user_email', 'meta': 'user'},
+        'pk_meta': 'user',
+        'sk': {
+            'entity': 'test_report_entity',
+            'meta': 'entity_report',
+            'subject': 'test_report_subject',
+        },
+        'sk_meta': 'entity_report',
+    }]
+    assert await get_user_subscriptions(
+        user_email='test_user_email2',
+    ) == [{
+        'period': Decimal('3600'),
+        'pk': {'email': 'test_user_email2', 'meta': 'user'},
+        'pk_meta': 'user',
+        'sk': {
+            'entity': 'test_report_entity',
+            'meta': 'entity_report',
+            'subject': 'test_report_subject2',
+        },
+        'sk_meta': 'entity_report',
+    }]
