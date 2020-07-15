ec9eb75f747f7e0d3706f1d225354fa7dbf33182
Andres Aguirre <aaguirre@fluidattacks.com>
2020-07-15 18:06:03 -0500

    integrates\feat(back): #2282 user view access to releaser
    - Added user view permissions to releaser
    
    
    Former-commit-id: d9fe5fdeb0ae5cded9d87f35c49b15fa7d2c2a2a

--- django-apps/integrates-back-async/backend/authz/model.py
+++ django-apps/integrates-back-async/backend/authz/model.py
@@ -403,9 +403,11 @@ GROUP_LEVEL_ROLES: Dict[str, Dict[str, Set[str]]] = dict(
             'backend_api_resolvers_project__get_drafts',
             'backend_api_resolvers_project__get_events',
             'backend_api_resolvers_project__get_service_attributes',
+            'backend_api_resolvers_project__get_users',
             'backend_api_resolvers_project_resolve_project',
             'backend_api_resolvers_resource__do_download_file',
             'backend_api_resolvers_resource_resolve_resources',
+            'backend_api_resolvers_user_resolve_for_group',
             'backend_api_resolvers_vulnerability__do_approve_vulnerability',
             ('backend_api_resolvers_vulnerability__do_'
                 'request_verification_vuln'),
