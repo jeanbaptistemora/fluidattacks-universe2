bdb53d9145714e5d13f5e55c2c81a2bf0ede6d08
Richard Almanza <rvega@fluidattacks.com>
2020-07-15 10:54:13 -0500

    integrates\style(back): #2263.10 api/resolvers/report
    - Added type definition to functions and parameters
    - Applied style requirements
    
    
    Former-commit-id: ed5ae58577f5670b2dad0e73d9ceb652679647e6

--- django-apps/integrates-back-async/backend/api/resolvers/report.py
+++ django-apps/integrates-back-async/backend/api/resolvers/report.py
@@ -2,14 +2,20 @@
 import asyncio
 import sys
 
+from typing import Any, cast
 import rollbar
-from ariadne import convert_kwargs_to_snake_case, convert_camel_case_to_snake
+from ariadne import (
+    convert_kwargs_to_snake_case,
+    convert_camel_case_to_snake
+)
 from asgiref.sync import sync_to_async
 
 from backend.decorators import require_login, require_project_access
 from backend.domain import (
-    finding as finding_domain, project as project_domain,
-    user as user_domain, vulnerability as vuln_domain
+    finding as finding_domain,
+    project as project_domain,
+    user as user_domain,
+    vulnerability as vuln_domain
 )
 from backend.exceptions import (
     PermissionDenied, RequestedReportError,
@@ -25,24 +31,34 @@ from backend.typing import (
 )
 from backend import authz, util
 from backend.utils import aio
+from graphql.type.definition import GraphQLResolveInfo
 
 
-@convert_kwargs_to_snake_case
+@convert_kwargs_to_snake_case  # type: ignore
 @require_login
-async def resolve_report(_, info, **parameters) -> ReportType:
+async def resolve_report(
+        _: Any,
+        info: GraphQLResolveInfo,
+        **parameters: str) -> ReportType:
     """Resolve report query."""
     return await resolve(info, **parameters)
 
 
-@convert_kwargs_to_snake_case
+@convert_kwargs_to_snake_case  # type: ignore
 @require_login
 @require_project_access
-async def resolve_report_mutation(_, info, **parameters):
+async def resolve_report_mutation(
+        _: Any,
+        info: GraphQLResolveInfo,
+        **parameters: str) -> SimplePayloadType:
     """Resolve reports mutation."""
     return await _do_request_project_report(info, **parameters)
 
 
-async def resolve(info, report_type: str, **parameters) -> ReportType:
+async def resolve(
+        info: GraphQLResolveInfo,
+        report_type: str,
+        **parameters: str) -> ReportType:
     """Async resolve fields."""
     result: ReportType = dict()
     for requested_field in info.field_nodes[0].selection_set.selections:
@@ -55,8 +71,9 @@ async def resolve(info, report_type: str, **parameters) -> ReportType:
         field_params = util.get_field_parameters(requested_field)
         if field_params:
             params.update(field_params)
-        requested_field = \
-            convert_camel_case_to_snake(requested_field.name.value)
+        requested_field = convert_camel_case_to_snake(
+            requested_field.name.value
+        )
         if requested_field.startswith('_'):
             continue
         resolver_func = getattr(
@@ -67,19 +84,23 @@ async def resolve(info, report_type: str, **parameters) -> ReportType:
     return result
 
 
-async def _get_url_complete(info, user_email: str) -> str:
+async def _get_url_complete(info: GraphQLResolveInfo, user_email: str) -> str:
     projects = await user_domain.get_projects(user_email)
     url = await aio.ensure_io_bound(
-        report.generate_complete_report, user_email, projects
+        report.generate_complete_report,
+        user_email, projects
     )
     util.cloudwatch_log(
         info.context,
         f'Security: Complete report succesfully requested by {user_email}'
     )
-    return url
+    return cast(str, url)
 
 
-async def _get_url_all_vulns(info, user_email: str, project_name: str) -> str:
+async def _get_url_all_vulns(
+        info: GraphQLResolveInfo,
+        user_email: str,
+        project_name: str) -> str:
     if authz.get_user_level_role(user_email) == 'admin':
         url = await aio.ensure_io_bound(
             report.generate_all_vulns_report,
@@ -99,11 +120,11 @@ async def _get_url_all_vulns(info, user_email: str, project_name: str) -> str:
         )
         rollbar.report_message(msg, 'error')
         raise PermissionDenied()
-    return url
+    return cast(str, url)
 
 
 async def _get_url_group_report(
-    info,
+    info: GraphQLResolveInfo,
     report_type: str,
     user_email: str,
     project_name: str,
@@ -137,7 +158,9 @@ async def _get_url_group_report(
     return url
 
 
-async def _get_url_all_users(info, user_email: str) -> str:
+async def _get_url_all_users(
+        info: GraphQLResolveInfo,
+        user_email: str) -> str:
     if authz.get_user_level_role(user_email) == 'admin':
         url = await sync_to_async(
             report.generate_all_users_report)(
@@ -156,10 +179,13 @@ async def _get_url_all_users(info, user_email: str) -> str:
         )
         rollbar.report_message(msg, 'error')
         raise PermissionDenied()
-    return url
+    return cast(str, url)
 
 
-async def _get_url(info, report_type: str, **parameters) -> str:
+async def _get_url(
+        info: GraphQLResolveInfo,
+        report_type: str,
+        **parameters: str) -> str:
     url = ''
     user_info = util.get_jwt_content(info.context)
     user_email = user_info['user_email']
@@ -184,25 +210,30 @@ async def _get_url(info, report_type: str, **parameters) -> str:
     return url
 
 
-async def _do_request_project_report(info, **parameters) -> SimplePayloadType:
+async def _do_request_project_report(
+        info: GraphQLResolveInfo,
+        **parameters: str) -> SimplePayloadType:
     success = False
     project_name = parameters.get('project_name', '')
     project_name = project_name.lower()
     report_type = parameters.get('report_type')
     user_info = util.get_jwt_content(info.context)
     user_email = user_info['user_email']
-    project_findings = \
-        await info.context.loaders['project'].load(project_name)
+    project_findings = await info.context.loaders['project'].load(project_name)
     project_findings = project_findings['findings']
     findings = await finding_domain.get_findings_async(project_findings)
     findings = [
-        await sync_to_async(finding_domain.cast_new_vulnerabilities)
-        (await vuln_domain.get_open_vuln_by_type(
-            str(finding['findingId']), info.context), finding)
-        for finding in findings]
-    description = \
-        await sync_to_async(
-            project_domain.get_description)(project_name.lower())
+        await sync_to_async(finding_domain.cast_new_vulnerabilities)(
+            await vuln_domain.get_open_vuln_by_type(
+                str(finding['findingId']), info.context
+            ),
+            finding
+        )
+        for finding in findings
+    ]
+    description = await sync_to_async(project_domain.get_description)(
+        project_name.lower()
+    )
 
     findings_ord = util.ord_asc_by_criticality(findings)
     if report_type == 'PDF':
@@ -218,7 +249,8 @@ async def _do_request_project_report(info, **parameters) -> SimplePayloadType:
         success = True
         util.cloudwatch_log(
             info.context,
-            'Security: PDF report successfully requested')
+            'Security: PDF report successfully requested'
+        )
     elif report_type == 'XLS':
         asyncio.create_task(
             sync_to_async(technical_report.generate_xls)(
@@ -230,7 +262,8 @@ async def _do_request_project_report(info, **parameters) -> SimplePayloadType:
         success = True
         util.cloudwatch_log(
             info.context,
-            'Security: XLS report successfully requested')
+            'Security: XLS report successfully requested'
+        )
     elif report_type == 'DATA':
         asyncio.create_task(
             sync_to_async(data_report.generate)(
@@ -243,6 +276,7 @@ async def _do_request_project_report(info, **parameters) -> SimplePayloadType:
         success = True
         util.cloudwatch_log(
             info.context,
-            'Security: DATA report successfully requested')
+            'Security: DATA report successfully requested'
+        )
 
     return SimplePayloadType(success=success)
