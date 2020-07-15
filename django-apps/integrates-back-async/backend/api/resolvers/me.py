a8ba2cfccd047c11479011e04a94ac1c9fea452d
Richard Almanza <rvega@fluidattacks.com>
2020-07-14 22:49:02 -0500

    integrates\style(back): #2263.8 api/resolvers/me
    - Added type definition to functions and parameters
    - Applied style requirements
    
    
    Former-commit-id: fea6160558a7c2f883c29e81c9e100624cf017ff

--- django-apps/integrates-back-async/backend/api/resolvers/me.py
+++ django-apps/integrates-back-async/backend/api/resolvers/me.py
@@ -3,10 +3,13 @@ import json
 import sys
 from datetime import datetime, timedelta
 from collections import defaultdict
-from typing import Dict, List, Set
+from typing import Dict, List, Set, Any, cast
 
 import rollbar
-from ariadne import convert_kwargs_to_snake_case, convert_camel_case_to_snake
+from ariadne import (
+    convert_kwargs_to_snake_case,
+    convert_camel_case_to_snake
+)
 from asgiref.sync import sync_to_async
 from django.conf import settings
 from jose import jwt
@@ -14,6 +17,7 @@ from mixpanel import Mixpanel
 from social_core.exceptions import AuthException
 from social_django.utils import load_strategy
 from social_django.utils import load_backend
+from graphql.type.definition import GraphQLResolveInfo
 
 from backend.api.resolvers import (
     organization as organization_resolver,
@@ -22,7 +26,10 @@ from backend.api.resolvers import (
 from backend.dal.organization import (
     get_ids_for_user as get_user_organizations,
 )
-from backend.decorators import require_login, enforce_user_level_auth_async
+from backend.decorators import (
+    require_login,
+    enforce_user_level_auth_async
+)
 from backend.domain import (
     subscriptions as subscriptions_domain,
     tag as tag_domain,
@@ -45,7 +52,7 @@ from backend import authz
 
 
 async def _get_role(
-        _,
+        _: GraphQLResolveInfo,
         user_email: str,
         entity: str = 'USER',
         identifier: str = '',
@@ -63,20 +70,25 @@ async def _get_role(
         )
     else:
         role = await sync_to_async(authz.get_user_level_role)(user_email)
-    return role
+    return cast(str, role)
 
 
-async def _get_organizations(info, user_email: str) -> List[OrganizationType]:
+async def _get_organizations(
+        info: GraphQLResolveInfo,
+        user_email: str) -> List[OrganizationType]:
     organization_ids = await get_user_organizations(user_email)
     organizations = await aio.materialize(
         organization_resolver.resolve(
-            info, organization_id, as_field=True)
+            info, organization_id, as_field=True
+        )
         for organization_id in organization_ids
     )
-    return organizations
+    return cast(List[OrganizationType], organizations)
 
 
-async def _get_projects(info, user_email: str) -> List[ProjectType]:
+async def _get_projects(
+        info: GraphQLResolveInfo,
+        user_email: str) -> List[ProjectType]:
     """Get projects."""
     project_names = await user_domain.get_projects(user_email)
     projects = await asyncio.gather(*[
@@ -88,27 +100,29 @@ async def _get_projects(info, user_email: str) -> List[ProjectType]:
     return projects
 
 
-async def _get_access_token(_, user_email: str) -> str:
+async def _get_access_token(_: GraphQLResolveInfo, user_email: str) -> str:
     """Get access token."""
     access_token = await sync_to_async(user_domain.get_data)(
         user_email, 'access_token')
     access_token_dict = {
         'hasAccessToken': bool(access_token),
         'issuedAt': str(access_token.get('iat', ''))
-        if isinstance(access_token, dict) else ''
+        if isinstance(access_token, dict)
+        else ''
     }
     return json.dumps(access_token_dict)
 
 
-async def _get_remember(_, user_email: str) -> bool:
+async def _get_remember(_: GraphQLResolveInfo, user_email: str) -> bool:
     """Get remember preference."""
-    remember = await \
-        sync_to_async(user_domain.get_data)(user_email, 'legal_remember')
+    remember = await sync_to_async(user_domain.get_data)(
+        user_email, 'legal_remember'
+    )
     return bool(remember)
 
 
 async def _get_permissions(
-        _,
+        _: GraphQLResolveInfo,
         user_email: str,
         entity: str = 'USER',
         identifier: str = '',
@@ -131,15 +145,16 @@ async def _get_permissions(
 
 
 @enforce_user_level_auth_async
-async def _get_tags(info, user_email: str) -> List[TagType]:
+async def _get_tags(
+        info: GraphQLResolveInfo,
+        user_email: str) -> List[TagType]:
     """Get tags."""
     projects = await user_domain.get_projects(
         user_email, access_pending_projects=False)
-    tags_dict: Dict[str, List] = defaultdict(list)
+    tags_dict: Dict[str, List[Dict[str, str]]] = defaultdict(list)
     organization: str = '-'
     if projects:
-        project_attrs = \
-            await info.context.loaders['project'].load(projects[0])
+        project_attrs = await info.context.loaders['project'].load(projects[0])
         organization = project_attrs['attrs'].get('companies', ['-'])[0]
     projects_filtered: List[str] = []
     for project in projects:
@@ -152,23 +167,30 @@ async def _get_tags(info, user_email: str) -> List[TagType]:
                 tags_dict[tag].append(dict(name=project))
     tags = []
     allowed_tags = await tag_domain.filter_allowed_tags(
-        organization, projects_filtered)
+        organization, projects_filtered
+    )
     for tag, projects in tags_dict.items():
         tags.append(dict(name=tag, projects=projects))
-    tags = [tag for tag in tags if tag.get('name', '') in allowed_tags]
+    tags = [
+        tag
+        for tag in tags
+        if tag.get('name', '') in allowed_tags
+    ]
     return tags
 
 
-async def _get_caller_origin(info, **_) -> str:
+async def _get_caller_origin(
+        info: GraphQLResolveInfo,
+        **_: Dict[Any, Any]) -> str:
     """Get caller_origin."""
     if hasattr(info.context, 'caller_origin'):
         origin = info.context.caller_origin
     else:
         origin = 'API'
-    return origin
+    return cast(str, origin)
 
 
-async def _resolve_fields(info) -> MeType:
+async def _resolve_fields(info: GraphQLResolveInfo) -> MeType:
     """Async resolve fields."""
     result: MeType = dict()
 
@@ -180,11 +202,13 @@ async def _resolve_fields(info) -> MeType:
             'user_email': jwt_content.get('user_email')
         }
         field_params = util.get_field_parameters(
-            requested_field, info.variable_values)
+            requested_field, info.variable_values
+        )
         if field_params:
             params.update(field_params)
-        requested_field = \
-            convert_camel_case_to_snake(requested_field.name.value)
+        requested_field = convert_camel_case_to_snake(
+            requested_field.name.value
+        )
         if requested_field.startswith('_'):
             continue
         resolver_func = getattr(
@@ -195,9 +219,12 @@ async def _resolve_fields(info) -> MeType:
     return result
 
 
-@convert_kwargs_to_snake_case
+@convert_kwargs_to_snake_case  # type: ignore
 @require_login
-async def resolve_me(_, info, caller_origin: str = '') -> MeType:
+async def resolve_me(
+        _: Any,
+        info: GraphQLResolveInfo,
+        caller_origin: str = '') -> MeType:
     """Resolve Me query."""
     jwt_content = util.get_jwt_content(info.context)
     user_email = jwt_content.get('user_email')
@@ -207,12 +234,16 @@ async def resolve_me(_, info, caller_origin: str = '') -> MeType:
     util.cloudwatch_log(
         info.context,
         f'Security: User {user_email} is accessing '
-        f'Integrates using {origin}')  # pragma: no cover
+        f'Integrates using {origin}'  # pragma: no cover
+    )
     return await _resolve_fields(info)
 
 
-@convert_kwargs_to_snake_case
-async def resolve_me_mutation(obj, info, **parameters):
+@convert_kwargs_to_snake_case  # type: ignore
+async def resolve_me_mutation(
+        obj,
+        info: GraphQLResolveInfo,
+        **parameters: Dict[str, str]) -> Any:
     """Wrap me mutations."""
     field = util.camelcase_to_snakecase(info.field_name)
     resolver_func = getattr(sys.modules[__name__], f'_do_{field}')
@@ -220,7 +251,8 @@ async def resolve_me_mutation(obj, info, **parameters):
 
 
 async def _do_subscribe_to_entity_report(
-    _, info,
+    _: Any,
+    info: GraphQLResolveInfo,
     frequency: str,
     report_entity: str,
     report_subject: str,
@@ -264,7 +296,10 @@ async def _do_subscribe_to_entity_report(
 
 
 async def _do_sign_in(
-        _, info, auth_token: str, provider: str) -> SignInPayloadType:
+        _: Any,
+        info: GraphQLResolveInfo,
+        auth_token: str,
+        provider: str) -> SignInPayloadType:
     """ Sign in with an OAuth2 access token """
     session_jwt = ''
     success = False
@@ -288,17 +323,22 @@ async def _do_sign_in(
             key=settings.JWT_SECRET,
         )
         mp_obj = Mixpanel(settings.MIXPANEL_API_TOKEN)
-        await sync_to_async(mp_obj.track)(email, 'MobileAuth', {
-            'email': email,
-            'provider': provider
-        })
+        await sync_to_async(mp_obj.track)(
+            email,
+            'MobileAuth',
+            {
+                'email': email,
+                'provider': provider
+            }
+        )
         success = True
     except AuthException as ex:
         rollbar.report_message(
             f'Couldn\'t perform social auth with {provider}',
             level='error',
             extra_data=ex,
-            payload_data=locals())
+            payload_data=locals()
+        )
 
     return SignInPayloadType(
         session_jwt=session_jwt,
@@ -308,7 +348,9 @@ async def _do_sign_in(
 
 @require_login
 async def _do_update_access_token(
-        _, info, expiration_time: int) -> UpdateAccessTokenPayloadType:
+        _: Any,
+        info: GraphQLResolveInfo,
+        expiration_time: int) -> UpdateAccessTokenPayloadType:
     """Resolve update_access_token mutation."""
     user_info = util.get_jwt_content(info.context)
     email = user_info['user_email']
@@ -331,47 +373,66 @@ async def _do_update_access_token(
             key=settings.JWT_SECRET_API
         )
 
-        success = await \
-            sync_to_async(user_domain.update_access_token)(email, token_data)
+        success = await sync_to_async(user_domain.update_access_token)(
+            email, token_data
+        )
         if success:
             util.cloudwatch_log(
-                info.context, '{email} update access token'.format(
-                    email=user_info['user_email']))  # pragma: no cover
+                info.context,
+                (f'{user_info["user_email"]} '  # pragma: no cover
+                 'update access token')
+            )
         else:
             util.cloudwatch_log(
-                info.context, '{email} attempted to update access token'
-                .format(email=user_info['user_email']))  # pragma: no cover
+                info.context,
+                (f'{user_info["user_email"]} '  # pragma: no cover
+                 'attempted to update access token')
+            )
     else:
         util.cloudwatch_log(
-            info.context, '{email} attempted to use expiration time \
-            greater than six months or minor than current time'
-            .format(email=user_info['user_email']))  # pragma: no cover
+            info.context,
+            (f'{user_info["user_email"]} '  # pragma: no cover
+             'attempted to use expiration time '
+             'greater than six months or minor '
+             'than current time')
+        )
         raise InvalidExpirationTime()
 
-    return UpdateAccessTokenPayloadType(success=success,
-                                        session_jwt=session_jwt)
+    return UpdateAccessTokenPayloadType(
+        success=success,
+        session_jwt=session_jwt
+    )
 
 
 @require_login
-async def _do_invalidate_access_token(_, info) -> SimplePayloadType:
+async def _do_invalidate_access_token(
+        _: Any,
+        info: GraphQLResolveInfo) -> SimplePayloadType:
     """Resolve invalidate_access_token mutation."""
     user_info = util.get_jwt_content(info.context)
 
-    success = await \
-        sync_to_async(user_domain.remove_access_token)(user_info['user_email'])
+    success = await sync_to_async(user_domain.remove_access_token)(
+        user_info['user_email']
+    )
     if success:
         util.cloudwatch_log(
-            info.context, '{email} invalidate access token'.format(
-                email=user_info['user_email']))  # pragma: no cover
+            info.context,
+            (f'{user_info["user_email"]} '  # pragma: no cover
+             'invalidate access token')
+        )
     else:
         util.cloudwatch_log(
-            info.context, '{email} attempted to invalidate access token'
-            .format(email=user_info['user_email']))  # pragma: no cover
+            info.context,
+            (f'{user_info["user_email"]} '  # pragma: no cover
+             'attempted to invalidate access token')
+        )
     return SimplePayloadType(success=success)
 
 
-async def _do_accept_legal(_, info,
-                           remember: bool = False) -> SimplePayloadType:
+async def _do_accept_legal(
+        _: Any,
+        info: GraphQLResolveInfo,
+        remember: bool = False) -> SimplePayloadType:
     """Resolve accept_legal mutation."""
     user_email = util.get_jwt_content(info.context)['user_email']
 
