334234fa71fda5e90585c99dff18704e8a11ea0f
Kevin Amado <kamado@fluidattacks.com>
2020-07-16 11:21:13 -0500

    integrates\feat(back): #2220.35 new event firing system
    - This trigger the event at specific hours
    
    Closes #2285
    
    
    Former-commit-id: b6eb9e4d9e47c4d268fda2a010ef16466459ba65

--- django-apps/integrates-back-async/backend/domain/subscriptions.py
+++ django-apps/integrates-back-async/backend/domain/subscriptions.py
@@ -79,6 +79,41 @@ def is_subscription_active_right_now(
     return bot_period * (bot_time // bot_period) % event_period < 1
 
 
+def should_process_event(
+    *,
+    bot_time: Optional[datetime] = None,
+    event_frequency: str,
+) -> bool:
+    """Given a job that runs every hour, should it process a periodic event?.
+
+    Such a bot should not run less or more often than the minimum event
+    frequency to trigger.
+    """
+    bot_time = bot_time or datetime.utcnow()
+    bot_time = bot_time.replace(minute=0)
+
+    event_frequency = event_frequency.lower()
+
+    return (
+        # Firth of month @ 10 GMT
+        event_frequency == 'monthly'
+        and bot_time.hour == 10
+        and bot_time.day == 1
+    ) or (
+        # Mondays @ 10 GMT
+        event_frequency == 'weekly'
+        and bot_time.hour == 10
+        and bot_time.weekday() == 0
+    ) or (
+        # Any day @ 10 GMT
+        event_frequency == 'daily'
+        and bot_time.hour == 10
+    ) or (
+        # @ any hour
+        event_frequency == 'hourly'
+    )
+
+
 async def get_user_subscriptions(
     *,
     user_email: str,
@@ -196,9 +231,8 @@ async def trigger_user_to_entity_report():
 
         if all((
             # This is expected to run every hour
-            is_subscription_active_right_now(
-                bot_period=3600,
-                event_period=event_period,
+            should_process_event(
+                event_frequency=event_frequency,
             ),
             # A user may be subscribed but now he does not have access to the
             #   group or organization, so let's ignore them!
