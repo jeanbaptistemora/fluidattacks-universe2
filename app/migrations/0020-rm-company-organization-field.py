c898a20a562c2a544e282ae0662ffc01a7e5d9e7
Andres Cuberos <acuberos@fluidattacks.com>
2020-07-16 10:55:53 -0500

    integrates\refac(back): #2256.12 rm organization info db
    - Remove *company/organization* info from users and groups.
    
    
    Former-commit-id: f4b2812fc2094538d8e78c3a0c1d885cce90fae1

---
+++ app/migrations/0020-rm-company-organization-field.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+#-.- coding: utf-8 -.-
+"""
+This migration deletes the fields company/organization from projects
+and users, since now the organization table is used for those relations.
+
+Execution Time:    2020-07-16 10:40:00 UTC-5
+Finalization Time: 2020-07-16 10:53:00 UTC-5
+"""
+import asyncio
+import os
+
+import rollbar
+from boto3.dynamodb.conditions import Attr
+from more_itertools import chunked
+
+from backend.dal import (
+    project as group_dal,
+    user as user_dal
+)
+from backend.utils import aio
+
+
+STAGE: str = os.environ['STAGE']
+
+
+async def main() -> None:
+    group_filter: Attr = (
+        Attr('organization').exists() |
+        Attr('companies').exists()
+    )
+    groups = await aio.ensure_io_bound(
+        group_dal.get_all, group_filter, 'project_name'
+    )
+    user_filter: Attr = (
+        Attr('organization').exists() |
+        Attr('company').exists()
+    )
+    users = await aio.ensure_io_bound(user_dal.get_all, user_filter, 'email')
+
+
+    if STAGE == 'test':
+        print('-----\n')
+        for user in users:
+            print(f'User {user["email"]} will be updated')
+        print('-----\n')
+        for group in groups:
+            print(f'Group {group["project_name"]} will be updated')
+    else:
+        for group_chunk in chunked(groups, 40):
+            await aio.materialize(
+                aio.ensure_io_bound(
+                    group_dal.update,
+                    group['project_name'],
+                    {'companies': None, 'organization': None}
+                )
+                for group in group_chunk
+        )
+
+        for user_chunk in chunked(users, 40):
+            await aio.materialize(
+                aio.ensure_io_bound(
+                    user_dal.update,
+                    user['email'],
+                    {'company': None, 'organization': None}
+                )
+                for user in user_chunk
+            )
+
+
+async def log(message: str) -> None:
+    print(message)
+    if STAGE != 'test':
+        await aio.ensure_io_bound(
+            rollbar.report_message,
+            message,
+            level='debug'
+        )
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
