4de24ec6835bd7361530e45a991970240c07445e
Kevin Amado <kamado@fluidattacks.com>
2020-07-17 14:01:36 -0500

    skims\feat(build): #2294.4 linters and sub pkgs
    - Fix a typo and build system linter errors
    
    
    Former-commit-id: c1bd38ffb4c5292dc0db4618570ec4af886aa74d

--- build/include/jobs/skims.sh
+++ build/include/jobs/skims.sh
@@ -15,14 +15,16 @@ function job_skims_lint {
       pushd skims/ \
     &&  { test -e poetry.lock || poetry install; } \
     &&  for pkg in \
-          src/skims \
+          src/apis \
+          src/cli \
+          src/core \
 
         do
               echo "[INFO] Linting: ${pkg}" \
-          &&  poetry run mypy "${mypy_args[@]}" "${pkg}" \
+          &&  poetry run mypy "${args_mypy[@]}" "${pkg}" \
           &&  poetry run prospector "${args_prospector[@]}" "${pkg}" \
           ||  return 1
         done \
   &&  popd \
-
+  ||  return 1
 }
