573360d6343499b1791d1583fe8cd0fc90ac6d9e
Richard Almanza <rvega@fluidattacks.com>
2020-07-23 14:45:09 -0500

    integrates\style(back): #2263.25 api/resolvers/finding
    - Added type definition to functions and parameters
    - Applied style requirements
    
    
    Former-commit-id: 8fe3b53bd923902a61cbe41c74d9b5b69989b533

--- django-apps/integrates-back-async/backend/api/resolvers/finding.py
+++ django-apps/integrates-back-async/backend/api/resolvers/finding.py
@@ -202,7 +202,7 @@ async def _get_cvss_version(info, identifier: str) -> str:
 
 
 @get_entity_cache_async
-async def _get_exploit(info, identifier: str) -> str:
+async def _get_exploit(info: GraphQLResolveInfo, identifier: str) -> str:
     """Get exploit."""
     finding = await info.context.loaders['finding'].load(identifier)
     if finding['exploit']['url']:
@@ -217,14 +217,18 @@ async def _get_exploit(info, identifier: str) -> str:
 
 
 @get_entity_cache_async
-async def _get_evidence(info, identifier: str) -> Dict[str, Dict[str, str]]:
+async def _get_evidence(
+        info: GraphQLResolveInfo,
+        identifier: str) -> Dict[str, Dict[str, str]]:
     """Get evidence."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['evidence']
+    return cast(Dict[str, Dict[str, str]], finding['evidence'])
 
 
 @get_entity_cache_async
-async def _get_comments(info, identifier: str) -> List[CommentType]:
+async def _get_comments(
+        info: GraphQLResolveInfo,
+        identifier: str) -> List[CommentType]:
     """Get comments."""
     finding = await info.context.loaders['finding'].load(identifier)
     finding_id = finding['id']
@@ -243,7 +247,9 @@ async def _get_comments(info, identifier: str) -> List[CommentType]:
 @require_integrates
 @rename_kwargs({'finding_id': 'identifier'})
 @get_entity_cache_async
-async def _get_observations(info, identifier: str) -> List[CommentType]:
+async def _get_observations(
+        info: GraphQLResolveInfo,
+        identifier: str) -> List[CommentType]:
     """Get observations."""
     finding = await info.context.loaders['finding'].load(identifier)
     finding_id = finding['id']
@@ -257,23 +263,31 @@ async def _get_observations(info, identifier: str) -> List[CommentType]:
 
 
 @get_entity_cache_async
-async def _get_state(info, identifier: str) -> str:
+async def _get_state(info: GraphQLResolveInfo, identifier: str) -> str:
     """Get state."""
     vulns = await info.context.loaders['vulnerability'].load(identifier)
 
-    state = 'open' \
-        if [vuln for vuln in vulns
-            if vuln['last_approved_status'] == 'open'] \
+    state = (
+        'open'
+        if [
+            vuln
+            for vuln in vulns
+            if vuln['last_approved_status'] == 'open'
+        ]
         else 'closed'
+    )
     return state
 
 
 @get_entity_cache_async
-async def _get_last_vulnerability(info, identifier: str) -> int:
+async def _get_last_vulnerability(
+        info: GraphQLResolveInfo,
+        identifier: str) -> int:
     """Get last_vulnerability."""
     finding = await info.context.loaders['finding'].load(identifier)
-    last_vuln_date = \
-        util.calculate_datediff_since(finding['last_vulnerability'])
+    last_vuln_date = util.calculate_datediff_since(
+        finding['last_vulnerability']
+    )
     return last_vuln_date.days
 
 
@@ -282,150 +296,168 @@ async def _get_last_vulnerability(info, identifier: str) -> int:
 @require_integrates
 @rename_kwargs({'finding_id': 'identifier'})
 @get_entity_cache_async
-async def _get_historic_state(info, identifier: str) -> List[Dict[str, str]]:
+async def _get_historic_state(
+        info: GraphQLResolveInfo,
+        identifier: str) -> List[Dict[str, str]]:
     """Get historic_state."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['historic_state']
+    return cast(List[Dict[str, str]], finding['historic_state'])
 
 
 @get_entity_cache_async
-async def _get_title(info, identifier: str) -> str:
+async def _get_title(info: GraphQLResolveInfo, identifier: str) -> str:
     """Get title."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['title']
+    return cast(str, finding['title'])
 
 
 @get_entity_cache_async
-async def _get_scenario(info, identifier: str) -> str:
+async def _get_scenario(info: GraphQLResolveInfo, identifier: str) -> str:
     """Get scenario."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['scenario']
+    return cast(str, finding['scenario'])
 
 
 @get_entity_cache_async
-async def _get_actor(info, identifier: str) -> str:
+async def _get_actor(info: GraphQLResolveInfo, identifier: str) -> str:
     """Get actor."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['actor']
+    return cast(str, finding['actor'])
 
 
 @get_entity_cache_async
-async def _get_description(info, identifier: str) -> str:
+async def _get_description(info: GraphQLResolveInfo, identifier: str) -> str:
     """Get description."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['description']
+    return cast(str, finding['description'])
 
 
 @get_entity_cache_async
-async def _get_requirements(info, identifier: str) -> str:
+async def _get_requirements(info: GraphQLResolveInfo, identifier: str) -> str:
     """Get requirements."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['requirements']
+    return cast(str, finding['requirements'])
 
 
 @get_entity_cache_async
-async def _get_attack_vector_desc(info, identifier: str) -> str:
+async def _get_attack_vector_desc(
+        info: GraphQLResolveInfo,
+        identifier: str) -> str:
     """Get attack_vector_desc."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['attack_vector_desc']
+    return cast(str, finding['attack_vector_desc'])
 
 
 @get_entity_cache_async
-async def _get_threat(info, identifier: str) -> str:
+async def _get_threat(
+        info: GraphQLResolveInfo,
+        identifier: str) -> str:
     """Get threat."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['threat']
+    return cast(str, finding['threat'])
 
 
 @get_entity_cache_async
-async def _get_recommendation(info, identifier: str) -> str:
+async def _get_recommendation(
+        info: GraphQLResolveInfo,
+        identifier: str) -> str:
     """Get recommendation."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['recommendation']
+    return cast(str, finding['recommendation'])
 
 
 @get_entity_cache_async
-async def _get_affected_systems(info, identifier: str) -> str:
+async def _get_affected_systems(
+        info: GraphQLResolveInfo,
+        identifier: str) -> str:
     """Get affected_systems."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['affected_systems']
+    return cast(str, finding['affected_systems'])
 
 
 @get_entity_cache_async
-async def _get_compromised_attributes(info, identifier: str) -> str:
+async def _get_compromised_attributes(
+        info: GraphQLResolveInfo,
+        identifier: str) -> str:
     """Get compromised_attributes."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['compromised_attributes']
+    return cast(str, finding['compromised_attributes'])
 
 
 @get_entity_cache_async
-async def _get_compromised_records(info, identifier: str) -> int:
+async def _get_compromised_records(
+        info: GraphQLResolveInfo,
+        identifier: str) -> int:
     """Get compromised_records."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['compromised_records']
+    return cast(int, finding['compromised_records'])
 
 
 @get_entity_cache_async
-async def _get_cwe_url(info, identifier: str) -> str:
+async def _get_cwe_url(info: GraphQLResolveInfo, identifier: str) -> str:
     """Get cwe_url."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['cwe_url']
+    return cast(str, finding['cwe_url'])
 
 
 @get_entity_cache_async
-async def _get_bts_url(info, identifier: str) -> str:
+async def _get_bts_url(info: GraphQLResolveInfo, identifier: str) -> str:
     """Get bts_url."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['bts_url']
+    return cast(str, finding['bts_url'])
 
 
 @get_entity_cache_async
-async def _get_risk(info, identifier: str) -> str:
+async def _get_risk(info: GraphQLResolveInfo, identifier: str) -> str:
     """Get risk."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['risk']
+    return cast(str, finding['risk'])
 
 
 @get_entity_cache_async
-async def _get_remediated(info, identifier: str) -> bool:
+async def _get_remediated(info: GraphQLResolveInfo, identifier: str) -> bool:
     """Get remediated."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['remediated']
+    return cast(bool, finding['remediated'])
 
 
 @get_entity_cache_async
-async def _get_type(info, identifier: str) -> str:
+async def _get_type(info: GraphQLResolveInfo, identifier: str) -> str:
     """Get type."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['type']
+    return cast(str, finding['type'])
 
 
 @get_entity_cache_async
-async def _get_age(info, identifier: str) -> int:
+async def _get_age(info: GraphQLResolveInfo, identifier: str) -> int:
     """Get age."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['age']
+    return cast(int, finding['age'])
 
 
 @get_entity_cache_async
-async def _get_is_exploitable(info, identifier: str) -> bool:
+async def _get_is_exploitable(
+        info: GraphQLResolveInfo,
+        identifier: str) -> bool:
     """Get is_exploitable."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['is_exploitable']
+    return cast(bool, finding['is_exploitable'])
 
 
 @get_entity_cache_async
-async def _get_severity_score(info, identifier: str) -> float:
+async def _get_severity_score(
+        info: GraphQLResolveInfo,
+        identifier: str) -> float:
     """Get severity_score."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['severity_score']
+    return cast(float, finding['severity_score'])
 
 
 @get_entity_cache_async
-async def _get_report_date(info, identifier: str) -> str:
+async def _get_report_date(info: GraphQLResolveInfo, identifier: str) -> str:
     """Get report_date."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['report_date']
+    return cast(str, finding['report_date'])
 
 
 @rename_kwargs({'identifier': 'finding_id'})
@@ -433,58 +465,70 @@ async def _get_report_date(info, identifier: str) -> str:
 @require_integrates
 @rename_kwargs({'finding_id': 'identifier'})
 @get_entity_cache_async
-async def _get_analyst(info, identifier: str) -> str:
+async def _get_analyst(info: GraphQLResolveInfo, identifier: str) -> str:
     """Get analyst."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['analyst']
+    return cast(str, finding['analyst'])
 
 
 @get_entity_cache_async
-async def _get_historic_treatment(info,
-                                  identifier: str) -> List[Dict[str, str]]:
+async def _get_historic_treatment(
+        info: GraphQLResolveInfo,
+        identifier: str) -> List[Dict[str, str]]:
     """Get historic_treatment."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['historic_treatment']
+    return cast(List[Dict[str, str]], finding['historic_treatment'])
 
 
 @get_entity_cache_async
-async def _get_current_state(info, identifier: str) -> str:
+async def _get_current_state(info: GraphQLResolveInfo, identifier: str) -> str:
     """Get current_state."""
     finding = await info.context.loaders['finding'].load(identifier)
-    return finding['current_state']
+    return cast(str, finding['current_state'])
 
 
 @get_entity_cache_async
-async def _get_new_remediated(info, identifier: str) -> bool:
+async def _get_new_remediated(
+        info: GraphQLResolveInfo,
+        identifier: str) -> bool:
     """Get new_remediated."""
     vulns = await info.context.loaders['vulnerability'].load(identifier)
-    open_vulns = [vuln for vuln in vulns
-                  if vuln['last_approved_status'] == 'open']
+    open_vulns = [
+        vuln
+        for vuln in vulns
+        if vuln['last_approved_status'] == 'open'
+    ]
     remediated_vulns = [vuln for vuln in vulns if vuln['remediated']]
     new_remediated = len(remediated_vulns) == len(open_vulns)
     return new_remediated
 
 
 @get_entity_cache_async
-async def _get_verified(info, identifier: str) -> bool:
+async def _get_verified(info: GraphQLResolveInfo, identifier: str) -> bool:
     """Get verified."""
     vulns = await info.context.loaders['vulnerability'].load(identifier)
-    remediated_vulns = \
-        [vuln for vuln in vulns
-         if vuln['last_approved_status'] == 'open' and vuln['remediated']]
+    remediated_vulns = [
+        vuln
+        for vuln in vulns
+        if vuln['last_approved_status'] == 'open' and vuln['remediated']
+    ]
     verified = len(remediated_vulns) == 0
     return verified
 
 
 async def resolve(
-        info, identifier: str, as_field: bool = False,
-        selection_set: SelectionSetNode = SelectionSetNode()) -> \
-        Dict[str, FindingType]:
+    info: GraphQLResolveInfo,
+    identifier: str,
+    as_field: bool = False,
+    selection_set: SelectionSetNode = SelectionSetNode()
+) -> Dict[str, FindingType]:
     """Async resolve fields."""
     result = dict()
-    requested_fields = \
-        selection_set.selections if as_field else \
-        info.field_nodes[0].selection_set.selections
+    requested_fields = (
+        selection_set.selections
+        if as_field
+        else info.field_nodes[0].selection_set.selections
+    )
 
     for requested_field in requested_fields:
         if util.is_skippable(info, requested_field):
@@ -495,8 +539,9 @@ async def resolve(
         field_params = util.get_field_parameters(requested_field)
         if field_params:
             params.update(field_params)
-        requested_field = \
-            convert_camel_case_to_snake(requested_field.name.value)
+        requested_field = convert_camel_case_to_snake(
+            requested_field.name.value
+        )
         if requested_field.startswith('_'):
             continue
         resolver_func = getattr(
@@ -715,8 +760,7 @@ async def _do_add_finding_comment(
             'user_id': comment_id,
             'comment_type': param_type,
             'content': parameters.get('content'),
-            'fullname': str.join(
-                ' ',
+            'fullname': ''.join(
                 [user_data['first_name'], user_data['last_name']]
             ),
             'parent': parameters.get('parent'),
