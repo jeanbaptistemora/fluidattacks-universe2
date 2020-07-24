2287299c3744ca77897274ad94809912b5d31d04
Richard Almanza <rvega@fluidattacks.com>
2020-07-23 19:50:10 -0500

    integrates\style(back): #2263.27 api/resolvers/resource
    - Added type definition to functions and parameters
    - Applied style requirements
    
    
    Former-commit-id: 4fbba58475a031f3dee9d16e6f4cfac6212c321a

--- django-apps/integrates-back-async/backend/api/resolvers/resource.py
+++ django-apps/integrates-back-async/backend/api/resolvers/resource.py
@@ -6,6 +6,7 @@ from ariadne import convert_kwargs_to_snake_case, convert_camel_case_to_snake
 from asgiref.sync import sync_to_async
 from django.conf import settings
 from mixpanel import Mixpanel
+from graphql.type.definition import GraphQLResolveInfo
 
 from backend.decorators import (
     require_login,
@@ -181,7 +182,10 @@ async def _do_add_environments(_, info, envs: List[Dict[str, str]],
 @require_login
 @enforce_group_level_auth_async
 @require_integrates
-async def _do_add_files(_, info, **parameters) -> SimplePayloadType:
+async def _do_add_files(
+        _: Any,
+        info: GraphQLResolveInfo,
+        **parameters: Any) -> SimplePayloadType:
     """Resolve add_files mutation."""
     success = False
     files_data = parameters['files_data']
@@ -192,46 +196,58 @@ async def _do_add_files(_, info, **parameters) -> SimplePayloadType:
 
     virus_scan.scan_file(uploaded_file, user_email, project_name)
 
-    add_file = await sync_to_async(resources.create_file)(new_files_data,
-                                                          uploaded_file,
-                                                          project_name,
-                                                          user_email)
+    add_file = await sync_to_async(resources.create_file)(
+        new_files_data, uploaded_file, project_name, user_email
+    )
     if add_file:
         await sync_to_async(resources.send_mail)(
-            project_name, user_email, new_files_data, 'added', 'file')
+            project_name, user_email, new_files_data, 'added', 'file'
+        )
 
         success = True
     else:
         logging_utils.log(
-            'Couldn\'t upload file', 'error', extra=parameters)
+            'Couldn\'t upload file',
+            'error',
+            extra=parameters
+        )
     if success:
         util.invalidate_cache(project_name)
         util.cloudwatch_log(
-            info.context, f'Security: Added resource files to \
-            {project_name} project successfully')  # pragma: no cover
+            info.context,
+            ('Security: Added resource files to '
+             f'{project_name} project successfully')  # pragma: no cover
+        )
     else:
         util.cloudwatch_log(
             info.context,
-            f'Security: Attempted to add resource files \
-from {project_name} project')  # pragma: no cover
+            ('Security: Attempted to add resource files '
+             f'from {project_name} project')  # pragma: no cover
+        )
     return SimplePayloadType(success=success)
 
 
 @require_login
 @enforce_group_level_auth_async
 @require_integrates
-async def _do_remove_files(_, info, files_data: Dict[str, Any],
-                           project_name: str) -> SimplePayloadType:
+async def _do_remove_files(
+        _: Any,
+        info: GraphQLResolveInfo,
+        files_data: Dict[str, Any],
+        project_name: str) -> SimplePayloadType:
     """Resolve remove_files mutation."""
     success = False
-    files_data = {re.sub(r'_([a-z])', lambda x: x.group(1).upper(), k): v
-                  for k, v in files_data.items()}
+    files_data = {
+        re.sub(r'_([a-z])', lambda x: x.group(1).upper(), k): v
+        for k, v in files_data.items()
+    }
     file_name = files_data.get('fileName')
     user_email = util.get_jwt_content(info.context)['user_email']
     remove_file = await resources.remove_file(str(file_name), project_name)
     if remove_file:
         await sync_to_async(resources.send_mail)(
-            project_name, user_email, [files_data], 'removed', 'file')
+            project_name, user_email, [files_data], 'removed', 'file'
+        )
         success = True
     else:
         logging_utils.log(
@@ -240,116 +256,156 @@ async def _do_remove_files(_, info, files_data: Dict[str, Any],
             extra={
                 'file_name': file_name,
                 'project_name': project_name,
-            })
+            }
+        )
     if success:
         util.invalidate_cache(project_name)
         util.cloudwatch_log(
-            info.context, f'Security: Removed Files from \
-            {project_name} project successfully')  # pragma: no cover
+            info.context,
+            ('Security: Removed Files from '
+             f'{project_name} project successfully')  # pragma: no cover
+        )
     else:
         util.cloudwatch_log(
-            info.context, f'Security: Attempted to remove files \
-from {project_name} project')  # pragma: no cover
+            info.context,
+            ('Security: Attempted to remove files '
+             f'from {project_name} project')  # pragma: no cover
+        )
     return SimplePayloadType(success=success)
 
 
 @require_login
 @enforce_group_level_auth_async
 @require_integrates
-async def _do_download_file(_, info, **parameters) -> DownloadFilePayloadType:
+async def _do_download_file(
+        _: Any,
+        info: GraphQLResolveInfo,
+        **parameters: Any) -> DownloadFilePayloadType:
     """Resolve download_file mutation."""
     success = False
     file_info = parameters['files_data']
     project_name = parameters['project_name'].lower()
     user_email = util.get_jwt_content(info.context)['user_email']
-    signed_url = await \
-        sync_to_async(resources.download_file)(file_info, project_name)
+    signed_url = await sync_to_async(resources.download_file)(
+        file_info, project_name
+    )
     if signed_url:
-        msg = 'Security: Downloaded file {file_name} in \
-project {project} successfully'\
-            .format(project=project_name, file_name=parameters['files_data'])
-        util.cloudwatch_log(
-            info.context, msg)  # pragma: no cover
+        msg = (
+            f'Security: Downloaded file {parameters["files_data"]} '
+            f'in project {project_name} successfully'
+        )
+        util.cloudwatch_log(info.context, msg)  # pragma: no cover
         mp_obj = Mixpanel(settings.MIXPANEL_API_TOKEN)
-        await sync_to_async(mp_obj.track)(user_email, 'DownloadProjectFile', {
-            'Project': project_name.upper(),
-            'Email': user_email,
-            'FileName': parameters['files_data'],
-        })
+        await sync_to_async(mp_obj.track)(
+            user_email,
+            'DownloadProjectFile',
+            {
+                'Project': project_name.upper(),
+                'Email': user_email,
+                'FileName': parameters['files_data'],
+            }
+        )
         success = True
     else:
         util.cloudwatch_log(
             info.context,
-            'Security: Attempted to download file {file_name} \
-in project {project}'.format(
-                project=project_name,
-                file_name=parameters['files_data']))  # pragma: no cover
+            ('Security: Attempted to download file '
+             f'{parameters["files_data"]}'
+             f' in project {project_name}')  # pragma: no cover
+        )
         logging_utils.log(
-            'Couldn\'t generate signed URL', 'error', extra=parameters)
+            'Couldn\'t generate signed URL',
+            'error',
+            extra=parameters
+        )
     return DownloadFilePayloadType(success=success, url=str(signed_url))
 
 
 @require_login
 @enforce_group_level_auth_async
 @require_integrates
-async def _do_update_environment(_, info, project_name: str,
-                                 env: Dict[str, str],
-                                 state: str) -> SimplePayloadType:
+async def _do_update_environment(
+        _: Any,
+        info: GraphQLResolveInfo,
+        project_name: str,
+        env: Dict[str, str],
+        state: str) -> SimplePayloadType:
     """Resolve update_environment mutation."""
     user_email = util.get_jwt_content(info.context)['user_email']
-    env = {re.sub(r'_([a-z])', lambda x: x.group(1).upper(), k): v
-           for k, v in env.items()}
+    env = {
+        re.sub(r'_([a-z])', lambda x: x.group(1).upper(), k): v
+        for k, v in env.items()
+    }
     success = await sync_to_async(resources.update_resource)(
-        env, project_name, 'environment', user_email)
+        env, project_name, 'environment', user_email
+    )
 
     if success:
         util.invalidate_cache(project_name)
         util.cloudwatch_log(
             info.context,
-            f'Security: Updated environment state in {project_name} '
-            'project successfully')  # pragma: no cover
+            (f'Security: Updated environment state in {project_name} '
+             'project successfully')  # pragma: no cover
+        )
 
         action = 'activated' if state == 'ACTIVE' else 'deactivated'
         await sync_to_async(resources.send_mail)(
-            project_name, user_email, [env], action, 'environment')
+            project_name, user_email, [env], action, 'environment'
+        )
     else:
         logging_utils.log(
-            'Couldn\'t update environment state', 'error', extra=locals())
+            'Couldn\'t update environment state',
+            'error',
+            extra=locals()
+        )
         util.cloudwatch_log(
             info.context,
-            'Security: Attempted to update environment state in '
-            f'{project_name} project')  # pragma: no cover
+            ('Security: Attempted to update environment state in '
+             f'{project_name} project')  # pragma: no cover
+        )
     return SimplePayloadType(success=success)
 
 
 @require_login
 @enforce_group_level_auth_async
 @require_integrates
-async def _do_update_repository(_, info, project_name: str,
-                                repo: Dict[str, str],
-                                state: str) -> SimplePayloadType:
+async def _do_update_repository(
+        _: Any,
+        info: GraphQLResolveInfo,
+        project_name: str,
+        repo: Dict[str, str],
+        state: str) -> SimplePayloadType:
     """Resolve update_repository mutation."""
     user_email = util.get_jwt_content(info.context)['user_email']
-    repo = {re.sub(r'_([a-z])', lambda x: x.group(1).upper(), k): v
-            for k, v in repo.items()}
+    repo = {
+        re.sub(r'_([a-z])', lambda x: x.group(1).upper(), k): v
+        for k, v in repo.items()
+    }
     success = await sync_to_async(resources.update_resource)(
-        repo, project_name, 'repository', user_email)
+        repo, project_name, 'repository', user_email
+    )
 
     if success:
         util.invalidate_cache(project_name)
         util.cloudwatch_log(
             info.context,
-            f'Security: Updated repository state in {project_name} '
-            'project successfully')  # pragma: no cover
+            (f'Security: Updated repository state in {project_name} '
+             'project successfully')  # pragma: no cover
+        )
 
         action = 'activated' if state == 'ACTIVE' else 'deactivated'
         await sync_to_async(resources.send_mail)(
-            project_name, user_email, [repo], action, 'repository')
+            project_name, user_email, [repo], action, 'repository'
+        )
     else:
         logging_utils.log(
-            'Couldn\'t update repository state', 'error', extra=locals())
+            'Couldn\'t update repository state',
+            'error',
+            extra=locals()
+        )
         util.cloudwatch_log(
             info.context,
-            'Security: Attempted to update repository state in '
-            f'{project_name} project')  # pragma: no cover
+            ('Security: Attempted to update repository state in '
+             f'{project_name} project')  # pragma: no cover
+        )
     return SimplePayloadType(success=success)
