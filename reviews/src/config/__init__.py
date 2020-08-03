7d289e8267b0a97dd2fe8cf91f31b742a7bcd80c
Daniel Salazar <dsalazar@fluidattacks.com>
2020-08-03 16:32:55 -0500

    reviews\refac(back): #2336.13 dynamic validators
    - Dynamically add validators in order to avoid
    boolean operations, this simplifies custom
    error messages
    
    
    Former-commit-id: 1f6d8beaf86db966c53713a2a139a9f34cea9f4b

--- reviews/src/config/__init__.py
+++ reviews/src/config/__init__.py
@@ -1,93 +1,70 @@
 # Standard libraries
 from typing import Any, Dict, List
+from types import MappingProxyType
 
 # Third party libraries
 from dynaconf import Dynaconf, Validator
 
-def get_validators() -> List[Validator]:
+
+def validate(config: Dynaconf) -> None:
 
     def dict_has_type_values(dictionary: Dict[str, Any], expected_type: type) -> bool:
         success: bool = True
         for value in dictionary.values():
             if not isinstance(value, expected_type):
                 success = False
+                break
         return success
 
-    validators: List[Validator] = [
-        # base
-        Validator('gitlab_url', 'tests', must_exist=True),
+    # Validate main keys
+    config.validators.register(
+        Validator('gitlab_url', 'tests', must_exist=True)
+    )
+    config.validators.validate()
 
-        # all_pipelines_successful
-        Validator('tests.all_pipelines_successful', must_exist=False)
-        | Validator('tests.all_pipelines_successful.fail',
-                    must_exist=True, is_type_of=bool),
-
-        # mr_message_syntax
-        Validator('tests.mr_message_syntax', must_exist=False)
-        | Validator('tests.mr_message_syntax.fail',
-                    must_exist=True, is_type_of=bool),
-
-        # branch_equals_to_user
-        Validator('tests.branch_equals_to_user', must_exist=False)
-        | Validator('tests.branch_equals_to_user.fail',
-                    must_exist=True, is_type_of=bool),
-
-        # mr_under_max_deltas
-        Validator('tests.mr_under_max_deltas', must_exist=False)
-        | Validator('tests.mr_under_max_deltas.fail',
-                     must_exist=True, is_type_of=bool)
-        & Validator('tests.mr_under_max_deltas.max_deltas',
-                    must_exist=True, is_type_of=int)
-        & Validator('tests.mr_under_max_deltas.repo_path',
-                    must_exist=True, is_type_of=str),
-
-        # most_relevant_type
-        Validator('tests.most_relevant_type', must_exist=False)
-        | Validator('tests.most_relevant_type.fail',
-                    must_exist=True, is_type_of=bool)
-        & Validator('tests.most_relevant_type.commit_regex',
-                    must_exist=True, is_type_of=str)
-        & Validator('tests.most_relevant_type.relevances', must_exist=True,
-                    condition=lambda x: dict_has_type_values(x, int)),
-
-        # commits_user_syntax
-        Validator('tests.commits_user_syntax', must_exist=False)
-        | Validator('tests.commits_user_syntax.fail',
-                    must_exist=True, is_type_of=bool)
-        & Validator('tests.commits_user_syntax.user_regex',
-                    must_exist=True, is_type_of=str),
-
-        # mr_user_syntax
-        Validator('tests.mr_user_syntax', must_exist=False)
-        | Validator('tests.mr_user_syntax.fail',
-                    must_exist=True, is_type_of=bool)
-        & Validator('tests.mr_user_syntax.user_regex',
-                    must_exist=True, is_type_of=str),
-
-        # max_commits_per_mr
-        Validator('tests.max_commits_per_mr',
-                    must_exist=False)
-        | Validator('tests.max_commits_per_mr.fail',
-                    must_exist=True, is_type_of=bool)
-        & Validator('tests.max_commits_per_mr.max_commits',
-                    must_exist=True, is_type_of=int),
-
-        # close_issue_directive
-        Validator('tests.close_issue_directive', must_exist=False)
-        | Validator('tests.close_issue_directive.fail',
-                    must_exist=True, is_type_of=bool)
-        & Validator('tests.close_issue_directive.mr_title_regex',
-                    must_exist=True, is_type_of=str),
-    ]
-
-    return validators
+    # Validate tests
+    tests: List[str] = list(config['tests'].keys())
+    for test in tests:
+        config.validators.register(
+            Validator(f'tests.{test}.fail',
+                      must_exist=True, is_type_of=bool),
+        )
+        if test in ('commits_user_syntax', 'mr_user_syntax'):
+            config.validators.register(
+                Validator(f'tests.{test}.user_regex',
+                          must_exist=True, is_type_of=str),
+            )
+        if test in 'mr_under_max_deltas':
+            config.validators.register(
+                Validator(f'tests.{test}.max_deltas',
+                          must_exist=True, is_type_of=int),
+                Validator(f'tests.{test}.repo_path',
+                          must_exist=True, is_type_of=str),
+            )
+        if test in 'most_relevant_type':
+            config.validators.register(
+                Validator(f'tests.{test}.commit_regex',
+                          must_exist=True, is_type_of=str),
+                Validator(f'tests.{test}.relevances', must_exist=True,
+                          condition=lambda x: dict_has_type_values(x, int)),
+            )
+        if test in 'max_commits_per_mr':
+            config.validators.register(
+                Validator(f'tests.{test}.max_commits',
+                          must_exist=True, is_type_of=int),
+            )
+        if test in 'close_issue_directive':
+            config.validators.register(
+                Validator(f'tests.{test}.mr_title_regex',
+                          must_exist=True, is_type_of=str),
+            )
+    config.validators.validate()
 
 
 def load(config_path: str) -> Dynaconf:
     config: Dynaconf = Dynaconf(
         envvar_prefix='REVIEWS',
         settings_files=[config_path],
-        validators=get_validators(),
     )
-    config.validators.validate()
+    validate(config)
     return config
