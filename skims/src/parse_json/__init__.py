d4ec7f6dd3920a398df84d9ce01080e6450f0b97
Kevin Amado <kamado@fluidattacks.com>
2020-08-03 10:35:59 -0500

    skims\feat(back): #2294.93 json with line number
    - Add a parser for json with line numbers
    
    
    Former-commit-id: 47917b1a54417e66cea89a96751aa2cec2be9a6e

---
+++ skims/src/parse_json/__init__.py
@@ -0,0 +1,79 @@
+# Standard library
+import ast
+
+# Third party libraries
+from frozendict import (
+    frozendict
+)
+import lark
+
+# Constants
+GRAMMAR = r"""
+    ?start: value
+
+    ?value: object
+            | array
+            | string
+            | SIGNED_NUMBER      -> number
+            | "true"             -> true
+            | "false"            -> false
+            | "null"             -> null
+
+    array  : "[" [value ("," value)*] "]"
+    object : "{" [pair ("," pair)*] "}"
+    pair   : string ":" value
+
+    string : ESCAPED_STRING
+
+    %import common.ESCAPED_STRING
+    %import common.SIGNED_NUMBER
+    %import common.WS
+
+    %ignore WS
+"""
+
+
+def loads(stream: str) -> frozendict:
+    json_parser = lark.Lark(
+        grammar=GRAMMAR,
+        parser='lalr',
+        lexer='standard',
+        propagate_positions=True,
+        maybe_placeholders=False,
+        transformer=Builder(),
+    )
+
+    return json_parser.parse(stream)
+
+
+class Builder(lark.Transformer[frozendict]):
+
+    pair = tuple
+    object = frozendict
+
+    @staticmethod
+    @lark.v_args(inline=True)
+    def string(token: lark.Token) -> frozendict:
+        return frozendict({
+            'column': token.column,
+            'item': ast.literal_eval(token),
+            'line': token.line
+        })
+
+    @staticmethod
+    @lark.v_args(inline=True)
+    def number(token: lark.Token) -> frozendict:
+        return frozendict({
+            'column': token.column,
+            'item': ast.literal_eval(token),
+            'line': token.line,
+        })
+
+    @staticmethod
+    @lark.v_args(tree=True)
+    def array(tree: lark.Tree) -> frozendict:
+        return frozendict({
+            'column': 0,
+            'item': tuple(tree.children),
+            'line': 0,
+        })
