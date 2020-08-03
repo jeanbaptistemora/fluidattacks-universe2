03c1bab07a9b92e6a10068f395d02fda54bd6df8
Kevin Amado <kamado@fluidattacks.com>
2020-08-02 21:25:48 -0500

    skims\feat(back): #2294.88 serialization services
    - I've always instructed others not to use pickle because of
      its insecure nature
    - This is a serialization format base on json to dump and load
      whitelisted python objects to a stream
    - it uses json to serialize basic types, and dump complex types
      by its constructor in json format. Since all our container
      types are immutable and there's no state, it's simple to
      implement
    - ![image](/uploads/f4c8c934218ace1a1d4ef4494bd90d39/image.png)
    
    
    Former-commit-id: 499790cb802500ef5d34d4a85290fcaf924e3cf9

---
+++ skims/src/utils/serialization.py
@@ -0,0 +1,195 @@
+# Standard libraries
+from collections import (
+    OrderedDict,
+)
+from enum import (
+    Enum,
+)
+import json
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    List,
+    Tuple,
+    TypeVar,
+)
+
+# Local libraries
+from utils.aio import (
+    unblock_cpu,
+)
+from utils.model import (
+    FindingEnum,
+    FindingMetadata,
+    FindingTypeEnum,
+    IntegratesVulnerabilityMetadata,
+    SkimsVulnerabilityMetadata,
+    Vulnerability,
+    VulnerabilityKindEnum,
+    VulnerabilityStateEnum,
+)
+
+# Constants
+TVar = TypeVar('TVar')
+
+# Factory signature, args, kwargs
+Serialized = Tuple[Tuple[str, str], Tuple[Any, ...], Dict[str, Any]]
+
+
+class LoadError(Exception):
+    pass
+
+
+def _dump_bytes(instance: bytes) -> Serialized:
+    return _serialize(instance, instance.hex())
+
+
+def _load_bytes(data: str) -> bytes:
+    return bytes.fromhex(data)
+
+
+def _dump_dict(instance: Dict[str, Any]) -> Serialized:
+    return _serialize(
+        instance, *((_dump(key), _dump(val)) for key, val in instance.items()),
+    )
+
+
+def _load_dict(*args: Tuple[Serialized, Serialized]) -> Dict[Any, Any]:
+    return dict((_deserialize(key), _deserialize(val)) for key, val in args)
+
+
+def _dump_enum(instance: Enum) -> Serialized:
+    return _serialize(instance, _dump(instance.value))
+
+
+def _load_enum(factory: Callable[..., TVar]) -> Callable[..., TVar]:
+    return lambda value: factory(_deserialize(value))
+
+
+def _load_list(*args: Serialized) -> List[Any]:
+    return list(_load_tuple(*args))
+
+
+def _dump_named_tuple(instance: Tuple[Any, ...]) -> Serialized:
+    return _serialize(instance, *map(_dump, instance))
+
+
+def _load_named_tuple(factory: Callable[..., TVar]) -> Callable[..., TVar]:
+    return lambda *args: factory(*map(_deserialize, args))
+
+
+def _dump_none(instance: None) -> Serialized:
+    return _serialize(instance)
+
+
+def _load_none() -> None:
+    return None
+
+
+def _load_ordered_dict(*args: Tuple[Serialized, Serialized]) -> Dict[Any, Any]:
+    return OrderedDict(_load_dict(*args))
+
+
+def _dump_tuple(instance: List[Any]) -> Serialized:
+    return _serialize(instance, *map(_dump, instance))
+
+
+def _load_tuple(*args: Serialized) -> Tuple[Any, ...]:
+    return tuple(map(_deserialize, args))
+
+
+def get_signature(factory: Any) -> Tuple[str, str]:
+    return (factory.__module__, factory.__name__)
+
+
+# This is what guarantees security, only this types are whitelisted
+ALLOWED_FACTORIES: Dict[type, Dict[str, Any]] = {
+    factory: dict(
+        dumper=dumper,
+        loader=loader,
+        signature=get_signature(factory),
+    )
+    for _dump_base in [lambda x: _serialize(x, x)]
+    for factory, dumper, loader in [
+        (bytes, _dump_bytes, _load_bytes),
+        (dict, _dump_dict, _load_dict),
+        (float, _dump_base, float),
+        (int, _dump_base, int),
+        (list, _dump_tuple, _load_list),
+        (OrderedDict, _dump_dict, _load_ordered_dict),
+        (str, _dump_base, str),
+        (tuple, _dump_tuple, _load_tuple),
+        (type(None), _dump_none, _load_none),
+        *[
+            (enum, _dump_enum, _load_enum(enum))
+            for enum in (
+                FindingEnum,
+                FindingTypeEnum,
+                VulnerabilityKindEnum,
+                VulnerabilityStateEnum,
+            )
+        ],
+        *[
+            (named_tuple, _dump_named_tuple, _load_named_tuple(named_tuple))
+            for named_tuple in (
+                FindingMetadata,
+                IntegratesVulnerabilityMetadata,
+                SkimsVulnerabilityMetadata,
+                Vulnerability,
+            )
+        ],
+    ]
+}
+SIGNATURE_TO_FACTORY: Dict[Any, type] = {
+    factory_attributes['signature']: factory
+    for factory, factory_attributes in ALLOWED_FACTORIES.items()
+}
+
+
+def _serialize(
+    instance: Any,
+    *args: Any,
+    **kwargs: Any,
+) -> Serialized:
+    signature = ALLOWED_FACTORIES[type(instance)]['signature']
+
+    return (signature, args, kwargs)
+
+
+def _deserialize(data: Serialized) -> Any:
+    signature, args, kwargs = tuple(data[0]), data[1], data[2]
+    factory: type = SIGNATURE_TO_FACTORY[signature]
+    loader: Callable[..., Any] = ALLOWED_FACTORIES[factory]['loader']
+
+    return loader(*args, **kwargs)
+
+
+def _dump(instance: Any) -> Serialized:
+    factory = type(instance)
+    dumper: Callable[..., Serialized] = ALLOWED_FACTORIES[factory]['dumper']
+
+    return dumper(instance)
+
+
+async def dump(instance: Any) -> bytes:
+    dumped: Serialized = await unblock_cpu(_dump, instance)
+    serialized: str = (
+        await unblock_cpu(json.dumps, dumped, separators=(',', ':'))
+    )
+
+    return await unblock_cpu(serialized.encode, 'utf-8')
+
+
+async def load(stream: bytes) -> Any:
+    try:
+        deserialized: Any = await unblock_cpu(json.loads, stream)
+
+        return await unblock_cpu(_deserialize, deserialized)
+    except (
+        AttributeError,
+        json.decoder.JSONDecodeError,
+        TypeError,
+        ValueError,
+    ) as exc:
+        raise LoadError(exc)
