a79627c07b6cdfe5ecc2c366e52603389ae82f98
Daniel Salazar <dsalazar@fluidattacks.com>
2020-08-03 18:08:23 -0500

    reviews\refac(back): #2336.14 custom errors
    - Add custom errors for validators
    
    
    Former-commit-id: 383adb285b5acfc6cab8de3fe2865ef45dc1451d

--- reviews/src/config/__init__.py
+++ reviews/src/config/__init__.py
@@ -1,6 +1,5 @@
 # Standard libraries
 from typing import Any, Dict, List
-from types import MappingProxyType
 
 # Third party libraries
 from dynaconf import Dynaconf, Validator
@@ -16,9 +15,18 @@ def validate(config: Dynaconf) -> None:
                 break
         return success
 
+    err_default: Dict[str,str] = {
+        'must_exist_true': '{name} is required.',
+        'must_exist_false': '{name} cannot exist.',
+        'condition': '{name} invalid for {function}({value}).',
+        'operations': '{name} must be {op_value}.',
+        'combined': 'combined validators failed {errors}.',
+    }
+
     # Validate main keys
     config.validators.register(
-        Validator('gitlab_url', 'tests', must_exist=True)
+        Validator('gitlab_url', 'tests', must_exist=True,
+                  messages={'must_exist_true': '{name} is required.'})
     )
     config.validators.validate()
 
@@ -26,37 +34,40 @@ def validate(config: Dynaconf) -> None:
     tests: List[str] = list(config['tests'].keys())
     for test in tests:
         config.validators.register(
-            Validator(f'tests.{test}.fail',
-                      must_exist=True, is_type_of=bool),
+            Validator(f'tests.{test}.fail', must_exist=True,
+                      is_type_of=bool, messages=err_default),
         )
         if test in ('commits_user_syntax', 'mr_user_syntax'):
             config.validators.register(
-                Validator(f'tests.{test}.user_regex',
-                          must_exist=True, is_type_of=str),
+                Validator(f'tests.{test}.user_regex', must_exist=True,
+                          is_type_of=str, messages=err_default),
             )
         if test in 'mr_under_max_deltas':
             config.validators.register(
-                Validator(f'tests.{test}.max_deltas',
-                          must_exist=True, is_type_of=int),
-                Validator(f'tests.{test}.repo_path',
-                          must_exist=True, is_type_of=str),
+                Validator(f'tests.{test}.max_deltas', must_exist=True,
+                          is_type_of=int, messages=err_default),
+                Validator(f'tests.{test}.repo_path', must_exist=True,
+                          is_type_of=str, messages=err_default),
             )
         if test in 'most_relevant_type':
             config.validators.register(
-                Validator(f'tests.{test}.commit_regex',
-                          must_exist=True, is_type_of=str),
+                Validator(f'tests.{test}.commit_regex', must_exist=True,
+                          is_type_of=str, messages=err_default),
                 Validator(f'tests.{test}.relevances', must_exist=True,
-                          condition=lambda x: dict_has_type_values(x, int)),
+                          condition=lambda x: dict_has_type_values(x, int),
+                          messages={'must_exist_true': '{name} is required.',
+                                    'condition': '{name} invalid. '
+                                    'All values must be int'}),
             )
         if test in 'max_commits_per_mr':
             config.validators.register(
-                Validator(f'tests.{test}.max_commits',
-                          must_exist=True, is_type_of=int),
+                Validator(f'tests.{test}.max_commits', must_exist=True,
+                          is_type_of=int, messages=err_default),
             )
         if test in 'close_issue_directive':
             config.validators.register(
-                Validator(f'tests.{test}.mr_title_regex',
-                          must_exist=True, is_type_of=str),
+                Validator(f'tests.{test}.mr_title_regex', must_exist=True,
+                          is_type_of=str, messages=err_default),
             )
     config.validators.validate()
 
