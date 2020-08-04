cab7ddb90616bf45577b06b6b40691a1f165576a
Andres Lemus <alemus@fluidattacks.com>
2020-08-04 15:24:53 -0500

    integrates\refac(front): #1719.42 lint cvss file
    Former-commit-id: 79a0ac1ee8d4bb26af842be10d9badd6b1f2912c

--- front/src/utils/cvss.ts
+++ front/src/utils/cvss.ts
@@ -1,63 +1,91 @@
+/* eslint @typescript-eslint/no-magic-numbers: ["error", { "ignore": [0,1,10] }] */
 import { ISeverityAttr } from "../scenes/Dashboard/containers/SeverityView/types";
 
-export const calcPrivilegesRequired: ((privileges: string, scope: string) => number) =
-  (privileges: string, scope: string): number => {
-    let privReq: number = parseFloat(privileges);
-    if (parseFloat(scope) === 1) {
-      if (privReq === 0.62) {
-        privReq = 0.68;
-      } else if (privReq === 0.27) {
-        privReq = 0.5;
-      }
-    } else {
-      if (privReq === 0.68) {
-        privReq = 0.62;
-      } else if (privReq === 0.5) {
-        privReq = 0.27;
-      }
+/**
+ * Values were taken from:
+ * @see https://www.first.org/cvss/specification-document 7.4. Metric Values
+ */
+export const calcPrivilegesRequired: (
+  privileges: string,
+  scope: string
+) => number = (privileges: string, scope: string): number => {
+  const LOW_SCOPE_U: number = 0.62;
+  const LOW_SCOPE_C: number = 0.68;
+  const HIGH_SCOPE_U: number = 0.27;
+  const HIGH_SCOPE_C: number = 0.5;
+
+  const privReq: number = parseFloat(privileges);
+
+  if (parseFloat(scope) === 1) {
+    if (privReq === LOW_SCOPE_U) {
+      return LOW_SCOPE_C;
+    } else if (privReq === HIGH_SCOPE_U) {
+      return HIGH_SCOPE_C;
     }
+  } else {
+    if (privReq === LOW_SCOPE_C) {
+      return LOW_SCOPE_U;
+    } else if (privReq === HIGH_SCOPE_C) {
+      return HIGH_SCOPE_U;
+    }
+  }
 
-    return privReq;
-  };
+  return privReq;
+};
 
-export const calcCVSSv3: ((data: ISeverityAttr["finding"]["severity"]) => number) =
-  (data: ISeverityAttr["finding"]["severity"]): number => {
-    const BASESCORE_FACTOR: number = 1.08;
-    const IMPACT_FACTOR_1: number = 6.42;
-    const IMPACT_FACTOR_2: number = 7.52;
-    const IMPACT_FACTOR_3: number = 0.029;
-    const IMPACT_FACTOR_4: number = 3.25;
-    const IMPACT_FACTOR_5: number = 0.02;
-    const IMPACT_FACTOR_6: number = 15;
-    const EXPLOITABILITY_FACTOR_1: number = 8.22;
+/**
+ * Values and formulas were taken from:
+ * @see https://www.first.org/cvss/specification-document
+ */
+export const calcCVSSv3: (
+  data: ISeverityAttr["finding"]["severity"]
+) => number = (data: ISeverityAttr["finding"]["severity"]): number => {
+  const BASESCORE_FACTOR: number = 1.08;
+  const IMPACT_FACTOR_1: number = 6.42;
+  const IMPACT_FACTOR_2: number = 7.52;
+  const IMPACT_FACTOR_3: number = 0.029;
+  const IMPACT_FACTOR_4: number = 3.25;
+  const IMPACT_FACTOR_5: number = 0.02;
+  const IMPACT_FACTOR_6: number = 15;
+  const EXPLOITABILITY_FACTOR_1: number = 8.22;
 
-    const impCon: number = parseFloat(data.confidentialityImpact);
-    const impInt: number = parseFloat(data.integrityImpact);
-    const impDis: number = parseFloat(data.availabilityImpact);
-    const sevScope: number = parseFloat(data.severityScope);
-    const attVec: number = parseFloat(data.attackVector);
-    const attCom: number = parseFloat(data.attackComplexity);
-    const privReq: number = calcPrivilegesRequired(data.privilegesRequired, data.severityScope);
-    const usrInt: number = parseFloat(data.userInteraction);
-    const explo: number = parseFloat(data.exploitability);
-    const remLev: number = parseFloat(data.remediationLevel);
-    const repConf: number = parseFloat(data.reportConfidence);
+  const impCon: number = parseFloat(data.confidentialityImpact);
+  const impInt: number = parseFloat(data.integrityImpact);
+  const impDis: number = parseFloat(data.availabilityImpact);
+  const sevScope: number = parseFloat(data.severityScope);
+  const attVec: number = parseFloat(data.attackVector);
+  const attCom: number = parseFloat(data.attackComplexity);
+  const privReq: number = calcPrivilegesRequired(
+    data.privilegesRequired,
+    data.severityScope
+  );
+  const usrInt: number = parseFloat(data.userInteraction);
+  const explo: number = parseFloat(data.exploitability);
+  const remLev: number = parseFloat(data.remediationLevel);
+  const repConf: number = parseFloat(data.reportConfidence);
 
-    const iscBase: number = 1 - ((1 - impCon) * (1 - impInt) * (1 - impDis));
+  const iscBase: number = 1 - (1 - impCon) * (1 - impInt) * (1 - impDis);
 
-    const impact: number = (sevScope === 1)
-      ? ((IMPACT_FACTOR_2 * (iscBase - IMPACT_FACTOR_3)) -
-        (IMPACT_FACTOR_4 * Math.pow((iscBase - IMPACT_FACTOR_5), IMPACT_FACTOR_6)))
+  const impact: number =
+    sevScope === 1
+      ? IMPACT_FACTOR_2 * (iscBase - IMPACT_FACTOR_3) -
+        IMPACT_FACTOR_4 * Math.pow(iscBase - IMPACT_FACTOR_5, IMPACT_FACTOR_6)
       : IMPACT_FACTOR_1 * iscBase;
-    const exploitability: number = (EXPLOITABILITY_FACTOR_1 * attVec * attCom * privReq * usrInt);
 
-    const basescore: number = (impact <= 0)
+  const exploitability: number =
+    EXPLOITABILITY_FACTOR_1 * attVec * attCom * privReq * usrInt;
+
+  const basescore: number =
+    impact <= 0
       ? 0
-      : ((sevScope === 1)
-        ? Math.ceil(Math.min(BASESCORE_FACTOR * (impact + exploitability), 10) * 10) / 10
-        : Math.ceil(Math.min(impact + exploitability, 10) * 10) / 10);
+      : sevScope === 1
+      ? Math.ceil(
+          Math.min(BASESCORE_FACTOR * (impact + exploitability), 10) * 10
+        ) / 10
+      : Math.ceil(Math.min(impact + exploitability, 10) * 10) / 10;
 
-    const temporal: number = Math.ceil(basescore * explo * remLev * repConf * 10) / 10;
+  const temporal: number =
+    Math.ceil(basescore * explo * remLev * repConf * 10) / 10;
 
-    return isNaN(temporal) ? 0 : temporal;
-  };
+  return isNaN(temporal) ? 0 : temporal;
+};
