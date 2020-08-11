abc7db9617f69bd83ad2778023418e19d25c02e4
Andres Lemus <alemus@fluidattacks.com>
2020-08-11 16:37:57 -0500

    integrates\refac(front): #1719.52 lint format_helpers
    Former-commit-id: 5d5b555709dcd048044ff8e07e23184cfa9e4d54

--- front/src/utils/formatHelpers.ts
+++ front/src/utils/formatHelpers.ts
@@ -1,66 +1,101 @@
 import _ from "lodash";
-import { ILastLogin, IUserDataAttr, IUsersAttr } from "../scenes/Dashboard/containers/ProjectUsersView/types";
 import translate from "./translations/translate";
+import {
+  ILastLogin,
+  IUserDataAttr,
+  IUsersAttr,
+} from "../scenes/Dashboard/containers/ProjectUsersView/types";
 
-type IUserList = IUsersAttr["project"]["users"];
+type User = IUsersAttr["project"]["users"][0];
 
-export const formatUserlist: ((userList: IUserDataAttr[]) => IUserList) =
-  (userList: IUserDataAttr[]): IUserList => userList.map((user: IUserDataAttr) => {
-    const lastLoginDate: number[] = JSON.parse(user.lastLogin);
-    let DAYS_IN_MONTH: number;
-    DAYS_IN_MONTH = 30;
-    const lastLogin: ILastLogin = {
-      label: "",
-      value: lastLoginDate,
-    };
-    let firstLogin: string; firstLogin = "";
-    if (!_.isUndefined(user.firstLogin)) {
-      firstLogin = user.firstLogin.split(" ")[0];
+export const formatUserlist: (userList: IUserDataAttr[]) => User[] = (
+  userList: IUserDataAttr[]
+): User[] =>
+  userList.map(
+    (user: IUserDataAttr): User => {
+      const missing: number = -1;
+      const lastLoginDate: number[] = JSON.parse(user.lastLogin);
+      const daysInMonth: number = 30;
+
+      const firstLogin: string = ((): string => {
+        if (lastLoginDate[0] === missing) {
+          return "-";
+        }
+        if (!_.isUndefined(user.firstLogin)) {
+          return user.firstLogin.split(" ")[0];
+        }
+
+        return "";
+      })();
+
+      const lastLogin: ILastLogin = {
+        label: ((): string => {
+          if (lastLoginDate[0] >= daysInMonth) {
+            return translate.t("search_findings.tab_users.months_ago", {
+              count: Math.round(lastLoginDate[0] / daysInMonth),
+            });
+          }
+          if (lastLoginDate[0] > 0) {
+            return translate.t("search_findings.tab_users.days_ago", {
+              count: lastLoginDate[0],
+            });
+          }
+          if (lastLoginDate[0] === missing) {
+            return "-";
+          }
+          const secsInMin: number = 60;
+          const secsInHour: number = 3600;
+          const roundedMinutes: number = Math.round(
+            lastLoginDate[1] / secsInMin
+          );
+          const roundedHour: number = Math.round(lastLoginDate[1] / secsInHour);
+
+          return roundedHour >= 1 && roundedMinutes >= secsInMin
+            ? translate.t("search_findings.tab_users.hours_ago", {
+                count: roundedHour,
+              })
+            : translate.t("search_findings.tab_users.minutes_ago", {
+                count: roundedMinutes,
+              });
+        })(),
+        value: lastLoginDate,
+      };
+
+      return { ...user, firstLogin, lastLogin };
     }
-    if (lastLoginDate[0] >= DAYS_IN_MONTH) {
-      const ROUNDED_MONTH: number = Math.round(lastLoginDate[0] / DAYS_IN_MONTH);
-      lastLogin.label = translate.t("search_findings.tab_users.months_ago", { count: ROUNDED_MONTH });
-    } else if (lastLoginDate[0] > 0 && lastLoginDate[0] < DAYS_IN_MONTH) {
-      lastLogin.label = translate.t("search_findings.tab_users.days_ago", { count: lastLoginDate[0] });
-    } else if (lastLoginDate[0] === -1) {
-      lastLogin.label = "-";
-      firstLogin = "-";
-    } else {
-      let SECONDS_IN_HOUR: number;
-      SECONDS_IN_HOUR = 3600;
-      const ROUNDED_HOUR: number = Math.round(lastLoginDate[1] / SECONDS_IN_HOUR);
-      let SECONDS_IN_MINUTES: number;
-      SECONDS_IN_MINUTES = 60;
-      const ROUNDED_MINUTES: number = Math.round(lastLoginDate[1] / SECONDS_IN_MINUTES);
-      lastLogin.label = ROUNDED_HOUR >= 1 && ROUNDED_MINUTES >= SECONDS_IN_MINUTES
-        ? translate.t("search_findings.tab_users.hours_ago", { count: ROUNDED_HOUR })
-        : translate.t("search_findings.tab_users.minutes_ago", { count: ROUNDED_MINUTES });
-    }
+  );
 
-    return { ...user, lastLogin, firstLogin };
-  });
+export const formatLastLogin: (value: ILastLogin) => string = (
+  value: ILastLogin
+): string => value.label;
 
-export const formatLastLogin: (value: ILastLogin) => string =
-  (value: ILastLogin): string => (value.label);
-
-export const castEventType: ((field: string) => string) = (field: string): string => {
-  const eventType: {[value: string]: string} = {
-    "AUTHORIZATION_SPECIAL_ATTACK": "search_findings.tab_events.type_values.auth_attack",
+export const castEventType: (field: string) => string = (
+  field: string
+): string => {
+  const eventType: Record<string, string> = {
+    AUTHORIZATION_SPECIAL_ATTACK:
+      "search_findings.tab_events.type_values.auth_attack",
     "Ambiente inestable": "search_findings.tab_events.type_values.uns_ambient",
-    "Ambiente no accesible": "search_findings.tab_events.type_values.inacc_ambient",
-    "CLIENT_APPROVES_CHANGE_TOE": "search_findings.tab_events.type_values.approv_change",
-    "CLIENT_DETECTS_ATTACK": "search_findings.tab_events.type_values.det_attack",
-    "HIGH_AVAILABILITY_APPROVAL": "search_findings.tab_events.type_values.high_approval",
-    "INCORRECT_MISSING_SUPPLIES": "search_findings.tab_events.type_values.incor_supplies",
-    "OTHER": "search_findings.tab_events.type_values.other",
-    "TOE_DIFFERS_APPROVED": "search_findings.tab_events.type_values.toe_differs",
+    "Ambiente no accesible":
+      "search_findings.tab_events.type_values.inacc_ambient",
+    CLIENT_APPROVES_CHANGE_TOE:
+      "search_findings.tab_events.type_values.approv_change",
+    CLIENT_DETECTS_ATTACK: "search_findings.tab_events.type_values.det_attack",
+    HIGH_AVAILABILITY_APPROVAL:
+      "search_findings.tab_events.type_values.high_approval",
+    INCORRECT_MISSING_SUPPLIES:
+      "search_findings.tab_events.type_values.incor_supplies",
+    OTHER: "search_findings.tab_events.type_values.other",
+    TOE_DIFFERS_APPROVED: "search_findings.tab_events.type_values.toe_differs",
   };
 
   return eventType[field];
 };
 
-export const castEventStatus: ((field: string) => string) = (field: string): string => {
-  const eventStatus: {[value: string]: string} = {
+export const castEventStatus: (field: string) => string = (
+  field: string
+): string => {
+  const eventStatus: Record<string, string> = {
     CREATED: "search_findings.tab_events.status_values.unsolve",
     SOLVED: "search_findings.tab_events.status_values.solve",
   };
@@ -68,10 +103,13 @@ export const castEventStatus: ((field: string) => string) = (field: string): str
   return eventStatus[field];
 };
 
-export const formatDropdownField: ((field: string) => string) = (field: string): string => {
-  const translationParameters: {[value: string]: string} = {
+export const formatDropdownField: (field: string) => string = (
+  field: string
+): string => {
+  const translationParameters: Record<string, string> = {
     ACCEPTED: "search_findings.tab_description.treatment.accepted",
-    ACCEPTED_UNDEFINED: "search_findings.tab_description.treatment.accepted_undefined",
+    ACCEPTED_UNDEFINED:
+      "search_findings.tab_description.treatment.accepted_undefined",
     ANONYMOUS_INTERNET: "search_findings.tab_description.scenario.anon_inter",
     ANONYMOUS_INTRANET: "search_findings.tab_description.scenario.anon_intra",
     ANYONE_INTERNET: "search_findings.tab_description.actor.any_internet",
@@ -79,9 +117,12 @@ export const formatDropdownField: ((field: string) => string) = (field: string):
     ANY_CUSTOMER: "search_findings.tab_description.actor.any_customer",
     ANY_EMPLOYEE: "search_findings.tab_description.actor.any_employee",
     APPLICATIONS: "search_findings.tab_description.ambit.applications",
-    AUTHORIZED_USER_EXTRANET: "search_findings.tab_description.scenario.auth_extra",
-    AUTHORIZED_USER_INTERNET: "search_findings.tab_description.scenario.auth_inter",
-    AUTHORIZED_USER_INTRANET: "search_findings.tab_description.scenario.auth_intra",
+    AUTHORIZED_USER_EXTRANET:
+      "search_findings.tab_description.scenario.auth_extra",
+    AUTHORIZED_USER_INTERNET:
+      "search_findings.tab_description.scenario.auth_inter",
+    AUTHORIZED_USER_INTRANET:
+      "search_findings.tab_description.scenario.auth_intra",
     DATABASES: "search_findings.tab_description.ambit.databases",
     INFRASTRUCTURE: "search_findings.tab_description.ambit.infra",
     IN_PROGRESS: "search_findings.tab_description.treatment.in_progress",
@@ -91,31 +132,50 @@ export const formatDropdownField: ((field: string) => string) = (field: string):
     SOME_CUSTOMERS: "search_findings.tab_description.actor.some_customer",
     SOME_EMPLOYEES: "search_findings.tab_description.actor.some_employee",
     SOURCE_CODE: "search_findings.tab_description.ambit.sourcecode",
-    UNAUTHORIZED_USER_EXTRANET: "search_findings.tab_description.scenario.unauth_extra",
-    UNAUTHORIZED_USER_INTERNET: "search_findings.tab_description.scenario.unauth_inter",
-    UNAUTHORIZED_USER_INTRANET: "search_findings.tab_description.scenario.unauth_intra",
+    UNAUTHORIZED_USER_EXTRANET:
+      "search_findings.tab_description.scenario.unauth_extra",
+    UNAUTHORIZED_USER_INTERNET:
+      "search_findings.tab_description.scenario.unauth_inter",
+    UNAUTHORIZED_USER_INTRANET:
+      "search_findings.tab_description.scenario.unauth_intra",
   };
 
   return translationParameters[field];
 };
 
-export const formatTreatment: ((treatment: string, findingState: string) => string) =
-  (treatment: string, findingState: string): string => {
-    const treatmentParameters: { [value: string]: string } = {
-      "-": (findingState === "closed") ? "-" : "-",
-      "ACCEPTED": (findingState === "open")
-        ? "search_findings.tab_description.treatment.accepted" : "-",
-      "ACCEPTED_UNDEFINED": (findingState === "open")
-        ? "search_findings.tab_description.treatment.accepted_undefined" : "-",
-      "ACCEPTED_UNDEFINED pending": (findingState === "open")
-        ? translate.t("search_findings.tab_description.treatment.accepted_undefined") +
-        translate.t("search_findings.tab_description.treatment.pending_approval") : "-",
-      "IN PROGRESS": (findingState === "open")
-        ? "search_findings.tab_description.treatment.in_progress" : "-",
-      "NEW": (findingState === "open")
-        ? "search_findings.tab_description.treatment.new" : "-",
-    };
-    const treatmentRes: string = translate.t(treatmentParameters[treatment]);
-
-    return treatmentRes;
+export const formatTreatment: (
+  treatment: string,
+  findingState: string
+) => string = (treatment: string, findingState: string): string => {
+  const treatmentParameters: Record<string, string> = {
+    "-": findingState === "closed" ? "-" : "-",
+    ACCEPTED:
+      findingState === "open"
+        ? "search_findings.tab_description.treatment.accepted"
+        : "-",
+    ACCEPTED_UNDEFINED:
+      findingState === "open"
+        ? "search_findings.tab_description.treatment.accepted_undefined"
+        : "-",
+    "ACCEPTED_UNDEFINED pending":
+      findingState === "open"
+        ? translate.t(
+            "search_findings.tab_description.treatment.accepted_undefined"
+          ) +
+          translate.t(
+            "search_findings.tab_description.treatment.pending_approval"
+          )
+        : "-",
+    "IN PROGRESS":
+      findingState === "open"
+        ? "search_findings.tab_description.treatment.in_progress"
+        : "-",
+    NEW:
+      findingState === "open"
+        ? "search_findings.tab_description.treatment.new"
+        : "-",
   };
+  const treatmentRes: string = translate.t(treatmentParameters[treatment]);
+
+  return treatmentRes;
+};
