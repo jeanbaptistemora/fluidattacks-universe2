ecb5ee779fa2efbcb64ae489f9f440b2372a815d
Andres Lemus <alemus@fluidattacks.com>
2020-08-13 14:47:52 -0500

    integrates\refac(front): #1719.60 lint welcome_view test
    Former-commit-id: 69f81b8ba61f13f9deb5cc18ec28cd77be97195e

--- front/src/scenes/Registration/containers/WelcomeView/index.spec.tsx
+++ front/src/scenes/Registration/containers/WelcomeView/index.spec.tsx
@@ -1,18 +1,20 @@
-import { MockedProvider, MockedResponse } from "@apollo/react-testing";
-import { mount, ReactWrapper, shallow, ShallowWrapper } from "enzyme";
-import _ from "lodash";
-import React from "react";
-// tslint:disable-next-line: no-submodule-imports
-import { act } from "react-dom/test-utils";
+/* eslint-disable react/jsx-props-no-spreading */
+import { GET_USER_AUTHORIZATION } from "./queries";
 import { Provider } from "react-redux";
-import { MemoryRouter, RouteComponentProps } from "react-router-dom";
-import wait from "waait";
+import React from "react";
+import { WelcomeView } from "./index";
+import _ from "lodash";
+import { act } from "react-dom/test-utils";
 import store from "../../../../store";
 import translate from "../../../../utils/translations/translate";
-import { WelcomeView } from "./index";
-import { GET_USER_AUTHORIZATION } from "./queries";
+import { MemoryRouter, RouteComponentProps } from "react-router-dom";
+import { MockedProvider, MockedResponse, wait } from "@apollo/react-testing";
+import { ReactWrapper, ShallowWrapper, mount, shallow } from "enzyme";
 
-describe("Welcome view", () => {
+describe("Welcome view", (): void => {
+  // Necessary to setup the window object within the test.
+  // eslint-disable-next-line fp/no-mutation
+  (window as typeof window & { userName: string }).userName = "Test";
 
   const routeProps: RouteComponentProps = {
     history: {
@@ -32,92 +34,97 @@ describe("Welcome view", () => {
     match: { isExact: true, params: {}, path: "/", url: "" },
   };
 
-  it("should return a function", () => {
-    expect(typeof (WelcomeView))
-      .toEqual("function");
+  it("should return a function", (): void => {
+    expect.hasAssertions();
+    expect(typeof WelcomeView).toStrictEqual("function");
   });
 
-  it("should render", () => {
-    const wrapper: ShallowWrapper = shallow(
-      <WelcomeView {...routeProps} />,
-    );
+  it("should render greetings message", (): void => {
+    expect.hasAssertions();
 
-    expect(wrapper)
-      .toHaveLength(1);
+    const wrapper: ShallowWrapper = shallow(<WelcomeView {...routeProps} />);
+
+    expect(wrapper.text()).toContain("Hello Test!");
   });
 
-  it("should render greetings message", () => {
-    (window as typeof window & { userName: string }).userName = "Test";
-    const wrapper: ShallowWrapper = shallow(
-      <WelcomeView {...routeProps} />,
-    );
-    expect(wrapper.text())
-      .toContain("Hello Test!");
-  });
+  it("should render legal notice", async (): Promise<void> => {
+    expect.hasAssertions();
 
-  it("should render legal notice", async () => {
-    const mocks: ReadonlyArray<MockedResponse> = [{
-      request: { query: GET_USER_AUTHORIZATION },
-      result: {
-        data: { me: { remember: false } },
+    const mocks: readonly MockedResponse[] = [
+      {
+        request: { query: GET_USER_AUTHORIZATION },
+        result: {
+          data: { me: { remember: false } },
+        },
       },
-    }];
+    ];
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
-        <MockedProvider mocks={mocks} addTypename={false}>
+        <MockedProvider addTypename={false} mocks={mocks}>
           <WelcomeView {...routeProps} />
         </MockedProvider>
-      </Provider>,
+      </Provider>
     );
-    await act(async () => { await wait(0); wrapper.update(); });
-    expect(wrapper.find("modal")
-      .text())
-      .toContain("Integrates, Copyright (c) 2020 Fluid Attacks");
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+
+    expect(wrapper.find("modal").text()).toContain(
+      "Integrates, Copyright (c) 2020 Fluid Attacks"
+    );
   });
 
-  it("should render already logged in", () => {
+  it("should render already logged in", (): void => {
+    expect.hasAssertions();
+
     localStorage.setItem("showAlreadyLoggedin", "1");
-    const wrapper: ShallowWrapper = shallow(
-      <WelcomeView {...routeProps} />,
-    );
-    expect(wrapper.find("h3")
-      .text())
-      .toContain("You are already logged in");
+    const wrapper: ShallowWrapper = shallow(<WelcomeView {...routeProps} />);
+
+    expect(wrapper.find("h3").text()).toContain("You are already logged in");
   });
 
-  it("should render concurrent session", () => {
+  it("should render concurrent session", (): void => {
+    expect.hasAssertions();
+
     localStorage.setItem("showAlreadyLoggedin", "0");
     localStorage.setItem("concurrentSession", "1");
-    const wrapper: ShallowWrapper = shallow(
-      <WelcomeView {...routeProps} />,
+    const wrapper: ShallowWrapper = shallow(<WelcomeView {...routeProps} />);
+
+    expect(wrapper.find("h3").text()).toContain(
+      translate.t("registration.concurrent_session_message")
     );
-    expect(wrapper.find("h3")
-      .text())
-      .toContain(translate.t("registration.concurrent_session_message"));
   });
 
-  it("should clear localstorage before redirect", () => {
-    const mocks: ReadonlyArray<MockedResponse> = [{
-      request: { query: GET_USER_AUTHORIZATION },
-      result: {
-        data: { me: { remember: false } },
+  it("should clear localstorage before redirect", (): void => {
+    expect.hasAssertions();
+
+    const mocks: readonly MockedResponse[] = [
+      {
+        request: { query: GET_USER_AUTHORIZATION },
+        result: {
+          data: { me: { remember: false } },
+        },
       },
-    }];
+    ];
     localStorage.setItem("showAlreadyLoggedin", "1");
-    localStorage.setItem("start_url", "/project/BWAPP/vulns/413372600/consulting");
+    localStorage.setItem(
+      "start_url",
+      "/project/BWAPP/vulns/413372600/consulting"
+    );
     const wrapper: ReactWrapper = mount(
-      <MockedProvider mocks={mocks} addTypename={false}>
+      <MockedProvider addTypename={false} mocks={mocks}>
         <MemoryRouter>
           <WelcomeView {...routeProps} />
         </MemoryRouter>
-      </MockedProvider>,
+      </MockedProvider>
     );
 
-    wrapper.find("Button")
-      .simulate("click");
-    expect(_.get(localStorage, "showAlreadyLoggedin"))
-      .toEqual(undefined);
-    expect(_.get(localStorage, "start_url"))
-      .toEqual(undefined);
+    wrapper.find("Button").simulate("click");
+
+    expect(_.get(localStorage, "showAlreadyLoggedin")).toBeUndefined();
+    expect(_.get(localStorage, "start_url")).toBeUndefined();
   });
 });
