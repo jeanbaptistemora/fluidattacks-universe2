92c40316f07581db25340fd9b7d1bc5e5d7b17de
Andres Lemus <alemus@fluidattacks.com>
2020-08-13 12:30:53 -0500

    integrates\refac(front): #1719.59 lint welcome_view index
    Former-commit-id: 3745976ba75c88bcdc69b97cf3c17a900893101e

--- front/src/scenes/Registration/containers/WelcomeView/index.tsx
+++ front/src/scenes/Registration/containers/WelcomeView/index.tsx
@@ -1,38 +1,34 @@
-/* tslint:disable:jsx-no-multiline-js
- *
- * Disabling this rule is necessary for accessing render props from
- * apollo components
- */
-
-import { MutationFunction, QueryResult } from "@apollo/react-common";
-import { Mutation, Query } from "@apollo/react-components";
-import _ from "lodash";
-import React from "react";
-import { Col, Row } from "react-bootstrap";
-import { Redirect, RouteComponentProps, useHistory } from "react-router-dom";
+/* eslint-disable react/forbid-component-props
+  ------
+  We need to override default styles from react-bootstrap
+*/
 import { Button } from "../../../../components/Button";
-import { default as logo } from "../../../../resources/integrates.svg";
-import { default as globalStyle } from "../../../../styles/global.css";
-import translate from "../../../../utils/translations/translate";
 import { CompulsoryNotice } from "../../components/CompulsoryNotice";
+import React from "react";
+import _ from "lodash";
+import { default as globalStyle } from "../../../../styles/global.css";
+import { default as logo } from "../../../../resources/integrates.svg";
 import { default as style } from "./index.css";
+import translate from "../../../../utils/translations/translate";
 import { ACCEPT_LEGAL_MUTATION, GET_USER_AUTHORIZATION } from "./queries";
+import { Col, Row } from "react-bootstrap";
+import { Mutation, Query } from "@apollo/react-components";
+import { MutationFunction, QueryResult } from "@apollo/react-common";
+import { Redirect, RouteComponentProps, useHistory } from "react-router-dom";
 
-type WelcomeViewProps = RouteComponentProps;
-
-const welcomeView: React.FC<WelcomeViewProps> = (): JSX.Element => {
+export const WelcomeView: React.FC<RouteComponentProps> = (): JSX.Element => {
   const history: ReturnType<typeof useHistory> = useHistory();
   const [isLegalModalOpen, setLegalModalOpen] = React.useState(true);
 
   const savedUrl: string = _.get(localStorage, "start_url", "/home");
   const initialUrl: string = savedUrl === "/logout" ? "/home" : savedUrl;
 
-  const loadDashboard: (() => void) = (): void => {
+  function loadDashboard(): void {
     localStorage.removeItem("showAlreadyLoggedin");
     localStorage.removeItem("concurrentSession");
     localStorage.removeItem("start_url");
     history.replace(initialUrl);
-  };
+  }
 
   const { userEmail, userName } = window as typeof window & Dictionary<string>;
 
@@ -41,13 +37,18 @@ const welcomeView: React.FC<WelcomeViewProps> = (): JSX.Element => {
       <div className={`${style.container} ${globalStyle.lightFg}`}>
         <div className={style.content}>
           <div className={style.imgDiv}>
-            <img className={style.img} src={logo} alt="logo" /><br />
-            <h1>{translate.t("registration.greeting")} {userName}!</h1>
+            <img alt={"logo"} className={style.img} src={logo} />
+            <br />
+            <h1>
+              {translate.t("registration.greeting")} {userName}
+              {"!"}
+            </h1>
           </div>
-          {localStorage.getItem("showAlreadyLoggedin") === "1"
-            ?
+          {localStorage.getItem("showAlreadyLoggedin") === "1" ? (
             <div>
-              <Row className={style.row}><h3>{translate.t("registration.logged_in_title")}</h3></Row>
+              <Row className={style.row}>
+                <h3>{translate.t("registration.logged_in_title")}</h3>
+              </Row>
               <Row>
                 <Col md={12}>
                   <p>{translate.t("registration.logged_in_message")}</p>
@@ -55,65 +56,75 @@ const welcomeView: React.FC<WelcomeViewProps> = (): JSX.Element => {
               </Row>
               <Row>
                 <Col md={12}>
-                  <Button bsStyle="primary" block={true} onClick={loadDashboard}>
+                  <Button
+                    block={true}
+                    bsStyle={"primary"}
+                    onClick={loadDashboard}
+                  >
                     {translate.t("registration.continue_as_btn")} {userEmail}
                   </Button>
                 </Col>
               </Row>
             </div>
-            :
-            localStorage.getItem("concurrentSession") === "1"
-              ?
-              <div>
-                <Row className={style.row}>
-                  <h3>{translate.t("registration.concurrent_session_message")}</h3>
-                </Row>
-                <Row>
-                  <Col md={12}>
-                    <Button bsStyle="primary" block={true} onClick={loadDashboard}>
-                      {translate.t("registration.continue_btn")}
-                    </Button>
-                  </Col>
-                </Row>
-              </div>
-            :
-            <Query query={GET_USER_AUTHORIZATION} fetchPolicy="network-only">
-              {({ data, loading }: QueryResult): JSX.Element => {
-                if (_.isUndefined(data) || loading) { return <React.Fragment />; }
+          ) : localStorage.getItem("concurrentSession") === "1" ? (
+            <div>
+              <Row className={style.row}>
+                <h3>
+                  {translate.t("registration.concurrent_session_message")}
+                </h3>
+              </Row>
+              <Row>
+                <Col md={12}>
+                  <Button
+                    block={true}
+                    bsStyle={"primary"}
+                    onClick={loadDashboard}
+                  >
+                    {translate.t("registration.continue_btn")}
+                  </Button>
+                </Col>
+              </Row>
+            </div>
+          ) : (
+            <Query fetchPolicy={"network-only"} query={GET_USER_AUTHORIZATION}>
+              {({
+                data,
+                loading,
+              }: QueryResult<{ me: { remember: boolean } }>): JSX.Element => {
+                if (_.isUndefined(data) || loading) {
+                  return <div />;
+                }
 
-                return (
-                  <React.Fragment>
-                    {data.me.remember
-                        ? <Redirect to={initialUrl === "/registration" ? "/home" : initialUrl}/>
-                        :
-                        <Mutation mutation={ACCEPT_LEGAL_MUTATION} onCompleted={loadDashboard}>
-                          {(acceptLegal: MutationFunction): JSX.Element => {
+                return data.me.remember ? (
+                  <Redirect
+                    to={initialUrl === "/registration" ? "/home" : initialUrl}
+                  />
+                ) : (
+                  <Mutation
+                    mutation={ACCEPT_LEGAL_MUTATION}
+                    onCompleted={loadDashboard}
+                  >
+                    {(acceptLegal: MutationFunction): JSX.Element => {
+                      function handleAccept(remember: boolean): void {
+                        setLegalModalOpen(false);
+                        void acceptLegal({ variables: { remember } }).catch();
+                      }
 
-                            const handleAccept: ((remember: boolean) => void) = (remember: boolean): void => {
-                              setLegalModalOpen(false);
-                              acceptLegal({ variables: { remember } })
-                                .catch();
-                            };
-
-                            return (
-                              <CompulsoryNotice
-                                content={translate.t("legalNotice.description")}
-                                onAccept={handleAccept}
-                                open={isLegalModalOpen}
-                              />
-                            );
-                          }}
-                        </Mutation>
-                    }
-                  </React.Fragment>
+                      return (
+                        <CompulsoryNotice
+                          content={translate.t("legalNotice.description")}
+                          onAccept={handleAccept}
+                          open={isLegalModalOpen}
+                        />
+                      );
+                    }}
+                  </Mutation>
                 );
               }}
             </Query>
-          }
+          )}
         </div>
       </div>
     </React.StrictMode>
   );
 };
-
-export { welcomeView as WelcomeView };
