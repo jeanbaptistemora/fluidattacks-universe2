80ba17b4860c100335010ea07b700dab3c37e427
Andres Aguirre <aaguirre@fluidattacks.com>
2020-08-13 17:25:34 -0500

    integrates\fix(front): #2417 error writing result to store
    - Changed the approach of modifying the response object by no propagate
    the response with observer.next
    
    
    Former-commit-id: 64b05b2954e0c9878f50dd4fe2ac05dfdb6ad966

--- front/src/utils/apollo.ts
+++ front/src/utils/apollo.ts
@@ -5,6 +5,7 @@
 */
 import { ApolloClient } from "apollo-client";
 import { ApolloProvider as BaseApolloProvider } from "@apollo/react-hooks";
+import { ErrorResponse } from "apollo-link-error";
 import Logger from "./logger";
 import React from "react";
 import { WebSocketLink } from "apollo-link-ws";
@@ -18,18 +19,34 @@ import translate from "./translations/translate";
 import { useHistory } from "react-router";
 import {
   ApolloLink,
+  ExecutionResult,
   FetchResult,
   NextLink,
   Observable,
   Operation,
 } from "apollo-link";
-import { ErrorHandler, ErrorResponse } from "apollo-link-error";
 import {
   FragmentDefinitionNode,
   GraphQLError,
   OperationDefinitionNode,
 } from "graphql";
 import { InMemoryCache, NormalizedCacheObject } from "apollo-cache-inmemory";
+import { ServerError, ServerParseError } from "apollo-link-http-common";
+
+interface IHandledErrorAttr {
+  graphQLErrors?: readonly GraphQLError[];
+  networkError?: Error | ServerError | ServerParseError;
+  skipForwarding?: () => void;
+  response?: ExecutionResult;
+  operation: Operation;
+  forward: NextLink;
+}
+
+interface IErrorHandlerAttr {
+  // It can return a void type according to apollo-link
+  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+  (error: IHandledErrorAttr): Observable<FetchResult> | void;
+}
 
 const getCookie: (name: string) => string = (name: string): string => {
   if (document.cookie !== "") {
@@ -144,8 +161,8 @@ const apiLink: ApolloLink = ApolloLink.split(
  * @see https://github.com/apollographql/react-apollo/issues/1548
  * @see https://github.com/apollographql/apollo-link/issues/855
  */
-const onError: (errorHandler: ErrorHandler) => ApolloLink = (
-  errorHandler: ErrorHandler
+const onError: (errorHandler: IErrorHandlerAttr) => ApolloLink = (
+  errorHandler: IErrorHandlerAttr
 ): ApolloLink =>
   new ApolloLink(
     (operation: Operation, forward: NextLink): Observable<FetchResult> =>
@@ -171,15 +188,26 @@ const onError: (errorHandler: ErrorHandler) => ApolloLink = (
                   });
                 },
                 next: (result: FetchResult): void => {
+                  // It is necessary to change the variable value
+                  // eslint-disable-next-line fp/no-let
+                  let isForwarded: boolean = true;
+                  const skipForwarding: () => void = (): void => {
+                    isForwarded = false;
+                  };
                   if (result.errors !== undefined) {
                     errorHandler({
                       forward,
                       graphQLErrors: result.errors,
                       operation,
                       response: result,
+                      skipForwarding: skipForwarding,
                     });
                   }
-                  observer.next(result);
+                  // isForwarded can change its value
+                  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+                  if (isForwarded) {
+                    observer.next(result);
+                  }
                 },
               });
             } catch (exception) {
@@ -205,54 +233,63 @@ type History = ReturnType<typeof useHistory>;
 const errorLink: (history: History) => ApolloLink = (
   history: History
 ): ApolloLink =>
-  onError(({ graphQLErrors, networkError, response }: ErrorResponse): void => {
-    if (networkError !== undefined) {
-      const { statusCode } = networkError as { statusCode?: number };
-      const forbidden: number = 403;
+  onError(
+    ({
+      graphQLErrors,
+      networkError,
+      response,
+      skipForwarding,
+    }: IHandledErrorAttr): void => {
+      if (networkError !== undefined) {
+        const { statusCode } = networkError as { statusCode?: number };
+        const forbidden: number = 403;
 
-      switch (statusCode) {
-        case undefined:
-          msgError(translate.t("group_alerts.error_network"), "Offline");
-          break;
-        case forbidden:
-          // Django CSRF expired
-          location.reload();
-          break;
-        default:
-          msgError(translate.t("group_alerts.error_textsad"));
-          Logger.warning("A network error occurred", { ...networkError });
-      }
-    } else {
-      if (graphQLErrors !== undefined) {
-        graphQLErrors.forEach((error: GraphQLError): void => {
-          switch (error.message) {
-            case "Login required":
-            case "Exception - User token has expired":
-              if (response !== undefined) {
-                response.data = undefined;
-                response.errors = [];
-              }
-              location.assign("/integrates/logout");
-              break;
-            case "Access denied":
-            case "Exception - Event not found":
-            case "Exception - Finding not found":
-            case "Exception - Organization name is invalid":
-            case "Exception - Project does not exist":
-              if (response !== undefined) {
-                response.data = undefined;
-                response.errors = [];
-              }
-              msgError(translate.t("group_alerts.access_denied"));
-              history.replace("/home");
-              break;
-            default:
-            // Propagate
-          }
-        });
+        switch (statusCode) {
+          case undefined:
+            msgError(translate.t("group_alerts.error_network"), "Offline");
+            break;
+          case forbidden:
+            // Django CSRF expired
+            location.reload();
+            break;
+          default:
+            msgError(translate.t("group_alerts.error_textsad"));
+            Logger.warning("A network error occurred", { ...networkError });
+        }
+      } else {
+        if (graphQLErrors !== undefined) {
+          graphQLErrors.forEach((error: GraphQLError): void => {
+            switch (error.message) {
+              case "Login required":
+              case "Exception - User token has expired":
+                if (response !== undefined) {
+                  if (_.isFunction(skipForwarding)) {
+                    skipForwarding();
+                  }
+                }
+                location.assign("/integrates/logout");
+                break;
+              case "Access denied":
+              case "Exception - Event not found":
+              case "Exception - Finding not found":
+              case "Exception - Organization name is invalid":
+              case "Exception - Project does not exist":
+                if (response !== undefined) {
+                  if (_.isFunction(skipForwarding)) {
+                    skipForwarding();
+                  }
+                }
+                msgError(translate.t("group_alerts.access_denied"));
+                history.replace("/home");
+                break;
+              default:
+              // Propagate
+            }
+          });
+        }
       }
     }
-  });
+  );
 
 type ProviderProps = Omit<
   React.ComponentProps<typeof BaseApolloProvider>,
