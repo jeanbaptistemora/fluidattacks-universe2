1a1bde6169298f8d3f79caa576d4729066563eea
Andres Cuberos <acuberos@fluidattacks.com>
2020-08-13 22:33:54 -0500

    integrates\test(front): #2151.54 increase org policies cov
    - Add tests to increase the coverage of the OrganizationPolicies
      component.
    
    
    Former-commit-id: d692159120f27cd21a40fecd14c207eb0eb02292

--- integrates/front/src/scenes/Dashboard/containers/OrganizationPoliciesView/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/containers/OrganizationPoliciesView/index.spec.tsx
@@ -11,6 +11,7 @@ import waitForExpect from "wait-for-expect";
 import store from "../../../../store";
 import { authzPermissionsContext } from "../../../../utils/authz/config";
 import { msgError, msgSuccess } from "../../../../utils/notifications";
+import translate from "../../../../utils/translations/translate";
 import { OrganizationPolicies } from "./index";
 import { GET_ORGANIZATION_POLICIES, UPDATE_ORGANIZATION_POLICIES } from "./queries";
 import { IOrganizationPolicies } from "./types";
@@ -31,7 +32,7 @@ describe("Organization policies view", () => {
       .toEqual("function");
   });
 
-  it("should render component", async (): Promise<void> => {
+  it("should render component with default values", async (): Promise<void> => {
     const mocks: ReadonlyArray<MockedResponse> = [
       {
         request: {
@@ -43,11 +44,12 @@ describe("Organization policies view", () => {
         result: {
           data: {
             organization: {
-              maxAcceptanceDays: 5,
-              maxAcceptanceSeverity: 7.5,
-              // tslint:disable-next-line: no-null-keyword
+              // tslint:disable: no-null-keyword
+              maxAcceptanceDays: null,
+              maxAcceptanceSeverity: 10,
               maxNumberAcceptations: null,
-              minAcceptanceSeverity: 3,
+              minAcceptanceSeverity: 0,
+              // tslint:enable: no-null-keyword
             },
           },
         },
@@ -81,14 +83,14 @@ describe("Organization policies view", () => {
         .find({ name: "maxAcceptanceDays" })
         .find("input")
         .prop("value"))
-      .toBe("5");
+      .toBe("");
 
     expect(
       wrapper
         .find({ name: "maxAcceptanceSeverity" })
         .find("input")
         .prop("value"))
-      .toBe("7.5");
+      .toBe("10.0");
 
     expect(
       wrapper
@@ -102,7 +104,7 @@ describe("Organization policies view", () => {
         .find({ name: "minAcceptanceSeverity" })
         .find("input")
         .prop("value"))
-      .toBe("3.0");
+      .toBe("0.0");
   });
 
   it("should render an error message", async () => {
@@ -157,8 +159,7 @@ describe("Organization policies view", () => {
             organization: {
               maxAcceptanceDays: 5,
               maxAcceptanceSeverity: 7.5,
-              // tslint:disable-next-line: no-null-keyword
-              maxNumberAcceptations: null,
+              maxNumberAcceptations: 5,
               minAcceptanceSeverity: 3,
             },
           },
@@ -285,6 +286,272 @@ describe("Organization policies view", () => {
             .find("input")
             .prop("value"))
           .toBe("2");
+      });
+    });
+  });
+
+  it("should not show save button", async () => {
+    const mocks: ReadonlyArray<MockedResponse> = [
+      {
+        request: {
+          query: GET_ORGANIZATION_POLICIES,
+          variables: {
+            organizationId: mockProps.organizationId,
+          },
+        },
+        result: {
+          data: {
+            organization: {
+              maxAcceptanceDays: 5,
+              maxAcceptanceSeverity: 7.5,
+              maxNumberAcceptations: 2,
+              minAcceptanceSeverity: 3,
+            },
+          },
+        },
+      },
+    ];
+    const wrapper: ReactWrapper = mount(
+      <MemoryRouter initialEntries={["/orgs/imamura/policies"]}>
+        <Provider store={store}>
+          <MockedProvider mocks={mocks} addTypename={false}>
+            <Route path="/orgs/:organizationName/policies">
+              <OrganizationPolicies {...mockProps} />
+            </Route>
+          </MockedProvider>
+        </Provider>
+      </MemoryRouter>,
+    );
+
+    await act(async () => {
+      await waitForExpect(() => {
+        wrapper.update();
+
+        expect(wrapper)
+          .toHaveLength(1);
+        expect(wrapper.find("tr"))
+          .toHaveLength(4);
+      });
+    });
+
+    const maxAcceptanceDays: ReactWrapper = wrapper
+      .find({ name: "maxAcceptanceDays" })
+      .find("input");
+    let saveButton: ReactWrapper = wrapper
+      .find("button")
+      .filterWhere((element: ReactWrapper) => element.contains("Save"))
+      .first();
+
+    expect(saveButton)
+      .toHaveLength(0);
+
+    maxAcceptanceDays.simulate("change", { target: { value: "2" }});
+
+    await act(async () => {
+      await waitForExpect(() => {
+        wrapper.update();
+
+        saveButton = wrapper
+          .find("button")
+          .filterWhere((element: ReactWrapper) => element.contains("Save"))
+          .first();
+        expect(saveButton)
+          .toHaveLength(0);
+      });
+    });
+  });
+
+  it("should handle errors", async () => {
+    const mocks: ReadonlyArray<MockedResponse> = [
+      {
+        request: {
+          query: GET_ORGANIZATION_POLICIES,
+          variables: {
+            organizationId: mockProps.organizationId,
+          },
+        },
+        result: {
+          data: {
+            organization: {
+              maxAcceptanceDays: 5,
+              maxAcceptanceSeverity: 7.5,
+              maxNumberAcceptations: 2,
+              minAcceptanceSeverity: 3,
+            },
+          },
+        },
+      },
+      {
+        request: {
+          query: UPDATE_ORGANIZATION_POLICIES,
+          variables: {
+            maxAcceptanceDays: 1,
+            maxAcceptanceSeverity: 7.5,
+            maxNumberAcceptations: 2,
+            minAcceptanceSeverity: 3,
+            organizationId: mockProps.organizationId,
+            organizationName: "imamura",
+          },
+        },
+        result: {
+          errors: [new GraphQLError("Exception - Acceptance days should be a positive integer")],
+        },
+      },
+      {
+        request: {
+          query: UPDATE_ORGANIZATION_POLICIES,
+          variables: {
+            maxAcceptanceDays: 1,
+            maxAcceptanceSeverity: 7.5,
+            maxNumberAcceptations: 2,
+            minAcceptanceSeverity: 3,
+            organizationId: mockProps.organizationId,
+            organizationName: "imamura",
+          },
+        },
+        result: {
+          errors: [new GraphQLError(
+            "Exception - Severity value should be a positive floating number between 0.0 a 10.0",
+          )],
+        },
+      },
+      {
+        request: {
+          query: UPDATE_ORGANIZATION_POLICIES,
+          variables: {
+            maxAcceptanceDays: 1,
+            maxAcceptanceSeverity: 7.5,
+            maxNumberAcceptations: 2,
+            minAcceptanceSeverity: 3,
+            organizationId: mockProps.organizationId,
+            organizationName: "imamura",
+          },
+        },
+        result: {
+          errors: [new GraphQLError(
+            "Exception - Min acceptance severity value should not be higher than the max value",
+          )],
+        },
+      },
+      {
+        request: {
+          query: UPDATE_ORGANIZATION_POLICIES,
+          variables: {
+            maxAcceptanceDays: 1,
+            maxAcceptanceSeverity: 7.5,
+            maxNumberAcceptations: 2,
+            minAcceptanceSeverity: 3,
+            organizationId: mockProps.organizationId,
+            organizationName: "imamura",
+          },
+        },
+        result: {
+          errors: [new GraphQLError("Exception - Number of acceptations should be zero or positive")],
+        },
+      },
+      {
+        request: {
+          query: UPDATE_ORGANIZATION_POLICIES,
+          variables: {
+            maxAcceptanceDays: 1,
+            maxAcceptanceSeverity: 7.5,
+            maxNumberAcceptations: 2,
+            minAcceptanceSeverity: 3,
+            organizationId: mockProps.organizationId,
+            organizationName: "imamura",
+          },
+        },
+        result: {
+          errors: [new GraphQLError("Access denied")],
+        },
+      },
+    ];
+    const mockedPermissions: PureAbility<string> = new PureAbility([
+      { action: "backend_api_resolvers_organization__do_update_organization_policies" },
+    ]);
+    const wrapper: ReactWrapper = mount(
+      <MemoryRouter initialEntries={["/orgs/imamura/policies"]}>
+        <Provider store={store}>
+          <MockedProvider mocks={mocks} addTypename={false}>
+            <Route path="/orgs/:organizationName/policies">
+              <authzPermissionsContext.Provider value={mockedPermissions}>
+                <OrganizationPolicies {...mockProps} />
+              </authzPermissionsContext.Provider>
+            </Route>
+          </MockedProvider>
+        </Provider>
+      </MemoryRouter>,
+    );
+
+    await act(async () => {
+      await waitForExpect(() => {
+        wrapper.update();
+
+        expect(wrapper)
+          .toHaveLength(1);
+        expect(wrapper.find("tr"))
+          .toHaveLength(4);
+      });
+    });
+
+    const form: ReactWrapper = wrapper.find("genericForm");
+    const maxAcceptanceDays: ReactWrapper = wrapper
+      .find({ name: "maxAcceptanceDays" })
+      .find("input");
+
+    maxAcceptanceDays.simulate("change", { target: { value: "1" }});
+    form.simulate("submit");
+
+    await act(async () => {
+      await waitForExpect(() => {
+        wrapper.update();
+
+        expect(msgError)
+          .toBeCalledWith(translate.t("organization.tabs.policies.errors.maxAcceptanceDays"));
+      });
+    });
+
+    form.simulate("submit");
+
+    await act(async () => {
+      await waitForExpect(() => {
+        wrapper.update();
+
+        expect(msgError)
+          .toBeCalledWith(translate.t("organization.tabs.policies.errors.acceptanceSeverity"));
+      });
+    });
+
+    form.simulate("submit");
+
+    await act(async () => {
+      await waitForExpect(() => {
+        wrapper.update();
+
+        expect(msgError)
+          .toBeCalledWith(translate.t("organization.tabs.policies.errors.acceptanceSeverityRange"));
+      });
+    });
+
+    form.simulate("submit");
+
+    await act(async () => {
+      await waitForExpect(() => {
+        wrapper.update();
+
+        expect(msgError)
+          .toBeCalledWith(translate.t("organization.tabs.policies.errors.maxNumberAcceptations"));
+      });
+    });
+
+    form.simulate("submit");
+
+    await act(async () => {
+      await waitForExpect(() => {
+        wrapper.update();
+
+        expect(msgError)
+          .toBeCalledWith(translate.t("group_alerts.error_textsad"));
       });
     });
   });
