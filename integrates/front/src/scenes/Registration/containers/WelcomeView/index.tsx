c209d5e335e63c51a9ddd18d4c220d762cf09008
Andres Lemus <alemus@fluidattacks.com>
2020-08-14 10:02:24 -0500

    integrates\refac(front): #2101.1 migrate welcome_view
    Former-commit-id: 89bcc9e9ed456637bbf3a5a9d507f5a0ee79d15c

--- integrates/front/src/scenes/Registration/containers/WelcomeView/index.tsx
+++ integrates/front/src/scenes/Registration/containers/WelcomeView/index.tsx
@@ -2,27 +2,31 @@
   ------
   We need to override default styles from react-bootstrap
 */
+import { ApolloError } from "apollo-client";
 import { Button } from "../../../../components/Button";
 import { CompulsoryNotice } from "../../components/CompulsoryNotice";
+import { GraphQLError } from "graphql";
+import Logger from "../../../../utils/logger";
 import React from "react";
 import _ from "lodash";
 import { default as globalStyle } from "../../../../styles/global.css";
 import { default as logo } from "../../../../resources/integrates.svg";
 import { default as style } from "./index.css";
 import translate from "../../../../utils/translations/translate";
+import { useHistory } from "react-router-dom";
 import { ACCEPT_LEGAL_MUTATION, GET_USER_AUTHORIZATION } from "./queries";
 import { Col, Row } from "react-bootstrap";
-import { Mutation, Query } from "@apollo/react-components";
-import { MutationFunction, QueryResult } from "@apollo/react-common";
-import { Redirect, useHistory } from "react-router-dom";
+import { useMutation, useQuery } from "@apollo/react-hooks";
 
 export const WelcomeView: React.FC = (): JSX.Element => {
-  const history: ReturnType<typeof useHistory> = useHistory();
-  const [isLegalModalOpen, setLegalModalOpen] = React.useState(true);
+  const { userEmail, userName } = window as typeof window &
+    Record<string, string>;
 
+  // Load on last visited url
   const savedUrl: string = _.get(localStorage, "start_url", "/home");
-  const initialUrl: string = savedUrl === "/logout" ? "/home" : savedUrl;
-
+  const initialUrl: string =
+    savedUrl === "/logout" || savedUrl === "/registration" ? "/home" : savedUrl;
+  const history: ReturnType<typeof useHistory> = useHistory();
   function loadDashboard(): void {
     localStorage.removeItem("showAlreadyLoggedin");
     localStorage.removeItem("concurrentSession");
@@ -30,7 +34,42 @@ export const WelcomeView: React.FC = (): JSX.Element => {
     history.replace(initialUrl);
   }
 
-  const { userEmail, userName } = window as typeof window & Dictionary<string>;
+  // Display legal notice
+  const { data, loading } = useQuery<{ me: { remember: boolean } }>(
+    GET_USER_AUTHORIZATION,
+    {
+      fetchPolicy: "network-only",
+      onCompleted: (userData: { me: { remember: boolean } }): void => {
+        if (userData.me.remember) {
+          loadDashboard();
+        }
+      },
+      onError: ({ graphQLErrors }: ApolloError): void => {
+        graphQLErrors.forEach((error: GraphQLError): void => {
+          Logger.error(
+            "An error occurred while fetching user authorization",
+            error
+          );
+        });
+      },
+    }
+  );
+  const [acceptLegal] = useMutation(ACCEPT_LEGAL_MUTATION, {
+    onCompleted: loadDashboard,
+    onError: ({ graphQLErrors }: ApolloError): void => {
+      graphQLErrors.forEach((error: GraphQLError): void => {
+        Logger.error(
+          "An error occurred while accepting user legal notice",
+          error
+        );
+      });
+    },
+  });
+  const [isLegalModalOpen, setLegalModalOpen] = React.useState(true);
+  function handleAccept(remember: boolean): void {
+    setLegalModalOpen(false);
+    void acceptLegal({ variables: { remember } });
+  }
 
   return (
     <div className={`${style.container} ${globalStyle.lightFg}`}>
@@ -83,42 +122,13 @@ export const WelcomeView: React.FC = (): JSX.Element => {
             </Row>
           </div>
         ) : (
-          <Query fetchPolicy={"network-only"} query={GET_USER_AUTHORIZATION}>
-            {({
-              data,
-              loading,
-            }: QueryResult<{ me: { remember: boolean } }>): JSX.Element => {
-              if (_.isUndefined(data) || loading) {
-                return <div />;
-              }
-
-              return data.me.remember ? (
-                <Redirect
-                  to={initialUrl === "/registration" ? "/home" : initialUrl}
-                />
-              ) : (
-                <Mutation
-                  mutation={ACCEPT_LEGAL_MUTATION}
-                  onCompleted={loadDashboard}
-                >
-                  {(acceptLegal: MutationFunction): JSX.Element => {
-                    function handleAccept(remember: boolean): void {
-                      setLegalModalOpen(false);
-                      void acceptLegal({ variables: { remember } }).catch();
-                    }
-
-                    return (
-                      <CompulsoryNotice
-                        content={translate.t("legalNotice.description")}
-                        onAccept={handleAccept}
-                        open={isLegalModalOpen}
-                      />
-                    );
-                  }}
-                </Mutation>
-              );
-            }}
-          </Query>
+          !(_.isUndefined(data) || loading || data.me.remember) && (
+            <CompulsoryNotice
+              content={translate.t("legalNotice.description")}
+              onAccept={handleAccept}
+              open={isLegalModalOpen}
+            />
+          )
         )}
       </div>
     </div>
