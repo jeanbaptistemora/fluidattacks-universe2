e246bc483f684e2ccafbfe2fffb8d9b946c9c4a6
Andres Cuberos <acuberos@fluidattacks.com>
2020-08-13 21:06:27 -0500

    integrates\test(front): #2151.53 increase org policies cov
    - Add tests to increase coverage of the OrganizationPolicies
      component.
    
    
    Former-commit-id: 1c39452b9f2362cf152b3a6c5a5ab7fd035bef52

--- integrates/front/src/scenes/Dashboard/containers/OrganizationPoliciesView/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/containers/OrganizationPoliciesView/index.spec.tsx
@@ -1,4 +1,5 @@
 import { MockedProvider, MockedResponse } from "@apollo/react-testing";
+import { PureAbility } from "@casl/ability";
 import { mount, ReactWrapper } from "enzyme";
 import { GraphQLError } from "graphql";
 import React from "react";
@@ -8,14 +9,16 @@ import { Provider } from "react-redux";
 import { MemoryRouter, Route } from "react-router";
 import waitForExpect from "wait-for-expect";
 import store from "../../../../store";
-import { msgError } from "../../../../utils/notifications";
+import { authzPermissionsContext } from "../../../../utils/authz/config";
+import { msgError, msgSuccess } from "../../../../utils/notifications";
 import { OrganizationPolicies } from "./index";
-import { GET_ORGANIZATION_POLICIES } from "./queries";
+import { GET_ORGANIZATION_POLICIES, UPDATE_ORGANIZATION_POLICIES } from "./queries";
 import { IOrganizationPolicies } from "./types";
 
 jest.mock("../../../../utils/notifications", (): Dictionary => {
   const mockedNotifications: Dictionary = jest.requireActual("../../../../utils/notifications");
   mockedNotifications.msgError = jest.fn();
+  mockedNotifications.msgSuccess = jest.fn();
 
   return mockedNotifications;
 });
@@ -136,6 +139,152 @@ describe("Organization policies view", () => {
           .toHaveBeenCalled();
         expect(wrapper.find("table"))
           .toHaveLength(0);
+      });
+    });
+  });
+
+  it("should update the policies", async () => {
+    const mocks: ReadonlyArray<MockedResponse> = [
+      {
+        request: {
+          query: GET_ORGANIZATION_POLICIES,
+          variables: {
+            organizationId: mockProps.organizationId,
+          },
+        },
+        result: {
+          data: {
+            organization: {
+              maxAcceptanceDays: 5,
+              maxAcceptanceSeverity: 7.5,
+              // tslint:disable-next-line: no-null-keyword
+              maxNumberAcceptations: null,
+              minAcceptanceSeverity: 3,
+            },
+          },
+        },
+      },
+      {
+        request: {
+          query: UPDATE_ORGANIZATION_POLICIES,
+          variables: {
+            maxAcceptanceDays: 2,
+            maxAcceptanceSeverity: 8.9,
+            maxNumberAcceptations: 1,
+            // tslint:disable-next-line: no-null-keyword
+            minAcceptanceSeverity: null,
+            organizationId: mockProps.organizationId,
+            organizationName: "imamura",
+          },
+        },
+        result: {
+          data: {
+            updateOrganizationPolicies: {
+              success: true,
+            },
+          },
+        },
+      },
+      {
+        request: {
+          query: GET_ORGANIZATION_POLICIES,
+          variables: {
+            organizationId: mockProps.organizationId,
+          },
+        },
+        result: {
+          data: {
+            organization: {
+              maxAcceptanceDays: 2,
+              maxAcceptanceSeverity: 8.9,
+              maxNumberAcceptations: 1,
+              minAcceptanceSeverity: 0,
+            },
+          },
+        },
+      },
+    ];
+    const mockedPermissions: PureAbility<string> = new PureAbility([
+      { action: "backend_api_resolvers_organization__do_update_organization_policies" },
+    ]);
+    const wrapper: ReactWrapper = mount(
+      <MemoryRouter initialEntries={["/orgs/imamura/policies"]}>
+        <Provider store={store}>
+          <MockedProvider mocks={mocks} addTypename={false}>
+            <Route path="/orgs/:organizationName/policies">
+              <authzPermissionsContext.Provider value={mockedPermissions}>
+                <OrganizationPolicies {...mockProps} />
+              </authzPermissionsContext.Provider>
+            </Route>
+          </MockedProvider>
+        </Provider>
+      </MemoryRouter>,
+    );
+
+    await act(async () => {
+      await waitForExpect(() => {
+        wrapper.update();
+
+        expect(wrapper)
+          .toHaveLength(1);
+        expect(wrapper.find("tr"))
+          .toHaveLength(4);
+      });
+    });
+
+    const form: ReactWrapper = wrapper.find("genericForm");
+    const maxAcceptanceDays: ReactWrapper = wrapper
+      .find({ name: "maxAcceptanceDays" })
+      .find("input");
+    const maxAcceptanceSeverity: ReactWrapper = wrapper
+      .find({ name: "maxAcceptanceSeverity" })
+      .find("input");
+    const maxNumberAcceptations: ReactWrapper = wrapper
+      .find({ name: "maxNumberAcceptations" })
+      .find("input");
+    const minAcceptanceSeverity: ReactWrapper = wrapper
+      .find({ name: "minAcceptanceSeverity" })
+      .find("input");
+    let saveButton: ReactWrapper = wrapper
+      .find("button")
+      .filterWhere((element: ReactWrapper) => element.contains("Save"))
+      .first();
+
+    expect(saveButton)
+      .toHaveLength(0);
+
+    maxAcceptanceDays.simulate("change", { target: { value: "2" }});
+    maxAcceptanceSeverity.simulate("change", { target: { value: "8.9" }});
+    maxNumberAcceptations.simulate("change", { target: { value: "1" }});
+    minAcceptanceSeverity.simulate("change", { target: { value: "" } });
+
+    await act(async () => {
+      await waitForExpect(() => {
+        wrapper.update();
+
+        saveButton = wrapper
+          .find("button")
+          .filterWhere((element: ReactWrapper) => element.contains("Save"))
+          .first();
+        expect(saveButton)
+          .toHaveLength(1);
+      });
+    });
+
+    form.simulate("submit");
+
+    await act(async () => {
+      await waitForExpect(() => {
+        wrapper.update();
+
+        expect(msgSuccess)
+          .toHaveBeenCalled();
+        expect(
+          wrapper
+            .find({ name: "maxAcceptanceDays" })
+            .find("input")
+            .prop("value"))
+          .toBe("2");
       });
     });
   });
