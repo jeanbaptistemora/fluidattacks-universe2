fb7b66c2756531b246176a182ac30c669dc35fdc
Kevin Amado <kamado@fluidattacks.com>
2020-08-18 12:17:28 -0500

    skims\feat(back): #2294.161 jquery vulns
    Former-commit-id: 93b557dae01648b3d12db6f4d856fd0c8688cd64

--- skims/skims/nvd/local.py
+++ skims/skims/nvd/local.py
@@ -20,11 +20,47 @@ DATABASE: Dict[str, Dict[str, List[List[str]]]] = {
             ['>=5.0.0', '<5.0.3'],
         ],
     },
+    'jquery': {
+        'CVE-2020-11022': [
+            ['>=1.2.0', '<3.5.0'],
+        ],
+        'CVE-2020-7656': [
+            ['<1.9.0'],
+        ],
+        'CVE-2019-11358': [
+            ['<3.4.0'],
+        ],
+        'CVE-2019-5428': [
+            ['<3.4.0'],
+        ],
+        'CVE-2017-16012': [
+            ['<3.0.0'],
+        ],
+        'CVE-2017-16011': [
+            ['>=1.7.1', '<1.9.0'],
+        ],
+        'CVE-2015-9251': [
+            ['<3.0.0'],
+        ],
+        'CVE-2014-6071': [
+            ['=1.4.2'],
+        ],
+        'CVE-2012-6708': [
+            ['>=1.7.1', '<1.9.0'],
+        ],
+        'CVE-2011-4969': [
+            ['<1.6.3'],
+        ],
+    },
 }
+"""Dictionary mapping products to their manually verified list of CVE."""
+
 IGNORED_CHARS = str.maketrans('', '', ''.join({'^', '~'}))
 
 
 def does_version_match(version: str, condition: str) -> bool:
+    """Given a version and a condition return True if version match condition.
+    """
     with suppress(ValueError):
         return match(version, condition)
     return False
