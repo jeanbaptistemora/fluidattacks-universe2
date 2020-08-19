cca8781b1437a4ee78e24cdcbe3331231813040a
Daniel Salazar <dsalazar@fluidattacks.com>
2020-08-19 16:41:49 -0500

    serves\feat(build): #2440.1 commit msg
    - Prepare tests to accept serves in commit and mr
    messages
    
    
    Former-commit-id: 95fc5cb4f835a4ad243a3cbe81f7d6df575ce6b5

--- .commitlint-parser-preset.js
+++ .commitlint-parser-preset.js
@@ -1,6 +1,6 @@
 module.exports = {
   parserOpts: {
-    headerPattern: /^(all|forces|integrates|reviews|skims|sorts)\\(\w*)\((\w*)\):\s(#[1-9]\d*)(.\d+)?\s(.*)$/,
+    headerPattern: /^(all|forces|integrates|reviews|skims|serves|sorts)\\(\w*)\((\w*)\):\s(#[1-9]\d*)(.\d+)?\s(.*)$/,
     headerCorrespondence: [ 'product', 'type', 'scope', 'ticket', 'part', 'subject' ],
   },
 };
