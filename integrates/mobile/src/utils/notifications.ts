116ee856a0edf2f792835d8d25b50c20fe629f6b
Andres Aguirre <aaguirre@fluidattacks.com>
2020-08-21 17:01:18 -0500

    integrates\fix(front): #2489 expo push token error
    - Catched the network error which is throw from getExpoPushTokenAsync
    to handle it
    
    
    Former-commit-id: a3eaca68bea094a0983a831742720fde36a981a1

--- integrates/mobile/src/utils/notifications.ts
+++ integrates/mobile/src/utils/notifications.ts
@@ -3,6 +3,8 @@ import { default as Constants } from "expo-constants";
 import * as Permissions from "expo-permissions";
 import { Platform } from "react-native";
 
+import { LOGGER } from "./logger";
+
 const getToken: (() => Promise<string>) = async (
 ): Promise<string> => {
   const token: string = await Notifications.getExpoPushTokenAsync();
@@ -25,18 +27,22 @@ export const getPushToken: (() => Promise<string>) = async (
    * @see https://docs.expo.io/versions/latest/sdk/notifications/#features
    */
   if (Constants.isDevice) {
-    const {
-      status: currentStatus,
-    } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    try {
+      const {
+        status: currentStatus,
+      } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
 
-    if (currentStatus === Permissions.PermissionStatus.GRANTED) {
-      return getToken();
-    }
+      if (currentStatus === Permissions.PermissionStatus.GRANTED) {
+        return getToken();
+      }
 
-    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
 
-    if (status === Permissions.PermissionStatus.GRANTED) {
-      return getToken();
+      if (status === Permissions.PermissionStatus.GRANTED) {
+        return getToken();
+      }
+    } catch (error) {
+      LOGGER.error("Couldn't get push token", error);
     }
 
     return "";
