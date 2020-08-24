5a47af2dcaecef3292d9d5df85bf9510cd17fdcd
David Acevedo <dacevedo@fluidattacks.com>
2020-08-24 18:33:17 -0500

    integrates\feat(infra): #2166.6 add permissions
    - include integrates dev for branch pipelines
    - add necessary permissions for the job
    
    
    Former-commit-id: 7e772fb29d3cf944b2002857e9365e4ec7087ee0

--- integrates/deploy/devicefarm/terraform/variables.tf
+++ integrates/deploy/devicefarm/terraform/variables.tf
@@ -7,11 +7,18 @@ data "aws_iam_policy_document" "device-farm" {
     principals {
       type        = "AWS"
       identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user-provision/integrates-dev",
         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user-provision/integrates-prod",
       ]
     }
     actions = [
+      "devicefarm:CreateDevicePool",
       "devicefarm:CreateProject",
+      "devicefarm:CreateUpload",
+      "devicefarm:GetRun",
+      "devicefarm:GetUpload",
+      "devicefarm:ListProjects",
+      "devicefarm:ScheduleRun",
     ]
     resources = [
       "*"
