84eb7e4cfe902354bab84e5c31a73fff4586248c
Daniel Salazar <dsalazar@fluidattacks.com>
2020-08-25 13:26:02 -0500

    observes\feat(infra): #2959.11 prod kms key
    - Add kms key for secrets
    
    
    Former-commit-id: 92bec94312e3d0f778db44dcdf7125b124710c97

---
+++ observes/infra/terraform/prod-key.tf
@@ -0,0 +1,109 @@
+data "aws_iam_policy_document" "key-prod" {
+
+  statement {
+    sid    = "Enable IAM User Permissions"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions = [
+      "kms:*"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid    = "Key Administrators"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user-provision/observes-prod",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/serves-admin"
+      ]
+    }
+    actions = [
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:TagResource",
+      "kms:UntagResource",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion",
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid    = "Key Users"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user-provision/observes-prod",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/serves-admin"
+      ]
+    }
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid    = "Attachment Of Persistent Resources"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user-provision/observes-prod",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/serves-admin"
+      ]
+    }
+    actions = [
+      "kms:CreateGrant",
+      "kms:ListGrants",
+      "kms:RevokeGrant",
+    ]
+    resources = [
+      "*"
+    ]
+    condition {
+      test     = "Bool"
+      variable = "kms:GrantIsForAWSResource"
+      values   = [
+        "true",
+      ]
+    }
+  }
+}
+
+resource "aws_kms_key" "key-prod" {
+  description             = "prod kms key for observes."
+  policy                  = data.aws_iam_policy_document.key-prod.json
+  deletion_window_in_days = 30
+  is_enabled              = true
+}
+
+resource "aws_kms_alias" "key-prod" {
+  name          = "alias/observes-prod"
+  target_key_id = aws_kms_key.key-prod.key_id
+}
