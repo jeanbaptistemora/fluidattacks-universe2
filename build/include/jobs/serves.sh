c10996e7ea4bb33b06025c0f136d8b26e8ae351e
Daniel Salazar <dsalazar@fluidattacks.com>
2020-08-24 20:00:08 -0500

    serves\feat(build): #2959.8 user provision rotation
    - Add rotation job for observes user provision credentials
    
    
    Former-commit-id: f2e3db77334ff823494f335f6459621b58ac4842

--- build/include/jobs/serves.sh
+++ build/include/jobs/serves.sh
@@ -646,6 +646,53 @@ function job_serves_rotate_keys_user_provision_serves {
   ||  return 1
 }
 
+function job_serves_rotate_keys_user_provision_observes {
+  local terraform_dir='services/user-provision/serves/terraform'
+  local gitlab_repo_id='4620828'
+
+  # Dev
+  local dev_resource_to_taint='aws_iam_access_key.dev-key'
+  local dev_output_key_id_name='dev-secret-key-id'
+  local dev_output_secret_key_name='dev-secret-key'
+  local dev_gitlab_key_id_name='OBSERVES_DEV_AWS_ACCESS_KEY_ID'
+  local dev_gitlab_secret_key_name='OBSERVES_DEV_AWS_SECRET_ACCESS_KEY'
+  local dev_gitlab_masked='true'
+  local dev_gitlab_protected='false'
+
+  # Prod
+  local prod_resource_to_taint='aws_iam_access_key.prod-key'
+  local prod_output_key_id_name='prod-secret-key-id'
+  local prod_output_secret_key_name='prod-secret-key'
+  local prod_gitlab_key_id_name='OBSERVES_PROD_AWS_ACCESS_KEY_ID'
+  local prod_gitlab_secret_key_name='OBSERVES_PROD_AWS_SECRET_ACCESS_KEY'
+  local prod_gitlab_masked='true'
+  local prod_gitlab_protected='true'
+
+      pushd serves \
+  &&  helper_serves_user_provision_rotate_keys \
+        "${terraform_dir}" \
+        "${dev_resource_to_taint}" \
+        "${dev_output_key_id_name}" \
+        "${dev_output_secret_key_name}" \
+        "${gitlab_repo_id}" \
+        "${dev_gitlab_key_id_name}" \
+        "${dev_gitlab_secret_key_name}" \
+        "${dev_gitlab_masked}" \
+        "${dev_gitlab_protected}" \
+  &&  helper_serves_user_provision_rotate_keys \
+        "${terraform_dir}" \
+        "${prod_resource_to_taint}" \
+        "${prod_output_key_id_name}" \
+        "${prod_output_secret_key_name}" \
+        "${gitlab_repo_id}" \
+        "${prod_gitlab_key_id_name}" \
+        "${prod_gitlab_secret_key_name}" \
+        "${prod_gitlab_masked}" \
+        "${prod_gitlab_protected}" \
+  &&  popd \
+  ||  return 1
+}
+
 function job_serves_test_lint_code {
 
       helper_use_pristine_workdir \
