8015d29385d6863c13c39201bb3e4cafef3dd2c1
Daniel Salazar <dsalazar@fluidattacks.com>
2020-08-25 12:57:04 -0500

    observes\refac(infra): #2959.10 rename tfstate
    Former-commit-id: 109eb068e5d656a96f6b3d359d3b1350c8e3712c

--- observes/infra/terraform/main.tf
+++ observes/infra/terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "fluidattacks-terraform-states-prod"
-    key     = "analytics.tfstate"
+    key     = "observes-infra.tfstate"
     region  = "us-east-1"
     encrypt = true
     dynamodb_table = "terraform_state_lock"
