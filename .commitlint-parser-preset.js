eb69d6b2e2854dd31ce46f1d347bbc40c8ce7474
Daniel Salazar <dsalazar@fluidattacks.com>
2020-08-26 14:16:19 -0500

    asserts\feat(build): #2982.1 commit msg
    - Add asserts as valid keyword for commit and
    mr messages
    
    
    Former-commit-id: 7a84d836036d5580a9dad9cc74a6d336946c46f5

--- .commitlint-parser-preset.js
+++ .commitlint-parser-preset.js
@@ -1,6 +1,6 @@
 module.exports = {
   parserOpts: {
-    headerPattern: /^(all|forces|integrates|observes|reviews|serves|skims|sorts)\\(\w*)\((\w*)\):\s(#[1-9]\d*)(.\d+)?\s(.*)$/,
+    headerPattern: /^(all|asserts|forces|integrates|observes|reviews|serves|skims|sorts)\\(\w*)\((\w*)\):\s(#[1-9]\d*)(.\d+)?\s(.*)$/,
     headerCorrespondence: [ 'product', 'type', 'scope', 'ticket', 'part', 'subject' ],
   },
 };
