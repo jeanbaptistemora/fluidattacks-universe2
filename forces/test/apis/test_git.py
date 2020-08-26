2779b982aa807767e22d9750d0037909eb8b264c
Andres Aguirre <aaguirre@fluidattacks.com>
2020-08-26 13:46:39 -0500

    all\feat(conf): #2978 sonarcloud exclusions
    - Added exclusion for analysis over migration files and test files in
    skims
    - Changed exclusion for duplications since front is in another folder
    and there are more files where it should ignore duplications
    - Fixed get_repository_metadata test
    
    
    Former-commit-id: 68767239ec134a9c90c8285bb42f0ed76a2dfdb8

--- forces/test/apis/test_git.py
+++ forces/test/apis/test_git.py
@@ -9,6 +9,6 @@ from forces.apis.git import get_repository_metadata
 @pytest.mark.asyncio  # type: ignore
 def test_get_repository_metadata_test() -> None:
     result = get_repository_metadata(repo_path='../')
-    assert result['git_repo'] == 'integrates'
-    assert 'integrates.git' in result['git_origin']
+    assert result['git_repo'] == 'product'
+    assert 'product.git' in result['git_origin']
     assert result['git_branch'] != 'master'
