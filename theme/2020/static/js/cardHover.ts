f3c742a365feabedc92cbb5bd807701e9781191d
John Perez <jperez@fluidattacks.com>
2020-08-28 09:38:35 -0500

    fix(front): #3619 fix dropdown of cards
    Former-commit-id: 3a3baf18b6cdce5f7327121814a898c0e14b5b53

--- theme/2020/static/js/cardHover.ts
+++ theme/2020/static/js/cardHover.ts
@@ -1,5 +1,5 @@
-let accordions: HTMLCollection = document.getElementsByClassName("accordion");
-let readmores: HTMLCollection = document.getElementsByClassName("readmore");
+const accordions: HTMLCollection = document.getElementsByClassName("accordion");
+const readmores: HTMLCollection = document.getElementsByClassName("readmore");
 
 let count: number;
 for (count = 0; count < accordions.length; count += 1) {
@@ -9,22 +9,29 @@ for (count = 0; count < accordions.length; count += 1) {
     const arrow: HTMLElement = (panel.nextElementSibling as HTMLElement).children[1] as HTMLElement;
 
     arrow.classList.toggle("rotate-180");
+    (panel.nextElementSibling as HTMLElement).classList.toggle("pointer");
     // tslint:disable-next-line: strict-boolean-expressions
-    panel.style.maxHeight ?
-    panel.style.maxHeight = ""
-    : panel.style.maxHeight = `${panel.scrollHeight}px`;
+    panel.style.height ?
+    panel.style.height = ""
+    : panel.style.height = "360px";
 
-    // tslint:disable-next-line: strict-boolean-expressions
-    panel.style.minHeight ?
-    panel.style.minHeight = ""
-    : panel.style.minHeight = "360px";
   });
 }
 
 for (count = 0; count < readmores.length; count += 1) {
   readmores[count].addEventListener("click", (event: Event) => {
-    (event.currentTarget as HTMLElement).classList.toggle("active");
-    const panel: HTMLElement = (event.currentTarget as HTMLElement).previousElementSibling as HTMLElement;
-    (panel.previousElementSibling as HTMLElement).click();
+
+    const current: HTMLElement = event.currentTarget as HTMLElement;
+    current.classList.toggle("active");
+    const panel: HTMLElement = current.previousElementSibling as HTMLElement;
+    const mainbt: HTMLElement = panel.previousElementSibling as HTMLElement;
+
+    if (mainbt.classList.contains("active")) {
+      mainbt.click();
+    }
+
+    if (current.classList.contains("pointer")) {
+      current.classList.remove("pointer");
+    }
   });
 }
