844087af67ff6a2b6ab777bbd4968bb5858df6d7
Anderson Taguada <ataguada@fluidattacks.com>
2020-09-03 13:56:46 -0500

    serves\fix(build): #2984.10 fix tf output variable
    Former-commit-id: 7b4e730520d84052c2f130b05433519c8973493c

--- serves/services/user-provision/melts/terraform/outputs.tf
+++ serves/services/user-provision/melts/terraform/outputs.tf
@@ -18,12 +18,12 @@ output "dev-secret-key-2" {
   value     = aws_iam_access_key.melts-dev-key-2.secret
 }
 
-output "prod-secret-key-id" {
+output "prod-secret-key-id-1" {
   sensitive = true
   value     = aws_iam_access_key.melts-prod-key-1.id
 }
 
-output "prod-secret-key" {
+output "prod-secret-key-1" {
   sensitive = true
   value     = aws_iam_access_key.melts-prod-key-1.secret
 }
