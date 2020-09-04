4a5c6591d4ddf58d18402cc34d22c51f6b57252a
David Acevedo <dacevedo@fluidattacks.com>
2020-09-04 11:17:55 -0500

    integrates\style(front): #56.3 format code
    Former-commit-id: 78728af05562b5d8df68a125d877509e7c790436

--- integrates/front/src/scenes/Dashboard/components/Navbar/index.tsx
+++ integrates/front/src/scenes/Dashboard/components/Navbar/index.tsx
@@ -3,7 +3,16 @@ import { ApolloError } from "apollo-client";
 import { GraphQLError } from "graphql";
 import _ from "lodash";
 import React from "react";
-import { Breadcrumb, BreadcrumbItem, Col, InputGroup, MenuItem, Row, SelectCallback, SplitButton } from "react-bootstrap";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  Col,
+  InputGroup,
+  MenuItem,
+  Row,
+  SelectCallback,
+  SplitButton,
+} from "react-bootstrap";
 import { RouteComponentProps, withRouter } from "react-router";
 import { Link, useHistory } from "react-router-dom";
 import { Field } from "redux-form";
@@ -20,59 +29,79 @@ import { default as style } from "./index.css";
 import { GET_USER_ORGANIZATIONS } from "./queries";
 import { stylizeBreadcrumbItem } from "./utils";
 
-export const navbarComponent: React.FC<RouteComponentProps> = (props: RouteComponentProps): JSX.Element => {
+export const navbarComponent: React.FC<RouteComponentProps> = (
+  props: RouteComponentProps,
+): JSX.Element => {
   const { push } = useHistory();
-  const [lastOrganization, setLastOrganization] = useStoredState("organization", { name: "" }, localStorage);
+  const [lastOrganization, setLastOrganization] = useStoredState(
+    "organization",
+    { name: "" },
+    localStorage,
+  );
   const { userEmail } = window as typeof window & { userEmail: string };
 
   const path: string = props.location.pathname;
   const pathData: string[] = path.split("/")
     .slice(2);
-  const pathOrganization: string = !path.includes("/orgs")
-    ? lastOrganization.name
-    : pathData[0].toLowerCase();
+  const pathOrganization: string = path.includes("/orgs")
+    ? pathData[0].toLowerCase()
+    : lastOrganization.name;
 
   // GraphQL operations
-  const { data, refetch: refetchOrganizationList } = useQuery(GET_USER_ORGANIZATIONS, {
-    onError: ({ graphQLErrors }: ApolloError): void => {
-      graphQLErrors.forEach((error: GraphQLError): void => {
-        msgError(translate.t("group_alerts.error_textsad"));
-        Logger.warning("An error occurred fetching organizations for the navbar", error);
-      });
+  const { data, refetch: refetchOrganizationList } = useQuery(
+    GET_USER_ORGANIZATIONS,
+    {
+      onError: ({ graphQLErrors }: ApolloError): void => {
+        graphQLErrors.forEach((error: GraphQLError): void => {
+          msgError(translate.t("group_alerts.error_textsad"));
+          Logger.warning(
+            "An error occurred fetching organizations for the navbar",
+            error,
+          );
+        });
+      },
     },
-  });
+  );
 
   // Auxiliary Operations
-  const handleOrganizationChange: ((eventKey: string, event: React.SyntheticEvent<SplitButton>) => void) =
-    (eventKey: string): void => {
-      if (eventKey !== lastOrganization.name) {
-        setLastOrganization({ name: eventKey });
-        push(`/orgs/${eventKey}/`);
-      }
+  const handleOrganizationChange: (
+    eventKey: string,
+    event: React.SyntheticEvent<SplitButton>,
+  ) => void = (eventKey: string): void => {
+    if (eventKey !== lastOrganization.name) {
+      setLastOrganization({ name: eventKey });
+      push(`/orgs/${eventKey}/`);
+    }
   };
-  const handleOrganizationClick: (event: React.MouseEvent<SplitButton, globalThis.MouseEvent>) => void =
-    () => {
-      push(`/orgs/${lastOrganization.name}/`);
+  const handleOrganizationClick: (
+    event: React.MouseEvent<SplitButton, globalThis.MouseEvent>,
+  ) => void = () => {
+    push(`/orgs/${lastOrganization.name}/`);
   };
-  const handleSearchSubmit: ((values: { projectName: string }) => void) = (values: { projectName: string }): void => {
+  const handleSearchSubmit: (values: {
+    projectName: string;
+  }) => void = (values: { projectName: string }): void => {
     const projectName: string = values.projectName.toLowerCase();
-    if (!_.isEmpty(projectName)) { push(`/groups/${projectName}/indicators`); }
+    if (!_.isEmpty(projectName)) {
+      push(`/groups/${projectName}/indicators`);
+    }
   };
 
   // Render Elements
-  const organizationList: Array<{ name: string }> = _.isEmpty(data) || _.isUndefined(data)
-    ? [{ name: "" }]
-    : data.me.organizations.sort((a: { name: string }, b: { name: string }) => (a.name > b.name) ? 1 : -1);
+  const organizationList: Array<{ name: string }> =
+    _.isEmpty(data) || _.isUndefined(data)
+      ? [{ name: "" }]
+      : data.me.organizations.sort((a: { name: string }, b: { name: string }) =>
+          a.name > b.name ? 1 : -1,
+        );
 
-  React.useEffect(
-    (): void => {
-      refetchOrganizationList();
-      setLastOrganization({ name: pathOrganization });
-    },
-    [pathOrganization],
-  );
+  React.useEffect((): void => {
+    refetchOrganizationList();
+    setLastOrganization({ name: pathOrganization });
+  },              [pathOrganization]);
 
-  const breadcrumbItems: JSX.Element[] = pathData.slice(1)
+  const breadcrumbItems: JSX.Element[] = pathData
+    .slice(1)
     .map((item: string, index: number) => {
       const baseLink: string = path.split("/")[1];
       const link: string = pathData.slice(0, index + 2)
@@ -87,47 +116,51 @@ export const navbarComponent: React.FC<RouteComponentProps> = (props: RouteCompo
 
   return (
     <React.StrictMode>
-      <Row id="navbar" className={style.container}>
+      <Row className={style.container} id={"navbar"}>
         <Col md={9} sm={12} xs={12}>
           <Breadcrumb className={style.breadcrumb}>
-          <BreadcrumbItem>
-          <div className={style.splitButton}>
-            <SplitButton
-              id={"organizationList"}
-              onClick={handleOrganizationClick}
-              onSelect={handleOrganizationChange as SelectCallback}
-              title={pathOrganization}
-            >
-              {organizationList.map((organization: {name: string}) =>
-                <MenuItem
-                  eventKey={organization.name}
-                  key={organization.name}
+            <BreadcrumbItem>
+              <div className={style.splitButton}>
+                <SplitButton
+                  id={"organizationList"}
+                  onClick={handleOrganizationClick}
+                  onSelect={handleOrganizationChange as SelectCallback}
+                  title={pathOrganization}
                 >
-                  {organization.name}
-                </MenuItem>,
-              )}
-            </SplitButton>
-          </div>
-          </BreadcrumbItem>
-          {breadcrumbItems}
-        </Breadcrumb>
+                  {organizationList.map((organization: { name: string }) => (
+                    <MenuItem
+                      eventKey={organization.name}
+                      key={organization.name}
+                    >
+                      {organization.name}
+                    </MenuItem>
+                  ))}
+                </SplitButton>
+              </div>
+            </BreadcrumbItem>
+            {breadcrumbItems}
+          </Breadcrumb>
         </Col>
         <Col md={3} sm={12} xs={12}>
-          {userEmail.endsWith("fluidattacks.com") ?
-           (<GenericForm name="searchBar" onSubmit={handleSearchSubmit}>
-              <InputGroup className={style.groupsInput}>
-                <Field
-                  name="projectName"
-                  component={Text}
-                  placeholder={translate.t("navbar.searchPlaceholder")}
-                  validate={[alphaNumeric]}
+          {userEmail.endsWith("fluidattacks.com") ? (
+            <React.Fragment>
+              <GenericForm name={"searchBar"} onSubmit={handleSearchSubmit}>
+                <InputGroup className={style.groupsInput}>
+                  <Field
+                    component={Text}
+                    name={"projectName"}
+                    placeholder={translate.t("navbar.searchPlaceholder")}
+                    validate={[alphaNumeric]}
                   />
-                <InputGroup.Button>
-                  <Button className={style.searchButton} type="submit"><FluidIcon icon="search" /></Button>
-                </InputGroup.Button>
-              </InputGroup>
-            </GenericForm>) : undefined
-          }
+                  <InputGroup.Button>
+                    <Button className={style.searchButton} type={"submit"}>
+                      <FluidIcon icon={"search"} />
+                    </Button>
+                  </InputGroup.Button>
+                </InputGroup>
+              </GenericForm>
+            </React.Fragment>
+          ) : undefined}
         </Col>
       </Row>
     </React.StrictMode>
