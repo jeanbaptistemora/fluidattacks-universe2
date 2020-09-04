4f88997693f9fd1f3bd91d7eb99fa6aba8a372fb
Daniel Murcia <dmurcia@fluidattacks.com>
2020-09-04 13:34:36 -0500

    integrates\feat(build): #3001.2 minio derivation
    - local minio derivation using provisioner
    
    
    Former-commit-id: da0c95d34a8cad4f92adfb167c7c8c3e146a7cbd

---
+++ build/provisioners/integrates_serve_minio_local.nix
@@ -0,0 +1,15 @@
+let
+  pkgs = import ../pkgs/integrates.nix;
+in
+  pkgs.stdenv.mkDerivation (
+       (import ../src/basic.nix)
+    // (import ../src/minio-local.nix pkgs)
+    // (rec {
+      name = "builder";
+
+      buildInputs = [
+        pkgs.git
+        pkgs.awscli
+      ];
+    })
+  )
