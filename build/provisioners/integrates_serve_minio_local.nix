1e864df044ca4b4546a540443f882e1231df4121
Daniel Murcia <dmurcia@fluidattacks.com>
2020-09-07 13:15:32 -0500

    integrates\feat(build): #3001.3 minio derivation
    - add needed packages for consuming integrates finding module
    
    
    Former-commit-id: b903ffb438bd8832e210ab254a18714c0f0df69f

--- build/provisioners/integrates_serve_minio_local.nix
+++ build/provisioners/integrates_serve_minio_local.nix
@@ -1,5 +1,7 @@
 let
   pkgs = import ../pkgs/integrates.nix;
+  builders.pythonPackage = import ../builders/python-package pkgs;
+  builders.pythonRequirements = import ../builders/python-requirements pkgs;
 in
   pkgs.stdenv.mkDerivation (
        (import ../src/basic.nix)
@@ -10,6 +12,17 @@ in
       buildInputs = [
         pkgs.git
         pkgs.awscli
+        pkgs.sops
+        pkgs.jq
+        (builders.pythonPackage {}).propagatedBuildInputs
+        (builders.pythonRequirements pkgs).propagatedBuildInputs
       ];
+
+      pyPkgIntegratesBack =
+        import ../../integrates/django-apps/integrates-back-async pkgs;
+
+      pyPkgReqsApp =
+        builders.pythonRequirements ../../integrates/deploy/containers/app/requirements.txt;
+
     })
   )
