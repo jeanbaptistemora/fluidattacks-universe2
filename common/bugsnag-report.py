2a40493e1ea58e78ad59b27464f8fef1bd7acfbf
Richard Almanza <rvega@fluidattacks.com>
2020-09-08 14:56:53 -0500

    all\refac(back): #1887.18 send more data to bugsnag
    - By submitting the CI_JOB_ID, the job track over time will be easier
    - JOB_COMPLETE, JOB_NAME and PARAMETERS will be used to filter reports
    
    
    Former-commit-id: 658de88cd7a54fafcbd51957f579aa0c22a48a6e

--- common/bugsnag-report.py
+++ common/bugsnag-report.py
@@ -3,6 +3,8 @@ import sys
 import os
 
 RELEASE_STAGE = os.environ['ENVIRONMENT_NAME']
+JOB_ID = os.environ.get('CI_JOB_ID', 'LOCAL')
+ARGV = sys.argv[1].split()
 
 BUGSNAG_API_KEYS = {
     'integrates_scheduler': '26294aec6a6dc262ee74b45d19aa7ec5',
@@ -17,17 +19,30 @@ def get_bugsnag_api_key(job_name):
     return '13748c4b5f6807a89f327c0f54fe6c7a'  # ETL
 
 
-def main(args):
+def main():
+    bugsnag.configure_request(
+        meta_data={
+            'JOB_INFO': {
+                'ENVIRONMENT': RELEASE_STAGE,
+                'CI_JOB_ID': JOB_ID,
+                'JOB_NAME': ARGV[1],
+                'PARAMATERS': ARGV[2:],
+                'JOB_COMPLETE': ' '.join(ARGV[1:]),
+            },
+        },
+    )
     bugsnag.configure(
-        api_key=get_bugsnag_api_key(args[1].lower()),
+        api_key=get_bugsnag_api_key(ARGV[1].lower()),
         release_stage=RELEASE_STAGE,
     )
     bugsnag.start_session()
     bugsnag.send_sessions()
 
-    if args[0] == 'failed':
-        raise Exception(f'Error with {" ".join(args[1:])}')
+    if ARGV[0] == 'failed':
+        raise Exception(
+            f'Error with {" ".join(ARGV[1:])} | CI_JOB_ID: {JOB_ID}'
+        )
 
 
 if __name__ == "__main__":
-    main(sys.argv[1].split())
+    main()
