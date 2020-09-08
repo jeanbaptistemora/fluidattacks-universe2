061602831d086117b427ab506402ba7e955261c4
David Acevedo <dacevedo@fluidattacks.com>
2020-09-08 16:29:49 -0500

    integrates\feat(infra): #2617 add roots table
    - just the base, likely to change while
    - polishing the access pattern
    
    
    Former-commit-id: 78036556ebf2df60c9c4dee08bbe6953afceb14d

--- integrates/deploy/database/terraform/integrates-table.tf
+++ integrates/deploy/database/terraform/integrates-table.tf
@@ -42,3 +42,24 @@ resource "aws_dynamodb_table" "integrates" {
     enabled = true
   }
 }
+
+resource "aws_dynamodb_table" "fi_roots" {
+  name           = "fi_roots"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "pk"
+  range_key      = "sk"
+
+  attribute {
+    name = "pk"
+    type = "S"
+  }
+
+  attribute {
+    name = "sk"
+    type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+}
