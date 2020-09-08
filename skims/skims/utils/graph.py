537bfa1124066414508e796fdf4b92cb78b2b591
Kevin Amado <kamado@fluidattacks.com>
2020-09-08 11:22:41 -0500

    skims\feat(back): #2975.226 symbolic evaluation
    - Add a small symbolic evaluation of strings concatenation
      which is used very often as java has no multiline strings
    
    
    Former-commit-id: 75655bc92665ad3d75caac9f1d029cc1261e0410

--- skims/skims/utils/graph.py
+++ skims/skims/utils/graph.py
@@ -12,6 +12,31 @@ from jmespath import (
 )
 
 
+def symbolic_evaluate(value: Any) -> Any:
+    """Evaluate a list of expressions, return a possibly simplified equivalent.
+
+    Examples:
+
+        >>> [StringLiteral, ADD, StringLiteral, ADD, StringLiteral]
+        StringLiteral
+    """
+    # Only a list of expressions can be evaluated
+    if not isinstance(value, list):
+        return value
+
+    # Recursively simplify the element
+    value = list(map(symbolic_evaluate, value))
+
+    # Value is composed from 'StringLiterals' joined by 'ADD'
+    if set(jsh('[0::2].type', value)) == {'StringLiteral'} \
+            and set(jsh('[1::2].type', value)) == {'ADD'}:
+        final = value[0]
+        final['text'] = '"' + ''.join(t['text'][1:-1] for t in value) + '"'
+        return final
+
+    return value
+
+
 def simplify(value: Any) -> Any:
     """Access single node elements in order to flatten a graph.
 
@@ -41,14 +66,21 @@ def yield_nodes(
     value: Any,
     key_predicates: Tuple[Callable[[str], bool], ...] = (),
     value_extraction: str = '@',
-    value_predicates: Tuple[Callable[[Any], bool], ...] = (),
+    value_predicates: Tuple[str, ...] = (),
+    post_actions: Tuple[Callable[[Any], Any], ...] = (
+        simplify,
+        symbolic_evaluate,
+    ),
 ) -> Iterator[Any]:
     """Recursively scan the graph and yield nodes that match the predicates."""
 
     def _yield_if_matches() -> Iterator[Any]:
         if all(jsh(pred, value) for pred in value_predicates) \
                 and all(pred(key) for pred in key_predicates):
-            yield jsh(value_extraction, value)
+            to_yield = jsh(value_extraction, value)
+            for post_action in post_actions:
+                to_yield = post_action(to_yield)
+            yield to_yield
 
     if isinstance(value, dict):
         yield from _yield_if_matches()
