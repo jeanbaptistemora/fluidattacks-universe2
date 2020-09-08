d194f818cca0a8652ad03cc49ae12095549058ae
Kevin Amado <kamado@fluidattacks.com>
2020-09-08 18:21:28 -0500

    skims\test(back): #2975.228 improve tests
    - Make them not share state with the test interpreter
    - Verify code, stdout and stderr streams in all cases
    
    
    Former-commit-id: 42e49c4f27f14c3dda9d4b3de7f45be7cb00eb79

--- skims/test/test_functional.py
+++ skims/test/test_functional.py
@@ -1,4 +1,5 @@
 # Standard library
+import asyncio
 from typing import (
     Callable,
     Dict,
@@ -10,12 +11,7 @@ from typing import (
 # Third party libraries
 from aioextensions import (
     collect,
-    run,
     run_decorator,
-)
-from click.testing import (
-    CliRunner,
-    Result,
 )
 
 # Local libraries
@@ -37,6 +33,9 @@ from utils.model import (
     LocalesEnum,
     VulnerabilityStateEnum,
 )
+from utils.system import (
+    call,
+)
 from zone import (
     set_locale,
     t,
@@ -47,10 +46,13 @@ from zone import (
 set_locale(LocalesEnum.EN)
 
 
-def _cli(*args: str) -> Result:
-    runner = CliRunner(mix_stderr=False)
+async def skims(*args: str) -> Tuple[int, bytes, bytes]:
+    process: asyncio.subprocess.Process = await call('skims', *args)
 
-    return runner.invoke(dispatch, args)
+    stdout, stderr = await process.communicate()
+    code = -1 if process.returncode is None else process.returncode
+
+    return code, stdout, stderr
 
 
 async def get_group_data(group: str) -> Set[Tuple[str, str, int, int]]:
@@ -116,31 +118,43 @@ async def match_expected(
     assert data == expected
 
 
-def test_help() -> None:
-    result = _cli('--help')
-    assert result.exit_code == 0
-    assert 'Usage:' in result.output
+@run_decorator
+async def test_help() -> None:
+    code, stdout, stderr = await skims('--help')
+    assert code == 0
+    assert b'Usage:' in stdout, stdout
+    assert not stderr, stderr
 
 
-def test_dispatch_config_not_found(test_config: Callable[[str], str]) -> None:
-    result = _cli('#')
-    assert result.exit_code != 0
-    assert "File '#' does not exist." in result.stderr, \
-        (result.stderr, result.stdout, result.output)
+@run_decorator
+async def test_config_not_found(test_config: Callable[[str], str]) -> None:
+    code, stdout, stderr = await skims('#')
+    assert code == 2
+    assert not stdout, stdout
+    assert b"File '#' does not exist." in stderr, stderr
 
 
-def test_dispatch_bad_extra_things(test_config: Callable[[str], str]) -> None:
-    result = _cli(test_config('bad_extra_things'))
-    assert result.exit_code == 1
+@run_decorator
+async def test_bad_extra_things(test_config: Callable[[str], str]) -> None:
+    code, stdout, stderr = await skims(test_config('bad_extra_things'))
+    assert code == 1
+    assert not stdout, stdout
+    assert b'Some keys were not recognized: unrecognized_key' in stderr, stderr
 
 
-def test_dispatch_token(
+@run_decorator
+async def test_token(
     test_config: Callable[[str], str],
     test_group: str,
 ) -> None:
-    config: str = test_config('correct_nothing_to_do')
-    result = _cli('--token', '123', '--group', test_group, config)
-    assert result.exit_code == 1
+    code, stdout, stderr = await skims(
+        '--token', '123',
+        '--group', test_group,
+        test_config('correct_nothing_to_do'),
+    )
+    assert code == 1
+    assert not stdout, stdout
+    assert b'Invalid API token' in stderr, stderr
 
 
 @run_decorator
@@ -159,29 +173,41 @@ async def test_reset_environment(
     await match_expected(test_group, set())
 
 
-def test_dispatch_debug_correct_nothing_to_do(
+@run_decorator
+async def test_debug_correct_nothing_to_do(
     test_config: Callable[[str], str],
     test_group: str,
     test_integrates_session: None,
 ) -> None:
-    config: str = test_config('correct_nothing_to_do')
-    result = _cli('--debug', '--group', test_group, config)
-    assert result.exit_code == 0
+    code, stdout, stderr = await skims(
+        '--debug',
+        '--group', test_group,
+        test_config('correct_nothing_to_do'),
+    )
+    assert code == 0
+    assert not stdout, stdout
+    assert b'[INFO] Success: True' in stderr, stderr
 
     # No findings should be created, there is nothing to do !
-    run(match_expected(test_group, set()))
+    await match_expected(test_group, set())
 
 
-def test_dispatch_correct(
+@run_decorator
+async def test_correct(
     test_config: Callable[[str], str],
     test_group: str,
     test_integrates_session: None,
 ) -> None:
-    result = _cli('--group', test_group, test_config('correct'))
-    assert result.exit_code == 0
+    code, stdout, stderr = await skims(
+        '--group', test_group,
+        test_config('correct'),
+    )
+    assert code == 0
+    assert not stdout, stdout
+    assert b'[INFO] Success: True' in stderr, stderr
 
     # The following findings must be met
-    run(match_expected(test_group, {
+    await match_expected(test_group, {
         # Finding, status, # closed, # open
         ('F009', 'APPROVED', 0, 9),
         ('F011', 'APPROVED', 0, 13),
@@ -191,19 +217,25 @@ def test_dispatch_correct(
         ('F061', 'APPROVED', 0, 10),
         ('F085', 'APPROVED', 0, 4),
         ('F117', 'APPROVED', 0, 2),
-    }))
+    })
 
 
-def test_dispatch_correct_nothing_to_do(
+@run_decorator
+async def test_correct_nothing_to_do(
     test_config: Callable[[str], str],
     test_group: str,
     test_integrates_session: None,
 ) -> None:
-    result = _cli('--group', test_group, test_config('correct_nothing_to_do'))
-    assert result.exit_code == 0
+    code, stdout, stderr = await skims(
+        '--group', test_group,
+        test_config('correct_nothing_to_do'),
+    )
+    assert code == 0
+    assert not stdout, stdout
+    assert b'[INFO] Success: True' in stderr, stderr
 
     # Skims should persist the null state, closing everything on Integrates
-    run(match_expected(test_group, {
+    await match_expected(test_group, {
         # Finding, status, # closed, # open
         ('F009', 'APPROVED', 9, 0),
         ('F011', 'APPROVED', 13, 0),
@@ -213,4 +245,4 @@ def test_dispatch_correct_nothing_to_do(
         ('F061', 'APPROVED', 10, 0),
         ('F085', 'APPROVED', 4, 0),
         ('F117', 'APPROVED', 2, 0),
-    }))
+    })
