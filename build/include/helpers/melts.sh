06f625553283e4b60585eed908cd83f6cda2b60c
Anderson Taguada <ataguada@fluidattacks.com>
2020-09-09 14:08:32 -0500

    melts\feat(build): #2984.15 setting linter to build
    Former-commit-id: 6c851533f82e021d8243cbf95c141bf633dc55b0

--- build/include/helpers/melts.sh
+++ build/include/helpers/melts.sh
@@ -1,24 +1,18 @@
 # shellcheck shell=bash
 
 function helper_test_lint_code_python {
-  local args_prospector=(
-    --full-pep8
-    --strictness veryhigh
-    --test-warnings
-  )
   local args_mypy=(
     --allow-any-generics
     # Remove following rule when all packages in melts are typed
     --follow-imports=skip
     --ignore-missing-imports
-    --strict
   )
 
       pushd melts \
   &&  echo '[INFO]: Checking static typing...' \
-  &&  mypy "${args_mypy[@]}" toolbox/sorts \
+  &&  mypy "${args_mypy[@]}" . \
   &&  echo '[INFO]: Linting...' \
-  &&  prospector "${args_prospector[@]}" toolbox/sorts \
+  &&  prospector --profile prospector_config.yml . \
   &&  popd \
   || return 1
 }
