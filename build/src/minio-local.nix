9564158c4ec477eb020dec40ff0b23819d893d7d
Daniel Murcia <dmurcia@fluidattacks.com>
2020-09-09 12:25:51 -0500

    integrates\feat(build): #3001.8 minio update
    - update sha256 checksum
    
    
    Former-commit-id: 2a03f96dd9ded3da7dafce9f9c84026f6ce28844

--- build/src/minio-local.nix
+++ build/src/minio-local.nix
@@ -3,7 +3,7 @@ pkgs:
 rec {
   srcExternalMinIOLocal = pkgs.fetchurl {
     url = "https://dl.min.io/server/minio/release/linux-amd64/minio";
-    sha256 = "9cfaf4bcf56d7f78f3ebd4277fa7fd11f20e59e4e724c468546b7165294b1e96";
+    sha256 = "b87a8bddb418a68112f03d32eeda13cb261a5d98ebcf1e468c90e39b9ed13d36";
   };
   srcExternalMinIOCli = pkgs.fetchurl {
     url = "https://dl.min.io/client/mc/release/linux-amd64/mc";
