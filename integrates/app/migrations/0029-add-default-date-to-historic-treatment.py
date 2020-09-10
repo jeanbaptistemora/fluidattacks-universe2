efeb93ec152dce80ad419f388ddf04a1f0fe24db
Andres Aguirre <aaguirre@fluidattacks.com>
2020-09-10 14:10:40 -0500

    integrates\feat(back): #3076.1 migration treatment dates
    - Added a default date to those treatments that do not have dates
    
    
    Former-commit-id: 42d3abac26bbfd1a3a49f6f6c4f368c1a5e50540

---
+++ integrates/app/migrations/0029-add-default-date-to-historic-treatment.py
@@ -0,0 +1,74 @@
+"""
+This migration adds a dafault date to those finding historic_treatments that
+do not have a date.
+"""
+
+from asyncio import run
+import copy
+import os
+from pprint import pprint
+from typing import Any
+import aioboto3
+
+from backend.dal.helpers import dynamodb
+from backend.dal.finding import update
+
+STAGE: str = os.environ['STAGE']
+FINDINGS_TABLE = 'FI_findings'
+
+
+async def scan(*, table_name:str, **options: Any) -> Any:
+    async with aioboto3.resource(
+        **dynamodb.RESOURCE_OPTIONS,
+    ) as dynamodb_resource:
+        table = await dynamodb_resource.Table(table_name)
+        response = await table.scan(**options)
+        for elem in response.get('Items', []):
+            yield elem
+
+        while 'LastEvaluatedKey' in response:
+            options['ExclusiveStartKey'] = response['LastEvaluatedKey']
+            response = await table.scan(**options)
+            for elem in response.get('Items', []):
+                yield elem
+
+
+async def main() -> None:
+    async for finding in scan(table_name=FINDINGS_TABLE):
+        finding_id = finding['finding_id']
+        project_name = finding['project_name']
+        historic_treatment = finding.get('historic_treatment', [])
+        old_historic_treatment = copy.deepcopy(historic_treatment)
+        to_update = False
+        first_date = None
+        last_date = None
+
+        for treatment_info in historic_treatment:
+            if 'date' in treatment_info:
+                last_date = treatment_info['date']
+                if not first_date:
+                    first_date = last_date
+            elif last_date:
+                to_update = True
+                treatment_info['date'] = last_date
+
+        for treatment_info in historic_treatment:
+            if 'date'in treatment_info:
+                break
+            else:
+                to_update = True
+                treatment_info['date'] = first_date or '0001-01-01 00:00:00'
+
+        if to_update:
+            print(f'old_historic_treatment =')
+            pprint(old_historic_treatment)
+            print(f'historic_treatment =')
+            pprint(historic_treatment)
+            print(
+                f'{project_name} & fin {finding_id} - '
+                f'historic_treatment / Success: '
+                f'{await update(finding_id, {"historic_treatment": historic_treatment})}'
+            )
+
+if __name__ == '__main__':
+    run(main())
