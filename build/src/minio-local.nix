31aafa149c4de97e02fa98754dee7a3c92c12f67
Daniel Murcia <dmurcia@fluidattacks.com>
2020-09-11 15:58:50 -0500

    integrates\fix(build): #3082.1 pin minio version
    Closes #3082
    
    
    Former-commit-id: fb610ed76ec8cff7802eaa99763eb428a6a05ad1

--- build/src/minio-local.nix
+++ build/src/minio-local.nix
@@ -2,11 +2,11 @@ pkgs:
 
 rec {
   srcExternalMinIOLocal = pkgs.fetchurl {
-    url = "https://dl.min.io/server/minio/release/linux-amd64/minio";
-    sha256 = "b87a8bddb418a68112f03d32eeda13cb261a5d98ebcf1e468c90e39b9ed13d36";
+    url = "https://dl.min.io/server/minio/release/linux-amd64/archive/minio.RELEASE.2020-09-10T22-02-45Z";
+    sha256 = "3a41a1e918a6cb43565aa4ebbb71cfe0a0da1e199a3f727f4a11a4c73a6026b1";
   };
   srcExternalMinIOCli = pkgs.fetchurl {
-    url = "https://dl.min.io/client/mc/release/linux-amd64/mc";
+    url = "https://dl.min.io/client/mc/release/linux-amd64/mc.RELEASE.2020-09-03T00-08-28Z";
     sha256 = "7418e5e111c55a3c6f0715b555a952ee47efd8a70fffadf0cd99f66ef1b195f7";
   };
 }
