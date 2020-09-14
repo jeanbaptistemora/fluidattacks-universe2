32a725818ddedda70568b7d554e5360e7a653dbf
Kevin Amado <kamado@fluidattacks.com>
2020-09-14 18:40:07 -0500

    skims\feat(back): #2975.252 iam statements as json
    Former-commit-id: 7420fc8d10b5c98011cf52adf092c3c19592600c

--- skims/test/test_parse_cfn_structure.py
+++ skims/test/test_parse_cfn_structure.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 # Local libraries
 from parse_cfn.loader import (
+    load_as_json,
     load_as_yaml,
 )
 from parse_cfn.structure import (
@@ -10,7 +11,7 @@ from parse_cfn.structure import (
 )
 
 
-def test_iterate_iam_policy_documents() -> None:
+def test_iterate_iam_policy_documents_as_yml() -> None:
     expected = (
         {
             'Action': [
@@ -99,5 +100,98 @@ def test_iterate_iam_policy_documents() -> None:
 
     with open('test/data/parse_cfn/full.yaml') as file:
         template = load_as_yaml(file.read())
+
+    assert tuple(iterate_iam_policy_documents(template)) == expected
+
+
+def test_iterate_iam_policy_documents_as_json() -> None:
+    expected = (
+        {
+            'Action': [
+                's3:ListBucket',
+                's3:GetBucketLocation',
+            ],
+            'Effect': 'Allow',
+            'Resource': {
+                'Fn::Sub': 'arn:aws:s3:::${pS3RestoreBucket}',
+                '__column__': 17,
+                '__line__': 24,
+            },
+            '__column__': 15,
+            '__line__': 18,
+        },
+        {
+            'Action': [
+                's3:GetObjectMetaData',
+                's3:GetObject',
+                's3:PutObject',
+                's3:ListMultipartUploadParts',
+                's3:AbortMultipartUpload',
+            ],
+            'Effect': 'Allow',
+            'Resource': '*',
+            '__column__': 15,
+            '__line__': 28,
+        },
+        {
+            'Action': [
+                'logs:CreateLogStream',
+                'logs:CreateLogGroup',
+                'logs:PutLogEvents',
+            ],
+            'Effect': 'Allow',
+            'Resource': [
+                'arn:aws:logs:*:*:*',
+            ],
+            '__column__': 19,
+            '__line__': 50,
+        },
+        {
+            'Action': [
+                'comprehend:Detect*',
+                'comprehend:BatchDetect*',
+            ],
+            'Effect': 'Allow',
+            'Resource': '*',
+            '__column__': 19,
+            '__line__': 68,
+        },
+        {
+            'Action': [
+                's3:ListBucket',
+                's3:GetBucketLocation',
+            ],
+            'Effect': 'Allow',
+            'Resource': '*',
+            '__column__': 15,
+            '__line__': 89,
+        },
+        {
+            'Action': '*',
+            'Condition': {
+                'StringNotEquals': {
+                    '__column__': 23,
+                    '__line__': 114,
+                    'aws:RequestedRegion': 'us-east-1',
+                },
+                '__column__': 21,
+                '__line__': 113,
+            },
+            'Effect': 'Deny',
+            'Resource': '*',
+            '__column__': 19,
+            '__line__': 109,
+        },
+        {
+            'Action': 'sts:AssumeRole',
+            'Effect': 'Allow',
+            'Resource': 'arn:aws:iam::*:role/cloud-lambda',
+            '__column__': 19,
+            '__line__': 119,
+        },
+    )
+
+    with open('test/data/parse_cfn/full.yaml.json') as file:
+        template = load_as_json(file.read())
 
     assert tuple(iterate_iam_policy_documents(template)) == expected
