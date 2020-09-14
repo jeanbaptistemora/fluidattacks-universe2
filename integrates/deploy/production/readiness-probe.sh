79ab22ccb5b9df1590b68417fbd9dc32498a9a5b
Daniel Salazar <dsalazar@fluidattacks.com>
2020-09-14 17:29:18 -0500

    integrates\feat(infra): #3032.53 prod probes
    - Improve readiness probe for production
    - Add liveness probe for production
    
    
    Former-commit-id: dd2a8ac29fb727d68d38856852ea6b7e3754259d

---
+++ integrates/deploy/production/readiness-probe.sh
@@ -0,0 +1,47 @@
+#! /usr/bin/env bash
+
+function test_aws_credentials {
+  if aws sts get-caller-identity | grep -q 'integrates-prod'
+  then
+    echo '[INFO] Passed: test_aws_credentials'
+  else
+        echo '[ERROR] AWS credentials could not be validated.' \
+    &&  return 1
+  fi
+}
+
+function test_curl_localhost {
+  if curl -sS http://localhost:8080 | grep -q 'FluidIntegrates'
+  then
+    echo '[INFO] Passed: test_curl_localhost'
+  else
+        echo '[ERROR] Localhost curl failed.' \
+    &&  return 1
+  fi
+}
+
+function test_curl_production {
+  if curl -sS "integrates.fluidattacks.com" | grep -q 'FluidIntegrates'
+  then
+    echo '[INFO] Passed: test_curl_production'
+  else
+        echo '[ERROR] Production curl failed.' \
+    &&  return 1
+  fi
+}
+
+function run_readiness_probe {
+      test_aws_credentials \
+  &&  test_curl_localhost
+}
+
+function run_liveness_probe {
+      test_aws_credentials \
+  &&  test_curl_localhost \
+  &&  test_curl_production
+}
+
+
+set -o pipefail
+
+"${1}"
