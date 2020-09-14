d1672d843b8d6a892f6cc7c61c4a0fb287ca34ac
Daniel Salazar <dsalazar@fluidattacks.com>
2020-09-14 12:05:03 -0500

    serves\refac(infra): #3032.51 decrease old cluster size
    - Move from 6 to 2 nodes
    - Make integrates deploy faster by allowing it terminate
    two pods simultaneously when deploying
    
    
    Former-commit-id: 9bcf4c2ac16e8d4e6f0ae271a31a1f8745387b9a

--- serves/infrastructure/eks/cluster.tf
+++ serves/infrastructure/eks/cluster.tf
@@ -59,10 +59,10 @@ resource "aws_launch_configuration" "k8s_nodes_launch_config" {
 }
 
 resource "aws_autoscaling_group" "k8s_nodes_autoscaling" {
-  desired_capacity     = 6
+  desired_capacity     = 2
   launch_configuration = aws_launch_configuration.k8s_nodes_launch_config.id
-  max_size             = 7
-  min_size             = 6
+  max_size             = 2
+  min_size             = 2
   name                 = "EKSWorkerNodes"
   vpc_zone_identifier  = aws_subnet.k8s_subnets_secondary.*.id
 
