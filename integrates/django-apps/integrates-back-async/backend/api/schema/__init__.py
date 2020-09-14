9282709e28dc523ca40a526bb545d3bf4405f0b5
David Acevedo <dacevedo@fluidattacks.com>
2020-09-14 15:47:13 -0500

    integrates\refac(back): #159.1 improve api structure
    - moved schema files to their own directory
    - each with their sdl file and ariadne mappings
    - this will allow for easier and clearer api changes
    
    
    Former-commit-id: 19fe1ff867f2ba468ecd2af293367ae1509ef248

---
+++ integrates/django-apps/integrates-back-async/backend/api/schema/__init__.py
@@ -0,0 +1,29 @@
+# Standard
+import os
+
+# Third party
+from ariadne import (
+    load_schema_from_path,
+    make_executable_schema,
+    snake_case_fallback_resolvers
+)
+from graphql import GraphQLSchema
+
+# Local
+from backend.api.schema.mutation import MUTATION
+from backend.api.schema.query import QUERY
+from backend.api.schema.scalars import SCALARS
+from backend.api.schema.types import TYPES
+
+
+SCHEMA_PATH: str = os.path.dirname(os.path.abspath(__file__))
+SDL_FILES: str = load_schema_from_path(SCHEMA_PATH)
+
+SCHEMA: GraphQLSchema = make_executable_schema(
+    SDL_FILES,
+    QUERY,
+    MUTATION,
+    *TYPES,
+    *SCALARS,
+    snake_case_fallback_resolvers
+)
