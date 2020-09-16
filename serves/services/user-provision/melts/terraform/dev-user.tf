af6b360138a90fc445594f832070417d23bfb705
Anderson Taguada <ataguada@fluidattacks.com>
2020-09-16 13:58:46 -0500

    serves\feat(back): #3075.2 increase s3 test scope
    Former-commit-id: 83820dd6ea437d4f93bc2ec5b3cb8fd861add909

--- serves/services/user-provision/melts/terraform/dev-user.tf
+++ serves/services/user-provision/melts/terraform/dev-user.tf
@@ -34,7 +34,7 @@ data "aws_iam_policy_document" "dev-policy-data" {
       "s3:Put*"
     ]
     resources = [
-      "arn:aws:s3:::continuous-repositoriest/continuoustest/*",
+      "arn:aws:s3:::continuous-repositoriest/continuoustest*/*",
     ]
   }
 
