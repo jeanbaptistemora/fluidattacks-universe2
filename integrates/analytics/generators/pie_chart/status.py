a77c36124964d7e0208ebe94a1310716096919e8
Sebastian Lizcano <slizcano@fluidattacks.com>
2020-09-17 15:26:10 -0500

    integrates\refac(back): #3099.1 vuln status and treatment
    - vuln status and treatment org
    
    
    Former-commit-id: 79a2c7e249f45f17947a31de7b87412473c82649

--- integrates/analytics/generators/pie_chart/status.py
+++ integrates/analytics/generators/pie_chart/status.py
@@ -1,7 +1,15 @@
 # Standard library
 import asyncio
+from typing import (
+    NamedTuple,
+    Tuple,
+)
 
 # Third party libraries
+from aioextensions import (
+    collect,
+)
+from async_lru import alru_cache
 from backend.domain import (
     project as group_domain,
 )
@@ -14,18 +22,44 @@ from analytics.colors import (
     RISK,
 )
 
+Status = NamedTuple('Status', [
+    ('closed_vulnerabilities', int),
+    ('open_vulnerabilities', int),
+])
 
-async def generate_one(group: str):
+
+@alru_cache(maxsize=None, typed=True)
+async def get_data_one_group(group: str) -> Status:
     item = await group_domain.get_attributes(group, [
         'open_vulnerabilities',
         'closed_vulnerabilities',
     ])
 
+    return Status(
+        open_vulnerabilities=item.get('open_vulnerabilities', 0),
+        closed_vulnerabilities=item.get('closed_vulnerabilities', 0),
+    )
+
+
+async def get_data_many_groups(groups: Tuple[str]) -> Status:
+    groups_data = await collect(map(get_data_one_group, list(groups)))
+
+    return Status(
+        open_vulnerabilities=sum(
+            [group.open_vulnerabilities for group in groups_data]
+        ),
+        closed_vulnerabilities=sum(
+            [group.closed_vulnerabilities for group in groups_data]
+        ),
+    )
+
+
+def format_document(data: Status) -> dict:
     return {
         'data': {
             'columns': [
-                ['Closed', item.get('closed_vulnerabilities', 0)],
-                ['Open', item.get('open_vulnerabilities', 0)],
+                ['Closed', data.closed_vulnerabilities],
+                ['Open', data.open_vulnerabilities],
             ],
             'type': 'pie',
             'colors': {
@@ -47,9 +81,22 @@ async def generate_one(group: str):
 async def generate_all():
     async for group in utils.iterate_groups():
         utils.json_dump(
-            document=await generate_one(group),
+            document=format_document(
+                data=await get_data_one_group(group),
+            ),
             entity='group',
             subject=group,
+        )
+
+    async for org_id, _, org_groups in (
+        utils.iterate_organizations_and_groups()
+    ):
+        utils.json_dump(
+            document=format_document(
+                data=await get_data_many_groups(org_groups),
+            ),
+            entity='organization',
+            subject=org_id,
         )
 
 
--- integrates/analytics/generators/pie_chart/treatment.py
+++ integrates/analytics/generators/pie_chart/treatment.py
@@ -1,7 +1,15 @@
 # Standard library
 import asyncio
+from typing import (
+    NamedTuple,
+    Tuple,
+)
 
 # Third party libraries
+from aioextensions import (
+    collect,
+)
+from async_lru import alru_cache
 from backend.domain import (
     project as group_domain,
 )
@@ -14,12 +22,42 @@ from analytics.colors import (
     TREATMENT,
 )
 
+Treatment = NamedTuple('Treatment', [
+    ('acceptedUndefined', int),
+    ('accepted', int),
+    ('inProgress', int),
+    ('undefined', int),
+])
 
-async def generate_one(group: str):
+
+@alru_cache(maxsize=None, typed=True)
+async def get_data_one_group(group: str):
     item = await group_domain.get_attributes(group, ['total_treatment'])
 
     treatment = item.get('total_treatment', {})
 
+    return Treatment(
+        acceptedUndefined=treatment.get('acceptedUndefined', 0),
+        accepted=treatment.get('accepted', 0),
+        inProgress=treatment.get('inProgress', 0),
+        undefined=treatment.get('undefined', 0),
+    )
+
+
+async def get_data_many_groups(groups: Tuple[str]):
+    groups_data = await collect(map(get_data_one_group, list(groups)))
+
+    return Treatment(
+        acceptedUndefined=sum(
+            [group.acceptedUndefined for group in groups_data]
+        ),
+        accepted=sum([group.accepted for group in groups_data]),
+        inProgress=sum([group.inProgress for group in groups_data]),
+        undefined=sum([group.undefined for group in groups_data]),
+    )
+
+
+def format_data(data: Treatment):
     translations = {
         'acceptedUndefined': 'Eternally accepted',
         'accepted': 'Temporarily Accepted',
@@ -30,7 +68,7 @@ async def generate_one(group: str):
     return {
         'data': {
             'columns': [
-                [translations[column], treatment.get(column, 0)]
+                [translations[column], getattr(data, column)]
                 for column in translations
             ],
             'type': 'pie',
@@ -55,9 +93,22 @@ async def generate_one(group: str):
 async def generate_all():
     async for group in utils.iterate_groups():
         utils.json_dump(
-            document=await generate_one(group),
+            document=format_data(
+                data=await get_data_one_group(group),
+            ),
             entity='group',
             subject=group,
+        )
+
+    async for org_id, _, org_groups in (
+        utils.iterate_organizations_and_groups()
+    ):
+        utils.json_dump(
+            document=format_data(
+                data=await get_data_many_groups(org_groups),
+            ),
+            entity='organization',
+            subject=org_id,
         )
 
 
