fc4b2b3d8b90f4c4c56f167228cea5315992203a
Daniel Aguirre <daguirre@fluidattacks.com>
2020-09-17 11:53:18 -0500

    integrates\fix(front): #3102.2 rename review to ephemeral
    - Rename the stage review to ephemeral to move it on bugsnag
    platform.
    
    
    Former-commit-id: 12b33981021595697695674172a1dc844c88c18d

--- integrates/front/src/utils/environment.ts
+++ integrates/front/src/utils/environment.ts
@@ -10,7 +10,7 @@ export const getEnvironment: () => string = (): string => {
     if (currentUrl === "localhost") {
       return "development";
     } else if (ephemeralDomainRegex.test(currentUrl)) {
-      return "review";
+      return "ephemeral";
     } else {
       return "production";
     }
