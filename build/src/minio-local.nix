3d1eac8edece70ebabb355d83329d1733c71b305
Daniel Murcia <dmurcia@fluidattacks.com>
2020-09-18 08:08:30 -0500

    integrates\fix(build): #3082.2 mc from archive
    - get mc from archive
    
    Closes #3082
    
    
    Former-commit-id: 358bc6ecd346c0236d2911168903e1786016b55b

--- build/src/minio-local.nix
+++ build/src/minio-local.nix
@@ -6,7 +6,7 @@ rec {
     sha256 = "3a41a1e918a6cb43565aa4ebbb71cfe0a0da1e199a3f727f4a11a4c73a6026b1";
   };
   srcExternalMinIOCli = pkgs.fetchurl {
-    url = "https://dl.min.io/client/mc/release/linux-amd64/mc.RELEASE.2020-09-03T00-08-28Z";
-    sha256 = "7418e5e111c55a3c6f0715b555a952ee47efd8a70fffadf0cd99f66ef1b195f7";
+    url = "https://dl.min.io/client/mc/release/linux-amd64/archive/mc.RELEASE.2020-09-18T00-13-21Z";
+    sha256 = "0fd638b98e1bb75df57aeda3c5544775ebec14c57968c5290647dfe0b23533a4";
   };
 }
