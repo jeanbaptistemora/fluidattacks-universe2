a3b4be554c3f257ac25841e0e3d3486e5736c31e
Anderson Taguada <ataguada@fluidattacks.com>
2020-09-21 20:22:54 -0500

    melts\refac(back): #3124 to-reattack optimized
    - no-deltas-test
    
    closes #3124
    
    
    Former-commit-id: 137bc50725ce1f94e336c553772017e3aa15ddcd

--- melts/tests/test_drills_to_reattack.py
+++ melts/tests/test_drills_to_reattack.py
@@ -10,9 +10,10 @@ NO_EXP_EXPECTED_URL: str = 'https://integrates.fluidattacks.com/dashboard#!/proj
 NO_EXP_UNEXPECTED_URL: str = 'https://integrates.fluidattacks.com/dashboard#!/project/continuoustest/975673437'
 
 def test_drills_to_reattack(relocate):
-    exp_message: str = to_reattack('continuoustest', True)[0]
-    no_exp_message: str = to_reattack('continuoustest', False)[0]
+    exp_message: str = to_reattack(True, 'continuoustest')[0]['findings'][0]['url']
+    no_exp_message: list = to_reattack(False, 'continuoustest')
     assert EXP_EXPECTED_URL in exp_message
     assert EXP_UNEXPECTED_URL not in exp_message
+    assert not no_exp_message
     assert True # temp disabled: NO_EXP_EXPECTED_URL in no_exp_message
     assert True # temp disabled: NO_EXP_UNEXPECTED_URL not in no_exp_message
--- melts/toolbox/drills/to_reattack.py
+++ melts/toolbox/drills/to_reattack.py
@@ -2,105 +2,228 @@
 from datetime import datetime as dt
 from glob import glob
 from os.path import relpath
-from typing import List, Dict
+from typing import List
+from dateutil.relativedelta import relativedelta
 
 # Local libraries
-from toolbox import api, utils
+from toolbox import api
 from toolbox.constants import API_TOKEN
 
 
 BASE_URL: str = 'https://integrates.fluidattacks.com/dashboard#!/project'
 
 
-def get_subs_unverified_findings(subs: str):
-    query = f'''
-        query {{
-            project(projectName: "{subs}") {{
-                findings (filters: {{verified: False}}) {{
-                    id,
-                    vulnerabilities (state: "open"){{
-                      id,
-                      historicVerification {{
-                          status,date
-                        }}
-                    }}
+def get_subs_unverified_findings(sub_name: str):
+
+    project_info_query = '''
+        name
+        findings(filters: {verified: False}) {
+            id
+            vulnerabilities(state: "open") {
+                id
+                historicVerification {
+                    status, date
+                }
+            }
+        }
+
+    '''
+
+    if sub_name == 'all':
+        query = f'''
+            query {{
+                me {{
+                    projects {{
+                                {project_info_query}
+                     }}
                 }}
             }}
-        }}
-    '''
+        '''
+    else:
+        query = f'''
+            query{{
+                project(projectName: "{sub_name}") {{
+                                {project_info_query}
+                }}
+            }}
+        '''
 
     return api.integrates.request(API_TOKEN, query)
 
 
 def get_url(subs_name: str, finding_id: str) -> str:
     """Return a string with an url associated to a subs finding"""
-    return f'  url:       {BASE_URL}/{subs_name}/{finding_id}'
+    return f'{BASE_URL}/{subs_name}/{finding_id}'
 
 
-def get_exploits(subs_name: str, finding_id: str) -> str:
+def get_exploits_paths(subs_name: str, finding_id: str) -> List:
     """Return a string with exploit paths associated to a subs finding"""
-    message: str = ''
+    exploits_paths: list = []
     exp_glob: str = \
         f'./groups/{subs_name}/forces/*/exploits/*{finding_id}*'
     exp_paths: List[str] = glob(exp_glob)
     for exp_path in map(relpath, exp_paths):
-        message += f'  exploit:   {exp_path}\n'
-    return message
+        exploits_paths.append(exp_path)
+    return exploits_paths
 
 
-def to_reattack(subs_name: str, with_exp: bool) -> tuple:
+def to_reattack(with_exp: bool, sub_name: str = 'all') -> list:
     """
     Return a string with non-verified findings from a subs.
     It includes integrates url and exploits paths in case they exist
 
-    param: subs_name: Name of the group to check
+    param: sub_name: Name of the group to check
     param: with_exp: Show findings with or without exploits
     """
-    message: str = ''
-    findings_raw = get_subs_unverified_findings(subs_name)
-    findings_raw = findings_raw.data['project'][
-        'findings'] if findings_raw.ok else []
-    findings_parsed: List[Dict] = []
-    findings_ret: List[Dict] = []
-    for finding in findings_raw:
-        # Find the oldest verification request
-        verivulns = filter(lambda x: x['historicVerification'] and
-                           x['historicVerification'][-1]['status']
-                           == 'REQUESTED',
-                           finding['vulnerabilities'])
-        vulnsort = sorted(verivulns,
-                          key=lambda x: x['historicVerification'][-1]['date'],)
-        oldest = (dt.now() -
-                  dt.strptime(vulnsort[0]['historicVerification'][-1]['date'],
-                              '%Y-%m-%d %H:%M:%S')).days
-        findings_parsed.append({'id': finding['id'],
-                                'vulns': len(vulnsort),
-                                'oldest': oldest})
-    findings_sorted = \
-        sorted(findings_parsed, key=lambda x: x['oldest'], reverse=True)
+    graphql_date_format = '%Y-%m-%d %H:%M:%S'
 
-    for finding in findings_sorted:
-        finding_id = finding['id']
-        finding_oldest = finding['oldest']
-        url: str
-        exploits: str = get_exploits(subs_name, finding_id)
+    projects_info = get_subs_unverified_findings(sub_name)
+    if sub_name != 'all':
+        class Tmp():
+            def __init__(self):
+                self.data = None
+        tmp = Tmp()
+        tmp.data = {'me': None}
+        tmp.data['me'] = {'projects': [None]}
+        tmp.data['me']['projects'][0] = projects_info.data['project']
+        projects_info = tmp
+
+    projects_info = list(projects_info.data['me']['projects'])
+    # claning empty findigs
+    projects_info = list(filter(
+        lambda info: info['findings'],
+        projects_info
+    ))
+    # cleaning findings without vulnerabilities
+    for project_info in projects_info:
+        project_info['findings'] = list(filter(
+            lambda finding:
+                finding['vulnerabilities'],
+            project_info['findings']
+        ))
+
+    total_findings = 0
+    total_vulnerabilities = 0
+    oldest_finding = {
+        'group': '',
+        'date_dif': relativedelta()
+    }
+
+    for project_info in projects_info:
+        for finding in project_info['findings']:
+            # Filter requested vulnerabilities
+            finding['vulnerabilities'] = filter(
+                lambda vulnerability:
+                    vulnerability
+                and
+                    vulnerability['historicVerification']
+                and
+                    vulnerability['historicVerification']
+                                 [-1]
+                                 ['status'] == 'REQUESTED',
+                finding['vulnerabilities']
+            )
+
+            # Order vulnerabilities by date
+            finding['vulnerabilities'] = list(sorted(
+                finding['vulnerabilities'],
+                key=lambda x: x['historicVerification']
+                               [-1]
+                               ['date']
+            ))
+            oldest_vulnerability = finding['vulnerabilities'][0]
+            oldest_vuln_dif = relativedelta(
+                dt.now(),
+                dt.strptime(
+                    oldest_vulnerability['historicVerification']
+                                        [-1]
+                                        ['date'],
+                    graphql_date_format
+                )
+            )
+            finding['vulnerability_counter'] = len(finding['vulnerabilities'])
+            finding['oldest_vulnerability'] = oldest_vulnerability
+            finding['oldest_vuln_dif'] = oldest_vuln_dif
+            finding['exploit_paths'] = get_exploits_paths(
+                project_info['name'],
+                finding['id']
+            )
+            finding['url'] = get_url(project_info["name"], finding["id"])
+
+            if with_exp and finding['exploit_paths']:
+                total_vulnerabilities += finding['vulnerability_counter']
+                if (oldest_finding['date_dif'].days <  # type: ignore
+                        finding['oldest_vuln_dif'].days):
+                    oldest_finding = {
+                        'group': project_info['name'],
+                        'date_dif': finding['oldest_vuln_dif']
+                    }
+            elif not with_exp and finding['exploit_paths']:
+                total_vulnerabilities += finding['vulnerability_counter']
+                # type: ignore
+                if (oldest_finding['date_dif'].days <  # type: ignore
+                        finding['oldest_vuln_dif'].days):
+
+                    oldest_finding = {
+                        'group': project_info['name'],
+                        'date_dif': finding['oldest_vuln_dif']
+                    }
+
         if with_exp:
-            if exploits:
-                findings_ret.append(finding)
-                url = get_url(subs_name, finding_id)
-                message += f'{url}\n'
-                message += exploits
-                message += f'  requested: {finding_oldest} days ago\n' \
-                           f'  vulns to verify: {finding["vulns"]}\n\n'
+            project_info['findings'] = list(filter(
+                lambda finding:
+                    finding['exploit_paths'],
+                project_info['findings']
+            ))
         else:
-            if not exploits:
-                findings_ret.append(finding)
-                url = get_url(subs_name, finding_id)
-                message += f'{url}\n'
-                message += f'  requested: {finding_oldest} days ago\n' \
-                           f'  vulns to verify: {finding["vulns"]}\n\n'
+            project_info['findings'] = list(filter(
+                lambda finding:
+                    not finding['exploit_paths'],
+                project_info['findings']
+            ))
 
-    return message, findings_ret
+        total_findings += len(project_info['findings'])
+
+    # clean trash
+    projects_info = list(filter(
+        lambda project_info:
+            project_info['findings'],
+        projects_info
+    ))
+
+    # Printing all
+    for project_info in projects_info:
+        if project_info['findings']:
+            # Head
+            print(project_info['name'])
+            # Body
+            for finding in project_info['findings']:
+                print('  url:               '
+                      f'{finding["url"]}\n'
+                      '  requested (days):  '
+                      f'{finding["oldest_vuln_dif"].days}\n'
+                      '  vulns to verify:   '
+                      f'{finding["vulnerability_counter"]}'
+                      )
+                if finding['exploit_paths']:
+                    print('  exploits: ')
+                    for path_exploit in finding['exploit_paths']:
+                        print(f'           - {path_exploit}')
+
+                print()
+            print()
+
+    # summary
+    summary = (
+        f"TO-DO: FIN: {total_findings}; "  # type: ignore
+        f"Vulns: {total_vulnerabilities}; "
+        f"Days since oldest request: {oldest_finding['date_dif'].days}; "
+        f"Group: {oldest_finding['group']};"
+    )
+
+    print(summary)
+
+    return projects_info
 
 
 def main(with_exp: bool):
@@ -109,23 +232,4 @@ def main(with_exp: bool):
 
     param: with_exp: Show findings with or without exploits
     """
-    total_vulns: int = 0
-    total_fin: int = 0
-    oldest: tuple = (0, 'null')
-    subs_names: List[str] = utils.integrates.get_my_projects()
-    for subs_name in subs_names:
-        message, pending_findings = to_reattack(subs_name, with_exp)
-        if message:
-            print(subs_name)
-            print(message)
-            total_vulns += sum(map(lambda x: x['vulns'], pending_findings))
-            total_fin += len(pending_findings)
-            old = pending_findings[0]['oldest']
-            oldest = (old, subs_name) if old > oldest[0] else oldest
-    summary = (
-        f"TO-DO: FIN: {total_fin}; "
-        f"Vulns: {total_vulns}; "
-        f"Days since oldest request: {oldest[0]}; "
-        f"Group: {oldest[1]};"
-    )
-    print(summary)
+    to_reattack(with_exp, 'continuoustest')
