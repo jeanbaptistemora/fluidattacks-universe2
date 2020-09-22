4bb764a687def2c40c1f93ab2af8a59c4d4f0755
Daniel Salazar <dsalazar@fluidattacks.com>
2020-09-22 14:51:18 -0500

    serves\feat(infra): #3331.1 add landing
    - Add landing dns record
    
    
    Former-commit-id: 08cde40fd99aaa29753c821adf7a104e2cc8e03b

--- serves/services/dns/terraform/campaigns.tf
+++ serves/services/dns/terraform/campaigns.tf
@@ -1,3 +1,11 @@
+resource "aws_route53_record" "landing" {
+  zone_id = aws_route53_zone.fs_maindomain.zone_id
+  name    = "landing.${aws_route53_zone.fs_maindomain.name}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["a2cbfad5d1d14c9eb6099182bb1adb48.unbouncepages.com"]
+}
+
 resource "aws_route53_record" "usa" {
   zone_id = aws_route53_zone.fs_maindomain.zone_id
   name    = "usa.${aws_route53_zone.fs_maindomain.name}"
