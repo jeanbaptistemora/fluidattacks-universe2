617384021bbd54671ef6a32c549678ff5de5812a
Sebastian Lizcano <slizcano@fluidattacks.com>
2020-09-24 14:28:24 -0500

    integrates\refac(back): #3099.6 add org indicators
    - add org indicators for resources distribution, severity gauge
    
    
    Former-commit-id: 537ebe4aada646f68b389926ae524dc86ae21b84

--- integrates/analytics/generators/gauge/severity.py
+++ integrates/analytics/generators/gauge/severity.py
@@ -1,7 +1,15 @@
 # Standard library
 import asyncio
+from typing import (
+    NamedTuple,
+    Tuple,
+)
 
 # Third party libraries
+from aioextensions import (
+    collect,
+)
+from async_lru import alru_cache
 from backend.api.dataloaders.finding import (
     FindingLoader,
 )
@@ -17,8 +25,14 @@ from analytics.colors import (
     RISK,
 )
 
+Severity = NamedTuple('Severity', [
+    ('max_open_severity', float),
+    ('max_severity_found', float),
+])
 
-async def generate_one(group: str):
+
+@alru_cache(maxsize=None, typed=True)
+async def generate_one(group: str) -> Severity:
     group_data = await GroupLoader().load(group)
 
     findings = await FindingLoader().load_many(
@@ -34,14 +48,34 @@ async def generate_one(group: str):
 
     max_open_severity = group_data['attrs'].get('max_open_severity', 0)
 
+    return Severity(
+        max_open_severity=max_open_severity,
+        max_severity_found=max_severity_found,
+    )
+
+
+async def get_data_many_groups(groups: Tuple[str]) -> Severity:
+    groups_data = await collect(map(generate_one, list(groups)))
+
+    return Severity(
+        max_open_severity=0 if not groups_data else max(
+            [group.max_open_severity for group in groups_data]
+        ),
+        max_severity_found=0 if not groups_data else max(
+            [group.max_severity_found for group in groups_data]
+        ),
+    )
+
+
+def format_data(data: Severity) -> dict:
     return {
         'color': {
             'pattern': [RISK.more_passive, RISK.more_agressive],
         },
         'data': {
             'columns': [
-                ['Max severity found', max_severity_found],
-                ['Max open severity', max_open_severity],
+                ['Max severity found', data.max_severity_found],
+                ['Max open severity', data.max_open_severity],
             ],
             'type': 'gauge',
         },
@@ -63,9 +97,22 @@ async def generate_one(group: str):
 async def generate_all():
     async for group in utils.iterate_groups():
         utils.json_dump(
-            document=await generate_one(group),
+            document=format_data(
+                data=await generate_one(group),
+            ),
             entity='group',
             subject=group,
+        )
+
+    async for org_id, _, org_groups in (
+        utils.iterate_organizations_and_groups()
+    ):
+        utils.json_dump(
+            document=format_data(
+                data=await get_data_many_groups(org_groups),
+            ),
+            entity='organization',
+            subject=org_id,
         )
 
 
--- integrates/analytics/generators/pie_chart/resources.py
+++ integrates/analytics/generators/pie_chart/resources.py
@@ -1,7 +1,18 @@
 # Standard library
 import asyncio
+from typing import (
+    List,
+    NamedTuple,
+    Tuple,
+)
 
 # Third party libraries
+from aioextensions import (
+    collect,
+)
+from async_lru import (
+    alru_cache,
+)
 from backend.api.dataloaders.project import (
     ProjectLoader as GroupLoader,
 )
@@ -14,29 +25,55 @@ from analytics.colors import (
     OTHER,
 )
 
+Resources = NamedTuple('Resources', [
+    ('environments', List[str]),
+    ('repositories', List[str]),
+])
 
+
+@alru_cache(maxsize=None, typed=True)
 async def generate_one(group: str):
     group_data = await GroupLoader().load(group)
 
     repositories = [
-        env
+        f'{env.get("urlRepo", "").lower()}+{env.get("branch", "").lower()}'
         for env in group_data['attrs'].get('repositories', [])
         if 'historic_state' not in env
         or env['historic_state'][-1]['state'].lower() == 'active'
     ]
 
     environments = [
-        env
+        str(env.get('urlEnv', '').lower())
         for env in group_data['attrs'].get('environments', [])
         if 'historic_state' not in env
         or env['historic_state'][-1]['state'].lower() == 'active'
     ]
 
+    return Resources(
+        environments=environments,
+        repositories=repositories,
+    )
+
+
+async def get_data_many_groups(groups: Tuple[str]) -> Resources:
+    groups_data = await collect(map(generate_one, list(groups)))
+
+    return Resources(
+        environments=list(
+            set(env for group in groups_data for env in group.environments)
+        ),
+        repositories=list(
+            set(repo for group in groups_data for repo in group.repositories)
+        ),
+    )
+
+
+def format_data(data: Resources) -> dict:
     return {
         'data': {
             'columns': [
-                ['Repositories', len(repositories)],
-                ['Environments', len(environments)],
+                ['Repositories', len(data.repositories)],
+                ['Environments', len(data.environments)],
             ],
             'type': 'pie',
             'colors': {
@@ -58,9 +95,22 @@ async def generate_one(group: str):
 async def generate_all():
     async for group in utils.iterate_groups():
         utils.json_dump(
-            document=await generate_one(group),
+            document=format_data(
+                data=await generate_one(group),
+            ),
             entity='group',
             subject=group,
+        )
+
+    async for org_id, _, org_groups in (
+        utils.iterate_organizations_and_groups()
+    ):
+        utils.json_dump(
+            document=format_data(
+                data=await get_data_many_groups(org_groups),
+            ),
+            entity='organization',
+            subject=org_id,
         )
 
 
