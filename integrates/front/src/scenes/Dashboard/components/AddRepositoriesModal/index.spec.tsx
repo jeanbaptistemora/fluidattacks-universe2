73dcd254b6447ef1c5e45fc0029799d16ad6b42a
Duvan Antivar <dantivar@fluidattacks.com>
2020-09-24 10:00:01 -0500

    integrates\refac(front): #2705.9 update addrepomodal
    - Lint addrepositoriestmodal spec tsx
    
    
    Former-commit-id: fd029fc48cad53720f77d46856a77a8baeaca186

--- integrates/front/src/scenes/Dashboard/components/AddRepositoriesModal/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/components/AddRepositoriesModal/index.spec.tsx
@@ -1,20 +1,21 @@
-import { mount, ReactWrapper, shallow, ShallowWrapper } from "enzyme";
-import * as React from "react";
-import { Provider } from "react-redux";
-
 import { AddRepositoriesModal } from "scenes/Dashboard/components/AddRepositoriesModal";
+import { Provider } from "react-redux";
+import React from "react";
 import store from "store";
+import { ReactWrapper, ShallowWrapper, mount, shallow } from "enzyme";
 
-const functionMock: (() => void) = (): void => undefined;
+const functionMock: () => void = (): void => undefined;
 
-describe("Add Repositories modal", () => {
+describe("Add Repositories modal", (): void => {
+  it("should return a function", (): void => {
+    expect.hasAssertions();
 
-  it("should return a function", () => {
-    expect(typeof (AddRepositoriesModal))
-      .toEqual("function");
+    expect(typeof AddRepositoriesModal).toStrictEqual("function");
   });
 
-  it("should render", () => {
+  it("should render", (): void => {
+    expect.hasAssertions();
+
     const wrapper: ShallowWrapper = shallow(
       <Provider store={store}>
         <AddRepositoriesModal
@@ -22,13 +23,15 @@ describe("Add Repositories modal", () => {
           onClose={functionMock}
           onSubmit={functionMock}
         />
-      </Provider>,
+      </Provider>
     );
-    expect(wrapper)
-      .toHaveLength(1);
+
+    expect(wrapper).toHaveLength(1);
   });
 
   it("should render input fields and add button", (): void => {
+    expect.hasAssertions();
+
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
         <AddRepositoriesModal
@@ -36,20 +39,19 @@ describe("Add Repositories modal", () => {
           onClose={functionMock}
           onSubmit={functionMock}
         />
-      </Provider>,
+      </Provider>
     );
-    expect(wrapper.find("renderReposFields")
-      .find("select"))
-      .toHaveLength(1);
-    expect(wrapper.find("renderReposFields")
-      .find("input"))
-      .toHaveLength(2);
-    expect(wrapper.find("renderReposFields")
-      .find(".glyphicon-plus"))
-      .toHaveLength(1);
+
+    expect(wrapper.find("renderReposFields").find("select")).toHaveLength(1);
+    expect(wrapper.find("renderReposFields").find("input")).toHaveLength(2);
+    expect(
+      wrapper.find("renderReposFields").find(".glyphicon-plus")
+    ).toHaveLength(1);
   });
 
   it("should add and remove input fields", (): void => {
+    expect.hasAssertions();
+
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
         <AddRepositoriesModal
@@ -57,27 +59,27 @@ describe("Add Repositories modal", () => {
           onClose={functionMock}
           onSubmit={functionMock}
         />
-      </Provider>,
+      </Provider>
     );
 
-    const addButton: ReactWrapper = wrapper.find("renderReposFields")
-                                      .find(".glyphicon-plus");
+    const addButton: ReactWrapper = wrapper
+      .find("renderReposFields")
+      .find(".glyphicon-plus");
     addButton.simulate("click");
-    expect(wrapper.find("renderReposFields")
-      .find("select"))
-      .toHaveLength(2);
-    expect(wrapper.find("renderReposFields")
-      .find("input"))
-      .toHaveLength(4);
 
-    const removeButton: ReactWrapper = wrapper.find("renderReposFields")
-                                         .find(".glyphicon-trash");
+    const RENDER_REPOS_FIELDS_LENGTH: number = 4;
+
+    expect(wrapper.find("renderReposFields").find("select")).toHaveLength(2);
+    expect(wrapper.find("renderReposFields").find("input")).toHaveLength(
+      RENDER_REPOS_FIELDS_LENGTH
+    );
+
+    const removeButton: ReactWrapper = wrapper
+      .find("renderReposFields")
+      .find(".glyphicon-trash");
     removeButton.simulate("click");
-    expect(wrapper.find("renderReposFields")
-      .find("select"))
-      .toHaveLength(1);
-    expect(wrapper.find("renderReposFields")
-      .find("input"))
-      .toHaveLength(2);
+
+    expect(wrapper.find("renderReposFields").find("select")).toHaveLength(1);
+    expect(wrapper.find("renderReposFields").find("input")).toHaveLength(2);
   });
 });
