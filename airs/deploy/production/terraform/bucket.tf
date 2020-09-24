8cd0f13b33b56956e7317ef476a2d3d4d3bf2b8a
Daniel Salazar <dsalazar@fluidattacks.com>
2020-09-24 14:30:59 -0500

    airs\feat(infra): #3359.2 new bucket
    - Add fluidattacks.com bucket
    
    
    Former-commit-id: e2fe0aea4a6693516cf8d66fb92a75ed79e18686

--- airs/deploy/production/terraform/bucket.tf
+++ airs/deploy/production/terraform/bucket.tf
@@ -11,3 +11,17 @@ resource "aws_s3_bucket" "web-bucket" {
     Pry = "General"
   }
 }
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "fluidattacks.com"
+  acl    = "private"
+  region = var.region
+
+  website {
+     index_document = "index.html"
+     error_document = "error/index.html"
+   }
+   tags = {
+    Pry = "General"
+  }
+}
