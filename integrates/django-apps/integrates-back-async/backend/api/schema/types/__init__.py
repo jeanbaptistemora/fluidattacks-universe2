901b5bc7a4700156d0b1663406100d22455a6b6f
David Acevedo <dacevedo@fluidattacks.com>
2020-09-25 16:16:51 -0500

    integrates\refac(back): #159.21 improve api structure
    - reduce types and bindings directory nesting
    - decoupled some types and bindings
    
    
    Former-commit-id: b70367bb283ef7f6fbbcb870b32e1c2871f25e54

--- integrates/django-apps/integrates-back-async/backend/api/schema/types/__init__.py
+++ integrates/django-apps/integrates-back-async/backend/api/schema/types/__init__.py
@@ -5,6 +5,9 @@ from typing import Tuple
 from ariadne import ObjectType
 
 # Local
+from backend.api.schema.types.event import EVENT
+from backend.api.schema.types.finding import FINDING
+from backend.api.schema.types.forces_executions import FORCES_EXECUTIONS
 from backend.api.schema.types.group import GROUP
 from backend.api.schema.types.mutation import MUTATION
 from backend.api.schema.types.organization import ORGANIZATION
@@ -12,9 +15,6 @@ from backend.api.schema.types.query import QUERY
 from backend.api.schema.types.stakeholder import STAKEHOLDER
 
 
-EVENT = ObjectType('Event')
-FINDING = ObjectType('Finding')
-FORCES_EXECUTIONS = ObjectType('ForcesExecutions')
 INTERNAL_NAME = ObjectType('InternalName')
 ME = ObjectType('Me')
 RESOURCE = ObjectType('Resource')
@@ -29,8 +29,8 @@ TYPES: Tuple[ObjectType, ...] = (
     ME,
     MUTATION,
     ORGANIZATION,
+    QUERY,
     RESOURCE,
     STAKEHOLDER,
-    VULNERABILITY,
-    QUERY
+    VULNERABILITY
 )
---
+++ integrates/django-apps/integrates-back-async/backend/api/schema/types/event.py
@@ -0,0 +1,10 @@
+# Standard
+# None
+
+# Third party
+from ariadne import ObjectType
+
+# Local
+# None
+
+EVENT = ObjectType('Event')
