54dcef44b238ba161761aa252e9ff3e5111ec3fc
David Acevedo <dacevedo@fluidattacks.com>
2020-09-25 17:34:51 -0500

    integrates\refac(back): #159.23 improve api structure
    - decoupled more types
    - explicit type bindings
    - removed duplicate comment type
    
    
    Former-commit-id: 5f165f410bf7494fe4c2f34ae1fa43da39073c71

--- integrates/django-apps/integrates-back-async/backend/api/schema/types/__init__.py
+++ integrates/django-apps/integrates-back-async/backend/api/schema/types/__init__.py
@@ -5,21 +5,33 @@ from typing import Tuple
 from ariadne import ObjectType
 
 # Local
-from backend.api.schema.types.event import EVENT
-from backend.api.schema.types.finding import FINDING
-from backend.api.schema.types.forces_executions import FORCES_EXECUTIONS
-from backend.api.schema.types.group import GROUP
-from backend.api.schema.types.internal_name import INTERNAL_NAME
-from backend.api.schema.types.me import ME
-from backend.api.schema.types.mutation import MUTATION
-from backend.api.schema.types.organization import ORGANIZATION
-from backend.api.schema.types.query import QUERY
-from backend.api.schema.types.resource import RESOURCE
-from backend.api.schema.types.stakeholder import STAKEHOLDER
-from backend.api.schema.types.vulnerability import VULNERABILITY
+from .bill import BILL
+from .bill_developer import BILL_DEVELOPER
+from .consult import CONSULT
+from .entity_report_subscription import ENTITY_REPORT_SUBSCRIPTION
+from .execution_vulnerabilities_new import EXECUTION_VULNERABILITIES_NEW
+from .execution_vulnerabilities import EXECUTION_VULNERABILITIES
+from .event import EVENT
+from .finding import FINDING
+from .forces_executions import FORCES_EXECUTIONS
+from .group import GROUP
+from .internal_name import INTERNAL_NAME
+from .me import ME
+from .mutation import MUTATION
+from .organization import ORGANIZATION
+from .query import QUERY
+from .resource import RESOURCE
+from .stakeholder import STAKEHOLDER
+from .vulnerability import VULNERABILITY
 
 
 TYPES: Tuple[ObjectType, ...] = (
+    BILL,
+    BILL_DEVELOPER,
+    CONSULT,
+    ENTITY_REPORT_SUBSCRIPTION,
+    EXECUTION_VULNERABILITIES_NEW,
+    EXECUTION_VULNERABILITIES,
     EVENT,
     FINDING,
     FORCES_EXECUTIONS,
---
+++ integrates/django-apps/integrates-back-async/backend/api/schema/types/execution_vulnerabilities_new.py
@@ -0,0 +1,10 @@
+# Standard
+# None
+
+# Third party
+from ariadne import ObjectType
+
+# Local
+# None
+
+EXECUTION_VULNERABILITIES_NEW = ObjectType('ExecutionVulnerabilitiesNew')
