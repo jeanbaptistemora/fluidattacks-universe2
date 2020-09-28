b97595b0c44068867dd86f4de9ec19396f3b1fd6
Kevin Amado <kamado@fluidattacks.com>
2020-09-28 11:28:36 -0500

    skims\feat(back): #3365.2 f073 model
    Former-commit-id: 5a26637bbe2c226817059b101bcf4f54d1714aaf

--- skims/skims/utils/model.py
+++ skims/skims/utils/model.py
@@ -250,6 +250,30 @@ class FindingEnum(Enum):
         title='utils.model.finding.enum.f061.title',
         type=FindingTypeEnum.SECURITY,
     )
+    F073: FindingMetadata = FindingMetadata(
+        auto_approve=False,
+        cwe='478',
+        description='utils.model.finding.enum.F073.description',
+        impact='utils.model.finding.enum.F073.impact',
+        recommendation='utils.model.finding.enum.F073.recommendation',
+        requirements='utils.model.finding.enum.F073.requirements',
+        severity={
+            "attackComplexity": 0.77,
+            "attackVector": 0.55,
+            "availabilityImpact": 0.0,
+            "confidentialityImpact": 0.0,
+            "exploitability": 0.94,
+            "integrityImpact": 0.22,
+            "privilegesRequired": 0.85,
+            "remediationLevel": 1.0,
+            "reportConfidence": 1.0,
+            "severityScope": 0.0,
+            "userInteraction": 0.85
+        },
+        threat='utils.model.finding.enum.F073.threat',
+        title='utils.model.finding.enum.F073.title',
+        type=FindingTypeEnum.HYGIENE,
+    )
     F085: FindingMetadata = FindingMetadata(
         auto_approve=True,
         cwe='922',
