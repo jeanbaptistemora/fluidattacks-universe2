7932eb76054f6e7b762e998b6261f09b877f9a9d
Andres Aguirre <aaguirre@fluidattacks.com>
2020-09-29 10:19:10 -0500

    integrates\refac(back): #3407.2 rename datetime utils
    - Removed datetime word from datetime utils
    - Removed str functions
    
    
    Former-commit-id: d3012065b47eafc63031217b79def5d487bf3fe4

--- integrates/django-apps/integrates-back-async/backend/utils/datetime.py
+++ integrates/django-apps/integrates-back-async/backend/utils/datetime.py
@@ -10,26 +10,21 @@ TZN = pytz.timezone(settings.TIME_ZONE)
 iso_format: str = '%Y-%m-%d %H:%M:%S'
 
 
-def get_str_as_datetime(
+def get_from_str(
         date_str: str, date_format: str = iso_format) -> datetime:
     unaware_datetime = datetime.strptime(date_str, date_format)
     return make_aware(unaware_datetime, TZN)
 
 
-def get_datetime_as_str(date: datetime, date_format: str = iso_format) -> str:
+def get_as_str(date: datetime, date_format: str = iso_format) -> str:
     return date.strftime(date_format)
 
 
-def get_current_datetime() -> datetime:
+def get_now() -> datetime:
     return datetime.now(tz=TZN)
 
 
-def get_current_datetime_as_str(date_format: str = iso_format) -> str:
-    now = get_current_datetime()
-    return get_datetime_as_str(now, date_format=date_format)
-
-
-def get_datetime_plus_delta(
+def get_plus_delta(
         date: datetime, days: int = 0, seconds: int = 0,
         microseconds: int = 0, milliseconds: int = 0, minutes: int = 0,
         hours: int = 0, weeks: int = 0) -> datetime:
@@ -40,47 +35,19 @@ def get_datetime_plus_delta(
     return date_plus_delta
 
 
-def get_datetime_plus_delta_as_str(
-        date: datetime, days: int = 0, seconds: int = 0, microseconds: int = 0,
-        milliseconds: int = 0, minutes: int = 0, hours: int = 0,
-        weeks: int = 0, date_format: str = iso_format) -> str:
-    date_plus_delta_as_str = get_datetime_as_str(
-        get_datetime_plus_delta(
-            date=date, days=days, seconds=seconds, microseconds=microseconds,
-            milliseconds=milliseconds, minutes=minutes, hours=hours,
-            weeks=weeks
-        ),
-        date_format=date_format
-    )
-    return date_plus_delta_as_str
-
-
-def get_current_datetime_plus_delta(
+def get_now_plus_delta(
         days: int = 0, seconds: int = 0, microseconds: int = 0,
         milliseconds: int = 0, minutes: int = 0, hours: int = 0,
         weeks: int = 0) -> datetime:
-    now = get_current_datetime()
-    now_plus_delta = get_datetime_plus_delta(
+    now = get_now()
+    now_plus_delta = get_plus_delta(
         now, days=days, seconds=seconds, microseconds=microseconds,
         milliseconds=milliseconds, minutes=minutes, hours=hours, weeks=weeks
     )
     return now_plus_delta
 
 
-def get_current_datetime_plus_delta_as_str(
-        days: int = 0, seconds: int = 0, microseconds: int = 0,
-        milliseconds: int = 0, minutes: int = 0, hours: int = 0,
-        weeks: int = 0, date_format: str = iso_format) -> str:
-    now = get_current_datetime()
-    now_plus_delta_as_str = get_datetime_plus_delta_as_str(
-        now, days=days, seconds=seconds, microseconds=microseconds,
-        milliseconds=milliseconds, minutes=minutes, hours=hours, weeks=weeks,
-        date_format=date_format
-    )
-    return now_plus_delta_as_str
-
-
-def get_datetime_minus_delta(
+def get_minus_delta(
         date: datetime, days: int = 0, seconds: int = 0,
         microseconds: int = 0, milliseconds: int = 0, minutes: int = 0,
         hours: int = 0, weeks: int = 0) -> datetime:
@@ -91,41 +58,13 @@ def get_datetime_minus_delta(
     return date_minus_delta
 
 
-def get_datetime_minus_delta_as_str(
-        date: datetime, days: int = 0, seconds: int = 0, microseconds: int = 0,
-        milliseconds: int = 0, minutes: int = 0, hours: int = 0,
-        weeks: int = 0, date_format: str = iso_format) -> str:
-    date_minus_delta_as_str = get_datetime_as_str(
-        get_datetime_minus_delta(
-            date=date, days=days, seconds=seconds, microseconds=microseconds,
-            milliseconds=milliseconds, minutes=minutes, hours=hours,
-            weeks=weeks
-        ),
-        date_format=date_format
-    )
-    return date_minus_delta_as_str
-
-
-def get_current_datetime_minus_delta(
+def get_now_minus_delta(
         days: int = 0, seconds: int = 0, microseconds: int = 0,
         milliseconds: int = 0, minutes: int = 0, hours: int = 0,
         weeks: int = 0) -> datetime:
-    now = get_current_datetime()
-    now_minus_delta = get_datetime_minus_delta(
+    now = get_now()
+    now_minus_delta = get_minus_delta(
         now, days=days, seconds=seconds, microseconds=microseconds,
         milliseconds=milliseconds, minutes=minutes, hours=hours, weeks=weeks
     )
     return now_minus_delta
-
-
-def get_current_datetime_minus_delta_as_str(
-        days: int = 0, seconds: int = 0, microseconds: int = 0,
-        milliseconds: int = 0, minutes: int = 0, hours: int = 0,
-        weeks: int = 0, date_format: str = iso_format) -> str:
-    now = get_current_datetime()
-    now_minus_delta_as_str = get_datetime_minus_delta_as_str(
-        now, days=days, seconds=seconds, microseconds=microseconds,
-        milliseconds=milliseconds, minutes=minutes, hours=hours, weeks=weeks,
-        date_format=date_format
-    )
-    return now_minus_delta_as_str
