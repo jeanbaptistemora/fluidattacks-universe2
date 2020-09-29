7f2515ba8849cbf9e060cbecbc920c0b8c75a55f
Andres Aguirre <aaguirre@fluidattacks.com>
2020-09-29 11:21:38 -0500

    integrates\test(back): #3407.3 datetime utils tests
    - Added unittest for datetime utils
    
    
    Former-commit-id: 0f344bf695c88c0c2d184d4b0a40dac7a61aea49

---
+++ integrates/test_async/test_datetime.py
@@ -0,0 +1,66 @@
+import pytz
+from datetime import datetime, timedelta
+
+from django.conf import settings
+from freezegun import freeze_time
+
+from backend.utils import (
+    datetime as datetime_utils
+)
+
+tzn = pytz.timezone(settings.TIME_ZONE)
+
+
+@freeze_time("2019-12-01")
+def test_get_from_str():
+    now = datetime_utils.get_now()
+    now_str = datetime_utils.get_as_str(now)
+    assert datetime_utils.get_from_str(now_str) == now
+
+
+@freeze_time("2019-12-01")
+def test_get_as_str():
+    now = datetime_utils.get_now()
+    now_str = datetime_utils.get_as_str(now)
+    assert datetime_utils.get_as_str(now) == now_str
+
+
+@freeze_time("2019-12-01")
+def test_get_now():
+    now = datetime.now(tz=tzn)
+    assert datetime_utils.get_now() == now
+
+
+@freeze_time("2019-12-01")
+def test_get_plus_delta():
+    now = datetime_utils.get_now()
+    delta = timedelta(days=1, minutes=1, seconds=1)
+    assert datetime_utils.get_plus_delta(
+        now, days=1, minutes=1, seconds=1
+    ) == now + delta
+
+
+@freeze_time("2019-12-01")
+def test_get_now_plus_delta():
+    now = datetime_utils.get_now()
+    delta = timedelta(days=1, minutes=1, seconds=1, hours=1)
+    assert datetime_utils.get_now_plus_delta(
+        days=1, minutes=1, seconds=1, hours=1
+    ) == now + delta
+
+
+@freeze_time("2019-12-01")
+def test_get_minus_delta():
+    now = datetime_utils.get_now()
+    delta = timedelta(days=1, minutes=1, seconds=1)
+    assert datetime_utils.get_minus_delta(
+        now, days=1, minutes=1, seconds=1
+    ) == now - delta
+
+@freeze_time("2019-12-01")
+def test_get_now_minus_delta():
+    now = datetime_utils.get_now()
+    delta = timedelta(days=1, minutes=1, seconds=1, hours=1)
+    assert datetime_utils.get_now_minus_delta(
+        days=1, minutes=1, seconds=1, hours=1
+    ) == now - delta
