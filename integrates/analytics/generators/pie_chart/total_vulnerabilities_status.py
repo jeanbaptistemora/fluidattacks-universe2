6875f95e356a2fc692307a11fbd6407d5b1afc08
Sebastian Lizcano <slizcano@fluidattacks.com>
2020-09-30 17:46:23 -0500

    integrates\refac(back): #3100.2 pie_chart portfolio
    - pie_chart portfolio for open and all vulns by groups,
    severity gauge
    
    
    Former-commit-id: 4b052409a096d4cc32cfa83115d6e71dc463af40

---
+++ integrates/analytics/generators/pie_chart/total_vulnerabilities_status.py
@@ -0,0 +1,63 @@
+# Standard library
+import asyncio
+from typing import (
+    List,
+)
+
+# Third party libraries
+from aioextensions import (
+    collect,
+)
+from async_lru import alru_cache
+from backend.domain import (
+    project as group_domain,
+)
+
+# Local libraries
+from analytics import (
+    utils,
+)
+from analytics.generators.pie_chart.utils import (
+    format_data,
+    PortfoliosGroupsInfo,
+    slice_groups,
+)
+
+
+@alru_cache(maxsize=None, typed=True)
+async def get_data_group(group: str) -> PortfoliosGroupsInfo:
+    item = await group_domain.get_attributes(group, [
+        'open_vulnerabilities',
+        'closed_vulnerabilities',
+    ])
+
+    return PortfoliosGroupsInfo(
+        group_name=group.lower(),
+        value=item.get('open_vulnerabilities', 0) +
+        item.get('closed_vulnerabilities', 0)
+    )
+
+
+async def get_data_groups(groups: List[str]) -> List[PortfoliosGroupsInfo]:
+    groups_data = await collect(map(get_data_group, groups))
+    total_vulnerabilities = sum(
+        [group.value for group in groups_data]
+    )
+
+    return slice_groups(groups_data, total_vulnerabilities)
+
+
+async def generate_all():
+    async for org_id, org_name, _ in utils.iterate_organizations_and_groups():
+        for portfolio, groups in await utils.get_portfolios_groups(org_name):
+            utils.json_dump(
+                document=format_data(
+                    groups_data=await get_data_groups(groups),
+                ),
+                entity='portfolio',
+                subject=f'{org_id}PORTFOLIO#{portfolio}',
+            )
+
+
+if __name__ == '__main__':
+    asyncio.run(generate_all())
---
+++ integrates/analytics/generators/pie_chart/utils.py
@@ -0,0 +1,62 @@
+from decimal import Decimal
+from operator import attrgetter
+from typing import (
+    List,
+    NamedTuple,
+)
+
+from analytics.colors import RISK
+
+PortfoliosGroupsInfo = NamedTuple('PortfoliosGroupsInfo', [
+    ('group_name', str),
+    ('value', Decimal),
+])
+
+MAX_GROUPS_DISPLAYED = 4
+
+
+def slice_groups(
+        groups_data: List[PortfoliosGroupsInfo],
+        total_value: Decimal) -> List[PortfoliosGroupsInfo]:
+    groups_data_sorted = sorted(
+        groups_data,
+        key=attrgetter('value'),
+        reverse=True
+    )
+    groups_data_sliced = groups_data_sorted[:MAX_GROUPS_DISPLAYED]
+
+    if len(groups_data_sorted) > MAX_GROUPS_DISPLAYED:
+        return groups_data_sliced + [
+            PortfoliosGroupsInfo(
+                group_name='others',
+                value=total_value - sum([
+                    group.value for group in groups_data_sliced
+                ])
+            )
+        ]
+
+    return groups_data_sliced
+
+
+def format_data(groups_data: List[PortfoliosGroupsInfo]) -> dict:
+    return dict(
+        data=dict(
+            columns=[
+                [group.group_name] + [group.value]
+                for group in groups_data
+            ],
+            type='pie',
+            colors=dict(
+                [group.group_name] + [color]
+                for group, color in zip(groups_data, reversed(RISK))
+            ),
+        ),
+        legend=dict(
+            position='right',
+        ),
+        pie=dict(
+            label=dict(
+                show=True,
+            ),
+        ),
+    )
