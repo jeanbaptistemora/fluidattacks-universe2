079e011e084e9c13c5c49520518004442e359e3f
Duvan Antivar <dantivar@fluidattacks.com>
2020-09-30 17:05:53 -0500

    integrates\refac(front): #793.4 update confirmdialog css
    - Update confirmdialog css
    
    
    Former-commit-id: 1318a4c64e623bb14e865dce571b206fe7f30aaa

--- integrates/front/src/components/ConfirmDialog/index.tsx
+++ integrates/front/src/components/ConfirmDialog/index.tsx
@@ -1,9 +1,9 @@
 import { Button } from "components/Button";
-import { ButtonToolbar } from "react-bootstrap";
 import { Modal } from "components/Modal";
 import React from "react";
 import _ from "lodash";
 import { translate } from "utils/translations/translate";
+import styled, { StyledComponent } from "styled-components";
 
 interface IConfirmFn {
   (confirmCallback: () => void, cancelCallback?: () => void): void;
@@ -14,6 +14,13 @@ interface IConfirmDialogProps {
   title: string;
   children: (confirm: IConfirmFn) => React.ReactNode;
 }
+
+const StyledToolbar: StyledComponent<
+  "div",
+  Record<string, unknown>
+> = styled.div.attrs({
+  className: "fr",
+})``;
 
 const ConfirmDialog: React.FC<IConfirmDialogProps> = (
   props: Readonly<IConfirmDialogProps>
@@ -59,16 +66,14 @@ const ConfirmDialog: React.FC<IConfirmDialogProps> = (
     <React.Fragment>
       <Modal
         footer={
-          // We need className to override default styles from react-bootstrap
-          // eslint-disable-next-line react/forbid-component-props
-          <ButtonToolbar className={"pull-right"}>
+          <StyledToolbar>
             <Button onClick={handleClose}>
               {translate.t("confirmmodal.cancel")}
             </Button>
             <Button onClick={handleProceed}>
               {translate.t("confirmmodal.proceed")}
             </Button>
-          </ButtonToolbar>
+          </StyledToolbar>
         }
         headerTitle={title}
         open={isOpen}
