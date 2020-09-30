4aeafc9ce624b451e7608fba3f8533fa6c883d5b
Duvan Antivar <dantivar@fluidattacks.com>
2020-09-30 14:20:31 -0500

    integrates\refac(front): #793.2 update button css
    - Add stylecomponent to button
    - Add custom css functions to global css
    
    
    Former-commit-id: 021562f6af6fbc5951aac21504230ce08e24c694

--- integrates/front/src/components/Button/index.tsx
+++ integrates/front/src/components/Button/index.tsx
@@ -1,11 +1,31 @@
-/* eslint-disable react/forbid-component-props, react/jsx-props-no-spreading
+/* eslint-disable react/forbid-component-props, react/jsx-props-no-spreading,
+   @typescript-eslint/no-unused-vars
   --------
-  We need className to override default styles and props spreading in
-  order to pass down props to react-bootstrap Button.
+  We need className to override default styles, props spreading in
+  order to pass down props to react-bootstrap Button and no unused
+  vars to allow the declaration of the new button css style.
 */
 import { Button } from "react-bootstrap";
 import React from "react";
 import style from "components/Button/index.css";
+import styled, { StyledComponent } from "styled-components";
+
+const _StyledButton: StyledComponent<
+  "button",
+  Record<string, unknown>
+> = styled.button.attrs(
+  (
+    props: React.ButtonHTMLAttributes<HTMLButtonElement>
+  ): {
+    className: string;
+    type: "submit" | "reset" | "button";
+  } => ({
+    className:
+      "b--bd b--bh b--red ba bg-bd bg-bh bg-white br0 cna-bd color-bd color-bh " +
+      "f2-5 fw100 ml2 montserrat o-bd ph3 pv2-5 red svg-box ws-normal",
+    type: props.type ?? "button",
+  })
+)``;
 
 const button: React.FC<Button.ButtonProps> = (
   props: Readonly<Button.ButtonProps>
