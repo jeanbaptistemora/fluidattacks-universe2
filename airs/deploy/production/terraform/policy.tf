fd7fd67e62945bd534ef45fdae904b32ceb24f16
Daniel Salazar <dsalazar@fluidattacks.com>
2020-10-01 17:48:52 -0500

    airs\feat(infra): #3445.1 prod bucket policy
    - Give cloudfront the ability to list items
    in the production bucket
    
    
    Former-commit-id: 2607993a9c735223d2c48182bbc55bbbd1e5af74

--- airs/deploy/production/terraform/policy.tf
+++ airs/deploy/production/terraform/policy.tf
@@ -8,6 +8,7 @@ data "aws_iam_policy_document" "bucket_policy" {
     }
     actions = [
       "s3:GetObject",
+      "s3:ListBucket",
     ]
     resources = [
       "arn:aws:s3:::fluidattacks.com/*",
