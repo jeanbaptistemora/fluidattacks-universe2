d2a399b7d138795d66440237695ac624c2f5cadb
Daniel Salazar <dsalazar@fluidattacks.com>
2020-09-30 19:44:14 -0500

    airs\refac(infra): #3429.7 rename dns resource
    Former-commit-id: 17b357dc340a26544af1eca620b90f47a7917c42

--- airs/deploy/production/terraform/dns.tf
+++ airs/deploy/production/terraform/dns.tf
@@ -1,4 +1,4 @@
-resource "aws_route53_record" "web-ephemeral-alias" {
+resource "aws_route53_record" "fluidattacks_com" {
   zone_id = data.aws_route53_zone.fluidattacks.id
   name    = "fluidattacks.com"
   type    = "A"
