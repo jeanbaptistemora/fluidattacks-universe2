d5377fb3e0ca338eb7f537413e5fee4a4c98802c
Daniel Salazar <dsalazar@fluidattacks.com>
2020-10-01 16:13:42 -0500

    serves\feat(infra): #3432.5 common subnet
    - Add common subnet to vpc where all common assets will run
    
    
    Former-commit-id: 2a86a3cee47c5b044616df95adc8197bf2707436

--- serves/services/fluid-vpc/terraform/subnets.tf
+++ serves/services/fluid-vpc/terraform/subnets.tf
@@ -9,3 +9,15 @@ resource "aws_subnet" "subnet-autoscaling-ci" {
     "management:product" = "serves"
   }
 }
+
+resource "aws_subnet" "subnet-common" {
+  vpc_id            = aws_vpc.fluid-vpc.id
+  cidr_block        = "192.168.10.0/23"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name                 = "common"
+    "management:type"    = "production"
+    "management:product" = "serves"
+  }
+}
