20bfb7d467824ed8b0d60d1dcb5a87b1af0a270b
Daniel Salazar <dsalazar@fluidattacks.com>
2020-10-01 12:38:59 -0500

    integrates\refac(infra): #3432.3 remove old cache cluster
    - Remove cache cluster attached to old vpn and old cluster
    
    
    Former-commit-id: e6d56966b7434ef9a0bddb47c8306b9f19afe777

--- integrates/deploy/cache-db/terraform/database.tf
+++ integrates/deploy/cache-db/terraform/database.tf
@@ -1,30 +1,3 @@
-resource "aws_elasticache_subnet_group" "cache-db" {
-  name       = "integrates-cache-db-subnet-group"
-  subnet_ids = var.eks-subnets
-}
-
-resource "aws_elasticache_replication_group" "cache-db" {
-  replication_group_id          = "integrates-cache-db"
-  replication_group_description = "Integrates Redis cache-db"
-  node_type                     = "cache.t2.medium"
-  parameter_group_name          = "default.redis5.0.cluster.on"
-  subnet_group_name             = aws_elasticache_subnet_group.cache-db.name
-  automatic_failover_enabled    = true
-  at_rest_encryption_enabled    = true
-  transit_encryption_enabled    = false
-  port                          = 6379
-
-  cluster_mode {
-    replicas_per_node_group = 1
-    num_node_groups         = 1
-  }
-
-  tags = {
-    "management:type"    = "production"
-    "management:product" = "integrates"
-  }
-}
-
 resource "aws_elasticache_subnet_group" "cache_db" {
   name       = "integrates-cache"
   subnet_ids = var.subnets
--- integrates/deploy/cache-db/terraform/variables.tf
+++ integrates/deploy/cache-db/terraform/variables.tf
@@ -2,16 +2,6 @@ data "aws_caller_identity" "current" {}
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
-variable "eks-subnets" {
-  type = list(string)
-  default = [
-    "subnet-0756fc7f774c4f5de",
-    "subnet-0f5137175d68f5284",
-    "subnet-cd196287",
-    "subnet-edb402c3",
-  ]
-}
-
 variable "subnets" {
   type = list(string)
   default = [
