dad1f34ca4cac0f5bf73340861d3ffb5dd648b5b
Andres Aguirre <aaguirre@fluidattacks.com>
2020-10-05 12:24:35 -0500

    integrates\test(back): #3454.2 test for default date
    - Added test for dafault date
    
    
    Former-commit-id: f29681ebfd4473cad18b4b8003105a482f63a0cd

--- integrates/test_async/test_datetime.py
+++ integrates/test_async/test_datetime.py
@@ -12,11 +12,24 @@ tzn = pytz.timezone(settings.TIME_ZONE)
 
 
 @freeze_time("2019-12-01")
+def test_default_date():
+    default_date = datetime_utils.get_from_str(datetime_utils.DEFAULT_STR)
+    assert datetime_utils.DEFAULT_STR == datetime_utils.get_as_str(default_date)
+    delta = timedelta(days=1, minutes=1, seconds=1, microseconds=1)
+    assert datetime_utils.get_plus_delta(
+        default_date, days=1, minutes=1, seconds=1, microseconds=1
+    ) == default_date + delta
+    assert datetime_utils.get_minus_delta(
+        default_date, days=1, minutes=1, seconds=1, microseconds=1
+    ) == default_date - delta
+
+
+@freeze_time("2019-12-01")
 def test_get_from_str():
     now = datetime_utils.get_now()
     now_str = datetime_utils.get_as_str(now)
     assert datetime_utils.get_from_str(now_str) == now
-
+    
 
 @freeze_time("2019-12-01")
 def test_get_as_str():
