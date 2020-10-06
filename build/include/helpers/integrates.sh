e425fecf70aa08595ab785ce87246b95fe3c6844
Daniel Aguirre <daguirre@fluidattacks.com>
2020-10-06 17:41:03 -0500

    integrates\feat(cross): #3643 change environmental var
    - Change MINION_LOCAL_ENABLED to false to avoid the change of
    endpoint during CI jobs and production tests.
    - Delete conditional that make fails on runs to prod when
    the branch != master because is an explicit validation
    anyone can change it manually and works and for that is
    unnecessary.
    
    
    Former-commit-id: 92a87a2234246bb0b64cbd6f4f66fa4a6553c9c0

--- build/include/helpers/integrates.sh
+++ build/include/helpers/integrates.sh
@@ -11,13 +11,8 @@ function helper_integrates_aws_login {
   export AWS_SECRET_ACCESS_KEY
 
       if [ "${user}"  == 'production' ]; then
-        if [ "${CI_COMMIT_REF_NAME}" == 'master' ]; then
-              AWS_ACCESS_KEY_ID="${INTEGRATES_PROD_AWS_ACCESS_KEY_ID}" \
-          &&  AWS_SECRET_ACCESS_KEY="${INTEGRATES_PROD_AWS_SECRET_ACCESS_KEY}"
-        else
-              echo 'Not enough permissions for logging in as production' \
-          &&  return 1
-        fi
+            AWS_ACCESS_KEY_ID="${INTEGRATES_PROD_AWS_ACCESS_KEY_ID}" \
+        &&  AWS_SECRET_ACCESS_KEY="${INTEGRATES_PROD_AWS_SECRET_ACCESS_KEY}"
       elif [ "${user}" == 'development' ]; then
             AWS_ACCESS_KEY_ID="${INTEGRATES_DEV_AWS_ACCESS_KEY_ID}" \
         &&  AWS_SECRET_ACCESS_KEY="${INTEGRATES_DEV_AWS_SECRET_ACCESS_KEY}"
