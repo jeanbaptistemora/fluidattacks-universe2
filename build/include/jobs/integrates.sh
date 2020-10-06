4c46e2ecbea6798d08efdbd950c2bb356e1e032f
Mario Varas <mvaras@fluidattacks.com>
2020-10-06 15:07:03 -0500

    integrates\fix(job): #3399.24 rm unnecessary delete
    Former-commit-id: d33b7133d00fee9e75dafb566f7ccf6d4fb0cc09

--- build/include/jobs/integrates.sh
+++ build/include/jobs/integrates.sh
@@ -1357,9 +1357,8 @@ function job_integrates_deploy_back_ephemeral_stop {
   &&  helper_common_update_kubeconfig "${cluster}" "${region}" \
   &&  echo '[INFO] Deleting deployment' \
   &&  kubectl delete deployment -n "${namespace}" "integrates-${CI_COMMIT_REF_SLUG}" \
-  &&  kubectl delete secret -n "${namespace}" "integrates-${CI_COMMIT_REF_SLUG}" \
   &&  kubectl delete deployment -n "${namespace}" "integrates2-${CI_COMMIT_REF_SLUG}" \
-  &&  kubectl delete secret -n "${namespace}" "integrates2-${CI_COMMIT_REF_SLUG}" \
+  &&  kubectl delete secret -n "${namespace}" "integrates-${CI_COMMIT_REF_SLUG}" \
   &&  popd \
   ||  return 1
 }
