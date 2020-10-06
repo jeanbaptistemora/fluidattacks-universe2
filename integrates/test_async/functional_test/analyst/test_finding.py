40faf566fe519d66bb453e8220922492a5c1bac6
Andres Aguirre <aaguirre@fluidattacks.com>
2020-10-06 17:55:17 -0500

    integrates\test(back): #3022.32 finding analyst test
    - Added some functional tests for finding with an analyst role
    
    
    Former-commit-id: 218e6dffb20cdce870abf1bec8b6091dbcedca04

--- integrates/test_async/functional_test/analyst/test_finding.py
+++ integrates/test_async/functional_test/analyst/test_finding.py
@@ -195,3 +195,207 @@ async def test_finding():
     result = await get_result(data)
     assert 'errors' not in result
     assert result['data']['submitDraft']['success']
+
+    query = f'''
+        mutation {{
+            rejectDraft(findingId: "{draft_id}") {{
+                success
+            }}
+        }}
+    '''
+    data = {'query': query}
+    result = await get_result(data)
+    assert 'errors' not in result
+    assert 'success' in result['data']['rejectDraft']
+    assert result['data']['rejectDraft']['success']
+
+    query = f'''
+        mutation {{
+            approveDraft(draftId: "{draft_id}") {{
+                success
+            }}
+        }}
+    '''
+    data = {'query': query}
+    result = await get_result(data, stakeholder='integratesmanager@gmail.com')
+    assert 'errors' not in result
+    assert result['data']['approveDraft']['success']
+
+    finding_id = draft_id
+    query = f'''
+        query {{
+            project(projectName: "{group_name}"){{
+                findings {{
+                    id
+                }}
+            }}
+        }}
+    '''
+    data = {'query': query}
+    result = await get_result(data)
+    assert 'errors' not in result
+    group_findings = result['data']['project']['findings']
+    finding_ids = [finding['id'] for finding in group_findings]
+    assert finding_id in finding_ids
+
+    query = f'''{{
+        finding(identifier: "{finding_id}"){{
+            id
+            projectName
+            releaseDate
+            openVulnerabilities
+            closedVulnerabilities
+            tracking
+            records
+            severity
+            cvssVersion
+            exploit
+            evidence
+            state
+            lastVulnerability
+            title
+            scenario
+            actor
+            description
+            requirements
+            attackVectorDesc
+            threat
+            recommendation
+            affectedSystems
+            compromisedAttributes
+            compromisedRecords
+            cweUrl
+            btsUrl
+            risk
+            remediated
+            type
+            age
+            isExploitable
+            severityScore
+            reportDate
+            historicTreatment
+            historicState
+            currentState
+            newRemediated
+            verified
+            vulnerabilities {{
+                id
+            }}
+            portsVulns {{
+                specific
+            }}
+            inputsVulns {{
+                specific
+            }}
+            linesVulns {{
+                specific
+            }}
+            pendingVulns {{
+                specific
+            }}
+            analyst
+            observations{{
+                content
+                email
+            }}
+            __typename
+        }}
+    }}'''
+    data = {'query': query}
+    result = await get_result(data)
+    assert 'errors' not in result
+    assert result['data']['finding']['id'] == finding_id
+    assert result['data']['finding']['projectName'] == group_name
+    result['data']['finding']['releaseDate'] = (
+        result['data']['finding']['releaseDate'][:-9]
+    )
+    assert result['data']['finding']['releaseDate'] == today
+    assert result['data']['finding']['openVulnerabilities'] == 1
+    assert result['data']['finding']['closedVulnerabilities'] == 2
+    assert result['data']['finding']['tracking'] == [{'closed': 2, 'cycle': 0, 'date': '2020-10-06', 'effectiveness': 66, 'open': 1}]
+    assert result['data']['finding']['records'] == '[]'
+    assert result['data']['finding']['severity'] == {
+        'attackComplexity': 0.77,
+        'attackVector': 0.62,
+        'availabilityImpact': 0.0,
+        'availabilityRequirement': 1.0,
+        'confidentialityImpact': 0.0,
+        'confidentialityRequirement': 1.0,
+        'exploitability': 0.91,
+        'integrityImpact': 0.22,
+        'integrityRequirement': 1.0,
+        'modifiedAttackComplexity': 0.77,
+        'modifiedAttackVector': 0.62,
+        'modifiedAvailabilityImpact': 0.0,
+        'modifiedConfidentialityImpact': 0.0,
+        'modifiedIntegrityImpact': 0.22,
+        'modifiedPrivilegesRequired': 0.62,
+        'modifiedSeverityScope': 0.0,
+        'modifiedUserInteraction': 0.85,
+        'privilegesRequired': 0.62,
+        'remediationLevel': 0.97,
+        'reportConfidence': 0.92,
+        'severityScope': 0.0,
+        'userInteraction': 0.85
+    }
+    assert result['data']['finding']['cvssVersion'] == '3.1'
+    assert result['data']['finding']['exploit'] == ''
+    assert len(result['data']['finding']['evidence']) == 7
+    assert result['data']['finding']['evidence']['evidence2']['description'] == evidence_description
+    assert f'unittesting-{finding_id}' in result['data']['finding']['evidence']['animation']['url']
+    assert result['data']['finding']['state'] == 'open'
+    assert result['data']['finding']['title'] == title
+    assert result['data']['finding']['scenario'] == ''
+    assert result['data']['finding']['actor'] == ''
+    assert result['data']['finding']['description'] == description
+    assert result['data']['finding']['requirements'] == requirements
+    assert result['data']['finding']['attackVectorDesc'] == ''
+    assert result['data']['finding']['threat'] == threat
+    assert result['data']['finding']['recommendation'] == recommendation
+    assert result['data']['finding']['affectedSystems'] == ''
+    assert result['data']['finding']['compromisedAttributes'] == ''
+    assert result['data']['finding']['compromisedRecords'] == 0
+    assert result['data']['finding']['cweUrl'] == cwe
+    assert result['data']['finding']['btsUrl'] == ''
+    assert result['data']['finding']['risk'] == risk
+    assert result['data']['finding']['remediated'] == False
+    assert result['data']['finding']['type'] == draft_type
+    assert result['data']['finding']['isExploitable'] == False
+    assert result['data']['finding']['severityScore'] == 2.9
+    result['data']['finding']['reportDate'] = (
+        result['data']['finding']['reportDate'][:-9]
+    )
+    assert result['data']['finding']['reportDate'] == today
+    assert result['data']['finding']['currentState'] == 'APPROVED'
+    assert result['data']['finding']['newRemediated'] == False
+    assert result['data']['finding']['verified'] == True
+    assert result['data']['finding']['analyst'] == 'integratesanalyst@gmail.com'
+    assert result['data']['finding']['observations'] == []
+    historic_state = result['data']['finding']['historicState']
+    for index in range(len(historic_state)):
+        historic_state[index]['date'] = (
+            historic_state[index]['date'][:-9]
+        )
+    assert historic_state == [
+        {
+            'analyst': 'integratesanalyst@gmail.com',
+            'date': today,
+            'state': 'CREATED'
+        },
+        {
+            'analyst': 'integratesanalyst@gmail.com',
+            'date': today,
+            'state': 'SUBMITTED'
+        },
+        {
+            'analyst': 'integratesanalyst@gmail.com',
+            'date': today,
+            'state': 'REJECTED'
+        },
+        {
+            'analyst': 'integratesmanager@gmail.com',
+            'date': today,
+            'state': 'APPROVED'
+        }
+    ]
+    assert result['data']['finding']['pendingVulns'] == []
