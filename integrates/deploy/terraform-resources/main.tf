0f36ebfc916d12673f67ca1a9c5240db97808e6f
Daniel Salazar <dsalazar@fluidattacks.com>
2020-10-08 15:49:11 -0500

    integrates\refac(infra): #3503.1 move tfstate
    - Use right bucket for integrates resources
    - Rename tfstate
    
    
    Former-commit-id: d22aa52dd6d26886f476e777d3f6ab99daa1d537

--- integrates/deploy/terraform-resources/main.tf
+++ integrates/deploy/terraform-resources/main.tf
@@ -45,8 +45,8 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "servestf"
-    key     = "integrates.tfstate"
+    bucket  = "fluidattacks-terraform-states-prod"
+    key     = "integrates-resources.tfstate"
     region  = "us-east-1"
     encrypt = true
     dynamodb_table = "terraform_state_lock"
