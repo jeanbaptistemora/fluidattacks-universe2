654ba5aa70d6106bc2273dc88ccb6f6ba218469b
John Perez <jperez@fluidattacks.com>
2020-10-13 17:37:27 -0500

    airs\refac(front): #3396.3 bugsnag report for airs
    - Added bugnsg plugin for webpack and its configuration to report builds
    
    
    Former-commit-id: 8cf8d3e3a9695b264828dc4ab88832b88da8b7ca

--- airs/theme/2020/webpack.prod.config.ts
+++ airs/theme/2020/webpack.prod.config.ts
@@ -1,7 +1,34 @@
+import _ from "lodash";
 import TerserPlugin from "terser-webpack-plugin";
 import webpack from "webpack";
+import {
+  BugsnagBuildReporterPlugin,
+  BugsnagSourceMapUploaderPlugin,
+} from "webpack-bugsnag-plugins";
 
 import { commonConfig } from "./webpack.common.config";
+
+let appVersion: string; appVersion = "1.0.0";
+const commitSha: string = _.isString(process.env.CI_COMMIT_SHA)
+  ? process.env.CI_COMMIT_SHA
+  : "";
+let bugsnagApiKey: string; bugsnagApiKey = "6d0d7e66955855de59cfff659e6edf31";
+const branchName: string =
+  // tslint:disable-next-line: strict-comparisons
+  process.env.CI_COMMIT_REF_NAME === undefined
+    ? "master"
+    : process.env.CI_COMMIT_REF_NAME;
+let bucketName: string;
+
+branchName === "master"
+  ? bucketName = "fluidattacks.com"
+  : bucketName = "web.eph.fluidattacks.com";
+
+let sourceMapPath: string;
+
+branchName === "master"
+  ? sourceMapPath = `https://${bucketName}.s3.amazonaws.com/theme/static/js/`
+  : sourceMapPath = `https://${bucketName}.s3.amazonaws.com/${branchName}theme/static/js/`;
 
 const prodConfig: webpack.Configuration = {
   ...commonConfig,
@@ -25,6 +52,22 @@ const prodConfig: webpack.Configuration = {
       }),
     ],
   },
+  plugins: [
+    new BugsnagSourceMapUploaderPlugin({
+      apiKey: bugsnagApiKey,
+      appVersion,
+      publicPath: sourceMapPath,
+    }),
+    new BugsnagBuildReporterPlugin({
+      apiKey: bugsnagApiKey,
+      appVersion,
+      sourceControl: {
+        provider: "gitlab",
+        repository: "https://gitlab.com/fluidattacks/product.git",
+        revision: `${commitSha}/product/airs`,
+      },
+    }),
+  ],
 };
 
 export = prodConfig;
