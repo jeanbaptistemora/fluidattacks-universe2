188cf9325af8e6b698bdbdc6fdcbafc3d5010d5c
Daniel Murcia <dmurcia@fluidattacks.com>
2020-10-14 10:41:50 -0500

    observes\feat(back): #3508.2 api client and utils
    - added module api_client and utils
    
    
    Former-commit-id: a908aca5b33897000ab86b61cccd539e384a30b7

---
+++ observes/etl/dif_gitlab_etl/dif_gitlab_etl/api_client.py
@@ -0,0 +1,38 @@
+# Standard libraries
+from typing import (
+    Any,
+    List,
+    NamedTuple
+)
+# Third party libraries
+from aiohttp import (
+    ClientSession,
+)
+# Local libraries
+
+
+class GitlabResource(NamedTuple):
+    project: str
+    resource: str
+
+
+class GitlabResourcePage(NamedTuple):
+    g_resource: GitlabResource
+    page: int
+    per_page: int
+
+
+async def get_resource(
+    session: ClientSession, resource: GitlabResourcePage
+) -> List[Any]:
+    endpoint = (
+        'https://gitlab.com/api/v4/projects/'
+        f'{resource.g_resource.project}/{resource.g_resource.resource}'
+    )
+    params = {
+        'page': resource.page,
+        'per_page': resource.per_page
+    }
+    async with session.get(endpoint, params=params) as response:
+        response.raise_for_status()
+        return await response.json()
