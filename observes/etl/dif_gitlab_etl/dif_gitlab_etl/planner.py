0db894fb675305fca962f93359fa0832b23faba3
Daniel Murcia <dmurcia@fluidattacks.com>
2020-10-14 11:24:40 -0500

    observes\feat(back): #3508.3 planner
    - added pagination planner
    
    
    Former-commit-id: ad5c0e56509c11c43ac99d1ae707af3688727b9b

---
+++ observes/etl/dif_gitlab_etl/dif_gitlab_etl/planner.py
@@ -0,0 +1,100 @@
+# Standard libraries
+from typing import Any, Awaitable, Callable, Dict, List
+# Third party libraries
+import aiohttp
+# Local libraries
+from dif_gitlab_etl.api_client import (
+    GitlabResource,
+    GitlabResourcePage,
+)
+from dif_gitlab_etl import api_client
+from dif_gitlab_etl.utils import log
+
+# lgu = last greater uploaded
+
+
+async def search_page_with(
+    get_resource: Callable[[GitlabResourcePage], Awaitable],
+    target_id: int,
+    last_seen: GitlabResourcePage
+) -> int:
+    found: bool = False
+    items: List[Any] = []
+    counter: int = 0
+    while not found:
+        items = await get_resource(
+            GitlabResourcePage(
+                g_resource=last_seen.g_resource,
+                page=last_seen.page + counter,
+                per_page=last_seen.per_page,
+            )
+        )
+        if items:
+            if int(items[-1]['id']) > target_id:
+                counter = counter + 1
+                if counter % 100 == 0:
+                    log('info', f'searching at offset {counter}*100')
+            else:
+                if counter == 0:
+                    log('warning', 'page id could be incorrect')
+                found = True
+        else:
+            log('info', 'page not found, default: last page returned')
+            counter = counter - 1
+            found = True
+    return last_seen.page + counter
+
+
+def calculate_interval(last_page_id: int, max_pages: int) -> range:
+    init_page_id = 1
+    if not last_page_id - max_pages < 1:
+        init_page_id = last_page_id - max_pages
+    return range(init_page_id, last_page_id + 1)
+
+
+# integrated functions
+
+
+async def get_lgu_id(resource: GitlabResource):
+    # temporal function definition
+    if resource:
+        return 0
+    return 0
+
+
+async def get_lgu_last_seen_page_id(resource: GitlabResource):
+    # temporal function definition
+    if resource:
+        return 6200
+    return 6200
+
+
+async def get_work_interval(
+    resource: GitlabResource, max_pages: int = 10
+) -> range:
+    return calculate_interval(await search_lgu_page(resource), max_pages)
+
+
+async def search_lgu_page(resource: GitlabResource) -> int:
+    """
+    Returns de id of a page where item_id is present
+    """
+    target_id: int = await get_lgu_id(resource)
+    last_seen: Dict[str, int] = await get_lgu_last_seen_page_id(resource)
+    log('info', 'Search lgu page started')
+    async with aiohttp.ClientSession() as session:
+
+        def build_get_resource(session):
+            def getter(*args):
+                api_client.get_resource(session, *args)
+            return getter
+
+        return await search_page_with(
+            build_get_resource(session),
+            target_id,
+            GitlabResourcePage(
+                g_resource=resource,
+                page=last_seen['page'],
+                per_page=last_seen['per_page'],
+            )
+        )
