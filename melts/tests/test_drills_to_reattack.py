1bf403b3e228faddc6b59f2b085f41628691190d
Anderson Taguada <ataguada@fluidattacks.com>
2020-10-14 13:04:44 -0500

    melts\fix(back): #3075.11 fix to rattack test
    Former-commit-id: 6fead1eb415e4c7a167bb94a7d98e97224370d5b

--- melts/tests/test_drills_to_reattack.py
+++ melts/tests/test_drills_to_reattack.py
@@ -12,10 +12,9 @@ EXP_UNEXPECTED_URL: str = 'https://integrates.fluidattacks.com/dashboard#!/proje
 NO_EXP_EXPECTED_URL: str = 'https://integrates.fluidattacks.com/dashboard#!/project/continuoustest/710340580'
 NO_EXP_UNEXPECTED_URL: str = 'https://integrates.fluidattacks.com/dashboard#!/project/continuoustest/975673437'
 
-@pytest.mark.skip(reason="Pending to fix")
 def test_drills_to_reattack(relocate):
-    exp_message: str = to_reattack(True, 'continuoustest')[0]['findings'][0]['url']
-    no_exp_message: list = to_reattack(False, 'continuoustest')
+    exp_message: str = to_reattack(True, 'continuoustest')['projects_info'][0]['findings'][0]['url']
+    no_exp_message: list = to_reattack(False, 'continuoustest')['projects_info']
     assert EXP_EXPECTED_URL in exp_message
     assert EXP_UNEXPECTED_URL not in exp_message
     assert not no_exp_message
