9875d3c3a54cb9c5baec0ea055c08ca041e57440
Andres Aguirre <aaguirre@fluidattacks.com>
2020-10-20 10:32:46 -0500

    integrates\test(back): #3022.57 report customeradmin test
    - Added some functional tests for report with a customeradmin role
    
    
    Former-commit-id: 8a6dab020d80a255ad9088e474cce8210ddc112b

---
+++ integrates/test_async/functional_test/customeradmin/test_report.py
@@ -0,0 +1,89 @@
+import pytest
+
+from test_async.functional_test.customeradmin.utils import get_result
+
+pytestmark = pytest.mark.asyncio
+
+
+async def test_report():
+    group_name = 'unittesting'
+    query = f'''
+        query {{
+            report(
+                projectName: "{group_name}",
+                reportType: PDF,
+                lang: EN
+            ) {{
+                url
+            }}
+        }}
+    '''
+    data = {'query': query}
+    result = await get_result(data)
+    assert 'url' in result['data']['report']
+    assert result['data']['report']['url'] == 'The report will be sent to integratesuser@gmail.com shortly'
+
+    query = f'''
+        query {{
+            report(
+                projectName: "{group_name}",
+                reportType: XLS
+            ) {{
+                url
+            }}
+        }}
+    '''
+    data = {'query': query}
+    result = await get_result(data)
+    assert 'url' in result['data']['report']
+    assert result['data']['report']['url'] == 'The report will be sent to integratesuser@gmail.com shortly'
+
+    query = f'''
+        query {{
+            report(
+                projectName: "{group_name}",
+                reportType: DATA) {{
+                url
+            }}
+        }}
+    '''
+    data = {'query': query}
+    result = await get_result(data)
+    assert 'url' in result['data']['report']
+    assert result['data']['report']['url'] == 'The report will be sent to integratesuser@gmail.com shortly'
+
+    query = f'''
+        query {{
+            report(reportType: ALL_VULNS, projectName: "{group_name}") {{
+                url
+            }}
+        }}
+    '''
+    data = {'query': query}
+    result = await get_result(data)
+    assert result['data']['report'] is None
+    assert result['errors'][0]['message'] == 'Access denied'
+
+    query = f'''
+        query {{
+            report(reportType: ALL_USERS) {{
+                url
+            }}
+        }}
+    '''
+    data = {'query': query}
+    result = await get_result(data)
+    assert result['data']['report'] is None
+    assert result['errors'][0]['message'] == 'Access denied'
+
+    query = f'''
+        query {{
+            report(reportType: COMPLETE) {{
+                url
+            }}
+        }}
+    '''
+    data = {'query': query}
+    result = await get_result(data)
+    assert 'url' in result['data']['report']
+    assert 'https://files.fluidattacks.com/' in result['data']['report']['url']
