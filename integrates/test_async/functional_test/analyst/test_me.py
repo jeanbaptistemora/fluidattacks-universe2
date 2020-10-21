8c51fe4a71af3e03e49843d3042acda8407b0106
Andres Aguirre <aaguirre@fluidattacks.com>
2020-10-21 15:30:30 -0500

    integrates\test(back): #3022.63 me analyst test
    - Added some functional tests for me with an analyst role
    
    
    Former-commit-id: 28294da0e8b8cf1dcfca8fc6250365b9c2de8f72

---
+++ integrates/test_async/functional_test/analyst/test_me.py
@@ -0,0 +1,173 @@
+import pytest
+from datetime import datetime, timedelta
+
+from test_async.functional_test.analyst.utils import get_result
+
+pytestmark = pytest.mark.asyncio
+
+
+async def test_me():
+    org_id = 'ORG#38eb8f25-7945-4173-ab6e-0af4ad8b7ef3'
+    org_name = 'okada'
+    group_name = 'unittesting'
+    query = '''
+        mutation {
+            signIn(
+                authToken: "badtoken",
+                provider: GOOGLE
+            ) {
+                sessionJwt
+                success
+            }
+        }
+    '''
+    data = {'query': query}
+    result = await get_result(data)
+    assert 'errors' not in result
+    assert 'success' in result['data']['signIn']
+    assert not result['data']['signIn']['success']
+
+    expiration_time = datetime.utcnow() + timedelta(weeks=8)
+    expiration_time = int(expiration_time.timestamp())
+    query = f'''
+        mutation {{
+            updateAccessToken(expirationTime: {expiration_time}) {{
+                sessionJwt
+                success
+            }}
+        }}
+    '''
+    data = {'query': query}
+    result = await get_result(data)
+    assert 'errors' not in result
+    assert result['data']['updateAccessToken']['success']
+    session_jwt = result['data']['updateAccessToken']['sessionJwt']
+
+    query = '''
+        mutation {
+            addPushToken(token: "ExponentPushToken[something123]") {
+                success
+            }
+        }
+    '''
+    data = {'query': query}
+    result = await get_result(data, session_jwt=session_jwt)
+    assert 'error' not in result
+    assert result['data']['addPushToken']['success']
+
+    frecuency = 'WEEKLY'
+    entity = 'GROUP'
+    query = f'''
+        mutation {{
+            subscribeToEntityReport(
+                frequency: {frecuency},
+                reportEntity: {entity},
+                reportSubject: "{org_id}"
+
+
+            ) {{
+                success
+            }}
+        }}
+    '''
+    data = {'query': query}
+    result = await get_result(data, session_jwt=session_jwt)
+    assert 'errors' not in result
+    assert result['data']['subscribeToEntityReport']['success']
+
+    query = '''
+        mutation {
+            acceptLegal(remember: false) {
+                success
+            }
+        }
+    '''
+    data = {'query': query}
+    result = await get_result(data, session_jwt=session_jwt)
+    assert 'errors' not in result
+    assert result['data']['acceptLegal']['success']
+
+    query = f'''{{
+        me(callerOrigin: "API") {{
+            accessToken
+            callerOrigin
+            organizations {{
+                name
+            }}
+            permissions(entity: USER)
+            projects {{
+                name
+                description
+            }}
+            remember
+            role(entity: USER)
+            sessionExpiration
+            subscriptionsToEntityReport{{
+                entity
+                frequency
+                subject
+
+            }}
+            tags(organizationId: "{org_id}") {{
+                name
+                projects {{
+                    name
+                }}
+            }}
+            __typename
+        }}
+    }}'''
+    data = {'query': query}
+    result = await get_result(data, session_jwt=session_jwt)
+    assert 'errors' not in result
+    assert '{"hasAccessToken": true' in result['data']['me']['accessToken']
+    assert result['data']['me']['callerOrigin'] == 'API'
+    assert result['data']['me']['organizations'] == [{'name': org_name}]
+    assert len(result['data']['me']['permissions']) == 3
+    assert len(result['data']['me']['projects']) == 2
+    assert result['data']['me']['remember'] == False
+    assert result['data']['me']['role'] == 'analyst'
+    assert result['data']['me']['sessionExpiration'] == str(expiration_time)
+    assert result['data']['me']['subscriptionsToEntityReport'] == [
+        {
+            'entity': entity,
+            'frequency': frecuency,
+            'subject': org_id
+        }
+    ]
+    assert result['data']['me']['tags'] == [
+        {
+            'name': 'another-tag',
+            'projects': [
+                {
+                    'name': 'continuoustesting'
+                },
+                {
+                    'name': 'oneshottest'
+                }
+            ]
+        },
+        {
+            'name': 'test-projects',
+            'projects': [
+                {
+                    'name': 'unittesting'
+                }, 
+                {
+                    'name': 'oneshottest'
+                }
+            ]
+        },
+        {
+            'name': 'test-updates',
+            'projects': [
+                {
+                    'name': 'unittesting'
+                }, 
+                {
+                    'name': 'oneshottest'
+                }
+            ]
+        }
+    ]
+    assert result['data']['me']['__typename'] == 'Me'
