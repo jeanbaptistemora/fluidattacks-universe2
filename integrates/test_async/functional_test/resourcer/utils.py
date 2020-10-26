6144ca9abd600134767d0ca8a4dfd368b1bef177
Andres Aguirre <aaguirre@fluidattacks.com>
2020-10-26 16:42:01 -0500

    integrates\test(back): #3022.74 resourcer utils
    - Added utils for resourcer functional tests
    
    
    Former-commit-id: af80aee1ae00033568e3740a04b6d0a637b5e00f

---
+++ integrates/test_async/functional_test/resourcer/utils.py
@@ -0,0 +1,6 @@
+from test_async.functional_test.utils import get_graphql_result
+
+async def get_result(data, stakeholder='integratesresourcer@gmail.com', session_jwt=None):
+    """Get result for resourcer role."""
+    result = await get_graphql_result(data, stakeholder, session_jwt)
+    return result
