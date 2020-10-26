f282c0138e767b53bc1560efddc14120cc24c9c9
Andres Aguirre <aaguirre@fluidattacks.com>
2020-10-26 11:35:23 -0500

    integrates\test(back): #3022.73 user executive test
    - Added some functional tests for user with an executive role
    
    
    Former-commit-id: ca45b3aeaec08466407c7c68141a9c45d0c6c391

---
+++ integrates/test_async/functional_test/executive/test_user.py
@@ -0,0 +1,94 @@
+import pytest
+
+from test_async.functional_test.executive.utils import get_result
+
+pytestmark = pytest.mark.asyncio
+
+
+async def test_user():
+    group_name = 'unittesting'
+    stakeholder = 'stakeholder@fluidattacks.com'
+    phone_number = '3453453453'
+    responsibility = 'test'
+    role = 'EXECUTIVE'
+    query = f'''
+        mutation {{
+            grantStakeholderAccess (
+                email: "{stakeholder}",
+                phoneNumber: "{phone_number}"
+                projectName: "{group_name}",
+                responsibility: "{responsibility}",
+                role: {role}
+            ) {{
+            success
+                grantedStakeholder {{
+                    email
+                }}
+            }}
+        }}
+    '''
+    data = {'query': query}
+    result = await get_result(data)
+    assert 'errors' in result
+    assert  result['errors'][0]['message'] == 'Access denied'
+
+    query = f'''
+        query {{
+            stakeholder(entity: PROJECT,
+                    projectName: "{group_name}",
+                    userEmail: "{stakeholder}") {{
+                email
+                role
+                responsibility
+                phoneNumber
+                firstLogin
+                lastLogin
+                projects {{
+                    name
+                }}
+                __typename
+            }}
+        }}
+    '''
+    data = {'query': query}
+    result = await get_result(data)
+    assert 'errors' in result
+    assert  result['errors'][0]['message'] == 'Access denied'
+
+    phone_number = '17364735'
+    responsibility = 'edited'
+    role = 'GROUP_MANAGER'
+    query = f'''
+        mutation {{
+            editStakeholder (
+                email: "{stakeholder}",
+                phoneNumber: "{phone_number}",
+                projectName: "{group_name}"
+                responsibility: "{responsibility}",
+                role: {role}
+            ) {{
+                success
+            }}
+        }}
+    '''
+    data = {'query': query}
+    result = await get_result(data)
+    assert 'errors' in result
+    assert  result['errors'][0]['message'] == 'Access denied'
+
+    query = f'''
+        mutation {{
+            removeStakeholderAccess (
+                projectName: "{group_name}",
+                userEmail: "{stakeholder}"
+            )
+            {{
+                removedEmail
+                success
+            }}
+        }}
+    '''
+    data = {'query': query}
+    result = await get_result(data)
+    assert 'errors' in result
+    assert  result['errors'][0]['message'] == 'Access denied'
