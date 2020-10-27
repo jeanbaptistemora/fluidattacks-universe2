59c07afdf3efeca4aacbadb7459532eeca708c90
Daniel Murcia <dmurcia@fluidattacks.com>
2020-10-27 08:53:05 -0500

    observes\feat(back): #3508.25 cli
    - added new dependencies
    - added cli module
    
    
    Former-commit-id: c955dedcdfb0605198bdbf05b224c5b9e956fcb8

---
+++ observes/etl/dif_gitlab_etl/dif_gitlab_etl/cli.py
@@ -0,0 +1,40 @@
+# Standard libraries
+import json
+from os import (
+    environ,
+)
+import sys
+from typing import (
+    List,
+)
+
+# Third party libraries
+import click
+
+# Local libraries
+from dif_gitlab_etl import executer
+from streamer_gitlab.log import log
+
+
+@click.command()
+@click.argument('projects', nargs=-1)
+@click.argument('auth_file', type=click.File('r'))
+def start_etl(projects: List[str], auth_file):
+    try:
+        environ['GITLAB_API_TOKEN']
+    except KeyError:
+        log('critical', 'Export GITLAB_API_TOKEN as environment variable')
+        sys.exit(1)
+    else:
+        auth = json.load(auth_file)
+        for project in projects:
+            log('info', f'Starting Gitlab ETL for {project}')
+            executer.start_etl(project, auth)
+
+
+@click.group()
+def main():
+    pass
+
+
+main.add_command(start_etl)
