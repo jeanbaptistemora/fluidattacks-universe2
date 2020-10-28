c15ffec905a6d4245c9b55e17eaeefbb00051226
Andres Aguirre <aaguirre@fluidattacks.com>
2020-10-28 14:04:56 -0500

    integrates\test(back): #3022.83 reviewer utils
    - Added utils for reviewer functional tests
    
    
    Former-commit-id: 5d7271aaac08c50d0d0487dcf29205c514f549c3

---
+++ integrates/test_async/functional_test/reviewer/utils.py
@@ -0,0 +1,6 @@
+from test_async.functional_test.utils import get_graphql_result
+
+async def get_result(data, stakeholder='integratesreviewer@fluidattacks.com', session_jwt=None):
+    """Get result for reviewer role."""
+    result = await get_graphql_result(data, stakeholder, session_jwt)
+    return result
