3233e1d75bba0f3ed37f9fe82c019fc54f403c8b
Daniel Murcia <dmurcia@fluidattacks.com>
2020-11-05 14:48:56 -0500

    observes\feat(back): #3465.8 etl module
    - added etl
    - new dependencies
    
    
    Former-commit-id: 9e95b762f42b669c38e3ead7af9a82593a4d45b7

---
+++ observes/etl/dif_dynamo_etl/dif_dynamo_etl/etl.py
@@ -0,0 +1,54 @@
+# Standard libraries
+from multiprocessing.context import Process
+from typing import Dict, Optional
+# Third party libraries
+import boto3
+import botocore
+# Local libraries
+from dif_dynamo_etl.wrappers import loader
+from dif_dynamo_etl.wrappers import transformer
+from streamer_dynamodb import extractor
+from streamer_dynamodb.extractor import TableSegment
+
+
+def boto_resource(auth):
+    resource_options: Dict[str, Optional[str]] = {
+        'service_name': 'dynamodb',
+        'aws_access_key_id': auth['aws_access_key_id'],
+        'aws_secret_access_key': auth['aws_secret_access_key'],
+        'region_name': auth['region_name'],
+        'config': botocore.config.Config(max_pool_connections=50),
+    }
+    return boto3.resource(**resource_options)
+
+
+def start_etl(db_client, table_segment: TableSegment, auth):
+    d_pages = extractor.extract_segment(db_client, table_segment)
+    t_pages = transformer.transform_pages(d_pages)
+    loader.upload_pages(t_pages, 'dynamodb_forces', auth)
+
+
+def parallel_start_etl(auth, table: str, workers: int = 4):
+    db_client = boto_resource(auth)
+    processes = []
+    for segment in range(workers):
+        table_segment = TableSegment(
+            table,
+            segment,
+            workers
+        )
+        etl_process = Process(
+            target=start_etl(db_client, table_segment, auth)
+        )
+        processes.append(etl_process)
+        etl_process.start()
+    for process in processes:
+        process.join()
+
+
+def start_new_data_etl(auth):
+    parallel_start_etl(auth, 'FI_forces')
+
+
+def start_old_data_etl(auth):
+    parallel_start_etl(auth, 'bb_executions')
