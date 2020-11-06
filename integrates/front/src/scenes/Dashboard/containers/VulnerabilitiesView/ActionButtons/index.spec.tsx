3d932e385e79c2f9b431ea18ec2c2afdca640415
Sebastian Lizcano <slizcano@fluidattacks.com>
2020-11-06 14:20:40 -0500

    integrates\test(front): #3629.4 add basic test
    - add basic test to vulnerabilitiesviews/actionbuttons
    
    
    Former-commit-id: ce1bb705952fdb4d0af6c4bc3f0e26f422bbba83

---
+++ integrates/front/src/scenes/Dashboard/containers/VulnerabilitiesView/ActionButtons/index.spec.tsx
@@ -0,0 +1,113 @@
+import { PureAbility } from "@casl/ability";
+import { mount, ReactWrapper } from "enzyme";
+import React from "react";
+// tslint:disable-next-line: no-submodule-imports
+import { act } from "react-dom/test-utils";
+import { ActionButtons, IActionButtonsProps } from "scenes/Dashboard/containers/VulnerabilitiesView/ActionButtons";
+import { authzPermissionsContext } from "utils/authz/config";
+
+describe("ActionButtons", () => {
+
+  const baseMockedProps: IActionButtonsProps = {
+    areVulnsSelected: false,
+    isEditing: false,
+    isReattackRequestedInAllVuln: false,
+    isRequestingReattack: false,
+    isVerified: false,
+    isVerifying: false,
+    onEdit: jest.fn(),
+    onRequestReattack: jest.fn(),
+    onVerify: jest.fn(),
+    openModal: jest.fn(),
+    state: "open",
+    subscription: "",
+  };
+
+  it("should return a function", () => {
+    expect(typeof (ActionButtons))
+      .toEqual("function");
+  });
+
+  it("should render a component", async () => {
+    const wrapper: ReactWrapper = mount(
+      <ActionButtons {...baseMockedProps} />,
+    );
+    expect(wrapper)
+      .toHaveLength(1);
+    const buttons: ReactWrapper = wrapper.find("Button");
+    expect(buttons)
+      .toHaveLength(1);
+    expect(buttons
+      .filterWhere((button: ReactWrapper): boolean =>
+        button
+          .text()
+          .includes("Edit")))
+      .toHaveLength(1);
+  });
+
+  it("should render request verification", async () => {
+    const requestMockProps: IActionButtonsProps = {
+      ...baseMockedProps,
+      isEditing: false,
+      isReattackRequestedInAllVuln: false,
+      isVerified: false,
+      state: "open",
+      subscription: "continuous",
+    };
+    const mockedPermissions: PureAbility<string> = new PureAbility([
+      { action: "backend_api_resolvers_vulnerability__do_request_verification_vuln" },
+    ]);
+    const wrapper: ReactWrapper = mount(
+      <ActionButtons {...requestMockProps} />,
+      {
+        wrappingComponent: authzPermissionsContext.Provider,
+        wrappingComponentProps: { value: mockedPermissions },
+      },
+    );
+
+    expect(wrapper)
+      .toHaveLength(1);
+    let buttons: ReactWrapper = wrapper.find("Button");
+    expect(buttons)
+      .toHaveLength(2);
+
+    let requestButton: ReactWrapper = buttons
+      .filterWhere((button: ReactWrapper): boolean =>
+        button
+          .text()
+          .includes("Reattack"));
+
+    expect(requestButton)
+      .toHaveLength(1);
+    expect(buttons
+      .filterWhere((button: ReactWrapper): boolean =>
+        button
+          .text()
+          .includes("Edit")))
+      .toHaveLength(1);
+
+    requestButton.simulate("click");
+    act(() => {
+      wrapper.setProps({ isRequestingReattack: true });
+      wrapper.update();
+    });
+    const { onRequestReattack } = requestMockProps;
+    expect(onRequestReattack)
+      .toHaveBeenCalled();
+
+    buttons = wrapper.find("Button");
+    requestButton = buttons
+    .filterWhere((button: ReactWrapper): boolean =>
+    button
+    .text()
+    .includes("Cancel"));
+    expect(requestButton)
+      .toHaveLength(1);
+    expect(buttons
+      .filterWhere((button: ReactWrapper): boolean =>
+        button
+          .text()
+          .includes("Edit")))
+      .toHaveLength(0);
+  });
+});
