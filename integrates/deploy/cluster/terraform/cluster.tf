ff5a11b3200287f1308d2e2137fbc02de275f954
Daniel Salazar <dsalazar@fluidattacks.com>
2020-11-09 15:21:11 -0500

    integrates\feat(cross): #3621.11 separate databases
    - Create separate containers for dynamo and redis
    - Only serve 2 workers per integrates ephemeral
    - Redistribute resources
    - Reduce prod ram to 4gb per replica
    - Increase cluster large nodes size by one
    
    
    Former-commit-id: cd46c0686744912228675adbc1403cb8c6b1027c

--- integrates/deploy/cluster/terraform/cluster.tf
+++ integrates/deploy/cluster/terraform/cluster.tf
@@ -26,9 +26,9 @@ module "eks" {
     {
       name                 = "large"
       instance_type        = "m5a.large"
-      asg_min_size         = 9
-      asg_desired_capacity = 9
-      asg_max_size         = 9
+      asg_min_size         = 10
+      asg_desired_capacity = 10
+      asg_max_size         = 10
       root_volume_type     = "gp2"
       root_volume_size     = "50"
       kubelet_extra_args   = "--node-labels=worker_group=large"
