1f40bbddd9c80b7fd80a26c979158db89f465f50
Diego Restrepo <drestrepo@fluidattacks.com>
2020-11-11 16:04:07 -0500

    skims\feat(back): #3544.6 add cors model
    Former-commit-id: c37da30815ddcb3967cb79c6d00c888b5196d494

--- skims/skims/utils/model.py
+++ skims/skims/utils/model.py
@@ -333,6 +333,30 @@ class FindingEnum(Enum):
         title='utils.model.finding.enum.F055_AWS.title',
         type=FindingTypeEnum.SECURITY,
     )
+    F055_CORS: FindingMetadata = FindingMetadata(
+        auto_approve=False,
+        cwe='942',
+        description='utils.model.finding.enum.F055_CORS.description',
+        impact='utils.model.finding.enum.F055_CORS.impact',
+        recommendation='utils.model.finding.enum.F055_CORS.recommendation',
+        requirements='utils.model.finding.enum.F055_CORS.requirements',
+        severity={
+            'attackComplexity': 0.44,
+            'attackVector': 0.85,
+            'availabilityImpact': 0.0,
+            'confidentialityImpact': 0.00,
+            'exploitability': 0.94,
+            'integrityImpact': 0.22,
+            'privilegesRequired': 0.85,
+            'remediationLevel': 1.0,
+            'reportConfidence': 1.0,
+            'severityScope': 0.0,
+            'userInteraction': 0.85,
+        },
+        threat='utils.model.finding.enum.F055_CORS.threat',
+        title='utils.model.finding.enum.F055_CORS.title',
+        type=FindingTypeEnum.SECURITY,
+    )
     F060: FindingMetadata = FindingMetadata(
         auto_approve=True,
         cwe='396',
