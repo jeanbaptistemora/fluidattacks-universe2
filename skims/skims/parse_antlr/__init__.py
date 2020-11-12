666c242995fd3baf11e621d2596ed75d556fae27
Kevin Amado <kamado@fluidattacks.com>
2020-11-12 17:00:57 -0500

    skims\feat(back): #3675.5 model formatting functions
    - This allow us to translate the raw model from ANTLR into
      something more easy to iterate and search from Python
    - Pending to refactor old code to use this functions
    
    
    Former-commit-id: 1aef70ff0cc62893035a9aad1357b195ccfdfcaa

--- skims/skims/parse_antlr/__init__.py
+++ skims/skims/parse_antlr/__init__.py
@@ -1,4 +1,7 @@
 # Standard library
+from collections import (
+    defaultdict,
+)
 import contextlib
 import json
 from typing import (
@@ -121,3 +124,78 @@ def parse_rule(
             values[token_name] = token_value
 
     return values
+
+
+def parse_rule2(model: List[Dict[str, Any]]) -> Dict[str, Any]:
+    result: Dict[str, Any] = {}
+    token_index = 0
+
+    for model_element in model:
+        token_name, token_value = next(iter(model_element.items()))
+
+        if token_name == 'Token':
+            token_name = f'Token[{token_index}]'
+            token_index += 1
+
+        if token_name in result:
+            if isinstance(result[token_name], list):
+                result[token_name].append(token_value)
+            else:
+                result[token_name] = [result[token_name], token_value]
+        else:
+            result[token_name] = token_value
+
+    return result
+
+
+def structure_model(model: Dict[str, Any]) -> Dict[str, Any]:
+    result: Dict[str, Any]
+
+    if isinstance(model, dict):
+        if len(model) == 1:
+            # Single key node
+            key, value = next(iter(model.items()))
+
+            if isinstance(value, list):
+                if len(value) == 1:
+                    # Single value list
+                    result = {key: format_model(value[0])}
+                else:
+                    # Multiple values list
+                    result = {key: list(map(format_model, value))}
+            else:
+                # Can happen?
+                raise NotImplementedError()
+
+        elif len(model) == 4:
+            # Token node
+            result = {'Token': model}
+        else:
+            result = dict(zip(model.keys(), map(format_model, model.values())))
+    else:
+        # Can happen?
+        raise NotImplementedError()
+
+    return result
+
+
+def structure_keys(model: Dict[str, Any]) -> defaultdict:
+    if isinstance(model, dict):
+        result: defaultdict = defaultdict(None)
+        for key, val in model.items():
+            if isinstance(val, dict):
+                result[key] = val
+            elif isinstance(val, list):
+                result[key] = parse_rule2(val)
+            else:
+                # Can happen?
+                raise NotImplementedError()
+    else:
+        # Can happen?
+        raise NotImplementedError()
+
+    return result
+
+
+def format_model(model: Dict[str, Any]) -> defaultdict:
+    return structure_keys(structure_model(model))
