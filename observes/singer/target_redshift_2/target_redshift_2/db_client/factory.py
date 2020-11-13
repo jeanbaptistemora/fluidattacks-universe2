4b8c4e582f55745262b532249460eb2d7822c491
Daniel Murcia <dmurcia@fluidattacks.com>
2020-11-13 16:01:01 -0500

    observes\feat(back): #3631.16 table factory
    - added table factory
    
    
    Former-commit-id: db2aafb567db094ede00f693b9e9ff07d3d2e13b

--- observes/singer/target_redshift_2/target_redshift_2/db_client/factory.py
+++ observes/singer/target_redshift_2/target_redshift_2/db_client/factory.py
@@ -1,9 +1,11 @@
-# Standard libraries
 from typing import (
     Any,
     Callable,
+    cast,
+    List,
     Optional,
     NamedTuple,
+    Tuple,
 )
 # Third party libraries
 import psycopg2 as postgres
@@ -18,9 +20,14 @@ from target_redshift_2.db_client.objects import (
     CursorExeAction,
     CursorFetchAction, DynamicSQLargs,
     FetchAction,
+    IsolatedColumn,
     PGCONN,
     PGCURR,
     SQLidPurifier,
+    Table,
+    TableCreationFail,
+    TableID,
+    TablePrototype,
 )
 from target_redshift_2.db_client import prototypes
 from target_redshift_2.utils import log
@@ -119,4 +126,124 @@ class ClientFactory(NamedTuple):
         )
         return Client(
             connection=dbcon, cursor=dbcur, prototype=prototype
+        )
+
+
+class TableFactory(NamedTuple):
+    """Generator of `Table` objects"""
+    db_client: Client
+    c_action_factory: 'CursorActionFactory'
+
+    def exist(self: 'TableFactory', table_id: TableID) -> bool:
+        """Check existence of a Table on the DB"""
+        statement = """
+            SELECT EXISTS (
+                SELECT * FROM information_schema.tables
+                WHERE table_schema = %(table_schema)s
+                AND table_name = %(table_name)s
+            );
+        """
+        action = self.db_client.execute(
+            statement,
+            DynamicSQLargs(
+                values={
+                    'table_schema': table_id.schema.schema_name,
+                    'table_name': table_id.table_name
+                }
+            )
+        )
+        action.act()
+        result: Tuple[bool] = self.db_client.cursor.fetchone()
+        return result[0]
+
+    def retrieve(self: 'TableFactory', table_id: TableID) -> Optional[Table]:
+        """Retrieve Table from DB"""
+        statement = """
+            SELECT ordinal_position AS position,
+                column_name,
+                data_type,
+                CASE WHEN character_maximum_length IS not null
+                        THEN character_maximum_length
+                        ELSE numeric_precision end AS max_length,
+                is_nullable,
+                column_default AS default_value
+            FROM information_schema.columns
+            WHERE table_name = %(table_name)s
+              AND table_schema = %(table_schema)s
+            ORDER BY ordinal_position;
+        """
+        action: CursorExeAction = self.db_client.execute(
+            statement,
+            DynamicSQLargs(
+                values={
+                    'table_schema': table_id.schema.schema_name,
+                    'table_name': table_id.table_name
+                }
+            )
+        )
+        fetch_action: CursorFetchAction = self.db_client.fetchall()
+
+        action.act()
+        results: List[Tuple[Any, ...]] = cast(
+            List[Tuple[Any, ...]], fetch_action.act()
+        )
+        columns = set()
+        for column in results:
+            columns.add(IsolatedColumn(column[1], column[2], column[5]))
+
+        prototype: TablePrototype = prototypes.table_prototype_1(
+            make_exe_action=self.c_action_factory.exe_action
+        )
+        return Table(
+            table_id,
+            primary_keys=frozenset(),
+            columns=frozenset(columns),
+            prototype=prototype
+        )
+
+    def create(
+        self: 'TableFactory',
+        table_draft: Table,
+        if_not_exist: bool = False
+    ) -> Table:
+        """Creates a Table in the DB and returns it"""
+        table_path: str = table_draft.table_path()
+        pkeys_fields: str = ''
+        if table_draft.primary_keys:
+            p_fields: str = ",".join(
+                [f"{{pkey_{n}}}" for n in range(len(table_draft.primary_keys))]
+            )
+            pkeys_fields = f',PRIMARY KEY({p_fields})'
+        not_exists: str = '' if not if_not_exist else 'IF NOT EXISTS '
+        fields: str = ",".join(
+            [
+                f"{{name_{n}}} {{field_type_{n}}}"
+                for n in range(len(table_draft.columns))
+            ]
+        )
+        fields_def: str = f'{fields}{pkeys_fields}'
+        statement: str = (
+            f"CREATE TABLE {not_exists}{{table_path}} ({fields_def})"
+        )
+        identifiers = {
+            'table_path': table_path
+        }
+        for index, value in enumerate(table_draft.primary_keys):
+            identifiers[f'pkey_{index}'] = value
+        for index, column in enumerate(table_draft.columns):
+            identifiers[f'name_{index}'] = column.name
+            identifiers[f'field_type_{index}'] = column.field_type
+
+        self.db_client.execute(
+            statement,
+            DynamicSQLargs(
+                identifiers=identifiers
+            )
+        )
+        result: Optional[Table] = self.retrieve(table_draft.id)
+        if result:
+            return result
+        raise TableCreationFail(
+            'Could not create and verify the existence of table: '
+            f'{table_draft.id}'
         )
