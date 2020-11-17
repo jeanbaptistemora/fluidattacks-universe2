6ab0c212091720426ab08e9c8b941cf7d420f0d9
Daniel Murcia <dmurcia@fluidattacks.com>
2020-11-17 12:20:19 -0500

    observes\feat(back): #3631.17 from rfield
    - added column generator from RedshiftField
    - added unit tests
    
    
    Former-commit-id: 15c80cfa40fc4d1ec7448c938b4218a1a42a770f

---
+++ observes/singer/target_redshift_2/target_redshift_2/factory_pack/columns.py
@@ -0,0 +1,14 @@
+# Standard libraries
+# Third party libraries
+# Local libraries
+from target_redshift_2.db_client.objects import IsolatedColumn
+from target_redshift_2.objects import RedshiftField
+
+
+def from_rfield(r_field: RedshiftField) -> IsolatedColumn:
+    """Transform `RedshiftField` into a `IsolatedColumn`"""
+    return IsolatedColumn(
+        name=r_field.name,
+        field_type=r_field.dbtype.value,
+        default_val=None
+    )
---
+++ observes/singer/target_redshift_2/tests/factory/test_columns.py
@@ -0,0 +1,21 @@
+# Standard libraries
+# Third party libraries
+# Local libraries
+from target_redshift_2.db_client.objects import (
+    DbTypes,
+    IsolatedColumn,
+)
+from target_redshift_2.factory_pack import columns
+from target_redshift_2.objects import RedshiftField
+
+
+def test_from_rfield():
+    # Arrange
+    field1 = RedshiftField('field1', DbTypes.BOOLEAN)
+    # Act
+    result = columns.from_rfield(field1)
+    # Assert
+    expected = IsolatedColumn(
+        name=field1.name, field_type=DbTypes.BOOLEAN.value, default_val=None
+    )
+    assert result == expected
