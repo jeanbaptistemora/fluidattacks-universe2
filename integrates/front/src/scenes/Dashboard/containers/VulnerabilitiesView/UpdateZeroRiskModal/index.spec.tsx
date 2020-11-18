2b504b67a6cd01f5fd61840eeacb519560a4da93
Andres Aguirre <aaguirre@fluidattacks.com>
2020-11-18 18:21:20 -0500

    integrates\test(front): #3496.25 handle reject zero risk
    - Added unittest to handle reject zero risk in update zero risk modal
    
    
    Former-commit-id: 3bc4497bb0328602270ff756e8d8507ba41b45bd

--- integrates/front/src/scenes/Dashboard/containers/VulnerabilitiesView/UpdateZeroRiskModal/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/containers/VulnerabilitiesView/UpdateZeroRiskModal/index.spec.tsx
@@ -11,6 +11,7 @@ import { GET_VULNERABILITIES } from "scenes/Dashboard/components/Vulnerabilities
 import { UpdateZeroRiskModal } from "scenes/Dashboard/containers/VulnerabilitiesView/UpdateZeroRiskModal";
 import {
   CONFIRM_ZERO_RISK_VULN,
+  REJECT_ZERO_RISK_VULN,
   REQUEST_ZERO_RISK_VULN,
 } from "scenes/Dashboard/containers/VulnerabilitiesView/UpdateZeroRiskModal/queries";
 import store from "store";
@@ -297,6 +298,134 @@ describe("update zero risk component", () => {
     expect(handleOnClose)
       .toHaveBeenCalled();
     expect(handleConfirmState)
+      .not
+      .toHaveBeenCalled();
+    expect(handleRefetchData)
+      .not
+      .toHaveBeenCalled();
+    expect(msgError)
+      .toHaveBeenCalled();
+  });
+
+  it("should handle reject zero risk", async () => {
+    const handleClearSelected: jest.Mock = jest.fn();
+    const handleOnClose: jest.Mock = jest.fn();
+    const handleConfirmState: jest.Mock = jest.fn();
+    const handleRejectState: jest.Mock = jest.fn();
+    const handleRequestState: jest.Mock = jest.fn();
+    const handleRefetchData: jest.Mock = jest.fn();
+    const mocksMutation: MockedResponse[] = [
+      {
+        request: {
+          query: REJECT_ZERO_RISK_VULN,
+          variables: {
+            findingId: "422286126",
+            justification: "This is a commenting test of reject zero risk in vulns",
+            vulnerabilities: ["ab25380d-dfe1-4cde-aefd-acca6990d6aa"],
+          },
+        },
+        result: { data: { rejectZeroRiskVuln : { success: true } } },
+      },
+      mocksVulns,
+    ];
+    const wrapperRequest: ReactWrapper = mount(
+      <Provider store={store}>
+        <MockedProvider mocks={mocksMutation} addTypename={false}>
+          <UpdateZeroRiskModal
+            findingId={"422286126"}
+            isConfirmingZeroRisk={false}
+            isRejectingZeroRisk={true}
+            isRequestingZeroRisk={false}
+            vulns={[{id: "ab25380d-dfe1-4cde-aefd-acca6990d6aa"}]}
+            clearSelected={handleClearSelected}
+            refetchData={handleRefetchData}
+            handleCloseModal={handleOnClose}
+            setConfirmState={handleConfirmState}
+            setRejectState={handleRejectState}
+            setRequestState={handleRequestState}
+          />
+        </MockedProvider>
+      </Provider>,
+    );
+    const justification: ReactWrapper = wrapperRequest.find("textarea");
+    justification.simulate("change", { target:
+      { value: "This is a commenting test of reject zero risk in vulns" } });
+    const form: ReactWrapper = wrapperRequest.find("form");
+    form.at(0)
+      .simulate("submit");
+    await act(async () => { await wait(0); wrapperRequest.update(); });
+
+    expect(wrapperRequest)
+      .toHaveLength(1);
+    expect(handleClearSelected)
+      .toHaveBeenCalled();
+    expect(handleOnClose)
+      .toHaveBeenCalled();
+    expect(handleRejectState)
+      .toHaveBeenCalled();
+    expect(handleRefetchData)
+      .toHaveBeenCalled();
+    expect(msgSuccess)
+      .toHaveBeenCalled();
+  });
+
+  it("should handle reject zero risk error", async () => {
+    const handleClearSelected: jest.Mock = jest.fn();
+    const handleOnClose: jest.Mock = jest.fn();
+    const handleConfirmState: jest.Mock = jest.fn();
+    const handleRejectState: jest.Mock = jest.fn();
+    const handleRequestState: jest.Mock = jest.fn();
+    const handleRefetchData: jest.Mock = jest.fn();
+    const mocksMutation: MockedResponse[] = [
+    {
+      request: {
+        query: REJECT_ZERO_RISK_VULN,
+        variables: {
+            findingId: "422286126",
+            justification: "This is a commenting test of reject zero risk in vulns",
+            vulnerabilities: ["ab25380d-dfe1-4cde-aefd-acca6990d6aa"],
+        },
+      },
+      result: {
+        errors: [
+          new GraphQLError("Exception - Zero risk vulnerability is not requested"),
+        ],
+      },
+    }];
+    const wrapper: ReactWrapper = mount(
+      <Provider store={store}>
+        <MockedProvider mocks={mocksMutation} addTypename={false}>
+          <UpdateZeroRiskModal
+            findingId={"422286126"}
+            isConfirmingZeroRisk={false}
+            isRejectingZeroRisk={true}
+            isRequestingZeroRisk={false}
+            vulns={[{id: "ab25380d-dfe1-4cde-aefd-acca6990d6aa"}]}
+            clearSelected={handleClearSelected}
+            refetchData={handleRefetchData}
+            handleCloseModal={handleOnClose}
+            setConfirmState={handleConfirmState}
+            setRejectState={handleRejectState}
+            setRequestState={handleRequestState}
+          />
+        </MockedProvider>
+      </Provider>,
+    );
+    const justification: ReactWrapper = wrapper.find("textarea");
+    justification.simulate("change", { target:
+      { value: "This is a commenting test of reject zero risk in vulns" } });
+    const form: ReactWrapper = wrapper.find("form");
+    form.at(0)
+      .simulate("submit");
+    await act(async () => { await wait(0); wrapper.update(); });
+    expect(wrapper)
+      .toHaveLength(1);
+    expect(handleClearSelected)
+      .not
+      .toHaveBeenCalled();
+    expect(handleOnClose)
+      .toHaveBeenCalled();
+    expect(handleRejectState)
       .not
       .toHaveBeenCalled();
     expect(handleRefetchData)
