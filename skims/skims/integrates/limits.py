cf25a75ea8d0fa14936857632a8582756322d033
Kevin Amado <kamado@fluidattacks.com>
2020-11-18 14:49:20 -0500

    skims\refac(conf): #3704.1 lower rate limits
    Closes #3704
    
    
    Former-commit-id: 1d81824ac0c847aadeb2b2387456add0622a9d90

--- skims/skims/integrates/limits.py
+++ skims/skims/integrates/limits.py
@@ -1,7 +1,7 @@
 # Units in requests-per-minute, rate limits apply to production environments
 
 # Rate limit to all calls to Integrates
-DEFAULT = 30
+DEFAULT = 10  # at most 1 in a 6 seconds interval
 
 # Rate limit on specific funcionalities
-DO_UPDATE_EVIDENCE = 2
+DO_UPDATE_EVIDENCE = 0.25  # at most 1 in a 4 minutes interval
