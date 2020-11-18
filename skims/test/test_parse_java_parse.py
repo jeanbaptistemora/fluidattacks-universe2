b1d72ce55fa53f932802c0c1089f4c43c47a81ca
Kevin Amado <kamado@fluidattacks.com>
2020-11-18 10:39:49 -0500

    skims\test(back): #3675.18 parametrize tests
    Former-commit-id: 6e69abfffa931f7b10b8a37cfc2ba1f29100144c

--- skims/test/test_parse_java_parse.py
+++ skims/test/test_parse_java_parse.py
@@ -5,6 +5,7 @@ import json
 from aioextensions import (
     run_decorator,
 )
+import pytest
 
 # Local libraries
 from parse_antlr.parse import (
@@ -29,51 +30,46 @@ from utils.model import (
 )
 
 
+@pytest.mark.parametrize(  # type: ignore
+    'path,name',
+    [
+        (
+            'test/data/lib_path/f031_cwe378/Test.java',
+            'small_java_program',
+        ),
+        (
+            'test/data/benchmark/owasp/BenchmarkTest00008.java',
+            'owasp_benchmark_00008',
+        ),
+        (
+            'test/data/benchmark/owasp/BenchmarkTest00167.java',
+            'owasp_benchmark_00167'
+        )
+    ],
+)
 @run_decorator
-async def test_graph_generation_easy() -> None:
-    path = 'test/data/lib_path/f031_cwe378/Test.java'
+async def test_graph_generation(path: str, name: str) -> None:
     parse_tree = await parse(
         Grammar.JAVA9,
         content=await get_file_raw_content(path),
         path=path,
     )
     model = model_from_parse_tree(parse_tree)
+    model_as_json = json.dumps(model, indent=2)
     graph = from_antlr_model(model)
     graph_as_json = export_graph_as_json(graph)
     graph_as_json_str = json.dumps(graph_as_json, indent=2, sort_keys=True)
 
-    assert export_graph(graph, 'test/outputs/test_graph_generation_easy.graph')
-    assert await graphviz_to_svg('test/outputs/test_graph_generation_easy.graph')
+    assert export_graph(graph, f'test/outputs/{name}.graph')
+    assert await graphviz_to_svg(f'test/outputs/{name}.graph')
 
-    with open('test/data/parse_antlr/graph_generation_easy.json', 'w') as handle:
+    with open(f'test/outputs/{name}.model.json', 'w') as handle:
+        handle.write(model_as_json)
+
+    with open(f'test/data/parse_java/{name}.graph.json', 'w') as handle:
         handle.write(graph_as_json_str)
 
-    with open('test/data/parse_antlr/graph_generation_easy.json') as handle:
-        expected = handle.read()
-
-    assert graph_as_json_str == expected
-
-
-@run_decorator
-async def test_graph_generation_hard() -> None:
-    path = 'test/data/benchmark/owasp/BenchmarkTest00008.java'
-    parse_tree = await parse(
-        Grammar.JAVA9,
-        content=await get_file_raw_content(path),
-        path=path,
-    )
-    model = model_from_parse_tree(parse_tree)
-    graph = from_antlr_model(model)
-    graph_as_json = export_graph_as_json(graph)
-    graph_as_json_str = json.dumps(graph_as_json, indent=2)
-
-    assert export_graph(graph, 'test/outputs/test_graph_generation_hard.graph')
-    assert await graphviz_to_svg('test/outputs/test_graph_generation_hard.graph')
-
-    with open('test/data/parse_antlr/graph_generation_hard.json', 'w') as handle:
-        handle.write(graph_as_json_str)
-
-    with open('test/data/parse_antlr/graph_generation_hard.json') as handle:
+    with open(f'test/data/parse_java/{name}.graph.json') as handle:
         expected = handle.read()
 
     assert graph_as_json_str == expected
