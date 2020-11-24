d55e45a1ad949ef5856e7ed648c520dafc62426a
Kevin Amado <kamado@fluidattacks.com>
2020-11-24 17:09:34 -0500

    skims\refac(back): #3678.4 rm unused code
    Former-commit-id: 9109eb8b77d21a99e2880c3c03180ab6bc663965

--- skims/skims/parse_antlr/model.py
+++ skims/skims/parse_antlr/model.py
@@ -11,7 +11,7 @@ from parse_antlr.common import (
 )
 
 
-def _parse_rule2(model: List[Dict[str, Any]]) -> Dict[str, Any]:
+def _parse_rule(model: List[Dict[str, Any]]) -> Dict[str, Any]:
     tokens: Dict[str, Any] = {}
     token_indexes: Dict[str, int] = {}
 
@@ -38,28 +38,6 @@ def _parse_rule2(model: List[Dict[str, Any]]) -> Dict[str, Any]:
     return tokens
 
 
-def _parse_rule(model: List[Dict[str, Any]]) -> Dict[str, Any]:
-    result: Dict[str, Any] = {}
-    token_index = 0
-
-    for model_element in model:
-        token_name, token_value = next(iter(model_element.items()))
-
-        if token_name == 'Token':
-            token_name = f'Token[{token_index}]'
-            token_index += 1
-
-        if token_name in result:
-            if isinstance(result[token_name], list):
-                result[token_name].append(token_value)
-            else:
-                result[token_name] = [result[token_name], token_value]
-        else:
-            result[token_name] = token_value
-
-    return result
-
-
 def _structure_model(model: Dict[str, Any]) -> Dict[str, Any]:
     result: Dict[str, Any]
 
@@ -79,41 +57,10 @@ def _structure_model(model: Dict[str, Any]) -> Dict[str, Any]:
                 # Can happen?
                 raise NotImplementedError()
 
-        elif len(model) == 4:
-            # Token node
-            result = {'Token': model}
-        else:
-            result = dict(zip(model, map(_structure_model, model.values())))
-    else:
-        # Can happen?
-        raise NotImplementedError()
-
-    return result
-
-
-def _structure_model2(model: Dict[str, Any]) -> Dict[str, Any]:
-    result: Dict[str, Any]
-
-    if isinstance(model, dict):
-        if len(model) == 1:
-            # Single key node
-            key, value = next(iter(model.items()))
-
-            if isinstance(value, list):
-                if len(value) == 1:
-                    # Single value list
-                    result = {key: _structure_model2(value[0])}
-                else:
-                    # Multiple values list
-                    result = {key: list(map(_structure_model2, value))}
-            else:
-                # Can happen?
-                raise NotImplementedError()
-
         elif is_positional_node(model):
             result = {'Token': model}
         else:
-            result = dict(zip(model, map(_structure_model2, model.values())))
+            result = dict(zip(model, map(_structure_model, model.values())))
     else:
         # Can happen?
         raise NotImplementedError()
@@ -126,10 +73,10 @@ def _structure_keys(model: Dict[str, Any]) -> Dict[str, Any]:
         result: Dict[str, Any] = {}
         for key, val in model.items():
             if isinstance(val, dict):
-                if key == 'Token':
+                if key == 'Token' or key.startswith('Token|'):
                     result[key] = val
                 else:
-                    result[key] = _structure_keys(val)
+                    result[key] = _parse_rule([_structure_keys(val)])
             elif isinstance(val, list):
                 result[key] = _parse_rule(list(map(_structure_keys, val)))
             else:
@@ -142,30 +89,9 @@ def _structure_keys(model: Dict[str, Any]) -> Dict[str, Any]:
     return result
 
 
-def _structure_keys2(model: Dict[str, Any]) -> Dict[str, Any]:
-    if isinstance(model, dict):
-        result: Dict[str, Any] = {}
-        for key, val in model.items():
-            if isinstance(val, dict):
-                if key == 'Token' or key.startswith('Token|'):
-                    result[key] = val
-                else:
-                    result[key] = _parse_rule2([_structure_keys2(val)])
-            elif isinstance(val, list):
-                result[key] = _parse_rule2(list(map(_structure_keys2, val)))
-            else:
-                # Can happen?
-                raise NotImplementedError()
-    else:
-        # Can happen?
-        raise NotImplementedError()
-
-    return result
-
-
 def from_parse_tree(parse_tree: Dict[str, Any]) -> Dict[str, Any]:
     model = parse_tree
-    model = _structure_model2(model)
-    model = _structure_keys2(model)
+    model = _structure_model(model)
+    model = _structure_keys(model)
 
     return model
