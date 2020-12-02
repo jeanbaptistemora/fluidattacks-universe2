16992895be34ed7cfcaa0bb9fbf6303701139f7e
David Acevedo <dacevedo@fluidattacks.com>
2020-12-02 17:15:22 -0500

    integrates\test(front): #2617.35 update git root
    - added testcase for upcoming feature
    
    
    Former-commit-id: e9029eab65b9f039d9167a4f19f033190e3731e4

--- integrates/front/src/scenes/Dashboard/containers/GroupScopeView/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/containers/GroupScopeView/index.spec.tsx
@@ -5,12 +5,13 @@ import { Provider } from "react-redux";
 import { PureAbility } from "@casl/ability";
 import React from "react";
 import type { ReactWrapper } from "enzyme";
+import { SwitchButton } from "components/SwitchButton";
 import { act } from "react-dom/test-utils";
 import { getCache } from "utils/apollo";
 import { mount } from "enzyme";
 import store from "store";
 import wait from "waait";
-import { ADD_GIT_ROOT, GET_ROOTS } from "./query";
+import { ADD_GIT_ROOT, GET_ROOTS, UPDATE_GIT_ROOT } from "./query";
 import { MemoryRouter, Route } from "react-router";
 import { authzGroupContext, authzPermissionsContext } from "utils/authz/config";
 
@@ -192,7 +193,7 @@ describe("GroupScopeView", (): void => {
     await act(
       async (): Promise<void> => {
         wrapper.find("form").simulate("submit");
-        const delay: number = 200;
+        const delay: number = 50;
         await wait(delay);
         wrapper.update();
       }
@@ -204,6 +205,159 @@ describe("GroupScopeView", (): void => {
       ["https://gitlab.com/fluidattacks/product", "master", "production"].join(
         ""
       )
+    );
+  });
+
+  // Will enable next MR
+  // eslint-disable-next-line jest/no-disabled-tests
+  it.skip("should update git roots", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    const initialQueryMock: MockedResponse = {
+      request: {
+        query: GET_ROOTS,
+        variables: { groupName: "unittesting" },
+      },
+      result: {
+        data: {
+          group: {
+            __typename: "Project",
+            roots: [
+              {
+                __typename: "GitRoot",
+                branch: "master",
+                environment: "production",
+                environmentUrls: [],
+                filter: null,
+                id: "ROOT#4039d098-ffc5-4984-8ed3-eb17bca98e19",
+                includesHealthCheck: false,
+                url: "https://gitlab.com/fluidattacks/product",
+              },
+            ],
+          },
+        },
+      },
+    };
+    const mutationMock: MockedResponse = {
+      request: {
+        query: UPDATE_GIT_ROOT,
+        variables: {
+          environment: "staging",
+          filter: { paths: ["node_modules/"], policy: "EXCLUDE" },
+          id: "ROOT#4039d098-ffc5-4984-8ed3-eb17bca98e19",
+          includesHealthCheck: true,
+        },
+      },
+      result: {
+        data: { updateGitRoot: { __typename: "SimplePayload", success: true } },
+      },
+    };
+    const finalQueryMock: MockedResponse = {
+      request: {
+        query: GET_ROOTS,
+        variables: { groupName: "unittesting" },
+      },
+      result: {
+        data: {
+          group: {
+            __typename: "Project",
+            roots: [
+              {
+                __typename: "GitRoot",
+                branch: "master",
+                environment: "staging",
+                environmentUrls: [],
+                filter: {
+                  __typename: "GitRootFilter",
+                  paths: ["node_modules/"],
+                  policy: "EXCLUDE",
+                },
+                id: "ROOT#4039d098-ffc5-4984-8ed3-eb17bca98e19",
+                includesHealthCheck: true,
+                url: "https://gitlab.com/fluidattacks/product",
+              },
+            ],
+          },
+        },
+      },
+    };
+
+    const wrapper: ReactWrapper = mount(
+      <Provider store={store}>
+        <authzGroupContext.Provider
+          value={new PureAbility([{ action: "has_drills_white" }])}
+        >
+          <authzPermissionsContext.Provider
+            value={
+              new PureAbility([
+                { action: "backend_api_mutations_add_git_root_mutate" },
+                { action: "backend_api_mutations_update_git_root_mutate" },
+              ])
+            }
+          >
+            <MemoryRouter
+              initialEntries={["/orgs/okada/groups/unittesting/scope"]}
+            >
+              <MockedProvider
+                cache={getCache()}
+                mocks={[initialQueryMock, mutationMock, finalQueryMock]}
+              >
+                <Route
+                  component={GroupScopeView}
+                  path={"/orgs/:organizationName/groups/:projectName/scope"}
+                />
+              </MockedProvider>
+            </MemoryRouter>
+          </authzPermissionsContext.Provider>
+        </authzGroupContext.Provider>
+      </Provider>
+    );
+
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+    const getfirstTableRow: () => ReactWrapper = (): ReactWrapper =>
+      wrapper.find("tr").at(1) as ReactWrapper;
+
+    getfirstTableRow().simulate("click");
+
+    const environment: ReactWrapper = wrapper
+      .find({ name: "environment" })
+      .find("input");
+    environment.simulate("change", { target: { value: "staging" } });
+
+    wrapper.find(SwitchButton).at(0).simulate("click");
+    wrapper.find(SwitchButton).at(1).simulate("click");
+
+    const includesHealthCheck: ReactWrapper = wrapper
+      .find({ name: "includesHealthCheck" })
+      .find("input");
+    includesHealthCheck.simulate("change", { target: { value: true } });
+
+    const policy: ReactWrapper = wrapper
+      .find({ name: "filter.policy" })
+      .find("select");
+    policy.simulate("change", { target: { value: "EXCLUDE" } });
+
+    const path1: ReactWrapper = wrapper
+      .find({ name: "filter.paths[0]" })
+      .find("input");
+    path1.simulate("change", { target: { value: "node_modules/" } });
+
+    await act(
+      async (): Promise<void> => {
+        wrapper.find("form").simulate("submit");
+        const delay: number = 50;
+        await wait(delay);
+        wrapper.update();
+      }
+    );
+
+    expect(getfirstTableRow().text()).toStrictEqual(
+      ["https://gitlab.com/fluidattacks/product", "master", "staging"].join("")
     );
   });
 });
