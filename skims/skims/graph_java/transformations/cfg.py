e0fc9d8d39fb506698897bd73393a6274fb21618
Kevin Amado <kamado@fluidattacks.com>
2020-12-03 14:53:06 -0500

    skims\feat(back): #3678.33 cfg if
    - Link if statements
    
    
    Former-commit-id: 50bac2f7d4ac81e9007e0aa9e0a61b8f74a65770

--- skims/skims/graph_java/transformations/cfg.py
+++ skims/skims/graph_java/transformations/cfg.py
@@ -1,6 +1,7 @@
 # Standar library
 from typing import (
     Any,
+    Dict,
     List,
 )
 
@@ -26,6 +27,7 @@ MAYBE = dict(**CFG, label_cfg_maybe='cfg_maybe')
 TRUE = dict(**CFG, label_cfg_true='cfg_true')
 
 # Types
+EdgeAttrs = Dict[str, str]
 Frame = Any  # will add types once I discover the pattern
 Stack = List[Frame]
 
@@ -39,7 +41,7 @@ def _block(
     graph.add_edge(n_id, c_id, **ALWAYS)
 
     # Recurse
-    _generic(graph, c_id, stack=stack)
+    _generic(graph, c_id, stack, edge_attrs=ALWAYS)
 
 
 def _block_statements(
@@ -61,7 +63,7 @@ def _block_statements(
         else:
             stack[-1]['next_id'] = stmt_b_id
 
-        _generic(graph, stmt_a_id, stack=stack)
+        _generic(graph, stmt_a_id, stack, edge_attrs=ALWAYS)
 
 
 def _expression_statements(
@@ -70,6 +72,33 @@ def _expression_statements(
     stack: Stack,
 ) -> None:
     _block_statements(graph, n_id, stack)
+
+
+def _if_statement(
+    graph: nx.DiGraph,
+    n_id: str,
+    stack: Stack,
+) -> None:
+    # if ( __0__ ) __1__ else __2__
+    match = g.match_ast(
+        graph, n_id,
+        'IF',
+        'LPAREN',
+        '__0__',
+        'RPAREN',
+        '__1__',
+    )
+
+    if then_id := match['__1__']:
+        # Propagate next id from parent if available
+        if stack[-2].get('next_id'):
+            stack[-1]['next_id'] = stack[-2]['next_id']
+
+        # Link `if` to `then` statement
+        graph.add_edge(n_id, then_id, **ALWAYS)
+
+        # Link whatever is inside the `then` to the next statement in chain
+        _generic(graph, then_id, stack, edge_attrs=TRUE)
 
 
 def _method_declaration(
@@ -81,13 +110,15 @@ def _method_declaration(
     graph.add_edge(n_id, c_id, **ALWAYS)
 
     # Recurse
-    _generic(graph, c_id, stack=stack)
+    _generic(graph, c_id, stack, edge_attrs=ALWAYS)
 
 
 def _generic(
     graph: nx.DiGraph,
     n_id: str,
     stack: Stack,
+    *,
+    edge_attrs: EdgeAttrs,
 ) -> None:
     n_attrs = graph.nodes[n_id]
     n_attrs_label_type = n_attrs['label_type']
@@ -100,10 +131,12 @@ def _generic(
         _block_statements(graph, n_id, stack)
     elif n_attrs_label_type == 'ExpressionStatements':
         _expression_statements(graph, n_id, stack)
+    elif n_attrs_label_type == 'IfThenStatement':
+        _if_statement(graph, n_id, stack)
     elif n_attrs_label_type == 'MethodDeclaration':
         _method_declaration(graph, n_id, stack)
     elif next_id := stack[-2].get('next_id'):
-        graph.add_edge(n_id, next_id, **ALWAYS)
+        graph.add_edge(n_id, next_id, **edge_attrs)
 
     stack.pop()
 
@@ -114,4 +147,4 @@ def analyze(graph: nx.DiGraph) -> None:
     for n_id in g.filter_nodes(graph, graph.nodes, g.pred_has_labels(
         label_type='MethodDeclaration',
     )):
-        _generic(graph, n_id, [])
+        _generic(graph, n_id, [], edge_attrs=ALWAYS)
