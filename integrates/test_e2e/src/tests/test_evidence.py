f7c1e61eeeb2d8f21e61be3a95ab5c482be407d4
Daniel Salazar <dsalazar@fluidattacks.com>
2020-12-04 15:04:28 -0500

    integrates\test(cross): #3757.12 migrate tests
    - Add tests for evidence, exploit and tracking
    
    
    Former-commit-id: 87c9b7b58c8193f779582916200e249f93690ec3

---
+++ integrates/test_e2e/src/tests/test_evidence.py
@@ -0,0 +1,41 @@
+# Third party libraries
+from selenium.webdriver.remote.webdriver import WebDriver
+
+# Local libraries
+import utils
+from model import (
+    AzureCredentials
+)
+
+
+def test_evidence(
+        driver: WebDriver,
+        azure_credentials: AzureCredentials,
+        integrates_endpoint: str,
+        timeout: int) -> None:
+    # Login
+    utils.login_azure(driver, azure_credentials, timeout)
+    utils.login_integrates_azure(driver, integrates_endpoint, timeout)
+
+    # Enter finding
+    driver.get(f'{integrates_endpoint}/orgs/okada/groups/unittesting/vulns')
+    finding = utils.wait_for_text(
+        driver,
+        'FIN.H.060. Insecure exceptions',
+        timeout,
+    )
+    finding.click()
+
+    # Enter finding evidences
+    severity = utils.wait_for_id(
+        driver,
+        'evidenceItem',
+        timeout,
+    )
+    severity.click()
+    utils.wait_for_text(
+        driver,
+        'exception',
+        timeout,
+    )
+    assert 'exception' in driver.page_source
