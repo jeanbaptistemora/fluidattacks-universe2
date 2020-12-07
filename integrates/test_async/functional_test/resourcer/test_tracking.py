22e6c93870a355a34d1bd72120e066d23ef53abb
Daniel Aguirre <daguirre@fluidattacks.com>
2020-12-07 09:06:17 -0500

    integrates\feat(back): #3765.3 add new tests
    - Add some functional test for tacking.
    - no-deltas-test
    
    
    Former-commit-id: aa061669529938bfc28b80b9f607d5592e6d629e

---
+++ integrates/test_async/functional_test/resourcer/test_tracking.py
@@ -0,0 +1,178 @@
+import pytest
+
+from test_async.functional_test.customer.utils import get_result
+
+pytestmark = pytest.mark.asyncio
+
+
+async def test_tracking():
+    finding_id = '436992569'
+    expected_output =  {
+        'id': finding_id,
+        'tracking': [
+            {
+                "cycle": 0,
+                "open": 1,
+                "closed": 0,
+                "effectiveness": 0,
+                "date": "2019-08-30",
+                "new": 1,
+                "in_progress": 0,
+                "accepted": 0,
+                "accepted_undefined": 0
+            },
+            {
+                "cycle": 1,
+                "open": 16,
+                "closed": 0,
+                "effectiveness": 0,
+                "date": "2019-09-12",
+                "new": 1,
+                "in_progress": 0,
+                "accepted": 0,
+                "accepted_undefined": 0
+            },
+            {
+                "cycle": 2,
+                "open": 22,
+                "closed": 4,
+                "effectiveness": 15,
+                "date": "2019-09-13",
+                "new": 2,
+                "in_progress": 0,
+                "accepted": 0,
+                "accepted_undefined": 0
+            },
+            {
+                "cycle": 3,
+                "open": 24,
+                "closed": 4,
+                "effectiveness": 14,
+                "date": "2019-09-16",
+                "new": 2,
+                "in_progress": 0,
+                "accepted": 0,
+                "accepted_undefined": 0
+            },
+            {
+                "cycle": 4,
+                "open": 24,
+                "closed": 4,
+                "effectiveness": 14,
+                "date": "2020-02-19",
+                "new": 2,
+                "in_progress": 0,
+                "accepted": 0,
+                "accepted_undefined": 0
+            }
+        ],
+    }
+    query = f'''{{
+        finding(identifier: "{finding_id}"){{
+            id
+            tracking
+        }}
+    }}'''
+    data = {'query': query}
+    result = await get_result(data)
+    assert 'errors' not in result
+    assert result['data']['finding']['id'] == expected_output.get('id')
+    assert result['data']['finding']['tracking'] == expected_output.get('tracking')
+
+    finding_id = '422286126'
+    expected_output =  {
+        'id': finding_id,
+        'tracking': [
+            {
+                "cycle": 0,
+                "open": 1,
+                "closed": 0,
+                "effectiveness": 0,
+                "date": "2020-09-09",
+                "new": 0,
+                "in_progress": 1,
+                "accepted": 0,
+                "accepted_undefined": 0
+            }
+        ],
+    }
+    query = f'''{{
+        finding(identifier: "{finding_id}"){{
+            id
+            tracking
+        }}
+    }}'''
+    data = {'query': query}
+    result = await get_result(data)
+    assert 'errors' not in result
+    assert result['data']['finding']['id'] == expected_output.get('id')
+    assert result['data']['finding']['tracking'] == expected_output.get('tracking')
+
+    finding_id = '463558592'
+    expected_output =  {
+        'id': finding_id,
+        'tracking': [
+            {
+                "cycle": 0,
+                "open": 1,
+                "closed": 1,
+                "effectiveness": 50,
+                "date": "2019-01-15",
+                "new": 0,
+                "in_progress": 0,
+                "accepted": 1,
+                "accepted_undefined": 0
+            }
+        ],
+    }
+    query = f'''{{
+        finding(identifier: "{finding_id}"){{
+            id
+            tracking
+        }}
+    }}'''
+    data = {'query': query}
+    result = await get_result(data)
+    assert 'errors' not in result
+    assert result['data']['finding']['id'] == expected_output.get('id')
+    assert result['data']['finding']['tracking'] == expected_output.get('tracking')
+
+    finding_id = '463461507'
+    expected_output =  {
+        'id': finding_id,
+        'tracking': [
+            {
+                "cycle": 0,
+                "open": 1,
+                "closed": 0,
+                "effectiveness": 0,
+                "date": "2019-09-12",
+                "new": 0,
+                "in_progress": 1,
+                "accepted": 0,
+                "accepted_undefined": 0
+            },
+            {
+                "cycle": 1,
+                "open": 2,
+                "closed": 0,
+                "effectiveness": 0,
+                "date": "2019-09-13",
+                "new": 1,
+                "in_progress": 0,
+                "accepted": 1,
+                "accepted_undefined": 0
+            }
+        ],
+    }
+    query = f'''{{
+        finding(identifier: "{finding_id}"){{
+            id
+            tracking
+        }}
+    }}'''
+    data = {'query': query}
+    result = await get_result(data)
+    assert 'errors' not in result
+    assert result['data']['finding']['id'] == expected_output.get('id')
+    assert result['data']['finding']['tracking'] == expected_output.get('tracking')
