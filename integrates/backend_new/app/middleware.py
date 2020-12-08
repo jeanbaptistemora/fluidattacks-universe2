551687fa8d0579ccd24680872ef36d3a87a0496e
Mario Varas <mvaras@fluidattacks.com>
2020-12-08 08:03:20 -0500

    integrates\feat(back): #3399.125 newrelic traces
    - This way, we can for now at least have some data
    of transactions
    - In the meantime, we should discover why newrelic
    api reports are no working.
    
    
    Former-commit-id: c04df4ac391bd1d8bf631abe28ee8ab550c237c2

--- integrates/backend_new/app/middleware.py
+++ integrates/backend_new/app/middleware.py
@@ -5,13 +5,17 @@ from collections import defaultdict
 from typing import Any, Callable
 
 # Third party libraries
+import newrelic.agent
 import sqreen
+
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.requests import Request
 from starlette.responses import Response
 
 # Local libraries
 import backend_new.app.utils as utils
+
+from __init__ import BASE_URL
 
 
 class CustomRequestMiddleware(BaseHTTPMiddleware):  # type: ignore
@@ -22,6 +26,10 @@ class CustomRequestMiddleware(BaseHTTPMiddleware):  # type: ignore
         call_next: Callable[..., Any]
     ) -> Response:
         request = utils.get_starlette_request(request)
+        url = str(request.url)
+        traceback = url.split(BASE_URL)[-1]
+        if url != traceback:
+            newrelic.agent.set_transaction_name(f'api:{traceback}')
         request.state.store = defaultdict(lambda: None)
         if request.session.get('username'):
             sqreen.identify({'username': request.session['username']})
