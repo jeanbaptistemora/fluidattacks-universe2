40fa39306e322ff9dfe2be5851810c74347e880d
David Acevedo <dacevedo@fluidattacks.com>
2020-12-10 10:54:03 -0500

    integrates\perf(back): #2617.47 improve git roots
    - get org roots concurrently
    - simplified duplicates lookup using tuples
    - this should make it nicer for users with
    - lots of orgs and groups
    - added performance tracking
    - decoupled some functions
    
    
    Former-commit-id: 08714dcc2c71346f74cd694dd5e5ad585021f9a2

--- integrates/backend_new/packages/integrates-back/backend/domain/root.py
+++ integrates/backend_new/packages/integrates-back/backend/domain/root.py
@@ -1,9 +1,11 @@
 # Standard
 import re
 from typing import Any, Dict, Optional, Tuple
+from urllib.parse import unquote
 
 # Third party
-from urllib.parse import unquote
+import newrelic.agent
+from aioextensions import collect
 from urllib3.util.url import parse_url, Url
 
 # Local
@@ -51,11 +53,6 @@ def format_root(root: Dict[str, Any]) -> Root:
     )
 
 
-def _is_active(root: Dict[str, Any]) -> bool:
-    state: str = root['historic_state'][-1]['state']
-    return state == 'ACTIVE'
-
-
 async def get_root_by_id(root_id: str) -> Dict[str, Any]:
     root: Optional[Dict[str, Any]] = await root_dal.get_root_by_id(root_id)
 
@@ -81,67 +78,23 @@ async def get_roots_by_group(group_name: str) -> Tuple[Dict[str, Any], ...]:
     )
 
 
-def _matches_root(
-    kind: str,
-    root: Dict[str, Any],
-    new_root: Dict[str, Any]
-) -> bool:
-    current_state: Dict[str, str] = root['historic_state'][-1]
-
-    if kind == root['kind'] == 'Git':
-        return bool(
-            root['url'] == new_root['url']
-            and root['branch'] == new_root['branch']
-        )
-
-    if kind == root['kind'] == 'IP':
-        return bool(
-            current_state['address'] == new_root['address']
-            and current_state['port'] == new_root['port']
-        )
-
-    if kind == root['kind'] == 'URL':
-        return bool(
-            current_state['host'] == new_root['host']
-            and current_state['path'] == new_root['path']
-            and current_state['port'] == new_root['port']
-            and current_state['protocol'] == new_root['protocol']
-        )
-
-    return False
-
-
-async def _is_unique_in_org(
-    org_id: str,
-    kind: str,
-    new_root: Dict[str, Any]
-) -> bool:
+@newrelic.agent.function_trace()  # type: ignore
+async def _is_git_unique_in_org(org_id: str, url: str, branch: str) -> bool:
     org_groups: Tuple[str, ...] = await org_domain.get_groups(org_id)
-
-    for group_name in org_groups:
-        group_roots = await get_roots_by_group(group_name)
-
-        if next(
-            (
-                root
-                for root in group_roots
-                if _matches_root(kind, root, new_root)
-            ),
-            None
-        ):
-            return False
-
-    return True
-
-
-def is_valid_git_repo(url: str, branch: str) -> bool:
-    return (
-        validations.is_valid_url(url)
-        and validations.is_valid_git_branch(branch)
+    org_roots: Tuple[Tuple[str, str], ...] = tuple(
+        (root['url'], root['branch'])
+        for group_roots in await collect(
+            get_roots_by_group(group_name)
+            for group_name in org_groups
+        )
+        for root in group_roots
+        if root['kind'] == 'Git'
     )
 
+    return (url, branch) not in org_roots
 
-def format_git_repo_url(raw_url: str) -> str:
+
+def _format_git_repo_url(raw_url: str) -> str:
     is_ssh: bool = (
         raw_url.startswith('ssh://')
         or bool(re.match(r'^\w+@.*', raw_url))
@@ -157,7 +110,12 @@ def format_git_repo_url(raw_url: str) -> str:
 
 async def add_git_root(user_email: str, **kwargs: Any) -> None:
     group_name: str = kwargs['group_name'].lower()
-    url: str = format_git_repo_url(kwargs['url'])
+    url: str = _format_git_repo_url(kwargs['url'])
+    branch: str = kwargs['branch']
+    is_valid: bool = (
+        validations.is_valid_url(url)
+        and validations.is_valid_git_branch(branch)
+    )
 
     enforcer = await authz.get_group_level_enforcer(user_email)
     if (
@@ -166,8 +124,7 @@ async def add_git_root(user_email: str, **kwargs: Any) -> None:
     ):
         raise PermissionDenied()
 
-    if is_valid_git_repo(url, kwargs['branch']):
-        kind: str = 'Git'
+    if is_valid:
         org_id: str = await org_domain.get_id_for_group(group_name)
         initial_state: Dict[str, Any] = {
             'date': datetime.get_as_str(datetime.get_now()),
@@ -178,13 +135,13 @@ async def add_git_root(user_email: str, **kwargs: Any) -> None:
             'user': user_email
         }
         root_attributes: Dict[str, Any] = {
-            'branch': kwargs['branch'],
+            'branch': branch,
             'historic_state': [initial_state],
-            'kind': kind,
+            'kind': 'Git',
             'url': url
         }
 
-        if await _is_unique_in_org(org_id, kind, root_attributes):
+        if await _is_git_unique_in_org(org_id, url, branch):
             await root_dal.create(group_name, root_attributes)
         else:
             raise RepeatedValues()
@@ -192,33 +149,80 @@ async def add_git_root(user_email: str, **kwargs: Any) -> None:
         raise InvalidParameter()
 
 
+@newrelic.agent.function_trace()  # type: ignore
+async def _is_ip_unique_in_org(org_id: str, address: str, port: int) -> bool:
+    org_groups: Tuple[str, ...] = await org_domain.get_groups(org_id)
+    org_roots: Tuple[Tuple[str, int], ...] = tuple(
+        (
+            root['historic_state'][-1]['address'],
+            root['historic_state'][-1]['port']
+        )
+        for group_roots in await collect(
+            get_roots_by_group(group_name)
+            for group_name in org_groups
+        )
+        for root in group_roots
+        if root['kind'] == 'IP'
+    )
+
+    return (address, port) not in org_roots
+
+
 async def add_ip_root(user_email: str, **kwargs: Any) -> None:
     group_name: str = kwargs['group_name'].lower()
+    address: str = kwargs['address']
+    port: int = kwargs['port']
     is_valid: bool = (
-        validations.is_valid_ip(kwargs['address'])
-        and 0 <= int(kwargs['port']) <= 65535
+        validations.is_valid_ip(address)
+        and 0 <= int(port) <= 65535
     )
 
     if is_valid:
-        kind: str = 'IP'
         org_id: str = await org_domain.get_id_for_group(group_name)
         initial_state: Dict[str, Any] = {
-            'address': kwargs['address'],
+            'address': address,
             'date': datetime.get_as_str(datetime.get_now()),
-            'port': kwargs['port'],
+            'port': port,
             'user': user_email
         }
 
-        if await _is_unique_in_org(org_id, kind, initial_state):
+        if await _is_ip_unique_in_org(org_id, address, port):
             root_attributes: Dict[str, Any] = {
                 'historic_state': [initial_state],
-                'kind': kind
+                'kind': 'IP'
             }
             await root_dal.create(group_name, root_attributes)
         else:
             raise RepeatedValues()
     else:
         raise InvalidParameter()
+
+
+@newrelic.agent.function_trace()  # type: ignore
+async def _is_url_unique_in_org(
+    org_id: str,
+    host: str,
+    path: str,
+    port: int,
+    protocol: str
+) -> bool:
+    org_groups: Tuple[str, ...] = await org_domain.get_groups(org_id)
+    org_roots: Tuple[Tuple[str, str, int, str], ...] = tuple(
+        (
+            root['historic_state'][-1]['host'],
+            root['historic_state'][-1]['path'],
+            root['historic_state'][-1]['port'],
+            root['historic_state'][-1]['protocol']
+        )
+        for group_roots in await collect(
+            get_roots_by_group(group_name)
+            for group_name in org_groups
+        )
+        for root in group_roots
+        if root['kind'] == 'URL'
+    )
+
+    return (host, path, port, protocol) not in org_roots
 
 
 async def add_url_root(user_email: str, **kwargs: Any) -> None:
@@ -230,22 +234,25 @@ async def add_url_root(user_email: str, **kwargs: Any) -> None:
     )
 
     if is_valid:
+        host: str = url_attributes.host
+        path: str = url_attributes.path or '/'
         default_port: int = 443 if url_attributes.scheme == 'https' else 80
-        kind: str = 'URL'
+        port: int = url_attributes.port or default_port
+        protocol: str = url_attributes.scheme.upper()
         org_id: str = await org_domain.get_id_for_group(group_name)
         initial_state: Dict[str, Any] = {
             'date': datetime.get_as_str(datetime.get_now()),
-            'host': url_attributes.host,
-            'path': url_attributes.path or '/',
-            'port': url_attributes.port or default_port,
-            'protocol': url_attributes.scheme.upper(),
+            'host': host,
+            'path': path,
+            'port': port,
+            'protocol': protocol,
             'user': user_email
         }
 
-        if await _is_unique_in_org(org_id, kind, initial_state):
+        if await _is_url_unique_in_org(org_id, host, path, port, protocol):
             root_attributes: Dict[str, Any] = {
                 'historic_state': [initial_state],
-                'kind': kind
+                'kind': 'URL'
             }
             await root_dal.create(group_name, root_attributes)
         else:
@@ -254,14 +261,15 @@ async def add_url_root(user_email: str, **kwargs: Any) -> None:
         raise InvalidParameter()
 
 
+def _is_active(root: Dict[str, Any]) -> bool:
+    state: str = root['historic_state'][-1]['state']
+    return state == 'ACTIVE'
+
+
 async def update_git_root(user_email: str, **kwargs: Any) -> None:
     root_id: str = kwargs['id']
     root: Dict[str, Any] = await get_root_by_id(root_id)
-    is_valid: bool = (
-        _is_active(root)
-        and is_valid_git_repo(root['url'], root['branch'])
-        and root['kind'] == 'Git'
-    )
+    is_valid: bool = _is_active(root) and root['kind'] == 'Git'
 
     enforcer = await authz.get_group_level_enforcer(user_email)
     if (
