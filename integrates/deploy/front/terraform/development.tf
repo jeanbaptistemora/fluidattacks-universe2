702f6c0d691b78f1ae2b90a429c2180099164389
Daniel Salazar <dsalazar@fluidattacks.com>
2020-12-14 11:56:59 -0500

    integrates\feat(infra): #3808.2 digital certificates
    - Disable versioning for buckets
    - Enable bucket encryption
    - Rename buckets to comply conventions
    - Add digital certificates for dev and prod domains
    
    
    Former-commit-id: e77eb580b05d364999fc7ace64d81193c51a3e17

--- integrates/deploy/front/terraform/development.tf
+++ integrates/deploy/front/terraform/development.tf
@@ -1,11 +1,51 @@
+# Bucket
 resource "aws_s3_bucket" "development" {
-  bucket = "front-development.fluidattacks.com"
+  bucket = "integrates.front.dev.fluidattacks.com"
   acl    = "private"
   region = var.region
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  versioning {
+    enabled = false
+  }
+
   tags = {
-    "Name"               = "front-development.fluidattacks.com"
+    "Name"               = "integrates.front.dev.fluidattacks.com"
     "management:type"    = "development"
     "management:product" = "integrates"
   }
+}
+
+# Certificate
+resource "aws_acm_certificate" "development" {
+  domain_name       = "integrates.front.dev.fluidattacks.com"
+  validation_method = "DNS"
+
+lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    "Name"               = "integrates-front-dev-certificate"
+    "management:type"    = "development"
+    "management:product" = "integrates"
+  }
+}
+resource "aws_route53_record" "development-validation" {
+  name    = aws_acm_certificate.development.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.development.domain_validation_options.0.resource_record_type
+  zone_id = data.aws_route53_zone.fluidattacks.id
+  records = [aws_acm_certificate.development.domain_validation_options.0.resource_record_value]
+  ttl     = 60
+}
+resource "aws_acm_certificate_validation" "development-validation" {
+  certificate_arn         = aws_acm_certificate.development.arn
+  validation_record_fqdns = [aws_route53_record.development-validation.fqdn]
 }
--- integrates/deploy/front/terraform/production.tf
+++ integrates/deploy/front/terraform/production.tf
@@ -1,11 +1,52 @@
+
+# Bucket
 resource "aws_s3_bucket" "production" {
-  bucket = "front-production.fluidattacks.com"
+  bucket = "integrates.front.prod.fluidattacks.com"
   acl    = "private"
   region = var.region
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  versioning {
+    enabled = false
+  }
+
   tags = {
-    "Name"               = "front-production.fluidattacks.com"
+    "Name"               = "integrates.front.prod.fluidattacks.com"
     "management:type"    = "production"
     "management:product" = "integrates"
   }
+}
+
+# Certificate
+resource "aws_acm_certificate" "production" {
+  domain_name       = "integrates.front.prod.fluidattacks.com"
+  validation_method = "DNS"
+
+lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    "Name"               = "integrates-front-prod-certificate"
+    "management:type"    = "production"
+    "management:product" = "integrates"
+  }
+}
+resource "aws_route53_record" "production-validation" {
+  name    = aws_acm_certificate.production.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.production.domain_validation_options.0.resource_record_type
+  zone_id = data.aws_route53_zone.fluidattacks.id
+  records = [aws_acm_certificate.production.domain_validation_options.0.resource_record_value]
+  ttl     = 60
+}
+resource "aws_acm_certificate_validation" "production-validation" {
+  certificate_arn         = aws_acm_certificate.production.arn
+  validation_record_fqdns = [aws_route53_record.production-validation.fqdn]
 }
