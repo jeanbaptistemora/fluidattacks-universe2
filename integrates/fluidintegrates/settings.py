248f7d195e9a53e50e302459f02564619505e056
Mario Varas <mvaras@fluidattacks.com>
2020-12-14 10:05:10 -0500

    integrates\refac(back): #3399.138 rm django settings code
    Former-commit-id: b011826d0654d39baf30a9e6a48c952e21f56971

--- integrates/fluidintegrates/settings.py
+++ integrates/fluidintegrates/settings.py
@@ -28,9 +28,6 @@ from __init__ import (
     FI_DEBUG,
     FI_DJANGO_SECRET_KEY,
     FI_ENVIRONMENT,
-    FI_JWT_SECRET,
-    FI_JWT_SECRET_API,
-    FI_MIXPANEL_API_TOKEN,
     FI_REDIS_SERVER,
 )
 
@@ -42,29 +39,17 @@ def get_installed_packages() -> List[str]:
 
 
 sys.path.append('/usr/src/app')
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = FI_DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = FI_DEBUG == 'True'
-CI_COMMIT_REF_NAME = os.environ['CI_COMMIT_REF_NAME']
-
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "fluid.la",
-                 "fluidattacks.com", "192.168.200.100.xip.io",
-                 ".integrates.fluidattacks.com",
-                 "integrates.fluidattacks.com"]
-ALLOWED_HOSTS += [
-    f'192.168.{i}.{j}' for i in range(256) for j in range(256)
-]
 
 if DEBUG:
     # Needed for mobile development so it can connect through LAN
     try:
         LOCAL_IPS = subprocess.check_output(
             ['/bin/hostname', '--all-ip-addresses'], encoding='UTF-8')
-        ALLOWED_HOSTS += LOCAL_IPS.strip().split()
     except subprocess.CalledProcessError as exc:
         print(f'Failed to run /bin/hostname --all-ip-addresses')
         print(f'The following error was raised: {exc}')
@@ -103,12 +88,6 @@ MIDDLEWARE = [
     'app.middleware.SocialAuthException'
 ]
 
-ROOT_URLCONF = 'fluidintegrates.urls'
-SETTINGS_PATH = os.path.normpath(os.path.dirname(__file__))
-
-ASGI_APPLICATION = 'fluidintegrates.asgi.application'
-
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
@@ -116,19 +95,9 @@ DATABASES: Dict[str, Dict[str, str]] = {
     'default': {}
 }
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.10/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'America/Bogota'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
+STATIC_BUCKET_NAME = 'fluidintegrates-static'
+CI_COMMIT_REF_NAME = os.environ['CI_COMMIT_REF_NAME']
+AWS_STORAGE_BUCKET_NAME = f'{STATIC_BUCKET_NAME}-{CI_COMMIT_REF_NAME}'
 AWS_ACCESS_KEY_ID = FI_AWS_CLOUDWATCH_ACCESS_KEY
 AWS_SECRET_ACCESS_KEY = FI_AWS_CLOUDWATCH_SECRET_KEY  # noqa
 AWS_REGION_NAME = 'us-east-1'
@@ -174,20 +143,6 @@ except ClientError as exc:
         S3_CLIENT.put_bucket_cors(Bucket=AWS_STORAGE_BUCKET_NAME,
                                   CORSConfiguration=CORS_CONFIGURATION)
 
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
-
-STATICFILES_DIRS = [
-    ('app', os.path.join(BASE_DIR, 'app', 'static', 'app')),
-    ('dashboard', os.path.join(BASE_DIR, 'app', 'static', 'dashboard')),
-    ('external', os.path.join(BASE_DIR, 'app', 'static', 'external')),
-    ('graphics', os.path.join(BASE_DIR, 'app', 'static', 'graphics')),
-    ('img', os.path.join(BASE_DIR, 'app', 'static', 'img')),
-    ('styles', os.path.join(BASE_DIR, 'app', 'static', 'styles')),
-]
-
 CACHE_OPTIONS: Dict[str, Union[str, int, Dict[str, bool]]] = {}
 
 if FI_ENVIRONMENT == 'development':
@@ -215,45 +170,3 @@ CACHES = {
         'KEY_PREFIX': 'integrates'
     }
 }
-
-CACHE_TTL = 60 * 60 * 8
-
-# JWT
-JWT_COOKIE_NAME = "integrates_session"
-JWT_COOKIE_SAMESITE = "Lax"
-JWT_SECRET = FI_JWT_SECRET
-JWT_SECRET_API = FI_JWT_SECRET_API
-
-# Session
-SESSION_ENGINE = 'app.django_redis_sessions'
-SESSION_EXPIRE_AT_BROWSER_CLOSE = True
-SESSION_COOKIE_HTTPONLY = True
-SESSION_COOKIE_NAME = "Integratesv3"
-SESSION_COOKIE_SAMESITE = 'Lax'
-SESSION_COOKIE_SAMESITE_KEYS = {SESSION_COOKIE_NAME, JWT_COOKIE_NAME}
-SESSION_COOKIE_SECURE = True
-SESSION_COOKIE_AGE = 40 * 60
-MOBILE_SESSION_AGE = 30 * 24 * 60 * 60
-CSRF_COOKIE_HTTPONLY = True
-# Temporary while ariadne migration is finished
-if DEBUG:
-    SESSION_COOKIE_SAMESITE_KEYS = {SESSION_COOKIE_NAME}
-CSRF_COOKIE_SECURE = True
-CLUSTER_SESSION = True
-
-if FI_ENVIRONMENT == 'development':
-    CLUSTER_SESSION = False
-
-SESSION_REDIS = {
-    'host': FI_REDIS_SERVER,
-    'port': 6379,
-    'db': 0,
-    'prefix': 'fi_session',
-    'socket_timeout': 1,
-    'retry_on_timeout': False,
-    'cluster': CLUSTER_SESSION
-}
-
-# django-analytical
-MIXPANEL_API_TOKEN = FI_MIXPANEL_API_TOKEN
-ANALYTICAL_AUTO_IDENTIFY = False
