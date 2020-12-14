7dcba7b0a9a5b29f835ab3aa0953f4db24458f02
Andres Aguirre <aaguirre@fluidattacks.com>
2020-12-14 12:54:37 -0500

    integrates\test(front): #3781.5 request zero risk test
    - Added unittest to request zero risk vulns
    
    
    Former-commit-id: 47ebf5d7548720953fa3bb24ad3cb95215fe7f64

--- integrates/front/src/scenes/Dashboard/components/Vulnerabilities/UpdateDescription/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/components/Vulnerabilities/UpdateDescription/index.spec.tsx
@@ -1,4 +1,4 @@
-import { MockedProvider } from "@apollo/react-testing";
+import { MockedProvider, MockedResponse, wait } from "@apollo/react-testing";
 import { PureAbility } from "@casl/ability";
 import { mount, ReactWrapper } from "enzyme";
 import React from "react";
@@ -12,17 +12,33 @@ import {
   groupLastHistoricTreatment,
 } from "scenes/Dashboard/components/Vulnerabilities/UpdateDescription/utils";
 import type { IHistoricTreatment } from "scenes/Dashboard/containers/DescriptionView/types";
+import { GET_FINDING_HEADER } from "scenes/Dashboard/containers/FindingContent/queries";
 import store from "store";
 import { authzPermissionsContext } from "utils/authz/config";
+import { msgSuccess } from "utils/notifications";
 import waitForExpect from "wait-for-expect";
+import { EditableField } from "../../EditableField";
+import { GET_VULNERABILITIES } from "../queries";
+import { REQUEST_ZERO_RISK_VULN } from "./queries";
+
+jest.mock("../../../../../utils/notifications", () => {
+  const mockedNotifications: Dictionary = jest.requireActual("../../../../../utils/notifications");
+  mockedNotifications.msgError = jest.fn();
+  mockedNotifications.msgSuccess = jest.fn();
+
+  return mockedNotifications;
+});
 
 describe("Update Description component", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
   const vulns: IVulnDataType[] = [
     {
       currentState: "",
       externalBts: "",
       historicTreatment: [],
-      id: "test",
+      id: "ab25380d-dfe1-4cde-aefd-acca6990d6aa",
       severity: "",
       specific: "",
       tag: "one",
@@ -30,7 +46,54 @@ describe("Update Description component", () => {
       where: "",
     },
   ];
+  const mocksVulns: MockedResponse = {
+    request: {
+      query: GET_VULNERABILITIES,
+      variables: {
+        analystField: false,
+        identifier: "422286126",
+      },
+    },
+    result: {
+      data: {
+        finding: {
+          btsUrl: "",
+          id: "ab25380d-dfe1-4cde-aefd-acca6990d6aa",
+          inputsVulns: [],
+          linesVulns: [],
+          portsVulns: [],
+          releaseDate: "",
+        },
+      },
+    },
+  };
+  const mocksFindingHeader: MockedResponse = {
+    request: {
+      query: GET_FINDING_HEADER,
+      variables: {
+        canGetExploit: false,
+        canGetHistoricState: false,
+        findingId: "422286126",
+      },
+    },
+    result: {
+      data: {
+        finding: {
+          closedVulns: 0,
+          id: "ab25380d-dfe1-4cde-aefd-acca6990d6aa",
+          openVulns: 0,
+          releaseDate: "",
+          reportDate: "",
+          severityScore: 1,
+          state: "default",
+          title: "",
+          tracking: [],
+        },
+      },
+    },
+  };
   const mockedPermissions: PureAbility<string> = new PureAbility([
+    { action: "backend_api_mutations_request_zero_risk_vuln_mutate" },
     { action: "backend_api_resolvers_vulnerability__do_update_treatment_vuln" },
     { action: "backend_api_mutations_update_vulns_treatment_mutate" },
   ]);
@@ -110,5 +173,71 @@ describe("Update Description component", () => {
           .toHaveLength(4);
       });
     });
+  });
+
+  it("should handle request zero risk", async () => {
+    const handleOnClose: jest.Mock = jest.fn();
+    const handleClearSelected: jest.Mock = jest.fn();
+    const mocksMutation: MockedResponse[] = [
+      {
+        request: {
+          query: REQUEST_ZERO_RISK_VULN,
+          variables: {
+            findingId: "422286126",
+            justification: "This is a commenting test of a request zero risk in vulns",
+            vulnerabilities: ["ab25380d-dfe1-4cde-aefd-acca6990d6aa"],
+          },
+        },
+        result: { data: { requestZeroRiskVuln : { success: true } } },
+      },
+      mocksVulns,
+      mocksFindingHeader,
+    ];
+    const wrapperRequest: ReactWrapper = mount(
+      <Provider store={store}>
+        <MockedProvider mocks={mocksMutation} addTypename={false}>
+          <authzPermissionsContext.Provider value={mockedPermissions}>
+            <UpdateTreatmentModal
+              findingId="422286126"
+              vulnerabilities={vulns}
+              vulnerabilitiesChunk={1}
+              handleClearSelected={handleClearSelected}
+              handleCloseModal={handleOnClose}
+            />
+          </authzPermissionsContext.Provider>
+        </MockedProvider>
+      </Provider>,
+    );
+
+    const treatmentFieldSelect: ReactWrapper = wrapperRequest
+      .find(EditableField)
+      .filter({ name: "treatment" })
+      .find("select");
+    treatmentFieldSelect.simulate("change", { target: { value: "ZERO_RISK" }});
+
+    const justificationFieldTextArea: ReactWrapper = wrapperRequest
+      .find(EditableField)
+      .filter({ name: "justification" })
+      .find("textarea");
+    justificationFieldTextArea.simulate(
+      "change",
+      { target: { value: "This is a commenting test of a request zero risk in vulns" } },
+    );
+    await act(async () => { await wait(0); wrapperRequest.update(); });
+
+    const form: ReactWrapper = wrapperRequest.find("form");
+    form
+      .at(0)
+      .simulate("submit");
+    await act(async () => { await wait(0); wrapperRequest.update(); });
+
+    expect(wrapperRequest)
+      .toHaveLength(1);
+    expect(handleClearSelected)
+      .toHaveBeenCalled();
+    expect(handleOnClose)
+      .toHaveBeenCalled();
+    expect(msgSuccess)
+      .toHaveBeenCalled();
   });
 });
