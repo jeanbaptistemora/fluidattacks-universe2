f922b0c00f39d60e8bf8e8a68c40eed7d0a26ae0
David Acevedo <dacevedo@fluidattacks.com>
2020-12-14 19:17:36 -0500

    integrates\refac(back): #2617.55 migrate git filter
    - added migration file for feedback
    
    
    Former-commit-id: 12a9aa1b049d9c463b63f9f7751a11dc72bc0a73

---
+++ integrates/backend_new/migrations/0043_migrate_git_filter.py
@@ -0,0 +1,82 @@
+# /usr/bin/env python3
+# -.- coding: utf-8 -.-
+"""
+This migration copies filter configs to a new format in git roots
+
+Execution Time:
+Finalization Time:
+"""
+# Standard
+import os
+from typing import Any, Dict, List
+
+# Third party
+from aioextensions import collect, run
+
+# Local
+from backend.dal import root as root_dal
+
+
+STAGE: str = os.environ['STAGE']
+SERVICES_REPO_DIR: str = f'{os.getcwd()}/services'
+
+
+def format_new_filter(filter_config: Dict[str, Any]) -> Dict[str, List[str]]:
+    if filter_config:
+        if filter_config['policy'] == 'EXCLUDE':
+            return {
+                **filter_config,
+                'exclude': filter_config['paths'],
+                'include': ['^.*$']
+            }
+
+        # INCLUDE
+        return {
+            **filter_config,
+            'exclude': [],
+            'include': filter_config['paths']
+        }
+
+    # NONE
+    return {'exclude': [], 'include': ['^.*$']}
+
+
+async def update_filter(
+    group_name: str,
+    root: Dict[str, Any]
+) -> None:
+    states_to_update = [
+        {
+            **state,
+            'filter': format_new_filter(state.get('filter'))
+        }
+        for state in root['historic_state']
+    ]
+
+    if STAGE == 'test':
+        print('[INFO] Will migrate', len(states_to_update), 'for', root['sk'])
+        print(states_to_update)
+    else:
+        await root_dal.update(
+            group_name,
+            root['sk'],
+            {'historic_state': states_to_update}
+        )
+
+
+async def main() -> None:
+    groups: List[str] = os.listdir(os.path.join(SERVICES_REPO_DIR, 'groups'))
+    print(f'[INFO] Found {len(groups)} groups')
+
+    for group_name in groups:
+        print(f'[INFO] Working on {group_name}')
+        roots = await root_dal.get_roots_by_group(group_name)
+
+        await collect(
+            update_filter(group_name, root)
+            for root in roots
+        )
+
+
+if __name__ == '__main__':
+    run(main())
