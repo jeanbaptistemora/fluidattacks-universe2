947c5d6b283b36c13aa013b280d1e14dd0b43571
Andres Aguirre <aaguirre@fluidattacks.com>
2020-12-15 12:51:30 -0500

    integrates\refac(front): #3781.7 update description queries
    - Refactored queries of update description modal to eslint
    
    
    Former-commit-id: 79c701652e24853c399508f20453dbd68f374c05

--- integrates/front/src/scenes/Dashboard/components/Vulnerabilities/UpdateDescription/queries.ts
+++ integrates/front/src/scenes/Dashboard/components/Vulnerabilities/UpdateDescription/queries.ts
@@ -1,55 +1,76 @@
-import { DocumentNode } from "graphql";
+import type { DocumentNode } from "graphql";
 import gql from "graphql-tag";
 
 const UPDATE_DESCRIPTION_MUTATION: DocumentNode = gql`
   mutation UpdateTreatmentVulnMutation(
-    $findingId: String!,
-    $severity: Int,
+    $findingId: String!
+    $severity: Int
     $tag: String
-    $treatmentManager: String,
-    $vulnerabilities: [String]!,
-    $externalBts: String!,
-    $acceptanceDate: String,
-    $justification: String!,
+    $treatmentManager: String
+    $vulnerabilities: [String]!
+    $externalBts: String!
+    $acceptanceDate: String
+    $justification: String!
     $treatment: UpdateClientDescriptionTreatment!
-    $isVulnTreatmentChanged: Boolean!,
-    $isVulnInfoChanged: Boolean!,
+    $isVulnTreatmentChanged: Boolean!
+    $isVulnInfoChanged: Boolean!
   ) {
-    updateTreatmentVuln (
-      externalBts: $externalBts,
-      findingId: $findingId,
-      severity: $severity,
-      tag: $tag,
-      vulnerabilities: $vulnerabilities,
-    ) @include (if: $isVulnInfoChanged) {
+    updateTreatmentVuln(
+      externalBts: $externalBts
+      findingId: $findingId
+      severity: $severity
+      tag: $tag
+      vulnerabilities: $vulnerabilities
+    ) @include(if: $isVulnInfoChanged) {
       success
     }
     updateVulnsTreatment(
-      acceptanceDate: $acceptanceDate,
-      findingId: $findingId,
-      justification: $justification,
-      treatment: $treatment,
-      treatmentManager: $treatmentManager,
-      vulnerabilities: $vulnerabilities,
-    ) @include (if: $isVulnTreatmentChanged) {
+      acceptanceDate: $acceptanceDate
+      findingId: $findingId
+      justification: $justification
+      treatment: $treatment
+      treatmentManager: $treatmentManager
+      vulnerabilities: $vulnerabilities
+    ) @include(if: $isVulnTreatmentChanged) {
       success
     }
   }
 `;
 
 const DELETE_TAGS_MUTATION: DocumentNode = gql`
-  mutation DeleteTagsVuln ($findingId: String!, $tag: String, $vulnerabilities: [String]!){
-    deleteTags(findingId: $findingId, tag: $tag, vulnerabilities: $vulnerabilities) {
+  mutation DeleteTagsVuln(
+    $findingId: String!
+    $tag: String
+    $vulnerabilities: [String]!
+  ) {
+    deleteTags(
+      findingId: $findingId
+      tag: $tag
+      vulnerabilities: $vulnerabilities
+    ) {
       success
     }
   }
 `;
 
 const REQUEST_ZERO_RISK_VULN: DocumentNode = gql`
-mutation RequestZeroRiskVuln ($findingId: String!, $justification: String!, $vulnerabilities: [String]!){
-  requestZeroRiskVuln(findingId: $findingId, justification: $justification, vulnerabilities: $vulnerabilities) {
-    success
+  mutation RequestZeroRiskVuln(
+    $findingId: String!
+    $justification: String!
+    $vulnerabilities: [String]!
+  ) {
+    requestZeroRiskVuln(
+      findingId: $findingId
+      justification: $justification
+      vulnerabilities: $vulnerabilities
+    ) {
+      success
+    }
   }
-}`;
+`;
 
-export { DELETE_TAGS_MUTATION, REQUEST_ZERO_RISK_VULN, UPDATE_DESCRIPTION_MUTATION };
+export {
+  DELETE_TAGS_MUTATION,
+  REQUEST_ZERO_RISK_VULN,
+  UPDATE_DESCRIPTION_MUTATION,
+};
