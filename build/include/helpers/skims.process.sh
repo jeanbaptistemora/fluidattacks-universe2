390179078293775e70f0c472493a75533fe9f601
Kevin Amado <kamado@fluidattacks.com>
2020-12-18 10:07:52 -0500

    skims\feat(build): #3827.7 per repo command
    Former-commit-id: ead2ab654dc43375ded204942b28706995f39878

--- build/include/helpers/skims.process.sh
+++ build/include/helpers/skims.process.sh
@@ -1,7 +1,7 @@
 # shellcheck shell=bash
 
 function helper_skims_process_group {
-  local success
+  local success='true'
 
   # Make sure these exports are declared in `skims_process_group_on_aws`
   export GITLAB_API_TOKEN
@@ -15,33 +15,35 @@ function helper_skims_process_group {
       helper_common_use_services \
   &&  helper_skims_aws_login prod \
   &&  echo '[INFO] Cloning repositories' \
-  &&  { helper_common_pull_services_repositories "${group}" ||  true; } \
-  &&  if ! test -e "groups/${group}/fusion"
-      then
-            echo '[WARNING] No repositories to test' \
-        &&  return 0
-      fi \
-  &&  echo '[INFO] Running skims:' \
-  &&  helper_skims_get_group_config_as_json "${group}" \
-        | helper_common_json_to_yaml \
-        | tee "${TEMP_FILE1}" \
-  &&  helper_skims_pull_cache "${group}" \
-  &&  if "${product}/bin/skims" --group "${group}" "${TEMP_FILE1}"
-      then
-            echo "[INFO] Succesfully processed: ${group}" \
-        &&  success='true'
-      else
-            echo "[ERROR] While running skims on: ${group}" \
-        &&  success='false'
-      fi \
+  &&  { helper_common_pull_services_repositories "${group}" || true; } \
+  &&  shopt -s nullglob \
+  &&  for namespace in "groups/${group}/fusion/"*
+      do
+            namespace="$(basename "${namespace}")" \
+        &&  echo "[INFO] Running skims: ${group} ${namespace}" \
+        &&  helper_skims_get_group_config_as_json "${group}" "${namespace}" \
+              | helper_common_json_to_yaml \
+              | tee "${TEMP_FILE1}" \
+        &&  helper_skims_pull_cache "${group}" "${namespace}" \
+        &&  if "${product}/bin/skims" "${TEMP_FILE1}"
+            then
+              echo "[INFO] Succesfully processed: ${group} ${namespace}"
+            else
+                  echo "[ERROR] While running skims on: ${group} ${namespace}" \
+              &&  success='false'
+            fi \
+        &&  helper_skims_push_cache "${group}" "${namespace}" \
+        ||  continue
+      done \
+  &&  shopt -u nullglob \
   &&  helper_common_remove_services_repositories "${group}" \
-  &&  helper_skims_push_cache "${group}" \
   &&  test "${success}" = 'true'
 }
 
 function helper_skims_pull_cache {
   local group="${1}"
-  local source="s3://skims.data/cache/${group}"
+  local namespace="${2}"
+  local source="s3://skims.data/cache/${group}/${namespace}"
   local target="${HOME}/.skims"
 
       echo "[INFO] Moving skims state from ${source} to ${target}" \
@@ -51,7 +53,8 @@ function helper_skims_pull_cache {
 
 function helper_skims_push_cache {
   local group="${1}"
-  local target="s3://skims.data/cache/${group}"
+  local namespace="${2}"
+  local source="s3://skims.data/cache/${group}/${namespace}"
   local source="${HOME}/.skims"
 
       echo "[INFO] Moving skims state from ${source} to ${target}" \
@@ -61,14 +64,15 @@ function helper_skims_push_cache {
 
 function helper_skims_get_group_config_as_json {
   local group="${1}"
+  local namespace="${2}"
 
   jq -e -n -r \
-    --arg 'language' "$(
-        yq -r .language < "groups/${group}/config/config.yml"
-      )" \
-    --arg 'working_dir' "groups/${group}/fusion" \
+    --arg 'language' "$(yq -r .language < "groups/${group}/config/config.yml")" \
+    --arg 'namespace' "${namespace}" \
+    --arg 'working_dir' "groups/${group}/fusion/${namespace}" \
     '{
       language: $language,
+      namespace: $namespace,
       path: {
         include: [
           "glob(*)"
