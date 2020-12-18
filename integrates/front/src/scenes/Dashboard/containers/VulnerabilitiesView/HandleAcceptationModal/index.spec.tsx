eddc30b0098be822af51189c46ab13d97430b9b4
Andres Aguirre <aaguirre@fluidattacks.com>
2020-12-18 14:49:12 -0500

    integrates\refac(front): #3782.1 handle acceptation tests
    - Refactored unittests of handle acceptation modal to eslint
    
    
    Former-commit-id: 5bb3dcf47ec06a1b7035a0c18234b60b3cf16d94

--- integrates/front/src/scenes/Dashboard/containers/VulnerabilitiesView/HandleAcceptationModal/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/containers/VulnerabilitiesView/HandleAcceptationModal/index.spec.tsx
@@ -1,33 +1,37 @@
-import { MockedProvider, MockedResponse } from "@apollo/react-testing";
-import { mount, ReactWrapper } from "enzyme";
+import { GET_VULNERABILITIES } from "scenes/Dashboard/components/Vulnerabilities/queries";
 import { GraphQLError } from "graphql";
-import React from "react";
-// tslint:disable-next-line: no-submodule-imports
-import { act } from "react-dom/test-utils";
+import { HANDLE_VULNS_ACCEPTATION } from "scenes/Dashboard/containers/VulnerabilitiesView/HandleAcceptationModal/queries";
+import { HandleAcceptationModal } from "scenes/Dashboard/containers/VulnerabilitiesView/HandleAcceptationModal/index";
+import { MockedProvider } from "@apollo/react-testing";
+import type { MockedResponse } from "@apollo/react-testing";
 import { Provider } from "react-redux";
+import React from "react";
+import type { ReactWrapper } from "enzyme";
+import { act } from "react-dom/test-utils";
+import { mount } from "enzyme";
 import store from "store";
 import waitForExpect from "wait-for-expect";
-
-import { GET_VULNERABILITIES } from "scenes/Dashboard/components/Vulnerabilities/queries";
-import { HandleAcceptationModal } from "scenes/Dashboard/containers/VulnerabilitiesView/HandleAcceptationModal/index";
-import {
-  HANDLE_VULNS_ACCEPTATION,
-} from "scenes/Dashboard/containers/VulnerabilitiesView/HandleAcceptationModal/queries";
 import { msgError, msgSuccess } from "utils/notifications";
 
-jest.mock("../../../../../utils/notifications", () => {
-  const mockedNotifications: Dictionary = jest.requireActual("../../../../../utils/notifications");
-  mockedNotifications.msgError = jest.fn();
-  mockedNotifications.msgSuccess = jest.fn();
+jest.mock(
+  "../../../../../utils/notifications",
+  (): Dictionary => {
+    const mockedNotifications: Dictionary<() => Dictionary> = jest.requireActual(
+      "../../../../../utils/notifications"
+    );
+    jest.spyOn(mockedNotifications, "msgError").mockImplementation();
+    jest.spyOn(mockedNotifications, "msgSuccess").mockImplementation();
 
-  return mockedNotifications;
-});
+    return mockedNotifications;
+  }
+);
 
-describe("handle vulns acceptation modal", () => {
-  afterEach(() => {
+describe("handle vulns acceptation modal", (): void => {
+  it("should handle vulns acceptation", async (): Promise<void> => {
+    expect.hasAssertions();
+
     jest.clearAllMocks();
-  });
-  it("should handle vulns acceptation", async () => {
+
     const handleOnClose: jest.Mock = jest.fn();
     const handleRefetchData: jest.Mock = jest.fn();
     const mocksMutation: MockedResponse[] = [
@@ -66,48 +70,61 @@ describe("handle vulns acceptation modal", () => {
     ];
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
-        <MockedProvider mocks={mocksMutation} addTypename={false}>
+        <MockedProvider addTypename={false} mocks={mocksMutation}>
           <HandleAcceptationModal
             findingId={"1"}
-            vulns={[{acceptation: "APPROVED", id: "test", specific: "", where: ""}]}
-            refetchData={handleRefetchData}
             handleCloseModal={handleOnClose}
+            refetchData={handleRefetchData}
+            vulns={[
+              { acceptation: "APPROVED", id: "test", specific: "", where: "" },
+            ]}
           />
         </MockedProvider>
-      </Provider>,
+      </Provider>
     );
-    await act(async () => {
-      await waitForExpect(() => {
-        wrapper.update();
-        expect(wrapper)
-          .toHaveLength(1);
-      });
-    });
+    await act(
+      async (): Promise<void> => {
+        await waitForExpect((): void => {
+          wrapper.update();
+
+          expect(wrapper).toHaveLength(1);
+        });
+      }
+    );
     const justification: ReactWrapper = wrapper.find("textarea");
-    justification.simulate("change", { target: { value: "This is a justification test" } });
-    const switchButton: ReactWrapper = wrapper.find("BootstrapTable")
+    justification.simulate("change", {
+      target: { value: "This is a justification test" },
+    });
+    const switchButton: ReactWrapper = wrapper
+      .find("BootstrapTable")
       .find("e")
       .find("div")
       .first();
     switchButton.simulate("click");
     const form: ReactWrapper = wrapper.find("form");
-    form.at(0)
-      .simulate("submit");
+    form.at(0).simulate("submit");
 
-    await act(async () => {
-      await waitForExpect(() => {
-        wrapper.update();
-        expect(msgSuccess)
-          .toHaveBeenCalled();
-        expect(handleRefetchData)
-        .toHaveBeenCalled();
-        expect(handleOnClose)
-          .toHaveBeenCalled();
-      });
-    });
+    await act(
+      async (): Promise<void> => {
+        await waitForExpect((): void => {
+          wrapper.update();
+
+          expect(msgSuccess).toHaveBeenCalledWith(
+            "Indefinite acceptation has been handled",
+            "Correct!"
+          );
+          expect(handleRefetchData).toHaveBeenCalledWith();
+          expect(handleOnClose).toHaveBeenCalledWith();
+        });
+      }
+    );
   });
 
-  it("should handle vulns acceptation errors", async () => {
+  it("should handle vulns acceptation errors", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    jest.clearAllMocks();
+
     const handleRefetchData: jest.Mock = jest.fn();
     const mocksMutation: MockedResponse[] = [
       {
@@ -122,7 +139,9 @@ describe("handle vulns acceptation modal", () => {
         },
         result: {
           errors: [
-            new GraphQLError("Exception - It cant handle acceptation without being requested"),
+            new GraphQLError(
+              "Exception - It cant handle acceptation without being requested"
+            ),
             new GraphQLError("Exception - Vulnerability not found"),
             new GraphQLError("Unexpected error"),
           ],
@@ -131,37 +150,48 @@ describe("handle vulns acceptation modal", () => {
     ];
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
-        <MockedProvider mocks={mocksMutation} addTypename={false}>
+        <MockedProvider addTypename={false} mocks={mocksMutation}>
           <HandleAcceptationModal
             findingId={"1"}
-            vulns={[{acceptation: "APPROVED", id: "test_error", specific: "", where: ""}]}
-            refetchData={jest.fn()}
             handleCloseModal={jest.fn()}
+            refetchData={jest.fn()}
+            vulns={[
+              {
+                acceptation: "APPROVED",
+                id: "test_error",
+                specific: "",
+                where: "",
+              },
+            ]}
           />
         </MockedProvider>
-      </Provider>,
+      </Provider>
     );
-    await act(async () => {
-      await waitForExpect(() => {
-        wrapper.update();
-        expect(wrapper)
-          .toHaveLength(1);
-      });
-    });
+    await act(
+      async (): Promise<void> => {
+        await waitForExpect((): void => {
+          wrapper.update();
+
+          expect(wrapper).toHaveLength(1);
+        });
+      }
+    );
     const justification: ReactWrapper = wrapper.find("textarea");
-    justification.simulate("change", { target: { value: "This is a justification test error" } });
-    const form: ReactWrapper = wrapper.find("form");
-    form.at(0)
-      .simulate("submit");
-    await act(async () => {
-      await waitForExpect(() => {
-        wrapper.update();
-        expect(handleRefetchData)
-          .not
-          .toHaveBeenCalled();
-        expect(msgError)
-          .toBeCalledTimes(3);
-      });
+    justification.simulate("change", {
+      target: { value: "This is a justification test error" },
     });
+    const form: ReactWrapper = wrapper.find("form");
+    form.at(0).simulate("submit");
+    await act(
+      async (): Promise<void> => {
+        await waitForExpect((): void => {
+          wrapper.update();
+          const expectedErrorMsgs: number = 3;
+
+          expect(handleRefetchData).not.toHaveBeenCalled();
+          expect(msgError).toHaveBeenCalledTimes(expectedErrorMsgs);
+        });
+      }
+    );
   });
 });
