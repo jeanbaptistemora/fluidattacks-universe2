c98423f0c11536d74f3e46f102d60dc81a284474
Daniel Salazar <dsalazar@fluidattacks.com>
2020-12-22 14:39:22 -0500

    airs\fix(infra): #3811.21 point to website endpoint
    - Use website endpoint in order to allow proper site load
    
    
    Former-commit-id: a7bbba6c18bdad1df08d050a82d94ea7eac022a5

--- airs/deploy/ephemeral/terraform/dns.tf
+++ airs/deploy/ephemeral/terraform/dns.tf
@@ -13,7 +13,7 @@ resource "cloudflare_record" "web-ephemeral-alias" {
   zone_id = lookup(data.cloudflare_zones.fluidattacks_com.zones[0], "id")
   name    = "web.eph.${lookup(data.cloudflare_zones.fluidattacks_com.zones[0], "name")}"
   type    = "CNAME"
-  value   = aws_s3_bucket.web-ephemeral-bucket.bucket_domain_name
+  value   = aws_s3_bucket.web-ephemeral-bucket.website_endpoint
   proxied = true
   ttl     = 1
 }
--- airs/deploy/production/terraform/dns.tf
+++ airs/deploy/production/terraform/dns.tf
@@ -13,7 +13,7 @@ resource "cloudflare_record" "fluidattacks_com" {
   zone_id = lookup(data.cloudflare_zones.fluidattacks_com.zones[0], "id")
   name    = lookup(data.cloudflare_zones.fluidattacks_com.zones[0], "name")
   type    = "CNAME"
-  value   = aws_s3_bucket.bucket.bucket_domain_name
+  value   = aws_s3_bucket.bucket.website_endpoint
   proxied = true
   ttl     = 1
 }
