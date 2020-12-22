f6198cf475d9a0b370096c61cfa7850d8b1eb05e
Daniel Salazar <dsalazar@fluidattacks.com>
2020-12-22 17:12:13 -0500

    airs\feat(infra): #3811.22 encrypt buckets
    - Encrypt production and development buckets
    
    
    Former-commit-id: 1c44fd8405cb957696557d18f4f9fd69e87b30b2

--- airs/deploy/ephemeral/terraform/bucket.tf
+++ airs/deploy/ephemeral/terraform/bucket.tf
@@ -3,6 +3,14 @@ resource "aws_s3_bucket" "web-ephemeral-bucket" {
   acl    = "private"
   region = var.region
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   website {
      index_document = "index.html"
      error_document = "error-index.html"
