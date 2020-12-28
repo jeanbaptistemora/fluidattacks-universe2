8186f661165c11d1e7cb2782c576d9c60bb00998
Kevin Amado <kamado@fluidattacks.com>
2020-12-28 11:45:51 -0500

    common\feat(build): #3836.42 use git workdir utility
    Former-commit-id: 9033798d1ea8a6e470d9bb765242c60056a80b41

---
+++ makes/utils/bash-lib/use-git-workdir/default.nix
@@ -0,0 +1,12 @@
+pkgs:
+
+let
+  makeTemplate = import ../../../../makes/utils/make-template pkgs;
+in
+  makeTemplate {
+    arguments = {
+      envGit = "${pkgs.git}/bin/git";
+    };
+    name = "utils-bash-lib-use-git-workdir";
+    template = ../../../../makes/utils/bash-lib/use-git-workdir/template.sh;
+  }
