b863fb38befe63f24d0ca7ccc6e77690ba355aee
Sebastian Lizcano <slizcano@fluidattacks.com>
2020-12-28 06:54:12 -0500

    integrates\refac(front): #3627.12 remove unnecessary code
    - remove unused query
    
    
    Former-commit-id: 082b9658c0717e9869887b873da4d9a82cfd4813

--- integrates/front/src/scenes/Dashboard/components/Vulnerabilities/queries.ts
+++ integrates/front/src/scenes/Dashboard/components/Vulnerabilities/queries.ts
@@ -1,50 +1,6 @@
 import { DocumentNode } from "graphql";
 import gql from "graphql-tag";
 
-export const GET_VULNERABILITIES: DocumentNode = gql`
-  query GetVulnerabilitiesQuery($identifier: String!, $analystField: Boolean!) {
-    finding(identifier: $identifier) {
-      id
-      releaseDate
-      portsVulns {
-        ...vulnInfo
-        analyst @include(if: $analystField)
-      }
-      linesVulns {
-        ...vulnInfo
-        analyst @include(if: $analystField)
-      }
-      inputsVulns {
-        ...vulnInfo
-        analyst @include(if: $analystField)
-      }
-    }
-  }
-  fragment vulnInfo on Vulnerability {
-    vulnType
-    where
-    specific
-    currentState
-    historicTreatment {
-      acceptanceDate
-      acceptanceStatus
-      date
-      justification
-      user
-      treatment
-      treatmentManager
-    }
-    id
-    externalBts
-    findingId
-    severity
-    tag
-    remediated
-    verification
-    zeroRisk
-  }
-  `;
-
 export const UPLOAD_VULNERABILITIES: DocumentNode = gql`
   mutation UploadVulnerabilites ($file: Upload!, $findingId: String!){
     uploadFile(findingId: $findingId, file: $file) {
