581b25a4978b637fa6b41c9ec9829d8871119149
Daniel Salazar <dsalazar@fluidattacks.com>
2020-12-28 11:55:53 -0500

    serves\feat(infra): #3810.40 cloduflare advanced cert
    - Create main advanced cert for all our endpoints
    
    
    Former-commit-id: 77168ea45e80ff15ff2eb906bf2bd3443b0f6c01

---
+++ serves/services/dns/terraform/certificates.tf
@@ -0,0 +1,21 @@
+resource "cloudflare_certificate_pack" "main" {
+  zone_id               = cloudflare_zone.fluidattacks_com.id
+  type                  = "advanced"
+  validation_method     = "txt"
+  validity_days         = 30
+  certificate_authority = "digicert"
+  cloudflare_branding   = false
+
+  hosts = [
+    cloudflare_zone.fluidattacks_com.zone,
+    "*.${cloudflare_zone.fluidattacks_com.zone}",
+    "*.integrates.${cloudflare_zone.fluidattacks_com.zone}",
+    "*.front.production.${cloudflare_zone.fluidattacks_com.zone}",
+    "*.front.development.${cloudflare_zone.fluidattacks_com.zone}",
+    "*.eph.${cloudflare_zone.fluidattacks_com.zone}",
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
