ee4b6d793fd02233f57337a59b7262677815e0d3
Kevin Amado <kamado@fluidattacks.com>
2020-12-28 13:13:02 -0500

    airs\refac(conf): #3864.1 delete property ua-22974464-4
    Former-commit-id: 33dd41ab807928553b64e1bc19e718ddbd997008

--- asserts/sphinx/source/conf.py
+++ asserts/sphinx/source/conf.py
@@ -43,10 +43,6 @@ def pygments_monkeypatch_style(mod_name, cls):
     pygments.styles.STYLE_MAP[mod_name] = mod_name + "::" + cls_name
 
 
-def setup(app):
-    app.add_javascript("https://www.googletagmanager.com/gtag/js?id=UA-22974464-4")
-    app.add_javascript("google-analytics.js")
-
 pygments_monkeypatch_style("Fluidattacks", Fluidattacks)
 
 # -- Project information -----------------------------------------------------
