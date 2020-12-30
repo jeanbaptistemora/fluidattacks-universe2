adc78c488db1a302ddd9b09badeb74857d069f7b
Kevin Amado <kamado@fluidattacks.com>
2020-12-30 15:42:30 -0500

    common\feat(build): #3836.51 check all arguments were used
    - This help us prevent bugs
    
    
    Former-commit-id: dd1eff21da8611253923f879dd73d35b617b2c1d

--- makes/utils/make-template/builder.sh
+++ makes/utils/make-template/builder.sh
@@ -6,9 +6,15 @@ function replace_var_in_file {
   local file="${1}"
   local var_name="${2}"
   local var_value="${!var_name}"
+  local var_name_tpl="__${var_name}__"
 
-      echo "[INFO] Replacing: __${var_name}__, with: ${var_value}" \
-  &&  sed -i "s|__${var_name}__|${var_value}|g" "${file}"
+      echo "[INFO] Replacing: ${var_name_tpl}, with: ${var_value}" \
+  &&  if ! grep --fixed-strings --quiet "${var_name_tpl}" "${file}"
+      then
+            echo "[ERROR] Argument is not being used: ${var_name_tpl}" \
+        &&  return 1
+      fi \
+  &&  sed -i "s|${var_name_tpl}|${var_value}|g" "${file}"
 }
 
 function main {
