45b1a3388d9a2379eba152c8a4f5b8c34d2fec12
Daniel Salazar <dsalazar@fluidattacks.com>
2021-01-04 12:37:13 -0500

    integrates\feat(cross): #3885.1 recover headers
    - Bring back old headers
    - Update front cloudflare provider
    
    
    Former-commit-id: 99b699e856cb4f5ad52b38ac1a047584596ff8cc

--- integrates/back/settings/uvicorn.py
+++ integrates/back/settings/uvicorn.py
@@ -5,4 +5,47 @@ from uvicorn.workers import UvicornWorker
 
 class IntegratesWorker(UvicornWorker):  # type: ignore
 
-    CONFIG_KWARGS = {'interface': 'asgi3'}
+    CONFIG_KWARGS = {
+        'interface': 'asgi3',
+        'headers': [
+            [
+                'Pragma',
+                'no-cache',
+            ],
+            [
+                'WWW-Authenticate',
+                'OAuth realm="Access to FLUIDIntegrates" charset="UTF-8"',
+            ],
+            [
+                'X-XSS-Protection',
+                '1; mode=block',
+            ],
+            [
+                'X-Permitted-Cross-Domain-Policies',
+                'master-only',
+            ],
+            [
+                'X-Content-Type-Options',
+                'nosniff',
+            ],
+            [
+                'Expires',
+                '0',
+            ],
+            [
+                'Content-Security-Policy',
+                "script-src 'self' 'unsafe-inline' 'unsafe-eval' "
+                'localhost:* *.amazonaws.com *.cloudfront.net '
+                '*.front.development.fluidattacks.com '
+                '*.front.production.fluidattacks.com '
+                '*.cloudflare.com *.cookiebot.com *.zdassets.com '
+                '*.newrelic.com *.mxpnl.com *.pingdom.net '
+                'bam.nr-data.net cdn.jsdelivr.net/npm/ '
+                'cdn.headwayapp.co *.cloudflareinsights.com;',
+            ],
+            [
+                'Accept-Encoding',
+                'identity',
+            ],
+        ],
+    }
--- integrates/deploy/front/terraform/main.tf
+++ integrates/deploy/front/terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 2.14.0"
+      version = "~> 2.15.0"
     }
   }
 
