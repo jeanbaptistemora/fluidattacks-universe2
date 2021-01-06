b6488efef837653c957ca2994308e5740dc32ef0
John Perez <jperez@fluidattacks.com>
2021-01-06 12:39:53 -0500

    airs\refac(front): #3905.4 add plugins
    - Added plugins to gatsby-config and new packages
    - Remove unnecessary packages
    
    
    Former-commit-id: bcad93c6ea11f8edc14bf252f5d552513e3d4c40

--- airs/new-front/gatsby-config.js
+++ airs/new-front/gatsby-config.js
@@ -8,7 +8,78 @@ module.exports = {
     image: "/images/fluid-attacks-logo.png", // Path to your image you placed in the 'static' folder
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/pages`,
+        name: `pages`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/images`,
+        name: `images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://fluidattacks.com`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-PCDDL8T",
+
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+      },
+    },
+    {
+      resolve: `gatsby-transformer-asciidoc`,
+      options: {
+        attributes: {
+          showtitle: true,
+        },
+        fileExtensions: [`adoc`],
+      },
+    },
+    {
+      resolve: `gatsby-transformer-asciidoc`,
+      options: {
+        attributes: {
+          imagesdir: `/content/images`,
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-typescript`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sass`,
   ],
 }
