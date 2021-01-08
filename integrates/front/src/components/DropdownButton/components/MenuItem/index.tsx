49787c1a06ac38d56551f79da040b647d6326b09
Frank Aristizabal <faristizabal@fluidattacks.com>
2021-01-08 14:01:50 -0500

    integrates\refac(front): #793.116 update dropdownbtn
    - Add styles to new dropdownbtn.
    - Add menuitem component with styled component.
    
    
    Former-commit-id: 4c72bb1e50ea9ac18fa8c0908ab0e4750b8e13cc

---
+++ integrates/front/src/components/DropdownButton/components/MenuItem/index.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/forbid-component-props, react/jsx-props-no-spreading
+  --------
+  We need className to override default styles and props spreading in
+  order to pass down props to react-bootstrap DropdownButton.
+*/
+import React from "react";
+import type { StyledComponent } from "styled-components";
+import style from "components/DropdownButton/index.css";
+import styled from "styled-components";
+
+interface IMenuItemsProps {
+  eventKey: string;
+  onClick: (eventKey: string) => void;
+  itemContent: React.ReactNode;
+}
+
+const StyledMenuItem: StyledComponent<
+  "button",
+  Record<string, unknown>
+> = styled.button.attrs({
+  className: "w-100 flex ba br0 pa3 mt2 outline-0 justify-between items-center",
+})``;
+
+const MenuItem: React.FC<IMenuItemsProps> = (
+  props: Readonly<IMenuItemsProps>
+): JSX.Element => {
+  const { itemContent, eventKey, onClick } = props;
+
+  function onClick1(): void {
+    onClick(eventKey);
+  }
+
+  return (
+    <StyledMenuItem className={style.menuItem} onClick={onClick1}>
+      {itemContent}
+    </StyledMenuItem>
+  );
+};
+
+export { MenuItem };
