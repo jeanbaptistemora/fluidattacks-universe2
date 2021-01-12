0bd36fba21eb315f7f10e030103f52555247e59e
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-01-12 16:20:59 -0500

    integrates\feat(back): #3847.2 add portfolio generator
    - add portfolio generator bar chart for vulnerabilities by level
    and vulnerabilities by tag
    
    
    Former-commit-id: bb88078e642a3f0d6957ef490134e7a919c031ae

---
+++ integrates/analytics/generators/bar_chart/vulnerabilities_by_level.py
@@ -0,0 +1,87 @@
+# Standard library
+from collections import Counter
+from itertools import chain
+from operator import itemgetter
+from typing import List
+
+# Third party libraries
+from aioextensions import (
+    collect,
+    run,
+)
+from async_lru import alru_cache
+from backend.api.dataloaders.project import ProjectLoader as GroupLoader
+from backend.api.dataloaders.finding_vulns import FindingVulnsLoader
+
+# Local libraries
+from analytics import utils
+from analytics.colors import RISK
+
+
+@alru_cache(maxsize=None, typed=True)
+async def get_data_one_group(group: str) -> Counter:
+    group_data = await GroupLoader().load(group.lower())
+    vulnerabilities = list(
+        chain.from_iterable(
+            await FindingVulnsLoader().load_many(group_data['findings'])
+        )
+    )
+
+    return Counter(filter(None, map(itemgetter('severity'), vulnerabilities)))
+
+
+async def get_data_many_groups(groups: List[str]) -> Counter:
+    groups_data = await collect(map(get_data_one_group, groups))
+
+    return sum(groups_data, Counter())
+
+
+def format_data(counters: Counter) -> dict:
+    data = counters.most_common()[:12]
+
+    return dict(
+        data=dict(
+            columns=[
+                ['Level'] + [value for _, value in data],
+            ],
+            colors={
+                'Level': RISK.neutral,
+            },
+            type='bar',
+        ),
+        legend=dict(
+            position='bottom',
+        ),
+        axis=dict(
+            x=dict(
+                categories=[str(key) for key, _ in data],
+                type='category',
+                tick=dict(
+                    rotate=utils.TICK_ROTATION,
+                    multiline=False,
+                ),
+            ),
+            y=dict(
+                min=0,
+                padding=dict(
+                    bottom=0,
+                ),
+            ),
+        ),
+    )
+
+
+async def generate_all():
+    async for org_id, org_name, _ in utils.iterate_organizations_and_groups():
+        for portfolio, groups in await utils.get_portfolios_groups(org_name):
+            utils.json_dump(
+                document=format_data(
+                    counters=await get_data_many_groups(groups),
+                ),
+                entity='portfolio',
+                subject=f'{org_id}PORTFOLIO#{portfolio}',
+            )
+
+
+if __name__ == '__main__':
+    run(generate_all())
