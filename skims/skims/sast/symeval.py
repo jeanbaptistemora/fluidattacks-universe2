778264d19233e629cc74cd29838c46df40948685
Kevin Amado <kamado@fluidattacks.com>
2021-01-15 12:31:37 -0500

    skims\feat(back): #3917.34 cross file evaluator
    - Add the skeleton of an evaluator that
      is able to lookup symbols in another
      shards of the graph
    - Some functions are pending to be implemented
    
    
    Former-commit-id: 637b6d42dbb1f18c971bcef8a35e18fcc554a765

---
+++ skims/skims/sast/symeval.py
@@ -0,0 +1,100 @@
+# Standard library
+from copy import (
+    deepcopy,
+)
+from typing import (
+    Tuple,
+)
+from model import (
+    graph_model,
+)
+from utils import (
+    graph as g,
+)
+from utils.logs import (
+    log_blocking,
+)
+
+
+class StopEvaluation(Exception):
+    pass
+
+
+def eval_syntax_steps(
+    _: graph_model.GraphDB,
+    shard: graph_model.GraphShard,
+    n_id: graph_model.NId,
+) -> graph_model.SyntaxSteps:
+    if n_id not in shard.syntax:
+        raise StopEvaluation()
+
+    syntax_steps = deepcopy(shard.syntax[n_id])
+
+    # Mark steps as dangerous or not
+    #
+    # If needed this can extend the syntax steps to be longer
+    # for instance in a method invocation
+    #
+    # Pending to implement
+
+    return syntax_steps
+
+
+def get_syntax_steps_from_path(
+    graph_db: graph_model.GraphDB,
+    shard: graph_model.GraphShard,
+    path: Tuple[str, ...],
+) -> graph_model.SyntaxSteps:
+    syntax_steps: graph_model.SyntaxSteps = []
+
+    for n_id in path:
+        try:
+            syntax_steps.extend(eval_syntax_steps(
+                _=graph_db,
+                shard=shard,
+                n_id=n_id,
+            ))
+        except StopEvaluation:
+            log_blocking('debug', 'Missing readers, %s @ %s', shard.path, n_id)
+            break
+
+    return syntax_steps
+
+
+def from_untrusted_n_id_to_dangerous_action_node(
+    graph_db: graph_model.GraphDB,
+    shard: graph_model.GraphShard,
+    *,
+    untrusted_n_id: graph_model.NId,
+) -> graph_model.GraphShardNodes:
+    # Find paths from this node to all CFG connected leaf-nodes
+    for _, path in g.branches_cfg(shard.graph, untrusted_n_id):
+        syntax_steps = get_syntax_steps_from_path(graph_db, shard, path)
+
+        # Check here if the syntax steps returned contain danger in the
+        # dangerous_action_node. Temporarily never happens
+        if '__never__' in syntax_steps:
+            yield shard, g.ROOT_NODE
+
+
+def from_untrusted_node_to_dangerous_action_node(
+    graph_db: graph_model.GraphDB,
+    *,
+    untrusted_node: graph_model.GraphUntrustedNode,
+) -> graph_model.GraphShardNodes:
+    # Start the evaluation from the untrusted nodes, in any shard
+    for shard in graph_db.shards:
+        for untrusted_n_id in g.filter_nodes(
+            shard.graph,
+            shard.graph.nodes,
+            predicate=g.pred_has_labels(label_input_type=untrusted_node.value),
+        ):
+            yield from from_untrusted_n_id_to_dangerous_action_node(
+                graph_db,
+                shard=shard,
+                # We start evaluation at the CFG-connected nodes
+                untrusted_n_id=g.lookup_first_cfg_parent(
+                    shard.graph,
+                    untrusted_n_id,
+                ),
+            )
--- skims/skims/utils/graph.py
+++ skims/skims/utils/graph.py
@@ -296,43 +296,51 @@ def get_ast_childs(
     )
 
 
+def lookup_first_cfg_parent(
+    graph: Graph,
+    n_id: NId,
+) -> str:
+    # Has child/parent CFG edges
+    if adj_cfg(graph, n_id) or pred_cfg(graph, n_id):
+        return n_id
+
+    # Lookup first parent who has parent CFG edges
+    for p_id in pred_ast_lazy(graph, n_id, depth=-1):
+        if pred_cfg(graph, p_id):
+            return p_id
+
+    # Base case, pass through
+    return n_id
+
+
 def flows(
     graph: Graph,
     *,
     input_type: str,
     sink_type: str,
 ) -> Tuple[Tuple[int, Tuple[str, ...]], ...]:
-
-    def lookup_first_cfg_parent(n_id: str) -> str:
-        # Has child/parent CFG edges
-        if adj_cfg(graph, n_id) or pred_cfg(graph, n_id):
-            return n_id
-
-        # Lookup first parent who has parent CFG edges
-        for p_id in pred_ast_lazy(graph, n_id, depth=-1):
-            if pred_cfg(graph, p_id):
-                return p_id
-
-        # Base case, pass through
-        return n_id
-
     return tuple(enumerate(sorted(
         path
         for s_id, t_id in product(
             # Inputs
-            map(lookup_first_cfg_parent, filter_nodes(
+            filter_nodes(
                 graph,
                 graph.nodes,
                 pred_has_labels(label_input_type=input_type),
-            )),
+            ),
             # Sinks
-            map(lookup_first_cfg_parent, filter_nodes(
+            filter_nodes(
                 graph,
                 graph.nodes,
                 pred_has_labels(label_sink_type=sink_type),
-            )),
+            ),
         )
-        for path in paths(graph, s_id, t_id, label_cfg='CFG')
+        for path in paths(
+            graph,
+            lookup_first_cfg_parent(graph, s_id),
+            lookup_first_cfg_parent(graph, t_id),
+            label_cfg='CFG',
+        )
     )))
 
 
