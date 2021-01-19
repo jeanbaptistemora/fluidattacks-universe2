c4dcc2491deabf44c406bdb5b08b0a015aee7c75
David Acevedo <dacevedo@fluidattacks.com>
2021-01-19 11:44:09 -0500

    integrates\refac(back): #3978.2 cleanup script
    Former-commit-id: ea736d821adfe1ef7c31acf5ab1ceb5936edfb73

---
+++ integrates/back/migrations/0059_remove_old_resources_data.py
@@ -0,0 +1,53 @@
+"""
+This migration aims to remove old repos and envs data
+now that they've been replaced by git roots (new scope module)
+
+Execution Time:
+Finalization Time:
+"""
+# Standard library
+import os
+import time
+
+# Third party libraries
+from aioextensions import (
+    collect,
+    run
+)
+
+# Local libraries
+from backend.dal import project as group_dal
+
+
+STAGE = os.environ['STAGE']
+
+
+async def remove_old_data(group_name: str) -> None:
+    if STAGE == 'test':
+        print('[INFO] Will clean', group_name)
+    else:
+        await group_dal.update(
+            group_name,
+            {'environments': None, 'repositories': None}
+        )
+
+
+async def main() -> None:
+    print('[INFO] Starting migration 0059')
+    groups = await group_dal.get_all(data_attr='project_name')
+    await collect(
+        remove_old_data(group['project_name'])
+        for group in groups
+    )
+    print('[INFO] Migration 0059 finished')
+
+
+if __name__ == '__main__':
+    execution_time = time.strftime(
+        'Execution Time: %Y-%m-%d at %H:%M:%S UTC%Z'
+    )
+    run(main())
+    finalization_time = time.strftime(
+        'Finalization Time: %Y-%m-%d at %H:%M:%S UTC%Z'
+    )
+    print(f'{execution_time}\n{finalization_time}')
