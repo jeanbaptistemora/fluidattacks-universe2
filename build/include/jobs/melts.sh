5ca177eb03618fdefd68047d922f4a89d11d1c66
Diego Restrepo <drestrepo@fluidattacks.com>
2021-01-19 06:58:27 -0500

    melts\refac(back): #3980.7 delete old test
    Former-commit-id: a9b09c66ef8b4c3d677ba35f4d22346790b3c50a

--- build/include/jobs/melts.sh
+++ build/include/jobs/melts.sh
@@ -8,53 +8,6 @@ function job_melts_lint_code {
   &&  helper_test_lint_code_python
 }
 
-function __clean_up {
-  rm -rf melts/.pytest_cache
-  git checkout -- "${STARTDIR}/services/groups/continuoustest"
-}
-
-function job_melts_test {
-  export DEV_AWS_ACCESS_KEY_ID="${MELTS_DEV_AWS_ACCESS_KEY_ID}"
-  export DEV_AWS_SECRET_ACCESS_KEY="${MELTS_DEV_AWS_SECRET_ACCESS_KEY}"
-  # needed to do propper clean up
-  # shellcheck disable=SC2015
-      env_prepare_python_packages \
-  &&  helper_melts_aws_login dev \
-  &&  if [ "${IS_LOCAL_BUILD}" = 'true' ]
-      then
-          helper_common_start_localstack
-      fi \
-  &&  helper_common_use_pristine_workdir \
-  &&  echo '[INFO] Cloning test repository' \
-  &&  helper_clone_test_repo \
-  &&  mv melts services \
-  &&  pushd services \
-  &&  echo '[INFO] Cloning continuoustest repository' \
-  &&  melts drills --pull-repos continuoustest \
-  &&  pushd melts \
-  &&  pytest \
-        --verbose \
-        --exitfirst \
-        --color=yes \
-        --capture=fd \
-        --durations=0 \
-        --failed-first \
-        --disable-warnings \
-        --cov=toolbox \
-        --cov-branch \
-        --cov-report term \
-        --cov-report html:.coverage-html \
-        --no-cov-on-fail \
-        --numprocesses=auto \
-        --random-order \
-        --reruns 10 \
-        --reruns-delay 1 \
-  &&  echo "[INFO] Checkout results at: ${PWD}/.coverage-html/index.html" \
-  &&  popd \
-  &&  { __clean_up; return 0; } \
-  ||  { __clean_up; return 1; }
-}
-
 function job_melts_deploy {
   export TWINE_USERNAME='__token__'
   export TWINE_PASSWORD
