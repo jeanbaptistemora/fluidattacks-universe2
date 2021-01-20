ed5750e2c2f8a6bd31c6ef51d91140e8466be6a1
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-01-20 08:25:51 -0500

    integrates\feat(front): #3896.1 add component
    - add component to display vulnerability info, no visual change yet
    
    
    Former-commit-id: e637c09696eefc004dfbaf16de0562430e7f8555

---
+++ integrates/front/src/scenes/Dashboard/components/Vulnerabilities/AdditionalInfo/index.tsx
@@ -0,0 +1,192 @@
+import type { IHistoricTreatment } from "../../../containers/DescriptionView/types";
+import type { IVulnRowAttr } from "scenes/Dashboard/components/Vulnerabilities/types";
+import React from "react";
+import _ from "lodash";
+import { getLastTreatment } from "scenes/Dashboard/components/Vulnerabilities/UpdateDescription/utils";
+import { useTranslation } from "react-i18next";
+import { Col100, Col40, Col60, Row } from "styles/styledComponents";
+
+interface IAdditionalInfoProps {
+  canDisplayAnalyst: boolean;
+  vulnerability: IVulnRowAttr;
+}
+
+export const AdditionalInfo: React.FC<IAdditionalInfoProps> = ({
+  canDisplayAnalyst,
+  vulnerability,
+}: IAdditionalInfoProps): JSX.Element => {
+  const { t } = useTranslation();
+
+  const lastTreatment: IHistoricTreatment = getLastTreatment(
+    vulnerability.historicTreatment
+  );
+  const currentExpiration: string =
+    vulnerability.currentState === "open" &&
+    lastTreatment.treatment === "ACCEPTED" &&
+    !_.isUndefined(lastTreatment.acceptanceDate)
+      ? lastTreatment.acceptanceDate
+      : "-";
+
+  return (
+    <React.StrictMode>
+      <Col100>
+        <ul>
+          <li>
+            <b>{t("search_findings.tab_vuln.vulnTable.location")}</b>
+            <ul>
+              <li>
+                <Row>
+                  <Col40>
+                    <b>{t("search_findings.tab_vuln.vulnTable.where")}</b>
+                  </Col40>
+                  <Col100>{vulnerability.where}</Col100>
+                </Row>
+              </li>
+              <li>
+                <Row>
+                  <Col40>
+                    <b>{t("search_findings.tab_vuln.vulnTable.specific")}</b>
+                  </Col40>
+                  <Col60>{vulnerability.specific}</Col60>
+                </Row>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <b>{t("search_findings.tab_vuln.vulnTable.reattacks")}</b>
+            <ul>
+              <li>
+                <Row>
+                  <Col40>
+                    <b>
+                      {t(
+                        "search_findings.tab_vuln.vulnTable.lastRequestedReattackDate"
+                      )}
+                    </b>
+                  </Col40>
+                  <Col60>{vulnerability.lastRequestedReattackDate}</Col60>
+                </Row>
+              </li>
+              <li>
+                <Row>
+                  <Col40>
+                    <b>{t("search_findings.tab_vuln.vulnTable.cycles")}</b>
+                  </Col40>
+                  <Col60>{vulnerability.cycles}</Col60>
+                </Row>
+              </li>
+              <li>
+                <Row>
+                  <Col40>
+                    <b>{t("search_findings.tab_vuln.vulnTable.efficacy")}</b>
+                  </Col40>
+                  <Col60>{vulnerability.efficacy}</Col60>
+                </Row>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <b>{t("search_findings.tab_vuln.vulnTable.treatments")}</b>
+            <ul>
+              <li>
+                <Row>
+                  <Col40>
+                    <b>
+                      {t("search_findings.tab_description.treatment.title")}
+                    </b>
+                  </Col40>
+                  <Col60>{vulnerability.treatment}</Col60>
+                </Row>
+              </li>
+              <li>
+                <Row>
+                  <Col40>
+                    <b>{t("search_findings.tab_description.treatment_mgr")}</b>
+                  </Col40>
+                  <Col60>{vulnerability.treatmentManager}</Col60>
+                </Row>
+              </li>
+              <li>
+                <Row>
+                  <Col40>
+                    <b>
+                      {t("search_findings.tab_vuln.vulnTable.treatmentDate")}
+                    </b>
+                  </Col40>
+                  <Col60>{vulnerability.treatmentDate}</Col60>
+                </Row>
+              </li>
+              <li>
+                <Row>
+                  <Col40>
+                    <b>
+                      {t(
+                        "search_findings.tab_vuln.vulnTable.treatmentExpiration"
+                      )}
+                    </b>
+                  </Col40>
+                  <Col60>{currentExpiration}</Col60>
+                </Row>
+              </li>
+              <li>
+                <Row>
+                  <Col40>
+                    <b>
+                      {t("search_findings.tab_vuln.vulnTable.treatmentChanges")}
+                    </b>
+                  </Col40>
+                  <Col60>{vulnerability.treatmentChanges}</Col60>
+                </Row>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <b>{t("search_findings.tab_vuln.vulnTable.info")}</b>
+            <ul>
+              <li>
+                <Row>
+                  <Col40>
+                    <b>{t("search_findings.tab_description.tag")}</b>
+                  </Col40>
+                  <Col60>{vulnerability.tag}</Col60>
+                </Row>
+              </li>
+              <li>
+                <Row>
+                  <Col40>
+                    <b>
+                      {t(
+                        "search_findings.tab_description.business_criticality"
+                      )}
+                    </b>
+                  </Col40>
+                  <Col60>{vulnerability.severity}</Col60>
+                </Row>
+              </li>
+              <li>
+                <Row>
+                  <Col40>
+                    <b>
+                      {t("search_findings.tab_vuln.vulnTable.vulnType.title")}
+                    </b>
+                  </Col40>
+                  <Col60>{vulnerability.vulnType}</Col60>
+                </Row>
+              </li>
+              {canDisplayAnalyst ? (
+                <li>
+                  <Row>
+                    <Col40>
+                      <b>{t("search_findings.tab_description.analyst")}</b>
+                    </Col40>
+                    <Col60>{vulnerability.analyst}</Col60>
+                  </Row>
+                </li>
+              ) : undefined}
+            </ul>
+          </li>
+        </ul>
+      </Col100>
+    </React.StrictMode>
+  );
+};
