4c1ed076f0fd8c40523d2706750a5f59de447552
Duvan Antivar <dantivar@fluidattacks.com>
2021-01-20 11:43:53 -0500

    airs\feat(front): #4018 remove slide
    - Remove third slide on home page
    
    
    Former-commit-id: f202de7c556f1495dbb3fd1ba80012176362193d

--- airs/theme/2020/static/js/contactSlides.ts
+++ airs/theme/2020/static/js/contactSlides.ts
@@ -7,17 +7,13 @@ const slideShow: (() => void) = (): void => {
   const dot1: HTMLElement = document.querySelector(".dot1") as HTMLElement;
   const slide2: HTMLElement = document.querySelector(".mySlide2") as HTMLElement;
   const dot2: HTMLElement = document.querySelector(".dot2") as HTMLElement;
-  const slide3: HTMLElement = document.querySelector(".mySlide3") as HTMLElement;
-  const dot3: HTMLElement = document.querySelector(".dot3") as HTMLElement;
   const homeSlides: HTMLElement = document.querySelector(".slideshow-home") as HTMLElement;
 
   dot1.addEventListener("click", (event: Event) => {
     (event.currentTarget as HTMLElement).classList.replace("bg-fluid-gray", "bg-fluid-red");
     dot2.classList.replace("bg-fluid-red", "bg-fluid-gray");
-    dot3.classList.replace("bg-fluid-red", "bg-fluid-gray");
     slide1.classList.remove("dn");
     slide2.classList.add("dn");
-    slide3.classList.add("dn");
     if (document.body.contains(homeSlides)) {
       if (homeSlides.classList.contains("bg-quote2")) {
         homeSlides.classList.replace("bg-quote2", "bg-quote1");
@@ -30,31 +26,13 @@ const slideShow: (() => void) = (): void => {
   dot2.addEventListener("click", (event: Event) => {
     dot1.classList.replace("bg-fluid-red", "bg-fluid-gray");
     (event.currentTarget as HTMLElement).classList.replace("bg-fluid-gray", "bg-fluid-red");
-    dot3.classList.replace("bg-fluid-red", "bg-fluid-gray");
     slide1.classList.add("dn");
     slide2.classList.remove("dn");
-    slide3.classList.add("dn");
     if (document.body.contains(homeSlides)) {
       if (homeSlides.classList.contains("bg-quote1")) {
         homeSlides.classList.replace("bg-quote1", "bg-quote2");
       } else if (homeSlides.classList.contains("bg-quote3")) {
         homeSlides.classList.replace("bg-quote3", "bg-quote2");
-      }
-    }
-  });
-
-  dot3.addEventListener("click", (event: Event) => {
-    dot1.classList.replace("bg-fluid-red", "bg-fluid-gray");
-    dot2.classList.replace("bg-fluid-red", "bg-fluid-gray");
-    (event.currentTarget as HTMLElement).classList.replace("bg-fluid-gray", "bg-fluid-red");
-    slide1.classList.add("dn");
-    slide2.classList.add("dn");
-    slide3.classList.remove("dn");
-    if (document.body.contains(homeSlides)) {
-      if (homeSlides.classList.contains("bg-quote1")) {
-        homeSlides.classList.replace("bg-quote1", "bg-quote3");
-      } else if (homeSlides.classList.contains("bg-quote2")) {
-        homeSlides.classList.replace("bg-quote2", "bg-quote3");
       }
     }
   });
