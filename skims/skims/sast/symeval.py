fc0015d693cccc1c2c69856c5bdef125ba8dd85b
Kevin Amado <kamado@fluidattacks.com>
2021-01-20 12:12:19 -0500

    skims\feat(back): #3717.52 declaration evaluator
    Former-commit-id: 55a95adbbeb5282aa5a9e0db467cfaf551d499fe

--- skims/skims/sast/symeval.py
+++ skims/skims/sast/symeval.py
@@ -24,14 +24,57 @@ class StopEvaluation(Exception):
 
 
 class EvaluatorArgs(NamedTuple):
+    dependencies: graph_model.SyntaxSteps
+    syntax_step: graph_model.SyntaxStep
+    syntax_step_index: int
     syntax_steps: graph_model.SyntaxSteps
 
 
 Evaluator = Callable[[EvaluatorArgs], None]
 
 
+def syntax_step_declaration(args: EvaluatorArgs) -> None:
+    # Analyze the arguments involved in the assignment
+    args_danger = any(
+        dependency.meta.danger for dependency in args.dependencies
+    )
+
+    # Analyze if the binding itself is sensitive
+    bind_danger = any((
+        # This type is an HTTP request from JavaX framework
+        args.syntax_step.var_type == 'HttpServletRequest',
+    ))
+
+    # Local context
+    args.syntax_step.meta.danger = bind_danger or args_danger
+
+
 EVALUATORS: Dict[object, Evaluator] = {
+    graph_model.SyntaxStepDeclaration: syntax_step_declaration,
 }
+
+
+def get_dependencies(
+    syntax_step_index: int,
+    syntax_steps: graph_model.SyntaxSteps,
+) -> graph_model.SyntaxSteps:
+    dependencies: graph_model.SyntaxSteps = []
+    dependencies_depth: int = 0
+    dependencies_expected_length: int = (
+        -syntax_steps[syntax_step_index].meta.dependencies
+    )
+
+    while len(dependencies) < dependencies_expected_length:
+        syntax_step_index -= 1
+
+        if dependencies_depth:
+            dependencies_depth += 1
+        else:
+            dependencies.append(syntax_steps[syntax_step_index])
+
+        dependencies_depth += syntax_steps[syntax_step_index].meta.dependencies
+
+    return dependencies
 
 
 def eval_syntax_steps(
@@ -41,25 +84,22 @@ def eval_syntax_steps(
 ) -> graph_model.SyntaxSteps:
     if n_id not in shard.syntax:
         # We were not able to fully understand this node syntax
-        raise StopEvaluation()
+        raise StopEvaluation(f'Missing Syntax Reader, {shard.path} @ {n_id}')
 
     syntax_steps = deepcopy(shard.syntax[n_id])
 
-    for syntax_step in syntax_steps:
-        if evaluator := EVALUATORS.get(type(syntax_step)):
+    for syntax_step_index, syntax_step in enumerate(syntax_steps):
+        syntax_step_type = type(syntax_step)
+        if evaluator := EVALUATORS.get(syntax_step_type):
             evaluator(EvaluatorArgs(
+                dependencies=get_dependencies(syntax_step_index, syntax_steps),
+                syntax_step=syntax_step,
+                syntax_step_index=syntax_step_index,
                 syntax_steps=syntax_steps,
             ))
         else:
             # We are not able to evaluate this step
-            raise StopEvaluation()
-
-    # Mark steps as dangerous or not
-    #
-    # If needed this can extend the syntax steps to be longer
-    # for instance in a method invocation
-    #
-    # Pending to implement
+            raise StopEvaluation(f'Missing evaluator, {syntax_step_type}')
 
     return syntax_steps
 
@@ -78,8 +118,8 @@ def get_syntax_steps_from_path(
                 shard=shard,
                 n_id=n_id,
             ))
-        except StopEvaluation:
-            log_blocking('debug', 'SymEval stopped, %s @ %s', shard.path, n_id)
+        except StopEvaluation as exc:
+            log_blocking('debug', str(exc))
             break
 
     return syntax_steps
