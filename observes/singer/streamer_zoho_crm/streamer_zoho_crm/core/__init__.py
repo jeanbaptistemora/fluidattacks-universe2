836a1f3fc0e66dd26b763ecfdcfa6bac2eaf6ed2
Daniel Murcia <dmurcia@fluidattacks.com>
2021-01-20 13:04:17 -0500

    observes\refac(back): #3981.12 core api bulk
    - migrate bulk module to core api
    
    
    Former-commit-id: d0774153a9a998f9f2a1f47403641e3ac528f39a

--- observes/singer/streamer_zoho_crm/streamer_zoho_crm/core/__init__.py
+++ observes/singer/streamer_zoho_crm/streamer_zoho_crm/core/__init__.py
@@ -19,7 +19,8 @@ from streamer_zoho_crm.api.users import (
     UserType,
     UsersDataPage,
 )
-from streamer_zoho_crm.core import users
+from streamer_zoho_crm.core import users, bulk
+from streamer_zoho_crm.db import Client as DbClient
 
 
 class IBulk(NamedTuple):
@@ -35,14 +36,30 @@ class IUsers(NamedTuple):
 
 class CoreClient(NamedTuple):
     users: IUsers
+    bulk: IBulk
 
 
-def new_client(api_client: ApiClient) -> CoreClient:
+def new_client(api_client: ApiClient, db_client: DbClient) -> CoreClient:
+    def create_bulk(module: ModuleName, page: int) -> None:
+        bulk.create_bulk_job(api_client, db_client, module, page)
+
+    def update_bulks() -> None:
+        bulk.update_all(api_client, db_client)
+
+    def extract(jobs_id: FrozenSet[str]) -> FrozenSet[BulkData]:
+        return bulk.get_bulk_data(api_client, jobs_id)
+
     def get_users(user_type: UserType, page: PageIndex) -> UsersDataPage:
         return users.get_users(api_client, user_type, page)
 
     return CoreClient(
         users=IUsers(
             get_users=get_users
+        ),
+        bulk=IBulk(
+            create=create_bulk,
+            update_all=update_bulks,
+            get_all=db_client.get_bulk_jobs,
+            extract_data=extract
         )
     )
---
+++ observes/singer/streamer_zoho_crm/streamer_zoho_crm/core/bulk/_bulk.py
@@ -0,0 +1,65 @@
+# Standard libraries
+from typing import (
+    FrozenSet,
+    Tuple,
+)
+# Third party libraries
+# Local libraries
+from streamer_zoho_crm import utils
+from streamer_zoho_crm.api import (
+    ApiClient,
+)
+from streamer_zoho_crm.api.bulk import (
+    BulkData,
+    BulkJob,
+    ModuleName,
+)
+from streamer_zoho_crm.db import Client as DbClient
+
+
+LOG = utils.get_log(__name__)
+
+
+def create_bulk_job(
+    api_client: ApiClient,
+    db_client: DbClient,
+    module: ModuleName,
+    page: int
+) -> None:
+    """Creates bulk job on crm and stores it on DB"""
+    job: BulkJob = api_client.bulk.create_bulk_read_job(module, page)
+    db_client.save_bulk_job(job)
+
+
+def update_all(
+    api_client: ApiClient,
+    db_client: DbClient
+) -> None:
+    """
+    Get the updated status of the jobs (from crm) and update them in the DB
+    """
+    jobs: FrozenSet[BulkJob] = db_client.get_bulk_jobs()
+    updated_jobs: FrozenSet[BulkJob] = frozenset(
+        map(lambda job: api_client.bulk.get_bulk_job(job.id), jobs)
+    )
+    current_status = frozenset(map(lambda j: (j.id, j.state), jobs))
+    updated_status = frozenset(map(lambda j: (j.id, j.state), updated_jobs))
+
+    need_update: FrozenSet[Tuple[str, str]] = updated_status - current_status
+    need_update_ids: FrozenSet[str] = frozenset(
+        [id for id, s in need_update]
+    )
+    LOG.info('Updating %s jobs status', len(need_update))
+    list(
+        map(
+            db_client.update_bulk_job,
+            frozenset(filter(lambda job: job.id in need_update_ids, jobs))
+        )
+    )
+
+
+def get_bulk_data(
+    api_client: ApiClient,
+    jobs_id: FrozenSet[str]
+) -> FrozenSet[BulkData]:
+    return frozenset(map(api_client.bulk.download_result, jobs_id))
