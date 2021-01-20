1b4b12b8425994ef31330beb27312aa7cfb28678
Frank Aristizabal <faristizabal@fluidattacks.com>
2021-01-20 11:02:52 -0500

    integrates\refac(front): #793.125 update modal
    - Create modal with styled components.
    - no-deltas-test.
    
    
    Former-commit-id: 35c133f06a2167b7b48ee5be3f0588b8d341017d

---
+++ integrates/front/src/components/NewModal/index.spec.tsx
@@ -0,0 +1,96 @@
+/* eslint-disable react/forbid-component-props
+  --------
+  Disable for testing purposes
+*/
+import { Button } from "components/Button";
+import { Modal } from "components/NewModal";
+import React from "react";
+import type { ShallowWrapper } from "enzyme";
+import { shallow } from "enzyme";
+import {
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  ModalTitle,
+} from "styles/styledComponents";
+
+describe("Generic modal", (): void => {
+  it("should return a function", (): void => {
+    expect.hasAssertions();
+    expect(typeof Modal).toStrictEqual("function");
+  });
+
+  it("should render modal title", (): void => {
+    expect.hasAssertions();
+
+    const wrapper: ShallowWrapper = shallow(
+      <Modal headerTitle={"Unit test title"} open={true}>
+        <p>{"Unit modal content"}</p>
+      </Modal>
+    );
+
+    expect(
+      wrapper.contains(
+        <ModalHeader>
+          <ModalTitle>{"Unit test title"}</ModalTitle>
+        </ModalHeader>
+      )
+    ).toBe(true);
+  });
+
+  it("should render modal body", (): void => {
+    expect.hasAssertions();
+
+    const wrapper: ShallowWrapper = shallow(
+      <Modal headerTitle={"Unit test title"} open={true}>
+        <p>{"Unit modal content"}</p>
+      </Modal>
+    );
+
+    expect(
+      wrapper.contains(
+        <ModalBody>
+          <p>{"Unit modal content"}</p>
+        </ModalBody>
+      )
+    ).toBe(true);
+  });
+
+  it("should render modal footer", (): void => {
+    expect.hasAssertions();
+
+    const wrapper: ShallowWrapper = shallow(
+      <Modal
+        footer={<Button>{"test btn"}</Button>}
+        headerTitle={"Unit test title"}
+        open={true}
+      >
+        <p>{"Unit modal content"}</p>
+      </Modal>
+    );
+
+    expect(
+      wrapper.contains(
+        <ModalFooter>
+          <Button>{"test btn"}</Button>
+        </ModalFooter>
+      )
+    ).toBe(true);
+  });
+
+  it("should render a modal", (): void => {
+    expect.hasAssertions();
+
+    const wrapper: ShallowWrapper = shallow(
+      <Modal
+        footer={<Button>{"test btn"}</Button>}
+        headerTitle={"Unit test title"}
+        open={true}
+      >
+        <p>{"Unit modal content"}</p>
+      </Modal>
+    );
+
+    expect(wrapper).toHaveLength(1);
+  });
+});
---
+++ integrates/front/src/components/NewModal/index.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import type { StyledComponent } from "styled-components";
+import style from "./index.css";
+import styled from "styled-components";
+
+import {
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  ModalTitle,
+} from "styles/styledComponents";
+
+interface IModalProps {
+  children: React.ReactNode;
+  footer?: React.ReactNode;
+  headerTitle: string;
+  open: boolean;
+}
+
+const StyledBgModal: StyledComponent<
+  "div",
+  Record<string, unknown>
+> = styled.div.attrs({
+  className: `fixed absolute--fill z-9999 ${style.bgModal}`,
+})``;
+
+const StyledModalContainer: StyledComponent<
+  "div",
+  Record<string, unknown>
+> = styled.div.attrs({
+  className: "fixed absolute--fill overflow-hidden overflow-y-auto pa3 z-9999",
+})``;
+
+const StyledModalDialog: StyledComponent<
+  "div",
+  Record<string, unknown>
+> = styled.div.attrs({
+  className: `flex v-mid center relative ma5 ${style.modalDialog}`,
+})``;
+
+const StyledModal: StyledComponent<
+  "div",
+  Record<string, unknown>
+> = styled.div.attrs({
+  className: `relative tl outline-0 ${style.modal}`,
+})``;
+
+const Modal: React.FC<IModalProps> = (
+  props: Readonly<IModalProps>
+): JSX.Element => {
+  const { children, footer = <div />, headerTitle, open } = props;
+
+  React.useEffect((): void => {
+    if (open) {
+      document.body.style.setProperty("overflow", "hidden");
+    } else {
+      document.body.style.removeProperty("overflow");
+    }
+  }, [open]);
+
+  return (
+    <div>
+      {open ? (
+        <React.Fragment>
+          <StyledBgModal />
+          <StyledModalContainer>
+            <StyledModalDialog>
+              <StyledModal>
+                <ModalHeader>
+                  <ModalTitle>{headerTitle}</ModalTitle>
+                </ModalHeader>
+                <ModalBody>{children}</ModalBody>
+                <ModalFooter>{footer}</ModalFooter>
+              </StyledModal>
+            </StyledModalDialog>
+          </StyledModalContainer>
+        </React.Fragment>
+      ) : undefined}
+    </div>
+  );
+};
+
+export { Modal };
