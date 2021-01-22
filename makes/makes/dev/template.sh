95a3c3bb640eb8ace248ba5a912379bf47c87a6b
Kevin Amado <kamado@fluidattacks.com>
2021-01-21 22:40:23 -0500

    common\feat(build): #4017.4 add aliases
    Former-commit-id: cd2bbf5541a0c075da14a93a30952ebe9ce78f5f

--- makes/makes/dev/template.sh
+++ makes/makes/dev/template.sh
@@ -6,7 +6,21 @@ function load_base {
   &&  echo '[INFO] ---' \
   &&  echo '[INFO] You can now execute `load_<product>`' \
   &&  echo '[INFO]   in order to make available all deps required to develop such product' \
-  &&  echo '[INFO]'
+  &&  echo '[INFO]' \
+  &&  echo '[INFO] Aliases:'  \
+  &&  echo '[INFO]   a: add git changes, hunk by hunk' \
+  &&  alias a='git add -p' \
+  &&  echo '[INFO]   c: create a new commit' \
+  &&  alias c='git commit --allow-empty' \
+  &&  echo '[INFO]   m: add your added changes to the last commit' \
+  &&  alias m='git commit --amend --no-edit --allow-empty' \
+  &&  echo '[INFO]   r: rebase' \
+  &&  alias r='git pull --autostash --progress --rebase --stat origin master' \
+  &&  echo '[INFO]   p: push changes to your branch' \
+  &&  alias p='git push -f' \
+  &&  echo '[INFO]   f: fetch changes from remote (useful for peer review)' \
+  &&  alias f='git fetch --all' \
+
 }
 
 function load_skims {
