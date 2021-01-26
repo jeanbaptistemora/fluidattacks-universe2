a1887a01f2e31917a4274d28957ad01bc58b4956
Kevin Amado <kamado@fluidattacks.com>
2021-01-26 15:30:56 -0500

    integrates\feat(back): #3985.17 compute reverse deps
    Former-commit-id: 5677a67c3bb68a69e5b9e682737d5d3c18ed2193

--- integrates/back/packages/integrates-back/backend/model/redis_model.py
+++ integrates/back/packages/integrates-back/backend/model/redis_model.py
@@ -14,6 +14,10 @@ ENTITIES: Dict[str, Dict[str, Set[str]]] = dict(
         attrs={
             'consulting',
         },
+        dependencies={
+            'add_event_consult',
+            'handle_vulns_acceptation',
+        },
     ),
     finding=dict(
         args={
@@ -31,6 +35,17 @@ ENTITIES: Dict[str, Dict[str, Set[str]]] = dict(
             'observations',
             'verified',
         },
+        dependencies={
+            'approve_draft',
+            'confirm_zero_risk_vuln',
+            'delete_finding',
+            'delete_vulnerability',
+            'delete_vulnerability_tags',
+            'request_zero_risk_vuln',
+            'update_vulns_treatment',
+            'upload_file',
+            'verify_request_vulnerability',
+        },
     ),
     forces_execution=dict(
         args={
@@ -40,6 +55,7 @@ ENTITIES: Dict[str, Dict[str, Set[str]]] = dict(
         attrs={
             'forces_execution'
         },
+        dependencies=set(),
     ),
 )
 # Pending:
@@ -69,6 +85,10 @@ def build_key(entity: str, attr: str, **args: str) -> str:
     if attr not in ENTITIES[entity]['attrs']:
         raise ValueError(f'Invalid attr: {entity}.{attr}')
 
+    extra_args: Set[str] = set(args) - ENTITIES[entity]['args']
+    if extra_args:
+        raise ValueError(f'Extra args for {entity}.{attr}: {extra_args}')
+
     missing_args: Set[str] = ENTITIES[entity]['args'] - set(args)
     if missing_args:
         raise ValueError(f'Missing args for {entity}.{attr}: {missing_args}')
@@ -88,4 +108,27 @@ def build_keys_for_entity(entity: str, **args: str) -> Set[str]:
         for attr in ENTITIES[entity]['attrs']
     }
 
+    return keys
+
+
+def build_keys_by_dependencies(dependency: str, **args: str) -> Set[str]:
+    keys: Set[str] = set()
+
+    for entity in ENTITIES:
+        if dependency in ENTITIES[entity]['dependencies']:
+            required_args: Dict[str, str] = {
+                arg_base: arg_value
+                for arg, arg_value in args.items()
+                for arg_base in [arg.replace(f'{entity}_', '')]
+                if arg_base in ENTITIES[entity]['args']
+            }
+
+            keys.update(build_keys_for_entity(entity, **required_args))
+
+    # >>> get_keys_to_delete('delete_vulnerability_tags', finding_id=123)
+    # {'finding.verified@id=123', 'finding.open_vulns@id=123',
+    #  'finding.age@id=123', 'finding.observations@id=123',
+    #  'finding.ports_vulns@id=123', 'finding.closed_vulns@id=123',
+    #  'finding.inputs_vulns@id=123', 'finding.lines_vulns@id=123',
+    #  'finding.vulns@id=123', 'finding.exploit@id=123'}
     return keys
