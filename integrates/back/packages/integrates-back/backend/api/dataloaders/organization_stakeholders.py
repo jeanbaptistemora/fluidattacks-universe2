e98a5e241355c59f3582899c321ad65811f16a51
Mario Varas <mvaras@fluidattacks.com>
2021-02-01 15:25:22 -0500

    integrates\feat(back): #3990.2 add stakeholders dataloader
    - For organizations
    
    
    Former-commit-id: 4f3a726312b2e2fe55561772a23dc73be77251c7

---
+++ integrates/back/packages/integrates-back/backend/api/dataloaders/organization_stakeholders.py
@@ -0,0 +1,54 @@
+# Standard libraries
+from typing import (
+    cast,
+    List,
+    Tuple
+)
+
+# Third party libraries
+from aiodataloader import DataLoader
+from aioextensions import collect
+
+# Local libraries
+from backend import authz
+from backend.domain import (
+    organization as org_domain,
+    user as stakeholder_domain
+)
+from backend.typing import Stakeholder as StakeholderType
+
+
+async def _get_stakeholder(email: str, org_id: str) -> StakeholderType:
+    stakeholder: StakeholderType = await stakeholder_domain.get_by_email(email)
+    org_role: str = await authz.get_organization_level_role(email, org_id)
+
+    return {**stakeholder, 'responsibility': '', 'role': org_role}
+
+
+async def get_stakeholders_by_organization(
+    organization_id: str
+) -> Tuple[StakeholderType, ...]:
+    org_stakeholders_emails: List[str] = \
+        await org_domain.get_users(organization_id)
+    org_stakeholders = await collect(
+        _get_stakeholder(email, organization_id)
+        for email in org_stakeholders_emails
+    )
+
+    return tuple(org_stakeholders)
+
+
+class OrganizationStakeholdersLoader(DataLoader):  # type: ignore
+    """Batches load calls within the same execution fragment."""
+    # pylint: disable=method-hidden
+    async def batch_load_fn(
+        self,
+        organization_names: List[str]
+    ) -> Tuple[Tuple[StakeholderType, ...], ...]:
+        return cast(
+            Tuple[Tuple[StakeholderType, ...], ...],
+            await collect(
+                get_stakeholders_by_organization(organization_name)
+                for organization_name in organization_names
+            )
+        )
