66a0fde3d228a34d67c240306a25f54ed7983b07
Daniel Murcia <dmurcia@fluidattacks.com>
2021-02-01 08:59:35 -0500

    observes\refac(job): #3972.24 code upload
    - migrated job to makes
    - adjust compute aws on code mirror
    
    
    Former-commit-id: c335debe37c2cb44a2869360e1d13dfc7dc88dbe

---
+++ makes/applications/observes/code-etl-upload/default.nix
@@ -0,0 +1,23 @@
+{ observesPkgs
+, outputs
+, path
+, ...
+} @ _:
+let
+  bins = import (path "/makes/libs/observes/bins") {
+    inherit path;
+    nixPkgs = observesPkgs;
+  };
+  makeEntrypoint = import (path "/makes/utils/make-entrypoint") path observesPkgs;
+in
+makeEntrypoint {
+  arguments = {
+    envCodeEtlBin = "${bins.codeEtl}/bin";
+    envMelts = outputs.apps.melts.program;
+    envUtilsBashLibAws = import (path "/makes/utils/aws") path observesPkgs;
+    envUtilsBashLibGit = import (path "/makes/utils/use-git-repo") path observesPkgs;
+    envUtilsBashLibSops = import (path "/makes/utils/sops") path observesPkgs;
+  };
+  name = "observes-code-etl-upload";
+  template = path "/makes/applications/observes/code-etl-upload/entrypoint.sh";
+}
---
+++ makes/applications/observes/code-etl-upload/entrypoint.sh
@@ -0,0 +1,54 @@
+# shellcheck shell=bash
+
+source '__envUtilsBashLibAws__'
+source '__envUtilsBashLibSops__'
+source '__envUtilsBashLibGit__'
+
+export PATH="__envCodeEtlBin__:${PATH:-}"
+melts='__envMelts__'
+
+function job_code_upload {
+  local group="${1}"
+
+      if test -z "${group}"
+      then
+            echo '[INFO] Please set the first argument to the group name' \
+        &&  return 1
+      fi \
+  &&  aws_login_prod 'observes' \
+  &&  sops_export_vars 'observes/secrets-prod.yaml' 'default' \
+        'REDSHIFT_DATABASE' \
+        'REDSHIFT_HOST' \
+        'REDSHIFT_PASSWORD' \
+        'REDSHIFT_PORT' \
+        'REDSHIFT_USER' \
+  &&  use_git_repo_services \
+    &&  echo "[INFO] Working on ${group}" \
+    &&  echo "[INFO] Cloning ${group}" \
+    &&  if  CI=true \
+            CI_COMMIT_REF_NAME='master' \
+            PROD_AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
+            PROD_AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
+            "${melts}" drills --pull-repos "${group}"
+        then
+              echo "[INFO] Apending mailmaps" \
+          &&  shopt -s nullglob \
+          &&  for repo in "groups/${group}/fusion/"*
+              do
+                    cp -f '.groups-mailmap' "${repo}/.mailmap" \
+                ||  return 1
+              done \
+          &&  echo "[INFO] Executing ${group}" \
+          &&  code-etl upload-code \
+                "${group}" \
+                "groups/${group}/fusion/"* \
+          &&  shopt -u nullglob \
+          &&  rm -rf "groups/${group}/fusion/" \
+
+        fi \
+  &&  popd \
+  ||  return 1
+
+}
+
+job_code_upload "${@}"
