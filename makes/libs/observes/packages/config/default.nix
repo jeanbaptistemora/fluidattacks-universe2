ecd4c56a9644a504ab62da0bab8ca38a4fe9c576
Daniel Murcia <dmurcia@fluidattacks.com>
2021-02-02 10:09:53 -0500

    observes\refac(job): #3972.30 target redshift lint
    - added target redshift dependencies
    - migrated lint job to makes
    
    
    Former-commit-id: 1dfeacb82b6057a5aa321edd5d1118af6c165731

--- makes/libs/observes/packages/config/default.nix
+++ makes/libs/observes/packages/config/default.nix
@@ -263,9 +263,27 @@
     srcPath = path "/observes/singer/target_redshift_2";
     python = {
       direct = [ ];
-      inherited = [ ];
+      inherited = [
+        "atomicwrites==1.4.0"
+        "attrs==20.3.0"
+        "mirakuru==2.3.0"
+        "more-itertools==8.6.0"
+        "packaging==20.8"
+        "pluggy==0.13.1"
+        "port-for==0.4"
+        "psutil==5.8.0"
+        "psycopg2==2.8.4"
+        "py==1.10.0"
+        "pyparsing==2.4.7"
+        "pytest-postgresql==2.5.2"
+        "pytest==5.2.0"
+        "wcwidth==0.2.5"
+      ];
     };
-    local = [ ];
+    local = [
+      "postgresClient"
+      "singerIO"
+    ];
     nix = [ ];
   };
   updateSyncDate = {
