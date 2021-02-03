26eef2434b5e0f48b79ef79c0873758a4af4fcc7
Kevin Amado <kamado@fluidattacks.com>
2021-02-02 19:31:21 -0500

    common\feat(build): #3836.152 wait for app
    Former-commit-id: c51e5d63cefa2044862785820c7ca4586b41e53f

---
+++ makes/applications/makes/wait/default.nix
@@ -0,0 +1,14 @@
+{ makesPkgs
+, path
+, ...
+} @ _:
+let
+  makeEntrypoint = import (path "/makes/utils/make-entrypoint") path makesPkgs;
+in
+makeEntrypoint {
+  arguments = {
+    envNc = "${makesPkgs.netcat}/bin/nc";
+  };
+  name = "makes-wait";
+  template = path "/makes/applications/makes/wait/entrypoint.sh";
+}
