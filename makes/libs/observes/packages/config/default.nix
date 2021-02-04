1bd914d17393be5af7e1dbc4941eda26d44601b1
Daniel Murcia <dmurcia@fluidattacks.com>
2021-02-04 13:14:47 -0500

    observes\feat(job): #3972.37 formstack lint
    - added tap formstack lint
    - added dependencies
    
    
    Former-commit-id: 1365e114801363ec0e7d804901572cb3e7277067

--- makes/libs/observes/packages/config/default.nix
+++ makes/libs/observes/packages/config/default.nix
@@ -290,8 +290,17 @@ rec {
   tapFormstack = {
     srcPath = path "/observes/singer/tap_formstack";
     python = {
-      direct = [ ];
-      inherited = [ ];
+      direct = [
+        "python-dateutil==2.8.1"
+        "requests==2.25.1"
+      ];
+      inherited = [
+        "certifi==2020.12.5"
+        "chardet==4.0.0"
+        "idna==2.10"
+        "six==1.15.0"
+        "urllib3==1.26.3"
+      ];
     };
     local = [ ];
     nix = [ ];
