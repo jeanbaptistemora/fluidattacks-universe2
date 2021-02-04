5f8c550cd6af7fbb88e182a7b572eb55a14af4ba
Kevin Amado <kamado@fluidattacks.com>
2021-02-04 13:46:38 -0500

    integrates\feat(build): #3836.176 concurrent ruby
    Former-commit-id: 56ab0e5ccdd1ebeb5fddf7c3542bd9ccd7a55821

---
+++ makes/packages/integrates/tools/concurrent-ruby/default.nix
@@ -0,0 +1,13 @@
+{ integratesPkgs
+, path
+, ...
+} @ _:
+let
+  buildRubyRequirement = import (path "/makes/utils/build-ruby-requirement") path integratesPkgs;
+in
+buildRubyRequirement {
+  dependencies = [ ];
+  name = "integrates-tools-concurrent-ruby";
+  ruby = integratesPkgs.ruby_2_6;
+  requirement = "concurrent-ruby:1.1.6";
+}
