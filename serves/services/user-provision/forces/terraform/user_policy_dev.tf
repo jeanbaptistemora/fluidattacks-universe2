90714b399a1a584237ab8c75d354b5382fb23fe1
Diego Restrepo <drestrepo@fluidattacks.com>
2021-02-04 15:01:15 -0500

    serves\feat(infra): #3973.10 add users policies
    Former-commit-id: 4c61f84da0f4b14dfb4e34baa751b21d433ba5ed

---
+++ serves/services/user-provision/forces/terraform/user_policy_dev.tf
@@ -0,0 +1,91 @@
+data "aws_iam_policy_document" "forces_dev_policy_data" {
+  # S3 access to the terraform state
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket",
+    ]
+    resources = [
+      "arn:aws:s3:::fluidattacks-terraform-states-prod",
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+    ]
+    resources = [
+      "arn:aws:s3:::fluidattacks-terraform-states-prod/forces.tfstate",
+    ]
+  }
+
+  # DynamoDB for locking terraform state
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:DeleteItem",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+    ]
+    resources = [
+      var.terraform_state_lock_arn,
+    ]
+  }
+
+  # IAM read break-build and AWS SSO role
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:GetUser",
+      "iam:GetRole",
+      "iam:GetPolicy",
+      "iam:GetPolicyVersion",
+      "iam:ListAccessKeys",
+      "iam:ListAttachedUserPolicies",
+      "iam:ListAttachedRolePolicies"
+    ]
+    resources = [
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/asserts/break-build-*",
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/asserts/break-build-*",
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/break-build-*",
+    ]
+  }
+
+  # KMS
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:List*",
+      "kms:Get*",
+      "kms:Describe*"
+    ]
+    resources = [
+      "arn:aws:kms:${var.region}:${data.aws_caller_identity.current.account_id}:alias/continuous-*"
+    ]
+  }
+
+  # Secretsmanager for forces secrets
+  statement {
+    effect = "Allow"
+    actions = [
+      "secretsmanager:DescribeSecret",
+      "secretsmanager:GetResourcePolicy",
+    ]
+    resources = [
+      "arn:aws:secretsmanager:${var.region}:${data.aws_caller_identity.current.account_id}:secret:forces*",
+    ]
+  }
+
+}
+
+resource "aws_iam_policy" "forces_dev_policy" {
+  description = "forces_dev policy"
+  name        = "forces_dev_policy"
+  path        = "/user_provision/"
+  policy      = data.aws_iam_policy_document.forces_dev_policy_data.json
+}
+
+resource "aws_iam_user_policy_attachment" "forces_dev_attach_policy" {
+  user       = "forces_dev"
+  policy_arn = aws_iam_policy.forces_dev_policy.arn
+}
---
+++ serves/services/user-provision/forces/terraform/user_policy_prod.tf
@@ -0,0 +1,108 @@
+data "aws_iam_policy_document" "forces_prod_policy_data" {
+  # S3 access to the terraform state
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket",
+    ]
+    resources = [
+      "arn:aws:s3:::fluidattacks-terraform-states-prod",
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+    ]
+    resources = [
+      "arn:aws:s3:::fluidattacks-terraform-states-prod/forces.tfstate",
+    ]
+  }
+
+  # DynamoDB for locking terraform state
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:DeleteItem",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+    ]
+    resources = [
+      var.terraform_state_lock_arn,
+    ]
+  }
+
+  # IAM Break Build and AWS SSO role
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:*"
+    ]
+    resources = [
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/asserts/break-build-*",
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/asserts/break-build-*",
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/break-build-*",
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/break-build-*",
+    ]
+  }
+
+  # KMS Create Keys
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:UntagResource",
+      "kms:TagResource",
+      "kms:List*",
+      "kms:Get*",
+      "kms:Describe*",
+      "kms:CreateKey",
+      "kms:CreateAlias",
+      "kms:UpdateAlias"
+    ]
+    resources = ["*"]
+  }
+
+  # KMS FUll permissions over owned KMS keys
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:*"
+    ]
+    resources = [
+      "arn:aws:kms:${var.region}:${data.aws_caller_identity.current.account_id}:alias/continuous-*"
+    ]
+  }
+
+  # Secretsmanager for forces
+  statement {
+    effect = "Allow"
+    actions = [
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:CreateSecret",
+      "secretsmanager:DeleteSecret",
+      "secretsmanager:DescribeSecret",
+      "secretsmanager:GetResourcePolicy",
+      "secretsmanager:PutSecretValue",
+      "secretsmanager:RestoreSecret",
+      "secretsmanager:UpdateSecret",
+      "secretsmanager:TagResource",
+    ]
+    resources = [
+      "arn:aws:secretsmanager:${var.region}:${data.aws_caller_identity.current.account_id}:secret:forces-*"
+    ]
+  }
+
+}
+
+resource "aws_iam_policy" "forces_prod_policy" {
+  description = "forces_prod policy"
+  name        = "forces_prod_policy"
+  path        = "/user_provision/"
+  policy      = data.aws_iam_policy_document.forces_prod_policy_data.json
+}
+
+resource "aws_iam_user_policy_attachment" "forces_prod_attach_policy" {
+  user       = "forces_prod"
+  policy_arn = aws_iam_policy.forces_prod_policy.arn
+}
