f4d1feac684f3cfadab1e868ddd869597c871133
Kevin Amado <kamado@fluidattacks.com>
2021-02-04 13:34:11 -0500

    integrates\feat(build): #3836.175 asciidoctor pdf
    Former-commit-id: 593c6060a473ec298911ed2feef2a4e88ceead29

---
+++ makes/packages/integrates/tools/asciidoctor-pdf/default.nix
@@ -0,0 +1,13 @@
+{ integratesPkgs
+, path
+, ...
+} @ _:
+let
+  buildRubyRequirement = import (path "/makes/utils/build-ruby-requirement") path integratesPkgs;
+in
+buildRubyRequirement {
+  dependencies = [ ];
+  name = "integrates-tools-asciidoctor-pdf";
+  ruby = integratesPkgs.ruby_2_6;
+  requirement = "asciidoctor-pdf:1.5.0.rc.3";
+}
