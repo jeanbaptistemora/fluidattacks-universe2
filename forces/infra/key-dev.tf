98327864484263d1ec0250f34ed03e1cc0d86e9d
Diego Restrepo <drestrepo@fluidattacks.com>
2021-02-05 11:45:43 -0500

    forces\feat(infra): #3973.13 add kms keys for secrets
    Former-commit-id: c6dedc3c547de5e97db76ef75a2c24aa37b2b2e6

---
+++ forces/infra/key-dev.tf
@@ -0,0 +1,133 @@
+data "aws_iam_policy_document" "key-forces-dev" {
+
+  statement {
+    sid    = "Enable IAM User Permissions"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions = [
+      "kms:*"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid    = "Key Administrators"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user_provision/forces_prod",
+      ]
+    }
+    actions = [
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:TagResource",
+      "kms:UntagResource",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion",
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid    = "Key Managger"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user_provision/forces_dev",
+      ]
+    }
+    actions = [
+      "kms:Describe*",
+      "kms:List*",
+      "kms:Disable*",
+      "kms:Get*",
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid    = "Key Users"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user_provision/forces_dev",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user_provision/forces_prod",
+      ]
+    }
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid    = "Attachment Of Persistent Resources"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user_provision/forces_prod",
+      ]
+    }
+    actions = [
+      "kms:CreateGrant",
+      "kms:ListGrants",
+      "kms:RevokeGrant",
+    ]
+    resources = [
+      "*"
+    ]
+    condition {
+      test     = "Bool"
+      variable = "kms:GrantIsForAWSResource"
+      values = [
+        "true",
+      ]
+    }
+  }
+}
+
+resource "aws_kms_key" "key-forces-dev" {
+  description             = "Normal user kms key for forces."
+  policy                  = data.aws_iam_policy_document.key-forces-dev.json
+  deletion_window_in_days = 30
+  is_enabled              = true
+
+  tags = {
+    "Name"               = "forces-development"
+    "management:type"    = "development"
+    "management:product" = "forces"
+  }
+}
+
+resource "aws_kms_alias" "key-forces-dev" {
+  name          = "alias/forces-dev"
+  target_key_id = aws_kms_key.key-forces-dev.key_id
+}
---
+++ forces/infra/key-prod.tf
@@ -0,0 +1,133 @@
+data "aws_iam_policy_document" "key-forces-production" {
+
+  statement {
+    sid    = "Enable IAM User Permissions"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions = [
+      "kms:*"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid    = "Key Administrators"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user-provision/serves-prod",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user_provision/forces_prod",
+      ]
+    }
+    actions = [
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:TagResource",
+      "kms:UntagResource",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion",
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid    = "key Managger"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user_provision/forces_dev",
+      ]
+    }
+    actions = [
+      "kms:DescribeKey",
+      "kms:GetKeyPolicy",
+      "kms:GetKeyRotationStatus",
+      "kms:ListResourceTags",
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid    = "Key Users"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user_provision/forces_prod",
+      ]
+    }
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid    = "Attachment Of Persistent Resources"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user-provision/serves-prod",
+      ]
+    }
+    actions = [
+      "kms:CreateGrant",
+      "kms:ListGrants",
+      "kms:RevokeGrant",
+    ]
+    resources = [
+      "*"
+    ]
+    condition {
+      test     = "Bool"
+      variable = "kms:GrantIsForAWSResource"
+      values = [
+        "true",
+      ]
+    }
+  }
+}
+
+resource "aws_kms_key" "key-forces-prod" {
+  description             = "production kms key for forces."
+  policy                  = data.aws_iam_policy_document.key-forces-production.json
+  deletion_window_in_days = 30
+  is_enabled              = true
+
+  tags = {
+    "Name"               = "forces-production"
+    "management:type"    = "production"
+    "management:product" = "airs"
+  }
+}
+
+resource "aws_kms_alias" "key-forces-prod" {
+  name          = "alias/forces-prod"
+  target_key_id = aws_kms_key.key-forces-prod.key_id
+}
