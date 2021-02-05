a54294bc074d5e0991329c79790d2339ffdbb323
Andres Cuberos <acuberos@fluidattacks.com>
2021-02-05 15:26:34 -0500

    integrates\feat(build): #3875.17 local mobile makes
    - Add makes application to deploy the mobile front locally.
    
    
    Former-commit-id: eb7e5142960abcab680d3131ef1c39298a82d207

---
+++ makes/applications/integrates/mobile/default.nix
@@ -0,0 +1,25 @@
+{ integratesPkgs
+, path
+, ...
+} @ attrs:
+let
+  makeEntrypoint = import (path "/makes/utils/make-entrypoint") path integratesPkgs;
+  makeSearchPaths = import (path "/makes/utils/make-search-paths") path integratesPkgs;
+in
+makeEntrypoint {
+  arguments = {
+    envSearchPaths = makeSearchPaths [
+      integratesPkgs.findutils
+      integratesPkgs.nodejs-12_x
+      integratesPkgs.xdg_utils
+    ];
+    envSecretsDev = path "/integrates/secrets-development.yaml";
+    envSetupIntegratesMobileDevelopment = import (path "/makes/packages/integrates/mobile/config/development") attrs.copy;
+    envSetupIntegratesMobileRuntime = import (path "/makes/packages/integrates/mobile/config/runtime") attrs.copy;
+    envUtilsAws = import (path "/makes/utils/aws") path integratesPkgs;
+    envUtilsCommon = path "/makes/utils/common/template.sh";
+    envUtilsSops = import (path "/makes/utils/sops") path integratesPkgs;
+  };
+  name = "integrates-mobile";
+  template = path "/makes/applications/integrates/mobile/entrypoint.sh";
+}
---
+++ makes/applications/integrates/mobile/entrypoint.sh
@@ -0,0 +1,32 @@
+# shellcheck shell=bash
+
+source '__envSearchPaths__'
+source '__envUtilsAws__'
+source '__envUtilsCommon__'
+source '__envUtilsSops__'
+
+function main {
+  local secrets=(
+    EXPO_PASS
+    EXPO_USER
+    GOOGLE_SERVICES_APP
+  )
+
+      aws_login_dev 'integrates' \
+  &&  sops_export_vars __envSecretsDev__ "${secrets[@]}" \
+  &&  pushd integrates/mobile \
+    &&  copy __envSetupIntegratesMobileDevelopment__/node_modules node_modules \
+    &&  copy __envSetupIntegratesMobileRuntime__/node_modules node_modules \
+    &&  chmod 755 node_modules/.bin/expo node_modules/.bin/expo-cli \
+      &&  echo "${GOOGLE_SERVICES_APP}" > google-services.json \
+      &&  npx --no-install expo login \
+          --username "${EXPO_USER}" \
+          --password "${EXPO_PASS}" \
+          --non-interactive \
+      &&  npm start -- --clear --non-interactive \
+    &&  rm -rf .expo google-services.json node_modules \
+  &&  popd \
+  ||  return 1
+}
+
+main "${@}"
