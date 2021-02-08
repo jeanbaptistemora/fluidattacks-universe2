1cdebb115c186d7bbd19b4e470757192f25af139
Daniel Salazar <dsalazar@fluidattacks.com>
2021-02-08 18:42:25 -0500

    serves\refac(infra): #4159.1 reformat code
    - Reformat dns code
    - no-deltas-test
    
    
    Former-commit-id: a8bdb6cded430a6862a3f4f8a52a8c2e5a232d7b

--- serves/services/dns/terraform/fluidattacks.tf
+++ serves/services/dns/terraform/fluidattacks.tf
@@ -353,75 +353,75 @@ resource "cloudflare_record" "mailgun_2" {
 # TXT Records
 
 resource "cloudflare_record" "spf_allowed" {
-  zone_id  = cloudflare_zone.fluidattacks_com.id
-  name     = cloudflare_zone.fluidattacks_com.zone
-  type     = "TXT"
-  value    = "v=spf1 include:_spf.google.com include:mail.zendesk.com include:spf.mandrillapp.com include:servers.mcsv.net include:transmail.net -all"
-  ttl      = 1
-  proxied  = false
+  zone_id = cloudflare_zone.fluidattacks_com.id
+  name    = cloudflare_zone.fluidattacks_com.zone
+  type    = "TXT"
+  value   = "v=spf1 include:_spf.google.com include:mail.zendesk.com include:spf.mandrillapp.com include:servers.mcsv.net include:transmail.net -all"
+  ttl     = 1
+  proxied = false
 }
 
 resource "cloudflare_record" "verify_zendesk" {
-  zone_id  = cloudflare_zone.fluidattacks_com.id
-  name     = "zendeskverification.${cloudflare_zone.fluidattacks_com.zone}"
-  type     = "TXT"
-  value    = "27f6e2e3b646cce6"
-  ttl      = 1
-  proxied  = false
+  zone_id = cloudflare_zone.fluidattacks_com.id
+  name    = "zendeskverification.${cloudflare_zone.fluidattacks_com.zone}"
+  type    = "TXT"
+  value   = "27f6e2e3b646cce6"
+  ttl     = 1
+  proxied = false
 }
 
 resource "cloudflare_record" "verify_google_1" {
-  zone_id  = cloudflare_zone.fluidattacks_com.id
-  name     = cloudflare_zone.fluidattacks_com.zone
-  type     = "TXT"
-  value    = "google-site-verification=SK6CMgAtuuw7tR6eCev6XY8D6rjn9BW8AGd5KWS1b5g"
-  ttl      = 1
-  proxied  = false
+  zone_id = cloudflare_zone.fluidattacks_com.id
+  name    = cloudflare_zone.fluidattacks_com.zone
+  type    = "TXT"
+  value   = "google-site-verification=SK6CMgAtuuw7tR6eCev6XY8D6rjn9BW8AGd5KWS1b5g"
+  ttl     = 1
+  proxied = false
 }
 
 resource "cloudflare_record" "verify_google_2" {
-  zone_id  = cloudflare_zone.fluidattacks_com.id
-  name     = cloudflare_zone.fluidattacks_com.zone
-  type     = "TXT"
-  value    = "google-site-verification=zVcDOWOKonibpIrLLyFEuy8jbpTJyOPiA39vngIpEvI"
-  ttl      = 1
-  proxied  = false
+  zone_id = cloudflare_zone.fluidattacks_com.id
+  name    = cloudflare_zone.fluidattacks_com.zone
+  type    = "TXT"
+  value   = "google-site-verification=zVcDOWOKonibpIrLLyFEuy8jbpTJyOPiA39vngIpEvI"
+  ttl     = 1
+  proxied = false
 }
 
 resource "cloudflare_record" "mail_dmarc" {
-  zone_id  = cloudflare_zone.fluidattacks_com.id
-  name     = "_dmarc.${cloudflare_zone.fluidattacks_com.zone}"
-  type     = "TXT"
-  value    = "v=DMARC1; p=quarantine; rua=mailto:technology+dmarc@fluidattacks.com"
-  ttl      = 1
-  proxied  = false
+  zone_id = cloudflare_zone.fluidattacks_com.id
+  name    = "_dmarc.${cloudflare_zone.fluidattacks_com.zone}"
+  type    = "TXT"
+  value   = "v=DMARC1; p=quarantine; rua=mailto:technology+dmarc@fluidattacks.com"
+  ttl     = 1
+  proxied = false
 }
 
 resource "cloudflare_record" "mail_dkim" {
-  zone_id  = cloudflare_zone.fluidattacks_com.id
-  name     = "google._domainkey.${cloudflare_zone.fluidattacks_com.zone}"
-  type     = "TXT"
-  value    = "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoVfDxzz1BbwFFyeQvKe7B4YMSR1HWmjCu4PQzESyAAc9XQDSbtoYQNCHaHisTJNgh4OGEWvgRcpsVljffC5jO3tHcra8xW8ls5O16sClQtfitcKhC1VxNbqYoAnUSNv9FBcsldK96jQgeMrsZUMo6SdldCDOkX7vOjgLzDw6dOMAENSoU3NsMfRwoDaanCf2gkFb+5mOtDUZCHukM5rpj+ePc3GJAzX8bakMdWD7BlZnPT0fRVcSQGOAM1GVcSDYR465hdBkADJg3KM2TdPTC/XLwEQXgqRZXVWMtSu/Rb/DcHILZNmzKxUk/B4eKjXGQDbs9hshgsqsZGYEbhOvrwIDAQAB"
-  ttl      = 1
-  proxied  = false
+  zone_id = cloudflare_zone.fluidattacks_com.id
+  name    = "google._domainkey.${cloudflare_zone.fluidattacks_com.zone}"
+  type    = "TXT"
+  value   = "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoVfDxzz1BbwFFyeQvKe7B4YMSR1HWmjCu4PQzESyAAc9XQDSbtoYQNCHaHisTJNgh4OGEWvgRcpsVljffC5jO3tHcra8xW8ls5O16sClQtfitcKhC1VxNbqYoAnUSNv9FBcsldK96jQgeMrsZUMo6SdldCDOkX7vOjgLzDw6dOMAENSoU3NsMfRwoDaanCf2gkFb+5mOtDUZCHukM5rpj+ePc3GJAzX8bakMdWD7BlZnPT0fRVcSQGOAM1GVcSDYR465hdBkADJg3KM2TdPTC/XLwEQXgqRZXVWMtSu/Rb/DcHILZNmzKxUk/B4eKjXGQDbs9hshgsqsZGYEbhOvrwIDAQAB"
+  ttl     = 1
+  proxied = false
 }
 
 resource "cloudflare_record" "mandrill_dkim" {
-  zone_id  = cloudflare_zone.fluidattacks_com.id
-  name     = "mandrill._domainkey.${cloudflare_zone.fluidattacks_com.zone}"
-  type     = "TXT"
-  value    = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCrLHiExVd55zd/IQ/J/mRwSRMAocV/hMB3jXwaHH36d9NaVynQFYV8NaWi69c1veUtRzGt7yAioXqLj7Z4TeEUoOLgrKsn8YnckGs9i3B3tVFB+Ch/4mPhXWiNfNdynHWBcPcbJ8kjEQ2U8y78dHZj1YeRXXVvWob2OaKynO8/lQIDAQAB;"
-  ttl      = 1
-  proxied  = false
+  zone_id = cloudflare_zone.fluidattacks_com.id
+  name    = "mandrill._domainkey.${cloudflare_zone.fluidattacks_com.zone}"
+  type    = "TXT"
+  value   = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCrLHiExVd55zd/IQ/J/mRwSRMAocV/hMB3jXwaHH36d9NaVynQFYV8NaWi69c1veUtRzGt7yAioXqLj7Z4TeEUoOLgrKsn8YnckGs9i3B3tVFB+Ch/4mPhXWiNfNdynHWBcPcbJ8kjEQ2U8y78dHZj1YeRXXVvWob2OaKynO8/lQIDAQAB;"
+  ttl     = 1
+  proxied = false
 }
 
 resource "cloudflare_record" "mailgun_smtp" {
-  zone_id  = cloudflare_zone.fluidattacks_com.id
-  name     = "smtp._domainkey.mailgun.${cloudflare_zone.fluidattacks_com.zone}"
-  type     = "TXT"
-  value    = "k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDWWMDVpf8LmPSxAzXN6maN9tmYF37+LNKt0ClL6xin8F5D6icNdvViPAFuZDUU8aAQPYacWHUPY0ay+95wt2XiGbpZsa7k4EPFYTdL2hfMNwaidDJKgL58kzBcfvR1r/VX3MPmiP0d6cQKqoDi+THtpqd2w270pgCCBKiYvujHmQIDAQAB"
-  ttl      = 1
-  proxied  = false
+  zone_id = cloudflare_zone.fluidattacks_com.id
+  name    = "smtp._domainkey.mailgun.${cloudflare_zone.fluidattacks_com.zone}"
+  type    = "TXT"
+  value   = "k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDWWMDVpf8LmPSxAzXN6maN9tmYF37+LNKt0ClL6xin8F5D6icNdvViPAFuZDUU8aAQPYacWHUPY0ay+95wt2XiGbpZsa7k4EPFYTdL2hfMNwaidDJKgL58kzBcfvR1r/VX3MPmiP0d6cQKqoDi+THtpqd2w270pgCCBKiYvujHmQIDAQAB"
+  ttl     = 1
+  proxied = false
 }
 
 resource "cloudflare_record" "zoho_verify_dkim" {
