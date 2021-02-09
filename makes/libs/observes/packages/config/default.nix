35736d3e30ad335ed595ec7f4e4007639209f259
Daniel Murcia <dmurcia@fluidattacks.com>
2021-02-09 11:25:43 -0500

    observes\refac(job): #3972.46 target redshift test
    - migrated job to makes
    - added dev package
    - use dev in linter
    
    
    Former-commit-id: 4053eee8e5dd231f2fdef641c2cb25b396bab324

--- makes/libs/observes/packages/config/default.nix
+++ makes/libs/observes/packages/config/default.nix
@@ -421,20 +421,37 @@ rec {
     python = {
       direct = [ ];
       inherited = [
-        "atomicwrites==1.4.0"
-        "attrs==20.3.0"
-        "mirakuru==2.3.0"
-        "more-itertools==8.6.0"
-        "packaging==20.8"
-        "pluggy==0.13.1"
-        "port-for==0.4"
-        "psutil==5.8.0"
         "psycopg2==2.8.4"
-        "py==1.10.0"
-        "pyparsing==2.4.7"
-        "pytest-postgresql==2.5.2"
-        "pytest==5.2.0"
-        "wcwidth==0.2.5"
+      ];
+    };
+    local = [
+      "postgresClient"
+      "singerIO"
+    ];
+    nix = [ ];
+  };
+
+  targetRedshiftDev = {
+    srcPath = targetRedshift.srcPath;
+    python = {
+      direct = mergeDeps [
+        targetRedshift.python.direct
+        [
+          "pytest==5.2.0"
+        ]
+      ];
+      inherited = mergeDeps [
+        targetRedshift.python.inherited
+        [
+          "atomicwrites==1.4.0"
+          "attrs==20.3.0"
+          "more-itertools==8.7.0"
+          "packaging==20.9"
+          "pluggy==0.13.1"
+          "py==1.10.0"
+          "pyparsing==2.4.7"
+          "wcwidth==0.2.5"
+        ]
       ];
     };
     local = [
