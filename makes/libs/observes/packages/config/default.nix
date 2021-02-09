769e73694ce2f0e3c4e867da097b2908a1e77227
Daniel Murcia <dmurcia@fluidattacks.com>
2021-02-09 09:28:56 -0500

    observes\refac(job): #3972.45 tap csv test
    - migrated job to makes
    - added dev package
    - use dev in linter
    
    
    Former-commit-id: 02386544a2b422ca9e3886a24b36d1b60db3b359

--- makes/libs/observes/packages/config/default.nix
+++ makes/libs/observes/packages/config/default.nix
@@ -321,6 +321,33 @@ rec {
     nix = [ ];
   };
 
+  tapCsvDev = {
+    srcPath = tapCsv.srcPath;
+    python = {
+      direct = mergeDeps [
+        tapCsv.python.direct
+        [
+          "pytest==5.2.0"
+        ]
+      ];
+      inherited = mergeDeps [
+        tapCsv.python.inherited
+        [
+          "atomicwrites==1.4.0"
+          "attrs==20.3.0"
+          "more-itertools==8.7.0"
+          "packaging==20.9"
+          "pluggy==0.13.1"
+          "py==1.10.0"
+          "pyparsing==2.4.7"
+          "wcwidth==0.2.5"
+        ]
+      ];
+    };
+    local = tapCsv.local;
+    nix = tapCsv.nix;
+  };
+
   tapFormstack = {
     srcPath = path "/observes/singer/tap_formstack";
     python = {
