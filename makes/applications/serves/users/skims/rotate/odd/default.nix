9a3c8084515d508cebe65176a1f9a6364d1846c4
Daniel Salazar <dsalazar@fluidattacks.com>
2021-02-11 18:31:34 -0500

    serves\feat(build): #4159.25 skims user rotation
    - Add new jobs
    - Remove old jobs
    - no-deltas-test
    
    
    Former-commit-id: 02dcd60af8dc20f538aa78feb85041cabc65749a

---
+++ makes/applications/serves/users/skims/rotate/odd/default.nix
@@ -0,0 +1,59 @@
+{ servesPkgs
+, path
+, ...
+} @ _:
+let
+  userRotateKeys = import (path "/makes/utils/user-rotate-keys") path servesPkgs;
+in
+userRotateKeys {
+  name = "serves-users-skims-rotate-odd";
+  product = "serves";
+  target = "serves/users/skims/terraform";
+  gitlab_project_id = "20741933";
+  keys = {
+    "aws_iam_access_key.skims_prod_key-1" = {
+      id = {
+        gitlab = {
+          id = "SKIMS_PROD_AWS_ACCESS_KEY_ID";
+          masked = true;
+          protected = true;
+        };
+        output = {
+          id = "skims_prod_secret_key_id-1";
+        };
+      };
+      secret = {
+        gitlab = {
+          id = "SKIMS_PROD_AWS_SECRET_ACCESS_KEY";
+          masked = true;
+          protected = true;
+        };
+        output = {
+          id = "skims_prod_secret_key-1";
+        };
+      };
+    };
+    "aws_iam_access_key.skims_dev_key-1" = {
+      id = {
+        gitlab = {
+          id = "SKIMS_DEV_AWS_ACCESS_KEY_ID";
+          masked = true;
+          protected = false;
+        };
+        output = {
+          id = "skims_dev_secret_key_id-1";
+        };
+      };
+      secret = {
+        gitlab = {
+          id = "SKIMS_DEV_AWS_SECRET_ACCESS_KEY";
+          masked = true;
+          protected = false;
+        };
+        output = {
+          id = "skims_dev_secret_key-1";
+        };
+      };
+    };
+  };
+}
