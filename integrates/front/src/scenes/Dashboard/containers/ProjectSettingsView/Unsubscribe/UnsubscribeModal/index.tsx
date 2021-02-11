6223d91f89e0e4a0f80c35a599a609b9b0a86105
Andres Aguirre <aaguirre@fluidattacks.com>
2021-02-11 15:48:29 -0500

    integrates\feat(front): #4115.8 unsubscribe modal
    - Added unsubscribe modal to confirm the unsubscription from a group
    
    
    Former-commit-id: 7dfe260d853b64ac43a00517b6bdd62466f1cc91

---
+++ integrates/front/src/scenes/Dashboard/containers/ProjectSettingsView/Unsubscribe/UnsubscribeModal/index.tsx
@@ -0,0 +1,106 @@
+import { Button } from "components/Button";
+import { Field } from "redux-form";
+import { GenericForm } from "scenes/Dashboard/components/GenericForm";
+import type { InjectedFormProps } from "redux-form";
+import { Modal } from "components/Modal";
+import React from "react";
+import { Text } from "utils/forms/fields";
+import { required } from "utils/validations";
+import { useTranslation } from "react-i18next";
+import {
+  Alert,
+  ButtonToolbar,
+  Col100,
+  ControlLabel,
+  FormGroup,
+  Row,
+} from "styles/styledComponents";
+
+interface IDeleteGroupModalProps {
+  groupName: string;
+  isOpen: boolean;
+  onClose: () => void;
+  onSubmit: (values: { confirmation: string }) => void;
+}
+
+const UnsubscribeModal: React.FC<IDeleteGroupModalProps> = (
+  props: IDeleteGroupModalProps
+): JSX.Element => {
+  const { groupName, isOpen, onClose, onSubmit } = props;
+  const { t } = useTranslation();
+
+  function formValidations(values: {
+    confirmation: string;
+  }): { confirmation?: string } {
+    return values.confirmation === groupName
+      ? {}
+      : {
+          confirmation: t(
+            "search_findings.services_table.errors.expected_group_name",
+            { groupName }
+          ),
+        };
+  }
+
+  return (
+    <React.StrictMode>
+      <Modal
+        headerTitle={t("search_findings.services_table.unsubscribe.title")}
+        open={isOpen}
+      >
+        <GenericForm
+          initialValues={{
+            confirmation: "",
+          }}
+          name={"unsubscribeFromGroup"}
+          onSubmit={onSubmit}
+          validate={formValidations}
+        >
+          {({ handleSubmit, valid }: InjectedFormProps): JSX.Element => (
+            <React.Fragment>
+              <ControlLabel>
+                {t("search_findings.services_table.unsubscribe.warning_title")}
+              </ControlLabel>
+              <Alert>
+                {t("search_findings.services_table.unsubscribe.warning_body")}
+              </Alert>
+              <FormGroup>
+                <ControlLabel>
+                  {t(
+                    "search_findings.services_table.unsubscribe.type_group_name"
+                  )}
+                </ControlLabel>
+                <Field
+                  component={Text}
+                  name={"confirmation"}
+                  placeholder={groupName.toLowerCase()}
+                  type={"text"}
+                  validate={required}
+                />
+              </FormGroup>
+              <hr />
+              <Row>
+                <Col100>
+                  <ButtonToolbar>
+                    <Button onClick={onClose}>
+                      {t("confirmmodal.cancel")}
+                    </Button>
+                    <Button
+                      disabled={!valid}
+                      onClick={handleSubmit}
+                      type={"submit"}
+                    >
+                      {t("confirmmodal.proceed")}
+                    </Button>
+                  </ButtonToolbar>
+                </Col100>
+              </Row>
+            </React.Fragment>
+          )}
+        </GenericForm>
+      </Modal>
+    </React.StrictMode>
+  );
+};
+
+export { UnsubscribeModal };
