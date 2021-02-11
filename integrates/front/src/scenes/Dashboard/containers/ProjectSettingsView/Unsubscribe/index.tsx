33e870b1a9d1cbd101eab4ef6f0b94151265ce47
Andres Aguirre <aaguirre@fluidattacks.com>
2021-02-11 17:40:28 -0500

    integrates\feat(front): #4115.9 unsubscribe section
    - Added the unsubscribe component which allows to unsubscribe from a
    group
    
    
    Former-commit-id: 989bd63905df0d03b780f964e00684909a30461b

---
+++ integrates/front/src/scenes/Dashboard/containers/ProjectSettingsView/Unsubscribe/index.tsx
@@ -0,0 +1,88 @@
+import type { ApolloError } from "apollo-client";
+import { Button } from "components/Button";
+import { Logger } from "utils/logger";
+import { UNSUBSCRIBE_FROM_GROUP_MUTATION } from "./UnsubscribeModal/queries";
+import { UnsubscribeModal } from "./UnsubscribeModal";
+import mixpanel from "mixpanel-browser";
+import { useMutation } from "@apollo/react-hooks";
+import { useTranslation } from "react-i18next";
+import {
+  ButtonToolbar,
+  Col40,
+  LastProjectSetting,
+  ProjectScopeText,
+  Row,
+} from "styles/styledComponents";
+import React, { useState } from "react";
+import { msgError, msgSuccess } from "utils/notifications";
+import { useHistory, useParams } from "react-router-dom";
+
+const Unsubscribe: React.FC = (): JSX.Element => {
+  const { projectName } = useParams<{ projectName: string }>();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { push } = useHistory();
+  const { t } = useTranslation();
+
+  const [unsubscribeFromGroupMutation] = useMutation(
+    UNSUBSCRIBE_FROM_GROUP_MUTATION,
+    {
+      onCompleted: (): void => {
+        msgSuccess(
+          t("search_findings.services_table.success"),
+          t("search_findings.services_table.success_title")
+        );
+
+        push("/home");
+      },
+      onError: (error: ApolloError): void => {
+        error.graphQLErrors.forEach((): void => {
+          Logger.warning("An error occurred unsubscribing from group", error);
+          msgError(t("group_alerts.error_textsad"));
+        });
+      },
+      variables: {
+        groupName: projectName,
+      },
+    }
+  );
+
+  function handleChange(): void {
+    setIsModalOpen(!isModalOpen);
+  }
+
+  function handleSubmit(): void {
+    mixpanel.track("UnsubscribeFromGroup");
+    void unsubscribeFromGroupMutation();
+    setIsModalOpen(!isModalOpen);
+  }
+
+  return (
+    <React.StrictMode>
+      <LastProjectSetting>
+        <Row>
+          <h3>{t("search_findings.services_table.unsubscribe.title")}</h3>
+        </Row>
+        <Row>
+          <ProjectScopeText>
+            {t("search_findings.services_table.unsubscribe.warning")}
+          </ProjectScopeText>
+          <Col40>
+            <ButtonToolbar>
+              <Button onClick={handleChange}>
+                {t("search_findings.services_table.unsubscribe.button")}
+              </Button>
+            </ButtonToolbar>
+          </Col40>
+        </Row>
+      </LastProjectSetting>
+      <UnsubscribeModal
+        groupName={projectName}
+        isOpen={isModalOpen}
+        onClose={handleChange}
+        onSubmit={handleSubmit}
+      />
+    </React.StrictMode>
+  );
+};
+
+export { Unsubscribe };
