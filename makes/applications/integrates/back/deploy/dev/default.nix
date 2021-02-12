2406a9cd28d988f62ecced33e04d6d3dcfe9894a
Kevin Amado <kamado@fluidattacks.com>
2021-02-12 12:30:55 -0500

    integrates\feat(build): #3836.233 ephemeral deploy
    - Update the ephemeral deployment to makes
    - Reduce the number of variables needed
    - Use envsubst instead of a sed in the for like before
      to make secret management simpler
    - Use stdin/stdout instead of files to prevent writing
      sensitive data to disk
    
    
    Former-commit-id: e5c3edd7d0a4771b4bedebfdb377878b83629a6c

--- makes/applications/integrates/back/deploy/dev/default.nix
+++ makes/applications/integrates/back/deploy/dev/default.nix
@@ -4,7 +4,19 @@
 , ...
 } @ _:
 makeEntrypoint integratesPkgs {
-  arguments = { };
+  arguments = {
+    envManifests = path "/makes/applications/integrates/back/deploy/dev/k8s";
+  };
+  searchPaths = {
+    envPaths = [
+      integratesPkgs.awscli
+      integratesPkgs.envsubst
+      integratesPkgs.kubectl
+    ];
+    envUtils = [
+      "/makes/utils/aws"
+    ];
+  };
   name = "integrates-back-deploy-dev";
   template = path "/makes/applications/integrates/back/deploy/dev/entrypoint.sh";
 }
--- makes/applications/integrates/back/deploy/dev/entrypoint.sh
+++ makes/applications/integrates/back/deploy/dev/entrypoint.sh
@@ -1,1 +1,38 @@
 # shellcheck shell=bash
+
+function apply {
+  envsubst -no-unset -no-empty -i "${1}" | kubectl apply -f -
+}
+
+function b64 {
+  echo -n "${1}" | base64 --wrap=0
+}
+
+function main {
+  export B64_CI_COMMIT_REF_NAME
+  export B64_CI_COMMIT_SHA
+  export B64_DEPLOYMENT_DATE
+  export B64_INTEGRATES_DEV_AWS_ACCESS_KEY_ID
+  export B64_INTEGRATES_DEV_AWS_SECRET_ACCESS_KEY
+
+      aws_login_dev integrates \
+  &&  aws_eks_update_kubeconfig 'integrates-cluster' 'us-east-1' \
+  &&  B64_CI_COMMIT_REF_NAME="$(b64 "${CI_COMMIT_REF_NAME}")" \
+  &&  B64_CI_COMMIT_SHA="$(b64 "${CI_COMMIT_SHA}")" \
+  &&  B64_DEPLOYMENT_DATE="$(b64 "$(date --utc --rfc-3339=seconds)")" \
+  &&  B64_INTEGRATES_DEV_AWS_ACCESS_KEY_ID="$(b64 "${INTEGRATES_DEV_AWS_ACCESS_KEY_ID}")" \
+  &&  B64_INTEGRATES_DEV_AWS_SECRET_ACCESS_KEY="$(b64 "${INTEGRATES_DEV_AWS_SECRET_ACCESS_KEY}")" \
+  &&  for manifest in __envManifests__/*
+      do
+            echo "[INFO] Applying: ${manifest}" \
+        &&  apply "${manifest}" \
+        ||  return 1
+      done \
+  &&  echo '[INFO] Rolling out update' \
+  &&  kubectl rollout status \
+        "deploy/integrates-${CI_COMMIT_REF_NAME}" \
+        -n "development" \
+        --timeout="10m"
+}
+
+main "${@}"
