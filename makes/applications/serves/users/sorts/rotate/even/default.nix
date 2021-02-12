678b38d7382631df4f0ed9590923e8cd3a3ea2bb
Daniel Salazar <dsalazar@fluidattacks.com>
2021-02-12 10:35:58 -0500

    serves\feat(build): #4159.26 sorts user rotation
    - Add new jobs
    - Remove old jobs
    - no-deltas-test
    
    
    Former-commit-id: 4604af7a0ffd10e3e8ac5cb27b90539d68d44a98

---
+++ makes/applications/serves/users/sorts/rotate/even/default.nix
@@ -0,0 +1,59 @@
+{ servesPkgs
+, path
+, ...
+} @ _:
+let
+  userRotateKeys = import (path "/makes/utils/user-rotate-keys") path servesPkgs;
+in
+userRotateKeys {
+  name = "serves-users-sorts-rotate-even";
+  product = "serves";
+  target = "serves/users/sorts/terraform";
+  gitlab_project_id = "20741933";
+  keys = {
+    "aws_iam_access_key.sorts_prod_key-2" = {
+      id = {
+        gitlab = {
+          id = "SORTS_PROD_AWS_ACCESS_KEY_ID";
+          masked = true;
+          protected = true;
+        };
+        output = {
+          id = "sorts_prod_access_key-2";
+        };
+      };
+      secret = {
+        gitlab = {
+          id = "SORTS_PROD_AWS_SECRET_ACCESS_KEY";
+          masked = true;
+          protected = true;
+        };
+        output = {
+          id = "sorts_prod_secret_key-2";
+        };
+      };
+    };
+    "aws_iam_access_key.sorts_dev_key-2" = {
+      id = {
+        gitlab = {
+          id = "SORTS_DEV_AWS_ACCESS_KEY_ID";
+          masked = true;
+          protected = false;
+        };
+        output = {
+          id = "sorts_dev_access_key-2";
+        };
+      };
+      secret = {
+        gitlab = {
+          id = "SORTS_DEV_AWS_SECRET_ACCESS_KEY";
+          masked = true;
+          protected = false;
+        };
+        output = {
+          id = "sorts_dev_secret_key-2";
+        };
+      };
+    };
+  };
+}
