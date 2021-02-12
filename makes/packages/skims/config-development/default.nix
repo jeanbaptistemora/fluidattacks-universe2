98fdccc3c85b6c6401edd9458f5a366f7c422777
Kevin Amado <kamado@fluidattacks.com>
2021-02-12 10:12:19 -0500

    skims\refac(build): #3836.228 use new search paths
    - In order to dependencies location
    
    
    Former-commit-id: 7d9ba1caeabe97f99a55b3b2843a1e8697303e16

--- makes/packages/skims/config-development/default.nix
+++ makes/packages/skims/config-development/default.nix
@@ -4,48 +4,48 @@
 } @ _:
 let
   buildPythonRequirements = import (path "/makes/utils/build-python-requirements") path skimsPkgs;
-  makeSearchPaths = import (path "/makes/utils/make-search-paths-deprecated") path skimsPkgs;
   makeTemplate = import (path "/makes/utils/make-template") path skimsPkgs;
+  pythonRequirements = buildPythonRequirements {
+    dependencies = [ ];
+    name = "skims-development";
+    requirements = {
+      direct = [
+        "bandit==1.6.2"
+        "pdoc3==0.8.4"
+        "pydeps==1.9.4"
+        "pytest-rerunfailures==9.0"
+        "pytest==5.4.3"
+      ];
+      inherited = [
+        "attrs==20.3.0"
+        "gitdb==4.0.5"
+        "GitPython==3.1.12"
+        "Mako==1.1.3"
+        "Markdown==3.3.3"
+        "MarkupSafe==1.1.1"
+        "more-itertools==8.6.0"
+        "packaging==20.8"
+        "pbr==5.5.1"
+        "pluggy==0.13.1"
+        "py==1.10.0"
+        "pyparsing==2.4.7"
+        "PyYAML==5.3.1"
+        "six==1.15.0"
+        "smmap==3.0.4"
+        "stdlib-list==0.8.0"
+        "stevedore==3.3.0"
+        "wcwidth==0.2.5"
+      ];
+    };
+    python = skimsPkgs.python38;
+  };
 in
 makeTemplate {
-  arguments = {
-    envSearchPaths = makeSearchPaths [ ];
-    envPythonRequirements = buildPythonRequirements {
-      dependencies = [ ];
-      name = "skims-development";
-      requirements = {
-        direct = [
-          "bandit==1.6.2"
-          "pdoc3==0.8.4"
-          "pydeps==1.9.4"
-          "pytest-rerunfailures==9.0"
-          "pytest==5.4.3"
-        ];
-        inherited = [
-          "attrs==20.3.0"
-          "gitdb==4.0.5"
-          "GitPython==3.1.12"
-          "Mako==1.1.3"
-          "Markdown==3.3.3"
-          "MarkupSafe==1.1.1"
-          "more-itertools==8.6.0"
-          "packaging==20.8"
-          "pbr==5.5.1"
-          "pluggy==0.13.1"
-          "py==1.10.0"
-          "pyparsing==2.4.7"
-          "PyYAML==5.3.1"
-          "six==1.15.0"
-          "smmap==3.0.4"
-          "stdlib-list==0.8.0"
-          "stevedore==3.3.0"
-          "wcwidth==0.2.5"
-        ];
-      };
-      python = skimsPkgs.python38;
-    };
-    envUtilsBashLibPython = path "/makes/utils/python/template.sh";
-  };
+  arguments = { };
   name = "skims-config-development";
+  searchPaths = {
+    envPaths = [ pythonRequirements ];
+    envPython38Paths = [ pythonRequirements ];
+  };
   template = path "/makes/packages/skims/config-development/template.sh";
 }
--- makes/packages/skims/config-development/template.sh
+++ makes/packages/skims/config-development/template.sh
@@ -1,12 +1,1 @@
 # shellcheck shell=bash
-
-source '__envSearchPaths__'
-source '__envUtilsBashLibPython__'
-
-function skims_setup_development {
-      make_python_path '3.8' \
-        '__envPythonRequirements__' \
-
-}
-
-skims_setup_development
