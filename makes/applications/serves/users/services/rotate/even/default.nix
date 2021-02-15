9c6e6ef2b7a68fd79bed729b816828f70c4f7a85
Daniel Salazar <dsalazar@fluidattacks.com>
2021-02-15 12:21:21 -0500

    serves\feat(build): #4159.30 services user rotation
    - Add new jobs
    - Remove old jobs
    - no-deltas-test
    
    
    Former-commit-id: 216edc40983a87fdf86208ec4d613a52d7343724

---
+++ makes/applications/serves/users/services/rotate/even/default.nix
@@ -0,0 +1,94 @@
+{ servesPkgs
+, path
+, ...
+} @ _:
+let
+  userRotateKeys = import (path "/makes/utils/user-rotate-keys") path servesPkgs;
+in
+userRotateKeys {
+  name = "serves-users-services-rotate-even";
+  product = "serves";
+  target = "serves/users/services/terraform";
+  keys = {
+    "aws_iam_access_key.continuous-prod-key-2" = {
+      id = {
+        gitlab = [
+          {
+            project_id = "20741933";
+            key_id = "SERVICES_PROD_AWS_ACCESS_KEY_ID";
+            masked = true;
+            protected = true;
+          }
+          {
+            project_id = "4603023";
+            key_id = "PROD_AWS_ACCESS_KEY_ID";
+            masked = true;
+            protected = true;
+          }
+        ];
+        output = {
+          id = "continuous-prod-secret-key-id-2";
+        };
+      };
+      secret = {
+        gitlab = [
+          {
+            project_id = "20741933";
+            key_id = "SERVICES_PROD_AWS_SECRET_ACCESS_KEY";
+            masked = true;
+            protected = true;
+          }
+          {
+            project_id = "4603023";
+            key_id = "PROD_AWS_SECRET_ACCESS_KEY";
+            masked = true;
+            protected = true;
+          }
+        ];
+        output = {
+          id = "continuous-prod-secret-key-2";
+        };
+      };
+    };
+    "aws_iam_access_key.continuous-dev-key-2" = {
+      id = {
+        gitlab = [
+          {
+            project_id = "20741933";
+            key_id = "SERVICES_DEV_AWS_ACCESS_KEY_ID";
+            masked = true;
+            protected = false;
+          }
+          {
+            project_id = "4603023";
+            key_id = "DEV_AWS_ACCESS_KEY_ID";
+            masked = true;
+            protected = false;
+          }
+        ];
+        output = {
+          id = "continuous-dev-secret-key-id-2";
+        };
+      };
+      secret = {
+        gitlab = [
+          {
+            project_id = "20741933";
+            key_id = "SERVICES_DEV_AWS_SECRET_ACCESS_KEY";
+            masked = true;
+            protected = false;
+          }
+          {
+            project_id = "4603023";
+            key_id = "DEV_AWS_SECRET_ACCESS_KEY";
+            masked = true;
+            protected = false;
+          }
+        ];
+        output = {
+          id = "continuous-dev-secret-key-2";
+        };
+      };
+    };
+  };
+}
