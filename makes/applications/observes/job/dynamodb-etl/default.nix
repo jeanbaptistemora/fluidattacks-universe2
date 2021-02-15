367b03a33efd33b76ee564e40b91b7bf59abbc75
Daniel Murcia <dmurcia@fluidattacks.com>
2021-02-15 14:16:14 -0500

    observes\refac(job): #3972.63 stmr dynamo
    - mirgated job to makes
    
    
    Former-commit-id: 021c7b1b9dffe895fb042124679f62a755ecf366

---
+++ makes/applications/observes/job/dynamodb-etl/default.nix
@@ -0,0 +1,25 @@
+{ makeEntrypoint
+, observesPkgs
+, packages
+, path
+, ...
+} @ _:
+let
+  nixPkgs = observesPkgs;
+in
+makeEntrypoint nixPkgs {
+  arguments = { };
+  searchPaths = {
+    envPaths = [
+      packages."observes/streamer-dynamodb"
+      packages."observes/tap-json"
+      packages."observes/target-redshift"
+    ];
+    envUtils = [
+      "/makes/utils/aws"
+      "/makes/utils/sops"
+    ];
+  };
+  name = "observes-job-dynamodb-etl";
+  template = path "/makes/applications/observes/job/dynamodb-etl/entrypoint.sh";
+}
---
+++ makes/applications/observes/job/dynamodb-etl/entrypoint.sh
@@ -0,0 +1,44 @@
+# shellcheck shell=bash
+
+function dynamodb_etl {
+      local conf="${1}"
+      local schema="${2}"
+      local db_creds
+      local dynamo_creds
+
+      db_creds=$(mktemp) \
+  &&  dynamo_creds=$(mktemp) \
+  &&  aws_login_prod 'observes' \
+  &&  sops_export_vars 'observes/secrets-prod.yaml' \
+        analytics_aws_access_key \
+        analytics_aws_secret_key \
+        analytics_aws_default_region \
+        analytics_auth_redshift \
+  &&  echo '[INFO] Generating secret files' \
+  &&  {
+        echo '{'
+        echo "\"AWS_ACCESS_KEY_ID\":\"${analytics_aws_access_key}\","
+        echo "\"AWS_SECRET_ACCESS_KEY\":\"${analytics_aws_secret_key}\","
+        echo "\"AWS_DEFAULT_REGION\":\"${analytics_aws_default_region}\""
+        echo '}'
+      } > "${dynamo_creds}" \
+  &&  echo "${analytics_auth_redshift}" > "${db_creds}" \
+  &&  echo '[INFO] Running streamer' \
+  &&  mkdir ./logs \
+  &&  observes-streamer-dynamodb \
+        --auth "${dynamo_creds}" \
+        --conf "${conf}" > .stream \
+  &&  echo '[INFO] Running tap' \
+  &&  observes-tap-json \
+      --date-formats '%Y-%m-%d %H:%M:%S' \
+      < .stream \
+      > .singer \
+  &&  echo '[INFO] Running target' \
+  &&  observes-target-redshift \
+        --auth "${db_creds}" \
+        --drop-schema \
+        --schema-name "${schema}" \
+        < .singer
+}
+
+dynamodb_etl "${@}"
