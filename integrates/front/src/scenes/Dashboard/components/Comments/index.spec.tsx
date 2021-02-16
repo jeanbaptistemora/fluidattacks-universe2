b5c879d623c3a246a8dcc971323f7b9cc2cd0344
Jonathan Mesa <jmesa@fluidattacks.com>
2021-02-16 10:41:37 -0500

    integrates\refac(front): #2705.19 update comments
    - Lint comments test
    
    
    Former-commit-id: d6793d38bee482f749e4293cb4ff6152c9a07544

--- integrates/front/src/scenes/Dashboard/components/Comments/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/components/Comments/index.spec.tsx
@@ -1,26 +1,27 @@
-import { shallow, ShallowWrapper } from "enzyme";
-import React from "react";
 import { Comments } from "scenes/Dashboard/components/Comments";
+import React from "react";
+import type { ShallowWrapper } from "enzyme";
+import { shallow } from "enzyme";
 
-const functionMock: (() => void) = (): void => undefined;
+const functionMock: () => void = (): void => undefined;
 
-describe("Comments Box", () => {
-
-  it("should return a function", () => {
-    expect(typeof (Comments))
-      .toEqual("function");
+describe("Comments Box", (): void => {
+  it("should return a function", (): void => {
+    expect.hasAssertions();
+    expect(typeof Comments).toStrictEqual("function");
   });
 
-  it("should render", () => {
+  it("should render", (): void => {
+    expect.hasAssertions();
+
     const wrapper: ShallowWrapper = shallow(
       <Comments
-        id="comments-test"
+        id={"comments-test"}
         onLoad={functionMock}
         onPostComment={functionMock}
-      />,
+      />
     );
 
-    expect(wrapper.contains(<div id="comments-test" />))
-      .toBeTruthy();
+    expect(wrapper.contains(<div id={"comments-test"} />)).toBe(true);
   });
 });
