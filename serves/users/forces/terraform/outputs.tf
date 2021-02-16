bcbf354b851f5e7ceb43a7a17bc9ec7be0baf170
Daniel Salazar <dsalazar@fluidattacks.com>
2021-02-16 10:27:59 -0500

    serves\feat(infra): #4159.41 forces extra keys
    - Add extra keys for forces user
    
    
    Former-commit-id: b20c394934b0391061b62263a096b5ce6a3067ec

--- serves/users/forces/terraform/outputs.tf
+++ serves/users/forces/terraform/outputs.tf
@@ -8,6 +8,16 @@ output "forces_prod_secret_key-1" {
   value     = aws_iam_access_key.forces_prod_key-1.secret
 }
 
+output "forces_prod_secret_key_id-2" {
+  sensitive = true
+  value     = aws_iam_access_key.forces_prod_key-2.id
+}
+
+output "forces_prod_secret_key-2" {
+  sensitive = true
+  value     = aws_iam_access_key.forces_prod_key-2.secret
+}
+
 output "forces_dev_secret_key_id-1" {
   sensitive = true
   value     = aws_iam_access_key.forces_dev_key-1.id
@@ -16,4 +26,14 @@ output "forces_dev_secret_key_id-1" {
 output "forces_dev_secret_key-1" {
   sensitive = true
   value     = aws_iam_access_key.forces_dev_key-1.secret
+}
+
+output "forces_dev_secret_key_id-2" {
+  sensitive = true
+  value     = aws_iam_access_key.forces_dev_key-2.id
+}
+
+output "forces_dev_secret_key-2" {
+  sensitive = true
+  value     = aws_iam_access_key.forces_dev_key-2.secret
 }
--- serves/users/forces/terraform/user_keys.tf
+++ serves/users/forces/terraform/user_keys.tf
@@ -2,6 +2,14 @@ resource "aws_iam_access_key" "forces_prod_key-1" {
   user = "forces_prod"
 }
 
+resource "aws_iam_access_key" "forces_prod_key-2" {
+  user = "forces_prod"
+}
+
 resource "aws_iam_access_key" "forces_dev_key-1" {
+  user = "forces_dev"
+}
+
+resource "aws_iam_access_key" "forces_dev_key-2" {
   user = "forces_dev"
 }
