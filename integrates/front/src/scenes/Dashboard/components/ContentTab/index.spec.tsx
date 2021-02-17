96bb17b785631eecc25d9db5317f005d060bfabd
Jonathan Mesa <jmesa@fluidattacks.com>
2021-02-17 13:45:41 -0500

    integrates\refac(front): #2705.21 update contenttab
    - Lint contenttab test
    
    
    Former-commit-id: 5b5c92103fb840b75cebfd7b556cb43c269e2a92

--- integrates/front/src/scenes/Dashboard/components/ContentTab/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/components/ContentTab/index.spec.tsx
@@ -1,33 +1,31 @@
-import { mount, ReactWrapper } from "enzyme";
-import React from "react";
-import { MemoryRouter } from "react-router";
 import { ContentTab } from "scenes/Dashboard/components/ContentTab";
+import { MemoryRouter } from "react-router";
+import React from "react";
+import type { ReactWrapper } from "enzyme";
+import { mount } from "enzyme";
 
-describe("ContentTab", () => {
-
+describe("ContentTab", (): void => {
   it("should return a function", (): void => {
-    expect(typeof (ContentTab))
-      .toEqual("function");
+    expect.hasAssertions();
+    expect(typeof ContentTab).toStrictEqual("function");
   });
 
-  it("should render a component", async () => {
+  it("should render a component", (): void => {
+    expect.hasAssertions();
+
     const wrapper: ReactWrapper = mount(
       <MemoryRouter>
         <ContentTab
-          icon="test-icon"
-          id="test-id"
-          link="test-link"
-          title="Tab-Title"
-          tooltip="Tab-Tooltip"
+          icon={"test-icon"}
+          id={"test-id"}
+          link={"test-link"}
+          title={"Tab-Title"}
+          tooltip={"Tab-Tooltip"}
         />
-      </MemoryRouter>,
+      </MemoryRouter>
     );
-    expect(wrapper.find("#test-id")
-        .hostNodes())
-      .toHaveLength(1);
-    expect(wrapper.find("#test-id")
-        .at(0)
-        .text())
-      .toContain("Tab-Title");
+
+    expect(wrapper.find("#test-id").hostNodes()).toHaveLength(1);
+    expect(wrapper.find("#test-id").at(0).text()).toContain("Tab-Title");
   });
 });
