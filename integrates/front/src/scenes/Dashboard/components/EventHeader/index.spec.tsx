16b74358ddab921f8e8b8ba91c8b3b03a01443fb
Jonathan Mesa <jmesa@fluidattacks.com>
2021-02-18 12:43:55 -0500

    integrates\refac(front): #2705.24 update eventheader
    - Lint eventheader test
    
    
    Former-commit-id: 8969770eabcde4d65050a98b8c7b52a46fd15e1b

--- integrates/front/src/scenes/Dashboard/components/EventHeader/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/components/EventHeader/index.spec.tsx
@@ -1,39 +1,54 @@
-import { shallow, ShallowWrapper } from "enzyme";
-import * as React from "react";
-import { EventHeader, IEventHeaderProps } from "scenes/Dashboard/components/EventHeader";
+import { EventHeader } from "scenes/Dashboard/components/EventHeader";
+import type { IEventHeaderProps } from "scenes/Dashboard/components/EventHeader";
+import React from "react";
+import type { ShallowWrapper } from "enzyme";
+import { shallow } from "enzyme";
 
-describe("EventHeader", () => {
-
-  it("should return a function", () => {
-    expect(typeof (EventHeader))
-      .toEqual("function");
+describe("EventHeader", (): void => {
+  it("should return a function", (): void => {
+    expect.hasAssertions();
+    expect(typeof EventHeader).toStrictEqual("function");
   });
 
-  it("should render event header with evidence", () => {
-    const mockProps: IEventHeaderProps  = {
+  it("should render event header with evidence", (): void => {
+    expect.hasAssertions();
+
+    const mockProps: IEventHeaderProps = {
       eventDate: "",
       eventStatus: "",
       eventType: "",
       id: "",
     };
     const wrapper: ShallowWrapper = shallow(
-      <EventHeader {...mockProps}/>,
+      <EventHeader
+        eventDate={mockProps.eventDate}
+        eventStatus={mockProps.eventStatus}
+        eventType={mockProps.eventType}
+        id={mockProps.id}
+      />
     );
-    expect(wrapper)
-      .toHaveLength(1);
+
+    expect(wrapper).toHaveLength(1);
   });
 
-  it("should render event header without evidence", () => {
-    const mockProps: IEventHeaderProps  = {
+  it("should render event header without evidence", (): void => {
+    expect.hasAssertions();
+
+    const mockProps: IEventHeaderProps = {
       eventDate: "",
       eventStatus: "",
       eventType: "",
       id: "",
     };
     const wrapper: ShallowWrapper = shallow(
-      <EventHeader {...mockProps}/>,
+      <EventHeader
+        eventDate={mockProps.eventDate}
+        eventStatus={mockProps.eventStatus}
+        eventType={mockProps.eventType}
+        id={mockProps.id}
+      />
     );
-    expect(wrapper)
-      .toHaveLength(1);
+
+    expect(wrapper).toHaveLength(1);
   });
 });
