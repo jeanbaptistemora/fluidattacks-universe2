5278e24b0e4a89fd33bd140173c89f25f415c6f3
Jonathan Mesa <jmesa@fluidattacks.com>
2021-02-18 13:34:38 -0500

    integrates\refac(front): #2705.25 update contenttab
    - Lint contenttab
    
    
    Former-commit-id: 8c2f50b5d4effbb68608da7fb34dcce124d4c621

--- integrates/front/src/scenes/Dashboard/components/ContentTab/index.tsx
+++ integrates/front/src/scenes/Dashboard/components/ContentTab/index.tsx
@@ -1,10 +1,10 @@
-import _ from "lodash";
-import React from "react";
+import { Badge } from "components/Badge";
+import type { IBadgeProps } from "components/Badge";
 import { NavLink } from "react-router-dom";
-
-import { Badge, IBadgeProps } from "components/Badge";
-import { TooltipWrapper } from "components/TooltipWrapper";
+import React from "react";
 import { Tab } from "styles/styledComponents";
+import { TooltipWrapper } from "components/TooltipWrapper";
+import _ from "lodash";
 
 interface IContentTabProps {
   icon: string;
@@ -19,18 +19,20 @@ interface IContentTabProps {
 }
 
 const contentTab: React.FC<IContentTabProps> = (
-  props: IContentTabProps,
+  props: IContentTabProps
 ): JSX.Element => {
-  const { tooltip, id, icon, title, plus } = props;
+  const { tooltip, id, icon, title, plus, link } = props;
 
   return (
     <TooltipWrapper id={`${id}Tooltip`} message={tooltip}>
       <Tab id={id}>
-        <NavLink activeClassName={"nav-active-bg"} to={props.link}>
+        <NavLink activeClassName={"nav-active-bg"} to={link}>
           <i className={icon} />
           &nbsp;{title}
           {!_.isUndefined(plus) && plus.visible ? (
-            <Badge {...plus.config}>pro</Badge>
+            // Next annotation needed so no new React elements have to be created
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            <Badge {...plus.config}>{"pro"}</Badge>
           ) : undefined}
         </NavLink>
       </Tab>
