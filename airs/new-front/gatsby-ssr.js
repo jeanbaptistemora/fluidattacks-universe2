c9e75f3295ae0c751cebade15931a7c206263059
John Perez <jperez@fluidattacks.com>
2021-02-18 15:52:04 -0500

    airs\feat(front): #3905.26 add scripts
    - Create `gatsby-ssr.js` file to modify the `head` and `body` parts of
    the html to add external scripts
    - Added Zoho CRM Live chat script
    - Added Cloudflare Web Analytics script
    
    
    Former-commit-id: 3a2d45d644b41f1511d0754b5747db275adcc99f

---
+++ airs/new-front/gatsby-ssr.js
@@ -0,0 +1,28 @@
+import React from "react";
+export const onRenderBody = (
+  { setHeadComponents, setPostBodyComponents },
+  pluginOptions
+) => {
+  setHeadComponents([
+    // Cloudflare Web Analytics
+    <script
+      defer
+      src="https://static.cloudflareinsights.com/beacon.min.js"
+      data-cf-beacon='{"token": "f4f99c985c414a5591e8077bf301b39b"}'
+    />,
+  ]);
+  setPostBodyComponents([
+    // Zoho CRM Live Chat
+    <script
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `
+        var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq ||
+        {widgetcode:"50094df2b302da078befa3cee2e8de00943ab0089a71d1aa34f6df9b6cb54ae5feb79731b60e202192c2895c9acefb61", values:{},ready:function(){}};
+        var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;
+        s.src="https://salesiq.zoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);d.write("<div id='zsiqwidget'></div>");
+      `,
+      }}
+    />,
+  ]);
+};
