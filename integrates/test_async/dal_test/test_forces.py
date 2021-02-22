eb97fa7c52af33f44c248d9e92b5dce423bf5522
Diego Restrepo <drestrepo@fluidattacks.com>
2021-02-22 15:32:42 -0500

    integrates\test(back): #4124.24 token test
    - check if tokens are being stored
    - add dependencies
    
    
    Former-commit-id: 412f80fff51345cf4e153c433d1a20eab330863d

--- integrates/test_async/dal_test/test_forces.py
+++ integrates/test_async/dal_test/test_forces.py
@@ -1,8 +1,12 @@
 import pytest
-
+from moto import mock_secretsmanager
+from botocore.exceptions import ClientError
 from backend.dal.forces import (
-    create_execution
+    create_execution,
+    update_secret_token,
+    get_secret_token,
 )
+
 from backend.utils import (
     datetime as datetime_utils,
 )
@@ -15,8 +19,18 @@ async def test_create_execution():
     group = "unittesting"
     execution_id = "random_id"
     now = datetime_utils.get_now()
-    assert await create_execution (
+    assert await create_execution(
         project_name=group,
         execution_id=execution_id,
-        date=now
+        date=now,
     )
+
+
+async def test_update_secret_token() -> None:
+    with mock_secretsmanager():
+        token = 'mock token'
+        result = await update_secret_token('unittesting', token)
+        assert result
+        assert await get_secret_token('unittesting') == token
+
+        assert await get_secret_token('unknown') is None
