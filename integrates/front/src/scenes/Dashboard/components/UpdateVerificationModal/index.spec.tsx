822c52c22796cfdda98598da00719be787d37ae6
Jonathan Mesa <jmesa@fluidattacks.com>
2021-02-23 16:47:04 -0500

    integrates\refac(front): #2705.49 update updateverifimodal
    - Lint updateverificationmodal test
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: f0f83dc9c9fde19cf17f0a5025eb1b9217ac8921

--- integrates/front/src/scenes/Dashboard/components/UpdateVerificationModal/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/components/UpdateVerificationModal/index.spec.tsx
@@ -1,22 +1,22 @@
-import { MockedProvider, MockedResponse } from "@apollo/react-testing";
-import { mount, ReactWrapper } from "enzyme";
+import { GET_FINDING_HEADER } from "scenes/Dashboard/containers/FindingContent/queries";
+import { GET_FINDING_VULN_INFO } from "scenes/Dashboard/containers/VulnerabilitiesView/queries";
 import { GraphQLError } from "graphql";
-import React from "react";
-// tslint:disable-next-line: no-submodule-imports
-import { act } from "react-dom/test-utils";
+import { MockedProvider } from "@apollo/react-testing";
+import type { MockedResponse } from "@apollo/react-testing";
 import { Provider } from "react-redux";
-import wait from "waait";
-
+import React from "react";
+import type { ReactWrapper } from "enzyme";
 import { UpdateVerificationModal } from "scenes/Dashboard/components/UpdateVerificationModal";
+import { act } from "react-dom/test-utils";
+import { mount } from "enzyme";
+import store from "store";
+import wait from "waait";
 import {
   REQUEST_VERIFICATION_VULN,
   VERIFY_VULNERABILITIES,
 } from "scenes/Dashboard/components/UpdateVerificationModal/queries";
-import { GET_FINDING_HEADER } from "scenes/Dashboard/containers/FindingContent/queries";
-import { GET_FINDING_VULN_INFO } from "scenes/Dashboard/containers/VulnerabilitiesView/queries";
-import store from "store";
 
-describe("update verification component", () => {
+describe("update verification component", (): void => {
   const mocksVulns: MockedResponse = {
     request: {
       query: GET_FINDING_VULN_INFO,
@@ -40,7 +40,9 @@ describe("update verification component", () => {
     },
   };
 
-  it("should handle request verification", async () => {
+  it("should handle request verification", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const handleOnClose: jest.Mock = jest.fn();
     const handleRequestState: jest.Mock = jest.fn();
     const handleRefetchData: jest.Mock = jest.fn();
@@ -50,105 +52,134 @@ describe("update verification component", () => {
           query: REQUEST_VERIFICATION_VULN,
           variables: {
             findingId: "",
-            justification: "This is a commenting test of a request verification in vulns",
+            justification:
+              "This is a commenting test of a request verification in vulns",
             vulnerabilities: ["test"],
           },
         },
-        result: { data: { requestVerificationVuln : { success: true } } },
+        result: { data: { requestVerificationVuln: { success: true } } },
       },
       mocksVulns,
     ];
     const wrapperRequest: ReactWrapper = mount(
       <Provider store={store}>
-        <MockedProvider mocks={mocksMutation} addTypename={false}>
+        <MockedProvider addTypename={false} mocks={mocksMutation}>
           <UpdateVerificationModal
+            clearSelected={jest.fn()}
             findingId={""}
             groupName={""}
+            handleCloseModal={handleOnClose}
             isReattacking={true}
             isVerifying={false}
-            vulns={[{currentState: "open", id: "test", specific: "", where: ""}]}
-            clearSelected={jest.fn()}
             refetchData={handleRefetchData}
-            handleCloseModal={handleOnClose}
             setRequestState={handleRequestState}
             setVerifyState={jest.fn()}
+            vulns={[
+              { currentState: "open", id: "test", specific: "", where: "" },
+            ]}
           />
         </MockedProvider>
-      </Provider>,
+      </Provider>
     );
     const justification: ReactWrapper = wrapperRequest.find("textarea");
-    justification.simulate("change", { target:
-      { value: "This is a commenting test of a request verification in vulns" } });
+    justification.simulate("change", {
+      target: {
+        value: "This is a commenting test of a request verification in vulns",
+      },
+    });
     const form: ReactWrapper = wrapperRequest.find("form");
-    form.at(0)
-      .simulate("submit");
-    await act(async () => { await wait(0); wrapperRequest.update(); });
-    expect(wrapperRequest)
-      .toHaveLength(1);
-    expect(handleOnClose)
-      .toHaveBeenCalled();
-    expect(handleRequestState)
-      .toHaveBeenCalled();
-    expect(handleRefetchData)
-      .toHaveBeenCalled();
+    form.at(0).simulate("submit");
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapperRequest.update();
+      }
+    );
+
+    expect(wrapperRequest).toHaveLength(1);
+    expect(handleOnClose).toHaveBeenCalled(); // eslint-disable-line jest/prefer-called-with
+    expect(handleRequestState).toHaveBeenCalled(); // eslint-disable-line jest/prefer-called-with
+    expect(handleRefetchData).toHaveBeenCalled(); // eslint-disable-line jest/prefer-called-with
   });
 
-  it("should handle request verification error", async () => {
+  it("should handle request verification error", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const handleOnClose: jest.Mock = jest.fn();
     const handleRequestState: jest.Mock = jest.fn();
     const mocksMutation: MockedResponse[] = [
-    {
-      request: {
-        query: REQUEST_VERIFICATION_VULN,
-        variables: {
-          findingId: "",
-          justification: "This is a commenting test of a request verification in vulns",
-          vulnerabilities: ["test_error"] },
+      {
+        request: {
+          query: REQUEST_VERIFICATION_VULN,
+          variables: {
+            findingId: "",
+            justification:
+              "This is a commenting test of a request verification in vulns",
+            vulnerabilities: ["test_error"],
+          },
+        },
+        result: {
+          errors: [
+            new GraphQLError(
+              "Exception - Request verification already requested"
+            ),
+            new GraphQLError(
+              "Exception - The vulnerability has already been closed"
+            ),
+            new GraphQLError("Exception - Vulnerability not found"),
+            new GraphQLError("Unexpected error"),
+          ],
+        },
       },
-      result: {
-        errors: [
-          new GraphQLError("Exception - Request verification already requested"),
-          new GraphQLError("Exception - The vulnerability has already been closed"),
-          new GraphQLError("Exception - Vulnerability not found"),
-          new GraphQLError("Unexpected error"),
-        ],
-      },
-    }];
+    ];
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
-        <MockedProvider mocks={mocksMutation} addTypename={false}>
+        <MockedProvider addTypename={false} mocks={mocksMutation}>
           <UpdateVerificationModal
+            clearSelected={jest.fn()}
             findingId={""}
             groupName={""}
+            handleCloseModal={handleOnClose}
             isReattacking={true}
             isVerifying={false}
-            vulns={[{currentState: "open", id: "test_error", specific: "", where: ""}]}
-            clearSelected={jest.fn()}
             refetchData={jest.fn()}
-            handleCloseModal={handleOnClose}
             setRequestState={handleRequestState}
             setVerifyState={jest.fn()}
+            vulns={[
+              {
+                currentState: "open",
+                id: "test_error",
+                specific: "",
+                where: "",
+              },
+            ]}
           />
         </MockedProvider>
-      </Provider>,
+      </Provider>
     );
     const justification: ReactWrapper = wrapper.find("textarea");
-    justification.simulate("change", { target:
-      { value: "This is a commenting test of a request verification in vulns" } });
+    justification.simulate("change", {
+      target: {
+        value: "This is a commenting test of a request verification in vulns",
+      },
+    });
     const form: ReactWrapper = wrapper.find("form");
-    form.at(0)
-      .simulate("submit");
-    await act(async () => { await wait(0); wrapper.update(); });
-    expect(wrapper)
-      .toHaveLength(1);
-    expect(handleOnClose)
-      .toHaveBeenCalled();
-    expect(handleRequestState)
-      .not
-      .toHaveBeenCalled();
+    form.at(0).simulate("submit");
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+
+    expect(wrapper).toHaveLength(1);
+    expect(handleOnClose).toHaveBeenCalled(); // eslint-disable-line jest/prefer-called-with
+    expect(handleRequestState).not.toHaveBeenCalled();
   });
 
-  it("should handle verify a request", async () => {
+  it("should handle verify a request", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const handleOnClose: jest.Mock = jest.fn();
     const handleVerifyState: jest.Mock = jest.fn();
     const handleRefetchData: jest.Mock = jest.fn();
@@ -159,11 +190,12 @@ describe("update verification component", () => {
           variables: {
             closedVulns: ["test"],
             findingId: "",
-            justification: "This is a commenting test of a verifying request verification in vulns",
+            justification:
+              "This is a commenting test of a verifying request verification in vulns",
             openVulns: [],
           },
         },
-        result: { data: { verifyRequestVuln : { success: true } } },
+        result: { data: { verifyRequestVuln: { success: true } } },
       },
       {
         request: {
@@ -194,96 +226,121 @@ describe("update verification component", () => {
     ];
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
-        <MockedProvider mocks={mocksMutation} addTypename={false}>
+        <MockedProvider addTypename={false} mocks={mocksMutation}>
           <UpdateVerificationModal
+            clearSelected={jest.fn()}
             findingId={""}
             groupName={""}
+            handleCloseModal={handleOnClose}
             isReattacking={false}
             isVerifying={true}
-            vulns={[{currentState: "open", id: "test", specific: "", where: ""}]}
-            clearSelected={jest.fn()}
             refetchData={handleRefetchData}
-            handleCloseModal={handleOnClose}
             setRequestState={jest.fn()}
             setVerifyState={handleVerifyState}
+            vulns={[
+              { currentState: "open", id: "test", specific: "", where: "" },
+            ]}
           />
         </MockedProvider>
-      </Provider>,
+      </Provider>
     );
     const justification: ReactWrapper = wrapper.find("textarea");
-    justification.simulate("change", { target:
-      { value: "This is a commenting test of a verifying request verification in vulns" } });
-    const switchButton: ReactWrapper = wrapper.find("BootstrapTable")
+    justification.simulate("change", {
+      target: {
+        value:
+          "This is a commenting test of a verifying request verification in vulns",
+      },
+    });
+    const switchButton: ReactWrapper = wrapper
+      .find("BootstrapTable")
       .find("e")
       .find("div")
       .first();
     switchButton.simulate("click");
     const form: ReactWrapper = wrapper.find("form");
-    form.at(0)
-      .simulate("submit");
-    await act(async () => { await wait(0); wrapper.update(); });
-    expect(wrapper)
-      .toHaveLength(1);
-    expect(handleOnClose)
-      .toHaveBeenCalled();
-    expect(handleVerifyState)
-      .toHaveBeenCalled();
-    expect(handleRefetchData)
-      .toHaveBeenCalled();
+    form.at(0).simulate("submit");
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+
+    expect(wrapper).toHaveLength(1);
+    expect(handleOnClose).toHaveBeenCalled(); // eslint-disable-line jest/prefer-called-with
+    expect(handleVerifyState).toHaveBeenCalled(); // eslint-disable-line jest/prefer-called-with
+    expect(handleRefetchData).toHaveBeenCalled(); // eslint-disable-line jest/prefer-called-with
   });
 
-  it("should handle verify a request error", async () => {
+  it("should handle verify a request error", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const handleOnClose: jest.Mock = jest.fn();
     const handleVerifyState: jest.Mock = jest.fn();
-    const mocksMutation: MockedResponse[] = [{
-      request: {
-        query: VERIFY_VULNERABILITIES,
-        variables: {
-          closedVulns: [],
-          findingId: "",
-          justification: "This is a commenting test of a verifying request verification in vulns",
-          openVulns: ["test_error"],
+    const mocksMutation: MockedResponse[] = [
+      {
+        request: {
+          query: VERIFY_VULNERABILITIES,
+          variables: {
+            closedVulns: [],
+            findingId: "",
+            justification:
+              "This is a commenting test of a verifying request verification in vulns",
+            openVulns: ["test_error"],
+          },
+        },
+        result: {
+          errors: [
+            new GraphQLError("Exception - Error verification not requested"),
+            new GraphQLError("Exception - Vulnerability not found"),
+            new GraphQLError("Unexpected error"),
+          ],
         },
       },
-      result: {
-        errors: [
-          new GraphQLError("Exception - Error verification not requested"),
-          new GraphQLError("Exception - Vulnerability not found"),
-          new GraphQLError("Unexpected error"),
-        ],
-      },
-    }];
+    ];
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
-        <MockedProvider mocks={mocksMutation} addTypename={false}>
+        <MockedProvider addTypename={false} mocks={mocksMutation}>
           <UpdateVerificationModal
+            clearSelected={jest.fn()}
             findingId={""}
             groupName={""}
+            handleCloseModal={handleOnClose}
             isReattacking={false}
             isVerifying={true}
-            vulns={[{currentState: "open", id: "test_error", specific: "", where: ""}]}
-            clearSelected={jest.fn()}
             refetchData={jest.fn()}
-            handleCloseModal={handleOnClose}
             setRequestState={jest.fn()}
             setVerifyState={handleVerifyState}
+            vulns={[
+              {
+                currentState: "open",
+                id: "test_error",
+                specific: "",
+                where: "",
+              },
+            ]}
           />
         </MockedProvider>
-      </Provider>,
+      </Provider>
     );
     const justification: ReactWrapper = wrapper.find("textarea");
-    justification.simulate("change", { target:
-      { value: "This is a commenting test of a verifying request verification in vulns" } });
+    justification.simulate("change", {
+      target: {
+        value:
+          "This is a commenting test of a verifying request verification in vulns",
+      },
+    });
     const form: ReactWrapper = wrapper.find("form");
-    form.at(0)
-      .simulate("submit");
-    await act(async () => { await wait(0); wrapper.update(); });
-    expect(wrapper)
-      .toHaveLength(1);
-    expect(handleOnClose)
-      .toHaveBeenCalled();
-    expect(handleVerifyState)
-      .not
-      .toHaveBeenCalled();
+    form.at(0).simulate("submit");
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+
+    expect(wrapper).toHaveLength(1);
+    expect(handleOnClose).toHaveBeenCalled(); // eslint-disable-line jest/prefer-called-with
+    expect(handleVerifyState).not.toHaveBeenCalled();
   });
 });
