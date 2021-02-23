6848676ba02f6b85dd34ff6965814f889ba8882a
Daniel Murcia <dmurcia@fluidattacks.com>
2021-02-23 11:00:49 -0500

    observes\feat(build): #4195.3 import mailchimp sdk
    - added mailchimp-marketing dependency
    
    
    Former-commit-id: 6ff03e1eba31cbbada6b6dd8e303f69c766177cc

--- makes/libs/observes/packages/config/default.nix
+++ makes/libs/observes/packages/config/default.nix
@@ -461,8 +461,18 @@ rec {
   tapMailchimp = {
     srcPath = path "/observes/singer/tap_mailchimp";
     python = {
-      direct = [ ];
-      inherited = [ ];
+      direct = [
+        "mailchimp-marketing==3.0.31"
+      ];
+      inherited = [
+        "certifi==2020.12.5"
+        "chardet==4.0.0"
+        "idna==2.10"
+        "python-dateutil==2.8.1"
+        "requests==2.25.1"
+        "six==1.15.0"
+        "urllib3==1.26.3"
+      ];
     };
     local = [ ];
     nix = [ ];
