954e0dedd4ec7c197b3e77c1d0ee252015d20bf5
Diego Restrepo <drestrepo@fluidattacks.com>
2021-02-23 09:32:04 -0500

    forces\fix(job): #4230.1 logging as forces prod
    - the user to login can depend on the product of the commit HEAD
    
    
    Former-commit-id: 2a4d7aea64808db19cba53d18a9f560326681d55

--- makes/applications/forces/process-groups/default.nix
+++ makes/applications/forces/process-groups/default.nix
@@ -8,6 +8,7 @@ let
 in
 makeEntrypoint rec {
   arguments = {
+    envUtilsAws = import (path "/makes/utils/aws") path forcesPkgs;
     envForces = applications.forces;
     envJq = forcesPkgs.jq;
     envUtilsBashLibGit = import (path "/makes/utils/use-git-repo") path forcesPkgs;
--- makes/applications/forces/process-groups/entrypoint.sh
+++ makes/applications/forces/process-groups/entrypoint.sh
@@ -2,14 +2,15 @@
 
 source '__envUtilsBashLibGit__'
 source '__envUtilsSops__'
+source '__envUtilsAws__'
 
 
 function main {
-
-      sops_export_vars "forces/secrets-prod.yaml" \
+      aws_login_prod forces \
+  &&  sops_export_vars "forces/secrets-prod.yaml" \
         groups_to_check \
   &&  use_git_repo_services \
-  &&  echo "${groups_to_check}" | '__envJq__/bin/jq' -c '.[]' -r | while read -r group; do
+  &&   '__envJq__/bin/jq' -c '.[]' -r <<< "${groups_to_check}" | while read -r group; do
             echo "[INFO] Runing forces for ${group}" \
         &&  sops_export_vars "groups/${group}/config/secrets-dev.yaml" \
               forces_api_token \
