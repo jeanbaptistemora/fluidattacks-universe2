18c61ceedce13259da896e5aa45d7bb45eb6dd17
Jonathan Mesa <jmesa@fluidattacks.com>
2021-02-23 09:36:34 -0500

    integrates\refac(front): #2705.42 update findingactions
    - Lint findingactions
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: 2b9051c5fb95f420027ef9f00311696e3d2a00e0

--- integrates/front/src/scenes/Dashboard/components/FindingActions/index.tsx
+++ integrates/front/src/scenes/Dashboard/components/FindingActions/index.tsx
@@ -1,17 +1,11 @@
-/* tslint:disable:jsx-no-multiline-js
- *
- * Disabling this rule is necessary for conditional rendering
- */
-
-import _ from "lodash";
-import React from "react";
-
 import { Button } from "components/Button";
-import { ConfirmDialog, IConfirmFn } from "components/ConfirmDialog";
-import { FluidIcon } from "components/FluidIcon";
-import { TooltipWrapper } from "components/TooltipWrapper";
 import { ButtonToolbar } from "styles/styledComponents";
 import { Can } from "utils/authz/Can";
+import { ConfirmDialog } from "components/ConfirmDialog";
+import { FluidIcon } from "components/FluidIcon";
+import type { IConfirmFn } from "components/ConfirmDialog";
+import React from "react";
+import { TooltipWrapper } from "components/TooltipWrapper";
 import { translate } from "utils/translations/translate";
 
 interface IFindingActionsProps {
@@ -19,65 +13,93 @@ interface IFindingActionsProps {
   hasVulns: boolean;
   isDraft: boolean;
   loading: boolean;
-  onApprove(): void;
-  onDelete(): void;
-  onReject(): void;
-  onSubmit(): void;
+  onApprove: () => void;
+  onDelete: () => void;
+  onReject: () => void;
+  onSubmit: () => void;
 }
 
-const findingActions: React.FC<IFindingActionsProps> = (props: IFindingActionsProps): JSX.Element => {
-  const { onApprove, onDelete, onReject, onSubmit } = props;
+const findingActions: React.FC<IFindingActionsProps> = (
+  props: IFindingActionsProps
+): JSX.Element => {
+  const {
+    hasSubmission,
+    hasVulns,
+    isDraft,
+    loading,
+    onApprove,
+    onDelete,
+    onReject,
+    onSubmit,
+  } = props;
 
-  const canApprove: boolean = props.hasVulns && props.hasSubmission;
+  const canApprove: boolean = hasVulns && hasSubmission;
 
   return (
     <ButtonToolbar>
-      {props.isDraft ? (
+      {isDraft ? (
         <React.Fragment>
-          <Can do="backend_api_mutations_submit_draft_mutate">
-            {props.hasSubmission ? undefined : (
+          <Can do={"backend_api_mutations_submit_draft_mutate"}>
+            {hasSubmission ? undefined : (
               <TooltipWrapper
-                id={"group.drafts.submit.text"}
                 displayClass={"dib"}
+                id={"group.drafts.submit.text"}
                 message={translate.t("group.drafts.submit.tooltip")}
               >
-                <Button disabled={props.loading} onClick={onSubmit}>
+                <Button disabled={loading} onClick={onSubmit}>
                   {translate.t("group.drafts.submit.text")}
                 </Button>
               </TooltipWrapper>
             )}
           </Can>
-          <Can do="backend_api_mutations_approve_draft_mutate">
+          <Can do={"backend_api_mutations_approve_draft_mutate"}>
             <ConfirmDialog title={translate.t("group.drafts.approve.title")}>
               {(confirm: IConfirmFn): React.ReactNode => {
-                const handleClick: (() => void) = (): void => { confirm(() => { onApprove(); }); };
+                const handleClick: () => void = (): void => {
+                  confirm((): void => {
+                    onApprove();
+                  });
+                };
 
                 return (
                   <TooltipWrapper
-                    id={"group.drafts.approve.text"}
                     displayClass={"dib"}
+                    id={"group.drafts.approve.text"}
                     message={translate.t("group.drafts.approve.tooltip")}
                   >
-                    <Button onClick={handleClick} disabled={!canApprove || props.loading}>
-                      <FluidIcon icon="verified" />&nbsp;{translate.t("group.drafts.approve.text")}
+                    <Button
+                      disabled={!canApprove || loading}
+                      // eslint-disable-next-line react/jsx-no-bind
+                      onClick={handleClick}
+                    >
+                      <FluidIcon icon={"verified"} />
+                      &nbsp;{translate.t("group.drafts.approve.text")}
                     </Button>
                   </TooltipWrapper>
                 );
               }}
             </ConfirmDialog>
           </Can>
-          <Can do="backend_api_mutations_reject_draft_mutate">
+          <Can do={"backend_api_mutations_reject_draft_mutate"}>
             <ConfirmDialog title={translate.t("group.drafts.reject.title")}>
               {(confirm: IConfirmFn): React.ReactNode => {
-                const handleClick: (() => void) = (): void => { confirm(() => { onReject(); }); };
+                const handleClick: () => void = (): void => {
+                  confirm((): void => {
+                    onReject();
+                  });
+                };
 
                 return (
                   <TooltipWrapper
-                    id={"group.drafts.reject.tooltip"}
                     displayClass={"dib"}
+                    id={"group.drafts.reject.tooltip"}
                     message={translate.t("group.drafts.reject.tooltip")}
                   >
-                    <Button onClick={handleClick} disabled={!props.hasSubmission || props.loading}>
+                    <Button
+                      disabled={!hasSubmission || loading}
+                      // eslint-disable-next-line react/jsx-no-bind
+                      onClick={handleClick}
+                    >
                       {translate.t("group.drafts.reject.text")}
                     </Button>
                   </TooltipWrapper>
@@ -87,14 +109,15 @@ const findingActions: React.FC<IFindingActionsProps> = (props: IFindingActionsPr
           </Can>
         </React.Fragment>
       ) : undefined}
-      <Can do="backend_api_mutations_delete_finding_mutate">
+      <Can do={"backend_api_mutations_delete_finding_mutate"}>
         <TooltipWrapper
-          id={"search_findings.delete.btn.tooltip"}
           displayClass={"dib"}
+          id={"search_findings.delete.btn.tooltip"}
           message={translate.t("search_findings.delete.btn.tooltip")}
         >
           <Button onClick={onDelete}>
-            <FluidIcon icon="delete" />&nbsp;{translate.t("search_findings.delete.btn.text")}
+            <FluidIcon icon={"delete"} />
+            &nbsp;{translate.t("search_findings.delete.btn.text")}
           </Button>
         </TooltipWrapper>
       </Can>
