bbc98c6cc544bbc977c14c977e64b161006e05d3
Jonathan Mesa <jmesa@fluidattacks.com>
2021-02-23 07:49:12 -0500

    integrates\refac(front): #2705.40 update remediationmodal
    - Lint remediationmodal test
    - No visual changes
    
    
    Former-commit-id: 390642e063fb530a10b16b0be5dfa0e5da21aa3e

--- integrates/front/src/scenes/Dashboard/components/RemediationModal/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/components/RemediationModal/index.spec.tsx
@@ -1,34 +1,38 @@
-import { shallow, ShallowWrapper } from "enzyme";
-import React from "react";
 import { Provider } from "react-redux";
-import { Action, createStore, Store } from "redux";
+import React from "react";
 import { RemediationModal } from "scenes/Dashboard/components/RemediationModal";
+import type { ShallowWrapper } from "enzyme";
+import { createStore } from "redux";
+import { shallow } from "enzyme";
+import type { Action, Store } from "redux";
 
-const functionMock: (() => void) = (): void => undefined;
+const functionMock: () => void = (): void => undefined;
 
-describe("Remediation modal", () => {
-
-  const store: Store<{}, Action<{}>> = createStore(() => ({}));
+describe("Remediation modal", (): void => {
+  const store: Store<
+    Record<string, unknown>,
+    Action<Record<string, unknown>>
+  > = createStore((): Record<string, unknown> => ({}));
   const wrapper: ShallowWrapper = shallow(
     <Provider store={store}>
       <RemediationModal
         isLoading={false}
         isOpen={true}
-        message="test"
+        message={"test"}
         onClose={functionMock}
         onSubmit={functionMock}
-        title="title"
+        title={"title"}
       />
-    </Provider>,
+    </Provider>
   );
 
-  it("should return a function", () => {
-    expect(typeof (RemediationModal))
-    .toEqual("function");
+  it("should return a function", (): void => {
+    expect.hasAssertions();
+    expect(typeof RemediationModal).toStrictEqual("function");
   });
 
-  it("should render", () => {
-    expect(wrapper)
-      .toHaveLength(1);
+  it("should render", (): void => {
+    expect.hasAssertions();
+    expect(wrapper).toHaveLength(1);
   });
 });
