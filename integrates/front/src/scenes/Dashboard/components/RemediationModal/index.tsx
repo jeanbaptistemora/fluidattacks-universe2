f0dbc24d5e63d7d16489ccce59b8bbfe1cd99481
Jonathan Mesa <jmesa@fluidattacks.com>
2021-02-23 09:01:11 -0500

    integrates\refac(front): #2705.41 update remediationmodal
    - Lint remediationmodal
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: 35a4615def60cca6f4b6251e9162d6e67c65b15a

--- integrates/front/src/scenes/Dashboard/components/RemediationModal/index.tsx
+++ integrates/front/src/scenes/Dashboard/components/RemediationModal/index.tsx
@@ -1,16 +1,13 @@
-/* tslint:disable jsx-no-multiline-js
- * JSX-NO-MULTILINE-JS: Disabling this rule is necessary for the sake of
- * readability of the code that dynamically renders the fields
- */
-
-import _ from "lodash";
-import React from "react";
-import { Field, InjectedFormProps } from "redux-form";
-import { ConfigurableValidator } from "revalidate";
-
 import { Button } from "components/Button";
-import { Modal } from "components/Modal";
+import type { ConfigurableValidator } from "revalidate";
+import { Field } from "redux-form";
 import { GenericForm } from "scenes/Dashboard/components/GenericForm";
+import type { InjectedFormProps } from "redux-form";
+import { Modal } from "components/Modal";
+import React from "react";
+import { TextArea } from "utils/forms/fields";
+import _ from "lodash";
+import { translate } from "utils/translations/translate";
 import {
   ButtonToolbar,
   Col100,
@@ -19,71 +16,81 @@ import {
   RequiredField,
   Row,
 } from "styles/styledComponents";
-import { TextArea } from "utils/forms/fields";
-import { translate } from "utils/translations/translate";
 import { maxLength, minLength, required } from "utils/validations";
 
-export interface IAddRemediationProps {
-  additionalInfo?: string;
-  children?: React.ReactNode;
+// ESLint annotations needed in order to avoid the mutations of defaultProps
+interface IAddRemediationProps {
+  additionalInfo?: string; // eslint-disable-line react/require-default-props
+  children?: React.ReactNode; // eslint-disable-line react/require-default-props
   isLoading: boolean;
   isOpen: boolean;
-  maxJustificationLength?: number;
+  maxJustificationLength?: number; // eslint-disable-line react/require-default-props
   message: string;
   title: string;
-  onClose(): void;
-  onSubmit(values: {}): void;
+  onClose: () => void;
+  onSubmit: (values: any) => void; // eslint-disable-line @typescript-eslint/no-explicit-any
 }
 
-const minJustificationLength: ConfigurableValidator = minLength(10);
-const remediationModal: React.FC<IAddRemediationProps> = (props: IAddRemediationProps): JSX.Element => {
-  const { children, onClose, onSubmit } = props;
+const MIN_LENGTH: number = 10;
+const minJustificationLength: ConfigurableValidator = minLength(MIN_LENGTH);
+const RemediationModal: React.FC<IAddRemediationProps> = (
+  props: IAddRemediationProps
+): JSX.Element => {
+  const {
+    additionalInfo,
+    children,
+    isOpen,
+    maxJustificationLength,
+    message,
+    title,
+    onClose,
+    onSubmit,
+  } = props;
 
-  const justificationValidations: ConfigurableValidator[] = [required, minJustificationLength];
-  if (_.isNumber(props.maxJustificationLength)) {
-    justificationValidations.push(maxLength(props.maxJustificationLength));
+  const justificationValidations: ConfigurableValidator[] = [
+    required,
+    minJustificationLength,
+  ];
+  if (_.isNumber(maxJustificationLength)) {
+    // Next annotation needed in order to use push()
+    // eslint-disable-next-line fp/no-mutating-methods
+    justificationValidations.push(maxLength(maxJustificationLength));
   }
 
   return (
     <React.StrictMode>
-      <Modal
-        open={props.isOpen}
-        headerTitle={props.title}
-      >
-        <GenericForm name="updateRemediation" onSubmit={onSubmit}>
+      <Modal headerTitle={title} open={isOpen}>
+        <GenericForm name={"updateRemediation"} onSubmit={onSubmit}>
           {({ pristine }: InjectedFormProps): JSX.Element => (
             <React.Fragment>
               {children}
               <FormGroup>
                 <ControlLabel>
                   <RequiredField>{"* "}</RequiredField>
-                  {props.message}
+                  {message}
                 </ControlLabel>
                 <Field
-                  name="treatmentJustification"
-                  type="text"
                   component={TextArea}
+                  name={"treatmentJustification"}
+                  rows={"6"}
+                  type={"text"}
                   validate={justificationValidations}
                   withCount={true}
-                  rows="6"
                 />
               </FormGroup>
-              {props.additionalInfo}
+              {additionalInfo}
               <br />
               <hr />
               <Row>
                 <Col100>
                   <ButtonToolbar>
-                    <Button
-                      id={"cancel-remediation"}
-                      onClick={onClose}
-                    >
+                    <Button id={"cancel-remediation"} onClick={onClose}>
                       {translate.t("confirmmodal.cancel")}
                     </Button>
                     <Button
-                      id={"proceed-remediation"}
-                      type="submit"
                       disabled={pristine || props.isLoading}
+                      id={"proceed-remediation"}
+                      type={"submit"}
                     >
                       {translate.t("confirmmodal.proceed")}
                     </Button>
@@ -98,4 +105,4 @@ const remediationModal: React.FC<IAddRemediationProps> = (props: IAddRemediation
   );
 };
 
-export { remediationModal as RemediationModal };
+export { RemediationModal, IAddRemediationProps };
