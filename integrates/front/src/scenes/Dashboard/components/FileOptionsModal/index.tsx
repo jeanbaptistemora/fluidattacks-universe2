ffe4deecd13e25dade3145cc247510e0604eec4d
Jonathan Mesa <jmesa@fluidattacks.com>
2021-02-23 11:24:57 -0500

    integrates\refac(front): #2705.45 update fileoptionsmodal
    - Lint fileoptionsmodal
    - No visual changes
    
    
    Former-commit-id: 1d09c9d59b058412118711636c218a055aefb55f

--- integrates/front/src/scenes/Dashboard/components/FileOptionsModal/index.tsx
+++ integrates/front/src/scenes/Dashboard/components/FileOptionsModal/index.tsx
@@ -1,12 +1,8 @@
-/* tslint:disable:jsx-no-multiline-js
- * Disabling this rule is necessary for conditional rendering
- */
-import { faDownload, faMinus } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-
 import { Button } from "components/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Modal } from "components/Modal";
+import React from "react";
+import { translate } from "utils/translations/translate";
 import {
   ButtonToolbar,
   ButtonToolbarCenter,
@@ -14,47 +10,58 @@ import {
   Col33,
   Row,
 } from "styles/styledComponents";
-import { translate } from "utils/translations/translate";
+import { faDownload, faMinus } from "@fortawesome/free-solid-svg-icons";
 
-export interface IFileOptionsModalProps {
+interface IFileOptionsModalProps {
   canRemove: boolean;
   fileName: string;
   isOpen: boolean;
-  onClose(): void;
-  onDelete(): void;
-  onDownload(): void;
+  onClose: () => void;
+  onDelete: () => void;
+  onDownload: () => void;
 }
 
-const fileOptionsModal: React.FC<IFileOptionsModalProps> = (props: IFileOptionsModalProps): JSX.Element => {
-  const { onClose, onDelete, onDownload } = props;
+const fileOptionsModal: React.FC<IFileOptionsModalProps> = (
+  props: IFileOptionsModalProps
+): JSX.Element => {
+  const { canRemove, fileName, isOpen, onClose, onDelete, onDownload } = props;
 
   return (
     <React.StrictMode>
       <Modal
-        open={props.isOpen}
-        headerTitle={translate.t("search_findings.tab_resources.modal_options_title")}
+        headerTitle={translate.t(
+          "search_findings.tab_resources.modal_options_title"
+        )}
+        open={isOpen}
       >
         <Row>
           <Col100>
             <label>
-              {translate.t("search_findings.tab_resources.modal_options_content")}
-              <b>{props.fileName}</b>?
+              {translate.t(
+                "search_findings.tab_resources.modal_options_content"
+              )}
+              <b>{fileName}</b>
+              {"?"}
             </label>
           </Col100>
           <ButtonToolbarCenter>
             <br />
-            {props.canRemove ? (
+            {canRemove ? (
               <Col33>
                 <Button onClick={onDelete}>
-                  <FontAwesomeIcon icon={faMinus} />&nbsp;
-                    {translate.t("search_findings.tab_resources.remove_repository")}
+                  <FontAwesomeIcon icon={faMinus} />
+                  &nbsp;
+                  {translate.t(
+                    "search_findings.tab_resources.remove_repository"
+                  )}
                 </Button>
               </Col33>
             ) : undefined}
             <Col33>
               <Button onClick={onDownload}>
-                <FontAwesomeIcon icon={faDownload} />&nbsp;
-                  {translate.t("search_findings.tab_resources.download")}
+                <FontAwesomeIcon icon={faDownload} />
+                &nbsp;
+                {translate.t("search_findings.tab_resources.download")}
               </Button>
             </Col33>
           </ButtonToolbarCenter>
@@ -63,7 +70,9 @@ const fileOptionsModal: React.FC<IFileOptionsModalProps> = (props: IFileOptionsM
         <Row>
           <Col100>
             <ButtonToolbar>
-              <Button onClick={onClose}>{translate.t("confirmmodal.cancel")}</Button>
+              <Button onClick={onClose}>
+                {translate.t("confirmmodal.cancel")}
+              </Button>
             </ButtonToolbar>
           </Col100>
         </Row>
@@ -72,4 +81,4 @@ const fileOptionsModal: React.FC<IFileOptionsModalProps> = (props: IFileOptionsM
   );
 };
 
-export { fileOptionsModal as FileOptionsModal };
+export { fileOptionsModal as FileOptionsModal, IFileOptionsModalProps };
