9636de1ebaf577ec4596c810d9d091cb36702d71
Daniel Murcia <dmurcia@fluidattacks.com>
2021-02-23 13:26:31 -0500

    observes\feat(back): #4195.5 def api client
    - define api client interface and builder
    
    
    Former-commit-id: 4c573b5a78f304243d298e37704662b05a3a1ac3

--- observes/singer/tap_mailchimp/tap_mailchimp/api.py
+++ observes/singer/tap_mailchimp/tap_mailchimp/api.py
@@ -1,12 +1,16 @@
 # Standard libraries
+from functools import partial
 from typing import (
     Any,
+    Callable,
     Dict,
     NamedTuple,
     Optional
 )
 # Third party libraries
-from mailchimp_marketing import Client as ApiClient
+from mailchimp_marketing import (
+    Client,
+)
 # Local libraries
 
 
@@ -19,7 +23,17 @@ class ApiData(NamedTuple):
     total_items: Optional[int]
 
 
-def list_audiences(client: ApiClient) -> ApiData:
+class ApiClient(NamedTuple):
+    list_audiences: Callable[[], ApiData]
+    get_audience: Callable[[str], ApiData]
+    list_abuse_reports: Callable[[str], ApiData]
+    get_abuse_report: Callable[[str, str], ApiData]
+    get_activity: Callable[[str], ApiData]
+    get_clients: Callable[[str], ApiData]
+    get_members: Callable[[str], ApiData]
+
+
+def _list_audiences(client: Client) -> ApiData:
     raw = client.lists.get_all_lists()
     return ApiData(
         data=raw['lists'],
@@ -28,7 +42,7 @@ def list_audiences(client: ApiClient) -> ApiData:
     )
 
 
-def get_audience(client: ApiClient, audience_id: str) -> ApiData:
+def _get_audience(client: Client, audience_id: str) -> ApiData:
     raw = client.lists.get_list(audience_id)
     links = raw.pop('_links')
     return ApiData(
@@ -38,7 +52,7 @@ def get_audience(client: ApiClient, audience_id: str) -> ApiData:
     )
 
 
-def list_abuse_reports(client: ApiClient, audience_id: str) -> ApiData:
+def _list_abuse_reports(client: Client, audience_id: str) -> ApiData:
     raw = client.lists.get_list_abuse_reports(audience_id)
     return ApiData(
         data=raw['abuse_reports'],
@@ -47,8 +61,8 @@ def list_abuse_reports(client: ApiClient, audience_id: str) -> ApiData:
     )
 
 
-def get_abuse_report(
-    client: ApiClient,
+def _get_abuse_report(
+    client: Client,
     audience_id: str,
     report_id: str
 ) -> ApiData:
@@ -63,7 +77,7 @@ def get_abuse_report(
     )
 
 
-def get_activity(client: ApiClient, audience_id: str) -> ApiData:
+def _get_activity(client: Client, audience_id: str) -> ApiData:
     raw = client.lists.get_list_recent_activity(audience_id)
     return ApiData(
         data=raw['activity'],
@@ -72,7 +86,7 @@ def get_activity(client: ApiClient, audience_id: str) -> ApiData:
     )
 
 
-def get_clients(client: ApiClient, audience_id: str) -> ApiData:
+def _get_clients(client: Client, audience_id: str) -> ApiData:
     raw = client.lists.get_list_clients(audience_id)
     return ApiData(
         data=raw['clients'],
@@ -81,10 +95,27 @@ def get_clients(client: ApiClient, audience_id: str) -> ApiData:
     )
 
 
-def get_members(client: ApiClient, audience_id: str) -> ApiData:
+def _get_members(client: Client, audience_id: str) -> ApiData:
     raw = client.lists.get_list_members_info(audience_id)
     return ApiData(
         data=raw['members'],
         links=raw['_links'],
         total_items=raw['total_items']
+    )
+
+
+def new_client() -> ApiClient:
+    client = Client()
+    client.set_config({
+        "api_key": "YOUR_API_KEY",
+        "server": "YOUR_SERVER_PREFIX"
+    })
+    return ApiClient(
+        list_audiences=partial(_list_audiences, client),
+        get_audience=partial(_get_audience, client),
+        list_abuse_reports=partial(_list_abuse_reports, client),
+        get_abuse_report=partial(_get_abuse_report, client),
+        get_activity=partial(_get_activity, client),
+        get_clients=partial(_get_clients, client),
+        get_members=partial(_get_members, client)
     )
