b90bceec5bf352dd691bfe5485d8480b0f15d167
Jonathan Mesa <jmesa@fluidattacks.com>
2021-02-23 10:52:40 -0500

    integrates\refac(front): #2705.44 update sidebar
    - Lint sidebar
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: 48b22674d98a3e4cef29d145453ff2f7bd944248

--- integrates/front/src/scenes/Dashboard/components/Sidebar/index.tsx
+++ integrates/front/src/scenes/Dashboard/components/Sidebar/index.tsx
@@ -1,26 +1,27 @@
-import _ from "lodash";
-import React from "react";
-import { slide as BurgerMenu } from "react-burger-menu";
-import Media from "react-media";
-import { useHistory } from "react-router-dom";
-
 import { Badge } from "components/Badge";
-import { TooltipWrapper } from "components/TooltipWrapper/index";
-import { default as logo } from "resources/integrates_sidebar.svg";
-import { default as style } from "scenes/Dashboard/components/Sidebar/index.css";
+import { slide as BurgerMenu } from "react-burger-menu";
 import { Can } from "utils/authz/Can";
+import Media from "react-media";
+import React from "react";
+import { TooltipWrapper } from "components/TooltipWrapper/index";
+import _ from "lodash";
+import logo from "resources/integrates_sidebar.svg";
+import style from "scenes/Dashboard/components/Sidebar/index.css";
 import { translate } from "utils/translations/translate";
+import { useHistory } from "react-router-dom";
 
 interface ISidebarProps {
   userEmail: string;
   userRole: string | undefined;
-  onLogoutClick(): void;
-  onOpenAccessTokenModal(): void;
-  onOpenAddOrganizationModal(): void;
-  onOpenAddUserModal(): void;
+  onLogoutClick: () => void;
+  onOpenAccessTokenModal: () => void;
+  onOpenAddOrganizationModal: () => void;
+  onOpenAddUserModal: () => void;
 }
 
-const sidebar: React.FC<ISidebarProps> = (props: ISidebarProps): JSX.Element => {
+const sidebar: React.FC<ISidebarProps> = (
+  props: ISidebarProps
+): JSX.Element => {
   const {
     userEmail,
     userRole,
@@ -29,11 +30,16 @@ const sidebar: React.FC<ISidebarProps> = (props: ISidebarProps): JSX.Element => 
     onOpenAddOrganizationModal,
     onOpenAddUserModal,
   } = props;
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const { push } = useHistory();
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const handleLogoClick: () => void = React.useCallback((): void => {
+    push("/home");
+  }, [push]);
 
-  const handleLogoClick: (() => void) = (): void => { push("/home"); };
-
-  const renderMenu: ((isNormalScreenSize: boolean) => JSX.Element) = (isNormalScreenSize: boolean): JSX.Element => (
+  const renderMenu: (isNormalScreenSize: boolean) => JSX.Element = (
+    isNormalScreenSize: boolean
+  ): JSX.Element => (
     <BurgerMenu
       burgerButtonClassName={style.burgerButton}
       crossButtonClassName={style.closeButton}
@@ -43,47 +49,77 @@ const sidebar: React.FC<ISidebarProps> = (props: ISidebarProps): JSX.Element => 
       noOverlay={isNormalScreenSize}
       width={210}
     >
-      <img className={style.logo} src={logo} alt="integrates-logo" onClick={handleLogoClick} />
+      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */}
+      <img
+        alt={"integrates-logo"}
+        className={style.logo}
+        onClick={handleLogoClick}
+        src={logo}
+      />
       <ul className={style.menuList}>
-        <Can do="backend_api_mutations_add_stakeholder_mutate">
-        <li onClick={onOpenAddUserModal}>
-          <TooltipWrapper id={"addUser"} message={translate.t("sidebar.user.tooltip")} placement="right">
-            <div className={style.item}><i className="icon pe-7s-plus" />
-              <span className={style.label}>{translate.t("sidebar.user.text")}</span>
-            </div>
-          </TooltipWrapper>
-        </li>
+        <Can do={"backend_api_mutations_add_stakeholder_mutate"}>
+          {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */}
+          <li onClick={onOpenAddUserModal}>
+            <TooltipWrapper
+              id={"addUser"}
+              message={translate.t("sidebar.user.tooltip")}
+              placement={"right"}
+            >
+              <div className={style.item}>
+                <i className={"icon pe-7s-plus"} />
+                <span className={style.label}>
+                  {translate.t("sidebar.user.text")}
+                </span>
+              </div>
+            </TooltipWrapper>
+          </li>
         </Can>
-        <Can do="backend_api_mutations_create_organization_mutate">
-        <li onClick={onOpenAddOrganizationModal}>
-          <TooltipWrapper id={"addOrg"} message={translate.t("sidebar.newOrganization.tooltip")} placement="right">
-            <div className={style.item}><i className="icon pe-7s-plus" />
-              <span className={style.label}>{translate.t("sidebar.newOrganization.text")}</span>
-            </div>
-          </TooltipWrapper>
-        </li>
+        <Can do={"backend_api_mutations_create_organization_mutate"}>
+          {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */}
+          <li onClick={onOpenAddOrganizationModal}>
+            <TooltipWrapper
+              id={"addOrg"}
+              message={translate.t("sidebar.newOrganization.tooltip")}
+              placement={"right"}
+            >
+              <div className={style.item}>
+                <i className={"icon pe-7s-plus"} />
+                <span className={style.label}>
+                  {translate.t("sidebar.newOrganization.text")}
+                </span>
+              </div>
+            </TooltipWrapper>
+          </li>
         </Can>
+        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */}
         <li onClick={onOpenAccessTokenModal}>
-          <TooltipWrapper id={"apiToken"} message={translate.t("sidebar.token.tooltip")} placement="right">
-            <div className={style.item}><i className="icon pe-7s-user" />
-              <span className={style.label}>{translate.t("sidebar.token.text")}</span>
-              <Badge>pro</Badge>
+          <TooltipWrapper
+            id={"apiToken"}
+            message={translate.t("sidebar.token.tooltip")}
+            placement={"right"}
+          >
+            <div className={style.item}>
+              <i className={"icon pe-7s-user"} />
+              <span className={style.label}>
+                {translate.t("sidebar.token.text")}
+              </span>
+              <Badge>{"pro"}</Badge>
             </div>
           </TooltipWrapper>
         </li>
       </ul>
       <div className={style.bottomBar}>
-        <div className={style.version}><small>{userEmail}</small></div>
-        {_.isUndefined(userRole) || _.isEmpty(userRole) ? (
-          undefined
-        ) :
+        <div className={style.version}>
+          <small>{userEmail}</small>
+        </div>
+        {_.isUndefined(userRole) || _.isEmpty(userRole) ? undefined : (
           <div className={style.version}>
             <small>
               {translate.t("sidebar.role")}&nbsp;
               {translate.t(`userModal.roles.${userRole}`)}
             </small>
           </div>
-        }
+        )}
         <div className={style.version}>
           <small>
             {translate.t("sidebar.deployment_date")}&nbsp;
@@ -94,17 +130,29 @@ const sidebar: React.FC<ISidebarProps> = (props: ISidebarProps): JSX.Element => 
           <small>
             {translate.t("sidebar.commit")}&nbsp;
             <a
+              // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
               href={`https://gitlab.com/fluidattacks/product/-/tree/${process.env.CI_COMMIT_SHA}`}
-              rel="noopener"
-              target="_blank"
+              rel={"noopener noreferrer"}
+              target={"_blank"}
             >
               {process.env.CI_COMMIT_SHORT_SHA}
             </a>
           </small>
         </div>
-        <TooltipWrapper displayClass={"flex"} id={"logOut"} message="Log out of Integrates" placement="right">
+        <TooltipWrapper
+          displayClass={"flex"}
+          id={"logOut"}
+          message={"Log out of Integrates"}
+          placement={"right"}
+        >
           <ul>
-            <li onClick={onLogoutClick}><a><span className="icon pe-7s-power" /></a></li>
+            {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */}
+            <li onClick={onLogoutClick}>
+              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+              <a>
+                <span className={"icon pe-7s-power"} />
+              </a>
+            </li>
           </ul>
         </TooltipWrapper>
       </div>
@@ -113,9 +161,7 @@ const sidebar: React.FC<ISidebarProps> = (props: ISidebarProps): JSX.Element => 
 
   return (
     <React.StrictMode>
-      <Media query="(min-width: 768px)">
-        {renderMenu}
-      </Media>
+      <Media query={"(min-width: 768px)"}>{renderMenu}</Media>
     </React.StrictMode>
   );
 };
