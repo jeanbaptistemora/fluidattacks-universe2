ca3763442953dcc37870d9c157852d6999e8a68c
Jonathan Mesa <jmesa@fluidattacks.com>
2021-02-23 15:18:10 -0500

    integrates\refac(front): #2705.47 update trackingitem
    - Lint trackingitem test
    - No visual changes
    
    
    Former-commit-id: d4e9b8a1a38f4b2493c97f139967b720e84da0e2

--- integrates/front/src/scenes/Dashboard/components/TrackingItem/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/components/TrackingItem/index.spec.tsx
@@ -1,71 +1,72 @@
-import { shallow, ShallowWrapper } from "enzyme";
 import React from "react";
+import type { ShallowWrapper } from "enzyme";
 import { TrackingItem } from "scenes/Dashboard/components/TrackingItem";
+import { shallow } from "enzyme";
 
-describe("TrackingItem", () => {
-
+describe("TrackingItem", (): void => {
   it("should return a function", (): void => {
-    expect(typeof (TrackingItem))
-      .toEqual("function");
+    expect.hasAssertions();
+    expect(typeof TrackingItem).toStrictEqual("function");
   });
 
-  it("should render a component", async () => {
+  it("should render a component", (): void => {
+    expect.hasAssertions();
+
     const wrapper: ShallowWrapper = shallow(
       <TrackingItem
-        closed={0}
-        cycle={1}
-        date="2019-01-17"
-        open={1}
         accepted={1}
         acceptedUndefined={0}
-        manager={"testmanager@test.test"}
+        closed={0}
+        cycle={1}
+        date={"2019-01-17"}
         justification={"test justification temporally accepted"}
-      />,
+        manager={"testmanager@test.test"}
+        open={1}
+      />
     );
-    expect(wrapper)
-      .toHaveLength(1);
-    expect(wrapper.text())
-      .toContain("2019-01-17");
-    expect(wrapper.text())
-      .toContain("Cycle: 1,");
-    expect(wrapper.text())
-      .toContain("Vulnerabilities found:\u00a01");
-    expect(wrapper.text())
-      .toContain("Justification:\u00a0test justification temporally accepted");
+
+    expect(wrapper).toHaveLength(1);
+    expect(wrapper.text()).toContain("2019-01-17");
+    expect(wrapper.text()).toContain("Cycle: 1,");
+    expect(wrapper.text()).toContain("Vulnerabilities found:\u00a01");
+    expect(wrapper.text()).toContain(
+      "Justification:\u00a0test justification temporally accepted"
+    );
   });
 
-  it("should render root item", async () => {
+  it("should render root item", (): void => {
+    expect.hasAssertions();
+
     const wrapper: ShallowWrapper = shallow(
       <TrackingItem
+        accepted={0}
+        acceptedUndefined={0}
         closed={0}
         cycle={0}
-        date="2019-01-17"
+        date={"2019-01-17"}
         open={1}
-        accepted={0}
-        acceptedUndefined={0}
-      />,
+      />
     );
-    expect(wrapper.text())
-      .toContain("2019-01-17");
-    expect(wrapper.text())
-      .toContain("Found");
-    expect(wrapper.text())
-      .toContain("Vulnerabilities found:\u00a01");
+
+    expect(wrapper.text()).toContain("2019-01-17");
+    expect(wrapper.text()).toContain("Found");
+    expect(wrapper.text()).toContain("Vulnerabilities found:\u00a01");
   });
 
-  it("should render closed item", async () => {
+  it("should render closed item", (): void => {
+    expect.hasAssertions();
+
     const wrapper: ShallowWrapper = shallow(
       <TrackingItem
-        closed={1}
-        cycle={2}
-        date="2019-01-17"
-        open={0}
         accepted={0}
         acceptedUndefined={0}
-      />,
+        closed={1}
+        cycle={2}
+        date={"2019-01-17"}
+        open={0}
+      />
     );
-    expect(wrapper.find("li")
-      .prop("className"))
-      .toContain("green");
+
+    expect(wrapper.find("li").prop("className")).toContain("green");
   });
 });
