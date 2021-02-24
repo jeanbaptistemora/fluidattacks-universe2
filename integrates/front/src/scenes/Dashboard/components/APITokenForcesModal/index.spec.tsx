5443dffd889b982f2bedbf7bca737532132d1bc9
Jonathan Mesa <jmesa@fluidattacks.com>
2021-02-24 16:30:25 -0500

    integrates\refac(front): #2705.56 update apitokenforcmodal
    - Lint apitokenforcesmodal test
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: 89e64f661f106bba530515e7912e762a3982d19e

--- integrates/front/src/scenes/Dashboard/components/APITokenForcesModal/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/components/APITokenForcesModal/index.spec.tsx
@@ -1,27 +1,31 @@
-import { MockedProvider, MockedResponse, wait } from "@apollo/react-testing";
-import { mount, ReactWrapper } from "enzyme";
-import React from "react";
-// tslint:disable-next-line: no-submodule-imports
-import { act } from "react-dom/test-utils";
-import { Provider } from "react-redux";
 import { APITokenForcesModal } from "scenes/Dashboard/components/APITokenForcesModal";
+import type { MockedResponse } from "@apollo/react-testing";
+import { Provider } from "react-redux";
+import React from "react";
+import type { ReactWrapper } from "enzyme";
+import { act } from "react-dom/test-utils";
+import { mount } from "enzyme";
+import store from "store";
+import waitForExpect from "wait-for-expect";
 import {
   GET_FORCES_TOKEN,
   UPDATE_FORCES_TOKEN_MUTATION,
 } from "scenes/Dashboard/components/APITokenForcesModal/queries";
-import store from "store";
-import waitForExpect from "wait-for-expect";
+import { MockedProvider, wait } from "@apollo/react-testing";
 
 describe("Update access token modal", (): void => {
   const handleOnClose: jest.Mock = jest.fn();
 
   it("should return a function", (): void => {
     expect.hasAssertions();
-    expect(typeof APITokenForcesModal)
-      .toStrictEqual("function");
+    expect(typeof APITokenForcesModal).toStrictEqual("function");
   });
 
-  it("should render an token modal with token and reset", async (): Promise<void> => {
+  it("should render an token modal with token and reset", async (): Promise<
+    void
+  > => {
+    expect.hasAssertions();
+
     const beforeValue: string = "before value";
     const afterValue: string = "after value";
     const mockQueryFull: MockedResponse[] = [
@@ -60,22 +64,25 @@ describe("Update access token modal", (): void => {
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
         <MockedProvider addTypename={false} mocks={mockQueryFull}>
-          <APITokenForcesModal onClose={handleOnClose} open={true} groupName="unittesting" />
+          <APITokenForcesModal
+            groupName={"unittesting"}
+            onClose={handleOnClose}
+            open={true}
+          />
         </MockedProvider>
-      </Provider>,
+      </Provider>
     );
     await act(
       async (): Promise<void> => {
         await wait(0);
         wrapper.update();
-      },
+      }
     );
 
     const revealButton: ReactWrapper = wrapper
       .find("button")
-      .filterWhere(
-        (element: ReactWrapper): boolean =>
-          element.contains("Reveal Token"),
+      .filterWhere((element: ReactWrapper): boolean =>
+        element.contains("Reveal Token")
       )
       .first();
 
@@ -85,25 +92,25 @@ describe("Update access token modal", (): void => {
       async (): Promise<void> => {
         await waitForExpect((): void => {
           wrapper.update();
-          expect(revealButton.prop("disabled"))
-            .toBe(false);
+
+          expect(revealButton.prop("disabled")).toBe(false);
 
           const tokenTextAreaBefore: ReactWrapper = wrapper
             .find("textarea")
             .first();
-          expect(tokenTextAreaBefore.text())
-            .toBe(beforeValue);
+
+          expect(tokenTextAreaBefore.text()).toBe(beforeValue);
 
           const resetButton: ReactWrapper = wrapper
             .find("button")
             .filterWhere((element: ReactWrapper): boolean =>
-              element.contains("Reset"),
+              element.contains("Reset")
             )
             .first();
-          expect(resetButton.prop("disabled"))
-            .toBe(false);
+
+          expect(resetButton.prop("disabled")).toBe(false);
         });
-      },
+      }
     );
 
     const form: ReactWrapper = wrapper.find("genericForm");
@@ -116,15 +123,16 @@ describe("Update access token modal", (): void => {
           const tokenTextAreaAfter: ReactWrapper = wrapper
             .find("textarea")
             .first();
-          expect(tokenTextAreaAfter.text())
-            .toBe(afterValue);
-        });
-      },
-    );
 
+          expect(tokenTextAreaAfter.text()).toBe(afterValue);
+        });
+      }
+    );
   });
 
   it("should render an token modal with token", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const tokenValue: string = "some value";
     const mockQueryFull: MockedResponse[] = [
       {
@@ -146,23 +154,26 @@ describe("Update access token modal", (): void => {
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
         <MockedProvider addTypename={false} mocks={mockQueryFull}>
-          <APITokenForcesModal onClose={handleOnClose} open={true} groupName="unnittesting" />
+          <APITokenForcesModal
+            groupName={"unnittesting"}
+            onClose={handleOnClose}
+            open={true}
+          />
         </MockedProvider>
-      </Provider>,
+      </Provider>
     );
 
     await act(
       async (): Promise<void> => {
         await wait(0);
         wrapper.update();
-      },
+      }
     );
 
     const revealButton: ReactWrapper = wrapper
       .find("button")
-      .filterWhere(
-        (element: ReactWrapper): boolean =>
-          element.contains("Reveal Token"),
+      .filterWhere((element: ReactWrapper): boolean =>
+        element.contains("Reveal Token")
       )
       .first();
 
@@ -171,34 +182,30 @@ describe("Update access token modal", (): void => {
       async (): Promise<void> => {
         await wait(2);
         wrapper.update();
-      },
+      }
     );
-    expect(revealButton.prop("disabled"))
-      .toBe(false);
+
+    expect(revealButton.prop("disabled")).toBe(false);
 
     const copyButton: ReactWrapper = wrapper
       .find("button")
-      .filterWhere((element: ReactWrapper): boolean =>
-        element.contains("Copy"),
-      )
+      .filterWhere((element: ReactWrapper): boolean => element.contains("Copy"))
       .first();
-    expect(copyButton.prop("disabled"))
-      .toBe(false);
+
+    expect(copyButton.prop("disabled")).toBe(false);
 
     const resetButton: ReactWrapper = wrapper
       .find("button")
       .filterWhere((element: ReactWrapper): boolean =>
-        element.contains("Reset"),
+        element.contains("Reset")
       )
       .first();
-    expect(resetButton.prop("disabled"))
-      .toBe(false);
 
-    const tokenTextArea: ReactWrapper = wrapper
-      .find("textarea")
-      .first();
-    expect(tokenTextArea.text())
-      .toBe(tokenValue);
+    expect(resetButton.prop("disabled")).toBe(false);
+
+    const tokenTextArea: ReactWrapper = wrapper.find("textarea").first();
+
+    expect(tokenTextArea.text()).toBe(tokenValue);
   });
 
   it("should render an token modal without token", async (): Promise<void> => {
@@ -225,32 +232,35 @@ describe("Update access token modal", (): void => {
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
         <MockedProvider addTypename={false} mocks={mockQueryNull}>
-          <APITokenForcesModal onClose={handleOnClose} open={true} groupName="unnittesting" />
+          <APITokenForcesModal
+            groupName={"unnittesting"}
+            onClose={handleOnClose}
+            open={true}
+          />
         </MockedProvider>
-      </Provider>,
+      </Provider>
     );
 
     await act(
       async (): Promise<void> => {
         await wait(0);
         wrapper.update();
-      },
+      }
     );
 
     const componentTitle: ReactWrapper = wrapper.find("h4");
 
     const revealButton: ReactWrapper = wrapper
       .find("button")
-      .filterWhere(
-        (element: ReactWrapper): boolean =>
-          element.contains("Reveal Token"),
+      .filterWhere((element: ReactWrapper): boolean =>
+        element.contains("Reveal Token")
       )
       .first();
 
     const closeButton: ReactWrapper = wrapper
       .find("button")
       .filterWhere((element: ReactWrapper): boolean =>
-        element.contains("Close"),
+        element.contains("Close")
       )
       .first();
 
@@ -263,42 +273,35 @@ describe("Update access token modal", (): void => {
           const generateButton: ReactWrapper = wrapper
             .find("button")
             .filterWhere((element: ReactWrapper): boolean =>
-              element.contains("Generate"),
+              element.contains("Generate")
             )
             .first();
           const copyButton: ReactWrapper = wrapper
             .find("button")
             .filterWhere((element: ReactWrapper): boolean =>
-              element.contains("Copy"),
+              element.contains("Copy")
             )
             .first();
           const revealButtonAfter: ReactWrapper = wrapper
             .find("button")
-            .filterWhere(
-              (element: ReactWrapper): boolean =>
-                element.contains("Reveal Token"),
+            .filterWhere((element: ReactWrapper): boolean =>
+              element.contains("Reveal Token")
             )
             .first();
+
           // When the token is revealed and does not exist, it cannot be copied
-          expect(copyButton.prop("disabled"))
-            .toBe(true);
-          expect(generateButton.prop("disabled"))
-            .toBe(false);
-          expect(revealButtonAfter.prop("disabled"))
-            .toBe(true);
+          expect(copyButton.prop("disabled")).toBe(true);
+          expect(generateButton.prop("disabled")).toBe(false);
+          expect(revealButtonAfter.prop("disabled")).toBe(true);
         });
-      },
+      }
     );
 
     closeButton.simulate("click");
 
-    expect(wrapper)
-      .toHaveLength(1);
-    expect(componentTitle.text())
-      .toBe("Manage DevSecOps token");
-    expect(revealButton)
-      .toHaveLength(1);
-    expect(handleOnClose)
-      .toHaveBeenCalledWith(expect.anything());
+    expect(wrapper).toHaveLength(1);
+    expect(componentTitle.text()).toBe("Manage DevSecOps token");
+    expect(revealButton).toHaveLength(1);
+    expect(handleOnClose).toHaveBeenCalledWith(expect.anything());
   });
 });
