5cff4ebb69ff06cfb419e93950195cc406ba1db2
Daniel Salazar <dsalazar@fluidattacks.com>
2021-02-23 21:26:33 -0500

    common\fix(build): #4226.12 ovewrite asserts libs
    - Overwrite asserts libs for some packages
    
    
    Former-commit-id: 5d0b4831d2c504e386d7a8de0c0cf9fd79e3ed18

--- makes/packages/asserts/env/default.nix
+++ makes/packages/asserts/env/default.nix
@@ -14,10 +14,14 @@ makeTemplate assertsPkgs {
       (path "/asserts")
     ];
     envPython37Paths = [
+      # Order matters. In order to overwrite libraries from pypi,
+      # we must attach python37Packages at the end.
+      packages.asserts.pypi.runtime
       assertsPkgs.python37Packages.mysql-connector
       assertsPkgs.python37Packages.psycopg2
       assertsPkgs.python37Packages.pyodbc
-      packages.asserts.pypi.runtime
+      assertsPkgs.python37Packages.brotli
+      assertsPkgs.python37Packages.urllib3
     ];
   };
   template = path "/makes/packages/asserts/env/template.sh";
