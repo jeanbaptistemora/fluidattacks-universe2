b6cbf5fcae5c5788c9ad9454672c6e9ca77f61f7
Jonathan Mesa <jmesa@fluidattacks.com>
2021-02-24 17:18:45 -0500

    integrates\refac(front): #2705.57 update apitokenforcmodal
    - Lint apitokenforcesmodal
    - No visual changes
    
    
    Former-commit-id: 422c894831408d9858d75036146cdc9a36bfd4c1

--- integrates/front/src/scenes/Dashboard/components/APITokenForcesModal/index.tsx
+++ integrates/front/src/scenes/Dashboard/components/APITokenForcesModal/index.tsx
@@ -1,74 +1,83 @@
 import { Button } from "components/Button";
-import { Modal } from "components/Modal";
-import _ from "lodash";
-import React from "react";
 import { Field } from "redux-form";
-import {
-  useGetAPIToken, useUpdateAPIToken,
-} from "scenes/Dashboard/components/APITokenForcesModal/hooks";
-
 import { GenericForm } from "scenes/Dashboard/components/GenericForm";
+import { Modal } from "components/Modal";
+import React from "react";
+import { TextArea } from "utils/forms/fields";
+import _ from "lodash";
+import { translate } from "utils/translations/translate";
 import {
   ButtonToolbar,
   Col100,
   ControlLabel,
   Row,
 } from "styles/styledComponents";
-import { TextArea } from "utils/forms/fields";
 import { msgError, msgSuccess } from "utils/notifications";
-import { translate } from "utils/translations/translate";
+import {
+  useGetAPIToken,
+  useUpdateAPIToken,
+} from "scenes/Dashboard/components/APITokenForcesModal/hooks";
 
 interface IAPITokenForcesModalProps {
   groupName: string;
   open: boolean;
-  onClose(): void;
+  onClose: () => void;
 }
 
-const apiTokenForcesModal: React.FC<IAPITokenForcesModalProps> = (
-  props: IAPITokenForcesModalProps,
+const APITokenForcesModal: React.FC<IAPITokenForcesModalProps> = (
+  props: IAPITokenForcesModalProps
 ): JSX.Element => {
   const { open, onClose, groupName } = props;
 
-  const [getApiToken, getTokenCalled, getTokenData, getTokenLoading] = useGetAPIToken(groupName);
+  const [
+    getApiToken,
+    getTokenCalled,
+    getTokenData,
+    getTokenLoading,
+  ] = useGetAPIToken(groupName);
   const [updateApiToken, updateResponse] = useUpdateAPIToken();
 
-  const currentToken: string | undefined = Boolean(updateResponse.data) ?
-    updateResponse.data?.updateForcesAccessToken.sessionJwt :
-    getTokenData?.project.forcesToken;
+  const currentToken: string | undefined = updateResponse.data
+    ? updateResponse.data.updateForcesAccessToken.sessionJwt
+    : getTokenData?.project.forcesToken;
 
-  const handleUpdateAPIToken: () => void = () => {
+  const handleUpdateAPIToken: () => void = React.useCallback((): void => {
     void updateApiToken({ variables: { groupName } });
-  };
-  const handleReveal: () => void = () => {
-    // tslint:disable-next-line: no-void-expression
-    void getApiToken();
-  };
-  const handleCopy: () => Promise<void> = async () => {
+  }, [groupName, updateApiToken]);
+  const handleReveal: () => void = React.useCallback((): void => {
+    void getApiToken(); // eslint-disable-line @typescript-eslint/no-confusing-void-expression
+  }, [getApiToken]);
+  const handleCopy: () => Promise<void> = React.useCallback(async (): Promise<
+    void
+  > => {
     const clipboard: Clipboard = navigator.clipboard;
 
     if (!_.isUndefined(clipboard)) {
-      await clipboard.writeText(
-        getTokenData?.project.forcesToken ?? "",
-      );
+      await clipboard.writeText(getTokenData?.project.forcesToken ?? "");
       document.execCommand("copy");
       msgSuccess(
         translate.t("update_forces_token.copy.successfully"),
-        translate.t("update_forces_token.copy.success"),
+        translate.t("update_forces_token.copy.success")
       );
     } else {
       msgError(translate.t("update_forces_token.copy.failed"));
     }
-  };
-  if (!Boolean(getTokenData?.project.forcesToken) && getTokenCalled && !getTokenLoading) {
+  }, [getTokenData?.project.forcesToken]);
+  if (
+    !getTokenData?.project.forcesToken && // eslint-disable-line @typescript-eslint/strict-boolean-expressions
+    getTokenCalled &&
+    !getTokenLoading
+  ) {
     msgError(translate.t("update_forces_token.token_no_exists"));
   }
 
   return (
     <Modal headerTitle={translate.t("update_forces_token.tittle")} open={open}>
       <GenericForm
+        initialValues={{ sessionJwt: currentToken }}
         name={"updateForcesToken"}
         onSubmit={handleUpdateAPIToken}
-        initialValues={{ sessionJwt: currentToken }}>
+      >
         <React.Fragment>
           <Row>
             <Col100>
@@ -76,19 +85,18 @@ const apiTokenForcesModal: React.FC<IAPITokenForcesModalProps> = (
                 <b>{translate.t("update_forces_token.access_token")}</b>
               </ControlLabel>
               <Field
-                className={"noresize"}
+                className={"noresize"} // eslint-disable-line react/forbid-component-props
                 component={TextArea}
                 disabled={true}
                 name={"sessionJwt"}
                 rows={"7"}
                 type={"text"}
               />
-              <Button onClick={handleCopy} disabled={!Boolean(currentToken)}>
+              {/* eslint-disable-next-line @typescript-eslint/strict-boolean-expressions */}
+              <Button disabled={!currentToken} onClick={handleCopy}>
                 {translate.t("update_forces_token.copy.copy")}
               </Button>
-              <Button
-                onClick={handleReveal}
-                disabled={getTokenCalled}>
+              <Button disabled={getTokenCalled} onClick={handleReveal}>
                 {translate.t("update_forces_token.reveal_token")}
               </Button>
             </Col100>
@@ -104,7 +112,10 @@ const apiTokenForcesModal: React.FC<IAPITokenForcesModalProps> = (
                   disabled={!getTokenCalled || getTokenLoading}
                   type={"submit"}
                 >
-                  {Boolean(currentToken) ? translate.t("update_forces_token.reset") : translate.t("update_forces_token.generate")}
+                  {/* eslint-disable-next-line @typescript-eslint/strict-boolean-expressions */}
+                  {currentToken
+                    ? translate.t("update_forces_token.reset")
+                    : translate.t("update_forces_token.generate")}
                 </Button>
               </ButtonToolbar>
             </Col100>
@@ -115,4 +126,4 @@ const apiTokenForcesModal: React.FC<IAPITokenForcesModalProps> = (
   );
 };
 
-export { IAPITokenForcesModalProps, apiTokenForcesModal as APITokenForcesModal };
+export { IAPITokenForcesModalProps, APITokenForcesModal };
