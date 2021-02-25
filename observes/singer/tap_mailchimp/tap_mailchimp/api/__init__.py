87c8231e5c2725c2de4d13f724e8708880a0a991
Daniel Murcia <dmurcia@fluidattacks.com>
2021-02-25 12:53:08 -0500

    observes\refac(back): #4195.11 api client
    - build api client from source
    
    
    Former-commit-id: 3e71ed67fdb404c4fdf0a52cb3b3e56301819e16

--- observes/singer/tap_mailchimp/tap_mailchimp/api/__init__.py
+++ observes/singer/tap_mailchimp/tap_mailchimp/api/__init__.py
@@ -1,26 +1,29 @@
 # Standard libraries
-from functools import partial
 from typing import (
     Any,
     Callable,
     Dict,
-    Iterable,
     NamedTuple,
     Optional,
-    cast
 )
 # Third party libraries
 from mailchimp_marketing import (
     Client,
 )
 # Local libraries
-
-
-JSON = Dict[str, Any]
+from tap_mailchimp.api import (
+    raw as raw_module,
+)
+from tap_mailchimp.api.raw import (
+    RawSource,
+)
+from tap_mailchimp.common import (
+    JSON,
+)
 
 
 class ApiData(NamedTuple):
-    data: Iterable[JSON]
+    data: JSON
     links: JSON
     total_items: Optional[int]
 
@@ -28,114 +31,38 @@ class ApiData(NamedTuple):
 class ApiClient(NamedTuple):
     list_audiences: Callable[[], ApiData]
     get_audience: Callable[[str], ApiData]
-    list_abuse_reports: Callable[[str], ApiData]
-    get_abuse_report: Callable[[str, str], ApiData]
-    get_activity: Callable[[str], ApiData]
-    get_clients: Callable[[str], ApiData]
-    get_members: Callable[[str], ApiData]
 
 
-def _list_audiences(client: Client) -> ApiData:
-    raw = client.lists.get_all_lists(fields='id')
-    return ApiData(
-        data=raw['lists'],
-        links=raw['_links'],
-        total_items=raw['total_items']
-    )
+def _pop_if_exist(raw: JSON, key: str) -> Any:
+    return raw.pop(key) if key in raw else None
 
 
-def _get_audience(client: Client, audience_id: str) -> ApiData:
-    raw = client.lists.get_list(audience_id)
+def create_api_data(raw: JSON) -> ApiData:
     links = raw.pop('_links')
+    total_items = _pop_if_exist(raw, 'total_items')
     return ApiData(
-        data=[raw],
+        data=raw,
         links=links,
-        total_items=None
+        total_items=total_items
     )
 
 
-def _list_abuse_reports(client: Client, audience_id: str) -> ApiData:
-    raw = client.lists.get_list_abuse_reports(audience_id)
-    return ApiData(
-        data=raw['abuse_reports'],
-        links=raw['_links'],
-        total_items=raw['total_items']
-    )
-
-
-def _get_abuse_report(
-    client: Client,
-    audience_id: str,
-    report_id: str
-) -> ApiData:
-    raw = client.lists.get_list_abuse_report_details(
-        audience_id, report_id
-    )
-    links = raw.pop('_links')
-    return ApiData(
-        data=[raw],
-        links=links,
-        total_items=None
-    )
-
-
-def _get_activity(client: Client, audience_id: str) -> ApiData:
-    raw = client.lists.get_list_recent_activity(audience_id)
-    return ApiData(
-        data=raw['activity'],
-        links=raw['_links'],
-        total_items=None
-    )
-
-
-def _get_clients(client: Client, audience_id: str) -> ApiData:
-    raw = client.lists.get_list_clients(audience_id)
-    return ApiData(
-        data=raw['clients'],
-        links=raw['_links'],
-        total_items=None
-    )
-
-
-def _get_members(client: Client, audience_id: str) -> ApiData:
-    raw = client.lists.get_list_members_info(audience_id)
-    return ApiData(
-        data=raw['members'],
-        links=raw['_links'],
-        total_items=raw['total_items']
+def new_client_from_source(
+    conf: Dict[str, str],
+    raw_source: RawSource
+) -> ApiClient:
+    client = Client()
+    client.set_config(conf)
+    return ApiClient(
+        list_audiences=lambda: create_api_data(
+            raw_source.list_audiences(client)
+        ),
+        get_audience=lambda item_id: create_api_data(
+            raw_source.get_audience(client, item_id)
+        ),
     )
 
 
 def new_client(conf: Dict[str, str]) -> ApiClient:
-    client = Client()
-    client.set_config(conf)
-    return ApiClient(
-        list_audiences=cast(
-            Callable[[], ApiData],
-            partial(_list_audiences, client)
-        ),
-        get_audience=cast(
-            Callable[[str], ApiData],
-            partial(_get_audience, client)
-        ),
-        list_abuse_reports=cast(
-            Callable[[str], ApiData],
-            partial(_list_abuse_reports, client)
-        ),
-        get_abuse_report=cast(
-            Callable[[str, str], ApiData],
-            partial(_get_abuse_report, client)
-        ),
-        get_activity=cast(
-            Callable[[str], ApiData],
-            partial(_get_activity, client)
-        ),
-        get_clients=cast(
-            Callable[[str], ApiData],
-            partial(_get_clients, client)
-        ),
-        get_members=cast(
-            Callable[[str], ApiData],
-            partial(_get_members, client)
-        ),
-    )
+    raw_source = raw_module.create_raw_source()
+    return new_client_from_source(conf, raw_source)
