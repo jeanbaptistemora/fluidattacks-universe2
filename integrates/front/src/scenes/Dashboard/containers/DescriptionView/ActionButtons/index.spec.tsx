7806b61e3603c01812bd170bbb8a1d9fc5d7c345
Jonathan Mesa <jmesa@fluidattacks.com>
2021-02-25 15:31:43 -0500

    integrates\refac(front): #2705.68 update descriptionview
    - Lint descriptionview: actionbuttons component test
    - No visual changes
    
    
    Former-commit-id: b207a8c2798e13dd55b904d6c9b90cfec9c91bd4

--- integrates/front/src/scenes/Dashboard/containers/DescriptionView/ActionButtons/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/containers/DescriptionView/ActionButtons/index.spec.tsx
@@ -1,11 +1,12 @@
+import { ActionButtons } from "scenes/Dashboard/containers/DescriptionView/ActionButtons";
+import type { IActionButtonsProps } from "scenes/Dashboard/containers/DescriptionView/ActionButtons";
 import { PureAbility } from "@casl/ability";
-import { mount, ReactWrapper } from "enzyme";
 import React from "react";
-import { ActionButtons, IActionButtonsProps } from "scenes/Dashboard/containers/DescriptionView/ActionButtons";
+import type { ReactWrapper } from "enzyme";
 import { authzPermissionsContext } from "utils/authz/config";
+import { mount } from "enzyme";
 
-describe("ActionButtons", () => {
-
+describe("ActionButtons", (): void => {
   const baseMockedProps: IActionButtonsProps = {
     isEditing: false,
     isPristine: false,
@@ -13,31 +14,38 @@ describe("ActionButtons", () => {
     onUpdate: jest.fn(),
   };
 
-  it("should return a function", () => {
-    expect(typeof (ActionButtons))
-      .toEqual("function");
+  it("should return a function", (): void => {
+    expect.hasAssertions();
+    expect(typeof ActionButtons).toStrictEqual("function");
   });
 
-  it("should render a component", async () => {
+  it("should render a component", (): void => {
+    expect.hasAssertions();
+
     const mockedPermissions: PureAbility<string> = new PureAbility([
       { action: "backend_api_mutations_update_finding_description_mutate" },
     ]);
+    const { isEditing, isPristine, onEdit, onUpdate } = baseMockedProps;
     const wrapper: ReactWrapper = mount(
       <authzPermissionsContext.Provider value={mockedPermissions}>
-        <ActionButtons {...baseMockedProps} />
-      </authzPermissionsContext.Provider>,
+        <ActionButtons
+          isEditing={isEditing}
+          isPristine={isPristine}
+          onEdit={onEdit}
+          onUpdate={onUpdate}
+        />
+      </authzPermissionsContext.Provider>
     );
-    expect(wrapper)
-      .toHaveLength(1);
-    const buttons: ReactWrapper = wrapper.find("Button");
-    expect(buttons)
-      .toHaveLength(1);
-    expect(buttons
-      .filterWhere((button: ReactWrapper): boolean =>
-        button
-          .text()
-          .includes("Edit")))
-      .toHaveLength(1);
-  });
 
+    expect(wrapper).toHaveLength(1);
+
+    const buttons: ReactWrapper = wrapper.find("Button");
+
+    expect(buttons).toHaveLength(1);
+    expect(
+      buttons.filterWhere((button: ReactWrapper): boolean =>
+        button.text().includes("Edit")
+      )
+    ).toHaveLength(1);
+  });
 });
