713239a332ceec483a5bf88fe1c70172f7882c3f
Jonathan Mesa <jmesa@fluidattacks.com>
2021-02-25 12:16:53 -0500

    integrates\refac(front): #2705.66 update commentsview
    - Lint commentsview test
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: d081101acb63ce21e074945aae3ef1c6a69135a9

--- integrates/front/src/scenes/Dashboard/containers/CommentsView/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/containers/CommentsView/index.spec.tsx
@@ -1,33 +1,25 @@
-import { MockedProvider, MockedResponse } from "@apollo/react-testing";
-import { mount, ReactWrapper } from "enzyme";
-import { default as $ } from "jquery";
-import _ from "lodash";
-import * as React from "react";
-// tslint:disable-next-line: no-submodule-imports
-import { act } from "react-dom/test-utils";
-import { MemoryRouter, Route } from "react-router";
-import wait from "waait";
-
+import $ from "jquery";
 import { CommentsView } from "scenes/Dashboard/containers/CommentsView";
+import { MockedProvider } from "@apollo/react-testing";
+import type { MockedResponse } from "@apollo/react-testing";
+import React from "react";
+import type { ReactWrapper } from "enzyme";
+import { act } from "react-dom/test-utils";
+import { mount } from "enzyme";
+import wait from "waait";
 import {
   GET_FINDING_CONSULTING,
   GET_FINDING_OBSERVATIONS,
 } from "scenes/Dashboard/containers/CommentsView/queries";
+import { MemoryRouter, Route } from "react-router";
 
-jest.mock("jquery-comments_brainkit", () => jest.requireActual("jquery-comments_brainkit")($));
+jest.mock("jquery-comments_brainkit", (): void => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  jest.requireActual("jquery-comments_brainkit")($);
+});
 
-describe("FindingCommentsView", () => {
-  let container: HTMLDivElement | undefined;
-  beforeEach(() => {
-    container = document.createElement("div");
-    document.body.appendChild(container);
-  });
-  afterEach(() => {
-    document.body.removeChild((container as HTMLDivElement));
-    container = undefined;
-  });
-
-  const mocks: ReadonlyArray<MockedResponse> = [
+describe("FindingCommentsView", (): void => {
+  const mocks: readonly MockedResponse[] = [
     {
       request: {
         query: GET_FINDING_CONSULTING,
@@ -37,16 +29,18 @@ describe("FindingCommentsView", () => {
         data: {
           finding: {
             __typename: "Finding",
-            consulting: [{
-              __typename: "Consult",
-              content: "This is a comment",
-              created: "2019/12/04 08:13:53",
-              email: "unittest@fluidattacks.com",
-              fullname: "Test User",
-              id: "1337260012345",
-              modified: "2019/12/04 08:13:53",
-              parent: "0",
-            }],
+            consulting: [
+              {
+                __typename: "Consult",
+                content: "This is a comment",
+                created: "2019/12/04 08:13:53",
+                email: "unittest@fluidattacks.com",
+                fullname: "Test User",
+                id: "1337260012345",
+                modified: "2019/12/04 08:13:53",
+                parent: "0",
+              },
+            ],
             id: "413372600",
           },
         },
@@ -62,98 +56,162 @@ describe("FindingCommentsView", () => {
           finding: {
             __typename: "Finding",
             id: "413372600",
-            observations: [{
-              __typename: "Comment",
-              content: "This is an observation",
-              created: "2019/12/04 08:13:53",
-              email: "unittest@fluidattacks.com",
-              fullname: "Test User",
-              id: "1337260012345",
-              modified: "2019/12/04 08:13:53",
-              parent: "0",
-            }],
+            observations: [
+              {
+                __typename: "Comment",
+                content: "This is an observation",
+                created: "2019/12/04 08:13:53",
+                email: "unittest@fluidattacks.com",
+                fullname: "Test User",
+                id: "1337260012345",
+                modified: "2019/12/04 08:13:53",
+                parent: "0",
+              },
+            ],
           },
         },
       },
-    }];
+    },
+  ];
 
-  it("should return a fuction", () => {
-    expect(typeof (CommentsView))
-      .toEqual("function");
+  it("should return a fuction", (): void => {
+    expect.hasAssertions();
+    expect(typeof CommentsView).toStrictEqual("function");
   });
 
-  it("should render a component", async () => {
+  it("should render a component", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    const container: HTMLDivElement = document.createElement("div");
+    document.body.appendChild(container);
     const wrapper: ReactWrapper = mount(
       <MemoryRouter initialEntries={["/TEST/vulns/413372600/consulting"]}>
-        <MockedProvider mocks={mocks} addTypename={true}>
-          <Route path={"/:projectName/vulns/:findingId/:type"} component={CommentsView}/>
+        <MockedProvider addTypename={true} mocks={mocks}>
+          <Route
+            component={CommentsView}
+            path={"/:projectName/vulns/:findingId/:type"}
+          />
         </MockedProvider>
       </MemoryRouter>,
-      { attachTo: container });
-    await act(async () => { await wait(0); });
-    expect(wrapper)
-      .toHaveLength(1);
+      { attachTo: container }
+    );
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+      }
+    );
+
+    expect(wrapper).toHaveLength(1);
+
+    document.body.removeChild(container);
   });
 
-  it("should render empty UI", async () => {
-    const emptyMocks: ReadonlyArray<MockedResponse> = [{
-      request: {
-        query: GET_FINDING_CONSULTING,
-        variables: { findingId: "413372600" },
-      },
-      result: {
-        data: {
-          finding: {
-            __typename: "Finding",
-            consulting: [],
-            id: "413372600",
+  it("should render empty UI", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    const container: HTMLDivElement = document.createElement("div");
+    document.body.appendChild(container);
+    const emptyMocks: readonly MockedResponse[] = [
+      {
+        request: {
+          query: GET_FINDING_CONSULTING,
+          variables: { findingId: "413372600" },
+        },
+        result: {
+          data: {
+            finding: {
+              __typename: "Finding",
+              consulting: [],
+              id: "413372600",
+            },
           },
         },
       },
-    }];
+    ];
     const wrapper: ReactWrapper = mount(
       <MemoryRouter initialEntries={["/TEST/vulns/413372600/consulting"]}>
-        <MockedProvider mocks={emptyMocks} addTypename={true}>
-          <Route path={"/:projectName/vulns/:findingId/:type"} component={CommentsView}/>
+        <MockedProvider addTypename={true} mocks={emptyMocks}>
+          <Route
+            component={CommentsView}
+            path={"/:projectName/vulns/:findingId/:type"}
+          />
         </MockedProvider>
       </MemoryRouter>,
-      { attachTo: container });
-    await act(async () => { await wait(0); wrapper.update(); });
-    expect(wrapper.text())
-      .toContain("No comments");
+      { attachTo: container }
+    );
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+
+    expect(wrapper.text()).toContain("No comments");
+
+    document.body.removeChild(container);
   });
 
-  it("should render comment", async () => {
+  it("should render comment", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    const container: HTMLDivElement = document.createElement("div");
+    document.body.appendChild(container);
     const wrapper: ReactWrapper = mount(
       <MemoryRouter initialEntries={["/TEST/vulns/413372600/consulting"]}>
-        <MockedProvider mocks={mocks} addTypename={true}>
-          <Route path={"/:projectName/vulns/:findingId/:type"} component={CommentsView}/>
+        <MockedProvider addTypename={true} mocks={mocks}>
+          <Route
+            component={CommentsView}
+            path={"/:projectName/vulns/:findingId/:type"}
+          />
         </MockedProvider>
       </MemoryRouter>,
-      { attachTo: container });
-    await act(async () => { await wait(0); wrapper.update(); });
-    const commentElement: ReactWrapper = wrapper.find("div")
+      { attachTo: container }
+    );
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+    const commentElement: ReactWrapper = wrapper
+      .find("div")
       .find({ id: "finding-consult" });
-    expect(commentElement)
-      .toHaveLength(1);
-    expect(wrapper.text())
-      .toContain("This is a comment");
+
+    expect(commentElement).toHaveLength(1);
+    expect(wrapper.text()).toContain("This is a comment");
+
+    document.body.removeChild(container);
   });
 
-  it("should render observation", async () => {
+  it("should render observation", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    const container: HTMLDivElement = document.createElement("div");
+    document.body.appendChild(container);
     const wrapper: ReactWrapper = mount(
       <MemoryRouter initialEntries={["/TEST/vulns/413372600/observations"]}>
-        <MockedProvider mocks={mocks} addTypename={true}>
-          <Route path={"/:projectName/vulns/:findingId/:type"} component={CommentsView}/>
+        <MockedProvider addTypename={true} mocks={mocks}>
+          <Route
+            component={CommentsView}
+            path={"/:projectName/vulns/:findingId/:type"}
+          />
         </MockedProvider>
       </MemoryRouter>,
-      { attachTo: container });
-    await act(async () => { await wait(0); wrapper.update(); });
-    const commentElement: ReactWrapper = wrapper.find("div")
+      { attachTo: container }
+    );
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+    const commentElement: ReactWrapper = wrapper
+      .find("div")
       .find({ id: "finding-observation" });
-    expect(commentElement)
-      .toHaveLength(1);
-    expect(wrapper.text())
-      .toContain("This is an observation");
+
+    expect(commentElement).toHaveLength(1);
+    expect(wrapper.text()).toContain("This is an observation");
+
+    document.body.removeChild(container);
   });
 });
