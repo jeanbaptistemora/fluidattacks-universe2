592612fb2c9eda6cedf4b1c43cce5ab279863fe6
Jonathan Mesa <jmesa@fluidattacks.com>
2021-02-26 15:07:58 -0500

    integrates\refac(front): #2705.73 update eventcommentsview
    - Lint eventcommentsview test
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: fd3428acfcd8b9e0f00ed09149345d3fd992f14d

--- integrates/front/src/scenes/Dashboard/containers/EventCommentsView/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/containers/EventCommentsView/index.spec.tsx
@@ -1,72 +1,23 @@
-import { MockedProvider, MockedResponse } from "@apollo/react-testing";
-import { mount, ReactWrapper } from "enzyme";
-import { default as $ } from "jquery";
-import _ from "lodash";
-import * as React from "react";
-// tslint:disable-next-line: no-submodule-imports
-import { act } from "react-dom/test-utils";
-import { MemoryRouter, Route } from "react-router";
-import wait from "waait";
-
+import $ from "jquery";
 import { EventCommentsView } from "scenes/Dashboard/containers/EventCommentsView";
 import { GET_EVENT_CONSULTING } from "scenes/Dashboard/containers/EventCommentsView/queries";
+import { MockedProvider } from "@apollo/react-testing";
+import type { MockedResponse } from "@apollo/react-testing";
+import React from "react";
+import type { ReactWrapper } from "enzyme";
+import { act } from "react-dom/test-utils";
+import { mount } from "enzyme";
+import wait from "waait";
+import { MemoryRouter, Route } from "react-router";
 
-jest.mock("jquery-comments_brainkit", () => jest.requireActual("jquery-comments_brainkit")($));
+jest.mock("jquery-comments_brainkit", (): void => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  jest.requireActual("jquery-comments_brainkit")($);
+});
 
-describe("EventCommentsView", () => {
-  let container: HTMLDivElement | undefined;
-  beforeEach(() => {
-    container = document.createElement("div");
-    document.body.appendChild(container);
-  });
-  afterEach(() => {
-    document.body.removeChild((container as HTMLDivElement));
-    container = undefined;
-  });
-
-  const mocks: ReadonlyArray<MockedResponse> = [{
-    request: {
-      query: GET_EVENT_CONSULTING,
-      variables: { eventId: "413372600" },
-    },
-    result: {
-      data: {
-        event: {
-          consulting: [{
-            content: "Hello world",
-            created: "2019/12/04 08:13:53",
-            email: "unittest@fluidattacks.com",
-            fullname: "Test User",
-            id: "1337260012345",
-            modified: "2019/12/04 08:13:53",
-            parent: "0",
-          }],
-          id: "413372600",
-        },
-      },
-    },
-  }];
-
-  it("should return a fuction", () => {
-    expect(typeof (EventCommentsView))
-      .toEqual("function");
-  });
-
-  it("should render a component", async () => {
-    const wrapper: ReactWrapper = mount(
-      <MemoryRouter initialEntries={["/TEST/events/413372600/comments"]}>
-        <MockedProvider mocks={mocks} addTypename={false}>
-          <Route path={"/:projectName/events/:eventId/comments"} component={EventCommentsView}/>
-        </MockedProvider>
-      </MemoryRouter>,
-      { attachTo: container });
-    await act(async () => { await wait(0); });
-    expect(wrapper)
-      .toHaveLength(1);
-  });
-
-  it("should render empty UI", async () => {
-    const emptyMocks: ReadonlyArray<MockedResponse> = [{
+describe("EventCommentsView", (): void => {
+  const mocks: readonly MockedResponse[] = [
+    {
       request: {
         query: GET_EVENT_CONSULTING,
         variables: { eventId: "413372600" },
@@ -74,38 +25,129 @@ describe("EventCommentsView", () => {
       result: {
         data: {
           event: {
-            consulting: [],
+            consulting: [
+              {
+                content: "Hello world",
+                created: "2019/12/04 08:13:53",
+                email: "unittest@fluidattacks.com",
+                fullname: "Test User",
+                id: "1337260012345",
+                modified: "2019/12/04 08:13:53",
+                parent: "0",
+              },
+            ],
             id: "413372600",
           },
         },
       },
-    }];
-    const wrapper: ReactWrapper = mount(
-      <MemoryRouter initialEntries={["/TEST/events/413372600/comments"]}>
-        <MockedProvider mocks={emptyMocks} addTypename={false}>
-          <Route path={"/:projectName/events/:eventId/comments"} component={EventCommentsView}/>
-        </MockedProvider>
-      </MemoryRouter>,
-      { attachTo: container });
-    await act(async () => { await wait(0); wrapper.update(); });
-    expect(wrapper.text())
-      .toContain("No comments");
+    },
+  ];
+
+  it("should return a fuction", (): void => {
+    expect.hasAssertions();
+    expect(typeof EventCommentsView).toStrictEqual("function");
   });
 
-  it("should render comment", async () => {
+  it("should render a component", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    const container: HTMLDivElement = document.createElement("div");
+    document.body.appendChild(container);
     const wrapper: ReactWrapper = mount(
       <MemoryRouter initialEntries={["/TEST/events/413372600/comments"]}>
-        <MockedProvider mocks={mocks} addTypename={false}>
-          <Route path={"/:projectName/events/:eventId/comments"} component={EventCommentsView}/>
+        <MockedProvider addTypename={false} mocks={mocks}>
+          <Route
+            component={EventCommentsView}
+            path={"/:projectName/events/:eventId/comments"}
+          />
         </MockedProvider>
       </MemoryRouter>,
-      { attachTo: container });
-    await act(async () => { await wait(0); wrapper.update(); });
-    const commentElement: ReactWrapper = wrapper.find("div")
+      { attachTo: container }
+    );
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+      }
+    );
+
+    expect(wrapper).toHaveLength(1);
+
+    document.body.removeChild(container);
+  });
+
+  it("should render empty UI", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    const container: HTMLDivElement = document.createElement("div");
+    document.body.appendChild(container);
+    const emptyMocks: readonly MockedResponse[] = [
+      {
+        request: {
+          query: GET_EVENT_CONSULTING,
+          variables: { eventId: "413372600" },
+        },
+        result: {
+          data: {
+            event: {
+              consulting: [],
+              id: "413372600",
+            },
+          },
+        },
+      },
+    ];
+    const wrapper: ReactWrapper = mount(
+      <MemoryRouter initialEntries={["/TEST/events/413372600/comments"]}>
+        <MockedProvider addTypename={false} mocks={emptyMocks}>
+          <Route
+            component={EventCommentsView}
+            path={"/:projectName/events/:eventId/comments"}
+          />
+        </MockedProvider>
+      </MemoryRouter>,
+      { attachTo: container }
+    );
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+
+    expect(wrapper.text()).toContain("No comments");
+
+    document.body.removeChild(container);
+  });
+
+  it("should render comment", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    const container: HTMLDivElement = document.createElement("div");
+    document.body.appendChild(container);
+    const wrapper: ReactWrapper = mount(
+      <MemoryRouter initialEntries={["/TEST/events/413372600/comments"]}>
+        <MockedProvider addTypename={false} mocks={mocks}>
+          <Route
+            component={EventCommentsView}
+            path={"/:projectName/events/:eventId/comments"}
+          />
+        </MockedProvider>
+      </MemoryRouter>,
+      { attachTo: container }
+    );
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+    const commentElement: ReactWrapper = wrapper
+      .find("div")
       .find({ id: "event-comments" });
-    expect(commentElement)
-      .toHaveLength(1);
-    expect(wrapper.text())
-      .toContain("Hello world");
+
+    expect(commentElement).toHaveLength(1);
+    expect(wrapper.text()).toContain("Hello world");
+
+    document.body.removeChild(container);
   });
 });
