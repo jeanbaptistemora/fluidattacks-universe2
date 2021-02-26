88f42bda7dea83a9d91257588e26d2d27589f119
Diego Restrepo <drestrepo@fluidattacks.com>
2021-02-26 16:39:08 -0500

    skims\feat(back): #3674.8 mark cookie
    - mark cookie sink
    
    
    Former-commit-id: d9e47d44509c0b0979faf9ae22ca26b91aebae86

--- skims/skims/sast/transformations/dangerous_action_nodes.py
+++ skims/skims/sast/transformations/dangerous_action_nodes.py
@@ -16,6 +16,7 @@ from utils import (
 
 def _mark_java(graph: graph_model.Graph) -> None:
     _mark_java_f063(graph)
+    _mark_java_f034(graph)
 
 
 def _mark_java_f063(graph: graph_model.Graph) -> None:
@@ -32,6 +33,47 @@ def _mark_java_f063(graph: graph_model.Graph) -> None:
             .F063_PATH_TRAVERSAL
             .name
         )
+
+
+def _mark_java_f034(graph: graph_model.Graph) -> None:
+    identifiers: Set[str] = {
+        *build_attr_paths('javax', 'servlet', 'http', 'Cookie'),
+    }
+
+    for n_id in g.yield_object_creation_expression(graph, identifiers):
+        graph.nodes[n_id]['label_sink_type'] = (
+            core_model
+            .FindingEnum
+            .F034
+            .name
+        )
+
+    for n_id in g.filter_nodes(
+            graph,
+            graph.nodes,
+            predicate=g.pred_has_labels(
+                label_type='method_invocation',
+            )
+    ):
+        match = g.match_ast_group(
+            graph,
+            n_id,
+            'identifier',
+        )
+        if identifier := match.get('identifier'):
+            identifiers = {
+                graph.nodes[iden].get('label_text')
+                for iden in identifier
+            }
+            if len(identifiers) < 2:
+                continue
+            if 'addCookie' in identifiers:
+                graph.nodes[n_id]['label_sink_type'] = (
+                    core_model
+                    .FindingEnum
+                    .F034
+                    .name
+                )
 
 
 def mark(
