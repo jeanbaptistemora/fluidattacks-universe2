77a2ea0f59dceb06fd11a6d1656998dff75b7856
Daniel Murcia <dmurcia@fluidattacks.com>
2021-02-26 08:27:19 -0500

    observes\test(back): #4195.12 mock client
    - added mock client
    - added mock data
    
    
    Former-commit-id: 942b7b9b919d6a5f941385972d02d049a9dc8a44

---
+++ observes/singer/tap_mailchimp/tests/mock_client.py
@@ -0,0 +1,42 @@
+# Standard libraries
+import json
+# Third party libraries
+from mailchimp_marketing import (
+    Client,
+)
+# Local libraries
+from tap_mailchimp.api import (
+    ApiClient,
+)
+from tap_mailchimp import (
+    api,
+)
+from tap_mailchimp.api.raw import (
+    RawSource,
+)
+from tap_mailchimp.common import (
+    JSON,
+)
+
+
+def _list_audiences(client: Client) -> JSON:
+    # pylint: disable=unused-argument
+    with open('./tests/mock_data/audience.json') as data:
+        return json.load(data)['list_audiences']
+
+
+def _get_audience(client: Client, audience_id: str) -> JSON:
+    # pylint: disable=unused-argument
+    with open('./tests/mock_data/audience.json') as data:
+        return json.load(data)['get_audience'][audience_id]
+
+
+def mock_data_source() -> RawSource:
+    return RawSource(
+        list_audiences=_list_audiences,
+        get_audience=_get_audience
+    )
+
+
+def new_client() -> ApiClient:
+    return api.new_client_from_source({}, mock_data_source())
