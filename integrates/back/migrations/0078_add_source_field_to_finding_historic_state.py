33df0d436122501f1f7762a280de8d58c89a2c7f
Andres Aguirre <aaguirre@fluidattacks.com>
2021-02-26 11:57:03 -0500

    integrates\feat(back): #3959.30 add source to finding
    - This migration uses the vulns, the analyst and the date to populate
    the source field into the finding historic state
    
    
    Former-commit-id: 37de002f0b0f15dabd0dc4ce44c7185a284edf3c

---
+++ integrates/back/migrations/0078_add_source_field_to_finding_historic_state.py
@@ -0,0 +1,118 @@
+"""
+This migration uses the vulns, the analyst and the date to populate
+the source field into the finding historic state
+"""
+# Standard library
+import copy
+from pprint import pprint
+from typing import (
+    Dict,
+    cast,
+)
+
+# Third party libraries
+from aioextensions import (
+    collect,
+    run,
+)
+from boto3.dynamodb.conditions import Key
+
+# Local libraries
+from backend.dal.helpers import dynamodb
+from backend.dal import (
+    finding as finding_dal,
+)
+from backend.typing import (
+    Finding,
+    Historic,
+)
+from backend.utils import (
+    datetime as datetime_utils,
+)
+
+FINDING_TABLE: str = 'FI_findings'
+VULNERABILITY_TABLE = 'FI_vulnerabilities'
+
+
+async def add_source_field_to_historic_state(
+    finding: Dict[str, Finding]
+) -> bool:
+    success = True
+    to_update = False
+    vulns = None
+    vulns_have_skims = False
+    finding_id = finding['finding_id']
+    old_finding_historic_state = cast(Historic, finding.get('historic_state', []))
+    finding_historic_state = copy.deepcopy(old_finding_historic_state)
+    vuln_query_attrs = {
+        'KeyConditionExpression': Key('finding_id').eq(finding_id),
+        'ProjectionExpression': ','.join({'historic_state'}),
+    }
+    skims_analysts = {
+        'api-kamado@fluidattacks.com',
+        'api-drestrepo@fluidattacks.com',
+        'kamado@fluidattacks.com',
+        'drestrepo@fluidattacks.com'
+    }
+    for finding_state_info in finding_historic_state:
+        if not finding_state_info.get('source'):
+            to_update = True
+            if (
+                finding_state_info.get('analyst', '') in skims_analysts
+                and (
+                    datetime_utils.get_from_str(finding_state_info['date'])
+                    > datetime_utils.get_from_str('2020-08-20 00:00:00')
+                )
+            ):
+                if vulns == None:
+                    vulns = await dynamodb.async_query(
+                        VULNERABILITY_TABLE,
+                        vuln_query_attrs
+                    )
+                    for vuln in vulns:
+                        vuln_historic_state = cast(Historic, vuln['historic_state'])
+                        for vuln_state_info in vuln_historic_state:
+                            if vuln_state_info['source'] == 'skims':
+                                vulns_have_skims = True
+                if vulns_have_skims:
+                    finding_state_info['source'] = 'skims'
+                else:
+                    finding_state_info['source'] = 'integrates'
+            else:
+                finding_state_info['source'] = 'integrates'
+
+    if to_update:
+        success = await finding_dal.update(
+            finding_id,
+            {
+                "historic_state": finding_historic_state
+            }
+        )
+        print(f'finding_id = {finding_id}')
+        print(f'skims_analysts and vulns_have_skims = {vulns_have_skims}')
+        print('old_finding_historic_state =')
+        pprint(old_finding_historic_state)
+        print('finding_historic_state =')
+        pprint(finding_historic_state)
+
+    return success
+
+
+async def main() -> None:
+    scan_attrs = {
+        'ProjectionExpression': ','.join({'finding_id', 'historic_state'})
+    }
+    findings = await dynamodb.async_scan(FINDING_TABLE, scan_attrs)
+
+    success = all(await collect(
+        [
+            add_source_field_to_historic_state(finding)
+            for finding in findings
+        ]
+    ))
+
+    print(f'Success: {success}')
+
+
+if __name__ == '__main__':
+    run(main())
