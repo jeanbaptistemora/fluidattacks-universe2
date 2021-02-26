0d06803ca042fe637a8e914ee6952d8eb837f71d
Jonathan Mesa <jmesa@fluidattacks.com>
2021-02-26 08:21:14 -0500

    integrates\refac(front): #2705.69 update descriptionview
    - Lint descriptionview actionbuttons
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: f9f4016a4419303b07c6a284a24ff3925b512ade

--- integrates/front/src/scenes/Dashboard/containers/DescriptionView/ActionButtons/index.tsx
+++ integrates/front/src/scenes/Dashboard/containers/DescriptionView/ActionButtons/index.tsx
@@ -1,66 +1,75 @@
-/* tslint:disable:jsx-no-multiline-js
- *
- * jsx-no-multiline-js: Necessary for using conditional rendering
- */
-
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import _ from "lodash";
-import React from "react";
-
 import { Button } from "components/Button";
-import { FluidIcon } from "components/FluidIcon";
-import { TooltipWrapper } from "components/TooltipWrapper";
 import { ButtonToolbarRow } from "styles/styledComponents";
 import { Can } from "utils/authz/Can";
+import { FluidIcon } from "components/FluidIcon";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { TooltipWrapper } from "components/TooltipWrapper";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { translate } from "utils/translations/translate";
 
-export interface IActionButtonsProps {
+interface IActionButtonsProps {
   isEditing: boolean;
   isPristine: boolean;
-  onEdit(): void;
-  onUpdate(): void;
+  onEdit: () => void;
+  onUpdate: () => void;
 }
 
-const actionButtons: React.FC<IActionButtonsProps> = (props: IActionButtonsProps): JSX.Element => {
-  const { onEdit, onUpdate } = props;
+const ActionButtons: React.FC<IActionButtonsProps> = (
+  props: IActionButtonsProps
+): JSX.Element => {
+  const { isEditing, isPristine, onEdit, onUpdate } = props;
 
   return (
     <ButtonToolbarRow>
-      <Can do="backend_api_mutations_update_finding_description_mutate">
-      {props.isEditing ? (
+      <Can do={"backend_api_mutations_update_finding_description_mutate"}>
+        {isEditing ? (
+          <TooltipWrapper
+            id={"search_findings.tab_description.save.tooltip.btn"}
+            message={translate.t(
+              "search_findings.tab_description.save.tooltip"
+            )}
+          >
+            <Button disabled={isPristine} onClick={onUpdate}>
+              <FluidIcon icon={"loading"} />
+              &nbsp;
+              {translate.t("search_findings.tab_description.save.text")}
+            </Button>
+          </TooltipWrapper>
+        ) : undefined}
         <TooltipWrapper
-        id={"search_findings.tab_description.save.tooltip.btn"}
-        message={translate.t("search_findings.tab_description.save.tooltip")}
+          id={
+            "search_findings.tab_description.editable.cancel_edit_tooltip-btn"
+          }
+          message={
+            isEditing
+              ? translate.t(
+                  "search_findings.tab_description.editable.cancel_tooltip"
+                )
+              : translate.t(
+                  "search_findings.tab_description.editable.editable_tooltip"
+                )
+          }
         >
-          <Button onClick={onUpdate} disabled={props.isPristine}>
-            <FluidIcon icon="loading" />&nbsp;
-            {translate.t("search_findings.tab_description.save.text")}
+          <Button onClick={onEdit}>
+            {isEditing ? (
+              <React.Fragment>
+                <FontAwesomeIcon icon={faTimes} />
+                &nbsp;
+                {translate.t("search_findings.tab_description.editable.cancel")}
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                <FluidIcon icon={"edit"} />
+                &nbsp;
+                {translate.t("search_findings.tab_description.editable.text")}
+              </React.Fragment>
+            )}
           </Button>
         </TooltipWrapper>
-      ) : undefined}
-      <TooltipWrapper
-        id={"search_findings.tab_description.editable.cancel_edit_tooltip-btn"}
-        message={props.isEditing
-          ? translate.t("search_findings.tab_description.editable.cancel_tooltip")
-          : translate.t("search_findings.tab_description.editable.editable_tooltip")
-        }
-      >
-        <Button onClick={onEdit}>
-          {props.isEditing ? (
-            <React.Fragment>
-              <FontAwesomeIcon icon={faTimes} />&nbsp;{translate.t("search_findings.tab_description.editable.cancel")}
-            </React.Fragment>
-          ) : (
-            <React.Fragment>
-              <FluidIcon icon="edit" />&nbsp;{translate.t("search_findings.tab_description.editable.text")}
-            </React.Fragment>
-          )}
-        </Button>
-      </TooltipWrapper>
       </Can>
     </ButtonToolbarRow>
   );
 };
 
-export { actionButtons as ActionButtons };
+export { ActionButtons, IActionButtonsProps };
