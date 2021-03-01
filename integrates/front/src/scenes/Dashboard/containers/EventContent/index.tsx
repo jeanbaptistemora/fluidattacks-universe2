3c469535ec4d3d290e30568494dc87996bfa71ef
Jonathan Mesa <jmesa@fluidattacks.com>
2021-03-01 12:37:29 -0500

    integrates\refac(front): #2705.76 update eventcontent
    - Lint eventcontent
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: c77a15ca3f53a0439f05ea01a2ebd1b104db1ace

--- integrates/front/src/scenes/Dashboard/containers/EventContent/index.tsx
+++ integrates/front/src/scenes/Dashboard/containers/EventContent/index.tsx
@@ -1,21 +1,19 @@
-/* tslint:disable:jsx-no-multiline-js
- *
- * Disabling this rule is necessary for accessing render props from
- * apollo components
- */
-import { QueryResult } from "@apollo/react-common";
-import { Query } from "@apollo/react-components";
-import { ApolloError } from "apollo-client";
-import { GraphQLError } from "graphql";
-import _ from "lodash";
-import React from "react";
-import { Redirect, Route, Switch, useParams, useRouteMatch } from "react-router";
-import { NavLink } from "react-router-dom";
-import { EventHeader } from "scenes/Dashboard/components/EventHeader";
+import type { ApolloError } from "apollo-client";
 import { EventCommentsView } from "scenes/Dashboard/containers/EventCommentsView";
-import { GET_EVENT_HEADER } from "scenes/Dashboard/containers/EventContent/queries";
 import { EventDescriptionView } from "scenes/Dashboard/containers/EventDescriptionView/index";
 import { EventEvidenceView } from "scenes/Dashboard/containers/EventEvidenceView";
+import { EventHeader } from "scenes/Dashboard/components/EventHeader";
+import { GET_EVENT_HEADER } from "scenes/Dashboard/containers/EventContent/queries";
+import type { GraphQLError } from "graphql";
+import { Logger } from "utils/logger";
+import { NavLink } from "react-router-dom";
+import { Query } from "@apollo/react-components";
+import type { QueryResult } from "@apollo/react-common";
+import React from "react";
+import _ from "lodash";
+import { msgError } from "utils/notifications";
+import { translate } from "utils/translations/translate";
+import { useTabTracking } from "utils/hooks";
 import {
   Col100,
   Row,
@@ -23,72 +21,114 @@ import {
   TabContent,
   TabsContainer,
 } from "styles/styledComponents";
-import { useTabTracking } from "utils/hooks";
-import { Logger } from "utils/logger";
-import { msgError } from "utils/notifications";
-import { translate } from "utils/translations/translate";
+import {
+  Redirect,
+  Route,
+  Switch,
+  useParams,
+  useRouteMatch,
+} from "react-router";
 
-const eventContent: React.FC = (): JSX.Element => {
+const EventContent: React.FC = (): JSX.Element => {
   const { eventId } = useParams<{ eventId: string }>();
   const { path, url } = useRouteMatch<{ path: string; url: string }>();
 
   // Side effects
   useTabTracking("Event");
 
-  const handleErrors: ((error: ApolloError) => void) = (
-    { graphQLErrors }: ApolloError,
-  ): void => {
-    graphQLErrors.forEach((error: GraphQLError): void => {
-      msgError(translate.t("group_alerts.error_textsad"));
-      Logger.warning("An error occurred loading event header", error);
-    });
-  };
+  const handleErrors: (error: ApolloError) => void = React.useCallback(
+    ({ graphQLErrors }: ApolloError): void => {
+      graphQLErrors.forEach((error: GraphQLError): void => {
+        msgError(translate.t("group_alerts.error_textsad"));
+        Logger.warning("An error occurred loading event header", error);
+      });
+    },
+    []
+  );
 
   return (
     <React.StrictMode>
-      <React.Fragment>
+      <div>
         <Row>
           <Col100>
-            <Query query={GET_EVENT_HEADER} variables={{ eventId }} onError={handleErrors}>
+            <Query
+              onError={handleErrors}
+              query={GET_EVENT_HEADER}
+              variables={{ eventId }}
+            >
               {({ data }: QueryResult): JSX.Element => {
-                if (_.isUndefined(data) || _.isEmpty(data)) { return <React.Fragment />; }
+                if (_.isUndefined(data) || _.isEmpty(data)) {
+                  return <div />;
+                }
 
-                return <EventHeader {...data.event} />;
+                // Eslint annotation needed as the DB handles "any" type
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+                const { eventDate, eventStatus, eventType, id } = data.event;
+
+                return (
+                  <EventHeader
+                    eventDate={eventDate}
+                    eventStatus={eventStatus}
+                    eventType={eventType}
+                    id={id}
+                  />
+                );
               }}
             </Query>
             <TabsContainer>
-              <Tab id="resourcesTab">
-                <NavLink activeClassName={"nav-active-bg"} to={`${url}/description`}>
-                  <i className="icon pe-7s-note2" />
+              <Tab id={"resourcesTab"}>
+                <NavLink
+                  activeClassName={"nav-active-bg"}
+                  to={`${url}/description`}
+                >
+                  <i className={"icon pe-7s-note2"} />
                   &nbsp;{translate.t("search_findings.tab_events.description")}
                 </NavLink>
               </Tab>
-              <Tab id="evidenceTab">
-                <NavLink activeClassName={"nav-active-bg"} to={`${url}/evidence`}>
-                  <i className="icon pe-7s-note2" />
+              <Tab id={"evidenceTab"}>
+                <NavLink
+                  activeClassName={"nav-active-bg"}
+                  to={`${url}/evidence`}
+                >
+                  <i className={"icon pe-7s-note2"} />
                   &nbsp;{translate.t("search_findings.tab_events.evidence")}
                 </NavLink>
               </Tab>
-              <Tab id="commentsTab">
-                <NavLink activeClassName={"nav-active-bg"} to={`${url}/comments`}>
-                  <i className="icon pe-7s-comment" />
+              <Tab id={"commentsTab"}>
+                <NavLink
+                  activeClassName={"nav-active-bg"}
+                  to={`${url}/comments`}
+                >
+                  <i className={"icon pe-7s-comment"} />
                   &nbsp;{translate.t("search_findings.tab_events.comments")}
                 </NavLink>
               </Tab>
             </TabsContainer>
             <TabContent>
               <Switch>
-                <Route path={`${path}/description`} component={EventDescriptionView} exact={true} />
-                <Route path={`${path}/evidence`} component={EventEvidenceView} exact={true} />
-                <Route path={`${path}/comments`} component={EventCommentsView} exact={true} />
+                <Route
+                  component={EventDescriptionView}
+                  exact={true}
+                  path={`${path}/description`}
+                />
+                <Route
+                  component={EventEvidenceView}
+                  exact={true}
+                  path={`${path}/evidence`}
+                />
+                <Route
+                  component={EventCommentsView}
+                  exact={true}
+                  path={`${path}/comments`}
+                />
                 <Redirect to={`${path}/description`} />
               </Switch>
             </TabContent>
           </Col100>
         </Row>
-      </React.Fragment>
-    </React.StrictMode >
+      </div>
+    </React.StrictMode>
   );
 };
 
-export { eventContent as EventContent };
+export { EventContent };
