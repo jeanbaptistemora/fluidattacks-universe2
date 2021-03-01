4d14e78ce8545e4a228176d8a7539754cc336721
Jonathan Mesa <jmesa@fluidattacks.com>
2021-03-01 12:14:47 -0500

    integrates\refac(front): #2705.75 update eventcontent
    - Lint eventcontent test and queries files
    - No visual changes
    
    
    Former-commit-id: 67f4cca3d646ac9979d8b821fce2b5d20ce11a8a

--- integrates/front/src/scenes/Dashboard/containers/EventContent/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/containers/EventContent/index.spec.tsx
@@ -1,58 +1,70 @@
-import { MockedProvider, MockedResponse } from "@apollo/react-testing";
-import { mount, ReactWrapper, shallow, ShallowWrapper } from "enzyme";
-import _ from "lodash";
-import * as React from "react";
-import { MemoryRouter, Route } from "react-router";
-import wait from "waait";
-
 import { EventContent } from "scenes/Dashboard/containers/EventContent";
 import { GET_EVENT_HEADER } from "scenes/Dashboard/containers/EventContent/queries";
+import { MockedProvider } from "@apollo/react-testing";
+import type { MockedResponse } from "@apollo/react-testing";
+import React from "react";
+import wait from "waait";
+import { MemoryRouter, Route } from "react-router";
+import type { ReactWrapper, ShallowWrapper } from "enzyme";
+import { mount, shallow } from "enzyme";
 
-describe("EventContent", () => {
-  const mocks: ReadonlyArray<MockedResponse> = [{
-    request: {
-      query: GET_EVENT_HEADER,
-      variables: { eventId: "413372600" },
-    },
-    result: {
-      data: {
-        event: {
-          eventDate: "2019-12-09 12:00",
-          eventStatus: "SOLVED",
-          eventType: "OTHER",
-          id: "413372600",
+describe("EventContent", (): void => {
+  const mocks: readonly MockedResponse[] = [
+    {
+      request: {
+        query: GET_EVENT_HEADER,
+        variables: { eventId: "413372600" },
+      },
+      result: {
+        data: {
+          event: {
+            eventDate: "2019-12-09 12:00",
+            eventStatus: "SOLVED",
+            eventType: "OTHER",
+            id: "413372600",
+          },
         },
       },
     },
-  }];
+  ];
 
-  it("should return a fuction", () => {
-    expect(typeof (EventContent))
-      .toEqual("function");
+  it("should return a fuction", (): void => {
+    expect.hasAssertions();
+    expect(typeof EventContent).toStrictEqual("function");
   });
 
-  it("should render a component", async () => {
+  it("should render a component", (): void => {
+    expect.hasAssertions();
+
     const wrapper: ShallowWrapper = shallow(
       <MemoryRouter initialEntries={["/TEST/events/413372600/description"]}>
-        <MockedProvider mocks={mocks} addTypename={false}>
-          <Route path={"/:projectName/events/:eventId/description"} component={EventContent}/>
+        <MockedProvider addTypename={false} mocks={mocks}>
+          <Route
+            component={EventContent}
+            path={"/:projectName/events/:eventId/description"}
+          />
         </MockedProvider>
-      </MemoryRouter>,
+      </MemoryRouter>
     );
-    expect(wrapper)
-      .toHaveLength(1);
+
+    expect(wrapper).toHaveLength(1);
   });
 
-  it("should render header component", async () => {
+  it("should render header component", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const wrapper: ReactWrapper = mount(
       <MemoryRouter initialEntries={["/TEST/events/413372600/description"]}>
-        <MockedProvider mocks={mocks} addTypename={false}>
-          <Route path={"/:projectName/events/:eventId/description"} component={EventContent}/>
+        <MockedProvider addTypename={false} mocks={mocks}>
+          <Route
+            component={EventContent}
+            path={"/:projectName/events/:eventId/description"}
+          />
         </MockedProvider>
-      </MemoryRouter>,
+      </MemoryRouter>
     );
     await wait(0);
-    expect(wrapper.text())
-      .toContain("Solved");
+
+    expect(wrapper.text()).toContain("Solved");
   });
 });
