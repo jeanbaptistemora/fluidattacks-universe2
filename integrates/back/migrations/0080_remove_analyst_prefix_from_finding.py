a9aa8a941e215b59ae53dd5ca78d4a108e607d30
Andres Aguirre <aaguirre@fluidattacks.com>
2021-03-01 14:29:40 -0500

    integrates\feat(back): #3959.35 analyst prefix migration
    - This migration removes the api prefix for some analysts
    into the finding historic state
    
    
    Former-commit-id: b581e15c28062edc49396742e83b61a04344ce96

---
+++ integrates/back/migrations/0080_remove_analyst_prefix_from_finding.py
@@ -0,0 +1,84 @@
+"""
+This migration removes the api prefix for some analysts
+into the finding historic state
+"""
+# Standard libraries
+import copy
+from pprint import (
+    pprint,
+)
+from typing import (
+    Dict,
+    cast,
+)
+
+# Third party libraries
+from aioextensions import (
+    collect,
+    run,
+)
+
+# Local libraries
+from backend.dal.helpers import (
+    dynamodb,
+)
+from backend.dal import (
+    finding as finding_dal,
+)
+from backend.typing import (
+    Finding,
+    Historic,
+)
+
+FINDING_TABLE: str = 'FI_findings'
+
+
+async def remove_analyst_prefix_from_historic_state(
+    finding: Dict[str, Finding]
+) -> bool:
+    success = True
+    to_update = False
+    finding_id = finding['finding_id']
+    prefix = 'api-'
+    old_historic_state = cast(Historic, finding.get('historic_state', []))
+    historic_state = copy.deepcopy(old_historic_state)
+
+    for state_info in historic_state:
+        if state_info.get('analyst', '').startswith(prefix):
+            to_update = True
+            state_info['analyst'] = state_info['analyst'][len(prefix):]
+
+    if to_update:
+        success = await finding_dal.update(
+            finding_id,
+            {
+                'historic_state': historic_state
+            }
+        )
+        print(f'finding_id = {finding_id}')
+        print('old_historic_state =')
+        pprint(old_historic_state)
+        print('historic_state =')
+        pprint(historic_state)
+
+    return success
+
+
+async def main() -> None:
+    scan_attrs = {
+        'ProjectionExpression': ','.join({'finding_id', 'historic_state'})
+    }
+    findings = await dynamodb.async_scan(FINDING_TABLE, scan_attrs)
+
+    success = all(await collect(
+        [
+            remove_analyst_prefix_from_historic_state(finding)
+            for finding in findings
+        ]
+    ))
+
+    print(f'Success: {success}')
+
+
+if __name__ == '__main__':
+    run(main())
