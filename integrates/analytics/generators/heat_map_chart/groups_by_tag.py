189eabd5eb5a2a3fea9712c7ae3cf21f754b5b7e
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-03-01 21:40:02 -0500

    integrates\feat(back): #4104.1 add generator
    - add generator for groups by tags and add js generic generator
    for heatmap chart
    - no-deltas-test
    
    
    Former-commit-id: 8892971f47c7b49a6fcfd50b588d0f9027c38812

---
+++ integrates/analytics/generators/heat_map_chart/groups_by_tag.py
@@ -0,0 +1,135 @@
+# Standard libraries
+from itertools import (
+    chain,
+)
+from typing import (
+    Counter,
+    List,
+    NamedTuple,
+    Set,
+    Tuple,
+)
+
+# Third party libraries
+from aioextensions import (
+    collect,
+    run,
+)
+from async_lru import (
+    alru_cache,
+)
+
+# Local libraries
+from backend.api import (
+    get_new_context,
+)
+from analytics import (
+    utils,
+)
+
+GroupsTags = NamedTuple('GroupsTags', [
+    ('counter', Counter[str]),
+    ('counter_group', Counter[str]),
+    ('groups', List[str]),
+    ('tags', Set[str]),
+])
+
+
+@alru_cache(maxsize=None, typed=True)
+async def get_data_one_group(group: str) -> GroupsTags:
+    context = get_new_context()
+    group_findings_loader = context.group_findings
+    finding_vulns_loader = context.finding_vulns
+
+    group_findings_data = await group_findings_loader.load(group.lower())
+    finding_ids = [finding['finding_id'] for finding in group_findings_data]
+
+    vulnerabilities = list(
+        chain.from_iterable(
+            await finding_vulns_loader.load_many(finding_ids)
+        )
+    )
+
+    tags: List[str] = list(filter(None, chain.from_iterable(
+        map(lambda x: x['tag'].split(', '), vulnerabilities)
+    )))
+
+    return GroupsTags(
+        counter=Counter(tags),
+        counter_group=Counter([f'{group.lower()}/{tag}' for tag in tags]),
+        groups=[group] if tags else [],
+        tags=set(tags)
+    )
+
+
+async def get_data_many_groups(groups: List[str]) -> GroupsTags:
+    groups_data = await collect(map(get_data_one_group, groups))
+    all_tags = [group_data.tags for group_data in groups_data]
+
+    return GroupsTags(
+        counter=sum(
+            [group_data.counter for group_data in groups_data],
+            Counter()
+        ),
+        counter_group=sum(
+            [group_data.counter_group for group_data in groups_data],
+            Counter()
+        ),
+        groups=[
+            group.lower() for group, group_data in zip(groups, groups_data)
+            if group_data.groups
+        ],
+        tags=set.union(*all_tags) if all_tags else set(),
+    )
+
+
+def format_data(data: GroupsTags) -> dict:
+    max_value: List[Tuple[str, int]] = data.counter_group.most_common(1)
+    tags: Set[str] = {tag for tag, _ in data.counter.most_common()[:10]}
+    groups: Set[str] = {
+        group
+        for group in data.groups
+        for tag in tags
+        if data.counter_group[f'{group}/{tag}'] > 0
+    }
+
+    return dict(
+        x=groups,
+        grid_values=[
+            {
+                'value': data.counter_group[f'{group}/{tag}'],
+                'x': group,
+                'y': tag,
+            }
+            for group in groups
+            for tag in tags
+        ],
+        y=tags,
+        max_value=max_value[0][1] if max_value else 1,
+        tick_rotate=utils.TICK_ROTATION,
+    )
+
+
+async def generate_all():
+    async for org_id, _, org_groups in (
+        utils.iterate_organizations_and_groups()
+    ):
+        utils.json_dump(
+            document=format_data(
+                data=await get_data_many_groups(list(org_groups)),
+            ),
+            entity='organization',
+            subject=org_id,
+        )
+
+    async for org_id, org_name, _ in utils.iterate_organizations_and_groups():
+        for portfolio, groups in await utils.get_portfolios_groups(org_name):
+            utils.json_dump(
+                document=format_data(data=await get_data_many_groups(groups)),
+                entity='portfolio',
+                subject=f'{org_id}PORTFOLIO#{portfolio}',
+            )
+
+
+if __name__ == '__main__':
+    run(generate_all())
