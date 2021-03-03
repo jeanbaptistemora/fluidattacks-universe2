b16defd7a25504a1f497efecd422cadcb93df53d
Rafael Rodriguez <rrodriguez@fluidattacks.com>
2021-03-03 10:00:22 -0500

    integrates\refac(front): #273.13 migrate to apollo hooks
    - Migrate event comments view to apollo hooks
    - no-deltas-test
    
    
    Former-commit-id: 40666d70d38d2d29374c25776149864813c1c4f8

--- integrates/front/src/scenes/Dashboard/containers/EventCommentsView/index.tsx
+++ integrates/front/src/scenes/Dashboard/containers/EventCommentsView/index.tsx
@@ -19,8 +19,13 @@ import type {
   ILoadCallback,
   IPostCallback,
 } from "scenes/Dashboard/components/Comments/types";
-import { Mutation, Query } from "@apollo/react-components";
-import type { MutationFunction, QueryResult } from "@apollo/react-common";
+import { useMutation, useQuery } from "@apollo/react-hooks";
+
+interface IEventConsultingData {
+  event: {
+    consulting: ICommentStructure[];
+  };
+}
 
 const EventCommentsView: React.FC = (): JSX.Element => {
   const { eventId } = useParams<{ eventId: string }>();
@@ -36,107 +41,100 @@ const EventCommentsView: React.FC = (): JSX.Element => {
     []
   );
 
+  const { data, loading } = useQuery<IEventConsultingData>(
+    GET_EVENT_CONSULTING,
+    {
+      fetchPolicy: "network-only",
+      onError: handleErrors,
+      variables: { eventId },
+    }
+  );
+
+  const getData: (callback: ILoadCallback) => void = React.useCallback(
+    (callbackFn: (cData: ICommentStructure[]) => void): void => {
+      if (!_.isUndefined(data)) {
+        callbackFn(
+          data.event.consulting.map(
+            (comment: ICommentStructure): ICommentStructure => ({
+              ...comment,
+              created_by_current_user: comment.email === userEmail,
+              id: Number(comment.id),
+              parent: Number(comment.parent),
+            })
+          )
+        );
+      }
+    },
+    [data, userEmail]
+  );
+
+  const handleAddCommentError: (addCommentError: ApolloError) => void = (
+    addCommentError: ApolloError
+  ): void => {
+    addCommentError.graphQLErrors.forEach(({ message }: GraphQLError): void => {
+      switch (message) {
+        case "Exception - Comment parent is invalid":
+          msgError(
+            translate.t("validations.invalidCommentParent", {
+              count: 1,
+            })
+          );
+          break;
+        default:
+          msgError(translate.t("group_alerts.error_textsad"));
+          Logger.warning(
+            "An error occurred posting event comment",
+            addCommentError
+          );
+      }
+    });
+  };
+
+  const [addComment] = useMutation(ADD_EVENT_CONSULT, {
+    onError: handleAddCommentError,
+  });
+
+  const handlePost: (
+    comment: ICommentStructure,
+    callbackFn: IPostCallback
+  ) => void = React.useCallback(
+    (comment: ICommentStructure, callbackFn: IPostCallback): void => {
+      interface IMutationResult {
+        data: {
+          addEventConsult: {
+            commentId: string;
+            success: boolean;
+          };
+        };
+      }
+      mixpanel.track("AddEventComment", { eventId });
+      void addComment({ variables: { eventId, ...comment } }).then(
+        (mtResult: unknown | null): void => {
+          const result: IMutationResult["data"] = (mtResult as IMutationResult)
+            .data;
+          if (result.addEventConsult.success) {
+            callbackFn({
+              ...comment,
+              id: Number(result.addEventConsult.commentId),
+            });
+          }
+        }
+      );
+    },
+    [addComment, eventId]
+  );
+
+  if (_.isUndefined(data) || loading) {
+    return <div />;
+  }
+
   return (
     <React.StrictMode>
-      <Query
-        fetchPolicy={"network-only"}
-        onError={handleErrors}
-        query={GET_EVENT_CONSULTING}
-        variables={{ eventId }}
-      >
-        {({ data, loading }: QueryResult): JSX.Element => {
-          if (_.isUndefined(data) || loading) {
-            return <div />;
-          }
-          const getData: (callback: ILoadCallback) => void = (
-            callbackFn: (cData: ICommentStructure[]) => void
-          ): void => {
-            callbackFn(
-              // Next eslint annotations needed due to the usage of any type in DB query results
-              // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-              data.event.consulting.map(
-                (comment: ICommentStructure): ICommentStructure => ({
-                  ...comment,
-                  created_by_current_user: comment.email === userEmail,
-                  id: Number(comment.id),
-                  parent: Number(comment.parent),
-                })
-              )
-            );
-          };
-
-          const handleAddCommentError: (
-            addCommentError: ApolloError
-          ) => void = (addCommentError: ApolloError): void => {
-            addCommentError.graphQLErrors.forEach(
-              ({ message }: GraphQLError): void => {
-                switch (message) {
-                  case "Exception - Comment parent is invalid":
-                    msgError(
-                      translate.t("validations.invalidCommentParent", {
-                        count: 1,
-                      })
-                    );
-                    break;
-                  default:
-                    msgError(translate.t("group_alerts.error_textsad"));
-                    Logger.warning(
-                      "An error occurred posting event comment",
-                      addCommentError
-                    );
-                }
-              }
-            );
-          };
-
-          return (
-            <Mutation
-              mutation={ADD_EVENT_CONSULT}
-              onError={handleAddCommentError} // eslint-disable-line react/jsx-no-bind -- Annotation needed due to nested callback
-            >
-              {(addComment: MutationFunction): JSX.Element => {
-                const handlePost: (
-                  comment: ICommentStructure,
-                  callbackFn: IPostCallback
-                ) => void = (
-                  comment: ICommentStructure,
-                  callbackFn: IPostCallback
-                ): void => {
-                  interface IMutationResult {
-                    data: {
-                      addEventConsult: {
-                        commentId: string;
-                        success: boolean;
-                      };
-                    };
-                  }
-                  mixpanel.track("AddEventComment", { eventId });
-                  void addComment({ variables: { eventId, ...comment } }).then(
-                    (mtResult: unknown | null): void => {
-                      const result: IMutationResult["data"] = (mtResult as IMutationResult)
-                        .data;
-                      if (result.addEventConsult.success) {
-                        callbackFn({
-                          ...comment,
-                          id: Number(result.addEventConsult.commentId),
-                        });
-                      }
-                    }
-                  );
-                };
-
-                return (
-                  <Comments
-                    id={"event-comments"}
-                    onLoad={getData} // eslint-disable-line react/jsx-no-bind -- Annotation needed due to nested callback
-                    onPostComment={handlePost} // eslint-disable-line react/jsx-no-bind -- Annotation needed due to nested callback
-                  />
-                );
-              }}
-            </Mutation>
-          );
-        }}
-      </Query>
+      <Comments
+        id={"event-comments"}
+        onLoad={getData}
+        onPostComment={handlePost}
+      />
     </React.StrictMode>
   );
 };
