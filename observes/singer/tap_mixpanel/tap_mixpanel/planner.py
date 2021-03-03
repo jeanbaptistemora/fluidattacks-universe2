22a858db0e735f27596e4989d974cbbf9400bab4
Daniel Murcia <dmurcia@fluidattacks.com>
2021-03-03 16:59:46 -0500

    observes\fix(back): #4256.4 fit the test
    - adjust planner to fit the test
    - added test to ci
    
    
    Former-commit-id: bbb40c7c2b6ae08e7f48812201b4d92dfce74056

--- observes/singer/tap_mixpanel/tap_mixpanel/planner.py
+++ observes/singer/tap_mixpanel/tap_mixpanel/planner.py
@@ -12,7 +12,7 @@ import pandas
 # Local libraries
 
 
-Date = datetime.date
+Timestamp = Union[pandas.Timestamp]
 Interval = Union[pandas.Interval]
 IntervalIndex = Union[pandas.IntervalIndex]
 
@@ -22,21 +22,24 @@ class TargetDates(NamedTuple):
     actual_range: Optional[Interval]
 
 
-def previous_date_ranges(init_date: Date) -> IntervalIndex:
+def previous_date_ranges(
+    init_date: Timestamp,
+    end_date: Timestamp
+) -> IntervalIndex:
     offset = pandas.offsets.MonthBegin(n=1)
     date_range = pandas.interval_range(
         start=init_date - offset,
-        end=datetime.date.today(),
+        end=end_date + pandas.DateOffset(days=1),
         freq='MS',
         closed='left'
     )
     return date_range
 
 
-def target_dates(init_date: Date) -> TargetDates:
+def target_dates(init_date: Timestamp) -> TargetDates:
     offset = pandas.offsets.MonthBegin(n=1)
-    today = datetime.date.today()
-    previous_ranges = previous_date_ranges(init_date)
+    today = Timestamp(datetime.date.today())
+    previous_ranges = previous_date_ranges(init_date, today)
     actual_range = Interval(left=today - offset, right=today, closed='both')
     return TargetDates(
         previous_ranges=previous_ranges,
