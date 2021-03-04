826f283ea9355a6d2197fa9350aef29ae0e62081
Kevin Amado <kamado@fluidattacks.com>
2021-03-04 15:15:11 -0500

    airs\feat(build): #3836.362 words spelling
    - Check words spelling, add a lot of non-compliant files
      to the exceptions list
    - no-deltas-test
    
    
    Former-commit-id: 803f6c21bbb84d64fbcc7eb932a38b191f06d802

--- makes/packages/airs/adoc/linter/template.sh
+++ makes/packages/airs/adoc/linter/template.sh
@@ -5,10 +5,8 @@ function check_adoc_main_title {
   local msg='File must contain exactly one title'
 
       titles_count="$(grep -Pc '^=\s.*$' "${target}" || true)" \
-  &&  if test "${titles_count}" = '1'
+  &&  if test "${titles_count}" != '1'
       then
-        echo "[INFO] PASSED: ${msg}: ${target}"
-      else
         abort "[ERROR] ${msg}: ${target}"
       fi
 }
@@ -23,10 +21,8 @@ function check_adoc_min_keywords {
           | tr ',' '\n' \
           | wc -l \
       )" \
-  &&  if test "${keywords}" -ge "${min_keywords}"
+  &&  if test "${keywords}" -lt "${min_keywords}"
       then
-        echo "[INFO] PASSED: ${msg}: ${target}"
-      else
         abort "[ERROR] ${msg}: ${target}"
       fi
 }
@@ -46,8 +42,44 @@ function check_adoc_fluid_attacks_name {
       -e '\bFLUIDAttacks' \
       "${target}"
   then
-    echo "[ERROR] ${msg}: ${target}"
-  else
-    echo "[INFO] PASSED: ${msg}: ${target}"
+    abort "[ERROR] ${msg}: ${target}"
   fi
+}
+
+function check_adoc_words_case {
+  local target="${1}"
+  local words=(
+    'AsciiDoc'
+    'bWAPP'
+    'CEH'
+    'COBOL'
+    'C Sharp'
+    'GlassFish'
+    'HTML'
+    'Java'
+    'JavaScript'
+    'Linux'
+    'MySQL'
+    'OpenSSL'
+    'OSCP'
+    'OSWP'
+    'OWASP'
+    'Python'
+    'Red Hat'
+    'RPG'
+    'Scala'
+    'SQLi'
+  )
+  local msg='Spelling'
+
+    for word in "${words[@]}"
+    do
+          case_insensitive="$(grep -ioP "( |^)${word}( |$)" "${target}" || true)" \
+      &&  case_sensitive="$(grep -oP "( |^)${word}( |$)" "${target}" || true)" \
+      &&  if test "${case_insensitive}" != "${case_sensitive}"
+          then
+            abort "[ERROR] ${msg}: ${word}: ${target}"
+          fi \
+      ||  return 1
+    done
 }
--- makes/packages/airs/lint/content/builder.sh
+++ makes/packages/airs/lint/content/builder.sh
@@ -3,11 +3,14 @@
 function main {
       find "${envAirs}" -wholename '*.adoc' \
         | grep --file "${envExclude}" --fixed-strings --invert-match \
+        | sort \
         | while read -r path
           do
-                check_adoc_fluid_attacks_name "${path}" \
+                echo "[INFO] Veryfying: ${path}" \
+            &&  check_adoc_fluid_attacks_name "${path}" \
             &&  check_adoc_main_title "${path}" \
             &&  check_adoc_min_keywords "${path}" \
+            &&  check_adoc_words_case "${path}" \
             ||  return 1
           done \
   &&  touch "${out}" \
