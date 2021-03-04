4e12331a9450c7d2ff37aa28c4787ff0d2fc6b91
Daniel Murcia <dmurcia@fluidattacks.com>
2021-03-04 16:41:20 -0500

    observes\feat(back): #4256.6 extractor
    - added get_backup
    - added get_extremes
    - added get_from_api
    
    
    Former-commit-id: 22ff49c8df06abfe3fe21eca5920bcd226bd285d

--- observes/singer/tap_mixpanel/tap_mixpanel/extractor.py
+++ observes/singer/tap_mixpanel/tap_mixpanel/extractor.py
@@ -1,14 +1,23 @@
 # Standard libraries
+import tempfile
+from contextlib import (
+    contextmanager,
+)
 from typing import (
     Any,
     Callable,
+    ContextManager,
     IO,
     Iterator,
     NamedTuple,
+    Tuple,
+    Union,
 )
 
 # Third party libraries
 import botocore
+import pandas
+
 # Local libraries
 
 
@@ -17,11 +26,14 @@ BACKUP_FOLDER = 'backup_mixpanel'
 
 
 class SingerFile(NamedTuple):
-    file: Iterator[IO[str]]
+    file_handler: Callable[[], ContextManager[IO[str]]]
 
 
 class Extractor(NamedTuple):
     get_singer: Callable[[int, int], SingerFile]
+
+
+Interval = Union[pandas.Interval]
 
 
 def in_backup(s3_client: Any, year: int, month: int) -> bool:
@@ -35,3 +47,41 @@ def in_backup(s3_client: Any, year: int, month: int) -> bool:
         if error.response['Error']['Code'] == "404":
             return False
         raise error
+
+
+def get_backup(s3_client: Any, year: int, month: int) -> SingerFile:
+    @contextmanager
+    def handler() -> Iterator[IO[str]]:
+        with tempfile.NamedTemporaryFile('w+') as tmp:
+            s3_client.meta.client.download_file(
+                BUCKET_NAME,
+                f'{BACKUP_FOLDER}/{year}-{month}.singer',
+                tmp.name
+            )
+            yield tmp
+    return SingerFile(file_handler=handler)
+
+
+def get_extremes(date_range: Interval) -> Tuple[str, str]:
+    start = date_range.left \
+        if date_range.closed == 'left' or date_range.closed == 'both' \
+        else date_range.left + pandas.DateOffset(days=1)
+    end = date_range.right \
+        if date_range.closed == 'right' or date_range.closed == 'both' \
+        else date_range.right - pandas.DateOffset(days=1)
+    start_date = pandas.to_datetime(start).strftime("%Y-%m-%d")
+    end_date = pandas.to_datetime(end).strftime("%Y-%m-%d")
+    return (start_date, end_date)
+
+
+def get_from_api(
+    api_client: Any,
+    event: str,
+    date_range: Interval,
+) -> SingerFile:
+    @contextmanager
+    def handler() -> Iterator[IO[str]]:
+        with tempfile.NamedTemporaryFile('w+') as tmp:
+            tmp.write(api_client.load_data(event, get_extremes(date_range)))
+            yield tmp
+    return SingerFile(file_handler=handler)
