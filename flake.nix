1e0776d2c09a4656a4074551b324eb5252b97c0b
Kevin Amado <kamado@fluidattacks.com>
2021-03-05 13:04:51 -0500

    reviews\perf(build): #3836.378 cache hits
    - Improve cache hits by re-using dependencies from
      other products
    - Reduce total closure size by pinning to the same hash
    
    
    Former-commit-id: d5dc1d28b1ced4a014189c7c5fb9b6575d6491a1

--- flake.nix
+++ flake.nix
@@ -2,20 +2,23 @@
   description = "Fluid Attacks, We hack your software!";
   inputs = {
     flakeCompat = { url = "github:edolstra/flake-compat"; flake = false; };
+
     srcAirsPkgs = { url = "https://github.com/nixos/nixpkgs/archive/7138a338b58713e0dea22ddab6a6785abec7376a.tar.gz"; flake = false; };
     srcAssertsPkgs = { url = "https://github.com/nixos/nixpkgs/archive/7138a338b58713e0dea22ddab6a6785abec7376a.tar.gz"; flake = false; };
     srcForcesPkgs = { url = "https://github.com/nixos/nixpkgs/archive/7138a338b58713e0dea22ddab6a6785abec7376a.tar.gz"; flake = false; };
-    srcIntegratesMobilePkgs = { url = "github:NixOS/nixpkgs/932941b79c3dbbef2de9440e1631dfec43956261"; flake = false; };
-    srcIntegratesPkgs = { url = "github:NixOS/nixpkgs/a437fe25652dea5b86de63891ce9c779c6e8bb9d"; flake = false; };
     srcMakesPkgs = { url = "https://github.com/nixos/nixpkgs/archive/7138a338b58713e0dea22ddab6a6785abec7376a.tar.gz"; flake = false; };
     srcMeltsPkgs = { url = "https://github.com/nixos/nixpkgs/archive/7138a338b58713e0dea22ddab6a6785abec7376a.tar.gz"; flake = false; };
-    srcObservesPkgs = { url = "github:NixOS/nixpkgs/a437fe25652dea5b86de63891ce9c779c6e8bb9d"; flake = false; };
-    srcReviewsPkgs = { url = "github:NixOS/nixpkgs/f99908924015bb83df8186b2c473919be35b43f0"; flake = false; };
+    srcReviewsPkgs = { url = "https://github.com/nixos/nixpkgs/archive/7138a338b58713e0dea22ddab6a6785abec7376a.tar.gz"; flake = false; };
+
     srcServesPkgs = { url = "github:NixOS/nixpkgs/f99908924015bb83df8186b2c473919be35b43f0"; flake = false; };
     srcSkimsBenchmarkOwaspRepo = { url = "github:OWASP/Benchmark/9a0c25a5f8443245c676965d20d22d5f93da3f99"; flake = false; };
     srcSkimsPkgs = { url = "github:NixOS/nixpkgs/a437fe25652dea5b86de63891ce9c779c6e8bb9d"; flake = false; };
     srcSkimsTreeSitterRepo = { url = "github:tree-sitter/tree-sitter-java"; flake = false; };
     srcSortsPkgs = { url = "github:NixOS/nixpkgs/5d5e970ce04933576957dfbf99cb7d4c1802c60d"; flake = false; };
+
+    srcIntegratesMobilePkgs = { url = "github:NixOS/nixpkgs/932941b79c3dbbef2de9440e1631dfec43956261"; flake = false; };
+    srcIntegratesPkgs = { url = "github:NixOS/nixpkgs/a437fe25652dea5b86de63891ce9c779c6e8bb9d"; flake = false; };
+    srcObservesPkgs = { url = "github:NixOS/nixpkgs/a437fe25652dea5b86de63891ce9c779c6e8bb9d"; flake = false; };
   };
   outputs = attrs: import ./makes attrs;
 }
