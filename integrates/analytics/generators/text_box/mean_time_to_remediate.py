dae93a5527727c1a9875162597777391f8f67d25
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-03-05 14:10:16 -0500

    integrates\refac(back): #4257.2 update mean_time generator
    - update mean_time_to_remediate generator to include 30 or 90 days
    
    
    Former-commit-id: abbbabd9ee20faab3e67aab0beee12ed59752c4e

--- integrates/analytics/generators/text_box/mean_time_to_remediate.py
+++ integrates/analytics/generators/text_box/mean_time_to_remediate.py
@@ -1,4 +1,7 @@
 # Standard library
+from datetime import (
+    date,
+)
 from decimal import (
     Decimal,
 )
@@ -6,6 +9,9 @@ from statistics import (
     mean,
 )
 from typing import (
+    Any,
+    List,
+    Optional,
     Tuple,
 )
 
@@ -19,24 +25,37 @@ from async_lru import (
 )
 
 # Local libraries
-from backend.api import get_new_context
-
+from backend.api import (
+    get_new_context,
+)
+from backend.domain.project import (
+    get_mean_remediate,
+)
 from analytics import (
     utils,
+)
+from newutils import (
+    datetime as datetime_utils,
 )
 
 
 @alru_cache(maxsize=None, typed=True)
-async def generate_one(group: str) -> Decimal:
-    context = get_new_context()
-    group_loader = context.group_all
-    group_data = await group_loader.load(group)
+async def generate_one(
+    group: str,
+    context: Any,
+    min_date: Optional[date] = None
+) -> Decimal:
+    return await get_mean_remediate(context, group.lower(), min_date)
 
-    return group_data['mean_remediate']
 
-
-async def get_many_groups(groups: Tuple[str]) -> Decimal:
-    groups_data = await collect(map(generate_one, list(groups)))
+async def get_many_groups(
+    groups: Tuple[str, ...],
+    context: Any,
+    min_date: Optional[date] = None
+) -> Decimal:
+    groups_data = await collect(
+        generate_one(group, context, min_date) for group in list(groups)
+    )
 
     return (
         Decimal(mean(groups_data)).quantize(Decimal('0.1'))
@@ -51,11 +70,12 @@ def format_data(mean_remediate: Decimal) -> dict:
     }
 
 
-async def generate_all():
+async def generate_all() -> None:
+    context = get_new_context()
     async for group in utils.iterate_groups():
         utils.json_dump(
             document=format_data(
-                mean_remediate=await generate_one(group),
+                mean_remediate=await generate_one(group, context),
             ),
             entity='group',
             subject=group,
@@ -67,7 +87,7 @@ async def generate_all():
         utils.json_dump(
             document=format_data(
                 mean_remediate=await get_many_groups(
-                    org_groups
+                    org_groups, context
                 ),
             ),
             entity='organization',
@@ -80,11 +100,56 @@ async def generate_all():
         for portfolio, groups in await utils.get_portfolios_groups(org_name):
             utils.json_dump(
                 document=format_data(
-                    mean_remediate=await get_many_groups(groups),
+                    mean_remediate=await get_many_groups(groups, context),
                 ),
                 entity='portfolio',
                 subject=f'{org_id}PORTFOLIO#{portfolio}',
             )
+
+    # Limit days
+    list_days: List[int] = [30, 90]
+    dates: List[date] = [
+        datetime_utils.get_now_minus_delta(days=list_days[0]).date(),
+        datetime_utils.get_now_minus_delta(days=list_days[1]).date()
+    ]
+    for days, min_date in zip(list_days, dates):
+        async for group in utils.iterate_groups():
+            utils.json_dump(
+                document=format_data(
+                    mean_remediate=await generate_one(
+                        group, context, min_date
+                    ),
+                ),
+                entity='group',
+                subject=f'{group}_{days}',
+            )
+
+        async for org_id, _, org_groups in (
+            utils.iterate_organizations_and_groups()
+        ):
+            utils.json_dump(
+                document=format_data(
+                    mean_remediate=await get_many_groups(
+                        org_groups, context
+                    ),
+                ),
+                entity='organization',
+                subject=f'{org_id}_{days}',
+            )
+
+        async for org_id, org_name, _ in (
+            utils.iterate_organizations_and_groups()
+        ):
+            for portfolio, groups in await utils.get_portfolios_groups(
+                org_name
+            ):
+                utils.json_dump(
+                    document=format_data(
+                        mean_remediate=await get_many_groups(groups, context),
+                    ),
+                    entity='portfolio',
+                    subject=f'{org_id}PORTFOLIO#{portfolio}_{days}',
+                )
 
 
 if __name__ == '__main__':
