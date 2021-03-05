33335004753e72e789993ea8dbb9e794bdf33b93
Rafael Rodriguez <rrodriguez@fluidattacks.com>
2021-03-05 11:17:33 -0500

    integrates\refac(front): #273.17 migrate to apollo hooks
    - Migrate severity view to apollo hooks
    - no-deltas-test
    
    
    Former-commit-id: 720108b2e8d1dbab6677c2995d4d752396b5f9fb

--- integrates/front/src/scenes/Dashboard/containers/SeverityView/index.tsx
+++ integrates/front/src/scenes/Dashboard/containers/SeverityView/index.tsx
@@ -1,10 +1,4 @@
-/* tslint:disable:jsx-no-multiline-js
- *
- * Disabling this rule is necessary for accessing render props from
- * apollo components
- */
-import { MutationFunction, MutationResult, QueryResult } from "@apollo/react-common";
-import { Mutation, Query } from "@apollo/react-components";
+import { useMutation, useQuery } from "@apollo/react-hooks";
 import { PureAbility } from "@casl/ability";
 import { useAbility } from "@casl/react";
 import { ApolloError } from "apollo-client";
@@ -62,162 +56,160 @@ const severityView: React.FC = (): JSX.Element => {
     });
   };
 
+  const {client, data, refetch } = useQuery<ISeverityAttr>(GET_SEVERITY, {
+    onError: handleErrors,
+    variables: { identifier: findingId },
+  });
+
+  const handleEditClick: (() => void) = (): void => {
+    setEditing(!isEditing);
+    if (!_.isUndefined(data)) {
+      const severityScore: string = Number(calcCVSSv3(data.finding.severity))
+        .toFixed(2);
+      client.writeData({ data: { finding: { id: findingId, severityScore, __typename: "Finding" } } });
+    }
+  };
+
+  const handleMtUpdateSeverityRes: ((mtResult: IUpdateSeverityAttr) => void) =
+    (mtResult: IUpdateSeverityAttr): void => {
+      if (!_.isUndefined(mtResult)) {
+        if (mtResult.updateSeverity.success) {
+          void refetch();
+          msgSuccess(translate.t("group_alerts.updated"), translate.t("group_alerts.updated_title"));
+          mixpanel.track("UpdateSeverity");
+        }
+      }
+  };
+
+  const handleMtError: ((error: ApolloError) => void) = (
+    { graphQLErrors }: ApolloError,
+  ): void => {
+    graphQLErrors.forEach((error: GraphQLError): void => {
+      msgError(translate.t("group_alerts.error_textsad"));
+      Logger.warning("An error occurred updating severity", error);
+    });
+  };
+
+  const [updateSeverity, mutationRes] = useMutation(UPDATE_SEVERITY_MUTATION, {
+    onCompleted: handleMtUpdateSeverityRes,
+    onError: handleMtError,
+    refetchQueries: [
+      {
+        query: GET_FINDING_HEADER,
+        variables: {
+          canGetExploit: groupPermissions.can("has_forces"),
+          canGetHistoricState: permissions.can("backend_api_resolvers_finding_historic_state_resolve"),
+          findingId,
+        },
+      },
+    ],
+  });
+
+  const handleUpdateSeverity: ((values: {}) => void) = (values: {}): void => {
+    setEditing(false);
+    void updateSeverity({ variables: { data: { ...values, id: findingId }, findingId } });
+  };
+
+  const handleFormChange: ((values: ISeverityAttr["finding"]["severity"]) => void) = (
+    values: ISeverityAttr["finding"]["severity"],
+  ): void => {
+    const severityScore: string = Number(calcCVSSv3(values))
+      .toFixed(2);
+    client.writeData({
+      data: {
+        finding: { id: findingId, severityScore, __typename: "Finding" },
+      },
+    });
+  };
+
+  if (_.isUndefined(data) || _.isEmpty(data)) {
+    return <div />;
+  }
+
   return (
     <React.StrictMode>
       <Row>
         <Col100>
-          <Query query={GET_SEVERITY} variables={{ identifier: findingId }} onError={handleErrors}>
-            {({ client, data, refetch }: QueryResult<ISeverityAttr>): JSX.Element => {
-              if (_.isUndefined(data) || _.isEmpty(data)) { return <React.Fragment />; }
-
-              const handleEditClick: (() => void) = (): void => {
-                setEditing(!isEditing);
-                const severityScore: string = Number(calcCVSSv3(data.finding.severity))
-                  .toFixed(2);
-                client.writeData({ data: { finding: { id: findingId, severityScore, __typename: "Finding" } } });
-              };
-
-              const handleMtUpdateSeverityRes: ((mtResult: IUpdateSeverityAttr) => void) =
-                (mtResult: IUpdateSeverityAttr): void => {
-                  if (!_.isUndefined(mtResult)) {
-                    if (mtResult.updateSeverity.success) {
-                      void refetch();
-                      msgSuccess(translate.t("group_alerts.updated"), translate.t("group_alerts.updated_title"));
-                      mixpanel.track("UpdateSeverity");
-                    }
-                  }
-                };
-
-              const handleMtError: ((error: ApolloError) => void) = (
-                { graphQLErrors }: ApolloError,
-              ): void => {
-                graphQLErrors.forEach((error: GraphQLError): void => {
-                  msgError(translate.t("group_alerts.error_textsad"));
-                  Logger.warning("An error occurred updating severity", error);
-                });
-              };
-
-              return (
+          <React.Fragment>
+            <Can do="backend_api_mutations_update_severity_mutate">
+              <ButtonToolbarRow>
+                <TooltipWrapper
+                  id={translate.t("search_findings.tab_severity.editable_tooltip.id")}
+                  message={translate.t("search_findings.tab_severity.editable_tooltip")}
+                >
+                  <Button onClick={handleEditClick}>
+                    <FluidIcon icon="edit" />&nbsp;{translate.t("search_findings.tab_severity.editable")}
+                  </Button>
+                </TooltipWrapper>
+              </ButtonToolbarRow>
+            </Can>
+            <br />
+            <GenericForm
+              name="editSeverity"
+              initialValues={{ ...data.finding.severity, cvssVersion: data.finding.cvssVersion }}
+              onSubmit={handleUpdateSeverity}
+              onChange={handleFormChange}
+            >
+              {({ pristine }: InjectedFormProps): React.ReactNode => (
                 <React.Fragment>
-                  <Can do="backend_api_mutations_update_severity_mutate">
-                    <ButtonToolbarRow>
-                      <TooltipWrapper
-                        id={translate.t("search_findings.tab_severity.editable_tooltip.id")}
-                        message={translate.t("search_findings.tab_severity.editable_tooltip")}
-                      >
-                        <Button onClick={handleEditClick}>
-                          <FluidIcon icon="edit" />&nbsp;{translate.t("search_findings.tab_severity.editable")}
+                  {isEditing ? (
+                    <React.Fragment>
+                      <ButtonToolbarRow>
+                        <Button type="submit" disabled={pristine || mutationRes.loading}>
+                          <FluidIcon icon="loading" />
+                          {translate.t("search_findings.tab_severity.update")}
                         </Button>
-                      </TooltipWrapper>
-                    </ButtonToolbarRow>
-                  </Can>
-                  <br />
-                  <Mutation
-                    mutation={UPDATE_SEVERITY_MUTATION}
-                    onCompleted={handleMtUpdateSeverityRes}
-                    onError={handleMtError}
-                    refetchQueries={[
-                      {
-                        query: GET_FINDING_HEADER,
-                        variables: {
-                          canGetExploit: groupPermissions.can("has_forces"),
-                          canGetHistoricState: permissions.can("backend_api_resolvers_finding_historic_state_resolve"),
-                          findingId,
-                        },
-                      },
-                    ]}
-                  >
-                    {(updateSeverity: MutationFunction, mutationRes: MutationResult): JSX.Element => {
-                      const handleUpdateSeverity: ((values: {}) => void) = (values: {}): void => {
-                        setEditing(false);
-                        void updateSeverity({ variables: { data: { ...values, id: findingId }, findingId } });
-                      };
-
-                      const handleFormChange: ((values: ISeverityAttr["finding"]["severity"]) => void) = (
-                        values: ISeverityAttr["finding"]["severity"],
-                      ): void => {
-                        const severityScore: string = Number(calcCVSSv3(values))
-                          .toFixed(2);
-                        client.writeData({
-                          data: {
-                            finding: { id: findingId, severityScore, __typename: "Finding" },
-                          },
-                        });
-                      };
+                      </ButtonToolbarRow>
+                      <Row>
+                        <EditableField
+                            style={"background-color: 000;" as React.CSSProperties}
+                            alignField="horizontal"
+                            component={Dropdown}
+                            currentValue={"3.1"}
+                            label={translate.t("search_findings.tab_severity.cvss_version")}
+                            name={"cvssVersion"}
+                            renderAsEditable={isEditing}
+                            validate={required}
+                          >
+                            <option value="" />
+                            <option value="3.1">3.1</option>
+                          </EditableField>
+                      </Row>
+                    </React.Fragment>
+                  ) : undefined}
+                  {castFieldsCVSS3(data.finding.severity, isEditing, formValues)
+                    .map((field: ISeverityField, index: number) => {
+                      const currentOption: string = field.options[field.currentValue];
 
                       return (
-                        <GenericForm
-                          name="editSeverity"
-                          initialValues={{ ...data.finding.severity, cvssVersion: data.finding.cvssVersion }}
-                          onSubmit={handleUpdateSeverity}
-                          onChange={handleFormChange}
-                        >
-                          {({ pristine }: InjectedFormProps): React.ReactNode => (
-                            <React.Fragment>
-                              {isEditing ? (
-                                <React.Fragment>
-                                  <ButtonToolbarRow>
-                                    <Button type="submit" disabled={pristine || mutationRes.loading}>
-                                      <FluidIcon icon="loading" />
-                                      {translate.t("search_findings.tab_severity.update")}
-                                    </Button>
-                                  </ButtonToolbarRow>
-                                  <Row>
-                                    <EditableField
-                                        style={"background-color: 000;" as React.CSSProperties}
-                                        alignField="horizontal"
-                                        component={Dropdown}
-                                        currentValue={"3.1"}
-                                        label={translate.t("search_findings.tab_severity.cvss_version")}
-                                        name={"cvssVersion"}
-                                        renderAsEditable={isEditing}
-                                        validate={required}
-                                      >
-                                        <option value="" />
-                                        <option value="3.1">3.1</option>
-                                      </EditableField>
-                                  </Row>
-                                </React.Fragment>
-                              ) : undefined}
-                              {castFieldsCVSS3(data.finding.severity, isEditing, formValues)
-                                .map((field: ISeverityField, index: number) => {
-                                  const currentOption: string = field.options[field.currentValue];
-
-                                  return (
-                                    <Row key={index}>
-                                      <EditableField
-                                        style={"background-color: 000;" as React.CSSProperties}
-                                        alignField="horizontal"
-                                        component={Dropdown}
-                                        currentValue={
-                                          `${Number(field.currentValue)
-                                            .toFixed(2)} | ${translate.t(currentOption)}`}
-                                        id={`Row${index}`}
-                                        label={field.title}
-                                        name={field.name}
-                                        renderAsEditable={isEditing}
-                                        tooltip={_.isEmpty(currentOption)
-                                          ? undefined : translate.t(currentOption.replace(/text/, "tooltip"))}
-                                        validate={required}
-                                      >
-                                        <option value="" />
-                                        {_.map(field.options, (text: string, value: string) => (
-                                          <option key={text} value={value}>{translate.t(text)}</option>
-                                        ))}
-                                      </EditableField>
-                                    </Row>
-                                  );
-                                })}
-                            </React.Fragment>
-                          )}
-                        </GenericForm>
+                        <Row key={index}>
+                          <EditableField
+                            style={"background-color: 000;" as React.CSSProperties}
+                            alignField="horizontal"
+                            component={Dropdown}
+                            currentValue={
+                              `${Number(field.currentValue)
+                                .toFixed(2)} | ${translate.t(currentOption)}`}
+                            id={`Row${index}`}
+                            label={field.title}
+                            name={field.name}
+                            renderAsEditable={isEditing}
+                            tooltip={_.isEmpty(currentOption)
+                              ? undefined : translate.t(currentOption.replace(/text/, "tooltip"))}
+                            validate={required}
+                          >
+                            <option value="" />
+                            {_.map(field.options, (text: string, value: string) => (
+                              <option key={text} value={value}>{translate.t(text)}</option>
+                            ))}
+                          </EditableField>
+                        </Row>
                       );
-                    }}
-                  </Mutation>
+                    })}
                 </React.Fragment>
-              );
-            }}
-          </Query>
+              )}
+            </GenericForm>
+          </React.Fragment>
         </Col100>
       </Row>
     </React.StrictMode>
