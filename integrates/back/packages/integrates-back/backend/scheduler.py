8b095d7863ef337c4a3c1ef71d0f354c627fbebd
Andres Aguirre <aaguirre@fluidattacks.com>
2021-03-05 08:47:10 -0500

    integrates\feat(back): #3670.12 delete imamura stakeholders
    - Added scheduler to delete stakeholders if only have access to
    imamura, and there are no logins in the last 60 days
    
    
    Former-commit-id: 024a210bcf5b17cd219000b3a2f23b790d59768f

--- integrates/back/packages/integrates-back/backend/scheduler.py
+++ integrates/back/packages/integrates-back/backend/scheduler.py
@@ -1038,3 +1038,48 @@ async def delete_obsolete_orgs() -> None:
                 org_name,
                 None
             )
+
+
+async def delete_imamura_stakeholders() -> None:
+    """
+    Delete stakeholders if only have access to imamura,
+    and there are no logins in the last 60 days
+    """
+    msg = '[scheduler]: delete_imamura_stakeholders is running'
+    LOGGER.info(msg, **NOEXTRA)
+    org_name = 'imamura'
+    org_id = await org_domain.get_id_by_name(org_name)
+    loaders = get_new_context()
+    organization_stakeholders_loader = loaders.organization_stakeholders
+    org_stakeholders = await organization_stakeholders_loader.load(org_id)
+    inactive_stakeholders = [
+        stakeholder
+        for stakeholder in org_stakeholders
+        if (
+            stakeholder['last_login']
+            and (
+                datetime_utils.get_plus_delta(
+                    datetime_utils.get_from_str(stakeholder['last_login']),
+                    days=60
+                ) < datetime_utils.get_now()
+            )
+        )
+    ]
+    inactive_stakeholder_orgs = await collect([
+        org_domain.get_user_organizations(inactive_stakeholder['email'])
+        for inactive_stakeholder in inactive_stakeholders
+    ])
+    stakeholders_to_delete = [
+        inactive_stakeholder
+        for inactive_stakeholder, orgs
+        in zip(inactive_stakeholders, inactive_stakeholder_orgs)
+        if len(orgs) == 1
+    ]
+    await collect([
+        org_domain.remove_user(
+            loaders,
+            org_id,
+            stakeholder_to_delete['email']
+        )
+        for stakeholder_to_delete in stakeholders_to_delete
+    ])
