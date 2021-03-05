9943754f8b41b6589d75c948fff02fcdf4950274
Kevin Amado <kamado@fluidattacks.com>
2021-03-05 17:19:21 -0500

    integrates\perf(build): #3836.384 cache hits
    - Improve cache hits by re-using dependnecies from
      other products
    - Reduce total closure size by pinning to the same hash
    
    
    Former-commit-id: 59caf009568468261405b87dfc8b3a2ecf2756ac

--- flake.nix
+++ flake.nix
@@ -6,6 +6,8 @@
     srcAirsPkgs = { url = "https://github.com/nixos/nixpkgs/archive/7138a338b58713e0dea22ddab6a6785abec7376a.tar.gz"; flake = false; };
     srcAssertsPkgs = { url = "https://github.com/nixos/nixpkgs/archive/7138a338b58713e0dea22ddab6a6785abec7376a.tar.gz"; flake = false; };
     srcForcesPkgs = { url = "https://github.com/nixos/nixpkgs/archive/7138a338b58713e0dea22ddab6a6785abec7376a.tar.gz"; flake = false; };
+    srcIntegratesMobilePkgs = { url = "https://github.com/nixos/nixpkgs/archive/932941b79c3dbbef2de9440e1631dfec43956261.tar.gz"; flake = false; };
+    srcIntegratesPkgs = { url = "https://github.com/nixos/nixpkgs/archive/932941b79c3dbbef2de9440e1631dfec43956261.tar.gz"; flake = false; };
     srcMakesPkgs = { url = "https://github.com/nixos/nixpkgs/archive/7138a338b58713e0dea22ddab6a6785abec7376a.tar.gz"; flake = false; };
     srcMeltsPkgs = { url = "https://github.com/nixos/nixpkgs/archive/7138a338b58713e0dea22ddab6a6785abec7376a.tar.gz"; flake = false; };
     srcObservesPkgs = { url = "https://github.com/nixos/nixpkgs/archive/932941b79c3dbbef2de9440e1631dfec43956261.tar.gz"; flake = false; };
@@ -13,9 +15,6 @@
     srcServesPkgs = { url = "https://github.com/nixos/nixpkgs/archive/7138a338b58713e0dea22ddab6a6785abec7376a.tar.gz"; flake = false; };
     srcSkimsPkgs = { url = "https://github.com/nixos/nixpkgs/archive/7138a338b58713e0dea22ddab6a6785abec7376a.tar.gz"; flake = false; };
     srcSortsPkgs = { url = "https://github.com/nixos/nixpkgs/archive/7138a338b58713e0dea22ddab6a6785abec7376a.tar.gz"; flake = false; };
-
-    srcIntegratesMobilePkgs = { url = "github:NixOS/nixpkgs/932941b79c3dbbef2de9440e1631dfec43956261"; flake = false; };
-    srcIntegratesPkgs = { url = "github:NixOS/nixpkgs/a437fe25652dea5b86de63891ce9c779c6e8bb9d"; flake = false; };
   };
   outputs = attrs: import ./makes attrs;
 }
