b827ad7b5a29b0073b6e56aa34073ad7d4960a28
Jonathan Mesa <jmesa@fluidattacks.com>
2021-03-08 11:14:10 -0500

    integrates\refac(front): #2705.97 update orgstakeholdview
    - Lint organizationstakeholdersview
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: 78e819b36efc78874861f6dc4b42e99f458d0fc1

--- integrates/front/src/scenes/Dashboard/containers/OrganizationStakeholdersView/index.tsx
+++ integrates/front/src/scenes/Dashboard/containers/OrganizationStakeholdersView/index.tsx
@@ -1,37 +1,36 @@
-import { useMutation, useQuery } from "@apollo/react-hooks";
-import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { ApolloError } from "apollo-client";
-import { GraphQLError } from "graphql";
-import _ from "lodash";
-import mixpanel from "mixpanel-browser";
-import React from "react";
-import { useParams } from "react-router";
-
+import { AddUserModal } from "scenes/Dashboard/components/AddUserModal";
+import type { ApolloError } from "apollo-client";
 import { Button } from "components/Button";
 import { DataTableNext } from "components/DataTableNext";
-import { timeFromNow } from "components/DataTableNext/formatters";
-import { IHeaderConfig } from "components/DataTableNext/types";
 import { FluidIcon } from "components/FluidIcon";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import type { GraphQLError } from "graphql";
+import type { IHeaderConfig } from "components/DataTableNext/types";
+import { Logger } from "utils/logger";
+import React from "react";
 import { TooltipWrapper } from "components/TooltipWrapper";
-import { AddUserModal } from "scenes/Dashboard/components/AddUserModal";
+import _ from "lodash";
+import mixpanel from "mixpanel-browser";
+import { timeFromNow } from "components/DataTableNext/formatters";
+import { translate } from "utils/translations/translate";
+import { useParams } from "react-router";
 import {
   ADD_STAKEHOLDER_MUTATION,
   EDIT_STAKEHOLDER_MUTATION,
   GET_ORGANIZATION_STAKEHOLDERS,
   REMOVE_STAKEHOLDER_MUTATION,
 } from "scenes/Dashboard/containers/OrganizationStakeholdersView/queries";
-import {
+import { ButtonToolbar, Col100, Row } from "styles/styledComponents";
+import type {
   IAddStakeholderAttrs,
   IEditStakeholderAttrs,
   IOrganizationStakeholders,
   IRemoveStakeholderAttrs,
   IStakeholderAttrs,
 } from "scenes/Dashboard/containers/OrganizationStakeholdersView/types";
-import { ButtonToolbar, Col100, Row } from "styles/styledComponents";
-import { Logger } from "utils/logger";
+import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { msgError, msgSuccess } from "utils/notifications";
-import { translate } from "utils/translations/translate";
+import { useMutation, useQuery } from "@apollo/react-hooks";
 
 const tableHeaders: IHeaderConfig[] = [
   {
@@ -41,10 +40,8 @@ const tableHeaders: IHeaderConfig[] = [
   },
   {
     dataField: "role",
-    formatter: (value: string) => translate.t(
-      `userModal.roles.${value}`,
-      { defaultValue: "-" },
-    ),
+    formatter: (value: string): string =>
+      translate.t(`userModal.roles.${value}`, { defaultValue: "-" }),
     header: translate.t("search_findings.users_table.userRole"),
     width: "15%",
   },
@@ -66,7 +63,9 @@ const tableHeaders: IHeaderConfig[] = [
   },
 ];
 
-const handleMtError: (mtError: ApolloError) => void = (mtError: ApolloError): void => {
+const handleMtError: (mtError: ApolloError) => void = (
+  mtError: ApolloError
+): void => {
   mtError.graphQLErrors.forEach(({ message }: GraphQLError): void => {
     switch (message) {
       case "Exception - Email is not valid":
@@ -86,57 +85,74 @@ const handleMtError: (mtError: ApolloError) => void = (mtError: ApolloError): vo
         break;
       default:
         msgError(translate.t("group_alerts.error_textsad"));
-        Logger.warning("An error occurred adding user to organization", mtError);
+        Logger.warning(
+          "An error occurred adding user to organization",
+          mtError
+        );
     }
   });
 };
 
-const organizationStakeholders: React.FC<IOrganizationStakeholders> =
-  (props: IOrganizationStakeholders): JSX.Element => {
+const OrganizationStakeholders: React.FC<IOrganizationStakeholders> = (
+  props: IOrganizationStakeholders
+): JSX.Element => {
   const { organizationId } = props;
   const { organizationName } = useParams<{ organizationName: string }>();
 
   // State management
   const [currentRow, setCurrentRow] = React.useState<Dictionary<string>>({});
-  const [isStakeholderModalOpen, setStakeholderModalOpen] = React.useState(false);
-  const [stakeholderModalAction, setStakeholderModalAction] = React.useState<"add" | "edit">("add");
+  const [isStakeholderModalOpen, setStakeholderModalOpen] = React.useState(
+    false
+  );
+  const [stakeholderModalAction, setStakeholderModalAction] = React.useState<
+    "add" | "edit"
+  >("add");
 
-  const openAddStakeholderModal: (() => void) = (): void => {
+  const openAddStakeholderModal: () => void = React.useCallback((): void => {
     setStakeholderModalAction("add");
     setStakeholderModalOpen(true);
-  };
-  const openEditStakeholderModal: (() => void) = (): void => {
+  }, []);
+  const openEditStakeholderModal: () => void = React.useCallback((): void => {
     setStakeholderModalAction("edit");
     setStakeholderModalOpen(true);
-  };
-  const closeStakeholderModal: (() => void) = (): void => {
+  }, []);
+  const closeStakeholderModal: () => void = React.useCallback((): void => {
     setStakeholderModalOpen(false);
-  };
+  }, []);
 
   // GraphQL Operations
-  const { data, refetch: refetchStakeholders } = useQuery(GET_ORGANIZATION_STAKEHOLDERS, {
-    notifyOnNetworkStatusChange: true,
-    onError: ({ graphQLErrors }: ApolloError): void => {
-      graphQLErrors.forEach((error: GraphQLError): void => {
-        msgError(translate.t("group_alerts.error_textsad"));
-        Logger.warning(
-          "An error occurred fetching organization stakeholders",
-          error,
-        );
-      });
-    },
-    variables: { organizationId },
-  });
+  const { data, refetch: refetchStakeholders } = useQuery(
+    GET_ORGANIZATION_STAKEHOLDERS,
+    {
+      notifyOnNetworkStatusChange: true,
+      onError: ({ graphQLErrors }: ApolloError): void => {
+        graphQLErrors.forEach((error: GraphQLError): void => {
+          msgError(translate.t("group_alerts.error_textsad"));
+          Logger.warning(
+            "An error occurred fetching organization stakeholders",
+            error
+          );
+        });
+      },
+      variables: { organizationId },
+    }
+  );
 
   const [grantStakeholderAccess] = useMutation(ADD_STAKEHOLDER_MUTATION, {
     onCompleted: (mtResult: IAddStakeholderAttrs): void => {
       if (mtResult.grantStakeholderOrganizationAccess.success) {
         void refetchStakeholders();
-        mixpanel.track("AddUserOrganzationAccess", { Organization: organizationName });
-        const { email } = mtResult.grantStakeholderOrganizationAccess.grantedStakeholder;
+        mixpanel.track("AddUserOrganzationAccess", {
+          Organization: organizationName,
+        });
+        const {
+          email,
+        } = mtResult.grantStakeholderOrganizationAccess.grantedStakeholder;
         msgSuccess(
-          `${email} ${translate.t("organization.tabs.users.addButton.success")}`,
-          translate.t("organization.tabs.users.successTitle"),
+          `${email} ${translate.t(
+            "organization.tabs.users.addButton.success"
+          )}`,
+          translate.t("organization.tabs.users.successTitle")
         );
       }
     },
@@ -146,108 +162,150 @@ const organizationStakeholders: React.FC<IOrganizationStakeholders> =
   const [editStakeholder] = useMutation(EDIT_STAKEHOLDER_MUTATION, {
     onCompleted: (mtResult: IEditStakeholderAttrs): void => {
       if (mtResult.editStakeholderOrganization.success) {
-        const { email } = mtResult.editStakeholderOrganization.modifiedStakeholder;
+        const {
+          email,
+        } = mtResult.editStakeholderOrganization.modifiedStakeholder;
         void refetchStakeholders();
 
-        mixpanel.track("EditUserOrganizationAccess", { Organization: organizationName });
+        mixpanel.track("EditUserOrganizationAccess", {
+          Organization: organizationName,
+        });
         msgSuccess(
-          `${email} ${translate.t("organization.tabs.users.editButton.success")}`,
-          translate.t("organization.tabs.users.successTitle"),
+          `${email} ${translate.t(
+            "organization.tabs.users.editButton.success"
+          )}`,
+          translate.t("organization.tabs.users.successTitle")
         );
       }
     },
     onError: handleMtError,
   });
 
-  const [removeStakeholderAccess, { loading: removing }] = useMutation(REMOVE_STAKEHOLDER_MUTATION, {
-    onCompleted: (mtResult: IRemoveStakeholderAttrs): void => {
-      if (mtResult.removeStakeholderOrganizationAccess.success) {
-        void refetchStakeholders();
+  const [removeStakeholderAccess, { loading: removing }] = useMutation(
+    REMOVE_STAKEHOLDER_MUTATION,
+    {
+      onCompleted: (mtResult: IRemoveStakeholderAttrs): void => {
+        if (mtResult.removeStakeholderOrganizationAccess.success) {
+          void refetchStakeholders();
 
-        mixpanel.track("RemoveUserOrganizationAccess", { Organization: organizationName });
-        msgSuccess(
-          `${currentRow.email} ${translate.t("organization.tabs.users.removeButton.success")}`,
-          translate.t("organization.tabs.users.successTitle"),
-        );
-        setCurrentRow({});
-      }
-    },
-    onError: (removeError: ApolloError): void => {
-      msgError(translate.t("group_alerts.error_textsad"));
-      Logger.warning("An error occurred removing stakeholder", removeError);
-    },
-  });
+          mixpanel.track("RemoveUserOrganizationAccess", {
+            Organization: organizationName,
+          });
+          msgSuccess(
+            `${currentRow.email} ${translate.t(
+              "organization.tabs.users.removeButton.success"
+            )}`,
+            translate.t("organization.tabs.users.successTitle")
+          );
+          setCurrentRow({});
+        }
+      },
+      onError: (removeError: ApolloError): void => {
+        msgError(translate.t("group_alerts.error_textsad"));
+        Logger.warning("An error occurred removing stakeholder", removeError);
+      },
+    }
+  );
 
   // Auxiliary elements
-  const handleSubmit: ((values: IStakeholderAttrs) => void) = (values: IStakeholderAttrs): void => {
-    closeStakeholderModal();
-    if (stakeholderModalAction === "add") {
-      void grantStakeholderAccess({ variables: {
-        ...values,
-        organizationId,
-      } });
-    } else {
-      void editStakeholder({ variables: {
-        ...values,
-        organizationId,
-      } });
-    }
-  };
-
-  const handleRemoveStakeholder: (() => void) = (): void => {
-    void removeStakeholderAccess({ variables: {
+  const handleSubmit: (values: IStakeholderAttrs) => void = React.useCallback(
+    (values: IStakeholderAttrs): void => {
+      closeStakeholderModal();
+      if (stakeholderModalAction === "add") {
+        void grantStakeholderAccess({
+          variables: {
+            ...values,
+            organizationId,
+          },
+        });
+      } else {
+        void editStakeholder({
+          variables: {
+            ...values,
+            organizationId,
+          },
+        });
+      }
+    },
+    [
+      closeStakeholderModal,
+      editStakeholder,
+      grantStakeholderAccess,
       organizationId,
-      userEmail: currentRow.email,
-    } });
-    setStakeholderModalAction("add");
-  };
+      stakeholderModalAction,
+    ]
+  );
 
-  const stakeholdersList: IStakeholderAttrs[] = _.isUndefined(data) || _.isEmpty(data)
-    ? []
-    : data.organization.stakeholders;
+  const handleRemoveStakeholder: () => void = React.useCallback((): void => {
+    void removeStakeholderAccess({
+      variables: {
+        organizationId,
+        userEmail: currentRow.email,
+      },
+    });
+    setStakeholderModalAction("add");
+  }, [currentRow.email, organizationId, removeStakeholderAccess]);
+
+  const stakeholdersList: IStakeholderAttrs[] =
+    _.isUndefined(data) || _.isEmpty(data)
+      ? []
+      : data.organization.stakeholders; // eslint-disable-line @typescript-eslint/no-unsafe-member-access
 
   return (
     <React.StrictMode>
-      <div id="users" className="tab-pane cont active" >
+      <div className={"tab-pane cont active"} id={"users"}>
         <Row>
           <Col100>
             <Row>
               <Col100>
                 <ButtonToolbar>
-                    <TooltipWrapper
-                      displayClass={"dib"}
-                      id={"organization.tabs.users.addButton.tooltip.btn"}
-                      message={translate.t("organization.tabs.users.addButton.tooltip")}
+                  <TooltipWrapper
+                    displayClass={"dib"}
+                    id={"organization.tabs.users.addButton.tooltip.btn"}
+                    message={translate.t(
+                      "organization.tabs.users.addButton.tooltip"
+                    )}
+                  >
+                    <Button id={"addUser"} onClick={openAddStakeholderModal}>
+                      <FontAwesomeIcon icon={faPlus} />
+                      &nbsp;
+                      {translate.t("organization.tabs.users.addButton.text")}
+                    </Button>
+                  </TooltipWrapper>
+                  <TooltipWrapper
+                    displayClass={"dib"}
+                    id={"organization.tabs.users.editButton.tooltip.btn"}
+                    message={translate.t(
+                      "organization.tabs.users.editButton.tooltip"
+                    )}
+                  >
+                    <Button
+                      disabled={_.isEmpty(currentRow)}
+                      id={"editUser"}
+                      onClick={openEditStakeholderModal}
                     >
-                      <Button id="addUser" onClick={openAddStakeholderModal}>
-                        <FontAwesomeIcon icon={faPlus} />
-                        &nbsp;{translate.t("organization.tabs.users.addButton.text")}
-                      </Button>
-                    </TooltipWrapper>
-                    <TooltipWrapper
-                      displayClass={"dib"}
-                      id={"organization.tabs.users.editButton.tooltip.btn"}
-                      message={translate.t("organization.tabs.users.editButton.tooltip")}
+                      <FluidIcon icon={"edit"} />
+                      &nbsp;
+                      {translate.t("organization.tabs.users.editButton.text")}
+                    </Button>
+                  </TooltipWrapper>
+                  <TooltipWrapper
+                    displayClass={"dib"}
+                    id={"organization.tabs.users.removeButton.tooltip.btn"}
+                    message={translate.t(
+                      "organization.tabs.users.removeButton.tooltip"
+                    )}
+                  >
+                    <Button
+                      disabled={_.isEmpty(currentRow) || removing}
+                      id={"removeUser"}
+                      onClick={handleRemoveStakeholder}
                     >
-                      <Button id="editUser" onClick={openEditStakeholderModal} disabled={_.isEmpty(currentRow)}>
-                        <FluidIcon icon="edit" />
-                        &nbsp;{translate.t("organization.tabs.users.editButton.text")}
-                      </Button>
-                    </TooltipWrapper>
-                    <TooltipWrapper
-                      displayClass={"dib"}
-                      id={"organization.tabs.users.removeButton.tooltip.btn"}
-                      message={translate.t("organization.tabs.users.removeButton.tooltip")}
-                    >
-                      <Button
-                        id="removeUser"
-                        onClick={handleRemoveStakeholder}
-                        disabled={_.isEmpty(currentRow) || removing}
-                      >
-                        <FontAwesomeIcon icon={faMinus} />
-                        &nbsp;{translate.t("organization.tabs.users.removeButton.text")}
-                      </Button>
-                    </TooltipWrapper>
+                      <FontAwesomeIcon icon={faMinus} />
+                      &nbsp;
+                      {translate.t("organization.tabs.users.removeButton.text")}
+                    </Button>
+                  </TooltipWrapper>
                 </ButtonToolbar>
               </Col100>
             </Row>
@@ -255,19 +313,19 @@ const organizationStakeholders: React.FC<IOrganizationStakeholders> =
             <Row>
               <Col100>
                 <DataTableNext
-                  id="tblUsers"
                   bordered={true}
                   dataset={stakeholdersList}
                   exportCsv={true}
                   headers={tableHeaders}
+                  id={"tblUsers"}
                   pageSize={10}
                   search={true}
-                  striped={true}
                   selectionMode={{
                     clickToSelect: true,
                     mode: "radio",
                     onSelect: setCurrentRow,
                   }}
+                  striped={true}
                 />
               </Col100>
             </Row>
@@ -277,16 +335,16 @@ const organizationStakeholders: React.FC<IOrganizationStakeholders> =
           action={stakeholderModalAction}
           editTitle={translate.t("organization.tabs.users.modalEditTitle")}
           initialValues={stakeholderModalAction === "edit" ? currentRow : {}}
+          onClose={closeStakeholderModal}
           onSubmit={handleSubmit}
           open={isStakeholderModalOpen}
-          onClose={closeStakeholderModal}
           organizationId={organizationId}
           title={translate.t("organization.tabs.users.modalAddTitle")}
-          type="organization"
+          type={"organization"}
         />
       </div>
     </React.StrictMode>
-    );
+  );
 };
 
-export { organizationStakeholders as OrganizationStakeholders };
+export { OrganizationStakeholders };
