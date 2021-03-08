93f24181e100f25c05f793bab88e6e36a4044ef4
Jonathan Mesa <jmesa@fluidattacks.com>
2021-03-08 14:42:59 -0500

    integrates\refac(front): #2705.99 update projauthorsview
    - Lint projectauthorsview
    - No visual changes
    
    
    Former-commit-id: 9330b7470448acdf0d452477094b8323a093cb9a

--- integrates/front/src/scenes/Dashboard/containers/ProjectAuthorsView/index.tsx
+++ integrates/front/src/scenes/Dashboard/containers/ProjectAuthorsView/index.tsx
@@ -1,56 +1,60 @@
-/* tslint:disable:jsx-no-multiline-js
- *
- * Disabling this rule is necessary for the sake of readability
- */
-
-// Third parties imports
-import { useQuery } from "@apollo/react-hooks";
-import { ApolloError } from "apollo-client";
-import _ from "lodash";
-import React, { ReactElement } from "react";
-import { useParams } from "react-router";
-
-// Local imports
-import { GraphQLError } from "graphql";
-
+import type { ApolloError } from "apollo-client";
 import { DataTableNext } from "components/DataTableNext";
-import { IHeaderConfig } from "components/DataTableNext/types";
-import styles from "scenes/Dashboard/containers/ProjectAuthorsView/index.css";
 import { GET_BILL } from "scenes/Dashboard/containers/ProjectAuthorsView/queries";
-import { IBillDeveloper, IData } from "scenes/Dashboard/containers/ProjectAuthorsView/types";
-import { Col100, Row } from "styles/styledComponents";
+import type { GraphQLError } from "graphql";
+import type { IHeaderConfig } from "components/DataTableNext/types";
 import { Logger } from "utils/logger";
+import React from "react";
+import type { ReactElement } from "react";
+import _ from "lodash";
 import { msgError } from "utils/notifications";
+import styles from "scenes/Dashboard/containers/ProjectAuthorsView/index.css";
 import { translate } from "utils/translations/translate";
+import { useParams } from "react-router";
+import { useQuery } from "@apollo/react-hooks";
+import { Col100, Row } from "styles/styledComponents";
+import type {
+  IBillDeveloper,
+  IData,
+} from "scenes/Dashboard/containers/ProjectAuthorsView/types";
 
-const projectAuthorsView: React.FC = (): JSX.Element => {
-
+const ProjectAuthorsView: React.FC = (): JSX.Element => {
   const now: Date = new Date();
   const thisYear: number = now.getFullYear();
   const thisMonth: number = now.getMonth();
-  const dateRange: Date[] = _
-    .range(0, 12)
-    .map((month: number) => new Date(thisYear, thisMonth - month));
+  const DATE_RANGE = 12;
+  const dateRange: Date[] = _.range(0, DATE_RANGE).map(
+    (month: number): Date => new Date(thisYear, thisMonth - month)
+  );
 
   const [billDate, setBillDate] = React.useState(dateRange[0].toISOString());
 
-  const formatText: ((value: string) => ReactElement<Text>) =
-    (value: string): ReactElement<Text> => <p className={styles.wrapped}>{value}</p>;
+  const formatText: (value: string) => ReactElement<Text> = (
+    value: string
+  ): ReactElement<Text> => <p className={styles.wrapped}>{value}</p>;
 
-  const formatCommit: ((value: string) => ReactElement<Text>) =
-    (value: string): ReactElement<Text> => <p className={styles.wrapped}>{value.slice(0, 8)}</p>;
+  const COMMIT_LENGTH = 8;
+  const formatCommit: (value: string) => ReactElement<Text> = (
+    value: string
+  ): ReactElement<Text> => (
+    <p className={styles.wrapped}>{value.slice(0, COMMIT_LENGTH)}</p>
+  );
 
-  const formatDate: ((date: Date) => string) = (date: Date): string => {
+  const formatDate: (date: Date) => string = (date: Date): string => {
     const month: number = date.getMonth() + 1;
     const monthStr: string = month.toString();
 
     return `${monthStr.padStart(2, "0")}/${date.getFullYear()}`;
   };
 
-  const handleDateChange: ((event: React.ChangeEvent<HTMLSelectElement>) => void) =
+  const handleDateChange: (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => void = React.useCallback(
     (event: React.ChangeEvent<HTMLSelectElement>): void => {
       setBillDate(event.target.value);
-    };
+    },
+    []
+  );
 
   const headersAuthorsTable: IHeaderConfig[] = [
     {
@@ -100,7 +104,7 @@ const projectAuthorsView: React.FC = (): JSX.Element => {
   });
 
   if (_.isUndefined(data) || _.isEmpty(data)) {
-    return <React.Fragment />;
+    return <div />;
   }
 
   const dataset: IBillDeveloper[] = (data as IData).project.bill.developers;
@@ -108,16 +112,23 @@ const projectAuthorsView: React.FC = (): JSX.Element => {
   return (
     <React.StrictMode>
       <Row>
+        {/* eslint-disable-next-line react/forbid-component-props */}
         <Col100 className={"pl0"}>
           <p>{translate.t("group.authors.tableAdvice")}</p>
         </Col100>
       </Row>
       <Row>
+        {/* eslint-disable-next-line react/forbid-component-props */}
         <Col100 className={styles.dateCol}>
-          <select onChange={handleDateChange} className={styles.selectDate}>
-            {dateRange.map((date: Date, index: number): JSX.Element => (
-              <option value={date.toISOString()} key={index}>{formatDate(date)}</option>
-            ))}
+          <select className={styles.selectDate} onChange={handleDateChange}>
+            {dateRange.map(
+              (date: Date, index: number): JSX.Element => (
+                // eslint-disable-next-line react/no-array-index-key
+                <option key={index} value={date.toISOString()}>
+                  {formatDate(date)}
+                </option>
+              )
+            )}
           </select>
         </Col100>
       </Row>
@@ -127,7 +138,7 @@ const projectAuthorsView: React.FC = (): JSX.Element => {
         defaultSorted={{ dataField: "actor", order: "asc" }}
         exportCsv={true}
         headers={headersAuthorsTable}
-        id="tblAuthorsList"
+        id={"tblAuthorsList"}
         pageSize={100}
         search={true}
         striped={true}
@@ -136,4 +147,4 @@ const projectAuthorsView: React.FC = (): JSX.Element => {
   );
 };
 
-export { projectAuthorsView as ProjectAuthorsView };
+export { ProjectAuthorsView };
