bb5975e755b909a2a193c3b4797cc150f37c89d3
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-03-08 08:08:06 -0500

    integrates\refac(back): #4257.3 update generator
    - update mean_time_to_remediate_non_treated generator to include
    30 or 90 days
    
    
    Former-commit-id: 7c8e39ec29119e3d62d1b5591e50e757ee99755b

--- integrates/analytics/generators/text_box/mean_time_to_remediate_non_treated.py
+++ integrates/analytics/generators/text_box/mean_time_to_remediate_non_treated.py
@@ -1,7 +1,14 @@
 # Standard library
+from datetime import (
+    date,
+)
 from decimal import Decimal
 from statistics import mean
-from typing import Tuple
+from typing import (
+    List,
+    Optional,
+    Tuple,
+)
 
 # Third party libraries
 from aioextensions import (
@@ -9,23 +16,37 @@ from aioextensions import (
     run,
 )
 from async_lru import alru_cache
-from backend.domain import project as group_domain
 
 # Local libraries
-from analytics import utils
+from backend.domain.project import (
+    get_mean_remediate_non_treated,
+)
+from analytics import (
+    utils,
+)
+from newutils import (
+    datetime as datetime_utils,
+)
 
 
 @alru_cache(maxsize=None, typed=True)
-async def generate_one(group: str) -> Decimal:
-    group_data = await group_domain.get_attributes(group, [
-        'mean_remediate_non_treated',
-    ])
+async def generate_one(
+    group: str,
+    min_date: Optional[date] = None
+) -> Decimal:
+    return await get_mean_remediate_non_treated(
+        group.lower(),
+        min_date
+    )
 
-    return group_data.get('mean_remediate_non_treated', 0)
 
-
-async def get_many_groups(groups: Tuple[str, ...]) -> Decimal:
-    groups_data = await collect(map(generate_one, list(groups)))
+async def get_many_groups(
+    groups: Tuple[str, ...],
+    min_date: Optional[date] = None
+) -> Decimal:
+    groups_data = await collect(
+        generate_one(group, min_date) for group in list(groups)
+    )
 
     return (
         Decimal(mean(groups_data)).quantize(Decimal('0.1'))
@@ -40,7 +61,7 @@ def format_data(mean_remediate: Decimal) -> dict:
     }
 
 
-async def generate_all():
+async def generate_all() -> None:
     async for group in utils.iterate_groups():
         utils.json_dump(
             document=format_data(
@@ -71,6 +92,46 @@ async def generate_all():
                 subject=f'{org_id}PORTFOLIO#{portfolio}',
             )
 
+    # Limit days
+    list_days: List[int] = [30, 90]
+    dates: List[date] = [
+        datetime_utils.get_now_minus_delta(days=list_days[0]).date(),
+        datetime_utils.get_now_minus_delta(days=list_days[1]).date()
+    ]
+    for days, min_date in zip(list_days, dates):
+        async for group in utils.iterate_groups():
+            utils.json_dump(
+                document=format_data(
+                    mean_remediate=await generate_one(group, min_date),
+                ),
+                entity='group',
+                subject=f'{group}_{days}',
+            )
+
+        async for org_id, _, org_groups in (
+            utils.iterate_organizations_and_groups()
+        ):
+            utils.json_dump(
+                document=format_data(
+                    mean_remediate=await get_many_groups(org_groups),
+                ),
+                entity='organization',
+                subject=f'{org_id}_{days}',
+            )
+
+        async for org_id, org_name, _ in (
+            utils.iterate_organizations_and_groups()
+        ):
+            for portfolio, groups in await utils.get_portfolios_groups(
+                org_name
+            ):
+                utils.json_dump(
+                    document=format_data(
+                        mean_remediate=await get_many_groups(groups),
+                    ),
+                    entity='portfolio',
+                    subject=f'{org_id}PORTFOLIO#{portfolio}_{days}',
+                )
 
 if __name__ == '__main__':
     run(generate_all())
