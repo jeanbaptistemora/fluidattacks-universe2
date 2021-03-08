041bd41c1daae38eee8fb37abd2395951302b0d3
Kevin Amado <kamado@fluidattacks.com>
2021-03-08 12:06:46 -0500

    common\refac(build): #3836.403 use builtins
    - Reduce boilerplate by using built-ins
    
    
    Former-commit-id: 5bb034c067ffa1b86178f8ca4b51563fba7431bd

--- makes/packages/forces/config-runtime/default.nix
+++ makes/packages/forces/config-runtime/default.nix
@@ -1,53 +1,57 @@
-{ nixpkgs
+{ buildPythonRequirements
+, makeTemplate
+, nixpkgs
 , path
 , ...
 }:
-let
-  buildPythonRequirements = import (path "/makes/utils/build-python-requirements") path nixpkgs;
-  makeTemplate = import (path "/makes/utils/make-template") path nixpkgs;
-in
 makeTemplate {
   arguments = {
-    envPython = "${nixpkgs.python38}/bin/python";
-    envPythonRequirements = buildPythonRequirements {
-      dependencies = [ ];
-      name = "forces-runtime";
-      requirements = {
-        direct = [
-          "aioextensions==20.8.2087641"
-          "aiogqlc==2.0.0b1"
-          "aiohttp==3.6.2"
-          "bugsnag==3.7.1"
-          "click==7.1.2"
-          "GitPython==3.1.7"
-          "more-itertools==8.5.0"
-          "oyaml==0.9"
-          "python-dateutil==2.8.1"
-          "pytz==2020.1"
-          "uvloop==0.14.0"
-        ];
-        inherited = [
-          "async-timeout==3.0.1"
-          "attrs==20.3.0"
-          "chardet==3.0.4"
-          "gitdb==4.0.5"
-          "idna==3.1"
-          "multidict==4.7.6"
-          "PyYAML==5.3.1"
-          "six==1.15.0"
-          "smmap==3.0.4"
-          "WebOb==1.8.6"
-          "yarl==1.6.3"
-        ];
-      };
-      python = nixpkgs.python38;
-    };
     envSrcForces = path "/forces";
-    envUtilsBashLibPython = path "/makes/utils/python/template.sh";
   };
   name = "forces-config-runtime";
   searchPaths = {
-    envPaths = [ nixpkgs.git ];
+    envPaths = [
+      nixpkgs.git
+      nixpkgs.python38
+    ];
+    envPython38Paths = [
+      (buildPythonRequirements {
+        dependencies = [ ];
+        name = "forces-runtime";
+        requirements = {
+          direct = [
+            "aioextensions==20.8.2087641"
+            "aiogqlc==2.0.0b1"
+            "aiohttp==3.6.2"
+            "bugsnag==3.7.1"
+            "click==7.1.2"
+            "GitPython==3.1.7"
+            "more-itertools==8.5.0"
+            "oyaml==0.9"
+            "python-dateutil==2.8.1"
+            "pytz==2020.1"
+            "uvloop==0.14.0"
+          ];
+          inherited = [
+            "async-timeout==3.0.1"
+            "attrs==20.3.0"
+            "chardet==3.0.4"
+            "gitdb==4.0.5"
+            "idna==3.1"
+            "multidict==4.7.6"
+            "PyYAML==5.3.1"
+            "six==1.15.0"
+            "smmap==3.0.4"
+            "WebOb==1.8.6"
+            "yarl==1.6.3"
+          ];
+        };
+        python = nixpkgs.python38;
+      })
+    ];
+    envPythonPaths = [
+      (path "/forces")
+    ];
   };
   template = path "/makes/packages/forces/config-runtime/template.sh";
 }
--- makes/packages/forces/config-runtime/template.sh
+++ makes/packages/forces/config-runtime/template.sh
@@ -1,16 +1,5 @@
 # shellcheck shell=bash
 
-source '__envUtilsBashLibPython__'
-
-function forces_setup_runtime {
-      make_python_path '3.8' \
-        '__envPythonRequirements__' \
-  &&  make_python_path_plain \
-        '__envSrcForces__'
-}
-
 function forces {
-  '__envPython__' '__envSrcForces__/forces/cli/__init__.py' "$@"
+  python3.8 '__envSrcForces__/forces/cli/__init__.py' "$@"
 }
-
-forces_setup_runtime
