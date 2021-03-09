3cfd407151b163d817a3952f2ed8ad2d652b6465
Jonathan Mesa <jmesa@fluidattacks.com>
2021-03-09 10:10:17 -0500

    integrates\refac(front): #2705.103 update projcontentview
    - Lint projectcontentview test
    - No visual changes
    
    
    Former-commit-id: 33495dd872908826f31e8bf8954e40b1d0872dea

--- integrates/front/src/scenes/Dashboard/containers/ProjectContent/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/containers/ProjectContent/index.spec.tsx
@@ -1,46 +1,56 @@
 import { MockedProvider } from "@apollo/react-testing";
-import { mount, ReactWrapper } from "enzyme";
-import * as React from "react";
-import { Provider } from "react-redux";
-import { MemoryRouter, Route } from "react-router-dom";
-import wait from "waait";
-
 import { ProjectContent } from "scenes/Dashboard/containers/ProjectContent";
+import { Provider } from "react-redux";
+import React from "react";
+import type { ReactWrapper } from "enzyme";
+import { mount } from "enzyme";
 import store from "store";
+import wait from "waait";
+import { MemoryRouter, Route } from "react-router-dom";
 
-describe("ProjectContent", () => {
-  it("should return a function", () => {
-    expect(typeof (ProjectContent))
-      .toEqual("function");
+describe("ProjectContent", (): void => {
+  it("should return a function", (): void => {
+    expect.hasAssertions();
+    expect(typeof ProjectContent).toStrictEqual("function");
   });
 
-  it("should render an error in component", async () => {
+  it("should render an error in component", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const wrapper: ReactWrapper = mount(
       <MemoryRouter initialEntries={["/project/TEST/indicators"]}>
         <Provider store={store}>
-          <MockedProvider mocks={[]} addTypename={false}>
-            <Route path={"/project/:projectName/indicators"} component={ProjectContent} />
+          <MockedProvider addTypename={false} mocks={[]}>
+            <Route
+              component={ProjectContent}
+              path={"/project/:projectName/indicators"}
+            />
           </MockedProvider>
         </Provider>
-      </MemoryRouter>,
+      </MemoryRouter>
     );
     await wait(0);
-    expect(wrapper)
-      .toHaveLength(1);
+
+    expect(wrapper).toHaveLength(1);
   });
 
-  it("should render a component", async () => {
+  it("should render a component", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const wrapper: ReactWrapper = mount(
       <MemoryRouter initialEntries={["/project/TEST/indicators"]}>
         <Provider store={store}>
-          <MockedProvider mocks={[]} addTypename={false}>
-            <Route path={"/project/:projectName/indicators"} component={ProjectContent}/>
+          <MockedProvider addTypename={false} mocks={[]}>
+            <Route
+              component={ProjectContent}
+              path={"/project/:projectName/indicators"}
+            />
           </MockedProvider>
         </Provider>
-      </MemoryRouter>,
+      </MemoryRouter>
     );
     await wait(0);
-    expect(wrapper)
-      .toHaveLength(1);
+
+    expect(wrapper).toHaveLength(1);
   });
 });
