4e0015c2eb5001d8370ddd444300550be71d15c9
Kevin Amado <kamado@fluidattacks.com>
2021-03-09 10:35:12 -0500

    common\refac(build): #3836.420 forget old image
    - We are now able to run over the pure oci image
    
    
    Former-commit-id: b1b1afa740d7ba28d944cb2e03c82ffaf3454fc9

--- makes/applications/makes/deploy/oci/ci/default.nix
+++ makes/applications/makes/deploy/oci/ci/default.nix
@@ -6,5 +6,5 @@ ociDeploy {
   oci = packages.makes.oci.ci;
   name = "makes-deploy-oci-ci";
   registry = "registry.gitlab.com";
-  tag = "fluidattacks/product/makes:ci2";
+  tag = "fluidattacks/product/makes:ci";
 }
