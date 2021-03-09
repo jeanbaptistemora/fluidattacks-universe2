9fdbf3ff748a3d13b0729bdcd683b101853ca9f5
Kevin Amado <kamado@fluidattacks.com>
2021-03-09 08:32:13 -0500

    common\fix(build): #3836.414 add missing folder
    - Skopeo expects this folder to unpack docker archives
    
    
    Former-commit-id: 5fd1fd2d65da0e593cb65aa265f5fd598a2a6586

--- makes/packages/makes/oci/ci/builder.sh
+++ makes/packages/makes/oci/ci/builder.sh
@@ -3,6 +3,7 @@
 function main {
       mkdir -p "${out}/etc/nix" \
   &&  mkdir -p "${out}/tmp" \
+  &&  mkdir -p "${out}/var/tmp" \
   &&  echo "build-users-group =" > "${out}/etc/nix/nix.conf" \
 
 }
