e1fd3b67b39d2ba89419c6d13e9b0f175abd66d6
Kevin Amado <kamado@fluidattacks.com>
2021-03-08 19:24:17 -0500

    common\perf(build): #3836.410 in-memory search paths
    - Make search paths to be skipped when there are
      no derivations to look for
    - This improves cache hits and make faster the
      search paths generation
    
    
    Former-commit-id: dd574900bb8c66e0054003045dd9b92c22dc72da

--- makes/utils/make-search-paths/default.nix
+++ makes/utils/make-search-paths/default.nix
@@ -1,6 +1,12 @@
 path: pkgs:
 let
   makeTemplate = import (path "/makes/utils/make-template") path pkgs;
+
+  export = envVar: envPath: envDrv:
+    "export ${envVar}=\"${envDrv}${envPath}:\${${envVar}:-}\"";
+
+  source = envDrv:
+    "source ${envDrv}";
 in
 { envClassPaths ? [ ]
 , envLibraries ? [ ]
@@ -12,19 +18,60 @@ in
 , envPython38Paths ? [ ]
 , envSources ? [ ]
 , envUtils ? [ ]
-}: makeTemplate {
-  arguments = {
-    inherit envClassPaths;
-    inherit envLibraries;
-    inherit envNodeBinaries;
-    inherit envNodeLibraries;
-    inherit envPaths;
-    inherit envPythonPaths;
-    inherit envPython37Paths;
-    inherit envPython38Paths;
-    inherit envSources;
-    envUtils = builtins.map (util: import (path util) path pkgs) envUtils;
-  };
+}:
+makeTemplate {
   name = "makes-utils-make-search-paths";
-  template = path "/makes/utils/make-search-paths/template.sh";
+  template = builtins.concatStringsSep "\n" (builtins.foldl'
+    (sources: { generator, derivations }:
+      if (derivations == [ ])
+      then sources
+      else sources ++ (builtins.map generator derivations)
+    )
+    [ ]
+    [
+      {
+        derivations = envClassPaths;
+        generator = export "CLASSPATH" "";
+      }
+      {
+        derivations = envLibraries;
+        generator = export "LD_LIBRARY_PATH" "/lib";
+      }
+      {
+        derivations = envLibraries;
+        generator = export "LD_LIBRARY_PATH" "/lib64";
+      }
+      {
+        derivations = envNodeBinaries;
+        generator = export "PATH" "/node_modules/.bin";
+      }
+      {
+        derivations = envNodeLibraries;
+        generator = export "NODE_PATH" "/node_modules";
+      }
+      {
+        derivations = envPaths;
+        generator = export "PATH" "/bin";
+      }
+      {
+        derivations = envPythonPaths;
+        generator = export "PYTHONPATH" "";
+      }
+      {
+        derivations = envPython37Paths;
+        generator = export "PYTHONPATH" "/lib/python3.7/site-packages";
+      }
+      {
+        derivations = envPython38Paths;
+        generator = export "PYTHONPATH" "/lib/python3.8/site-packages";
+      }
+      {
+        derivations = envSources;
+        generator = source;
+      }
+      {
+        derivations = builtins.map (util: import (path util) path pkgs) envUtils;
+        generator = source;
+      }
+    ]);
 }
