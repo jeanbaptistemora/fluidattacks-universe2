b1182977bfe2541301d3d08bdf9754d4ef9bf0cf
Mario Varas <mvaras@fluidattacks.com>
2021-03-09 14:10:47 -0500

    serves\feat(infra): #4286.5 list all buckets permission
    - For sorts prod role
    
    
    Former-commit-id: fb0cbee1724912371521e850ee2ed42992d3bd9c

--- serves/users/sorts/terraform/roles.tf
+++ serves/users/sorts/terraform/roles.tf
@@ -32,6 +32,16 @@ data "aws_iam_policy_document" "sorts_sagemaker_policy" {
   statement {
     effect = "Allow"
     actions = [
+      "s3:ListAllMyBuckets"
+    ]
+    resources = [
+      "arn:aws:s3:::*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
       "s3:PutObject"
     ]
     resources = [
