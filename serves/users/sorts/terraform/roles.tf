b983524ad68d0f9ff632e3056fc54103a4fe80a3
Mario Varas <mvaras@fluidattacks.com>
2021-03-09 10:30:38 -0500

    serves\feat(build): #4286.3 unify sorts roles in one file
    - One roles file for n roles
    
    
    Former-commit-id: 5885a5a077744ccc6a87320ebd81ba073bf658ea

--- serves/users/sorts/terraform/roles.tf
+++ serves/users/sorts/terraform/roles.tf
@@ -1,3 +1,4 @@
+# sorts_sagemaker role definition
 data "aws_iam_policy_document" "sorts_sagemaker_assume_policy" {
   statement {
     sid    = "SageMakerAssumeRolePolicy"
@@ -78,6 +79,46 @@ resource "aws_iam_role" "sorts_sagemaker" {
 
 resource "aws_iam_role_policy" "sorts_sagemaker_policy" {
   name   = "sorts_sagemaker_policy"
+  policy = data.aws_iam_policy_document.sorts_sagemaker_policy.json
+  role   = aws_iam_role.sorts_sagemaker.id
+}
+
+# sorts-prod role definition
+data "aws_iam_policy_document" "sorts_prod_assume_policy" {
+  statement {
+    sid    = "OktaSAMLAccess"
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRoleWithSAML"
+    ]
+    principals {
+      type        = "Federated"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:saml-provider/okta-saml-provider"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "SAML:aud"
+
+      values = [
+        "https://signin.aws.amazon.com/saml"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "sorts-prod" {
+  name               = "sorts-prod"
+  assume_role_policy = data.aws_iam_policy_document.sorts_prod_assume_policy.json
+
+  tags = {
+    "Name"               = "sorts-prod"
+    "management:type"    = "production"
+    "management:product" = "serves"
+  }
+}
+
+resource "aws_iam_role_policy" "sorts-prod_policy" {
+  name   = "sorts-prod_policy"
   policy = data.aws_iam_policy_document.sorts_sagemaker_policy.json
   role   = aws_iam_role.sorts_sagemaker.id
 }
