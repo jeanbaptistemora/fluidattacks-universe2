aa82741f34b61dcb89dda3ee0abe1d2bd81becd6
Mario Varas <mvaras@fluidattacks.com>
2021-03-10 09:07:57 -0500

    serves\feat(infra): #4286.6 add list bucket permissions
    - For sorts bucket
    
    
    Former-commit-id: 530ba51e2eafeef61ecb0b9e7f504dbe71bee4ac

--- serves/users/sorts/terraform/roles.tf
+++ serves/users/sorts/terraform/roles.tf
@@ -32,10 +32,20 @@ data "aws_iam_policy_document" "sorts_sagemaker_policy" {
   statement {
     effect = "Allow"
     actions = [
-      "s3:ListAllMyBuckets"
+      "s3:ListAllMyBuckets",
+      "s3:GetBucketLocation"
     ]
     resources = [
       "arn:aws:s3:::*"
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:ListBucket"
+    ]
+    resources = [
+      "arn:aws:s3:::sorts"
     ]
   }
 
