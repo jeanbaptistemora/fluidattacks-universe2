bc73c4ccda601e65e40696d12f505c89284251de
Jonathan Mesa <jmesa@fluidattacks.com>
2021-03-10 09:10:12 -0500

    integrates\refac(front): #2705.107 update projeventsview
    - Lint projecteventsview test
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: 2b1feaed46e3d1786b32f7c8f6c044f8fbc346c5

--- integrates/front/src/scenes/Dashboard/containers/ProjectEventsView/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/containers/ProjectEventsView/index.spec.tsx
@@ -1,22 +1,22 @@
-import { MockedProvider, MockedResponse } from "@apollo/react-testing";
-import { PureAbility } from "@casl/ability";
-import { mount, ReactWrapper } from "enzyme";
-import { GraphQLError } from "graphql";
-import _ from "lodash";
-import * as React from "react";
-// tslint:disable-next-line: no-submodule-imports
-import { act } from "react-dom/test-utils";
-import { Provider } from "react-redux";
-import { MemoryRouter, Route } from "react-router-dom";
-import wait from "waait";
-
-import { ProjectEventsView } from "scenes/Dashboard/containers/ProjectEventsView";
 import { GET_EVENTS } from "scenes/Dashboard/containers/ProjectEventsView/queries";
-import store from "store";
+import { GraphQLError } from "graphql";
+import { MockedProvider } from "@apollo/react-testing";
+import type { MockedResponse } from "@apollo/react-testing";
+import { ProjectEventsView } from "scenes/Dashboard/containers/ProjectEventsView";
+import { Provider } from "react-redux";
+import { PureAbility } from "@casl/ability";
+import React from "react";
+import type { ReactWrapper } from "enzyme";
+import _ from "lodash";
+import { act } from "react-dom/test-utils";
 import { authzPermissionsContext } from "utils/authz/config";
+import { mount } from "enzyme";
+import store from "store";
+import wait from "waait";
+import { MemoryRouter, Route } from "react-router-dom";
 
-describe("EventsView", () => {
-  const mocks: ReadonlyArray<MockedResponse> = [
+describe("EventsView", (): void => {
+  const mocks: readonly MockedResponse[] = [
     {
       request: {
         query: GET_EVENTS,
@@ -27,21 +27,24 @@ describe("EventsView", () => {
       result: {
         data: {
           project: {
-            events: [{
-              closingDate: "-",
-              detail: "Test description",
-              eventDate: "2018-10-17 00:00:00",
-              eventStatus: "SOLVED",
-              eventType: "AUTHORIZATION_SPECIAL_ATTACK",
-              id: "463457733",
-              projectName: "unittesting",
-            }],
+            events: [
+              {
+                closingDate: "-",
+                detail: "Test description",
+                eventDate: "2018-10-17 00:00:00",
+                eventStatus: "SOLVED",
+                eventType: "AUTHORIZATION_SPECIAL_ATTACK",
+                id: "463457733",
+                projectName: "unittesting",
+              },
+            ],
           },
         },
       },
-    }];
+    },
+  ];
 
-  const mockError: ReadonlyArray<MockedResponse> = [
+  const mockError: readonly MockedResponse[] = [
     {
       request: {
         query: GET_EVENTS,
@@ -52,88 +55,131 @@ describe("EventsView", () => {
       result: {
         errors: [new GraphQLError("Access denied")],
       },
-    }];
+    },
+  ];
 
-  it("should return a fuction", () => {
-    expect(typeof (ProjectEventsView))
-      .toEqual("function");
+  it("should return a fuction", (): void => {
+    expect.hasAssertions();
+    expect(typeof ProjectEventsView).toStrictEqual("function");
   });
 
-  it("should render an error in component", async () => {
+  it("should render an error in component", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const wrapper: ReactWrapper = mount(
       <MemoryRouter initialEntries={["/project/unittesting/events"]}>
         <Provider store={store}>
-          <MockedProvider mocks={mockError} addTypename={false}>
-            <Route component={ProjectEventsView} path={"/project/:projectName/events"} />
+          <MockedProvider addTypename={false} mocks={mockError}>
+            <Route
+              component={ProjectEventsView}
+              path={"/project/:projectName/events"}
+            />
           </MockedProvider>
         </Provider>
-      </MemoryRouter>,
+      </MemoryRouter>
     );
-    await act(async () => { await wait(0); wrapper.update(); });
-    expect(wrapper.find("Query")
-      .children())
-      .toHaveLength(0);
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+
+    expect(wrapper.find("Query").children()).toHaveLength(0);
   });
 
-  it("should render a component", async () => {
+  it("should render a component", (): void => {
+    expect.hasAssertions();
+
     const wrapper: ReactWrapper = mount(
       <MemoryRouter initialEntries={["/project/unittesting/events"]}>
         <Provider store={store}>
-          <MockedProvider mocks={mocks} addTypename={false}>
-            <Route component={ProjectEventsView} path={"/project/:projectName/events"} />
+          <MockedProvider addTypename={false} mocks={mocks}>
+            <Route
+              component={ProjectEventsView}
+              path={"/project/:projectName/events"}
+            />
           </MockedProvider>
         </Provider>
-      </MemoryRouter>,
+      </MemoryRouter>
     );
-    expect(wrapper)
-      .toHaveLength(1);
+
+    expect(wrapper).toHaveLength(1);
   });
 
-  it("should render events table", async () => {
+  it("should render events table", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const wrapper: ReactWrapper = mount(
       <MemoryRouter initialEntries={["/project/unittesting/events"]}>
         <Provider store={store}>
-          <MockedProvider mocks={mocks} addTypename={false}>
-            <Route component={ProjectEventsView} path={"/project/:projectName/events"} />
+          <MockedProvider addTypename={false} mocks={mocks}>
+            <Route
+              component={ProjectEventsView}
+              path={"/project/:projectName/events"}
+            />
           </MockedProvider>
         </Provider>
-      </MemoryRouter>,
+      </MemoryRouter>
     );
-    await act(async () => { await wait(0); wrapper.update(); });
-    expect(wrapper.find("table"))
-      .toHaveLength(1);
-    expect(wrapper
-      .find("td")
-      .filterWhere((td: ReactWrapper) => _.includes(td.text(), "Authorization for special attack")))
-      .toHaveLength(1);
-    expect(wrapper
-      .find("td")
-      .filterWhere((td: ReactWrapper) =>
-        td.containsMatchingElement(<span>Solved</span>)))
-      .toHaveLength(1);
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+
+    expect(wrapper.find("table")).toHaveLength(1);
+    expect(
+      wrapper
+        .find("td")
+        .filterWhere((td: ReactWrapper): boolean =>
+          _.includes(td.text(), "Authorization for special attack")
+        )
+    ).toHaveLength(1);
+    expect(
+      wrapper
+        .find("td")
+        .filterWhere((td: ReactWrapper): boolean =>
+          td.containsMatchingElement(<span>{"Solved"}</span>)
+        )
+    ).toHaveLength(1);
   });
 
-  it("should render new event modal", async () => {
+  it("should render new event modal", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const mockedPermissions: PureAbility<string> = new PureAbility([
       { action: "backend_api_mutations_create_event_mutate" },
     ]);
     const wrapper: ReactWrapper = mount(
       <MemoryRouter initialEntries={["/project/unittesting/events"]}>
         <Provider store={store}>
-          <MockedProvider mocks={mocks} addTypename={false}>
+          <MockedProvider addTypename={false} mocks={mocks}>
             <authzPermissionsContext.Provider value={mockedPermissions}>
-              <Route component={ProjectEventsView} path={"/project/:projectName/events"} />
+              <Route
+                component={ProjectEventsView}
+                path={"/project/:projectName/events"}
+              />
             </authzPermissionsContext.Provider>
           </MockedProvider>
         </Provider>
-      </MemoryRouter>,
+      </MemoryRouter>
     );
-    await act(async () => { await wait(0); wrapper.update(); });
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
     const newButton: ReactWrapper = wrapper
       .find("Button")
-      .filterWhere((button: ReactWrapper) => _.includes(button.text(), "New"));
-    expect(newButton)
-      .toHaveLength(1);
+      .filterWhere((button: ReactWrapper): boolean =>
+        _.includes(button.text(), "New")
+      );
+
+    expect(newButton).toHaveLength(1);
+
     newButton.simulate("click");
 
     const dateField: ReactWrapper = wrapper
@@ -165,27 +211,16 @@ describe("EventsView", () => {
       .find({ name: "actionAfterBlocking" })
       .find("select");
 
-    const evidenceFiles: ReactWrapper = wrapper
-      .find("span")
-      .find(".fa-search");
+    const evidenceFiles: ReactWrapper = wrapper.find("span").find(".fa-search");
 
-    expect(wrapper.containsMatchingElement(<h4>New Event</h4>))
-      .toBe(true);
-    expect(dateField)
-      .toHaveLength(1);
-    expect(typeField)
-      .toHaveLength(1);
-    expect(contextField)
-      .toHaveLength(1);
-    expect(checkBoxes)
-      .toHaveLength(2);
-    expect(textAreaField)
-      .toHaveLength(1);
-    expect(actionBeforeBlockingField)
-      .toHaveLength(1);
-    expect(actionAfterBlockedField)
-      .toHaveLength(1);
-    expect(evidenceFiles)
-      .toHaveLength(2);
+    expect(wrapper.containsMatchingElement(<h4>{"New Event"}</h4>)).toBe(true);
+    expect(dateField).toHaveLength(1);
+    expect(typeField).toHaveLength(1);
+    expect(contextField).toHaveLength(1);
+    expect(checkBoxes).toHaveLength(2);
+    expect(textAreaField).toHaveLength(1);
+    expect(actionBeforeBlockingField).toHaveLength(1);
+    expect(actionAfterBlockedField).toHaveLength(1);
+    expect(evidenceFiles).toHaveLength(2);
   });
 });
