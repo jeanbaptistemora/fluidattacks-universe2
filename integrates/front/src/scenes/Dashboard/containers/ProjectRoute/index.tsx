9bd915f3524c19a139eb10b2778675b929dd1ce8
Jonathan Mesa <jmesa@fluidattacks.com>
2021-03-11 16:48:04 -0500

    integrates\refac(front): #2705.118 update projectroute
    - Lint projectroute
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: 4bf3ff40e176cdf060880366f93460ca93205f24

--- integrates/front/src/scenes/Dashboard/containers/ProjectRoute/index.tsx
+++ integrates/front/src/scenes/Dashboard/containers/ProjectRoute/index.tsx
@@ -1,68 +1,90 @@
-/* tslint:disable jsx-no-multiline-js
- * JSX-NO-MULTILINE-JS: Disabling this rule is necessary for the sake of
- * readability of conditional rendering
- */
-import { useQuery } from "@apollo/react-hooks";
-import { PureAbility } from "@casl/ability";
-import { ApolloError } from "apollo-client";
-import { GraphQLError } from "graphql";
-import _ from "lodash";
-import React from "react";
-import { Redirect, Route, Switch, useParams, useRouteMatch } from "react-router-dom";
-
+import type { ApolloError } from "apollo-client";
 import { EventContent } from "scenes/Dashboard/containers/EventContent";
 import { FindingContent } from "scenes/Dashboard/containers/FindingContent";
-import { ProjectContent } from "scenes/Dashboard/containers/ProjectContent";
 import { GET_GROUP_DATA } from "scenes/Dashboard/containers/ProjectRoute/queries";
-import { IProjectData, IProjectRoute } from "scenes/Dashboard/containers/ProjectRoute/types";
 import { GET_USER_PERMISSIONS } from "scenes/Dashboard/queries";
-import { authzGroupContext, authzPermissionsContext } from "utils/authz/config";
+import type { GraphQLError } from "graphql";
 import { Logger } from "utils/logger";
+import { ProjectContent } from "scenes/Dashboard/containers/ProjectContent";
+import type { PureAbility } from "@casl/ability";
+import React from "react";
+import _ from "lodash";
 import { msgError } from "utils/notifications";
 import { translate } from "utils/translations/translate";
+import { useQuery } from "@apollo/react-hooks";
+import type {
+  IProjectData,
+  IProjectRoute,
+} from "scenes/Dashboard/containers/ProjectRoute/types";
+import {
+  Redirect,
+  Route,
+  Switch,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
+import { authzGroupContext, authzPermissionsContext } from "utils/authz/config";
 
-const projectRoute: React.FC<IProjectRoute> = (props: IProjectRoute): JSX.Element => {
+const ProjectRoute: React.FC<IProjectRoute> = (
+  props: IProjectRoute
+): JSX.Element => {
   const { setUserRole } = props;
-  const { organizationName, projectName } = useParams<{ organizationName: string; projectName: string }>();
+  const { organizationName, projectName } = useParams<{
+    organizationName: string;
+    projectName: string;
+  }>();
   const { path } = useRouteMatch();
 
   const attributes: PureAbility<string> = React.useContext(authzGroupContext);
-  const permissions: PureAbility<string> = React.useContext(authzPermissionsContext);
+  const permissions: PureAbility<string> = React.useContext(
+    authzPermissionsContext
+  );
 
   // Side effects
-  const onProjectChange: (() => void) = (): void => {
+  const onProjectChange: () => void = (): void => {
     attributes.update([]);
     permissions.update([]);
   };
-  React.useEffect(onProjectChange, [projectName]);
+  React.useEffect(onProjectChange, [attributes, permissions, projectName]);
 
   // GraphQL operations
   useQuery(GET_USER_PERMISSIONS, {
-    onCompleted: (permData: { me: { permissions: string[]; role: string | undefined } }): void => {
-      permissions.update(permData.me.permissions.map((action: string) => ({ action })));
+    onCompleted: (permData: {
+      me: { permissions: string[]; role: string | undefined };
+    }): void => {
+      permissions.update(
+        permData.me.permissions.map((action: string): { action: string } => ({
+          action,
+        }))
+      );
       if (permData.me.permissions.length === 0) {
-        Logger.error("Empty permissions", JSON.stringify(permData.me.permissions));
+        Logger.error(
+          "Empty permissions",
+          JSON.stringify(permData.me.permissions)
+        );
       }
       setUserRole(permData.me.role);
     },
     onError: ({ graphQLErrors }: ApolloError): void => {
-      graphQLErrors.forEach((permissionsError: GraphQLError) => {
-        Logger.error(
-          "Couldn't load group-level permissions",
-          permissionsError,
-        );
+      graphQLErrors.forEach((permissionsError: GraphQLError): void => {
+        Logger.error("Couldn't load group-level permissions", permissionsError);
       });
     },
     variables: {
       entity: "PROJECT",
-      identifier: projectName.toLowerCase() },
+      identifier: projectName.toLowerCase(),
+    },
   });
 
   const { data, error } = useQuery<IProjectData>(GET_GROUP_DATA, {
-    onCompleted: ({ project }: IProjectData) => {
-      attributes.update(project.serviceAttributes.map((attribute: string) => ({
-        action: attribute,
-      })));
+    onCompleted: ({ project }: IProjectData): void => {
+      attributes.update(
+        project.serviceAttributes.map((attribute: string): {
+          action: string;
+        } => ({
+          action: attribute,
+        }))
+      );
     },
     onError: ({ graphQLErrors }: ApolloError): void => {
       graphQLErrors.forEach((groupError: GraphQLError): void => {
@@ -74,31 +96,38 @@ const projectRoute: React.FC<IProjectRoute> = (props: IProjectRoute): JSX.Elemen
   });
 
   if (!_.isUndefined(error)) {
-    return (
-      <Redirect path={path} to={"/home"} />
-    );
+    return <Redirect path={path} to={"/home"} />;
   }
-  if (_.isUndefined(data) || _.isEmpty(data)) { return <React.Fragment />; }
+  if (_.isUndefined(data) || _.isEmpty(data)) {
+    return <div />;
+  }
 
-  if (organizationName !== data?.project.organization) {
-    return (
-        <Redirect path={path} to={"/home"} />
-    );
+  if (organizationName !== data.project.organization) {
+    return <Redirect path={path} to={"/home"} />;
   }
 
   return (
     <React.StrictMode>
-      <React.Fragment>
+      <div>
         <Switch>
-          <Route path={`${path}/events/:eventId(\\d+)`} component={EventContent} />
-          <Route path={`${path}/:type(vulns|drafts)/:findingId(\\d+)`} component={FindingContent} />
+          <Route
+            component={EventContent}
+            path={`${path}/events/:eventId(\\d+)`}
+          />
+          <Route
+            component={FindingContent}
+            path={`${path}/:type(vulns|drafts)/:findingId(\\d+)`}
+          />
           {/* Necessary to support legacy URLs before finding had its own path */}
-          <Redirect path={`${path}/:findingId(\\d+)`} to={`${path}/vulns/:findingId(\\d+)`} />
-          <Route path={path} component={ProjectContent} />
+          <Redirect
+            path={`${path}/:findingId(\\d+)`}
+            to={`${path}/vulns/:findingId(\\d+)`}
+          />
+          <Route component={ProjectContent} path={path} />
         </Switch>
-      </React.Fragment>
+      </div>
     </React.StrictMode>
   );
 };
 
-export { projectRoute as ProjectRoute };
+export { ProjectRoute };
