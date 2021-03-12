5b98f69865ea3a9710d5c74126a34eb5d3447ecc
Jonathan Mesa <jmesa@fluidattacks.com>
2021-03-12 10:52:38 -0500

    integrates\refac(front): #2705.119 update projsettingsview
    - Lint projectsettingsview agenttoken component
    - No visual changes
    
    
    Former-commit-id: ee8dcea20fde1ad63d6bff67d69d2c91fcec6f4c

--- integrates/front/src/scenes/Dashboard/containers/ProjectSettingsView/AgentToken/index.tsx
+++ integrates/front/src/scenes/Dashboard/containers/ProjectSettingsView/AgentToken/index.tsx
@@ -1,45 +1,50 @@
-import { Button } from "components/Button";
-import _ from "lodash";
-import React, { useState } from "react";
-import { useTranslation } from "react-i18next";
 import { APITokenForcesModal } from "scenes/Dashboard/components/APITokenForcesModal";
+import { Button } from "components/Button";
+import type { IServicesProps } from "../Services/types";
+import { translate } from "utils/translations/translate";
+import { useTranslation } from "react-i18next";
 import {
   ButtonToolbar,
   Flex,
   LastProjectSetting,
   ProjectScopeText,
 } from "styles/styledComponents";
-import { translate } from "utils/translations/translate";
-import { IServicesProps } from "../Services/types";
+import React, { useState } from "react";
 
-const agentToken: React.FC<IServicesProps> = (props: IServicesProps): JSX.Element => {
+const AgentToken: React.FC<IServicesProps> = (
+  props: IServicesProps
+): JSX.Element => {
   const { groupName } = props;
   const { t } = useTranslation();
 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleChange: () => void = () => { setIsModalOpen(!isModalOpen); };
+  const handleChange: () => void = React.useCallback((): void => {
+    setIsModalOpen(!isModalOpen);
+  }, [isModalOpen]);
 
   return (
     <React.StrictMode>
       <LastProjectSetting>
         <Flex>
-          <h2>
-            {translate.t("search_findings.agentTokenSection.title")}
-          </h2>
+          <h2>{translate.t("search_findings.agentTokenSection.title")}</h2>
         </Flex>
         <ProjectScopeText>
-            {t("search_findings.agentTokenSection.about")}
+          {t("search_findings.agentTokenSection.about")}
         </ProjectScopeText>
         <ButtonToolbar>
           <Button onClick={handleChange}>
             {t("search_findings.agentTokenSection.generate")}
           </Button>
         </ButtonToolbar>
-        <APITokenForcesModal groupName={groupName} open={isModalOpen} onClose={handleChange}/>
+        <APITokenForcesModal
+          groupName={groupName}
+          onClose={handleChange}
+          open={isModalOpen}
+        />
       </LastProjectSetting>
     </React.StrictMode>
   );
 };
 
-export { agentToken as AgentToken };
+export { AgentToken };
