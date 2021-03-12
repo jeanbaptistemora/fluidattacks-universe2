b4caf7d23c343cf2d06d9941117843ac90d9997b
Daniel Murcia <dmurcia@fluidattacks.com>
2021-03-12 10:07:03 -0500

    observes\refac(build): #4277.13 batch stb env
    - fit env to standard
    
    
    Former-commit-id: 1689454b6d5d94e45f31174eea3ef8d5ebb2f3e5

---
+++ makes/packages/observes/env/runtime/batch-stability/default.nix
@@ -0,0 +1,46 @@
+{ buildPythonPackage
+, buildPythonRequirements
+, makeTemplate
+, nixpkgs
+, path
+, ...
+}:
+let
+  pythonRequirements = buildPythonRequirements {
+    name = "observes-env-runtime-batch-stability-python";
+    requirements = {
+      direct = [
+        "boto3==1.17.17"
+        "bugsnag==4.0.2"
+      ];
+      inherited = [
+        "botocore==1.20.17"
+        "jmespath==0.10.0"
+        "python-dateutil==2.8.1"
+        "s3transfer==0.3.4"
+        "six==1.15.0"
+        "urllib3==1.26.3"
+        "WebOb==1.8.7"
+      ];
+    };
+    python = nixpkgs.python38;
+  };
+  self = buildPythonPackage {
+    name = "observes-batch-stability";
+    packagePath = path "/observes/services/batch_stability";
+    python = nixpkgs.python38;
+  };
+in
+makeTemplate {
+  name = "observes-env-runtime-batch-stability";
+  searchPaths = {
+    envPaths = [
+      pythonRequirements
+      self
+    ];
+    envPython38Paths = [
+      pythonRequirements
+      self
+    ];
+  };
+}
