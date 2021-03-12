621da43fefc1d69bfa830fa580c774b6be2071a0
David Acevedo <dacevedo@fluidattacks.com>
2021-03-12 16:22:49 -0500

    integrates\refac(doc): #4106.72 improve historics
    - changed the design to solve an overfetching problem
    - that also limited setting up pagination
    - historic items now have their own facet with keys
    - that differ from those of the access pattern to get
    - roots. a duplicate of the latest will be kept so
    - we dont get n historic items mixed which would likely
    - be inefficient and impossible to paginate
    - tldr; went from n historic items to:
    - 1 root = 1 meta + 1 historic
    - n roots = (1 meta + 1 historic) * n
    
    
    Former-commit-id: 3d3c3c7bbf64db9ba7e0afd93eb3d66213a382c8

--- integrates/back/packages/modules/dynamodb/table.py
+++ integrates/back/packages/modules/dynamodb/table.py
@@ -10,7 +10,7 @@ from newutils.context import DB_MODEL_PATH
 def load_facets(model: Dict[str, Any]) -> Dict[str, Facet]:
     return {
         facet['FacetName']: Facet(
-            attrs=tuple(facet['NonKeyAttributes']),
+            attrs=tuple(facet.get('NonKeyAttributes', [])),
             pk_alias=facet['KeyAttributeAlias']['PartitionKeyAlias'],
             sk_alias=facet['KeyAttributeAlias']['SortKeyAlias']
         )
