510a952d9cc15dae23b0f33fe7f0d5e4e41d75ef
Jonathan Mesa <jmesa@fluidattacks.com>
2021-03-15 09:06:14 -0500

    integrates\refac(front): #2705.123 update projsettingsview
    - Lint projectsettingsview portfolio test
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: 38f3ff423816897adc6fda78ad9a2048b800ecb5

--- integrates/front/src/scenes/Dashboard/containers/ProjectSettingsView/Portfolio/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/containers/ProjectSettingsView/Portfolio/index.spec.tsx
@@ -1,41 +1,44 @@
-import { MockedProvider, MockedResponse } from "@apollo/react-testing";
-import { PureAbility } from "@casl/ability";
-import { mount, ReactWrapper } from "enzyme";
 import { GraphQLError } from "graphql";
-import * as React from "react";
-// tslint:disable-next-line: no-submodule-imports
-import { act } from "react-dom/test-utils";
+import type { IPortfolioProps } from "scenes/Dashboard/containers/ProjectSettingsView/Portfolio";
+import { MockedProvider } from "@apollo/react-testing";
+import type { MockedResponse } from "@apollo/react-testing";
+import { Portfolio } from "scenes/Dashboard/containers/ProjectSettingsView/Portfolio";
 import { Provider } from "react-redux";
+import { PureAbility } from "@casl/ability";
+import React from "react";
+import type { ReactWrapper } from "enzyme";
+import { act } from "react-dom/test-utils";
+import { authzPermissionsContext } from "utils/authz/config";
+import { mount } from "enzyme";
+import store from "store";
 import wait from "waait";
-
-import { IPortfolioProps, Portfolio } from "scenes/Dashboard/containers/ProjectSettingsView/Portfolio";
 import {
   ADD_TAGS_MUTATION,
   GET_TAGS,
   REMOVE_TAG_MUTATION,
 } from "scenes/Dashboard/containers/ProjectSettingsView/queries";
-import store from "store";
-import { authzPermissionsContext } from "utils/authz/config";
 import { msgError, msgSuccess } from "utils/notifications";
 
-jest.mock("../../../../../utils/notifications", () => {
-  const mockedNotifications: Dictionary = jest.requireActual("../../../../../utils/notifications");
-  mockedNotifications.msgError = jest.fn();
-  mockedNotifications.msgSuccess = jest.fn();
+jest.mock(
+  "../../../../../utils/notifications",
+  (): Dictionary => {
+    const mockedNotifications: Dictionary = jest.requireActual(
+      "../../../../../utils/notifications"
+    );
 
-  return mockedNotifications;
-});
+    mockedNotifications.msgError = jest.fn(); // eslint-disable-line fp/no-mutation, jest/prefer-spy-on
+    mockedNotifications.msgSuccess = jest.fn(); // eslint-disable-line fp/no-mutation, jest/prefer-spy-on
 
-describe("Portfolio", () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+    return mockedNotifications;
+  }
+);
 
+describe("Portfolio", (): void => {
   const mockProps: IPortfolioProps = {
     projectName: "TEST",
   };
 
-  const mocksTags: ReadonlyArray<MockedResponse> = [
+  const mocksTags: readonly MockedResponse[] = [
     {
       request: {
         query: GET_TAGS,
@@ -46,10 +49,7 @@ describe("Portfolio", () => {
       result: {
         data: {
           project: {
-            tags: [
-              "test-tag1",
-              "test-tag2",
-            ],
+            tags: ["test-tag1", "test-tag2"],
           },
         },
       },
@@ -64,247 +64,339 @@ describe("Portfolio", () => {
       result: {
         data: {
           project: {
-            tags: [
-              "test-tag1",
-            ],
+            tags: ["test-tag1"],
           },
         },
       },
     },
   ];
 
-  it("should return a function", () => {
-    expect(typeof (Portfolio))
-      .toEqual("function");
+  it("should return a function", (): void => {
+    expect.hasAssertions();
+    expect(typeof Portfolio).toStrictEqual("function");
   });
 
-  it("should add a tag", async () => {
-    const mocksMutation: ReadonlyArray<MockedResponse> = [{
-      request: {
-        query: ADD_TAGS_MUTATION,
-        variables:  {
-          projectName: "TEST",
-          tagsData: JSON.stringify([
-            "test-new-tag",
-          ]),
+  it("should add a tag", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    const mocksMutation: readonly MockedResponse[] = [
+      {
+        request: {
+          query: ADD_TAGS_MUTATION,
+          variables: {
+            projectName: "TEST",
+            tagsData: JSON.stringify(["test-new-tag"]),
+          },
         },
+        result: { data: { addTags: { success: true } } },
       },
-      result: { data: { addTags : { success: true } } },
-    }];
+    ];
     const mockedPermissions: PureAbility<string> = new PureAbility([
       { action: "backend_api_mutations_add_group_tags_mutate" },
     ]);
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
-        <MockedProvider mocks={mocksTags.concat(mocksMutation)} addTypename={false}>
+        <MockedProvider
+          addTypename={false}
+          mocks={mocksTags.concat(mocksMutation)}
+        >
           <authzPermissionsContext.Provider value={mockedPermissions}>
-            <Portfolio {...mockProps} />
+            <Portfolio projectName={mockProps.projectName} />
           </authzPermissionsContext.Provider>
         </MockedProvider>
-      </Provider>,
+      </Provider>
     );
-    await act(async () => { await wait(0); wrapper.update(); });
-    const addButton: ReactWrapper = wrapper.find("button")
-      .findWhere((element: ReactWrapper) => element.contains("Add"))
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+    const addButton: ReactWrapper = wrapper
+      .find("button")
+      .findWhere((element: ReactWrapper): boolean => element.contains("Add"))
       .at(0);
     addButton.simulate("click");
     const addTagsModal: ReactWrapper = wrapper.find("AddTagsModal");
     const tagInput: ReactWrapper = addTagsModal
-      .find({name: "tags[0]", type: "text"})
+      .find({ name: "tags[0]", type: "text" })
       .at(0)
       .find("input");
     tagInput.simulate("change", { target: { value: "test-new-tag" } });
-    const form: ReactWrapper = addTagsModal
-      .find("genericForm")
-      .at(0);
+    const form: ReactWrapper = addTagsModal.find("genericForm").at(0);
     form.simulate("submit");
-    await act(async () => { await wait(0); wrapper.update(); });
-    expect(msgSuccess)
-      .toBeCalled();
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+
+    expect(msgSuccess).toHaveBeenCalled(); // eslint-disable-line jest/prefer-called-with
+
+    jest.clearAllMocks();
   });
 
-  it("should remove a tag", async () => {
-    const mocksMutation: ReadonlyArray<MockedResponse> = [{
-      request: {
-        query: REMOVE_TAG_MUTATION,
-        variables:  {
-          projectName: "TEST",
-          tagToRemove: "test-tag1",
+  it("should remove a tag", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    const mocksMutation: readonly MockedResponse[] = [
+      {
+        request: {
+          query: REMOVE_TAG_MUTATION,
+          variables: {
+            projectName: "TEST",
+            tagToRemove: "test-tag1",
+          },
         },
+        result: { data: { removeTag: { success: true } } },
       },
-      result: { data: { removeTag : { success: true } } },
-    }];
+    ];
     const mockedPermissions: PureAbility<string> = new PureAbility([
       { action: "backend_api_mutations_remove_group_tag_mutate" },
     ]);
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
-        <MockedProvider mocks={mocksTags.concat(mocksMutation)} addTypename={false}>
+        <MockedProvider
+          addTypename={false}
+          mocks={mocksTags.concat(mocksMutation)}
+        >
           <authzPermissionsContext.Provider value={mockedPermissions}>
-            <Portfolio {...mockProps} />
+            <Portfolio projectName={mockProps.projectName} />
           </authzPermissionsContext.Provider>
         </MockedProvider>
-      </Provider>,
+      </Provider>
     );
-    await act(async () => { await wait(0); wrapper.update(); });
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
     const fileInfo: ReactWrapper = wrapper
       .find("tr")
-      .findWhere((element: ReactWrapper) => element.contains("test-tag1"))
+      .findWhere((element: ReactWrapper): boolean =>
+        element.contains("test-tag1")
+      )
       .at(0);
     fileInfo.simulate("click");
     const removeButton: ReactWrapper = wrapper
       .find("button")
-      .findWhere((element: ReactWrapper) => element.contains("Remove"))
+      .findWhere((element: ReactWrapper): boolean => element.contains("Remove"))
       .at(0);
     removeButton.simulate("click");
-    await act(async () => { await wait(0); wrapper.update(); });
-    expect(msgSuccess)
-      .toHaveBeenCalled();
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+
+    expect(msgSuccess).toHaveBeenCalled(); // eslint-disable-line jest/prefer-called-with
+
+    jest.clearAllMocks();
   });
 
-  it("should sort tags", async () => {
+  it("should sort tags", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
-        <MockedProvider mocks={mocksTags} addTypename={false}>
-          <Portfolio {...mockProps} />
+        <MockedProvider addTypename={false} mocks={mocksTags}>
+          <Portfolio projectName={mockProps.projectName} />
         </MockedProvider>
-      </Provider>,
+      </Provider>
     );
-    await act(async () => { await wait(0); wrapper.update(); });
-    let firstRowInfo: ReactWrapper = wrapper
-      .find("RowPureContent")
-      .at(0);
-    expect(firstRowInfo.text())
-      .toEqual("test-tag1");
-    const tagHeader: ReactWrapper = wrapper
-      .find({"aria-label": "Portfolio sortable"});
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+    const firstRowInfo: ReactWrapper = wrapper.find("RowPureContent").at(0);
+
+    expect(firstRowInfo.text()).toStrictEqual("test-tag1");
+
+    const tagHeader: ReactWrapper = wrapper.find({
+      "aria-label": "Portfolio sortable",
+    });
     tagHeader.simulate("click");
-    firstRowInfo = wrapper
-      .find("RowPureContent")
-      .at(0);
-    expect(firstRowInfo.text())
-      .toEqual("test-tag2");
+    const firstRowInfoAux: ReactWrapper = wrapper.find("RowPureContent").at(0);
+
+    expect(firstRowInfoAux.text()).toStrictEqual("test-tag2");
+
+    jest.clearAllMocks();
   });
 
-  it("should handle errors when add a tag", async () => {
-    const mocksMutation: ReadonlyArray<MockedResponse> = [{
-      request: {
-        query: ADD_TAGS_MUTATION,
-        variables:  {
-          projectName: "TEST",
-          tagsData: JSON.stringify([
-            "test-new-tag",
-          ]),
+  it("should handle errors when add a tag", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    const mocksMutation: readonly MockedResponse[] = [
+      {
+        request: {
+          query: ADD_TAGS_MUTATION,
+          variables: {
+            projectName: "TEST",
+            tagsData: JSON.stringify(["test-new-tag"]),
+          },
+        },
+        result: {
+          errors: [
+            new GraphQLError("Access denied"),
+            new GraphQLError("Exception - One or more values already exist"),
+          ],
         },
       },
-      result: { errors: [
-        new GraphQLError("Access denied"),
-        new GraphQLError("Exception - One or more values already exist"),
-      ]},
-    }];
+    ];
     const mockedPermissions: PureAbility<string> = new PureAbility([
       { action: "backend_api_mutations_add_group_tags_mutate" },
     ]);
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
-        <MockedProvider mocks={mocksTags.concat(mocksMutation)} addTypename={false}>
+        <MockedProvider
+          addTypename={false}
+          mocks={mocksTags.concat(mocksMutation)}
+        >
           <authzPermissionsContext.Provider value={mockedPermissions}>
-            <Portfolio {...mockProps} />
+            <Portfolio projectName={mockProps.projectName} />
           </authzPermissionsContext.Provider>
         </MockedProvider>
-      </Provider>,
+      </Provider>
     );
-    await act(async () => { await wait(0); wrapper.update(); });
-    const addButton: ReactWrapper = wrapper.find("button")
-      .findWhere((element: ReactWrapper) => element.contains("Add"))
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+    const addButton: ReactWrapper = wrapper
+      .find("button")
+      .findWhere((element: ReactWrapper): boolean => element.contains("Add"))
       .at(0);
     addButton.simulate("click");
     const addTagsModal: ReactWrapper = wrapper.find("AddTagsModal");
     const tagInput: ReactWrapper = addTagsModal
-      .find({name: "tags[0]", type: "text"})
+      .find({ name: "tags[0]", type: "text" })
       .at(0)
       .find("input");
     tagInput.simulate("change", { target: { value: "test-new-tag" } });
-    const form: ReactWrapper = addTagsModal
-      .find("genericForm")
-      .at(0);
+    const form: ReactWrapper = addTagsModal.find("genericForm").at(0);
     form.simulate("submit");
-    await act(async () => { await wait(0); wrapper.update(); });
-    expect(msgError)
-      .toBeCalledTimes(2);
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+
+    expect(msgError).toHaveBeenCalledTimes(2);
+
+    jest.clearAllMocks();
   });
 
-  it("should handle error when remove a tag", async () => {
-    const mocksMutation: ReadonlyArray<MockedResponse> = [{
-      request: {
-        query: REMOVE_TAG_MUTATION,
-        variables:  {
-          projectName: "TEST",
-          tagToRemove: "test-tag1",
+  it("should handle error when remove a tag", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    const mocksMutation: readonly MockedResponse[] = [
+      {
+        request: {
+          query: REMOVE_TAG_MUTATION,
+          variables: {
+            projectName: "TEST",
+            tagToRemove: "test-tag1",
+          },
         },
+        result: { errors: [new GraphQLError("Access denied")] },
       },
-      result: { errors: [
-        new GraphQLError("Access denied"),
-      ]},
-    }];
+    ];
     const mockedPermissions: PureAbility<string> = new PureAbility([
       { action: "backend_api_mutations_remove_group_tag_mutate" },
     ]);
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
-        <MockedProvider mocks={mocksTags.concat(mocksMutation)} addTypename={false}>
+        <MockedProvider
+          addTypename={false}
+          mocks={mocksTags.concat(mocksMutation)}
+        >
           <authzPermissionsContext.Provider value={mockedPermissions}>
-            <Portfolio {...mockProps} />
+            <Portfolio projectName={mockProps.projectName} />
           </authzPermissionsContext.Provider>
         </MockedProvider>
-      </Provider>,
+      </Provider>
     );
-    await act(async () => { await wait(0); wrapper.update(); });
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
     const fileInfo: ReactWrapper = wrapper
       .find("tr")
-      .findWhere((element: ReactWrapper) => element.contains("test-tag1"))
+      .findWhere((element: ReactWrapper): boolean =>
+        element.contains("test-tag1")
+      )
       .at(0);
     fileInfo.simulate("click");
     const removeButton: ReactWrapper = wrapper
       .find("button")
-      .findWhere((element: ReactWrapper) => element.contains("Remove"))
+      .findWhere((element: ReactWrapper): boolean => element.contains("Remove"))
       .at(0);
     removeButton.simulate("click");
-    await act(async () => { await wait(0); wrapper.update(); });
-    expect(msgError)
-      .toBeCalled();
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+
+    expect(msgError).toHaveBeenCalled(); // eslint-disable-line jest/prefer-called-with
+
+    jest.clearAllMocks();
   });
 
-  it("should handle error when there are repeated tags", async () => {
+  it("should handle error when there are repeated tags", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const mockedPermissions: PureAbility<string> = new PureAbility([
       { action: "backend_api_mutations_add_group_tags_mutate" },
     ]);
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
-        <MockedProvider mocks={mocksTags} addTypename={false}>
+        <MockedProvider addTypename={false} mocks={mocksTags}>
           <authzPermissionsContext.Provider value={mockedPermissions}>
-            <Portfolio {...mockProps} />
+            <Portfolio projectName={mockProps.projectName} />
           </authzPermissionsContext.Provider>
         </MockedProvider>
-      </Provider>,
+      </Provider>
     );
-    await act(async () => { await wait(0); wrapper.update(); });
-    const addButton: ReactWrapper = wrapper.find("button")
-      .findWhere((element: ReactWrapper) => element.contains("Add"))
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+    const addButton: ReactWrapper = wrapper
+      .find("button")
+      .findWhere((element: ReactWrapper): boolean => element.contains("Add"))
       .at(0);
     addButton.simulate("click");
     const addTagsModal: ReactWrapper = wrapper.find("AddTagsModal");
     const tagInput: ReactWrapper = addTagsModal
-      .find({name: "tags[0]", type: "text"})
+      .find({ name: "tags[0]", type: "text" })
       .at(0)
       .find("input");
     tagInput.simulate("change", { target: { value: "test-tag1" } });
-    const form: ReactWrapper = addTagsModal
-      .find("genericForm")
-      .at(0);
+    const form: ReactWrapper = addTagsModal.find("genericForm").at(0);
     form.simulate("submit");
-    await act(async () => { await wait(0); wrapper.update(); });
-    expect(msgError)
-      .toBeCalled();
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+
+    expect(msgError).toHaveBeenCalled(); // eslint-disable-line jest/prefer-called-with
+
+    jest.clearAllMocks();
   });
 });
