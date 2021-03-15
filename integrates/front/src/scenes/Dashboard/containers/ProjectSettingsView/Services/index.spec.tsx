32237030e167c26c4b13aaa8ee35cc2586acf262
Jonathan Mesa <jmesa@fluidattacks.com>
2021-03-15 16:33:03 -0500

    integrates\refac(front): #2705.126 update projsettingsview
    - Lint projectsettingsview services component test
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: 6c9a0e61d3719669435c4fcc3f78dd73ebfe1c9a

--- integrates/front/src/scenes/Dashboard/containers/ProjectSettingsView/Services/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/containers/ProjectSettingsView/Services/index.spec.tsx
@@ -1,17 +1,23 @@
-import { MockedProvider, MockedResponse } from "@apollo/react-testing";
-import { PureAbility } from "@casl/ability";
-import { mount, ReactWrapper } from "enzyme";
-import * as React from "react";
-// tslint:disable-next-line: no-submodule-imports
-import { act } from "react-dom/test-utils";
-import { Provider } from "react-redux";
+/* eslint-disable @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
+ -- Annotations added due to extended usage of "any" type in enzyme lib
+ */
 import { MemoryRouter } from "react-router-dom";
-import wait from "waait";
-
-import { EDIT_GROUP_DATA, GET_GROUP_DATA } from "scenes/Dashboard/containers/ProjectSettingsView/queries";
+import { MockedProvider } from "@apollo/react-testing";
+import type { MockedResponse } from "@apollo/react-testing";
+import { Provider } from "react-redux";
+import { PureAbility } from "@casl/ability";
+import React from "react";
+import type { ReactWrapper } from "enzyme";
 import { Services } from "scenes/Dashboard/containers/ProjectSettingsView/Services";
-import store from "store";
+import { act } from "react-dom/test-utils";
 import { authzPermissionsContext } from "utils/authz/config";
+import { mount } from "enzyme";
+import store from "store";
+import wait from "waait";
+import {
+  EDIT_GROUP_DATA,
+  GET_GROUP_DATA,
+} from "scenes/Dashboard/containers/ProjectSettingsView/queries";
 
 interface IFormValues {
   drills: boolean;
@@ -20,12 +26,8 @@ interface IFormValues {
   type: string;
 }
 
-describe("Services", () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
-  const mockResponses: ReadonlyArray<MockedResponse> = [
+describe("Services", (): void => {
+  const mockResponses: readonly MockedResponse[] = [
     {
       request: {
         query: GET_GROUP_DATA,
@@ -66,7 +68,7 @@ describe("Services", () => {
       request: {
         query: GET_GROUP_DATA,
         variables: {
-          groupName : "oneshottest",
+          groupName: "oneshottest",
         },
       },
       result: {
@@ -105,237 +107,197 @@ describe("Services", () => {
     { action: "backend_api_mutations_edit_group_mutate" },
   ]);
 
-  it("should return a function", () => {
-    expect(typeof Services)
-      .toEqual("function");
+  it("should return a function", (): void => {
+    expect.hasAssertions();
+    expect(typeof Services).toStrictEqual("function");
   });
 
   [
     { group: "unittesting", rows: 4 },
     { group: "oneshottest", rows: 3 },
-    { group: "not-exists", rows: 0},
-  ].forEach((test: { group: string; rows: number}) => {
-    it(`should render services for: ${test.group}`, async () => {
+    { group: "not-exists", rows: 0 },
+  ].forEach((test: { group: string; rows: number }): void => {
+    it(`should render services for: ${test.group}`, async (): Promise<void> => {
+      expect.hasAssertions();
+
       const wrapper: ReactWrapper = mount(
         <Provider store={store}>
-          <MockedProvider mocks={mockResponses} addTypename={false}>
+          <MockedProvider addTypename={false} mocks={mockResponses}>
             <authzPermissionsContext.Provider value={mockedPermissions}>
               <MemoryRouter initialEntries={["/home"]}>
                 <Services groupName={test.group} />
               </MemoryRouter>
             </authzPermissionsContext.Provider>
           </MockedProvider>
-        </Provider>,
+        </Provider>
       );
-      await act(async () => { await wait(1); wrapper.update(); });
+      await act(
+        async (): Promise<void> => {
+          await wait(1);
+          wrapper.update();
+        }
+      );
 
       const table: ReactWrapper = wrapper.find("table");
       const tableBody: ReactWrapper = table.find("tbody");
       const rows: ReactWrapper = tableBody.find("tr");
 
-      expect(rows)
-        .toHaveLength(test.rows);
+      expect(rows).toHaveLength(test.rows);
+
+      jest.clearAllMocks();
     });
   });
 
-  it("should toggle buttons properly", async () => {
+  it("should toggle buttons properly", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
-        <MockedProvider mocks={mockResponses} addTypename={false}>
+        <MockedProvider addTypename={false} mocks={mockResponses}>
           <authzPermissionsContext.Provider value={mockedPermissions}>
             <MemoryRouter initialEntries={["/home"]}>
-              <Services groupName="unittesting" />
+              <Services groupName={"unittesting"} />
             </MemoryRouter>
           </authzPermissionsContext.Provider>
         </MockedProvider>
-      </Provider>,
+      </Provider>
     );
-    await act(async () => { await wait(0); wrapper.update(); });
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
 
-    const formValues: (() => IFormValues) = (): IFormValues => store.getState().form.editGroup.values;
+    const formValues: () => IFormValues = (): IFormValues =>
+      store.getState().form.editGroup.values;
 
     // Wrappers are functions because references get rapidly changed
-    const table: (() => ReactWrapper) = (): ReactWrapper => wrapper.find("table");
-    const tableBody: (() => ReactWrapper) = (): ReactWrapper =>
-      table()
-        .find("tbody");
-    const rows: (() => ReactWrapper) = (): ReactWrapper =>
-      tableBody()
-        .find("tr");
+    const table: () => ReactWrapper = (): ReactWrapper => wrapper.find("table");
+    const tableBody: () => ReactWrapper = (): ReactWrapper =>
+      table().find("tbody");
+    const rows: () => ReactWrapper = (): ReactWrapper => tableBody().find("tr");
 
-    const typeRow: (() => ReactWrapper) = (): ReactWrapper =>
-      rows()
-        .at(0);
-    const integratesRow: (() => ReactWrapper) = (): ReactWrapper =>
-      rows()
-        .at(1);
-    const drillsRow: (() => ReactWrapper) = (): ReactWrapper =>
-      rows()
-        .at(2);
-    const forcesRow: (() => ReactWrapper) = (): ReactWrapper =>
-      rows()
-        .at(3);
+    const typeRow: () => ReactWrapper = (): ReactWrapper => rows().at(0);
+    const integratesRow: () => ReactWrapper = (): ReactWrapper => rows().at(1);
+    const drillsRow: () => ReactWrapper = (): ReactWrapper => rows().at(2);
+    const TEST_FORCES_ROW_INDEX = 3;
+    const forcesRow: () => ReactWrapper = (): ReactWrapper =>
+      rows().at(TEST_FORCES_ROW_INDEX);
 
-    const typeRowLeft: (() => ReactWrapper) = (): ReactWrapper =>
-      typeRow()
-        .find("td")
-        .first();
-    const integratesRowLeft: (() => ReactWrapper) = (): ReactWrapper =>
-      integratesRow()
-        .find("td")
-        .first();
-    const drillsRowLeft: (() => ReactWrapper) = (): ReactWrapper =>
-      drillsRow()
-        .find("td")
-        .first();
-    const forcesRowLeft: (() => ReactWrapper) = (): ReactWrapper =>
-      forcesRow()
-        .find("td")
-        .first();
+    const typeRowLeft: () => ReactWrapper = (): ReactWrapper =>
+      typeRow().find("td").first();
+    const integratesRowLeft: () => ReactWrapper = (): ReactWrapper =>
+      integratesRow().find("td").first();
+    const drillsRowLeft: () => ReactWrapper = (): ReactWrapper =>
+      drillsRow().find("td").first();
+    const forcesRowLeft: () => ReactWrapper = (): ReactWrapper =>
+      forcesRow().find("td").first();
 
-    expect(rows())
-      .toHaveLength(4);
-    expect(
-      typeRowLeft()
-        .text())
-          .toEqual("Subscription type");
-    expect(
-      integratesRowLeft()
-        .text())
-          .toEqual("Integrates");
-    expect(
-      drillsRowLeft()
-        .text())
-          .toEqual("Drills");
-    expect(
-      forcesRowLeft()
-        .text())
-          .toEqual("Forces");
+    expect(rows()).toHaveLength(4);
+    expect(typeRowLeft().text()).toStrictEqual("Subscription type");
+    expect(integratesRowLeft().text()).toStrictEqual("Integrates");
+    expect(drillsRowLeft().text()).toStrictEqual("Drills");
+    expect(forcesRowLeft().text()).toStrictEqual("Forces");
 
-    const integratesSwitch: (() => ReactWrapper) = (): ReactWrapper =>
-      integratesRow()
-        .find("#integratesSwitch")
-        .at(0);
-    const drillsSwitch: (() => ReactWrapper) = (): ReactWrapper =>
-      drillsRow()
-        .find("#drillsSwitch")
-        .at(0);
-    const forcesSwitch: (() => ReactWrapper) = (): ReactWrapper =>
-      forcesRow()
-        .find("#forcesSwitch")
-        .at(0);
+    const integratesSwitch: () => ReactWrapper = (): ReactWrapper =>
+      integratesRow().find("#integratesSwitch").at(0);
+    const drillsSwitch: () => ReactWrapper = (): ReactWrapper =>
+      drillsRow().find("#drillsSwitch").at(0);
+    const forcesSwitch: () => ReactWrapper = (): ReactWrapper =>
+      forcesRow().find("#forcesSwitch").at(0);
 
-    expect(formValues())
-      .toEqual({
-        comments: "",
-        confirmation: "",
-        drills: true,
-        forces: true,
-        integrates: true,
-        reason: "NONE",
-        type: "CONTINUOUS",
-      });
+    expect(formValues()).toStrictEqual({
+      comments: "",
+      confirmation: "",
+      drills: true,
+      forces: true,
+      integrates: true,
+      reason: "NONE",
+      type: "CONTINUOUS",
+    });
 
-    integratesSwitch()
-      .simulate("click");
+    integratesSwitch().simulate("click");
 
-    expect(formValues())
-      .toEqual({
-        comments: "",
-        confirmation: "",
-        drills: false,
-        forces: false,
-        integrates: false,
-        reason: "NONE",
-        type: "CONTINUOUS",
-      });
+    expect(formValues()).toStrictEqual({
+      comments: "",
+      confirmation: "",
+      drills: false,
+      forces: false,
+      integrates: false,
+      reason: "NONE",
+      type: "CONTINUOUS",
+    });
 
-    drillsSwitch()
-      .simulate("click");
+    drillsSwitch().simulate("click");
 
-    expect(formValues())
-      .toEqual({
-        comments: "",
-        confirmation: "",
-        drills: true,
-        forces: false,
-        integrates: true,
-        reason: "NONE",
-        type: "CONTINUOUS",
-      });
+    expect(formValues()).toStrictEqual({
+      comments: "",
+      confirmation: "",
+      drills: true,
+      forces: false,
+      integrates: true,
+      reason: "NONE",
+      type: "CONTINUOUS",
+    });
 
-    drillsSwitch()
-      .simulate("click");
+    drillsSwitch().simulate("click");
 
-    expect(formValues())
-      .toEqual({
-        comments: "",
-        confirmation: "",
-        drills: false,
-        forces: false,
-        integrates: true,
-        reason: "NONE",
-        type: "CONTINUOUS",
-      });
+    expect(formValues()).toStrictEqual({
+      comments: "",
+      confirmation: "",
+      drills: false,
+      forces: false,
+      integrates: true,
+      reason: "NONE",
+      type: "CONTINUOUS",
+    });
 
-    const proceedButton: (() => ReactWrapper) = (): ReactWrapper =>
-      wrapper
-        .find("Button")
-        .first();
-    const genericForm: (() => ReactWrapper) = (): ReactWrapper =>
-      wrapper
-        .find("genericForm")
-        .first();
+    const proceedButton: () => ReactWrapper = (): ReactWrapper =>
+      wrapper.find("Button").first();
+    const genericForm: () => ReactWrapper = (): ReactWrapper =>
+      wrapper.find("genericForm").first();
 
-    expect(
-      proceedButton()
-        .exists())
-          .toEqual(true);
+    expect(proceedButton().exists()).toStrictEqual(true);
 
-    genericForm()
-      .simulate("submit");
+    genericForm().simulate("submit");
 
-    forcesSwitch()
-      .simulate("click");
+    forcesSwitch().simulate("click");
 
-    expect(formValues())
-      .toEqual({
-        comments: "",
-        confirmation: "",
-        drills: true,
-        forces: true,
-        integrates: true,
-        reason: "NONE",
-        type: "CONTINUOUS",
-      });
+    expect(formValues()).toStrictEqual({
+      comments: "",
+      confirmation: "",
+      drills: true,
+      forces: true,
+      integrates: true,
+      reason: "NONE",
+      type: "CONTINUOUS",
+    });
 
-    drillsSwitch()
-      .simulate("click");
+    drillsSwitch().simulate("click");
 
-    expect(formValues())
-      .toEqual({
-        comments: "",
-        confirmation: "",
-        drills: false,
-        forces: false,
-        integrates: true,
-        reason: "NONE",
-        type: "CONTINUOUS",
-      });
+    expect(formValues()).toStrictEqual({
+      comments: "",
+      confirmation: "",
+      drills: false,
+      forces: false,
+      integrates: true,
+      reason: "NONE",
+      type: "CONTINUOUS",
+    });
 
-    forcesSwitch()
-      .simulate("click");
+    forcesSwitch().simulate("click");
 
-    expect(formValues())
-      .toEqual({
-        comments: "",
-        confirmation: "",
-        drills: true,
-        forces: true,
-        integrates: true,
-        reason: "NONE",
-        type: "CONTINUOUS",
-      });
+    expect(formValues()).toStrictEqual({
+      comments: "",
+      confirmation: "",
+      drills: true,
+      forces: true,
+      integrates: true,
+      reason: "NONE",
+      type: "CONTINUOUS",
+    });
 
+    jest.clearAllMocks();
   });
 });
