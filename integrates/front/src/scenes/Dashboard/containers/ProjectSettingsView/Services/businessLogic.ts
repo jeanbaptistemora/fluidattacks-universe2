a1cca1c1a2962a9f953b41b000c80e35763e3a29
Jonathan Mesa <jmesa@fluidattacks.com>
2021-03-15 15:39:49 -0500

    integrates\refac(front): #2705.125 update projsettingsview
    - Lint projectsettingsview services component
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: b4ef1c86632c111fa12eaf1c73b090a1dc5e64c4

--- integrates/front/src/scenes/Dashboard/containers/ProjectSettingsView/Services/businessLogic.ts
+++ integrates/front/src/scenes/Dashboard/containers/ProjectSettingsView/Services/businessLogic.ts
@@ -1,62 +1,95 @@
-import _ from "lodash";
-import { IFormData, IGroupData } from "scenes/Dashboard/containers/ProjectSettingsView/Services/types";
 import { translate } from "utils/translations/translate";
+import type {
+  IFormData,
+  IGroupData,
+} from "scenes/Dashboard/containers/ProjectSettingsView/Services/types";
 
-const serviceStateToString: ((value: boolean | string | undefined) => string) =
-  (value: boolean | string | undefined): string => {
-    let service: string;
+const serviceStateToString: (value: boolean | string | undefined) => string = (
+  value: boolean | string | undefined
+): string => {
+  switch (typeof value) {
+    case "boolean":
+      return value ? "active" : "inactive";
+    case "string":
+      return value.toLowerCase().replace("_", "");
+    default:
+      return "";
+  }
+};
 
-    switch (typeof(value)) {
-      case "boolean":
-        service = value ? "active" : "inactive";
-        break;
-      case "string":
-        service =
-          value
-            .toLowerCase()
-            .replace("_", "");
-        break;
-      default:
-        service = "";
-    }
-
-    return service;
-  };
-
-const serviceDiff: ((msg: string, old: string, now: string) => string) =
-  (msg: string, old: string, now: string): string => {
-    const as: string = translate.t("searchFindings.servicesTable.modal.diff.as");
-    const from: string = translate.t("searchFindings.servicesTable.modal.diff.from");
-    const keep: string = translate.t("searchFindings.servicesTable.modal.diff.keep");
-    const mod: string = translate.t("searchFindings.servicesTable.modal.diff.mod");
-    const to: string = translate.t("searchFindings.servicesTable.modal.diff.to");
-
-    const msgString: string = translate.t(`searchFindings.servicesTable.${msg}`);
-    const nowString: string = translate.t(`searchFindings.servicesTable.${now}`);
-    const oldString: string = translate.t(`searchFindings.servicesTable.${old}`);
-
-    return now === old
-      ? `${keep} ${msgString} ${as} ${nowString}`
-      : `${mod} ${msgString} ${from} ${oldString} ${to} ${nowString} *`;
-  };
-
-export const computeConfirmationMessage: ((data: IGroupData, form: IFormData) => string[]) =
-  (data: IGroupData, form: IFormData): string[] => ([
-      serviceDiff("type", serviceStateToString(data.project.subscription), serviceStateToString(form.type)),
-      serviceDiff("group", serviceStateToString(true), serviceStateToString(form.integrates ? true : "deleted_soon")),
-      serviceDiff("integrates", serviceStateToString(true), serviceStateToString(form.integrates)),
-      serviceDiff("drills", serviceStateToString(data.project.hasDrills), serviceStateToString(form.drills)),
-      serviceDiff("forces", serviceStateToString(data.project.hasForces), serviceStateToString(form.forces)),
-    ]
+const serviceDiff: (msg: string, old: string, now: string) => string = (
+  msg: string,
+  old: string,
+  now: string
+): string => {
+  const as: string = translate.t("searchFindings.servicesTable.modal.diff.as");
+  const from: string = translate.t(
+    "searchFindings.servicesTable.modal.diff.from"
   );
+  const keep: string = translate.t(
+    "searchFindings.servicesTable.modal.diff.keep"
+  );
+  const mod: string = translate.t(
+    "searchFindings.servicesTable.modal.diff.mod"
+  );
+  const to: string = translate.t("searchFindings.servicesTable.modal.diff.to");
 
-export const isDowngrading: ((before: boolean | undefined, after: boolean | undefined) => boolean) =
-  (before: boolean | undefined, after: boolean | undefined): boolean =>
-    before === true && after === false;
+  const msgString: string = translate.t(`searchFindings.servicesTable.${msg}`);
+  const nowString: string = translate.t(`searchFindings.servicesTable.${now}`);
+  const oldString: string = translate.t(`searchFindings.servicesTable.${old}`);
 
-export const isDowngradingServices: ((data: IGroupData, form: IFormData) => boolean) =
-  (data: IGroupData, form: IFormData): boolean => ([
+  return now === old
+    ? `${keep} ${msgString} ${as} ${nowString}`
+    : `${mod} ${msgString} ${from} ${oldString} ${to} ${nowString} *`;
+};
+
+const computeConfirmationMessage: (
+  data: IGroupData,
+  form: IFormData
+) => string[] = (data: IGroupData, form: IFormData): string[] => [
+  serviceDiff(
+    "type",
+    serviceStateToString(data.project.subscription),
+    serviceStateToString(form.type)
+  ),
+  serviceDiff(
+    "group",
+    serviceStateToString(true),
+    serviceStateToString(form.integrates ? true : "deleted_soon")
+  ),
+  serviceDiff(
+    "integrates",
+    serviceStateToString(true),
+    serviceStateToString(form.integrates)
+  ),
+  serviceDiff(
+    "drills",
+    serviceStateToString(data.project.hasDrills),
+    serviceStateToString(form.drills)
+  ),
+  serviceDiff(
+    "forces",
+    serviceStateToString(data.project.hasForces),
+    serviceStateToString(form.forces)
+  ),
+];
+
+const isDowngrading: (
+  before: boolean | undefined,
+  after: boolean | undefined
+) => boolean = (
+  before: boolean | undefined,
+  after: boolean | undefined
+): boolean => before === true && after === false;
+
+const isDowngradingServices: (data: IGroupData, form: IFormData) => boolean = (
+  data: IGroupData,
+  form: IFormData
+): boolean =>
+  [
     isDowngrading(true, form.integrates),
     isDowngrading(data.project.hasDrills, form.drills),
     isDowngrading(data.project.hasForces, form.forces),
-  ].some((result: boolean) => result));
+  ].some((result: boolean): boolean => result);
+
+export { computeConfirmationMessage, isDowngrading, isDowngradingServices };
