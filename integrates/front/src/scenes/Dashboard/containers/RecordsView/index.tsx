155385c01b6518f628afca047cc2265b12f5f7a4
Jonathan Mesa <jmesa@fluidattacks.com>
2021-03-15 22:24:41 -0500

    integrates\refac(front): #2705.130 update recordsview
    - Lint recordsview
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: 17eb2502fca45d297ae2340bb0ffa4dacc88fbbe

--- integrates/front/src/scenes/Dashboard/containers/RecordsView/index.tsx
+++ integrates/front/src/scenes/Dashboard/containers/RecordsView/index.tsx
@@ -1,46 +1,58 @@
-import { useMutation, useQuery } from "@apollo/react-hooks";
-import { faCloudUploadAlt, faList } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { ApolloError } from "apollo-client";
-import { GraphQLError } from "graphql";
-import _ from "lodash";
-import mixpanel from "mixpanel-browser";
-import React from "react";
-import { useParams } from "react-router";
-import { Field, InjectedFormProps } from "redux-form";
-
+import type { ApolloError } from "apollo-client";
 import { Button } from "components/Button";
-import { DataTableNext } from "components/DataTableNext";
-import { FluidIcon } from "components/FluidIcon";
-import { TooltipWrapper } from "components/TooltipWrapper";
-import { GenericForm } from "scenes/Dashboard/components/GenericForm";
-import { REMOVE_EVIDENCE_MUTATION, UPDATE_EVIDENCE_MUTATION } from "scenes/Dashboard/containers/EvidenceView/queries";
-import { GET_FINDING_RECORDS } from "scenes/Dashboard/containers/RecordsView/queries";
-import { default as globalStyle } from "styles/global.css";
-import { ButtonToolbarRow, Col100, Row, RowCenter } from "styles/styledComponents";
 import { Can } from "utils/authz/Can";
+import { DataTableNext } from "components/DataTableNext";
+import { Field } from "redux-form";
 import { FileInput } from "utils/forms/fields";
+import { FluidIcon } from "components/FluidIcon";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { GET_FINDING_RECORDS } from "scenes/Dashboard/containers/RecordsView/queries";
+import { GenericForm } from "scenes/Dashboard/components/GenericForm";
+import type { GraphQLError } from "graphql";
+import type { InjectedFormProps } from "redux-form";
 import { Logger } from "utils/logger";
+import React from "react";
+import { TooltipWrapper } from "components/TooltipWrapper";
+import _ from "lodash";
+import globalStyle from "styles/global.css";
+import mixpanel from "mixpanel-browser";
 import { msgError } from "utils/notifications";
 import { translate } from "utils/translations/translate";
+import { useParams } from "react-router";
+import {
+  ButtonToolbarRow,
+  Col100,
+  Row,
+  RowCenter,
+} from "styles/styledComponents";
+import {
+  REMOVE_EVIDENCE_MUTATION,
+  UPDATE_EVIDENCE_MUTATION,
+} from "scenes/Dashboard/containers/EvidenceView/queries";
+import { faCloudUploadAlt, faList } from "@fortawesome/free-solid-svg-icons";
 import { required, validRecordsFile } from "utils/validations";
+import { useMutation, useQuery } from "@apollo/react-hooks";
 
-const recordsView: React.FC = (): JSX.Element => {
+const RecordsView: React.FC = (): JSX.Element => {
   const { findingId } = useParams<{ findingId: string }>();
 
   const [isEditing, setEditing] = React.useState(false);
-  const handleEditClick: (() => void) = (): void => { setEditing(!isEditing); };
+  const handleEditClick: () => void = React.useCallback((): void => {
+    setEditing(!isEditing);
+  }, [isEditing]);
 
-  const handleErrors: ((error: ApolloError) => void) = (
-    { graphQLErrors }: ApolloError,
-  ): void => {
+  const handleErrors: (error: ApolloError) => void = ({
+    graphQLErrors,
+  }: ApolloError): void => {
     graphQLErrors.forEach((error: GraphQLError): void => {
       msgError(translate.t("groupAlerts.errorTextsad"));
       Logger.warning("An error occurred loading finding records", error);
     });
   };
 
-  const handleRemoveErrors: ((removeError: ApolloError) => void) = (removeError: ApolloError): void => {
+  const handleRemoveErrors: (removeError: ApolloError) => void = (
+    removeError: ApolloError
+  ): void => {
     msgError(translate.t("groupAlerts.errorTextsad"));
     Logger.warning("An error occurred removing records", removeError);
   };
@@ -50,11 +62,13 @@ const recordsView: React.FC = (): JSX.Element => {
     variables: { findingId },
   });
 
-  const handleUpdateResult: (() => void) = (): void => {
+  const handleUpdateResult: () => void = (): void => {
     void refetch();
   };
 
-  const handleUpdateError: ((updateError: ApolloError) => void) = (updateError: ApolloError): void => {
+  const handleUpdateError: (updateError: ApolloError) => void = (
+    updateError: ApolloError
+  ): void => {
     updateError.graphQLErrors.forEach(({ message }: GraphQLError): void => {
       switch (message) {
         case "Exception - Wrong File Structure":
@@ -78,23 +92,32 @@ const recordsView: React.FC = (): JSX.Element => {
     onError: handleUpdateError,
   });
 
-  const handleSubmit: ((values: { filename: FileList }) => void) = (
-    values: { filename: FileList },
-  ): void => {
-    setEditing(false);
-    void updateRecords({ variables: { evidenceId: "RECORDS", file: values.filename[0], findingId } });
-  };
+  const handleSubmit: (values: {
+    filename: FileList;
+  }) => void = React.useCallback(
+    (values: { filename: FileList }): void => {
+      setEditing(false);
+      void updateRecords({
+        variables: {
+          evidenceId: "RECORDS",
+          file: values.filename[0],
+          findingId,
+        },
+      });
+    },
+    [findingId, updateRecords]
+  );
 
   const [removeRecords, removeRes] = useMutation(REMOVE_EVIDENCE_MUTATION, {
     onCompleted: handleUpdateResult,
     onError: handleRemoveErrors,
   });
 
-  const handleRemoveClick: (() => void) = (): void => {
+  const handleRemoveClick: () => void = React.useCallback((): void => {
     mixpanel.track("RemoveRecords");
     setEditing(false);
     void removeRecords({ variables: { evidenceId: "RECORDS", findingId } });
-  };
+  }, [findingId, removeRecords]);
 
   if (_.isUndefined(data) || _.isEmpty(data)) {
     return <div />;
@@ -103,16 +126,23 @@ const recordsView: React.FC = (): JSX.Element => {
   return (
     <React.StrictMode>
       <React.Fragment>
-        <Can do="backend_api_mutations_update_evidence_mutate">
+        <Can do={"backend_api_mutations_update_evidence_mutate"}>
           <Row>
+            {/* eslint-disable-next-line react/forbid-component-props */}
             <Col100 className={"pa0"}>
               <ButtonToolbarRow>
                 <TooltipWrapper
-                  id={translate.t("searchFindings.tabRecords.editableTooltip.id")}
-                  message={translate.t("searchFindings.tabRecords.editableTooltip")}
+                  id={translate.t(
+                    "searchFindings.tabRecords.editableTooltip.id"
+                  )}
+                  message={translate.t(
+                    "searchFindings.tabRecords.editableTooltip"
+                  )}
                 >
+                  {/* eslint-disable-next-line react/forbid-component-props */}
                   <Button className={"fr"} onClick={handleEditClick}>
-                    <FluidIcon icon="edit" />&nbsp;{translate.t("searchFindings.tabRecords.editable")}
+                    <FluidIcon icon={"edit"} />
+                    &nbsp;{translate.t("searchFindings.tabRecords.editable")}
                   </Button>
                 </TooltipWrapper>
               </ButtonToolbarRow>
@@ -121,58 +151,72 @@ const recordsView: React.FC = (): JSX.Element => {
         </Can>
         <br />
         {isEditing ? (
-          <GenericForm name="records" onSubmit={handleSubmit}>
+          <GenericForm name={"records"} onSubmit={handleSubmit}>
             {({ pristine }: InjectedFormProps): React.ReactNode => (
-              <React.Fragment>
-                <ButtonToolbarRow className={"mb3"}>
-                  <Field
-                    accept=".csv"
-                    component={FileInput}
-                    className={"fr"}
-                    id="recordsFile"
-                    name="filename"
-                    validate={[required, validRecordsFile]}
-                  />
-                  <Button className={"h-25"} type="submit" disabled={pristine || updateRes.loading}>
-                    <FontAwesomeIcon icon={faCloudUploadAlt} />
-                    &nbsp;{translate.t("searchFindings.tabEvidence.update")}
-                  </Button>
-                </ButtonToolbarRow>
-              </React.Fragment>
+              // eslint-disable-next-line react/forbid-component-props
+              <ButtonToolbarRow className={"mb3"}>
+                <Field
+                  accept={".csv"}
+                  // eslint-disable-next-line react/forbid-component-props
+                  className={"fr"}
+                  component={FileInput}
+                  id={"recordsFile"}
+                  name={"filename"}
+                  validate={[required, validRecordsFile]}
+                />
+                <Button
+                  // eslint-disable-next-line react/forbid-component-props
+                  className={"h-25"}
+                  disabled={pristine || updateRes.loading}
+                  type={"submit"}
+                >
+                  <FontAwesomeIcon icon={faCloudUploadAlt} />
+                  &nbsp;{translate.t("searchFindings.tabEvidence.update")}
+                </Button>
+              </ButtonToolbarRow>
             )}
           </GenericForm>
         ) : undefined}
+        {/* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access */}
         {isEditing && !_.isEmpty(JSON.parse(data.finding.records)) ? (
           <Row>
+            {/* eslint-disable-next-line react/forbid-component-props */}
             <Col100 className={"pa0"}>
-              <Button className={"fr"} onClick={handleRemoveClick} disabled={removeRes.loading}>
-                <FluidIcon icon="delete" />
+              <Button
+                // eslint-disable-next-line react/forbid-component-props
+                className={"fr"}
+                disabled={removeRes.loading}
+                onClick={handleRemoveClick}
+              >
+                <FluidIcon icon={"delete"} />
                 &nbsp;{translate.t("searchFindings.tabEvidence.remove")}
               </Button>
             </Col100>
           </Row>
         ) : undefined}
         <RowCenter>
+          {/* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access */}
           {_.isEmpty(JSON.parse(data.finding.records)) ? (
             <div className={globalStyle["no-data"]}>
-              <FontAwesomeIcon size={"3x"} icon={faList} />
+              <FontAwesomeIcon icon={faList} size={"3x"} />
               <p>{translate.t("group.findings.records.noData")}</p>
             </div>
           ) : (
-              <DataTableNext
-                bordered={true}
-                dataset={JSON.parse(data.finding.records)}
-                exportCsv={false}
-                headers={[]}
-                id="tblRecords"
-                pageSize={15}
-                search={false}
-              />
-            )}
+            <DataTableNext
+              bordered={true}
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+              dataset={JSON.parse(data.finding.records)}
+              exportCsv={false}
+              headers={[]}
+              id={"tblRecords"}
+              pageSize={15}
+              search={false}
+            />
+          )}
         </RowCenter>
       </React.Fragment>
     </React.StrictMode>
   );
 };
 
-export { recordsView as RecordsView };
+export { RecordsView };
