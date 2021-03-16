1c893021fd10e6f4aa437243dc8dcb2db3b1f46b
Jonathan Mesa <jmesa@fluidattacks.com>
2021-03-16 08:16:41 -0500

    integrates\refac(front): #2705.133 update projsettingsview
    - Lint projectsettingsview test
    - No visual changes
    
    
    Former-commit-id: f04aad50d8bb08dfbca0a45defb8ab64aa1c802f

--- integrates/front/src/scenes/Dashboard/containers/ProjectSettingsView/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/containers/ProjectSettingsView/index.spec.tsx
@@ -1,33 +1,34 @@
-import { MockedProvider, MockedResponse } from "@apollo/react-testing";
-import { mount, ReactWrapper } from "enzyme";
-import { GraphQLError } from "graphql";
-import * as React from "react";
-import { Provider } from "react-redux";
-import wait from "waait";
-
-import { MemoryRouter, Route } from "react-router";
-import { ProjectSettingsView } from "scenes/Dashboard/containers/ProjectSettingsView";
 import { GET_TAGS } from "scenes/Dashboard/containers/ProjectSettingsView/queries";
+import { GraphQLError } from "graphql";
+import { MockedProvider } from "@apollo/react-testing";
+import type { MockedResponse } from "@apollo/react-testing";
+import { ProjectSettingsView } from "scenes/Dashboard/containers/ProjectSettingsView";
+import { Provider } from "react-redux";
+import React from "react";
+import type { ReactWrapper } from "enzyme";
+import { mount } from "enzyme";
 import store from "store";
+import wait from "waait";
+import { MemoryRouter, Route } from "react-router";
 
-describe("ProjectSettingsView", () => {
+describe("ProjectSettingsView", (): void => {
   const mocksTags: Readonly<MockedResponse> = {
-      request: {
-        query: GET_TAGS,
-        variables: {
-          projectName: "TEST",
+    request: {
+      query: GET_TAGS,
+      variables: {
+        projectName: "TEST",
+      },
+    },
+    result: {
+      data: {
+        project: {
+          tags: ["test"],
         },
       },
-      result: {
-        data: {
-          project: {
-            tags: ["test"],
-          },
-        },
-      },
+    },
   };
 
-  const mockError: ReadonlyArray<MockedResponse> = [
+  const mockError: readonly MockedResponse[] = [
     {
       request: {
         query: GET_TAGS,
@@ -41,68 +42,69 @@ describe("ProjectSettingsView", () => {
     },
   ];
 
-  it("should return a function", () => {
-    expect(typeof (ProjectSettingsView))
-      .toEqual("function");
+  it("should return a function", (): void => {
+    expect.hasAssertions();
+    expect(typeof ProjectSettingsView).toStrictEqual("function");
   });
 
-  it("should render tags component", async () => {
+  it("should render tags component", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
-        <MockedProvider mocks={[mocksTags]} addTypename={false}>
-          <MemoryRouter
-            initialEntries={["/orgs/okada/groups/TEST/scope"]}
-          >
+        <MockedProvider addTypename={false} mocks={[mocksTags]}>
+          <MemoryRouter initialEntries={["/orgs/okada/groups/TEST/scope"]}>
             <Route
               component={ProjectSettingsView}
               path={"/orgs/:organizationName/groups/:projectName/scope"}
             />
           </MemoryRouter>
         </MockedProvider>
-      </Provider>,
+      </Provider>
     );
     await wait(0);
-    expect(wrapper)
-      .toHaveLength(1);
+
+    expect(wrapper).toHaveLength(1);
   });
 
-  it("should render a error in component", async () => {
+  it("should render a error in component", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
-        <MockedProvider mocks={mockError} addTypename={false}>
-          <MemoryRouter
-            initialEntries={["/orgs/okada/groups/TEST/scope"]}
-          >
+        <MockedProvider addTypename={false} mocks={mockError}>
+          <MemoryRouter initialEntries={["/orgs/okada/groups/TEST/scope"]}>
             <Route
               component={ProjectSettingsView}
               path={"/orgs/:organizationName/groups/:projectName/scope"}
             />
           </MemoryRouter>
         </MockedProvider>
-      </Provider>,
+      </Provider>
     );
     await wait(0);
-    expect(wrapper)
-      .toHaveLength(1);
+
+    expect(wrapper).toHaveLength(1);
   });
 
-  it("should render files component", async () => {
+  it("should render files component", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const wrapper: ReactWrapper = mount(
       <Provider store={store}>
-        <MockedProvider mocks={[mocksTags]} addTypename={false}>
-          <MemoryRouter
-            initialEntries={["/orgs/okada/groups/TEST/scope"]}
-          >
+        <MockedProvider addTypename={false} mocks={[mocksTags]}>
+          <MemoryRouter initialEntries={["/orgs/okada/groups/TEST/scope"]}>
             <Route
               component={ProjectSettingsView}
               path={"/orgs/:organizationName/groups/:projectName/scope"}
             />
           </MemoryRouter>
         </MockedProvider>
-      </Provider>,
+      </Provider>
     );
     await wait(0);
-    expect(wrapper.find("#tblFiles"))
-      .toBeTruthy();
+
+    // eslint-disable-next-line jest/no-restricted-matchers
+    expect(wrapper.find("#tblFiles")).toBeTruthy();
   });
 });
