8e92b00db0c9a8022eeaea5b6003070646801fe7
Jonathan Mesa <jmesa@fluidattacks.com>
2021-03-16 16:39:51 -0500

    integrates\refac(front): #2705.138 update severityview
    - Lint severityview test
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: 8e59915fdfbfce92457859d1f6de9f899c85d37f

--- integrates/front/src/scenes/Dashboard/containers/SeverityView/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/containers/SeverityView/index.spec.tsx
@@ -1,21 +1,21 @@
-import { MockedProvider, MockedResponse } from "@apollo/react-testing";
-import { PureAbility } from "@casl/ability";
-import { mount, ReactWrapper } from "enzyme";
-import { GraphQLError } from "graphql";
-import * as React from "react";
-// tslint:disable-next-line: no-submodule-imports
-import { act } from "react-dom/test-utils";
-import { Provider } from "react-redux";
-import {  MemoryRouter, Route } from "react-router-dom";
-import wait from "waait";
-
-import { SeverityView } from "scenes/Dashboard/containers/SeverityView";
 import { GET_SEVERITY } from "scenes/Dashboard/containers/SeverityView/queries";
-import store from "store";
+import { GraphQLError } from "graphql";
+import { MockedProvider } from "@apollo/react-testing";
+import type { MockedResponse } from "@apollo/react-testing";
+import { Provider } from "react-redux";
+import { PureAbility } from "@casl/ability";
+import React from "react";
+import type { ReactWrapper } from "enzyme";
+import { SeverityView } from "scenes/Dashboard/containers/SeverityView";
+import { act } from "react-dom/test-utils";
 import { authzPermissionsContext } from "utils/authz/config";
+import { mount } from "enzyme";
+import store from "store";
+import wait from "waait";
+import { MemoryRouter, Route } from "react-router-dom";
 
-describe("SeverityView", () => {
-  const mocks: ReadonlyArray<MockedResponse> = [
+describe("SeverityView", (): void => {
+  const mocks: readonly MockedResponse[] = [
     {
       request: {
         query: GET_SEVERITY,
@@ -55,9 +55,10 @@ describe("SeverityView", () => {
           },
         },
       },
-    }];
+    },
+  ];
 
-  const mockError: ReadonlyArray<MockedResponse> = [
+  const mockError: readonly MockedResponse[] = [
     {
       request: {
         query: GET_SEVERITY,
@@ -71,85 +72,131 @@ describe("SeverityView", () => {
     },
   ];
 
-  it("should return a function", () => {
-    expect(typeof (SeverityView))
-      .toEqual("function");
+  it("should return a function", (): void => {
+    expect.hasAssertions();
+    expect(typeof SeverityView).toStrictEqual("function");
   });
 
-  it("should render a component", async () => {
+  it("should render a component", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const wrapper: ReactWrapper = mount(
       <MemoryRouter initialEntries={["/TEST/vulns/438679960/severity"]}>
         <Provider store={store}>
-          <MockedProvider mocks={mocks} addTypename={false}>
-            <Route component={SeverityView} path={"/:projectName/vulns/:findingId/severity"} />
+          <MockedProvider addTypename={false} mocks={mocks}>
+            <Route
+              component={SeverityView}
+              path={"/:projectName/vulns/:findingId/severity"}
+            />
           </MockedProvider>
         </Provider>
-      </MemoryRouter>,
+      </MemoryRouter>
     );
-    await act(async () => { await wait(0); wrapper.update(); });
-    expect(wrapper)
-      .toHaveLength(1);
-    expect(wrapper.text())
-      .toContain("1.00 | High: Exploit is not required or it can be automated");
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+
+    expect(wrapper).toHaveLength(1);
+    expect(wrapper.text()).toContain(
+      "1.00 | High: Exploit is not required or it can be automated"
+    );
   });
 
-  it("should render an error in component", async () => {
+  it("should render an error in component", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const wrapper: ReactWrapper = mount(
       <MemoryRouter initialEntries={["/TEST/vulns/438679960/severity"]}>
         <Provider store={store}>
-          <MockedProvider mocks={mockError} addTypename={false}>
-            <Route component={SeverityView} path={"/:projectName/vulns/:findingId/severity"} />
+          <MockedProvider addTypename={false} mocks={mockError}>
+            <Route
+              component={SeverityView}
+              path={"/:projectName/vulns/:findingId/severity"}
+            />
           </MockedProvider>
         </Provider>
-      </MemoryRouter>,
+      </MemoryRouter>
     );
-    await act(async () => { await wait(0); wrapper.update(); });
-    expect(wrapper)
-      .toHaveLength(1);
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+
+    expect(wrapper).toHaveLength(1);
   });
 
-  it("should render as editable", async () => {
+  it("should render as editable", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const mockedPermissions: PureAbility<string> = new PureAbility([
       { action: "backend_api_mutations_update_severity_mutate" },
     ]);
     const wrapper: ReactWrapper = mount(
       <MemoryRouter initialEntries={["/TEST/vulns/438679960/severity"]}>
         <Provider store={store}>
-          <MockedProvider mocks={mocks} addTypename={false}>
+          <MockedProvider addTypename={false} mocks={mocks}>
             <authzPermissionsContext.Provider value={mockedPermissions}>
-              <Route component={SeverityView} path={"/:projectName/vulns/:findingId/severity"} />
+              <Route
+                component={SeverityView}
+                path={"/:projectName/vulns/:findingId/severity"}
+              />
             </authzPermissionsContext.Provider>
           </MockedProvider>
         </Provider>
-      </MemoryRouter>,
+      </MemoryRouter>
     );
-    await act(async () => { await wait(0); wrapper.update(); });
-    const editButton: ReactWrapper = wrapper.find("button")
-      .findWhere((element: ReactWrapper) => element.contains("Edit"))
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+    const editButton: ReactWrapper = wrapper
+      .find("button")
+      .findWhere((element: ReactWrapper): boolean => element.contains("Edit"))
       .at(0);
-    expect(editButton)
-      .toHaveLength(1);
+
+    expect(editButton).toHaveLength(1);
+
     editButton.simulate("click");
-    act(() => { wrapper.update(); });
-    expect(wrapper.text())
-      .toContain("Update");
+    act((): void => {
+      wrapper.update();
+    });
+
+    expect(wrapper.text()).toContain("Update");
   });
 
-  it("should render as readonly", async () => {
+  it("should render as readonly", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const wrapper: ReactWrapper = mount(
       <MemoryRouter initialEntries={["/TEST/vulns/438679960/severity"]}>
         <Provider store={store}>
-          <MockedProvider mocks={mockError} addTypename={false}>
-            <Route component={SeverityView} path={"/:projectName/vulns/:findingId/severity"} />
+          <MockedProvider addTypename={false} mocks={mockError}>
+            <Route
+              component={SeverityView}
+              path={"/:projectName/vulns/:findingId/severity"}
+            />
           </MockedProvider>
         </Provider>
-      </MemoryRouter>,
+      </MemoryRouter>
     );
-    await act(async () => { await wait(0); wrapper.update(); });
-    const editButton: ReactWrapper = wrapper.find("button")
-      .findWhere((element: ReactWrapper) => element.contains("Edit"))
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+    const editButton: ReactWrapper = wrapper
+      .find("button")
+      .findWhere((element: ReactWrapper): boolean => element.contains("Edit"))
       .at(0);
-    expect(editButton)
-      .toHaveLength(0);
+
+    expect(editButton).toHaveLength(0);
   });
 });
