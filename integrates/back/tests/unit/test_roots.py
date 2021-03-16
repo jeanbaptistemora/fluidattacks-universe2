b56ee3a3b85b1e21203d9f72cf60679a71966279
Andres Aguirre <aaguirre@fluidattacks.com>
2021-03-16 18:59:07 -0500

    integrates\test(back): #4014.6 get root id by filename test
    - Added unittest for test_get_root_id_by_filename
    
    
    Former-commit-id: 2341b6a9794d128eb579d84517e94ca8121934f7

---
+++ integrates/back/tests/unit/test_roots.py
@@ -0,0 +1,25 @@
+# Third party libraries
+import pytest
+
+# Local libraries
+from backend.api import get_new_context
+from newutils import (
+    roots as roots_utils,
+)
+
+# Constants
+pytestmark = [
+    pytest.mark.asyncio,
+]
+
+
+async def test_get_root_id_by_filename():
+    loaders = get_new_context()
+    group_name = 'unittesting'
+    group_roots_loader = loaders.group_roots
+    group_roots = await group_roots_loader.load(group_name)
+    root_id = roots_utils.get_root_id_by_filename(
+        'product/integrates/test.config.json',
+        group_roots
+    )
+    assert root_id == 'ROOT#4039d098-ffc5-4984-8ed3-eb17bca98e19'
