ef325198ab8c41d0ec20b401914a5ff543ee5d45
Jonathan Mesa <jmesa@fluidattacks.com>
2021-03-15 23:07:34 -0500

    integrates\refac(front): #2705.132 update projsettingsview
    - Lint projectsettingsview queries file
    - No visual changes
    
    
    Former-commit-id: bc2dc0dfa0335ac7d7d252f112c02f280582217f

--- integrates/front/src/scenes/Dashboard/containers/ProjectSettingsView/queries.ts
+++ integrates/front/src/scenes/Dashboard/containers/ProjectSettingsView/queries.ts
@@ -1,7 +1,7 @@
-import { DocumentNode } from "graphql";
+import type { DocumentNode } from "graphql";
 import gql from "graphql-tag";
 
-export const GET_GROUP_DATA: DocumentNode = gql`
+const GET_GROUP_DATA: DocumentNode = gql`
   query GetGroupData($groupName: String!) {
     project(projectName: $groupName) {
       hasDrills
@@ -10,9 +10,9 @@ export const GET_GROUP_DATA: DocumentNode = gql`
       subscription
     }
   }
-  `;
+`;
 
-export const EDIT_GROUP_DATA: DocumentNode = gql`
+const EDIT_GROUP_DATA: DocumentNode = gql`
   mutation EditGroupData(
     $comments: String!
     $groupName: String!
@@ -34,62 +34,64 @@ export const EDIT_GROUP_DATA: DocumentNode = gql`
       success
     }
   }
-  `;
+`;
 
-export const REMOVE_TAG_MUTATION: DocumentNode = gql`
+const REMOVE_TAG_MUTATION: DocumentNode = gql`
   mutation RemoveTagMutation($tagToRemove: String!, $projectName: String!) {
-    removeTag (
-      tag: $tagToRemove,
-      projectName: $projectName,
-    ) {
+    removeTag(tag: $tagToRemove, projectName: $projectName) {
       success
     }
   }
-  `;
+`;
 
-export const GET_TAGS: DocumentNode = gql`
+const GET_TAGS: DocumentNode = gql`
   query GetTagsQuery($projectName: String!) {
-    project(projectName: $projectName){
+    project(projectName: $projectName) {
       tags
     }
   }
-  `;
+`;
 
-export const ADD_TAGS_MUTATION: DocumentNode = gql`
+const ADD_TAGS_MUTATION: DocumentNode = gql`
   mutation AddTagsMutation($projectName: String!, $tagsData: JSONString!) {
-    addTags (
-      tags: $tagsData,
-      projectName: $projectName) {
+    addTags(tags: $tagsData, projectName: $projectName) {
       success
     }
   }
-  `;
+`;
 
-export const UPDATE_ENVIRONMENT_MUTATION: DocumentNode = gql`
-  mutation UpdateEnvironmentMutation($projectName: String!, $env: EnvironmentInput!, $state: ResourceState!) {
+const UPDATE_ENVIRONMENT_MUTATION: DocumentNode = gql`
+  mutation UpdateEnvironmentMutation(
+    $projectName: String!
+    $env: EnvironmentInput!
+    $state: ResourceState!
+  ) {
     updateEnvironment(projectName: $projectName, env: $env, state: $state) {
       success
     }
   }
 `;
 
-export const ADD_ENVIRONMENTS_MUTATION: DocumentNode = gql`
-  mutation AddEnvironmentsMutation($projectName: String!, $envs: [EnvironmentInput]!) {
+const ADD_ENVIRONMENTS_MUTATION: DocumentNode = gql`
+  mutation AddEnvironmentsMutation(
+    $projectName: String!
+    $envs: [EnvironmentInput]!
+  ) {
     addEnvironments(projectName: $projectName, envs: $envs) {
       success
     }
   }
 `;
 
-export const GET_ENVIRONMENTS: DocumentNode = gql`
-query GetEnvironmentsQuery($projectName: String!) {
-  resources (projectName: $projectName) {
-    environments
+const GET_ENVIRONMENTS: DocumentNode = gql`
+  query GetEnvironmentsQuery($projectName: String!) {
+    resources(projectName: $projectName) {
+      environments
+    }
   }
-}
 `;
 
-export const GET_FILES: DocumentNode = gql`
+const GET_FILES: DocumentNode = gql`
   query GetFilesQuery($projectName: String!) {
     resources(projectName: $projectName) {
       files
@@ -97,8 +99,11 @@ export const GET_FILES: DocumentNode = gql`
   }
 `;
 
-export const DOWNLOAD_FILE_MUTATION: DocumentNode = gql`
-  mutation DownloadFileMutation($filesData: JSONString!, $projectName: String!) {
+const DOWNLOAD_FILE_MUTATION: DocumentNode = gql`
+  mutation DownloadFileMutation(
+    $filesData: JSONString!
+    $projectName: String!
+  ) {
     downloadFile(filesData: $filesData, projectName: $projectName) {
       success
       url
@@ -106,7 +111,7 @@ export const DOWNLOAD_FILE_MUTATION: DocumentNode = gql`
   }
 `;
 
-export const REMOVE_FILE_MUTATION: DocumentNode = gql`
+const REMOVE_FILE_MUTATION: DocumentNode = gql`
   mutation RemoveFileMutation($filesData: JSONString!, $projectName: String!) {
     removeFiles(filesData: $filesData, projectName: $projectName) {
       success
@@ -114,10 +119,29 @@ export const REMOVE_FILE_MUTATION: DocumentNode = gql`
   }
 `;
 
-export const UPLOAD_FILE_MUTATION: DocumentNode = gql`
-  mutation UploadFileMutation($file: Upload!, $filesData: JSONString!, $projectName: String!) {
+const UPLOAD_FILE_MUTATION: DocumentNode = gql`
+  mutation UploadFileMutation(
+    $file: Upload!
+    $filesData: JSONString!
+    $projectName: String!
+  ) {
     addFiles(file: $file, filesData: $filesData, projectName: $projectName) {
       success
     }
   }
 `;
+
+export {
+  GET_GROUP_DATA,
+  EDIT_GROUP_DATA,
+  REMOVE_TAG_MUTATION,
+  GET_TAGS,
+  ADD_TAGS_MUTATION,
+  UPDATE_ENVIRONMENT_MUTATION,
+  ADD_ENVIRONMENTS_MUTATION,
+  GET_ENVIRONMENTS,
+  GET_FILES,
+  DOWNLOAD_FILE_MUTATION,
+  REMOVE_FILE_MUTATION,
+  UPLOAD_FILE_MUTATION,
+};
