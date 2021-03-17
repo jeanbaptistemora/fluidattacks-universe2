33cfb8c44647ef6fe04f156a468a1924cf864cce
Mario Varas <mvaras@fluidattacks.com>
2021-03-17 09:16:34 -0500

    sorts\feat(back): #4270.3 new train process step
    - This code has no use by now, just needed for testing
    - Useful to keep schematizing this new incoming feature
    
    
    Former-commit-id: 460ea4e03fd438dd1a645dc7609b6f1e874b305e

---
+++ sorts/training/tune_hyperparameters.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# Standard libraries
+import os
+import tempfile
+
+# Third party libraries
+from sagemaker.sklearn.estimator import SKLearn as SKLearnEstimator
+from sagemaker.tuner import (
+    CategoricalParameter,
+    HyperparameterTuner
+)
+
+# Local libraries
+from evaluate_results import get_best_model_name
+from sagemaker_provisioner import get_estimator
+
+
+def deploy_hyperparameter_tuning_job() -> None:
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        model_name_file: str = os.path.join(tmp_dir, 'best_model.txt')
+        model: str = get_best_model_name(model_name_file)
+    estimator: SKLearnEstimator = get_estimator(model)
+    tuner = HyperparameterTuner(
+        estimator,
+        metric_definitions=[
+            {'Name': 'precision', 'Regex': 'Precision: (.*?)%'},
+            {'Name': 'recall', 'Regex': 'Recall: (.*?)%'},
+            {'Name': 'fscore', 'Regex': 'F1-Score: (.*?)%'},
+            {'Name': 'overfit', 'Regex': 'Overfit: (.*?)%'}
+        ],
+        objective_metric_name='fscore',
+        objective_type='Minimize',
+        hyperparameter_ranges={
+            'activation': CategoricalParameter(['tanh', 'relu'])
+        }
+    )
+
+    tuner.fit({
+        'train': 's3://sorts/training/binary_encoded_training_data.csv'
+    })
+
+
+if __name__ == '__main__':
+    deploy_hyperparameter_tuning_job()
