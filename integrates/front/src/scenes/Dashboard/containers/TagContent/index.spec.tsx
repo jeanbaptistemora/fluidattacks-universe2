93f6ef5824db727afa8f4fb619b283a5c0b69675
Juan Chaves <jchaves@fluidattacks.com>
2021-03-18 14:36:17 -0500

    integrates\refac(front): #2705.141 update tagcontent
    - Lint tagcontent test
    - No visual changes
    
    
    Former-commit-id: 1057b54181f1ff5460d7f39ee3270e6446403cfc

--- integrates/front/src/scenes/Dashboard/containers/TagContent/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/containers/TagContent/index.spec.tsx
@@ -1,15 +1,16 @@
-import { MockedProvider, MockedResponse } from "@apollo/react-testing";
-import { mount, ReactWrapper } from "enzyme";
-import React from "react";
-import { Provider } from "react-redux";
-import { Route } from "react-router";
-import { MemoryRouter } from "react-router-dom";
 import { GET_ORGANIZATION_ID } from "scenes/Dashboard/containers/OrganizationContent/queries";
+import { MemoryRouter } from "react-router-dom";
+import { MockedProvider } from "@apollo/react-testing";
+import type { MockedResponse } from "@apollo/react-testing";
+import { Provider } from "react-redux";
+import React from "react";
+import type { ReactWrapper } from "enzyme";
+import { Route } from "react-router";
 import { TagContent } from "scenes/Dashboard/containers/TagContent";
+import { mount } from "enzyme";
 import store from "store";
 
-describe("TagContent", () => {
-
+describe("TagContent", (): void => {
   const mocks: MockedResponse = {
     request: {
       query: GET_ORGANIZATION_ID,
@@ -26,23 +27,30 @@ describe("TagContent", () => {
     },
   };
 
-  it("should return a function", () => {
-    expect(typeof (TagContent))
-      .toEqual("function");
+  it("should return a function", (): void => {
+    expect.hasAssertions();
+
+    expect(typeof TagContent).toStrictEqual("function");
   });
 
-  it("should render a component", () => {
+  it("should render a component", (): void => {
+    expect.hasAssertions();
+
     const wrapper: ReactWrapper = mount(
-      <MemoryRouter initialEntries={["/orgs/testorg/portfolios/test-projects/indicators"]}>
+      <MemoryRouter
+        initialEntries={["/orgs/testorg/portfolios/test-projects/indicators"]}
+      >
         <Provider store={store}>
-          <MockedProvider mocks={[mocks]} addTypename={false}>
-            <Route path="/orgs/:organizationName/portfolios/:tagName/indicators" component={TagContent} />
+          <MockedProvider addTypename={false} mocks={[mocks]}>
+            <Route
+              component={TagContent}
+              path={"/orgs/:organizationName/portfolios/:tagName/indicators"}
+            />
           </MockedProvider>
         </Provider>
-      </MemoryRouter>,
+      </MemoryRouter>
     );
 
-    expect(wrapper)
-      .toHaveLength(1);
+    expect(wrapper).toHaveLength(1);
   });
 });
