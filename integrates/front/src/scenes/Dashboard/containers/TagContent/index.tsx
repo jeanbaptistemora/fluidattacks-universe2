489b5471da3a3541fbbb17632f5a4ef333186a0c
Juan Chaves <jchaves@fluidattacks.com>
2021-03-18 16:13:47 -0500

    integrates\refac(front): #2705.142 update tagcontent
    - Lint tagcontent index
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: 44c3812d1923779c1d27c7c786df06fbee2915e8

--- integrates/front/src/scenes/Dashboard/containers/TagContent/index.tsx
+++ integrates/front/src/scenes/Dashboard/containers/TagContent/index.tsx
@@ -1,21 +1,26 @@
-import { useQuery } from "@apollo/react-hooks";
-import { ApolloError } from "apollo-client";
-import { GraphQLError } from "graphql";
-import _ from "lodash";
-import React from "react";
-import { useParams, useRouteMatch } from "react-router";
-import { Redirect, Route, Switch } from "react-router-dom";
-import { ContentTab } from "scenes/Dashboard/components/ContentTab";
+import type { ApolloError } from "apollo-client";
 import { ChartsForPortfolioView } from "scenes/Dashboard/containers/ChartsForPortfolioView";
+import { ContentTab } from "scenes/Dashboard/components/ContentTab";
 import { GET_ORGANIZATION_ID } from "scenes/Dashboard/containers/OrganizationContent/queries";
-import { TagsGroup } from "scenes/Dashboard/containers/TagContent/TagGroup";
-import { default as globalStyle } from "styles/global.css";
-import { Col100, Row, StickyContainer, TabsContainer } from "styles/styledComponents";
+import type { GraphQLError } from "graphql";
 import { Logger } from "utils/logger";
+import React from "react";
+import { TagsGroup } from "scenes/Dashboard/containers/TagContent/TagGroup";
+import _ from "lodash";
+import globalStyle from "styles/global.css";
 import { msgError } from "utils/notifications";
 import { translate } from "utils/translations/translate";
+import { useQuery } from "@apollo/react-hooks";
+import {
+  Col100,
+  Row,
+  StickyContainer,
+  TabsContainer,
+} from "styles/styledComponents";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { useParams, useRouteMatch } from "react-router";
 
-const tagContent: React.FC = (): JSX.Element => {
+const TagContent: React.FC = (): JSX.Element => {
   const { organizationName } = useParams<{ organizationName: string }>();
   const { path, url } = useRouteMatch();
 
@@ -32,46 +37,62 @@ const tagContent: React.FC = (): JSX.Element => {
   });
 
   if (_.isUndefined(data) || _.isEmpty(data)) {
-    return <React.Fragment />;
+    return <div />;
   }
 
   return (
     <React.StrictMode>
-      <React.Fragment>
+      <div>
         <Row>
           <Col100>
             <StickyContainer>
               <TabsContainer>
-                  <ContentTab
-                    icon="icon pe-7s-graph3"
-                    id="tagIndicatorsTab"
-                    link={`${url}/analytics`}
-                    title={translate.t("organization.tabs.portfolios.tabs.indicators.text")}
-                    tooltip={translate.t("organization.tabs.portfolios.tabs.indicators.tooltip")}
+                <ContentTab
+                  icon={"icon pe-7s-graph3"}
+                  id={"tagIndicatorsTab"}
+                  link={`${url}/analytics`}
+                  title={translate.t(
+                    "organization.tabs.portfolios.tabs.indicators.text"
+                  )}
+                  tooltip={translate.t(
+                    "organization.tabs.portfolios.tabs.indicators.tooltip"
+                  )}
+                />
+                <ContentTab
+                  icon={"icon pe-7s-albums"}
+                  id={"tagGroupsTab"}
+                  link={`${url}/groups`}
+                  title={translate.t(
+                    "organization.tabs.portfolios.tabs.group.text"
+                  )}
+                  tooltip={translate.t(
+                    "organization.tabs.portfolios.tabs.group.tooltip"
+                  )}
+                />
+              </TabsContainer>
+            </StickyContainer>
+            {/* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access*/}
+            <div className={globalStyle.tabContent}>
+              <Switch>
+                <Route exact={true} path={`${path}/analytics`}>
+                  <ChartsForPortfolioView
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+                    organizationId={data.organizationId.id}
                   />
-                  <ContentTab
-                    icon="icon pe-7s-albums"
-                    id="tagGroupsTab"
-                    link={`${url}/groups`}
-                    title={translate.t("organization.tabs.portfolios.tabs.group.text")}
-                    tooltip={translate.t("organization.tabs.portfolios.tabs.group.tooltip")}
-                  />
-                </TabsContainer>
-              </StickyContainer>
-              <div className={globalStyle.tabContent}>
-                <Switch>
-                  <Route path={`${path}/analytics`} exact={true} >
-                    <ChartsForPortfolioView organizationId={data.organizationId.id} />
-                  </Route>
-                  <Route path={`${path}/groups`} component={TagsGroup} exact={true} />
-                  <Redirect to={`${path}/analytics`} />
-                </Switch>
-              </div>
+                </Route>
+                <Route
+                  component={TagsGroup}
+                  exact={true}
+                  path={`${path}/groups`}
+                />
+                <Redirect to={`${path}/analytics`} />
+              </Switch>
+            </div>
           </Col100>
         </Row>
-      </React.Fragment>
+      </div>
     </React.StrictMode>
   );
 };
 
-export { tagContent as TagContent };
+export { TagContent };
