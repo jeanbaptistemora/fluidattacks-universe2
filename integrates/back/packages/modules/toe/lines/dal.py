9189bf9623561141b76eead43aeea952cdfe9881
Andres Aguirre <aaguirre@fluidattacks.com>
2021-03-19 16:55:56 -0500

    integrates\refac(back): #4014.12 update toe lines dal
    - Removed the code that used the old database model
    in toe lines dal
    - Added function to update toe lines in the dal
    
    
    Former-commit-id: 5117a66f84f8c9b2f1e2bf8ee9232507cdbcaca3

--- integrates/back/packages/modules/toe/lines/dal.py
+++ integrates/back/packages/modules/toe/lines/dal.py
@@ -1,74 +1,30 @@
 # Standard libraries
-import functools
 import logging
 import logging.config
-from typing import (
-    Dict,
-    Set
-)
 
 # Third party libraries
 from botocore.exceptions import ClientError
 
 # Local libraries
 from back.settings import LOGGING
-from backend.dal.helpers import dynamodb
-from backend.typing import (
-    ToeLines,
-    DalToeLines
+from backend.exceptions import (
+    UnavailabilityError,
 )
+from dynamodb import model
+from dynamodb.types import GitRootToeLines
 
 # Constants
 logging.config.dictConfig(LOGGING)
 LOGGER = logging.getLogger(__name__)
-TABLE_NAME: str = 'FI_toe_lines'
-MAPPING: Dict[str, Dict[str, str]] = {
-    'pk': {
-        'prefix': 'ROOT#',
-        'name': 'root_id'
-    },
-    'sk': {
-        'prefix': 'FILENAME#',
-        'name': 'filename'
-    }
-}
-FIELD_NAMES: Set[str] = functools.reduce(
-    lambda names, item: {*names, item['name']},
-    MAPPING.values(),
-    set()
-)
 
 
-def _map_keys_to_dal(toe_lines: ToeLines) -> DalToeLines:
-    """
-    Map domain keys to its DynamoDB representation
-    """
-    mapped_toe_lines = {
-        key: getattr(toe_lines, key)
-        for key in toe_lines._fields
-        if key not in FIELD_NAMES
-    }
-    mapped_toe_lines.update({
-        key: (
-            f'{MAPPING[key]["prefix"]}'
-            f'{getattr(toe_lines, MAPPING[key]["name"])}'
-        )
-        for key in MAPPING
-    })
-    dal_toe_lines = DalToeLines(**mapped_toe_lines)
-
-    return dal_toe_lines
-
-
-async def create(toe_lines: ToeLines) -> bool:
-    """Create a toe lines"""
-    success = False
+async def update(root_toe_lines: GitRootToeLines) -> None:
     try:
-        dal_toe_lines = _map_keys_to_dal(toe_lines)
-        success = await dynamodb.async_put_item(
-            TABLE_NAME, dal_toe_lines._asdict()
+        await model.update_git_root_toe_lines(
+            group_name=root_toe_lines.group_name,
+            root_id=root_toe_lines.root_id,
+            root_toe_lines=root_toe_lines
         )
     except ClientError as ex:
         LOGGER.exception(ex, extra={'extra': locals()})
-
-    return success
+        raise UnavailabilityError() from ex
