250ad9d5411d18b623cf36acbff7c65caa93011a
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-03-23 12:28:25 -0500

    integrates\fix(back): #4125.14 enable load_truncated_images
    - enable load_truncated_images to avoid raising error when
    converting images to png in convert_evidences_to_png
    
    
    Former-commit-id: afa0389900ce3fc328e201505214bf7926271655

--- integrates/back/packages/integrates-back/backend/reports/reports/technical.py
+++ integrates/back/packages/integrates-back/backend/reports/reports/technical.py
@@ -12,7 +12,10 @@ from typing import (
 # Third party libraries
 from tempfile import TemporaryDirectory
 from botocore.exceptions import ClientError
-from PIL import Image
+from PIL import (
+    Image,
+    ImageFile,
+)
 
 # Local libraries
 from back.settings import LOGGING
@@ -26,7 +29,6 @@ from newutils import reports as reports_utils
 from newutils.passphrase import get_passphrase
 from notifications import domain as notifications_domain
 
-
 logging.config.dictConfig(LOGGING)
 
 LOGGER = logging.getLogger(__name__)
@@ -34,21 +36,35 @@ LOGGER = logging.getLogger(__name__)
 
 def convert_evidences_to_png(
     findings: List[Dict[str, FindingType]],
-    tempdir: str
+    tempdir: str,
+    group_name: str
 ) -> None:
     """
     Standardize all evidences to png, converting evidences
     like .gif, .jpg and evidences without extension.
     """
+    ImageFile.LOAD_TRUNCATED_IMAGES = True
     for finding in findings:
         for evidence in cast(List[Any], finding.get('evidence_set', [])):
-            img_id = evidence['id'].split('/')[-1]
-            new_name = img_id.split('.')[0]
-            evidence['id'] = new_name
-            evidence['name'] = f'image::{tempdir}/{new_name}[align=center]'
-            img = Image.open(f'{tempdir}/{img_id}')
-            img.save(f'{tempdir}/{new_name}', 'png', optimize=True)
-            img.close()
+            try:
+                img_id = evidence['id'].split('/')[-1]
+                new_name = img_id.split('.')[0]
+                evidence['id'] = new_name
+                evidence['name'] = f'image::{tempdir}/{new_name}[align=center]'
+                img = Image.open(f'{tempdir}/{img_id}')
+                img.save(f'{tempdir}/{new_name}', 'png', optimize=True)
+                img.close()
+            except OSError as exc:
+                LOGGER.exception(
+                    exc,
+                    extra=dict(
+                        extra=dict(
+                            evidence_id=evidence['id'],
+                            finding_id=finding['findingId'],
+                            group_name=group_name,
+                        )
+                    )
+                )
 
 
 async def generate_pdf_file(
@@ -65,7 +81,7 @@ async def generate_pdf_file(
     with TemporaryDirectory() as tempdir:
         pdf_maker = CreatorPDF(lang, 'tech', tempdir)
         await download_evidences_for_pdf(findings_ord, tempdir)
-        convert_evidences_to_png(findings_ord, tempdir)
+        convert_evidences_to_png(findings_ord, tempdir, group_name)
         pdf_maker.tech(findings_ord, group_name, description, user_email)
     report_filename = await secure_pdf.create_full(
         user_email, pdf_maker.out_name, group_name
