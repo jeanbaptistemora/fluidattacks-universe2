9427ef0caff23788b9c17a8e8bf1d6c46e401036
Andres Aguirre <aaguirre@fluidattacks.com>
2021-03-23 16:49:40 -0500

    integrates\feat(back): #4014.17 domain toe lines by root
    - Added domain to get toe by root id and group name
    
    
    Former-commit-id: cb1056d72c429d9c9a0c0ff079416703166b0a51

--- integrates/back/packages/modules/toe/lines/domain.py
+++ integrates/back/packages/modules/toe/lines/domain.py
@@ -1,6 +1,19 @@
+# Standard libraries
+from typing import Tuple
+
 # Local libraries
 from toe.lines import dal as toe_lines_dal
 from dynamodb.types import GitRootToeLines
+
+
+async def get_by_root(
+    group_name: str,
+    root_id: str
+) -> Tuple[GitRootToeLines, ...]:
+    return await toe_lines_dal.get_by_root(
+        group_name=group_name,
+        root_id=root_id
+    )
 
 
 async def update(root_toe_lines: GitRootToeLines) -> None:
