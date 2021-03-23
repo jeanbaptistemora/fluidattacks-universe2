8acb3c397a1958d02912d2876e2acc8ef3fafa1d
Andres Aguirre <aaguirre@fluidattacks.com>
2021-03-23 16:49:24 -0500

    integrates\feat(back): #4014.16 dal toe lines by root
    - Added dal to get toe by root id and group name
    
    
    Former-commit-id: a50fa22993b4ad291adb34975521ba121f1ab55d

--- integrates/back/packages/modules/toe/lines/dal.py
+++ integrates/back/packages/modules/toe/lines/dal.py
@@ -1,6 +1,7 @@
 # Standard libraries
 import logging
 import logging.config
+from typing import Tuple
 
 # Third party libraries
 from botocore.exceptions import ClientError
@@ -16,6 +17,21 @@ from dynamodb.types import GitRootToeLines
 # Constants
 logging.config.dictConfig(LOGGING)
 LOGGER = logging.getLogger(__name__)
+
+
+async def get_by_root(
+    *,
+    group_name: str,
+    root_id: str
+) -> Tuple[GitRootToeLines, ...]:
+    try:
+        return await model.get_toe_lines_by_root(
+            group_name=group_name,
+            root_id=root_id
+        )
+    except ClientError as ex:
+        LOGGER.exception(ex, extra={'extra': locals()})
+        raise UnavailabilityError() from ex
 
 
 async def update(root_toe_lines: GitRootToeLines) -> None:
