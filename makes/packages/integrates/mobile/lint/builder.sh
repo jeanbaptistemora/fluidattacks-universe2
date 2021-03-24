55ebebcbd735ffdf8548a327d289336cf9e519e2
Jonathan Mesa <jmesa@fluidattacks.com>
2021-03-24 12:14:43 -0500

    integrates\refac(build): #2705.155 remove tslint support
    - Remove support for old tslint in integrates mobile
    - New eslint linter already supported
    
    
    Former-commit-id: a4ed9614fc46b8925172b2d7f0d90afe641aee46

--- makes/packages/integrates/mobile/lint/builder.sh
+++ makes/packages/integrates/mobile/lint/builder.sh
@@ -5,7 +5,6 @@ function main {
       copy "${envSrcIntegratesMobile}" "${out}" \
   &&  copy "${envSetupIntegratesMobileDevRuntime}/node_modules" "${out}/node_modules" \
   &&  pushd "${out}" \
-    &&  npm run lint:tslint \
     &&  lint_typescript "$(pwd)" "$(pwd)" \
   &&  popd \
   ||  return 1
