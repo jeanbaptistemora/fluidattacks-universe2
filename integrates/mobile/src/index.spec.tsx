b35362a8f2e9149045c1650543733236c41913b7
Jonathan Mesa <jmesa@fluidattacks.com>
2021-03-24 09:05:50 -0500

    integrates\refac(front): #2705.153 update app root
    - Lint app root test
    - No visual changes
    
    
    Former-commit-id: 8b419f4e8eed5a04d3ad9f8e44166b3b80f6eec1

--- integrates/mobile/src/index.spec.tsx
+++ integrates/mobile/src/index.spec.tsx
@@ -1,28 +1,29 @@
-import { shallow, ShallowWrapper } from "enzyme";
+import { App } from "./app";
 import React from "react";
-// tslint:disable-next-line: no-submodule-imports
+import type { ShallowWrapper } from "enzyme";
 import { act } from "react-dom/test-utils";
+import { shallow } from "enzyme";
 import { useColorScheme } from "react-native";
 
-import { App } from "./app";
-
 describe("App root", (): void => {
-  afterEach((): void => {
-    jest.clearAllMocks();
-  });
-
   it("should return a function", (): void => {
-    expect(typeof (App))
-      .toEqual("function");
+    expect.hasAssertions();
+    expect(typeof App).toStrictEqual("function");
   });
 
   it("should render", (): void => {
+    expect.hasAssertions();
+
     const wrapper: ShallowWrapper = shallow(<App />);
-    expect(wrapper)
-      .toHaveLength(1);
+
+    expect(wrapper).toHaveLength(1);
+
+    jest.clearAllMocks();
   });
 
   it("should change color scheme", (): void => {
+    expect.hasAssertions();
+
     jest.mock("react-native/Libraries/Utilities/useColorScheme");
 
     (useColorScheme as jest.Mock)
@@ -30,18 +31,21 @@ describe("App root", (): void => {
       .mockReturnValueOnce("dark");
 
     const wrapper: ShallowWrapper = shallow(<App />);
-    expect(wrapper)
-      .toHaveLength(1);
 
-    expect(wrapper
-      .find("StatusBar")
-      .prop("barStyle"))
-      .toEqual("dark-content");
+    expect(wrapper).toHaveLength(1);
 
-    act((): void => { wrapper.setProps({}); });
-    expect(wrapper
-      .find("StatusBar")
-      .prop("barStyle"))
-      .toEqual("light-content");
+    expect(wrapper.find("StatusBar").prop("barStyle")).toStrictEqual(
+      "dark-content"
+    );
+
+    act((): void => {
+      wrapper.setProps({});
+    });
+
+    expect(wrapper.find("StatusBar").prop("barStyle")).toStrictEqual(
+      "light-content"
+    );
+
+    jest.clearAllMocks();
   });
 });
