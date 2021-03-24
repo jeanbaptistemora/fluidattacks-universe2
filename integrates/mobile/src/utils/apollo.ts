e092470c68077e1f21fb181b113313e4d8830d43
Jonathan Mesa <jmesa@fluidattacks.com>
2021-03-24 15:10:10 -0500

    integrates\refac(front): #2705.158 update utils
    - Lint apollo provider file in mobile
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: 6d00c637db3ac6fd847456347f017bc24e24158d

--- integrates/mobile/src/utils/apollo.ts
+++ integrates/mobile/src/utils/apollo.ts
@@ -1,34 +1,34 @@
+import { Alert } from "react-native";
+import type { ErrorResponse } from "@apollo/client/link/error";
+import { LOGGER } from "./logger";
+import type React from "react";
+import { RetryLink } from "@apollo/client/link/retry";
+import _ from "lodash";
+import { getEnvironment } from "./environment";
+import { i18next } from "./translations/translate";
+import { logout } from "./socialAuth";
+import { setContext } from "@apollo/client/link/context";
+import { useHistory } from "react-router-native";
 import {
   ApolloClient,
   ApolloLink,
   ApolloProvider as BaseApolloProvider,
-  createHttpLink,
-  FetchResult,
   InMemoryCache,
+  Observable,
+  createHttpLink,
+} from "@apollo/client";
+import type { ExecutionResult, GraphQLError } from "graphql";
+import type {
+  FetchResult,
   NextLink,
   NormalizedCacheObject,
-  Observable,
   Operation,
   ServerError,
   ServerParseError,
 } from "@apollo/client";
-// tslint:disable-next-line: no-submodule-imports
-import { setContext } from "@apollo/client/link/context";
-// tslint:disable-next-line: no-submodule-imports
-import { ErrorResponse } from "@apollo/client/link/error";
-// tslint:disable-next-line: no-submodule-imports
-import { RetryLink } from "@apollo/client/link/retry";
-import * as SecureStore from "expo-secure-store";
-import { ExecutionResult, GraphQLError } from "graphql";
-import _ from "lodash";
-import React from "react";
-import { Alert } from "react-native";
-import { useHistory } from "react-router-native";
 
-import { getEnvironment } from "./environment";
-import { LOGGER } from "./logger";
-import { logout } from "./socialAuth";
-import { i18next } from "./translations/translate";
+import { createElement, useMemo } from "react";
+import { deleteItemAsync, getItemAsync } from "expo-secure-store";
 
 /**
  * Handled error attributes
@@ -39,12 +39,17 @@ interface IHandledErrorAttr {
   networkError?: Error | ServerError | ServerParseError;
   operation: Operation;
   response?: ExecutionResult;
-  skipForwarding?(): void;
+  skipForwarding?: () => void;
 }
 
-// It can return a void type according to apollo-link
-// tslint:disable-next-line:invalid-void
-type ErrorHandler = (error: IHandledErrorAttr) => Observable<FetchResult> | void;
+/*
+ * It can return a void type according to apollo-link
+ */
+// eslint-disable-next-line @typescript-eslint/no-type-alias
+type ErrorHandler = (
+  error: IHandledErrorAttr
+  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+) => Observable<FetchResult> | void;
 
 type History = ReturnType<typeof useHistory>;
 
@@ -56,124 +61,150 @@ const apiHost: string = getEnvironment().url;
  * @see https://github.com/apollographql/react-apollo/issues/1548
  * @see https://github.com/apollographql/apollo-link/issues/855
  */
-const onError: ((errorHandler: ErrorHandler) => ApolloLink) = (
-  errorHandler: ErrorHandler,
+const onError: (errorHandler: ErrorHandler) => ApolloLink = (
+  errorHandler: ErrorHandler
 ): ApolloLink =>
-  new ApolloLink((
-    operation: Operation,
-    forward: NextLink,
-  ): Observable<FetchResult> =>
-    new Observable((
-      observer: ZenObservable.SubscriptionObserver<FetchResult>,
-    ): (() => void) => {
-      let subscription: ZenObservable.Subscription | undefined;
+  new ApolloLink(
+    (operation: Operation, forward: NextLink): Observable<FetchResult> =>
+      new Observable(
+        (
+          observer: ZenObservable.SubscriptionObserver<FetchResult>
+        ): (() => void) => {
+          // eslint-disable-next-line fp/no-let, @typescript-eslint/init-declarations
+          let subscription: ZenObservable.Subscription | undefined;
 
-      try {
-        const operationObserver: Observable<FetchResult> = forward(operation);
-        let isForwarded: boolean = true;
-        const skipForwarding: () => void = (): void => {
-          isForwarded = false;
-        };
+          try {
+            const operationObserver: Observable<FetchResult> = forward(
+              operation
+            );
+            // eslint-disable-next-line fp/no-let
+            let isForwarded: boolean = true;
+            const skipForwarding: () => void = (): void => {
+              // eslint-disable-next-line fp/no-mutation
+              isForwarded = false;
+            };
 
-        subscription = operationObserver.subscribe({
-          complete: (): void => {
-            if (isForwarded) {
-              observer.complete.bind(observer)();
-            }
-          },
-          error: (networkError: ErrorResponse["networkError"]): void => {
+            // eslint-disable-next-line fp/no-mutation
+            subscription = operationObserver.subscribe({
+              complete: (): void => {
+                if (isForwarded) {
+                  observer.complete.bind(observer)();
+                }
+              },
+              error: (networkError: ErrorResponse["networkError"]): void => {
+                errorHandler({
+                  forward,
+                  networkError,
+                  operation,
+                });
+              },
+              next: (result: FetchResult): void => {
+                if (result.errors !== undefined) {
+                  errorHandler({
+                    forward,
+                    graphQLErrors: result.errors,
+                    operation,
+                    response: result,
+                    skipForwarding,
+                  });
+                }
+                if (isForwarded) {
+                  observer.next(result);
+                }
+              },
+            });
+          } catch (exception: unknown) {
             errorHandler({
               forward,
-              networkError,
+              networkError: exception as Error,
               operation,
             });
-          },
-          next: (result: FetchResult): void => {
-            if (result.errors !== undefined) {
-              errorHandler({
-                forward,
-                graphQLErrors: result.errors,
-                operation,
-                response: result,
-                skipForwarding,
-              });
-            }
-            if (isForwarded) {
-              observer.next(result);
-            }
-          },
-        });
-      } catch (exception) {
-        errorHandler({
-          forward,
-          networkError: exception as Error,
-          operation,
-        });
-      }
+          }
 
-      return (): void => {
-        if (subscription !== undefined) {
-          subscription.unsubscribe();
+          return (): void => {
+            if (subscription !== undefined) {
+              subscription.unsubscribe();
+            }
+          };
         }
-      };
-    }),
+      )
   );
 
 // Top-level error handling
-const errorLink: ((history: History) => ApolloLink) = (
-  history: History,
+const errorLink: (history: History) => ApolloLink = (
+  history: History
 ): ApolloLink =>
-  onError(({ graphQLErrors, networkError, response, skipForwarding }: IHandledErrorAttr): void => {
-    if (networkError !== undefined) {
-      const { statusCode } = networkError as { statusCode?: number };
+  onError(
+    ({
+      graphQLErrors,
+      networkError,
+      response,
+      skipForwarding,
+    }: IHandledErrorAttr): void => {
+      if (networkError !== undefined) {
+        const { statusCode } = networkError as { statusCode?: number };
 
-      switch (statusCode) {
-        case undefined:
-          Alert.alert(
-            i18next.t("common.networkError.title"),
-            i18next.t("common.networkError.msg"));
-          break;
-        default:
-          Alert.alert(
-            i18next.t("common.error.title"),
-            i18next.t("common.error.msg"));
-          LOGGER.warning("A network error occurred", { ...networkError });
-      }
-    } else {
-      if (graphQLErrors !== undefined) {
-        graphQLErrors.forEach(async (error: GraphQLError): Promise<void> => {
-          if (error.message === "Login required") {
-            if (response !== undefined) {
-              if (_.isFunction(skipForwarding)) {
-                skipForwarding();
-              }
-            }
-            await logout();
+        switch (statusCode) {
+          case undefined:
             Alert.alert(
-              i18next.t("common.sessionExpired.title"),
-              i18next.t("common.sessionExpired.msg"));
-            history.replace("/Login");
+              i18next.t("common.networkError.title"),
+              i18next.t("common.networkError.msg")
+            );
+            break;
+          default:
+            Alert.alert(
+              i18next.t("common.error.title"),
+              i18next.t("common.error.msg")
+            );
+            LOGGER.warning("A network error occurred", { ...networkError });
+        }
+      } else if (graphQLErrors !== undefined) {
+        graphQLErrors.forEach(
+          // eslint-disable-next-line @typescript-eslint/no-misused-promises
+          async (error: GraphQLError): Promise<void> => {
+            if (error.message === "Login required") {
+              if (response !== undefined) {
+                if (_.isFunction(skipForwarding)) {
+                  skipForwarding();
+                }
+              }
+              await logout();
+              Alert.alert(
+                i18next.t("common.sessionExpired.title"),
+                i18next.t("common.sessionExpired.msg")
+              );
+              history.replace("/Login");
+            }
           }
-        });
+        );
       }
     }
-  });
+  );
 
-const authLink: ApolloLink = setContext(async (): Promise<Record<string, unknown>> => {
-  let token: string;
-  try {
-    token = await SecureStore.getItemAsync("integrates_session") as string;
-  } catch (exception) {
-    token = "";
-    await SecureStore.deleteItemAsync("integrates_session");
+const authLink: ApolloLink = setContext(
+  async (): Promise<Record<string, unknown>> => {
+    try {
+      const token: string = (await getItemAsync(
+        "integrates_session"
+      )) as string;
+
+      return {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      };
+    } catch (exception: unknown) {
+      const token: string = "";
+      await deleteItemAsync("integrates_session");
+
+      return {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      };
+    }
   }
-
-  return {
-    headers: {
-      authorization: `Bearer ${token}`,
-    },
-  };
-});
+);
 
 const apiLink: ApolloLink = createHttpLink({
   uri: `${apiHost}/api`,
@@ -195,11 +226,11 @@ type ProviderProps = Omit<
   React.ComponentProps<typeof BaseApolloProvider>,
   "client"
 >;
-const apolloProvider: React.FC<ProviderProps> = (
-  props: ProviderProps,
+const ApolloProvider: React.FC<ProviderProps> = (
+  props: ProviderProps
 ): JSX.Element => {
   const history: History = useHistory();
-  const client: ApolloClient<NormalizedCacheObject> = React.useMemo(
+  const client: ApolloClient<NormalizedCacheObject> = useMemo(
     (): ApolloClient<NormalizedCacheObject> =>
       new ApolloClient({
         cache: new InMemoryCache(),
@@ -210,10 +241,10 @@ const apolloProvider: React.FC<ProviderProps> = (
           apiLink,
         ]),
       }),
-    [],
+    [history]
   );
 
-  return React.createElement(BaseApolloProvider, { client, ...props });
+  return createElement(BaseApolloProvider, { client, ...props });
 };
 
-export { apolloProvider as ApolloProvider };
+export { ApolloProvider };
