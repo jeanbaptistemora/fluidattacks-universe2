8fb18ffbd99a5d4d28e04c8c66d7609c40efac08
Daniel Murcia <dmurcia@fluidattacks.com>
2021-03-24 13:15:55 -0500

    observes\feat(back): #4195.48 paginator
    - new package paginator
    
    
    Former-commit-id: 65a222f19b087264bc15627aa2e9ba582b11ded0

---
+++ observes/common/paginator/paginator/__init__.py
@@ -0,0 +1,92 @@
+# Standard libraries
+import asyncio
+from asyncio.events import AbstractEventLoop
+from typing import (
+    AsyncGenerator,
+    Callable,
+    cast,
+    Iterator,
+    NamedTuple,
+    Optional,
+    Tuple,
+    TypeVar,
+)
+
+# Third party libraries
+from aioextensions import (
+    in_thread,
+    resolve,
+)
+
+# Local libraries
+
+
+class AllPages(NamedTuple):
+    pass
+
+
+class PageId(NamedTuple):
+    page: int
+    per_page: int
+
+
+class PageRange(NamedTuple):
+    page_range: range
+    per_page: int
+    pages: Callable[[], Iterator[PageId]]
+
+
+def new_page_range(
+    page_range: range,
+    per_page: int,
+) -> PageRange:
+    def next_page() -> Iterator[PageId]:
+        for p_num in page_range:
+            yield PageId(
+                page=p_num,
+                per_page=per_page
+            )
+    return PageRange(
+        page_range=page_range,
+        per_page=per_page,
+        pages=next_page
+    )
+
+
+Data = TypeVar('Data')
+
+
+def _iter_over_async(
+    ait: AsyncGenerator[Data, None], loop: AbstractEventLoop
+) -> Iterator[Data]:
+    ait = ait.__aiter__()
+
+    async def get_next() -> Tuple[bool, Optional[Data]]:
+        try:
+            obj: Data = await ait.__anext__()
+            return False, obj
+        except StopAsyncIteration:
+            return True, None
+
+    while True:
+        done, obj = loop.run_until_complete(get_next())
+        if done:
+            break
+        yield cast(Data, obj)
+
+
+def get_pages(
+    page_range: PageRange,
+    getter: Callable[[PageId], Data]
+) -> Iterator[Data]:
+
+    async def pages() -> AsyncGenerator[Data, None]:
+        jobs = map(
+            lambda page: in_thread(getter, page),
+            page_range.pages()
+        )
+        for item in resolve(jobs):
+            yield await item
+
+    loop = asyncio.get_event_loop()
+    return _iter_over_async(pages(), loop)
