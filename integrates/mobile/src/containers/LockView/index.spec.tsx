8f86cbaee4f931e6719f55211ba95072faef6f9d
Juan Chaves <jchaves@fluidattacks.com>
2021-03-25 15:42:05 -0500

    integrates\refac(front): #2705.168 update lockview
    - Linted test
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: f07508faec854899f7248b08014bb7c90cdcdc9d

--- integrates/mobile/src/containers/LockView/index.spec.tsx
+++ integrates/mobile/src/containers/LockView/index.spec.tsx
@@ -1,49 +1,50 @@
-import { mount, ReactWrapper } from "enzyme";
-import * as LocalAuthentication from "expo-local-authentication";
-import * as SecureStore from "expo-secure-store";
-import React from "react";
-// tslint:disable-next-line: no-submodule-imports
-import { act } from "react-dom/test-utils";
 import { I18nextProvider } from "react-i18next";
-import { Button, Provider as PaperProvider } from "react-native-paper";
+import { LockView } from ".";
 import { NativeRouter } from "react-router-native";
-import wait from "waait";
-
+import React from "react";
+import type { ReactWrapper } from "enzyme";
+import { act } from "react-dom/test-utils";
+import { authenticateAsync } from "expo-local-authentication";
+import { getItemAsync } from "expo-secure-store";
 import { i18next } from "../../utils/translations/translate";
-
-import { LockView } from "./index";
+import { mount } from "enzyme";
+import wait from "waait";
+import { Button, Provider as PaperProvider } from "react-native-paper";
 
 const mockHistoryReplace: jest.Mock = jest.fn();
 
-jest.mock("react-router-native", (): Record<string, unknown> => {
-  const mockedRouter: Record<
-  string,
-  () => Record<string, unknown>
-> = jest.requireActual("react-router-native");
+jest.mock(
+  "react-router-native",
+  (): Record<string, unknown> => {
+    const mockedRouter: Record<
+      string,
+      () => Record<string, unknown>
+    > = jest.requireActual("react-router-native");
 
-  return {
-    ...mockedRouter,
-    useHistory: (): Record<string, unknown> => ({
-      ...mockedRouter.useHistory(),
-      replace: mockHistoryReplace,
-    }),
-  };
-});
+    return {
+      ...mockedRouter,
+      useHistory: (): Record<string, unknown> => ({
+        ...mockedRouter.useHistory(),
+        replace: mockHistoryReplace,
+      }),
+    };
+  }
+);
 
 jest.mock("expo-secure-store");
 jest.mock("expo-local-authentication");
 
 describe("LockView", (): void => {
-  afterEach((): void => {
+  it("should return a function", (): void => {
+    expect.hasAssertions();
+
+    expect(typeof LockView).toStrictEqual("function");
+
     jest.clearAllMocks();
   });
 
-  it("should return a function", (): void => {
-    expect(typeof (LockView))
-      .toEqual("function");
-  });
-
   it("should render", (): void => {
+    expect.hasAssertions();
 
     const wrapper: ReactWrapper = mount(
       <PaperProvider>
@@ -52,19 +53,20 @@ describe("LockView", (): void => {
             <LockView />
           </NativeRouter>
         </I18nextProvider>
-      </PaperProvider>,
+      </PaperProvider>
     );
 
-    expect(wrapper)
-      .toHaveLength(1);
-    expect(wrapper.text())
-      .toContain("Unlock to continue");
+    expect(wrapper).toHaveLength(1);
+    expect(wrapper.text()).toContain("Unlock to continue");
+
+    jest.clearAllMocks();
   });
 
   it("should prompt for biometric unlock", async (): Promise<void> => {
-    (SecureStore.getItemAsync as jest.Mock)
-      .mockResolvedValueOnce(JSON.stringify({}));
-    (LocalAuthentication.authenticateAsync as jest.Mock).mockResolvedValue({
+    expect.hasAssertions();
+
+    (getItemAsync as jest.Mock).mockResolvedValueOnce(JSON.stringify({}));
+    (authenticateAsync as jest.Mock).mockResolvedValue({
       success: true,
     });
 
@@ -75,25 +77,31 @@ describe("LockView", (): void => {
             <LockView />
           </NativeRouter>
         </I18nextProvider>
-      </PaperProvider>,
+      </PaperProvider>
     );
 
-    await act(async (): Promise<void> => { await wait(0); wrapper.update(); });
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
 
-    expect(wrapper)
-      .toHaveLength(1);
+    expect(wrapper).toHaveLength(1);
+    // ESLint suggested the change from toBeCalled to the current method
+    expect(authenticateAsync).toHaveBeenCalledWith();
+    expect(mockHistoryReplace).toHaveBeenCalledWith("/Dashboard", {});
 
-    expect(LocalAuthentication.authenticateAsync)
-      .toHaveBeenCalled();
-    expect(mockHistoryReplace)
-      .toHaveBeenCalledWith("/Dashboard", {});
+    jest.clearAllMocks();
   });
 
   it("should handle unsuccessful biometric unlock", async (): Promise<void> => {
-    (SecureStore.getItemAsync as jest.Mock)
+    expect.hasAssertions();
+
+    (getItemAsync as jest.Mock)
       .mockResolvedValueOnce("some.session.token")
       .mockResolvedValueOnce(JSON.stringify({}));
-    (LocalAuthentication.authenticateAsync as jest.Mock).mockResolvedValue({
+    (authenticateAsync as jest.Mock).mockResolvedValue({
       success: false,
     });
 
@@ -104,19 +112,21 @@ describe("LockView", (): void => {
             <LockView />
           </NativeRouter>
         </I18nextProvider>
-      </PaperProvider>,
+      </PaperProvider>
     );
 
-    await act(async (): Promise<void> => { await wait(0); wrapper.update(); });
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
 
-    expect(wrapper)
-      .toHaveLength(1);
-    expect(mockHistoryReplace)
-      .not
-      .toHaveBeenCalled();
-    expect(wrapper.find(Button))
-      .toHaveLength(1);
-    expect(wrapper.text())
-      .toContain("Authenticate");
+    expect(wrapper).toHaveLength(1);
+    expect(mockHistoryReplace).not.toHaveBeenCalled();
+    expect(wrapper.find(Button)).toHaveLength(1);
+    expect(wrapper.text()).toContain("Authenticate");
+
+    jest.clearAllMocks();
   });
 });
