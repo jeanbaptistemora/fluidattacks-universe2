4717cf24038a002e7d27d6d74d7884276099aca1
Juan Chaves <jchaves@fluidattacks.com>
2021-03-25 14:17:20 -0500

    integrates\refac(front): #2705.166 update dashboardview
    - Linted test
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: bf4d20030923ccb41a7a74d1250136fe6e98c23e

--- integrates/mobile/src/containers/DashboardView/index.spec.tsx
+++ integrates/mobile/src/containers/DashboardView/index.spec.tsx
@@ -1,51 +1,54 @@
-// tslint:disable-next-line: no-submodule-imports
-import { MockedProvider, MockedResponse } from "@apollo/client/testing";
-import { mount, ReactWrapper } from "enzyme";
+import { DashboardView } from ".";
 import { GraphQLError } from "graphql";
-import React from "react";
-// tslint:disable-next-line: no-submodule-imports
-import { act } from "react-dom/test-utils";
 import { I18nextProvider } from "react-i18next";
-import { Alert, AppState, AppStateEvent, AppStateStatus } from "react-native";
-import { Provider as PaperProvider, Text } from "react-native-paper";
 import { MemoryRouter } from "react-router-native";
-import wait from "waait";
-
-import { i18next } from "../../utils/translations/translate";
-
-import { DashboardView } from "./index";
+import { MockedProvider } from "@apollo/client/testing";
+import type { MockedResponse } from "@apollo/client/testing";
 import { ORGS_QUERY } from "./queries";
+import { Provider as PaperProvider } from "react-native-paper";
+import React from "react";
+import type { ReactWrapper } from "enzyme";
+import type { Text } from "react-native-paper";
+import { act } from "react-dom/test-utils";
+import { i18next } from "../../utils/translations/translate";
+import { mount } from "enzyme";
+import wait from "waait";
+import { Alert, AppState } from "react-native";
+import type { AppStateEvent, AppStateStatus } from "react-native";
 
 const mockHistoryReplace: jest.Mock = jest.fn();
 
-jest.mock("react-router-native", (): Record<string, unknown> => {
-  const mockedRouter: Record<
-  string,
-  () => Record<string, unknown>
-> = jest.requireActual("react-router-native");
+jest.mock(
+  "react-router-native",
+  (): Record<string, unknown> => {
+    const mockedRouter: Record<
+      string,
+      () => Record<string, unknown>
+    > = jest.requireActual("react-router-native");
 
-  return {
-    ...mockedRouter,
-    useHistory: (): Record<string, unknown> => ({
-      ...mockedRouter.useHistory(),
-      replace: mockHistoryReplace,
-    }),
-  };
-});
+    return {
+      ...mockedRouter,
+      useHistory: (): Record<string, unknown> => ({
+        ...mockedRouter.useHistory(),
+        replace: mockHistoryReplace,
+      }),
+    };
+  }
+);
 
 jest.mock("../../utils/socialAuth");
 
 describe("DashboardView", (): void => {
-  afterEach((): void => {
+  it("should return a function", (): void => {
+    expect.hasAssertions();
+
+    expect(typeof DashboardView).toStrictEqual("function");
+
     jest.clearAllMocks();
   });
 
-  it("should return a function", (): void => {
-    expect(typeof (DashboardView))
-      .toEqual("function");
-  });
-
   it("should render", async (): Promise<void> => {
+    expect.hasAssertions();
 
     const projectMock: Readonly<MockedResponse> = {
       request: {
@@ -78,27 +81,35 @@ describe("DashboardView", (): void => {
     const wrapper: ReactWrapper = mount(
       <PaperProvider>
         <I18nextProvider i18n={i18next}>
-          <MemoryRouter initialEntries={[{ pathname: "/Dashboard", state: { user: { fullName: "Test" } } }]}>
-            <MockedProvider mocks={[projectMock]} addTypename={false}>
+          <MemoryRouter
+            initialEntries={[
+              { pathname: "/Dashboard", state: { user: { fullName: "Test" } } },
+            ]}
+          >
+            <MockedProvider addTypename={false} mocks={[projectMock]}>
               <DashboardView />
             </MockedProvider>
           </MemoryRouter>
         </I18nextProvider>
-      </PaperProvider>,
+      </PaperProvider>
     );
-    await act(async (): Promise<void> => { await wait(0); wrapper.update(); });
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
 
-    expect(wrapper)
-      .toHaveLength(1);
-    expect(wrapper.text())
-      .toContain("58.3%");
-    expect(wrapper.text())
-      .toContain("0%Compared");
-    expect(wrapper.text())
-      .toContain("of 12 found in 1 system");
+    expect(wrapper).toHaveLength(1);
+    expect(wrapper.text()).toContain("58.3%");
+    expect(wrapper.text()).toContain("0%Compared");
+    expect(wrapper.text()).toContain("of 12 found in 1 system");
+
+    jest.clearAllMocks();
   });
 
   it("should render empty", async (): Promise<void> => {
+    expect.hasAssertions();
 
     const emptyMock: Readonly<MockedResponse> = {
       request: {
@@ -116,25 +127,35 @@ describe("DashboardView", (): void => {
     const wrapper: ReactWrapper = mount(
       <PaperProvider>
         <I18nextProvider i18n={i18next}>
-          <MemoryRouter initialEntries={[{ pathname: "/Dashboard", state: { user: { fullName: "Test" } } }]}>
-            <MockedProvider mocks={[emptyMock]} addTypename={false}>
+          <MemoryRouter
+            initialEntries={[
+              { pathname: "/Dashboard", state: { user: { fullName: "Test" } } },
+            ]}
+          >
+            <MockedProvider addTypename={false} mocks={[emptyMock]}>
               <DashboardView />
             </MockedProvider>
           </MemoryRouter>
         </I18nextProvider>
-      </PaperProvider>,
+      </PaperProvider>
     );
-    await act(async (): Promise<void> => { await wait(0); wrapper.update(); });
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
 
-    expect(wrapper)
-      .toHaveLength(1);
-    expect(wrapper.text())
-      .toContain("0%");
-    expect(wrapper.text())
-      .toContain("of 0 found in 0 systems");
+    expect(wrapper).toHaveLength(1);
+    expect(wrapper.text()).toContain("0%");
+    expect(wrapper.text()).toContain("of 0 found in 0 systems");
+
+    jest.clearAllMocks();
   });
 
   it("should handle errors", async (): Promise<void> => {
+    expect.hasAssertions();
+
     jest.mock("react-native/Libraries/Alert/Alert");
 
     const errorMock: Readonly<MockedResponse> = {
@@ -142,32 +163,42 @@ describe("DashboardView", (): void => {
         query: ORGS_QUERY,
       },
       result: {
-        errors: [
-          new GraphQLError("Unexpected error"),
-        ],
+        errors: [new GraphQLError("Unexpected error")],
       },
     };
 
     const wrapper: ReactWrapper = mount(
       <PaperProvider>
         <I18nextProvider i18n={i18next}>
-          <MemoryRouter initialEntries={[{ pathname: "/Dashboard", state: { user: { fullName: "Test" } } }]}>
-            <MockedProvider mocks={[errorMock]} addTypename={false}>
+          <MemoryRouter
+            initialEntries={[
+              { pathname: "/Dashboard", state: { user: { fullName: "Test" } } },
+            ]}
+          >
+            <MockedProvider addTypename={false} mocks={[errorMock]}>
               <DashboardView />
             </MockedProvider>
           </MemoryRouter>
         </I18nextProvider>
-      </PaperProvider>,
+      </PaperProvider>
     );
-    await act(async (): Promise<void> => { await wait(0); wrapper.update(); });
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
 
-    expect(wrapper)
-      .toHaveLength(1);
-    expect(Alert.alert)
-      .toHaveBeenCalled();
+    expect(wrapper).toHaveLength(1);
+    // eslint-disable-next-line jest/prefer-called-with
+    expect(Alert.alert).toHaveBeenCalled();
+
+    jest.clearAllMocks();
   });
 
   it("should exclude orgs without analytics", async (): Promise<void> => {
+    expect.hasAssertions();
+
     jest.mock("react-native/Libraries/Alert/Alert");
 
     const errorMock: Readonly<MockedResponse> = {
@@ -193,68 +224,79 @@ describe("DashboardView", (): void => {
                 name: "okada",
               },
               {
-                // tslint:disable-next-line: no-null-keyword
                 analytics: null,
                 name: "testorg2",
               },
             ],
           },
         },
-        errors: [
-          new GraphQLError("Exception - Document not found"),
-        ],
+        errors: [new GraphQLError("Exception - Document not found")],
       },
     };
 
     const wrapper: ReactWrapper = mount(
       <PaperProvider>
         <I18nextProvider i18n={i18next}>
-          <MemoryRouter initialEntries={[{ pathname: "/Dashboard", state: { user: { fullName: "Test" } } }]}>
-            <MockedProvider mocks={[errorMock]} addTypename={false}>
+          <MemoryRouter
+            initialEntries={[
+              { pathname: "/Dashboard", state: { user: { fullName: "Test" } } },
+            ]}
+          >
+            <MockedProvider addTypename={false} mocks={[errorMock]}>
               <DashboardView />
             </MockedProvider>
           </MemoryRouter>
         </I18nextProvider>
-      </PaperProvider>,
+      </PaperProvider>
     );
-    await act(async (): Promise<void> => { await wait(0); wrapper.update(); });
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
 
-    expect(wrapper)
-      .toHaveLength(1);
-    expect(wrapper.text())
-      .toContain("58.3%");
-    expect(wrapper.text())
-      .toContain("of 12 found in 1 system");
-    expect(Alert.alert)
-      .not
-      .toHaveBeenCalled();
+    expect(wrapper).toHaveLength(1);
+    expect(wrapper.text()).toContain("58.3%");
+    expect(wrapper.text()).toContain("of 12 found in 1 system");
+    expect(Alert.alert).not.toHaveBeenCalled();
+
+    jest.clearAllMocks();
   });
 
   it("should refresh on resume", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    // Needed for the test to succesfully run
+    // eslint-disable-next-line fp/no-let
     let stateListener: (
-      state: AppStateStatus,
-    ) => Promise<void> = async (): Promise<void> => undefined;
+      state: AppStateStatus
+    ) => Promise<void> = async (): Promise<void> => wait(0);
 
     jest.mock(
       "react-native",
-      (): Record<string, {}> => {
+      (): Record<string, unknown> => {
         const mockedRN: Record<string, unknown> = jest.requireActual(
-          "react-native",
+          "react-native"
         );
 
         return {
           ...mockedRN,
           AppState: { addEventListener: jest.fn() },
         };
-      },
+      }
     );
 
-    (AppState.addEventListener as jest.Mock).mockImplementation((
-      _: AppStateEvent,
-      listener: (state: AppStateStatus) => Promise<void>,
-    ): void => {
-      stateListener = listener;
-    });
+    (AppState.addEventListener as jest.Mock).mockImplementation(
+      (
+        _: AppStateEvent,
+        listener: (state: AppStateStatus) => Promise<void>
+      ): void => {
+        // Needed for the test to succesfully run
+        // eslint-disable-next-line fp/no-mutation
+        stateListener = listener;
+      }
+    );
 
     const projectMock: Readonly<MockedResponse> = {
       request: {
@@ -338,72 +380,85 @@ describe("DashboardView", (): void => {
             ]}
           >
             <MockedProvider
-              mocks={[projectMock, newProjectMock]}
               addTypename={false}
+              mocks={[projectMock, newProjectMock]}
             >
               <DashboardView />
             </MockedProvider>
           </MemoryRouter>
         </I18nextProvider>
-      </PaperProvider>,
+      </PaperProvider>
     );
-    await act(async (): Promise<void> => { await wait(0); wrapper.update(); });
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
 
-    expect(wrapper)
-      .toHaveLength(1);
-    expect(wrapper.text())
-      .toContain("58.3%");
-    expect(wrapper.text())
-      .toContain("-8.3%Compared");
-    expect(wrapper.text())
-      .toContain("of 12 found in 1 system");
+    expect(wrapper).toHaveLength(1);
+    expect(wrapper.text()).toContain("58.3%");
+    expect(wrapper.text()).toContain("-8.3%Compared");
+    expect(wrapper.text()).toContain("of 12 found in 1 system");
 
     await stateListener("background");
     await stateListener("active");
-    await act(async (): Promise<void> => { await wait(0); wrapper.update(); });
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
 
-    expect(wrapper.text())
-      .toContain("91.7%");
-    expect(wrapper.text())
-      .toContain("+25%Compared");
-    expect(wrapper.text())
-      .toContain("of 12 found in 1 system");
-    expect(wrapper.text())
-      .toContain("100%");
-    expect(wrapper.text())
-      .toContain("+37.5%Compared");
-    expect(wrapper.text())
-      .toContain("of 8 found in 2 systems");
+    expect(wrapper.text()).toContain("91.7%");
+    expect(wrapper.text()).toContain("+25%Compared");
+    expect(wrapper.text()).toContain("of 12 found in 1 system");
+    expect(wrapper.text()).toContain("100%");
+    expect(wrapper.text()).toContain("+37.5%Compared");
+    expect(wrapper.text()).toContain("of 8 found in 2 systems");
 
     wrapper.unmount();
+    jest.clearAllMocks();
   });
 
   it("should perform logout", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const wrapper: ReactWrapper = mount(
       <PaperProvider>
         <I18nextProvider i18n={i18next}>
-          <MemoryRouter initialEntries={[{ pathname: "/Dashboard", state: { user: { fullName: "Test" } } }]}>
+          <MemoryRouter
+            initialEntries={[
+              { pathname: "/Dashboard", state: { user: { fullName: "Test" } } },
+            ]}
+          >
             <MockedProvider>
               <DashboardView />
             </MockedProvider>
           </MemoryRouter>
         </I18nextProvider>
-      </PaperProvider>,
+      </PaperProvider>
     );
-    await act(async (): Promise<void> => { await wait(0); wrapper.update(); });
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
 
-    expect(wrapper)
-      .toHaveLength(1);
+    expect(wrapper).toHaveLength(1);
 
-    const logoutBtn: ReactWrapper<React.ComponentProps<typeof Text>> = wrapper
-      .find({ children: "Logout" })
-      .at(0);
+    const logoutBtn: ReactWrapper<
+      React.ComponentProps<typeof Text>
+    > = wrapper.find({ children: "Logout" }).at(0);
 
-    expect(logoutBtn)
-      .toHaveLength(1);
+    expect(logoutBtn).toHaveLength(1);
 
     await (logoutBtn.invoke("onPress") as () => Promise<void>)();
-    expect(mockHistoryReplace)
-      .toHaveBeenCalled();
+
+    // eslint-disable-next-line jest/prefer-called-with
+    expect(mockHistoryReplace).toHaveBeenCalled();
+
+    jest.clearAllMocks();
   });
 });
