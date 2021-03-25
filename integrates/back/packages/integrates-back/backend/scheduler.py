cb066116a144bac7e7c6c912cfda17da0ec70579
Andres Aguirre <aaguirre@fluidattacks.com>
2021-03-25 14:22:42 -0500

    integrates\feat(back): #4014.27 get toe lines from cvs
    - Added a function that maps the lines cvs into root toe lines items
    
    
    Former-commit-id: cf61b01cb2a2b24880573bf59bf10fb9bca66955

--- integrates/back/packages/integrates-back/backend/scheduler.py
+++ integrates/back/packages/integrates-back/backend/scheduler.py
@@ -2,6 +2,7 @@
 # pylint: disable=too-many-lines
 
 # Standard libraries
+import csv
 import logging
 import logging.config
 from itertools import chain
@@ -52,10 +53,17 @@ from backend.typing import (
     Historic as HistoricType,
     MailContent as MailContentType,
     Project as ProjectType,
+    Root,
 )
 from batch import dal as batch_dal
+from dynamodb.types import (
+    GitRootToeLines,
+)
 from events import domain as events_domain
-from newutils import datetime as datetime_utils
+from newutils import (
+    datetime as datetime_utils,
+    roots as roots_utils,
+)
 from newutils.groups import (
     has_integrates_services,
 )
@@ -1294,3 +1302,56 @@ async def delete_obsolete_groups() -> None:
         _set_group_pending_deletion_dates(loaders, obsolete_groups),
         _delete_groups(loaders, obsolete_groups)
     ])
+
+
+def _format_date(date_str: str) -> str:
+    date = datetime_utils.get_from_str(date_str, date_format='%Y-%m-%d')
+    formated_date_str = datetime_utils.get_as_str(date)
+
+    return formated_date_str
+
+
+def _get_group_toe_lines_from_cvs(
+    lines_csv_path: str,
+    group_name: str,
+    group_roots: Tuple[Root, ...]
+) -> Tuple[GitRootToeLines, ...]:
+    default_date = datetime_utils.DEFAULT_STR
+    lines_csv_fields: List[Tuple[str, Callable, Any, str]] = [
+        # field_name, field_formater, field_default_value, cvs_field_name,
+        ('filename', str, '', 'filename'),
+        ('comments', str, '', 'comments'),
+        ('modified_commit', str, '', 'modified-commit'),
+        ('loc', int, 0, 'loc'),
+        ('tested_lines', int, 0, 'tested-lines'),
+        ('modified_date', _format_date, default_date, 'modified-date'),
+        ('tested_date', _format_date, default_date, 'tested-date'),
+    ]
+    group_toe_lines = list()
+    with open(lines_csv_path) as csv_file:
+        for row in csv.DictReader(csv_file):
+            new_toe_lines = dict()
+            for (
+                field_name,
+                formater,
+                default_value,
+                cvs_field_name
+            ) in lines_csv_fields:
+                cvs_field_value = row.get(cvs_field_name)
+                try:
+                    new_toe_lines[field_name] = (
+                        formater(cvs_field_value)
+                        if cvs_field_value
+                        else default_value
+                    )
+                except ValueError:
+                    new_toe_lines[field_name] = default_value
+
+            new_toe_lines['root_id'] = roots_utils.get_root_id_by_filename(
+                row['filename'],
+                group_roots
+            )
+            new_toe_lines['group_name'] = group_name
+            group_toe_lines.append(GitRootToeLines(**new_toe_lines))
+
+    return tuple(group_toe_lines)
