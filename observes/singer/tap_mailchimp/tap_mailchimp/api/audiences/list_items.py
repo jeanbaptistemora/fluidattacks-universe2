523bbae2a6deff10c5c3319f505aa685e92a4a0a
Daniel Murcia <dmurcia@fluidattacks.com>
2021-03-25 12:21:10 -0500

    observes\refac(back): #4195.57 move list items
    - move list_items to common
    
    
    Former-commit-id: 183f5533883afcb3cdee56854dc1d9e216fb16c4

--- observes/singer/tap_mailchimp/tap_mailchimp/api/audiences/list_items.py
+++ observes/singer/tap_mailchimp/tap_mailchimp/api/audiences/list_items.py
@@ -1,33 +1,17 @@
 # Standard libraries
-import math
 from functools import (
     partial,
 )
-from itertools import (
-    chain,
-)
 from typing import (
-    Callable,
     Dict,
     Iterator,
-    TypeVar,
 )
 
 # Third party libraries
 
 # Local libraries
-import paginator
-from paginator import (
-    PageId,
-)
 from tap_mailchimp.api.common import (
-    api_data,
-)
-from tap_mailchimp.api.common.api_data import (
-    ApiData,
-)
-from tap_mailchimp.common.objs import (
-    JSON,
+    list_items,
 )
 from tap_mailchimp.api.common.raw import (
     AbsReportId,
@@ -39,45 +23,10 @@ from tap_mailchimp.api.common.raw import (
 )
 
 
-MAX_PER_PAGE = 1000
-
-
-class NoneTotal(Exception):
-    pass
-
-
-SomeId = TypeVar('SomeId')
-
-
-def _list_items(
-    raw_list: Callable[[PageId], JSON],
-    items_list_key: str,
-    id_builder: Callable[[Dict[str, str]], SomeId]
-) -> Iterator[SomeId]:
-    getter: Callable[[PageId], ApiData] = (
-        lambda page: api_data.create_api_data(
-            raw_list(page)
-        )
-    )
-    test_page = getter(PageId(page=0, per_page=1))
-    chunk_size = MAX_PER_PAGE
-    if test_page.total_items is None:
-        raise NoneTotal()
-    total_pages = math.ceil(test_page.total_items / chunk_size)
-    pages = paginator.new_page_range(range(total_pages), chunk_size)
-    results: Iterator[ApiData] = paginator.get_pages(pages, getter)
-
-    def extract_aud_ids(a_data: ApiData) -> Iterator[SomeId]:
-        audiences_data = a_data.data[items_list_key]
-        return iter(map(id_builder, audiences_data))
-
-    return chain.from_iterable(map(extract_aud_ids, results))
-
-
 def list_audiences(
     raw_source: RawSource,
 ) -> Iterator[AudienceId]:
-    return _list_items(
+    return list_items(
         raw_source.list_audiences,
         'lists',
         lambda item: AudienceId(item['id'])
@@ -94,7 +43,7 @@ def list_abuse_reports(
             str_id=item['id']
         )
 
-    return _list_items(
+    return list_items(
         partial(raw_source.list_abuse_reports, audience),
         'abuse_reports',
         id_builder
@@ -111,7 +60,7 @@ def list_members(
             str_id=item['id']
         )
 
-    return _list_items(
+    return list_items(
         partial(raw_source.list_members, audience),
         'members',
         id_builder
@@ -128,7 +77,7 @@ def list_growth_hist(
             str_id=item['month']
         )
 
-    return _list_items(
+    return list_items(
         partial(raw_source.list_growth_hist, audience),
         'history',
         id_builder
@@ -145,7 +94,7 @@ def list_interest_catg(
             str_id=item['id']
         )
 
-    return _list_items(
+    return list_items(
         partial(raw_source.list_interest_catg, audience),
         'categories',
         id_builder
--- observes/singer/tap_mailchimp/tap_mailchimp/api/common/__init__.py
+++ observes/singer/tap_mailchimp/tap_mailchimp/api/common/__init__.py
@@ -1,15 +1,41 @@
 # Standard libraries
+import math
 import logging
+from itertools import (
+    chain,
+)
 from typing import (
-    Optional,
+    Callable,
+    Dict,
+    Iterator, Optional,
+    TypeVar,
 )
 
 # Third party libraries
 
 # Local libraries
+import paginator
+from paginator import (
+    PageId,
+)
+from tap_mailchimp.api.common import (
+    api_data,
+)
+from tap_mailchimp.api.common.api_data import (
+    ApiData,
+)
+from tap_mailchimp.common.objs import (
+    JSON,
+)
 
 
 LOG = logging.getLogger(__name__)
+MAX_PER_PAGE = 1000
+SomeId = TypeVar('SomeId')
+
+
+class NoneTotal(Exception):
+    pass
 
 
 def list_items_alert(ref: str, total: Optional[int]) -> None:
@@ -22,3 +48,28 @@ def list_items_alert(ref: str, total: Optional[int]) -> None:
             ref,
             total
         )
+
+
+def list_items(
+    raw_list: Callable[[PageId], JSON],
+    items_list_key: str,
+    id_builder: Callable[[Dict[str, str]], SomeId]
+) -> Iterator[SomeId]:
+    getter: Callable[[PageId], ApiData] = (
+        lambda page: api_data.create_api_data(
+            raw_list(page)
+        )
+    )
+    test_page = getter(PageId(page=0, per_page=1))
+    chunk_size = MAX_PER_PAGE
+    if test_page.total_items is None:
+        raise NoneTotal()
+    total_pages = math.ceil(test_page.total_items / chunk_size)
+    pages = paginator.new_page_range(range(total_pages), chunk_size)
+    results: Iterator[ApiData] = paginator.get_pages(pages, getter)
+
+    def extract_aud_ids(a_data: ApiData) -> Iterator[SomeId]:
+        audiences_data = a_data.data[items_list_key]
+        return iter(map(id_builder, audiences_data))
+
+    return chain.from_iterable(map(extract_aud_ids, results))
