adf1230f256f3f27bcad9c024298a42d9ccf6c26
Jonathan Mesa <jmesa@fluidattacks.com>
2021-03-26 13:17:02 -0500

    integrates\refac(front): #2705.172 update welcomeview
    - Lint welcomeview test
    - No visual changes
    - no-deltas-test
    
    
    Former-commit-id: aaa466c454a3e9a7525cba7abd2356d8072e6c8e

--- integrates/mobile/src/containers/WelcomeView/index.spec.tsx
+++ integrates/mobile/src/containers/WelcomeView/index.spec.tsx
@@ -1,71 +1,80 @@
-// tslint:disable-next-line: no-submodule-imports
-import { MockedProvider, MockedResponse } from "@apollo/client/testing";
-import { mount, ReactWrapper } from "enzyme";
 import { GraphQLError } from "graphql";
-import React from "react";
-// tslint:disable-next-line: no-submodule-imports
-import { act } from "react-dom/test-utils";
 import { I18nextProvider } from "react-i18next";
-import { Provider as PaperProvider } from "react-native-paper";
 import { MemoryRouter } from "react-router-native";
-import wait from "waait";
-
-import { i18next } from "../../utils/translations/translate";
-
-import { WelcomeView } from "./index";
+import { MockedProvider } from "@apollo/client/testing";
+import type { MockedResponse } from "@apollo/client/testing";
+import { Provider as PaperProvider } from "react-native-paper";
+import React from "react";
+import type { ReactWrapper } from "enzyme";
 import { SIGN_IN_MUTATION } from "./queries";
+import { WelcomeView } from ".";
+import { act } from "react-dom/test-utils";
+import { i18next } from "../../utils/translations/translate";
+import { mount } from "enzyme";
+import wait from "waait";
 
 const mockHistoryReplace: jest.Mock = jest.fn();
 
-jest.mock("react-router-native", (): Record<string, unknown> => {
-  const mockedRouter: Record<
-  string,
-  () => Record<string, unknown>
-> = jest.requireActual("react-router-native");
+jest.mock(
+  "react-router-native",
+  (): Record<string, unknown> => {
+    const mockedRouter: Record<
+      string,
+      () => Record<string, unknown>
+    > = jest.requireActual("react-router-native");
 
-  return {
-    ...mockedRouter,
-    useHistory: (): Record<string, unknown> => ({
-      ...mockedRouter.useHistory(),
-      replace: mockHistoryReplace,
-    }),
-  };
-});
+    return {
+      ...mockedRouter,
+      useHistory: (): Record<string, unknown> => ({
+        ...mockedRouter.useHistory(),
+        replace: mockHistoryReplace,
+      }),
+    };
+  }
+);
 
 describe("WelcomeView", (): void => {
-  afterEach((): void => {
-    jest.clearAllMocks();
-  });
-
   it("should return a function", (): void => {
-    expect(typeof (WelcomeView))
-      .toEqual("function");
+    expect.hasAssertions();
+    expect(typeof WelcomeView).toStrictEqual("function");
   });
 
   it("should render", async (): Promise<void> => {
+    expect.hasAssertions();
+
     const wrapper: ReactWrapper = mount(
       <PaperProvider>
         <I18nextProvider i18n={i18next}>
-          <MemoryRouter initialEntries={[{
-            pathname: "/Welcome",
-            state: { user: { firstName: "John", fullName: "John Doe" } },
-          }]}>
+          <MemoryRouter
+            initialEntries={[
+              {
+                pathname: "/Welcome",
+                state: { user: { firstName: "John", fullName: "John Doe" } },
+              },
+            ]}
+          >
             <MockedProvider addTypename={false}>
               <WelcomeView />
             </MockedProvider>
           </MemoryRouter>
         </I18nextProvider>
-      </PaperProvider>,
+      </PaperProvider>
     );
-    await act(async (): Promise<void> => { await wait(0); wrapper.update(); });
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
 
-    expect(wrapper)
-      .toHaveLength(1);
-    expect(wrapper.text())
-      .toContain("Hello John!");
+    expect(wrapper).toHaveLength(1);
+    expect(wrapper.text()).toContain("Hello John!");
+
+    jest.clearAllMocks();
   });
 
   it("should auth against the API", async (): Promise<void> => {
+    expect.hasAssertions();
 
     const signInMock: Readonly<MockedResponse> = {
       request: {
@@ -84,75 +93,93 @@ describe("WelcomeView", (): void => {
     const wrapper: ReactWrapper = mount(
       <PaperProvider>
         <I18nextProvider i18n={i18next}>
-          <MemoryRouter initialEntries={[{
-            pathname: "/Welcome",
-            state: {
-              user: {
-                email: "test@fluidattacks.com",
-                firstName: "John",
-                fullName: "John Doe",
-                lastName: "Doe",
+          <MemoryRouter
+            initialEntries={[
+              {
+                pathname: "/Welcome",
+                state: {
+                  user: {
+                    email: "test@fluidattacks.com",
+                    firstName: "John",
+                    fullName: "John Doe",
+                    lastName: "Doe",
+                  },
+                },
               },
-            },
-          }]}>
-            <MockedProvider mocks={[signInMock]} addTypename={false}>
+            ]}
+          >
+            <MockedProvider addTypename={false} mocks={[signInMock]}>
               <WelcomeView />
             </MockedProvider>
           </MemoryRouter>
         </I18nextProvider>
-      </PaperProvider>,
+      </PaperProvider>
     );
-    await act(async (): Promise<void> => { await wait(0); wrapper.update(); });
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
 
-    expect(wrapper)
-      .toHaveLength(1);
-    expect(mockHistoryReplace)
-      .toHaveBeenCalledWith("/Dashboard", {
-        user: {
-          email: "test@fluidattacks.com",
-          firstName: "John",
-          fullName: "John Doe",
-          lastName: "Doe",
-        },
-      });
+    expect(wrapper).toHaveLength(1);
+    expect(mockHistoryReplace).toHaveBeenCalledWith("/Dashboard", {
+      user: {
+        email: "test@fluidattacks.com",
+        firstName: "John",
+        fullName: "John Doe",
+        lastName: "Doe",
+      },
+    });
+
+    jest.clearAllMocks();
   });
 
   it("should handle errors", async (): Promise<void> => {
+    expect.hasAssertions();
 
     const signInMock: Readonly<MockedResponse> = {
       request: {
         query: SIGN_IN_MUTATION,
       },
       result: {
-        errors: [
-          new GraphQLError("Unexpected error"),
-        ],
+        errors: [new GraphQLError("Unexpected error")],
       },
     };
 
     const wrapper: ReactWrapper = mount(
       <PaperProvider>
         <I18nextProvider i18n={i18next}>
-          <MemoryRouter initialEntries={[{
-            pathname: "/Welcome",
-            state: { user: { firstName: "John", fullName: "John Doe" } },
-          }]}>
-            <MockedProvider mocks={[signInMock]} addTypename={false}>
+          <MemoryRouter
+            initialEntries={[
+              {
+                pathname: "/Welcome",
+                state: { user: { firstName: "John", fullName: "John Doe" } },
+              },
+            ]}
+          >
+            <MockedProvider addTypename={false} mocks={[signInMock]}>
               <WelcomeView />
             </MockedProvider>
           </MemoryRouter>
         </I18nextProvider>
-      </PaperProvider>,
+      </PaperProvider>
     );
-    await act(async (): Promise<void> => { await wait(0); wrapper.update(); });
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
 
-    expect(wrapper)
-      .toHaveLength(1);
-    expect(mockHistoryReplace)
-      .toHaveBeenCalledWith("/Login");
+    expect(wrapper).toHaveLength(1);
+    expect(mockHistoryReplace).toHaveBeenCalledWith("/Login");
+
+    jest.clearAllMocks();
   });
 
   it("should handle unsuccesful api auth", async (): Promise<void> => {
+    expect.hasAssertions();
 
     const signInMock: Readonly<MockedResponse> = {
       request: {
@@ -171,22 +198,31 @@ describe("WelcomeView", (): void => {
     const wrapper: ReactWrapper = mount(
       <PaperProvider>
         <I18nextProvider i18n={i18next}>
-          <MemoryRouter initialEntries={[{
-            pathname: "/Welcome",
-            state: { user: { firstName: "John", fullName: "John Doe" } },
-          }]}>
-            <MockedProvider mocks={[signInMock]} addTypename={false}>
+          <MemoryRouter
+            initialEntries={[
+              {
+                pathname: "/Welcome",
+                state: { user: { firstName: "John", fullName: "John Doe" } },
+              },
+            ]}
+          >
+            <MockedProvider addTypename={false} mocks={[signInMock]}>
               <WelcomeView />
             </MockedProvider>
           </MemoryRouter>
         </I18nextProvider>
-      </PaperProvider>,
+      </PaperProvider>
     );
-    await act(async (): Promise<void> => { await wait(0); wrapper.update(); });
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
 
-    expect(wrapper)
-      .toHaveLength(1);
-    expect(mockHistoryReplace)
-      .toHaveBeenCalledWith("/Login");
+    expect(wrapper).toHaveLength(1);
+    expect(mockHistoryReplace).toHaveBeenCalledWith("/Login");
   });
+
+  jest.clearAllMocks();
 });
