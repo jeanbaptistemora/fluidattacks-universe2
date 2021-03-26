2948abd7b48804ee81bf988019e678ef8a3476f0
Daniel Murcia <dmurcia@fluidattacks.com>
2021-03-26 12:33:03 -0500

    observes\refac(build): #4277.23 timedoc toks env
    - fit run env to standard
    
    
    Former-commit-id: 16aff8e7ba0caf25a99206d70a8e349bc120a516

---
+++ makes/packages/observes/env/service-timedoctor-tokens/runtime/default.nix
@@ -0,0 +1,22 @@
+{ buildPythonPackage
+, makeTemplate
+, nixpkgs
+, path
+, ...
+}:
+let
+  self = buildPythonPackage {
+    name = "observes-service-timedoctor-tokens";
+    packagePath = path "/observes/services/timedoctor_tokens";
+    python = nixpkgs.python38;
+  };
+in
+makeTemplate {
+  name = "observes-env-service-timedoctor-tokens-runtime";
+  searchPaths = {
+    envPython38Paths = [
+      packages.observes.env.service-timedoctor-tokens.runtime.python
+      self
+    ];
+  };
+}
