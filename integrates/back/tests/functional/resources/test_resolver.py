ace1a7e2cfdbfe1d1e37ebff97bf48f911747f74
Briam Agudelo <bagudelo@fluidattacks.com>
2021-03-26 13:24:00 -0500

    integrates\test(back): #4093.88 modify resources
    - Add not errors assert
    - Add files assert
    
    
    Former-commit-id: 861e1db95c1f065d81e4120a7939ebf410a74486

--- integrates/back/tests/functional/resources/test_resolver.py
+++ integrates/back/tests/functional/resources/test_resolver.py
@@ -1,4 +1,5 @@
 # Standard libraries
+import json
 import pytest
 from typing import (
     Any,
@@ -13,27 +14,75 @@ from . import query
 @pytest.mark.resolver_test_group('resources')
 async def test_admin(populate: bool):
     assert populate
+    files: Dict[str, str] = [
+        {
+            "description": "Test",
+            "file_name": "test.zip",
+            "uploader": "unittest@fluidattacks.com",
+            "upload_date": "2019-03-01 15:21"
+        },
+        {
+            "description": "Test",
+            "file_name": "shell.exe",
+            "uploader": "unittest@fluidattacks.com",
+            "upload_date": "2019-04-24 14:56"
+        },
+        {
+            "description": "Test",
+            "file_name": "shell2.exe",
+            "uploader": "unittest@fluidattacks.com",
+            "upload_date": "2019-04-24 14:59"
+        },
+        {
+            "description": "Test",
+            "file_name": "asdasd.py",
+            "uploader": "unittest@fluidattacks.com",
+            "upload_date": "2019-08-06 14:28"
+        }
+    ]
     result: Dict[str, Any] = await query(
         user='admin@gmail.com',
         group='group-1',
     )
+    assert 'errors' not in result
     assert result['data']['resources']['projectName'] == 'group-1'
-    assert 'test.zip' in result['data']['resources']['files']
-    assert 'shell.exe' in result['data']['resources']['files']
-    assert 'shell2.exe' in result['data']['resources']['files']
-    assert 'asdasd.py' in result['data']['resources']['files']
+    assert json.loads(result['data']['resources']['files']) == files
 
 
 @pytest.mark.asyncio
 @pytest.mark.resolver_test_group('resources')
 async def test_analyst(populate: bool):
     assert populate
+    files: Dict[str, str] = [
+        {
+            "description": "Test",
+            "file_name": "test.zip",
+            "uploader": "unittest@fluidattacks.com",
+            "upload_date": "2019-03-01 15:21"
+        },
+        {
+            "description": "Test",
+            "file_name": "shell.exe",
+            "uploader": "unittest@fluidattacks.com",
+            "upload_date": "2019-04-24 14:56"
+        },
+        {
+            "description": "Test",
+            "file_name": "shell2.exe",
+            "uploader": "unittest@fluidattacks.com",
+            "upload_date": "2019-04-24 14:59"
+        },
+        {
+            "description": "Test",
+            "file_name": "asdasd.py",
+            "uploader": "unittest@fluidattacks.com",
+            "upload_date": "2019-08-06 14:28"
+        }
+    ]
     result: Dict[str, Any] = await query(
         user='analyst@gmail.com',
         group='group-1',
     )
+    assert 'errors' not in result
     assert result['data']['resources']['projectName'] == 'group-1'
-    assert 'test.zip' in result['data']['resources']['files']
-    assert 'shell.exe' in result['data']['resources']['files']
-    assert 'shell2.exe' in result['data']['resources']['files']
-    assert 'asdasd.py' in result['data']['resources']['files']
+    assert json.loads(result['data']['resources']['files']) == files
