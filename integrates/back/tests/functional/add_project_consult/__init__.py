f0833c7fb8ff35f2bdb3ef06626bc5abe57d8465
Briam Agudelo <bagudelo@fluidattacks.com>
2021-03-30 17:49:23 -0500

    integrates\test(back): #4093.99 extenralize query
    - Make generic query for resolver
    - Only make assertions in tests
    - Move tests to init
    - Add test for analyst
    - Change populate data
    - no-deltas-test
    
    
    Former-commit-id: 63a2d52b8225b2bdfb6463028531c5265673bbf3

---
+++ integrates/back/tests/functional/add_project_consult/__init__.py
@@ -0,0 +1,47 @@
+# Standard libraries
+import json
+import os
+from typing import (
+    Any,
+    Dict,
+)
+
+# Third party libraries
+from starlette.datastructures import (
+    UploadFile,
+)
+
+# Local libraries
+from backend.api import (
+    get_new_context,
+)
+from back.tests.functional.utils import (
+    get_graphql_result,
+)
+
+
+async def query(
+    *,
+    user: str,
+    group: str,
+) -> Dict[str, Any]:
+    query: str = f"""
+        mutation {{
+            addProjectConsult(
+                content: "Test consult",
+                parent: "0",
+                projectName: "{group}",
+            ) {{
+                success
+                commentId
+            }}
+        }}
+    """
+    data: Dict[str, str] = {
+        'query': query,
+    }
+    return await get_graphql_result(
+        data,
+        stakeholder=user,
+        context=get_new_context(),
+    )
---
+++ integrates/back/tests/functional/add_project_consult/test_resolver.py
@@ -0,0 +1,38 @@
+# Standard libraries
+import pytest
+from typing import (
+    Any,
+    Dict,
+    List,
+)
+
+# Local libraries
+from . import query
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('add_project_consult')
+async def test_admin(populate: bool):
+    assert populate
+    group_name: str = 'group1'
+    result: Dict[str, Any] = await query(
+        user='admin@gmail.com',
+        group=group_name,
+    )
+    assert 'errors' not in result
+    assert 'success' in result['data']['addProjectConsult']
+    assert result['data']['addProjectConsult']['success']
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('add_project_consult')
+async def test_analyst(populate: bool):
+    assert populate
+    group_name: str = 'group1'
+    result: Dict[str, Any] = await query(
+        user='analyst@gmail.com',
+        group=group_name,
+    )
+    assert 'errors' not in result
+    assert 'success' in result['data']['addProjectConsult']
+    assert result['data']['addProjectConsult']['success']
