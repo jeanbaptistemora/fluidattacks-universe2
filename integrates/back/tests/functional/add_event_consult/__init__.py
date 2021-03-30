78d10e196479aba548a2a962b13acddb456f995b
Briam Agudelo <bagudelo@fluidattacks.com>
2021-03-30 16:03:55 -0500

    integrates\test(back): #4093.97 extenralize query
    - Make generic query for resolver
    - Only make assertions in tests
    - Move tests to init
    - no-deltas-test
    
    
    Former-commit-id: 1a0adfbe618ad1e34412092a9bb55bf97b3cf3ad

---
+++ integrates/back/tests/functional/add_event_consult/__init__.py
@@ -0,0 +1,45 @@
+# Standard libraries
+import json
+import os
+from typing import (
+    Any,
+    Dict,
+)
+
+# Third party libraries
+from starlette.datastructures import (
+    UploadFile,
+)
+
+# Local libraries
+from backend.api import (
+    get_new_context,
+)
+from back.tests.functional.utils import (
+    get_graphql_result,
+)
+
+
+async def query(
+    *,
+    user: str,
+    event: str,
+) -> Dict[str, Any]:
+    query: str = f"""
+        mutation {{
+            addEventConsult(eventId: "{event}",
+                            parent: "0",
+                            content: "Test content of new event") {{
+                success
+                commentId
+            }}
+        }}
+    """
+    data: Dict[str, str] = {
+        'query': query,
+    }
+    return await get_graphql_result(
+        data,
+        stakeholder=user,
+        context=get_new_context(),
+    )
---
+++ integrates/back/tests/functional/add_event_consult/test_resolver.py
@@ -0,0 +1,38 @@
+# Standard libraries
+import pytest
+from typing import (
+    Any,
+    Dict,
+    List,
+)
+
+# Local libraries
+from . import query
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('add_event_consult')
+async def test_admin(populate: bool):
+    assert populate
+    event_id: str = '418900971'
+    result: Dict[str, Any] = await query(
+        user='admin@gmail.com',
+        event=event_id,
+    )
+    assert 'errors' not in result
+    assert 'success' in result['data']['addEventConsult']
+    assert result['data']['addEventConsult']
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('add_event_consult')
+async def test_analyst(populate: bool):
+    assert populate
+    event_id: str = '418900971'
+    result: Dict[str, Any] = await query(
+        user='analyst@gmail.com',
+        event=event_id,
+    )
+    assert 'errors' not in result
+    assert 'success' in result['data']['addEventConsult']
+    assert result['data']['addEventConsult']
