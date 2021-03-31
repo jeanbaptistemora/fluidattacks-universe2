1ba24104f2332b6d37ad7bc6573f005b8c0fa39f
Briam Agudelo <bagudelo@fluidattacks.com>
2021-03-31 11:20:06 -0500

    integrates\test(back): #4093.100 extenralize query
    - Make generic query for resolver
    - Only make assertions in tests
    - Move tests to init
    - Add test for analyst
    - Change populate data
    - no-deltas-test
    
    
    Former-commit-id: 8f79847581187c2e6566282230c90951a4fed7d8

---
+++ integrates/back/tests/functional/add_tags/__init__.py
@@ -0,0 +1,51 @@
+# Standard libraries
+import json
+import os
+from typing import (
+    Any,
+    Dict,
+    List,
+)
+
+# Third party libraries
+from starlette.datastructures import (
+    UploadFile,
+)
+
+# Local libraries
+from backend.api import (
+    get_new_context,
+)
+from back.tests.functional.utils import (
+    get_graphql_result,
+)
+
+
+async def query(
+    *,
+    user: str,
+    group: str,
+    tags: List[str],
+) -> Dict[str, Any]:
+    query: str = """
+        mutation AddTagsMutation($projectName: String!, $tagsData: JSONString!) {
+            addTags (
+                tags: $tagsData,
+                projectName: $projectName) {
+                success
+            }
+        }
+    """
+    variables: Dict[str, Any] = {
+        'projectName': group,
+        'tagsData': json.dumps(tags)
+    }
+    data: Dict[str, Any] = {
+        'query': query,
+        'variables': variables
+    }
+    return await get_graphql_result(
+        data,
+        stakeholder=user,
+        context=get_new_context(),
+    )
---
+++ integrates/back/tests/functional/add_tags/test_resolver.py
@@ -0,0 +1,41 @@
+# Standard libraries
+import pytest
+from typing import (
+    Any,
+    Dict,
+    List,
+)
+
+# Local libraries
+from . import query
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('add_tags')
+async def test_admin(populate: bool):
+    assert populate
+    group_name: str = 'group1'
+    tag_list: List[str] = ['testing']
+    result: Dict[str, Any] = await query(
+        user='admin@gmail.com',
+        group=group_name,
+        tags=tag_list,
+    )
+    assert 'errors' not in result
+    assert 'success' in result['data']['addTags']
+    assert result['data']['addTags']['success']
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('add_tags')
+async def test_analyst(populate: bool):
+    assert populate
+    group_name: str = 'group1'
+    tag_list: List[str] = ['testing']
+    result: Dict[str, Any] = await query(
+        user='analyst@gmail.com',
+        group=group_name,
+        tags=tag_list,
+    )
+    assert 'errors' in result
+    assert result['errors'][0]['message'] == 'Access denied'
