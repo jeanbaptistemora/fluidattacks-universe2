e253bd6f54651911c3ede52a6520624d28f799fc
Briam Agudelo <bagudelo@fluidattacks.com>
2021-03-31 11:45:56 -0500

    integrates\test(back): #4093.101 extenralize query
    - Make generic query for resolver
    - Only make assertions in tests
    - Move tests to init
    - no-deltas-test
    
    
    Former-commit-id: 327fcb3347f377b879c45551469b91a9fd8a80c6

---
+++ integrates/back/tests/functional/approve_draft/__init__.py
@@ -0,0 +1,42 @@
+# Standard libraries
+import json
+import os
+from typing import (
+    Any,
+    Dict,
+)
+
+# Third party libraries
+from starlette.datastructures import (
+    UploadFile,
+)
+
+# Local libraries
+from backend.api import (
+    get_new_context,
+)
+from back.tests.functional.utils import (
+    get_graphql_result,
+)
+
+
+async def query(
+    *,
+    user: str,
+    draft: str,
+) -> Dict[str, Any]:
+    query: str = f"""
+        mutation {{
+            approveDraft(draftId: "{draft}") {{
+                success
+            }}
+        }}
+    """
+    data: Dict[str, Any] = {
+        'query': query
+    }
+    return await get_graphql_result(
+        data,
+        stakeholder=user,
+        context=get_new_context(),
+    )
---
+++ integrates/back/tests/functional/approve_draft/test_resolver.py
@@ -0,0 +1,39 @@
+# Standard libraries
+import pytest
+from typing import (
+    Any,
+    Dict,
+    List,
+)
+
+# Local libraries
+from . import query
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('approve_draft')
+async def test_admin(populate: bool):
+    assert populate
+    group_name: str = 'group-1'
+    draft_name: str = '475041513'
+    result: Dict[str, Any] = await query(
+        user='admin@gmail.com',
+        draft=draft_name,
+    )
+    assert 'errors' not in result
+    assert 'success' in result['data']['approveDraft']
+    assert result['data']['approveDraft']['success']
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('approve_draft')
+async def test_analyst(populate: bool):
+    assert populate
+    group_name: str = 'group-1'
+    draft_name: str = '475041513'
+    result: Dict[str, Any] = await query(
+        user='analyst@gmail.com',
+        draft=draft_name,
+    )
+    assert 'errors' in result
+    assert result['errors'][0]['message'] == 'Access denied'
