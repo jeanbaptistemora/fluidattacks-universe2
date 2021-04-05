fa635db4ef5284591f0f7a3832eafa8d2f5f7640
Briam Agudelo <bagudelo@fluidattacks.com>
2021-04-05 11:31:58 -0500

    integrates\test(back): #4093.104 extenralize query
    - Make generic query for resolver
    - Only make assertions in tests
    - no-deltas-test
    
    
    Former-commit-id: e5ca23430e84632b36530e956e02bcda04faaeff

---
+++ integrates/back/tests/functional/event/__init__.py
@@ -0,0 +1,58 @@
+# Standard libraries
+import json
+import os
+from typing import (
+    Any,
+    Dict,
+    List,
+)
+
+# Third party libraries
+from starlette.datastructures import (
+    UploadFile,
+)
+
+# Local libraries
+from backend.api import (
+    get_new_context,
+)
+from back.tests.functional.utils import (
+    get_graphql_result,
+)
+
+
+async def query(
+    *,
+    user: str,
+    event: str,
+) -> Dict[str, Any]:
+    query: str = f"""{{
+        event(identifier: "{event}"){{
+            client
+            evidence
+            projectName
+            eventType
+            detail
+            eventDate
+            eventStatus
+            historicState
+            affectation
+            accessibility
+            affectedComponents
+            context
+            subscription
+            closingDate
+            consulting {{
+                content
+            }}
+            __typename
+        }}
+    }}"""
+    data: Dict[str, Any] = {
+        'query': query
+    }
+    return await get_graphql_result(
+        data,
+        stakeholder=user,
+        context=get_new_context(),
+    )
