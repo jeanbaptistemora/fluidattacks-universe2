e2c027c85bd928e6a7c272b40c98eb431c9e0c6f
Andres Aguirre <aaguirre@fluidattacks.com>
2021-04-05 12:26:55 -0500

    integrates\test(front): #4014.43 toe lines view test
    - Added unittest to display the toe lines table
    
    
    Former-commit-id: 8de8b03067890d708fbbf917c74966a109e2c2b6

---
+++ integrates/front/src/scenes/Dashboard/containers/GroupToeLinesView/index.spec.tsx
@@ -0,0 +1,130 @@
+import { MockedProvider, wait } from "@apollo/react-testing";
+import type { MockedResponse } from "@apollo/react-testing";
+import type { ReactWrapper } from "enzyme";
+import { mount } from "enzyme";
+import React from "react";
+import { act } from "react-dom/test-utils";
+import { Provider } from "react-redux";
+import { MemoryRouter, Route } from "react-router";
+
+import { GET_TOE_LINES } from "./queries";
+
+import { GroupToeLinesView } from ".";
+import { DataTableNext } from "components/DataTableNext";
+import type { ITableProps } from "components/DataTableNext/types";
+import store from "store";
+
+describe("GroupToeLinesView", (): void => {
+  it("should return a function", (): void => {
+    expect.hasAssertions();
+    expect(typeof GroupToeLinesView).toStrictEqual("function");
+  });
+
+  it("should display group toe lines", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    const mokedToeLines: MockedResponse = {
+      request: {
+        query: GET_TOE_LINES,
+        variables: { groupName: "unittesting" },
+      },
+      result: {
+        data: {
+          group: {
+            name: "unittesting",
+            roots: [
+              {
+                id: "4039d098-ffc5-4984-8ed3-eb17bca98e19",
+                toeLines: [
+                  {
+                    comments: "comment test",
+                    filename: "product/test/test.config",
+                    loc: 8,
+                    modifiedCommit: "983466z",
+                    modifiedDate: "2019-08-01T00:00:00-05:00",
+                    testedDate: "2021-02-28T00:00:00-05:00",
+                    testedLines: 4,
+                  },
+                ],
+              },
+              {
+                id: "765b1d0f-b6fb-4485-b4e2-2c2cb1555b1a",
+                toeLines: [
+                  {
+                    comments: "comment test",
+                    filename: "integrates_1/test2/test.sh",
+                    loc: 120,
+                    modifiedCommit: "273412t",
+                    modifiedDate: "2020-11-19T00:00:00-05:00",
+                    testedDate: "2021-01-20T00:00:00-05:00",
+                    testedLines: 172,
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      },
+    };
+    const wrapper: ReactWrapper = mount(
+      <MemoryRouter initialEntries={["/unittesting/toelines"]}>
+        <Provider store={store}>
+          <MockedProvider addTypename={false} mocks={[mokedToeLines]}>
+            <Route
+              component={GroupToeLinesView}
+              path={"/:projectName/toelines"}
+            />
+          </MockedProvider>
+        </Provider>
+      </MemoryRouter>
+    );
+    await act(
+      async (): Promise<void> => {
+        await wait(0);
+        wrapper.update();
+      }
+    );
+
+    const toeLinesTable: ReactWrapper<ITableProps> = wrapper
+      .find(DataTableNext)
+      .filter({ id: "tblToeLines" });
+    const tableHeader: ReactWrapper = toeLinesTable.find("Header");
+    const simpleRows: ReactWrapper = toeLinesTable.find("SimpleRow");
+    const firstRow: ReactWrapper = simpleRows.at(0);
+    const secondRow: ReactWrapper = simpleRows.at(1);
+
+    expect(tableHeader.text()).toStrictEqual(
+      [
+        "Filename",
+        "Loc",
+        "Tested lines",
+        "Modified date",
+        "Modified commit",
+        "Tested date",
+        "Comments",
+      ].join("")
+    );
+    expect(firstRow.text()).toStrictEqual(
+      [
+        "integrates_1/test2/test.sh",
+        "120",
+        "172",
+        "2020-11-19",
+        "273412t",
+        "2021-01-20",
+        "comment test",
+      ].join("")
+    );
+    expect(secondRow.text()).toStrictEqual(
+      [
+        "product/test/test.config",
+        "8",
+        "4",
+        "2019-08-01",
+        "983466z",
+        "2021-02-28",
+        "comment test",
+      ].join("")
+    );
+  });
+});
