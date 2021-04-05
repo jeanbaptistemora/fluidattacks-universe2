5446077fe1dbad4f9c87fb9569cc8eef89ea104e
Briam Agudelo <bagudelo@fluidattacks.com>
2021-04-05 12:07:41 -0500

    integrates\test(back): #4093.105 extenralize query
    - Make generic query for resolver
    - Only make assertions in tests
    - Move tests to init
    - no-deltas-test
    
    
    Former-commit-id: 2cf3651a2a14587bd8c28e4b145b1950a2019f54

---
+++ integrates/back/tests/functional/events/__init__.py
@@ -0,0 +1,45 @@
+# Standard libraries
+import json
+import os
+from typing import (
+    Any,
+    Dict,
+    List,
+)
+
+# Third party libraries
+from starlette.datastructures import (
+    UploadFile,
+)
+
+# Local libraries
+from backend.api import (
+    get_new_context,
+)
+from back.tests.functional.utils import (
+    get_graphql_result,
+)
+
+
+async def query(
+    *,
+    user: str,
+    group: str,
+) -> Dict[str, Any]:
+    query: str = f"""{{
+        events(projectName: "{group}"){{
+            id
+            eventStatus
+            projectName
+            detail
+            evidence
+        }}
+    }}"""
+    data: Dict[str, Any] = {
+        'query': query
+    }
+    return await get_graphql_result(
+        data,
+        stakeholder=user,
+        context=get_new_context(),
+    )
---
+++ integrates/back/tests/functional/events/test_resolver.py
@@ -0,0 +1,69 @@
+# Standard libraries
+import pytest
+from typing import (
+    Any,
+    Dict,
+    List,
+)
+
+# Local libraries
+from . import query
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('events')
+async def test_admin(populate: bool):
+    assert populate
+    expected: List[Dict[str, str]] = [
+        {
+            'id': '418900971',
+            'projectName': 'group1',
+            'eventStatus': 'CREATED',
+            'evidence': 'evidence1',
+            'detail': 'Integrates unit test1',
+        },
+        {
+            'id': '418900980',
+            'projectName': 'group1',
+            'eventStatus': 'CREATED',
+            'evidence': 'evidence2',
+            'detail': 'Integrates unit test2',
+        },
+    ]
+    group_name: str = 'group1'
+    result: Dict[str, Any] = await query(
+        user='admin@gmail.com',
+        group=group_name
+    )
+    assert 'errors' not in result
+    assert result['data']['events'] == expected
+
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('events')
+async def test_analyst(populate: bool):
+    assert populate
+    expected: List[Dict[str, str]] = [
+        {
+            'id': '418900971',
+            'projectName': 'group1',
+            'eventStatus': 'CREATED',
+            'evidence': 'evidence1',
+            'detail': 'Integrates unit test1',
+        },
+        {
+            'id': '418900980',
+            'projectName': 'group1',
+            'eventStatus': 'CREATED',
+            'evidence': 'evidence2',
+            'detail': 'Integrates unit test2',
+        },
+    ]
+    group_name: str = 'group1'
+    result: Dict[str, Any] = await query(
+        user='analyst@gmail.com',
+        group=group_name
+    )
+    assert 'errors' not in result
+    assert result['data']['events'] == expected
