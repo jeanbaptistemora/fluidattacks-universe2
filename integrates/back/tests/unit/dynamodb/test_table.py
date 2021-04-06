2193293868f68d7409a87b4b9ea6a274a188eb09
David Acevedo <dacevedo@fluidattacks.com>
2021-04-06 13:33:04 -0500

    integrates\test(back): #4329.3 dynamodb module
    - added testcase for table
    
    
    Former-commit-id: 7f8c3b2c5a0de07485d5a469c18aeb313eb14870

---
+++ integrates/back/tests/unit/dynamodb/test_table.py
@@ -0,0 +1,57 @@
+# Local
+from dynamodb import table
+from dynamodb.types import Facet, Index, PrimaryKey, Table
+
+
+def test_load_table() -> None:
+    result = table.load_table({
+        "DataModel": [{
+            "GlobalSecondaryIndexes": [
+                {
+                    "IndexName": "test_index",
+                    "KeyAttributes": {
+                        "PartitionKey": {
+                            "AttributeName": "sk",
+                            "AttributeType": "S"
+                        },
+                        "SortKey": {
+                            "AttributeName": "pk",
+                            "AttributeType": "S"
+                        }
+                    },
+                }
+            ],
+            "KeyAttributes": {
+                "PartitionKey": {"AttributeName": "pk", "AttributeType": "S"},
+                "SortKey": {"AttributeName": "sk", "AttributeType": "S"}
+            },
+            "TableFacets": [
+                {
+                    "FacetName": "test_facet",
+                    "KeyAttributeAlias": {
+                        "PartitionKeyAlias": "ENTITY1#name",
+                        "SortKeyAlias": "ENTITY2#id"
+                    },
+                    "NonKeyAttributes": ["attr1", "attr2", "attr3"],
+                },
+            ],
+            "TableName": "test_table",
+        }]
+    })
+    assert result == Table(
+        facets={
+            'test_facet': Facet(
+                attrs=("attr1", "attr2", "attr3"),
+                pk_alias='ENTITY1#name',
+                sk_alias='ENTITY2#id'
+            )
+        },
+        indexes={
+            'test_index': Index(
+                name='test_index',
+                primary_key=PrimaryKey(partition_key='sk', sort_key='pk')
+            )
+        },
+        name='test_table',
+        primary_key=PrimaryKey(partition_key='pk', sort_key='sk')
+    )
