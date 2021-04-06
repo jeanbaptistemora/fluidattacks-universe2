04f608d8dd31e54bb3d87ff52d5abce01db25ca5
Briam Agudelo <bagudelo@fluidattacks.com>
2021-04-06 14:15:04 -0500

    integrates\test(back): #4093.117 extenralize query
    - Make generic query for resolver
    - Only make assertions in tests
    - Move tests to init
    - Add test for analyst
    - Change populate data
    - no-deltas-test
    
    
    Former-commit-id: 6d33a29b784cde23d7cdd838da5c6f47cfc6ad11

---
+++ integrates/back/tests/functional/submit_draft/__init__.py
@@ -0,0 +1,42 @@
+# Standard libraries
+import json
+import os
+from typing import (
+    Any,
+    Dict,
+)
+
+# Third party libraries
+from starlette.datastructures import (
+    UploadFile,
+)
+
+# Local libraries
+from backend.api import (
+    get_new_context,
+)
+from back.tests.functional.utils import (
+    get_graphql_result,
+)
+
+
+async def query(
+    *,
+    user: str,
+    finding: str,
+) -> Dict[str, Any]:
+    query: str = f"""
+        mutation {{
+            submitDraft(findingId: "{finding}") {{
+                success
+            }}
+        }}
+    """
+    data: Dict[str, Any] = {
+        'query': query
+    }
+    return await get_graphql_result(
+        data,
+        stakeholder=user,
+        context=get_new_context(),
+    )
---
+++ integrates/back/tests/functional/submit_draft/test_resolver.py
@@ -0,0 +1,36 @@
+# Standard libraries
+import pytest
+from typing import (
+    Any,
+    Dict,
+)
+
+# Local libraries
+from . import query
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('submit_draft')
+async def test_admin(populate: bool):
+    assert populate
+    finding_id: str = '475041513'
+    result: Dict[str, Any] = await query(
+        user='admin@gmail.com',
+        finding=finding_id
+    )
+    assert 'errors' not in result
+    assert result['data']['submitDraft']['success']
+
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('submit_draft')
+async def test_analyst(populate: bool):
+    assert populate
+    finding_id: str = '475041514'
+    result: Dict[str, Any] = await query(
+        user='analyst@gmail.com',
+        finding=finding_id
+    )
+    assert 'errors' not in result
+    assert result['data']['submitDraft']['success']
