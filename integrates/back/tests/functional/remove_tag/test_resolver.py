e0abea4dca945de2b6dea706447dab0654acfcb2
Briam Agudelo <bagudelo@fluidattacks.com>
2021-04-06 12:23:15 -0500

    integrates\test(back): #4093.114 extenralize query
    - Make generic query for resolver
    - Only make assertions in tests
    - Move tests to init
    - Add test for analyst
    - Change populate data
    - no-deltas-test
    
    
    Former-commit-id: 09c45010867b29835fe3583618becfd1f4af67f9

---
+++ integrates/back/tests/functional/remove_tag/test_resolver.py
@@ -0,0 +1,41 @@
+# Standard libraries
+import pytest
+from typing import (
+    Any,
+    Dict,
+)
+
+# Local libraries
+from . import query
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('remove_tag')
+async def test_admin(populate: bool):
+    assert populate
+    tag_name: str = 'test1'
+    group_name: str = 'group1'
+    result: Dict[str, Any] = await query(
+        user='admin@gmail.com',
+        group=group_name,
+        tag=tag_name
+    )
+    assert 'errors' not in result
+    assert 'success' in result['data']['removeTag']
+    assert result['data']['removeTag']['success']
+
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('remove_tag')
+async def test_analyst(populate: bool):
+    assert populate
+    tag_name: str = 'test2'
+    group_name: str = 'group1'
+    result: Dict[str, Any] = await query(
+        user='analyst@gmail.com',
+        group=group_name,
+        tag=tag_name
+    )
+    assert 'errors' in result
+    assert result['errors'][0]['message'] == 'Access denied'
