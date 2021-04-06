cf902a71176b4b47798eaac9215db15fd5b38cb7
Briam Agudelo <bagudelo@fluidattacks.com>
2021-04-05 19:00:49 -0500

    integrates\test(back): #4093.111 extenralize query
    - Make generic query for resolver
    - Only make assertions in tests
    - Move tests to init
    - no-deltas-test
    
    
    Former-commit-id: 7216d07acb45cdd887d3e515cd3f9d1e21d449ec

---
+++ integrates/back/tests/functional/remove_event_evidence/test_resolver.py
@@ -0,0 +1,34 @@
+# Standard libraries
+import pytest
+from typing import (
+    Any,
+    Dict,
+)
+
+# Local libraries
+from . import query
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('remove_event_evidence')
+async def test_admin(populate: bool):
+    assert populate
+    event_id: str = '418900971'
+    result: Dict[str, Any] = await query(
+        user='admin@gmail.com',
+        event=event_id
+    )
+    assert result['data']['removeEventEvidence']['success'] 
+
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('remove_event_evidence')
+async def test_analyst(populate: bool):
+    assert populate
+    event_id: str = '418900980'
+    result: Dict[str, Any] = await query(
+        user='analyst@gmail.com',
+        event=event_id
+    )
+    assert result['data']['removeEventEvidence']['success']
