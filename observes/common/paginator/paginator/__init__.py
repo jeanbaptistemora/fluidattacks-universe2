40a48a4025698de47fc52b775da9cc06f8735e71
Daniel Murcia <dmurcia@fluidattacks.com>
2021-04-06 16:17:02 -0500

    observes\feat(back): #4347.1 get until end
    - get until end paginator
    - added empty page type
    - added default limits
    
    
    Former-commit-id: e825b2c018b553696a1a0f706a443b3bcea94780

--- observes/common/paginator/paginator/__init__.py
+++ observes/common/paginator/paginator/__init__.py
@@ -4,6 +4,7 @@ from asyncio.events import AbstractEventLoop
 from typing import (
     AsyncGenerator,
     Callable,
+    Union,
     cast,
     Iterator,
     NamedTuple,
@@ -26,6 +27,10 @@ class AllPages(NamedTuple):
     pass
 
 
+class EmptyPage(NamedTuple):
+    pass
+
+
 class PageId(NamedTuple):
     page: int
     per_page: int
@@ -37,24 +42,20 @@ class PageRange(NamedTuple):
     pages: Callable[[], Iterator[PageId]]
 
 
-def new_page_range(
-    page_range: range,
-    per_page: int,
-) -> PageRange:
-    def next_page() -> Iterator[PageId]:
-        for p_num in page_range:
-            yield PageId(
-                page=p_num,
-                per_page=per_page
-            )
-    return PageRange(
-        page_range=page_range,
-        per_page=per_page,
-        pages=next_page
-    )
+class Limits(NamedTuple):
+    max_calls: int
+    max_period: float
+    min_period: float
+    greediness: int
 
 
 Data = TypeVar('Data')
+DEFAULT_LIMITS = Limits(
+    max_calls=5,
+    max_period=1,
+    min_period=0.2,
+    greediness=10,
+)
 
 
 def _iter_over_async(
@@ -76,15 +77,33 @@ def _iter_over_async(
         yield cast(Data, obj)
 
 
+def new_page_range(
+    page_range: range,
+    per_page: int,
+) -> PageRange:
+    def next_page() -> Iterator[PageId]:
+        for p_num in page_range:
+            yield PageId(
+                page=p_num,
+                per_page=per_page
+            )
+    return PageRange(
+        page_range=page_range,
+        per_page=per_page,
+        pages=next_page
+    )
+
+
 def get_pages(
     page_range: PageRange,
     getter: Callable[[PageId], Data],
+    limits: Limits = DEFAULT_LIMITS
 ) -> Iterator[Data]:
 
     @rate_limited(
-        max_calls=5,
-        max_calls_period=1,
-        min_seconds_between_calls=0.2,
+        max_calls=limits.max_calls,
+        max_calls_period=limits.max_period,
+        min_seconds_between_calls=limits.min_period,
     )
     async def get_page(page: PageId) -> Data:
         return await in_thread(getter, page)
@@ -94,8 +113,28 @@ def get_pages(
             get_page,
             page_range.pages()
         )
-        for item in resolve(jobs, worker_greediness=10):
+        for item in resolve(jobs, worker_greediness=limits.greediness):
             yield await item
 
     loop = asyncio.get_event_loop()
     return _iter_over_async(pages(), loop)
+
+
+def get_until_end(
+    start: PageId,
+    getter: Callable[[PageId], Union[Data, EmptyPage]],
+    pages_chunk: int,
+) -> Iterator[Data]:
+    empty_page_retrieved = False
+    actual_page = start.page
+    while not empty_page_retrieved:
+        pages = new_page_range(
+            range(actual_page, actual_page + pages_chunk),
+            start.per_page
+        )
+        for response in get_pages(pages, getter):
+            if isinstance(response, EmptyPage):
+                empty_page_retrieved = True
+                break
+            yield response
+        actual_page = actual_page + pages_chunk
