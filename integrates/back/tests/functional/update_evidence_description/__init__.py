7106bf0ac6228824e9e6d342f2b9d842fc2db457
Briam Agudelo <bagudelo@fluidattacks.com>
2021-04-07 12:48:59 -0500

    integrates\test(back): #4093.121 extenralize query
    - Make generic query for resolver
    - Only make assertions in tests
    - Move tests to init
    - no-deltas-test
    
    
    Former-commit-id: c4e94aafc68da05d6dbac984be0561419ffdb85d

---
+++ integrates/back/tests/functional/update_evidence_description/__init__.py
@@ -0,0 +1,47 @@
+# Standard libraries
+import json
+import os
+from typing import (
+    Any,
+    Dict,
+    List,
+)
+
+# Third party libraries
+from starlette.datastructures import (
+    UploadFile,
+)
+
+# Local libraries
+from backend.api import (
+    get_new_context,
+)
+from back.tests.functional.utils import (
+    get_graphql_result,
+)
+
+
+async def query(
+    *,
+    user: str,
+    description: str,
+    draft: str,
+    evidence: str,
+) -> Dict[str, Any]:
+    query: str = f"""
+        mutation {{
+                updateEvidenceDescription(
+                description: "{description}",
+                findingId: "{draft}",
+                evidenceId: {evidence}
+            ) {{
+                success
+            }}
+        }}
+    """
+    data: Dict[str, str] = { 'query': query }
+    return await get_graphql_result(
+        data,
+        stakeholder=user,
+        context=get_new_context(),
+    )
