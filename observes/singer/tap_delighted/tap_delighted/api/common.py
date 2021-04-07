a3e48d62e7765dcfb0fb0ce86cf19e1d0e150a32
Daniel Murcia <dmurcia@fluidattacks.com>
2021-04-07 12:48:09 -0500

    observes\feat(back): #4347.3 handle api limits
    - added api limits handler
    
    
    Former-commit-id: fd7d9acb66217d7c37177015cb9a573ffb400b96

---
+++ observes/singer/tap_delighted/tap_delighted/api/common.py
@@ -0,0 +1,36 @@
+# Standard libraries
+import time
+from typing import (
+    Callable,
+    Optional,
+    TypeVar,
+)
+
+# Third party libraries
+from delighted.errors import (
+    TooManyRequestsError,
+)
+
+# Local libraries
+
+
+class MaxRetriesReached(Exception):
+    pass
+
+
+RType = TypeVar('RType')
+
+
+def handle_rate_limit(
+    request: Callable[[], RType],
+    max_retries: Optional[int]
+) -> RType:
+    retries = 0
+    retries_limit = max_retries if max_retries else float('inf')
+    while retries < retries_limit:
+        try:
+            return request()
+        except TooManyRequestsError as error:
+            time.sleep(error.retry_after)
+            retries = retries + 1
+    raise MaxRetriesReached()
