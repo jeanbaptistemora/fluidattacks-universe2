50157ecf0838b248702f606a755e8f8054f7d75e
Jonathan Mesa <jmesa@fluidattacks.com>
2021-04-07 11:48:56 -0500

    integrates\feat(cross): #3654.11 use template finding
    - Use accepted_finding versioned template file for email,
    Mandrill template acceptedfinding now deprecated
    - Adjust styles in components for this template
    - Visual changes for this email
    
    
    Former-commit-id: af291ac41c26be022d82b23d8d28875e4e0f711f

--- integrates/back/packages/integrates-back/backend/mailer.py
+++ integrates/back/packages/integrates-back/backend/mailer.py
@@ -588,8 +588,17 @@ async def send_mail_unsolved_events(
 async def send_mail_accepted_finding(
         email_to: List[str],
         context: MailContentType) -> None:
-    await _send_mail_async(
-        'acceptedfinding', email_to, context=context, tags=GENERAL_TAG
+    context["finding_url"] = (
+        f'{BASE_URL}/orgs/{context["organization"]}/groups/' +
+        f'{context["project"]}/vulns/{context["finding_id"]}')
+    context["justification"] = f'"{context["justification"]}"'.splitlines()
+    await _send_mails_async_new(
+        email_to,
+        context,
+        GENERAL_TAG,
+        f'A finding treatment has changed to {context["treatment"]} ' +
+        f'in [{context["project"]}]',
+        'accepted_finding'
     )
 
 
