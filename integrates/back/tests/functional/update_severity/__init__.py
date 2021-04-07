847a93416fb051c3753b956c45e9f681fe988049
Briam Agudelo <bagudelo@fluidattacks.com>
2021-04-07 14:52:47 -0500

    integrates\test(back): #4093.123 extenralize query
    - Make generic query for resolver
    - Only make assertions in tests
    - Move tests to init
    - no-deltas-test
    
    
    Former-commit-id: b447cb454340d0963386c9bc4872ddf905eb474e

---
+++ integrates/back/tests/functional/update_severity/__init__.py
@@ -0,0 +1,59 @@
+# Standard libraries
+import json
+import os
+from typing import (
+    Any,
+    Dict,
+    List,
+)
+
+# Third party libraries
+from starlette.datastructures import (
+    UploadFile,
+)
+
+# Local libraries
+from backend.api import (
+    get_new_context,
+)
+from back.tests.functional.utils import (
+    get_graphql_result,
+)
+
+
+async def query(
+    *,
+    user: str,
+    draft: str,
+) -> Dict[str, Any]:
+    query: str = f"""
+        mutation {{
+            updateSeverity (
+            findingId: "{draft}",
+            data: {{
+                attackComplexity: 0.77, attackVector: 0.62,
+                availabilityImpact: "0", availabilityRequirement: "1",
+                confidentialityImpact: "0", confidentialityRequirement: "1",
+                cvssVersion: "3.1", exploitability: 0.91, id: "{draft}",
+                integrityImpact: "0.22", integrityRequirement: "1",
+                modifiedAttackComplexity: 0.77, modifiedAttackVector: 0.62,
+                modifiedAvailabilityImpact: "0",
+                modifiedConfidentialityImpact: "0",
+                modifiedIntegrityImpact: "0.22",
+                modifiedPrivilegesRequired: "0.62",
+                modifiedSeverityScope: 0, modifiedUserInteraction: 0.85,
+                privilegesRequired: "0.62", remediationLevel: "0.97",
+                reportConfidence: "0.92",
+                severity: "2.9", severityScope: 0, userInteraction: 0.85
+            }}
+            ) {{
+                success
+            }}
+        }}
+    """
+    data: Dict[str, str] = { 'query': query }
+    return await get_graphql_result(
+        data,
+        stakeholder=user,
+        context=get_new_context(),
+    )
---
+++ integrates/back/tests/functional/update_severity/test_resolver.py
@@ -0,0 +1,38 @@
+# Standard libraries
+import pytest
+from typing import (
+    Any,
+    Dict,
+)
+
+# Local libraries
+from . import query
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('update_severity')
+async def test_admin(populate: bool):
+    assert populate
+    draft_id: str = '475041513'
+    result: Dict[str, Any] = await query(
+        user='admin@gmail.com',
+        draft=draft_id
+    )
+    assert 'errors' not in result
+    assert 'success' in result['data']['updateSeverity']
+    assert result['data']['updateSeverity']['success']
+
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('update_severity')
+async def test_analyst(populate: bool):
+    assert populate
+    draft_id: str = '475041513'
+    result: Dict[str, Any] = await query(
+        user='analyst@gmail.com',
+        draft=draft_id
+    )
+    assert 'errors' not in result
+    assert 'success' in result['data']['updateSeverity']
+    assert result['data']['updateSeverity']['success']
