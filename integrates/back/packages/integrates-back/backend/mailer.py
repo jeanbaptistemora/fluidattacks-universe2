b0a7d32d4aee98be547cdef8157b4638c2579717
Jonathan Mesa <jmesa@fluidattacks.com>
2021-04-07 14:54:12 -0500

    integrates\feat(cross): #3654.13 use template unsolved
    - Use unsolved_events versioned template file for email,
    Mandrill template "unsolvedevents" now deprecated
    - Visual changes for this email
    
    
    Former-commit-id: 6a92dadf450a53eea5ae9f64765a5ed7815dee74

--- integrates/back/packages/integrates-back/backend/mailer.py
+++ integrates/back/packages/integrates-back/backend/mailer.py
@@ -596,8 +596,12 @@ async def send_mail_resources(
 async def send_mail_unsolved_events(
         email_to: List[str],
         context: MailContentType) -> None:
-    await _send_mail_async(
-        'unsolvedevents', email_to, context=context, tags=GENERAL_TAG
+    await _send_mails_async_new(
+        email_to,
+        context,
+        GENERAL_TAG,
+        f'Unsolved events in [{context["project"]}]',
+        'unsolved_events'
     )
 
 
