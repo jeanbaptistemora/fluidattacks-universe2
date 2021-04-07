beeaf02f5a74ec85c15b7f58d936b3d3666d75b6
Daniel Salazar <dsalazar@fluidattacks.com>
2021-04-06 23:03:06 -0500

    integrates\feat(build): #4320.22 production new cluster
    - Move production to new cluster
    
    
    Former-commit-id: a97fd1fbfb8596e01ef51f9d10aefccf48a0659f

--- makes/applications/integrates/back/deploy/prod/entrypoint.sh
+++ makes/applications/integrates/back/deploy/prod/entrypoint.sh
@@ -17,7 +17,7 @@ function main {
   export B64_INTEGRATES_PROD_AWS_SECRET_ACCESS_KEY
 
       aws_login_prod integrates \
-  &&  aws_eks_update_kubeconfig 'integrates-cluster' 'us-east-1' \
+  &&  aws_eks_update_kubeconfig 'makes-k8s' 'us-east-1' \
   &&  B64_CI_COMMIT_REF_NAME="$(b64 "${CI_COMMIT_REF_NAME}")" \
   &&  B64_CI_COMMIT_SHA="$(b64 "${CI_COMMIT_SHA}")" \
   &&  B64_INTEGRATES_PROD_AWS_ACCESS_KEY_ID="$(b64 "${INTEGRATES_PROD_AWS_ACCESS_KEY_ID}")" \
