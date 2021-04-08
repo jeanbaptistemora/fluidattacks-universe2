c092f93ddd7f261de0518f95e119331beb99b22f
Mario Varas <mvaras@fluidattacks.com>
2021-04-08 09:05:13 -0500

    sorts\test(back): #4298.14 improve sorts tests
    - New file for testing general functions, thought to mainly test
    training-related methods
    - Added get_best_combination test, so if we modify the structure
    of our training results csvs we can know if something breaks
    
    
    Former-commit-id: ed8639f0763d83ee85f9ad804fbf7dd0482d9b19

---
+++ sorts/test/test_utils.py
@@ -0,0 +1,29 @@
+# Standard libraries
+import os
+
+# Third party libraries
+import csv
+
+# Local libraries
+from training.training_script.train import get_best_combination
+
+DATA_PATH: str = f'{os.path.dirname(__file__)}/data'
+
+
+def test_get_best_combination() -> None:
+    expected_results = {
+        'best_features': (
+            'seldom_contributors',
+            'num_lines',
+            'commit_frequency'
+        ),
+        'best_f1': '69'
+
+    }
+    with open(
+        os.path.join(DATA_PATH, 'test_model_train_results.csv'), 'r'
+    ) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        best_features, best_f1 = get_best_combination(list(csv_reader))
+        assert best_features == expected_results['best_features']
+        assert best_f1 == expected_results['best_f1']
