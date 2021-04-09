d3822aa91d52d1b657ed43a23f08b2809aa91ede
Briam Agudelo <bagudelo@fluidattacks.com>
2021-04-09 14:39:39 -0500

    integrates\test(back): #4093.137 create draft
    - Add test for closer
    - Change populate data
    
    
    Former-commit-id: 65543a7f5e5ef53bb23c497c6aa10058a52270bd

--- integrates/back/tests/functional/create_draft/test_resolver.py
+++ integrates/back/tests/functional/create_draft/test_resolver.py
@@ -31,3 +31,15 @@ async def test_analyst(populate: bool):
     assert 'errors' not in result
     assert 'success' in result['data']['createDraft']
     assert result['data']['createDraft']['success']
+
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('create_draft')
+async def test_closer(populate: bool):
+    assert populate
+    result: Dict[str, Any] = await query(
+        user='closer@gmail.com',
+    )
+    assert 'errors' in result
+    assert result['errors'][0]['message'] == 'Access denied'
