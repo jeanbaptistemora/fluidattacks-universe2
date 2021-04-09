570827b1602a63afad050c168044e63c40aa1ab3
Briam Agudelo <bagudelo@fluidattacks.com>
2021-04-09 10:11:14 -0500

    integrates\test(back): #4093.131 updt event evidence
    - Add test for closer
    - Change populate data
    
    
    Former-commit-id: a2b6894996713454d70ef89c31e5ca2c21ba9892

--- integrates/back/tests/functional/update_event_evidence/test_resolver.py
+++ integrates/back/tests/functional/update_event_evidence/test_resolver.py
@@ -32,3 +32,17 @@ async def test_analyst(populate: bool):
         event=event_id
     )
     assert result['data']['updateEventEvidence']['success']
+
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('update_event_evidence')
+async def test_closer(populate: bool):
+    assert populate
+    event_id: str = '418900971'
+    result: Dict[str, Any] = await query(
+        user='closer@gmail.com',
+        event=event_id
+    )
+    assert 'errors' in result
+    assert result['errors'][0]['message'] == 'Access denied'
