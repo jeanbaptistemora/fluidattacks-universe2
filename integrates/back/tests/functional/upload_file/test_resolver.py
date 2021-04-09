dd646ec78aec6ac921b38067040cfac3f5f62117
Briam Agudelo <bagudelo@fluidattacks.com>
2021-04-09 15:43:44 -0500

    integrates\test(back): #4093.138 upload file
    - Add test for closer
    - Change populate data
    
    
    Former-commit-id: be2950277d9887956e55b49ec5a2434880c5f880

--- integrates/back/tests/functional/upload_file/test_resolver.py
+++ integrates/back/tests/functional/upload_file/test_resolver.py
@@ -34,3 +34,17 @@ async def test_analyst(populate: bool):
     )
     assert 'errors' not in result
     assert result['data']['uploadFile']['success']
+
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('upload_file')
+async def test_closer(populate: bool):
+    assert populate
+    finding_id: str = '475041513'
+    result: Dict[str, Any] = await query(
+        user='closer@gmail.com',
+        finding=finding_id
+    )
+    assert 'errors' not in result
+    assert result['data']['uploadFile']['success']
