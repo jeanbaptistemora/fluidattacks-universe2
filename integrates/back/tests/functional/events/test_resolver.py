964602a81dc35b0e4b64944e3767fa906bb67b0e
Briam Agudelo <bagudelo@fluidattacks.com>
2021-04-09 12:22:51 -0500

    integrates\test(back): #4093.133 events
    - Add test for closer
    - Change populate data
    
    
    Former-commit-id: 762b2b0c9982757b220972afecabc679c5888009

--- integrates/back/tests/functional/events/test_resolver.py
+++ integrates/back/tests/functional/events/test_resolver.py
@@ -67,3 +67,33 @@ async def test_analyst(populate: bool):
     )
     assert 'errors' not in result
     assert result['data']['events'] == expected
+
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('events')
+async def test_closer(populate: bool):
+    assert populate
+    expected: List[Dict[str, str]] = [
+        {
+            'id': '418900971',
+            'projectName': 'group1',
+            'eventStatus': 'CREATED',
+            'evidence': 'evidence1',
+            'detail': 'Integrates unit test1',
+        },
+        {
+            'id': '418900980',
+            'projectName': 'group1',
+            'eventStatus': 'CREATED',
+            'evidence': 'evidence2',
+            'detail': 'Integrates unit test2',
+        },
+    ]
+    group_name: str = 'group1'
+    result: Dict[str, Any] = await query(
+        user='closer@gmail.com',
+        group=group_name
+    )
+    assert 'errors' not in result
+    assert result['data']['events'] == expected
