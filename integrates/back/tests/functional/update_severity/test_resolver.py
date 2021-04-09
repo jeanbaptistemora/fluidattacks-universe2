1b963e698ebe8cb2c1a1cd940fc8a622ce2b9413
Briam Agudelo <bagudelo@fluidattacks.com>
2021-04-09 16:03:45 -0500

    integrates\test(back): #4093.139 updt severity
    - Add test for closer
    - Change populate data
    
    
    Former-commit-id: b664375f17d75556dcc3377c731c0f1e3980f766

--- integrates/back/tests/functional/update_severity/test_resolver.py
+++ integrates/back/tests/functional/update_severity/test_resolver.py
@@ -36,3 +36,17 @@ async def test_analyst(populate: bool):
     assert 'errors' not in result
     assert 'success' in result['data']['updateSeverity']
     assert result['data']['updateSeverity']['success']
+
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('update_severity')
+async def test_closer(populate: bool):
+    assert populate
+    draft_id: str = '475041513'
+    result: Dict[str, Any] = await query(
+        user='closer@gmail.com',
+        draft=draft_id
+    )
+    assert 'errors' in result
+    assert result['errors'][0]['message'] == 'Access denied'
