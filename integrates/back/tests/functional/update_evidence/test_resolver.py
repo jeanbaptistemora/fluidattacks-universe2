dd6299c236db4d2e1ceedb2802791eb66c85d616
Briam Agudelo <bagudelo@fluidattacks.com>
2021-04-12 13:09:47 -0500

    integrates\test(back): #4093.140 updt evidence
    - Add test for closer
    - Change populate data
    
    
    Former-commit-id: 89f7659780182a721d1e25a734c49e9b085921b7

--- integrates/back/tests/functional/update_evidence/test_resolver.py
+++ integrates/back/tests/functional/update_evidence/test_resolver.py
@@ -36,3 +36,18 @@ async def test_analyst(populate: bool):
     assert 'errors' not in result
     assert 'success' in result['data']['updateEvidence']
     assert result['data']['updateEvidence']['success']
+
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group('update_evidence')
+async def test_closer(populate: bool):
+    assert populate
+    draft_id: str = '475041513'
+    result: Dict[str, Any] = await query(
+        user='closer@gmail.com',
+        draft=draft_id
+    )
+    assert 'errors' not in result
+    assert 'success' in result['data']['updateEvidence']
+    assert result['data']['updateEvidence']['success']
