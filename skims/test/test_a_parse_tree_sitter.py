1da6bda86af65f85e1737ca4b5fe8b5e2754a9cf
Diego Restrepo <drestrepo@fluidattacks.com>
2021-04-12 11:29:40 -0500

    skims\test(back): #4331.17 parametrize test files
    - this makes it easier to track file changes and which components
      it affected.
    
    
    Former-commit-id: 92eaa4c05e7cd96e645b778f818967d9a8f694c6

--- skims/test/test_a_parse_tree_sitter.py
+++ skims/test/test_a_parse_tree_sitter.py
@@ -1,4 +1,5 @@
 # Third party libraries
+from typing import Tuple
 from aioextensions import (
     run_decorator,
 )
@@ -24,23 +25,46 @@ from utils.encodings import (
 
 @pytest.mark.skims_test_group('unittesting')
 @run_decorator
-async def test_graph_generation() -> None:
+@pytest.mark.parametrize(
+    "files_to_test,suffix_out",
+    [
+        (
+            (
+                'skims/test/data/benchmark/owasp/BenchmarkTest00001.java',
+                'skims/test/data/benchmark/owasp/BenchmarkTest00008.java',
+                'skims/test/data/benchmark/owasp/BenchmarkTest00167.java',
+            ),
+            'benchmark',
+        ),
+        (
+            (
+                'skims/test/data/lib_path/f031_cwe378/Test.java',
+                'skims/test/data/lib_path/f063_path_traversal/Test.java',
+            ),
+            'findings',
+        ),
+        (
+            ('skims/test/data/sast/TestCFG.java', ),
+            'cfg',
+        ),
+    ],
+)
+async def test_graph_generation(
+    files_to_test: Tuple[str, ...],
+    suffix_out: str,
+) -> None:
     # Test the GraphDB
-    graph_db = await get_graph_db((
-        'skims/test/data/lib_path/f031_cwe378/Test.java',
-        'skims/test/data/lib_path/f063_path_traversal/Test.java',
-        'skims/test/data/benchmark/owasp/BenchmarkTest00001.java',
-        'skims/test/data/benchmark/owasp/BenchmarkTest00008.java',
-        'skims/test/data/benchmark/owasp/BenchmarkTest00167.java',
-        'skims/test/data/sast/TestCFG.java',
-    ))
+    graph_db = await get_graph_db(files_to_test)
     graph_db_as_json_str = json_dumps(graph_db, indent=2, sort_keys=True)
 
     if SHOULD_UPDATE_TESTS:
-        with open('skims/test/data/sast/root-graph.json', 'w') as handle:
+        with open(
+                f'skims/test/data/sast/root-graph_{suffix_out}.json',
+                'w',
+        ) as handle:
             handle.write(graph_db_as_json_str)
 
-    with open('skims/test/data/sast/root-graph.json') as handle:
+    with open(f'skims/test/data/sast/root-graph_{suffix_out}.json') as handle:
         expected = handle.read()
 
     # Test SymEval
@@ -49,16 +73,20 @@ async def test_graph_generation() -> None:
         for finding in core_model.FindingEnum
     }
     syntax_steps_as_json_str = json_dumps(
-        syntax_steps, indent=2, sort_keys=True,
+        syntax_steps,
+        indent=2,
+        sort_keys=True,
     )
 
     if SHOULD_UPDATE_TESTS:
         with open(
-            'skims/test/data/sast/root-graph-syntax.json', 'w',
+                f'skims/test/data/sast/root-graph-syntax_{suffix_out}.json',
+                'w',
         ) as handle:
             handle.write(syntax_steps_as_json_str)
 
-    with open('skims/test/data/sast/root-graph-syntax.json') as handle:
+    with open(f'skims/test/data/sast/root-graph-syntax_{suffix_out}.json',
+              ) as handle:
         expected = handle.read()
 
     assert syntax_steps_as_json_str == expected
