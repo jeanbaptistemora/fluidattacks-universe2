bf6856cca542cae0c92a98970c34cd0d89cb5caa
Jonathan Mesa <jmesa@fluidattacks.com>
2021-04-12 08:43:35 -0500

    integrates\feat(cross): #3654.28 add template new draft
    - Add new_draft versioned template file for email,
    Mandrill template "new-draft" now deprecated
    - Visual changes for this email
    
    
    Former-commit-id: 317bba7067e12fa8e89f6914232df4ae2092ef17

--- integrates/back/packages/integrates-back/backend/mailer.py
+++ integrates/back/packages/integrates-back/backend/mailer.py
@@ -438,8 +438,14 @@ async def get_email_recipients(
 async def send_mail_new_draft(
         email_to: List[str],
         context: MailContentType) -> None:
-    await _send_mail_async(
-        'new-draft', email_to, context=context, tags=GENERAL_TAG)
+    await _send_mails_async_new(
+        email_to,
+        context,
+        GENERAL_TAG,
+        f'New draft submitted in [{context["project"]}] - ' +
+        f'[Finding#{context["finding_id"]}]',
+        'new_draft'
+    )
 
 
 async def send_mail_analytics(
