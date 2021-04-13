3ba9a64f9163088b2946ded3fe753c14d3f24ee1
Daniel Salazar <dsalazar@fluidattacks.com>
2021-04-13 15:30:41 -0500

    makes\feat(conf): #4320.57 log size
    - Increase log size for runners
    - Decrease concurrent requests
    - Decrease log level
    - Mount secondary builds partition
    - Increase minimum node ips
    
    
    Former-commit-id: 9352071b4a6711ca4f6ccd511ba30ba45396fea1

--- makes/applications/makes/k8s/src/terraform/ci-init.sh
+++ makes/applications/makes/k8s/src/terraform/ci-init.sh
@@ -2,16 +2,22 @@
 # Make disk gpt and create partition
 yum install -y parted
 parted /dev/nvme1n1 --script -- mklabel gpt
-parted -a optimal /dev/nvme1n1 mkpart primary 0% 100%
+parted -a optimal /dev/nvme1n1 mkpart primary 0% 65%
+parted -a optimal /dev/nvme1n1 mkpart primary 65% 100%
 
 # Wait for partition to be visible
 sleep 1
 
 # Make partition xfs
 mkfs -t xfs /dev/nvme1n1p1
+mkfs -t xfs /dev/nvme1n1p2
 
-# Mount partition keeping existing docker files
+# Mount docker partition keeping existing docker files
 mv /var/lib/docker /tmp/
 mkdir -p /var/lib/docker
 mount /dev/nvme1n1p1 /var/lib/docker
 mv /tmp/docker/* /var/lib/docker/
+
+# Mount builds partition
+mkdir -p /builds
+mount /dev/nvme1n1p2 /builds
--- makes/applications/makes/k8s/src/terraform/vpc-cni.tf
+++ makes/applications/makes/k8s/src/terraform/vpc-cni.tf
@@ -5,7 +5,7 @@ resource "null_resource" "aws_node_config" {
   provisioner "local-exec" {
     command = <<-EOT
       kubectl -n kube-system set env daemonset aws-node \
-        MINIMUM_IP_TARGET=7 \
+        MINIMUM_IP_TARGET=9 \
         WARM_IP_TARGET=2
     EOT
   }
