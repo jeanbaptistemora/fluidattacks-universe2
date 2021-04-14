4fd6559394f8ccb12ee6678845cdb2b51db9b94c
Andres Aguirre <aaguirre@fluidattacks.com>
2021-04-14 09:25:17 -0500

    integrates\feat(back): #4014.61 get toe inputs by group dal
    - Added dal function for getting the toe inputs by group
    
    
    Former-commit-id: ba48929f3fb377c25aaf3cac184b4dba5635a992

---
+++ integrates/back/packages/modules/toe/inputs/dal.py
@@ -0,0 +1,39 @@
+# Standard libraries
+import logging
+import logging.config
+from typing import Tuple
+
+# Third party libraries
+from botocore.exceptions import ClientError
+
+# Local libraries
+from back.settings import LOGGING
+from backend.exceptions import (
+    UnavailabilityError,
+)
+from data_containers.toe_inputs import GitRootToeInput
+from dynamodb import model
+from dynamodb.types import GitRootToeInputItem
+
+# Constants
+logging.config.dictConfig(LOGGING)
+LOGGER = logging.getLogger(__name__)
+
+
+def _format_git_toe_input(
+    toe_input_item: GitRootToeInputItem
+) -> GitRootToeInput:
+    return GitRootToeInput(**toe_input_item._asdict())
+
+
+async def get_by_group(
+    group_name: str
+) -> Tuple[GitRootToeInput, ...]:
+    try:
+        toe_input_items = await model.get_toe_inputs_by_group(
+            group_name=group_name
+        )
+        return tuple(map(_format_git_toe_input, toe_input_items))
+    except ClientError as ex:
+        LOGGER.exception(ex, extra={'extra': locals()})
+        raise UnavailabilityError() from ex
