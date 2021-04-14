0e1f00728b766cd0da890c0fcb604d2f7c170452
Andres Aguirre <aaguirre@fluidattacks.com>
2021-04-14 10:14:04 -0500

    integrates\test(back): #4014.63 toe inputs by group test
    - Added unittest to get the toe inputs by group
    
    
    Former-commit-id: 66baf1187fdf9413de8340ccdce83648a0e52ed1

---
+++ integrates/back/tests/unit/domain_test/test_toe_inputs.py
@@ -0,0 +1,50 @@
+# Third party libraries
+import pytest
+
+# Local libraries
+from data_containers.toe_inputs import GitRootToeInput
+from toe.inputs import domain as toe_inputs_domain
+
+# Constants
+pytestmark = [
+    pytest.mark.asyncio,
+]
+
+
+async def test_get_by_group() -> None:
+    group_name = 'unittesting'
+    group_toe_inputs = await toe_inputs_domain.get_by_group(group_name)
+    assert group_toe_inputs == (
+        GitRootToeInput(
+            commit='hh66uu5',
+            component='test.com/api/Test',
+            created_date='2000-01-01T00:00:00-05:00',
+            entry_point='idTest',
+            group_name='unittesting',
+            seen_first_time_by='',
+             tested_date='2020-01-02T00:00:00-05:00',
+             verified='Yes',
+             vulns='FIN.S.0001.Test'),
+        GitRootToeInput(
+            commit='e91320h',
+            component='test.com/test/test.aspx',
+            created_date='2020-03-14T00:00:00-05:00',
+            entry_point='btnTest',
+            group_name='unittesting',
+            seen_first_time_by='test@test.com',
+            tested_date='2021-02-02T00:00:00-05:00',
+            verified='No',
+            vulns=''
+        ),
+        GitRootToeInput(
+            commit='d83027t',
+            component='test.com/test2/test.aspx',
+            created_date='2020-01-11T00:00:00-05:00',
+            entry_point='-',
+            group_name='unittesting',
+            seen_first_time_by='test2@test.com',
+            tested_date='2021-02-11T00:00:00-05:00',
+            verified='No',
+            vulns='FIN.S.0003.Test'
+        ),
+    )
