105af5c4d140bbb2cd50893d0b28661b3be450df
Andres Aguirre <aaguirre@fluidattacks.com>
2021-04-14 12:49:58 -0500

    integrates\feat(back): #4014.67 delete toe input domain
    - Add domain function to delete a toe input
    
    
    Former-commit-id: 6b2ea4bb7c82ee406b11010312ea4d5cb0f5e13b

--- integrates/back/packages/modules/toe/inputs/domain.py
+++ integrates/back/packages/modules/toe/inputs/domain.py
@@ -8,6 +8,18 @@ from data_containers.toe_inputs import GitRootToeInput
 from toe.inputs import dal as toe_inputs_dal
 
 
+async def delete(
+    entry_point: str,
+    component: str,
+    group_name: str
+) -> None:
+    await toe_inputs_dal.delete(
+        entry_point,
+        component,
+        group_name
+    )
+
+
 async def get_by_group(
     group_name: str
 ) -> Tuple[GitRootToeInput, ...]:
