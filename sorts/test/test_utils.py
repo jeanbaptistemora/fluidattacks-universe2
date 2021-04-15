b76c9d59a6684ce124b5d603eb9bbbf9b7f3c742
Mario Varas <mvaras@fluidattacks.com>
2021-04-15 10:42:16 -0500

    sorts\test(back): #4298.18 add new utils tests
    - Covering dataset-related functions
    
    
    Former-commit-id: ecdebb9dfe406deaa9c51cc1cb2c29df5c3227f5

--- sorts/test/test_utils.py
+++ sorts/test/test_utils.py
@@ -5,7 +5,11 @@ import os
 import csv
 
 # Local libraries
-from training.training_script.train import get_best_combination
+from training.training_script.train import (
+    get_best_combination,
+    get_features_combinations,
+    get_tried_combinations
+)
 
 DATA_PATH: str = f'{os.path.dirname(__file__)}/data'
 
@@ -27,3 +31,41 @@ def test_get_best_combination() -> None:
         best_features, best_f1 = get_best_combination(list(csv_reader))
         assert best_features == expected_results['best_features']
         assert best_f1 == expected_results['best_f1']
+
+
+def test_get_tried_combinations() -> None:
+    expected_result = [
+        ('seldom_contributors', 'commit_frequency'),
+        ('seldom_contributors', 'num_lines'),
+        ('seldom_contributors', 'num_lines', 'commit_frequency'),
+        ('seldom_contributors',),
+        ('seldom_contributors', 'num_lines', 'commit_frequency'),
+        ('seldom_contributors', 'num_lines', 'commit_frequency'),
+        ('seldom_contributors', 'num_lines', 'commit_frequency'),
+        ('num_lines', 'commit_frequency'),
+        ('seldom_contributors', 'num_lines')]
+
+    with open(
+        os.path.join(DATA_PATH, 'test_model_train_results.csv'), 'r'
+    ) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        tried_combinations = get_tried_combinations(list(csv_reader))
+        assert tried_combinations == expected_result
+
+
+def test_get_features_combinations() -> None:
+    expected_result = [
+        ('num_commits',),
+        ('file_age',),
+        ('seldom_contributors',),
+        ('num_commits', 'file_age'),
+        ('num_commits', 'seldom_contributors'),
+        ('file_age', 'seldom_contributors'),
+        ('num_commits', 'file_age', 'seldom_contributors')
+    ]
+    combinations = get_features_combinations([
+        'num_commits',
+        'file_age',
+        'seldom_contributors'
+    ])
+    assert combinations == expected_result
