9f8c268d06f1ce1a44ee34de0153421fb835b8ac
Kevin Amado <kamado@fluidattacks.com>
2021-04-16 15:07:15 -0500

    skims\feat(back): #4382.37 csp model
    - Add the model for the CSP finding
    
    
    Former-commit-id: 109cabda7d76f3c7e1f13adf3ba69fb57fabd26f

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -454,6 +454,23 @@ class FindingEnum(Enum):
         title='utils.model.finding.enum.f042.title',
         type=FindingTypeEnum.SECURITY,
     )
+    F043_DAST_CSP: FindingMetadata = FindingMetadata.new(
+        auto_approve=False,
+        code='F043_DAST_CSP',
+        cwe='644',
+
+        attack_complexity=0.44,
+        attack_vector=0.85,
+        availability_impact=0.0,
+        confidentiality_impact=0.0,
+        exploitability=0.1,
+        integrity_impact=0.22,
+        privileges_required=0.85,
+        remediation_level=0.95,
+        report_confidence=1.0,
+        severity_scope=0.0,
+        user_interaction=0.85,
+    )
     F043_DAST_RP: FindingMetadata = FindingMetadata.new(
         auto_approve=False,
         code='F043_DAST_RP',
