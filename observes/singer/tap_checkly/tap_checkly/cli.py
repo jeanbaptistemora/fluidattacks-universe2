216c57de2bbf1077f0f3635c188392397b1767f7
Daniel Murcia <dmurcia@fluidattacks.com>
2021-04-19 10:27:28 -0500

    observes\feat(back): #4348.10 cli interface
    - add cli module
    - streams progress 1/11
    - today progress +1, ETA 11 days
    
    
    Former-commit-id: 61ea4d8f9ed50e2069b668c5998539d7d981b2b9

---
+++ observes/singer/tap_checkly/tap_checkly/cli.py
@@ -0,0 +1,50 @@
+# Standard libraries
+from typing import (
+    Optional,
+)
+
+# Third party libraries
+import click
+
+# Local libraries
+from tap_checkly import (
+    executor,
+)
+from tap_checkly.api import (
+    Credentials,
+)
+from tap_checkly.streams import (
+    SupportedStreams,
+)
+
+
+@click.command()
+@click.option('--api-key', type=str, required=True)
+@click.option('--all-streams', is_flag=True, default=False)
+@click.argument(
+    'name',
+    type=click.Choice(
+        [x.value for x in iter(SupportedStreams)],
+        case_sensitive=False),
+    required=False,
+    default=None
+)
+def stream(
+    name: Optional[str],
+    api_key: str,
+    all_streams: bool
+) -> None:
+    creds = Credentials.new(api_key)
+    if all_streams:
+        executor.stream_all(creds)
+    elif name:
+        executor.stream(creds, name)
+
+
+@click.group()
+def main() -> None:
+    # cli group entrypoint
+    pass
+
+
+main.add_command(stream)
