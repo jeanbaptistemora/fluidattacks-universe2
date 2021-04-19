7a2185aaaabb77a288e60424e2529261107cd138
Briam Agudelo <bagudelo@fluidattacks.com>
2021-04-19 11:51:50 -0500

    integrates\test(back): #4093.162 add generic data
    - Add generic data in event
    - Add generic data in events
    - Delete redundant data
    - no-deltas-test
    
    
    Former-commit-id: 0252f9b4ab50fb06e73aa80363b472b4bbb7c9c1

--- integrates/back/tests/functional/add_event_consult/conftest.py
+++ integrates/back/tests/functional/add_event_consult/conftest.py
@@ -16,30 +16,6 @@ from back.tests import (
 @pytest.fixture(autouse=True, scope='session')
 async def populate(generic_data: Dict[str, Any]) -> bool:
     data: Dict[str, Any] = {
-        'groups': [
-            {
-                'project_name': 'group1',
-                'description': 'this is group1',
-                'language': 'en',
-                'historic_configuration': [{
-                    'date': '2020-05-20 17:00:00',
-                    'has_drills': True,
-                    'has_forces': True,
-                    'requester': 'unknown',
-                    'type': 'continuous',
-                }],
-                'project_status': 'ACTIVE',
-                'closed_vulnerabilities': 1,
-                'open_vulnerabilities': 1,
-                'last_closing_date': 40,
-                'max_open_severity': 4.3,
-                'open_findings': 1,
-                'mean_remediate': 2,
-                'mean_remediate_low_severity': 3,
-                'mean_remediate_medium_severity': 4,
-                'tag': ['testing'],
-            },
-        ],
         'events': [
             {
                 'project_name': 'group1',
