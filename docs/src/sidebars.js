0e77a41b8efc51776fbd7d79d2c8a7bde1dfb94d
Diego Gomez <igomez@fluidattacks.com>
2021-04-20 17:06:59 -0500

    docs\feat(doc): #4352.111 migrate requirement 235
    - 20 requirements migrated this week
    - 118 left to migrate
    - d = 118/20 = 5.9
    
    
    Former-commit-id: 8ad544afdfcffbe12659ba29a8851461167f4012

--- docs/src/sidebars.js
+++ docs/src/sidebars.js
@@ -63,6 +63,7 @@ module.exports = {
         'criteria/authentication/r229',
         'criteria/authentication/r231',
         'criteria/authentication/r232',
+        'criteria/authentication/r235',
       ],
     },
     {
