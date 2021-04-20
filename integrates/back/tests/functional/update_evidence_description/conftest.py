876f1d61fae59078a39f7a30b78c5042eae5ccdc
Briam Agudelo <bagudelo@fluidattacks.com>
2021-04-20 11:00:35 -0500

    integrates\test(back): #4093.173 add generic data
    - Add generic data in update_description
    - Add generic data in updt_event_evidence
    - Add generic data in update_evidence
    - Add generic data in updt_evidence_description
    - no-deltas-test
    
    
    Former-commit-id: c1dc9e5d58f4d0b81142f6cc38dbafdb0ce67768

--- integrates/back/tests/functional/update_evidence_description/conftest.py
+++ integrates/back/tests/functional/update_evidence_description/conftest.py
@@ -14,72 +14,8 @@ from back.tests import (
 @pytest.mark.asyncio
 @pytest.mark.resolver_test_group('update_evidence_description')
 @pytest.fixture(autouse=True, scope='session')
-async def populate() -> bool:
+async def populate(generic_data: Dict[str, Any]) -> bool:
     data: Dict[str, Any] = {
-        'users': [
-            {
-                'email': 'admin@gmail.com',
-                'first_login': '',
-                'first_name': '',
-                'last_login': '',
-                'last_name': '',
-                'legal_remember': False,
-                'phone_number': '-',
-                'push_tokens': [],
-                'is_registered': True,
-            },
-            {
-                'email': 'analyst@gmail.com',
-                'first_login': '',
-                'first_name': '',
-                'last_login': '',
-                'last_name': '',
-                'legal_remember': False,
-                'phone_number': '-',
-                'push_tokens': [],
-                'is_registered': True,
-            },
-            {
-                'email': 'closer@gmail.com',
-                'first_login': '',
-                'first_name': '',
-                'last_login': '',
-                'last_name': '',
-                'legal_remember': False,
-                'phone_number': '-',
-                'push_tokens': [],
-                'is_registered': True,
-            },
-        ],
-        'orgs': [
-            {
-                'name': 'orgtest',
-                'id': '40f6da5f-4f66-4bf0-825b-a2d9748ad6db',
-                'users': [
-                    'admin@gmail.com',
-                    'analyst@gmail.com',
-                ],
-                'groups': [
-                    'group1',
-                ],
-                'policy': {},
-            },
-        ],
-        'groups': [
-            {
-                'project_name': 'group1',
-                'description': '-',
-                'language': 'en',
-                'historic_configuration': [{
-                    'date': '2020-05-20 17:00:00',
-                    'has_drills': False,
-                    'has_forces': False,
-                    'requester': 'unknown',
-                    'type': 'continuous',
-                }],
-                'project_status': 'ACTIVE',
-            },
-        ],
         'findings': [
             {
                 'finding_id': '475041513',
@@ -169,43 +105,5 @@ async def populate() -> bool:
                 'interested': 'aaa@gmail.com',
             },
         ],
-        'policies': [
-            {
-                'level': 'user',
-                'subject': 'admin@gmail.com',
-                'object': 'self',
-                'role': 'admin',
-            },
-            {
-                'level': 'user',
-                'subject': 'analyst@gmail.com',
-                'object': 'self',
-                'role': 'user',
-            },
-            {
-                'level': 'group',
-                'subject': 'analyst@gmail.com',
-                'object': 'group1',
-                'role': 'analyst',
-            },
-            {
-                'level': 'user',
-                'subject': 'closer@gmail.com',
-                'object': 'self',
-                'role': 'user',
-            },
-            {
-                'level': 'group',
-                'subject': 'closer@gmail.com',
-                'object': 'group1',
-                'role': 'closer',
-            },
-            {
-                'level': 'organization',
-                'subject': 'analyst@gmail.com',
-                'object': 'ORG#40f6da5f-4f66-4bf0-825b-a2d9748ad6db',
-                'role': 'customer',
-            },
-        ],
     }
-    return await db.populate(data)
+    return await db.populate({**generic_data, **data})
