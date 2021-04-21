c34391be652ca855b79ad67f2dfd5438d6ac26f7
David Acevedo <dacevedo@fluidattacks.com>
2021-04-21 14:08:53 -0500

    integrates\test(back): #398.14 deactivate root
    - added testcases for ip and url roots
    
    
    Former-commit-id: aefd4841ab5401e6b44fd6e03e2177d44064302e

--- integrates/back/tests/functional/deactivate_root/test_resolver.py
+++ integrates/back/tests/functional/deactivate_root/test_resolver.py
@@ -1,6 +1,6 @@
 # Standard
 import pytest
-from typing import Any, Dict
+from typing import Optional
 
 # Local
 from . import query
@@ -8,14 +8,35 @@ from . import query
 
 @pytest.mark.asyncio
 @pytest.mark.resolver_test_group('deactivate_root')
-async def test_git_root(populate: bool):
+@pytest.mark.parametrize(
+    ('root_id',),
+    (
+        ('63298a73-9dff-46cf-b42d-9b2f01a56690',),
+        ('83cadbdc-23f3-463a-9421-f50f8d0cb1e5',),
+        ('eee8b331-98b9-4e32-a3c7-ec22bd244ae8',),
+    )
+)
+@pytest.mark.parametrize(
+    ('reason', 'new_repo'),
+    (
+        ('MOVED_TO_ANOTHER_REPO', 'https://test.com'),
+        ('CODE_EXITS_THE_PROJECT', None),
+        ('REGISTERED_BY_MISTAKE', None),
+    )
+)
+async def test_deactivate_root(
+    populate: bool,
+    root_id: str,
+    reason: str,
+    new_repo: Optional[str]
+):
     assert populate
-    result: Dict[str, Any] = await query(
+    result = await query(
         email='admin@gmail.com',
         group_name='group1',
-        id='63298a73-9dff-46cf-b42d-9b2f01a56690',
-        new_repo='https://test.com',
-        reason='MOVED_TO_ANOTHER_REPO'
+        id=root_id,
+        new_repo=new_repo,
+        reason=reason
     )
     assert 'errors' not in result
     assert result['data']['deactivateRoot']['success']
