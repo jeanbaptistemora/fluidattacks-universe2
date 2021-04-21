7502f8cf5970f262ac32f67b7de1e1790e47bd7a
Daniel Salazar <dsalazar@fluidattacks.com>
2021-04-21 17:14:12 -0500

    makes\feat(infra): #4449.13 okta kms key
    - Create own okta kms key for encrypting
    all sensitive information
    
    
    Former-commit-id: 638a00e33fdfc0c338338dcb98ab0d41d69103ea

---
+++ makes/applications/makes/okta/src/terraform/key.tf
@@ -0,0 +1,120 @@
+data "aws_iam_policy_document" "okta" {
+
+  statement {
+    sid    = "Enable IAM User Permissions"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions = [
+      "kms:*"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid    = "Key Administrators"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/FLUIDServes_TF",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user-provision/serves-prod",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user-provision/serves-dev",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/makes_prod"
+      ]
+    }
+    actions = [
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:TagResource",
+      "kms:UntagResource",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion",
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid    = "Key Users"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/FLUIDServes_TF",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user-provision/serves-prod",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user-provision/serves-dev",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/makes_prod"
+      ]
+    }
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid    = "Attachment Of Persistent Resources"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/FLUIDServes_TF",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user-provision/serves-prod",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user-provision/serves-dev",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/makes_prod"
+      ]
+    }
+    actions = [
+      "kms:CreateGrant",
+      "kms:ListGrants",
+      "kms:RevokeGrant",
+    ]
+    resources = [
+      "*"
+    ]
+    condition {
+      test     = "Bool"
+      variable = "kms:GrantIsForAWSResource"
+      values = [
+        "true",
+      ]
+    }
+  }
+}
+
+resource "aws_kms_key" "okta" {
+  policy                  = data.aws_iam_policy_document.okta.json
+  deletion_window_in_days = 30
+  is_enabled              = true
+
+  tags = {
+    "Name"               = "makes-okta"
+    "management:type"    = "production"
+    "management:product" = "makes"
+  }
+}
+
+resource "aws_kms_alias" "okta" {
+  name          = "alias/makes-okta"
+  target_key_id = aws_kms_key.okta.key_id
+}
