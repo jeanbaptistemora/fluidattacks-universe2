ece2e068301b9897c6a72e2f11de60400a87a95c
Rafael Rodriguez <rrodriguez@fluidattacks.com>
2021-04-21 14:14:39 -0500

    docs\feat(doc): #4352.133 migrate requirement 356
    - 19 requirements migrated this week
    - 98 left to migrate
    - d = 98/19 = 5.15
    
    
    Former-commit-id: d4ddfa30150538fa973b07926c5230c409662a0b

--- docs/src/sidebars.js
+++ docs/src/sidebars.js
@@ -219,6 +219,7 @@ module.exports = {
           'criteria/networks/r257',
           'criteria/networks/r258',
           'criteria/networks/r259',
+          'criteria/networks/r356',
         ],
     },
     {
