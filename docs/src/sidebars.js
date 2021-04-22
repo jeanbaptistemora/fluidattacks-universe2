bc28043bbbc3bed5bee7edc9bec318ddd3ce8271
Rafael Rodriguez <rrodriguez@fluidattacks.com>
2021-04-22 15:39:27 -0500

    docs\feat(doc): #4352.159 migrate requirement 151
    - 33 requirements migrated this week
    - 72 left to migrate
    - d = 72/33 = 2.18
    
    
    Former-commit-id: 41ca6f36030dd0d8ac69a81db085a00fda04f7f6

--- docs/src/sidebars.js
+++ docs/src/sidebars.js
@@ -210,6 +210,7 @@ module.exports = {
           'criteria/cryptography/r148',
           'criteria/cryptography/r149',
           'criteria/cryptography/r150',
+          'criteria/cryptography/r151',
         ],
     },
     {
