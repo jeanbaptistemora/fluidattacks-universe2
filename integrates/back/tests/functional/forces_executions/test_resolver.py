d33bc236ae0db0b1ade0055071c8751c6da69be9
Briam Agudelo <bagudelo@fluidattacks.com>
2021-04-22 13:09:20 -0500

    integrates\test(back): #4458.7 decrease tech debt
    - Refect test resolver
    - Solved 4 smells in 0.5 hrs (8 smells/hr)
    - Remaining: 642 smells, 80.25 hrs
    - no-deltas-test
    
    
    Former-commit-id: 8de81e0d679d2635007277c1f54e08c4a1324873

--- integrates/back/tests/functional/forces_executions/test_resolver.py
+++ integrates/back/tests/functional/forces_executions/test_resolver.py
@@ -11,53 +11,18 @@ from . import query
 
 @pytest.mark.asyncio
 @pytest.mark.resolver_test_group('forces_executions')
-async def test_admin(populate: bool):
+@pytest.mark.parametrize(
+    ['email'],
+    [
+        ['admin@gmail.com'],
+        ['analyst@gmail.com'],
+        ['closer@gmail.com'],
+    ]
+)
+async def test_get_forces_executions(populate: bool, email: str):
     assert populate
     result: Dict[str, Any] = await query(
-        user='admin@gmail.com',
-        group='group1',
-    )
-    executions = result['data']['forcesExecutions']['executions']
-    assert result['data']['forcesExecutions']['fromDate'] == '2020-02-01 00:00:00+00:00'
-    assert result['data']['forcesExecutions']['toDate'] == '2020-02-28 23:59:59+00:00'
-    assert executions[0]['date'] == '2020-02-05T00:00:00-05:00'
-    assert executions[0]['exitCode'] == '1'
-    assert executions[0]['gitBranch'] == 'master'
-    assert executions[0]['gitCommit'] == '6e7b34c1358db2ff4123c3c76e7fe3bf9f2838f6'
-    assert executions[0]['gitOrigin'] == 'http://test.com'
-    assert executions[0]['gitRepo'] == 'Repository'
-    assert executions[0]['kind'] == 'dynamic'
-    assert executions[0]['strictness'] == 'strict'
-
-
-@pytest.mark.asyncio
-@pytest.mark.resolver_test_group('forces_executions')
-async def test_analyst(populate: bool):
-    assert populate
-    result: Dict[str, Any] = await query(
-        user='analyst@gmail.com',
-        group='group1',
-    )
-    executions = result['data']['forcesExecutions']['executions']
-    assert result['data']['forcesExecutions']['fromDate'] == '2020-02-01 00:00:00+00:00'
-    assert result['data']['forcesExecutions']['toDate'] == '2020-02-28 23:59:59+00:00'
-    assert executions[0]['date'] == '2020-02-05T00:00:00-05:00'
-    assert executions[0]['exitCode'] == '1'
-    assert executions[0]['gitBranch'] == 'master'
-    assert executions[0]['gitCommit'] == '6e7b34c1358db2ff4123c3c76e7fe3bf9f2838f6'
-    assert executions[0]['gitOrigin'] == 'http://test.com'
-    assert executions[0]['gitRepo'] == 'Repository'
-    assert executions[0]['kind'] == 'dynamic'
-    assert executions[0]['strictness'] == 'strict'
-
-
-
-@pytest.mark.asyncio
-@pytest.mark.resolver_test_group('forces_executions')
-async def test_analyst(populate: bool):
-    assert populate
-    result: Dict[str, Any] = await query(
-        user='closer@gmail.com',
+        user=email,
         group='group1',
     )
     executions = result['data']['forcesExecutions']['executions']
