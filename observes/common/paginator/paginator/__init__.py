fe11cc16e81ae1483acc13950609eb3a90c52e48
Daniel Murcia <dmurcia@fluidattacks.com>
2021-04-27 08:37:10 -0500

    observes\refac(back): #4349.2 int index module
    - move functions to int index module
    
    
    Former-commit-id: 56b95a93f451cfa199fb6fa4f14b6364a4857d7c

--- observes/common/paginator/paginator/__init__.py
+++ observes/common/paginator/paginator/__init__.py
@@ -1,26 +1,10 @@
-# Standard libraries
-import asyncio
-from asyncio.events import AbstractEventLoop
-from typing import (
-    AsyncGenerator,
-    Callable,
-    Type,
-    Union,
-    cast,
-    Iterator,
-    Optional,
-    Tuple,
-    TypeVar,
-)
-
-# Third party libraries
-from aioextensions import (
-    in_thread,
-    rate_limited,
-    resolve,
-)
-
 # Local libraries
+from paginator.int_index import (
+    build_getter,
+    get_pages,
+    get_until_end,
+    new_page_range,
+)
 from paginator.objs import (
     AllPages,
     EmptyPage,
@@ -31,115 +15,6 @@ from paginator.objs import (
 )
 
 
-Data = TypeVar('Data')
-ResultPage = TypeVar('ResultPage')
-EPage = Union[ResultPage, EmptyPage]
-PageGetter = Callable[[PageId], EPage[Data]]
-DEFAULT_LIMITS = Limits(
-    max_calls=5,
-    max_period=1,
-    min_period=0.2,
-    greediness=10,
-)
-
-
-def _iter_over_async(
-    ait: AsyncGenerator[Data, None], loop: AbstractEventLoop
-) -> Iterator[Data]:
-    ait = ait.__aiter__()
-
-    async def get_next() -> Tuple[bool, Optional[Data]]:
-        try:
-            obj: Data = await ait.__anext__()
-            return False, obj
-        except StopAsyncIteration:
-            return True, None
-
-    while True:
-        done, obj = loop.run_until_complete(get_next())
-        if done:
-            break
-        yield cast(Data, obj)
-
-
-def new_page_range(
-    page_range: range,
-    per_page: int,
-) -> PageRange:
-    def next_page() -> Iterator[PageId]:
-        for p_num in page_range:
-            yield PageId(
-                page=p_num,
-                per_page=per_page
-            )
-    return PageRange(
-        page_range=page_range,
-        per_page=per_page,
-        pages=next_page
-    )
-
-
-def get_pages(
-    page_range: PageRange,
-    getter: Callable[[PageId], Data],
-    limits: Limits = DEFAULT_LIMITS
-) -> Iterator[Data]:
-
-    @rate_limited(
-        max_calls=limits.max_calls,
-        max_calls_period=limits.max_period,
-        min_seconds_between_calls=limits.min_period,
-    )
-    async def get_page(page: PageId) -> Data:
-        return await in_thread(getter, page)
-
-    async def pages() -> AsyncGenerator[Data, None]:
-        jobs = map(
-            get_page,
-            page_range.pages()
-        )
-        for item in resolve(jobs, worker_greediness=limits.greediness):
-            yield await item
-
-    loop = asyncio.get_event_loop()
-    return _iter_over_async(pages(), loop)
-
-
-# _type is necessary to correctly infer the type var
-def get_until_end(
-    _type: Type[Data],
-    start: PageId,
-    getter: PageGetter[Data],
-    pages_chunk: int,
-) -> Iterator[Data]:
-    empty_page_retrieved = False
-    actual_page = start.page
-    while not empty_page_retrieved:
-        pages = new_page_range(
-            range(actual_page, actual_page + pages_chunk),
-            start.per_page
-        )
-        for response in get_pages(pages, getter):
-            if isinstance(response, EmptyPage):
-                empty_page_retrieved = True
-                break
-            yield response
-        actual_page = actual_page + pages_chunk
-
-
-def build_getter(
-    _type: Type[ResultPage],
-    get_page: Callable[[PageId], ResultPage],
-    is_empty: Callable[[ResultPage], bool],
-) -> PageGetter[ResultPage]:
-    def getter(page: PageId) -> EPage[ResultPage]:
-        result: ResultPage = get_page(page)
-        if is_empty(result):
-            return EmptyPage()
-        return result
-    return getter
-
-
 __all__ = [
     'AllPages',
     'EmptyPage',
@@ -147,4 +22,8 @@ __all__ = [
     'PageOrAll',
     'PageRange',
     'Limits',
+    'build_getter',
+    'get_pages',
+    'get_until_end',
+    'new_page_range',
 ]
---
+++ observes/common/paginator/paginator/int_index/__init__.py
@@ -0,0 +1,140 @@
+# Standard libraries
+import asyncio
+from asyncio.events import (
+    AbstractEventLoop,
+)
+from typing import (
+    AsyncGenerator,
+    Callable,
+    Type,
+    Union,
+    cast,
+    Iterator,
+    Optional,
+    Tuple,
+    TypeVar,
+)
+
+# Third party libraries
+from aioextensions import (
+    in_thread,
+    rate_limited,
+    resolve,
+)
+
+# Local libraries
+from paginator.objs import (
+    EmptyPage,
+    PageId,
+    PageRange,
+    Limits,
+)
+
+
+_Data = TypeVar('_Data')
+ResultPage = TypeVar('ResultPage')
+EPage = Union[ResultPage, EmptyPage]
+PageGetter = Callable[[PageId], EPage[_Data]]
+DEFAULT_LIMITS = Limits(
+    max_calls=5,
+    max_period=1,
+    min_period=0.2,
+    greediness=10,
+)
+
+
+def _iter_over_async(
+    ait: AsyncGenerator[_Data, None], loop: AbstractEventLoop
+) -> Iterator[_Data]:
+    ait = ait.__aiter__()
+
+    async def get_next() -> Tuple[bool, Optional[_Data]]:
+        try:
+            obj: _Data = await ait.__anext__()
+            return False, obj
+        except StopAsyncIteration:
+            return True, None
+
+    while True:
+        done, obj = loop.run_until_complete(get_next())
+        if done:
+            break
+        yield cast(_Data, obj)
+
+
+def new_page_range(
+    page_range: range,
+    per_page: int,
+) -> PageRange:
+    def next_page() -> Iterator[PageId]:
+        for p_num in page_range:
+            yield PageId(
+                page=p_num,
+                per_page=per_page
+            )
+    return PageRange(
+        page_range=page_range,
+        per_page=per_page,
+        pages=next_page
+    )
+
+
+def get_pages(
+    page_range: PageRange,
+    getter: Callable[[PageId], _Data],
+    limits: Limits = DEFAULT_LIMITS
+) -> Iterator[_Data]:
+
+    @rate_limited(
+        max_calls=limits.max_calls,
+        max_calls_period=limits.max_period,
+        min_seconds_between_calls=limits.min_period,
+    )
+    async def get_page(page: PageId) -> _Data:
+        return await in_thread(getter, page)
+
+    async def pages() -> AsyncGenerator[_Data, None]:
+        jobs = map(
+            get_page,
+            page_range.pages()
+        )
+        for item in resolve(jobs, worker_greediness=limits.greediness):
+            yield await item
+
+    loop = asyncio.get_event_loop()
+    return _iter_over_async(pages(), loop)
+
+
+# _type is necessary to correctly infer the type var
+def get_until_end(
+    _type: Type[_Data],
+    start: PageId,
+    getter: PageGetter[_Data],
+    pages_chunk: int,
+) -> Iterator[_Data]:
+    empty_page_retrieved = False
+    actual_page = start.page
+    while not empty_page_retrieved:
+        pages = new_page_range(
+            range(actual_page, actual_page + pages_chunk),
+            start.per_page
+        )
+        for response in get_pages(pages, getter):
+            if isinstance(response, EmptyPage):
+                empty_page_retrieved = True
+                break
+            yield response
+        actual_page = actual_page + pages_chunk
+
+
+def build_getter(
+    _type: Type[ResultPage],
+    get_page: Callable[[PageId], ResultPage],
+    is_empty: Callable[[ResultPage], bool],
+) -> PageGetter[ResultPage]:
+    def getter(page: PageId) -> EPage[ResultPage]:
+        result: ResultPage = get_page(page)
+        if is_empty(result):
+            return EmptyPage()
+        return result
+    return getter
