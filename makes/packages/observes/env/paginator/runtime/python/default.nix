061dd9c6f5086dc86890251ade8611c667dbd158
Daniel Murcia <dmurcia@fluidattacks.com>
2021-04-27 17:28:07 -0500

    observes\feat(back): #4349.5 obj index paginator
    - add obj index types
    - add returns dependency
    
    
    Former-commit-id: eb7555b7da8620dda61149e88dba3ae323d1fac4

--- makes/packages/observes/env/paginator/runtime/python/default.nix
+++ makes/packages/observes/env/paginator/runtime/python/default.nix
@@ -7,8 +7,11 @@ buildPythonRequirements {
   requirements = {
     direct = [
       "aioextensions==20.11.1621472"
+      "returns==0.16.0"
     ];
-    inherited = [ ];
+    inherited = [
+      "typing-extensions==3.7.4.3"
+    ];
   };
   python = nixpkgs.python38;
 }
---
+++ observes/common/paginator/paginator/object_index/objs.py
@@ -0,0 +1,61 @@
+# pylint: skip-file
+# Standard libraries
+from typing import (
+    Callable,
+    NamedTuple,
+    TypeVar,
+    Union,
+)
+from typing_extensions import final
+
+# Third party libraries
+from returns.maybe import Maybe
+from returns.primitives.container import BaseContainer
+from returns.primitives.hkt import SupportsKind1
+
+# Local libraries
+from paginator.common import AllPages
+
+
+class PageId(NamedTuple):
+    page: str
+    per_page: int
+
+
+_Data = TypeVar("_Data")
+
+
+@final
+class PageResult(
+    BaseContainer,
+    SupportsKind1["PageResult", _Data],
+):
+    def __init__(
+        self,
+        data: _Data,
+        next_item: str,
+        total_items: Maybe[int],
+    ) -> None:
+        super().__init__(
+            {
+                "data": data,
+                "next_item": next_item,
+                "total_items": total_items,
+            }
+        )
+
+    @property
+    def data(self) -> _Data:
+        return self._inner_value["data"]
+
+    @property
+    def next_item(self) -> str:
+        return self._inner_value["next_item"]
+
+    @property
+    def total_items(self) -> Maybe[int]:
+        return self._inner_value["total_items"]
+
+
+PageOrAll = Union[AllPages, PageId]
+PageGetter = Callable[[PageId], Maybe[PageResult[_Data]]]
