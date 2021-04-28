746fe848373e2e89f9f60300c079652c1a1367a5
Daniel Salazar <dsalazar@fluidattacks.com>
2021-04-27 19:00:28 -0500

    makes\feat(infra): #4449.30 groups with rules
    - Add support for groups with rules
    - Adapt schema
    - Migrate groups with rules
    - Update statefile
    
    
    Former-commit-id: fe542dd9679e23e6d1ea8c44655e6b10d7b3d5f7

--- makes/applications/makes/okta/src/terraform/okta-groups.tf
+++ makes/applications/makes/okta/src/terraform/okta-groups.tf
@@ -1,6 +1,27 @@
 resource "okta_group" "groups" {
-  for_each    = { for _, group in local.groups : group.id => group }
+  for_each = {
+    for _, group in local.groups : group.id => group
+    if group.type == "users"
+  }
+
   name        = each.value.name
   description = each.value.description
   users       = [for user in each.value.users : okta_user.users[user].id]
+}
+
+resource "okta_group" "groups_rules" {
+  for_each = {
+    for _, group in local.groups : group.id => group
+    if group.type == "rules"
+  }
+
+  name        = each.value.name
+  description = each.value.description
+  users       = null
+
+  lifecycle {
+    ignore_changes = [
+      users,
+    ]
+  }
 }
