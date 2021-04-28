16d09771f8544fab2a4a948a6b29c299044373ee
Diego Restrepo <drestrepo@fluidattacks.com>
2021-04-28 11:51:31 -0500

    skims\feat(back): #4477.5 add csharp cfg
    - add c_sharp control flow graph generic evaluator
    
    
    Former-commit-id: 89e337a64e6a33647792b85fe146b96534f1aff4

--- skims/skims/sast_transformations/control_flow/__init__.py
+++ skims/skims/sast_transformations/control_flow/__init__.py
@@ -4,6 +4,7 @@ from model.graph_model import (
     GraphShardMetadataLanguage,
 )
 from sast_transformations.control_flow.java import add as java_add
+from sast_transformations.control_flow.c_sharp import add as c_sharp_add
 
 
 def add(
@@ -12,3 +13,5 @@ def add(
 ) -> None:
     if language == GraphShardMetadataLanguage.JAVA:
         java_add(graph)
+    elif language == GraphShardMetadataLanguage.CSHARP:
+        c_sharp_add(graph)
