2a15c81f51ea52590b3493571c8e8657b946c14e
Daniel Murcia <dmurcia@fluidattacks.com>
2021-04-28 09:38:44 -0500

    observes\feat(back): #4349.6 obj index paginator
    - add get until end procedure
    
    
    Former-commit-id: ae42a29ed30fdd9da853383246277ad5c8d5d28f

---
+++ observes/common/paginator/paginator/object_index/__init__.py
@@ -0,0 +1,36 @@
+# pylint: skip-file
+# Standard libraries
+from typing import (
+    Type,
+    Iterator,
+    TypeVar,
+)
+
+# Third party libraries
+from returns.maybe import Maybe, Nothing
+
+# Local libraries
+from paginator.object_index.objs import (
+    PageId,
+    PageGetter,
+    PageResult,
+)
+
+
+_Data = TypeVar("_Data")
+
+
+def get_until_end(
+    start: PageId,
+    getter: PageGetter[_Data],
+) -> Iterator[PageResult[_Data]]:
+    empty_page_retrieved = False
+    next_page_id: PageId = start
+    while not empty_page_retrieved:
+        page: Maybe[PageResult[_Data]] = getter(next_page_id)
+        if page == Nothing:
+            empty_page_retrieved = True
+            break
+        result_page = page.unwrap()
+        yield result_page
+        next_page_id = PageId(result_page.next_item, start.per_page)
