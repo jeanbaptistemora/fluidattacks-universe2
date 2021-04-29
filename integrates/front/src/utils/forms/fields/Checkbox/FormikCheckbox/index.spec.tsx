8ce38c2bf298cc08dcb09fa9dd35b50c75bd7cb0
Juan Chaves <jchaves@fluidattacks.com>
2021-04-29 14:38:48 -0500

    integrates\test(front): #4486.3 checkbox component
    - Added a test to FormikCheckbox
    
    
    Former-commit-id: 408ae87c84f28ba19949607dde0faf47aeb9ba1b

---
+++ integrates/front/src/utils/forms/fields/Checkbox/FormikCheckbox/index.spec.tsx
@@ -0,0 +1,36 @@
+import type { ReactWrapper } from "enzyme";
+import { mount } from "enzyme";
+import { Field, Form, Formik } from "formik";
+import React from "react";
+import { object, string } from "yup";
+
+import { FormikCheckbox } from "utils/forms/fields//Checkbox/FormikCheckbox";
+
+const FormikCheckboxSchema = object().shape({
+  textTest: string().required(),
+});
+
+describe("Checkbox Field", (): void => {
+  it("should return a function", (): void => {
+    expect.hasAssertions();
+    expect(typeof FormikCheckbox).toStrictEqual("function");
+  });
+
+  it("should render checkbox component", (): void => {
+    expect.hasAssertions();
+
+    const wrapper: ReactWrapper = mount(
+      <Formik initialValues={{ checkboxTest: false }} onSubmit={jest.fn()}>
+        <Form name={""}>
+          <Field
+            component={FormikCheckbox}
+            name={"checkboxTest"}
+            validationSchema={FormikCheckboxSchema}
+          />
+        </Form>
+      </Formik>
+    );
+
+    expect(wrapper.find("Checkbox").first().props().name).toBe("checkboxTest");
+  });
+});
