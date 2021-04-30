5a62a369acbf20ad4e897f4c71999bc6b5c4c06c
Kevin Amado <kamado@fluidattacks.com>
2021-04-30 15:16:41 -0500

    skims\refac(back): #4852.4 model cvss values
    - Model availability impact, confidentiality, integrity
      and attack compelxity values
    
    
    Former-commit-id: a8ccf551da7f7abf171c748cf2d60ddde3b49035

--- skims/skims/model/cvss3_model.py
+++ skims/skims/model/cvss3_model.py
@@ -4,8 +4,31 @@ from enum import (
 )
 
 
+class AttackComplexity(Enum):
+    low: float = 0.44
+    high: float = 0.77
+
+
 class AttackVector(Enum):
     physical: float = 0.20
     local: float = 0.55
     adjacent: float = 0.62
     network: float = 0.85
+
+
+class AvailabilityImpact(Enum):
+    none: float = 0.00
+    low: float = 0.22
+    high: float = 0.56
+
+
+class ConfidentialityImpact(Enum):
+    none: float = 0.00
+    low: float = 0.22
+    high: float = 0.56
+
+
+class IntegrityImpact(Enum):
+    none: float = 0.00
+    low: float = 0.22
+    high: float = 0.56
