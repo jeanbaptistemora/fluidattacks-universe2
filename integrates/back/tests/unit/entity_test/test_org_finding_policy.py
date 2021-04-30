026d8916ccc80de4f097c09c6c004a108aa525b5
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-04-30 11:10:57 -0500

    integrates\test(back): #3506.13 add handle_org_policy mt
    - add test for add handle org findings policies mutation
    - no-deltas-test
    
    
    Former-commit-id: feee5294ed7cc48432a8c3b0632ffa7a265a0aa0

---
+++ integrates/back/tests/unit/entity_test/test_org_finding_policy.py
@@ -0,0 +1,137 @@
+# Standard libraries
+from typing import (
+    Any,
+    Dict,
+)
+import pytest
+
+# Third party libraries
+from ariadne import graphql
+
+# Local libraries
+from back.tests.unit.utils import create_dummy_session
+from backend.api import apply_context_attrs
+from backend.api.schema import SCHEMA
+from custom_exceptions import (
+    PolicyAlreadyHandled,
+)
+from organizations_finding_policies import domain as policies_domain
+
+
+pytestmark = [
+    pytest.mark.asyncio,
+]
+
+
+async def _get_result_async(
+    data: Dict[str, Any],
+    stakeholder: str = 'integratesmanager@gmail.com'
+) -> Dict[str, Any]:
+    """Get result."""
+    request = await create_dummy_session(username=stakeholder)
+    request = apply_context_attrs(request)
+    _, result = await graphql(SCHEMA, data, context_value=request)
+
+    return result
+
+
+@pytest.mark.changes_db
+async def test_handle_org_finding_policy_acceptation():
+    org_name = 'okada'
+    finding_name = 'F037. Fuga de información técnica'
+    finding_id = '457497318'
+    vulns_query = '''
+        query GetFindingVulnInfo($findingId: String!) {
+            finding(identifier: $findingId) {
+                vulnerabilities(state: "open") {
+                    historicTreatment {
+                        user
+                        treatment
+                    }
+                }
+            }
+        }
+    '''
+    vulns_data = {'query': vulns_query, 'variables': {'findingId': finding_id}}
+    result = await _get_result_async(vulns_data)
+    assert 'errors' not in result
+    vulns = result['data']['finding']['vulnerabilities']
+    assert vulns[0]['historicTreatment'][-1]['treatment'] == 'NEW'
+
+    add_mutation = '''
+        mutation AddOrgFindingPolicy($findingName: String! $orgName: String!) {
+            addOrgFindingPolicy(
+                findingName: $findingName
+                organizationName: $orgName
+            ) {
+                success
+            }
+        }
+    '''
+    data = {
+        'query': add_mutation,
+        'variables': {'orgName': org_name, 'findingName': finding_name}
+    }
+    result = await _get_result_async(data, 'integratescustomer@gmail.com')
+    assert 'errors' not in result
+    assert result['data']['addOrgFindingPolicy']['success']
+
+    approver_user = 'integratesuser@gmail.com'
+    handle_mutation = '''
+        mutation HandleOrgFindingPolicyAcceptation(
+            $findingPolicyId: ID!
+            $orgName: String!
+            $status: OrganizationFindindPolicy!
+        ) {
+            handleOrgFindingPolicyAcceptation(
+                findingPolicyId: $findingPolicyId
+                organizationName: $orgName
+                status: $status
+            ) {
+                success
+            }
+        }
+    '''
+
+    finding_policy = await policies_domain.get_finding_policy_by_name(
+        org_name=org_name,
+        finding_name=finding_name.split('.')[0].lower(),
+    )
+    hande_acceptation_data = {
+        'query': handle_mutation,
+        'variables': {
+            'findingPolicyId': finding_policy.id,
+            'orgName': org_name,
+            'status': 'APPROVED'
+        }
+    }
+
+    result = await _get_result_async(
+        hande_acceptation_data,
+        stakeholder=approver_user
+    )    
+    assert 'errors' not in result
+    assert result['data']['handleOrgFindingPolicyAcceptation']['success']
+
+    result = await _get_result_async(
+        hande_acceptation_data,
+        stakeholder='integratescustomer@gmail.com'
+    )
+    assert 'errors' in result
+    assert result['errors'][0]['message'] == 'Access denied'
+
+    result = await _get_result_async(
+        hande_acceptation_data,
+        stakeholder=approver_user
+    )
+    assert 'errors' in result
+    assert result['errors'][0]['message'] == str(PolicyAlreadyHandled())
+
+    vulns_data = {'query': vulns_query, 'variables': {'findingId': finding_id}}
+    result = await _get_result_async(vulns_data)
+    assert 'errors' not in result
+    vulns = result['data']['finding']['vulnerabilities']
+    assert (
+        vulns[0]['historicTreatment'][-1]['treatment'] == 'ACCEPTED_UNDEFINED'
+    )
+    assert vulns[0]['historicTreatment'][-1]['user'] == approver_user
