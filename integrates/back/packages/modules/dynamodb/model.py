cf40a28fb6eb1e63d5eebfc274d467e13468639b
Andres Aguirre <aaguirre@fluidattacks.com>
2021-04-30 14:57:13 -0500

    integrates\feat(back): #4329.13 model get finding
    - Add function to get a finding by finding id and group name
    
    
    Former-commit-id: 0db3e6efbcfbfbedf287488aa81f5dd0151149ba

--- integrates/back/packages/modules/dynamodb/model.py
+++ integrates/back/packages/modules/dynamodb/model.py
@@ -10,6 +10,10 @@ from boto3.dynamodb.conditions import Attr, Key
 from dynamodb import historics, keys, operations
 from dynamodb.table import load_table
 from dynamodb.types import (
+    FindingItem,
+    FindingMetadata,
+    FindingState,
+    FindingVerification,
     GitRootCloning,
     GitRootItem,
     GitRootMetadata,
@@ -876,3 +880,104 @@ async def update_org_finding_policy_state(
     )
 
     await operations.batch_write_item(items=historic, table=TABLE)
+
+
+def _build_finding(
+    *,
+    item_id: str,
+    key_structure: PrimaryKey,
+    raw_items: Tuple[Item, ...],
+) -> FindingItem:
+    metadata = historics.get_metadata(
+        item_id=item_id,
+        key_structure=key_structure,
+        raw_items=raw_items
+    )
+    state = historics.get_latest(
+        item_id=item_id,
+        key_structure=key_structure,
+        historic_prefix='STATE',
+        raw_items=raw_items
+    )
+    verification = historics.get_latest(
+        item_id=item_id,
+        key_structure=key_structure,
+        historic_prefix='VERIFICATION',
+        raw_items=raw_items
+    )
+
+    return FindingItem(
+        metadata=FindingMetadata(
+            actor=metadata['actor'],
+            affected_systems=metadata['affected_systems'],
+            analyst_email=metadata['analyst_email'],
+            attack_vector_desc=metadata['attack_vector_desc'],
+            bts_url=metadata['bts_url'],
+            compromised_attributes=metadata['compromised_attributes'],
+            compromised_records=metadata['compromised_records'],
+            cvss_version=metadata['cvss_version'],
+            cwe_url=metadata['cwe_url'],
+            description=metadata['description'],
+            evidences=metadata['evidences'],
+            group_name=metadata['group_name'],
+            id=metadata['id'],
+            scenario=metadata['scenario'],
+            severity=metadata['severity'],
+            sorts=metadata['sorts'],
+            risk=metadata['risk'],
+            recommendation=metadata['recommendation'],
+            requirements=metadata['requirements'],
+            title=metadata['title'],
+            threat=metadata['threat'],
+            type=metadata['type'],
+        ),
+        state=FindingState(
+            modified_by=state['modified_by'],
+            modified_date=state['modified_date'],
+            source=state['source'],
+            status=state['status'],
+        ),
+        verification=FindingVerification(
+            comment_id=verification['comment_id'],
+            modified_by=verification['modified_by'],
+            modified_date=verification['modified_date'],
+            status=verification['status'],
+            vuln_uuids=verification['vuln_uuids'],
+        )
+    )
+
+
+async def get_finding(
+    *,
+    group_name: str,
+    finding_id: str
+) -> Optional[FindingItem]:
+    primary_key = keys.build_key(
+        facet=TABLE.facets['finding_metadata'],
+        values={'group_name': group_name, 'id': finding_id},
+    )
+
+    index = TABLE.indexes['inverted_index']
+    key_structure = index.primary_key
+    results = await operations.query(
+        condition_expression=(
+            Key(key_structure.partition_key).eq(primary_key.sort_key) &
+            Key(key_structure.sort_key).begins_with(primary_key.partition_key)
+        ),
+        facets=(
+            TABLE.facets['finding_metadata'],
+            TABLE.facets['finding_state'],
+            TABLE.facets['finding_verification'],
+        ),
+        index=index,
+        table=TABLE
+    )
+
+    if results:
+        return _build_finding(
+            item_id=primary_key.partition_key,
+            key_structure=key_structure,
+            raw_items=results
+        )
+
+    return None
