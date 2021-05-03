fb72dbd4d893a59402f44803d02816f313c2955d
Andres Aguirre <aaguirre@fluidattacks.com>
2021-05-03 13:15:34 -0500

    integrates\feat(back): #4329.14 finding types
    - Add types for findings
    
    
    Former-commit-id: b5a2abe45743f8f6216e108033d71c5066645f0b

---
+++ integrates/back/packages/modules/data_containers/finding.py
@@ -0,0 +1,86 @@
+# Standard
+from typing import NamedTuple, Union
+
+
+class FindingEvidence(NamedTuple):
+    description: str
+    modified_date: str
+    url: str
+
+
+class FindingEvidences(NamedTuple):
+    animation: FindingEvidence
+    evidence1: FindingEvidence
+    evidence2: FindingEvidence
+    evidence3: FindingEvidence
+    evidence4: FindingEvidence
+    evidence5: FindingEvidence
+    exploitation: FindingEvidence
+
+
+class Finding20Severity(NamedTuple):
+    access_complexity: float
+    access_vector: float
+    authentication: float
+    availability_impact: float
+    availability_requirement: float
+    collateral_damage_potential: float
+    confidence_level: float
+    confidentiality_impact: float
+    confidentiality_requirement: float
+    exploitability: float
+    finding_distribution: float
+    integrity_impact: float
+    integrity_requirement: float
+    resolution_level: float
+
+
+class Finding31Severity(NamedTuple):
+    attack_complexity: float
+    attack_vector: float
+    availability_impact: float
+    availability_requirement: float
+    confidentiality_impact: float
+    confidentiality_requirement: float
+    exploitability: float
+    integrity_impact: float
+    integrity_requirement: float
+    modified_attack_complexity: float
+    modified_attack_vector: float
+    modified_availability_impact: float
+    modified_confidentiality_impact: float
+    modified_integrity_impact: float
+    modified_privileges_required: float
+    modified_user_interaction: float
+    modified_severity_scope: float
+    privileges_required: float
+    remediation_level: float
+    report_confidence: float
+    severity_scope: float
+    user_interaction: float
+
+
+class Finding(NamedTuple):
+    actor: str
+    affected_systems: str
+    analyst_email: str
+    attack_vector_desc: str
+    bts_url: str
+    compromised_attributes: str
+    compromised_records: int
+    cvss_version: float
+    cwe_url: str
+    description: str
+    evidences: FindingEvidences
+    group_name: str
+    id: str
+    scenario: str
+    severity: Union[Finding20Severity, Finding31Severity]
+    sorts: str
+    records: FindingEvidence
+    risk: str
+    recommendation: str
+    requirements: str
+    title: str
+    threat: str
+    type: str
