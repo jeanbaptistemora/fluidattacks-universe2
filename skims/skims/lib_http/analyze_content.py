0839067e96b47f002e05d56b637acf4fa9a7a262
Kevin Amado <kamado@fluidattacks.com>
2021-05-04 13:28:02 -0500

    skims\feat(back): #4382.94 build content vulns
    - Add a function to build content vulnerabilities
    
    
    Former-commit-id: 4ac7ff922caa480dcfda25a99b6b403b9e1a22e4

--- skims/skims/lib_http/analyze_content.py
+++ skims/skims/lib_http/analyze_content.py
@@ -1,8 +1,18 @@
 # Standard library
+from __future__ import (
+    annotations,
+)
 from typing import (
+    Any,
     Callable,
     Dict,
+    List,
     NamedTuple,
+)
+
+# Third party library
+from bs4.element import (
+    Tag,
 )
 
 # Local libraries
@@ -12,15 +22,73 @@ from lib_http.types import (
 from model import (
     core_model,
 )
+from utils.ctx import (
+    CTX,
+)
+from utils.encodings import (
+    serialize_namespace_into_vuln,
+)
+from utils.string import (
+    to_snippet_blocking,
+)
+from zone import (
+    t,
+)
 
 
-class Description(NamedTuple):
-    key: str
-    kwargs: Dict[str, str]
+class Location(NamedTuple):
+    column: int
+    description: str
+    line: int
+
+    @classmethod
+    def from_soup_tag(
+        cls: Any,
+        tag: Tag,
+        *desc: str,
+        **desc_kwargs: str,
+    ) -> Location:
+        return Location(
+            column=tag.sourcepos,
+            description=t(f"lib_http.analyze_content.{desc}", **desc_kwargs),
+            line=tag.sourceline,
+        )
 
 
 class ContentCheckCtx(NamedTuple):
     url: URLContext
+
+
+def build_vulnerabilities(
+    locations: List[Location],
+    finding: core_model.FindingEnum,
+    ctx: ContentCheckCtx,
+) -> core_model.Vulnerabilities:
+    return tuple(
+        core_model.Vulnerability(
+            finding=finding,
+            kind=core_model.VulnerabilityKindEnum.INPUTS,
+            state=core_model.VulnerabilityStateEnum.OPEN,
+            # Must start with home so integrates allows it
+            stream="home,response,content",
+            what=serialize_namespace_into_vuln(
+                kind=core_model.VulnerabilityKindEnum.INPUTS,
+                namespace=CTX.config.namespace,
+                what=ctx.url,
+            ),
+            where=f"{location.line}",
+            skims_metadata=core_model.SkimsVulnerabilityMetadata(
+                cwe=(finding.value.cwe,),
+                description=location.description,
+                snippet=to_snippet_blocking(
+                    column=location.column,
+                    content=ctx.url.content,
+                    line=location.line,
+                ),
+            ),
+        )
+        for location in locations
+    )
 
 
 def get_check_ctx(url: URLContext) -> ContentCheckCtx:
