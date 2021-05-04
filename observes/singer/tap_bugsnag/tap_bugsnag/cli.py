08bd357baf482b6a1e94c5e23b1938feca62f577
Daniel Murcia <dmurcia@fluidattacks.com>
2021-05-04 11:17:04 -0500

    observes\feat(back): #4349.15 execute stream
    - add executor
    - add cli interface
    - streams progress 0/9
    - today progress +0, ETA ? days
    
    
    Former-commit-id: 4377189c8044839959f2aa7c446a28581c987f5c

---
+++ observes/singer/tap_bugsnag/tap_bugsnag/cli.py
@@ -0,0 +1,46 @@
+# Standard libraries
+from typing import (
+    Optional,
+)
+
+# Third party libraries
+import click
+
+# Local libraries
+from tap_bugsnag import (
+    executor,
+)
+from tap_bugsnag.api import (
+    Credentials,
+)
+from tap_bugsnag.streams import (
+    SupportedStreams,
+)
+
+
+@click.command()
+@click.option("--api-key", type=str, required=True)
+@click.option("--all-streams", is_flag=True, default=False)
+@click.argument(
+    "name",
+    type=click.Choice(
+        [x.value for x in iter(SupportedStreams)], case_sensitive=False
+    ),
+    required=False,
+    default=None,
+)
+def stream(name: Optional[str], api_key: str, all_streams: bool) -> None:
+    creds = Credentials.new(api_key)
+    if name:
+        executor.stream(creds, name)
+    elif all_streams:
+        executor.stream_all(creds)
+
+
+@click.group()
+def main() -> None:
+    # cli group entrypoint
+    pass
+
+
+main.add_command(stream)
