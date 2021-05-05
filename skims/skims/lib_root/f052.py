ef922e13190dc06ce92ba61a53cf62152c266edf
Diego Restrepo <drestrepo@fluidattacks.com>
2021-05-05 15:20:37 -0500

    skims\feat(back): #4526.7 add insecure instances
    - insecure algorithm classes can be instantiated
    
    
    Former-commit-id: 084ac5964e1f1a25a9709c228f46e93da04a8e8a

--- skims/skims/lib_root/f052.py
+++ skims/skims/lib_root/f052.py
@@ -10,7 +10,7 @@ from model import (
 )
 
 
-def _csharp_yield_insecure_member_access(
+def _csharp_yield_member_access(
     graph_db: graph_model.GraphDB, members: Set[str]
 ) -> graph_model.GraphShardNodes:
     for shard in graph_db.shards_by_langauge(
@@ -23,6 +23,26 @@ def _csharp_yield_insecure_member_access(
         ):
             match = g.match_ast(shard.graph, member, "__0__")
             if shard.graph.nodes[match["__0__"]].get("label_text") in members:
+                yield shard, member
+
+
+def _csharp_yield_object_creation(
+    graph_db: graph_model.GraphDB, members: Set[str]
+) -> graph_model.GraphShardNodes:
+    for shard in graph_db.shards_by_langauge(
+        graph_model.GraphShardMetadataLanguage.CSHARP,
+    ):
+        for member in g.filter_nodes(
+            shard.graph,
+            nodes=shard.graph.nodes,
+            predicate=g.pred_has_labels(
+                label_type="object_creation_expression"
+            ),
+        ):
+            match = g.match_ast(shard.graph, member, "identifier")
+            if (identifier := match["identifier"]) and shard.graph.nodes[
+                identifier
+            ]["label_text"] in members:
                 yield shard, member
 
 
@@ -46,14 +66,16 @@ def csharp_insecure_hash(
         "SHA1Managed",
     }
 
+    def n_ids() -> graph_model.GraphShardNodes:
+        yield from _csharp_yield_member_access(graph_db, insecure_cyphers)
+        yield from _csharp_yield_object_creation(graph_db, insecure_cyphers)
+
     return get_vulnerabilities_from_n_ids(
         cwe=("310", "327"),
         desc_key="src.lib_path.f052.insecure_hash.description",
         desc_params=dict(lang="CSharp"),
         finding=FINDING,
-        graph_shard_nodes=_csharp_yield_insecure_member_access(
-            graph_db, insecure_cyphers
-        ),
+        graph_shard_nodes=n_ids(),
     )
 
 
@@ -70,14 +92,16 @@ def csharp_insecure_cipher(
         "RC2CryptoServiceProvider",
     }
 
+    def n_ids() -> graph_model.GraphShardNodes:
+        yield from _csharp_yield_member_access(graph_db, insecure_cyphers)
+        yield from _csharp_yield_object_creation(graph_db, insecure_cyphers)
+
     return get_vulnerabilities_from_n_ids(
         cwe=("310", "327"),
         desc_key="src.lib_path.f052.insecure_cipher.description",
         desc_params=dict(lang="CSharp"),
         finding=FINDING,
-        graph_shard_nodes=_csharp_yield_insecure_member_access(
-            graph_db, insecure_cyphers
-        ),
+        graph_shard_nodes=n_ids(),
     )
 
 
--- skims/test/data/lib_path/f052/csharp.cs
+++ skims/test/data/lib_path/f052/csharp.cs
@@ -11,6 +11,8 @@ namespace Cypher_Example
             HMACMD5 myAes = HMACMD5.Create();
             DES myAes = DES.Create();
             TripleDES myAes = TripleDES.Create();
+            var hashProvider3 = new SHA1Managed();
+            var hashProvider3 = new RC2CryptoServiceProvider();
         }
     }
 }
