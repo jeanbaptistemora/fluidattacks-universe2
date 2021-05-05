1b859cde27b4787d4f42f24683cd0d25b5b60081
Andres Aguirre <aaguirre@fluidattacks.com>
2021-05-05 15:48:13 -0500

    integrates\feat(back): #4329.21 domain create draft
    - Add domain function to create a draft
    - Change float by Decimal since dynamodb does not support
    float but decimal
    - Add some default values to finding types
    
    
    Former-commit-id: 85869dc031b6d83bc96d7b4abd2ad5ba1b550a28

---
+++ integrates/back/packages/modules/findings/domain/draft_new.py
@@ -0,0 +1,56 @@
+# Standard libraries
+import random
+from typing import Any
+
+# Local libraries
+from backend import util
+from custom_exceptions import InvalidDraftTitle
+from model.findings import create as findings_create
+from model.findings.types import (
+    Finding,
+    FindingState,
+)
+from newutils import (
+    datetime as datetime_utils,
+    findings as findings_utils,
+)
+
+
+async def create_draft(
+    context: Any,
+    group_name: str,
+    title: str,
+    **kwargs: Any
+) -> None:
+    if not findings_utils.is_valid_finding_title(title):
+        raise InvalidDraftTitle()
+
+    group_name = group_name.lower()
+    last_fs_id = 550000000
+    finding_id = str(random.randint(last_fs_id, 1000000000))
+    user_info = await util.get_jwt_content(context)
+    analyst_email = user_info['user_email']
+    source = util.get_source(context)
+    draft = Finding(
+        affected_systems=kwargs.get('affected_systems', ''),
+        analyst_email=analyst_email,
+        attack_vector_desc=kwargs.get('attack_vector_desc', ''),
+        cwe_url=kwargs.get('cwe_url', ''),
+        description=kwargs.get('description', ''),
+        group_name=group_name,
+        id=finding_id,
+        state=FindingState(
+            modified_by=analyst_email,
+            modified_date=datetime_utils.get_iso_date(),
+            source=source,
+            status='CREATED',
+        ),
+        risk=kwargs.get('risk', ''),
+        recommendation=kwargs.get('recommendation', ''),
+        requirements=kwargs.get('requirements', ''),
+        title=title,
+        threat=kwargs.get('threat', ''),
+        type=kwargs.get('type', ''),
+    )
+
+    await findings_create.create_finding(finding=draft)
--- integrates/back/packages/modules/model/findings/types.py
+++ integrates/back/packages/modules/model/findings/types.py
@@ -1,4 +1,5 @@
 # Standard
+from decimal import Decimal
 from typing import Tuple, NamedTuple, Union, Optional
 
 
@@ -18,92 +19,93 @@ class FindingVerification(NamedTuple):
 
 
 class FindingEvidence(NamedTuple):
-    description: str
-    modified_date: str
-    url: str
+    description: str = ''
+    modified_date: str = ''
+    url: str = ''
 
 
 class FindingEvidences(NamedTuple):
-    animation: FindingEvidence
-    evidence1: FindingEvidence
-    evidence2: FindingEvidence
-    evidence3: FindingEvidence
-    evidence4: FindingEvidence
-    evidence5: FindingEvidence
-    exploitation: FindingEvidence
+    animation: FindingEvidence = FindingEvidence()
+    evidence1: FindingEvidence = FindingEvidence()
+    evidence2: FindingEvidence = FindingEvidence()
+    evidence3: FindingEvidence = FindingEvidence()
+    evidence4: FindingEvidence = FindingEvidence()
+    evidence5: FindingEvidence = FindingEvidence()
+    exploitation: FindingEvidence = FindingEvidence()
 
 
 class FindingRecords(NamedTuple):
-    description: str
-    modified_date: str
-    url: str
+    description: str = ''
+    modified_date: str = ''
+    url: str = ''
 
 
 class Finding20Severity(NamedTuple):
-    access_complexity: float
-    access_vector: float
-    authentication: float
-    availability_impact: float
-    availability_requirement: float
-    collateral_damage_potential: float
-    confidence_level: float
-    confidentiality_impact: float
-    confidentiality_requirement: float
-    exploitability: float
-    finding_distribution: float
-    integrity_impact: float
-    integrity_requirement: float
-    resolution_level: float
+    access_complexity: Decimal = Decimal('0.0')
+    access_vector: Decimal = Decimal('0.0')
+    authentication: Decimal = Decimal('0.0')
+    availability_impact: Decimal = Decimal('0.0')
+    availability_requirement: Decimal = Decimal('0.0')
+    collateral_damage_potential: Decimal = Decimal('0.0')
+    confidence_level: Decimal = Decimal('0.0')
+    confidentiality_impact: Decimal = Decimal('0.0')
+    confidentiality_requirement: Decimal = Decimal('0.0')
+    exploitability: Decimal = Decimal('0.0')
+    finding_distribution: Decimal = Decimal('0.0')
+    integrity_impact: Decimal = Decimal('0.0')
+    integrity_requirement: Decimal = Decimal('0.0')
+    resolution_level: Decimal = Decimal('0.0')
 
 
 class Finding31Severity(NamedTuple):
-    attack_complexity: float
-    attack_vector: float
-    availability_impact: float
-    availability_requirement: float
-    confidentiality_impact: float
-    confidentiality_requirement: float
-    exploitability: float
-    integrity_impact: float
-    integrity_requirement: float
-    modified_attack_complexity: float
-    modified_attack_vector: float
-    modified_availability_impact: float
-    modified_confidentiality_impact: float
-    modified_integrity_impact: float
-    modified_privileges_required: float
-    modified_user_interaction: float
-    modified_severity_scope: float
-    privileges_required: float
-    remediation_level: float
-    report_confidence: float
-    severity_scope: float
-    user_interaction: float
+    attack_complexity: Decimal = Decimal('0.0')
+    attack_vector: Decimal = Decimal('0.0')
+    availability_impact: Decimal = Decimal('0.0')
+    availability_requirement: Decimal = Decimal('0.0')
+    confidentiality_impact: Decimal = Decimal('0.0')
+    confidentiality_requirement: Decimal = Decimal('0.0')
+    exploitability: Decimal = Decimal('0.0')
+    integrity_impact: Decimal = Decimal('0.0')
+    integrity_requirement: Decimal = Decimal('0.0')
+    modified_attack_complexity: Decimal = Decimal('0.0')
+    modified_attack_vector: Decimal = Decimal('0.0')
+    modified_availability_impact: Decimal = Decimal('0.0')
+    modified_confidentiality_impact: Decimal = Decimal('0.0')
+    modified_integrity_impact: Decimal = Decimal('0.0')
+    modified_privileges_required: Decimal = Decimal('0.0')
+    modified_user_interaction: Decimal = Decimal('0.0')
+    modified_severity_scope: Decimal = Decimal('0.0')
+    privileges_required: Decimal = Decimal('0.0')
+    remediation_level: Decimal = Decimal('0.0')
+    report_confidence: Decimal = Decimal('0.0')
+    severity_scope: Decimal = Decimal('0.0')
+    user_interaction: Decimal = Decimal('0.0')
 
 
 class Finding(NamedTuple):
-    actor: str
-    affected_systems: str
     analyst_email: str
-    attack_vector_desc: str
-    bts_url: str
-    compromised_attributes: str
-    compromised_records: int
-    cvss_version: str
-    cwe_url: str
-    description: str
-    evidences: FindingEvidences
     group_name: str
     id: str
-    scenario: str
-    severity: Union[Finding20Severity, Finding31Severity]
-    sorts: str
     state: FindingState
-    records: FindingRecords
-    risk: str
-    recommendation: str
-    requirements: str
     title: str
-    threat: str
-    type: str
-    verification: Optional[FindingVerification]
+    actor: str = ''
+    affected_systems: str = ''
+    attack_vector_desc: str = ''
+    bts_url: str = ''
+    compromised_attributes: str = ''
+    compromised_records: int = 0
+    cvss_version: str = '3.1'
+    cwe_url: str = ''
+    description: str = ''
+    evidences: FindingEvidences = FindingEvidences()
+    scenario: str = ''
+    severity: Union[Finding20Severity, Finding31Severity] = \
+        Finding31Severity()
+    sorts: str = 'No'
+    records: FindingRecords = FindingRecords()
+    recommendation: str = ''
+    requirements: str = ''
+    risk: str = ''
+    threat: str = ''
+    type: str = ''
+    verification: Optional[FindingVerification] = None
