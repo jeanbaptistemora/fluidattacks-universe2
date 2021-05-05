e9610282ca498f7a69823185e8b173920e007a27
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-05-05 09:48:03 -0500

    integrates\test(back): #3506.20 add finding_policies query
    - add test to query organization findings policies and move
    `test_add_org_finding_policy` to its own file
    - no-deltas-test
    
    
    Former-commit-id: 0c141ac6367d64dc5302f94bb14b2bb9e13b7e0f

--- integrates/back/tests/unit/entity_test/test_org_finding_policy.py
+++ integrates/back/tests/unit/entity_test/test_org_finding_policy.py
@@ -283,3 +283,71 @@ async def test_deactivate_org_finding_policy():
     vulns = result['data']['finding']['vulnerabilities']
     assert vulns[0]['historicTreatment'][-1]['treatment'] == 'NEW'
     assert vulns[0]['historicTreatment'][-1]['user'] == approver_user
+
+
+@pytest.mark.changes_db
+async def test_add_org_finding_policy():
+    org_name = 'okada'
+    fin_name = 'F031. Permisos excesivos'
+    query = '''
+        mutation AddOrgFindingPolicy(
+            $findingName: String!
+            $orgName: String!
+        ) {
+            addOrgFindingPolicy(
+                findingName: $findingName
+                organizationName: $orgName
+            ) {
+                success
+            }
+        }
+    '''
+
+    data = {
+        'query': query,
+        'variables': {'orgName': org_name, 'findingName': fin_name}
+    }
+    result = await _get_result_async(
+        data,
+        stakeholder='integratescustomer@gmail.com'
+    )
+    assert 'errors' not in result
+    assert result['data']['addOrgFindingPolicy']['success']
+
+    result = await _get_result_async(
+        data,
+        stakeholder='org_testuser5@gmail.com'
+    )
+    assert 'errors' in result
+    assert result['errors'][0]['message'] == 'Access denied'
+
+
+async def test_get_org_finding_policies():
+    id = '8b35ae2a-56a1-4f64-9da7-6a552683bf46'
+    name = 'F007. Cross site request forgery'
+    org_id = 'ORG#38eb8f25-7945-4173-ab6e-0af4ad8b7ef3'
+    status = 'APPROVED'
+    query = '''
+        query GetOrganizationPolicies($organizationId: String!) {
+            organization(organizationId: $organizationId) {
+                id
+                findingPolicies {
+                    id
+                    name
+                    status
+                    lastStatusUpdate
+                }
+            }
+        }
+    '''
+    data = {'query': query, 'variables': {'organizationId': org_id}}
+    result = await _get_result_async(data)
+
+    assert 'errors' not in result
+    assert result['data']['organization']['id'] == org_id
+    assert len(result['data']['organization']['findingPolicies']) == 1
+    assert result['data']['organization']['findingPolicies'][0]['id'] == id
+    assert result['data']['organization']['findingPolicies'][0]['name'] == name
+    assert (
+        result['data']['organization']['findingPolicies'][0]['status'] == status
+    )
