f4830ec5d330d34c3bf3d09e35796a23a36004f8
Kevin Amado <kamado@fluidattacks.com>
2021-05-10 13:08:49 -0500

    integrates\feat(build): #4551.1 dev env linters
    - Add linters to the dev environment
    
    
    Former-commit-id: 9a05dd72aae63c6e6c94cd2f683b592e02693c7a

--- makes/packages/makes/dev/integrates/back/default.nix
+++ makes/packages/makes/dev/integrates/back/default.nix
@@ -1,4 +1,5 @@
-{ makeTemplate
+{ lintPython
+, makeTemplate
 , packages
 , path
 , ...
@@ -6,6 +7,11 @@
 makeTemplate {
   arguments = {
     envIntegratesBackEnv = packages.integrates.back.env;
+  };
+  searchPaths = {
+    envSources = [
+      lintPython
+    ];
   };
   name = "makes-dev-integrates-back";
   template = path "/makes/packages/makes/dev/integrates/back/template.sh";
