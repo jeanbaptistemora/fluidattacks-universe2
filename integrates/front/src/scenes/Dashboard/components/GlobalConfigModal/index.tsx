4dedf80715abf016109118f6277eb798f01ab0fe
Jonathan Mesa <jmesa@fluidattacks.com>
2021-05-11 12:34:50 -0500

    integrates\feat(front): #4443.23 daily digest modal
    - add global config modal
    - no-deltas-test
    
    
    Former-commit-id: 427ea049e333709fab04e069e501f8adf83260dc

---
+++ integrates/front/src/scenes/Dashboard/components/GlobalConfigModal/index.tsx
@@ -0,0 +1,161 @@
+import { useMutation, useQuery } from "@apollo/client";
+import type { ApolloError } from "@apollo/client";
+import { Form, Formik } from "formik";
+import type { GraphQLError } from "graphql";
+import _ from "lodash";
+import React, { useCallback, useState } from "react";
+
+import { Button } from "components/Button";
+import { Modal } from "components/Modal";
+import { SwitchButton } from "components/SwitchButton";
+import { TooltipWrapper } from "components/TooltipWrapper";
+import {
+  SUBSCRIBE_TO_ENTITY_REPORT,
+  SUBSCRIPTIONS_TO_ENTITY_REPORT,
+} from "scenes/Dashboard/components/GlobalConfigModal/queries";
+import type { ISubscriptionsToEntityReport } from "scenes/Dashboard/components/GlobalConfigModal/types";
+import {
+  ButtonToolbar,
+  Col100,
+  Col40,
+  ControlLabel,
+  FormGroup,
+  Row,
+} from "styles/styledComponents";
+import { Logger } from "utils/logger";
+import { msgError } from "utils/notifications";
+import { translate } from "utils/translations/translate";
+
+interface IGlobalConfigModalProps {
+  open: boolean;
+  onClose: () => void;
+}
+
+const GlobalConfigModal: React.FC<IGlobalConfigModalProps> = (
+  props: IGlobalConfigModalProps
+): JSX.Element => {
+  const { onClose, open } = props;
+  const [isDigestEnabled, setDigestSubscription] = useState(false);
+
+  const { data: dataSubscriptions } = useQuery<ISubscriptionsToEntityReport>(
+    SUBSCRIPTIONS_TO_ENTITY_REPORT,
+    {
+      onCompleted: (result: ISubscriptionsToEntityReport): void => {
+        setDigestSubscription(
+          _.size(
+            _.filter(result.me.subscriptionsToEntityReport, {
+              entity: "DIGEST",
+            })
+          ) > 0
+        );
+      },
+      onError: ({ graphQLErrors }: ApolloError): void => {
+        graphQLErrors.forEach((error: GraphQLError): void => {
+          msgError(translate.t("configuration.errorText"));
+          Logger.warning(
+            "An error occurred loading the subscriptions info",
+            error
+          );
+        });
+      },
+    }
+  );
+
+  const [subscribe, { loading }] = useMutation(SUBSCRIBE_TO_ENTITY_REPORT, {
+    onError: (updateError: ApolloError): void => {
+      updateError.graphQLErrors.forEach(({ message }: GraphQLError): void => {
+        msgError(translate.t("configuration.errorText"));
+        Logger.warning(
+          "An error occurred changing the subscriptions info",
+          message
+        );
+      });
+    },
+  });
+
+  const onSubmitChange = useCallback(
+    (values: { digest: boolean }): void => {
+      void subscribe({
+        variables: {
+          frequency: values.digest ? "DAILY" : "NEVER",
+          reportEntity: "DIGEST",
+          reportSubject: "ALL_GROUPS",
+        },
+      });
+      setDigestSubscription(values.digest);
+    },
+    [subscribe, setDigestSubscription]
+  );
+
+  if (_.isUndefined(dataSubscriptions) || _.isEmpty(dataSubscriptions)) {
+    return <div />;
+  }
+
+  return (
+    <React.StrictMode>
+      <Modal headerTitle={translate.t("configuration.title")} open={open}>
+        <Formik
+          enableReinitialize={true}
+          initialValues={{
+            digest: isDigestEnabled,
+          }}
+          name={"config"}
+          onSubmit={onSubmitChange}
+        >
+          {({ values, dirty, setFieldValue }): JSX.Element => {
+            function onHandleDigestChange(): void {
+              setFieldValue("digest", !values.digest);
+            }
+
+            return (
+              <Form>
+                <Row>
+                  <Col40>
+                    <TooltipWrapper
+                      id={"config.digest"}
+                      message={translate.t("configuration.digest.tooltip")}
+                      placement={"top"}
+                    >
+                      <FormGroup>
+                        <ControlLabel>
+                          {translate.t("configuration.digest.label")}
+                        </ControlLabel>
+                        <SwitchButton
+                          checked={values.digest}
+                          disabled={false}
+                          name={"digest"}
+                          offlabel={translate.t(
+                            "configuration.digest.unsubscribed"
+                          )}
+                          onChange={onHandleDigestChange}
+                          onlabel={translate.t(
+                            "configuration.digest.subscribed"
+                          )}
+                        />
+                      </FormGroup>
+                    </TooltipWrapper>
+                  </Col40>
+                </Row>
+                <hr />
+                <Row>
+                  <Col100>
+                    <ButtonToolbar>
+                      <Button id={"config-close"} onClick={onClose}>
+                        {translate.t("configuration.close")}
+                      </Button>
+                      <Button disabled={!dirty || loading} type={"submit"}>
+                        {translate.t("configuration.confirm")}
+                      </Button>
+                    </ButtonToolbar>
+                  </Col100>
+                </Row>
+              </Form>
+            );
+          }}
+        </Formik>
+      </Modal>
+    </React.StrictMode>
+  );
+};
+
+export { IGlobalConfigModalProps, GlobalConfigModal };
