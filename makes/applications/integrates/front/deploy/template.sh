56984c16c5c1ad8b9f60d5ce6d79888e46b99def
David Acevedo <dacevedo@fluidattacks.com>
2021-05-13 13:36:22 -0500

    makes\fix(build): #4572.4 front deploy
    - removed usage of an old input variable
    
    
    Former-commit-id: 32e6bd29e7ea60ebb14e4f314e3ef18649bdc308

--- makes/applications/integrates/front/deploy/template.sh
+++ makes/applications/integrates/front/deploy/template.sh
@@ -14,7 +14,8 @@ function deploy {
   local branch="${3}"
   local bugsnag_key='99a64555a50340cfa856f6623c6bf35d'
   local deployment_date
-  local base_url="integrates.front.${env}.fluidattacks.com/${branch}"
+  local bucket_name="integrates.front.${env}.fluidattacks.com"
+  local base_url="${bucket_name}/${branch}"
   local cached_urls=(
     "https://${base_url}/static/dashboard/app-bundle.min.js"
     "https://${base_url}/static/dashboard/app-style.min.css"
@@ -30,13 +31,13 @@ function deploy {
     &&  _replace app '__CI_COMMIT_REF_NAME__' "${branch}" \
     &&  _replace app '__CI_COMMIT_SHA__' "${CI_COMMIT_SHA}" \
     &&  _replace app '__CI_COMMIT_SHORT_SHA__' "${CI_COMMIT_SHORT_SHA}" \
-    &&  _replace app '__INTEGRATES_BUCKET_NAME__' "integrates.front.${endpoint}.fluidattacks.com" \
+    &&  _replace app '__INTEGRATES_BUCKET_NAME__' "${bucket_name}" \
     &&  _replace app '__INTEGRATES_DEPLOYMENT_DATE__' "${deployment_date}" \
     &&  aws_s3_sync \
           app \
           "s3://${base_url}/" \
           --delete \
-    &&  makes-announce-bugsnag "${bugsnag_key}" "${endpoint}" \
+    &&  makes-announce-bugsnag "${bugsnag_key}" "${env}" \
     &&  cloudflare_purge_cache \
           "${CLOUDFLARE_API_TOKEN}" \
           "fluidattacks.com" \
