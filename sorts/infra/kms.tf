fa9ed1bf9719475456396083cc3e6b28275fa886
Mario Varas <mvaras@fluidattacks.com>
2021-05-13 14:04:09 -0500

    sorts\feat(infra): #4581.2 create kms alias
    - Created new sorts kms
    
    
    Former-commit-id: c942fe929ece32d94a7c0d2dfde310b24bec1642

---
+++ sorts/infra/kms.tf
@@ -0,0 +1,82 @@
+data "aws_iam_policy_document" "key_sorts" {
+  statement {
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/makes_prod",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/FLUIDServes_TF",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user_provision/sorts_prod",
+      ]
+    }
+    actions = [
+      "kms:*",
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user_provision/sorts_prod",
+      ]
+    }
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user_provision/sorts_prod",
+      ]
+    }
+    actions = [
+      "kms:CreateGrant",
+      "kms:ListGrants",
+      "kms:RevokeGrant",
+    ]
+    resources = [
+      "*"
+    ]
+    condition {
+      test     = "Bool"
+      variable = "kms:GrantIsForAWSResource"
+      values = [
+        "true",
+      ]
+    }
+  }
+}
+
+resource "aws_kms_key" "key_sorts" {
+  policy                  = data.aws_iam_policy_document.key_sorts.json
+  deletion_window_in_days = 30
+  is_enabled              = true
+
+  tags = {
+    "Name"               = "sorts-kms"
+    "management:type"    = "production"
+    "management:product" = "sorts"
+  }
+}
+
+resource "aws_kms_alias" "key_sorts" {
+  name          = "alias/sorts_prod"
+  target_key_id = aws_kms_key.key_sorts.key_id
+}
