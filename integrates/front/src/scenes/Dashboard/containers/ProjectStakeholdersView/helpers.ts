7bf6587ba2c1678d82baeef474a7b74615e4cf1b
Juan Chaves <jchaves@fluidattacks.com>
2021-05-14 14:08:56 -0500

    integrates\refac(front): #4570.7 reduce cognitive complexity
    - Moved some error handlers into their own helper functions
    - speed: 7 issues / 2 days = 3 issues/day
    todo: 16 issues
    eta: 16 / 3 = 5 days
    
    Former-commit-id: 668d8701938f059cd97a49581d378da13567ce44

---
+++ integrates/front/src/scenes/Dashboard/containers/ProjectStakeholdersView/helpers.ts
@@ -0,0 +1,89 @@
+import type { ApolloError, ObservableQuery } from "@apollo/client";
+import type { GraphQLError } from "graphql";
+
+import { Logger } from "utils/logger";
+import { msgError } from "utils/notifications";
+import { translate } from "utils/translations/translate";
+
+const handleGrantError = (grantError: ApolloError): void => {
+  grantError.graphQLErrors.forEach(({ message }: GraphQLError): void => {
+    switch (message) {
+      case "Exception - Email is not valid":
+        msgError(translate.t("validations.email"));
+        break;
+      case "Exception - Invalid field in form":
+        msgError(translate.t("validations.invalidValueInField"));
+        break;
+      case "Exception - Invalid characters":
+        msgError(translate.t("validations.invalidChar"));
+        break;
+      case "Exception - Invalid phone number in form":
+        msgError(translate.t("validations.invalidPhoneNumberInField"));
+        break;
+      case "Exception - Invalid email address in form":
+        msgError(translate.t("validations.invalidEmailInField"));
+        break;
+      case "Exception - Groups without an active Fluid Attacks service " +
+        "can not have Fluid Attacks staff":
+        msgError(
+          translate.t("validations.fluidAttacksStaffWithoutFluidAttacksService")
+        );
+        break;
+      case "Exception - Groups with any active Fluid Attacks service " +
+        "can only have Hackers provided by Fluid Attacks":
+        msgError(
+          translate.t("validations.noFluidAttacksHackersInFluidAttacksService")
+        );
+        break;
+      case "Exception - The stakeholder has been granted access to the group previously":
+        msgError(translate.t("validations.stakeholderHasGroupAccess"));
+        break;
+      default:
+        msgError(translate.t("groupAlerts.errorTextsad"));
+        Logger.warning(
+          "An error occurred adding stakeholder to project",
+          grantError
+        );
+    }
+  });
+};
+
+const handleEditError = (
+  editError: ApolloError,
+  refetch: ObservableQuery["refetch"]
+): void => {
+  editError.graphQLErrors.forEach(({ message }: GraphQLError): void => {
+    switch (message) {
+      case "Exception - Invalid field in form":
+        msgError(translate.t("validations.invalidValueInField"));
+        break;
+      case "Exception - Invalid characters":
+        msgError(translate.t("validations.invalidChar"));
+        break;
+      case "Exception - Invalid phone number in form":
+        msgError(translate.t("validations.invalidPhoneNumberInField"));
+        break;
+      case "Exception - Groups without an active Fluid Attacks service " +
+        "can not have Fluid Attacks staff":
+        msgError(
+          translate.t("validations.fluidAttacksStaffWithoutFluidAttacksService")
+        );
+        break;
+      case "Exception - Groups with any active Fluid Attacks service " +
+        "can only have Hackers provided by Fluid Attacks":
+        msgError(
+          translate.t("validations.noFluidAttacksHackersInFluidAttacksService")
+        );
+        break;
+      case "Access denied or stakeholder not found":
+        msgError(translate.t("groupAlerts.expiredInvitation"));
+        void refetch();
+        break;
+      default:
+        msgError(translate.t("groupAlerts.errorTextsad"));
+        Logger.warning("An error occurred editing user", editError);
+    }
+  });
+};
+
+export { handleGrantError, handleEditError };
