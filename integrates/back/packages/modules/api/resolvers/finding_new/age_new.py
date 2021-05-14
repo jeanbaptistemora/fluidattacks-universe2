5bc35ae0eaafb03a8ee818ff9932cf96e54e3704
Andres Aguirre <aaguirre@fluidattacks.com>
2021-05-14 13:21:08 -0500

    integrates\feat(back): #4329.37 finding indicator resolvers
    - Add resolvers for the unreliable indicators
    - Remove the state resolver that get all the finding vulns
    
    
    Former-commit-id: 45888486d97f0706fff209eeefd8ffd6d1f2b745

---
+++ integrates/back/packages/modules/api/resolvers/finding_new/age_new.py
@@ -0,0 +1,11 @@
+from graphql.type.definition import GraphQLResolveInfo
+
+from model.findings.types import Finding
+
+
+async def resolve(
+    parent: Finding,
+    _info: GraphQLResolveInfo,
+    **_kwargs: None
+) -> str:
+    return parent.unreliable_indicators.unreliable_age
