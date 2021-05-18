882fadd0085c793912da0b34029dbe9a9dd04f43
Jonathan Mesa <jmesa@fluidattacks.com>
2021-05-18 15:02:41 -0500

    makes\feat(back): #4443.36 add job for subs
    - add job for proccessing subs in aws
    
    
    Former-commit-id: fe00468a4097b59d750344bf6f5e3b1fed65d1ff

---
+++ makes/applications/integrates/subscriptions/user-to-entity-on-aws/default.nix
@@ -0,0 +1,16 @@
+{ computeOnAws
+, ...
+}:
+computeOnAws {
+  attempts = 3;
+  command = [ "./m" "integrates.subscriptions.user-to-entity" ];
+  jobname = "integrates-subscriptions-user-to-entity";
+  jobqueue = "dedicated_later";
+  name = "integrates-subscriptions-user-to-entity-on-aws";
+  product = "integrates";
+  secrets = [
+    "PRODUCT_API_TOKEN"
+  ];
+  timeout = 14400;
+  vcpus = 1;
+}
