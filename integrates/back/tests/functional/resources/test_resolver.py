678724725e73b809990a22a1b1877332aff9e1d3
Briam Agudelo <bagudelo@fluidattacks.com>
2021-05-19 17:12:14 -0500

    integrates\test(back): #4624.1 migrate func tests
    - Migrate closer role for resources resolver
    
    
    Former-commit-id: a33d2d527167929203b1b8cc338d933e4bf437c9

--- integrates/back/tests/functional/resources/test_resolver.py
+++ integrates/back/tests/functional/resources/test_resolver.py
@@ -12,7 +12,15 @@ from . import query
 
 @pytest.mark.asyncio
 @pytest.mark.resolver_test_group('resources')
-async def test_admin(populate: bool):
+@pytest.mark.parametrize(
+    ['email'],
+    [
+        ['admin@gmail.com'],
+        ['analyst@gmail.com'],
+        ['closer@gmail.com'],
+    ]
+)
+async def test_resources(populate: bool, email: str):
     assert populate
     files: List[Dict[str, str]] = [
         {
@@ -41,86 +49,7 @@ async def test_admin(populate: bool):
         }
     ]
     result: Dict[str, Any] = await query(
-        user='admin@gmail.com',
-        group='group1',
-    )
-    assert 'errors' not in result
-    assert result['data']['resources']['projectName'] == 'group1'
-    assert json.loads(result['data']['resources']['files']) == files
-
-
-@pytest.mark.asyncio
-@pytest.mark.resolver_test_group('resources')
-async def test_analyst(populate: bool):
-    assert populate
-    files: List[Dict[str, str]] = [
-        {
-            "description": "Test",
-            "file_name": "test.zip",
-            "uploader": "unittest@fluidattacks.com",
-            "upload_date": "2019-03-01 15:21"
-        },
-        {
-            "description": "Test",
-            "file_name": "shell.exe",
-            "uploader": "unittest@fluidattacks.com",
-            "upload_date": "2019-04-24 14:56"
-        },
-        {
-            "description": "Test",
-            "file_name": "shell2.exe",
-            "uploader": "unittest@fluidattacks.com",
-            "upload_date": "2019-04-24 14:59"
-        },
-        {
-            "description": "Test",
-            "file_name": "asdasd.py",
-            "uploader": "unittest@fluidattacks.com",
-            "upload_date": "2019-08-06 14:28"
-        }
-    ]
-    result: Dict[str, Any] = await query(
-        user='analyst@gmail.com',
-        group='group1',
-    )
-    assert 'errors' not in result
-    assert result['data']['resources']['projectName'] == 'group1'
-    assert json.loads(result['data']['resources']['files']) == files
-
-
-
-@pytest.mark.asyncio
-@pytest.mark.resolver_test_group('resources')
-async def test_closer(populate: bool):
-    assert populate
-    files: List[Dict[str, str]] = [
-        {
-            "description": "Test",
-            "file_name": "test.zip",
-            "uploader": "unittest@fluidattacks.com",
-            "upload_date": "2019-03-01 15:21"
-        },
-        {
-            "description": "Test",
-            "file_name": "shell.exe",
-            "uploader": "unittest@fluidattacks.com",
-            "upload_date": "2019-04-24 14:56"
-        },
-        {
-            "description": "Test",
-            "file_name": "shell2.exe",
-            "uploader": "unittest@fluidattacks.com",
-            "upload_date": "2019-04-24 14:59"
-        },
-        {
-            "description": "Test",
-            "file_name": "asdasd.py",
-            "uploader": "unittest@fluidattacks.com",
-            "upload_date": "2019-08-06 14:28"
-        }
-    ]
-    result: Dict[str, Any] = await query(
-        user='closer@gmail.com',
+        user=email,
         group='group1',
     )
     assert 'errors' not in result
