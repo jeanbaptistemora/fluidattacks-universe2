a4f8965656d0efb27b6ed95c7c3cdb2839141944
Daniel Murcia <dmurcia@fluidattacks.com>
2021-05-19 15:32:00 -0500

    observes\refac(build): #4378.25 batch stability
    - separate python env
    - refac runtime env
    
    
    Former-commit-id: 2265e09ad8b253e11c0b7b6b47ee58b8c183db11

--- makes/packages/observes/env/service-batch-stability/runtime/default.nix
+++ makes/packages/observes/env/service-batch-stability/runtime/default.nix
@@ -1,36 +1,11 @@
-{ buildPythonPackage
-, buildPythonRequirements
-, makeTemplate
-, nixpkgs
+{ makeTemplate
+, packages
 , path
 , ...
 }:
+with packages.observes.env;
 let
-  pythonRequirements = buildPythonRequirements {
-    name = "observes-env-runtime-batch-stability-python";
-    requirements = {
-      direct = [
-        "boto3==1.17.56"
-        "bugsnag==4.0.2"
-        "click==7.1.2"
-      ];
-      inherited = [
-        "botocore==1.20.56"
-        "jmespath==0.10.0"
-        "python-dateutil==2.8.1"
-        "s3transfer==0.4.2"
-        "six==1.15.0"
-        "urllib3==1.26.4"
-        "WebOb==1.8.7"
-      ];
-    };
-    python = nixpkgs.python38;
-  };
-  self = buildPythonPackage {
-    name = "observes-batch-stability";
-    packagePath = path "/observes/services/batch_stability";
-    python = nixpkgs.python38;
-  };
+  self = path "/observes/services/batch_stability";
 in
 makeTemplate {
   name = "observes-env-runtime-batch-stability";
@@ -39,12 +14,13 @@ makeTemplate {
       self
     ];
     envPaths = [
-      pythonRequirements
+      service-batch-stability.runtime.python
+    ];
+    envPythonPaths = [
       self
     ];
     envPython38Paths = [
-      pythonRequirements
-      self
+      service-batch-stability.runtime.python
     ];
   };
 }
