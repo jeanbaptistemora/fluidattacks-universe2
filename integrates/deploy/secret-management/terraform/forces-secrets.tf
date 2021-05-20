f2b19c5ee8c149bb0362ea3277628319796f80bf
Kevin Amado <kamado@fluidattacks.com>
2021-05-20 09:41:15 -0500

    integrates\fix(infra): #4628.1 use direct ref
    - Reference the alias arn via a tf object
    
    
    Former-commit-id: 46bdf9c8eac80d60cfba7322791c0a9b19ef856b

--- integrates/deploy/secret-management/terraform/forces-secrets.tf
+++ integrates/deploy/secret-management/terraform/forces-secrets.tf
@@ -1,7 +1,7 @@
 resource "aws_secretsmanager_secret" "forces_api_tokens" {
   for_each   = { for name in var.projects_forces : name => name }
   name       = "forces_token_${each.value}"
-  kms_key_id = "arn:aws:kms:us-east-1:${data.aws_caller_identity.current.account_id}:alias/forces_key_${each.value}"
+  kms_key_id = aws_kms_alias.keys_aliases[each.value].arn
 
   tags = {
     "Name"               = "integrates_api_token_forces"
