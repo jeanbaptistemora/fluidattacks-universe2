b7c94b3c2008175c22c797244016cfc487f8417f
Daniel Murcia <dmurcia@fluidattacks.com>
2021-05-20 10:46:02 -0500

    observes\feat(back): #4598.9 meta table
    - add meta table
    
    
    Former-commit-id: 98893c13ff5e0d144f3af129f5740602a61050f6

--- observes/common/postgres_client/postgres_client/table/common/__init__.py
+++ observes/common/postgres_client/postgres_client/table/common/__init__.py
@@ -1,8 +1,16 @@
 # Standard libraries
 from __future__ import annotations
 from typing import (
+    FrozenSet,
     NamedTuple,
 )
+
+# Local libraries
+from postgres_client.table.common.column import Column
+
+
+class InvalidPrimaryKey(Exception):
+    pass
 
 
 class TableID(NamedTuple):
@@ -12,3 +20,30 @@ class TableID(NamedTuple):
     @classmethod
     def new(cls, schema: str, table_name: str) -> TableID:
         return cls(schema=schema, table_name=table_name)
+
+
+class MetaTable(NamedTuple):
+    table_id: TableID
+    primary_keys: FrozenSet[str]
+    columns: FrozenSet[Column]
+    path: str
+
+    @classmethod
+    def new(
+        cls,
+        table_id: TableID,
+        primary_keys: FrozenSet[str],
+        columns: FrozenSet[Column],
+    ) -> MetaTable:
+        columns_names = [col.name for col in columns]
+        invalid_keys = list(
+            filter(lambda key: key not in columns_names, primary_keys)
+        )
+        if invalid_keys:
+            raise InvalidPrimaryKey(str(invalid_keys))
+        return cls(
+            table_id=table_id,
+            primary_keys=primary_keys,
+            columns=columns,
+            path=f'"{table_id.schema}"."{table_id.table_name}"',
+        )
