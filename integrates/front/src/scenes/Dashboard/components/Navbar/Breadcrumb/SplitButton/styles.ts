0fe2941d88bcd15aeb0d7b326c04365983d4b281
David Acevedo <dacevedo@fluidattacks.com>
2021-05-20 16:00:19 -0500

    integrates\refac(front): #4587 org breadcrumb
    - decreased caret padding
    
    
    Former-commit-id: 81eecd302b1ce2c70b4c7e5ad783a9ef8386e8c9

--- integrates/front/src/scenes/Dashboard/components/Navbar/Breadcrumb/SplitButton/styles.ts
+++ integrates/front/src/scenes/Dashboard/components/Navbar/Breadcrumb/SplitButton/styles.ts
@@ -15,7 +15,7 @@ const IconButton = styled.button.attrs(
     className,
     type,
   }): Partial<React.ButtonHTMLAttributes<HTMLButtonElement>> => ({
-    className: `br0 outline-0 ${className ?? ""}`,
+    className: `br0 outline-0 ph0 ${className ?? ""}`,
     type: type ?? "button",
   })
 )``;
