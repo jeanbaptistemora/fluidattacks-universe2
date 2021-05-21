489381b7d66a578102b929c7531b514dfa3623fa
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-05-21 07:23:49 -0500

    integrates\test(front): #3506.34 add graphql_error test
    - add graphql_error test for add_organization_finding_policy query
    
    
    Former-commit-id: 47f037acbde370a58f73d9f9b9194486b5341e60

--- integrates/front/src/scenes/Dashboard/containers/OrganizationPoliciesView/FindingPolicies/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/containers/OrganizationPoliciesView/FindingPolicies/index.spec.tsx
@@ -2,8 +2,10 @@ import { MockedProvider } from "@apollo/client/testing";
 import type { MockedResponse } from "@apollo/client/testing";
 import type { ReactWrapper } from "enzyme";
 import { mount } from "enzyme";
+import { GraphQLError } from "graphql";
 import React from "react";
 import { act } from "react-dom/test-utils";
+import { useTranslation } from "react-i18next";
 import { MemoryRouter, Route } from "react-router-dom";
 import waitForExpect from "wait-for-expect";
 
@@ -13,7 +15,7 @@ import {
   GET_ORGANIZATION_FINDINGS_TITLES,
 } from "scenes/Dashboard/containers/OrganizationPoliciesView/FindingPolicies/queries";
 import { GET_ORGANIZATION_POLICIES } from "scenes/Dashboard/containers/OrganizationPoliciesView/queries";
-import { msgSuccess } from "utils/notifications";
+import { msgError, msgSuccess } from "utils/notifications";
 
 jest.mock(
   "../../../../../utils/notifications",
@@ -21,6 +23,7 @@ jest.mock(
     const mockedNotifications: Dictionary<
       () => Dictionary
     > = jest.requireActual("../../../../../utils/notifications");
+    jest.spyOn(mockedNotifications, "msgError").mockImplementation();
     jest.spyOn(mockedNotifications, "msgSuccess").mockImplementation();
 
     return mockedNotifications;
@@ -159,6 +162,85 @@ describe("Organization findings policies view", (): void => {
 
         await waitForExpect((): void => {
           expect(msgSuccess).toHaveBeenCalledTimes(1);
+        });
+      }
+    );
+  });
+
+  it("add organization findings policies mutation message error", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    const mockMutation: MockedResponse = {
+      request: {
+        query: ADD_ORGANIZATION_FINDING_POLICY,
+        variables: {
+          name: "F060. Insecure exceptions",
+          organizationName: "okada",
+        },
+      },
+      result: {
+        errors: [
+          new GraphQLError(
+            "Exception - The finding name policy already exists"
+          ),
+        ],
+      },
+    };
+
+    const { t } = useTranslation();
+    const wrapper: ReactWrapper = mount(
+      <MemoryRouter initialEntries={["/orgs/okada/policies"]}>
+        <MockedProvider
+          addTypename={false}
+          mocks={[mockFindingTitleQuery, mockQuery, mockMutation]}
+        >
+          <Route path={"/orgs/:organizationName/policies"}>
+            <FindingPolicies
+              findingPolicies={[
+                {
+                  id: "97ad7167-51aa-4214-a612-16a833df6565",
+                  lastStatusUpdate: "2021-05-21T06:16:48",
+                  name: "F060. Insecure exceptions",
+                  status: "APPROVED",
+                },
+              ]}
+              organizationId={organizationId}
+            />
+          </Route>
+        </MockedProvider>
+      </MemoryRouter>
+    );
+
+    await act(
+      async (): Promise<void> => {
+        expect.hasAssertions();
+
+        await waitForExpect((): void => {
+          wrapper.update();
+
+          expect(wrapper).toHaveLength(1);
+        });
+      }
+    );
+
+    const name: ReactWrapper = wrapper.find({ name: "name" }).find("input");
+
+    name.simulate("change", {
+      target: { name: "name", value: "F060. Insecure exceptions" },
+    });
+
+    wrapper.find("Formik").simulate("submit");
+
+    await act(
+      async (): Promise<void> => {
+        expect.hasAssertions();
+
+        wrapper.update();
+
+        await waitForExpect((): void => {
+          expect(msgError).toHaveBeenCalledWith(
+            t("organization.tabs.policies.findings.errors.duplicateFinding")
+          );
         });
       }
     );
