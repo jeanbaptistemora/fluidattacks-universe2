d626e06c76360b10c347a542a01bd4518c610e66
John Perez <jperez@fluidattacks.com>
2021-05-20 19:33:10 -0500

    airs\fix(front): #3905.161 blog inner pages
    - Added Tags list component
    
    
    Former-commit-id: dd10cca2e2e53592a3f502d90c551fdcfc1216fb

---
+++ airs/new-front/src/components/TagsList/index.tsx
@@ -0,0 +1,90 @@
+/* eslint @typescript-eslint/no-confusing-void-expression:0 */
+import { Link, graphql, useStaticQuery } from "gatsby";
+import { decode } from "he";
+import React from "react";
+
+import {
+  BlogItemItem,
+  BlogItemList,
+  BlogItemListContainer,
+  BlogItemName,
+  BlogItemTitle,
+} from "../../styles/styledComponents";
+
+interface IData {
+  allAsciidoc: {
+    edges: [
+      {
+        node: {
+          fields: {
+            slug: string;
+          };
+          pageAttributes: {
+            tags: string;
+          };
+        };
+      }
+    ];
+  };
+}
+
+const TagsList: React.FC = (): JSX.Element => {
+  const data: IData = useStaticQuery(graphql`
+    query TagsList {
+      allAsciidoc(
+        filter: {
+          fields: { slug: { regex: "/blog/" } }
+          pageAttributes: { image: { regex: "" } }
+        }
+        sort: { fields: pageAttributes___date, order: DESC }
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            pageAttributes {
+              tags
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const tagsListRaw = data.allAsciidoc.edges
+    .map((edge): string[] => edge.node.pageAttributes.tags.split(", "))
+    .flat();
+
+  const countCoincidences: (tag: string) => number = (tag: string): number => {
+    const tagCount: number = tagsListRaw.filter((item): boolean => item === tag)
+      .length;
+
+    return tagCount;
+  };
+
+  const tagsSet = new Set(tagsListRaw);
+
+  return (
+    <React.Fragment>
+      <BlogItemTitle>{"Tags:"}</BlogItemTitle>
+      <BlogItemListContainer>
+        <BlogItemList>
+          {Array.from(tagsSet).map(
+            (tag): JSX.Element => (
+              <BlogItemItem key={tag}>
+                <Link to={`/blog/tags/${tag}`}>
+                  <BlogItemName>
+                    {`${decode(tag)} (${countCoincidences(tag)})`}
+                  </BlogItemName>
+                </Link>
+              </BlogItemItem>
+            )
+          )}
+        </BlogItemList>
+      </BlogItemListContainer>
+    </React.Fragment>
+  );
+};
+
+export { TagsList };
