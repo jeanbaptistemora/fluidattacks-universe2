ea372080d1d22a242d3e10ecac39d010e9efa165
John Perez <jperez@fluidattacks.com>
2021-05-20 20:16:10 -0500

    airs\fix(front): #3905.162 blog inner pages
    - Added Categories list component
    
    
    Former-commit-id: ec7d058a9807304c8069afce09cc6c6bcf6cde6a

---
+++ airs/new-front/src/components/CategoriesList/index.tsx
@@ -0,0 +1,93 @@
+/* eslint @typescript-eslint/no-confusing-void-expression:0 */
+import { Link, graphql, useStaticQuery } from "gatsby";
+import { decode } from "he";
+import React from "react";
+
+import {
+  BlogItemItem,
+  BlogItemList,
+  BlogItemListContainer,
+  BlogItemName,
+  BlogItemTitle,
+} from "../../styles/styledComponents";
+
+interface IData {
+  allAsciidoc: {
+    edges: [
+      {
+        node: {
+          fields: {
+            slug: string;
+          };
+          pageAttributes: {
+            category: string;
+          };
+        };
+      }
+    ];
+  };
+}
+
+const CategoriesList: React.FC = (): JSX.Element => {
+  const data: IData = useStaticQuery(graphql`
+    query CategoriesList {
+      allAsciidoc(
+        filter: {
+          fields: { slug: { regex: "/blog/" } }
+          pageAttributes: { image: { regex: "" } }
+        }
+        sort: { fields: pageAttributes___date, order: DESC }
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            pageAttributes {
+              category
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const categoriesListRaw = data.allAsciidoc.edges.map(
+    (edge): string => edge.node.pageAttributes.category
+  );
+
+  const countCoincidences: (category: string) => number = (
+    category: string
+  ): number => {
+    const categoriesCount: number = categoriesListRaw.filter(
+      (item): boolean => item === category
+    ).length;
+
+    return categoriesCount;
+  };
+
+  const categoriesSet = new Set(categoriesListRaw);
+
+  return (
+    <React.Fragment>
+      <BlogItemTitle>{"Categories:"}</BlogItemTitle>
+      <BlogItemListContainer>
+        <BlogItemList>
+          {Array.from(categoriesSet).map(
+            (category): JSX.Element => (
+              <BlogItemItem key={category}>
+                <Link to={`/blog/categories/${category}`}>
+                  <BlogItemName>
+                    {`${decode(category)} (${countCoincidences(category)})`}
+                  </BlogItemName>
+                </Link>
+              </BlogItemItem>
+            )
+          )}
+        </BlogItemList>
+      </BlogItemListContainer>
+    </React.Fragment>
+  );
+};
+
+export { CategoriesList };
