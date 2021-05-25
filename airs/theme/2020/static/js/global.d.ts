d7c6e6741d21c418dbe781f74f6d5ac24be1fb46
John Perez <jperez@fluidattacks.com>
2021-05-24 20:48:27 -0500

    airs\refac(front): #3905.189 delete pelican
    - Remove scripts and styles used in pelican
    - Remove jobs to test pelican code and styles
    
    
    Former-commit-id: 2f17e55938915c314a52ce7fe198011f6b7ecef8

--- airs/theme/2020/static/js/global.d.ts
+++
@@ -1,8 +0,0 @@
-interface IDictionary<T = {}> { [key: string]: T; }
-
-interface ILoggerAttr {
-  // tslint:disable-next-line: completed-docs
-  error(msg: string, extra?: unknown): void;
-  // tslint:disable-next-line: completed-docs
-  warning(msg: string, extra?: unknown): void;
-}
--- build/include/jobs/airs.sh
+++ build/include/jobs/airs.sh
@@ -1,35 +1,5 @@
 # shellcheck shell=bash
 
-function job_airs_test_lint_code {
-      helper_common_use_pristine_workdir \
-  &&  pushd airs \
-  &&  env_prepare_python_packages \
-  &&  helper_airs_list_touched_files | xargs pre-commit run -v --files \
-  &&  npm install --prefix theme/2020/ \
-  &&  echo '[INFO] Testing Pelican website'\
-  &&  npm run --prefix theme/2020/ lint \
-  &&  popd \
-  ||  return 1
-}
-
-function job_airs_test_lint_styles {
-  local err_count
-
-      pushd airs/theme/2020 \
-  &&  npm install \
-  &&  echo "[INFO] Running Stylelint to lint SCSS files" \
-  &&  if npm run lint:stylelint
-      then
-        echo '[INFO] All styles are ok!'
-      else
-            err_count="$(npx stylelint '**/*.scss' | wc -l || true)" \
-        &&  echo "[ERROR] ${err_count} errors found in styles!" \
-        &&  return 1
-      fi \
-  &&  popd \
-  || return 1
-}
-
 function job_airs_deploy_local {
       helper_common_use_pristine_workdir \
   &&  airs dev \
