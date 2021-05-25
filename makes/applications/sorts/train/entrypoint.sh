66700e5f864000196bb1c788c478dc3f849d45a7
Mario Varas <mvaras@fluidattacks.com>
2021-05-25 09:02:09 -0500

    sorts\feat(build): #4581.23 init db before train
    - Get redshift secrets + init db via script
    
    
    Former-commit-id: 0caee7483c31c4a332ec544d201791346e08703d

--- makes/applications/sorts/train/entrypoint.sh
+++ makes/applications/sorts/train/entrypoint.sh
@@ -5,7 +5,14 @@ function main {
   export SORTS_PROD_AWS_SECRET_ACCESS_KEY
 
       aws_login_prod 'sorts' \
+  &&  sops_export_vars 'sorts/secrets.yaml' \
+        'REDSHIFT_DATABASE' \
+        'REDSHIFT_HOST' \
+        'REDSHIFT_PASSWORD' \
+        'REDSHIFT_PORT' \
+        'REDSHIFT_USER' \
   &&  pushd sorts \
+    &&  python3.8 training/redshift/db.py init-db \
     &&  echo "[INFO] Preparing extracted features data..." \
     &&  python3.8 training/merge_features.py \
     &&  echo "[INFO] Initializing training..." \
