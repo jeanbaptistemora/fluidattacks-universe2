680a8e370a107088543c3249c09cffc1f51ef47e
Diego Restrepo <drestrepo@fluidattacks.com>
2021-05-27 14:31:46 -0500

    skims\refac(back): #4477.47 create lookups
    - move functions to common
    
    
    Former-commit-id: 0c64bf0b6210420d1a246df420a31f05d9f9d865

--- skims/skims/sast_symbolic_evaluation/lookup/__init__.py
+++ skims/skims/sast_symbolic_evaluation/lookup/__init__.py
@@ -6,11 +6,11 @@ from sast_symbolic_evaluation.types import (
     LookedUpClassField,
     LookedUpMethod,
 )
-from sast_symbolic_evaluation.lookup.java import (
-    lookup_class as _lookup_class_java,
-    lookup_field as _lookup_field_java,
-    lookup_method as _lookup_method_java,
-    lookup_shard_by_class as _lookup_shard_by_class_java,
+from sast_symbolic_evaluation.lookup.common import (
+    lookup_class as _lookup_class_common,
+    lookup_field as _lookup_field_common,
+    lookup_method as _lookup_method_common,
+    lookup_shard_by_class as _lookup_shard_by_class_common,
 )
 
 
@@ -19,7 +19,18 @@ def lookup_class(
     class_name: str,
 ) -> Optional[LookedUpClass]:
     if args.shard.metadata.language == GraphShardMetadataLanguage.JAVA:
-        return _lookup_class_java(args, class_name)
+        return _lookup_class_common(
+            args,
+            class_name,
+            GraphShardMetadataLanguage.JAVA,
+        )
+
+    if args.shard.metadata.language == GraphShardMetadataLanguage.CSHARP:
+        return _lookup_class_common(
+            args,
+            class_name,
+            GraphShardMetadataLanguage.CSHARP,
+        )
 
     return None
 
@@ -30,7 +41,20 @@ def lookup_field(
     field_class: Optional[str] = None,
 ) -> Optional[LookedUpClassField]:
     if args.shard.metadata.language == GraphShardMetadataLanguage.JAVA:
-        return _lookup_field_java(args, field_name, field_class)
+        return _lookup_field_common(
+            args,
+            field_name,
+            GraphShardMetadataLanguage.JAVA,
+            field_class,
+        )
+
+    if args.shard.metadata.language == GraphShardMetadataLanguage.CSHARP:
+        return _lookup_field_common(
+            args,
+            field_name,
+            GraphShardMetadataLanguage.CSHARP,
+            field_class,
+        )
 
     return None
 
@@ -41,7 +65,20 @@ def lookup_method(
     method_class: Optional[str] = None,
 ) -> Optional[LookedUpMethod]:
     if args.shard.metadata.language == GraphShardMetadataLanguage.JAVA:
-        return _lookup_method_java(args, method_name, method_class)
+        return _lookup_method_common(
+            args,
+            method_name,
+            GraphShardMetadataLanguage.JAVA,
+            method_class,
+        )
+
+    if args.shard.metadata.language == GraphShardMetadataLanguage.CSHARP:
+        return _lookup_method_common(
+            args,
+            method_name,
+            GraphShardMetadataLanguage.CSHARP,
+            method_class,
+        )
 
     return None
 
@@ -51,6 +88,17 @@ def lookup_shard_by_class(
     class_name: str,
 ) -> Optional[GraphShard]:
     if args.shard.metadata.language == GraphShardMetadataLanguage.JAVA:
-        return _lookup_shard_by_class_java(args, class_name)
+        return _lookup_shard_by_class_common(
+            args,
+            class_name,
+            GraphShardMetadataLanguage.JAVA,
+        )
+
+    if args.shard.metadata.language == GraphShardMetadataLanguage.CSHARP:
+        return _lookup_shard_by_class_common(
+            args,
+            class_name,
+            GraphShardMetadataLanguage.CSHARP,
+        )
 
     return None
--- skims/skims/sast_symbolic_evaluation/lookup/common.py
+++ skims/skims/sast_symbolic_evaluation/lookup/common.py
@@ -21,14 +21,16 @@ from utils.string import (
 def _lookup_class_in_shard(
     shard: graph_model.GraphShard,
     class_name: str,
+    language: graph_model.GraphShardMetadataLanguage,
 ) -> Optional[graph_model.GraphShardMetadataClass]:
     # First lookup the class in the current shard
-    if data := shard.metadata.java.classes.get(class_name):
+    language_data = getattr(shard.metadata, language.value)
+    if data := language_data.classes.get(class_name):
         return data
-    if data := shard.metadata.java.classes.get(f".{class_name}"):
+    if data := language_data.classes.get(f".{class_name}"):
         return data
-    for class_path, class_data in shard.metadata.java.classes.items():
-        qualified = shard.metadata.java.package + class_path
+    for class_path, class_data in language_data.classes.items():
+        qualified = language_data.package + class_path
 
         if class_name == qualified or qualified.endswith(f".{class_name}"):
             return class_data
@@ -39,27 +41,36 @@ def _lookup_class_in_shard(
 def lookup_shard_by_class(
     args: EvaluatorArgs,
     class_name: str,
+    language: graph_model.GraphShardMetadataLanguage,
 ) -> Optional[graph_model.GraphShard]:
-    if path := args.graph_db.shards_by_language_class["java"].get(class_name):
+    language_data = getattr(args.shard.metadata, language.value)
+
+    if path := args.graph_db.shards_by_language_class[language.value].get(
+        class_name
+    ):
         return args.graph_db.shards_by_path_f(path)
 
-    if path := args.graph_db.shards_by_language_class["java"].get(
+    if path := args.graph_db.shards_by_language_class[language.value].get(
         f".{class_name}"
     ):
         return args.graph_db.shards_by_path_f(path)
 
     # It can be access to a static field
     _class = ".".join(class_name.split(".")[:-1])
-    if path := args.graph_db.shards_by_language_class["java"].get(_class):
+    if path := args.graph_db.shards_by_language_class[language.value].get(
+        _class
+    ):
         return args.graph_db.shards_by_path_f(path)
 
     # la clase actual puyede estar en el shard actual
-    if class_name in args.shard.metadata.java.classes:
+    if class_name in language_data.classes:
         return args.shard
     # Is possible that the class does not have a package
     if class_name.startswith("."):
         class_name = class_name.replace(".", "", 1)
-    for key, path in args.graph_db.shards_by_language_class["java"].items():
+    for key, path in args.graph_db.shards_by_language_class[
+        language.value
+    ].items():
         if key.endswith(f".{class_name}"):
             return args.graph_db.shards_by_path_f(path)
 
@@ -69,18 +80,19 @@ def lookup_shard_by_class(
 def lookup_class(
     args: EvaluatorArgs,
     class_name: str,
+    language: graph_model.GraphShardMetadataLanguage,
 ) -> Optional[LookedUpClass]:
     # First lookup in the current shard
-    if data := _lookup_class_in_shard(args.shard, class_name):
+    if data := _lookup_class_in_shard(args.shard, class_name, language):
         return LookedUpClass(
             metadata=data,
             shard_path=args.shard.path,
         )
 
     # Now lookoup in other shards different than the current shard
-    if (shard := lookup_shard_by_class(args, class_name)) and (
+    if (shard := lookup_shard_by_class(args, class_name, language)) and (
         # pylint:disable=used-before-assignment
-        data := _lookup_class_in_shard(shard, class_name)
+        data := _lookup_class_in_shard(shard, class_name, language)
     ):
         return LookedUpClass(
             metadata=data,
@@ -93,10 +105,13 @@ def lookup_class(
 def _lookup_field_in_shard(
     shard: graph_model.GraphShard,
     field_name: str,
+    language: graph_model.GraphShardMetadataLanguage,
 ) -> Optional[graph_model.GraphShardMetadataClassField]:
-    for class_path, class_data in shard.metadata.java.classes.items():
+    language_data = getattr(shard.metadata, language.value)
+
+    for class_path, class_data in language_data.classes.items():
         for field_path, field_data in class_data.fields.items():
-            qualified = shard.metadata.java.package + class_path + field_path
+            qualified = language_data.package + class_path + field_path
 
             if qualified == field_name or (
                 field_name and qualified.endswith(field_name)
@@ -109,6 +124,7 @@ def _lookup_field_in_shard(
 def lookup_field(
     args: EvaluatorArgs,
     field_name: str,
+    language: graph_model.GraphShardMetadataLanguage,
     field_class: Optional[str] = None,
 ) -> Optional[LookedUpClassField]:
     # method_name can be an empty string
@@ -116,21 +132,21 @@ def lookup_field(
         return None
 
     # First lookup in the current shard
-    if data := _lookup_field_in_shard(args.shard, field_name):
+    if data := _lookup_field_in_shard(args.shard, field_name, language):
         return LookedUpClassField(data, args.shard.path)
 
     # Now lookoup in other shards different than the current shard
     if (
         field_class
-        and (shard := lookup_shard_by_class(args, field_class))
+        and (shard := lookup_shard_by_class(args, field_class, language))
         # pylint:disable=used-before-assignment
-        and (data := _lookup_field_in_shard(shard, field_name))
+        and (data := _lookup_field_in_shard(shard, field_name, language))
     ):
         return LookedUpClassField(data, shard.path)
 
     # Can be an static field
-    if (shard := lookup_shard_by_class(args, field_name)) and (
-        data := _lookup_field_in_shard(shard, field_name)
+    if (shard := lookup_shard_by_class(args, field_name, language)) and (
+        data := _lookup_field_in_shard(shard, field_name, language)
     ):
         return LookedUpClassField(data, shard.path)
 
@@ -140,11 +156,13 @@ def lookup_field(
 def _lookup_method_in_shard(
     shard: graph_model.GraphShard,
     method_name: str,
+    language: graph_model.GraphShardMetadataLanguage,
 ) -> Optional[graph_model.GraphShardMetadataClassMethod]:
     # First lookup the class in the current shard
-    for class_path, class_data in shard.metadata.java.classes.items():
+    language_data = getattr(shard.metadata, language.value)
+    for class_path, class_data in language_data.classes.items():
         for method_path, method_data in class_data.methods.items():
-            canon = shard.metadata.java.package + class_path + method_path
+            canon = language_data.package + class_path + method_path
 
             if canon == method_name or canon.endswith(f".{method_name}"):
                 return method_data
@@ -155,6 +173,7 @@ def _lookup_method_in_shard(
 def lookup_method(
     args: EvaluatorArgs,
     method_name: str,
+    language: graph_model.GraphShardMetadataLanguage,
     method_class: Optional[str] = None,
 ) -> Optional[LookedUpMethod]:
     # method_name can be an empty string
@@ -164,9 +183,9 @@ def lookup_method(
     # Lookup in other shards different than the current shard
     if (
         method_class
-        and (shard := lookup_shard_by_class(args, method_class))
+        and (shard := lookup_shard_by_class(args, method_class, language))
         # pylint:disable=used-before-assignment
-        and (data := _lookup_method_in_shard(shard, method_name))
+        and (data := _lookup_method_in_shard(shard, method_name, language))
     ):
         return LookedUpMethod(
             metadata=data,
@@ -176,7 +195,7 @@ def lookup_method(
         method_name = method_name.split("this.")[-1]
     # Lookup in the current shard
     if not method_class and (
-        data := _lookup_method_in_shard(args.shard, method_name)
+        data := _lookup_method_in_shard(args.shard, method_name, language)
     ):
         return LookedUpMethod(
             metadata=data,
@@ -188,8 +207,8 @@ def lookup_method(
     if (
         _method_name
         and _method_class
-        and (shard := lookup_shard_by_class(args, _method_class))
-        and (data := _lookup_method_in_shard(shard, method_name))
+        and (shard := lookup_shard_by_class(args, _method_class, language))
+        and (data := _lookup_method_in_shard(shard, method_name, language))
     ):
         return LookedUpMethod(
             metadata=data,
