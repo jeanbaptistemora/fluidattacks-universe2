a94ba04ae4e7877c0f2f337db26faa5a0770ddf1
Kevin Amado <kamado@fluidattacks.com>
2021-05-31 14:12:29 -0500

    makes\feat(infra): #4703.1 add machines
    - Increase the maximum cluster size so
      jobs are not queued for too much
    
    Closes #4703
    
    
    Former-commit-id: 5022e3876ea0bf8ec34a5ab96209d1f5ccff1569

--- makes/applications/makes/compute/src/terraform/aws_batch.tf
+++ makes/applications/makes/compute/src/terraform/aws_batch.tf
@@ -135,7 +135,7 @@ locals {
     },
     skims = {
       bid_percentage      = 100
-      max_vcpus           = 32
+      max_vcpus           = 48
       spot_iam_fleet_role = aws_iam_role.aws_ecs_instance_role.arn
       type                = "SPOT"
     },
