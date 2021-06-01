6995858a79414a75d152a822f5d772cce855e34f
Frank Aristizabal <faristizabal@fluidattacks.com>
2021-06-01 15:34:26 -0500

    airs\refac(front): #4184.1 update solutions tmpl
    - Add phrases for new solutions.
    
    
    Former-commit-id: 7c8efe6865c9fa14e7b6506ee4f89cb7f3bf71f9

--- airs/front/src/utils/translations/en.ts
+++ airs/front/src/utils/translations/en.ts
@@ -204,40 +204,52 @@ const pageTexts: ResourceKey = {
   solutions: {
     attackSimulation: {
       paragraph:
-        "You can discover through simulated attack scenarios whether your" +
+        "You can discover through simulated attack scenarios whether your " +
         "company’s cybersecurity is ready to respond to current threats.",
       subtitle: "Attack Simulation",
     },
     devSecOps: {
       paragraph:
-        "You can integrate security into your DevOps approach at any time in" +
+        "You can integrate security into your DevOps approach at any time in " +
         "your SDLC and ensure your team’s accountability.",
       subtitle: "DevSecOps",
     },
     ethicalHacking: {
       paragraph:
-        "You can give our ethical hackers permission to search for and" +
+        "You can give our ethical hackers permission to search for and " +
         "discover what unethical hackers might exploit in your IT systems.",
       subtitle: "EthicalHacking",
     },
     penetrationTesting: {
       paragraph:
-        "You can have simulated attacks on your systems performed by our" +
+        "You can have simulated attacks on your systems performed by our " +
         "pentesters to discover unseen vulnerabilities via automated tools.",
       subtitle: "Penetration Testing",
     },
     redTeaming: {
       paragraph:
-        "You can include our 100% Red Team in your projects to report" +
+        "You can include our 100% Red Team in your projects to report " +
         "security weaknesses to your development team and top management.",
       subtitle: "Red Teaming",
     },
+    secureCode: {
+      paragraph:
+        "You can verify if your code lines comply with various required standards " +
+        "and if there are security vulnerabilities to remediate.",
+      subtitle: "Secure Code Review",
+    },
     securityTesting: {
       paragraph:
-        "You can obtain detailed and reliable reports on security" +
-        "vulnerabilities in your IT infrastructure, applications, and source" +
+        "You can obtain detailed and reliable reports on security " +
+        "vulnerabilities in your IT infrastructure, applications, and source " +
         "code.",
       subtitle: "Security Testing",
+    },
+    vulnerabilityManagement: {
+      paragraph:
+        "You can examine data on the identification, classification and " +
+        "prioritization of vulnerabilities in your systems and software.",
+      subtitle: "Vulnerability Management",
     },
   },
 };
