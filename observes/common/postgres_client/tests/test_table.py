a3d7c065e62d17d50065671f43afa9f3a98c2708
Daniel Murcia <dmurcia@fluidattacks.com>
2021-06-02 09:05:15 -0500

    observes\test(back): #4598.25 merge table tests
    - merge table tests
    
    
    Former-commit-id: b7f67d21a3da06ae6b03e286df52da5eaeaa7317

--- observes/common/postgres_client/tests/test_table.py
+++ observes/common/postgres_client/tests/test_table.py
@@ -28,7 +28,25 @@ def setup_db(postgresql_my: Any) -> None:
         "INSERT INTO test_schema.table_number_one (Name) "
         "VALUES ('Juan Lopez');"
     )
+    temp_cur.execute("CREATE SCHEMA test_schema_2")
     postgresql_my.commit()
+
+
+@pytest.mark.timeout(15, method="thread")
+def test_create_like(postgresql_my: Any) -> None:
+    setup_db(postgresql_my)
+    db_client = client.new_test_client(postgresql_my)
+    blueprint_id = TableID(schema="test_schema", table_name="table_number_one")
+    new_table_id = TableID(schema="test_schema_2", table_name="the_table")
+    blueprint_io = DbTable.retrieve(db_client.cursor, blueprint_id, False)
+    new_table_io = DbTable.create_like(
+        db_client.cursor, blueprint_id, new_table_id, False
+    )
+    blueprint = unsafe_perform_io(blueprint_io)
+    new_table = unsafe_perform_io(new_table_io)
+    assert new_table.table.table_id != blueprint.table.table_id
+    assert new_table.table.columns == blueprint.table.columns
+    assert new_table.table.primary_keys == blueprint.table.primary_keys
 
 
 @pytest.mark.timeout(15, method="thread")
