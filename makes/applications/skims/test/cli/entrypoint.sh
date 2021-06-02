93e84bc0237f90dcd0417739b11a5a682f3d6256
Kevin Amado <kamado@fluidattacks.com>
2021-06-02 14:42:19 -0500

    skims\test(back): #4704.18 test expected date
    - Add a test for the cli expected date cmd
    
    
    Former-commit-id: dc1ef15ec46c020eeb083fafccd8079db3fc50d0

--- makes/applications/skims/test/cli/entrypoint.sh
+++ makes/applications/skims/test/cli/entrypoint.sh
@@ -15,6 +15,9 @@ function main {
   local output
 
       output="$(mktemp)" \
+  &&  assert skims expected-code-date --group continuoustest --finding-code F117 |& tee "${output}" \
+    &&  assert grep -HnP '^(0|1622\d+)$' "${output}" \
+    &&  assert grep -HnP 'Success' "${output}" \
   &&  assert skims language --group continuoustest |& tee "${output}" \
     &&  assert grep -HnP '^EN$' "${output}" \
     &&  assert grep -HnP 'Success' "${output}" \
