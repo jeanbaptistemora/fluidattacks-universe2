d2079ff11b361fb6783e7edfd7a351eaef17e959
Kevin Amado <kamado@fluidattacks.com>
2021-06-02 12:56:28 -0500

    skims\feat(back): #4704.14 expected code date
    - Add a CLI command in order to get the
      expected code date.
    - This code date is computed as the last
      reattack request on the given finding
    - With this we can ensure that skims
      only run with source code after this
      date
    
    
    Former-commit-id: abfb42cde73b909016b71ceb6d3a438af46fb2c0

---
+++ skims/skims/core/expected_code_date.py
@@ -0,0 +1,63 @@
+from datetime import (
+    datetime,
+)
+from integrates.dal import (
+    get_finding_vulnerabilities,
+    get_group_language,
+)
+from integrates.domain import (
+    get_closest_finding_ids,
+)
+from integrates.graphql import (
+    create_session,
+)
+from model import (
+    core_model,
+    time_model,
+)
+import sys
+from typing import (
+    Tuple,
+)
+
+
+async def main(
+    finding_code: str,
+    group: str,
+    token: str,
+) -> bool:
+    success: bool = True
+
+    create_session(api_token=token)
+
+    locale: core_model.LocalesEnum = await get_group_language(group)
+    finding_ids: Tuple[str, ...] = await get_closest_finding_ids(
+        finding=core_model.FINDING_ENUM_FROM_STR[finding_code],
+        group=group,
+        locale=locale,
+    )
+
+    max_reattack_date: datetime = time_model.min_posible()
+
+    for finding_id in finding_ids:
+        vulnerability: core_model.Vulnerability
+        vulnerabilities_store = await get_finding_vulnerabilities(
+            # Any finding works well
+            finding=core_model.FindingEnum.F004,
+            finding_id=finding_id,
+        )
+        async for vulnerability in vulnerabilities_store.iterate():
+            verification = vulnerability.integrates_metadata.verification
+
+            if (
+                verification
+                and verification[-1].state
+                == core_model.VulnerabilityVerificationStateEnum.REQUESTED
+                and verification[-1].date > max_reattack_date
+            ):
+                max_reattack_date = verification[-1].date
+
+    sys.stdout.write(str(int(max_reattack_date.timestamp())))
+    sys.stdout.write("\n")
+
+    return success
