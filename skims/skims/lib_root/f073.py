874579780554f97f084656b72d9ccdd02f48ea05
Andres Cuberos <acuberos@fluidattacks.com>
2021-06-04 13:28:56 -0500

    skims\feat(back): #4689.4 go switch without default
    - Find vulnerabilities in Go code related to the switch without
      default finding.
    
    
    Former-commit-id: 2932c387ec25c01ba013c55db4b3b9b58e61af16

--- skims/skims/lib_root/f073.py
+++ skims/skims/lib_root/f073.py
@@ -68,9 +68,40 @@ def c_sharp_switch_without_default(
     )
 
 
+def go_switch_without_default(
+    graph_db: graph_model.GraphDB,
+) -> core_model.Vulnerabilities:
+    def n_ids() -> graph_model.GraphShardNodes:
+        for shard in graph_db.shards_by_langauge(
+            graph_model.GraphShardMetadataLanguage.GO,
+        ):
+            for switch_id in g.filter_nodes(
+                shard.graph,
+                nodes=shard.graph.nodes,
+                predicate=g.pred_has_labels(
+                    label_type="type_switch_statement"
+                ),
+            ):
+                if not g.filter_nodes(
+                    shard.graph,
+                    nodes=g.adj_ast(shard.graph, switch_id, depth=1),
+                    predicate=g.pred_has_labels(label_type="default_case"),
+                ):
+                    yield shard, switch_id
+
+    return get_vulnerabilities_from_n_ids(
+        cwe=("478",),
+        desc_key="src.lib_path.f073.switch_no_default",
+        desc_params={},
+        finding=FINDING,
+        graph_shard_nodes=n_ids(),
+    )
+
+
 # Constants
 FINDING: core_model.FindingEnum = core_model.FindingEnum.F073
 QUERIES: graph_model.Queries = (
     (FINDING, java_switch_without_default),
     (FINDING, c_sharp_switch_without_default),
+    (FINDING, go_switch_without_default),
 )
