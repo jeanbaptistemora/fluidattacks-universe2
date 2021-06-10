fa43ef8267c3dcaccc931e263c3e766e4b0929c3
Mario Varas <mvaras@fluidattacks.com>
2021-06-10 11:31:05 -0500

    makes\refac(build): #4764.5 rm unneeded invocation
    - Made as a separated job now
    
    
    Former-commit-id: 5324ec91dd612149e0188d8a85c6ccc930eaf381

--- makes/applications/sorts/extract-features/entrypoint.sh
+++ makes/applications/sorts/extract-features/entrypoint.sh
@@ -41,9 +41,7 @@ function main {
   use_git_repo_services \
     && groups_file="$(mktemp)" \
     && ls -1 groups > "${groups_file}" \
-    && execute_chunk_parallel extract_features "${groups_file}" \
-    && echo "[INFO] Preparing extracted features data..." \
-    && python3.8 training/merge_features.py
+    && execute_chunk_parallel extract_features "${groups_file}"
 
 }
 
