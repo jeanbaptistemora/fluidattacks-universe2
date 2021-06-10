be5d83a8522e877bfa29f56e9ca7ebda852f0164
Felipe Ruiz <fruiz@fluidattacks.com>
2021-06-09 19:05:09 -0500

    docs\feat(build): #4770.2 docs writing guidelines
    -I added the writing folder to linter.
    
    
    Former-commit-id: 03e213af27bdebe4157c23dd6a72da05abb4fc39

--- makes/packages/docs/lint/default.nix
+++ makes/packages/docs/lint/default.nix
@@ -8,6 +8,7 @@ makeDerivation {
     envSrcDocsAbout = path "/docs/src/docs/about/";
     envSrcDocsSquad = path "/docs/src/docs/squad/";
     envSrcDocsMachine = path "/docs/src/docs/machine/";
+    envSrcDocsWriting = path "/docs/src/docs/writing/";
   };
   builder = path "/makes/packages/docs/lint/builder.sh";
   name = "docs-lint";
