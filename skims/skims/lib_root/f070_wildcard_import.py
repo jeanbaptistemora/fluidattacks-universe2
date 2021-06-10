560441b386584244d6aff1f881d81015a3b6c0c0
Andres Cuberos <acuberos@fluidattacks.com>
2021-06-09 22:12:41 -0500

    skims\feat(back): #4689.6 go wildcard imports
    - Support method to report wildcard imports in Go.
    - Add tests.
    
    
    Former-commit-id: 30cc28ed01638ca4ee4475b9f9742e9f35e1bd2d

--- skims/skims/lib_root/f070_wildcard_import.py
+++ skims/skims/lib_root/f070_wildcard_import.py
@@ -43,6 +43,36 @@ def java_wildcard_import(
     )
 
 
+def go_wildcard_import(
+    graph_db: graph_model.GraphDB,
+) -> core_model.Vulnerabilities:
+    def n_ids() -> graph_model.GraphShardNodes:
+        for shard in graph_db.shards_by_langauge(
+            graph_model.GraphShardMetadataLanguage.GO,
+        ):
+            for import_dcl_id in g.filter_nodes(
+                shard.graph,
+                nodes=shard.graph.nodes,
+                predicate=g.pred_has_labels(label_type="import_spec"),
+            ):
+                import_dcl_c_ids = g.adj_ast(shard.graph, import_dcl_id)
+                import_identifier_id = import_dcl_c_ids[0]
+                import_identifier = shard.graph.nodes[import_identifier_id]
+                if import_identifier["label_type"] == "dot":
+                    yield shard, import_dcl_id
+
+    return get_vulnerabilities_from_n_ids(
+        cwe=("398",),
+        desc_key="src.lib_path.f070_wildcard_import.wildcard_import",
+        desc_params={},
+        finding=FINDING,
+        graph_shard_nodes=n_ids(),
+    )
+
+
 # Constants
 FINDING: core_model.FindingEnum = core_model.FindingEnum.F070_WILDCARD_IMPORT
-QUERIES: graph_model.Queries = ((FINDING, java_wildcard_import),)
+QUERIES: graph_model.Queries = (
+    (FINDING, java_wildcard_import),
+    (FINDING, go_wildcard_import),
+)
