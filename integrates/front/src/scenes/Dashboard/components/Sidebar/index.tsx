f937747f1a7d0c6c39fc85afd1dd48be78684975
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-06-13 17:07:10 -0500

    integrates\refac(front): #4771.1 remove collapsable option
    - remove button of collapsable option
    
    
    Former-commit-id: 3d9f4d82f9ca9fdea704808de87c449a8dd86390

--- integrates/front/src/scenes/Dashboard/components/Sidebar/index.tsx
+++ integrates/front/src/scenes/Dashboard/components/Sidebar/index.tsx
@@ -1,38 +1,17 @@
-import {
-  faAngleDoubleLeft,
-  faAngleDoubleRight,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useCallback } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
-import {
-  Logo,
-  Preloader,
-  SidebarButton,
-  SidebarContainer,
-  SidebarMenu,
-} from "./styles";
+import { Logo, Preloader, SidebarContainer, SidebarMenu } from "./styles";
 
 import { useApolloNetworkStatus } from "utils/apollo";
-import { useStoredState } from "utils/hooks";
 
 const Sidebar: React.FC = (): JSX.Element => {
-  const [collapsed, setCollapsed] = useStoredState(
-    "sidebarCollapsed",
-    true,
-    localStorage
-  );
-  const toggleSidebar = useCallback((): void => {
-    setCollapsed((currentValue): boolean => !currentValue);
-  }, [setCollapsed]);
-
   const status = useApolloNetworkStatus();
   const isLoading: boolean =
     status.numPendingQueries > 0 || status.numPendingMutations > 0;
 
   return (
-    <SidebarContainer collapsed={collapsed}>
+    <SidebarContainer collapsed={true}>
       <SidebarMenu>
         <li>
           <Link to={"/home"}>
@@ -41,11 +20,6 @@ const Sidebar: React.FC = (): JSX.Element => {
         </li>
       </SidebarMenu>
       {isLoading ? <Preloader /> : undefined}
-      <SidebarButton onClick={toggleSidebar}>
-        <FontAwesomeIcon
-          icon={collapsed ? faAngleDoubleRight : faAngleDoubleLeft}
-        />
-      </SidebarButton>
     </SidebarContainer>
   );
 };
