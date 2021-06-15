ec6788e63845326ca674124fa29ef779eee594dc
Alejandro Salgado <asalgado@fluidattacks.com>
2021-06-15 14:36:35 -0500

    skims\feat(build): #4783.6 unsafe ssl server
    - Add ssl server with vulnerabilities
    
    
    Former-commit-id: ebde5064f1f3da59b75deeabeffbbe492a9ea1d2

---
+++ makes/applications/skims/test/mocks/ssl/unsafe/default.nix
@@ -0,0 +1,48 @@
+{ nixpkgs
+, makeEntrypoint
+, makeTemplate
+, path
+, sslCerts
+, ...
+}:
+makeEntrypoint {
+  arguments = {
+    envConfig = makeTemplate {
+      arguments = {
+        envHttpServerSslCert = sslCerts {
+          name = "skims-test-mocks-ssl-unsafe";
+          options = [ "-subj" "/CN=localhost" ];
+        };
+        envHttpServerRoot = path "/makes/applications/skims/test/mocks/ssl/http/server/root";
+      };
+      name = "nginx-conf";
+      template = ''
+        events {}
+        daemon off;
+        http {
+          server {
+            index index.html;
+            listen localhost:4446 ssl;
+            location / {
+              root __envHttpServerRoot__;
+            }
+            server_name localhost;
+            ssl_prefer_server_ciphers off;
+            ssl_ciphers "ADH-AES128-SHA:ADH-AES256-SHA:ADH-CAMELLIA128-SHA:ADH-CAMELLIA256-SHA:ADH-DES-CBC3-SHA:ADH-RC4-MD5:ADH-SEED-SHA:AES128-SHA:AES256-SHA:CAMELLIA128-SHA:ADH-SEED-SHA:AES128-SHA:AES256-SHA:CAMELLIA128-SHA:DH-DSS-AES256-SHA:DH-DSS-CAMELLIA128-SHA:DH-DSS-CAMELLIA256-SHA:DH-DSS-DES-CBC3-SHA:DH-DSS-SEED-SHA:DES-CBC3-SHA:aNULL:eNULL:EXPORT:DES:MD5:PSK:RC4";
+            ssl_certificate __envHttpServerSslCert__/cert.crt;
+            ssl_certificate_key __envHttpServerSslCert__/cert.key;
+          ssl_protocols SSLv3 TLSv1 TLSv1.1;
+          }
+        }
+        pid /dev/null;
+      '';
+    };
+  };
+  name = "skims-test-mocks-ssl-unsafe";
+  searchPaths = {
+    envPaths = [
+      nixpkgs.nginxLocal
+    ];
+  };
+  template = "nginx -c __envConfig__";
+}
