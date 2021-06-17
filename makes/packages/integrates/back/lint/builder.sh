c5763ba028fc1de4a776a0eb7a7eae7f578f2b8b
Briam Agudelo <bagudelo@fluidattacks.com>
2021-06-17 15:25:16 -0500

    integrates\test(back): #4673.43 fix tests files
    - Fix integrates back lint job
    - Issue finished
    
    
    Former-commit-id: b7582f55c89d8c8190e2ea9dd2625c4c31944c2f

--- makes/packages/integrates/back/lint/builder.sh
+++ makes/packages/integrates/back/lint/builder.sh
@@ -5,7 +5,7 @@ function main {
   lint_python_package "${envIntegrates}/back/migrations" \
     && lint_python_package "${envIntegrates}/lambda" \
     && lint_python_package "${envIntegrates}/deploy/permissions_matrix" \
-    && lint_python_package "${envIntegrates}/back/tests" || true \
+    && lint_python_package "${envIntegrates}/back/tests" \
     && lint_python_imports \
       "${envIntegratesImportsConfig}" \
       "${envIntegrates}/back/src" \
