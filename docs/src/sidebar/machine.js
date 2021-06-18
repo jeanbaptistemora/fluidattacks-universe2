e954a0c712bd8ffee50c3369476ead7ef6851c44
Rafael Rodriguez <rrodriguez@fluidattacks.com>
2021-06-18 09:32:28 -0500

    docs\refac(doc): #4757.4 modify docs
    - Change analytics position
    - Improve image styling
    
    
    Former-commit-id: 116ba992c9ba7a75da00e46ad254dba48562ea98

--- docs/src/sidebar/machine.js
+++ docs/src/sidebar/machine.js
@@ -4,17 +4,6 @@ const MACHINE = [
     label: 'Web',
     items: [
       'machine/web/asm',
-      {
-        type: 'category',
-        label: 'Analytics',
-        items: [
-          'machine/web/analytics/introduction',
-          'machine/web/analytics/common',
-          'machine/web/analytics/organization',
-          'machine/web/analytics/portfolio',
-          'machine/web/analytics/groups',
-        ],
-      },
       'machine/web/support',
       {
         type: 'category',
@@ -72,6 +61,17 @@ const MACHINE = [
           },
           'machine/web/vulnerabilities/reporting-vulns',
           'machine/web/vulnerabilities/delete',
+        ],
+      },
+      {
+        type: 'category',
+        label: 'Analytics',
+        items: [
+          'machine/web/analytics/introduction',
+          'machine/web/analytics/common',
+          'machine/web/analytics/organization',
+          'machine/web/analytics/portfolio',
+          'machine/web/analytics/groups',
         ],
       },
     ],
