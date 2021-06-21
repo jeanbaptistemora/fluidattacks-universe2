51263cff274f637ee41734f9c38c9473051692bd
Kevin Amado <kamado@fluidattacks.com>
2021-06-21 12:06:18 -0500

    makes\feat(infra): #4806.8 default branch
    - Add default branch and license
    
    
    Former-commit-id: d7b18c400b57c5e43183105cdba4ec82464e0059

--- makes/applications/makes/foss/modules/github/resource.tf
+++ makes/applications/makes/foss/modules/github/resource.tf
@@ -1,8 +1,13 @@
+resource "github_branch_default" "default" {
+  repository = github_repository.repo.name
+  branch     = "main"
+}
+
 resource "github_repository" "repo" {
   allow_merge_commit     = false
   allow_squash_merge     = false
   allow_rebase_merge     = true
-  auto_init              = false
+  auto_init              = true
   delete_branch_on_merge = false
   description            = var.description
   has_downloads          = false
@@ -11,6 +16,7 @@ resource "github_repository" "repo" {
   has_wiki               = false
   homepage_url           = var.homepage
   is_template            = false
+  license_template       = "mpl-2.0"
   name                   = var.name
   topics                 = var.topics
   vulnerability_alerts   = false
