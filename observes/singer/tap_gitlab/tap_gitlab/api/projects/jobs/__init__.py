35a5d77c8bb081325477a344b334cf75b9ea2b0e
Daniel Murcia <dmurcia@fluidattacks.com>
2021-06-21 11:38:28 -0500

    observes\feat(back): #4793.19 jobs api
    - add jobs api and list all method
    
    
    Former-commit-id: 64280e469dc01a134d90d5953ff705dde0372f13

---
+++ observes/singer/tap_gitlab/tap_gitlab/api/projects/jobs/__init__.py
@@ -0,0 +1,54 @@
+# pylint: skip-file
+
+from __future__ import (
+    annotations,
+)
+
+from paginator.int_index_2 import (
+    get_until_end,
+)
+from paginator.pages import (
+    PageId,
+)
+from returns.io import (
+    IO,
+)
+from returns.maybe import (
+    Maybe,
+)
+from returns.unsafe import (
+    unsafe_perform_io,
+)
+from tap_gitlab.api.projects.ids import (
+    ProjectId,
+)
+from tap_gitlab.api.projects.jobs.page import (
+    JobsPage,
+    list_jobs,
+    Scope,
+)
+from tap_gitlab.api.raw_client import (
+    RawClient,
+)
+from typing import (
+    Iterator,
+    List,
+    NamedTuple,
+)
+
+
+class JobApi(NamedTuple):
+    client: RawClient
+    proj: ProjectId
+    scopes: List[Scope]
+
+    def list_all_updated_before(
+        self,
+        start: PageId[int],
+    ) -> IO[Iterator[JobsPage]]:
+        def getter(page: PageId[int]) -> Maybe[JobsPage]:
+            return unsafe_perform_io(
+                list_jobs(self.client, self.proj, page, self.scopes)
+            )
+
+        return IO(get_until_end(start, getter, 10))
