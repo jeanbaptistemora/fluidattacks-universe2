9d8036bb77d61661a7d0e290b6cd532ac4c5b02e
Kevin Amado <kamado@fluidattacks.com>
2021-06-21 12:15:11 -0500

    makes\feat(infra): #4806.9 protect branch
    - Protect branch and enforce some security measures
    
    
    Former-commit-id: 7751cf405b051c9b1a59d138297ad71537bf6e77

--- makes/applications/makes/foss/modules/github/resource.tf
+++ makes/applications/makes/foss/modules/github/resource.tf
@@ -3,6 +3,24 @@ resource "github_branch_default" "default" {
   branch     = "main"
 }
 
+resource "github_branch_protection" "main" {
+  allows_force_pushes    = false
+  allows_deletions       = false
+  enforce_admins         = true
+  pattern                = "main"
+  repository_id          = github_repository.repo.node_id
+  require_signed_commits = true
+  required_status_checks {
+    strict   = false
+    contexts = []
+  }
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    require_code_owner_reviews      = false
+    required_approving_review_count = 1
+  }
+}
+
 resource "github_repository" "repo" {
   allow_merge_commit     = false
   allow_squash_merge     = false
