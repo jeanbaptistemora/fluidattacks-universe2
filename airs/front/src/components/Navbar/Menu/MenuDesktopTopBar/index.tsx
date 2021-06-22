966a8dfd4b12e1758a949e08eb6e86e4e3fb6f86
Frank Aristizabal <faristizabal@fluidattacks.com>
2021-06-22 13:23:53 -0500

    airs\refac(front): #4544.8 update menu
    - Add components and styles
     for new overlay menu.
    
    
    Former-commit-id: 7e92d1d3be256b81672656c20c9abb5f4a6469b3

---
+++ airs/front/src/components/Navbar/Menu/MenuDesktopTopBar/index.tsx
@@ -0,0 +1,62 @@
+/* eslint react/forbid-component-props: 0 */
+import { faTimes } from "@fortawesome/pro-light-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "gatsby";
+import React from "react";
+
+import {
+  NavbarContactButton,
+  NavbarList,
+} from "../../../../styles/styledComponents";
+import { Search } from "../../../Search";
+import {
+  CloseMenuButton,
+  CloseMenuButtonContainer,
+  TopBarButton,
+} from "../../styles/navbarStyledComponents";
+
+interface IProps {
+  close: () => void;
+}
+
+const searchIndices = [
+  { name: `fluidattacks_airs`, title: `fluidattacks_airs` },
+];
+
+const MenuDesktopTopBar: React.FC<IProps> = ({
+  close,
+}: IProps): JSX.Element => (
+  <NavbarList>
+    <CloseMenuButtonContainer>
+      <CloseMenuButton onClick={close}>
+        <FontAwesomeIcon className={"f2 nt1 nl2 c-fluid-bk"} icon={faTimes} />
+      </CloseMenuButton>
+      <Search indices={searchIndices} />
+    </CloseMenuButtonContainer>
+    <li className={"fr pv4 dib dn-l show-item-m"}>
+      <Link className={"no-underline"} to={"/contact-us/"}>
+        <NavbarContactButton>{"Contact now"}</NavbarContactButton>
+      </Link>
+    </li>
+    <li className={"fr mr3 pv4 hide-item-m"}>
+      <Link
+        className={"no-underline"}
+        to={"https://landing.fluidattacks.com/us/ebook/"}
+      >
+        <TopBarButton>{"Download eBook"}</TopBarButton>
+      </Link>
+    </li>
+    <li className={"fr mr3 pv4 hide-item-m"}>
+      <Link className={"no-underline"} to={"/contact-us/"}>
+        <TopBarButton onClick={close}>{"Get a Demo"}</TopBarButton>
+      </Link>
+    </li>
+    <li className={"fr mr3 pv4"}>
+      <Link className={"no-underline"} to={"https://app.fluidattacks.com/"}>
+        <TopBarButton>{"Log in"}</TopBarButton>
+      </Link>
+    </li>
+  </NavbarList>
+);
+
+export { MenuDesktopTopBar };
