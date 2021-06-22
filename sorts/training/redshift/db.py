510814e5e6c2e703871d1fd286e98548823a2ab2
Mario Varas <mvaras@fluidattacks.com>
2021-06-22 11:36:01 -0500

    sorts\feat(back): #4581.33 perform reset operation
    - First deletes all info in our training table and then we
    iterate each training .csv to refill it. Result is that we
    have our table updated with the latest training results
    - This is intended to be a manually executed operation when
    needed
    
    
    Former-commit-id: 8fd8bfbdf26c17698bfd6e57f6385364d1dbf22b

--- sorts/training/redshift/db.py
+++ sorts/training/redshift/db.py
@@ -2,6 +2,7 @@ import click
 from contextlib import (
     contextmanager,
 )
+import csv
 import os
 from psycopg2 import (
     connect,
@@ -10,10 +11,15 @@ from psycopg2.extensions import (
     cursor as cursor_cls,
     ISOLATION_LEVEL_AUTOCOMMIT,
 )
+import tempfile
+from training.constants import (
+    S3_BUCKET,
+)
 from typing import (
+    Any,
     Dict,
     Iterator,
-    Union,
+    List,
 )
 
 
@@ -68,17 +74,12 @@ def initialize() -> None:
         )
 
 
-def delete() -> None:
+def delete(table: str) -> None:
     with db_cursor() as cursor:
-        cursor.execute(
-            """
-                DROP TABLE IF EXISTS sorts.training;
-                DROP TABLE IF EXISTS sorts.dataset
-            """
-        )
+        cursor.execute(f"TRUNCATE TABLE sorts.{table}")
 
 
-def insert(table: str, item: Dict[str, Union[str, int]]) -> None:
+def insert(table: str, item: Dict[str, Any]) -> None:
     query_columns: str = ",\n".join(item.keys())
     query_values: str = ",\n".join(f"%({key})s" for key in item.keys())
     with db_cursor() as cursor:
@@ -97,6 +98,41 @@ def insert(table: str, item: Dict[str, Union[str, int]]) -> None:
         )
 
 
+def reset() -> None:
+    """
+    First deletes all info in our training table and
+    then we iterate each training .csv to refill it.
+    Result is that we have our table updated with the last results
+    """
+    delete("training")
+    redshift_table_columns: List[str] = [
+        "model",
+        "features",
+        "precision",
+        "recall",
+        "f_score",
+        "overfit",
+        "tuned_parameters",
+    ]
+    with tempfile.TemporaryDirectory() as tmpdir:
+        for obj in S3_BUCKET.objects.filter(Prefix="training-output/results"):
+            if obj.key.endswith(".csv"):
+                filename: str = os.path.basename(obj.key)
+                local_file: str = os.path.join(tmpdir, filename)
+                S3_BUCKET.download_file(obj.key, local_file)
+
+        for file in os.listdir(tmpdir):
+            print(f"[INFO] Updating {file.split('_')[0]} training info...")
+            with open(os.path.join(tmpdir, file), "r") as csv_file:
+                reader = csv.DictReader(csv_file)
+                for row in reader:
+                    item = dict(zip(redshift_table_columns, row.values()))
+                    item["tuned_parameters"] = ", ".join(
+                        item["tuned_parameters"]
+                    )
+                    insert("training", item)
+
+
 @click.group(
     help="Redshift integration for Sorts. Handles all Sorts stored data"
 )
@@ -111,7 +147,7 @@ def init_db() -> None:
 
 @cli.command(help="Delete and create again Redshift schema & Sorts tables")
 def reset_db() -> None:
-    delete()
+    reset()
 
 
 if __name__ == "__main__":
