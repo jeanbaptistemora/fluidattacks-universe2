30a16eb4ab5303b45a6cce053b24bcd10609975c
Kevin Amado <kamado@fluidattacks.com>
2021-06-23 13:17:20 -0500

    integrates\test(back): #4830.4 add tests
    - Add tests for the uploaded vulns
    - I will add a duplicated test next
    
    
    Former-commit-id: 686977b5ee76adcc3adcca4696b9d4190eb1e6c4

--- integrates/back/tests/functional/upload_file/test_resolver.py
+++ integrates/back/tests/functional/upload_file/test_resolver.py
@@ -5,7 +5,27 @@ import pytest
 from typing import (
     Any,
     Dict,
+    List,
 )
+from vulnerabilities import (
+    dal as vulns_dal,
+)
+
+
+async def _get_vulns(finding_id: str) -> List[Dict[str, Any]]:
+    return sorted(
+        (
+            dict(
+                commit_hash=vuln.get("commit_hash", ""),
+                repo_nickname=vuln.get("repo_nickname", ""),
+                specific=vuln.get("specific", ""),
+                type=vuln.get("type", ""),
+                where=vuln.get("where", ""),
+            )
+            for vuln in await vulns_dal.get_by_finding(finding_id)
+        ),
+        key=str,
+    )
 
 
 @pytest.mark.asyncio
@@ -24,6 +44,29 @@ async def test_upload_file(populate: bool, email: str) -> None:
     result: Dict[str, Any] = await get_result(user=email, finding=finding_id)
     assert "errors" not in result
     assert result["data"]["uploadFile"]["success"]
+    assert await _get_vulns(finding_id) == [
+        {
+            "commit_hash": "",
+            "repo_nickname": "product",
+            "specific": "phone",
+            "type": "",
+            "where": "https://example.com",
+        },
+        {
+            "commit_hash": "5b5c92105b5c92105b5c92105b5c92105b5c9210",
+            "repo_nickname": "product",
+            "specific": "123",
+            "type": "",
+            "where": "path/to/file1.ext",
+        },
+        {
+            "commit_hash": "e17059d1e17059d1e17059d1e17059d1e17059d1",
+            "repo_nickname": "product",
+            "specific": "345",
+            "type": "",
+            "where": "path/to/file3.ext",
+        },
+    ]
 
 
 @pytest.mark.asyncio
