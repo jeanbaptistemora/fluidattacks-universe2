db637ceaf628ac294d4a623c7b4f511ce236d72e
Daniel Murcia <dmurcia@fluidattacks.com>
2021-06-29 14:25:54 -0500

    observes\feat(back): #4791.19 until incomplete
    - add ProcessStatus
    - add process_until_incomplete
    
    
    Former-commit-id: 418b467444ce3a220c8d5597de2908247252bfab

--- observes/singer/tap_gitlab/tap_gitlab/intervals/progress.py
+++ observes/singer/tap_gitlab/tap_gitlab/intervals/progress.py
@@ -7,6 +7,7 @@ from returns.primitives.container import (
 )
 from returns.primitives.hkt import (
     SupportsKind1,
+    SupportsKind2,
 )
 from singer_io.common import (
     JSON,
@@ -21,7 +22,9 @@ from tap_gitlab.intervals.interval import (
     Interval,
 )
 from typing import (
+    Callable,
     final,
+    Tuple,
     TypeVar,
 )
 
@@ -48,6 +51,41 @@ class ProgressInterval(
     @property
     def completed(self) -> bool:
         return self._inner_value["completed"]
+
+
+_State = TypeVar("_State")
+
+
+@final
+class ProcessStatus(
+    BaseContainer,
+    SupportsKind2["ProcessStatus", _DataType, _State],
+):
+    def __init__(
+        self,
+        p_intervals: NTuple[ProgressInterval[_DataType]],
+        incomplete_is_present: bool,
+        function_state: _State,
+    ) -> None:
+        super().__init__(
+            {
+                "p_intervals": p_intervals,
+                "incomplete_is_present": incomplete_is_present,
+                "function_state": function_state,
+            }
+        )
+
+    @property
+    def p_intervals(self) -> NTuple[ProgressInterval[_DataType]]:
+        return self._inner_value["p_intervals"]
+
+    @property
+    def incomplete_is_present(self) -> bool:
+        return self._inner_value["incomplete_is_present"]
+
+    @property
+    def function_state(self) -> _State:
+        return self._inner_value["function_state"]
 
 
 @final
@@ -83,6 +121,39 @@ class FragmentedProgressInterval(
         return tuple(
             ProgressInterval(item, completed) for item, completed in intervals
         )
+
+    def process_until_incomplete(
+        self,
+        function: Callable[
+            [_State, ProgressInterval[_DataType]],
+            Tuple[_State, NTuple[ProgressInterval[_DataType]]],
+        ],
+        init_state: _State,
+    ) -> NTuple[ProgressInterval[_DataType]]:
+        def _process(
+            prev: ProcessStatus[_DataType, _State],
+            interval: ProgressInterval[_DataType],
+        ) -> ProcessStatus[_DataType, _State]:
+            if prev.incomplete_is_present:
+                return ProcessStatus(
+                    prev.p_intervals + (interval,),
+                    prev.incomplete_is_present,
+                    prev.function_state,
+                )
+            state, results = function(prev.function_state, interval)
+            incomplete = tuple(
+                filter(lambda p_invl: p_invl.completed is False, results)
+            )
+            return ProcessStatus(
+                prev.p_intervals + results, bool(incomplete), state
+            )
+
+        status: ProcessStatus[_DataType, _State] = ProcessStatus(
+            tuple(), False, init_state
+        )
+        for interval in self.progress_intervals:
+            status = _process(status, interval)
+        return status.p_intervals
 
     def to_json(self) -> JSON:
         return {
