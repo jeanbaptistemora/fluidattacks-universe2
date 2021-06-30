7eb3b2ae7289e2bc4bcc73a24d513d3aff5a3c72
Daniel Murcia <dmurcia@fluidattacks.com>
2021-06-30 13:51:35 -0500

    observes\feat(back): #4791.25 emit mrs intvl
    - add emit_mrs_interval
    - add new _stream_data version
    
    
    Former-commit-id: d1d06a1d5f91b14df3201f121d564649e9669585

--- observes/singer/tap_gitlab/tap_gitlab/emitter.py
+++ observes/singer/tap_gitlab/tap_gitlab/emitter.py
@@ -1,3 +1,5 @@
+# pylint: skip-file
+
 from __future__ import (
     annotations,
 )
@@ -19,6 +21,14 @@ from returns.io import (
 from returns.primitives.types import (
     Immutable,
 )
+from returns.result import (
+    Failure,
+    Result,
+    Success,
+)
+from returns.unsafe import (
+    unsafe_perform_io,
+)
 from singer_io import (
     factory,
 )
@@ -30,6 +40,21 @@ from tap_gitlab.api.auth import (
 )
 from tap_gitlab.api.client import (
     ApiClient,
+)
+from tap_gitlab.api.projects.merge_requests import (
+    MrApi,
+)
+from tap_gitlab.intervals.alias import (
+    NTuple,
+)
+from tap_gitlab.intervals.interval import (
+    IntervalFactory,
+    InvalidInterval,
+    MIN,
+    OpenLeftInterval,
+)
+from tap_gitlab.intervals.progress import (
+    ProgressInterval,
 )
 from tap_gitlab.state import (
     JobStreamState,
@@ -44,9 +69,12 @@ from tap_gitlab.streams import (
 from typing import (
     Iterator,
     Optional,
+    Tuple,
+    TypeVar,
 )
 
 LOG = logging.getLogger(__name__)
+_ApiPage = TypeVar("_ApiPage", bound=ApiPage)
 
 
 def _to_singer(
@@ -67,14 +95,31 @@ def _emit_pages(
         count = count + 1
 
 
-def _stream_data(
+def _old_stream_data(
     stream: SupportedStreams, pages: IO[Iterator[ApiPage]], max_pages: int
 ) -> None:
     pages.map(partial(_emit_pages, stream, max_pages))
 
 
+def _stream_data(
+    stream: SupportedStreams,
+    pages: Iterator[_ApiPage],
+    emitted_pages: int,
+    max_pages: int,
+) -> Result[int, _ApiPage]:
+    count = emitted_pages
+    for page in pages:
+        if count >= max_pages:
+            return Failure(page)
+        for item in _to_singer(stream, page):
+            factory.emit(item)
+        count = count + 1
+    return Success(count)
+
+
 class Emitter(Immutable):
     api: ApiClient
+    interval_factory: IntervalFactory
     max_pages: int
 
     def __new__(cls, creds: Credentials, max_pages: int = 10) -> Emitter:
@@ -83,16 +128,82 @@ class Emitter(Immutable):
         object.__setattr__(self, "max_pages", max_pages)
         return self
 
+    def emit_mrs_interval(
+        self,
+        stream: MrStream,
+        pages_emitted: int,
+        p_interval: ProgressInterval[datetime],
+    ) -> Tuple[int, NTuple[ProgressInterval[datetime]]]:
+        api: MrApi = self.api.project(stream.project).mrs(
+            stream.scope, stream.mr_state
+        )
+        if not p_interval.completed:
+            interval = p_interval.interval
+            if isinstance(interval, OpenLeftInterval):
+                pages = (
+                    api.list_all_updated_before(PageId(interval.upper, 100))
+                    if isinstance(interval.lower, MIN)
+                    else api.list_all_updated_between(
+                        interval.lower, interval.upper
+                    )
+                )
+                # temp unsafe_perform_io for fast coupling
+                result = _stream_data(
+                    SupportedStreams.MERGE_REQUESTS,
+                    unsafe_perform_io(pages),
+                    pages_emitted,
+                    self.max_pages,
+                )
+                emitted = result.value_or(self.max_pages)
+                new_p_interval: NTuple[ProgressInterval[datetime]] = (
+                    result.map(
+                        lambda _: tuple(
+                            (ProgressInterval(p_interval.interval, True),)
+                        )
+                    )
+                    .lash(
+                        lambda page: Success(
+                            tuple(
+                                (
+                                    ProgressInterval(
+                                        self.interval_factory.new_lopen(
+                                            interval.lower, page.max_date
+                                        ),
+                                        False,
+                                    ),
+                                    ProgressInterval(
+                                        self.interval_factory.new_lopen(
+                                            page.max_date, interval.upper
+                                        ),
+                                        True,
+                                    ),
+                                )
+                            )
+                        )
+                    )
+                    .unwrap()
+                )
+                return (emitted, new_p_interval)
+            raise InvalidInterval("Expected OpenLeftInterval")
+        return (pages_emitted, (p_interval,))
+
     def emit_mrs(
-        self, stream: MrStream, _state: Optional[MrStreamState] = None
+        self, stream: MrStream, state: Optional[MrStreamState] = None
     ) -> None:
+        if state:
+            state.state.process_until_incomplete(
+                partial(self.emit_mrs_interval, stream), 0
+            )
+            return None
         start = PageId(datetime.now(pytz.utc), 100)
         pages = (
             self.api.project(stream.project)
             .mrs(stream.scope, stream.mr_state)
             .list_all_updated_before(start)
         )
-        _stream_data(SupportedStreams.MERGE_REQUESTS, pages, self.max_pages)
+        _old_stream_data(
+            SupportedStreams.MERGE_REQUESTS, pages, self.max_pages
+        )
 
     def emit_jobs(
         self, stream: JobStream, _state: Optional[JobStreamState] = None
@@ -103,4 +214,4 @@ class Emitter(Immutable):
             .jobs(list(stream.scopes))
             .list_all(start)
         )
-        _stream_data(SupportedStreams.JOBS, pages, self.max_pages)
+        _old_stream_data(SupportedStreams.JOBS, pages, self.max_pages)
