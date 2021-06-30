dc15e6f67276f0fee3d9ad61a8d3320a45d5e78a
Daniel Salazar <dsalazar@fluidattacks.com>
2021-06-30 16:53:31 -0500

    observes\feat(infra): #4894.7 kms key rotation
    - Enable rotation for kms keys
    
    
    Former-commit-id: 690357e6ade7deccc0f0d0be3691e03ff549033f

--- observes/infra/terraform/prod-key.tf
+++ observes/infra/terraform/prod-key.tf
@@ -101,6 +101,7 @@ resource "aws_kms_key" "key-prod" {
   policy                  = data.aws_iam_policy_document.key-prod.json
   deletion_window_in_days = 30
   is_enabled              = true
+  enable_key_rotation     = true
 
   tags = {
     "Name"               = "observes-production"
