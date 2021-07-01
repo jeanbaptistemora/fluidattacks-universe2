f1ff139a009f59c39c86d46cd11230da8fccbdf4
Kevin Amado <kamado@fluidattacks.com>
2021-07-01 14:10:10 -0500

    makes\refac(infra): #4253.3 rm homepage
    - Makes wont be publishing its docs in docs.fluidattacks.com
      so lets remove it from now
    
    
    Former-commit-id: 5113085282e5ed4ae640e6e9a99ac08bc9ae014b

--- makes/applications/makes/foss/src/module.tf
+++ makes/applications/makes/foss/src/module.tf
@@ -2,7 +2,7 @@ module "github_makes" {
   source = "../../../../../makes/applications/makes/foss/modules/github"
 
   description = "A SecDevOps framework powered by Nix"
-  homepage    = "https://docs.fluidattacks.com"
+  homepage    = null
   name        = "makes"
   token       = var.GITHUB_API_TOKEN
   topics      = ["build", "devops", "nix"]
