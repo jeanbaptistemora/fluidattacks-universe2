c6d928e24c7c4b136cffd8585f6fb8625f2f17f5
Jonathan Mesa <jmesa@fluidattacks.com>
2021-07-02 14:00:25 +0000

    integrates\feat(back): #4329.117 update vuln entity
    - add function to update vuln treatment and its historics
    
    
    Former-commit-id: 1e7a34a003aae26d16096b29e1725c384a0f5dc5

--- integrates/back/src/db_model/vulnerabilities/update.py
+++ integrates/back/src/db_model/vulnerabilities/update.py
@@ -2,6 +2,10 @@ from .historics.state import (
     format_state_item,
     VulnerabilityState,
 )
+from .historics.treatment import (
+    format_treatment_item,
+    VulnerabilityTreatment,
+)
 from boto3.dynamodb.conditions import (
     Attr,
 )
@@ -38,6 +42,37 @@ async def update_state(
     await operations.put_item(
         condition_expression=condition_expression,
         facet=TABLE.facets["vulnerability_state"],
+        item=latest,
+        table=TABLE,
+    )
+    items.append(historic)
+    await operations.batch_write_item(items=tuple(items), table=TABLE)
+
+
+async def update_treatment(
+    *,
+    finding_id: str,
+    treatment: VulnerabilityTreatment,
+    uuid: str,
+) -> None:
+    items = []
+    key_structure = TABLE.primary_key
+    treatment_item = format_treatment_item(treatment)
+    latest, historic = historics.build_historic(
+        attributes=treatment_item,
+        historic_facet=TABLE.facets["vulnerability_historic_treatment"],
+        key_structure=key_structure,
+        key_values={
+            "finding_id": finding_id,
+            "iso8601utc": treatment.modified_date,
+            "uuid": uuid,
+        },
+        latest_facet=TABLE.facets["vulnerability_treatment"],
+    )
+    condition_expression = Attr(key_structure.partition_key).exists()
+    await operations.put_item(
+        condition_expression=condition_expression,
+        facet=TABLE.facets["vulnerability_treatment"],
         item=latest,
         table=TABLE,
     )
