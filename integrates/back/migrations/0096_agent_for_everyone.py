c99caf93a7ee5b417f77ada8e7fd4934536cc1be
David Acevedo <dacevedo@fluidattacks.com>
2021-07-02 14:08:08 -0500

    integrates\refac(back): #4880.6 agent for everyone
    - added migration script
    
    
    Former-commit-id: 568f4388ac43984ef1197529fcd802c79b3c1ecc

---
+++ integrates/back/migrations/0096_agent_for_everyone.py
@@ -0,0 +1,64 @@
+# pylint: disable=invalid-name
+"""
+This migration enables the devsecops agent to all active groups
+
+Execution Time:
+Finalization Time:
+"""
+
+from aioextensions import (
+    collect,
+    run,
+)
+from forces.domain import (
+    create_forces_user,
+)
+from graphql.type import (
+    GraphQLResolveInfo,
+)
+from groups.dal import (
+    get_active_groups,
+    get_attributes,
+    update,
+)
+import time
+
+
+async def update_group(group_name: str) -> None:
+    historic_config = (
+        await get_attributes(group_name, ["historic_configuration"])
+    )["historic_configuration"]
+
+    await update(
+        group_name,
+        {
+            "historic_configuration": [
+                *historic_config[:-1],
+                {
+                    **historic_config[-1],
+                    "has_forces": True,
+                },
+            ]
+        },
+    )
+
+    info = GraphQLResolveInfo(
+        None, None, None, None, None, None, None, None, None, None, {}
+    )
+    await create_forces_user(info, group_name)
+
+
+async def main() -> None:
+    groups = await get_active_groups()
+    await collect(tuple(update_group(group_name) for group_name in groups))
+
+
+if __name__ == "__main__":
+    execution_time = time.strftime(
+        "Execution Time:    %Y-%m-%d at %H:%M:%S UTC%Z"
+    )
+    run(main())
+    finalization_time = time.strftime(
+        "Finalization Time: %Y-%m-%d at %H:%M:%S UTC%Z"
+    )
+    print(f"{execution_time}\n{finalization_time}")
