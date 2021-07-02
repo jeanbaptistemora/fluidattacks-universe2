9ca5b7c721745cd2fd6b05f9119c80ac5ee49242
Daniel Salazar <dsalazar@fluidattacks.com>
2021-07-02 12:28:31 -0500

    makes\refac(infra): #4923.1 hide autoscaling attrs
    - Hide private ip
    - Hide user
    
    
    Former-commit-id: e823de870a2d4133630b94ec15f2e4de53b6f9ff

--- makes/applications/makes/ci/config/entrypoint.sh
+++ makes/applications/makes/ci/config/entrypoint.sh
@@ -1,8 +1,6 @@
 # shellcheck shell=bash
 
 function main {
-  local bastion_ip='192.168.3.11'
-  local bastion_user='ubuntu'
   local tmp_file_1
   local tmp_file_2
   local secrets_to_replace=(
@@ -12,12 +10,16 @@ function main {
     autoscaling_token_4
     autoscaling_access_key
     autoscaling_secret_key
+    autoscaling_bastion_ip
+    autoscaling_bastion_user
     autoscaling_bastion_key_b64
   )
 
   echo '[INFO] Exporting secrets' \
     && aws_login_prod makes \
-    && sops_export_vars makes/applications/makes/secrets/src/production.yaml "${secrets_to_replace[@]}" \
+    && sops_export_vars \
+      makes/applications/makes/secrets/src/production.yaml \
+      "${secrets_to_replace[@]}" \
     && pushd makes/applications/makes/ci/src \
     && echo '[INFO] Creating temporary files' \
     && tmp_file_1="$(mktemp)" \
@@ -25,12 +27,15 @@ function main {
     && echo '[INFO] Adding bastion to known hosts' \
     && mkdir -p ~/.ssh \
     && touch ~/.ssh/known_hosts \
-    && ssh-keyscan -H "${bastion_ip}" >> ~/.ssh/known_hosts \
+    && ssh-keyscan -H "${autoscaling_bastion_ip}" >> ~/.ssh/known_hosts \
     && echo '[INFO] Exporting bastion SSH key' \
     && echo -n "${autoscaling_bastion_key_b64}" | base64 -d > "${tmp_file_1}" \
     && ssh-keygen -y -f "${tmp_file_1}" > "${tmp_file_1}.pub" \
     && echo '[INFO] Executing test: $ sudo whoami' \
-    && ssh -i "${tmp_file_1}" "${bastion_user}@${bastion_ip}" 'sudo whoami' \
+    && ssh -i \
+      "${tmp_file_1}" \
+      "${autoscaling_bastion_user}@${autoscaling_bastion_ip}" \
+      'sudo whoami' \
     && echo '[INFO] Writing config with secrets' \
     && cp "__envConfig__" "${tmp_file_2}" \
     && for secret in "${secrets_to_replace[@]}"; do
@@ -40,15 +45,27 @@ function main {
         || return 1
     done \
     && echo '[INFO] Moving file to bastion: config.toml to /etc/gitlab-runner/config.toml' \
-    && scp -i "${tmp_file_1}" "${tmp_file_2}" "${bastion_user}@${bastion_ip}:/port/config.toml" \
-    && ssh -i "${tmp_file_1}" "${bastion_user}@${bastion_ip}" \
+    && scp -i \
+      "${tmp_file_1}" \
+      "${tmp_file_2}" \
+      "${autoscaling_bastion_user}@${autoscaling_bastion_ip}:/port/config.toml" \
+    && ssh -i \
+      "${tmp_file_1}" \
+      "${autoscaling_bastion_user}@${autoscaling_bastion_ip}" \
       'sudo mv /port/config.toml /etc/gitlab-runner/config.toml' \
     && echo '[INFO] Moving file to bastion: init.sh to /etc/gitlab-runner/init.sh' \
-    && scp -i "${tmp_file_1}" "__envInit__" "${bastion_user}@${bastion_ip}:/port/init.sh" \
-    && ssh -i "${tmp_file_1}" "${bastion_user}@${bastion_ip}" \
+    && scp -i \
+      "${tmp_file_1}" \
+      "__envInit__" \
+      "${autoscaling_bastion_user}@${autoscaling_bastion_ip}:/port/init.sh" \
+    && ssh -i \
+      "${tmp_file_1}" \
+      "${autoscaling_bastion_user}@${autoscaling_bastion_ip}" \
       'sudo mv /port/init.sh /etc/gitlab-runner/init.sh' \
     && echo '[INFO] Reloading bastion config: /etc/gitlab-runner/config.toml' \
-    && ssh -i "${tmp_file_1}" "${bastion_user}@${bastion_ip}" \
+    && ssh -i \
+      "${tmp_file_1}" \
+      "${autoscaling_bastion_user}@${autoscaling_bastion_ip}" \
       'sudo killall -SIGHUP gitlab-runner' \
     && echo '[INFO] Removing temporary files' \
     && rm -rf "${tmp_file_1}" "${tmp_file_2}" \
