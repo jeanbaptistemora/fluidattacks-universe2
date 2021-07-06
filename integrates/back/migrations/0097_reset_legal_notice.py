b875edb504ca80a2dbc338063c1648c093f159ff
Rafael Rodriguez <rrodriguez@fluidattacks.com>
2021-07-06 15:04:07 -0500

    integrates\feat(back): #4902.1 reset legal notice
    - Add migration for resetting acceptance of legal terms
    
    
    Former-commit-id: c6b3b2d9ad7cb0b8cb6e934c91a6e97102dd836c

---
+++ integrates/back/migrations/0097_reset_legal_notice.py
@@ -0,0 +1,42 @@
+# pylint: disable=invalid-name
+"""
+This migration aims to reset the acceptance of the legal notice
+for every ASM user as a result of the changes in our legal terms
+
+"""
+
+from aioextensions import (
+    run,
+)
+from boto3.dynamodb.conditions import (
+    Attr,
+)
+import time
+from users import (
+    dal as users_dal,
+    domain as users_domain,
+)
+
+PROD: bool = False
+
+
+async def main() -> None:
+    users = await users_dal.get_all(Attr("legal_remember").eq(True))
+
+    for user in users:
+        print(f"Legal terms acceptance will be reset for {user['email']}")
+
+        if PROD:
+            await users_domain.update_legal_remember(user["email"], False)
+            print(f"Legal terms acceptance of {user['email']} has been reset")
+
+
+if __name__ == "__main__":
+    execution_time = time.strftime(
+        "Execution Time:    %Y-%m-%d at %H:%M:%S UTC%Z"
+    )
+    run(main())
+    finalization_time = time.strftime(
+        "Finalization Time: %Y-%m-%d at %H:%M:%S UTC%Z"
+    )
+    print(f"{execution_time}\n{finalization_time}")
