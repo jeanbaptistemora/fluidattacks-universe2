3b26bc769d5ccc37579895ad2ddf0e3544b598e3
Kevin Amado <kamado@fluidattacks.com>
2021-07-07 17:32:01 -0500

    makes\refac(build): #4253.13 migrate jobs
    - Migrate jobs from makes3 to makes 21.08
    
    
    Former-commit-id: 23642c59d0665ad9351d1a49e2793a7268a36d9d

--- makes/makes.nix
+++ makes/makes.nix
@@ -1,5 +1,18 @@
 # https://github.com/fluidattacks/makes
+{ config
+, ...
+}:
 {
+  deployContainerImage = {
+    enable = true;
+    images = {
+      makesProd = {
+        src = config.inputs.product.makes-oci;
+        registry = "registry.gitlab.com";
+        tag = "fluidattacks/product/makes:latest";
+      };
+    };
+  };
   formatBash = {
     enable = true;
     targets = [ "/" ];
