d094269b7765f8bf2a466c9ee5ee0c9e6f758487
Daniel Murcia <dmurcia@fluidattacks.com>
2021-07-07 08:44:58 -0500

    observes\feat(build): #4791.44 boto dependency
    - add boto3 dependency to target
    
    
    Former-commit-id: e34c2a3e445fcd0769935428aef00d3348e3867d

--- makes/packages/observes/env/target-redshift/runtime/python/default.nix
+++ makes/packages/observes/env/target-redshift/runtime/python/default.nix
@@ -9,6 +9,7 @@ buildPythonRequirements {
   ];
   requirements = {
     direct = [
+      "boto3==1.17.104"
       "click==7.1.2"
       "jsonschema==3.2.0"
       "psycopg2==2.8.4"
@@ -16,10 +17,15 @@ buildPythonRequirements {
     ];
     inherited = [
       "attrs==20.3.0"
+      "botocore==1.20.104"
       "importlib-metadata==3.4.0"
+      "jmespath==0.10.0"
       "pyrsistent==0.17.3"
+      "python-dateutil==2.8.1"
+      "s3transfer==0.4.2"
       "six==1.15.0"
       "typing-extensions==3.7.4.3"
+      "urllib3==1.26.6"
       "zipp==3.4.0"
     ];
   };
