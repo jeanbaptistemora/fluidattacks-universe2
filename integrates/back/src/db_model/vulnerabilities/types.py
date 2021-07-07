a8d1ec921a6e733169b8d3badc4fe1228d2ca4a1
Jonathan Mesa <jmesa@fluidattacks.com>
2021-07-07 18:49:50 -0500

    integrates\refac(back): #4329.134 update vuln entity
    - create types separate file for consistency
    with the structure in findings entity
    
    
    Former-commit-id: e05144ed52eb38dcc74e5fde577397f28ece222a

---
+++ integrates/back/src/db_model/vulnerabilities/types.py
@@ -0,0 +1,69 @@
+from .enums import (
+    VulnerabilityAcceptanceStatus,
+    VulnerabilityApprovalStatus,
+    VulnerabilityDeletionJustification,
+    VulnerabilityStateStatus,
+    VulnerabilityTreatmentStatus,
+    VulnerabilityType,
+    VulnerabilityVerificationStatus,
+    VulnerabilityZeroRiskStatus,
+)
+from db_model.enums import (
+    Source,
+)
+from typing import (
+    List,
+    NamedTuple,
+    Optional,
+)
+
+
+class VulnerabilityState(NamedTuple):
+    approval_status: Optional[VulnerabilityApprovalStatus]
+    justification: Optional[VulnerabilityDeletionJustification]
+    modified_by: str
+    modified_date: str
+    source: Source
+    status: VulnerabilityStateStatus
+
+
+class VulnerabilityTreatment(NamedTuple):
+    accepted_until: Optional[str]
+    acceptance_status: Optional[VulnerabilityAcceptanceStatus]
+    justification: Optional[str]
+    manager: Optional[str]
+    modified_by: str
+    modified_date: str
+    status: VulnerabilityTreatmentStatus
+
+
+class VulnerabilityVerification(NamedTuple):
+    modified_by: str
+    modified_date: str
+    status: VulnerabilityVerificationStatus
+
+
+class VulnerabilityZeroRisk(NamedTuple):
+    comment_id: str
+    modified_by: str
+    modified_date: str
+    status: VulnerabilityZeroRiskStatus
+
+
+class Vulnerability(NamedTuple):
+    bts_url: Optional[str]
+    commit: Optional[str]
+    custom_severity: Optional[int]
+    finding_id: str
+    hash: Optional[int]
+    repo: Optional[str]
+    specific: str
+    state: VulnerabilityState
+    stream: Optional[List[str]]
+    uuid: str
+    tags: Optional[List[str]]
+    treatment: Optional[VulnerabilityTreatment]
+    type: VulnerabilityType
+    verification: Optional[VulnerabilityVerification]
+    where: str
+    zero_risk: Optional[VulnerabilityZeroRisk]
