3fe1f029bfd7875ff3d7a50706bd749d7a8382ea
Andres Aguirre <aaguirre@fluidattacks.com>
2021-07-08 14:23:54 -0500

    integrates\refac(back): #4329.141 register finding types
    - Register the finding types to be serialized and deserialize
    into the cache
    
    
    Former-commit-id: 790dfd77fed4a0db3deab80e423f0fa76be2f082

--- integrates/back/src/redis_cluster/serialization.py
+++ integrates/back/src/redis_cluster/serialization.py
@@ -4,21 +4,66 @@ from data_containers.toe_inputs import (
 from data_containers.toe_lines import (
     GitRootToeLines,
 )
+from db_model.enums import (
+    Source,
+)
 from db_model.findings.enums import (
+    FindingSorts,
+    FindingStateJustification,
     FindingStateStatus,
+    FindingStatus,
+    FindingVerificationStatus,
 )
 from db_model.findings.types import (
+    Finding,
+    Finding20Severity,
+    Finding31Severity,
+    FindingEvidence,
+    FindingEvidences,
     FindingState,
+    FindingUnreliableIndicators,
+    FindingVerification,
 )
 import safe_pickle
 from safe_pickle import (
+    _deserialize,
     dump,
+    dump_raw,
     load,
+    serialize,
+    Serialized,
+)
+from typing import (
+    Any,
+    List,
+    Set,
 )
 
+
+def set_dump(instance: List[Any]) -> Serialized:
+    return serialize(instance, *map(dump_raw, instance))
+
+
+def set_load(*args: Serialized) -> Set[Any]:
+    return set(map(_deserialize, args))
+
+
 # Side effects
+safe_pickle.register(set, set_dump, set_load)
+safe_pickle.register_enum(FindingSorts)
+safe_pickle.register_enum(FindingStateJustification)
 safe_pickle.register_enum(FindingStateStatus)
+safe_pickle.register_enum(FindingStatus)
+safe_pickle.register_enum(FindingVerificationStatus)
+safe_pickle.register_enum(Source)
+safe_pickle.register_namedtuple(Finding)
+safe_pickle.register_namedtuple(Finding20Severity)
+safe_pickle.register_namedtuple(Finding31Severity)
+safe_pickle.register_namedtuple(FindingEvidence)
+safe_pickle.register_namedtuple(FindingEvidences)
 safe_pickle.register_namedtuple(FindingState)
+safe_pickle.register_namedtuple(FindingUnreliableIndicators)
+safe_pickle.register_namedtuple(FindingVerification)
 safe_pickle.register_namedtuple(GitRootToeInput)
 safe_pickle.register_namedtuple(GitRootToeLines)
 
