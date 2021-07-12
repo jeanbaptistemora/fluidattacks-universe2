2931d0f3937a04e91c2254cc97b49b88490c9b61
John Perez <jperez@fluidattacks.com>
2021-07-12 13:51:48 -0500

    airs\feat(front): #4974.1 bugsnag report
    - Add bugsnag report for Airs
    
    
    Former-commit-id: 4532aac96e8d35e3314452502cdbb5f4c958bd78

--- airs/front/gatsby-browser.js
+++ airs/front/gatsby-browser.js
@@ -1,3 +1,50 @@
 import "./static/scss/index.scss";
 import "gatsby-plugin-breadcrumb/gatsby-plugin-breadcrumb.css";
 import "animate.css/animate.min.css";
+import Bugsnag from "@bugsnag/js";
+import BugsnagPluginReact from "@bugsnag/plugin-react";
+import _ from "lodash";
+import React, { Fragment } from "react";
+
+const getEnvironment = () => {
+  if (_.isUndefined(window)) {
+    return "development";
+  }
+  const currentUrl = window.location.hostname;
+  const ephemeralDomainRegex = /^web.eph.fluidattacks.com/gu;
+
+  if (currentUrl === "localhost") {
+    return "development";
+  } else if (ephemeralDomainRegex.test(currentUrl)) {
+    return "ephemeral";
+  }
+
+  return "production";
+};
+
+Bugsnag.start({
+  apiKey: "6d0d7e66955855de59cfff659e6edf31",
+  appVersion: "airs_version",
+  onError: (event) => {
+    event.errors.forEach((error) => {
+      const message = event.context;
+      event.context = error.errorMessage;
+      error.errorMessage = _.isString(message) ? message : "";
+      event.groupingHash = event.context;
+    });
+
+    return true;
+  },
+  plugins: [new BugsnagPluginReact(React)],
+  releaseStage: getEnvironment(),
+});
+
+const reactPlugin = Bugsnag.getPlugin("react");
+
+const BugsnagErrorBoundary = _.isUndefined(reactPlugin)
+  ? Fragment
+  : reactPlugin.createErrorBoundary(React);
+
+export const wrapRootElement = ({ element }) => (
+  <BugsnagErrorBoundary>{element}</BugsnagErrorBoundary>
+);
