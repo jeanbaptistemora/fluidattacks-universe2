2c0bedc15731323b4ac7b41b33373bb3e995e5fe
Kevin Amado <kamado@fluidattacks.com>
2021-07-13 15:43:26 -0500

    feat(conf): #4960.6 non upgradable model
    - Add a model for this finding
    
    
    Former-commit-id: d4754447c06543ee53decb4857c00a2fb18d61de

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -1014,6 +1014,26 @@ class FindingEnum(Enum):
             user_interaction=cvss3_model.UserInteraction.required,
         ),
     )
+    F079: FindingMetadata = FindingMetadata.new(
+        auto_approve=False,
+        code="F079",
+        cwe=829,
+        execution_queue=ExecutionQueue.f117,
+        requirements=[302],
+        score=cvss3_model.Score(
+            attack_complexity=cvss3_model.AttackComplexity.high,
+            attack_vector=cvss3_model.AttackVector.network,
+            availability_impact=cvss3_model.AvailabilityImpact.none,
+            confidentiality_impact=cvss3_model.ConfidentialityImpact.none,
+            exploitability=cvss3_model.Exploitability.poc,
+            integrity_impact=cvss3_model.IntegrityImpact.low,
+            privileges_required=cvss3_model.PrivilegesRequired.low,
+            remediation_level=cvss3_model.RemediationLevel.unavailable,
+            report_confidence=cvss3_model.ReportConfidence.confirmed,
+            severity_scope=cvss3_model.SeverityScope.unchanged,
+            user_interaction=cvss3_model.UserInteraction.none,
+        ),
+    )
     F085: FindingMetadata = FindingMetadata.new(
         auto_approve=True,
         code="F085",
