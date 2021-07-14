cca378aff2da209b68cb61c59e4be0ac0a5317bc
Frank Aristizabal <faristizabal@fluidattacks.com>
2021-07-14 14:57:45 -0500

    airs\refac(front): #4544.23 update menu
    - Add components
    to the new menu for mobile.
    
    
    Former-commit-id: 7e4ebae43ba9664d933e4b8f9d880741a471a7b0

---
+++ airs/front/src/components/Navbar/Menu/MenuMobile/ResourcesList/index.tsx
@@ -0,0 +1,44 @@
+/* eslint import/no-namespace:0 */
+/* eslint react/forbid-component-props: 0 */
+/* eslint import/no-unresolved:0 */
+import { faAngleDown } from "@fortawesome/pro-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+import {
+  FontAwesomeContainerSmall,
+  InnerContentList,
+  InnerListContainer,
+  InnerListItem,
+  ListItem,
+  ListItemCheckbox,
+  ListItemLabel,
+} from "../../../../../styles/styledComponents";
+import { BodyLink } from "../BodyLink";
+
+const ResourcesList: React.FC = (): JSX.Element => (
+  <div>
+    <ListItem>
+      <ListItemCheckbox
+        className={"resources-title"}
+        id={"resources-title"}
+        name={"resources"}
+      />
+      <ListItemLabel htmlFor={"resources-title"}>
+        {"Resources"}
+        <FontAwesomeContainerSmall>
+          <FontAwesomeIcon className={"c-gray-176"} icon={faAngleDown} />
+        </FontAwesomeContainerSmall>
+      </ListItemLabel>
+      <InnerListContainer className={"resources-list"}>
+        <InnerContentList>
+          <InnerListItem>
+            <BodyLink link={"/resources/criteria/"} name={"Criteria"} />
+          </InnerListItem>
+        </InnerContentList>
+      </InnerListContainer>
+    </ListItem>
+  </div>
+);
+
+export { ResourcesList };
