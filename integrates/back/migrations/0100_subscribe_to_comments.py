ced2bcc385ec303b0ff24a4b8dd0f470d554b2f3
Jonathan Mesa <jmesa@fluidattacks.com>
2021-07-14 11:44:31 -0500

    integrates\feat(back): #4972.3 add migration
    - add migration to subscribe fluid attacks users to comments
    
    
    Former-commit-id: a62f219ab8602ba95739cece9d69c59ceece3f23

---
+++ integrates/back/migrations/0100_subscribe_to_comments.py
@@ -0,0 +1,81 @@
+# pylint: disable=invalid-name
+"""
+Subscribe Fluid Attacks staff to "COMMENTS"
+
+Execution Time:
+Finalization Time:
+"""
+
+from aioextensions import (
+    collect,
+    run,
+)
+from group_access.domain import (
+    get_users_to_notify,
+)
+from groups.dal import (
+    get_active_groups,
+)
+from itertools import (
+    chain,
+)
+from subscriptions import (
+    domain as subs_domain,
+)
+import time
+
+PROD: bool = False
+
+
+async def _get_subscriptions_to_comments() -> int:
+    subs = await subs_domain.get_subscriptions_to_entity_report(
+        audience="user",
+    )
+    return [sub for sub in subs if sub["sk"]["entity"] == "COMMENTS"]
+
+
+async def main() -> None:
+    groups = await get_active_groups()
+    active_users = set(
+        chain.from_iterable(
+            await collect(get_users_to_notify(group) for group in groups)
+        )
+    )
+    print(f"Active: {len(active_users)} users")
+
+    fluid_users = [
+        user for user in active_users if "@fluidattacks.com" in user
+    ]
+    print(f"Fluid: {len(fluid_users)} users")
+
+    print(
+        f"Subscriptions BEFORE migration: "
+        f"{len(await _get_subscriptions_to_comments())}"
+    )
+
+    if PROD:
+        await collect(
+            subs_domain.subscribe_user_to_entity_report(
+                event_frequency="DAILY",
+                report_entity="COMMENTS",
+                report_subject="ALL_GROUPS",
+                user_email=user,
+            )
+            for user in fluid_users
+        )
+
+        print(
+            f"Subscriptions AFTER migration: "
+            f"{len(await _get_subscriptions_to_comments())}"
+        )
+
+
+if __name__ == "__main__":
+    execution_time = time.strftime(
+        "Execution Time:    %Y-%m-%d at %H:%M:%S UTC%Z"
+    )
+    run(main())
+    finalization_time = time.strftime(
+        "Finalization Time: %Y-%m-%d at %H:%M:%S UTC%Z"
+    )
+    print(f"{execution_time}\n{finalization_time}")
