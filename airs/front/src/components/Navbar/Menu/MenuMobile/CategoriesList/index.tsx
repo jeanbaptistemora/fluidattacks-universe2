1565eb65e994dcb6ba16c637022980c38e570f8c
Frank Aristizabal <faristizabal@fluidattacks.com>
2021-07-16 11:30:55 -0500

    airs\refac(front): #4544.36 update menu
    - Update categories menu dropdowns.
    - no-deltas-test
    
    
    Former-commit-id: f37155b687f8a923083d66af8be9f0af8cf011b2

--- airs/front/src/components/Navbar/Menu/MenuMobile/CategoriesList/index.tsx
+++ airs/front/src/components/Navbar/Menu/MenuMobile/CategoriesList/index.tsx
@@ -3,7 +3,7 @@
 /* eslint import/no-unresolved:0 */
 import { faAngleDown } from "@fortawesome/pro-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 import {
   FontAwesomeContainerSmall,
@@ -11,49 +11,55 @@ import {
   InnerListContainer,
   InnerListItem,
   ListItem,
-  ListItemCheckbox,
   ListItemLabel,
 } from "../../../../../styles/styledComponents";
 import { BodyLink } from "../BodyLink";
 
-const CategoriesList: React.FC = (): JSX.Element => (
-  <div>
-    <ListItem>
-      <ListItemCheckbox
-        className={"categories-title"}
-        id={"categories-title"}
-        name={"categories"}
-      />
-      <ListItemLabel htmlFor={"categories-title"}>
-        {"Categories"}
-        <FontAwesomeContainerSmall>
-          <FontAwesomeIcon className={"c-gray-176"} icon={faAngleDown} />
-        </FontAwesomeContainerSmall>
-      </ListItemLabel>
-      <InnerListContainer className={"categories-list"}>
-        <InnerContentList>
-          <InnerListItem>
-            <BodyLink link={"/categories/sast/"} name={"SAST"} />
-          </InnerListItem>
-          <InnerListItem>
-            <BodyLink link={"/categories/dast/"} name={"DAST"} />
-          </InnerListItem>
-          <InnerListItem>
-            <BodyLink link={"/solutions/penetration-testing/"} name={"MPT"} />
-          </InnerListItem>
-          <InnerListItem>
-            <BodyLink link={"/categories/sca/"} name={"SCA"} />
-          </InnerListItem>
-          <InnerListItem>
-            <BodyLink link={"/categories/re/"} name={"RE"} />
-          </InnerListItem>
-          <InnerListItem>
-            <BodyLink link={"/categories/ptaas/"} name={"PTaaS"} />
-          </InnerListItem>
-        </InnerContentList>
-      </InnerListContainer>
-    </ListItem>
-  </div>
-);
+const CategoriesList: React.FC = (): JSX.Element => {
+  const [isTouch, setIsTouch] = useState(false);
+  const handleOpenClose = useCallback((): void => {
+    setIsTouch(!isTouch);
+  }, [isTouch]);
+
+  return (
+    <div>
+      <ListItem>
+        <ListItemLabel onClick={handleOpenClose}>
+          {"Categories"}
+          <FontAwesomeContainerSmall>
+            <FontAwesomeIcon className={"c-gray-176"} icon={faAngleDown} />
+          </FontAwesomeContainerSmall>
+        </ListItemLabel>
+        <InnerListContainer
+          style={{
+            animation: "fadein 0.5s",
+            display: isTouch ? "block" : "none",
+          }}
+        >
+          <InnerContentList>
+            <InnerListItem>
+              <BodyLink link={"/categories/sast/"} name={"SAST"} />
+            </InnerListItem>
+            <InnerListItem>
+              <BodyLink link={"/categories/dast/"} name={"DAST"} />
+            </InnerListItem>
+            <InnerListItem>
+              <BodyLink link={"/solutions/penetration-testing/"} name={"MPT"} />
+            </InnerListItem>
+            <InnerListItem>
+              <BodyLink link={"/categories/sca/"} name={"SCA"} />
+            </InnerListItem>
+            <InnerListItem>
+              <BodyLink link={"/categories/re/"} name={"RE"} />
+            </InnerListItem>
+            <InnerListItem>
+              <BodyLink link={"/categories/ptaas/"} name={"PTaaS"} />
+            </InnerListItem>
+          </InnerContentList>
+        </InnerListContainer>
+      </ListItem>
+    </div>
+  );
+};
 
 export { CategoriesList };
