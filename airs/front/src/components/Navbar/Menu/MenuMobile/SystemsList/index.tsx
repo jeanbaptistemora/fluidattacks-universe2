6fb4d8da582da93cd01df8e1ad48f3041c08087e
Frank Aristizabal <faristizabal@fluidattacks.com>
2021-07-16 14:36:35 -0500

    airs\refac(front): #4544.40 update menu
    - Update systems menu dropdown.
    - no-deltas-test
    
    
    Former-commit-id: 28113454f524533dc1d90a523ce123c6e5d7dab4

--- airs/front/src/components/Navbar/Menu/MenuMobile/SystemsList/index.tsx
+++ airs/front/src/components/Navbar/Menu/MenuMobile/SystemsList/index.tsx
@@ -3,7 +3,7 @@
 /* eslint import/no-unresolved:0 */
 import { faAngleDown } from "@fortawesome/pro-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 import {
   FontAwesomeContainerSmall,
@@ -11,64 +11,76 @@ import {
   InnerListContainer,
   InnerListItem,
   ListItem,
-  ListItemCheckbox,
   ListItemLabel,
 } from "../../../../../styles/styledComponents";
 import { BodyLink } from "../BodyLink";
 
-const SystemsList: React.FC = (): JSX.Element => (
-  <div>
-    <ListItem>
-      <ListItemCheckbox
-        className={"systems-title"}
-        id={"systems-title"}
-        name={"systems"}
-      />
-      <ListItemLabel htmlFor={"systems-title"}>
-        {"Systems"}
-        <FontAwesomeContainerSmall>
-          <FontAwesomeIcon className={"c-gray-176"} icon={faAngleDown} />
-        </FontAwesomeContainerSmall>
-      </ListItemLabel>
-      <InnerListContainer className={"systems-list"}>
-        <InnerContentList>
-          <InnerListItem>
-            <BodyLink link={"/systems/web-apps/"} name={"Web Applications"} />
-          </InnerListItem>
-          <InnerListItem>
-            <BodyLink
-              link={"/systems/mobile-apps/"}
-              name={"Mobile Applications"}
-            />
-          </InnerListItem>
-          <InnerListItem>
-            <BodyLink link={"/systems/thick-clients/"} name={"Thick Clients"} />
-          </InnerListItem>
-          <InnerListItem>
-            <BodyLink link={"/systems/apis/"} name={"APIs and Microservices"} />
-          </InnerListItem>
-          <InnerListItem>
-            <BodyLink
-              link={"/systems/cloud-infrastructure/"}
-              name={"Cloud Infrastructure"}
-            />
-          </InnerListItem>
-          <InnerListItem>
-            <BodyLink
-              link={"/systems/networks-and-hosts/"}
-              name={"Networks and Hosts"}
-            />
-          </InnerListItem>
-          <InnerListItem>
-            <BodyLink link={"/systems/iot/"} name={"Internet of Things"} />
-          </InnerListItem>
-          <InnerListItem>
-            <BodyLink link={"/systems/ot/"} name={"SCADA and OT"} />
-          </InnerListItem>
-        </InnerContentList>
-      </InnerListContainer>
-    </ListItem>
-  </div>
-);
+const SystemsList: React.FC = (): JSX.Element => {
+  const [isTouch, setIsTouch] = useState(false);
+  const handleOpenClose = useCallback((): void => {
+    setIsTouch(!isTouch);
+  }, [isTouch]);
+
+  return (
+    <div>
+      <ListItem>
+        <ListItemLabel onClick={handleOpenClose}>
+          {"Systems"}
+          <FontAwesomeContainerSmall>
+            <FontAwesomeIcon className={"c-gray-176"} icon={faAngleDown} />
+          </FontAwesomeContainerSmall>
+        </ListItemLabel>
+        <InnerListContainer
+          style={{
+            animation: "fadein 0.5s",
+            display: isTouch ? "block" : "none",
+          }}
+        >
+          <InnerContentList>
+            <InnerListItem>
+              <BodyLink link={"/systems/web-apps/"} name={"Web Applications"} />
+            </InnerListItem>
+            <InnerListItem>
+              <BodyLink
+                link={"/systems/mobile-apps/"}
+                name={"Mobile Applications"}
+              />
+            </InnerListItem>
+            <InnerListItem>
+              <BodyLink
+                link={"/systems/thick-clients/"}
+                name={"Thick Clients"}
+              />
+            </InnerListItem>
+            <InnerListItem>
+              <BodyLink
+                link={"/systems/apis/"}
+                name={"APIs and Microservices"}
+              />
+            </InnerListItem>
+            <InnerListItem>
+              <BodyLink
+                link={"/systems/cloud-infrastructure/"}
+                name={"Cloud Infrastructure"}
+              />
+            </InnerListItem>
+            <InnerListItem>
+              <BodyLink
+                link={"/systems/networks-and-hosts/"}
+                name={"Networks and Hosts"}
+              />
+            </InnerListItem>
+            <InnerListItem>
+              <BodyLink link={"/systems/iot/"} name={"Internet of Things"} />
+            </InnerListItem>
+            <InnerListItem>
+              <BodyLink link={"/systems/ot/"} name={"SCADA and OT"} />
+            </InnerListItem>
+          </InnerContentList>
+        </InnerListContainer>
+      </ListItem>
+    </div>
+  );
+};
 
 export { SystemsList };
