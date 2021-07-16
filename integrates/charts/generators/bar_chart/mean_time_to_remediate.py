a915f55b9281162efa61811dba89f1eb68db526e
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-07-15 19:22:06 -0500

    integrates\feat(back): #4993.3 update generator
    - update mean_time_to_remediate_serverity generator to include
    30 or 90 days options
    - no-deltas-test
    
    
    Former-commit-id: 962383e6e4d8203d646c3c447923a1683994b52f

--- integrates/charts/generators/bar_chart/mean_time_to_remediate.py
+++ integrates/charts/generators/bar_chart/mean_time_to_remediate.py
@@ -11,11 +11,22 @@ from charts import (
 from charts.colors import (
     RISK,
 )
+from dataloaders import (
+    Dataloaders,
+    get_new_context,
+)
+from datetime import (
+    date,
+)
 from decimal import (
     Decimal,
+    ROUND_CEILING,
 )
 from groups import (
     domain as groups_domain,
+)
+from newutils import (
+    datetime as datetime_utils,
 )
 from statistics import (
     mean,
@@ -23,6 +34,7 @@ from statistics import (
 from typing import (
     List,
     NamedTuple,
+    Optional,
 )
 
 Remediate = NamedTuple(
@@ -37,43 +49,65 @@ Remediate = NamedTuple(
 
 
 @alru_cache(maxsize=None, typed=True)
-async def get_data_one_group(group: str) -> Remediate:
-    group_data = await groups_domain.get_attributes(
-        group,
+async def get_data_one_group(
+    *, group: str, loaders: Dataloaders, min_date: Optional[date] = None
+) -> Remediate:
+    critical, high, medium, low = await collect(
         [
-            "mean_remediate_critical_severity",
-            "mean_remediate_high_severity",
-            "mean_remediate_medium_severity",
-            "mean_remediate_low_severity",
-        ],
+            groups_domain.get_mean_remediate_severity(
+                loaders, group, 9, 10, min_date
+            ),
+            groups_domain.get_mean_remediate_severity(
+                loaders, group, 7, 8.9, min_date
+            ),
+            groups_domain.get_mean_remediate_severity(
+                loaders, group, 4, 6.9, min_date
+            ),
+            groups_domain.get_mean_remediate_severity(
+                loaders, group, 0.1, 3.9, min_date
+            ),
+        ]
     )
 
     return Remediate(
-        critical_severity=group_data.get(
-            "mean_remediate_critical_severity", 0
-        ),
-        high_severity=group_data.get("mean_remediate_high_severity", 0),
-        medium_severity=group_data.get("mean_remediate_medium_severity", 0),
-        low_severity=group_data.get("mean_remediate_low_severity", 0),
+        critical_severity=critical,
+        high_severity=high,
+        medium_severity=medium,
+        low_severity=low,
     )
 
 
-async def get_data_many_groups(groups: List[str]) -> Remediate:
-    groups_data = await collect(map(get_data_one_group, groups))
+async def get_data_many_groups(
+    *, groups: List[str], loaders: Dataloaders, min_date: Optional[date] = None
+) -> Remediate:
+    groups_data = await collect(
+        [
+            get_data_one_group(group=group, loaders=loaders, min_date=min_date)
+            for group in groups
+        ]
+    )
 
     return Remediate(
         critical_severity=Decimal(
             mean([group.critical_severity for group in groups_data])
-        ).quantize(Decimal("0.1")),
+        )
+        .quantize(Decimal("0.1"))
+        .to_integral_exact(rounding=ROUND_CEILING),
         high_severity=Decimal(
             mean([group.high_severity for group in groups_data])
-        ).quantize(Decimal("0.1")),
+        )
+        .quantize(Decimal("0.1"))
+        .to_integral_exact(rounding=ROUND_CEILING),
         medium_severity=Decimal(
             mean([group.medium_severity for group in groups_data])
-        ).quantize(Decimal("0.1")),
+        )
+        .quantize(Decimal("0.1"))
+        .to_integral_exact(rounding=ROUND_CEILING),
         low_severity=Decimal(
             mean([group.low_severity for group in groups_data])
-        ).quantize(Decimal("0.1")),
+        )
+        .quantize(Decimal("0.1"))
+        .to_integral_exact(rounding=ROUND_CEILING),
     )
 
 
@@ -100,42 +134,72 @@ def format_data(data: Remediate) -> dict:
                 categories=[translations[column] for column in translations],
                 type="category",
             ),
+            y=dict(
+                min=0,
+                padding=dict(
+                    bottom=0,
+                ),
+            ),
         ),
+        barChartYTickFormat=True,
     )
 
 
 async def generate_all() -> None:
-    async for group in utils.iterate_groups():
-        utils.json_dump(
-            document=format_data(data=await get_data_one_group(group)),
-            entity="group",
-            subject=group,
-        )
-
-    async for org_id, _, org_groups in (
-        utils.iterate_organizations_and_groups()
-    ):
-        if org_groups:
+    loaders: Dataloaders = get_new_context()
+    list_days: List[int] = [30, 90]
+    dates: List[date] = [
+        datetime_utils.get_now_minus_delta(days=list_days[0]).date(),
+        datetime_utils.get_now_minus_delta(days=list_days[1]).date(),
+    ]
+    for days, min_date in zip([None, *list_days], [None, *dates]):
+        async for group in utils.iterate_groups():
             utils.json_dump(
                 document=format_data(
-                    data=await get_data_many_groups(list(org_groups)),
+                    data=await get_data_one_group(
+                        group=group, loaders=loaders, min_date=min_date
+                    ),
                 ),
-                entity="organization",
-                subject=org_id,
+                entity="group",
+                subject=f"{group}_{days}" if days else group,
             )
 
-    async for org_id, org_name, _ in (
-        utils.iterate_organizations_and_groups()
-    ):
-        for portfolio, groups in await utils.get_portfolios_groups(org_name):
-            if groups:
+        async for org_id, _, org_groups in (
+            utils.iterate_organizations_and_groups()
+        ):
+            if org_groups:
                 utils.json_dump(
                     document=format_data(
-                        data=await get_data_many_groups(groups),
+                        data=await get_data_many_groups(
+                            groups=list(org_groups),
+                            loaders=loaders,
+                            min_date=min_date,
+                        ),
                     ),
-                    entity="portfolio",
-                    subject=f"{org_id}PORTFOLIO#{portfolio}",
+                    entity="organization",
+                    subject=f"{org_id}_{days}" if days else org_id,
                 )
+
+        async for org_id, org_name, _ in (
+            utils.iterate_organizations_and_groups()
+        ):
+            for portfolio, groups in await utils.get_portfolios_groups(
+                org_name
+            ):
+                if groups:
+                    utils.json_dump(
+                        document=format_data(
+                            data=await get_data_many_groups(
+                                groups=groups,
+                                loaders=loaders,
+                                min_date=min_date,
+                            ),
+                        ),
+                        entity="portfolio",
+                        subject=f"{org_id}PORTFOLIO#{portfolio}_{days}"
+                        if days
+                        else f"{org_id}PORTFOLIO#{portfolio}",
+                    )
 
 
 if __name__ == "__main__":
