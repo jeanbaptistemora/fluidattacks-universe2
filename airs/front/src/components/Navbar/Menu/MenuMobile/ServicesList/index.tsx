39a2fa45982f95e6d0795520078cd95eb97a1156
Frank Aristizabal <faristizabal@fluidattacks.com>
2021-07-16 12:29:26 -0500

    airs\refac(front): #4544.38 update menu
    - Update resources menu dropdowns.
    - no-deltas-test
    
    
    Former-commit-id: 4104191ed6a5ebde028a9531362624f9ee6d141b

--- airs/front/src/components/Navbar/Menu/MenuMobile/ServicesList/index.tsx
+++ airs/front/src/components/Navbar/Menu/MenuMobile/ServicesList/index.tsx
@@ -3,7 +3,7 @@
 /* eslint import/no-unresolved:0 */
 import { faAngleDown } from "@fortawesome/pro-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 import {
   FontAwesomeContainerSmall,
@@ -11,46 +11,52 @@ import {
   InnerListContainer,
   InnerListItem,
   ListItem,
-  ListItemCheckbox,
   ListItemLabel,
 } from "../../../../../styles/styledComponents";
 import { BodyLink } from "../BodyLink";
 
-const ServicesList: React.FC = (): JSX.Element => (
-  <div>
-    <ListItem>
-      <ListItemCheckbox
-        className={"services-title"}
-        id={"services-title"}
-        name={"services"}
-      />
-      <ListItemLabel htmlFor={"services-title"}>
-        {"Services"}
-        <FontAwesomeContainerSmall>
-          <FontAwesomeIcon className={"c-gray-176"} icon={faAngleDown} />
-        </FontAwesomeContainerSmall>
-      </ListItemLabel>
-      <InnerListContainer className={"services-list"}>
-        <InnerContentList>
-          <InnerListItem>
-            <BodyLink
-              link={"/services/continuous-hacking/"}
-              name={"Continuous Hacking"}
-            />
-          </InnerListItem>
-          <InnerListItem>
-            <BodyLink
-              link={"/services/one-shot-hacking/"}
-              name={"One-Shot Hacking"}
-            />
-          </InnerListItem>
-          <InnerListItem>
-            <BodyLink link={"/services/comparative/"} name={"Comparative"} />
-          </InnerListItem>
-        </InnerContentList>
-      </InnerListContainer>
-    </ListItem>
-  </div>
-);
+const ServicesList: React.FC = (): JSX.Element => {
+  const [isTouch, setIsTouch] = useState(false);
+  const handleOpenClose = useCallback((): void => {
+    setIsTouch(!isTouch);
+  }, [isTouch]);
+
+  return (
+    <div>
+      <ListItem>
+        <ListItemLabel onClick={handleOpenClose}>
+          {"Services"}
+          <FontAwesomeContainerSmall>
+            <FontAwesomeIcon className={"c-gray-176"} icon={faAngleDown} />
+          </FontAwesomeContainerSmall>
+        </ListItemLabel>
+        <InnerListContainer
+          style={{
+            animation: "fadein 0.5s",
+            display: isTouch ? "block" : "none",
+          }}
+        >
+          <InnerContentList>
+            <InnerListItem>
+              <BodyLink
+                link={"/services/continuous-hacking/"}
+                name={"Continuous Hacking"}
+              />
+            </InnerListItem>
+            <InnerListItem>
+              <BodyLink
+                link={"/services/one-shot-hacking/"}
+                name={"One-Shot Hacking"}
+              />
+            </InnerListItem>
+            <InnerListItem>
+              <BodyLink link={"/services/comparative/"} name={"Comparative"} />
+            </InnerListItem>
+          </InnerContentList>
+        </InnerListContainer>
+      </ListItem>
+    </div>
+  );
+};
 
 export { ServicesList };
