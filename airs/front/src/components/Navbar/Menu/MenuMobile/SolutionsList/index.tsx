0e47cc4dc3708144fabffda60f7eef0c98e53d76
Frank Aristizabal <faristizabal@fluidattacks.com>
2021-07-16 14:15:51 -0500

    airs\refac(front): #4544.39 update menu
    - Update solution menu dropdown.
    - no-deltas-test
    
    
    Former-commit-id: 6188bd264cf554bed0585b296616dd34eea61362

--- airs/front/src/components/Navbar/Menu/MenuMobile/SolutionsList/index.tsx
+++ airs/front/src/components/Navbar/Menu/MenuMobile/SolutionsList/index.tsx
@@ -3,7 +3,7 @@
 /* eslint import/no-unresolved:0 */
 import { faAngleDown } from "@fortawesome/pro-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 import {
   FontAwesomeContainerSmall,
@@ -11,73 +11,79 @@ import {
   InnerListContainer,
   InnerListItem,
   ListItem,
-  ListItemCheckbox,
   ListItemLabel,
 } from "../../../../../styles/styledComponents";
 import { BodyLink } from "../BodyLink";
 
-const SolutionsList: React.FC = (): JSX.Element => (
-  <div>
-    <ListItem>
-      <ListItemCheckbox
-        className={"solutions-title"}
-        id={"solutions-title"}
-        name={"solutions"}
-      />
-      <ListItemLabel htmlFor={"solutions-title"}>
-        {"Solutions"}
-        <FontAwesomeContainerSmall>
-          <FontAwesomeIcon className={"c-gray-176"} icon={faAngleDown} />
-        </FontAwesomeContainerSmall>
-      </ListItemLabel>
-      <InnerListContainer className={"solutions-list"}>
-        <InnerContentList>
-          <InnerListItem>
-            <BodyLink link={"/solutions/devsecops/"} name={"DevSecOps"} />
-          </InnerListItem>
-          <InnerListItem>
-            <BodyLink
-              link={"/solutions/security-testing/"}
-              name={"Security Testing"}
-            />
-          </InnerListItem>
-          <InnerListItem>
-            <BodyLink
-              link={"/solutions/penetration-testing/"}
-              name={"Penetration Testing"}
-            />
-          </InnerListItem>
-          <InnerListItem>
-            <BodyLink
-              link={"/solutions/ethical-hacking/"}
-              name={"Ethical Hacking"}
-            />
-          </InnerListItem>
-          <InnerListItem>
-            <BodyLink link={"/solutions/red-teaming/"} name={"Red Teaming"} />
-          </InnerListItem>
-          <InnerListItem>
-            <BodyLink
-              link={"/solutions/attack-simulation/"}
-              name={"Breach and Attack Simulation"}
-            />
-          </InnerListItem>
-          <InnerListItem>
-            <BodyLink
-              link={"/solutions/secure-code-review/"}
-              name={"Secure Code Review"}
-            />
-          </InnerListItem>
-          <InnerListItem>
-            <BodyLink
-              link={"/solutions/vulnerability-management/"}
-              name={"Vulnerability Management"}
-            />
-          </InnerListItem>
-        </InnerContentList>
-      </InnerListContainer>
-    </ListItem>
-  </div>
-);
+const SolutionsList: React.FC = (): JSX.Element => {
+  const [isTouch, setIsTouch] = useState(false);
+  const handleOpenClose = useCallback((): void => {
+    setIsTouch(!isTouch);
+  }, [isTouch]);
+
+  return (
+    <div>
+      <ListItem>
+        <ListItemLabel onClick={handleOpenClose}>
+          {"Solutions"}
+          <FontAwesomeContainerSmall>
+            <FontAwesomeIcon className={"c-gray-176"} icon={faAngleDown} />
+          </FontAwesomeContainerSmall>
+        </ListItemLabel>
+        <InnerListContainer
+          style={{
+            animation: "fadein 0.5s",
+            display: isTouch ? "block" : "none",
+          }}
+        >
+          <InnerContentList>
+            <InnerListItem>
+              <BodyLink link={"/solutions/devsecops/"} name={"DevSecOps"} />
+            </InnerListItem>
+            <InnerListItem>
+              <BodyLink
+                link={"/solutions/security-testing/"}
+                name={"Security Testing"}
+              />
+            </InnerListItem>
+            <InnerListItem>
+              <BodyLink
+                link={"/solutions/penetration-testing/"}
+                name={"Penetration Testing"}
+              />
+            </InnerListItem>
+            <InnerListItem>
+              <BodyLink
+                link={"/solutions/ethical-hacking/"}
+                name={"Ethical Hacking"}
+              />
+            </InnerListItem>
+            <InnerListItem>
+              <BodyLink link={"/solutions/red-teaming/"} name={"Red Teaming"} />
+            </InnerListItem>
+            <InnerListItem>
+              <BodyLink
+                link={"/solutions/attack-simulation/"}
+                name={"Breach and Attack Simulation"}
+              />
+            </InnerListItem>
+            <InnerListItem>
+              <BodyLink
+                link={"/solutions/secure-code-review/"}
+                name={"Secure Code Review"}
+              />
+            </InnerListItem>
+            <InnerListItem>
+              <BodyLink
+                link={"/solutions/vulnerability-management/"}
+                name={"Vulnerability Management"}
+              />
+            </InnerListItem>
+          </InnerContentList>
+        </InnerListContainer>
+      </ListItem>
+    </div>
+  );
+};
 
 export { SolutionsList };
