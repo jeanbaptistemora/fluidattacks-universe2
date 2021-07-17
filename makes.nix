6ffad030a4e58852fa9a5ed19219e622d4f15a4f
Kevin Amado <kamado@fluidattacks.com>
2021-07-16 20:27:22 -0500

    makes\refac(build): #4253.24 centralize config
    - Due to the module system we can set this value
      at the top level and just extend in the sub-modules
    
    
    Former-commit-id: 669dd541368a6c43625c95ce050084e07331c22e

--- makes.nix
+++ makes.nix
@@ -15,5 +15,28 @@
     name = "fluidattacks";
     pubKey = "fluidattacks.cachix.org-1:upiUCP8kWnr7NxVSJtTOM+SBqL0pZhZnUoqPG04sBv0=";
   };
+  lintTerraform = {
+    enable = true;
+    config = ''
+      config {
+        module = true
+      }
+      plugin "aws" {
+        enabled = true
+        deep_check = true
+      }
+      rule "aws_resource_missing_tags" {
+        enabled = true
+        tags = [
+          "Name",
+          "management:type",
+          "management:product",
+        ]
+        exclude = [
+          "aws_iam_policy",
+        ]
+      }
+    '';
+  };
   requiredMakesVersion = "21.08";
 }
--- makes/makes.nix
+++ makes/makes.nix
@@ -38,27 +38,6 @@
     targets = [ "/" ];
   };
   lintTerraform = {
-    enable = true;
-    config = ''
-      config {
-        module = true
-      }
-      plugin "aws" {
-        enabled = true
-        deep_check = true
-      }
-      rule "aws_resource_missing_tags" {
-        enabled = true
-        tags = [
-          "Name",
-          "management:type",
-          "management:product",
-        ]
-        exclude = [
-          "aws_iam_policy",
-        ]
-      }
-    '';
     modules = {
       makesCi = {
         src = "/makes/applications/makes/ci/src/terraform";
