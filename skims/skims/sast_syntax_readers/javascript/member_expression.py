ce971ce9ce738f6a7d83d0fbbe680d265ad13d81
Diego Restrepo <drestrepo@fluidattacks.com>
2021-07-21 08:40:08 -0500

    skims\perf(back): #4846.300 syntax reader
    - member_expression
    
    
    Former-commit-id: b08d98aa705ee4a142a4cb71813637fd275f5cd5

--- skims/skims/sast_syntax_readers/javascript/member_expression.py
+++ skims/skims/sast_syntax_readers/javascript/member_expression.py
@@ -1,14 +1,26 @@
 from model.graph_model import (
+    SyntaxStepMemberAccessExpression,
+    SyntaxStepMeta,
     SyntaxStepsLazy,
 )
 from sast_syntax_readers.types import (
+    MissingCaseHandling,
     SyntaxReaderArgs,
 )
-from utils.graph.transformation import (
-    build_js_member_expression_ids,
+from utils import (
+    graph as g,
 )
 
 
 def reader(args: SyntaxReaderArgs) -> SyntaxStepsLazy:
-    ids = build_js_member_expression_ids(args.graph, args.n_id)
-    yield from args.generic(args.fork_n_id(ids[-1]))
+    match = g.match_ast(args.graph, args.n_id, "__0__", ".", "__1__")
+    if access_key := args.graph.nodes[match["__1__"]].get("label_text"):
+        yield SyntaxStepMemberAccessExpression(
+            meta=SyntaxStepMeta.default(
+                args.n_id,
+                [args.generic(args.fork_n_id(match["__0__"]))],
+            ),
+            member=access_key,
+        )
+    else:
+        raise MissingCaseHandling(args)
