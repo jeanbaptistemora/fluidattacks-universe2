148ad249710871bb811b6cb9fe08648617a310cb
Karen Camargo <kcamargo@fluidattacks.com>
2021-07-27 12:05:20 -0500

    airs\refac(cross): #4458.149 decrease tech debt
    - Removing linebreak
    - Solved 3 smells in 0.75 hrs (4 smells/hr)
    - Num of MRs: 149. Cur perfor: 9.42 smells/hr
    - Remaining: 150 smells, 15.92 hrs
    
    
    Former-commit-id: d06a58758f324e8a09e74d5e8a8c3e4765b7d2f1

--- airs/front/gatsby-config.js
+++ airs/front/gatsby-config.js
@@ -3,8 +3,8 @@ module.exports = {
   siteMetadata: {
     author: "Fluid Attacks",
     description:
-      "We're a pentesting and ethical hacking company that identifies and \
-      reports all your applications and software vulnerabilities ASAP.",
+      'We\'re a pentesting and ethical hacking company that identifies and '
+      + 'reports all your applications and software vulnerabilities ASAP.',
     keywords:
       "Fluid Attacks, Pentesting, Ethical Hacking, Security Testing, Cybersecurity",
     siteUrl: "https://fluidattacks.com", // No trailing slash allowed!
--- airs/infra/js/headers.js
+++ airs/infra/js/headers.js
@@ -1,44 +1,44 @@
 let securityHeaders = {
-  "Content-Security-Policy" : "script-src \
-                              'self' \
-                              'unsafe-inline' \
-                              *.cloudflareinsights.com \
-                              *.cookiebot.com \
-                              *.google-analytics.com \
-                              *.googletagmanager.com \
-                              *.hotjar.com \
-                              *.typekit.net \
-                              *.zoho.com \
-                              *.zohocdn.com \
-                              *.zohopublic.com \
-                              *.zohostatic.com \
-                              cdnjs.cloudflare.com/ajax/libs/highlight.js/ \
-                              googleads.g.doubleclick.net/pagead/viewthroughconversion/ \
-                              snap.licdn.com \
-                              www.googleadservices.com/pagead/; \
-                              frame-ancestors \
-                              'self'; \
-                              object-src \
-                              'none'; \
-                              upgrade-insecure-requests;",
+  "Content-Security-Policy" : "script-src "
+                              + "'self' "
+                              + "'unsafe-inline' "
+                              + "*.cloudflareinsights.com "
+                              + "*.cookiebot.com "
+                              + "*.google-analytics.com "
+                              + "*.googletagmanager.com "
+                              + "*.hotjar.com "
+                              + "*.typekit.net "
+                              + "*.zoho.com "
+                              + "*.zohocdn.com "
+                              + "*.zohopublic.com "
+                              + "*.zohostatic.com "
+                              + "cdnjs.cloudflare.com/ajax/libs/highlight.js/ "
+                              + "googleads.g.doubleclick.net/pagead/viewthroughconversion/ "
+                              + "snap.licdn.com "
+                              + "www.googleadservices.com/pagead/; "
+                              + "frame-ancestors "
+                              + "'self'; "
+                              + "object-src "
+                              + "'none'; "
+                              + "upgrade-insecure-requests;",
   "Strict-Transport-Security" : "max-age=31536000",
   "X-Xss-Protection" : "0",
   "X-Frame-Options" : "DENY",
   "X-Content-Type-Options" : "nosniff",
   "X-Permitted-Cross-Domain-Policies": "none",
   "Referrer-Policy" : "strict-origin-when-cross-origin",
-  "Permissions-Policy" : "geolocation=(self), \
-                          midi=(self), \
-                          push=(self), \
-                          sync-xhr=(self), \
-                          microphone=(self), \
-                          camera=(self), \
-                          magnetometer=(self), \
-                          gyroscope=(self), \
-                          speaker=(self), \
-                          vibrate=(self), \
-                          fullscreen=(self), \
-                          payment=(self)",
+  "Permissions-Policy" : "geolocation=(self), "
+                          + "midi=(self), "
+                          + "push=(self), "
+                          + "sync-xhr=(self), "
+                          + "microphone=(self), "
+                          + "camera=(self), "
+                          + "magnetometer=(self), "
+                          + "gyroscope=(self), "
+                          + "speaker=(self), "
+                          + "vibrate=(self), "
+                          + "fullscreen=(self), "
+                          + "payment=(self)",
 }
 
 let sanitiseHeaders = {}
