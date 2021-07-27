59fe908f972b2d5ef425e4c77b3d470072c01435
Andres Aguirre <aaguirre@fluidattacks.com>
2021-07-27 13:05:18 -0500

    integrates\refac(back): #4329.199 remove group mutation
    - Add new mutation to support the group removing
    and the new dataloaders
    
    
    Former-commit-id: d21d0b17620567437d97588058b401cad86f9c31

---
+++ integrates/back/src/api/mutations/remove_group_new.py
@@ -0,0 +1,82 @@
+from api import (
+    APP_EXCEPTIONS,
+)
+from ariadne.utils import (
+    convert_kwargs_to_snake_case,
+)
+import authz
+from custom_exceptions import (
+    PermissionDenied,
+)
+from custom_types import (
+    SimplePayload,
+)
+from decorators import (
+    concurrent_decorators,
+    enforce_group_level_auth_async,
+    require_asm,
+    require_login,
+)
+from graphql.type.definition import (
+    GraphQLResolveInfo,
+)
+from groups import (
+    domain as groups_domain,
+)
+from newutils import (
+    logs as logs_utils,
+    token as token_utils,
+)
+from redis_cluster.operations import (
+    redis_del_by_deps_soon,
+)
+from typing import (
+    Any,
+)
+
+
+@convert_kwargs_to_snake_case
+@concurrent_decorators(
+    require_login,
+    enforce_group_level_auth_async,
+    require_asm,
+)
+async def mutate(
+    _: Any, info: GraphQLResolveInfo, group_name: str, reason: str
+) -> SimplePayload:
+    try:
+        loaders = info.context.loaders
+        group_name = group_name.lower()
+        user_info = await token_utils.get_jwt_content(info.context)
+        requester_email = user_info["user_email"]
+        success = False
+        success = await groups_domain.edit(
+            context=loaders,
+            comments="",
+            group_name=group_name,
+            has_machine=False,
+            has_squad=False,
+            has_asm=False,
+            reason=reason,
+            requester_email=requester_email,
+            service="WHITE",
+            subscription="continuous",
+        )
+        if success:
+            redis_del_by_deps_soon("remove_group", group_name=group_name)
+            await authz.revoke_cached_group_service_policies(group_name)
+            logs_utils.cloudwatch_log(
+                info.context,
+                f"Security: Deleted group {group_name} successfully",
+            )
+    except PermissionDenied:
+        logs_utils.cloudwatch_log(
+            info.context,
+            "Security: Unauthorized role attempted to delete group",
+        )
+    except APP_EXCEPTIONS:
+        logs_utils.cloudwatch_log(
+            info.context, f"Security: Attempted to delete group {group_name}"
+        )
+        raise
+    return SimplePayload(success=success)
