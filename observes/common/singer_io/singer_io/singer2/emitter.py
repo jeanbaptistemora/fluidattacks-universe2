3a39ece40a39fe311c5d6abf5c44cff769e6f8b4
Daniel Murcia <dmurcia@fluidattacks.com>
2021-07-27 16:40:28 -0500

    observes\refac(back): #5069.12 emit state
    - refac emit singer state
    - refac emit singer
    
    
    Former-commit-id: 41d34b1d7ae04a839433776cbe7b1dfa68b29604

--- observes/common/singer_io/singer_io/singer2/emitter.py
+++ observes/common/singer_io/singer_io/singer2/emitter.py
@@ -10,8 +10,10 @@ from returns.maybe import (
     Maybe,
 )
 from singer_io.singer2._objs import (
+    SingerMessage,
     SingerRecord,
     SingerSchema,
+    SingerState,
 )
 from singer_io.singer2.json import (
     JsonEmitter,
@@ -33,6 +35,7 @@ class SingerEmitter:
             lambda date: date.to_utc_str()
         )
         json_obj: JsonObj = {
+            "type": JsonValue("RECORD"),
             "stream": JsonValue(record.stream),
             "record": JsonValue(record.record),
             "time_extracted": JsonValue(time_str.value_or(None)),
@@ -44,6 +47,7 @@ class SingerEmitter:
             schema.bookmark_properties
         ).map(lambda fset: [JsonValue(item) for item in fset])
         json_obj: JsonObj = {
+            "type": JsonValue("SCHEMA"),
             "stream": JsonValue(schema.stream),
             "schema": JsonValue(schema.schema.to_json()),
             "key_properties": JsonValue(
@@ -54,3 +58,17 @@ class SingerEmitter:
             ),
         }
         return self.emitter.emit(json_obj)
+
+    def emit_state(self, state: SingerState) -> IO[None]:
+        json_obj: JsonObj = {
+            "type": JsonValue("STATE"),
+            "value": JsonValue(state.value),
+        }
+        return self.emitter.emit(json_obj)
+
+    def emit(self, singer: SingerMessage) -> IO[None]:
+        if isinstance(singer, SingerRecord):
+            return self.emit_record(singer)
+        if isinstance(singer, SingerSchema):
+            return self.emit_schema(singer)
+        return self.emit_state(singer)
