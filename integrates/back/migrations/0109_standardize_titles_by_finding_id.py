1199073bde1969e463caf5ebe94e751bba264c5a
Jonathan Mesa <jmesa@fluidattacks.com>
2021-07-28 13:53:57 -0500

    integrates\refac(back): #4903.30 add migration
    - add migration for updating findings titles from a csv with
    the info
    - each finding is updated directly using its finding_id
    
    
    Former-commit-id: 7dff078f5dee383692be23e6db985073cbc00d2c

---
+++ integrates/back/migrations/0109_standardize_titles_by_finding_id.py
@@ -0,0 +1,67 @@
+# pylint: disable=invalid-name
+"""
+This migration aims to standardize all findings titles, found in:
+https://gitlab.com/fluidattacks/product/-/blob/master/makes/makes/criteria/src/vulnerabilities/data.yaml
+
+In this case, we take a finding_id and update its title directly from the csv
+
+Execution Time:
+Finalization Time:
+"""
+
+from aioextensions import (
+    collect,
+    run,
+)
+import csv
+from findings import (
+    dal as findings_dal,
+)
+import time
+
+PROD: bool = False
+TABLE_NAME: str = "FI_findings"
+
+
+async def main() -> None:
+    # Read migration matchs
+    with open("0109_findings_info.csv", mode="r") as infile:
+        reader = csv.reader(infile)
+        findings = [
+            {
+                "finding_id": rows[1],
+                "title": rows[4],
+            }
+            for rows in reader
+            if rows[1] != "finding_id"
+        ]
+
+    print(f"    === findings to update: {len(findings)}")
+    print(f"    === sample: {findings[:3]}")
+
+    success = False
+    if PROD:
+        success = all(
+            await collect(
+                findings_dal.update(
+                    finding["finding_id"],
+                    {
+                        "finding": finding["title"],
+                    },
+                )
+                for finding in findings
+            )
+        )
+
+    print(f"    === success: {success}")
+
+
+if __name__ == "__main__":
+    execution_time = time.strftime(
+        "Execution Time:    %Y-%m-%d at %H:%M:%S UTC%Z"
+    )
+    run(main())
+    finalization_time = time.strftime(
+        "Finalization Time: %Y-%m-%d at %H:%M:%S UTC%Z"
+    )
+    print(f"{execution_time}\n{finalization_time}")
