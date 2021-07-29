891e683977bbdd9e938149de211a59657f3bf6a1
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-07-28 22:01:25 -0500

    integrates\refac(back): #5041.8 update migration
    - update migration to use scan to avoid running it
    just in active groups
    
    
    Former-commit-id: baaa663d843590f59a1135df6a37f997c9cfe623

--- integrates/back/migrations/0104_remove_finding_cwe.py
+++ integrates/back/migrations/0104_remove_finding_cwe.py
@@ -8,69 +8,45 @@ from aioextensions import (
     run,
 )
 from custom_types import (
+    DynamoQuery,
     Finding,
 )
-from dataloaders import (
-    Dataloaders,
-    get_new_context,
+from dynamodb.operations_legacy import (
+    scan,
 )
 from findings.dal import (
     update,
 )
-from groups.domain import (
-    get_active_groups,
-)
-from itertools import (
-    chain,
-)
-from more_itertools import (
-    chunked,
-)
 import time
 from typing import (
     Dict,
-    List,
 )
 
 # Constants
 PROD: bool = False
+TABLE_NAME: str = "FI_findings"
 
 
-async def _update_finding(
+async def update_finding(
     finding: Dict[str, Finding],
 ) -> None:
     finding_id: str = str(finding["finding_id"])
     data_to_update: Dict[str, Finding] = {"cwe": None}
-    if PROD:
+    if PROD and "cwe" in finding:
         print(
             "Updating finding",
             finding_id,
+            finding["cwe"],
             data_to_update,
         )
         await update(finding_id, data_to_update)
 
 
-async def update_findings(*, groups: List[str], loaders: Dataloaders) -> None:
-    findings = await loaders.group_findings.load_many(groups)
-    drafts = await loaders.group_drafts.load_many(groups)
-    groups_findings: List[Dict[str, Finding]] = list(
-        chain.from_iterable(filter(None, findings + drafts))
-    )
-    await collect(
-        [_update_finding(finding) for finding in groups_findings],
-        workers=20,
-    )
-
-
 async def main() -> None:
-    loaders: Dataloaders = get_new_context()
-    groups = await get_active_groups()
+    scan_attrs: DynamoQuery = {}
+    findings = await scan(TABLE_NAME, scan_attrs)
     await collect(
-        [
-            update_findings(groups=list_group, loaders=loaders)
-            for list_group in chunked(groups, 20)
-        ],
-        workers=20,
+        [update_finding(finding) for finding in findings], workers=20
     )
 
 
