52f8b2c60bd44f694970c913aaecc472b0a36753
Briam Agudelo <bagudelo@fluidattacks.com>
2021-08-02 11:46:32 -0500

    integrates\test(back): #4624.86 migrate func tests
    - Create add forces execution job test
    - Speed: 65 files / 40 days = 1.63 files/day
    - TODO: 16 files
    - ETA: 16 / 1.63 = 1.96 weeks
    - no-deltas-test
    
    
    Former-commit-id: 41b68ff0c7ca5e9808acc1ac8419a1b8b705599f

---
+++ integrates/back/tests/functional/add_forces_execution/test_resolver.py
@@ -0,0 +1,51 @@
+from . import (
+    get_result,
+)
+import pytest
+from typing import (
+    Any,
+    Dict,
+)
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group("add_forces_execution")
+@pytest.mark.parametrize(
+    ["email"],
+    [
+        ["service_forces@gmail.com"],
+    ],
+)
+async def test_add_forces_execution(populate: bool, email: str) -> None:
+    assert populate
+    result: Dict[str, Any] = await get_result(
+        user=email,
+        group="group1",
+    )
+    assert "errors" not in result
+    assert result["data"]["addForcesExecution"]["success"]
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group("add_forces_execution")
+@pytest.mark.parametrize(
+    ["email"],
+    [
+        ["admin@gmail.com"],
+        ["customer@gmail.com"],
+        ["customeradmin@gmail.com"],
+        ["executive@gmail.com"],
+        ["analyst@gmail.com"],
+        ["closer@gmail.com"],
+        ["resourcer@gmail.com"],
+        ["reviewer@gmail.com"],
+    ],
+)
+async def test_add_forces_execution_fail(populate: bool, email: str) -> None:
+    assert populate
+    result: Dict[str, Any] = await get_result(
+        user=email,
+        group="group1",
+    )
+    assert "errors" in result
+    assert result["errors"][0]["message"] == "Access denied"
