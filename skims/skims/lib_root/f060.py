f183fece1eab0ba90c15ee921be1f2004d8cd9d1
Andres Cuberos <acuberos@fluidattacks.com>
2021-08-02 21:13:39 -0500

    skims\feat(back): #5082.5 kotlin generic exceptions
    - Add method to find the use of generic exceptions in Kotlin
      files.
    
    
    Former-commit-id: fb823541daae02acfc197bb572ae4744d3ea8b15

--- skims/skims/lib_root/f060.py
+++ skims/skims/lib_root/f060.py
@@ -231,6 +231,42 @@ def java_insecure_exceptions(
     )
 
 
+def kotlin_generic_exceptions(
+    graph_db: graph_model.GraphDB,
+) -> core_model.Vulnerabilities:
+    def n_ids() -> graph_model.GraphShardNodes:
+        for shard in graph_db.shards_by_langauge(
+            graph_model.GraphShardMetadataLanguage.KOTLIN
+        ):
+            for catch_clause in g.filter_nodes(
+                shard.graph,
+                shard.graph.nodes,
+                g.pred_has_labels(label_type="catch_block"),
+            ):
+                exception_type_node = g.get_ast_childs(
+                    shard.graph, catch_clause, "user_type"
+                )
+                exception_type = (
+                    g.get_ast_childs(
+                        shard.graph, exception_type_node[0], "type_identifier"
+                    )
+                    if exception_type_node
+                    else tuple()
+                )
+                if exception_type and shard.graph.nodes[exception_type[0]][
+                    "label_text"
+                ] in {"Error", "Exception", "Throwable"}:
+                    yield shard, catch_clause
+
+    return get_vulnerabilities_from_n_ids(
+        cwe=("397",),
+        desc_key="src.lib_path.f060.insecure_exceptions.description",
+        desc_params=dict(lang="Kotlin"),
+        finding=FINDING,
+        graph_shard_nodes=n_ids(),
+    )
+
+
 # Constants
 FINDING: core_model.FindingEnum = core_model.FindingEnum.F060
 QUERIES: graph_model.Queries = (
@@ -238,4 +274,5 @@ QUERIES: graph_model.Queries = (
     (FINDING, c_sharp_insecure_exceptions),
     (FINDING, c_sharp_throws_for_generic_exception),
     (FINDING, java_insecure_exceptions),
+    (FINDING, kotlin_generic_exceptions),
 )
