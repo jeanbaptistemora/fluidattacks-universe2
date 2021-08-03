1de658a7df8e25a1b6d10a6fa6168d776fea86e6
David Acevedo <dacevedo@fluidattacks.com>
2021-08-03 09:22:32 -0500

    integrates\refac(front): #2617.82 prepare url roots
    - added confirm dialog to enable incoming changes
    
    
    Former-commit-id: 8562f098f689a97d0a759e03edd1fcee88adf1b0

--- integrates/front/src/scenes/Dashboard/containers/GroupScopeView/URLRoots/index.tsx
+++ integrates/front/src/scenes/Dashboard/containers/GroupScopeView/URLRoots/index.tsx
@@ -10,6 +10,7 @@ import { Container } from "./styles";
 import { ADD_URL_ROOT } from "../queries";
 import type { IURLRootAttr } from "../types";
 import { Button } from "components/Button";
+import { ConfirmDialog } from "components/ConfirmDialog";
 import { DataTableNext } from "components/DataTableNext";
 import { Can } from "utils/authz/Can";
 import { Logger } from "utils/logger";
@@ -48,14 +49,17 @@ export const URLRoots: React.FC<IURLRootsProps> = ({
           case "Exception - Error empty value is not valid":
             msgError(t("group.scope.url.errors.invalid"));
             break;
+          case "Exception - Active root with the same URL/branch already exists":
+            msgError(t("group.scope.common.errors.duplicateUrl"));
+            break;
           default:
             msgError(t("groupAlerts.errorTextsad"));
-            Logger.error("Couldn't add git roots", error);
+            Logger.error("Couldn't add url roots", error);
         }
       });
     },
   });
-  const handleSubmit = useCallback(
+  const handleUrlSubmit = useCallback(
     async ({ url }: { url: string }): Promise<void> => {
       await addUrlRoot({ variables: { groupName, url } });
     },
@@ -65,48 +69,54 @@ export const URLRoots: React.FC<IURLRootsProps> = ({
   return (
     <React.Fragment>
       <h2>{t("group.scope.url.title")}</h2>
-      <Container>
-        <DataTableNext
-          bordered={true}
-          columnToggle={true}
-          dataset={roots}
-          exportCsv={true}
-          extraButtons={
-            <Can do={"api_mutations_add_url_root_mutate"}>
-              <div className={"mb3"}>
-                <Button onClick={openAddModal}>
-                  <FontAwesomeIcon icon={faPlus} />
-                  &nbsp;{t("group.scope.common.add")}
-                </Button>
-              </div>
-            </Can>
-          }
-          headers={[
-            {
-              dataField: "host",
-              header: t("group.scope.url.host"),
-            },
-            {
-              dataField: "path",
-              header: t("group.scope.url.path"),
-            },
-            {
-              dataField: "port",
-              header: t("group.scope.url.port"),
-            },
-            {
-              dataField: "protocol",
-              header: t("group.scope.url.protocol"),
-            },
-          ]}
-          id={"tblURLRoots"}
-          pageSize={10}
-          search={true}
-          striped={true}
-        />
-      </Container>
+      <ConfirmDialog title={t("group.scope.common.confirm")}>
+        {(): JSX.Element => {
+          return (
+            <Container>
+              <DataTableNext
+                bordered={true}
+                columnToggle={true}
+                dataset={roots}
+                exportCsv={true}
+                extraButtons={
+                  <Can do={"api_mutations_add_url_root_mutate"}>
+                    <div className={"mb3"}>
+                      <Button onClick={openAddModal}>
+                        <FontAwesomeIcon icon={faPlus} />
+                        &nbsp;{t("group.scope.common.add")}
+                      </Button>
+                    </div>
+                  </Can>
+                }
+                headers={[
+                  {
+                    dataField: "host",
+                    header: t("group.scope.url.host"),
+                  },
+                  {
+                    dataField: "path",
+                    header: t("group.scope.url.path"),
+                  },
+                  {
+                    dataField: "port",
+                    header: t("group.scope.url.port"),
+                  },
+                  {
+                    dataField: "protocol",
+                    header: t("group.scope.url.protocol"),
+                  },
+                ]}
+                id={"tblURLRoots"}
+                pageSize={10}
+                search={true}
+                striped={true}
+              />
+            </Container>
+          );
+        }}
+      </ConfirmDialog>
       {isManagingRoot === false ? undefined : (
-        <ManagementModal onClose={closeModal} onSubmit={handleSubmit} />
+        <ManagementModal onClose={closeModal} onSubmit={handleUrlSubmit} />
       )}
     </React.Fragment>
   );
