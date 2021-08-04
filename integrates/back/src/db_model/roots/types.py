7a5125ce504ce24657738c9b64c333f1cd8f22f2
David Acevedo <dacevedo@fluidattacks.com>
2021-08-04 16:13:25 -0500

    integrates\refac(back): #5111.10 decouple model
    - getting roots up to speed with the module structure
      used by recent migrations
    
    
    Former-commit-id: 1e41dcc65d80ac1a72d8fb5c7d51785678d2274c

---
+++ integrates/back/src/db_model/roots/types.py
@@ -0,0 +1,86 @@
+from typing import (
+    List,
+    NamedTuple,
+    Optional,
+    Union,
+)
+
+
+class GitRootMetadata(NamedTuple):
+    branch: str
+    type: str
+    url: str
+
+
+class GitRootCloning(NamedTuple):
+    modified_date: str
+    reason: str
+    status: str
+
+
+class GitRootState(NamedTuple):
+    environment_urls: List[str]
+    environment: str
+    gitignore: List[str]
+    includes_health_check: bool
+    modified_by: str
+    modified_date: str
+    nickname: str
+    other: Optional[str]
+    reason: Optional[str]
+    status: str
+
+
+class GitRootItem(NamedTuple):
+    cloning: GitRootCloning
+    group_name: str
+    id: str
+    metadata: GitRootMetadata
+    state: GitRootState
+
+
+class IPRootMetadata(NamedTuple):
+    address: str
+    type: str
+    port: str
+
+
+class IPRootState(NamedTuple):
+    modified_by: str
+    modified_date: str
+    other: Optional[str]
+    reason: Optional[str]
+    status: str
+
+
+class IPRootItem(NamedTuple):
+    group_name: str
+    id: str
+    metadata: IPRootMetadata
+    state: IPRootState
+
+
+class URLRootMetadata(NamedTuple):
+    host: str
+    path: str
+    port: str
+    protocol: str
+    type: str
+
+
+class URLRootState(NamedTuple):
+    modified_by: str
+    modified_date: str
+    other: Optional[str]
+    reason: Optional[str]
+    status: str
+
+
+class URLRootItem(NamedTuple):
+    group_name: str
+    id: str
+    metadata: URLRootMetadata
+    state: URLRootState
+
+
+RootItem = Union[GitRootItem, IPRootItem, URLRootItem]
