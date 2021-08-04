f2003fd2e87557642648f752d0062e1817cbf21f
Daniel Murcia <dmurcia@fluidattacks.com>
2021-08-04 16:24:43 -0500

    observes\feat(build): #5118.1 import sgqlc
    - add sgqlc dependency
    
    
    Former-commit-id: f9392de022149d969ac233cb6838271d90404c1f

--- makes/packages/observes/env/tap-announcekit/runtime/python/default.nix
+++ makes/packages/observes/env/tap-announcekit/runtime/python/default.nix
@@ -8,8 +8,10 @@ buildPythonRequirements {
     direct = [
       "click==7.1.2"
       "returns==0.16.0"
+      "sgqlc==14.0"
     ];
     inherited = [
+      "graphql-core==3.1.5"
       "typing-extensions==3.10.0.0"
     ];
   };
