9186c9e19e3ba5e51f5df643ab6d54d2273b268f
Daniel Murcia <dmurcia@fluidattacks.com>
2021-08-05 11:18:07 -0500

    observes\feat(back): #5118.5 update schema
    - add update schema to cli
    
    
    Former-commit-id: d0043d38f40d857aeb1acfcb42919578690f06ef

--- observes/singer/tap_announcekit/tap_announcekit/cli.py
+++ observes/singer/tap_announcekit/tap_announcekit/cli.py
@@ -1,9 +1,14 @@
+# pylint: skip-file
 import click
+import logging
 from os import (
     environ,
 )
 from returns.io import (
     IO,
+)
+from returns.maybe import (
+    Maybe,
 )
 import sys
 from tap_announcekit import (
@@ -12,6 +17,13 @@ from tap_announcekit import (
 from tap_announcekit.auth import (
     Creds,
 )
+import tempfile
+from typing import (
+    IO as IO_FILE,
+    Optional,
+)
+
+LOG = logging.getLogger(__name__)
 
 
 def get_creds() -> Creds:
@@ -19,9 +31,30 @@ def get_creds() -> Creds:
 
 
 @click.command()
-def get_api_schema() -> IO[None]:
+@click.option("--out", type=click.File("w+"), default=sys.stdout)
+def get_api_schema(out: IO_FILE[str]) -> IO[None]:
     # schema is public no need for real creds
-    return utils.get_api_schema(Creds("", ""), sys.stdout)
+    return utils.get_api_schema(Creds("", ""), out)
+
+
+@click.command()
+@click.option("--api-schema", type=click.File("r"), default=None)
+@click.option("--out", type=click.File("w+"), default=sys.stdout)
+def update_schema(
+    api_schema: Optional[IO_FILE[str]], out: IO_FILE[str]
+) -> IO[None]:
+    def open_schema() -> IO_FILE[str]:
+        return Maybe.from_optional(api_schema).or_else_call(
+            lambda: tempfile.NamedTemporaryFile("w+")
+        )
+
+    with open_schema() as schema_file:
+        if not api_schema:
+            # schema is public no need for real creds
+            LOG.info("Getting current schema")
+            utils.get_api_schema(Creds("", ""), schema_file)
+        schema_file.seek(0)
+        return utils.gen_schema_code(schema_file, out)
 
 
 @click.group()
@@ -31,3 +64,4 @@ def main() -> None:
 
 
 main.add_command(get_api_schema)
+main.add_command(update_schema)
