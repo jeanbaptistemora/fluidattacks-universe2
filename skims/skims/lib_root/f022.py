32d79d4a0db39a9f91fb72dd6e1d5a3979c502a9
Andres Cuberos <acuberos@fluidattacks.com>
2021-08-05 21:25:12 -0500

    skims\feat(back): #5082.10 unencrypted channel kotlin
    - Add method to find use of unencrypted channel in Kotlin
      files.
    
    
    Former-commit-id: 0892f63c7cbabc1bed45edaccf25c27e114accf3

---
+++ skims/skims/lib_root/f022.py
@@ -0,0 +1,73 @@
+from lib_root import (
+    get_composite_name_kotlin,
+    yield_kotlin_method_invocation,
+)
+from model.core_model import (
+    FindingEnum,
+    Vulnerabilities,
+)
+from model.graph_model import (
+    GraphDB,
+    GraphShardNodes,
+    Queries,
+)
+from sast.query import (
+    get_vulnerabilities_from_n_ids,
+)
+from typing import (
+    Set,
+)
+from utils import (
+    graph as g,
+)
+from utils.string import (
+    complete_attrs_on_set,
+)
+
+
+def _kotlin_yield_unencrypted_channels(graph_db: GraphDB) -> GraphShardNodes:
+    unencrypted_methods = complete_attrs_on_set(
+        {
+            "org.apache.commons.net.ftp.FTPClient",
+            "org.apache.commons.net.smtp.SMTPClient",
+            "org.apache.commons.net.telnet.TelnetClient",
+        }
+    )
+    unsafe_methods = complete_attrs_on_set({"ConnectionSpec.Builder"})
+    unsafe_protocol: Set[str] = {"CLEARTEXT"}
+    for shard, method_id, method_name in yield_kotlin_method_invocation(
+        graph_db
+    ):
+        if method_name in unencrypted_methods:
+            yield shard, method_id
+        if method_name in unsafe_methods:
+            match = g.match_ast_group(
+                shard.graph, method_id, "value_argument", depth=3
+            )
+            parameters = [
+                g.adj_ast(shard.graph, argument_id)[0]
+                for argument_id in match["value_argument"]
+            ]
+            if parameters:
+                param_id = parameters[0]
+                param_value = get_composite_name_kotlin(
+                    shard.graph, g.pred_ast(shard.graph, param_id)[0]
+                )
+                protocol = param_value.split(".")[-1]
+                if protocol in unsafe_protocol:
+                    yield shard, param_id
+
+
+def kotlin_unencrypted_channel(graph_db: GraphDB) -> Vulnerabilities:
+    return get_vulnerabilities_from_n_ids(
+        cwe=("319"),
+        desc_key="src.lib_path.f022.unencrypted_channel",
+        desc_params={},
+        finding=FINDING,
+        graph_shard_nodes=_kotlin_yield_unencrypted_channels(graph_db),
+    )
+
+
+# Constants
+FINDING: FindingEnum = FindingEnum.F022
+QUERIES: Queries = ((FINDING, kotlin_unencrypted_channel),)
