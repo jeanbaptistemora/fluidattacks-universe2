be027a2259a3ff6ba575dccb5e05c518b1787b7b
Daniel Murcia <dmurcia@fluidattacks.com>
2021-08-09 17:39:00 -0500

    observes\feat(back): #5157.3 build project obj
    - add to_proj proj builder
    
    
    Former-commit-id: 0cce7ba3768decab7d7a027573e6bd0e92291b3c

--- observes/singer/tap_announcekit/tap_announcekit/stream/project.py
+++ observes/singer/tap_announcekit/tap_announcekit/stream/project.py
@@ -5,6 +5,13 @@ from datetime import (
     datetime,
 )
 import logging
+from singer_io.singer2.json import (
+    to_opt_primitive,
+    to_primitive,
+)
+from tap_announcekit.api.gql_schema import (
+    Project as RawProject,
+)
 from typing import (
     Optional,
 )
@@ -61,3 +68,32 @@ class Project(_Project):
     def __init__(self, obj: _Project) -> None:
         for key, val in obj.__dict__.items():
             object.__setattr__(self, key, val)
+
+
+def to_proj(raw: RawProject) -> Project:
+    draft = _Project(
+        _new_proj_id(to_primitive(raw.id, str)),
+        to_primitive(raw.encoded_id, str),
+        to_primitive(raw.name, str),
+        to_primitive(raw.slug, str),
+        to_opt_primitive(raw.website, str),
+        to_primitive(raw.is_authors_listed, bool),
+        to_primitive(raw.is_whitelabel, bool),
+        to_primitive(raw.is_subscribable, bool),
+        to_primitive(raw.is_slack_subscribable, bool),
+        to_primitive(raw.is_feedback_enabled, bool),
+        to_primitive(raw.is_demo, bool),
+        to_primitive(raw.is_readonly, bool),
+        to_opt_primitive(raw.image_id, str),
+        to_opt_primitive(raw.favicon_id, str),
+        raw.created_at,
+        to_opt_primitive(raw.ga_property, str),
+        to_primitive(raw.avatar, str),
+        to_opt_primitive(raw.favicon, str),
+        to_primitive(raw.locale, str),
+        to_opt_primitive(raw.uses_new_feed_hostname, bool),
+        to_primitive(raw.payment_gateway, str),
+        raw.trial_until,
+        to_primitive(raw.metadata, str),
+    )
+    return Project(draft)
