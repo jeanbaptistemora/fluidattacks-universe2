b0b488975b8326070470e34c5e36dcd230388dfd
Daniel Murcia <dmurcia@fluidattacks.com>
2021-08-10 17:31:10 -0500

    observes\feat(infra): #5164.2 dev key
    - add observes dev key
    
    
    Former-commit-id: c437d2d08ea99aef1bac6180206971ca98e818fc

---
+++ observes/infra/terraform/dev-key.tf
@@ -0,0 +1,117 @@
+data "aws_iam_policy_document" "key-dev" {
+
+  statement {
+    sid    = "Enable IAM policies"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = [module.external.aws_root.arn]
+    }
+    actions = [
+      "kms:*"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid    = "Key Administrators"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        module.external.aws_iam_roles["observes_prod"].arn,
+        module.external.aws_iam_roles["makes_prod"].arn,
+        module.external.aws_iam_users["observes-prod"].arn
+      ]
+    }
+    actions = [
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:TagResource",
+      "kms:UntagResource",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion",
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid    = "Key Users"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        module.external.aws_iam_roles["observes_dev"].arn,
+        module.external.aws_iam_users["observes-dev"].arn
+      ]
+    }
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid    = "Attachment Of Persistent Resources"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        module.external.aws_iam_roles["observes_dev"].arn,
+        module.external.aws_iam_users["observes-dev"].arn
+      ]
+    }
+    actions = [
+      "kms:CreateGrant",
+      "kms:ListGrants",
+      "kms:RevokeGrant",
+    ]
+    resources = [
+      "*"
+    ]
+    condition {
+      test     = "Bool"
+      variable = "kms:GrantIsForAWSResource"
+      values = [
+        "true",
+      ]
+    }
+  }
+}
+
+resource "aws_kms_key" "key-dev" {
+  description             = "dev kms key for observes."
+  policy                  = data.aws_iam_policy_document.key-dev.json
+  deletion_window_in_days = 30
+  is_enabled              = true
+  enable_key_rotation     = true
+
+  tags = {
+    "Name"               = "observes-dev"
+    "management:type"    = "development"
+    "management:product" = "observes"
+  }
+}
+
+resource "aws_kms_alias" "key-dev" {
+  name          = "alias/observes-dev"
+  target_key_id = aws_kms_key.key-dev.key_id
+}
