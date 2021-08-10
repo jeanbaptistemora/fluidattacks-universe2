553b5ae512e19d028a321191e763655bf135b44f
Jonathan Mesa <jmesa@fluidattacks.com>
2021-08-10 15:08:07 -0500

    integrates\refac(back): #4329.253 migrate scheduler
    - duplicate scheduler reset_expired_accepted_findings
    and use the new finding dataloader
    
    
    Former-commit-id: c61a7c7d6d397dd9c47a131c2f818bdbbd75a128

---
+++ integrates/back/src/schedulers/reset_expired_accepted_findings_new.py
@@ -0,0 +1,97 @@
+from aioextensions import (
+    collect,
+)
+from dataloaders import (
+    Dataloaders,
+    get_new_context,
+)
+from db_model.findings.types import (
+    Finding,
+)
+from groups import (
+    domain as groups_domain,
+)
+from itertools import (
+    chain,
+)
+from newutils import (
+    datetime as datetime_utils,
+)
+from typing import (
+    cast,
+    Dict,
+    List,
+    Tuple,
+)
+from vulnerabilities import (
+    domain as vulns_domain,
+)
+
+
+async def reset_group_expired_accepted_findings(
+    context: Dataloaders, group_name: str, today: str
+) -> None:
+    finding_vulns_loader = context.finding_vulns
+    group_findings_loader = context.group_findings_new
+
+    group_findings: Tuple[Finding] = await group_findings_loader.load(
+        group_name
+    )
+    vulns = list(
+        chain.from_iterable(
+            await finding_vulns_loader.load_many(
+                [finding.id for finding in group_findings]
+            )
+        )
+    )
+
+    for vuln in vulns:
+        finding_id = cast(str, vuln.get("finding_id"))
+        historic_treatment = cast(
+            List[Dict[str, str]], vuln.get("historic_treatment", [{}])
+        )
+        is_accepted_expired = (
+            historic_treatment[-1].get("acceptance_date", today) < today
+        )
+        is_undefined_accepted_expired = (
+            (historic_treatment[-1].get("treatment") == "ACCEPTED_UNDEFINED")
+            and (
+                historic_treatment[-1].get("acceptance_status") == "SUBMITTED"
+            )
+            and datetime_utils.get_plus_delta(
+                datetime_utils.get_from_str(
+                    historic_treatment[-1].get(
+                        "date", datetime_utils.DEFAULT_STR
+                    )
+                ),
+                days=5,
+            )
+            <= datetime_utils.get_from_str(today)
+        )
+        if is_accepted_expired or is_undefined_accepted_expired:
+            updated_values = {"treatment": "NEW"}
+            await vulns_domain.add_vulnerability_treatment(
+                finding_id=finding_id,
+                updated_values=updated_values,
+                vuln=vuln,
+                user_email=historic_treatment[-1].get("user", ""),
+                date=datetime_utils.get_as_str(datetime_utils.get_now()),
+            )
+
+
+async def reset_expired_accepted_findings() -> None:
+    """Update treatment if acceptance date expires"""
+    today = datetime_utils.get_now_as_str()
+    context: Dataloaders = get_new_context()
+    groups = await groups_domain.get_active_groups()
+    await collect(
+        [
+            reset_group_expired_accepted_findings(context, group_name, today)
+            for group_name in groups
+        ],
+        workers=40,
+    )
+
+
+async def main() -> None:
+    await reset_expired_accepted_findings()
