bb8f495f89f5f798975905f40392512dc6bc13be
Juan Echeverri <jecheverri@fluidattacks.com>
2021-08-10 15:36:20 -0500

    skims\feat(back): #5153.1 add data for f017
    - Add f017 to model/core_model.py
    - Add f017.yaml to criteria/vulnerabilities
    
    
    Former-commit-id: d6117d1abfb875e67128a655197df63d1d7d3ab0

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -295,6 +295,26 @@ class FindingEnum(Enum):
             user_interaction=cvss3_model.UserInteraction.required,
         ),
     )
+    F017: FindingMetadata = FindingMetadata.new(
+        auto_approve=True,
+        code="F017",
+        cwe=319,
+        execution_queue=ExecutionQueue.http,
+        requirements=[32, 181],
+        score=cvss3_model.Score(
+            attack_complexity=cvss3_model.AttackComplexity.low,
+            attack_vector=cvss3_model.AttackVector.adjacent,
+            availability_impact=cvss3_model.AvailabilityImpact.none,
+            confidentiality_impact=cvss3_model.ConfidentialityImpact.high,
+            exploitability=cvss3_model.Exploitability.high,
+            integrity_impact=cvss3_model.IntegrityImpact.none,
+            privileges_required=cvss3_model.PrivilegesRequired.none,
+            remediation_level=cvss3_model.RemediationLevel.unavailable,
+            report_confidence=cvss3_model.ReportConfidence.confirmed,
+            severity_scope=cvss3_model.SeverityScope.changed,
+            user_interaction=cvss3_model.UserInteraction.required,
+        ),
+    )
     F021: FindingMetadata = FindingMetadata.new(
         auto_approve=True,
         code="F021",
