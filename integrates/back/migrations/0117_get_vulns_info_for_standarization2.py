25ad1fc2e0df01fb36e27c78d5566f0eb1dcca35
Jonathan Mesa <jmesa@fluidattacks.com>
2021-08-10 10:38:12 -0500

    integrates\refac(back): #4903.46 add migration
    - add migration for retrieveing vuln info that will be used for
    standarization purposes
    
    
    Former-commit-id: 2e92411decd5c784ee7886962a808f2a340c31bc

---
+++ integrates/back/migrations/0117_get_vulns_info_for_standarization2.py
@@ -0,0 +1,73 @@
+# pylint: disable=invalid-name
+"""
+Extract info related to vulns from a csv list for finding
+standarization purposes
+
+Execution Time:
+Finalization Time:
+"""
+
+from aioextensions import (
+    run,
+)
+import csv
+from dataloaders import (
+    Dataloaders,
+    get_new_context,
+)
+from newutils.vulnerabilities import (
+    get_last_status,
+)
+import time
+
+PROD: bool = False
+
+
+async def main() -> None:
+    # Read file with uuids
+    with open("0117.csv", mode="r") as f:
+        reader = csv.reader(f)
+        vulns_uuids = [row[0] for row in reader]
+    print(f"    === vulns uuids: {len(vulns_uuids)}")
+    print(f"    === sample: {vulns_uuids[:3]}")
+
+    context: Dataloaders = get_new_context()
+    vuln_loader = context.vulnerability
+    vulns = await vuln_loader.load_many(vulns_uuids)
+
+    info = [
+        {
+            "vuln_uuid": vuln["id"],
+            "vuln_status": get_last_status(vuln),
+        }
+        for vuln in vulns
+    ]
+
+    csv_columns = [
+        "vuln_uuid",
+        "vuln_status",
+    ]
+    csv_file = "0117_vuln_info_aug_10.csv"
+    success = False
+    try:
+        with open(csv_file, "w") as f:
+            writer = csv.DictWriter(f, fieldnames=csv_columns)
+            writer.writeheader()
+            for data in info:
+                writer.writerow(data)
+        success = True
+    except IOError:
+        print("   === I/O error")
+
+    print(f"   === success: {success}")
+
+
+if __name__ == "__main__":
+    execution_time = time.strftime(
+        "Execution Time:    %Y-%m-%d at %H:%M:%S UTC%Z",
+    )
+    run(main())
+    finalization_time = time.strftime(
+        "Finalization Time: %Y-%m-%d at %H:%M:%S UTC%Z",
+    )
+    print(f"{execution_time}\n{finalization_time}")
