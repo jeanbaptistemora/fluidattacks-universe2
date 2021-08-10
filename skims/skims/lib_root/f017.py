0ec052a08a7ea58c9bd7b12b64d56352a9f4826d
Juan Echeverri <jecheverri@fluidattacks.com>
2021-08-10 18:47:16 -0500

    skims\feat(back): #5153.3 add decoder jwt verification
    - Add csharp_verify_decoder to lib_root checks
    
    
    Former-commit-id: c2642c87ca48e3e0aff5219d7f8a36dcbcb1c21a

--- skims/skims/lib_root/f017.py
+++ skims/skims/lib_root/f017.py
@@ -9,9 +9,69 @@ from model import (
 from sast.query import (
     get_vulnerabilities_from_n_ids,
 )
+from typing import (
+    Any,
+    Tuple,
+)
 from utils import (
     graph as g,
 )
+from utils.graph.transformation import (
+    build_member_access_expression_key,
+)
+
+
+def csharp_verify_decoder(
+    graph_db: graph_model.GraphDB,
+) -> core_model.Vulnerabilities:
+    def n_ids() -> graph_model.GraphShardNodes:
+        for shard in graph_db.shards_by_language(
+            graph_model.GraphShardMetadataLanguage.CSHARP,
+        ):
+            for member in g.filter_nodes(
+                shard.graph,
+                nodes=shard.graph.nodes,
+                predicate=g.pred_has_labels(
+                    label_type="member_access_expression"
+                ),
+            ):
+                method = build_member_access_expression_key(
+                    shard.graph,
+                    member,
+                )
+                if method == "decoder.Decode":
+                    pred = g.pred(shard.graph, member)
+                    props = g.get_ast_childs(
+                        shard.graph, pred[0], depth=4, label_type="identifier"
+                    )
+                    if not verify_prop(shard.graph, props):
+                        yield shard, member
+
+    def verify_prop(
+        graph: graph_model.GraphShard, props: Tuple[Any, ...]
+    ) -> bool:
+        prop_value = False
+        for prop in props:
+            if graph.nodes[prop].get("label_text") == "verify":
+                arg = g.pred(
+                    graph,
+                    prop,
+                    depth=2,
+                )[1]
+                arg_value = g.get_ast_childs(
+                    graph, arg, label_type="boolean_literal"
+                )
+                if graph.nodes[arg_value[0]].get("label_text") == "true":
+                    prop_value = True
+        return prop_value
+
+    return get_vulnerabilities_from_n_ids(
+        cwe=("319",),
+        desc_key="F017.title",
+        desc_params=dict(lang="C#"),
+        finding=FINDING,
+        graph_shard_nodes=n_ids(),
+    )
 
 
 def csharp_jwt_signed(
@@ -40,7 +100,6 @@ def csharp_jwt_signed(
         graph: graph_model.GraphShard, depth: int = 1, elem_jwt: int = 0
     ) -> bool:
         pred = g.pred(graph, elem_jwt, depth)
-        print(pred[0])
         if (
             graph.nodes[pred[0]].get("label_type")
             == "member_access_expression"
@@ -64,4 +123,7 @@ def csharp_jwt_signed(
 
 
 FINDING: core_model.FindingEnum = core_model.FindingEnum.F017
-QUERIES: graph_model.Queries = ((FINDING, csharp_jwt_signed),)
+QUERIES: graph_model.Queries = (
+    (FINDING, csharp_verify_decoder),
+    (FINDING, csharp_jwt_signed),
+)
