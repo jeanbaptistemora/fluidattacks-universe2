8b5f22955e7b6d27c10f46fd3ca96397935e418e
Andres Aguirre <aaguirre@fluidattacks.com>
2021-08-11 10:36:51 -0500

    makes\refac(back): #4329.258 api status in batch
    - Get the api status flag in the batch application
    and propagate it to the integrates env
    
    
    Former-commit-id: 7f7723852431c8544568c5db9fb4770966013b41

--- makes/applications/integrates/batch/entrypoint.sh
+++ makes/applications/integrates/batch/entrypoint.sh
@@ -3,12 +3,13 @@
 function main {
   local login_env
   local env="${1:-}"
+  local api_status="${2:-no-migration}"
   login_env=$(case "${env}" in
     test) echo "dev" ;;
     *) echo "${env}" ;;
   esac)
 
-  source __envIntegratesEnv__ "${login_env}" \
+  source __envIntegratesEnv__ "${login_env}" "${api_status}" \
     && if test "${env}" == 'prod'; then
       DAEMON=true integrates-cache
     elif test "${env}" == 'dev'; then
@@ -17,7 +18,7 @@ function main {
         && DAEMON=true integrates-storage
     fi \
     && pushd integrates \
-    && python3 -m back.src.batch.dispatch "${@:2}" \
+    && python3 -m back.src.batch.dispatch "${@:3}" \
     && popd \
     || return 1
 }
