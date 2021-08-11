b03b1254e2df57c8e67e6d56f997934925774881
Jonathan Mesa <jmesa@fluidattacks.com>
2021-08-11 16:00:03 -0500

    integrates\refac(back): #4329.262 update groups domain
    - duplicate groups domain functions for using the new
    finding dataloader, previous to their usage in other modules
    
    
    Former-commit-id: 20a445e0eeacdb59d4d4b10e2b0a96a35b33ad86

--- integrates/back/src/groups/domain.py
+++ integrates/back/src/groups/domain.py
@@ -825,6 +825,31 @@ async def get_closed_vulnerabilities(context: Any, group_name: str) -> int:
     return closed_vulnerabilities
 
 
+async def get_closed_vulnerabilities_new(context: Any, group_name: str) -> int:
+    group_findings_loader = context.group_findings_new
+    group_findings_loader.clear(group_name)
+    finding_vulns_loader = context.finding_vulns_nzr
+
+    group_findings: Tuple[Finding] = await group_findings_loader.load(
+        group_name
+    )
+    findings_vulns = await finding_vulns_loader.load_many_chained(
+        [finding.id for finding in group_findings]
+    )
+
+    last_approved_status = await collect(
+        [
+            in_process(vulns_utils.get_last_status, vuln)
+            for vuln in findings_vulns
+        ]
+    )
+    closed_vulnerabilities = 0
+    for status in last_approved_status:
+        if status == "closed":
+            closed_vulnerabilities += 1
+    return closed_vulnerabilities
+
+
 async def get_description(group_name: str) -> str:
     return await groups_dal.get_description(group_name)
 
@@ -902,6 +927,25 @@ async def get_mean_remediate(
     group_findings = await group_findings_loader.load(group_name)
     vulns = await finding_vulns_loaders.load_many_chained(
         [str(finding["finding_id"]) for finding in group_findings]
+    )
+    return await vulns_utils.get_mean_remediate_vulnerabilities(
+        vulns, min_date
+    )
+
+
+async def get_mean_remediate_new(
+    context: Any,
+    group_name: str,
+    min_date: Optional[date] = None,
+) -> Decimal:
+    group_findings_loader = context.group_findings_new
+    finding_vulns_loaders = context.finding_vulns
+
+    group_findings: Tuple[Finding] = await group_findings_loader.load(
+        group_name
+    )
+    vulns = await finding_vulns_loaders.load_many_chained(
+        [finding.id for finding in group_findings]
     )
     return await vulns_utils.get_mean_remediate_vulnerabilities(
         vulns, min_date
@@ -996,6 +1040,24 @@ async def get_open_finding(context: Any, group_name: str) -> int:
     group_findings = await group_findings_loader.load(group_name)
     vulns = await finding_vulns_loader.load_many_chained(
         [finding["finding_id"] for finding in group_findings]
+    )
+
+    finding_vulns_dict = defaultdict(list)
+    for vuln in vulns:
+        finding_vulns_dict[vuln["finding_id"]].append(vuln)
+    finding_vulns = list(finding_vulns_dict.values())
+    return await vulns_utils.get_open_findings(finding_vulns)
+
+
+async def get_open_finding_new(context: Any, group_name: str) -> int:
+    finding_vulns_loader = context.finding_vulns_nzr
+    group_findings_loader = context.group_findings_new
+
+    group_findings: Tuple[Finding] = await group_findings_loader.load(
+        group_name
+    )
+    vulns = await finding_vulns_loader.load_many_chained(
+        [finding.id for finding in group_findings]
     )
 
     finding_vulns_dict = defaultdict(list)
