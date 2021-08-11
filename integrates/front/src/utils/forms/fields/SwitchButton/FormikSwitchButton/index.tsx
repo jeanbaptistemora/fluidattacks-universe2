93cbbcc6b71259b27b738425ad1b05cddd21c880
Alejandro Trujillo <atrujillo@fluidattacks.com>
2021-08-11 10:27:06 -0500

    integrates\feat(front): #3083.49 remove redux remainings
    - Created FormikSwitchButton
    
    
    Former-commit-id: 5439549c7bdedba398efee458d1a2220a61b3f08

---
+++ integrates/front/src/utils/forms/fields/SwitchButton/FormikSwitchButton/index.tsx
@@ -0,0 +1,35 @@
+import type { FieldProps } from "formik";
+import _ from "lodash";
+import React from "react";
+
+import type { ISwitchButtonProps } from "components/SwitchButton";
+import { SwitchButton } from "components/SwitchButton/index";
+
+declare type FormikSwitchButtonProps = FieldProps & ISwitchButtonProps;
+
+// Custom SwitchButton whose state can be managed by redux-form
+export const FormikSwitchButton: React.FC<FormikSwitchButtonProps> = (
+  // Readonly utility type does not work on deeply nested types
+  // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+  props: Readonly<FormikSwitchButtonProps>
+): JSX.Element => {
+  const { field, id, disabled, offlabel, onlabel, onChange } = props;
+  const { checked } = field;
+  // eslint-disable-next-line @typescript-eslint/no-shadow
+  function handleOnChange(checked: boolean): void {
+    if (!_.isUndefined(onChange)) {
+      onChange(checked);
+    }
+  }
+
+  return (
+    <SwitchButton
+      checked={checked ?? false}
+      disabled={disabled}
+      id={id}
+      offlabel={offlabel}
+      onChange={handleOnChange}
+      onlabel={onlabel}
+    />
+  );
+};
