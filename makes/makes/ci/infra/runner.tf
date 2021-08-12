d01347844e436544f70f5eb40e0e9475eab77964
Daniel Salazar <dsalazar@fluidattacks.com>
2021-08-12 12:45:55 -0500

    makes\feat(infra): #5163 external cache
    - Implement external cache module to reuse from several
    runners
    
    
    Former-commit-id: 0c50d077f4a9fa2def8b91968d9c9ca0073bed15

--- makes/makes/ci/infra/runner.tf
+++ makes/makes/ci/infra/runner.tf
@@ -43,6 +43,23 @@ variable "runner_block_device" {
   }
 }
 
+module "fluidattacks_ci_cache" {
+  source  = "npalm/gitlab-runner/aws//modules/cache"
+  version = "4.28.0"
+
+  environment             = "fluidattacks-ci-cache"
+  cache_bucket_versioning = false
+  cache_expiration_days   = 30
+  cache_lifecycle_clear   = true
+  cache_lifecycle_prefix  = ""
+  create_cache_bucket     = true
+
+  tags = {
+    "management:type"    = "production"
+    "management:product" = "makes"
+  }
+}
+
 module "fluidattacks_ci" {
   source  = "npalm/gitlab-runner/aws"
   version = "4.28.0"
