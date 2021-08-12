b6d28a0a91634202a0d442922948e779e3cd2cb8
Jonathan Mesa <jmesa@fluidattacks.com>
2021-08-12 14:26:55 -0500

    integrates\refac(back): #4903.49 add migration
    - add migration for moving vulns to a designated finding for
    standarization purposes
    
    
    Former-commit-id: f6361d2caaf12eeaca0e8c497656d40afc5efc73

---
+++ integrates/back/migrations/0119_move_vulns_for_standarization.py
@@ -0,0 +1,119 @@
+# pylint: disable=invalid-name
+"""
+This migration will move vulns to a designated finding for
+standarization purposes. The vulns info is loaded from a csv file.
+
+Related issue:
+https://gitlab.com/fluidattacks/product/-/issues/4903
+
+Execution Time:
+Finalization Time:
+"""
+
+from aioextensions import (
+    collect,
+    run,
+)
+import csv
+from dataloaders import (
+    Dataloaders,
+    get_new_context,
+)
+import time
+from typing import (
+    Dict,
+)
+from vulnerabilities import (
+    dal as vulns_dal,
+)
+
+PROD: bool = True
+
+
+async def process_vuln(
+    context: Dataloaders,
+    vuln_info: Dict[str, str],
+) -> bool:
+    group_findings_loader = context.group_findings
+    group_findings = await group_findings_loader.load(vuln_info["group_name"])
+    group_findings_titles = [finding["title"] for finding in group_findings]
+
+    if vuln_info["definitive_name"] not in group_findings_titles:
+        print(
+            f'  --- {vuln_info["vuln_uuid"]}, '
+            f'finding "{vuln_info["definitive_name"]}" NOT found'
+        )
+        return False
+    target_finding = next(
+        finding
+        for finding in group_findings
+        if finding["title"] == vuln_info["definitive_name"]
+    )
+
+    vuln = await vulns_dal.get(vuln_info["vuln_uuid"])
+    if len(vuln) < 0:
+        print(f'   --- ERROR: {vuln_info["vuln_uuid"]} NOT found')
+    new_vuln = vuln[0]
+    new_vuln["finding_id"] = target_finding["id"]
+
+    success = False
+    if PROD:
+        # Create new one
+        success = await vulns_dal.create(new_vuln)
+        print(
+            f'   --- {new_vuln["finding_id"]}, '
+            f'vuln {new_vuln["UUID"]} created: {success}'
+        )
+        # Delete old one
+        if success:
+            success = await vulns_dal.delete(
+                vuln_info["vuln_uuid"], vuln_info["finding_id"]
+            )
+            print(
+                f'   --- {vuln_info["finding_id"]}, '
+                f'vuln {vuln_info["vuln_uuid"]} deleted: {success}'
+            )
+    else:
+        print(
+            f'   === target_finding: {new_vuln["finding_id"]}, '
+            f'new_vuln {new_vuln["UUID"]}'
+        )
+        return True
+
+    return success
+
+
+async def main() -> None:
+    # Read file with deletion info
+    with open("0119.csv", mode="r") as f:
+        reader = csv.reader(f)
+        vulns_to_move = [
+            {
+                "group_name": row[0],
+                "finding_id": row[1],
+                "finding_name": row[2],
+                "vuln_uuid": row[3],
+                "definitive_name": row[4],
+            }
+            for row in reader
+        ]
+    print(f"    === vulns to move: {len(vulns_to_move)}")
+    print(f"    === sample: {vulns_to_move[:1]}")
+
+    context: Dataloaders = get_new_context()
+    success = all(
+        await collect(process_vuln(context, vuln) for vuln in vulns_to_move)
+    )
+
+    print(f"   === success: {success}")
+
+
+if __name__ == "__main__":
+    execution_time = time.strftime(
+        "Execution Time:    %Y-%m-%d at %H:%M:%S UTC%Z"
+    )
+    run(main())
+    finalization_time = time.strftime(
+        "Finalization Time: %Y-%m-%d at %H:%M:%S UTC%Z"
+    )
+    print(f"{execution_time}\n{finalization_time}")
