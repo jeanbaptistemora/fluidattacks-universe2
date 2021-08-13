e32f9ce781d95b6dcab810ff2bdcb12ef49f58dc
Daniel Murcia <dmurcia@fluidattacks.com>
2021-08-13 12:03:30 -0500

    observes\test(back): #5157.16 proj schema
    - test json schema creation
    
    
    Former-commit-id: c0a64e56c538efcdf36d895be39b47296f3fc9cb

---
+++ observes/singer/tap_announcekit/tests/stream/project/test_internal.py
@@ -0,0 +1,20 @@
+import pytest
+from singer_io.singer2.json_schema import (
+    JsonSchemaFactory,
+)
+from tap_announcekit.stream.project._builders import (
+    proj_query,
+)
+from tap_announcekit.stream.project._encode import (
+    project_schema,
+)
+
+
+def test_query() -> None:
+    proj_query("1234")
+
+
+@pytest.mark.xfail(reason="in development")
+def test_proj_schema() -> None:
+    schema = project_schema()
+    JsonSchemaFactory.from_json(schema)
--- observes/singer/tap_announcekit/tests/stream/project/test_project.py
+++
@@ -1,7 +0,0 @@
-from tap_announcekit.stream.project._builders import (
-    proj_query,
-)
-
-
-def test_query() -> None:
-    proj_query("1234")
