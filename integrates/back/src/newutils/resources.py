371f28e85e8360a8c15ff968a75b22fb40369c95
John Perez <jperez@fluidattacks.com>
2021-08-13 16:20:18 -0500

    integrates\refac(back): #5087.2 use operation
    - add sign urls operation to resources utils
    
    
    Former-commit-id: a0e5ea3e7673d95d26df708e98ebbedc2854af77

--- integrates/back/src/newutils/resources.py
+++ integrates/back/src/newutils/resources.py
@@ -15,6 +15,16 @@ async def download_file(file_info: str, group_name: str) -> str:
     return await s3_ops.sign_url(file_url, 10, FI_AWS_S3_RESOURCES_BUCKET)
 
 
+async def upload_file(file_info: str, group_name: str) -> str:
+    group_name = group_name.lower()
+    file_url = f"{group_name}/{file_info}"
+    return await s3_ops.sing_upload_url(
+        file_url,
+        10,
+        FI_AWS_S3_RESOURCES_BUCKET,
+    )
+
+
 async def remove_file(file_name: str) -> bool:
     return await s3_ops.remove_file(FI_AWS_S3_RESOURCES_BUCKET, file_name)
 
