756e63450f74c46e4d1d9b42afd73556ce311684
Andres Aguirre <aaguirre@fluidattacks.com>
2021-08-13 14:22:27 -0500

    integrates\refac(back): #4329.275 where to findings
    - Add support for the new group findings dataloader in
    the where to findings of the disjoint force directed graph
    
    
    Former-commit-id: 1b82c3f2d76db5627565f902e047b23dca92908f

--- integrates/charts/generators/disjoint_force_directed_graph/where_to_findings.py
+++ integrates/charts/generators/disjoint_force_directed_graph/where_to_findings.py
@@ -4,28 +4,54 @@ from aioextensions import (
 from charts import (
     utils,
 )
+from context import (
+    FI_API_STATUS,
+)
 from dataloaders import (
     get_new_context,
+)
+from db_model.findings.types import (
+    Finding,
+)
+from findings import (
+    domain as findings_domain,
 )
 from frozendict import (
     frozendict,
 )
+from typing import (
+    Tuple,
+)
 
 
-async def generate_one(group: str) -> dict:
+async def generate_one(group: str) -> dict:  # pylint: disable=too-many-locals
     context = get_new_context()
     finding_vulns_loader = context.finding_vulns_nzr
-    group_findings_loader = context.group_findings
     data: dict = {
         "nodes": set(),
         "links": set(),
     }
-    group_findings = await group_findings_loader.load(group)
-    for finding in group_findings:
-        finding_id = finding["finding_id"]
-        finding_title = finding["title"]
-        finding_cvss = finding["cvss_temporal"]
-
+    if FI_API_STATUS == "migration":
+        group_findings_new_loader = context.group_findings_new
+        group_findings_new: Tuple[
+            Finding, ...
+        ] = await group_findings_new_loader.load(group)
+        group_findings_data = [
+            (
+                finding.id,
+                finding.title,
+                findings_domain.get_severity_score_new(finding.severity),
+            )
+            for finding in group_findings_new
+        ]
+    else:
+        group_findings_loader = context.group_findings
+        group_findings = await group_findings_loader.load(group)
+        group_findings_data = [
+            (finding["finding_id"], finding["title"], finding["cvss_temporal"])
+            for finding in group_findings
+        ]
+    for finding_id, finding_title, finding_cvss in group_findings_data:
         finding_vulns = await finding_vulns_loader.load(finding_id)
         for vulnerability in finding_vulns:
             source = utils.get_vulnerability_source(vulnerability)
