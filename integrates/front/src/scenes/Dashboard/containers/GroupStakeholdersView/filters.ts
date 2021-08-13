270b104f385f93b6b78854ce170fc98bb19d6f43
Alejandro Trujillo <atrujillo@fluidattacks.com>
2021-08-13 13:52:53 -0500

    integrates\feat(front): #5131.1 stakeholders filter
    - Created filter options file
    
    
    Former-commit-id: 131274e03e6508ba9d1c23eb91a662a94316badc

---
+++ integrates/front/src/scenes/Dashboard/containers/GroupStakeholdersView/filters.ts
@@ -0,0 +1,20 @@
+const selectOptionsRole = {
+  admin: "Admin",
+  analyst: "Analyst",
+  closer: "Closer",
+  customer: "User",
+  customeradmin: "User Manager",
+  executive: "Executive",
+  // eslint-disable-next-line camelcase
+  group_manager: "Group Manager",
+  resourcer: "Resourcer",
+  reviewer: "Reviewer",
+};
+
+const selectOptionsInvitation = {
+  CONFIRMED: "Confirmed",
+  PENDING: "Pending",
+  UNREGISTERED: "Unregistered",
+};
+
+export { selectOptionsInvitation, selectOptionsRole };
