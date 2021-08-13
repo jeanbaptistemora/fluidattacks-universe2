6fedb704d30e33e8628be18c77a86842e746441f
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-08-13 07:52:10 -0500

    integrates\refac(back): #5061.7 use splitted file
    - use types and functions splitted in bar_chart utils generator
    - no-deltas-test
    
    
    Former-commit-id: fdf9cf5f98e0bda1a2f9af33c7e65ad22fc85f99

--- integrates/charts/generators/bar_chart/mttr_benchmarking.py
+++ integrates/charts/generators/bar_chart/mttr_benchmarking.py
@@ -8,11 +8,13 @@ from async_lru import (
 from charts import (
     utils,
 )
-from charts.colors import (
-    RISK,
-)
-from custom_types import (
-    Vulnerability,
+from charts.generators.bar_chart.utils import (
+    format_data,
+    get_best_mttr,
+    get_mean_organizations,
+    get_valid_organizations,
+    get_vulnerability_reattacks,
+    OrganizationBenchmarking,
 )
 from dataloaders import (
     Dataloaders,
@@ -29,26 +31,9 @@ from statistics import (
     mean,
 )
 from typing import (
-    Any,
-    Dict,
     List,
-    NamedTuple,
     Tuple,
 )
-
-
-class OrganizationBenchmarking(NamedTuple):
-    is_valid: bool
-    mttr: Decimal
-    organization_id: str
-
-
-def get_vulnerability_reattacks(*, vulnerability: Vulnerability) -> int:
-    return sum(
-        1
-        for verification in vulnerability["historic_verification"]
-        if verification.get("status") == "REQUESTED"
-    )
 
 
 @alru_cache(maxsize=None, typed=True)
@@ -70,14 +55,14 @@ async def get_data_one_organization(
 ) -> OrganizationBenchmarking:
     groups_mttr_data = await collect(
         [get_mean_remediate(loaders, group.lower()) for group in groups],
-        workers=12,
+        workers=24,
     )
     groups_reattacks_data = await collect(
         [
             get_group_reattacks(group=group.lower(), loaders=loaders)
             for group in groups
         ],
-        workers=12,
+        workers=24,
     )
 
     mttr: Decimal = (
@@ -92,79 +77,6 @@ async def get_data_one_organization(
         is_valid=sum(groups_reattacks_data) > 1000,
         organization_id=organization_id,
         mttr=mttr,
-    )
-
-
-def format_data(data: Tuple[Decimal, Decimal, Decimal]) -> Dict[str, Any]:
-
-    return dict(
-        data=dict(
-            columns=[
-                [
-                    "Mean time to remediate",
-                    Decimal("0") if data[0] == Decimal("0") else data[0],
-                    data[1],
-                    data[2],
-                ]
-            ],
-            colors={
-                "Mean time to remediate": RISK.neutral,
-            },
-            type="bar",
-        ),
-        axis=dict(
-            x=dict(
-                categories=[
-                    "My organization",
-                    "Average all organizations",
-                    "Best organization",
-                ],
-                type="category",
-            ),
-            y=dict(
-                min=0,
-                padding=dict(
-                    bottom=0,
-                ),
-            ),
-        ),
-        barChartYTickFormat=True,
-    )
-
-
-def get_valid_organizations(
-    *,
-    organizations: Tuple[OrganizationBenchmarking, ...],
-    organization_id: str,
-) -> List[OrganizationBenchmarking]:
-    return [
-        organization
-        for organization in organizations
-        if organization_id != organization.organization_id
-        and organization.is_valid
-        and organization.mttr != Decimal("Infinity")
-    ]
-
-
-def get_mean_organizations(
-    *, organizations: List[OrganizationBenchmarking]
-) -> Decimal:
-    return (
-        Decimal(
-            mean([organization.mttr for organization in organizations])
-        ).to_integral_exact(rounding=ROUND_CEILING)
-        if organizations
-        else Decimal("0")
-    )
-
-
-def get_best_mttr(*, organizations: List[OrganizationBenchmarking]) -> Decimal:
-    return (
-        Decimal(
-            min([organization.mttr for organization in organizations])
-        ).to_integral_exact(rounding=ROUND_CEILING)
-        if organizations
-        else Decimal("0")
     )
 
 
