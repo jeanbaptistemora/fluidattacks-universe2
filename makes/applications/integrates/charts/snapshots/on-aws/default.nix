f9ea4a302729ca03a32512e5fae2121e11f52512
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-08-17 07:24:28 -0500

    integrates\feat(build): #5061.11 add job to snapshots
    - add job for proccessing `integrates.charts.snapshots` in aws
    
    
    Former-commit-id: 0f62847a8a60e73c8fdfd277c7d4ece5e332f12c

---
+++ makes/applications/integrates/charts/snapshots/on-aws/default.nix
@@ -0,0 +1,16 @@
+{ computeOnAws
+, ...
+}:
+computeOnAws {
+  attempts = 1;
+  command = [ "./m" "integrates.charts.snapshots" ];
+  jobname = "integrates-charts-snapshots";
+  jobqueue = "dedicated_later";
+  name = "integrates-charts-snapshots-on-aws";
+  product = "integrates";
+  secrets = [
+    "PRODUCT_API_TOKEN"
+  ];
+  timeout = 14400;
+  vcpus = 2;
+}
