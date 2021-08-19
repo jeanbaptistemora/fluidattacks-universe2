e8cb16e542d7a903be01d1307c68d75d44a90a54
David Acevedo <dacevedo@fluidattacks.com>
2021-08-19 13:48:58 -0500

    integrates\refac(front): #5202.2 update some deps
    - updated webpack dev server
    - remove unneeded dep lock
    - no-deltas-test
    
    
    Former-commit-id: 6b9b539d11832d3fcdf9129d997bb8d841e6e359

--- integrates/front/webpack.dev.config.ts
+++ integrates/front/webpack.dev.config.ts
@@ -1,25 +1,25 @@
 import type { Configuration } from "webpack";
-import { HotModuleReplacementPlugin } from "webpack";
 import type { Configuration as DevServerConfig } from "webpack-dev-server";
 
 import { commonConfig } from "./webpack.common.config";
 
 interface IWebpackConfig extends Configuration {
-  devServer: DevServerConfig;
+  devServer: DevServerConfig & {
+    client: Pick<DevServerConfig, "overlay">;
+  };
 }
 
 const devConfig: IWebpackConfig = {
   ...commonConfig,
   devServer: {
-    compress: true,
+    client: {
+      overlay: true,
+    },
     headers: { "Access-Control-Allow-Origin": "https://localhost:8001" },
     historyApiFallback: true,
     hot: true,
     https: true,
-    overlay: true,
     port: 3000,
-    publicPath: (commonConfig.output as NonNullable<IWebpackConfig["output"]>)
-      .publicPath as string,
   },
   devtool: false,
   mode: "development",
@@ -27,7 +27,6 @@ const devConfig: IWebpackConfig = {
     ...commonConfig.output,
     publicPath: "https://localhost:3000/dashboard/",
   },
-  plugins: [...(commonConfig.plugins as []), new HotModuleReplacementPlugin()],
 };
 
 export = devConfig;
