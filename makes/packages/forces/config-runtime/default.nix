c09046e114017c330770181e7170c8dd0f501e1e
Kevin Amado <kamado@fluidattacks.com>
2021-08-23 13:39:39 -0500

    forces\refac(build): #5217.5 pypi from makes
    - Use makes to create the pypi environment
    
    
    Former-commit-id: 043c4a7e86405f3f90f21025eedc307bcd3c0652

--- makes/packages/forces/config-runtime/default.nix
+++ makes/packages/forces/config-runtime/default.nix
@@ -1,4 +1,4 @@
-{ buildPythonRequirements
+{ makes
 , makeTemplate
 , nixpkgs
 , path
@@ -14,39 +14,10 @@ makeTemplate {
       nixpkgs.git
       nixpkgs.python38
     ];
-    envPython38Paths = [
-      (buildPythonRequirements {
-        dependencies = [ ];
+    envSources = [
+      (makes.makePythonPypiEnvironment {
         name = "forces-runtime";
-        requirements = {
-          direct = [
-            "aioextensions==20.8.2087641"
-            "aiogqlc==2.0.0b1"
-            "aiohttp==3.6.2"
-            "bugsnag==3.7.1"
-            "click==7.1.2"
-            "GitPython==3.1.7"
-            "more-itertools==8.5.0"
-            "oyaml==0.9"
-            "python-dateutil==2.8.1"
-            "pytz==2020.1"
-            "uvloop==0.14.0"
-          ];
-          inherited = [
-            "async-timeout==3.0.1"
-            "attrs==20.3.0"
-            "chardet==3.0.4"
-            "gitdb==4.0.5"
-            "idna==3.1"
-            "multidict==4.7.6"
-            "PyYAML==5.4.1"
-            "six==1.15.0"
-            "smmap==3.0.4"
-            "WebOb==1.8.6"
-            "yarl==1.6.3"
-          ];
-        };
-        python = nixpkgs.python38;
+        sourcesYaml = ./pypi-sources.yaml;
       })
     ];
     envPythonPaths = [
