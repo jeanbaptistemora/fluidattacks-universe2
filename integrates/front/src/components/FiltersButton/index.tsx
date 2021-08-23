db1f5dbc9d2dc4d047860c208717efd9545eccdc
Alejandro Trujillo <atrujillo@fluidattacks.com>
2021-08-23 16:48:26 -0500

    integrates\feat(front): #5122.1 locations filters
    - Created FiltersButton component
    
    
    Former-commit-id: a12504f32081f0dbf5191ddf177c015c5df55132

---
+++ integrates/front/src/components/FiltersButton/index.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/forbid-component-props, react/jsx-props-no-spreading
+  --------
+  We need className to override default styles from react-bootstrap and props
+  spreading is the technique used by react-bootstrap-table2 creators to pass
+  down props
+  */
+import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import _ from "lodash";
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+import { Button } from "components/Button";
+
+interface IFilterButtonProps {
+  isFilterEnabled: boolean;
+  onUpdateEnableFilter: () => void;
+}
+
+export const FiltersButton: React.FC<IFilterButtonProps> = (
+  props: Readonly<IFilterButtonProps>
+): JSX.Element => {
+  const { onUpdateEnableFilter, isFilterEnabled } = props;
+  const { t } = useTranslation();
+
+  function handleUpdateEnableFilter(): void {
+    if (!_.isUndefined(onUpdateEnableFilter)) {
+      onUpdateEnableFilter();
+    }
+  }
+
+  return (
+    <Button onClick={handleUpdateEnableFilter}>
+      {isFilterEnabled ? (
+        <FontAwesomeIcon icon={faMinus} />
+      ) : (
+        <FontAwesomeIcon icon={faPlus} />
+      )}
+      {t("dataTableNext.filters")}
+    </Button>
+  );
+};
