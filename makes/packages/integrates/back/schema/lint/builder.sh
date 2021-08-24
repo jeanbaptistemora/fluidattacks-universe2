6f542ff4ee320dbde845a39be232931b5a74cadf
Juan Chaves <jchaves@fluidattacks.com>
2021-08-23 19:38:13 -0500

    integrates\refac(back): #3668.35 remove api lint exceptions
    - Added descriptions to several mutations, entities and
    fields
    - Removed arguments-have-descriptions from being excepted in
    the linter
    
    
    Former-commit-id: 74d3d982bbb9a9c2e9aa50d7fa501f18a8c9581c

--- makes/packages/integrates/back/schema/lint/builder.sh
+++ makes/packages/integrates/back/schema/lint/builder.sh
@@ -6,7 +6,7 @@ function main {
     && copy "${envNodeRequirements}/node_modules" "${out}/node_modules" \
     && pushd "${out}" \
     && npx --no-install graphql-schema-linter \
-      --except 'enum-values-have-descriptions,fields-are-camel-cased,fields-have-descriptions,input-object-values-are-camel-cased,relay-page-info-spec,arguments-have-descriptions' \
+      --except 'enum-values-have-descriptions,fields-are-camel-cased,fields-have-descriptions,input-object-values-are-camel-cased,relay-page-info-spec' \
       "${envIntegratesApiSchema}/**/*.graphql" \
       "${envIntegratesApiSchema}/types/**/*.graphql" \
     && popd \
