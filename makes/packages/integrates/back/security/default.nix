2a3f2762c6b17c5b0b65064ed4a61046f0264a16
Kevin Amado <kamado@fluidattacks.com>
2021-08-24 09:42:49 -0500

    integrates\refac(build): #5217.7 pypi from makes
    - Handle these requirements from pypi with makes
    
    
    Former-commit-id: bb4a3d22335a1c34f071c5ecb53903cfdd8f4841

--- makes/packages/integrates/back/security/default.nix
+++ makes/packages/integrates/back/security/default.nix
@@ -1,32 +1,8 @@
 { nixpkgs
 , makeDerivation
-, buildPythonRequirements
 , path
 , ...
 } @ _:
-let
-  pythonRequirements = buildPythonRequirements {
-    name = "integrates-back-security-python";
-    python = nixpkgs.python37;
-    requirements = {
-      direct = [
-        "bandit==1.6.2"
-      ];
-      inherited = [
-        "gitdb==4.0.5"
-        "GitPython==3.1.14"
-        "importlib-metadata==3.7.3"
-        "pbr==5.5.1"
-        "PyYAML==5.4.1"
-        "six==1.15.0"
-        "smmap==3.0.5"
-        "stevedore==3.3.0"
-        "typing-extensions==3.7.4.3"
-        "zipp==3.4.1"
-      ];
-    };
-  };
-in
 makeDerivation {
   arguments = {
     envSrcIntegratesBack = path "/integrates/back";
@@ -34,11 +10,6 @@ makeDerivation {
   builder = path "/makes/packages/integrates/back/security/builder.sh";
   name = "integrates-back-security";
   searchPaths = {
-    envPaths = [
-      pythonRequirements
-    ];
-    envPython37Paths = [
-      pythonRequirements
-    ];
+    envPaths = [ nixpkgs.python37Packages.bandit ];
   };
 }
