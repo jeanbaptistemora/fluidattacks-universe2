ec29507e3ad1b708297fbf855c8f6a249a487b67
Kevin Amado <kamado@fluidattacks.com>
2021-08-25 16:54:43 -0500

    observes\refac(build): #5217.36 pypi with makes
    - Handle PyPI requirements with Makes
    
    
    Former-commit-id: 20ed80461bd8c25951d694457ce2308c6c58c60a

--- makes/packages/observes/env/service-batch-stability/runtime/default.nix
+++ makes/packages/observes/env/service-batch-stability/runtime/default.nix
@@ -1,9 +1,8 @@
-{ makeTemplate
-, packages
+{ makes
+, makeTemplate
 , path
 , ...
 }:
-with packages.observes.env;
 let
   self = path "/observes/services/batch_stability";
 in
@@ -13,14 +12,14 @@ makeTemplate {
     envMypyPaths = [
       self
     ];
-    envPaths = [
-      service-batch-stability.runtime.python
-    ];
     envPythonPaths = [
       self
     ];
-    envPython38Paths = [
-      service-batch-stability.runtime.python
+    envSources = [
+      (makes.makePythonPypiEnvironment {
+        name = "observes-env-runtime-batch-stability";
+        sourcesYaml = ./pypi-sources.yaml;
+      })
     ];
   };
 }
