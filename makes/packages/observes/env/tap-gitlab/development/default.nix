741fc69df09977b66a5a0f42ac2b7fef371566c5
Kevin Amado <kamado@fluidattacks.com>
2021-08-25 12:24:39 -0500

    observes\refac(build): #5217.20 pypi with makes
    - Handle PyPI requirements with Makes
    
    
    Former-commit-id: c4cd892788e4898aab4f52ee11e41cca180bf7ec

--- makes/packages/observes/env/tap-gitlab/development/default.nix
+++ makes/packages/observes/env/tap-gitlab/development/default.nix
@@ -1,19 +1,17 @@
-{ makeTemplate
+{ makes
+, makeTemplate
 , packages
 , ...
 }:
-with packages.observes.env;
 makeTemplate {
   name = "observes-env-tap-gitlab-development";
   searchPaths = {
-    envPaths = [
-      tap-gitlab.development.python
-    ];
-    envPython38Paths = [
-      tap-gitlab.development.python
-    ];
     envSources = [
-      tap-gitlab.runtime
+      (makes.makePythonPypiEnvironment {
+        name = "observes-env-tap-gitlab-development";
+        sourcesYaml = ./pypi-sources.yaml;
+      })
+      packages.observes.env.tap-gitlab.runtime
     ];
     envUtils = [
       "/makes/utils/lint-python"
