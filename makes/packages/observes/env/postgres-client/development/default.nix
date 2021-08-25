ff07b543b3844b6c1c7dc0e6c653ee8bbc11fdf0
Kevin Amado <kamado@fluidattacks.com>
2021-08-25 17:05:04 -0500

    observes\refac(build): #5217.37 pypi with makes
    - Handle PyPI requirements with Makes
    
    
    Former-commit-id: bfb95a3ab0a13abc0be3521878b0091a7562017a

--- makes/packages/observes/env/postgres-client/development/default.nix
+++ makes/packages/observes/env/postgres-client/development/default.nix
@@ -1,19 +1,17 @@
-{ makeTemplate
+{ makes
+, makeTemplate
 , packages
 , ...
 }:
-with packages.observes.env;
 makeTemplate {
   name = "observes-env-postgres-client-development";
   searchPaths = {
-    envPaths = [
-      postgres-client.development.python
-    ];
-    envPython38Paths = [
-      postgres-client.development.python
-    ];
     envSources = [
-      postgres-client.runtime
+      (makes.makePythonPypiEnvironment {
+        name = "observes-env-postgres-client-development";
+        sourcesYaml = ./pypi-sources.yaml;
+      })
+      packages.observes.env.postgres-client.runtime
     ];
   };
 }
