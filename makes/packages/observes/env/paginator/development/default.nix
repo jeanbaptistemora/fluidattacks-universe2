b2ccfe4b09a5db4f11153f61def6a4c3b9fccdb1
Kevin Amado <kamado@fluidattacks.com>
2021-08-26 10:01:58 -0500

    observes\refac(build): #5217.40 pypi with makes
    - Handle PyPI requirements with Makes
    
    
    Former-commit-id: 6e1ae7a728d4e68ac17a827db3168c3d1fbc8e4f

--- makes/packages/observes/env/paginator/development/default.nix
+++ makes/packages/observes/env/paginator/development/default.nix
@@ -1,19 +1,17 @@
-{ makeTemplate
+{ makes
+, makeTemplate
 , packages
 , ...
 }:
-with packages.observes.env;
 makeTemplate {
   name = "observes-env-paginator-development";
   searchPaths = {
-    envPaths = [
-      paginator.development.python
-    ];
-    envPython38Paths = [
-      paginator.development.python
-    ];
     envSources = [
-      paginator.runtime
+      (makes.makePythonPypiEnvironment {
+        name = "observes-env-paginator-runtime";
+        sourcesYaml = ./pypi-sources.yaml;
+      })
+      packages.observes.env.paginator.runtime
     ];
   };
 }
