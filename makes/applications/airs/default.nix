b43c39ff3fc1bcf51e76d987cb587d265ae7f43e
Diego Restrepo <drestrepo@fluidattacks.com>
2021-08-26 11:03:47 -0500

    airs\fix(build): #5139.4 fix bug
    - use sync script
    
    
    Former-commit-id: ce9e15b7669f271ed2a56276a9cc400676b2c531

--- makes/applications/airs/default.nix
+++ makes/applications/airs/default.nix
@@ -1,14 +1,17 @@
-{ nixpkgs
+{ makes
+, nixpkgs
 , makeEntrypoint
 , packages
 , applications
 , path
 , ...
 }:
-makeEntrypoint {
+makeEntrypoint
+{
   arguments = {
     envAirsBuild = applications.airs.build;
     envAirsDevelopment = applications.airs.development;
+    envSyncGatsby = path "/makes/applications/airs/gatsby.py";
   };
   name = "airs";
   searchPaths = {
@@ -17,9 +20,15 @@ makeEntrypoint {
       nixpkgs.gnused
       nixpkgs.gzip
       nixpkgs.nodejs
-      nixpkgs.python37
+      nixpkgs.python38
       nixpkgs.utillinux
       packages.makes.announce.bugsnag
+    ];
+    envSources = [
+      (makes.makePythonPypiEnvironment {
+        name = "gatsby-upload";
+        sourcesYaml = ./pypi-sources.yaml;
+      })
     ];
     envUtils = [
       "/makes/utils/aws"
--- makes/applications/airs/entrypoint.sh
+++ makes/applications/airs/entrypoint.sh
@@ -102,7 +102,7 @@ function deploy_eph {
   __envAirsBuild__ \
     && aws_login_dev airs \
     && compress_files "${src}/public" \
-    && sync_files "${src}/public" "s3://web.eph.fluidattacks.com/${CI_COMMIT_REF_NAME}" \
+    && python '__envSyncGatsby__' "${src}/public" "web.eph.fluidattacks.com" "${CI_COMMIT_REF_NAME}" \
     && announce_to_bugsnag ephemeral
 }
 
@@ -112,7 +112,7 @@ function deploy_prod {
   __envAirsBuild__ \
     && aws_login_prod airs \
     && compress_files "${src}/public" \
-    && sync_files "${src}/public" 's3://fluidattacks.com' \
+    && python '__envSyncGatsby__' "${src}/public" "fluidattacks.com" \
     && announce_to_bugsnag production
 }
 
