2ae6336eee1d858765e39ec5daa01f6a3c0100f9
Kevin Amado <kamado@fluidattacks.com>
2021-08-26 10:57:41 -0500

    observes\refac(build): #5217.43 pypi with makes
    - Handle PyPI requirements with Makes
    
    
    Former-commit-id: a7fa0e4bf54bcf1b3fa07665945bc864a7976c44

--- makes/packages/observes/env/gitlab-etl/development/default.nix
+++ makes/packages/observes/env/gitlab-etl/development/default.nix
@@ -1,16 +1,17 @@
-{ makeTemplate
+{ makes
+, makeTemplate
 , packages
 , ...
 }:
-with packages.observes.env;
 makeTemplate {
   name = "observes-env-gitlab-etl-development";
   searchPaths = {
-    envPython38Paths = [
-      gitlab-etl.development.python
-    ];
     envSources = [
-      gitlab-etl.runtime
+      (makes.makePythonPypiEnvironment {
+        name = "observes-env-gitlab-etl-development";
+        sourcesYaml = ./pypi-sources.yaml;
+      })
+      packages.observes.env.gitlab-etl.runtime
     ];
   };
 }
