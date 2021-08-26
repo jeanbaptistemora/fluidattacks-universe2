85ece93112a960350782cb57435d27de8dc26afc
Alejandro Salgado <asalgado@fluidattacks.com>
2021-08-26 08:28:24 -0500

    skims\feat(back): #5230.2 add lambda param syntax
    - Redefine parameter reader to include lambda parameters in c# syntax
    
    
    Former-commit-id: b723fe6dcba972fbc1244b81c2a603eaf69faaa8

--- skims/skims/sast_syntax_readers/c_sharp/parameter.py
+++ skims/skims/sast_syntax_readers/c_sharp/parameter.py
@@ -12,27 +12,28 @@ from utils import (
 )
 
 
+def _get_var_type(args: SyntaxReaderArgs, type_id: str) -> str:
+    type_node = args.graph.nodes[type_id]
+
+    if var_type_text := type_node.get("label_text"):
+        return var_type_text
+
+    if array_type := type_node.get("label_field_type"):
+        return args.graph.nodes[array_type]["label_text"]
+
+    raise MissingCaseHandling(args)
+
+
 def reader(args: SyntaxReaderArgs) -> SyntaxStepsLazy:
     match = g.match_ast(args.graph, args.n_id, "__0__", "__1__")
-    if (
-        len(match) == 2
-        and (var_type := match["__0__"])
-        and (var_identifier := match["__1__"])
-    ):
-        type_node = args.graph.nodes[var_type]
-        if var_type_text := args.graph.nodes[var_type].get("label_text"):
-            var_type_str = var_type_text
-        elif "label_text" not in type_node and (
-            array_type := type_node.get("label_field_type")
-        ):
-            var_type_str = args.graph.nodes[array_type]["label_text"]
-        else:
-            raise MissingCaseHandling(args)
+    type_id = match["__0__"]
+    identifier_id = match["__1__"]
 
-        yield SyntaxStepDeclaration(
-            meta=SyntaxStepMeta.default(args.n_id),
-            var=args.graph.nodes[var_identifier]["label_text"],
-            var_type=var_type_str,
-        )
-    else:
+    if len(match) != 2 or identifier_id is None:
         raise MissingCaseHandling(args)
+
+    yield SyntaxStepDeclaration(
+        meta=SyntaxStepMeta.default(args.n_id),
+        var=args.graph.nodes[identifier_id]["label_text"],
+        var_type=None if type_id is None else _get_var_type(args, type_id),
+    )
