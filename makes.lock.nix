a226c46eef4f7212e9b2d8fd76736e06c3c36186
Daniel Salazar <dsalazar@fluidattacks.com>
2021-08-26 15:23:42 -0500

    docs\feat(build): #4819 update makes
    - Update makes for using the last version
    of calculateCvss3
    
    
    Former-commit-id: 2200db227e76b10a8654481e655747542193c607

--- makes.lock.nix
+++ makes.lock.nix
@@ -1,6 +1,6 @@
 {
   makesSrc = builtins.fetchGit {
     url = "https://github.com/fluidattacks/makes";
-    rev = "9fc15d3ad0a612e8c17f6c9eb6f6f24dfeb7300d";
+    rev = "4320b4566479e9ff8c8d38fd76a16f276cef52a1";
   };
 }
