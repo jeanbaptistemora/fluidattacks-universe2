65105b03bd05a91f13170dfdf3a8d81a84dbd898
Mario Varas <mvaras@fluidattacks.com>
2021-08-27 10:39:04 -0500

    makes\feat(build): #5232.4 get redshift vars exported
    - Needed to query redshift when executing Sorts
    
    
    Former-commit-id: 40a9a87714644a81498f4a025964f38536dc4b09

--- makes/applications/sorts/entrypoint.sh
+++ makes/applications/sorts/entrypoint.sh
@@ -1,5 +1,11 @@
 # shellcheck shell=bash
 
 aws_login_prod 'sorts' \
-  && sops_export_vars 'sorts/secrets.yaml' 'MIXPANEL_API_TOKEN_SORTS'
+  && sops_export_vars 'sorts/secrets.yaml' \
+    'MIXPANEL_API_TOKEN_SORTS' \
+    'REDSHIFT_DATABASE' \
+    'REDSHIFT_HOST' \
+    'REDSHIFT_PASSWORD' \
+    'REDSHIFT_PORT' \
+    'REDSHIFT_USER'
 sorts "${@}"
