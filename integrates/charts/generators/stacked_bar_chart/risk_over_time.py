0fc9718affd086ef509c94e234745b98cc271966
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-09-06 07:43:06 -0500

    integrates\refac(back): #5055.21 update generator
    - update `risk_over_time` stacked_bar_chart generator to use monthly
    over time instead of weekly when appropiate
    
    
    Former-commit-id: 5998cdfb81682165c182aeb4dadafcbc920a0637

--- integrates/charts/generators/stacked_bar_chart/risk_over_time.py
+++ integrates/charts/generators/stacked_bar_chart/risk_over_time.py
@@ -14,6 +14,7 @@ from charts.generators.stacked_bar_chart.utils import (
     RISK_OVER_TIME,
     sum_over_time_many_groups,
     translate_date,
+    translate_date_last,
 )
 from dataloaders import (
     get_new_context,
@@ -25,18 +26,28 @@ from typing import (
     Dict,
     Iterable,
     List,
+    NamedTuple,
     Optional,
+    Tuple,
 )
 
 
+class RiskOverTime(NamedTuple):
+    monthly: Dict[str, Dict[datetime, float]]
+    weekly: Dict[str, Dict[datetime, float]]
+    should_use_monthly: bool
+
+
 @alru_cache(maxsize=None, typed=True)
-async def get_group_document(
+async def get_group_document(  # pylint: disable=too-many-locals
     group: str, days: Optional[int] = None
-) -> Dict[str, Dict[datetime, float]]:
+) -> RiskOverTime:
     data: List[GroupDocumentData] = []
+    data_monthly: List[GroupDocumentData] = []
     context = get_new_context()
     group_loader = context.group
 
+    data_name_monthly = "remediated_over_time_month"
     data_name = "remediated_over_time"
     if days == 30:
         data_name = f"{data_name}_30"
@@ -48,6 +59,9 @@ async def get_group_document(
         # Last 12 weeks
         elements[-12:]
         for elements in group_data[data_name]
+    ]
+    group_over_time_monthly = [
+        elements[-12:] for elements in group_data[data_name_monthly]
     ]
 
     if group_over_time:
@@ -72,33 +86,86 @@ async def get_group_document(
     else:
         print(f"[WARNING] {group} has no remediated_over_time attribute")
 
-    return {
-        "date": {datum.date: 0 for datum in data},
-        "Closed": {datum.date: datum.closed for datum in data},
-        "Accepted": {datum.date: datum.accepted for datum in data},
-        "Found": {
-            datum.date: datum.closed + datum.accepted + datum.opened
-            for datum in data
+    if group_over_time_monthly:
+        group_found_over_time = group_over_time_monthly[0]
+        group_closed_over_time = group_over_time_monthly[1]
+        group_accepted_over_time = group_over_time_monthly[2]
+
+        for accepted, closed, found in zip(
+            group_accepted_over_time,
+            group_closed_over_time,
+            group_found_over_time,
+        ):
+            data_monthly.append(
+                GroupDocumentData(
+                    accepted=accepted["y"],
+                    closed=closed["y"],
+                    opened=found["y"] - closed["y"] - accepted["y"],
+                    date=(
+                        translate_date_last(found["x"])
+                        if translate_date_last(found["x"]) < datetime.now()
+                        else datetime.combine(
+                            datetime.now(),
+                            datetime.min.time(),
+                        )
+                    ),
+                    total=found["y"],
+                )
+            )
+
+    return RiskOverTime(
+        should_use_monthly=False
+        if days
+        else len(group_data[data_name][0] if group_data[data_name] else [])
+        > 12,
+        monthly={
+            "date": {datum.date: 0 for datum in data_monthly},
+            "Closed": {datum.date: datum.closed for datum in data_monthly},
+            "Accepted": {datum.date: datum.accepted for datum in data_monthly},
+            "Found": {
+                datum.date: datum.closed + datum.accepted + datum.opened
+                for datum in data_monthly
+            },
         },
-    }
+        weekly={
+            "date": {datum.date: 0 for datum in data},
+            "Closed": {datum.date: datum.closed for datum in data},
+            "Accepted": {datum.date: datum.accepted for datum in data},
+            "Found": {
+                datum.date: datum.closed + datum.accepted + datum.opened
+                for datum in data
+            },
+        },
+    )
 
 
 async def get_many_groups_document(
     groups: Iterable[str], days: Optional[int] = None
 ) -> Dict[str, Dict[datetime, float]]:
-    group_documents = await collect(
-        get_group_document(group, days) for group in groups
+    group_documents: Tuple[RiskOverTime, ...] = await collect(
+        [get_group_document(group, days) for group in groups]
+    )
+    should_use_monthly: bool = any(
+        [group.should_use_monthly for group in group_documents]
     )
 
-    return sum_over_time_many_groups(group_documents, RISK_OVER_TIME)
+    return sum_over_time_many_groups(
+        [group_document.monthly for group_document in group_documents]
+        if should_use_monthly
+        else [group_document.weekly for group_document in group_documents],
+        RISK_OVER_TIME,
+    )
 
 
 async def generate_all() -> None:
     y_label: str = "Vulnerabilities"
     async for group in utils.iterate_groups():
+        group_document: RiskOverTime = await get_group_document(group)
         utils.json_dump(
             document=format_document(
-                document=await get_group_document(group),
+                document=group_document.monthly
+                if group_document.should_use_monthly
+                else group_document.weekly,
                 y_label=y_label,
             ),
             entity="group",
@@ -134,7 +201,7 @@ async def generate_all() -> None:
         async for group in utils.iterate_groups():
             utils.json_dump(
                 document=format_document(
-                    document=await get_group_document(group, days),
+                    document=(await get_group_document(group, days)).weekly,
                     y_label=y_label,
                 ),
                 entity="group",
