bdcbe31fa5313b0ed366f82aee9cf641f4de1497
Andres Aguirre <aaguirre@fluidattacks.com>
2021-09-06 18:15:28 -0500

    integrates\refac(build): #4329.360 load data from db design
    - Load the migrated facets from de database design
    - Add flag to include and ignore data during the migration
    
    
    Former-commit-id: ef26a0183a69c62d2408636d8de5dc871875ee3a

--- makes/applications/integrates/db/entrypoint.sh
+++ makes/applications/integrates/db/entrypoint.sh
@@ -2,37 +2,55 @@
 
 function populate {
   local email="${1:-integratesmanager@gmail.com}"
-  local db_status="${2:-}"
+  local api_status="${2:-no-migration}"
   local db_design='__envNewDbDesign__/database-design.json'
   local TMP_ITEMS='.tmp_integrates_vms'
   local i=0
+  local included_facets=()
+  local excluded_data_files=()
+  local facets=''
 
-  if test "${db_status}" == 'migration'; then
-    echo '[INFO] Populating from new database design...' \
-      && jq -c '{integrates_vms: [{PutRequest: {Item: .DataModel[].TableFacets[].TableData[]}}]}' \
-        ${db_design} > "${STATE_PATH}/${TMP_ITEMS}" \
-      && items_len=$(jq '.integrates_vms | length' "${STATE_PATH}/${TMP_ITEMS}") \
-      && echo "items qy: ${items_len}" \
-      && while [ $((i * 25)) -lt "$items_len" ]; do
-        local ilow=$((i * 25)) \
-          && local ihigh=$(((i + 1) * 25)) \
-          && jq -c "{integrates_vms: .integrates_vms[$ilow:$ihigh]}" \
-            "${STATE_PATH}/${TMP_ITEMS}" > "${STATE_PATH}/integrates_vms${i}.json" \
-          && ((i++))
-      done \
-      && rm "${STATE_PATH}/${TMP_ITEMS}"
-  else
-    echo "[INFO] Admin email: ${email}" \
-      && sed "s/2020-09-04.*/$(date -u +%Y-%m-%dT%H:%M:%S.000000%z)\"/g" \
-        < '__envDb__/data/forces.json' \
-      | sed "s/33e5d863252940edbfb144ede56d56cf/aaa/g" \
-        | sed "s/a125217504d447ada2b81da3e4bdab0e/bbb/g" \
-          > "${STATE_PATH}/forces.now.json" \
-      && for data in '__envDb__/data/'*'.json'; do
-        sed "s/__adminEmail__/${email}/g" "${data}" \
-          > "${STATE_PATH}/$(basename "${data}")"
-      done
+  if ! { test "${api_status}" == 'migration' || test "${api_status}" == 'no-migration'; }; then
+    echo '[ERROR] Second argument must be one of: migration,  no-migration' \
+      && return 1
   fi \
+    && if test "${api_status}" == 'migration'; then
+      included_facets+=(
+        "finding_id"
+      ) \
+      excluded_data_files+=(
+        "findings"
+      )
+    fi \
+    && facets=$(echo "${included_facets[@]}" | jq -R 'split(" ")') \
+    && echo '[INFO] Populating from new database design...' \
+    && jq -c --arg facets "${facets}" \
+      '{integrates_vms: [.DataModel[].TableFacets[] | select(.FacetName as $fn | $facets | index($fn) ) | {PutRequest: {Item: .TableData[]}}]}' \
+      ${db_design} > "${STATE_PATH}/${TMP_ITEMS}" \
+    && items_len=$(jq '.integrates_vms | length' "${STATE_PATH}/${TMP_ITEMS}") \
+    && echo "items qy: ${items_len}" \
+    && while [ $((i * 25)) -lt "$items_len" ]; do
+      local ilow=$((i * 25)) \
+        && local ihigh=$(((i + 1) * 25)) \
+        && jq -c "{integrates_vms: .integrates_vms[$ilow:$ihigh]}" \
+          "${STATE_PATH}/${TMP_ITEMS}" > "${STATE_PATH}/integrates_vms${i}.json" \
+        && ((i++))
+    done \
+    && rm "${STATE_PATH}/${TMP_ITEMS}"
+  echo "[INFO] Admin email: ${email}" \
+    && sed "s/2020-09-04.*/$(date -u +%Y-%m-%dT%H:%M:%S.000000%z)\"/g" \
+      < '__envDb__/data/forces.json' \
+    | sed "s/33e5d863252940edbfb144ede56d56cf/aaa/g" \
+      | sed "s/a125217504d447ada2b81da3e4bdab0e/bbb/g" \
+        > "${STATE_PATH}/forces.now.json" \
+    && for data in '__envDb__/data/'*'.json'; do
+      sed "s/__adminEmail__/${email}/g" "${data}" \
+        > "${STATE_PATH}/$(basename "${data}")"
+    done \
+    && for data_file in "${excluded_data_files[@]}"; do
+      echo "[INFO] Ignoring data file: ${data_file}" \
+        && rm "${STATE_PATH}/${data_file}.json"
+    done \
     && for data in "${STATE_PATH}/"*'.json'; do
       echo "[INFO] Writing data from: ${data}" \
         && aws dynamodb batch-write-item \
