1a4fdc673d6ba12ef8eaef24feddefe480eaddb0
Juan Echeverri <jecheverri@fluidattacks.com>
2021-09-07 13:02:50 -0500

    skims\feat(back): #5153.25 add model f211
    - add F211.yaml to vulnerabilities
    - add F211 entry to core_model.FindingEnum
    - update json config files
    
    
    Former-commit-id: aad35ea55b0eb968b751afe004000ec235c8857b

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -1267,6 +1267,26 @@ class FindingEnum(Enum):
             user_interaction=cvss3_model.UserInteraction.required,
         ),
     )
+    F211: FindingMetadata = FindingMetadata.new(
+        auto_approve=True,
+        code="F211",
+        cwe=405,
+        execution_queue=ExecutionQueue.control,
+        requirements=[72],
+        score=cvss3_model.Score(
+            attack_complexity=cvss3_model.AttackComplexity.low,
+            attack_vector=cvss3_model.AttackVector.network,
+            availability_impact=cvss3_model.AvailabilityImpact.none,
+            confidentiality_impact=cvss3_model.ConfidentialityImpact.none,
+            exploitability=cvss3_model.Exploitability.poc,
+            integrity_impact=cvss3_model.IntegrityImpact.low,
+            privileges_required=cvss3_model.PrivilegesRequired.none,
+            remediation_level=cvss3_model.RemediationLevel.unavailable,
+            report_confidence=cvss3_model.ReportConfidence.unknown,
+            severity_scope=cvss3_model.SeverityScope.unchanged,
+            user_interaction=cvss3_model.UserInteraction.none,
+        ),
+    )
     F234: FindingMetadata = FindingMetadata.new(
         auto_approve=False,
         code="F234",
