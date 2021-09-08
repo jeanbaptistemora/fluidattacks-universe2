70041c421ee5f014b85de85daffc24edf7dd1b11
Mario Varas <mvaras@fluidattacks.com>
2021-09-08 10:23:29 -0500

    makes\feat(build): #5263.4 add mixpanel secret
    - Sometimes making extract-features job fail
    
    
    Former-commit-id: ff9b1e64a0f09053ef371b6e2052447020aa4f50

--- makes/applications/sorts/extract-features/entrypoint.sh
+++ makes/applications/sorts/extract-features/entrypoint.sh
@@ -40,6 +40,7 @@ function main {
 
   aws_login_prod 'sorts' \
     && sops_export_vars 'sorts/secrets.yaml' \
+      'MIXPANEL_API_TOKEN_SORTS' \
       'REDSHIFT_DATABASE' \
       'REDSHIFT_HOST' \
       'REDSHIFT_PASSWORD' \
