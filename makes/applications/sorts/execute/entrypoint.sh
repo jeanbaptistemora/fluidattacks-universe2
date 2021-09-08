97ea6987bc0eade793622b31eb863be7d345a4f6
Mario Varas <mvaras@fluidattacks.com>
2021-09-08 10:54:48 -0500

    makes\feat(build): #5263.5 add missing secrets
    - Sometimes making execute job fail
    
    
    Former-commit-id: 99c2124a6a68909c1217755206372cbdde9f6b96

--- makes/applications/sorts/execute/entrypoint.sh
+++ makes/applications/sorts/execute/entrypoint.sh
@@ -25,7 +25,15 @@ function execute {
 function main {
   local groups_file
 
-  use_git_repo_services \
+  aws_login_prod 'sorts' \
+    && sops_export_vars 'sorts/secrets.yaml' \
+      'MIXPANEL_API_TOKEN_SORTS' \
+      'REDSHIFT_DATABASE' \
+      'REDSHIFT_HOST' \
+      'REDSHIFT_PASSWORD' \
+      'REDSHIFT_PORT' \
+      'REDSHIFT_USER' \
+    && use_git_repo_services \
     && groups_file="$(mktemp)" \
     && ls -1 groups > "${groups_file}" \
     && execute_chunk_parallel execute "${groups_file}"
