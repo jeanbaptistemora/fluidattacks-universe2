8dd7be862845604e940f3877c0d7a9488ad88c03
David Acevedo <dacevedo@fluidattacks.com>
2021-09-10 10:57:18 -0500

    integrates\refac(back): #398.55 move url attrs
    - added migration script

---
+++ integrates/back/migrations/0134_move_url_attrs.py
@@ -0,0 +1,98 @@
+# pylint: disable=invalid-name
+"""
+This migration moves the host, path, port and protocol attributes from the
+metadata to the state items on url roots
+"""
+
+from aioextensions import (
+    collect,
+    run,
+)
+from db_model import (
+    roots as roots_model,
+    TABLE,
+)
+from db_model.roots.get import (
+    GroupRootsLoader,
+    RootStatesLoader,
+)
+from db_model.roots.types import (
+    URLRootItem,
+    URLRootState,
+)
+from dynamodb import (
+    operations,
+)
+from dynamodb.types import (
+    PrimaryKey,
+)
+from groups.dal import (
+    get_all,
+)
+import time
+from typing import (
+    List,
+)
+
+
+async def update_root(root: URLRootItem) -> None:
+    historic_state: List[URLRootState] = await RootStatesLoader().load(root.id)
+    # Update historic states
+    await collect(
+        tuple(
+            operations.update_item(
+                item={
+                    "host": root.state.host,
+                    "path": root.state.path,
+                    "port": root.state.port,
+                    "protocol": root.state.protocol,
+                },
+                key=PrimaryKey(
+                    partition_key=f"ROOT#{root.id}",
+                    sort_key=f"STATE#{state.modified_date}",
+                ),
+                table=TABLE,
+            )
+            for state in historic_state
+        )
+    )
+    # Update latest state
+    await roots_model.update_root_state(
+        group_name=root.group_name, root_id=root.id, state=root.state
+    )
+    # Update metadata
+    await operations.update_item(
+        item={"host": None, "path": None, "port": None, "protocol": None},
+        key=PrimaryKey(
+            partition_key=f"ROOT#{root.id}",
+            sort_key=f"GROUP#{root.group_name}",
+        ),
+        table=TABLE,
+    )
+
+
+async def main() -> None:
+    groups = tuple(
+        group["project_name"]
+        for group in await get_all(data_attr="project_name")
+    )
+    group_roots = await GroupRootsLoader().load_many(groups)
+    await collect(
+        tuple(
+            update_root(root)
+            for roots in group_roots
+            for root in roots
+            if isinstance(root, URLRootItem)
+        )
+    )
+
+
+if __name__ == "__main__":
+    execution_time = time.strftime(
+        "Execution Time:    %Y-%m-%d at %H:%M:%S UTC%Z"
+    )
+    run(main())
+    finalization_time = time.strftime(
+        "Finalization Time: %Y-%m-%d at %H:%M:%S UTC%Z"
+    )
+    print(f"{execution_time}\n{finalization_time}")
