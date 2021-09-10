e7a7bf2287e002af0d9d5da49771e37e88a516a8
Andres Aguirre <aaguirre@fluidattacks.com>
2021-09-10 17:19:22 +0000

    integrates\test(back): #4329.392 create register by week
    - Refactor unit test to create register by week with the new db model

--- integrates/back/tests/unit/test_scheduler.py
+++ integrates/back/tests/unit/test_scheduler.py
@@ -265,6 +265,20 @@ async def test_create_register_by_week() -> None:
         assert item[0] is not None
 
 
+@pytest.mark.skipif(not MIGRATION, reason="Finding migration")
+async def test_create_register_by_week_new() -> None:
+    loaders = get_new_context()
+    group_name = "unittesting"
+    test_data = await update_indicators_new.create_register_by_week(
+        loaders, group_name
+    )
+    assert isinstance(test_data.vulnerabilities, list)
+    for item in test_data.vulnerabilities:
+        assert isinstance(item, list)
+        assert isinstance(item[0], dict)
+        assert item[0] is not None
+
+
 def test_create_data_format_chart() -> None:
     registers = OrderedDict(
         [
