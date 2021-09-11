688d90ffc5dd67edcfd8f8ae3da46c81c19c466a
Alejandro Salgado <asalgado@fluidattacks.com>
2021-09-11 15:15:31 -0500

    skims\test(back): #5153.35 add lib_root f035 test
    - Add lib_root/f035 test
    - Update lib_root test csv output

---
+++ skims/test/data/lib_root/f035/Test.cs
@@ -0,0 +1,16 @@
+﻿public class Startup
+{
+    public void start(IServiceCollection services)
+    {
+        services.Configure<IdentityOptions>( options =>
+        {
+            options.Password.RequireDigit = true;
+            options.Password.RequireDigit = false;
+            options.Password.RequiredLength = 8;
+            options.Password.RequireNonAlphanumeric = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequireLowercase = true;
+            options.Password.RequiredUniqueChars = 5;
+        });
+    }
+}
