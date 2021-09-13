582adcd88bd5106cc694d47bc7b645e0aedf901d
Alejandro Salgado <asalgado@fluidattacks.com>
2021-09-13 08:35:50 -0500

    skims\feat(back): #5305.1 add walker definition
    - Add Walker definition to sast_transformations/control_flow/types.py

--- skims/skims/sast_transformations/control_flow/types.py
+++ skims/skims/sast_transformations/control_flow/types.py
@@ -1,9 +1,20 @@
+from model.graph_model import (
+    Graph,
+)
 from typing import (
     Any,
+    Callable,
     Dict,
     List,
+    NamedTuple,
+    Set,
 )
 
 EdgeAttrs = Dict[str, str]
 Frame = Any  # will add types once I discover the pattern
 Stack = List[Frame]
+
+
+class Walker(NamedTuple):
+    applicable_node_label_types: Set[str]
+    walk_fun: Callable[[Graph, str, Stack], None]
