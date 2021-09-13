28b987993afbbe477b98be68fe855314704137ad
Andres Aguirre <aaguirre@fluidattacks.com>
2021-09-13 14:40:02 +0000

    integrates\test(back): #4329.396 get actions
    - Now the get actions test can be passed for the both
    finding db model

--- integrates/back/tests/unit/dal_test/test_batch.py
+++ integrates/back/tests/unit/dal_test/test_batch.py
@@ -15,7 +15,6 @@ pytestmark = [
 ]
 
 
-@pytest.mark.skipif(MIGRATION, reason="Finding migration")
 async def test_get_actions() -> None:
     all_actions = await batch_dal.get_actions()
     assert isinstance(all_actions, list)
