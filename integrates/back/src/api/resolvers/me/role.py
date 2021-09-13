6d1f0068f30fda5bd839f2c5f590e99b624317c1
Juan Chaves <jchaves@fluidattacks.com>
2021-09-13 19:46:11 +0000

    integrates\refac(back): #2447.14 fetch role information
    - Removed the args from the me role field and resolver,
    group-level roles will be served through the group entity
    while org-level roles will be served through the
    organization entity

--- integrates/back/src/api/resolvers/me/role.py
+++ integrates/back/src/api/resolvers/me/role.py
@@ -1,7 +1,4 @@
 import authz
-from custom_exceptions import (
-    InvalidParameter,
-)
 from custom_types import (
     Me,
 )
@@ -13,29 +10,8 @@ from typing import (
 )
 
 
-async def resolve(parent: Me, _info: GraphQLResolveInfo, **kwargs: str) -> str:
-    # As Entity is no longer a required arg, this check is needed to keep
-    # backwards compatibility while enforcing the need for a non-null
-    # identifier if the entity is not USER
-    if (
-        "entity" in kwargs
-        and kwargs.get("entity") != "USER"
-        and "identifier" not in kwargs
-    ):
-        raise InvalidParameter()
-
+async def resolve(
+    parent: Me, _info: GraphQLResolveInfo, **_kwargs: str
+) -> str:
     user_email: str = cast(str, parent["user_email"])
-    entity: str = kwargs.get("entity", "USER")
-    identifier: str = kwargs.get("identifier", "")
-    role = ""
-    group_entities = {"GROUP", "PROJECT"}
-
-    if entity == "USER":
-        role = await authz.get_user_level_role(user_email)
-    elif entity in group_entities and identifier:
-        role = await authz.get_group_level_role(user_email, identifier)
-    elif entity == "ORGANIZATION" and identifier:
-        role = await authz.get_organization_level_role(user_email, identifier)
-    else:
-        raise InvalidParameter()
-    return role
+    return await authz.get_user_level_role(user_email)
