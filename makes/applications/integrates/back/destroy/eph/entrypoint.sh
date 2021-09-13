29ea026da0d334ae4b7495bf7130584076f23c8d
Daniel Salazar <dsalazar@fluidattacks.com>
2021-09-13 14:51:47 -0500

    integrates\feat(infra): #5275 autoscale production
    - Add basic configuration for autoscaling production
    - Remove horizontal pod autoscalers in back.destroy.eph
    - Other minor changes

--- makes/applications/integrates/back/destroy/eph/entrypoint.sh
+++ makes/applications/integrates/back/destroy/eph/entrypoint.sh
@@ -6,6 +6,7 @@ function main {
   aws_login_dev integrates \
     && aws_eks_update_kubeconfig makes-k8s us-east-1 \
     && kubectl delete --all deployment -n "${namespace}" \
+    && kubectl delete --all hpa -n "${namespace}" \
     && kubectl delete --all secret -n "${namespace}" \
     && kubectl delete --all service -n "${namespace}" \
     && kubectl delete --all ingress -n "${namespace}"
