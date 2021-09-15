f0ce32389aa11be5f93fc4d003b8329cdbf2d01e
Karen Camargo <kcamargo@fluidattacks.com>
2021-09-15 11:43:30 -0500

    integrates\fix(back): #5317.1 add migration
    - Inactivate drills service
    - Update deprecated project status to 'deleted'

---
+++ integrates/back/migrations/0136_fix_drill_service_on_deleted_groups.py
@@ -0,0 +1,82 @@
+# pylint: disable=invalid-name
+"""
+This migration updates drills service to false on finished groups
+
+Execution Time:
+Finalization Time:
+"""
+
+from aioextensions import (
+    collect,
+    run,
+)
+from groups.dal import (
+    get_attributes,
+    update,
+)
+import time
+
+# Constants
+PROD: bool = False
+
+
+async def update_group(group_name: str) -> bool:
+    historic_config = (
+        await get_attributes(group_name, ["historic_configuration"])
+    )["historic_configuration"]
+
+    success = False
+
+    if PROD:
+        print(f"Working on {group_name} group!")
+        success = await update(
+            group_name,
+            {
+                "historic_configuration": [
+                    *historic_config[:-1],
+                    {
+                        **historic_config[-1],
+                        "has_drills": False,
+                    },
+                ],
+                "project_status": "DELETED",
+            },
+        )
+    else:
+        print(f"Working on {group_name} group!")
+        print(f"Actual data {historic_config}")
+    return success
+
+
+async def main() -> None:
+    groups_to_modify = [
+        "abington",
+        "apatin",
+        "arteaga",
+        "carpentersville",
+        "gillette",
+        "hiachson",
+        "morondava",
+        "nurch",
+        "pimgup",
+        "pucsole",
+        "rapla",
+        "trowpe",
+    ]
+    success = all(
+        await collect(
+            tuple(update_group(group_name) for group_name in groups_to_modify)
+        )
+    )
+    print(f"Success: {success}")
+
+
+if __name__ == "__main__":
+    execution_time = time.strftime(
+        "Execution Time:    %Y-%m-%d at %H:%M:%S UTC%Z"
+    )
+    run(main())
+    finalization_time = time.strftime(
+        "Finalization Time: %Y-%m-%d at %H:%M:%S UTC%Z"
+    )
+    print(f"{execution_time}\n{finalization_time}")
