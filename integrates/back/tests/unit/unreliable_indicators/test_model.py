85fb82bdc8157a0a82ec17c80c7b488bc1f50696
Andres Aguirre <aaguirre@fluidattacks.com>
2021-09-16 16:53:09 +0000

    integrates\test(back): #4329.432 indicator entity attrs
    - Add unit test to keep the model indicators attributes in order

--- integrates/back/tests/unit/unreliable_indicators/test_model.py
+++ integrates/back/tests/unit/unreliable_indicators/test_model.py
@@ -12,3 +12,9 @@ pytestmark = [
 def test_model_integrity_entity_names() -> None:
     entity_names = list(map(lambda key: str(key), model.ENTITIES.keys()))
     assert entity_names == sorted(entity_names)
+
+
+def test_model_integrity_entity_attrs() -> None:
+    for entity in model.ENTITIES.values():
+        entity_attrs = list(map(lambda key: str(key), entity["attrs"].keys()))
+        assert entity_attrs == sorted(entity_attrs)
