059ad20cba651f847a7510139afaface041d9814
Andres Cuberos <acuberos@fluidattacks.com>
2021-09-17 20:15:41 +0000

    integrates\fix(back): #5331.2 format and lint file
    - Run makes locally and fix issues since the pipeline is failing.

--- integrates/back/src/schedulers/machine_queue_all.py
+++ integrates/back/src/schedulers/machine_queue_all.py
@@ -1,6 +1,5 @@
-from aioextensions import collect
-from custom_types import (
-    Group,
+from aioextensions import (
+    collect,
 )
 from dataloaders import (
     Dataloaders,
@@ -10,7 +9,9 @@ from groups.domain import (
     get_active_groups,
     get_attributes,
 )
-from newutils.utils import get_key_or_fallback
+from newutils.utils import (
+    get_key_or_fallback,
+)
 import random
 from schedulers.common import (
     info,
@@ -27,8 +28,7 @@ async def main() -> None:
     groups: List[str] = await get_active_groups()
     dataloaders: Dataloaders = get_new_context()
     groups_data = await collect(
-        get_attributes(group, ["historic_configuration"])
-        for group in groups
+        get_attributes(group, ["historic_configuration"]) for group in groups
     )
     groups_confs = [
         group_data["historic_configuration"][-1] for group_data in groups_data
