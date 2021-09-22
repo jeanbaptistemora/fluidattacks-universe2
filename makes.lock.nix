7c66931ffe3580549b20831b58e5157c367aa600
Kevin Amado <kamado@fluidattacks.com>
2021-09-21 20:26:19 -0500

    common\refac(build): #5344.4 update makes
    - Update makes to its latest version

--- makes.lock.nix
+++ makes.lock.nix
@@ -1,6 +1,7 @@
 {
   makesSrc = builtins.fetchGit {
+    ref = "main";
     url = "https://github.com/fluidattacks/makes";
-    rev = "8dc3af7b2aae60dac3787b526393650d168d2caa";
+    rev = "068a36c17e4691dc6d78a2b924acfc4a084f6cf5";
   };
 }
