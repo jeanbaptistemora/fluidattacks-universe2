f107ba8f0db64b0f01bcad0e7fed303e49abba9d
Alejandro Salgado <asalgado@fluidattacks.com>
2021-09-22 13:58:19 -0500

    skims\refac(back): #5304.4 specify elem acces expr
    - Change __1__ parameter with argument keyword in match_ast call of
    element_access_expression for C#

--- skims/skims/sast_syntax_readers/c_sharp/element_access_expression.py
+++ skims/skims/sast_syntax_readers/c_sharp/element_access_expression.py
@@ -18,15 +18,13 @@ def reader(args: SyntaxReaderArgs) -> graph_model.SyntaxStepsLazy:
     if (object_id := match["__0__"]) and (
         bracket := match["bracketed_argument_list"]
     ):
-        match_bracket = g.match_ast(
-            args.graph, bracket, "__0__", "__1__", "__2__"
-        )
+        argument_id = g.match_ast_d(args.graph, bracket, "argument")
         yield graph_model.SyntaxStepArrayAccess(
             meta=graph_model.SyntaxStepMeta.default(
                 args.n_id,
                 [
                     args.generic(args.fork_n_id(object_id)),
-                    args.generic(args.fork_n_id(match_bracket["__1__"])),
+                    args.generic(args.fork_n_id(argument_id)),
                 ],
             ),
         )
