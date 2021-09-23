d8259f2e26a36cb4a45a62b6b36ff652ac297da3
Juan Echeverri <jecheverri@fluidattacks.com>
2021-09-22 19:26:12 -0500

    skims\refac(back): #5095.1 add model f381
    - Add F381.yaml to vulnerabilities
    - Add F381 entry to core_model.FindingEnum
    - Add F381 to json config files

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -1380,6 +1380,26 @@ class FindingEnum(Enum):
             user_interaction=cvss3_model.UserInteraction.required,
         ),
     )
+    F381: FindingMetadata = FindingMetadata.new(
+        auto_approve=True,
+        code="F381",
+        cwe=749,
+        execution_queue=ExecutionQueue.f014,
+        requirements=[266],
+        score=cvss3_model.Score(
+            attack_complexity=cvss3_model.AttackComplexity.high,
+            attack_vector=cvss3_model.AttackVector.network,
+            availability_impact=cvss3_model.AvailabilityImpact.none,
+            confidentiality_impact=cvss3_model.ConfidentialityImpact.none,
+            exploitability=cvss3_model.Exploitability.unproven,
+            integrity_impact=cvss3_model.IntegrityImpact.low,
+            privileges_required=cvss3_model.PrivilegesRequired.none,
+            remediation_level=cvss3_model.RemediationLevel.official_fix,
+            report_confidence=cvss3_model.ReportConfidence.reasonable,
+            severity_scope=cvss3_model.SeverityScope.unchanged,
+            user_interaction=cvss3_model.UserInteraction.required,
+        ),
+    )
 
 
 FINDING_ENUM_FROM_STR: Dict[str, FindingEnum] = {
