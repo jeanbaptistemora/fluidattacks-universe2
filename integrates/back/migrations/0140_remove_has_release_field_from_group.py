e6a5306aa4f230a23aeda62cd89ad4b4092c2350
Andres Aguirre <aaguirre@fluidattacks.com>
2021-09-24 17:52:44 +0000

    integrates\feat(back): #2880.1 remove has release migration
    - Add 0140_remove_has_release_field_from_group migration to remove
    the hasRelease attribute

---
+++ integrates/back/migrations/0140_remove_has_release_field_from_group.py
@@ -0,0 +1,52 @@
+# pylint: disable=invalid-name
+"""
+This migration removes the hasRelease field from groups
+since it is not used
+"""
+
+from aioextensions import (
+    collect,
+    run,
+)
+from boto3.dynamodb.conditions import (
+    Attr,
+)
+from custom_types import (
+    Group,
+)
+from dynamodb import (
+    operations_legacy as dynamodb_ops,
+)
+from groups import (
+    dal as groups_dal,
+)
+
+GROUP_TABLE = "FI_projects"
+
+
+async def remove_has_release_field_from_group(group: Group) -> bool:
+    success = await groups_dal.update(
+        group["project_name"], {"hasRelease": None}
+    )
+    print(f'Removed hasRelease from {group["project_name"]}')
+    return success
+
+
+async def main() -> None:
+    scan_attrs = {
+        "FilterExpression": Attr("hasRelease").exists(),
+        "ProjectionExpression": ",".join({"project_name"}),
+    }
+    groups = await dynamodb_ops.scan(GROUP_TABLE, scan_attrs)
+
+    success = all(
+        await collect(
+            [remove_has_release_field_from_group(group) for group in groups]
+        )
+    )
+
+    print(f"Success: {success}")
+
+
+if __name__ == "__main__":
+    run(main())
