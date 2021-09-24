3026e5ae81bf2029be59476be67682de6a9a4a97
Rafael Rodriguez <rrodriguez@fluidattacks.com>
2021-09-24 10:26:38 -0500

    integrates\refac(back): #5359.1 modify data report
    - Group evidences in folders by finding name
    - Append extension in case they don't have one

--- integrates/back/src/reports/report_types/data.py
+++ integrates/back/src/reports/report_types/data.py
@@ -10,6 +10,9 @@ from custom_types import (
 from db_model.findings.types import (
     Finding,
 )
+from magic import (
+    Magic,
+)
 import os
 from s3.operations import (
     download_file,
@@ -32,6 +35,7 @@ async def _append_evidences(
     *,
     directory: str,
     group: str,
+    findings_ord: List[Dict[str, FindingType]],
 ) -> None:
     target_folders: Dict[str, str] = {
         "": "evidences",
@@ -47,11 +51,68 @@ async def _append_evidences(
         _, extension = os.path.splitext(key)
 
         if extension in target_folders:
-            target_name = os.path.join(directory, target_folders[extension])
+            # Determine the folder name in which to save the evidence
+            finding_folder = ""
+            for finding in findings_ord:
+                if finding["findingId"] in key:
+                    finding_folder = finding["finding"]
+            target_name = os.path.join(
+                directory, target_folders[extension], finding_folder
+            )
             os.makedirs(target_name, exist_ok=True)
             target_name = os.path.join(target_name, os.path.basename(key))
             if not os.path.isdir(target_name):
                 await download_file(EVIDENCES_BUCKET, key, target_name)
+                # Append extension in case it doesn't have one
+                if extension == "":
+                    mime = Magic(mime=True)
+                    mime_type = mime.from_file(target_name)
+                    os.rename(
+                        target_name,
+                        target_name + f".{mime_type.split('/')[1]}",
+                    )
+
+
+async def _append_evidences_new(
+    *,
+    directory: str,
+    group: str,
+    findings_ord: Tuple[Finding, ...],
+) -> None:
+    target_folders: Dict[str, str] = {
+        "": "evidences",
+        ".csv": "compromised-records",
+        ".gif": "evidences",
+        ".jpg": "evidences",
+        ".png": "evidences",
+        ".txt": "compromised-records",
+    }
+
+    # Walk everything under the S3 evidences bucket and save relevant info
+    for key in await list_files(EVIDENCES_BUCKET, group):
+        _, extension = os.path.splitext(key)
+
+        if extension in target_folders:
+            # Determine the folder name in which to save the evidence
+            finding_folder = ""
+            for finding in findings_ord:
+                if finding.id in key:
+                    finding_folder = finding.title
+            target_name = os.path.join(
+                directory, target_folders[extension], finding_folder
+            )
+            os.makedirs(target_name, exist_ok=True)
+            target_name = os.path.join(target_name, os.path.basename(key))
+            if not os.path.isdir(target_name):
+                await download_file(EVIDENCES_BUCKET, key, target_name)
+                # Append extension in case it doesn't have one
+                if extension == "":
+                    mime = Magic(mime=True)
+                    mime_type = mime.from_file(target_name)
+                    os.rename(
+                        target_name,
+                        target_name + f".{mime_type.split('/')[1]}",
+                    )
 
 
 async def _append_pdf_report(
@@ -219,6 +280,7 @@ async def generate(
         await _append_evidences(
             directory=directory,
             group=group,
+            findings_ord=findings_ord,
         )
         return _encrypted_zip_file(
             passphrase=passphrase,
@@ -252,9 +314,10 @@ async def generate_new(
             group_name=group,
             passphrase=passphrase,
         )
-        await _append_evidences(
+        await _append_evidences_new(
             directory=directory,
             group=group,
+            findings_ord=findings_ord,
         )
         return _encrypted_zip_file(
             passphrase=passphrase,
