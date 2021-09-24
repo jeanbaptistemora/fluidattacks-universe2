e21c51cd1d9de3155a6c51d3547a94486a3df84d
Kevin Amado <kamado@fluidattacks.com>
2021-09-24 14:47:09 -0500

    skims\refac(build): #5364.8 migrate to makes
    - Migrate this job to makes keeping compatibility with
      both build systems

--- makes.lock.nix
+++ makes.lock.nix
@@ -2,6 +2,6 @@
   makesSrc = builtins.fetchGit {
     ref = "main";
     url = "https://github.com/fluidattacks/makes";
-    rev = "619df93d26b5cef1994f82255349176ec4997a5c";
+    rev = "4a6ed2304fb8ea457340c874899f2aa060a88b8e";
   };
 }
--- makes/foss/modules/skims/compute-on-aws-batch/makes.nix
+++ makes/foss/modules/skims/compute-on-aws-batch/makes.nix
@@ -3,18 +3,28 @@
 }:
 {
   computeOnAwsBatch = {
-    skimsOwaspBenchmarkAndUpload = {
-      allowDuplicates = false;
+    skimsOwaspBenchmarkAndUpload = rec {
       attempts = 1;
       attemptDurationSeconds = 86400;
       command = [ "m" "/skims/owasp-benchmark-and-upload" ];
       definition = "makes";
       environment = [ "PRODUCT_API_TOKEN" ];
-      memory = 4 * 1800;
+      memory = vcpus * 1800;
       name = "skims-benchmark";
       queue = "dedicated_later";
       setup = [ outputs."/secretsForAwsFromEnv/skimsProd" ];
       vcpus = 4;
+    };
+    skimsProcessGroup = rec {
+      attempts = 1;
+      attemptDurationSeconds = 86400;
+      command = [ "m" "/skims/process-group" ];
+      definition = "makes";
+      environment = [ "PRODUCT_API_TOKEN" ];
+      memory = vcpus * 1800;
+      queue = null;
+      setup = [ outputs."/secretsForAwsFromEnv/skimsProd" ];
+      vcpus = 1;
     };
   };
 }
