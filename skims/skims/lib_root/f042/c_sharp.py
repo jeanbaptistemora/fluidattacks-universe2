123ac129b01f990bab7f8eaf18b24f4165f2f93d
Juan Echeverri <jecheverri@fluidattacks.com>
2021-09-27 06:53:30 -0500

    skims\refac(back): #5325.36 refac f042
    - Refac f042 to use sysntax step instead iterate the tree
    - no-deltas-test

--- skims/skims/lib_root/f042/c_sharp.py
+++ skims/skims/lib_root/f042/c_sharp.py
@@ -5,80 +5,64 @@ from model import (
 from sast.query import (
     get_vulnerabilities_from_n_ids,
 )
-from typing import (
-    Set,
-)
-from utils import (
-    graph as g,
+from sast_syntax_readers.utils_generic import (
+    get_dependencies,
 )
 
 
-def c_sharp_check_cookie_security(
-    shard: graph_model.GraphShard, checks: Set[str], name_var: str
-) -> int:
-    count_checks = 0
-    for syntax_steps in shard.syntax.values():
-        *dependencies, syntax_step = syntax_steps
-
-        if syntax_step.type != "SyntaxStepAssignment":
-            continue
-
-        # this funcion search for var.attribute = true
-        # so it should only have 1 dependency, the boolean value
-        if len(dependencies) != 1:
-            continue
-
-        dep = dependencies[0]
-        if dep.type != "SyntaxStepLiteral" or dep.value != "true":
-            continue
-
-        var, attribute = syntax_step.var.split(".", maxsplit=1)
-        if var == name_var and attribute in checks:
-            count_checks += 1
-
-    return count_checks
-
-
-def insecurely_generated_cookies(
-    graph_db: graph_model.GraphDB,
-) -> core_model.Vulnerabilities:
-
+def insecure_props(shard: graph_model.GraphShard, name_var: str) -> bool:
     security_props = {
         "HttpOnly",
         "Secure",
     }
 
+    count_checks = 0
+    for syntax_steps in shard.syntax.values():
+        for index, syntax_step in enumerate(syntax_steps):
+            if (
+                isinstance(syntax_step, graph_model.SyntaxStepAssignment)
+                and syntax_step.var.split(".")[0] == name_var
+                and syntax_step.var.split(".")[1] in security_props
+                and len(get_dependencies(index, syntax_steps)) > 0
+                and isinstance(
+                    get_dependencies(index, syntax_steps)[0],
+                    graph_model.SyntaxStepLiteral,
+                )
+            ):
+                if get_dependencies(index, syntax_steps)[0].value == "true":
+                    count_checks += 1
+                elif get_dependencies(index, syntax_steps)[0].value == "false":
+                    return True
+    if count_checks > 1:
+        return False
+    return True
+
+
+def insecurely_generated_cookies(
+    graph_db: graph_model.GraphDB,
+) -> core_model.Vulnerabilities:
     def n_ids() -> graph_model.GraphShardNodes:
         for shard in graph_db.shards_by_language(
             graph_model.GraphShardMetadataLanguage.CSHARP,
         ):
-            for member in g.filter_nodes(
-                shard.graph,
-                nodes=shard.graph.nodes,
-                predicate=g.pred_has_labels(
-                    label_type="object_creation_expression"
-                ),
-            ):
-                match_object_creation = g.match_ast(
-                    shard.graph, member, "__0__"
-                )
-                name_var = shard.graph.nodes[
-                    g.match_ast(
-                        shard.graph, g.pred(shard.graph, member, 2)[1], "__0__"
-                    )["__0__"]
-                ].get("label_text")
-                if (
-                    shard.graph.nodes[match_object_creation["__1__"]].get(
-                        "label_text"
-                    )
-                    == "HttpCookie"
-                ) and (
-                    c_sharp_check_cookie_security(
-                        shard, security_props, name_var
-                    )
-                    != len(security_props)
-                ):
-                    yield shard, member
+            for syntax_steps in shard.syntax.values():
+                for index, syntax_step in enumerate(syntax_steps):
+                    if (
+                        isinstance(
+                            syntax_step, graph_model.SyntaxStepDeclaration
+                        )
+                        and len(get_dependencies(index, syntax_steps)) > 0
+                        and isinstance(
+                            get_dependencies(index, syntax_steps)[0],
+                            graph_model.SyntaxStepObjectInstantiation,
+                        )
+                        and get_dependencies(index, syntax_steps)[
+                            0
+                        ].object_type
+                        == "HttpCookie"
+                        and insecure_props(shard, syntax_step.var)
+                    ):
+                        yield shard, syntax_step.meta.n_id
 
     return get_vulnerabilities_from_n_ids(
         cwe=("614",),
