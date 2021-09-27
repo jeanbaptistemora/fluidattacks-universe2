5f58f44e12e126fba0cd44e3956da2cef491c9ff
Alejandro Salgado <asalgado@fluidattacks.com>
2021-09-27 11:33:12 -0500

    skims\refac(back): #5325.37 add methods transform
    - Add methods to handle text nodes in utils/graph/transformation.py

--- skims/skims/utils/graph/transformation.py
+++ skims/skims/utils/graph/transformation.py
@@ -1,10 +1,12 @@
-from model import (
-    graph_model,
+from model.graph_model import (
+    Graph,
 )
 from typing import (
     cast,
     Iterable,
+    List,
     Optional,
+    Tuple,
 )
 from utils import (
     graph as g,
@@ -12,7 +14,7 @@ from utils import (
 
 
 def get_identifiers_ids(
-    graph: graph_model.Graph,
+    graph: Graph,
     n_id: str,
     nested_key: str,
 ) -> Iterable[str]:
@@ -39,23 +41,15 @@ def get_identifiers_ids(
 
 
 def get_base_identifier_id(
-    graph: graph_model.Graph,
+    graph: Graph,
     n_id: str,
     nested_key: str,
 ) -> Optional[str]:
     return next(iter(get_identifiers_ids(graph, n_id, nested_key)), None)
 
 
-def node_to_str(graph: graph_model.Graph, n_id: str) -> str:
-    node = graph.nodes[n_id]
-    result_str = node["label_text"] if "label_text" in node else ""
-    for c_id in g.adj_ast(graph, n_id):
-        result_str += node_to_str(graph, c_id)
-    return result_str
-
-
 def get_identifiers_ids_js(
-    graph: graph_model.Graph,
+    graph: Graph,
     n_id: str,
     nested_key: str,
 ) -> Iterable[str]:
@@ -87,3 +81,58 @@ def get_identifiers_ids_js(
 
     if this := match_access["this"]:
         yield this.pop()
+
+
+def get_node_text(graph: Graph, n_id: str) -> str:
+    return graph.nodes[n_id].get("label_text", "")
+
+
+def iter_childs(graph: Graph, n_id: str) -> Iterable[str]:
+    for c_id in g.adj_ast(graph, n_id):
+        yield from iter_childs(graph, c_id)
+    yield n_id
+
+
+def lazy_text_childs(graph: Graph, n_id: str) -> Iterable[str]:
+    for c_id in iter_childs(graph, n_id):
+        if "label_text" in graph.nodes[c_id]:
+            yield c_id
+
+
+def lazy_childs_text(graph: Graph, n_id: str) -> Iterable[str]:
+    for c_id in lazy_text_childs(graph, n_id):
+        yield graph.nodes[c_id]["label_text"]
+
+
+def lazy_childs_text_ids(graph: Graph, n_id: str) -> Iterable[Tuple[str, str]]:
+    for c_id in lazy_text_childs(graph, n_id):
+        yield get_node_text(graph, c_id), c_id
+
+
+def lazy_n_ids_to_str(graph: Graph, n_ids: Iterable[str]) -> Iterable[str]:
+    for n_id in n_ids:
+        yield get_node_text(graph, n_id)
+
+
+def get_childs(graph: Graph, n_id: str) -> List[str]:
+    return list(iter_childs(graph, n_id))
+
+
+def get_text_childs(graph: Graph, n_id: str) -> List[str]:
+    return list(lazy_text_childs(graph, n_id))
+
+
+def get_childs_text(graph: Graph, n_id: str) -> List[str]:
+    return list(lazy_childs_text(graph, n_id))
+
+
+def get_childs_text_with_ids(graph: Graph, n_id: str) -> List[Tuple[str, str]]:
+    return list(lazy_childs_text_ids(graph, n_id))
+
+
+def n_ids_to_str(graph: Graph, n_ids: Iterable[str], sep: str = "") -> str:
+    return sep.join(lazy_n_ids_to_str(graph, n_ids))
+
+
+def node_to_str(graph: Graph, n_id: str, sep: str = "") -> str:
+    return sep.join(lazy_childs_text(graph, n_id))
