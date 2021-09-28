e9f4632d85ca6096a5d2195c132f6ef35e473781
Alejandro Salgado <asalgado@fluidattacks.com>
2021-09-28 13:35:05 -0500

    skims\refac(back): #5325.42 redef var decl cs
    - Redefine variable declaration syntax reader for C#

--- skims/skims/sast_syntax_readers/c_sharp/variable_declaration.py
+++ skims/skims/sast_syntax_readers/c_sharp/variable_declaration.py
@@ -14,18 +14,18 @@ from utils.graph.text_nodes import (
 
 
 def reader(args: SyntaxReaderArgs) -> graph_model.SyntaxStepsLazy:
-    match_declaration = g.match_ast_group(
+    var_type_id = args.graph.nodes[args.n_id]["label_field_type"]
+
+    var_declarators = g.match_ast_group_d(
         args.graph,
         args.n_id,
         "variable_declarator",
-        "__0__",
     )
-    var_type_id = match_declaration["__0__"]
-    var_declarators = match_declaration["variable_declarator"]
+
     if not var_declarators:
         raise MissingCaseHandling(args)
 
-    for var_declarator in var_declarators or set():
+    for var_declarator in var_declarators:
         match_declarator = g.match_ast(
             args.graph,
             var_declarator,
@@ -33,6 +33,7 @@ def reader(args: SyntaxReaderArgs) -> graph_model.SyntaxStepsLazy:
             "equals_value_clause",
         )
         type_identifier = match_declarator["identifier"]
+
         deps_src = []
         if equals_value := match_declarator["equals_value_clause"]:
             match_equals = g.match_ast(
@@ -44,15 +45,8 @@ def reader(args: SyntaxReaderArgs) -> graph_model.SyntaxStepsLazy:
             if match_equals["="] and (deps_id := match_equals["__0__"]):
                 deps_src = [args.generic(args.fork_n_id(deps_id))]
 
-        if args.graph.nodes[var_type_id]["label_type"] == "qualified_name":
-            var_type = node_to_str(args.graph, var_type_id)
-        elif label_text := args.graph.nodes[var_type_id].get("label_text"):
-            var_type = label_text
-        else:
-            var_type = args.graph.nodes[var_type_id]["label_type"]
-
         yield graph_model.SyntaxStepDeclaration(
             meta=graph_model.SyntaxStepMeta.default(args.n_id, deps_src),
             var=args.graph.nodes[type_identifier]["label_text"],
-            var_type=var_type,
+            var_type=node_to_str(args.graph, var_type_id),
         )
