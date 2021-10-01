518a6bcbab6554fe3074890557e6f29ef4e8ae83
Juan Echeverri <jecheverri@fluidattacks.com>
2021-10-01 15:41:41 -0500

    skims\feat(back): #5153.57 add model f091
    - Add F091.yaml to vulnerabilities
    - Add F091 entry to core_model.FindingEnum
    - Add F091 to json config files

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -940,6 +940,26 @@ class FindingEnum(Enum):
             user_interaction=cvss3_model.UserInteraction.none,
         ),
     )
+    F091: FindingMetadata = FindingMetadata.new(
+        auto_approve=True,
+        code="F091",
+        cwe=117,
+        execution_queue=ExecutionQueue.f014,
+        requirements=[80, 173],
+        score=cvss3_model.Score(
+            attack_complexity=cvss3_model.AttackComplexity.low,
+            attack_vector=cvss3_model.AttackVector.network,
+            availability_impact=cvss3_model.AvailabilityImpact.none,
+            confidentiality_impact=cvss3_model.ConfidentialityImpact.low,
+            exploitability=cvss3_model.Exploitability.poc,
+            integrity_impact=cvss3_model.IntegrityImpact.none,
+            privileges_required=cvss3_model.PrivilegesRequired.none,
+            remediation_level=cvss3_model.RemediationLevel.unavailable,
+            report_confidence=cvss3_model.ReportConfidence.unknown,
+            severity_scope=cvss3_model.SeverityScope.unchanged,
+            user_interaction=cvss3_model.UserInteraction.none,
+        ),
+    )
     F092: FindingMetadata = FindingMetadata.new(
         auto_approve=False,
         code="F092",
