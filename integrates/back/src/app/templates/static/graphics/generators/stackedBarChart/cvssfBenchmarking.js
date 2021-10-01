eb8b935fb9a2a4bf1dcd4301d06a5f7e3e11822a
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-10-01 10:08:46 -0500

    integrates\refac(back): #5271.11 remove unnecessary code
    - remove not longer necessary code from `cvssf_benchmarking`
    js static generator

--- integrates/back/src/app/templates/static/graphics/generators/stackedBarChart/cvssfBenchmarking.js
+++ integrates/back/src/app/templates/static/graphics/generators/stackedBarChart/cvssfBenchmarking.js
@@ -3,43 +3,6 @@
 const defaultPaddingRatio = 0.05;
 
 function render(dataDocument, height, width) {
-  if (dataDocument.normalizedToolTip && dataDocument.totalBar) {
-    const percentage = 100;
-    const { totalBar } = dataDocument;
-
-    dataDocument.tooltip.format.value = (datum, _r, _id, index) => {
-      const tooltipValue = `${ parseFloat((datum / totalBar[index] * percentage).toFixed(2)) } %`;
-
-      return tooltipValue;
-    };
-
-    dataDocument.data.labels.format = {
-      Closed: (datum, _id, index) => {
-        const labelValue = `${ parseFloat((datum / totalBar[index] * percentage).toFixed(2)) } %`;
-
-        return labelValue;
-      },
-    };
-  }
-
-  if (dataDocument.percentageValues) {
-    const { percentageValues } = dataDocument;
-
-    dataDocument.tooltip.format.value = (_datum, _r, id, index) => {
-      const tooltipValue = `${ parseFloat(percentageValues[id][index]) } %`;
-
-      return tooltipValue;
-    };
-
-    dataDocument.data.labels.format = {
-      Closed: (_datum, id, index) => {
-        const labelValue = `${ parseFloat(percentageValues[id][index]) } %`;
-
-        return labelValue;
-      },
-    };
-  }
-
   if (dataDocument.percentageValues && dataDocument.maxPercentageValues) {
     const { percentageValues, maxPercentageValues } = dataDocument;
 
