bdd748e081e23b556d9ca0bb6066c385951a53d4
Alejandro Salgado <asalgado@fluidattacks.com>
2021-10-01 16:48:35 -0500

    skims\refac(back): #5304.8 redef cs assign expr
    - no-deltas-test
    - Redefine syntax reader for assignment expression in C#

--- skims/skims/sast_syntax_readers/c_sharp/assignment_expression.py
+++ skims/skims/sast_syntax_readers/c_sharp/assignment_expression.py
@@ -2,83 +2,23 @@ from model import (
     graph_model,
 )
 from sast_syntax_readers.types import (
-    MissingCaseHandling,
     SyntaxReaderArgs,
 )
-from typing import (
-    Iterator,
-    Union,
-)
-from utils import (
-    graph as g,
+from utils.graph.text_nodes import (
+    node_to_str,
 )
 
 
 def reader(args: SyntaxReaderArgs) -> graph_model.SyntaxStepsLazy:
-    assignments = tuple(_expression(args, args.n_id))
-    last_assignment = assignments[-1]
-    for var_identifier_id in assignments[:-1]:
-        yield graph_model.SyntaxStepAssignment(
-            meta=last_assignment.meta,  # type: ignore
-            var=args.graph.nodes[var_identifier_id]["label_text"],
-        )
-    yield last_assignment
+    left_id = args.graph.nodes[args.n_id]["label_field_left"]
+    right_id = args.graph.nodes[args.n_id]["label_field_right"]
 
-
-def _expression(
-    args: SyntaxReaderArgs, expression_id: str
-) -> Iterator[Union[str, graph_model.SyntaxStepAssignment]]:
-    assignment_operators = {
-        "=",
-        "+=",
-        "-=",
-        "*=",
-        "/=",
-        "%=",
-        "&=",
-        "^=",
-        "|=",
-        "<<=",
-        ">>=",
-        ">>>=",
-    }
-    match = g.match_ast(
-        args.graph,
-        expression_id,
-        "__0__",
-        "__1__",
-        "__2__",
+    yield graph_model.SyntaxStepAssignment(
+        meta=graph_model.SyntaxStepMeta.default(
+            args.n_id,
+            [
+                args.generic(args.fork_n_id(right_id)),
+            ],
+        ),
+        var=node_to_str(args.graph, left_id),
     )
-    if (
-        (var_identifier_id := match["__0__"])
-        and (op_id := match["__1__"])
-        and (args.graph.nodes[op_id]["label_text"] in assignment_operators)
-        and (src_id := match["__2__"])
-    ):
-        if args.graph.nodes[src_id]["label_type"] == "assignment_expression":
-            yield var_identifier_id
-            yield from _expression(args, src_id)
-        else:
-            identifier_type = args.graph.nodes[var_identifier_id]["label_type"]
-            if identifier_type == "identifier":
-                var_identifier_str = args.graph.nodes[var_identifier_id][
-                    "label_text"
-                ]
-            elif identifier_type == "member_access_expression":
-                var_identifier_str = g.text_nodes.node_to_str(
-                    args.graph, var_identifier_id
-                )
-            else:
-                raise MissingCaseHandling(args)
-
-            yield graph_model.SyntaxStepAssignment(
-                meta=graph_model.SyntaxStepMeta.default(
-                    args.n_id,
-                    [
-                        args.generic(args.fork_n_id(src_id)),
-                    ],
-                ),
-                var=var_identifier_str,
-            )
-    else:
-        raise MissingCaseHandling(args)
