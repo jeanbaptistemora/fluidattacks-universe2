55a7a68954c6bbbbcc8d05ba610295233b5bbe58
Daniel Murcia <dmurcia@fluidattacks.com>
2021-10-05 14:15:49 -0500

    observes\refac(back): #5409.9 split module
    - split pure iter module

---
+++ observes/common/purity/purity/v1/_pure_iter/__init__.py
@@ -0,0 +1,11 @@
+from purity.v1._pure_iter.factory import (
+    PureIterFactory,
+)
+from purity.v1._pure_iter.obj import (
+    PureIter,
+)
+
+__all__ = [
+    "PureIter",
+    "PureIterFactory",
+]
---
+++ observes/common/purity/purity/v1/_pure_iter/obj.py
@@ -0,0 +1,59 @@
+from __future__ import (
+    annotations,
+)
+
+from collections import (
+    deque,
+)
+from dataclasses import (
+    dataclass,
+)
+from purity.v1._frozen import (
+    FrozenList,
+)
+from purity.v1._patch import (
+    Patch,
+)
+from returns.io import (
+    IO,
+)
+from returns.primitives.hkt import (
+    SupportsKind1,
+)
+from typing import (
+    Callable,
+    Iterable,
+    Iterator,
+    TypeVar,
+    Union,
+)
+
+_I = TypeVar("_I")
+_R = TypeVar("_R")
+
+
+@dataclass(frozen=True)
+class _PureIter(
+    SupportsKind1["_PureIter[_I]", _I],
+):
+    _iter_obj: Patch[Callable[[], Iterable[_I]]]
+
+
+class PureIter(_PureIter[_I]):
+    def __init__(self, obj: _PureIter[_I]):
+        super().__init__(obj._iter_obj)
+
+    def __iter__(self) -> Iterator[_I]:
+        return iter(self._iter_obj.unwrap())
+
+    def map_each(self, function: Callable[[_I], _R]) -> PureIter[_R]:
+        draft = _PureIter(Patch(lambda: map(function, self)))
+        return PureIter(draft)
+
+    @staticmethod
+    def consume(p_iter: PureIter[IO[None]]) -> IO[None]:
+        deque(p_iter, maxlen=0)
+        return IO(None)
+
+
+Mappable = Union[FrozenList[_I], PureIter[_I]]
