ce4ef5d0f5af86c5f80e9ef1f509883e1e7c67f4
Kevin Amado <kamado@fluidattacks.com>
2021-10-05 17:33:58 -0500

    common\refac(build): #5408 migrate to makes
    - Speed: 31048 loc / 6 days = 5174 loc/day
    - TODO: 17733 loc
    - ETA: 17733 / 5174 = 3 days

--- makes.lock.nix
+++ makes.lock.nix
@@ -2,6 +2,6 @@
   makesSrc = builtins.fetchGit {
     ref = "main";
     url = "https://github.com/fluidattacks/makes";
-    rev = "3159fe0ab88b9a26a268c443c71de68725a93415";
+    rev = "2f18403a393cdebefe0a7bffb6dcb3047583ffc7";
   };
 }
