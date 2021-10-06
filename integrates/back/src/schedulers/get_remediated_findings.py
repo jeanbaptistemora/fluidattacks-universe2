35bb3505e466aff14296a5804cf839417a92475f
Jonathan Mesa <jmesa@fluidattacks.com>
2021-10-06 21:39:02 +0000

    integrates\refac(back): #4329.488 replace scheduler
    - replace old scheduler get_remediated_findings
    with new one using the new finding entity

--- integrates/back/src/schedulers/get_remediated_findings.py
+++ integrates/back/src/schedulers/get_remediated_findings.py
@@ -20,13 +20,13 @@ from findings import (
 from groups import (
     domain as groups_domain,
 )
+from itertools import (
+    chain,
+)
 import logging
 import logging.config
 from mailer import (
     findings as findings_mail,
-)
-from newutils.utils import (
-    get_key_or_fallback,
 )
 from settings import (
     LOGGING,
@@ -44,37 +44,37 @@ LOGGER = logging.getLogger(__name__)
 
 
 async def get_remediated_findings() -> None:
-    """Summary mail send with findings that have not been verified yet."""
+    """Summary mail send with findings that have not been verified yet"""
     active_groups = await groups_domain.get_active_groups()
-    findings = []
-    pending_verification_findings = await collect(
-        findings_domain.get_pending_verification_findings(
-            get_new_context(), group
+    findings = tuple(
+        chain.from_iterable(
+            await collect(
+                findings_domain.get_pending_verification_findings_new(
+                    get_new_context(), group
+                )
+                for group in active_groups
+            )
         )
-        for group in active_groups
     )
-    for group_findings in pending_verification_findings:
-        findings += group_findings
 
     if findings:
         try:
             mail_to = [FI_MAIL_PROJECTS]
-            context: MailContentType = {"findings": [], "total": 0}
+            mail_context: MailContentType = {"findings": [], "total": 0}
             for finding in findings:
-                cast(List[Dict[str, str]], context["findings"]).append(
+                cast(List[Dict[str, str]], mail_context["findings"]).append(
                     {
-                        "finding_name": finding["finding"],
+                        "finding_name": finding.title,
                         "finding_url": (
-                            f"{BASE_URL}/groups/"
-                            f"{str.lower(str(get_key_or_fallback(finding)))}/"
-                            f'{finding["finding_id"]}/description'
+                            f"{BASE_URL}/groups/{finding.group_name}/"
+                            f"{finding.id}/description"
                         ),
-                        "group": str.upper(str(get_key_or_fallback(finding))),
+                        "group": finding.group_name,
                     }
                 )
-            context["total"] = len(findings)
+            mail_context["total"] = len(findings)
             scheduler_send_mail(
-                findings_mail.send_mail_new_remediated, mail_to, context
+                findings_mail.send_mail_new_remediated, mail_to, mail_context
             )
         except (TypeError, KeyError) as ex:
             LOGGER.exception(ex, extra={"extra": locals()})
