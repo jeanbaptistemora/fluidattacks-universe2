a174af70635a5181a27d4dde79f0ac65fc173250
Daniel Murcia <dmurcia@fluidattacks.com>
2021-10-07 13:57:59 -0500

    observes\feat(back): #5310.26 total pages query
    - add total pages query

--- observes/singer/tap_announcekit/tap_announcekit/streams/posts/_queries.py
+++ observes/singer/tap_announcekit/tap_announcekit/streams/posts/_queries.py
@@ -19,7 +19,7 @@ from tap_announcekit.streams.posts._objs import (
 class PostQuery:
     post: PostId
 
-    def _select_fields(self, query: Query) -> IO[None]:
+    def _select_fields(self, query: Query) -> IO[Query]:
         proj = query.raw.post(
             project_id=self.post.proj.proj_id, post_id=self.post.post_id
         )
@@ -27,12 +27,11 @@ class PostQuery:
         for attr, _ in Post.__annotations__.items():
             _attr = "id" if attr == "obj_id" else attr
             getattr(proj, _attr)()
-        return IO(None)
+        return IO(proj)
 
     def query(self) -> IO[Query]:
         query = ApiClient.new_query()
-        query.bind(self._select_fields)
-        return query
+        return query.bind(self._select_fields)
 
 
 @dataclass(frozen=True)
@@ -40,15 +39,29 @@ class PostIdsQuery:
     proj: ProjectId
     page: int
 
-    def _select_fields(self, query: Query) -> IO[None]:
+    def _select_fields(self, query: Query) -> IO[Query]:
         proj = query.raw.posts(project_id=self.proj.proj_id, page=self.page)
         proj.list().id()
         proj.list().project_id()
         proj.count()
         proj.pages()
-        return IO(None)
+        return IO(proj)
 
     def query(self) -> IO[Query]:
         query = ApiClient.new_query()
-        query.bind(self._select_fields)
-        return query
+        return query.bind(self._select_fields)
+
+
+@dataclass(frozen=True)
+class TotalPagesQuery:
+    proj: ProjectId
+
+    def _select_fields(self, query: Query) -> IO[Query]:
+        proj = query.raw.posts(project_id=self.proj.proj_id, page=0)
+        proj.count()
+        proj.pages()
+        return IO(proj)
+
+    def query(self) -> IO[Query]:
+        query = ApiClient.new_query()
+        return query.bind(self._select_fields)
