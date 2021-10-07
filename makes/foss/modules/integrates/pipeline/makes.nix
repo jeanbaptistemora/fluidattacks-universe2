85e9b544e096ce04f37da5152e838e751b780e09
Andres Aguirre <aaguirre@fluidattacks.com>
2021-10-07 19:55:51 +0000

    integrates\refac(build): #4329.496 deactivate unit tests
    - Deactivate the old finding unit tests from the ci

--- makes/foss/modules/integrates/pipeline/makes.nix
+++ makes/foss/modules/integrates/pipeline/makes.nix
@@ -308,16 +308,6 @@ in
         ])
         ++ [
         {
-          output = "/integrates/back/test/unit";
-          gitlabExtra = gitlabTest // {
-            artifacts = {
-              name = "coverage_xml_$CI_COMMIT_REF_NAME_$CI_COMMIT_SHA";
-              paths = [ "integrates/coverage.xml" ];
-              expire_in = "1 week";
-            };
-          };
-        }
-        {
           args = [ "migration" ];
           output = "/integrates/back/test/unit";
           gitlabExtra = gitlabTest // {
@@ -367,7 +357,7 @@ in
           output = "/integrates/coverage";
           gitlabExtra = {
             needs = [
-              "/integrates/back/test/unit"
+              "/integrates/back/test/unit__migration"
               "/integrates/front/test"
               "/integrates/mobile/test"
             ];
