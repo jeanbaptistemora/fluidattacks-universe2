823b30ca5cb1c806bb8502c3866a709423a2f1f9
Kevin Amado <kamado@fluidattacks.com>
2021-10-11 10:20:18 -0500

    sorts\refac(build): #5408 migrate to makes
    - Speed: 6201 loc / 12 days = 516 loc/day
    - TODO: 12883 loc
    - ETA: 12883 / 516 = 24 days

--- makes/foss/units/sorts/main.nix
+++ makes/foss/units/sorts/main.nix
@@ -1,1 +1,15 @@
-{ inputs, ... }: inputs.product.sorts
+{ inputs
+, makeScript
+, ...
+}:
+makeScript {
+  name = "sorts";
+  searchPaths = {
+    source = [
+      (inputs.legacy.importUtility "aws")
+      (inputs.legacy.importUtility "sops")
+      inputs.product.sorts-config-runtime
+    ];
+  };
+  entrypoint = ./entrypoint.sh;
+}
