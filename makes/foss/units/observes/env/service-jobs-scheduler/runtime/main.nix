ec0b581fbe0b76f8260c85df22d352d01bc2c499
Kevin Amado <kamado@fluidattacks.com>
2021-10-11 13:38:58 -0500

    observes\refac(build): #5408 migrate to makes
    - Speed: 7541 loc / 12 days = 628 loc/day
    - TODO: 11906 loc
    - ETA: 11906 / 628 = 18 days

---
+++ makes/foss/units/observes/env/service-jobs-scheduler/runtime/main.nix
@@ -0,0 +1,39 @@
+{ inputs
+, makePythonPypiEnvironment
+, makeTemplate
+, projectPath
+, ...
+}:
+let
+  self = projectPath "/observes/services/jobs_scheduler";
+in
+makeTemplate {
+  name = "observes-env-service-jobs-scheduler-runtime";
+  searchPaths = {
+    bin = [
+      inputs.product.observes-job-batch-stability
+      inputs.product.observes-scheduled-on-aws-bugsnag-etl
+      inputs.product.observes-scheduled-on-aws-checkly-etl
+      inputs.product.observes-scheduled-on-aws-code-etl-amend
+      inputs.product.observes-scheduled-on-aws-code-etl-mirror
+      inputs.product.observes-scheduled-on-aws-code-etl-upload
+      inputs.product.observes-scheduled-on-aws-delighted-etl
+      inputs.product.observes-scheduled-on-aws-dynamodb-forces-etl
+      inputs.product.observes-scheduled-on-aws-dynamodb-integrates-etl
+      inputs.product.observes-scheduled-on-aws-formstack-etl
+      inputs.product.observes-scheduled-on-aws-gitlab-etl-challenges
+      inputs.product.observes-scheduled-on-aws-gitlab-etl-default
+      inputs.product.observes-scheduled-on-aws-gitlab-etl-product
+      inputs.product.observes-scheduled-on-aws-gitlab-etl-services
+    ];
+    pythonMypy = [ self ];
+    pythonPackage = [ self ];
+    source = [
+      (makePythonPypiEnvironment {
+        name = "observes-env-service-jobs-scheduler-runtime";
+        sourcesYaml = ./pypi-sources.yaml;
+      })
+      inputs.product.observes-env-utils-logger-runtime
+    ];
+  };
+}
