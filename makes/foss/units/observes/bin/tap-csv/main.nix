2c447e29e3768b385f601bc030de9fbea707e350
John Perez <jperez@fluidattacks.com>
2021-10-12 17:25:37 -0500

    observes\refac(build): #5408 migrate to makes
    - Speed: 7683 loc / 13 days = 591 loc/day
    - TODO: 11699 loc
    - ETA: 11699 / 591 = 19 days

---
+++ makes/foss/units/observes/bin/tap-csv/main.nix
@@ -0,0 +1,15 @@
+{ inputs
+, makeScript
+, outputs
+, ...
+}:
+makeScript {
+  entrypoint = "import_and_run tap_csv.cli main";
+  searchPaths = {
+    source = [
+      outputs."/observes/common/import-and-run"
+      inputs.product.observes-env-tap-csv-runtime
+    ];
+  };
+  name = "observes-bin-tap-csv";
+}
