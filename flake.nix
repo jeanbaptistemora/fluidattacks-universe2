2d6e20aed136b591c16d18c5e6e164ee35843bed
Kevin Amado <kamado@fluidattacks.com>
2021-10-12 10:57:18 -0500

    observes\refac(build): #5408 migrate to makes
    - Speed: 7648 loc / 13 days = 588 loc/day
    - TODO: 11715 loc
    - ETA: 11715 / 588 = 19 days

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Fluid Attacks, We hack your software!";
   inputs = {
     flakeCompat = { url = "github:edolstra/flake-compat"; flake = false; };
-    makes = { url = "https://github.com/fluidattacks/makes/archive/4789fb8e6f2799ef9ecf33f9701d58ce2571b86f.tar.gz"; flake = false; };
+    makes = { url = "https://github.com/fluidattacks/makes/archive/d4a5025ab4ae17a498f118bf26dece6b77867e35.tar.gz"; flake = false; };
     nixpkgsSource = { url = "https://github.com/nixos/nixpkgs/archive/932941b79c3dbbef2de9440e1631dfec43956261.tar.gz"; flake = false; };
     nixpkgsSource2 = { url = "https://github.com/nixos/nixpkgs/archive/7138a338b58713e0dea22ddab6a6785abec7376a.tar.gz"; flake = false; };
     nixpkgsSource3 = { url = "https://github.com/nixos/nixpkgs/archive/a1d64d9419422ae9779ab5cada5828127a24e100.tar.gz"; flake = false; };
