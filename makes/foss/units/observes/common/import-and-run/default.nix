7bfcc64ca3deeaf10a9a08cd14dd0f35ed8b7129
Kevin Amado <kamado@fluidattacks.com>
2021-10-12 09:22:56 -0500

    observes\refac(build): #5408 migrate to makes
    - Speed: 7612 loc / 13 days = 585 loc/day
    - TODO: 11805 loc
    - ETA: 11805 / 585 = 20 days

--- makes/foss/units/observes/common/import-and-run/default.nix
+++
@@ -1,8 +0,0 @@
-{ makeTemplate
-, path
-, ...
-}:
-makeTemplate {
-  name = "observes-generic-runner";
-  template = path "/makes/foss/units/observes/common/import-and-run/template.sh";
-}
---
+++ makes/foss/units/observes/common/import-and-run/main.nix
@@ -0,0 +1,7 @@
+{ makeTemplate
+, ...
+}:
+makeTemplate {
+  name = "observes-generic-runner";
+  template = ./template.sh;
+}
