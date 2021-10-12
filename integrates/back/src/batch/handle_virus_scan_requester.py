3da9fab983e31cc00dd28e5bf83f5c2d14fddbd8
John Perez <jperez@fluidattacks.com>
2021-10-12 15:51:54 -0500

    integrates\feat(back): #5434.8 add job
    - Add job to scan files for virus

---
+++ integrates/back/src/batch/handle_virus_scan_requester.py
@@ -0,0 +1,62 @@
+from batch.dal import (
+    delete_action,
+)
+from batch.types import (
+    BatchProcessing,
+)
+import logging
+import logging.config
+from mailer.resources import (
+    send_mail_handled_file,
+)
+from newutils import (
+    resources as resources_utils,
+)
+from settings import (
+    LOGGING,
+    NOEXTRA,
+)
+
+logging.config.dictConfig(LOGGING)
+
+# Constants
+LOGGER = logging.getLogger(__name__)
+
+
+async def handle_virus_scan_requester(*, item: BatchProcessing) -> None:
+    group_name = item.entity
+    file_name = item.additional_info
+    user_email = item.subject
+    message = (
+        f"Processing virus scan on file {file_name} requested by "
+        f"{user_email}"
+    )
+    LOGGER.info(":".join([item.subject, message]), **NOEXTRA)
+
+    handled_file = await resources_utils.get_file(
+        file_name, group_name, user_email
+    )
+
+    if handled_file:
+        await send_mail_handled_file(
+            user_email,
+            group_name,
+            file_name,
+            uploaded=True,
+        )
+    else:
+        await resources_utils.remove_file(file_name)
+        await send_mail_handled_file(
+            user_email,
+            group_name,
+            file_name,
+            uploaded=False,
+        )
+
+    await delete_action(
+        action_name=item.action_name,
+        additional_info=group_name,
+        entity=file_name,
+        subject=user_email,
+        time=item.time,
+    )
