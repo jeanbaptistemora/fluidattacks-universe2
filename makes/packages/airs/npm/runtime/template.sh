062d10318ea529dbc621fa35c9a2b6bbcbbf7c6f
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-10-13 06:03:04 -0500

    airs\refac(cross): #5342.19 update some deps
    - update package lock dependencies
    - no-deltas-test

--- makes/packages/airs/npm/runtime/template.sh
+++ makes/packages/airs/npm/runtime/template.sh
@@ -3,11 +3,11 @@
 function install_fontawesome_pro {
   local options="${1}"
   local deps=(
-    @fortawesome/fontawesome-pro@5.15.3
-    @fortawesome/pro-duotone-svg-icons@5.15.3
-    @fortawesome/pro-light-svg-icons@5.15.3
-    @fortawesome/pro-regular-svg-icons@5.15.3
-    @fortawesome/pro-solid-svg-icons@5.15.3
+    @fortawesome/fontawesome-pro@5.15.4
+    @fortawesome/pro-duotone-svg-icons@5.15.4
+    @fortawesome/pro-light-svg-icons@5.15.4
+    @fortawesome/pro-regular-svg-icons@5.15.4
+    @fortawesome/pro-solid-svg-icons@5.15.4
   )
 
   npm install "${options}" "${deps[@]}"
