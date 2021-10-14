4f4f1bbe27dc3508b7800188ecb7d9af3929d64a
Kevin Amado <kamado@fluidattacks.com>
2021-10-14 09:19:43 -0500

    common\refac(build): #5408 migrate to makes
    - Speed: 8543 loc / 15 days = 569 loc/day
    - TODO: 10745 loc
    - ETA: 10745 / 569 = 18 days

--- makes/applications/makes/profiles/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-# shellcheck shell=bash
-
-function dump {
-  : && cat __envIntegratesBack__ > makes/profiles/integrates-back.sh \
-    && cat __envSkims__ > makes/profiles/skims.sh
-}
-
-dump "${@}"
