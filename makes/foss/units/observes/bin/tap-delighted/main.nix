289d859500828b6e6b2975b59ccaccf61434eea1
Kevin Amado <kamado@fluidattacks.com>
2021-10-15 12:13:54 -0500

    observes\refac(build): #5408 migrate to makes
    - Speed: 9699 loc / 16 days = 606 loc/day
    - TODO: 9758 loc
    - ETA: 9758 / 606 = 16 days

---
+++ makes/foss/units/observes/bin/tap-delighted/main.nix
@@ -0,0 +1,15 @@
+{ makeScript
+, inputs
+, outputs
+, ...
+}:
+makeScript {
+  entrypoint = "import_and_run tap_delighted.cli main";
+  searchPaths = {
+    source = [
+      outputs."/observes/common/import-and-run"
+      inputs.product.observes-env-tap-delighted-runtime
+    ];
+  };
+  name = "observes-bin-tap-delighted";
+}
--- makes/foss/units/observes/job/delighted-etl/main.nix
+++ makes/foss/units/observes/job/delighted-etl/main.nix
@@ -6,7 +6,7 @@
 makeScript {
   searchPaths = {
     bin = [
-      inputs.product.observes-bin-tap-delighted
+      outputs."/observes/bin/tap-delighted"
       inputs.product.observes-tap-json
       inputs.product.observes-target-redshift
       outputs."/observes/bin/service/job-last-success"
