f6ba24be59024f90c914e16e4979c0b3b5862dec
Kevin Amado <kamado@fluidattacks.com>
2021-10-15 11:38:58 -0500

    observes\refac(build): #5408 migrate to makes
    - Speed: 9647 loc / 16 days = 602 loc/day
    - TODO: 9848 loc
    - ETA: 9848 / 602 = 16 days

---
+++ makes/foss/units/observes/bin/tap-mailchimp/main.nix
@@ -0,0 +1,16 @@
+{ makeScript
+, inputs
+, outputs
+, ...
+}:
+makeScript {
+  entrypoint = "import_and_run tap_mailchimp.cli main";
+  searchPaths = {
+    bin = [ inputs.nixpkgs.python38 ];
+    source = [
+      outputs."/observes/common/import-and-run"
+      inputs.product.observes-env-tap-mailchimp-runtime
+    ];
+  };
+  name = "observes-bin-tap-mailchimp";
+}
