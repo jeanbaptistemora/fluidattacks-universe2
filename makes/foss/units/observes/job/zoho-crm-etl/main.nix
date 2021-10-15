eda172cba0b973585ef1b5d2b47de2472b2aa61a
Kevin Amado <kamado@fluidattacks.com>
2021-10-15 12:30:12 -0500

    observes\refac(build): #5408 migrate to makes
    - Speed: 9735 loc / 16 days = 608 loc/day
    - TODO: 9704 loc
    - ETA: 9704 / 608 = 15 days

--- makes/foss/units/observes/job/zoho-crm-etl/main.nix
+++ makes/foss/units/observes/job/zoho-crm-etl/main.nix
@@ -7,7 +7,7 @@ makeScript {
   searchPaths = {
     bin = [
       inputs.product.observes-bin-streamer-zoho-crm
-      inputs.product.observes-bin-tap-csv
+      outputs."/observes/bin/tap-csv"
       inputs.product.observes-tap-json
       inputs.product.observes-target-redshift
       outputs."/observes/bin/service/job-last-success"
