2475af3e9fc754165c8fb0895191f703354d7318
Alejandro Trujillo <atrujillo@fluidattacks.com>
2021-10-22 10:24:33 -0500

    skims\feat(back): #5381.12 revert requirements f165
    - reverting commit 776059c3
    - Speed: 2 cfm_methods / 2 days = 1 cfm_methods/day
    - TODO: 43 cfm_methods
    - ETA: 43 / 1 = 43 days
    - no-deltas-test

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -1137,25 +1137,6 @@ class FindingEnum(Enum):
             user_interaction=cvss3_model.UserInteraction.required,
         ),
     )
-    F165: FindingMetadata = FindingMetadata.new(
-        auto_approve=False,
-        code="F160",
-        cwe=284,
-        requirements=[185, 265, 266],
-        score=cvss3_model.Score(
-            attack_complexity=cvss3_model.AttackComplexity.low,
-            attack_vector=cvss3_model.AttackVector.network,
-            availability_impact=cvss3_model.AvailabilityImpact.none,
-            confidentiality_impact=cvss3_model.ConfidentialityImpact.none,
-            exploitability=cvss3_model.Exploitability.poc,
-            integrity_impact=cvss3_model.IntegrityImpact.low,
-            privileges_required=cvss3_model.PrivilegesRequired.none,
-            remediation_level=cvss3_model.RemediationLevel.unavailable,
-            report_confidence=cvss3_model.ReportConfidence.confirmed,
-            severity_scope=cvss3_model.SeverityScope.unchanged,
-            user_interaction=cvss3_model.UserInteraction.none,
-        ),
-    )
     F207: FindingMetadata = FindingMetadata.new(
         auto_approve=False,
         code="F207",
