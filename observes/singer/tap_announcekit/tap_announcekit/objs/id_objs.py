596432876d3c1bd2eab560e39092c645545356c1
Daniel Murcia <dmurcia@fluidattacks.com>
2021-10-25 11:10:43 -0500

    observes\feat(back): #5498.1 feedback def
    - feedback definition

--- observes/singer/tap_announcekit/tap_announcekit/objs/id_objs.py
+++ observes/singer/tap_announcekit/tap_announcekit/objs/id_objs.py
@@ -5,12 +5,19 @@ from __future__ import (
 from dataclasses import (
     dataclass,
 )
+from returns.primitives.hkt import (
+    SupportsKind2,
+)
 from singer_io.singer2.json import (
     to_primitive,
 )
 from typing import (
     Any,
+    TypeVar,
 )
+
+_ID = TypeVar("_ID")
+_T = TypeVar("_T")
 
 
 @dataclass(frozen=True)
@@ -42,6 +49,12 @@ class ExtUserId:
 
 
 @dataclass(frozen=True)
+class FeedbackId:
+    post: PostId
+    obj_id: str
+
+
+@dataclass(frozen=True)
 class UserId:
     user_id: str
 
@@ -57,3 +70,9 @@ class ImageId:
     @staticmethod
     def from_any(raw: Any) -> ImageId:
         return ImageId(to_primitive(raw, str))
+
+
+@dataclass(frozen=True)
+class IndexedObj(SupportsKind2["IndexedObj[_ID, _T]", _ID, _T]):
+    id_obj: _ID
+    obj: _T
---
+++ observes/singer/tap_announcekit/tap_announcekit/objs/post/feedback.py
@@ -0,0 +1,46 @@
+from dataclasses import (
+    dataclass,
+)
+from datetime import (
+    datetime,
+)
+from enum import (
+    Enum,
+)
+from purity.v1 import (
+    FrozenList,
+)
+from tap_announcekit.objs.id_objs import (
+    ExtUserId,
+    FeedbackId,
+    IndexedObj,
+)
+from typing import (
+    Optional,
+)
+
+
+class ActionSource(Enum):
+    WIDGET = "widget"
+    EMAIL = "email"
+    FEED = "feed"
+
+
+@dataclass(frozen=True)
+class Feedback:
+    reaction: Optional[str]
+    feedback: Optional[str]
+    source: ActionSource
+    created_at: datetime
+    external_user_id: ExtUserId
+
+
+@dataclass(frozen=True)
+class FeedbackPage:
+    page: int
+    pages: int
+    count: int
+    items: FrozenList[Feedback]
+
+
+FeedbackObj = IndexedObj[FeedbackId, Feedback]
