1328a5bf94d08e4f36ca9a54ab4214dad282ea29
Diego Restrepo <drestrepo@fluidattacks.com>
2021-10-26 07:28:39 -0500

    forces\test(back): #5478.9 add more test
    - increase coverage

--- forces/test/apis/test_git.py
+++ forces/test/apis/test_git.py
@@ -1,5 +1,6 @@
 from forces.apis.git import (
     check_remotes,
+    extract_repo_name,
     get_repo_name_from_vars,
     get_repository_metadata,
 )
@@ -34,3 +35,14 @@ async def test_check_remotes(test_token: str) -> None:
 def test_get_repo_name_from_vars() -> None:
     os.environ["REPO_NAME"] = "product"
     assert get_repo_name_from_vars() == "product"
+
+
+def test_extract_repo_name() -> None:
+    assert (
+        extract_repo_name("git@gitlab.com:fluidattacks/product.git")
+        == "product"
+    )
+    assert (
+        extract_repo_name("https://127.0.0..1:8080/fluidattacks/product")
+        == "product"
+    )
