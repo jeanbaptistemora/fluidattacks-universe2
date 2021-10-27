e4c99a80d612eee5ebffd70d60641c6d0ff497ac
Juan Echeverri <jecheverri@fluidattacks.com>
2021-10-27 10:09:52 -0500

    skims\feat(back): #5381.20 check f372 terraform
    - Add method to check if content is served over http in tf
    - Add test to lib_path/f372
    - Update lib_path.csv
    - Speed: 1 tfm_method / 1 day = 1 tfm_methods/day
    - TODO: 19 tfm_methods
    - ETA: 19 / 1 = 19 days
    - no-deltas-test

--- skims/skims/lib_path/f372.py
+++ skims/skims/lib_path/f372.py
@@ -1,8 +1,12 @@
 from aioextensions import (
     in_process,
 )
+from aws.model import (
+    AWSCloudfrontDistribution,
+)
 from lib_path.common import (
     EXTENSIONS_CLOUDFORMATION,
+    EXTENSIONS_TERRAFORM,
     get_vulnerabilities_from_aws_iterator_blocking,
     SHIELD,
 )
@@ -17,6 +21,14 @@ from parse_cfn.loader import (
 )
 from parse_cfn.structure import (
     iter_cloudfront_distributions,
+)
+from parse_hcl2.loader import (
+    load as load_terraform,
+)
+from parse_hcl2.structure import (
+    get_argument,
+    iter_aws_cloudfront_distribution,
+    iterate_block_attributes,
 )
 from state.cache import (
     CACHE_ETERNALLY,
@@ -34,7 +46,7 @@ from utils.function import (
 )
 
 
-def _serves_content_over_http_iterate_vulnerabilities(
+def cfn_content_over_http_iterate_vulnerabilities(
     distributions_iterator: Iterator[Union[Any, Node]]
 ) -> Iterator[Union[Any, Node]]:
     for dist in distributions_iterator:
@@ -61,6 +73,41 @@ def _serves_content_over_http_iterate_vulnerabilities(
                         yield cache_b.inner["ViewerProtocolPolicy"]
 
 
+def tfm_content_over_http_iterate_vulnerabilities(
+    buckets_iterator: Iterator[Union[AWSCloudfrontDistribution, Node]]
+) -> Iterator[Union[Any, Node]]:
+    for bucket in buckets_iterator:
+        if isinstance(bucket, AWSCloudfrontDistribution):
+            if get_argument(
+                key="default_cache_behavior",
+                body=bucket.data,
+            ):
+                for attr in iterate_block_attributes(
+                    get_argument(
+                        body=bucket.data, key="default_cache_behavior"
+                    )
+                ):
+                    if (
+                        attr.key == "viewer_protocol_policy"
+                        and attr.val == "allow-all"
+                    ):
+                        yield attr
+            if get_argument(
+                key="ordered_cache_behavior",
+                body=bucket.data,
+            ):
+                for attr in iterate_block_attributes(
+                    get_argument(
+                        body=bucket.data, key="ordered_cache_behavior"
+                    )
+                ):
+                    if (
+                        attr.key == "viewer_protocol_policy"
+                        and attr.val == "allow-all"
+                    ):
+                        yield attr
+
+
 def _cfn_serves_content_over_http(
     content: str,
     path: str,
@@ -71,10 +118,26 @@ def _cfn_serves_content_over_http(
         description_key="src.lib_path.f372.serves_content_over_http",
         finding=core_model.FindingEnum.F372,
         path=path,
-        statements_iterator=_serves_content_over_http_iterate_vulnerabilities(
+        statements_iterator=cfn_content_over_http_iterate_vulnerabilities(
             distributions_iterator=iter_cloudfront_distributions(
                 template=template
             )
+        ),
+    )
+
+
+def _tfm_serves_content_over_http(
+    content: str,
+    path: str,
+    model: Any,
+) -> core_model.Vulnerabilities:
+    return get_vulnerabilities_from_aws_iterator_blocking(
+        content=content,
+        description_key="src.lib_path.f372.serves_content_over_http",
+        finding=core_model.FindingEnum.F372,
+        path=path,
+        statements_iterator=tfm_content_over_http_iterate_vulnerabilities(
+            buckets_iterator=iter_aws_cloudfront_distribution(model=model)
         ),
     )
 
@@ -92,6 +155,22 @@ async def cfn_serves_content_over_http(
         content=content,
         path=path,
         template=template,
+    )
+
+
+@CACHE_ETERNALLY
+@SHIELD
+@TIMEOUT_1MIN
+async def tfm_serves_content_over_http(
+    content: str,
+    path: str,
+    model: Any,
+) -> core_model.Vulnerabilities:
+    return await in_process(
+        _tfm_serves_content_over_http,
+        content=content,
+        path=path,
+        model=model,
     )
 
 
@@ -115,5 +194,15 @@ async def analyze(
                     template=template,
                 )
             )
+    if file_extension in EXTENSIONS_TERRAFORM:
+        content = await content_generator()
+        model = await load_terraform(stream=content, default=[])
+        coroutines.append(
+            tfm_serves_content_over_http(
+                content=content,
+                path=path,
+                model=model,
+            )
+        )
 
     return coroutines
