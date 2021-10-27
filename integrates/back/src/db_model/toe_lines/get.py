7779e347fa4f3d6608525dd11259f5df0baf1383
Andres Aguirre <aaguirre@fluidattacks.com>
2021-10-27 17:27:15 +0000

    integrates\feat(back): #5469.17 get toe lines by group
    - Add toe lines dataloader to get the toe lines by group

---
+++ integrates/back/src/db_model/toe_lines/get.py
@@ -0,0 +1,54 @@
+from .types import (
+    ToeLines,
+)
+from .utils import (
+    format_toe_lines,
+)
+from aiodataloader import (
+    DataLoader,
+)
+from aioextensions import (
+    collect,
+)
+from boto3.dynamodb.conditions import (
+    Key,
+)
+from dynamodb import (
+    keys,
+    operations,
+)
+from dynamodb.model import (
+    TABLE,
+)
+from typing import (
+    List,
+    Tuple,
+)
+
+
+async def _get_toe_lines_by_group(
+    group_name: str,
+) -> Tuple[ToeLines, ...]:
+    primary_key = keys.build_key(
+        facet=TABLE.facets["toe_lines_metadata"],
+        values={"group_name": group_name},
+    )
+    key_structure = TABLE.primary_key
+    lines_key = primary_key.sort_key.split("#")[0]
+    items = await operations.query(
+        condition_expression=(
+            Key(key_structure.partition_key).eq(primary_key.partition_key)
+            & Key(key_structure.sort_key).begins_with(lines_key)
+        ),
+        facets=(TABLE.facets["toe_lines_metadata"],),
+        table=TABLE,
+    )
+    return tuple(format_toe_lines(item=item) for item in items)
+
+
+class GroupToeLinesLoader(DataLoader):
+    # pylint: disable=no-self-use,method-hidden
+    async def batch_load_fn(
+        self, group_names: List[str]
+    ) -> Tuple[Tuple[ToeLines, ...], ...]:
+        return await collect(tuple(map(_get_toe_lines_by_group, group_names)))
