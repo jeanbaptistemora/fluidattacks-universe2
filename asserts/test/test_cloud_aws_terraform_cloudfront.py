739185deb56e2a534c863ea1f07da99f12179432
Juan Echeverri <jecheverri@fluidattacks.com>
2021-10-27 12:15:06 -0500

    asserts\refac(back): #5381.23 remove asserts method
    - remove migrated methods
    - remove test
    - Speed: 1 tfm_method / 1 day = 1 tfm_methods/day
    - TODO: 19 tfm_methods
    - ETA: 19 / 1 = 19 days

--- asserts/test/test_cloud_aws_terraform_cloudfront.py
+++ asserts/test/test_cloud_aws_terraform_cloudfront.py
@@ -16,15 +16,6 @@ VULN: str = "test/static/terraform/vulnerable"
 NOT_EXISTS: str = "test/static/terraform/not-exists"
 
 
-def test_serves_content_over_http():
-    """test cloudfront.serves_content_over_http."""
-    result = cloudfront.serves_content_over_http(VULN)
-    assert result.is_open()
-    assert result.get_vulns_number() == 2
-    assert cloudfront.serves_content_over_http(SAFE).is_closed()
-    assert cloudfront.serves_content_over_http(NOT_EXISTS).is_unknown()
-
-
 def test_serves_content_over_insecure_protocols():
     """test cloudfront.serves_content_over_insecure_protocols."""
     result = cloudfront.serves_content_over_insecure_protocols(VULN)
