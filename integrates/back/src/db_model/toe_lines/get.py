91830e33c227274c53c0435c136cf4540c6764c2
Andres Aguirre <aaguirre@fluidattacks.com>
2021-10-27 19:12:09 +0000

    integrates\feat(back): #5469.18 get toe lines by root
    - Add toe lines dataloader to get the toe lines by root

--- integrates/back/src/db_model/toe_lines/get.py
+++ integrates/back/src/db_model/toe_lines/get.py
@@ -52,3 +52,30 @@ class GroupToeLinesLoader(DataLoader):
         self, group_names: List[str]
     ) -> Tuple[Tuple[ToeLines, ...], ...]:
         return await collect(tuple(map(_get_toe_lines_by_group, group_names)))
+
+
+async def _get_toe_lines_by_root(
+    group_name: str, root_id: str
+) -> Tuple[ToeLines, ...]:
+    primary_key = keys.build_key(
+        facet=TABLE.facets["toe_lines_metadata"],
+        values={"group_name": group_name, "root_id": root_id},
+    )
+    key_structure = TABLE.primary_key
+    items = await operations.query(
+        condition_expression=(
+            Key(key_structure.partition_key).eq(primary_key.partition_key)
+            & Key(key_structure.sort_key).begins_with(primary_key.sort_key)
+        ),
+        facets=(TABLE.facets["toe_lines_metadata"],),
+        table=TABLE,
+    )
+    return tuple(format_toe_lines(item=item) for item in items)
+
+
+class RootToeLinesLoader(DataLoader):
+    # pylint: disable=no-self-use,method-hidden
+    async def batch_load_fn(
+        self, roots: List[Tuple[str, str]]
+    ) -> Tuple[Tuple[ToeLines, ...], ...]:
+        return await collect(tuple(map(_get_toe_lines_by_root, *zip(*roots))))
