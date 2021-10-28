d7e5061e5f82222e38880a649e3125153f628f97
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-10-28 08:40:45 -0500

    integrates\feat(back): #2228.3 remove subscription too
    - remove all user subscription although when running subscriptions
    scheduler it will be deleted if has not permission better to
    removed all possible early

---
+++ integrates/back/src/remove_user/domain.py
@@ -0,0 +1,39 @@
+from aioextensions import (
+    collect,
+)
+from organizations.domain import (
+    get_user_organizations,
+    remove_user,
+)
+from subscriptions.domain import (
+    get_user_subscriptions,
+    unsubscribe_user_to_entity_report,
+)
+from users.domain import (
+    delete,
+)
+
+
+async def remove_user_all_organizations(*, email: str) -> None:
+    organizations_ids = await get_user_organizations(email)
+    await collect(
+        tuple(
+            remove_user(organization_id, email)
+            for organization_id in organizations_ids
+        )
+    )
+
+    subscriptions = await get_user_subscriptions(user_email=email)
+    await collect(
+        tuple(
+            unsubscribe_user_to_entity_report(
+                report_entity=subscription["sk"]["entity"],
+                report_subject=subscription["sk"]["subject"],
+                user_email=email,
+            )
+            for subscription in subscriptions
+        )
+    )
+
+    if not bool(await get_user_organizations(email)):
+        await delete(email)
