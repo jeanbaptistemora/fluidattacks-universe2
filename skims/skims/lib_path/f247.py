1b9f7f3ab34a418a2dd4ebea5b2bdc1f051a4c72
Juan Echeverri <jecheverri@fluidattacks.com>
2021-10-29 13:05:57 -0500

    skims\feat(back): #5381.37 check f247 ebs terraform
    - Add method to check if aws ebs volumes are encrypted
    - Add test to lib_path/f0247
    - Update lib_path.csv
    - Speed: 4 tfm_method / 2 day = 2 tfm_methods/day
    - TODO: 16 tfm_methods
    - ETA: 16 / 2 = 8 days

--- skims/skims/lib_path/f247.py
+++ skims/skims/lib_path/f247.py
@@ -2,6 +2,7 @@ from aioextensions import (
     in_process,
 )
 from aws.model import (
+    AWSEbsVolume,
     AWSFsxWindowsFileSystem,
 )
 from lib_path.common import (
@@ -19,6 +20,7 @@ from parse_hcl2.loader import (
     load as load_terraform,
 )
 from parse_hcl2.structure import (
+    iter_aws_ebs_volume,
     iter_aws_fsx_windows_file_system,
 )
 from parse_hcl2.tokens import (
@@ -43,12 +45,28 @@ from utils.function import (
 def tfm_fsx_unencrypted_volumes_iterate_vulnerabilities(
     buckets_iterator: Iterator[Union[AWSFsxWindowsFileSystem, Node]]
 ) -> Iterator[Union[Any, Node]]:
-    kms_key = False
     for bucket in buckets_iterator:
+        kms_key = False
         for elem in bucket.data:
             if isinstance(elem, Attribute) and elem.key == "kms_key_id":
                 kms_key = True
         if not kms_key:
+            yield bucket
+
+
+def tfm_ebs_unencrypted_volumes_iterate_vulnerabilities(
+    buckets_iterator: Iterator[Union[AWSEbsVolume, Node]]
+) -> Iterator[Union[Any, Node]]:
+    for bucket in buckets_iterator:
+        vol_encrypted = False
+        for elem in bucket.data:
+            if (
+                isinstance(elem, Attribute)
+                and elem.key == "encrypted"
+                and elem.val is True
+            ):
+                vol_encrypted = True
+        if not vol_encrypted:
             yield bucket
 
 
@@ -70,6 +88,24 @@ def _tfm_fsx_unencrypted_volumes(
     )
 
 
+def _tfm_ebs_unencrypted_volumes(
+    content: str,
+    path: str,
+    model: Any,
+) -> core_model.Vulnerabilities:
+    return get_vulnerabilities_from_aws_iterator_blocking(
+        content=content,
+        description_key="F247.title",
+        finding=core_model.FindingEnum.F247,
+        path=path,
+        statements_iterator=(
+            tfm_ebs_unencrypted_volumes_iterate_vulnerabilities(
+                buckets_iterator=iter_aws_ebs_volume(model=model)
+            )
+        ),
+    )
+
+
 @CACHE_ETERNALLY
 @SHIELD
 @TIMEOUT_1MIN
@@ -80,6 +116,22 @@ async def tfm_fsx_unencrypted_volumes(
 ) -> core_model.Vulnerabilities:
     return await in_process(
         _tfm_fsx_unencrypted_volumes,
+        content=content,
+        path=path,
+        model=model,
+    )
+
+
+@CACHE_ETERNALLY
+@SHIELD
+@TIMEOUT_1MIN
+async def tfm_ebs_unencrypted_volumes(
+    content: str,
+    path: str,
+    model: Any,
+) -> core_model.Vulnerabilities:
+    return await in_process(
+        _tfm_ebs_unencrypted_volumes,
         content=content,
         path=path,
         model=model,
@@ -99,6 +151,13 @@ async def analyze(
         model = await load_terraform(stream=content, default=[])
         coroutines.append(
             tfm_fsx_unencrypted_volumes(
+                content=content,
+                path=path,
+                model=model,
+            )
+        )
+        coroutines.append(
+            tfm_ebs_unencrypted_volumes(
                 content=content,
                 path=path,
                 model=model,
