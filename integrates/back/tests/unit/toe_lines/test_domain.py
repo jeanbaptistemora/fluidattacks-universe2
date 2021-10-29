0b094611740d9f1c3de02a4c91107c3f8cefa87f
Andres Aguirre <aaguirre@fluidattacks.com>
2021-10-29 14:09:47 +0000

    integrates\test(back): #5469.29 update toe lines
    - Add domain test to update toe lines

--- integrates/back/tests/unit/toe_lines/test_domain.py
+++ integrates/back/tests/unit/toe_lines/test_domain.py
@@ -13,6 +13,7 @@ from toe.lines import (
 )
 from toe.lines.types import (
     ToeLinesAttributesToAdd,
+    ToeLinesAttributesToUpdate,
 )
 
 # Constants
@@ -57,4 +58,49 @@ async def test_add() -> None:
         root_id="4039d098-ffc5-4984-8ed3-eb17bca98e19",
         seen_at="2018-08-01T05:00:00+00:00",
         sorts_risk_level=Decimal("1"),
+    )
+
+
+@pytest.mark.changes_db
+async def test_update() -> None:
+    group_name = "unittesting"
+    root_id = "4039d098-ffc5-4984-8ed3-eb17bca98e19"
+    filename = "product/test/new#.new"
+    loaders = get_new_context()
+    current_value = await loaders.toe_lines.load(
+        (group_name, root_id, filename)
+    )
+    attributes = ToeLinesAttributesToUpdate(
+        attacked_at="2022-08-01T05:00:00+00:00",
+        attacked_by="hacker2@test.com",
+        attacked_lines=434,
+        be_present=False,
+        comments="comment test 2",
+        first_attack_at="2021-04-01T05:00:00+00:00",
+        loc=1111,
+        modified_commit="993466z",
+        modified_date="2020-08-01T05:00:00+00:00",
+        seen_at="2019-08-01T05:00:00+00:00",
+        sorts_risk_level=Decimal("0.5"),
+    )
+    await toe_lines_domain.update(
+        current_value, group_name, root_id, filename, attributes
+    )
+    loaders = get_new_context()
+    toe_lines = await loaders.toe_lines.load((group_name, root_id, filename))
+    assert toe_lines == ToeLines(
+        attacked_at="2022-08-01T05:00:00+00:00",
+        attacked_by="hacker2@test.com",
+        attacked_lines=434,
+        be_present=False,
+        comments="comment test 2",
+        filename="product/test/new#.new",
+        first_attack_at="2021-04-01T05:00:00+00:00",
+        group_name="unittesting",
+        loc=1111,
+        modified_commit="993466z",
+        modified_date="2020-08-01T05:00:00+00:00",
+        root_id="4039d098-ffc5-4984-8ed3-eb17bca98e19",
+        seen_at="2019-08-01T05:00:00+00:00",
+        sorts_risk_level=Decimal("0.5"),
     )
