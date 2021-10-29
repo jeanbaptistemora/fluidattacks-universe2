4c645601e08ac44d03bae3939de2c033b6128c90
Alejandro Trujillo <atrujillo@fluidattacks.com>
2021-10-29 14:22:54 -0500

    skims\feat(back): #5381.40 check f259
    - added f259 to analyze checks
    - created test file
    - updated lib_path results
    - Speed: 2 cfn_methods / 2 days = 1 cfn_methods/day
    - TODO: 40 cfn_methods
    - ETA: 40 / 1 = 40 days
    - no-deltas-test

--- skims/skims/lib_path/analyze.py
+++ skims/skims/lib_path/analyze.py
@@ -16,6 +16,7 @@ from lib_path import (
     f117,
     f200,
     f247,
+    f259,
     f372,
     f380,
 )
@@ -68,6 +69,7 @@ CHECKS: Tuple[Tuple[core_model.FindingEnum, Any], ...] = (
     (core_model.FindingEnum.F117, f117.analyze),
     (core_model.FindingEnum.F200, f200.analyze),
     (core_model.FindingEnum.F247, f247.analyze),
+    (core_model.FindingEnum.F259, f259.analyze),
     (core_model.FindingEnum.F372, f372.analyze),
     (core_model.FindingEnum.F380, f380.analyze),
 )
