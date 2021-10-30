b4209fddd20fe6e6aa33aae6628226461a3521a2
Alejandro Salgado <asalgado@fluidattacks.com>
2021-10-30 15:19:22 -0500

    skims\refac(back): #5534.1 nametuple definition
    - Add CfgArgs NamedTuple to CFG types

--- skims/skims/sast_transformations/control_flow/types.py
+++ skims/skims/sast_transformations/control_flow/types.py
@@ -1,10 +1,12 @@
 from model.graph_model import (
     Graph,
+    GraphShardMetadataLanguage,
 )
 from mypy_extensions import (
     NamedArg,
 )
 from typing import (
+    Any,
     Callable,
     Dict,
     List,
@@ -25,6 +27,29 @@ GenericType = Callable[
     ],
     None,
 ]
+
+
+CFG_ARGS = Any
+
+
+class CfgArgs(NamedTuple):
+    generic: Callable[[CFG_ARGS, Stack], None]
+    graph: Graph
+    n_id: str
+    language: GraphShardMetadataLanguage
+    edge_attrs: EdgeAttrs
+
+    def fork_n_id(self, n_id: str) -> CFG_ARGS:
+        return CfgArgs(
+            generic=self.generic,
+            graph=self.graph,
+            n_id=n_id,
+            language=self.language,
+            edge_attrs=self.edge_attrs,
+        )
+
+
+CfgBuilder = Callable[[CfgArgs, Stack], None]
 
 
 class Walker(NamedTuple):
