1b29caa7e4a9656b3daf7a2e58afe3bb7593c2a2
Andres Aguirre <aaguirre@fluidattacks.com>
2021-11-02 19:06:22 +0000

    integrates\feat(back): #4014.134 make group dir
    - Add the group dir to pull the repositories

--- integrates/back/src/batch/toe_lines.py
+++ integrates/back/src/batch/toe_lines.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 
 
 async def apply_git_config(repo_path: str) -> None:
@@ -6,3 +7,8 @@ async def apply_git_config(repo_path: str) -> None:
     await asyncio.create_subprocess_exec(
         "git", f"--git-dir={repo_path}/.git", "config", "core.quotepath", "off"
     )
+
+
+def make_group_dir(tmpdir: str, group_name: str) -> None:
+    group_dir = os.path.join(tmpdir, "groups", group_name)
+    os.makedirs(group_dir, exist_ok=True)
