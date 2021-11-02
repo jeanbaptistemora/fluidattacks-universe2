6486b92f279ddff0613ae53fc2b5edeb0122053b
Andres Aguirre <aaguirre@fluidattacks.com>
2021-11-02 19:29:06 +0000

    integrates\feat(back): #4014.135 pull group repositories
    - Pull the repositories for a group from the data lake

--- integrates/back/src/batch/toe_lines.py
+++ integrates/back/src/batch/toe_lines.py
@@ -12,3 +12,12 @@ async def apply_git_config(repo_path: str) -> None:
 def make_group_dir(tmpdir: str, group_name: str) -> None:
     group_dir = os.path.join(tmpdir, "groups", group_name)
     os.makedirs(group_dir, exist_ok=True)
+
+
+def pull_repositories(tmpdir: str, group_name: str) -> None:
+    make_group_dir(tmpdir, group_name)
+    os.system(  # nosec
+        f"PROD_AWS_ACCESS_KEY_ID=$SERVICES_PROD_AWS_ACCESS_KEY_ID "
+        f"PROD_AWS_SECRET_ACCESS_KEY=$SERVICES_PROD_AWS_SECRET_ACCESS_KEY "
+        f"melts drills --pull-repos {group_name}"
+    )
