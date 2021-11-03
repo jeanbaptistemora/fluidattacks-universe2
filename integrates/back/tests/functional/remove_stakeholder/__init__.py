b4834fe22e58c52d0311501f09650ec30767018f
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-11-03 07:25:08 -0500

    integrates\test(back): #2228.13 update functional test
    - update functional test of `remove_stakeholder` mutation to verify
    the user doesn't appear as stakeholder or run me query

--- integrates/back/tests/functional/remove_stakeholder/__init__.py
+++ integrates/back/tests/functional/remove_stakeholder/__init__.py
@@ -27,3 +27,60 @@ async def get_result_mutation(
         stakeholder=user,
         context=get_new_context(),
     )
+
+
+async def get_result_me_query(
+    *,
+    user: str,
+    organization_id: str,
+) -> Dict[str, Any]:
+    query: str = f"""
+        query {{
+            me(callerOrigin: "API") {{
+                remember
+                role
+                tags(organizationId: "${organization_id}") {{
+                    organization
+                }}
+                __typename
+            }}
+        }}
+    """
+    data: Dict[str, str] = {
+        "query": query,
+    }
+    return await get_graphql_result(
+        data,
+        stakeholder=user,
+        context=get_new_context(),
+    )
+
+
+async def get_result_stakeholder_query(
+    *, user: str, stakeholder: str, group: str
+) -> Dict[str, Any]:
+    query: str = f"""
+        query {{
+            stakeholder(entity: GROUP,
+                    groupName: "{group}",
+                    userEmail: "{stakeholder}") {{
+                email
+                role
+                responsibility
+                firstLogin
+                lastLogin
+                groups {{
+                    name
+                }}
+                __typename
+            }}
+        }}
+    """
+    data: Dict[str, str] = {
+        "query": query,
+    }
+    return await get_graphql_result(
+        data,
+        stakeholder=user,
+        context=get_new_context(),
+    )
--- integrates/back/tests/functional/remove_stakeholder/test_resolver.py
+++ integrates/back/tests/functional/remove_stakeholder/test_resolver.py
@@ -1,5 +1,7 @@
 from . import (
+    get_result_me_query,
     get_result_mutation,
+    get_result_stakeholder_query,
 )
 import pytest
 from typing import (
@@ -11,16 +13,52 @@ from typing import (
 @pytest.mark.asyncio
 @pytest.mark.resolver_test_group("remove_stakeholder")
 @pytest.mark.parametrize(
-    ["email"],
+    ["email", "role", "admin_email"],
     [
-        ["system_owner@gmail.com"],
+        ["system_owner@gmail.com", "system_owner", "admin@gmail.com"],
     ],
 )
-async def test_remove_stakeholder(populate: bool, email: str) -> None:
+async def test_remove_stakeholder(
+    populate: bool, email: str, role: str, admin_email: str
+) -> None:
     assert populate
+    group_name: str = "group1"
+    organization_id: str = "ORG#40f6da5f-4f66-4bf0-825b-a2d9748ad6db"
+    result_me_query: Dict[str, Any] = await get_result_me_query(
+        user=email, organization_id=organization_id
+    )
+    result_stakeholder_query: Dict[
+        str, Any
+    ] = await get_result_stakeholder_query(
+        user=admin_email, stakeholder=email, group=group_name
+    )
+
+    assert not result_me_query["data"]["me"]["remember"]
+    assert result_me_query["data"]["me"]["role"] == role
+    assert result_stakeholder_query["data"]["stakeholder"]["email"] == email
+    assert result_stakeholder_query["data"]["stakeholder"]["role"] == role
+    assert (
+        result_stakeholder_query["data"]["stakeholder"]["responsibility"] == ""
+    )
+
     result: Dict[str, Any] = await get_result_mutation(
         user=email,
     )
 
     assert "errors" not in result
     assert result["data"]["removeStakeholder"]["success"]
+
+    result_me_query = await get_result_me_query(
+        user=email, organization_id=organization_id
+    )
+    result_stakeholder_query = await get_result_stakeholder_query(
+        user=admin_email, stakeholder=email, group=group_name
+    )
+
+    assert "errors" in result_me_query
+    assert "errors" in result_stakeholder_query
+    assert result_me_query["errors"][0]["message"] == "Access denied"
+    assert (
+        result_stakeholder_query["errors"][0]["message"]
+        == "Access denied or stakeholder not found"
+    )
