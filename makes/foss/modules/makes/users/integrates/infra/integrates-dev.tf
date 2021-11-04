986f55dc1d0c3fc75f1e51cd2c6a2db5e2b3783d
Daniel Salazar <dsalazar@fluidattacks.com>
2021-11-04 17:35:16 -0500

    makes\refac(infra): #5473 destroy modules
    - Destroy credentials modules for integrates-dev

--- makes/foss/modules/makes/users/integrates/infra/integrates-dev.tf
+++ makes/foss/modules/makes/users/integrates/infra/integrates-dev.tf
@@ -19,25 +19,6 @@ resource "aws_iam_access_key" "integrates-dev-key-2" {
   user = "integrates-dev"
 }
 
-module "publish_credentials_dev" {
-  source       = "../../modules/publish_credentials"
-  gitlab_token = var.gitlab_token
-  key_1        = aws_iam_access_key.integrates-dev-key-1
-  key_2        = aws_iam_access_key.integrates-dev-key-2
-  prefix       = "INTEGRATES_DEV"
-  protected    = false
-}
-
-module "publish_credentials_dev_services" {
-  source       = "../../modules/publish_credentials"
-  gitlab_token = var.gitlab_token_services
-  key_1        = aws_iam_access_key.integrates-dev-key-1
-  key_2        = aws_iam_access_key.integrates-dev-key-2
-  prefix       = "INTEGRATES_DEV"
-  project_id   = "4603023"
-  protected    = false
-}
-
 # CloudFlare
 
 resource "cloudflare_api_token" "integrates_development" {
