d0fc4f84435e5004a8a9c9c7520d4a1da4ac07e6
Kevin Amado <kamado@fluidattacks.com>
2021-11-04 16:57:08 -0500

    skims\feat(back): #5549.7 coerce ranges
    - Coerce to valid semver ranges, this effectively
      allow us to match Maven versions

--- skims/tools/semver-match/index.js
+++ skims/tools/semver-match/index.js
@@ -1,21 +1,51 @@
-const semverIntersects = require('semver/ranges/intersects');
-const semverRange = require('semver/classes/range');
+const semverIntersects = require("semver/ranges/intersects");
+const semverRange = require("semver/classes/range");
+
+const coerce = (constraint) => {
+  // Coerce versions like a.b.c.d-pre+build to a.b.c-pre-build
+  // i.e. remove version components after the third one so it complies semver
+  const constraintTokens = constraint.split(/([-+])/);
+  const version = constraintTokens[0];
+  const tags = constraintTokens.slice(1).join("");
+
+  let versionCoerced = version.split(".");
+
+  if (versionCoerced.length < 3) {
+    versionCoerced.push("*");
+  };
+
+  return versionCoerced.slice(0, 3).join(".") + tags;
+};
+
+const coerceRange = (range) => range
+  .replace(/\s+/g, " ")
+  .replace(/\s*\|\|\s*/g, "||")
+  .split("||")
+  .map((token) => token
+    .split(" ")
+    .map(coerce)
+    .join(" "))
+  .join("||");
 
 const argv = process.argv;
-let left = argv[2];
+const left = argv[2];
 const right = argv[3];
+const leftCoerced = coerceRange(left);
+const rightCoerced = coerceRange(right);
 
-// Sanitize a little bit the left constraint
-left = left.split('-', 1).slice(0, 1)[0]; // Ignore prerelease tag
-left = left.split('+', 1).slice(0, 1)[0]; // Ignore build tag
-left = left.split('.').slice(0, 3).join('.'); // Ignore fourth+ components
-console.error('Using left constraint:', new semverRange(left).range);
-console.error('Using right constraint:', new semverRange(right).range);
+console.error("Given left constraint:", left);
+console.error("Given right constraint:", right);
+console.error();
+console.error("Coerced left constraint:", leftCoerced);
+console.error("Coerced right constraint:", rightCoerced);
+console.error();
+console.error("Using left constraint:", new semverRange(leftCoerced).range);
+console.error("Using right constraint:", new semverRange(rightCoerced).range);
 
 try {
   console.log(JSON.stringify({
     success: true,
-    match: semverIntersects(left, right),
+    match: semverIntersects(leftCoerced, rightCoerced),
   }, null, true));
 }
 catch (error) {
