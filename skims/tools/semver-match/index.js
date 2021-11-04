f1e4e5c330a61eebc7e0cd24107515e007fd1b49
Kevin Amado <kamado@fluidattacks.com>
2021-11-04 13:09:39 -0500

    skims\feat(back): #5549.4 edge cases
    - Handle edge cases:
      - exclude tags for prerelease and build
      - ignore versions after the fourth point

--- skims/tools/semver-match/index.js
+++ skims/tools/semver-match/index.js
@@ -1,8 +1,16 @@
 const semverIntersects = require('semver/ranges/intersects');
+const semverRange = require('semver/classes/range');
 
 const argv = process.argv;
-const left = argv[2];
+let left = argv[2];
 const right = argv[3];
+
+// Sanitize a little bit the left constraint
+left = left.split('-', 1).slice(0, 1)[0]; // Ignore prerelease tag
+left = left.split('+', 1).slice(0, 1)[0]; // Ignore build tag
+left = left.split('.').slice(0, 3).join('.'); // Ignore fourth+ components
+console.error('Using left constraint:', new semverRange(left).range);
+console.error('Using right constraint:', new semverRange(right).range);
 
 try {
   console.log(JSON.stringify({
