f0e39330998f1be6d78376d5179668ea9e1ce6da
Diego Restrepo <drestrepo@fluidattacks.com>
2021-11-04 14:59:56 -0500

    skims\fix(build): #5553.1 pin provider version
    - pin aws terraform provider version

--- makes/foss/units/integrates/db/main.tf
+++ makes/foss/units/integrates/db/main.tf
@@ -1,6 +1,17 @@
 variable "host" {}
 variable "port" {}
 
+terraform {
+  required_version = "~> 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.63.0"
+    }
+  }
+}
+
 provider "aws" {
   endpoints {
     dynamodb = "http://${var.host}:${var.port}"
