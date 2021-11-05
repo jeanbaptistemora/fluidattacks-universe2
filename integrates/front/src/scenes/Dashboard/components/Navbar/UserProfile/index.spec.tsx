5277d9cac979645fb9d17d6471a63b36b02e7258
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-11-05 13:39:13 -0500

    integrates\test(front): #2228.17 update unit test
    - update unit test by adding a test for when the mutation
    raise and exception
    - no-deltas-test

--- integrates/front/src/scenes/Dashboard/components/Navbar/UserProfile/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/components/Navbar/UserProfile/index.spec.tsx
@@ -2,6 +2,7 @@ import type { MockedResponse } from "@apollo/client/testing";
 import { MockedProvider } from "@apollo/client/testing";
 import type { ReactWrapper } from "enzyme";
 import { mount } from "enzyme";
+import { GraphQLError } from "graphql";
 import { reset } from "mixpanel-browser";
 import React from "react";
 import { act } from "react-dom/test-utils";
@@ -13,6 +14,16 @@ import { DropdownButton, NavbarButton } from "../styles";
 import { Modal } from "components/Modal";
 import { UserProfile } from "scenes/Dashboard/components/Navbar/UserProfile/index";
 import { REMOVE_STAKEHOLDER_MUTATION } from "scenes/Dashboard/components/Navbar/UserProfile/queries";
+import { msgError } from "utils/notifications";
+
+jest.mock("../../../../../utils/notifications", (): Dictionary => {
+  const mockedNotifications: Dictionary<() => Dictionary> = jest.requireActual(
+    "../../../../../utils/notifications"
+  );
+  jest.spyOn(mockedNotifications, "msgError").mockImplementation();
+
+  return mockedNotifications;
+});
 
 jest.mock("mixpanel-browser", (): Dictionary => {
   const mockedMixPanel: Dictionary<() => Dictionary> =
@@ -20,6 +31,21 @@ jest.mock("mixpanel-browser", (): Dictionary => {
   jest.spyOn(mockedMixPanel, "reset").mockImplementation();
 
   return mockedMixPanel;
+});
+
+const mockHistoryPush: jest.Mock = jest.fn();
+
+jest.mock("react-router-dom", (): Record<string, unknown> => {
+  const mockedRouter: Record<string, () => Record<string, unknown>> =
+    jest.requireActual("react-router-dom");
+
+  return {
+    ...mockedRouter,
+    useHistory: (): Record<string, unknown> => ({
+      ...mockedRouter.useHistory(),
+      push: mockHistoryPush,
+    }),
+  };
 });
 
 describe("User Profile", (): void => {
@@ -121,6 +147,102 @@ describe("User Profile", (): void => {
 
         expect(wrapper).toHaveLength(1);
         expect(reset).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    jest.clearAllMocks();
+  });
+
+  it("should delete account error", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    const { t } = useTranslation();
+    const mockQueryFalse: MockedResponse[] = [
+      {
+        request: {
+          query: REMOVE_STAKEHOLDER_MUTATION,
+        },
+        result: {
+          errors: [new GraphQLError("Unexpected error")],
+        },
+      },
+    ];
+
+    const wrapper: ReactWrapper = mount(
+      <MemoryRouter initialEntries={["/orgs/okada"]}>
+        <MockedProvider addTypename={false} mocks={mockQueryFalse}>
+          <UserProfile userRole={"customer"} />
+        </MockedProvider>
+      </MemoryRouter>
+    );
+
+    await act(async (): Promise<void> => {
+      await waitForExpect((): void => {
+        wrapper.update();
+
+        expect(wrapper).toHaveLength(1);
+        expect(wrapper.find(DropdownButton)).toHaveLength(0);
+      });
+    });
+
+    const navbarButton: ReactWrapper = wrapper.find(NavbarButton).first();
+    const NUMBER_OF_DROPDOWN_BUTTONS: number = 5;
+    navbarButton.simulate("click");
+
+    await act(async (): Promise<void> => {
+      await waitForExpect((): void => {
+        wrapper.update();
+
+        expect(wrapper).toHaveLength(1);
+        expect(wrapper.find(DropdownButton)).toHaveLength(
+          NUMBER_OF_DROPDOWN_BUTTONS
+        );
+        expect(wrapper.find(Modal).first().prop("open")).toBe(false);
+      });
+    });
+
+    const deleteAccountDropdown: ReactWrapper = wrapper
+      .find("button")
+      .filterWhere((element: ReactWrapper): boolean =>
+        element.contains(t("navbar.deleteAccount.text").toString())
+      )
+      .first();
+
+    deleteAccountDropdown.simulate("click");
+
+    await act(async (): Promise<void> => {
+      await waitForExpect((): void => {
+        wrapper.update();
+
+        expect(wrapper).toHaveLength(1);
+        expect(
+          wrapper
+            .find("Modal")
+            .filterWhere((element: ReactWrapper): boolean =>
+              element.contains(t("navbar.deleteAccount.text").toString())
+            )
+            .first()
+            .prop("open")
+        ).toBe(true);
+      });
+    });
+
+    const proceedButton: ReactWrapper = wrapper
+      .find("button")
+      .filterWhere((element: ReactWrapper): boolean =>
+        element.contains("Proceed")
+      )
+      .first();
+
+    proceedButton.simulate("click");
+
+    await act(async (): Promise<void> => {
+      await waitForExpect((): void => {
+        wrapper.update();
+
+        expect(wrapper).toHaveLength(1);
+        expect(msgError).toHaveBeenCalledWith(t("groupAlerts.errorTextsad"));
+        expect(mockHistoryPush).toHaveBeenCalledWith("/home");
       });
     });
 
