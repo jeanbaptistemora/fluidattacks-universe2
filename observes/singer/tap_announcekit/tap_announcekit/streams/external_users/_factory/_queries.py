27ab1e90dcd6d2cc292e1ab50c1052ae8e9dffde
Daniel Murcia <dmurcia@fluidattacks.com>
2021-11-05 11:45:40 -0500

    observes\feat(back): #5500.3 ext user ids query
    - add ext user ids query
    - add DataPage type

---
+++ observes/singer/tap_announcekit/tap_announcekit/streams/external_users/_factory/_queries.py
@@ -0,0 +1,64 @@
+from dataclasses import (
+    dataclass,
+)
+from purity.v1 import (
+    Transform,
+)
+from returns.io import (
+    IO,
+)
+from tap_announcekit.api.client import (
+    Operation,
+    Query,
+    QueryFactory,
+)
+from tap_announcekit.api.gql_schema import (
+    PageOfFeedback as RawFeedbackPage,
+)
+from tap_announcekit.objs.id_objs import (
+    ExtUserId,
+    ProjectId,
+)
+from tap_announcekit.objs.page import (
+    DataPage,
+)
+from typing import (
+    Any,
+    cast,
+)
+
+
+@dataclass(frozen=True)
+class ExtUserIdsQuery:
+    _to_obj: Transform[RawFeedbackPage, DataPage[ExtUserId]]
+    proj: ProjectId
+    page: int
+
+    @staticmethod
+    def _select_page_fields(page_selection: Any) -> IO[None]:
+        props = DataPage.__annotations__.copy()
+        del props["items"]
+        for attr in props:
+            getattr(page_selection, attr)()
+        return IO(None)
+
+    def _select_fields(self, operation: Operation) -> IO[None]:
+        page_selection = operation.externalUsers(
+            project_id=self.proj.id_str, page=self.page
+        )
+        self._select_page_fields(page_selection)
+        page_selection.items().id()
+        return IO(None)
+
+    @property
+    def query(
+        self,
+    ) -> Query[DataPage[ExtUserId]]:
+        return QueryFactory.select(
+            self._select_fields,
+            Transform(
+                lambda p: self._to_obj(
+                    cast(RawFeedbackPage, p.externalUsers),
+                )
+            ),
+        )
