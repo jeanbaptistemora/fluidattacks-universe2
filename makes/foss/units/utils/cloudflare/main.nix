c0471b2bf39bdd135ddc9a0f019f52fc377ac869
John Perez <jperez@fluidattacks.com>
2021-11-05 12:13:23 -0500

    makes\refac(build): #5408 migrate to makes
    - Speed: 19420 loc / 37 days = 524 loc/day
    - TODO: 891 loc
    - ETA: 891 / 524 = 1 days

---
+++ makes/foss/units/utils/cloudflare/main.nix
@@ -0,0 +1,14 @@
+{ inputs
+, makeTemplate
+, ...
+}:
+makeTemplate {
+  searchPaths = {
+    bin = [
+      inputs.nixpkgs.curl
+      inputs.nixpkgs.jq
+    ];
+  };
+  name = "utils-cloudflare";
+  template = ./template.sh;
+}
