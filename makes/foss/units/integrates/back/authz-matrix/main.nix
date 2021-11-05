8e522014c97a07984314b7992119acc1d99d46e7
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-11-05 13:21:20 -0500

    integrates\refac(build): #5352.30 remove unnecessary code
    - remove unecessary python_package in `authz-matrix/main.nix`

--- makes/foss/units/integrates/back/authz-matrix/main.nix
+++ makes/foss/units/integrates/back/authz-matrix/main.nix
@@ -1,11 +1,9 @@
-{ inputs
-, makeScript
+{ makeScript
 , outputs
 , ...
 }:
 makeScript {
   name = "integrates-back-authz-matrix";
   replace.__argIntegratesBackEnv__ = outputs."/integrates/back/env";
-  searchPaths.pythonPackage39 = [ inputs.nixpkgs.python39Packages.pandas ];
   entrypoint = ./entrypoint.sh;
 }
