473f4119ee6ba616958925a9f4d3bb969ca23d99
Kevin Amado <kamado@fluidattacks.com>
2021-11-08 11:27:29 -0500

    makes\refac(build): #5408 migrate to makes
    - Speed: 19682 loc / 40 days = 492 loc/day
    - TODO: 875 loc
    - ETA: 875 / 492 = 1 days

--- makes/foss/units/skims/config-runtime/pypi/main.nix
+++ makes/foss/units/skims/config-runtime/pypi/main.nix
@@ -2,12 +2,13 @@
 , makePythonPypiEnvironment
 , ...
 }:
-makePythonPypiEnvironment {
+makePythonPypiEnvironment rec {
   name = "skims-runtime";
-  searchPathsRuntime = {
+  searchPathsBuild = {
     bin = [ inputs.nixpkgs.gcc ];
     pythonPackage38 = [ inputs.nixpkgs.python38Packages.pygraphviz ];
   };
+  searchPathsRuntime = searchPathsBuild;
   sourcesYaml = ./sources.yaml;
   withSetuptools_57_4_0 = true;
   withWheel_0_37_0 = true;
