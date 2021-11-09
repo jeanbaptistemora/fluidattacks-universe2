c3f0c682796c57cebb7c9f2ed4da4c24d57e19cd
Diego Restrepo <drestrepo@fluidattacks.com>
2021-11-09 10:16:20 -0500

    makes\refac(build): #5478.23 update makes version
    - update for dynamodb usage

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Fluid Attacks, We hack your software!";
   inputs = {
     flakeCompat = { url = "github:edolstra/flake-compat"; flake = false; };
-    makes = { url = "https://github.com/fluidattacks/makes/archive/4c95c568f14fb84bc0f7af8bd2bceeafffdf7dc2.tar.gz"; flake = false; };
+    makes = { url = "https://github.com/fluidattacks/makes/archive/449933dbac97f785eb26f5f55f4ef322e504c933.tar.gz"; flake = false; };
     nixpkgsSource = { url = "https://github.com/nixos/nixpkgs/archive/932941b79c3dbbef2de9440e1631dfec43956261.tar.gz"; flake = false; };
     nixpkgsSource2 = { url = "https://github.com/nixos/nixpkgs/archive/7138a338b58713e0dea22ddab6a6785abec7376a.tar.gz"; flake = false; };
     nixpkgsSource3 = { url = "https://github.com/nixos/nixpkgs/archive/a1d64d9419422ae9779ab5cada5828127a24e100.tar.gz"; flake = false; };
