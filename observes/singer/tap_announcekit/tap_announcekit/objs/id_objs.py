6a302eadb97bb4340827a6438540113cd1e202df
Daniel Murcia <dmurcia@fluidattacks.com>
2021-11-12 07:32:32 -0500

    observes\feat(back): #5595.3 define label
    - define label obj

--- observes/singer/tap_announcekit/tap_announcekit/objs/id_objs.py
+++ observes/singer/tap_announcekit/tap_announcekit/objs/id_objs.py
@@ -91,6 +91,12 @@ class WidgetId:
 
 
 @dataclass(frozen=True)
+class LabelId:
+    proj: ProjectId
+    id_str: str
+
+
+@dataclass(frozen=True)
 class IndexedObj(SupportsKind2["IndexedObj[_ID, _T]", _ID, _T]):
     id_obj: _ID
     obj: _T
