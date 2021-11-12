be8ac25bc2dbbf8eeeccfe5b725169c320718689
Kevin Amado <kamado@fluidattacks.com>
2021-11-12 14:12:35 -0500

    skims\feat(back): #5597.2 duplicate 011
    - Duplicate finding 011 to change its title

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -1587,6 +1587,26 @@ class FindingEnum(Enum):
             user_interaction=cvss3_model.UserInteraction.required,
         ),
     )
+    F393: FindingMetadata = FindingMetadata.new(
+        auto_approve=True,
+        code="F393",
+        cwe=937,
+        execution_queue=ExecutionQueue.sca,
+        requirements=[262],
+        score=cvss3_model.Score(
+            attack_complexity=cvss3_model.AttackComplexity.low,
+            attack_vector=cvss3_model.AttackVector.network,
+            availability_impact=cvss3_model.AvailabilityImpact.low,
+            confidentiality_impact=cvss3_model.ConfidentialityImpact.low,
+            exploitability=cvss3_model.Exploitability.poc,
+            integrity_impact=cvss3_model.IntegrityImpact.low,
+            privileges_required=cvss3_model.PrivilegesRequired.low,
+            remediation_level=cvss3_model.RemediationLevel.official_fix,
+            report_confidence=cvss3_model.ReportConfidence.confirmed,
+            severity_scope=cvss3_model.SeverityScope.unchanged,
+            user_interaction=cvss3_model.UserInteraction.none,
+        ),
+    )
 
 
 FINDING_ENUM_FROM_STR: Dict[str, FindingEnum] = {
