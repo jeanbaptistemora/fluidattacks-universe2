c84fa00806ef8f180c3918f6e31d195d1c83fe9c
Daniel Murcia <dmurcia@fluidattacks.com>
2021-11-12 07:22:13 -0500

    observes\feat(back): #5595.1 define feed
    - define Feed obj

--- observes/singer/tap_announcekit/tap_announcekit/objs/id_objs.py
+++ observes/singer/tap_announcekit/tap_announcekit/objs/id_objs.py
@@ -79,6 +79,12 @@ class ImageId:
 
 
 @dataclass(frozen=True)
+class FeedId:
+    proj: ProjectId
+    id_str: str
+
+
+@dataclass(frozen=True)
 class IndexedObj(SupportsKind2["IndexedObj[_ID, _T]", _ID, _T]):
     id_obj: _ID
     obj: _T
