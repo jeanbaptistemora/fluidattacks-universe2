73768edff8c2bef55161da3e8bd67d7e9b44ace0
Alejandro Salgado <asalgado@fluidattacks.com>
2021-11-16 10:39:26 -0500

    skims\feat(back): #5603.1 add strict iter_ast
    - Move iter_node to iter_ast in symbolic_eval.utils
    - Add strict parameter to iter_ast

--- skims/skims/symbolic_eval/utils.py
+++ skims/skims/symbolic_eval/utils.py
@@ -13,10 +13,14 @@ from utils import (
 )
 
 
-def iter_nodes(graph: Graph, n_id: str) -> Iterator[str]:
-    yield n_id
+def iter_ast(graph: Graph, n_id: str, strict: bool = False) -> Iterator[str]:
+    if not strict or not g.is_connected_to_cfg(graph, n_id):
+        yield n_id
+
     for c_id in g.adj_ast(graph, n_id):
-        yield from iter_nodes(graph, c_id)
+        if strict and g.is_connected_to_cfg(graph, c_id):
+            continue
+        yield from iter_ast(graph, c_id)
 
 
 def iter_paths(graph: Graph, cfg_n_id: str) -> Iterator[Path]:
@@ -35,14 +39,16 @@ def get_paths(graph: Graph, n_id: str) -> Iterator[Path]:
     yield from iter_paths(graph, g.lookup_first_cfg_parent(graph, n_id))
 
 
-def filter_nodes(graph: Graph, n_id: str, types: Set[str]) -> Iterator[str]:
-    for c_id in iter_nodes(graph, n_id):
+def filter_ast(
+    graph: Graph, n_id: str, types: Set[str], strict: bool = False
+) -> Iterator[str]:
+    for c_id in iter_ast(graph, n_id, strict):
         if graph.nodes[c_id]["label_type"] in types:
             yield c_id
 
 
 def search_method_invocation(graph: Graph, methods: Set[str]) -> Iterator[str]:
-    for n_id in filter_nodes(graph, "1", {"MethodInvocation"}):
+    for n_id in filter_ast(graph, "1", {"MethodInvocation"}):
         for method in methods:
             if method in graph.nodes[n_id]["expression"]:
                 yield n_id
