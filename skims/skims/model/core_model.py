b281b860be886e2b2d067e55a9ffb6bacdb68f98
Juan Echeverri <jecheverri@fluidattacks.com>
2021-11-17 11:19:31 -0500

    skims\feat(back): #5589.1 add f134 model
    - Add f134 to core_model.py
    - Update F134.yaml in static module
    - Update .json test files

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -1267,6 +1267,26 @@ class FindingEnum(Enum):
             user_interaction=cvss3_model.UserInteraction.required,
         ),
     )
+    F134: FindingMetadata = FindingMetadata.new(
+        auto_approve=True,
+        code="F134",
+        cwe=16,
+        execution_queue=ExecutionQueue.http,
+        requirements=[62, 266, 349],
+        score=cvss3_model.Score(
+            attack_complexity=cvss3_model.AttackComplexity.low,
+            attack_vector=cvss3_model.AttackVector.network,
+            availability_impact=cvss3_model.AvailabilityImpact.none,
+            confidentiality_impact=cvss3_model.ConfidentialityImpact.none,
+            exploitability=cvss3_model.Exploitability.poc,
+            integrity_impact=cvss3_model.IntegrityImpact.low,
+            privileges_required=cvss3_model.PrivilegesRequired.none,
+            remediation_level=cvss3_model.RemediationLevel.unavailable,
+            report_confidence=cvss3_model.ReportConfidence.confirmed,
+            severity_scope=cvss3_model.SeverityScope.unchanged,
+            user_interaction=cvss3_model.UserInteraction.required,
+        ),
+    )
     F160: FindingMetadata = FindingMetadata.new(
         auto_approve=False,
         code="F160",
