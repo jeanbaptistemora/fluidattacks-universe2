70600d8fc5439a62cb6badaf4abb5b6c1cea2aa0
Daniel Salazar <dsalazar@fluidattacks.com>
2021-11-19 14:59:36 -0500

    makes\refac(infra): #5473 rm makes dev
    - Remove makes dev user

---
+++ makes/foss/modules/makes/users/makes/infra/user.tf
@@ -0,0 +1,27 @@
+resource "aws_iam_user" "prod" {
+  name = "serves-prod"
+  path = "/user-provision/"
+
+  tags = {
+    "Name"            = "serves-prod"
+    "management:area" = "cost"
+    "management:type" = "product"
+  }
+}
+
+resource "aws_iam_access_key" "prod-key-1" {
+  user = "serves-prod"
+}
+
+resource "aws_iam_access_key" "prod-key-2" {
+  user = "serves-prod"
+}
+
+module "publish_credentials_prod" {
+  source       = "../../modules/publish_credentials"
+  gitlab_token = var.gitlab_token
+  key_1        = aws_iam_access_key.prod-key-1
+  key_2        = aws_iam_access_key.prod-key-2
+  prefix       = "MAKES_PROD"
+  protected    = true
+}
