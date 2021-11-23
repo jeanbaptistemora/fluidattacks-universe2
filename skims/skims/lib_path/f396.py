d9b6afa86007943211aadd2d37909907beb3b065
Alejandro Trujillo <atrujillo@fluidattacks.com>
2021-11-23 11:59:21 -0500

    skims\feat(back): #5381.122 f396
    - migrated method
    - added translation
    - Speed: 3 cfn_methods / 2 days = 1.5 cfn_methods/day
    - TODO: 31 cfn_methods
    - ETA: 31 / 1.5 = 20.67 days
    - no-deltas-test

---
+++ skims/skims/lib_path/f396.py
@@ -0,0 +1,121 @@
+from aioextensions import (
+    in_process,
+)
+from aws.model import (
+    AWSKmsKey,
+)
+from lib_path.common import (
+    EXTENSIONS_CLOUDFORMATION,
+    FALSE_OPTIONS,
+    get_line_by_extension,
+    get_vulnerabilities_from_aws_iterator_blocking,
+    SHIELD,
+)
+from metaloaders.model import (
+    Node,
+)
+from model import (
+    core_model,
+)
+from parse_cfn.loader import (
+    load_templates,
+)
+from parse_cfn.structure import (
+    iter_kms_keys,
+)
+from state.cache import (
+    CACHE_ETERNALLY,
+)
+from typing import (
+    Any,
+    Awaitable,
+    Callable,
+    Iterator,
+    List,
+    Union,
+)
+from utils.function import (
+    get_node_by_keys,
+    TIMEOUT_1MIN,
+)
+
+
+def _cfn_kms_key_is_key_rotation_absent_or_disabled_iter_vulns(
+    file_ext: str,
+    keys_iterator: Iterator[Union[AWSKmsKey, Node]],
+) -> Iterator[Union[AWSKmsKey, Node]]:
+    for key in keys_iterator:
+        en_key_rot = get_node_by_keys(key, ["EnableKeyRotation"])
+        if not isinstance(en_key_rot, Node):
+            yield AWSKmsKey(
+                column=key.start_column,
+                data=key.data,
+                line=get_line_by_extension(key.start_line, file_ext),
+            )
+        elif en_key_rot.raw in FALSE_OPTIONS:
+            yield en_key_rot
+
+
+def _cfn_kms_key_is_key_rotation_absent_or_disabled(
+    content: str,
+    file_ext: str,
+    path: str,
+    template: Any,
+) -> core_model.Vulnerabilities:
+    return get_vulnerabilities_from_aws_iterator_blocking(
+        content=content,
+        description_key=(
+            "src.lib_path.f396.kms_key_is_key_rotation_absent_or_disabled"
+        ),
+        finding=core_model.FindingEnum.F396,
+        path=path,
+        statements_iterator=(
+            _cfn_kms_key_is_key_rotation_absent_or_disabled_iter_vulns(
+                file_ext=file_ext,
+                keys_iterator=iter_kms_keys(template=template),
+            )
+        ),
+    )
+
+
+@CACHE_ETERNALLY
+@SHIELD
+@TIMEOUT_1MIN
+async def cfn_kms_key_is_key_rotation_absent_or_disabled(
+    content: str,
+    file_ext: str,
+    path: str,
+    template: Any,
+) -> core_model.Vulnerabilities:
+    return await in_process(
+        _cfn_kms_key_is_key_rotation_absent_or_disabled,
+        content=content,
+        file_ext=file_ext,
+        path=path,
+        template=template,
+    )
+
+
+@SHIELD
+async def analyze(
+    content_generator: Callable[[], Awaitable[str]],
+    file_extension: str,
+    path: str,
+    **_: None,
+) -> List[Awaitable[core_model.Vulnerabilities]]:
+    coroutines: List[Awaitable[core_model.Vulnerabilities]] = []
+    if file_extension in EXTENSIONS_CLOUDFORMATION:
+        content = await content_generator()
+        async for template in load_templates(
+            content=content, fmt=file_extension
+        ):
+            coroutines.append(
+                cfn_kms_key_is_key_rotation_absent_or_disabled(
+                    content=content,
+                    file_ext=file_extension,
+                    path=path,
+                    template=template,
+                )
+            )
+
+    return coroutines
