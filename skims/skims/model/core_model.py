fe9260b8a9c99fddf7f457bea08d4316fc42a466
Alejandro Trujillo <atrujillo@fluidattacks.com>
2021-11-23 11:11:57 -0500

    skims\feat(back): #5381.121 requirements f396
    - added f396 to manifests and test configs
    - created f396 translations
    - added f396 FindingEnum
    - Speed: 3 cfn_methods / 2 days = 1.5 cfn_methods/day
    - TODO: 31 cfn_methods
    - ETA: 31 / 1.5 = 20.67 days

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -1647,6 +1647,26 @@ class FindingEnum(Enum):
             user_interaction=cvss3_model.UserInteraction.none,
         ),
     )
+    F396: FindingMetadata = FindingMetadata.new(
+        auto_approve=False,
+        code="F396",
+        cwe=255,
+        execution_queue=ExecutionQueue.aws,
+        requirements=[266],
+        score=cvss3_model.Score(
+            attack_complexity=cvss3_model.AttackComplexity.high,
+            attack_vector=cvss3_model.AttackVector.network,
+            availability_impact=cvss3_model.AvailabilityImpact.none,
+            confidentiality_impact=cvss3_model.ConfidentialityImpact.low,
+            exploitability=cvss3_model.Exploitability.poc,
+            integrity_impact=cvss3_model.IntegrityImpact.none,
+            privileges_required=cvss3_model.PrivilegesRequired.none,
+            remediation_level=cvss3_model.RemediationLevel.official_fix,
+            report_confidence=cvss3_model.ReportConfidence.reasonable,
+            severity_scope=cvss3_model.SeverityScope.unchanged,
+            user_interaction=cvss3_model.UserInteraction.none,
+        ),
+    )
 
 
 FINDING_ENUM_FROM_STR: Dict[str, FindingEnum] = {
