43d0c7173e5d6da72481bd141875b258661112b5
Daniel Salazar <dsalazar@fluidattacks.com>
2021-11-23 14:55:54 -0500

    makes\refac(infra): #5473 rm sorts dev
    - Remove sorts dev user

--- makes/foss/modules/makes/users/sorts/infra/user.tf
+++ makes/foss/modules/makes/users/sorts/infra/user.tf
@@ -1,26 +1,20 @@
+resource "aws_iam_user" "sorts_prod" {
+  name = "sorts_prod"
+  path = "/user_provision/"
+
+  tags = {
+    "Name"            = "sorts_prod"
+    "management:area" = "cost"
+    "management:type" = "product"
+  }
+}
+
 resource "aws_iam_access_key" "sorts_prod_key-1" {
   user = "sorts_prod"
 }
 
 resource "aws_iam_access_key" "sorts_prod_key-2" {
   user = "sorts_prod"
-}
-
-resource "aws_iam_access_key" "sorts_dev_key-1" {
-  user = "sorts_dev"
-}
-
-resource "aws_iam_access_key" "sorts_dev_key-2" {
-  user = "sorts_dev"
-}
-
-module "publish_credentials_dev" {
-  source       = "../../modules/publish_credentials"
-  gitlab_token = var.gitlab_token
-  key_1        = aws_iam_access_key.sorts_dev_key-1
-  key_2        = aws_iam_access_key.sorts_dev_key-2
-  prefix       = "SORTS_DEV"
-  protected    = false
 }
 
 module "publish_credentials_prod" {
