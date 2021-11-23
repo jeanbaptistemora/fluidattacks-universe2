e8dc4741d62ec5ec81ba700ae23273409db57471
Daniel Salazar <dsalazar@fluidattacks.com>
2021-11-23 12:36:56 -0500

    makes\fix(infra): #5634 remove https
    - Remove https from custom_domain
    as it is not necessary

--- makes/foss/modules/makes/status/infra/dashboard.tf
+++ makes/foss/modules/makes/status/infra/dashboard.tf
@@ -1,6 +1,6 @@
 resource "checkly_dashboard" "main" {
   custom_url      = "fluidattacks"
-  custom_domain   = "https://status.fluidattacks.com/"
+  custom_domain   = "status.fluidattacks.com"
   logo            = "https://res.cloudinary.com/fluid-attacks/image/upload/q_auto,f_auto/v1619554789/airs/logo-fluid-attacks-light_lsckin.webp"
   header          = "Status Page"
   refresh_rate    = 60
