4d0f66815e53097fe70cbab1dbc77af99be685e6
Alejandro Trujillo <atrujillo@fluidattacks.com>
2021-11-23 15:24:25 -0500

    skims\feat(back): #5381.127 requirements f325
    - added f325 to manifests and test configs
    - created f325 translations
    - added f325 FindingEnum
    - Speed: 3 cfn_methods / 2 days = 1.5 cfn_methods/day
    - TODO: 30 cfn_methods
    - ETA: 30 / 1.5 = 20 days
    - no-deltas-test

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -1527,6 +1527,26 @@ class FindingEnum(Enum):
             user_interaction=cvss3_model.UserInteraction.none,
         ),
     )
+    F325: FindingMetadata = FindingMetadata.new(
+        auto_approve=True,
+        code="F325",
+        cwe=250,
+        execution_queue=ExecutionQueue.aws,
+        requirements=[95, 96, 186],
+        score=cvss3_model.Score(
+            attack_complexity=cvss3_model.AttackComplexity.low,
+            attack_vector=cvss3_model.AttackVector.network,
+            availability_impact=cvss3_model.AvailabilityImpact.low,
+            confidentiality_impact=cvss3_model.ConfidentialityImpact.low,
+            exploitability=cvss3_model.Exploitability.poc,
+            integrity_impact=cvss3_model.IntegrityImpact.low,
+            privileges_required=cvss3_model.PrivilegesRequired.low,
+            remediation_level=cvss3_model.RemediationLevel.unavailable,
+            report_confidence=cvss3_model.ReportConfidence.unknown,
+            severity_scope=cvss3_model.SeverityScope.unchanged,
+            user_interaction=cvss3_model.UserInteraction.none,
+        ),
+    )
     F335: FindingMetadata = FindingMetadata.new(
         auto_approve=True,
         code="F335",
