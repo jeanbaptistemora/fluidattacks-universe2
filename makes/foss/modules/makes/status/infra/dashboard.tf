66163ba29eda066c72624ed1a0b36a571c9df05b
Daniel Salazar <dsalazar@fluidattacks.com>
2021-11-23 12:28:19 -0500

    makes\feat(infra): #5634 dashboard as code
    - Update checkly provider
    - Implement status dashboard as code

---
+++ makes/foss/modules/makes/status/infra/dashboard.tf
@@ -0,0 +1,15 @@
+resource "checkly_dashboard" "main" {
+  custom_url      = "fluidattacks"
+  custom_domain   = "https://status.fluidattacks.com/"
+  logo            = "https://res.cloudinary.com/fluid-attacks/image/upload/q_auto,f_auto/v1619554789/airs/logo-fluid-attacks-light_lsckin.webp"
+  header          = "Status Page"
+  refresh_rate    = 60
+  paginate        = true
+  pagination_rate = 60
+  hide_tags       = true
+  width           = "FULL"
+
+  tags = [
+    "production",
+  ]
+}
--- makes/foss/modules/makes/status/infra/main.tf
+++ makes/foss/modules/makes/status/infra/main.tf
@@ -8,7 +8,7 @@ terraform {
     }
     checkly = {
       source  = "checkly/checkly"
-      version = "1.2.0"
+      version = "1.3.0"
     }
   }
 
