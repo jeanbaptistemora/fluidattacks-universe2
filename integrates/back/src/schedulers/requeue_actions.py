8ba9358e417b009dd53ec5a10a59120fc81239f6
Andres Aguirre <aaguirre@fluidattacks.com>
2021-11-24 20:04:27 +0000

    integrates\refac(back): #5469.66 requeue finished jobs
    - Only requeue jobs that are in the SUCCEEDED or FAILED status

--- integrates/back/src/schedulers/requeue_actions.py
+++ integrates/back/src/schedulers/requeue_actions.py
@@ -14,6 +14,32 @@ from typing import (
 
 async def requeue_actions() -> None:
     pending_actions: List[BatchProcessing] = await batch_dal.get_actions()
+    action_queues = {
+        pending_action.queue for pending_action in pending_actions
+    }
+    queues_jobs = await batch_dal.list_queues_jobs(
+        queues=list(action_queues),
+        statuses=[
+            batch_dal.JobStatus.SUBMITTED,
+            batch_dal.JobStatus.PENDING,
+            batch_dal.JobStatus.RUNNABLE,
+            batch_dal.JobStatus.STARTING,
+            batch_dal.JobStatus.RUNNING,
+        ],
+    )
+    non_executed_jobs = await batch_dal.decribe_jobs(
+        set(queues_jobs.id for queues_jobs in queues_jobs)
+    )
+    non_executed_jobs_info = {
+        (
+            job_description.container.command[4],  # action_name
+            job_description.container.command[5],  # subject
+            job_description.container.command[6],  # entity
+            job_description.container.command[7],  # time
+            job_description.container.command[8],  # additional_info
+        )
+        for job_description in non_executed_jobs
+    }
     await collect(
         [
             batch_dal.put_action_to_batch(
@@ -25,7 +51,14 @@ async def requeue_actions() -> None:
                 queue=action.queue,
             )
             for action in pending_actions
-            if action.action_name != "refresh_toe_lines"
+            if (
+                action.action_name,
+                action.subject,
+                action.entity,
+                action.time,
+                action.additional_info,
+            )
+            not in non_executed_jobs_info
         ],
         workers=20,
     )
