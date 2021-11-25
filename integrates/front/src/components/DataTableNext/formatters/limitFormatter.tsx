6993f3e3c24d4a1c029f1e8dc76d5a93f2d3cd5f
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-11-25 18:07:40 -0500

    integrates\refac(front): #5652.1 update formatter
    - update where column `limitFormatter` to display each location in a
    different line in `group_finding_view`

--- integrates/front/src/components/DataTableNext/formatters/limitFormatter.tsx
+++ integrates/front/src/components/DataTableNext/formatters/limitFormatter.tsx
@@ -1,23 +1,28 @@
-/* eslint-disable react/forbid-component-props
-  --------
-  We need className to override default styles from react-bootstrap.
-*/
 import React from "react";
 
+import styles from "components/DataTableNext/index.css";
 import { translate } from "utils/translations/translate";
 
 export const limitFormatter: (value: string) => JSX.Element = (
   value: string
 ): JSX.Element => {
   const linesLimit: number = 15;
-  const valueArray: string[] = value
-    .split(",")
-    .map((element: string): string => element.trim());
-  const newValue: string = valueArray.slice(0, linesLimit).join(", ");
+  const valueArray: string[] = Array.from(
+    new Set(value.split(",").map((element: string): string => element.trim()))
+  );
 
   return (
     <div>
-      <p>{newValue}</p>
+      {valueArray.slice(0, linesLimit).map(
+        (formatValue: string, index: number): JSX.Element => (
+          <p
+            className={`mb0 ${styles.textMesure} tl truncate`}
+            key={index.toString()}
+          >
+            {formatValue}
+          </p>
+        )
+      )}
       {valueArray.length > linesLimit && (
         <p>{translate.t("dataTableNext.more")}</p>
       )}
