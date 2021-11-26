d9e8b89749e3186b38b55396ee859d3e31a20600
Jonathan Mesa <jmesa@fluidattacks.com>
2021-11-26 14:47:42 -0500

    integrates\refac(back): #4329.671 migrate vulns
    - Speed: 68 fn / 17 days = 4 fn/day
    - TODO: 21 fn
    - ETA: 21 / 4 = 5 days

--- integrates/back/src/api/resolvers/finding/vulnerabilities_to_reattack.py
+++ integrates/back/src/api/resolvers/finding/vulnerabilities_to_reattack.py
@@ -1,37 +1,25 @@
-from aiodataloader import (
-    DataLoader,
-)
-from custom_types import (
-    Finding as FindingType,
-    Vulnerability,
-)
 from db_model.findings.types import (
     Finding,
+)
+from db_model.vulnerabilities.types import (
+    Vulnerability,
 )
 from graphql.type.definition import (
     GraphQLResolveInfo,
 )
 from newutils.vulnerabilities import (
-    filter_open_vulns,
-    is_reattack_requested,
+    filter_open_vulns_new,
+    filter_remediated,
 )
 from typing import (
-    Dict,
     List,
+    Tuple,
 )
 
 
 async def resolve(
     parent: Finding, info: GraphQLResolveInfo, **_kwargs: None
-) -> List[Dict[str, FindingType]]:
-    finding_id: str = parent.id
-    finding_vulns_loader: DataLoader = info.context.loaders.finding_vulns_nzr
-    vulns: List[Vulnerability] = await finding_vulns_loader.load(finding_id)
-    vulnerabilities_to_reattack: List[Vulnerability] = list(
-        filter(is_reattack_requested, vulns)
-    )
-    vulnerabilities_to_reattack = filter_open_vulns(
-        vulnerabilities_to_reattack
-    )
-
-    return vulnerabilities_to_reattack
+) -> List[Vulnerability]:
+    finding_vulns_loader = info.context.loaders.finding_vulns_nzr_typed
+    vulns: Tuple[Vulnerability] = await finding_vulns_loader.load(parent.id)
+    return list(filter_open_vulns_new(filter_remediated(vulns)))
