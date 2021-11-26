7c598e11c137a4505a750a09a591579f010319bd
Alejandro Trujillo <atrujillo@fluidattacks.com>
2021-11-26 12:40:29 -0500

    skims\feat(back): #5381.144 f250
    - migrated method
    - added translation
    - Speed: 3 cfn_methods / 2 days = 1.5 cfn_methods/day
    - TODO: 29 cfn_methods
    - ETA: 29 / 1.5 = 19.33 days
    - no-deltas-test

---
+++ skims/skims/lib_path/f250.py
@@ -0,0 +1,116 @@
+from aioextensions import (
+    in_process,
+)
+from aws.model import (
+    AWSFSxFileSystem,
+)
+from lib_path.common import (
+    EXTENSIONS_CLOUDFORMATION,
+    get_line_by_extension,
+    get_vulnerabilities_from_aws_iterator_blocking,
+    SHIELD,
+)
+from metaloaders.model import (
+    Node,
+)
+from model import (
+    core_model,
+)
+from parse_cfn.loader import (
+    load_templates,
+)
+from parse_cfn.structure import (
+    iter_fsx_file_systems,
+)
+from state.cache import (
+    CACHE_ETERNALLY,
+)
+from typing import (
+    Any,
+    Awaitable,
+    Callable,
+    Iterator,
+    List,
+    Union,
+)
+from utils.function import (
+    get_node_by_keys,
+    TIMEOUT_1MIN,
+)
+
+
+def _cfn_fsx_has_unencrypted_volumes_iter_vulns(
+    file_ext: str,
+    fsx_iterator: Iterator[Union[AWSFSxFileSystem, Node]],
+) -> Iterator[Union[AWSFSxFileSystem, Node]]:
+    for fsx in fsx_iterator:
+        kms_key_id = get_node_by_keys(fsx, ["KmsKeyId"])
+        if not isinstance(kms_key_id, Node):
+            yield AWSFSxFileSystem(
+                column=fsx.start_column,
+                data=fsx.data,
+                line=get_line_by_extension(fsx.start_line, file_ext),
+            )
+
+
+def _cfn_fsx_has_unencrypted_volumes(
+    content: str,
+    file_ext: str,
+    path: str,
+    template: Any,
+) -> core_model.Vulnerabilities:
+    return get_vulnerabilities_from_aws_iterator_blocking(
+        content=content,
+        description_key=("src.lib_path.f250.fsx_has_unencrypted_volumes"),
+        finding=core_model.FindingEnum.F250,
+        path=path,
+        statements_iterator=(
+            _cfn_fsx_has_unencrypted_volumes_iter_vulns(
+                file_ext=file_ext,
+                fsx_iterator=iter_fsx_file_systems(template=template),
+            )
+        ),
+    )
+
+
+@CACHE_ETERNALLY
+@SHIELD
+@TIMEOUT_1MIN
+async def cfn_fsx_has_unencrypted_volumes(
+    content: str,
+    file_ext: str,
+    path: str,
+    template: Any,
+) -> core_model.Vulnerabilities:
+    return await in_process(
+        _cfn_fsx_has_unencrypted_volumes,
+        content=content,
+        file_ext=file_ext,
+        path=path,
+        template=template,
+    )
+
+
+@SHIELD
+async def analyze(
+    content_generator: Callable[[], Awaitable[str]],
+    file_extension: str,
+    path: str,
+    **_: None,
+) -> List[Awaitable[core_model.Vulnerabilities]]:
+    coroutines: List[Awaitable[core_model.Vulnerabilities]] = []
+    if file_extension in EXTENSIONS_CLOUDFORMATION:
+        content = await content_generator()
+        async for template in load_templates(
+            content=content, fmt=file_extension
+        ):
+            coroutines.append(
+                cfn_fsx_has_unencrypted_volumes(
+                    content=content,
+                    file_ext=file_extension,
+                    path=path,
+                    template=template,
+                )
+            )
+
+    return coroutines
