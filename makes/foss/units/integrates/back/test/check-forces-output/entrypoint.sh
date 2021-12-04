ec5db31f67b50227ec7750034693d9970b398bcd
Juan Chaves <jchaves@fluidattacks.com>
2021-12-04 11:02:03 -0500

    integrates\refac(build): #5547.8 validate forces output
    - Added another keyword to check for in the check output
    job

--- makes/foss/units/integrates/back/test/check-forces-output/entrypoint.sh
+++ makes/foss/units/integrates/back/test/check-forces-output/entrypoint.sh
@@ -1,12 +1,12 @@
 # shellcheck shell=bash
 
 function check_output() {
-  if ! grep -q "ERROR\|TypeError\|IndexError" "$1"; then
+  if ! grep -q "ERROR\|TypeError\|IndexError\|Traceback" "$1"; then
     echo "[INFO] All clear!"
     result_code=0
   else
     echo "[ERROR] Failed check"
-    grep -q "ERROR\|TypeError\|IndexError" "$1"
+    grep -q "ERROR\|TypeError\|IndexError\|Traceback" "$1"
     result_code=1
   fi
 }
@@ -24,7 +24,7 @@ function main {
     && DAEMON=true dynamodb-for-integrates \
     && echo "[INFO] Running DevSecOps agent check..." \
     && mkdir -p "${out}" \
-    && forces --token "${TEST_FORCES_TOKEN}" > "${out}/forces-output.log" || true \
+    && forces --token "${TEST_FORCES_TOKEN}" --lax > "${out}/forces-output.log" || true \
     && check_output "${out}/forces-output.log" \
     && rm -rf "${out}" \
     && return "${result_code}" \
