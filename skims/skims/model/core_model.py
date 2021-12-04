344043541cc26a6d38d60505c329fb764e78303f
Juan Echeverri <jecheverri@fluidattacks.com>
2021-12-03 20:33:10 -0500

    skims\feat(back): #5381.181 add model f177
    - Add F177.yaml to vulnerabilities
    - Add F177 entry to core_model.FindingEnum
    - Add F177 to json config files

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -1287,6 +1287,26 @@ class FindingEnum(Enum):
             user_interaction=cvss3_model.UserInteraction.required,
         ),
     )
+    F177: FindingMetadata = FindingMetadata.new(
+        auto_approve=False,
+        code="F177",
+        cwe=16,
+        execution_queue=ExecutionQueue.aws,
+        requirements=[266],
+        score=cvss3_model.Score(
+            attack_complexity=cvss3_model.AttackComplexity.high,
+            attack_vector=cvss3_model.AttackVector.adjacent,
+            availability_impact=cvss3_model.AvailabilityImpact.none,
+            confidentiality_impact=cvss3_model.ConfidentialityImpact.low,
+            exploitability=cvss3_model.Exploitability.poc,
+            integrity_impact=cvss3_model.IntegrityImpact.none,
+            privileges_required=cvss3_model.PrivilegesRequired.none,
+            remediation_level=cvss3_model.RemediationLevel.official_fix,
+            report_confidence=cvss3_model.ReportConfidence.unknown,
+            severity_scope=cvss3_model.SeverityScope.unchanged,
+            user_interaction=cvss3_model.UserInteraction.none,
+        ),
+    )
     F200: FindingMetadata = FindingMetadata.new(
         auto_approve=False,
         code="F200",
