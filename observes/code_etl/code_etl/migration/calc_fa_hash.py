ded628c0c12d88b31aaed56bdea7a98aef750463
Daniel Murcia <dmurcia@fluidattacks.com>
2021-12-06 12:11:27 -0500

    observes\fix(back): #5685.9 raw decode error
    - handle raw decode error

--- observes/code_etl/code_etl/migration/calc_fa_hash.py
+++ observes/code_etl/code_etl/migration/calc_fa_hash.py
@@ -32,6 +32,9 @@ from purity.v1 import (
 from returns.io import (
     IO,
 )
+from returns.maybe import (
+    Maybe,
+)
 from returns.unsafe import (
     unsafe_perform_io,
 )
@@ -40,18 +43,40 @@ from typing import (
 )
 
 
+class RawDecodeError(Exception):
+    def __init__(
+        self,
+        target: str,
+        raw: Any,
+    ):
+        super().__init__(
+            f"TypeError when trying to build `{target}` "
+            f"from raw obj `{str(raw)}`"
+        )
+
+
 def _calc_id(raw: FrozenList[Any]) -> CommitDataId:
-    data = CommitData(
-        User(raw[0], raw[1]),
-        datetime.fromisoformat(raw[2]),
-        User(raw[3], raw[4]),
-        datetime.fromisoformat(raw[5]),
-        raw[6],
-        raw[7],
-        Deltas(raw[8], raw[9], raw[10], raw[11]),
-    )
-    _id = CommitDataId(raw[11], raw[12], CommitId(raw[13], gen_fa_hash(data)))
-    return _id
+    try:
+        data = CommitData(
+            User(raw[0], raw[1]),
+            datetime.fromisoformat(raw[2]),
+            User(raw[3], raw[4]),
+            datetime.fromisoformat(raw[5]),
+            raw[6],
+            raw[7],
+            Deltas(raw[8], raw[9], raw[10], raw[11]),
+        )
+        _id = CommitDataId(
+            raw[11], raw[12], CommitId(raw[13], gen_fa_hash(data))
+        )
+        return _id
+    except TypeError as err:
+        raise RawDecodeError("CommitId", raw) from err
+
+
+def _try_calc_id(raw: FrozenList[Any]) -> Maybe[CommitDataId]:
+    _raw = Maybe.from_optional(raw if raw[0] is not None else None)
+    return _raw.map(_calc_id)
 
 
 def update_query(schema: SchemaID, cid: CommitDataId) -> Query:
@@ -106,7 +131,9 @@ def calc_hash(client: Client, schema: SchemaID, namespace: str) -> IO[None]:
     while True:
         items = unsafe_perform_io(client.cursor.fetch_many(2000))
         for item in items:
-            client.cursor.execute_query(update_query(schema, _calc_id(item)))
+            _try_calc_id(item).map(
+                lambda c: client.cursor.execute_query(update_query(schema, c))
+            )
         if len(items) == 0:
             break
     return IO(None)
