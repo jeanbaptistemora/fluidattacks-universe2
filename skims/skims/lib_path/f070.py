ca2cc2cff4067ee255af5a838f9c9ff48e506ad5
Alejandro Trujillo <atrujillo@fluidattacks.com>
2021-12-06 14:28:11 -0500

    skims\feat(back): #5381.187 f070
    - migrated method
    - added translation
    - Speed: 3 cfn_methods / 2 days = 1.5 cfn_methods/day
    - TODO: 19 cfn_methods
    - ETA: 19 / 1.5 = 12.67 days
    - no-deltas-test

--- skims/skims/lib_path/f070.py
+++ skims/skims/lib_path/f070.py
@@ -1,9 +1,13 @@
 from aioextensions import (
     in_process,
 )
+from aws.model import (
+    AWSElbV2,
+)
 from lib_path.common import (
     EXTENSIONS_TERRAFORM,
     get_aws_iterator,
+    get_line_by_extension,
     get_vulnerabilities_from_iterator_blocking,
     SHIELD,
 )
@@ -12,6 +16,9 @@ from metaloaders.model import (
 )
 from model import (
     core_model,
+)
+from parse_cfn.structure import (
+    iter_elb2_load_listeners,
 )
 from parse_hcl2.loader import (
     load as load_terraform,
@@ -34,6 +41,7 @@ from typing import (
     Union,
 )
 from utils.function import (
+    get_node_by_keys,
     TIMEOUT_1MIN,
 )
 
@@ -52,6 +60,30 @@ def tfm_lb_target_group_insecure_port_iterate_vulnerabilities(
                 and elem.val != 443
             ):
                 yield elem
+
+
+def _cfn_elb2_uses_insecure_security_policy_iterate_vulnerabilities(
+    file_ext: str,
+    listeners_iterator: Iterator[Union[AWSElbV2, Node]],
+) -> Iterator[Union[AWSElbV2, Node]]:
+    for listener in listeners_iterator:
+        acceptable = (
+            "ELBSecurityPolicy-2016-08",
+            "ELBSecurityPolicy-TLS-1-1-2017-01",
+            "ELBSecurityPolicy-FS-2018-06",
+            "ELBSecurityPolicy-TLS-1-2-Ext-2018-06",
+        )
+        ssl_policy = listener.raw.get("SslPolicy", "")
+        if ssl_policy not in acceptable:
+            ssl_pol_node = get_node_by_keys(listener, ["SslPolicy"])
+            if isinstance(ssl_pol_node, Node):
+                yield ssl_pol_node
+            else:
+                yield AWSElbV2(
+                    column=listener.start_column,
+                    data=listener.data,
+                    line=get_line_by_extension(listener.start_line, file_ext),
+                )
 
 
 def _tfm_lb_target_group_insecure_port(
@@ -73,6 +105,27 @@ def _tfm_lb_target_group_insecure_port(
     )
 
 
+def _cfn_elb2_uses_insecure_security_policy(
+    content: str,
+    file_ext: str,
+    path: str,
+    template: Any,
+) -> core_model.Vulnerabilities:
+    return get_vulnerabilities_from_iterator_blocking(
+        content=content,
+        cwe={_FINDING_F070_CWE},
+        description_key="src.lib_path.f070.elb2_uses_insecure_security_policy",
+        finding=_FINDING_F070,
+        iterator=get_aws_iterator(
+            _cfn_elb2_uses_insecure_security_policy_iterate_vulnerabilities(
+                file_ext=file_ext,
+                listeners_iterator=iter_elb2_load_listeners(template=template),
+            )
+        ),
+        path=path,
+    )
+
+
 @CACHE_ETERNALLY
 @SHIELD
 @TIMEOUT_1MIN
@@ -86,6 +139,24 @@ async def tfm_lb_target_group_insecure_port(
         content=content,
         path=path,
         model=model,
+    )
+
+
+@CACHE_ETERNALLY
+@SHIELD
+@TIMEOUT_1MIN
+async def cfn_elb2_uses_insecure_security_policy(
+    content: str,
+    file_ext: str,
+    path: str,
+    template: Any,
+) -> core_model.Vulnerabilities:
+    return await in_process(
+        _cfn_elb2_uses_insecure_security_policy,
+        content=content,
+        file_ext=file_ext,
+        path=path,
+        template=template,
     )
 
 
