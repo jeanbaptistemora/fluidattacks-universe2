a73d122e804d658b53d50a1298c3d38570987ce5
Alejandro Salgado <asalgado@fluidattacks.com>
2021-12-07 14:32:31 -0500

    skims\feat(back): #5538.46 add obj creation case
    - Add evaluation case for an object creation

---
+++ skims/skims/symbolic_eval/cases/object_creation.py
@@ -0,0 +1,22 @@
+from model.core_model import (
+    FindingEnum,
+)
+from symbolic_eval.types import (
+    Evaluator,
+    SymbolicEvalArgs,
+)
+from typing import (
+    Dict,
+)
+
+FINDING_EVALUATORS: Dict[FindingEnum, Evaluator] = {}
+
+
+def evaluate(args: SymbolicEvalArgs) -> bool:
+    if al_id := args.graph.nodes[args.n_id].get("arguments_id"):
+        args.evaluation[args.n_id] = args.generic(args.fork_n_id(al_id))
+
+    if finding_evaluator := FINDING_EVALUATORS.get(args.finding):
+        args.evaluation[args.n_id] = finding_evaluator(args)
+
+    return args.evaluation[args.n_id]
