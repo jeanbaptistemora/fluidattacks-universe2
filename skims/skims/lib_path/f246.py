83dd457f2676e55aecd70c7d2541d3728d80044f
Alejandro Trujillo <atrujillo@fluidattacks.com>
2021-12-09 13:43:06 -0500

    skims\feat(back): #5381.195 f246
    - migrated method
    - added translation
    - Speed: 3 cfn_methods / 2 days = 1.5 cfn_methods/day
    - TODO: 18 cfn_methods
    - ETA: 18 / 1.5 = 12 days

--- skims/skims/lib_path/f246.py
+++ skims/skims/lib_path/f246.py
@@ -1,17 +1,25 @@
 from aioextensions import (
     in_process,
 )
+from aws.model import (
+    AWSRdsCluster,
+)
 from lib_path.common import (
     EXTENSIONS_TERRAFORM,
     get_aws_iterator,
+    get_line_by_extension,
     get_vulnerabilities_from_iterator_blocking,
     SHIELD,
+    TRUE_OPTIONS,
 )
 from metaloaders.model import (
     Node,
 )
 from model import (
     core_model,
+)
+from parse_cfn.structure import (
+    iter_rds_clusters_and_instances,
 )
 from parse_hcl2.loader import (
     load as load_terraform,
@@ -23,6 +31,9 @@ from parse_hcl2.structure import (
 from parse_hcl2.tokens import (
     Attribute,
 )
+from state.cache import (
+    CACHE_ETERNALLY,
+)
 from typing import (
     Any,
     Awaitable,
@@ -32,6 +43,7 @@ from typing import (
     Union,
 )
 from utils.function import (
+    get_node_by_keys,
     TIMEOUT_1MIN,
 )
 
@@ -65,6 +77,24 @@ def tfm_db_has_unencrypted_storage_iterate_vulnerabilities(
                     yield elem
         if not protection_attr:
             yield bucket
+
+
+def _cfn_rds_has_unencrypted_storage_iterate_vulnerabilities(
+    file_ext: str,
+    rds_iterator: Iterator[Union[AWSRdsCluster, Node]],
+) -> Iterator[Union[AWSRdsCluster, Node]]:
+    for red_res in rds_iterator:
+        storage_encrypted = red_res.raw.get("StorageEncrypted", False)
+        if storage_encrypted not in TRUE_OPTIONS:
+            st_enc_node = get_node_by_keys(red_res, ["StorageEncrypted"])
+            if isinstance(st_enc_node, Node):
+                yield st_enc_node
+            else:
+                yield AWSRdsCluster(
+                    column=red_res.start_column,
+                    data=red_res.data,
+                    line=get_line_by_extension(red_res.start_line, file_ext),
+                )
 
 
 def _tfm_rds_has_unencrypted_storage(
@@ -105,6 +135,29 @@ def _tfm_db_has_unencrypted_storage(
     )
 
 
+def _cfn_rds_has_unencrypted_storage(
+    content: str,
+    file_ext: str,
+    path: str,
+    template: Any,
+) -> core_model.Vulnerabilities:
+    return get_vulnerabilities_from_iterator_blocking(
+        content=content,
+        cwe={_FINDING_F246_CWE},
+        description_key="src.lib_path.f246.rds_has_unencrypted_storage",
+        finding=_FINDING_F246,
+        iterator=get_aws_iterator(
+            _cfn_rds_has_unencrypted_storage_iterate_vulnerabilities(
+                file_ext=file_ext,
+                rds_iterator=iter_rds_clusters_and_instances(
+                    template=template
+                ),
+            )
+        ),
+        path=path,
+    )
+
+
 @SHIELD
 @TIMEOUT_1MIN
 async def tfm_rds_has_unencrypted_storage(
@@ -132,6 +185,24 @@ async def tfm_db_has_unencrypted_storage(
         content=content,
         path=path,
         model=model,
+    )
+
+
+@CACHE_ETERNALLY
+@SHIELD
+@TIMEOUT_1MIN
+async def cfn_rds_has_unencrypted_storage(
+    content: str,
+    file_ext: str,
+    path: str,
+    template: Any,
+) -> core_model.Vulnerabilities:
+    return await in_process(
+        _cfn_rds_has_unencrypted_storage,
+        content=content,
+        file_ext=file_ext,
+        path=path,
+        template=template,
     )
 
 
