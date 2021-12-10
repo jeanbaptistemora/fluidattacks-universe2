6b131c049dca842108753c8a07115a5e6a3328d1
Alejandro Trujillo <atrujillo@fluidattacks.com>
2021-12-10 11:51:08 -0500

    skims\feat(back): #5381.199 f256
    - migrated method
    - added translation
    - Speed: 3 cfn_methods / 2 days = 1.5 cfn_methods/day
    - TODO: 16 cfn_methods
    - ETA: 16 / 1.5 = 10.67 days
    - no-deltas-test

--- skims/skims/lib_path/f256.py
+++ skims/skims/lib_path/f256.py
@@ -5,6 +5,7 @@ from aws.model import (
     AWSRdsCluster,
 )
 from lib_path.common import (
+    EXTENSIONS_CLOUDFORMATION,
     EXTENSIONS_TERRAFORM,
     get_cloud_iterator,
     get_vulnerabilities_from_iterator_blocking,
@@ -15,6 +16,9 @@ from metaloaders.model import (
 )
 from model import (
     core_model,
+)
+from parse_cfn.loader import (
+    load_templates,
 )
 from parse_cfn.structure import (
     iter_rds_clusters_and_instances,
@@ -303,6 +307,18 @@ async def analyze(
     **_: None,
 ) -> List[Awaitable[core_model.Vulnerabilities]]:
     coroutines: List[Awaitable[core_model.Vulnerabilities]] = []
+    if file_extension in EXTENSIONS_CLOUDFORMATION:
+        content = await content_generator()
+        async for template in load_templates(
+            content=content, fmt=file_extension
+        ):
+            coroutines.append(
+                cfn_rds_has_not_automated_backups(
+                    content=content,
+                    path=path,
+                    template=template,
+                )
+            )
     if file_extension in EXTENSIONS_TERRAFORM:
         content = await content_generator()
         model = await load_terraform(stream=content, default=[])
