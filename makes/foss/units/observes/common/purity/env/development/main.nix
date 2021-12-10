10a15fd7200d560b47bac78414c698faf98218e9
Daniel Murcia <dmurcia@fluidattacks.com>
2021-12-10 16:53:33 -0500

    observes\feat(build): #5297.1 purity dev
    - add dev env
    - add stubs on runtime

---
+++ makes/foss/units/observes/common/purity/env/development/main.nix
@@ -0,0 +1,17 @@
+{ makePythonPypiEnvironment
+, makeTemplate
+, outputs
+, ...
+}:
+makeTemplate {
+  name = "observes-common-purity-env-development";
+  searchPaths = {
+    source = [
+      (makePythonPypiEnvironment {
+        name = "observes-common-purity-env-development";
+        sourcesYaml = ./pypi-sources.yaml;
+      })
+      outputs."/observes/common/purity/env/runtime"
+    ];
+  };
+}
