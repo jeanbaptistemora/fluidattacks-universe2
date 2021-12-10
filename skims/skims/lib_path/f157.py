29f6113aae12fed5b8a86a476b92f4a26580c8de
Juan Echeverri <jecheverri@fluidattacks.com>
2021-12-10 14:56:24 -0500

    skims\feat(back): #5712.10 check f157 azure
    - Add method to check if azure data factory has public network enabled
    - no-deltas-test

---
+++ skims/skims/lib_path/f157.py
@@ -0,0 +1,119 @@
+from aioextensions import (
+    in_process,
+)
+from aws.model import (
+    AWSCloudfrontDistribution,
+)
+from lib_path.common import (
+    EXTENSIONS_TERRAFORM,
+    get_cloud_iterator,
+    get_vulnerabilities_from_iterator_blocking,
+    SHIELD,
+)
+from metaloaders.model import (
+    Node,
+)
+from model import (
+    core_model,
+)
+from parse_hcl2.loader import (
+    load as load_terraform,
+)
+from parse_hcl2.structure.azure import (
+    iter_azurerm_data_factory,
+)
+from parse_hcl2.tokens import (
+    Attribute,
+)
+from state.cache import (
+    CACHE_ETERNALLY,
+)
+from typing import (
+    Any,
+    Awaitable,
+    Callable,
+    Iterator,
+    List,
+    Union,
+)
+from utils.function import (
+    TIMEOUT_1MIN,
+)
+
+_FINDING_F157 = core_model.FindingEnum.F157
+_FINDING_F016_CWE = _FINDING_F157.value.cwe
+
+
+def tfm_azure_content_over_insecure_protocols_iterate_vulnerabilities(
+    buckets_iterator: Iterator[Union[AWSCloudfrontDistribution, Node]]
+) -> Iterator[Union[Any, Node]]:
+    for bucket in buckets_iterator:
+        public_attr = False
+        for elem in bucket.data:
+            if (
+                isinstance(elem, Attribute)
+                and elem.key == "public_network_enabled"
+            ):
+                public_attr = True
+                if elem.val is True:
+                    yield elem
+        if not public_attr:
+            yield bucket
+
+
+def _tfm_azure_serves_content_over_insecure_protocols(
+    content: str,
+    path: str,
+    model: Any,
+) -> core_model.Vulnerabilities:
+    return get_vulnerabilities_from_iterator_blocking(
+        content=content,
+        cwe={_FINDING_F016_CWE},
+        description_key=(
+            "src.lib_path.f016.serves_content_over_insecure_protocols"
+        ),
+        finding=_FINDING_F157,
+        iterator=get_cloud_iterator(
+            tfm_azure_content_over_insecure_protocols_iterate_vulnerabilities(
+                buckets_iterator=iter_azurerm_data_factory(model=model)
+            )
+        ),
+        path=path,
+    )
+
+
+@CACHE_ETERNALLY
+@SHIELD
+@TIMEOUT_1MIN
+async def tfm_azure_serves_content_over_insecure_protocols(
+    content: str,
+    path: str,
+    model: Any,
+) -> core_model.Vulnerabilities:
+    return await in_process(
+        _tfm_azure_serves_content_over_insecure_protocols,
+        content=content,
+        path=path,
+        model=model,
+    )
+
+
+@SHIELD
+async def analyze(
+    content_generator: Callable[[], Awaitable[str]],
+    file_extension: str,
+    path: str,
+    **_: None,
+) -> List[Awaitable[core_model.Vulnerabilities]]:
+    coroutines: List[Awaitable[core_model.Vulnerabilities]] = []
+    if file_extension in EXTENSIONS_TERRAFORM:
+        content = await content_generator()
+        model = await load_terraform(stream=content, default=[])
+        coroutines.append(
+            tfm_azure_serves_content_over_insecure_protocols(
+                content=content,
+                path=path,
+                model=model,
+            )
+        )
+    return coroutines
