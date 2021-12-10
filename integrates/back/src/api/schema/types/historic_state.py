f88ad0e82bfc6581b9e123641bb17a05b5a01465
Juan Chaves <jchaves@fluidattacks.com>
2021-12-10 14:43:08 -0500

    integrates\refac(back): #772.1 remove generic scalars
    - Added a historicState type definition for findings
    - Temporarily restored an exception to the schema linter
    to merge this change in while the other tweaks are done
    before the type is fully ready for general use

---
+++ integrates/back/src/api/schema/types/historic_state.py
@@ -0,0 +1,5 @@
+from ariadne import (
+    ObjectType,
+)
+
+HISTORIC_STATE = ObjectType("HistoricState")
--- makes/foss/units/integrates/linters/back/schema/builder.sh
+++ makes/foss/units/integrates/linters/back/schema/builder.sh
@@ -3,7 +3,7 @@
 function main {
   mkdir -p "${out}" \
     && graphql-schema-linter \
-      --except 'fields-are-camel-cased,input-object-values-are-camel-cased,relay-page-info-spec' \
+      --except 'fields-are-camel-cased,input-object-values-are-camel-cased,defined-types-are-used,relay-page-info-spec' \
       "${envIntegratesApiSchema}/**/*.graphql" \
       "${envIntegratesApiSchema}/types/**/*.graphql"
 }
