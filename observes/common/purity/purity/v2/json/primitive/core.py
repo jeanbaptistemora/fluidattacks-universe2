078b381a4d6324d7815b792586050983a88d3c2d
Daniel Murcia <dmurcia@fluidattacks.com>
2021-12-10 10:45:27 -0500

    observes\feat(back): #5674.5 jval factory
    - add jval factory to frozen objs

--- observes/common/purity/purity/v2/json/primitive/core.py
+++ observes/common/purity/purity/v2/json/primitive/core.py
@@ -6,9 +6,13 @@ from decimal import (
     Decimal,
 )
 from typing import (
+    Any,
     Type,
     TypeVar,
     Union,
+)
+from typing_extensions import (
+    TypeGuard,
 )
 
 Primitive = Union[str, int, float, Decimal, bool, None]
@@ -24,3 +28,10 @@ PrimitiveTVar = TypeVar(
     "PrimitiveTVar", str, int, float, Decimal, bool, Type[None]
 )
 NotNonePrimTvar = TypeVar("NotNonePrimTvar", str, int, float, Decimal, bool)
+
+
+def is_primitive(raw: Any) -> TypeGuard[Primitive]:
+    primitives = (str, int, float, bool, Decimal, type(None))
+    if isinstance(raw, primitives):
+        return True
+    return False
