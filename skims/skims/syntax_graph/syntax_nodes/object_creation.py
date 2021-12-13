b86045fd4ee281fe585cbc1f41671872ff42a2bd
Alejandro Salgado <asalgado@fluidattacks.com>
2021-12-13 14:07:30 -0500

    skims\fix(back): #5506.38 rm none args_id
    - Remove None arguments_id from ObjectCreation node

--- skims/skims/syntax_graph/syntax_nodes/object_creation.py
+++ skims/skims/syntax_graph/syntax_nodes/object_creation.py
@@ -12,11 +12,12 @@ def build_object_creation_node(
     args.syntax_graph.add_node(
         args.n_id,
         name=name,
-        arguments_id=arguments_id,
         label_type="ObjectCreation",
     )
 
     if arguments_id:
+        args.syntax_graph.nodes[args.n_id]["arguments_id"] = arguments_id
+
         args.syntax_graph.add_edge(
             args.n_id,
             args.generic(args.fork_n_id(arguments_id)),
