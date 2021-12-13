e5a519f90321135efb07542d3e2b24597da4c482
Andres Aguirre <aaguirre@fluidattacks.com>
2021-12-13 17:23:28 +0000

    integrates\feat(back): #5469.119 refresh toe lines access
    - add permission to the hacker and the architect to
    refresh the toe lines

--- integrates/back/src/authz/model.py
+++ integrates/back/src/authz/model.py
@@ -662,6 +662,7 @@ GROUP_LEVEL_ROLES_FOR_FLUIDATTACKS: Dict[str, Dict[str, Set[str]]] = dict(
     hacker=dict(
         actions={
             *GROUP_LEVEL_ROLES["hacker"]["actions"],
+            "api_mutations_refresh_toe_lines_mutate",
             "api_resolvers_git_root_services_toe_lines_resolve",
             "api_resolvers_group_disambiguation_resolve",
             "api_resolvers_group_toe_inputs_resolve",
@@ -827,6 +828,7 @@ GROUP_LEVEL_ROLES_FOR_FLUIDATTACKS: Dict[str, Dict[str, Set[str]]] = dict(
     architect=dict(
         actions={
             *GROUP_LEVEL_ROLES["architect"]["actions"],
+            "api_mutations_refresh_toe_lines_mutate",
             "api_resolvers_git_root_services_toe_lines_resolve",
             "api_resolvers_group_disambiguation_resolve",
             "api_resolvers_group_toe_inputs_resolve",
