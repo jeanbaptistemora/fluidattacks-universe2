142aac9c7ba51df887391156c2cea3f19c673c75
Diego Restrepo <drestrepo@fluidattacks.com>
2021-12-15 11:39:00 -0500

    integrates\feat(front): #5686.16 add component
    - add Execution, this component show information about
      a skims execution

---
+++ integrates/front/src/scenes/Dashboard/containers/GroupMachineView/execution.tsx
@@ -0,0 +1,161 @@
+import React from "react";
+
+import type { IExecution } from "./types";
+
+import { DataTableNext } from "components/DataTableNext";
+import type { IHeaderConfig } from "components/DataTableNext/types";
+import { Col33, Col60, Row } from "styles/styledComponents";
+import { formatDate } from "utils/formatHelpers";
+import { translate } from "utils/translations/translate";
+
+const Execution: React.FC<IExecution> = (
+  props: Readonly<IExecution>
+): JSX.Element => {
+  const {
+    jobId,
+    createdAt,
+    startedAt,
+    stoppedAt,
+    duration,
+    findingsExecuted,
+    name,
+    queue,
+    rootId,
+    rootNickname,
+  } = props;
+
+  const headersExecutionTable: IHeaderConfig[] = [
+    {
+      align: "center",
+      dataField: "finding",
+      header: translate.t("group.machine.finding.finding"),
+    },
+    {
+      align: "center",
+      dataField: "open",
+      header: translate.t("group.machine.finding.open"),
+    },
+    {
+      align: "center",
+      dataField: "modified",
+      header: translate.t("group.machine.finding.modified"),
+      wrapped: true,
+    },
+  ];
+
+  return (
+    <div>
+      {/* eslint-disable-next-line react/forbid-component-props */}
+      <Row className={"nb3"}>
+        <Col33>
+          <p>
+            <b>{translate.t("group.machine.job.id")}</b>
+          </p>
+        </Col33>
+        <Col60>
+          <p>{jobId}</p>
+        </Col60>
+      </Row>
+      {/* eslint-disable-next-line react/forbid-component-props */}
+      <Row className={"nb3"}>
+        <Col33>
+          <p>
+            <b>{translate.t("group.machine.job.name")}</b>
+          </p>
+        </Col33>
+        <Col33>
+          <p>{name}</p>
+        </Col33>
+      </Row>
+      {/* eslint-disable-next-line react/forbid-component-props */}
+      <Row className={"nb3"}>
+        <Col33>
+          <p>
+            <b>{translate.t("group.machine.job.queue")}</b>
+          </p>
+        </Col33>
+        <Col33>
+          <p>{queue}</p>
+        </Col33>
+      </Row>
+      {/* eslint-disable-next-line react/forbid-component-props */}
+      <Row className={"nb3"}>
+        <Col33>
+          <p>
+            <b>{translate.t("group.machine.rootId")}</b>
+          </p>
+        </Col33>
+        <Col60>
+          <p>{rootId}</p>
+        </Col60>
+      </Row>
+      {/* eslint-disable-next-line react/forbid-component-props */}
+      <Row className={"nb3"}>
+        <Col33>
+          <p>
+            <b>{translate.t("group.machine.root")}</b>
+          </p>
+        </Col33>
+        <Col33>
+          <p>{rootNickname}</p>
+        </Col33>
+      </Row>
+      {/* eslint-disable-next-line react/forbid-component-props */}
+      <Row className={"nb3"}>
+        <Col33>
+          <p>
+            <b>{translate.t("group.machine.date.create")}</b>
+          </p>
+        </Col33>
+        <Col33>
+          <p>{formatDate(createdAt)}</p>
+        </Col33>
+      </Row>
+      {/* eslint-disable-next-line react/forbid-component-props */}
+      <Row className={"nb3"}>
+        <Col33>
+          <p>
+            <b>{translate.t("group.machine.date.start")}</b>
+          </p>
+        </Col33>
+        <Col33>
+          <p>{formatDate(startedAt)}</p>
+        </Col33>
+      </Row>
+      {/* eslint-disable-next-line react/forbid-component-props */}
+      <Row className={"nb3"}>
+        <Col33>
+          <p>
+            <b>{translate.t("group.machine.date.stop")}</b>
+          </p>
+        </Col33>
+        <Col33>
+          <p>{formatDate(stoppedAt)}</p>
+        </Col33>
+      </Row>
+      {/* eslint-disable-next-line react/forbid-component-props */}
+      <Row className={"nb3"}>
+        <Col33>
+          <p>
+            <b>{translate.t("group.machine.date.duration")}</b>
+          </p>
+        </Col33>
+        <Col33>
+          <p>{duration}</p>
+        </Col33>
+      </Row>
+      <DataTableNext
+        bordered={true}
+        dataset={findingsExecuted}
+        exportCsv={false}
+        headers={headersExecutionTable}
+        id={"tblMachineExecutions"}
+        pageSize={100}
+        search={false}
+      />
+      <br />
+    </div>
+  );
+};
+
+export { Execution };
