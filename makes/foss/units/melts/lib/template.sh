a84a8584663ff11cad2d35c0dc766f70ac388434
Daniel Salazar <dsalazar@fluidattacks.com>
2021-12-16 13:00:44 -0500

    melts\refac(build): #5473 new services user
    - Use new services user

--- makes/foss/units/melts/lib/template.sh
+++ makes/foss/units/melts/lib/template.sh
@@ -1,15 +1,15 @@
 # shellcheck shell=bash
 
 function clone_services_repository {
-  export SERVICES_PROD_AWS_ACCESS_KEY_ID
-  export SERVICES_PROD_AWS_SECRET_ACCESS_KEY
+  export PROD_SERVICES_AWS_ACCESS_KEY_ID
+  export PROD_SERVICES_AWS_SECRET_ACCESS_KEY
   local group="${1}"
 
   aws_login_prod 'services' \
     && CI='true' \
       CI_COMMIT_REF_NAME='master' \
-      PROD_AWS_ACCESS_KEY_ID="${SERVICES_PROD_AWS_ACCESS_KEY_ID}" \
-      PROD_AWS_SECRET_ACCESS_KEY="${SERVICES_PROD_AWS_SECRET_ACCESS_KEY}" \
+      PROD_AWS_ACCESS_KEY_ID="${PROD_SERVICES_AWS_ACCESS_KEY_ID}" \
+      PROD_AWS_SECRET_ACCESS_KEY="${PROD_SERVICES_AWS_SECRET_ACCESS_KEY}" \
       melts drills --pull-repos "${group}"
 }
 
