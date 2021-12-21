781438eac4564e44c197fc911cbab7d99c857ac8
Daniel Salazar <dsalazar@fluidattacks.com>
2021-12-21 17:11:42 -0500

    integrates\feat(back): #4393 webhook logs
    - Add logging for webhooks

--- integrates/back/src/billing/domain.py
+++ integrates/back/src/billing/domain.py
@@ -2,6 +2,11 @@ from context import (
     FI_STRIPE_API_KEY,
     FI_STRIPE_WEBHOOK_KEY,
 )
+import logging
+import logging.config
+from settings import (
+    LOGGING,
+)
 from starlette.requests import (
     Request,
 )
@@ -13,43 +18,45 @@ from typing import (
     Optional,
 )
 
+logging.config.dictConfig(LOGGING)
+LOGGER = logging.getLogger(__name__)
+
 stripe.api_key = FI_STRIPE_API_KEY
 
 
 async def main(request: Request) -> JSONResponse:
     """Parse webhook request and execute event"""
+
     body: Optional[bytes] = await request.body()
     signature: Optional[str] = request.headers.get("stripe-signature")
+    message: str = ""
+    status: str = "success"
+
     try:
         event = stripe.Webhook.construct_event(
             body,
             signature,
             FI_STRIPE_WEBHOOK_KEY,
         )
-    except ValueError:
-        print("Invalid payload")
-        return JSONResponse(
-            {
-                "message": "Invalid payload",
-                "error": True,
-            }
-        )
-    except stripe.error.SignatureVerificationError:
-        return JSONResponse(
-            {
-                "message": "Invalid signature",
-                "error": True,
-            }
-        )
 
-    if event.type == "payment_intent.succeeded":
-        print("PaymentIntent was successful!")
-    else:
-        print(f"Unhandled event type {event.type}")
+        if event.type == "payment_intent.succeeded":
+            message = "PaymentIntent was successful!"
+        else:
+            message = f"Unhandled event type: {event.type}"
+            status = "failed"
+            LOGGER.warning(message, extra=dict(extra=locals()))
+    except ValueError as ex:
+        message = "Invalid payload"
+        status = "failed"
+        LOGGER.exception(ex, extra=dict(extra=locals()))
+    except stripe.error.SignatureVerificationError as ex:
+        message = "Invalid signature"
+        status = "failed"
+        LOGGER.exception(ex, extra=dict(extra=locals()))
 
     return JSONResponse(
         {
-            "message": "OK",
-            "error": False,
+            "status": status,
+            "message": message,
         }
     )
