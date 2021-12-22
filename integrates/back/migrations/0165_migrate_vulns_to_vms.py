c9c7b95730679939a4a47fb02ffa6b91fbb81d0e
Jonathan Mesa <jmesa@fluidattacks.com>
2021-12-22 12:43:13 -0500

    integrates\refac(back): #4329.788 modify migration
    - process sequentially by group in order to have control in progress
    - query vuln in both tables before update in order to find
    inconsistencies
    - skip already migrated vulns
    - no-deltas-test

--- integrates/back/migrations/0165_migrate_vulns_to_vms.py
+++ integrates/back/migrations/0165_migrate_vulns_to_vms.py
@@ -10,13 +10,15 @@ Details at https://gitlab.com/fluidattacks/product/-/issues/5690
 Execution Time:
 Finalization Time:
 """
-
 from aioextensions import (
     collect,
     run,
 )
 from boto3.dynamodb.conditions import (
     Key,
+)
+from custom_exceptions import (
+    VulnNotFound,
 )
 from dataloaders import (
     Dataloaders,
@@ -28,6 +30,12 @@ from datetime import (
 from db_model import (
     TABLE,
 )
+from db_model.findings.types import (
+    Finding,
+)
+from db_model.vulnerabilities import (
+    utils as db_model_utils,
+)
 from db_model.vulnerabilities.enums import (
     VulnerabilityStateStatus,
 )
@@ -35,29 +43,31 @@ from db_model.vulnerabilities.types import (
     Vulnerability,
     VulnerabilityHistoric,
 )
-from db_model.vulnerabilities.utils import (
-    historic_entry_type_to_str,
-)
 from dynamodb import (
     keys,
     operations,
+    operations_legacy,
 )
 from groups import (
     domain as groups_domain,
+)
+from itertools import (
+    chain,
+)
+from newutils import (
+    vulnerabilities as vulns_utils,
 )
 from newutils.vulnerabilities import (
     adjust_historic_dates,
 )
 import simplejson as json  # type: ignore
 import time
+from typing import (
+    List,
+    Optional,
+)
 
 PROD: bool = False
-
-
-def _log(vuln: Vulnerability, progress: float, result: bool) -> str:
-    msg = f"{vuln.finding_id},{vuln.id},{progress:.4f},{result}"
-    print(msg)
-    return msg
 
 
 async def _add_historic(
@@ -67,7 +77,7 @@ async def _add_historic(
 ) -> None:
     key_structure = TABLE.primary_key
     latest_entry = historic[-1]
-    entry_type = historic_entry_type_to_str(latest_entry)
+    entry_type = db_model_utils.historic_entry_type_to_str(latest_entry)
 
     historic_key = keys.build_key(
         facet=TABLE.facets[f"vulnerability_historic_{entry_type}"],
@@ -118,7 +128,38 @@ async def _add_historic(
     )
 
 
-async def _add_metadata(*, vulnerability: Vulnerability) -> None:
+async def _get_vulnerability_vms(vuln_id: str) -> Optional[Vulnerability]:
+    primary_key = keys.build_key(
+        facet=TABLE.facets["vulnerability_metadata"],
+        values={"id": vuln_id},
+    )
+    key_structure = TABLE.primary_key
+    response = await operations.query(
+        condition_expression=(
+            Key(key_structure.partition_key).eq(primary_key.partition_key)
+            & Key(key_structure.sort_key).begins_with(primary_key.sort_key)
+        ),
+        facets=(TABLE.facets["vulnerability_metadata"],),
+        table=TABLE,
+    )
+    if not response.items:
+        return None
+    return db_model_utils.format_vulnerability(response.items[0])
+
+
+async def _get_vulnerability_old(vuln_id: str) -> Vulnerability:
+    hash_key = "UUID"
+    query_attrs = {
+        "IndexName": "gsi_uuid",
+        "KeyConditionExpression": Key(hash_key).eq(vuln_id),
+    }
+    items = await operations_legacy.query("FI_vulnerabilities", query_attrs)
+    if not items:
+        raise VulnNotFound()
+    return vulns_utils.format_vulnerability(items[0])
+
+
+async def _add_metadata(vulnerability: Vulnerability) -> None:
     key_structure = TABLE.primary_key
     vulnerability_key = keys.build_key(
         facet=TABLE.facets["vulnerability_metadata"],
@@ -138,63 +179,128 @@ async def _add_metadata(*, vulnerability: Vulnerability) -> None:
     )
 
 
-async def _process_vulnerability(
-    loaders: Dataloaders,
-    vuln: Vulnerability,
-    progress: float,
+def _log(
+    group_name: str, finding_id: str, vuln_id: str, status: str, result: bool
 ) -> str:
-    if vuln.state.status == VulnerabilityStateStatus.DELETED:
-        return _log(vuln, progress, False)
+    msg = f"{group_name},{finding_id},{vuln_id},{status},{result}"
+    print(msg)
+    return msg
+
+
+async def _process_vulnerability(
+    *,
+    loaders: Dataloaders,
+    group_name: str,
+    finding_id: str,
+    vuln_id: str,
+) -> str:
+    current_vuln = await _get_vulnerability_old(vuln_id)
+    if current_vuln.state.status == VulnerabilityStateStatus.DELETED:
+        return _log(group_name, finding_id, vuln_id, "DELETED", False)
 
     state_loader = loaders.vulnerability_historic_state
     treatment_loader = loaders.vulnerability_historic_treatment
     verification_loader = loaders.vulnerability_historic_verification
     zero_risk_loader = loaders.vulnerability_historic_zero_risk
-    historic_state = adjust_historic_dates(await state_loader.load(vuln.id))
+    state_loader.clear(vuln_id)
+    treatment_loader.clear(vuln_id)
+    verification_loader.clear(vuln_id)
+    zero_risk_loader.clear(vuln_id)
+    historic_state = adjust_historic_dates(await state_loader.load(vuln_id))
     historic_treatment = adjust_historic_dates(
-        await treatment_loader.load(vuln.id)
+        await treatment_loader.load(vuln_id)
     )
     historic_verification = adjust_historic_dates(
-        await verification_loader.load(vuln.id)
+        await verification_loader.load(vuln_id)
     )
     historic_zero_risk = adjust_historic_dates(
-        await zero_risk_loader.load(vuln.id)
+        await zero_risk_loader.load(vuln_id)
     )
+    current_vuln = current_vuln._replace(
+        state=historic_state[-1],
+        treatment=historic_treatment[-1] if historic_treatment else None,
+        verification=historic_verification[-1]
+        if historic_verification
+        else None,
+        zero_risk=historic_zero_risk[-1] if historic_zero_risk else None,
+    )
+
+    vuln_in_vms = await _get_vulnerability_vms(vuln_id)
+    if current_vuln == vuln_in_vms:
+        return _log(group_name, finding_id, vuln_id, "ALREADY_MIGRATED", True)
 
     if not PROD:
-        return _log(vuln, progress, False)
+        return _log(group_name, finding_id, vuln_id, "NOT_PROD", False)
 
-    await _add_metadata(
-        vulnerability=vuln._replace(
-            state=historic_state[-1],
-            treatment=historic_treatment[-1] if historic_treatment else None,
-            verification=historic_verification[-1]
-            if historic_verification
-            else None,
-            zero_risk=historic_zero_risk[-1] if historic_zero_risk else None,
-        )
-    )
+    await _add_metadata(vulnerability=current_vuln)
     await _add_historic(
         historic=historic_state,
-        vulnerability_id=vuln.id,
+        vulnerability_id=vuln_id,
     )
     if historic_treatment:
         await _add_historic(
             historic=historic_treatment,
-            vulnerability_id=vuln.id,
+            vulnerability_id=vuln_id,
         )
     if historic_verification:
         await _add_historic(
             historic=historic_verification,
-            vulnerability_id=vuln.id,
+            vulnerability_id=vuln_id,
         )
     if historic_zero_risk:
         await _add_historic(
             historic=historic_zero_risk,
-            vulnerability_id=vuln.id,
+            vulnerability_id=vuln_id,
         )
 
-    return _log(vuln, progress, True)
+    return _log(group_name, finding_id, vuln_id, "MIGRATED", True)
+
+
+async def _process_finding(
+    *,
+    loaders: Dataloaders,
+    finding: Finding,
+) -> List[str]:
+    vulns = await loaders.finding_vulns_typed.load(finding.id)
+    vulns_uuids = [vuln.id for vuln in vulns]
+    return list(
+        await collect(
+            tuple(
+                _process_vulnerability(
+                    loaders=loaders,
+                    group_name=finding.group_name,
+                    finding_id=finding.id,
+                    vuln_id=vuln_id,
+                )
+                for vuln_id in vulns_uuids
+            ),
+            workers=64,
+        )
+    )
+
+
+async def _process_group(
+    *,
+    loaders: Dataloaders,
+    group_name: str,
+) -> List[str]:
+    findings_loader = loaders.group_drafts_and_findings
+    findings = await findings_loader.load(group_name)
+    print(f" - group {group_name} : {len(findings)} findings")
+    return list(
+        chain.from_iterable(
+            await collect(
+                tuple(
+                    _process_finding(
+                        loaders=loaders,
+                        finding=finding,
+                    )
+                    for finding in findings
+                ),
+                workers=128,
+            )
+        )
+    )
 
 
 async def main() -> None:
@@ -202,38 +308,21 @@ async def main() -> None:
     group_names = sorted(await groups_domain.get_active_groups())
     print(f"groups({len(group_names)}): {group_names}")
 
-    findings_loader = loaders.group_drafts_and_findings
-    findings = await findings_loader.load_many_chained(group_names)
-    finding_ids = [finding.id for finding in findings]
-    print(f"findings({len(finding_ids)})")
+    for group_name in group_names:
+        start = time.time()
+        results = await _process_group(loaders=loaders, group_name=group_name)
+        success = all(item.split(",")[-1] == "True" for item in results)
+        print(
+            f" - group {group_name} FINISHED in "
+            f"{timedelta(seconds=time.time() - start)} - "
+            f"results({len(results)}), success: {success}"
+        )
 
-    vulns_to_migrate = await loaders.finding_vulns_typed.load_many_chained(
-        finding_ids
-    )
-    print(f"vulns to migrate({len(vulns_to_migrate)})")
+        csv_file = "0165_results.csv"
+        with open(csv_file, "a", encoding="utf8") as f:
+            for item in results:
+                f.write(f"{item}\n")
 
-    start = time.time()
-    results = await collect(
-        tuple(
-            _process_vulnerability(
-                loaders=loaders,
-                vuln=vuln,
-                progress=index / len(vulns_to_migrate),
-            )
-            for index, vuln in enumerate(vulns_to_migrate)
-        ),
-        workers=64,
-    )
-    print(f"processing time: {timedelta(seconds=time.time() - start)}")
-
-    csv_file = "0165_results.csv"
-    with open(csv_file, "w", encoding="utf8") as f:
-        for item in results:
-            f.write(f"{item}\n")
-
-    print(f"results({len(results)})")
-    success = all(item.split(",")[-1] == "True" for item in results)
-    print(f"success: {success}")
     print("done!")
 
 
