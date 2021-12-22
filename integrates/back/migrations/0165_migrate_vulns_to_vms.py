a256caeef202ce46f097138fecc505d0f826c2d0
Jonathan Mesa <jmesa@fluidattacks.com>
2021-12-22 09:31:48 -0500

    integrates\refac(back): #4329.787 modify migration
    - add modified versions of used functions from vulns_model
    in order to use less operations
    - also using batch_write_item to overwrite existing items
    - reduce collect workers
    - no-deltas-test

--- integrates/back/migrations/0165_migrate_vulns_to_vms.py
+++ integrates/back/migrations/0165_migrate_vulns_to_vms.py
@@ -15,18 +15,32 @@ from aioextensions import (
     collect,
     run,
 )
+from boto3.dynamodb.conditions import (
+    Key,
+)
 from dataloaders import (
     Dataloaders,
     get_new_context,
 )
+from datetime import (
+    timedelta,
+)
 from db_model import (
-    vulnerabilities as vulns_model,
+    TABLE,
 )
 from db_model.vulnerabilities.enums import (
     VulnerabilityStateStatus,
 )
 from db_model.vulnerabilities.types import (
     Vulnerability,
+    VulnerabilityHistoric,
+)
+from db_model.vulnerabilities.utils import (
+    historic_entry_type_to_str,
+)
+from dynamodb import (
+    keys,
+    operations,
 )
 from groups import (
     domain as groups_domain,
@@ -34,6 +48,7 @@ from groups import (
 from newutils.vulnerabilities import (
     adjust_historic_dates,
 )
+import simplejson as json  # type: ignore
 import time
 
 PROD: bool = False
@@ -43,6 +58,84 @@ def _log(vuln: Vulnerability, progress: float, result: bool) -> str:
     msg = f"{vuln.finding_id},{vuln.id},{progress:.4f},{result}"
     print(msg)
     return msg
+
+
+async def _add_historic(
+    *,
+    historic: VulnerabilityHistoric,
+    vulnerability_id: str,
+) -> None:
+    key_structure = TABLE.primary_key
+    latest_entry = historic[-1]
+    entry_type = historic_entry_type_to_str(latest_entry)
+
+    historic_key = keys.build_key(
+        facet=TABLE.facets[f"vulnerability_historic_{entry_type}"],
+        values={"id": vulnerability_id},
+    )
+    current_response = await operations.query(
+        condition_expression=(
+            Key(key_structure.partition_key).eq(historic_key.partition_key)
+            & Key(key_structure.sort_key).begins_with(historic_key.sort_key)
+        ),
+        facets=(TABLE.facets[f"vulnerability_historic_{entry_type}"],),
+        table=TABLE,
+    )
+    current_items = current_response.items
+    current_keys = {
+        keys.build_key(
+            facet=TABLE.facets[f"vulnerability_historic_{entry_type}"],
+            values={
+                "id": vulnerability_id,
+                "iso8601utc": item["sk"].split("#")[1],
+            },
+        )
+        for item in current_items
+    }
+
+    new_keys = tuple(
+        keys.build_key(
+            facet=TABLE.facets[f"vulnerability_historic_{entry_type}"],
+            values={
+                "id": vulnerability_id,
+                "iso8601utc": entry.modified_date,
+            },
+        )
+        for entry in historic
+    )
+    new_items = tuple(
+        {
+            key_structure.partition_key: key.partition_key,
+            key_structure.sort_key: key.sort_key,
+            **json.loads(json.dumps(entry)),
+        }
+        for key, entry in zip(new_keys, historic)
+    )
+    await operations.batch_write_item(items=new_items, table=TABLE)
+    await operations.batch_delete_item(
+        keys=tuple(key for key in current_keys if key not in new_keys),
+        table=TABLE,
+    )
+
+
+async def _add_metadata(*, vulnerability: Vulnerability) -> None:
+    key_structure = TABLE.primary_key
+    vulnerability_key = keys.build_key(
+        facet=TABLE.facets["vulnerability_metadata"],
+        values={
+            "finding_id": vulnerability.finding_id,
+            "id": vulnerability.id,
+        },
+    )
+    vulnerability_item = {
+        key_structure.partition_key: vulnerability_key.partition_key,
+        key_structure.sort_key: vulnerability_key.sort_key,
+        **json.loads(json.dumps(vulnerability)),
+    }
+    await operations.batch_write_item(
+        items=(vulnerability_item,),
+        table=TABLE,
+    )
 
 
 async def _process_vulnerability(
@@ -57,37 +150,47 @@ async def _process_vulnerability(
     treatment_loader = loaders.vulnerability_historic_treatment
     verification_loader = loaders.vulnerability_historic_verification
     zero_risk_loader = loaders.vulnerability_historic_zero_risk
-    historic_state = await state_loader.load(vuln.id)
-    historic_treatment = await treatment_loader.load(vuln.id)
-    historic_verification = await verification_loader.load(vuln.id)
-    historic_zero_risk = await zero_risk_loader.load(vuln.id)
+    historic_state = adjust_historic_dates(await state_loader.load(vuln.id))
+    historic_treatment = adjust_historic_dates(
+        await treatment_loader.load(vuln.id)
+    )
+    historic_verification = adjust_historic_dates(
+        await verification_loader.load(vuln.id)
+    )
+    historic_zero_risk = adjust_historic_dates(
+        await zero_risk_loader.load(vuln.id)
+    )
 
     if not PROD:
         return _log(vuln, progress, False)
 
-    await vulns_model.add(vulnerability=vuln)
-
-    await vulns_model.update_historic(
-        finding_id=vuln.finding_id,
-        historic=adjust_historic_dates(historic_state),
+    await _add_metadata(
+        vulnerability=vuln._replace(
+            state=historic_state[-1],
+            treatment=historic_treatment[-1] if historic_treatment else None,
+            verification=historic_verification[-1]
+            if historic_verification
+            else None,
+            zero_risk=historic_zero_risk[-1] if historic_zero_risk else None,
+        )
+    )
+    await _add_historic(
+        historic=historic_state,
         vulnerability_id=vuln.id,
     )
     if historic_treatment:
-        await vulns_model.update_historic(
-            finding_id=vuln.finding_id,
-            historic=adjust_historic_dates(historic_treatment),
+        await _add_historic(
+            historic=historic_treatment,
             vulnerability_id=vuln.id,
         )
     if historic_verification:
-        await vulns_model.update_historic(
-            finding_id=vuln.finding_id,
-            historic=adjust_historic_dates(historic_verification),
+        await _add_historic(
+            historic=historic_verification,
             vulnerability_id=vuln.id,
         )
     if historic_zero_risk:
-        await vulns_model.update_historic(
-            finding_id=vuln.finding_id,
-            historic=adjust_historic_dates(historic_zero_risk),
+        await _add_historic(
+            historic=historic_zero_risk,
             vulnerability_id=vuln.id,
         )
 
@@ -97,33 +200,40 @@ async def _process_vulnerability(
 async def main() -> None:
     loaders: Dataloaders = get_new_context()
     group_names = sorted(await groups_domain.get_active_groups())
-    print(f"groups({len(group_names)}): {group_names[:3]}")
+    print(f"groups({len(group_names)}): {group_names}")
 
     findings_loader = loaders.group_drafts_and_findings
     findings = await findings_loader.load_many_chained(group_names)
     finding_ids = [finding.id for finding in findings]
-    print(f"findings({len(finding_ids)}): {finding_ids[:3]}")
+    print(f"findings({len(finding_ids)})")
 
     vulns_to_migrate = await loaders.finding_vulns_typed.load_many_chained(
         finding_ids
     )
-    print(f"vulns to migrate({len(vulns_to_migrate)}): {vulns_to_migrate[:1]}")
+    print(f"vulns to migrate({len(vulns_to_migrate)})")
 
+    start = time.time()
     results = await collect(
-        _process_vulnerability(
-            loaders=loaders,
-            vuln=vuln,
-            progress=index / len(vulns_to_migrate),
-        )
-        for index, vuln in enumerate(vulns_to_migrate)
+        tuple(
+            _process_vulnerability(
+                loaders=loaders,
+                vuln=vuln,
+                progress=index / len(vulns_to_migrate),
+            )
+            for index, vuln in enumerate(vulns_to_migrate)
+        ),
+        workers=64,
     )
-    print(f"results({len(results)})")
+    print(f"processing time: {timedelta(seconds=time.time() - start)}")
 
     csv_file = "0165_results.csv"
     with open(csv_file, "w", encoding="utf8") as f:
         for item in results:
             f.write(f"{item}\n")
 
+    print(f"results({len(results)})")
+    success = all(item.split(",")[-1] == "True" for item in results)
+    print(f"success: {success}")
     print("done!")
 
 
