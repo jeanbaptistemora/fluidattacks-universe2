a2989737f3ca614391ff95513f5a35e8f0b9bb99
Daniel Murcia <dmurcia@fluidattacks.com>
2021-12-28 13:26:46 -0500

    observes\refac(back): #5781.1 migrate queries
    - adapt update queries into Query objs

--- observes/code_etl/code_etl/client/query.py
+++ observes/code_etl/code_etl/client/query.py
@@ -2,6 +2,9 @@ from code_etl.client.encoder import (
     CommitTableRow,
     RawRow,
 )
+from code_etl.utils import (
+    COMMIT_HASH_SENTINEL,
+)
 from dataclasses import (
     fields,
 )
@@ -49,5 +52,106 @@ def insert_row(table: TableID) -> Query:
                 "schema": table.schema.name,
                 "table": table.table_name,
             }
+        ),
+    )
+
+
+def insert_unique_row(table: TableID) -> Query:
+    _fields = ",".join(tuple(f.name for f in fields(CommitTableRow)))
+    values = ",".join(tuple(f"%({f.name})s" for f in fields(CommitTableRow)))
+    return Query(
+        f"""
+        INSERT INTO {{schema}}.{{table}} ({_fields}) SELECT {values}
+        WHERE NOT EXISTS (
+            SELECT hash, namespace, repository
+            FROM {{schema}}.{{table}}
+            WHERE
+                hash = %(hash)s
+                and namespace = %(namespace)s
+                and repository = %(repository)s
+        )
+        """,
+        SqlArgs(
+            identifiers={
+                "schema": table.schema.name,
+                "table": table.table_name,
+            }
+        ),
+    )
+
+
+def update_users(table: TableID) -> Query:
+    return Query(
+        """
+        UPDATE {schema}.{table}
+        SET
+            author_email = %(author_email)s,
+            author_name = %(author_name)s,
+            committer_email = %(committer_email)s,
+            committer_name = %(committer_name)s
+        WHERE
+            hash = %(hash)s
+            and namespace = %(namespace)s
+            and repository = %(repository)s
+        """,
+        SqlArgs(
+            identifiers={
+                "schema": table.schema.name,
+                "table": table.table_name,
+            }
+        ),
+    )
+
+
+def commit_exists(
+    table: TableID,
+    namespace: str,
+    repo: str,
+    commit_hash: str,
+) -> Query:
+    return Query(
+        """
+        SELECT hash, namespace, repository
+        FROM {schema}.{table}
+        WHERE
+            hash = %(hash)s
+            and namespace = %(namespace)s
+            and repository = %(repository)s
+        """,
+        SqlArgs(
+            {
+                "namespace": namespace,
+                "repository": repo,
+                "hash": commit_hash,
+            },
+            {
+                "schema": table.schema.name,
+                "table": table.table_name,
+            },
+        ),
+    )
+
+
+def last_commit_hash(table: TableID, namespace: str, repo: str) -> Query:
+    return Query(
+        """
+        SELECT hash FROM {schema}.{table}
+        WHERE
+            hash != %(hash)s
+            and namespace = %(namespace)s
+            and repository = %(repository)s
+        ORDER BY seen_at DESC, authored_at DESC
+        LIMIT 1
+        """,
+        SqlArgs(
+            {
+                "namespace": namespace,
+                "repository": repo,
+                "hash": COMMIT_HASH_SENTINEL,
+            },
+            {
+                "schema": table.schema.name,
+                "table": table.table_name,
+            },
         ),
     )
