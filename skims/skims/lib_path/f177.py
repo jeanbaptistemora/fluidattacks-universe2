3a1e6eab1ce3e2cccda69d53ed13f75c10cf9f00
Alejandro Trujillo <atrujillo@fluidattacks.com>
2021-12-29 16:21:56 -0500

    skims\feat(back): #5381.242 ec2 f177
    - migrated method
    - added translation
    - Speed: 5 cfn_methods / 2 days = 2.5 cfn_methods/day
    - TODO: 11 cfn_methods
    - ETA: 11 / 2.5 = 4.4 days
    - no-deltas-test

--- skims/skims/lib_path/f177.py
+++ skims/skims/lib_path/f177.py
@@ -1,6 +1,9 @@
 from aioextensions import (
     in_process,
 )
+from aws.model import (
+    AWSEC2,
+)
 from lib_path.common import (
     EXTENSIONS_TERRAFORM,
     get_cloud_iterator,
@@ -13,6 +16,9 @@ from metaloaders.model import (
 from model import (
     core_model,
 )
+from parse_cfn.structure import (
+    iter_ec2_ingress_egress,
+)
 from parse_hcl2.loader import (
     load as load_terraform,
 )
@@ -22,6 +28,9 @@ from parse_hcl2.structure.aws import (
 )
 from parse_hcl2.tokens import (
     Attribute,
+)
+from state.cache import (
+    CACHE_ETERNALLY,
 )
 from typing import (
     Any,
@@ -71,6 +80,29 @@ def aws_instance_use_default_security_group_iterate_vulnerabilities(
             yield bucket
 
 
+def _cfn_ec2_has_unrestricted_dns_access_iterate_vulnerabilities(
+    ec2_iterator: Iterator[Union[AWSEC2, Node]],
+) -> Iterator[Union[AWSEC2, Node]]:
+    for ec2_res in ec2_iterator:
+        cidr = ec2_res.raw.get("CidrIp", None) or ec2_res.raw.get(
+            "CidrIpv6", None
+        )
+        is_public_cidr = cidr in (
+            "::/0",
+            "0.0.0.0/0",
+        )
+        if not is_public_cidr:
+            continue
+        from_port = ec2_res.inner.get("FromPort")
+        to_port = ec2_res.inner.get("ToPort")
+        if (
+            from_port
+            and to_port
+            and float(from_port.raw) <= 53 <= float(to_port.raw)
+        ):
+            yield from_port
+
+
 def _ec2_use_default_security_group(
     content: str,
     path: str,
@@ -109,6 +141,27 @@ def _aws_instance_use_default_security_group(
     )
 
 
+def _cfn_ec2_has_unrestricted_dns_access(
+    content: str,
+    path: str,
+    template: Any,
+) -> core_model.Vulnerabilities:
+    return get_vulnerabilities_from_iterator_blocking(
+        content=content,
+        cwe={_FINDING_F177_CWE},
+        description_key=("src.lib_path.f177.ec2_has_unrestricted_dns_access"),
+        finding=_FINDING_F177,
+        iterator=get_cloud_iterator(
+            _cfn_ec2_has_unrestricted_dns_access_iterate_vulnerabilities(
+                ec2_iterator=iter_ec2_ingress_egress(
+                    template=template, ingress=True, egress=True
+                ),
+            )
+        ),
+        path=path,
+    )
+
+
 @SHIELD
 @TIMEOUT_1MIN
 async def ec2_use_default_security_group(
@@ -136,6 +189,22 @@ async def aws_instance_use_default_security_group(
         content=content,
         path=path,
         model=model,
+    )
+
+
+@CACHE_ETERNALLY
+@SHIELD
+@TIMEOUT_1MIN
+async def cfn_ec2_has_unrestricted_dns_access(
+    content: str,
+    path: str,
+    template: Any,
+) -> core_model.Vulnerabilities:
+    return await in_process(
+        _cfn_ec2_has_unrestricted_dns_access,
+        content=content,
+        path=path,
+        template=template,
     )
 
 
