912b4c3bae09268b8b602f9e6605d44755dd6305
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-12-29 07:48:02 -0500

    integrates\refac(front): #5658.12 remove unused component
    - remove unused `zero_risk_confirmation_table` component as was replaced

--- integrates/front/src/scenes/Dashboard/containers/VulnerabilitiesView/HandleAcceptanceModal/ZeroRiskConfirmationTable/types.ts
+++
@@ -1,9 +0,0 @@
-import type { IVulnDataAttr } from "../types";
-
-interface IZeroRiskConfirmationTableProps {
-  acceptanceVulns: IVulnDataAttr[];
-  isConfirmZeroRiskSelected: boolean;
-  setAcceptanceVulns: (vulns: IVulnDataAttr[]) => void;
-}
-
-export { IZeroRiskConfirmationTableProps };
