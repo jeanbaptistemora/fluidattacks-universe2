3932fe660e5e897090cdb72fa0024d39f53e48f6
Alejandro Trujillo <atrujillo@fluidattacks.com>
2021-12-30 09:11:39 -0500

    asserts\refac(back): #5381.247 removed asserts method
    - removed method
    - removed asserts test
    - Speed: 7 cfn_methods / 2 days = 3.5 cfn_methods/day
    - TODO: 9 cfn_methods
    - ETA: 9 / 3.5 = 2.57 days
    - no-deltas-test

--- asserts/test/test_cloud_aws_cloudformation_ec2.py
+++ asserts/test/test_cloud_aws_cloudformation_ec2.py
@@ -38,14 +38,6 @@ def test_security_group_allows_anyone_to_admin_ports():
     assert ec2.security_group_allows_anyone_to_admin_ports(SAFE).is_closed()
 
 
-def test_has_unrestricted_ftp_access():
-    """test ec2.has_unrestricted_ftp_access."""
-    result = ec2.has_unrestricted_ftp_access(VULN)
-    assert result.is_open()
-    assert result.get_vulns_number() == 2 * 4
-    assert ec2.has_unrestricted_ftp_access(SAFE).is_closed()
-
-
 def test_has_security_groups_ip_ranges_in_rfc1918():
     """test ec2.has_security_groups_ip_ranges_in_rfc1918."""
     result = ec2.has_security_groups_ip_ranges_in_rfc1918(VULN)
