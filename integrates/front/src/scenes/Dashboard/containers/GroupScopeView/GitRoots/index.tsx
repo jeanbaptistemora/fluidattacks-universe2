94cf089e73333597489b4e162600423266c66b14
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-12-30 15:49:53 -0500

    integrates\feat(front): #5656.18 add custom_search option
    - add position option to datatable custom_search to control the
    position of textsearch to the right and to reduce vertical space
    use and to keep uniformity

--- integrates/front/src/scenes/Dashboard/containers/GroupScopeView/GitRoots/index.tsx
+++ integrates/front/src/scenes/Dashboard/containers/GroupScopeView/GitRoots/index.tsx
@@ -136,6 +136,7 @@ export const GitRoots: React.FC<IGitRootsProps> = ({
     useStoredState<boolean>("rootsCustomFilters", false);
 
   const [searchTextFilter, setSearchTextFilter] = useState("");
+  const [searchEnvsTextFilter, setSearchEnvsTextFilter] = useState("");
   const [filterGroupScopeTable, setFilterGroupScopeTable] =
     useStoredState<IFilterSet>(
       "filterGroupScopeSet",
@@ -268,6 +269,11 @@ export const GitRoots: React.FC<IGitRootsProps> = ({
     event: React.ChangeEvent<HTMLInputElement>
   ): void {
     setSearchTextFilter(event.target.value);
+  }
+  function onSearchEnvsTextChange(
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void {
+    setSearchEnvsTextFilter(event.target.value);
   }
   const filterSearchTextRoots: IGitRootAttr[] = filterSearchText(
     roots,
@@ -404,6 +410,17 @@ export const GitRoots: React.FC<IGitRootsProps> = ({
     },
   ];
 
+  const envDataset: Record<string, unknown>[] = Object.entries(
+    rootsGroupedByEnvs
+  ).map(
+    ([environmentUrl, repositoryUrls]): Record<string, unknown> => ({
+      environmentUrl,
+      repositoryUrls,
+    })
+  );
+  const filterEnvSearchTextDataset: Record<string, unknown>[] =
+    filterSearchText(envDataset, searchEnvsTextFilter);
+
   return (
     <React.Fragment>
       <h2>{t("group.scope.git.title")}</h2>
@@ -527,32 +544,35 @@ export const GitRoots: React.FC<IGitRootsProps> = ({
         }}
       </ConfirmDialog>
       <br />
-      <h2>{t("group.scope.git.envUrls")}</h2>
-      <DataTableNext
-        bordered={true}
-        dataset={Object.entries(rootsGroupedByEnvs).map(
-          ([environmentUrl, repositoryUrls]): Record<string, unknown> => ({
-            environmentUrl,
-            repositoryUrls,
-          })
-        )}
-        expandRow={{
-          expandByColumnOnly: true,
-          renderer: renderEnvDescription,
-          showExpandColumn: true,
-        }}
-        exportCsv={false}
-        headers={[
-          {
-            dataField: "environmentUrl",
-            header: t("group.scope.git.repo.url"),
-          },
-        ]}
-        id={"tblGitRootEnvs"}
-        pageSize={10}
-        search={true}
-        striped={true}
-      />
+      <h2 className={"mb0 pb0"}>{t("group.scope.git.envUrls")}</h2>
+      <div className={"flex flex-wrap nt2"}>
+        <DataTableNext
+          bordered={true}
+          customSearch={{
+            customSearchDefault: searchEnvsTextFilter,
+            isCustomSearchEnabled: true,
+            onUpdateCustomSearch: onSearchEnvsTextChange,
+            position: "right",
+          }}
+          dataset={filterEnvSearchTextDataset}
+          expandRow={{
+            expandByColumnOnly: true,
+            renderer: renderEnvDescription,
+            showExpandColumn: true,
+          }}
+          exportCsv={false}
+          headers={[
+            {
+              dataField: "environmentUrl",
+              header: t("group.scope.git.repo.url"),
+            },
+          ]}
+          id={"tblGitRootEnvs"}
+          pageSize={10}
+          search={false}
+          striped={true}
+        />
+      </div>
       {isManagingRoot === false ? undefined : (
         <ManagementModal
           initialValues={
