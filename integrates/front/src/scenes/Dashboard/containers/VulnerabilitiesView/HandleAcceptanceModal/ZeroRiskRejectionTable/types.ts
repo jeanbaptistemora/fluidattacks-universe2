35e9489db44920953688a2a09ec7bd524eb3a513
Sebastian Lizcano <slizcano@fluidattacks.com>
2021-12-30 08:38:27 -0500

    integrates\refac(front): #5658.17 remove unused component
    - remove unused `zero_risk_rejection_table` component as was replaced
    for a single component for confirmation/rejection

--- integrates/front/src/scenes/Dashboard/containers/VulnerabilitiesView/HandleAcceptanceModal/ZeroRiskRejectionTable/types.ts
+++
@@ -1,9 +0,0 @@
-import type { IVulnDataAttr } from "../types";
-
-interface IZeroRiskRejectionTableProps {
-  acceptanceVulns: IVulnDataAttr[];
-  isRejectZeroRiskSelected: boolean;
-  setAcceptanceVulns: (vulns: IVulnDataAttr[]) => void;
-}
-
-export { IZeroRiskRejectionTableProps };
