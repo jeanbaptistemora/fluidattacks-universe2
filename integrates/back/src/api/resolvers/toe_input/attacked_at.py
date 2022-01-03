17c178705b1661c041eb0a95dad7eaf846d6eb3b
Andres Aguirre <aaguirre@fluidattacks.com>
2022-01-03 15:43:18 +0000

    integrates\feat(back): #5469.179 input internal resolvers
    - add resolver for the toe input attributes that are internal

---
+++ integrates/back/src/api/resolvers/toe_input/attacked_at.py
@@ -0,0 +1,16 @@
+from db_model.toe_inputs.types import (
+    ToeInput,
+)
+from decorators import (
+    enforce_group_level_auth_async,
+)
+from graphql.type.definition import (
+    GraphQLResolveInfo,
+)
+
+
+@enforce_group_level_auth_async
+async def resolve(
+    parent: ToeInput, _info: GraphQLResolveInfo, **_kwargs: None
+) -> str:
+    return parent.attacked_at
