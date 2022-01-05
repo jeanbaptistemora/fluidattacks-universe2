1346b6b6fad62b069ab24d9ab57fc98e1af9a1c8
Juan Echeverri <jecheverri@fluidattacks.com>
2022-01-05 13:08:04 -0500

    asserts\refac(back): #5381.274 remove asserts methods
    - remove migrated method
    - remove test
    - SPEED: 5 tfm_method / 2 day = 2.5 tfm_methods/day
    - TODO: 7 tfm_methods
    - ETA: 7 / 2.5 = 2,8 days

--- asserts/fluidasserts/cloud/aws/terraform/ec2.py
+++ asserts/fluidasserts/cloud/aws/terraform/ec2.py
@@ -53,61 +53,6 @@ def _tipify_rules(res_props):
 
 @api(risk=MEDIUM, kind=SAST)
 @unknown_if(FileNotFoundError)
-def allows_all_outbound_traffic(
-    path: str, exclude: Optional[List[str]] = None
-) -> tuple:
-    """
-    Check if any ``aws_security_group`` allows all outbound traffic.
-
-    The following checks are performed:
-
-    * F1000 Missing egress rule means all traffic is allowed outbound,
-        Make this explicit if it is desired configuration
-
-    When you specify a VPC security group, Amazon EC2 creates a
-    **default egress rule** that **allows egress traffic** on **all ports
-    and IP protocols to any location**.
-
-    The default rule is removed only when you specify one or more egress
-    rules in the **egress** directive.
-
-    :param path: Location of Terraform template file.
-    :param exclude: Paths that contains any string from this list are ignored.
-    :returns: - ``OPEN`` if any of the referenced rules is not followed.
-              - ``UNKNOWN`` on errors.
-              - ``CLOSED`` otherwise.
-    :rtype: :class:`fluidasserts.Result`
-    """
-    vulnerabilities: list = []
-    for yaml_path, res_name, res_props in helper.iterate_rsrcs_in_tf_template(
-        starting_path=path,
-        resource_types=[
-            "aws_security_group",
-        ],
-        exclude=exclude,
-    ):
-
-        security_groups_egress = res_props.get("egress", [])
-
-        if not security_groups_egress:
-            vulnerabilities.append(
-                Vulnerability(
-                    path=yaml_path,
-                    entity="aws_security_group",
-                    identifier=res_name,
-                    reason="allows all outbound traffic",
-                )
-            )
-
-    return _get_result_as_tuple(
-        vulnerabilities=vulnerabilities,
-        msg_open="EC2 security groups allows all outbound traffic",
-        msg_closed="EC2 security groups do not allow all outbound traffic",
-    )
-
-
-@api(risk=MEDIUM, kind=SAST)
-@unknown_if(FileNotFoundError)
 def has_unrestricted_ip_protocols(
     path: str, exclude: Optional[List[str]] = None
 ) -> tuple:
