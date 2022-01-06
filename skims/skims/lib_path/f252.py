e01c383d135960354145dedc3c48402ff01fe770
Juan Echeverri <jecheverri@fluidattacks.com>
2022-01-05 20:29:26 -0500

    skims\feat(back): #5381.281 check 252 aws tfm
    - Add method to check if aws EC2 has unrestricted ports
    - Add test to lib_path/f252
    - Update lib_path.csv
    - SPEED: 7 tfm_method / 2 day = 3.5 tfm_methods/day
    - TODO: 5 tfm_methods
    - ETA: 5 / 3.5 = 1.4 days
    - no-deltas-test

--- skims/skims/lib_path/f252.py
+++ skims/skims/lib_path/f252.py
@@ -3,9 +3,14 @@ from aioextensions import (
 )
 from aws.model import (
     AWSEC2,
+    AWSEC2Rule,
+)
+from itertools import (
+    chain,
 )
 from lib_path.common import (
     EXTENSIONS_CLOUDFORMATION,
+    EXTENSIONS_TERRAFORM,
     get_cloud_iterator,
     get_vulnerabilities_from_iterator_blocking,
     SHIELD,
@@ -21,6 +26,18 @@ from parse_cfn.loader import (
 )
 from parse_cfn.structure import (
     iter_ec2_ingress_egress,
+)
+from parse_hcl2.common import (
+    get_argument,
+    get_attribute,
+    get_block_attribute,
+)
+from parse_hcl2.loader import (
+    load as load_terraform,
+)
+from parse_hcl2.structure.aws import (
+    iter_aws_security_group,
+    iter_aws_security_group_rule,
 )
 from state.cache import (
     CACHE_ETERNALLY,
@@ -55,6 +72,42 @@ def _cfn_ec2_has_unrestricted_ports_iterate_vulnerabilities(
             yield from_port
 
 
+def tfm_ec2_has_unrestricted_ports_iterate_vulnerabilities(
+    resource_iterator: Iterator[Any],
+) -> Iterator[Any]:
+    for resource in resource_iterator:
+        if isinstance(resource, AWSEC2):
+            if ingress_block := get_argument(
+                key="ingress",
+                body=resource.data,
+            ):
+                ingress_from_port = get_block_attribute(
+                    block=ingress_block, key="from_port"
+                )
+                ingress_to_port = get_block_attribute(
+                    block=ingress_block, key="to_port"
+                )
+                if float(ingress_from_port.val) != float(ingress_to_port.val):
+                    yield ingress_block
+            if egress_block := get_argument(
+                key="egress",
+                body=resource.data,
+            ):
+                egress_from_port = get_block_attribute(
+                    block=egress_block, key="from_port"
+                )
+                egress_to_port = get_block_attribute(
+                    block=egress_block, key="to_port"
+                )
+                if float(egress_from_port.val) != float(egress_to_port.val):
+                    yield egress_block
+        elif isinstance(resource, AWSEC2Rule):
+            from_port_attr = get_attribute(body=resource.data, key="from_port")
+            to_port_attr = get_attribute(body=resource.data, key="to_port")
+            if float(from_port_attr.val) != float(to_port_attr.val):
+                yield resource
+
+
 def _cfn_ec2_has_unrestricted_ports(
     content: str,
     path: str,
@@ -76,6 +129,28 @@ def _cfn_ec2_has_unrestricted_ports(
     )
 
 
+def _tfm_ec2_has_unrestricted_ports(
+    content: str,
+    path: str,
+    model: Any,
+) -> core_model.Vulnerabilities:
+    return get_vulnerabilities_from_iterator_blocking(
+        content=content,
+        cwe={_FINDING_F252_CWE},
+        description_key="src.lib_path.f252.ec2_has_unrestricted_ports",
+        finding=_FINDING_F252,
+        iterator=get_cloud_iterator(
+            tfm_ec2_has_unrestricted_ports_iterate_vulnerabilities(
+                resource_iterator=chain(
+                    iter_aws_security_group(model=model),
+                    iter_aws_security_group_rule(model=model),
+                )
+            )
+        ),
+        path=path,
+    )
+
+
 @CACHE_ETERNALLY
 @SHIELD
 @TIMEOUT_1MIN
@@ -89,6 +164,22 @@ async def cfn_ec2_has_unrestricted_ports(
         content=content,
         path=path,
         template=template,
+    )
+
+
+#  @CACHE_ETERNALLY
+@SHIELD
+@TIMEOUT_1MIN
+async def tfm_ec2_has_unrestricted_ports(
+    content: str,
+    path: str,
+    model: Any,
+) -> core_model.Vulnerabilities:
+    return await in_process(
+        _tfm_ec2_has_unrestricted_ports,
+        content=content,
+        path=path,
+        model=model,
     )
 
 
@@ -112,5 +203,15 @@ async def analyze(
                     template=template,
                 )
             )
+    if file_extension in EXTENSIONS_TERRAFORM:
+        content = await content_generator()
+        model = await load_terraform(stream=content, default=[])
+        coroutines.append(
+            tfm_ec2_has_unrestricted_ports(
+                content=content,
+                path=path,
+                model=model,
+            )
+        )
 
     return coroutines
