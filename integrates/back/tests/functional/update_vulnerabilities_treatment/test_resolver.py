82a85de47a83c1a834ef7c59fcaf0f2209292107
Sebastian Lizcano <slizcano@fluidattacks.com>
2022-01-06 08:25:50 -0500

    integrates\test(back): #5808.4 update functional test
    - update functional test to verify that user with pending
    `invitation_state` cannot be used as a valid assigned to
    vulnerabilities treatment
    - no-deltas-test

--- integrates/back/tests/functional/update_vulnerabilities_treatment/test_resolver.py
+++ integrates/back/tests/functional/update_vulnerabilities_treatment/test_resolver.py
@@ -1,6 +1,11 @@
 from . import (
+    get_stakeholders,
     get_vulnerability,
+    grant_stakeholder,
     put_mutation,
+)
+from custom_exceptions import (
+    InvalidAssigned,
 )
 from freezegun.api import (  # type: ignore
     freeze_time,
@@ -130,3 +135,53 @@ async def test_update_vulnerabilities_treatment_fail(
     )
     assert "errors" in result
     assert result["errors"][0]["message"] == "Access denied"
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group("update_vulnerabilities_treatment")
+@pytest.mark.parametrize(
+    ("email", "vulnerability", "treatment", "assigned"),
+    (
+        (
+            "customeradmin@gmail.com",
+            "be09edb7-cd5c-47ed-bee4-97c645acdce8",
+            "IN_PROGRESS",
+            "nonconfirmeduser@test.com",
+        ),
+    ),
+)
+async def test_update_vulnerabilities_treatment_non_confirmed(
+    populate: bool,
+    email: str,
+    vulnerability: str,
+    treatment: str,
+    assigned: str,
+) -> None:
+    assert populate
+    finding_id: str = "3c475384-834c-47b0-ac71-a41a022e401c"
+    group_name: str = "group1"
+    result_grant: Dict[str, Any] = await grant_stakeholder(
+        user=email,
+        stakeholder=assigned,
+        group=group_name,
+    )
+    assert "errors" not in result_grant
+    assert result_grant["data"]["grantStakeholderAccess"]["success"]
+
+    stakeholders: Dict[str, Any] = await get_stakeholders(
+        user=email, group=group_name
+    )
+    for stakeholder in stakeholders["data"]["group"]["stakeholders"]:
+        if stakeholder["email"] == assigned:
+            assert stakeholder["invitationState"] == "PENDING"
+
+    result: Dict[str, Any] = await put_mutation(
+        user=email,
+        finding=finding_id,
+        vulnerability=vulnerability,
+        treatment=treatment,
+        assigned=assigned,
+        acceptance_date="",
+    )
+    assert "errors" in result
+    assert result["errors"][0]["message"] == str(InvalidAssigned())
