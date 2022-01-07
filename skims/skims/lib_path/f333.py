946a0db507801bd0e114eab6db327da28ee5417b
Juan Echeverri <jecheverri@fluidattacks.com>
2022-01-06 19:24:24 -0500

    skims\feat(back): #5381.286 check f333 aws tfm
    - Add method to check if EC2 will be launched with public ip addresses
    - Add test to lib_path/f333
    - Update lib_path.csv
    - SPEED: 9 tfm_method / 3 day = 3 tfm_methods/day
    - TODO: 3 tfm_methods
    - ETA: 3 / 3 = 1 days
    - no-deltas-test

--- skims/skims/lib_path/f333.py
+++ skims/skims/lib_path/f333.py
@@ -4,6 +4,11 @@ from aioextensions import (
 from aws.model import (
     AWSEC2,
     AWSIamManagedPolicy,
+    AWSInstance,
+    AWSLaunchTemplate,
+)
+from itertools import (
+    chain,
 )
 from lib_path.common import (
     EXTENSIONS_CLOUDFORMATION,
@@ -29,7 +34,9 @@ from parse_cfn.structure import (
     iterate_iam_policy_documents,
 )
 from parse_hcl2.common import (
+    get_argument,
     get_attribute,
+    get_block_attribute,
 )
 from parse_hcl2.loader import (
     load as load_terraform,
@@ -146,6 +153,28 @@ def tfm_ec2_has_not_an_iam_instance_profile_iterate_vulnerabilities(
             body=resource.data,
         ):
             yield resource
+
+
+def tfm_ec2_associate_public_ip_address_iterate_vulnerabilities(
+    resource_iterator: Iterator[Any],
+) -> Iterator[Any]:
+    for resource in resource_iterator:
+        if isinstance(resource, AWSLaunchTemplate):
+            if network_interfaces := get_argument(
+                key="network_interfaces",
+                body=resource.data,
+            ):
+                net_public_ip = get_block_attribute(
+                    block=network_interfaces, key="associate_public_ip_address"
+                )
+                if net_public_ip.val is True:
+                    yield net_public_ip
+        elif isinstance(resource, AWSInstance):
+            public_ip = get_attribute(
+                body=resource.data, key="associate_public_ip_address"
+            )
+            if public_ip.val is True:
+                yield public_ip
 
 
 def _ec2_has_terminate_shutdown_behavior(
@@ -270,6 +299,28 @@ def _tfm_ec2_has_not_an_iam_instance_profile(
     )
 
 
+def _tfm_ec2_associate_public_ip_address(
+    content: str,
+    path: str,
+    model: Any,
+) -> core_model.Vulnerabilities:
+    return get_vulnerabilities_from_iterator_blocking(
+        content=content,
+        cwe={_FINDING_F333_CWE},
+        description_key=("lib_path.f333.ec2_public_ip_addresses"),
+        finding=_FINDING_F333,
+        iterator=get_cloud_iterator(
+            tfm_ec2_associate_public_ip_address_iterate_vulnerabilities(
+                resource_iterator=chain(
+                    iter_aws_instance(model=model),
+                    iter_aws_launch_template(model=model),
+                )
+            )
+        ),
+        path=path,
+    )
+
+
 @CACHE_ETERNALLY
 @SHIELD
 @TIMEOUT_1MIN
@@ -370,6 +421,22 @@ async def tfm_ec2_has_not_an_iam_instance_profile(
     )
 
 
+@CACHE_ETERNALLY
+@SHIELD
+@TIMEOUT_1MIN
+async def tfm_ec2_associate_public_ip_address(
+    content: str,
+    path: str,
+    model: Any,
+) -> core_model.Vulnerabilities:
+    return await in_process(
+        _tfm_ec2_associate_public_ip_address,
+        content=content,
+        path=path,
+        model=model,
+    )
+
+
 @SHIELD
 async def analyze(
     content_generator: Callable[[], Awaitable[str]],
@@ -425,6 +492,13 @@ async def analyze(
         )
         coroutines.append(
             tfm_ec2_has_not_an_iam_instance_profile(
+                content=content,
+                path=path,
+                model=model,
+            )
+        )
+        coroutines.append(
+            tfm_ec2_associate_public_ip_address(
                 content=content,
                 path=path,
                 model=model,
