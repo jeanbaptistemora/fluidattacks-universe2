6cc8e454e7478110e7e29d2b4efc8e63538fb85c
Andres Aguirre <aaguirre@fluidattacks.com>
2022-01-11 15:11:02 +0000

    integrates\refac(back): #4329.817 get afficacy
    - get the vulnerability historic verifaction inside
    the get efficacy domain function

--- integrates/back/src/vulnerabilities/domain/verification.py
+++ integrates/back/src/vulnerabilities/domain/verification.py
@@ -10,14 +10,18 @@ from decimal import (
     Decimal,
 )
 from typing import (
+    Any,
     Tuple,
 )
 
 
-def get_efficacy(
-    historic: Tuple[VulnerabilityVerification, ...],
+async def get_efficacy(
+    loaders: Any,
     vuln: Vulnerability,
 ) -> Decimal:
+    historic: Tuple[
+        VulnerabilityVerification, ...
+    ] = await loaders.vulnerability_historic_verification.load(vuln.id)
     cycles: int = get_reattack_cycles(historic)
     if cycles and vuln.state.status == VulnerabilityStateStatus.CLOSED:
         return Decimal(100 / cycles).quantize(Decimal("0.01"))
