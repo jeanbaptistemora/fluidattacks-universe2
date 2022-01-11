7fc8c39597c574d7fa64b6968825e3c5f60a84a7
Jonathan Mesa <jmesa@fluidattacks.com>
2022-01-11 15:23:15 -0500

    integrates\feat(back): #5690.5 redshift vulns
    - add vulns metadata table definition in a suitable fn for
    initialization

--- integrates/back/src/db_model/vulnerabilities/remove.py
+++ integrates/back/src/db_model/vulnerabilities/remove.py
@@ -2,7 +2,7 @@ from boto3.dynamodb.conditions import (
     Key,
 )
 from db_model import (
-    TABLE,
+    TABLE as DYNAMODB_TABLE,
 )
 from dynamodb import (
     keys,
@@ -11,27 +11,44 @@ from dynamodb import (
 from dynamodb.types import (
     PrimaryKey,
 )
+import logging
+import logging.config
+from redshift import (
+    operations as redshift_ops,
+)
+from redshift.operations import (
+    SCHEMA_NAME,
+)
+from settings import (
+    LOGGING,
+    NOEXTRA,
+)
+
+logging.config.dictConfig(LOGGING)
+
+LOGGER = logging.getLogger(__name__)
+REDSHIFT_METADATA_TABLE: str = f"{SCHEMA_NAME}.vulns_metadata"
 
 
 async def remove(*, vulnerability_id: str) -> None:
     primary_key = keys.build_key(
-        facet=TABLE.facets["vulnerability_metadata"],
+        facet=DYNAMODB_TABLE.facets["vulnerability_metadata"],
         values={"id": vulnerability_id},
     )
 
-    key_structure = TABLE.primary_key
+    key_structure = DYNAMODB_TABLE.primary_key
     response = await operations.query(
         condition_expression=(
             Key(key_structure.partition_key).eq(primary_key.partition_key)
         ),
         facets=(
-            TABLE.facets["vulnerability_metadata"],
-            TABLE.facets["vulnerability_historic_state"],
-            TABLE.facets["vulnerability_historic_treatment"],
-            TABLE.facets["vulnerability_historic_verification"],
-            TABLE.facets["vulnerability_historic_zero_risk"],
+            DYNAMODB_TABLE.facets["vulnerability_metadata"],
+            DYNAMODB_TABLE.facets["vulnerability_historic_state"],
+            DYNAMODB_TABLE.facets["vulnerability_historic_treatment"],
+            DYNAMODB_TABLE.facets["vulnerability_historic_verification"],
+            DYNAMODB_TABLE.facets["vulnerability_historic_zero_risk"],
         ),
-        table=TABLE,
+        table=DYNAMODB_TABLE,
     )
     await operations.batch_delete_item(
         keys=tuple(
@@ -41,5 +58,29 @@ async def remove(*, vulnerability_id: str) -> None:
             )
             for item in response.items
         ),
-        table=TABLE,
+        table=DYNAMODB_TABLE,
+    )
+
+
+async def initialize_metadata_table() -> None:
+    LOGGER.info(
+        f"Ensuring {REDSHIFT_METADATA_TABLE} table exists...", **NOEXTRA
+    )
+    await redshift_ops.execute(
+        f"""
+            CREATE TABLE IF NOT EXISTS {REDSHIFT_METADATA_TABLE} (
+                id VARCHAR,
+                custom_severity INTEGER,
+                finding_id VARCHAR NOT NULL,
+                skims_method VARCHAR,
+                type VARCHAR NOT NULL,
+
+                UNIQUE (
+                    id
+                ),
+                PRIMARY KEY (
+                    id
+                )
+            )
+        """,
     )
