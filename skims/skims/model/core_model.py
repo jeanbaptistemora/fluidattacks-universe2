2205b4f6f979fc6c0a103cec4b8ac586e53ec33b
Juan Echeverri <jecheverri@fluidattacks.com>
2022-01-14 18:59:49 -0500

    skims\feat(back): #5712.43 add f402 model
    - Update f402 in core_model.py
    - Update .json test and config files

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -2027,6 +2027,26 @@ class FindingEnum(Enum):
             user_interaction=cvss3_model.UserInteraction.none,
         ),
     )
+    F402: FindingMetadata = FindingMetadata.new(
+        auto_approve=False,
+        code="F402",
+        cwe=778,
+        execution_queue=ExecutionQueue.cloud,
+        requirements=[75, 376, 377, 378],
+        score=cvss3_model.Score(
+            attack_complexity=cvss3_model.AttackComplexity.high,
+            attack_vector=cvss3_model.AttackVector.network,
+            availability_impact=cvss3_model.AvailabilityImpact.none,
+            confidentiality_impact=cvss3_model.ConfidentialityImpact.none,
+            exploitability=cvss3_model.Exploitability.poc,
+            integrity_impact=cvss3_model.IntegrityImpact.low,
+            privileges_required=cvss3_model.PrivilegesRequired.low,
+            remediation_level=cvss3_model.RemediationLevel.official_fix,
+            report_confidence=cvss3_model.ReportConfidence.reasonable,
+            severity_scope=cvss3_model.SeverityScope.unchanged,
+            user_interaction=cvss3_model.UserInteraction.none,
+        ),
+    )
 
 
 FINDING_ENUM_FROM_STR: Dict[str, FindingEnum] = {
