7957935b382579317e546dd04c5b26c23a22d7bf
Alejandro Trujillo <atrujillo@fluidattacks.com>
2022-01-14 14:15:19 -0500

    skims\refac(back): #5848.7 requirements f400
    - added FindingEnum
    - added tests
    - added finding to manifests
    - no-deltas-test

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -1987,6 +1987,26 @@ class FindingEnum(Enum):
             user_interaction=cvss3_model.UserInteraction.none,
         ),
     )
+    F400: FindingMetadata = FindingMetadata.new(
+        auto_approve=False,
+        code="F400",
+        cwe=778,
+        execution_queue=ExecutionQueue.cloud,
+        requirements=[75, 376, 377, 378],
+        score=cvss3_model.Score(
+            attack_complexity=cvss3_model.AttackComplexity.high,
+            attack_vector=cvss3_model.AttackVector.network,
+            availability_impact=cvss3_model.AvailabilityImpact.none,
+            confidentiality_impact=cvss3_model.ConfidentialityImpact.none,
+            exploitability=cvss3_model.Exploitability.poc,
+            integrity_impact=cvss3_model.IntegrityImpact.low,
+            privileges_required=cvss3_model.PrivilegesRequired.low,
+            remediation_level=cvss3_model.RemediationLevel.official_fix,
+            report_confidence=cvss3_model.ReportConfidence.confirmed,
+            severity_scope=cvss3_model.SeverityScope.unchanged,
+            user_interaction=cvss3_model.UserInteraction.none,
+        ),
+    )
     F401: FindingMetadata = FindingMetadata.new(
         auto_approve=False,
         code="F401",
