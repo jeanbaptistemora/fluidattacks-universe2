0deed5a8fe5a54df5934d64ec2d5fcae26794274
Alejandro Trujillo <atrujillo@fluidattacks.com>
2022-01-14 11:49:01 -0500

    skims\refac(back): #5848.6 move check
    - added cfn and tfm methods to f024
    - added translation
    - added test
    - updated lib_path results
    - no-deltas-test

--- skims/skims/lib_path/f024_aws.py
+++ skims/skims/lib_path/f024_aws.py
@@ -380,6 +380,70 @@ def _cfn_ec2_has_security_groups_ip_ranges_in_rfc1918_iter_vulns(
             yield cidr
 
 
+def _cfn_ec2_has_unrestricted_ports_iterate_vulnerabilities(
+    ec2_iterator: Iterator[Union[AWSEC2, Node]],
+) -> Iterator[Union[AWSEC2, Node]]:
+    for ec2_res in ec2_iterator:
+        from_port = ec2_res.inner.get("FromPort")
+        to_port = ec2_res.inner.get("ToPort")
+        if (
+            from_port
+            and to_port
+            and float(from_port.raw) != float(to_port.raw)
+        ):
+            yield from_port
+
+
+def tfm_ec2_has_unrestricted_ports_iterate_vulnerabilities(
+    resource_iterator: Iterator[Any],
+) -> Iterator[Any]:
+    for resource in resource_iterator:
+        if isinstance(resource, AWSEC2):
+            if ingress_block := get_argument(
+                key="ingress",
+                body=resource.data,
+            ):
+                ingress_from_port = get_block_attribute(
+                    block=ingress_block, key="from_port"
+                )
+                ingress_to_port = get_block_attribute(
+                    block=ingress_block, key="to_port"
+                )
+                if (
+                    ingress_from_port
+                    and ingress_to_port
+                    and float(ingress_from_port.val)
+                    != float(ingress_to_port.val)
+                ):
+                    yield ingress_block
+            if egress_block := get_argument(
+                key="egress",
+                body=resource.data,
+            ):
+                egress_from_port = get_block_attribute(
+                    block=egress_block, key="from_port"
+                )
+                egress_to_port = get_block_attribute(
+                    block=egress_block, key="to_port"
+                )
+                if (
+                    egress_from_port
+                    and egress_to_port
+                    and float(egress_from_port.val)
+                    != float(egress_to_port.val)
+                ):
+                    yield egress_block
+        elif isinstance(resource, AWSEC2Rule):
+            from_port_attr = get_attribute(body=resource.data, key="from_port")
+            to_port_attr = get_attribute(body=resource.data, key="to_port")
+            if (
+                from_port_attr
+                and to_port_attr
+                and float(from_port_attr.val) != float(to_port_attr.val)
+            ):
+                yield resource
+
+
 def _cnf_unrestricted_ports(
     content: str,
     path: str,
@@ -530,6 +594,49 @@ def _cfn_ec2_has_security_groups_ip_ranges_in_rfc1918(
                 ec2_iterator=iter_ec2_ingress_egress(
                     template=template, ingress=True, egress=True
                 ),
+            )
+        ),
+        path=path,
+    )
+
+
+def _cfn_ec2_has_unrestricted_ports(
+    content: str,
+    path: str,
+    template: Any,
+) -> core_model.Vulnerabilities:
+    return get_vulnerabilities_from_iterator_blocking(
+        content=content,
+        cwe={_FINDING_F024_CWE},
+        description_key=("src.lib_path.f024.ec2_has_unrestricted_ports"),
+        finding=_FINDING_F024,
+        iterator=get_cloud_iterator(
+            _cfn_ec2_has_unrestricted_ports_iterate_vulnerabilities(
+                ec2_iterator=iter_ec2_ingress_egress(
+                    template=template, ingress=True, egress=True
+                ),
+            )
+        ),
+        path=path,
+    )
+
+
+def _tfm_ec2_has_unrestricted_ports(
+    content: str,
+    path: str,
+    model: Any,
+) -> core_model.Vulnerabilities:
+    return get_vulnerabilities_from_iterator_blocking(
+        content=content,
+        cwe={_FINDING_F024_CWE},
+        description_key="src.lib_path.f024.ec2_has_unrestricted_ports",
+        finding=_FINDING_F024,
+        iterator=get_cloud_iterator(
+            tfm_ec2_has_unrestricted_ports_iterate_vulnerabilities(
+                resource_iterator=chain(
+                    iter_aws_security_group(model=model),
+                    iter_aws_security_group_rule(model=model),
+                )
             )
         ),
         path=path,
@@ -706,6 +813,38 @@ async def cfn_ec2_has_security_groups_ip_ranges_in_rfc1918(
     )
 
 
+@CACHE_ETERNALLY
+@SHIELD
+@TIMEOUT_1MIN
+async def cfn_ec2_has_unrestricted_ports(
+    content: str,
+    path: str,
+    template: Any,
+) -> core_model.Vulnerabilities:
+    return await in_process(
+        _cfn_ec2_has_unrestricted_ports,
+        content=content,
+        path=path,
+        template=template,
+    )
+
+
+@CACHE_ETERNALLY
+@SHIELD
+@TIMEOUT_1MIN
+async def tfm_ec2_has_unrestricted_ports(
+    content: str,
+    path: str,
+    model: Any,
+) -> core_model.Vulnerabilities:
+    return await in_process(
+        _tfm_ec2_has_unrestricted_ports,
+        content=content,
+        path=path,
+        model=model,
+    )
+
+
 @SHIELD
 async def analyze(
     content_generator: Callable[[], Awaitable[str]],
@@ -762,6 +901,13 @@ async def analyze(
                     template=template,
                 )
             )
+            coroutines.append(
+                cfn_ec2_has_unrestricted_ports(
+                    content=content,
+                    path=path,
+                    template=template,
+                )
+            )
     if file_extension in EXTENSIONS_TERRAFORM:
         content = await content_generator()
         model = await load_terraform(stream=content, default=[])
@@ -786,4 +932,12 @@ async def analyze(
                 model=model,
             )
         )
+        coroutines.append(
+            tfm_ec2_has_unrestricted_ports(
+                content=content,
+                path=path,
+                model=model,
+            )
+        )
+
     return coroutines
