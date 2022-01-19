6c172d5ebab46850c8c621a247ebc562cd702b43
Diego Restrepo <drestrepo@fluidattacks.com>
2022-01-19 09:09:18 -0500

    skims\refac(back): #5836.12 increase rate limit
    - the uncontrolled use of requests is generating problems in ASM

--- skims/skims/utils/limits.py
+++ skims/skims/utils/limits.py
@@ -1,7 +1,7 @@
 # Units in requests-per-minute, rate limits apply to production environments
 
 # Rate limit to all calls to Integrates
-INTEGRATES_DEFAULT = 20  # at most 1 in a 6 seconds interval
+INTEGRATES_DEFAULT = 10  # at most 1 in a 6 seconds interval
 # Rate limit on specific funcionalities
 INTEGRATES_DO_UPDATE_EVIDENCE = 0.25  # at most 1 in a 4 minutes interval
 
