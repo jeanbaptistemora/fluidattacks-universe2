1b51552aaaacb5362cad3b4e239e7e7198ef3b4d
Sebastian Lizcano <slizcano@fluidattacks.com>
2022-01-19 17:48:54 -0500

    integrates\refac(back): #5658.58 update migration
    - update migration by increasing workers adding more logging and
    not using cache to avoid redis problems

--- integrates/back/migrations/0169_populate_vuln_id_email_and_gsi_3.py
+++ integrates/back/migrations/0169_populate_vuln_id_email_and_gsi_3.py
@@ -54,7 +54,7 @@ logging.config.dictConfig(LOGGING)
 # Constants
 LOGGER = logging.getLogger(__name__)
 LOGGER_CONSOLE = logging.getLogger("console")
-PROD = False
+PROD = True
 
 
 async def populate_index(*, current: Dict, group_name: str) -> None:
@@ -66,7 +66,7 @@ async def populate_index(*, current: Dict, group_name: str) -> None:
         treatment=format_vulnerability(current).treatment
     )
     if user_email:
-        enforcer = await get_group_level_enforcer(user_email)
+        enforcer = await get_group_level_enforcer(user_email, with_cache=False)
         if not enforcer(group_name, "valid_assigned"):
             user_email = ""
     gsi_3_key = keys.build_key(
@@ -97,6 +97,16 @@ async def populate_index(*, current: Dict, group_name: str) -> None:
     ) != vulnerability_item.get(
         gsi_3_index.primary_key.sort_key
     ):
+        LOGGER_CONSOLE.info(
+            "Item is going to be updated!",
+            extra={
+                "extra": {
+                    "vulnerability_item": vulnerability_item,
+                    "gsi_3_index": gsi_3_index,
+                    "vulnerability_key": vulnerability_key,
+                }
+            },
+        )
         if PROD:
             await operations.update_item(
                 condition_expression=Attr(
@@ -135,7 +145,7 @@ async def populate_by_finding(
             populate_index(current=current_item, group_name=group_name)
             for current_item in response.items
         ),
-        workers=8,
+        workers=32,
     )
 
 
@@ -144,6 +154,14 @@ async def populate_by_group(
     loaders: Dataloaders,
     group_name: str,
 ) -> None:
+    LOGGER_CONSOLE.info(
+        "Working on group",
+        extra={
+            "extra": {
+                "group_name": group_name,
+            }
+        },
+    )
     group_drafts_and_findings: Tuple[
         Finding, ...
     ] = await loaders.group_drafts_and_findings.load(group_name)
@@ -156,31 +174,31 @@ async def populate_by_group(
             populate_by_finding(finding=finding, group_name=group_name)
             for finding in all_findings
         ),
-        workers=2,
+        workers=16,
     )
 
 
 async def main() -> None:
     groups = await get_all_groups(data_attr="project_name")
     loaders = get_new_context()
+    groups_names = list(
+        sorted(set(str(group["project_name"]).lower() for group in groups))
+    )
     await collect(
         tuple(
-            populate_by_group(
-                loaders=loaders,
-                group_name=group["project_name"],
-            )
-            for group in groups
+            populate_by_group(loaders=loaders, group_name=group_name)
+            for group_name in groups_names
         ),
-        workers=2,
+        workers=8,
     )
 
 
 if __name__ == "__main__":
     execution_time = time.strftime(
-        "Execution Time:    %Y-%m-%d at %H:%M:%S UTC%Z"
+        "Execution Time:    %Y-%m-%d at %H:%M:%S UTC"
     )
     run(main())
     finalization_time = time.strftime(
-        "Finalization Time: %Y-%m-%d at %H:%M:%S UTC%Z"
+        "Finalization Time: %Y-%m-%d at %H:%M:%S UTC"
     )
     print(f"{execution_time}\n{finalization_time}")
