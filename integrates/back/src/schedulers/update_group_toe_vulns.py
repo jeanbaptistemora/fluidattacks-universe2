8548ea688d5a37e6650135a78d44fcbab450d7e1
Andres Aguirre <aaguirre@fluidattacks.com>
2022-01-19 19:15:13 +0000

    integrates\refac(back): #5469.209 update group toe vulns
    - Stop updating the be present attribute in this scheduler since it
    changes the be present until attribute
    - Process all concurrently to cut down the running time

--- integrates/back/src/schedulers/update_group_toe_vulns.py
+++ integrates/back/src/schedulers/update_group_toe_vulns.py
@@ -1,3 +1,6 @@
+from aioextensions import (
+    collect,
+)
 from dataloaders import (
     get_new_context,
 )
@@ -61,110 +64,138 @@ def _strip_first_dir(where: str) -> Tuple[str, str]:
     return (where[0 : where.find("/")], where[where.find("/") + 1 :])
 
 
-async def main() -> None:
-    groups = await groups_domain.get_alive_group_names()
+async def update_toe_input(
+    current_value: ToeInput, attributes: ToeInputAttributesToUpdate
+) -> None:
+    _log(
+        "  updating toe_input",
+        group_name=current_value.group_name,
+        has_vulnerabilities=attributes.has_vulnerabilities,
+        where=current_value.component,
+        specific=current_value.entry_point,
+    )
+    await toe_inputs_domain.update(current_value, attributes)
+
+
+async def process_toe_inputs(
+    group_name: str, vulnerabilities: Tuple[Vulnerability, ...]
+) -> None:
     loaders = get_new_context()
+    group_toe_inputs = await loaders.group_toe_inputs.load_nodes(
+        GroupToeInputsRequest(group_name=group_name)
+    )
+    updations = []
 
-    for group in groups:
-        _log("group", id=group)
-
-        _log("  getting root nicknames")
-        root_nicknames: Dict[str, str] = {
-            root.id: root.state.nickname
-            for root in await loaders.group_roots.load(group)
-        }
-
-        findings: Tuple[Finding, ...]
-        findings = await loaders.group_findings.load(group)
-
-        # pylint: disable=consider-using-generator
-        _log("  getting vulnerabilities in all findings")
-        vulnerabilities: Tuple[Vulnerability, ...] = tuple(
-            [
-                vuln
-                for finding in findings
-                for vuln in await loaders.finding_vulns_nzr_typed.load(
-                    finding.id
-                )
-                if vuln.state.status
-                in {
-                    VulnerabilityStateStatus.OPEN,
-                    VulnerabilityStateStatus.CLOSED,
-                }
-            ]
+    for toe_input in group_toe_inputs:
+        has_vulnerabilities: bool = any(
+            vulnerability.state.status is VulnerabilityStateStatus.OPEN
+            # ToeInput is not associated to a root_id
+            # and vulnerability.root_id == toe_input.root_id
+            and vulnerability.where == toe_input.component
+            and vulnerability.specific == toe_input.entry_point
+            for vulnerability in vulnerabilities
         )
 
-        _log("  getting toe inputs")
-        group_toe_inputs: Tuple[ToeInput, ...] = tuple(
-            edge.node
-            for conn in [
-                await loaders.group_toe_inputs.load(
-                    GroupToeInputsRequest(group_name=group)
-                )
-            ]
-            for edge in conn.edges
-        )
-        _log("  getting toe lines")
-        group_toe_lines: Tuple[ToeLines, ...] = tuple(
-            edge.node
-            for conn in [
-                await loaders.group_toe_lines.load(
-                    GroupToeLinesRequest(group_name=group)
-                )
-            ]
-            for edge in conn.edges
-        )
-
-        for toe_input in group_toe_inputs:
-            has_vulnerabilities: bool = any(
-                vulnerability.state.status is VulnerabilityStateStatus.OPEN
-                # ToeInput is not associated to a root_id
-                # and vulnerability.root_id == toe_input.root_id
-                and vulnerability.where == toe_input.component
-                and vulnerability.specific == toe_input.entry_point
-                for vulnerability in vulnerabilities
-            )
-
-            if toe_input.has_vulnerabilities != has_vulnerabilities:
-                _log(
-                    "  updating toe_input",
-                    has_vulnerabilities=has_vulnerabilities,
-                    where=toe_input.component,
-                    specific=toe_input.entry_point,
-                )
-
-                await toe_inputs_domain.update(
+        if toe_input.has_vulnerabilities != has_vulnerabilities:
+            updations.append(
+                update_toe_input(
                     toe_input,
                     ToeInputAttributesToUpdate(
-                        be_present=toe_input.be_present,
-                        has_vulnerabilities=has_vulnerabilities,
+                        has_vulnerabilities=has_vulnerabilities
                     ),
                 )
-
-        for toe_line in group_toe_lines:
-            has_vulnerabilities = any(
-                vulnerability.state.status is VulnerabilityStateStatus.OPEN
-                and vulnerability_where_repo
-                == root_nicknames[toe_line.root_id]
-                and vulnerability_where_path.startswith(toe_line.filename)
-                for vulnerability in vulnerabilities
-                for vulnerability_where_repo, vulnerability_where_path in [
-                    _strip_first_dir(vulnerability.where)
-                ]
             )
 
-            if toe_line.has_vulnerabilities != has_vulnerabilities:
-                _log(
-                    "  updating toe_line",
-                    filename=toe_line.filename,
-                    has_vulnerabilities=has_vulnerabilities,
-                    root_id=toe_line.root_id,
-                )
+    await collect(tuple(updations))
 
-                await toe_lines_domain.update(
+
+async def update_toe_lines(
+    current_value: ToeLines, attributes: ToeInputAttributesToUpdate
+) -> None:
+    _log(
+        "  updating toe_line",
+        group_name=current_value.group_name,
+        filename=current_value.filename,
+        has_vulnerabilities=attributes.has_vulnerabilities,
+        root_id=current_value.root_id,
+    )
+    await toe_lines_domain.update(current_value, attributes)
+
+
+async def process_toe_lines(
+    group_name: str,
+    vulnerabilities: Tuple[Vulnerability, ...],
+    root_nicknames: Dict[str, str],
+) -> None:
+    loaders = get_new_context()
+    group_toe_lines = await loaders.group_toe_lines.load_nodes(
+        GroupToeLinesRequest(group_name=group_name)
+    )
+
+    updations = []
+
+    for toe_line in group_toe_lines:
+        has_vulnerabilities = any(
+            vulnerability.state.status is VulnerabilityStateStatus.OPEN
+            and vulnerability_where_repo == root_nicknames[toe_line.root_id]
+            and vulnerability_where_path.startswith(toe_line.filename)
+            for vulnerability in vulnerabilities
+            for vulnerability_where_repo, vulnerability_where_path in [
+                _strip_first_dir(vulnerability.where)
+            ]
+        )
+
+        if toe_line.has_vulnerabilities != has_vulnerabilities:
+            updations.append(
+                update_toe_lines(
                     toe_line,
                     ToeLinesAttributesToUpdate(
-                        be_present=toe_line.be_present,
                         has_vulnerabilities=has_vulnerabilities,
+                        is_moving_toe_lines=True,
                     ),
                 )
+            )
+
+    await collect(tuple(updations))
+
+
+async def process_group(group_name: str) -> None:
+    loaders = get_new_context()
+    _log("group", id=group_name)
+    root_nicknames: Dict[str, str] = {
+        root.id: root.state.nickname
+        for root in await loaders.group_roots.load(group_name)
+    }
+
+    findings: Tuple[Finding, ...]
+    findings = await loaders.group_findings.load(group_name)
+
+    # pylint: disable=consider-using-generator
+    vulnerabilities: Tuple[Vulnerability, ...] = tuple(
+        [
+            vuln
+            for finding in findings
+            for vuln in await loaders.finding_vulns_nzr_typed.load(finding.id)
+            if vuln.state.status
+            in {
+                VulnerabilityStateStatus.OPEN,
+                VulnerabilityStateStatus.CLOSED,
+            }
+        ]
+    )
+
+    await collect(
+        (
+            process_toe_inputs(group_name, vulnerabilities),
+            process_toe_lines(group_name, vulnerabilities, root_nicknames),
+        )
+    )
+
+
+async def main() -> None:
+    group_names = await groups_domain.get_alive_group_names()
+
+    await collect(
+        tuple(process_group(group_name) for group_name in group_names),
+        workers=5,
+    )
