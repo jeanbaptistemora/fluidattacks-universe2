f139c87333f491c82e11ffb3b2961c3772fd20c6
Alejandro Trujillo <atrujillo@fluidattacks.com>
2022-01-19 15:49:22 -0500

    skims\refac(back): #5848.16 move check
    - moved methods to f250
    - added translations
    - moved tests
    - updated lib_path results
    - no-deltas-test

--- skims/skims/lib_path/f250.py
+++ skims/skims/lib_path/f250.py
@@ -3,9 +3,11 @@ from aioextensions import (
 )
 from aws.model import (
     AWSFSxFileSystem,
+    AWSS3Bucket,
 )
 from lib_path.common import (
     EXTENSIONS_CLOUDFORMATION,
+    EXTENSIONS_TERRAFORM,
     get_cloud_iterator,
     get_line_by_extension,
     get_vulnerabilities_from_iterator_blocking,
@@ -22,6 +24,16 @@ from parse_cfn.loader import (
 )
 from parse_cfn.structure import (
     iter_fsx_file_systems,
+    iter_s3_buckets,
+)
+from parse_hcl2.common import (
+    get_argument,
+)
+from parse_hcl2.loader import (
+    load as load_terraform,
+)
+from parse_hcl2.structure.aws import (
+    iter_s3_buckets as tfm_iter_s3_buckets,
 )
 from state.cache import (
     CACHE_ETERNALLY,
@@ -35,7 +47,6 @@ from typing import (
     Union,
 )
 from utils.function import (
-    get_node_by_keys,
     TIMEOUT_1MIN,
 )
 
@@ -48,12 +59,40 @@ def _cfn_fsx_has_unencrypted_volumes_iter_vulns(
     fsx_iterator: Iterator[Union[AWSFSxFileSystem, Node]],
 ) -> Iterator[Union[AWSFSxFileSystem, Node]]:
     for fsx in fsx_iterator:
-        kms_key_id = get_node_by_keys(fsx, ["KmsKeyId"])
+        kms_key_id = fsx.inner.get("KmsKeyId")
         if not isinstance(kms_key_id, Node):
             yield AWSFSxFileSystem(
                 column=fsx.start_column,
                 data=fsx.data,
                 line=get_line_by_extension(fsx.start_line, file_ext),
+            )
+
+
+def _tfm_unencrypted_buckets_iterate_vulnerabilities(
+    buckets_iterator: Iterator[Union[AWSS3Bucket, Node]]
+) -> Iterator[Union[AWSS3Bucket, Node]]:
+    for bucket in buckets_iterator:
+        if isinstance(bucket, Node):
+            if not bucket.raw.get("BucketEncryption", None):
+                yield bucket
+        elif isinstance(bucket, AWSS3Bucket) and not get_argument(
+            key="server_side_encryption_configuration",
+            body=bucket.data,
+        ):
+            yield bucket
+
+
+def _cfn_unencrypted_buckets_iterate_vulnerabilities(
+    file_ext: str,
+    buckets_iterator: Iterator[Union[AWSS3Bucket, Node]],
+) -> Iterator[Union[AWSS3Bucket, Node]]:
+    for bucket in buckets_iterator:
+        bck_encrypt = bucket.inner.get("BucketEncryption")
+        if not isinstance(bck_encrypt, Node):
+            yield AWSS3Bucket(
+                column=bucket.start_column,
+                data=bucket.data,
+                line=get_line_by_extension(bucket.start_line, file_ext),
             )
 
 
@@ -78,6 +117,46 @@ def _cfn_fsx_has_unencrypted_volumes(
     )
 
 
+def _cfn_unencrypted_buckets(
+    content: str,
+    file_ext: str,
+    path: str,
+    template: Any,
+) -> core_model.Vulnerabilities:
+    return get_vulnerabilities_from_iterator_blocking(
+        content=content,
+        cwe={_FINDING_F250_CWE},
+        description_key="src.lib_path.f250.unencrypted_buckets",
+        finding=_FINDING_F250,
+        iterator=get_cloud_iterator(
+            _cfn_unencrypted_buckets_iterate_vulnerabilities(
+                file_ext=file_ext,
+                buckets_iterator=iter_s3_buckets(template=template),
+            )
+        ),
+        path=path,
+    )
+
+
+def _tfm_unencrypted_buckets(
+    content: str,
+    path: str,
+    model: Any,
+) -> core_model.Vulnerabilities:
+    return get_vulnerabilities_from_iterator_blocking(
+        content=content,
+        cwe={_FINDING_F250_CWE},
+        description_key="src.lib_path.f250.unencrypted_buckets",
+        finding=_FINDING_F250,
+        iterator=get_cloud_iterator(
+            _tfm_unencrypted_buckets_iterate_vulnerabilities(
+                buckets_iterator=tfm_iter_s3_buckets(model=model)
+            )
+        ),
+        path=path,
+    )
+
+
 @CACHE_ETERNALLY
 @SHIELD
 @TIMEOUT_1MIN
@@ -93,6 +172,42 @@ async def cfn_fsx_has_unencrypted_volumes(
         file_ext=file_ext,
         path=path,
         template=template,
+    )
+
+
+@CACHE_ETERNALLY
+@SHIELD
+@TIMEOUT_1MIN
+async def cfn_unencrypted_buckets(
+    content: str,
+    file_ext: str,
+    path: str,
+    template: Any,
+) -> core_model.Vulnerabilities:
+    # cfn_nag W41 S3 Bucket should have encryption option set
+    return await in_process(
+        _cfn_unencrypted_buckets,
+        content=content,
+        file_ext=file_ext,
+        path=path,
+        template=template,
+    )
+
+
+@CACHE_ETERNALLY
+@SHIELD
+@TIMEOUT_1MIN
+async def tfm_unencrypted_buckets(
+    content: str,
+    path: str,
+    model: Any,
+) -> core_model.Vulnerabilities:
+    # cfn_nag W41 S3 Bucket should have encryption option set
+    return await in_process(
+        _tfm_unencrypted_buckets,
+        content=content,
+        path=path,
+        model=model,
     )
 
 
@@ -117,5 +232,23 @@ async def analyze(
                     template=template,
                 )
             )
+            coroutines.append(
+                cfn_unencrypted_buckets(
+                    content=content,
+                    file_ext=file_extension,
+                    path=path,
+                    template=template,
+                )
+            )
+    elif file_extension in EXTENSIONS_TERRAFORM:
+        content = content_generator()
+        model = await load_terraform(stream=content, default=[])
+        coroutines.append(
+            tfm_unencrypted_buckets(
+                content=content,
+                path=path,
+                model=model,
+            )
+        )
 
     return coroutines
