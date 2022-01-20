5530d5ab3943da82f4f49e86e355b990b016f7e8
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-01-20 15:29:04 -0500

    skims\feat(back): #5709.43 add finding to model
    - Add finding 082 to core_model
    - Add finding skims manifests .json files
    - Add finding to sast .json files
    - SPEED: 4 apk_methods / 5 days = 0.8 apk_methods/day
    - TODO: 5 apk_methods
    - ETA: 5 / 0.8 = 6.25 days

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -847,6 +847,26 @@ class FindingEnum(Enum):
             user_interaction=cvss3_model.UserInteraction.none,
         ),
     )
+    F082: FindingMetadata = FindingMetadata.new(
+        auto_approve=False,
+        code="F082",
+        cwe=459,
+        execution_queue=ExecutionQueue.apk,
+        requirements=[183],
+        score=cvss3_model.Score(
+            attack_complexity=cvss3_model.AttackComplexity.low,
+            attack_vector=cvss3_model.AttackVector.network,
+            availability_impact=cvss3_model.AvailabilityImpact.none,
+            confidentiality_impact=cvss3_model.ConfidentialityImpact.none,
+            exploitability=cvss3_model.Exploitability.unproven,
+            integrity_impact=cvss3_model.IntegrityImpact.none,
+            privileges_required=cvss3_model.PrivilegesRequired.none,
+            remediation_level=cvss3_model.RemediationLevel.unavailable,
+            report_confidence=cvss3_model.ReportConfidence.unknown,
+            severity_scope=cvss3_model.SeverityScope.unchanged,
+            user_interaction=cvss3_model.UserInteraction.none,
+        ),
+    )
     F085: FindingMetadata = FindingMetadata.new(
         auto_approve=True,
         code="F085",
