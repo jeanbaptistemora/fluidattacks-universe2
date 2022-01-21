f13cec5e54021dc00066a11a1e07aa034d64e20d
Alejandro Salgado <asalgado@fluidattacks.com>
2022-01-21 14:58:41 -0500

    skims\refac(back): #5868.30 refactor libpath f250
    - no-deltas-test
    - Refactor f250 checks of lib_path
    - Modify checks name in output csv

---
+++ skims/skims/lib_path/f250/cloudformation.py
@@ -0,0 +1,133 @@
+from aws.model import (
+    AWSEC2,
+    AWSFSxFileSystem,
+    AWSS3Bucket,
+)
+from lib_path.common import (
+    FALSE_OPTIONS,
+    get_cloud_iterator,
+    get_line_by_extension,
+    get_vulnerabilities_from_iterator_blocking,
+)
+from metaloaders.model import (
+    Node,
+)
+from model.core_model import (
+    FindingEnum,
+    Vulnerabilities,
+)
+from parse_cfn.structure import (
+    iter_ec2_volumes,
+    iter_fsx_file_systems,
+    iter_s3_buckets,
+)
+from typing import (
+    Any,
+    Iterator,
+    Union,
+)
+
+
+def _cfn_fsx_has_unencrypted_volumes_iter_vulns(
+    file_ext: str,
+    fsx_iterator: Iterator[Union[AWSFSxFileSystem, Node]],
+) -> Iterator[Union[AWSFSxFileSystem, Node]]:
+    for fsx in fsx_iterator:
+        kms_key_id = fsx.inner.get("KmsKeyId")
+        if not isinstance(kms_key_id, Node):
+            yield AWSFSxFileSystem(
+                column=fsx.start_column,
+                data=fsx.data,
+                line=get_line_by_extension(fsx.start_line, file_ext),
+            )
+
+
+def _cfn_unencrypted_buckets_iterate_vulnerabilities(
+    file_ext: str,
+    buckets_iterator: Iterator[Union[AWSS3Bucket, Node]],
+) -> Iterator[Union[AWSS3Bucket, Node]]:
+    for bucket in buckets_iterator:
+        bck_encrypt = bucket.inner.get("BucketEncryption")
+        if not isinstance(bck_encrypt, Node):
+            yield AWSS3Bucket(
+                column=bucket.start_column,
+                data=bucket.data,
+                line=get_line_by_extension(bucket.start_line, file_ext),
+            )
+
+
+def _cfn_ec2_has_unencrypted_volumes_iterate_vulnerabilities(
+    file_ext: str,
+    ec2_iterator: Iterator[Union[AWSEC2, Node]],
+) -> Iterator[Union[AWSEC2, Node]]:
+    for ec2_res in ec2_iterator:
+        if "Encrypted" not in ec2_res.raw:
+            yield AWSEC2(
+                column=ec2_res.start_column,
+                data=ec2_res.data,
+                line=get_line_by_extension(ec2_res.start_line, file_ext),
+            )
+        else:
+            vol_encryption = ec2_res.inner.get("Encrypted")
+            if vol_encryption.raw in FALSE_OPTIONS:
+                yield vol_encryption
+            elif "KmsKeyId" not in ec2_res.raw:
+                yield AWSEC2(
+                    column=ec2_res.start_column,
+                    data=ec2_res.data,
+                    line=get_line_by_extension(ec2_res.start_line, file_ext),
+                )
+
+
+def cfn_fsx_has_unencrypted_volumes(
+    content: str, file_ext: str, path: str, template: Any
+) -> Vulnerabilities:
+    return get_vulnerabilities_from_iterator_blocking(
+        content=content,
+        cwe={FindingEnum.F250.value.cwe},
+        description_key="src.lib_path.f250.fsx_has_unencrypted_volumes",
+        finding=FindingEnum.F250,
+        iterator=get_cloud_iterator(
+            _cfn_fsx_has_unencrypted_volumes_iter_vulns(
+                file_ext=file_ext,
+                fsx_iterator=iter_fsx_file_systems(template=template),
+            )
+        ),
+        path=path,
+    )
+
+
+def cfn_unencrypted_buckets(
+    content: str, file_ext: str, path: str, template: Any
+) -> Vulnerabilities:
+    return get_vulnerabilities_from_iterator_blocking(
+        content=content,
+        cwe={FindingEnum.F250.value.cwe},
+        description_key="src.lib_path.f250.unencrypted_buckets",
+        finding=FindingEnum.F250,
+        iterator=get_cloud_iterator(
+            _cfn_unencrypted_buckets_iterate_vulnerabilities(
+                file_ext=file_ext,
+                buckets_iterator=iter_s3_buckets(template=template),
+            )
+        ),
+        path=path,
+    )
+
+
+def cfn_ec2_has_unencrypted_volumes(
+    content: str, file_ext: str, path: str, template: Any
+) -> Vulnerabilities:
+    return get_vulnerabilities_from_iterator_blocking(
+        content=content,
+        cwe={FindingEnum.F250.value.cwe},
+        description_key="src.lib_path.f250.ec2_has_unencrypted_volumes",
+        finding=FindingEnum.F250,
+        iterator=get_cloud_iterator(
+            _cfn_ec2_has_unencrypted_volumes_iterate_vulnerabilities(
+                file_ext=file_ext,
+                ec2_iterator=iter_ec2_volumes(template=template),
+            )
+        ),
+        path=path,
+    )
