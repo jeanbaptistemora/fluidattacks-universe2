2409ba3598c75d0aeb9aae18d8f9ad9f4b3f5e4a
Sebastian Lizcano <slizcano@fluidattacks.com>
2022-01-21 09:50:22 -0500

    integrates\refac(front): #5658.70 update component
    - update `reattack_vulnerabilities` button component by moving to on the
    component the visibility based on `have` in group service_attributes

--- integrates/front/src/scenes/Dashboard/containers/Tasks/ActionsButtons/ReattackVulnerabilities/index.tsx
+++ integrates/front/src/scenes/Dashboard/containers/Tasks/ActionsButtons/ReattackVulnerabilities/index.tsx
@@ -1,17 +1,20 @@
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 
 import { Button } from "components/Button";
 import { FluidIcon } from "components/FluidIcon";
 import { TooltipWrapper } from "components/TooltipWrapper";
 import { Can } from "utils/authz/Can";
+import { Have } from "utils/authz/Have";
+import { msgInfo } from "utils/notifications";
 
 interface IReattackVulnButtonProps {
   areVulnsSelected: boolean;
   areVulnerabilitiesReattacked: boolean;
   isEditing: boolean;
+  isOpen: boolean;
   isRequestingReattack: boolean;
   onRequestReattack: () => void;
   openModal: () => void;
@@ -21,6 +24,7 @@ export const ReattackVulnerabilities: React.FC<IReattackVulnButtonProps> = ({
   areVulnsSelected,
   isEditing,
   areVulnerabilitiesReattacked,
+  isOpen,
   isRequestingReattack,
   onRequestReattack,
   openModal,
@@ -28,51 +32,61 @@ export const ReattackVulnerabilities: React.FC<IReattackVulnButtonProps> = ({
   const { t } = useTranslation();
 
   const shouldRenderRequestVerifyBtn: boolean = !isEditing;
+  const displayMessage = (): void => {
+    msgInfo(
+      t("searchFindings.tabVuln.info.text"),
+      t("searchFindings.tabVuln.info.title"),
+      !isRequestingReattack || isOpen
+    );
+  };
+  useEffect(displayMessage, [isRequestingReattack, isOpen, t]);
 
   return (
-    <Can do={"api_mutations_request_vulnerabilities_verification_mutate"}>
-      {isRequestingReattack ? (
-        <Button
-          disabled={!areVulnsSelected}
-          id={"confirm-reattack"}
-          onClick={openModal}
-        >
-          <FluidIcon icon={"verified"} />
-          &nbsp;
-          {t("searchFindings.tabVuln.buttons.reattack")}
-        </Button>
-      ) : undefined}
-      {shouldRenderRequestVerifyBtn ? (
-        <TooltipWrapper
-          displayClass={"dib"}
-          id={"searchFindings.tabVuln.buttonsTooltip.cancelReattack.id"}
-          message={
-            isRequestingReattack
-              ? t("searchFindings.tabVuln.buttonsTooltip.cancel")
-              : t("searchFindings.tabDescription.requestVerify.tooltip")
-          }
-        >
+    <Have I={"is_continuous"}>
+      <Can do={"api_mutations_request_vulnerabilities_verification_mutate"}>
+        {isRequestingReattack ? (
           <Button
-            disabled={areVulnerabilitiesReattacked}
-            id={"start-reattack"}
-            onClick={onRequestReattack}
+            disabled={!areVulnsSelected}
+            id={"confirm-reattack"}
+            onClick={openModal}
           >
-            {isRequestingReattack ? (
-              <React.Fragment>
-                <FontAwesomeIcon icon={faTimes} />
-                &nbsp;
-                {t("searchFindings.tabDescription.cancelVerify")}
-              </React.Fragment>
-            ) : (
-              <React.Fragment>
-                <FluidIcon icon={"verified"} />
-                &nbsp;
-                {t("searchFindings.tabDescription.requestVerify.text")}
-              </React.Fragment>
-            )}
+            <FluidIcon icon={"verified"} />
+            &nbsp;
+            {t("searchFindings.tabVuln.buttons.reattack")}
           </Button>
-        </TooltipWrapper>
-      ) : undefined}
-    </Can>
+        ) : undefined}
+        {shouldRenderRequestVerifyBtn ? (
+          <TooltipWrapper
+            displayClass={"dib"}
+            id={"searchFindings.tabVuln.buttonsTooltip.cancelReattack.id"}
+            message={
+              isRequestingReattack
+                ? t("searchFindings.tabVuln.buttonsTooltip.cancel")
+                : t("searchFindings.tabDescription.requestVerify.tooltip")
+            }
+          >
+            <Button
+              disabled={areVulnerabilitiesReattacked}
+              id={"start-reattack"}
+              onClick={onRequestReattack}
+            >
+              {isRequestingReattack ? (
+                <React.Fragment>
+                  <FontAwesomeIcon icon={faTimes} />
+                  &nbsp;
+                  {t("searchFindings.tabDescription.cancelVerify")}
+                </React.Fragment>
+              ) : (
+                <React.Fragment>
+                  <FluidIcon icon={"verified"} />
+                  &nbsp;
+                  {t("searchFindings.tabDescription.requestVerify.text")}
+                </React.Fragment>
+              )}
+            </Button>
+          </TooltipWrapper>
+        ) : undefined}
+      </Can>
+    </Have>
   );
 };
