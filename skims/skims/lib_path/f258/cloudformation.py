74707d0a298b0a8b653895b38fbb7b6e3080ae18
Alejandro Salgado <asalgado@fluidattacks.com>
2022-01-21 15:36:20 -0500

    skims\refac(back): #5868.32 refactor libpath f258
    - no-deltas-test
    - Refactor f258 checks of lib_path
    - Modify checks name in output csv

--- skims/skims/lib_path/f258/cloudformation.py
+++ skims/skims/lib_path/f258/cloudformation.py
@@ -1,47 +1,30 @@
-from aioextensions import (
-    in_process,
-)
 from aws.model import (
     AWSElbV2,
 )
 from lib_path.common import (
-    EXTENSIONS_CLOUDFORMATION,
     FALSE_OPTIONS,
     get_cloud_iterator,
     get_line_by_extension,
     get_vulnerabilities_from_iterator_blocking,
-    SHIELD,
 )
 from metaloaders.model import (
     Node,
 )
-from model import (
-    core_model,
-)
-from parse_cfn.loader import (
-    load_templates,
+from model.core_model import (
+    FindingEnum,
+    Vulnerabilities,
 )
 from parse_cfn.structure import (
     iter_elb2_load_balancers,
 )
-from state.cache import (
-    CACHE_ETERNALLY,
-)
 from typing import (
     Any,
-    Awaitable,
-    Callable,
     Iterator,
-    List,
     Union,
 )
 from utils.function import (
     get_node_by_keys,
-    TIMEOUT_1MIN,
 )
-
-_FINDING_F258 = core_model.FindingEnum.F258
-_FINDING_F258_CWE = _FINDING_F258.value.cwe
 
 
 def _cfn_elb2_has_not_deletion_protection_iterate_vulnerabilities(
@@ -74,17 +57,14 @@ def _cfn_elb2_has_not_deletion_protection_iterate_vulnerabilities(
                 )
 
 
-def _cfn_elb2_has_not_deletion_protection(
-    content: str,
-    file_ext: str,
-    path: str,
-    template: Any,
-) -> core_model.Vulnerabilities:
+def cfn_elb2_has_not_deletion_protection(
+    content: str, file_ext: str, path: str, template: Any
+) -> Vulnerabilities:
     return get_vulnerabilities_from_iterator_blocking(
         content=content,
-        cwe={_FINDING_F258_CWE},
+        cwe={FindingEnum.F258.value.cwe},
         description_key="src.lib_path.f258.elb2_has_not_deletion_protection",
-        finding=_FINDING_F258,
+        finding=FindingEnum.F258,
         iterator=get_cloud_iterator(
             _cfn_elb2_has_not_deletion_protection_iterate_vulnerabilities(
                 file_ext=file_ext,
@@ -95,46 +75,3 @@ def _cfn_elb2_has_not_deletion_protection(
         ),
         path=path,
     )
-
-
-@CACHE_ETERNALLY
-@SHIELD
-@TIMEOUT_1MIN
-async def cfn_elb2_has_not_deletion_protection(
-    content: str,
-    file_ext: str,
-    path: str,
-    template: Any,
-) -> core_model.Vulnerabilities:
-    return await in_process(
-        _cfn_elb2_has_not_deletion_protection,
-        content=content,
-        file_ext=file_ext,
-        path=path,
-        template=template,
-    )
-
-
-@SHIELD
-async def analyze(
-    content_generator: Callable[[], str],
-    file_extension: str,
-    path: str,
-    **_: None,
-) -> List[Awaitable[core_model.Vulnerabilities]]:
-    coroutines: List[Awaitable[core_model.Vulnerabilities]] = []
-    if file_extension in EXTENSIONS_CLOUDFORMATION:
-        content = content_generator()
-        async for template in load_templates(
-            content=content, fmt=file_extension
-        ):
-            coroutines.append(
-                cfn_elb2_has_not_deletion_protection(
-                    content=content,
-                    file_ext=file_extension,
-                    path=path,
-                    template=template,
-                )
-            )
-
-    return coroutines
