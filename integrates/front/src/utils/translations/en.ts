4beff95794dae775152f99bbcabc1168e1029180
Daniel Salazar <dsalazar@fluidattacks.com>
2022-01-21 12:58:35 -0500

    integrates\rever(front): #4393 rename new group btn
    - Rename new group button to its previous name

--- integrates/front/src/utils/translations/en.ts
+++ integrates/front/src/utils/translations/en.ts
@@ -1073,7 +1073,7 @@ export const enTranslations: ResourceKey = {
           name: "Group Name",
           new: {
             group: "New Group",
-            text: "Create Group",
+            text: "New Group",
             tooltip: "Create a new group",
           },
           noGroupName: "There are no group names available at the moment",
