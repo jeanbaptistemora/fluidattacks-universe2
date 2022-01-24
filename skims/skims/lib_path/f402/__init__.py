1d6be30f8c5650f78e64b908a941ddc12a3622c6
Alejandro Salgado <asalgado@fluidattacks.com>
2022-01-24 14:41:26 -0500

    skims\refac(back): #5868.44 refactor libpath f402
    - no-deltas-test
    - Refactor f402 checks of lib_path
    - Modify checks name in output csv

---
+++ skims/skims/lib_path/f402/__init__.py
@@ -0,0 +1,80 @@
+from aioextensions import (
+    in_process,
+)
+from lib_path.common import (
+    EXTENSIONS_TERRAFORM,
+    SHIELD,
+)
+from lib_path.f402.terraform import (
+    tfm_azure_app_service_logging_disabled,
+    tfm_azure_storage_logging_disabled,
+)
+from model.core_model import (
+    Vulnerabilities,
+)
+from parse_hcl2.loader import (
+    load as load_terraform,
+)
+from state.cache import (
+    CACHE_ETERNALLY,
+)
+from typing import (
+    Any,
+    Awaitable,
+    Callable,
+    List,
+)
+from utils.function import (
+    TIMEOUT_1MIN,
+)
+
+
+@CACHE_ETERNALLY
+@SHIELD
+@TIMEOUT_1MIN
+async def run_tfm_azure_storage_logging_disabled(
+    content: str, path: str, model: Any
+) -> Vulnerabilities:
+    return await in_process(
+        tfm_azure_storage_logging_disabled,
+        content=content,
+        path=path,
+        model=model,
+    )
+
+
+@CACHE_ETERNALLY
+@SHIELD
+@TIMEOUT_1MIN
+async def run_tfm_azure_app_service_logging_disabled(
+    content: str, path: str, model: Any
+) -> Vulnerabilities:
+    return await in_process(
+        tfm_azure_app_service_logging_disabled,
+        content=content,
+        path=path,
+        model=model,
+    )
+
+
+@SHIELD
+async def analyze(
+    content_generator: Callable[[], str],
+    file_extension: str,
+    path: str,
+    **_: None,
+) -> List[Awaitable[Vulnerabilities]]:
+    coroutines: List[Awaitable[Vulnerabilities]] = []
+
+    if file_extension in EXTENSIONS_TERRAFORM:
+        content = content_generator()
+        model = await load_terraform(stream=content, default=[])
+
+        coroutines.append(
+            run_tfm_azure_storage_logging_disabled(content, path, model)
+        )
+        coroutines.append(
+            run_tfm_azure_app_service_logging_disabled(content, path, model)
+        )
+
+    return coroutines
---
+++ skims/skims/lib_path/f402/terraform.py
@@ -0,0 +1,100 @@
+from lib_path.common import (
+    get_cloud_iterator,
+    get_vulnerabilities_from_iterator_blocking,
+)
+from model.core_model import (
+    FindingEnum,
+    Vulnerabilities,
+)
+from parse_hcl2.common import (
+    get_argument,
+    get_block_attribute,
+    get_block_block,
+)
+from parse_hcl2.structure.azure import (
+    iter_azurerm_app_service,
+    iter_azurerm_storage_account,
+)
+from typing import (
+    Any,
+    Iterator,
+)
+
+
+def _tfm_azure_storage_logging_disabled_iterate_vulnerabilities(
+    resource_iterator: Iterator[Any],
+) -> Iterator[Any]:
+    for resource in resource_iterator:
+        if queue_props := get_argument(
+            key="queue_properties",
+            body=resource.data,
+        ):
+            if logging_attr := get_block_block(queue_props, "logging"):
+                attrs = [
+                    get_block_attribute(logging_attr, req)
+                    for req in ["delete", "read", "write"]
+                ]
+                if not all((req.val if req else False for req in attrs)):
+                    yield logging_attr
+            else:
+                yield queue_props
+        else:
+            yield resource
+
+
+def _tfm_azure_app_service_logging_disabled_iterate_vulnerabilities(
+    resource_iterator: Iterator[Any],
+) -> Iterator[Any]:
+    for resource in resource_iterator:
+        if logs := get_argument(
+            key="logs",
+            body=resource.data,
+        ):
+            failed_request = get_block_attribute(
+                block=logs, key="failed_request_tracing_enabled"
+            )
+            detailed_error = get_block_attribute(
+                block=logs, key="detailed_error_messages_enabled"
+            )
+            if (not failed_request or failed_request.val is False) or (
+                not detailed_error or detailed_error.val is False
+            ):
+                yield logs
+        else:
+            yield resource
+
+
+def tfm_azure_storage_logging_disabled(
+    content: str, path: str, model: Any
+) -> Vulnerabilities:
+    return get_vulnerabilities_from_iterator_blocking(
+        content=content,
+        cwe={FindingEnum.F402.value.cwe},
+        description_key="lib_path.f402.tfm_azure_storage_logging_disabled",
+        finding=FindingEnum.F402,
+        iterator=get_cloud_iterator(
+            _tfm_azure_storage_logging_disabled_iterate_vulnerabilities(
+                resource_iterator=iter_azurerm_storage_account(model=model)
+            )
+        ),
+        path=path,
+    )
+
+
+def tfm_azure_app_service_logging_disabled(
+    content: str, path: str, model: Any
+) -> Vulnerabilities:
+    return get_vulnerabilities_from_iterator_blocking(
+        content=content,
+        cwe={FindingEnum.F402.value.cwe},
+        description_key=(
+            "lib_path.f402.tfm_azure_failed_request_tracing_disabled"
+        ),
+        finding=FindingEnum.F402,
+        iterator=get_cloud_iterator(
+            _tfm_azure_app_service_logging_disabled_iterate_vulnerabilities(
+                resource_iterator=iter_azurerm_app_service(model=model)
+            )
+        ),
+        path=path,
+    )
