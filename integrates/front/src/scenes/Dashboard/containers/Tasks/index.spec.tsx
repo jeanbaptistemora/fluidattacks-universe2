761b084daa925d25ba80ec5945a70f0204dbffe4
Sebastian Lizcano <slizcano@fluidattacks.com>
2022-01-25 08:45:35 -0500

    integrates\test(front): #5658.76 add mock queries
    - add mock data for `get_me_vulnerabilities_assigned` and
    `get_user_organizations_groups` queries
    - no-deltas-test

--- integrates/front/src/scenes/Dashboard/containers/Tasks/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/containers/Tasks/index.spec.tsx
@@ -1,16 +1,136 @@
+import type { MockedResponse } from "@apollo/client/testing";
 import { PureAbility } from "@casl/ability";
 import type { ReactWrapper } from "enzyme";
 import { mount } from "enzyme";
 import React from "react";
 import { act } from "react-dom/test-utils";
+import { useTranslation } from "react-i18next";
 import { MemoryRouter, Route } from "react-router-dom";
 import waitForExpect from "wait-for-expect";
 
 import { TasksContent } from "scenes/Dashboard/containers/Tasks";
 import { AssignedVulnerabilitiesContext } from "scenes/Dashboard/context";
+import {
+  GET_ME_VULNERABILITIES_ASSIGNED,
+  GET_USER_ORGANIZATIONS_GROUPS,
+} from "scenes/Dashboard/queries";
+import type {
+  IGetMeVulnerabilitiesAssigned,
+  IGetUserOrganizationsGroups,
+} from "scenes/Dashboard/types";
 import { authzPermissionsContext } from "utils/authz/config";
+import { msgError, msgInfo } from "utils/notifications";
+
+jest.mock("../../../../utils/notifications", (): Dictionary => {
+  const mockedNotifications: Dictionary<() => Dictionary> = jest.requireActual(
+    "../../../../utils/notifications"
+  );
+  jest.spyOn(mockedNotifications, "msgError").mockImplementation();
+  jest.spyOn(mockedNotifications, "msgInfo").mockImplementation();
+
+  return mockedNotifications;
+});
 
 describe("VulnerabilitiesView", (): void => {
+  const mocksVulnerabilities: MockedResponse = {
+    request: {
+      query: GET_ME_VULNERABILITIES_ASSIGNED,
+    },
+    result: {
+      data: {
+        me: {
+          vulnerabilitiesAssigned: [
+            {
+              __typename: "Vulnerability",
+              currentState: "open",
+              externalBugTrackingSystem: null,
+              findingId: "422286126",
+              groupName: "group1",
+              id: "89521e9a-b1a3-4047-a16e-15d530dc1340",
+              lastTreatmentDate: "2019-07-05 09:56:40",
+              lastVerificationDate: null,
+              remediated: false,
+              reportDate: "2019-07-05 09:56:40",
+              severity: "",
+              specific: "specific-1",
+              stream: "home > blog > articulo",
+              tag: "tag-1, tag-2",
+              treatment: "IN_PROGRESS",
+              treatmentAcceptanceDate: "",
+              treatmentAcceptanceStatus: "",
+              treatmentAssigned: "assigned-user-1",
+              treatmentDate: "2019-07-05 09:56:40",
+              treatmentJustification: "test progress justification",
+              verification: null,
+              vulnerabilityType: "inputs",
+              where: "https://example.com/inputs",
+              zeroRisk: null,
+            },
+            {
+              __typename: "Vulnerability",
+              currentState: "open",
+              externalBugTrackingSystem: null,
+              findingId: "422286126",
+              groupName: "group2",
+              id: "6903f3e4-a8ee-4a5d-ac38-fb738ec7e540",
+              lastTreatmentDate: "2019-07-05 09:56:40",
+              lastVerificationDate: null,
+              remediated: false,
+              reportDate: "2020-07-05 09:56:40",
+              severity: "",
+              specific: "specific-3",
+              stream: null,
+              tag: "tag-3",
+              treatment: "IN_PROGRESS",
+              treatmentAcceptanceDate: "",
+              treatmentAcceptanceStatus: "",
+              treatmentAssigned: "assigned-user-1",
+              treatmentDate: "2019-07-05 09:56:40",
+              treatmentJustification: "test progress justification",
+              verification: null,
+              vulnerabilityType: "lines",
+              where: "https://example.com/tests",
+              zeroRisk: null,
+            },
+          ],
+        },
+      },
+    },
+  };
+
+  const mocksUserGroups: MockedResponse = {
+    request: {
+      query: GET_USER_ORGANIZATIONS_GROUPS,
+    },
+    result: {
+      data: {
+        me: {
+          organizations: [
+            {
+              groups: [
+                {
+                  name: "group1",
+                  permissions: [
+                    "api_mutations_request_vulnerabilities_verification_mutate",
+                  ],
+                  serviceAttributes: ["is_continuous"],
+                },
+                {
+                  name: "group2",
+                  permissions: [
+                    "api_mutations_request_vulnerabilities_verification_mutate",
+                  ],
+                  serviceAttributes: [],
+                },
+              ],
+              name: "orgtest",
+            },
+          ],
+        },
+      },
+    },
+  };
+
   it("should return a function", (): void => {
     expect.hasAssertions();
 
