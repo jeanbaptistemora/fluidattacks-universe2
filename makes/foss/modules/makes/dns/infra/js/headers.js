c6bc32ba12af843d23167be829784fc9ed8ff1ca
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-01-31 11:06:01 -0500

    makes\refac(back): #5936.1 modify headers
    - Modify security headers

--- makes/foss/modules/makes/dns/infra/js/headers.js
+++ makes/foss/modules/makes/dns/infra/js/headers.js
@@ -1,7 +1,13 @@
 let securityHeaders = {
-  "Content-Security-Policy" : "upgrade-insecure-requests",
+  "Content-Security-Policy" : "script-src "
+                              + "'self'; "
+                              + "frame-ancestors "
+                              + "'self'; "
+                              + "object-src "
+                              + "'none'; "
+                              + "upgrade-insecure-requests;",
   "Strict-Transport-Security" : "max-age=31536000",
-  "X-Xss-Protection" : "1; mode=block",
+  "X-Xss-Protection" : "0",
   "X-Frame-Options" : "DENY",
   "X-Content-Type-Options" : "nosniff",
   "X-Permitted-Cross-Domain-Policies": "none",
