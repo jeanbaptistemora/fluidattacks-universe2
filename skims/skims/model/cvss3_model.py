8794fdb4e295656dffb28b22da124b97bb57b30f
Lewis Contreras <lcontreras@fluidattacks.com>
2022-01-31 09:27:43 -0500

    skims\refac(back): #4903.99 add score values
    - Add score values
    - Change constant names

--- skims/skims/model/cvss3_model.py
+++ skims/skims/model/cvss3_model.py
@@ -1,4 +1,3 @@
-# pylint: disable=invalid-name
 from enum import (
     Enum,
 )
@@ -10,69 +9,72 @@ from typing import (
 
 
 class AttackComplexity(Enum):
-    low: float = 0.77
-    high: float = 0.44
+    L: float = 0.77
+    H: float = 0.44
 
 
 class AttackVector(Enum):
-    physical: float = 0.20
-    local: float = 0.55
-    adjacent: float = 0.62
-    network: float = 0.85
+    P: float = 0.20
+    L: float = 0.55
+    A: float = 0.62
+    N: float = 0.85
 
 
 class AvailabilityImpact(Enum):
-    none: float = 0.00
-    low: float = 0.22
-    high: float = 0.56
+    N: float = 0.00
+    L: float = 0.22
+    H: float = 0.56
 
 
 class ConfidentialityImpact(Enum):
-    none: float = 0.00
-    low: float = 0.22
-    high: float = 0.56
+    N: float = 0.00
+    L: float = 0.22
+    H: float = 0.56
 
 
 class Exploitability(Enum):
-    unproven: float = 0.91
-    poc: float = 0.94
-    functional: float = 0.97
-    high: float = 1.0
+    U: float = 0.91
+    P: float = 0.94
+    X: float = 0.94
+    F: float = 0.97
+    H: float = 1.0
 
 
 class IntegrityImpact(Enum):
-    none: float = 0.00
-    low: float = 0.22
-    high: float = 0.56
+    N: float = 0.00
+    L: float = 0.22
+    H: float = 0.56
 
 
 class PrivilegesRequired(Enum):
-    none: float = 0.85
-    low: float = 0.62
-    high: float = 0.27
+    N: float = 0.85
+    L: float = 0.62
+    H: float = 0.27
 
 
 class RemediationLevel(Enum):
-    official_fix: float = 0.95
-    temporary_fix: float = 0.96
-    workaround: float = 0.97
-    unavailable: float = 1.00
+    O: float = 0.95
+    T: float = 0.96
+    W: float = 0.97
+    U: float = 1.00
+    X: float = 1.00
 
 
 class ReportConfidence(Enum):
-    unknown: float = 0.92
-    reasonable: float = 0.96
-    confirmed: float = 1.00
+    U: float = 0.92
+    R: float = 0.96
+    C: float = 1.00
+    X: float = 1.00
 
 
 class SeverityScope(Enum):
-    unchanged: float = 0.0
-    changed: float = 1.0
+    U: float = 0.0
+    C: float = 1.0
 
 
 class UserInteraction(Enum):
-    required: float = 0.62
-    none: float = 0.85
+    R: float = 0.62
+    N: float = 0.85
 
 
 class Score(NamedTuple):
