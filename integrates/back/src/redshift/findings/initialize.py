f416b931c25c14f92992f675b6d15b212e70b1ca
Jonathan Mesa <jmesa@fluidattacks.com>
2022-01-31 15:01:53 -0500

    integrates\feat(back): #5690.56 migrate to redshift
    - add additional queries for finding severity table, both versions

--- integrates/back/src/redshift/findings/initialize.py
+++ integrates/back/src/redshift/findings/initialize.py
@@ -18,6 +18,8 @@ logging.config.dictConfig(LOGGING)
 LOGGER = logging.getLogger(__name__)
 METADATA_TABLE: str = f"{SCHEMA_NAME}.findings_metadata"
 STATE_TABLE: str = f"{SCHEMA_NAME}.findings_state"
+SEVERITY_CVSS20: str = f"{SCHEMA_NAME}.findings_severity_cvss20"
+SEVERITY_CVSS31: str = f"{SCHEMA_NAME}.findings_severity_cvss31"
 VERIFICATION_TABLE: str = f"{SCHEMA_NAME}.findings_verification"
 VERIFICATION_VULN_IDS_TABLE: str = (
     f"{SCHEMA_NAME}.findings_verification_vuln_ids"
@@ -61,6 +63,76 @@ async def _initialize_state_table() -> None:
                 PRIMARY KEY (
                     id,
                     modified_date
+                ),
+                FOREIGN KEY (id)
+                    REFERENCES {METADATA_TABLE}(id)
+            )
+        """,
+    )
+
+
+async def _initialize_severity_cvss20_table() -> None:
+    LOGGER.info(f"Ensuring {SEVERITY_CVSS20} table exists...", **NOEXTRA)
+    await execute(
+        f"""
+            CREATE TABLE IF NOT EXISTS {SEVERITY_CVSS20} (
+                id VARCHAR,
+                access_complexity DECIMAL(5,3) NOT NULL,
+                access_vector DECIMAL(5,3) NOT NULL,
+                authentication DECIMAL(5,3) NOT NULL,
+                availability_impact DECIMAL(5,3) NOT NULL,
+                availability_requirement DECIMAL(5,3) NOT NULL,
+                collateral_damage_potential DECIMAL(5,3) NOT NULL,
+                confidence_level DECIMAL(5,3) NOT NULL,
+                confidentiality_impact DECIMAL(5,3) NOT NULL,
+                confidentiality_requirement DECIMAL(5,3) NOT NULL,
+                exploitability DECIMAL(5,3) NOT NULL,
+                finding_distribution DECIMAL(5,3) NOT NULL,
+                integrity_impact DECIMAL(5,3) NOT NULL,
+                integrity_requirement DECIMAL(5,3) NOT NULL,
+                resolution_level DECIMAL(5,3) NOT NULL
+
+                PRIMARY KEY (
+                    id
+                ),
+                FOREIGN KEY (id)
+                    REFERENCES {METADATA_TABLE}(id)
+            )
+        """,
+    )
+
+
+async def _initialize_severity_cvss31_table() -> None:
+    LOGGER.info(f"Ensuring {SEVERITY_CVSS31} table exists...", **NOEXTRA)
+    await execute(
+        f"""
+            CREATE TABLE IF NOT EXISTS {SEVERITY_CVSS31} (
+                id VARCHAR,
+                attack_complexity DECIMAL(4,2) NOT NULL,
+                attack_vector DECIMAL(4,2) NOT NULL,
+                availability_impact DECIMAL(4,2) NOT NULL,
+                availability_requirement DECIMAL(4,2) NOT NULL,
+                confidentiality_impact DECIMAL(4,2) NOT NULL,
+                confidentiality_requirement DECIMAL(4,2) NOT NULL,
+                exploitability DECIMAL(4,2) NOT NULL,
+                integrity_impact DECIMAL(4,2) NOT NULL,
+                integrity_requirement DECIMAL(4,2) NOT NULL,
+                modified_attack_complexity DECIMAL(4,2) NOT NULL,
+                modified_attack_vector DECIMAL(4,2) NOT NULL,
+                modified_availability_impact DECIMAL(4,2) NOT NULL,
+                modified_confidentiality_impact DECIMAL(4,2) NOT NULL,
+                modified_integrity_impact DECIMAL(4,2) NOT NULL,
+                modified_privileges_required DECIMAL(4,2) NOT NULL,
+                modified_user_interaction DECIMAL(4,2) NOT NULL,
+                modified_severity_scope DECIMAL(4,2) NOT NULL,
+                privileges_required DECIMAL(4,2) NOT NULL,
+                remediation_level DECIMAL(4,2) NOT NULL,
+                report_confidence DECIMAL(4,2) NOT NULL,
+                severity_scope DECIMAL(4,2) NOT NULL,
+                user_interaction DECIMAL(4,2) NOT NULL
+
+                PRIMARY KEY (
+                    id
                 ),
                 FOREIGN KEY (id)
                     REFERENCES {METADATA_TABLE}(id)
@@ -118,6 +190,8 @@ async def initialize_tables() -> None:
     await collect(
         (
             _initialize_state_table(),
+            _initialize_severity_cvss20_table(),
+            _initialize_severity_cvss31_table(),
             _initialize_verification_table(),
             _initialize_verification_vuln_ids_table(),
         )
