54e188d61fc7738eab89d587da3b0a8f77357160
Sebastian Lizcano <slizcano@fluidattacks.com>
2022-02-01 09:42:47 -0500

    integrates\test(back): #5865.5 update functional test
    - update functional test by adding a second test after requesting
    a group report that verify that a exception is raised

--- integrates/back/tests/functional/report/test_resolver.py
+++ integrates/back/tests/functional/report/test_resolver.py
@@ -1,6 +1,9 @@
 from . import (
     get_result,
 )
+from custom_exceptions import (
+    ReportAlreadyRequested,
+)
 import pytest
 from typing import (
     Any,
@@ -52,3 +55,25 @@ async def test_get_report_fail(populate: bool, email: str) -> None:
     )
     assert "errors" in result
     assert result["errors"][0]["message"] == "Access denied"
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group("report")
+@pytest.mark.parametrize(
+    ["email"],
+    [
+        ["admin@gmail.com"],
+        ["customeradmin@gmail.com"],
+        ["hacker@gmail.com"],
+        ["customer_manager@fluidattacks.com"],
+    ],
+)
+async def test_get_report_second_time_fail(populate: bool, email: str) -> None:
+    assert populate
+    group: str = "group1"
+    result: Dict[str, Any] = await get_result(
+        user=email,
+        group_name=group,
+    )
+    assert "errors" in result
+    assert result["errors"][0]["message"] == str(ReportAlreadyRequested())
