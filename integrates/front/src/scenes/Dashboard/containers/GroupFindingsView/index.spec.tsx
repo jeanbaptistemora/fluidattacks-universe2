5c0f240082e105da926e5858e2cd07eda3238bee
Sebastian Lizcano <slizcano@fluidattacks.com>
2022-02-01 11:55:56 -0500

    integrates\test(front): #5658.7 update unit test
    - update `reports_modal` unit test

--- integrates/front/src/scenes/Dashboard/containers/GroupFindingsView/index.spec.tsx
+++ integrates/front/src/scenes/Dashboard/containers/GroupFindingsView/index.spec.tsx
@@ -7,14 +7,29 @@ import React from "react";
 import { act } from "react-dom/test-utils";
 import { MemoryRouter, Route } from "react-router-dom";
 import wait from "waait";
+import waitForExpect from "wait-for-expect";
 
 import { Button } from "components/Button";
 import { DataTableNext } from "components/DataTableNext";
 import { CustomToggleList } from "components/DataTableNext/customToggleList";
 import type { ITableProps } from "components/DataTableNext/types";
 import { GroupFindingsView } from "scenes/Dashboard/containers/GroupFindingsView";
-import { GET_FINDINGS } from "scenes/Dashboard/containers/GroupFindingsView/queries";
+import {
+  GET_FINDINGS,
+  REQUEST_GROUP_REPORT,
+} from "scenes/Dashboard/containers/GroupFindingsView/queries";
 import { ReportsModal } from "scenes/Dashboard/containers/GroupFindingsView/reportsModal";
+import { msgError } from "utils/notifications";
+import { translate } from "utils/translations/translate";
+
+jest.mock("../../../../utils/notifications", (): Dictionary => {
+  const mockedNotifications: Dictionary<() => Dictionary> = jest.requireActual(
+    "../../../../utils/notifications"
+  );
+  jest.spyOn(mockedNotifications, "msgError").mockImplementation();
+
+  return mockedNotifications;
+});
 
 describe("GroupFindingsView", (): void => {
   const apolloDataMock: readonly MockedResponse[] = [
@@ -125,6 +140,23 @@ describe("GroupFindingsView", (): void => {
     },
   ];
 
+  const mockReportError: MockedResponse = {
+    request: {
+      query: REQUEST_GROUP_REPORT,
+      variables: {
+        groupName: "testgroup",
+        reportType: "PDF",
+      },
+    },
+    result: {
+      errors: [
+        new GraphQLError(
+          "Exception - The user already has a requested report for the same group"
+        ),
+      ],
+    },
+  };
+
   it("should return a function", (): void => {
     expect.hasAssertions();
     expect(typeof GroupFindingsView).toStrictEqual("function");
@@ -148,13 +180,23 @@ describe("GroupFindingsView", (): void => {
     expect(wrapper).toHaveLength(1);
   });
 
-  it("should render a svg", (): void => {
+  it("should render report modal and mock request error", async (): Promise<void> => {
     expect.hasAssertions();
+
+    jest.clearAllMocks();
 
     const handleClose: jest.Mock = jest.fn();
     const wrapper: ReactWrapper = mount(
-      <MemoryRouter initialEntries={["/groups/TEST/vulns"]}>
-        <ReportsModal hasMobileApp={true} isOpen={true} onClose={handleClose} />
+      <MemoryRouter initialEntries={["orgs/testorg/groups/testgroup/vulns"]}>
+        <MockedProvider addTypename={true} mocks={[mockReportError]}>
+          <Route path={"orgs/:organizationName/groups/:groupName/vulns"}>
+            <ReportsModal
+              hasMobileApp={true}
+              isOpen={true}
+              onClose={handleClose}
+            />
+          </Route>
+        </MockedProvider>
       </MemoryRouter>
     );
 
@@ -179,6 +221,20 @@ describe("GroupFindingsView", (): void => {
     expect(reportPdf).toStrictEqual("file-pdf");
     expect(reportXls).toStrictEqual("file-excel");
     expect(reportZip).toStrictEqual("file-archive");
+
+    const reporButtontPdf: ReactWrapper = wrapper.find("Button#report-pdf");
+
+    reporButtontPdf.simulate("click");
+
+    await act(async (): Promise<void> => {
+      await waitForExpect((): void => {
+        wrapper.update();
+
+        expect(msgError).toHaveBeenCalledWith(
+          translate.t("groupAlerts.reportAlreadyRequested")
+        );
+      });
+    });
   });
 
   it("should render an error in component", async (): Promise<void> => {
