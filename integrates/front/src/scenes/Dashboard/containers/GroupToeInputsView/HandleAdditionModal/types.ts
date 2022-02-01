662cc97dd7910387bd0f2e1b60301ad4dd90c61b
Andres Aguirre <aaguirre@fluidattacks.com>
2022-02-01 14:56:07 +0000

    integrates\feat(front): #5469.240 add modal types
    - Add initial types for the modal that handle the toe input addition

---
+++ integrates/front/src/scenes/Dashboard/containers/GroupToeInputsView/HandleAdditionModal/types.ts
@@ -0,0 +1,78 @@
+interface IFormValues {
+  component: string;
+  entryPoint: string;
+  rootId: string | undefined;
+}
+
+interface IHandleAdditionModalFormProps {
+  roots: Root[];
+  handleCloseModal: () => void;
+}
+
+interface IAddToeInputResultAttr {
+  addToeInput: {
+    success: boolean;
+  };
+}
+
+interface IHandleAdditionModalProps {
+  groupName: string;
+  handleCloseModal: () => void;
+  refetchData: () => void;
+}
+
+interface IGitRootAttr {
+  __typename: "GitRoot";
+  branch: string;
+  cloningStatus: {
+    message: string;
+    status: "FAIL" | "OK" | "UNKNOWN";
+  };
+  credentials: {
+    id: string;
+    key: string;
+    name: string;
+    type: "" | "SSH";
+  };
+  environment: string;
+  environmentUrls: string[];
+  gitignore: string[];
+  includesHealthCheck: boolean;
+  id: string;
+  nickname: string;
+  state: "ACTIVE" | "INACTIVE";
+  url: string;
+}
+
+interface IIPRootAttr {
+  __typename: "IPRoot";
+  address: string;
+  id: string;
+  nickname: string;
+  port: number;
+  state: "ACTIVE" | "INACTIVE";
+}
+
+interface IURLRootAttr {
+  __typename: "URLRoot";
+  host: string;
+  id: string;
+  nickname: string;
+  path: string;
+  port: number;
+  protocol: "HTTP" | "HTTPS";
+  state: "ACTIVE" | "INACTIVE";
+}
+
+type Root = IGitRootAttr | IIPRootAttr | IURLRootAttr;
+
+export {
+  IFormValues,
+  IHandleAdditionModalProps,
+  IHandleAdditionModalFormProps,
+  IAddToeInputResultAttr,
+  Root,
+  IGitRootAttr,
+  IIPRootAttr,
+  IURLRootAttr,
+};
