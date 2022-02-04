062a3113a96925318e59f8aa4c8fc4a7397e5725
Sebastian Lizcano <slizcano@fluidattacks.com>
2022-02-04 13:10:49 -0500

    integrates\test(back): #5881.10 add mock data
    - add mock data for `remove_stakeholder_organization_access` functional
    test to be used later on
    - no-deltas-test

--- integrates/back/tests/functional/remove_stakeholder_organization_access/__init__.py
+++ integrates/back/tests/functional/remove_stakeholder_organization_access/__init__.py
@@ -32,3 +32,60 @@ async def get_result(
         stakeholder=user,
         context=get_new_context(),
     )
+
+
+async def put_access_token(
+    *,
+    user: str,
+    expiration_time: int,
+) -> Dict[str, Any]:
+    query: str = """
+        mutation UpdateAccessTokenMutation($expirationTime: Int!) {
+            updateAccessToken(expirationTime: $expirationTime) {
+                sessionJwt
+                success
+            }
+        }
+    """
+    data: Dict[str, Any] = {
+        "query": query,
+        "variables": {
+            "expirationTime": expiration_time,
+        },
+    }
+
+    return await get_graphql_result(
+        data,
+        stakeholder=user,
+        context=get_new_context(),
+    )
+
+
+async def get_organizations(
+    *,
+    user: str,
+    session_jwt: str,
+) -> Dict[str, Any]:
+    query: str = """
+        query GetUserOrganizationsGroups {
+            me {
+                organizations {
+                    groups {
+                        name
+                        permissions
+                        serviceAttributes
+                    }
+                    name
+                }
+            }
+        }
+    """
+    data: Dict[str, str] = {
+        "query": query,
+    }
+    return await get_graphql_result(
+        data,
+        stakeholder=user,
+        context=get_new_context(),
+        session_jwt=session_jwt,
+    )
