213ee5576305a23caae6ea6e29c182d8c39d7511
Jonathan Mesa <jmesa@fluidattacks.com>
2022-02-07 13:50:08 -0500

    integrates\refac(back): #4329.881 vuln indicators
    - use unreliable_reattack_cycles for calculating efficacy

--- integrates/back/src/vulnerabilities/domain/verification.py
+++ integrates/back/src/vulnerabilities/domain/verification.py
@@ -21,10 +21,21 @@ async def get_efficacy(
     loaders: Any,
     vuln: Vulnerability,
 ) -> Decimal:
-    if vuln.state.status == VulnerabilityStateStatus.CLOSED:
-        cycles: int = await get_reattack_cycles(loaders, vuln)
-        if cycles:
-            return Decimal(100 / cycles).quantize(Decimal("0.01"))
+    if vuln.state.status != VulnerabilityStateStatus.CLOSED:
+        return Decimal(0)
+
+    if (
+        vuln.unreliable_indicators.unreliable_reattack_cycles
+        and vuln.verification.status
+        == VulnerabilityVerificationStatus.VERIFIED
+    ):
+        # If the indicator is present, we'll trust it
+        cycles = vuln.unreliable_indicators.unreliable_reattack_cycles
+    else:
+        cycles = await get_reattack_cycles(loaders, vuln)
+
+    if cycles:
+        return Decimal(100 / cycles).quantize(Decimal("0.01"))
     return Decimal(0)
 
 
