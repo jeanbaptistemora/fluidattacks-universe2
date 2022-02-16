14065b1b6e429451674d747e88fbb96f139bdfcf
Sebastian Lizcano <slizcano@fluidattacks.com>
2022-02-16 17:37:07 -0500

    integrates\refac(back): #5689.16 update generator
    - update `total_vulnerabilities_by_group` pie_chart generator by
    including organizations too

--- integrates/charts/generators/pie_chart/total_vulnerabilities_status.py
+++ integrates/charts/generators/pie_chart/total_vulnerabilities_status.py
@@ -39,13 +39,24 @@ async def get_data_group(group: str) -> PortfoliosGroupsInfo:
 
 
 async def get_data_groups(groups: List[str]) -> List[PortfoliosGroupsInfo]:
-    groups_data = await collect(map(get_data_group, groups))
+    groups_data = await collect(map(get_data_group, groups), workers=32)
     total_vulnerabilities = sum([group.value for group in groups_data])
 
     return slice_groups(groups_data, total_vulnerabilities)
 
 
 async def generate_all() -> None:
+    async for org_id, _, org_groups in (
+        utils.iterate_organizations_and_groups()
+    ):
+        utils.json_dump(
+            document=format_data(
+                groups_data=await get_data_groups(list(org_groups)),
+            ),
+            entity="organization",
+            subject=org_id,
+        )
+
     async for org_id, org_name, _ in utils.iterate_organizations_and_groups():
         for portfolio, groups in await utils.get_portfolios_groups(org_name):
             utils.json_dump(
