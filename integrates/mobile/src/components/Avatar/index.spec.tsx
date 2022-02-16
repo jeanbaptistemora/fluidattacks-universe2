2a40ab64126835b6f99621defc38bd3fb15d2fc2
Sebastian Lizcano <slizcano@fluidattacks.com>
2022-02-16 08:29:31 -0500

    integrates\test(front): #5906.2 update unit test
    - update unit test by adding apollo mocked provider as it will be
    added a `useMutation` one later on

--- integrates/mobile/src/components/Avatar/index.spec.tsx
+++ integrates/mobile/src/components/Avatar/index.spec.tsx
@@ -1,9 +1,33 @@
-import { shallow } from "enzyme";
-import type { ShallowWrapper } from "enzyme";
+import { MockedProvider } from "@apollo/client/testing";
+import { mount } from "enzyme";
+import type { ReactWrapper } from "enzyme";
 import React from "react";
-import { Avatar as PaperAvatar } from "react-native-paper";
+import { I18nextProvider } from "react-i18next";
+import {
+  Avatar as PaperAvatar,
+  Provider as PaperProvider,
+} from "react-native-paper";
+import { MemoryRouter } from "react-router-native";
 
 import { Avatar } from ".";
+import { i18next } from "../../utils/translations/translate";
+
+const mockHistoryReplace: jest.Mock = jest.fn();
+
+jest.mock("react-router-native", (): Record<string, unknown> => {
+  const mockedRouter: Record<string, () => Record<string, unknown>> =
+    jest.requireActual("react-router-native");
+
+  return {
+    ...mockedRouter,
+    useHistory: (): Record<string, unknown> => ({
+      ...mockedRouter.useHistory(),
+      replace: mockHistoryReplace,
+    }),
+  };
+});
+
+jest.mock("../../utils/socialAuth");
 
 describe("Avatar", (): void => {
   it("should return a function", (): void => {
@@ -15,29 +39,53 @@ describe("Avatar", (): void => {
   it("should render initials", (): void => {
     expect.hasAssertions();
 
-    const wrapper: ShallowWrapper = shallow(
-      <Avatar
-        photoUrl={undefined}
-        size={40}
-        userName={
-          "Simón José Antonio de la Santísima Trinidad Bolívar y Palacios Ponte-Andrade y Blanco"
-        }
-      />
+    const wrapper: ReactWrapper = mount(
+      <PaperProvider>
+        <I18nextProvider i18n={i18next}>
+          <MemoryRouter
+            initialEntries={[
+              { pathname: "/Dashboard", state: { user: { fullName: "Test" } } },
+            ]}
+          >
+            <MockedProvider addTypename={false} mocks={[]}>
+              <Avatar
+                photoUrl={undefined}
+                size={40}
+                userName={
+                  "Simón José Antonio de la Santísima Trinidad Bolívar y Palacios Ponte-Andrade y Blanco"
+                }
+              />
+            </MockedProvider>
+          </MemoryRouter>
+        </I18nextProvider>
+      </PaperProvider>
     );
 
     expect(wrapper).toHaveLength(1);
-    expect(wrapper.render().text()).toStrictEqual("SJ");
+    expect(wrapper.find(PaperAvatar.Text).text()).toStrictEqual("SJ");
   });
 
   it("should render profile picture", (): void => {
     expect.hasAssertions();
 
-    const wrapper: ShallowWrapper = shallow(
-      <Avatar
-        photoUrl={"https://some.com/image.png"}
-        size={40}
-        userName={"Test"}
-      />
+    const wrapper: ReactWrapper = mount(
+      <PaperProvider>
+        <I18nextProvider i18n={i18next}>
+          <MemoryRouter
+            initialEntries={[
+              { pathname: "/Dashboard", state: { user: { fullName: "Test" } } },
+            ]}
+          >
+            <MockedProvider addTypename={false} mocks={[]}>
+              <Avatar
+                photoUrl={"https://some.com/image.png"}
+                size={40}
+                userName={"Test"}
+              />
+            </MockedProvider>
+          </MemoryRouter>
+        </I18nextProvider>
+      </PaperProvider>
     );
 
     expect(wrapper).toHaveLength(1);
