8bc7d47f0d5af996c8061d8abec39b2104cbead1
Daniel Murcia <dmurcia@fluidattacks.com>
2022-02-16 16:46:58 -0500

    observes\feat(build): #5993.9 flake pkg
    - dynamo conf flake pkg init

---
+++ observes/etl/dynamo_etl_conf/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Dynamo ETLs configuration";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    purity.url = "gitlab:dmurciaatfluid/purity";
+  };
+  outputs = { self, nixpkgs, purity }:
+    let
+      system = "x86_64-linux";
+      legacy_pkgs = nixpkgs.legacyPackages."${system}";
+      lib = {
+        buildPythonPackage = legacy_pkgs.python39.pkgs.buildPythonPackage;
+        pytestCheckHook = legacy_pkgs.python39.pkgs.pytestCheckHook;
+        buildEnv = legacy_pkgs.python39.buildEnv;
+      };
+      pythonPkgs = legacy_pkgs.python39Packages // {
+        fa_purity = purity.defaultPackage."${system}";
+      };
+      src = self;
+      args = {
+        inherit src lib pythonPkgs;
+      };
+      self_pkg = import ./build/pkg/default.nix args;
+      build_env = env: import ./build/env.nix (args // {
+        inherit env;
+      });
+    in
+    {
+      packages."${system}" = {
+        pkg = self_pkg.runtime;
+        env.runtime = build_env "runtime";
+        env.dev = build_env "dev";
+      };
+      defaultPackage."${system}" = self.packages."${system}".pkg;
+    };
+}
