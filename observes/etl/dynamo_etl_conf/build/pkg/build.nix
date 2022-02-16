3578b0cc90977f1859b3d4706ce1d327d2a47de1
Daniel Murcia <dmurcia@fluidattacks.com>
2022-02-16 16:43:08 -0500

    observes\feat(build): #5993.7 dynamo conf
    - build pkg using nix

---
+++ observes/etl/dynamo_etl_conf/build/pkg/build.nix
@@ -0,0 +1,16 @@
+{ lib
+, src
+, propagatedBuildInputs
+, nativeBuildInputs
+}:
+lib.buildPythonPackage rec {
+  pname = "dynamo_etl_conf";
+  version = "0.1.0";
+  format = "pyproject";
+  checkInputs = [
+    lib.pytestCheckHook
+  ];
+  doCheck = true;
+  pythonImportsCheck = [ "dynamo_etl_conf" ];
+  inherit src propagatedBuildInputs nativeBuildInputs;
+}
