ad59563ea552f5f0228736a1ea90f43bfa8dc74e
Sebastian Lizcano <slizcano@fluidattacks.com>
2022-02-17 13:14:27 -0500

    integrates\test(front): #5906.6 update unit test
    - update unit test by including mocked succesfully remove_account
    mutation to verify history replaced

--- integrates/mobile/src/components/Avatar/index.spec.tsx
+++ integrates/mobile/src/components/Avatar/index.spec.tsx
@@ -1,13 +1,19 @@
+import type { MockedResponse } from "@apollo/client/testing";
 import { MockedProvider } from "@apollo/client/testing";
 import { mount } from "enzyme";
 import type { ReactWrapper } from "enzyme";
 import React from "react";
+import { act } from "react-dom/test-utils";
 import { I18nextProvider } from "react-i18next";
+import { Alert, Modal, TouchableOpacity } from "react-native";
 import {
   Avatar as PaperAvatar,
   Provider as PaperProvider,
 } from "react-native-paper";
 import { MemoryRouter } from "react-router-native";
+import wait from "waait";
+
+import { REMOVE_ACCOUNT_MUTATION } from "./queries";
 
 import { Avatar } from ".";
 import { i18next } from "../../utils/translations/translate";
@@ -36,8 +42,23 @@ describe("Avatar", (): void => {
     expect(typeof Avatar).toStrictEqual("function");
   });
 
-  it("should render initials", (): void => {
+  it("should render initials and successfully remove account", async (): Promise<void> => {
     expect.hasAssertions();
+
+    jest.mock("react-native/Libraries/Alert/Alert");
+
+    const removeAccountMock: Readonly<MockedResponse> = {
+      request: {
+        query: REMOVE_ACCOUNT_MUTATION,
+      },
+      result: {
+        data: {
+          removeStakeholder: {
+            success: true,
+          },
+        },
+      },
+    };
 
     const wrapper: ReactWrapper = mount(
       <PaperProvider>
@@ -47,7 +68,7 @@ describe("Avatar", (): void => {
               { pathname: "/Dashboard", state: { user: { fullName: "Test" } } },
             ]}
           >
-            <MockedProvider addTypename={false} mocks={[]}>
+            <MockedProvider addTypename={false} mocks={[removeAccountMock]}>
               <Avatar
                 photoUrl={undefined}
                 size={40}
@@ -63,6 +84,33 @@ describe("Avatar", (): void => {
 
     expect(wrapper).toHaveLength(1);
     expect(wrapper.find(PaperAvatar.Text).text()).toStrictEqual("SJ");
+    expect(wrapper.find(Modal).prop("visible")).toStrictEqual(false);
+
+    const avatarTouchable: ReactWrapper<
+      React.ComponentProps<typeof TouchableOpacity>
+    > = wrapper.find(TouchableOpacity).first();
+    (avatarTouchable.invoke("onPress") as () => void)();
+
+    expect(wrapper.find(Modal).prop("visible")).toStrictEqual(true);
+
+    const deleteBtn: ReactWrapper<
+      React.ComponentProps<typeof TouchableOpacity>
+    > = wrapper.find(TouchableOpacity).last();
+    (deleteBtn.invoke("onPress") as () => void)();
+
+    expect(Alert.alert).toHaveBeenCalledTimes(1);
+
+    await act(async (): Promise<void> => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+      await (Alert.alert as jest.Mock).mock.calls[0][2][1].onPress();
+      await wait(0);
+      wrapper.update();
+    });
+
+    expect(wrapper).toHaveLength(1);
+    expect(mockHistoryReplace).toHaveBeenCalledWith("/Login");
+
+    jest.clearAllMocks();
   });
 
   it("should render profile picture", (): void => {
