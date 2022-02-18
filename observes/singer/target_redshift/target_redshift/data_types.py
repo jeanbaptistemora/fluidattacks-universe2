a52d556195d3d63e91749cf2fd68cd1a134a9b4e
Daniel Murcia <dmurcia@fluidattacks.com>
2022-02-18 17:40:10 -0500

    observes\rever(back): #5993.17 big ints
    - this reverts commit 37dc35cd0a12e0abe37b301dc76ab40df8c4f41c.
    - warning and error on jobs due to big int changes

--- observes/singer/target_redshift/target_redshift/data_types.py
+++ observes/singer/target_redshift/target_redshift/data_types.py
@@ -28,7 +28,7 @@ class NullOrEmptyType(Exception):
 
 raw_type_map: Dict[str, RedshiftDataType] = {
     "boolean": RedshiftDataType.BOOLEAN,
-    "integer": RedshiftDataType.BIGINT,
+    "integer": RedshiftDataType.INTEGER,
     "number": RedshiftDataType.DOUBLE_PRECISION,
     "string": RedshiftDataType.VARCHAR,
 }
