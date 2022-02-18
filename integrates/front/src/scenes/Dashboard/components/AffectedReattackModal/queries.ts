77f42dc9cec25fe90d08b118251b8e7669ad7bcf
Juan Chaves <jchaves@fluidattacks.com>
2022-02-17 19:05:27 -0500

    integrates\feat(front): #6001.4 reattack hold flow
    - Added an unsolved event query and type to fetch the info
    for the event dropdown in the affected reattack modal

--- integrates/front/src/scenes/Dashboard/components/AffectedReattackModal/queries.ts
+++ integrates/front/src/scenes/Dashboard/components/AffectedReattackModal/queries.ts
@@ -1,6 +1,20 @@
 import { gql } from "@apollo/client";
 import type { DocumentNode } from "graphql";
 
+const GET_UNSOLVED_EVENTS: DocumentNode = gql`
+  query GetUnsolvedEventsQuery($groupName: String!) {
+    group(groupName: $groupName) {
+      events(filter: { eventStatus: "CREATED" }) {
+        description
+        detail
+        id
+        eventStatus
+        eventType
+      }
+    }
+  }
+`;
+
 const UPDATE_EVENT_AFFECTATIONS: DocumentNode = gql`
   mutation updateEventAffectations(
     $eventId: String!
@@ -19,4 +33,4 @@ const UPDATE_EVENT_AFFECTATIONS: DocumentNode = gql`
   }
 `;
 
-export { UPDATE_EVENT_AFFECTATIONS };
+export { GET_UNSOLVED_EVENTS, UPDATE_EVENT_AFFECTATIONS };
--- integrates/front/src/scenes/Dashboard/components/AffectedReattackModal/types.ts
+++ integrates/front/src/scenes/Dashboard/components/AffectedReattackModal/types.ts
@@ -1,10 +1,40 @@
 import type { ExecutionResult } from "graphql";
 
+interface IAffectedVuln {
+  affected: boolean;
+  findingId: string;
+  groupName: string;
+  id: string;
+  specific: string;
+  where: string;
+}
+
+interface IUnsolvedEvent {
+  description: string;
+  detail: string;
+  id: string;
+  eventStatus: string;
+  eventType: string;
+}
+
+interface IAffectedReattackModal {
+  vulns: IAffectedVuln[];
+  clearSelected: () => void;
+  handleCloseModal: () => void;
+  setRequestState: () => void;
+}
+
 interface IUpdateEventAffectations {
   updateEventAffectations: {
     success: boolean;
   };
 }
 
-export type UpdateEventAffectationsResult =
-  ExecutionResult<IUpdateEventAffectations>;
+type UpdateEventAffectationsResult = ExecutionResult<IUpdateEventAffectations>;
+
+export {
+  IAffectedVuln,
+  UpdateEventAffectationsResult,
+  IUnsolvedEvent,
+  IAffectedReattackModal,
+};
