8f5d5216e58ae856ad4991b07b24f5847db7095e
Diego Restrepo <drestrepo@fluidattacks.com>
2022-02-22 17:43:57 -0500

    makes\fix(back): #5988.11 unbound variable
    - unbound variable group
    - do no delete from bash

--- makes/foss/units/skims/batch/entrypoint.sh
+++ makes/foss/units/skims/batch/entrypoint.sh
@@ -1,20 +1,5 @@
 # shellcheck shell=bash
-
-function clean_file_system {
-  local group="${1}"
-  local paths=(
-    ~/.skims
-    "groups/${group}/fusion"
-  )
-
-  rm -rf "${paths[@]}" || true
-}
-
 function main {
-  export -f clean_file_system
-
-  local env="${1:-}"
-
   shopt -s nullglob \
     && ensure_gitlab_env_vars \
       INTEGRATES_API_TOKEN \
@@ -22,11 +7,7 @@ function main {
       PROD_SERVICES_AWS_SECRET_ACCESS_KEY \
     && use_git_repo_services \
     && aws_login_prod 'skims' \
-    && python3 -m batch.__init__ "${@:2}" \
-    && popd \
-    && if test "${env}" != 'test'; then
-      clean_file_system "${group}"
-    fi
+    && python3 -m batch.__init__ "${@:2}"
 }
 
 main "${@}"
