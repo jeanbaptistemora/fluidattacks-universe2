480497a84496063765e08a0d437f3cbfe4cf88bd
David Acevedo <dacevedo@fluidattacks.com>
2022-02-23 14:42:32 -0500

    makes\feat(infra): #5961 pixie crds
    - added custom resource definitions for newrelic's pixie

--- makes/foss/modules/makes/kubernetes/infra/new-relic.tf
+++ makes/foss/modules/makes/kubernetes/infra/new-relic.tf
@@ -1,5 +1,25 @@
 variable "newRelicLicenseKey" {}
 
+variable "pixie_crd_commit" {
+  default = "eea8fff0ec9032ef47bc862baae6470812352d44"
+}
+
+data "http" "pixie_viziers_yaml" {
+  url = "https://raw.githubusercontent.com/pixie-io/pixie/${var.pixie_crd_commit}/k8s/operator/crd/base/px.dev_viziers.yaml"
+}
+
+data "http" "pixie_olm_yaml" {
+  url = "https://raw.githubusercontent.com/pixie-io/pixie/${var.pixie_crd_commit}/k8s/operator/helm/crds/olm_crd.yaml"
+}
+
+resource "kubectl_manifest" "pixie_viziers_crd" {
+  yaml_body = data.http.pixie_viziers_yaml.body
+}
+
+resource "kubectl_manifest" "pixie_olm_crd" {
+  yaml_body = data.http.pixie_olm_yaml.body
+}
+
 resource "helm_release" "newrelic" {
   name       = "newrelic"
   repository = "https://helm-charts.newrelic.com"
