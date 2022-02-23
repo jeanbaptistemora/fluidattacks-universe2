95322d82f09ec4e9c92467b9d9e1779809d6ac60
Andres Aguirre <aaguirre@fluidattacks.com>
2022-02-23 17:41:29 +0000

    integrates\feat(back): #5796.33 group inputs by root
    - Filter the group inputs by root id in the API

--- integrates/back/src/api/resolvers/group/toe_inputs.py
+++ integrates/back/src/api/resolvers/group/toe_inputs.py
@@ -9,6 +9,7 @@ from dataloaders import (
 )
 from db_model.toe_inputs.types import (
     GroupToeInputsRequest,
+    RootToeInputsRequest,
     ToeInput,
     ToeInputsConnection,
 )
@@ -31,7 +32,20 @@ async def resolve(
     parent: Group, info: GraphQLResolveInfo, **kwargs: None
 ) -> Tuple[ToeInput, ...]:
     loaders: Dataloaders = info.context.loaders
-    response: ToeInputsConnection = await loaders.group_toe_inputs.load(
+    if kwargs.get("root_id") is not None:
+        response: ToeInputsConnection = await loaders.root_toe_inputs.load(
+            RootToeInputsRequest(
+                group_name=parent["name"],
+                root_id=kwargs["root_id"],
+                after=kwargs.get("after"),
+                be_present=kwargs.get("be_present"),
+                first=kwargs.get("first"),
+                paginate=True,
+            )
+        )
+        return response
+
+    response = await loaders.group_toe_inputs.load(
         GroupToeInputsRequest(
             group_name=parent["name"],
             after=kwargs.get("after"),
