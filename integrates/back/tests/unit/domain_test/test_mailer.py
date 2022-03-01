04b6ea8c2828ffd74e341ce3953cc7b5f50d050c
Sebastian Lizcano <slizcano@fluidattacks.com>
2022-03-01 11:30:05 -0500

    integrates\test(back): #6061.8 update unit test
    - update `test_get_recipient_first_name` mailer unit test to verify
    that non active (pending, unregistered) are not notified

--- integrates/back/tests/unit/domain_test/test_mailer.py
+++ integrates/back/tests/unit/domain_test/test_mailer.py
@@ -1,5 +1,14 @@
+from aioextensions import (
+    collect,
+)
 from context import (
     FI_MAIL_CUSTOMER_SUCCESS,
+)
+from group_access.domain import (
+    get_group_users,
+)
+from groups.domain import (
+    invite_to_group,
 )
 from mailer.common import (
     get_recipient_first_name,
@@ -26,4 +35,22 @@ async def test_get_recipient_first_name() -> None:
             "nonexistinguser@fluidattacks.com", is_access_granted=True
         )
         is not None
+    )
+
+    await invite_to_group(
+        email="nonexistinguser@fluidattacks.com",
+        responsibility="Tester",
+        role="USER",
+        group_name="oneshottest",
+        modified_by="integratesmanager@gmail.com",
+    )
+    inactive_users = await get_group_users("oneshottest", active=False)
+    active_users = await get_group_users("oneshottest", active=True)
+    users = inactive_users + active_users
+    recipients = await collect(
+        tuple(get_recipient_first_name(email) for email in users)
+    )
+
+    assert len([recipient for recipient in recipients if recipient]) < len(
+        users
     )
