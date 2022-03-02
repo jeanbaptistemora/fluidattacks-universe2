3aeca3a28734b15bd742ce1ef20688a8c6f01992
Jonathan Mesa <jmesa@fluidattacks.com>
2022-03-02 23:00:40 +0000

    integrates\refac(back): #6108.17 hybrid resolvers
    - continue modifying group resolvers to accept new group type
    - deprecate field totalTreatment as it is no longer in use and it is
    just a dict serialized
    - charts get group's total_treatment directly from domain

--- integrates/back/src/api/resolvers/group/total_treatment.py
+++ integrates/back/src/api/resolvers/group/total_treatment.py
@@ -1,5 +1,9 @@
-from custom_types import (
+from dataloaders import (
+    Dataloaders,
+)
+from db_model.groups.types import (
     Group,
+    GroupUnreliableIndicators,
 )
 from decorators import (
     require_asm,
@@ -8,11 +12,27 @@ from graphql.type.definition import (
     GraphQLResolveInfo,
 )
 import simplejson as json  # type: ignore
+from typing import (
+    Any,
+    Dict,
+    Union,
+)
 
 
 @require_asm
 async def resolve(
-    parent: Group, _info: GraphQLResolveInfo, **_kwargs: None
+    parent: Union[Group, Dict[str, Any]],
+    info: GraphQLResolveInfo,
+    **_kwargs: None,
 ) -> object:
-    total_treatment: str = parent.get("total_treatment", {})
+    if isinstance(parent, dict):
+        total_treatment: Dict[str, int] = parent.get("total_treatment", {})
+    else:
+        loaders: Dataloaders = info.context.loaders
+        group_name: str = parent.name
+        group_indicators: GroupUnreliableIndicators = (
+            await loaders.group_indicators_typed.load(group_name)
+        )
+        total_treatment = group_indicators.treatment_summary._asdict()
+
     return json.dumps(total_treatment, use_decimal=True)
