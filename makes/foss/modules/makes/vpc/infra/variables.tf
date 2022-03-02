b1fe0383c82f434626eb0eab203d65951321bafd
Daniel Salazar <dsalazar@fluidattacks.com>
2022-03-02 15:43:27 -0500

    makes\feat(infra): #5189 new saml
    - Add new saml provider as an okta app is needed
    - Use split tunnel
    - Create subnet association with batch

--- makes/foss/modules/makes/vpc/infra/variables.tf
+++ makes/foss/modules/makes/vpc/infra/variables.tf
@@ -2,14 +2,12 @@ variable "cloudflare_email" {}
 variable "cloudflare_api_key" {}
 variable "vpnDataRaw" {}
 
-data "aws_caller_identity" "current" {}
 data "cloudflare_ip_ranges" "cloudflare" {}
 data "cloudflare_zones" "fluidattacks_com" {
   filter {
     name = "fluidattacks.com"
   }
 }
-
 
 locals {
   vpnData = {
--- makes/foss/modules/makes/vpc/infra/vpn.tf
+++ makes/foss/modules/makes/vpc/infra/vpn.tf
@@ -51,15 +51,46 @@ resource "aws_cloudwatch_log_group" "main" {
   }
 }
 
+resource "aws_iam_saml_provider" "main" {
+  name                   = "vpn"
+  saml_metadata_document = <<-EOL
+    <?xml version="1.0" encoding="UTF-8"?><md:EntityDescriptor entityID="http://www.okta.com/exkgf65wndCoERhf8357" xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata"><md:IDPSSODescriptor WantAuthnRequestsSigned="false" protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol"><md:KeyDescriptor use="signing"><ds:KeyInfo xmlns:ds="http://www.w3.org/2000/09/xmldsig#"><ds:X509Data><ds:X509Certificate>MIIDqDCCApCgAwIBAgIGAW2yeIqDMA0GCSqGSIb3DQEBCwUAMIGUMQswCQYDVQQGEwJVUzETMBEG
+    A1UECAwKQ2FsaWZvcm5pYTEWMBQGA1UEBwwNU2FuIEZyYW5jaXNjbzENMAsGA1UECgwET2t0YTEU
+    MBIGA1UECwwLU1NPUHJvdmlkZXIxFTATBgNVBAMMDGZsdWlkYXR0YWNrczEcMBoGCSqGSIb3DQEJ
+    ARYNaW5mb0Bva3RhLmNvbTAeFw0xOTEwMDkyMTQwNDdaFw0yOTEwMDkyMTQxNDdaMIGUMQswCQYD
+    VQQGEwJVUzETMBEGA1UECAwKQ2FsaWZvcm5pYTEWMBQGA1UEBwwNU2FuIEZyYW5jaXNjbzENMAsG
+    A1UECgwET2t0YTEUMBIGA1UECwwLU1NPUHJvdmlkZXIxFTATBgNVBAMMDGZsdWlkYXR0YWNrczEc
+    MBoGCSqGSIb3DQEJARYNaW5mb0Bva3RhLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC
+    ggEBAJZ2XfEX8jtFui4ll7MlUr8ByRQv2R3jaBcBliIh88JFO8PSscqeOCOAm083XO6CYW7FQnMf
+    3ceDutnqIFbYemdILiUZXdD07yw+RwCN/WABs2gwpYLNR5ZEHnFrj0a6HMuzcN69l+UnLI6V9iDL
+    Jcfz/+U9CoC0KCoW8+IKwrh2RIAMUIBLLBiQaF2kehFL6f+Pd8koa8mPzu3VQyaKDaXJf5GML7Vu
+    Ew8MAxF2u8KgolH2MbbybkS4fqFJLDqzVT2RZXy9qUoII6qg0UqdX3do0yTRkLPhfNIIfABgkdre
+    VyZXReUTYuLH+sWFq7G9REq45gA5wzNw4tynPyeerAECAwEAATANBgkqhkiG9w0BAQsFAAOCAQEA
+    ULhbwMD6EOskawoayLezhPHeAtmzM8stYDaJdQWYzoirb2X+PM42zDVmCOiEQRYZxiUBnlmexPfZ
+    Fd/p3ztmrhMCy/QqY901JaGvdUgtelPpVZ5uO9pXAhbY/rXK2jfZCWPu98oIxdWf4p8roTtIzdtw
+    lVWI/jhY7rAHn95Vbjrh+OU7O7mTuyguVDXSXNkE3fSR7pp5YxFEw18Q2lJbduTtUDI/DxrXT+Yn
+    J1ufkupsSdNt7VAVjA/sBE2qmnE9c4+uaWnR/nUYrRkLLRXRF7+Gdge5HqJ1H/XGPyt0RT9UaJsV
+    5pBqdjpXGt+eZOKx4yze9tfx9rP+OUbzjZwBUw==</ds:X509Certificate></ds:X509Data></ds:KeyInfo></md:KeyDescriptor><md:NameIDFormat>urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress</md:NameIDFormat><md:SingleSignOnService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" Location="https://fluidattacks.okta.com/app/aws_clientvpn/exkgf65wndCoERhf8357/sso/saml"/><md:SingleSignOnService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect" Location="https://fluidattacks.okta.com/app/aws_clientvpn/exkgf65wndCoERhf8357/sso/saml"/></md:IDPSSODescriptor></md:EntityDescriptor>
+  EOL
+
+  tags = {
+    "Name"               = "vpn"
+    "management:area"    = "cost"
+    "management:product" = "makes"
+    "management:type"    = "product"
+  }
+}
+
 resource "aws_ec2_client_vpn_endpoint" "main" {
   vpc_id                 = aws_vpc.fluid-vpc.id
   server_certificate_arn = module.acm.acm_certificate_arn
   client_cidr_block      = "10.0.0.0/22"
   session_timeout_hours  = 12
+  split_tunnel           = true
 
   authentication_options {
     type              = "federated-authentication"
-    saml_provider_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:saml-provider/okta-saml-provider"
+    saml_provider_arn = aws_iam_saml_provider.main.arn
   }
 
   connection_log_options {
@@ -73,6 +104,11 @@ resource "aws_ec2_client_vpn_endpoint" "main" {
     "management:product" = "makes"
     "management:type"    = "product"
   }
+}
+
+resource "aws_ec2_client_vpn_network_association" "main" {
+  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.main.id
+  subnet_id              = aws_subnet.main["batch"].id
 }
 
 module "vpn" {
