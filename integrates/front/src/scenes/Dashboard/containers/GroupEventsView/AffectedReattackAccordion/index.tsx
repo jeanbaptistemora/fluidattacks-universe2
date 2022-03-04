ae8d264dce5ff4f9e4bd9f3e9c55490e319a535a
Juan Chaves <jchaves@fluidattacks.com>
2022-03-03 21:06:37 -0500

    integrates\refac(front): #6001.20 reattack hold flow
    - Refactored the populatePanel function to a const as
    suggested by dacevedoatfluid

--- integrates/front/src/scenes/Dashboard/containers/GroupEventsView/AffectedReattackAccordion/index.tsx
+++ integrates/front/src/scenes/Dashboard/containers/GroupEventsView/AffectedReattackAccordion/index.tsx
@@ -1,10 +1,9 @@
 import { Collapse } from "antd";
-import { Field, useFormikContext } from "formik";
+import { Field } from "formik";
 import React from "react";
 
 import type { IAffectedAccordionProps, IFinding, IReattackVuln } from "./types";
 
-import type { IFormValues } from "../AddModal";
 import { FormikCheckbox } from "utils/forms/fields";
 
 export const AffectedReattackAccordion: React.FC<IAffectedAccordionProps> = (
@@ -12,69 +11,37 @@ export const AffectedReattackAccordion: React.FC<IAffectedAccordionProps> = (
 ): JSX.Element => {
   const { findings } = props;
   const { Panel } = Collapse;
-  const { setFieldValue } = useFormikContext<IFormValues>();
 
-  const checkAll = (
-    findingId: string,
-    vulnerabilitiesToReattack: IReattackVuln[]
-  ): JSX.Element => {
-    function onChange(): void {
-      vulnerabilitiesToReattack.forEach(({ id }: IReattackVuln): void => {
-        setFieldValue(id, true);
-      });
-    }
-
-    return (
-      <Field
-        component={FormikCheckbox}
-        key={findingId}
-        label={""}
-        name={findingId}
-        onChange={onChange}
-        type={"checkbox"}
-      />
-    );
-  };
-
-  const populatePanel = (): JSX.Element[] => {
-    const panelOptions = findings.map(
-      ({ title, id, vulnerabilitiesToReattack }: IFinding): JSX.Element => {
-        if (vulnerabilitiesToReattack.length > 0) {
-          return (
-            <Panel
-              collapsible={"header"}
-              extra={checkAll(id, vulnerabilitiesToReattack)}
-              header={title}
-              key={id}
-            >
-              {vulnerabilitiesToReattack.map(
-                (vuln: IReattackVuln): JSX.Element => {
-                  return (
-                    <Field
-                      component={FormikCheckbox}
-                      key={vuln.id}
-                      label={`Where: ${vuln.where} | Spec: ${vuln.specific}`}
-                      name={"affectedReattacks"}
-                      type={"checkbox"}
-                      value={vuln.id}
-                    />
-                  );
-                }
-              )}
-            </Panel>
-          );
-        }
-
-        return <div key={id} />;
+  const panelOptions = findings.map(
+    ({ title, id, vulnerabilitiesToReattack }: IFinding): JSX.Element => {
+      if (vulnerabilitiesToReattack.length > 0) {
+        return (
+          <Panel header={title} key={id}>
+            {vulnerabilitiesToReattack.map(
+              (vuln: IReattackVuln): JSX.Element => {
+                return (
+                  <Field
+                    component={FormikCheckbox}
+                    key={vuln.id}
+                    label={`Where: ${vuln.where} | Spec: ${vuln.specific}`}
+                    name={"affectedReattacks"}
+                    type={"checkbox"}
+                    value={`${id} ${vuln.id}`}
+                  />
+                );
+              }
+            )}
+          </Panel>
+        );
       }
-    );
 
-    return panelOptions;
-  };
+      return <div key={id} />;
+    }
+  );
 
   return (
     <React.StrictMode>
-      <Collapse ghost={true}>{populatePanel()}</Collapse>
+      <Collapse ghost={true}>{panelOptions}</Collapse>
     </React.StrictMode>
   );
 };
