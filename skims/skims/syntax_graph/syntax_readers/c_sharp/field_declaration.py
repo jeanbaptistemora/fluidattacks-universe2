1410c6d3d7482bea8db6b540a0ca619ca729d40f
Juan Echeverri <jecheverri@fluidattacks.com>
2022-03-04 10:21:51 -0500

    skims\feat(back): #5999.11 make nodes more generic
    - Remove innecesary node

--- skims/skims/syntax_graph/syntax_readers/c_sharp/field_declaration.py
+++ skims/skims/syntax_graph/syntax_readers/c_sharp/field_declaration.py
@@ -1,9 +1,6 @@
 from model.graph_model import (
     NId,
 )
-from syntax_graph.syntax_nodes.field_declaration import (
-    build_field_declaration_node,
-)
 from syntax_graph.types import (
     SyntaxGraphArgs,
 )
@@ -15,4 +12,4 @@ from utils import (
 def reader(args: SyntaxGraphArgs) -> NId:
     match_var = g.match_ast(args.ast_graph, args.n_id, "variable_declaration")
     var = match_var["variable_declaration"]
-    return build_field_declaration_node(args, var)
+    return args.generic(args.fork_n_id(var))
