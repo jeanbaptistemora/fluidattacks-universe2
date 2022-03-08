d215fa75454c7d00d0639fe76bdd11160b8dc3cd
David Acevedo <dacevedo@fluidattacks.com>
2022-03-08 09:35:01 -0500

    integrates\refac(job): #6079 improve dx
    - m . /integrates/front now supports running other scripts via arg
    - similar to docs
    - updated some deps

--- makes/foss/units/integrates/front/entrypoint.sh
+++ makes/foss/units/integrates/front/entrypoint.sh
@@ -1,17 +1,14 @@
 # shellcheck shell=bash
 
 function main {
+  local action="${1:-start}"
   export INTEGRATES_DEPLOYMENT_DATE
 
   INTEGRATES_DEPLOYMENT_DATE="$(date -u '+%FT%H:%M:%SZ')" \
     && pushd integrates/front \
     && rm -rf node_modules \
     && copy __argSetupIntegratesFrontDevRuntime__ node_modules \
-    && for bin in webpack webpack-cli; do
-      copy "$(realpath "node_modules/.bin/${bin}")" "node_modules/.bin/${bin}2" \
-        && mv "node_modules/.bin/${bin}"{2,}
-    done \
-    && npm start \
+    && npm run "${action}" \
     && popd \
     || return 1
 }
