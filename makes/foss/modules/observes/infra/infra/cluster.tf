0817b3e004107207f726d71a6f8641853d8e6518
Daniel Murcia <dmurcia@fluidattacks.com>
2022-03-09 13:39:54 -0500

    observes\fix(infra): #6118.21 vpc access
    - allow redshift trafic into the vpc
    - copy statement needs to comunicate with s3

--- makes/foss/modules/observes/infra/infra/cluster.tf
+++ makes/foss/modules/observes/infra/infra/cluster.tf
@@ -82,6 +82,14 @@ resource "aws_security_group" "expose-redshift" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  egress {
+    description      = "VPC access"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
   tags = {
     "Name"               = "observes"
     "management:area"    = "cost"
