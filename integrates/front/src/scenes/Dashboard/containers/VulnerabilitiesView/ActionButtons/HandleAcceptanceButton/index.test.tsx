cc15c248b3a81231cd6907f8b6f3f3fafa8c864c
Sebastian Lizcano <slizcano@fluidattacks.com>
2022-03-10 13:58:11 -0500

    integrates\test(front): #6081.8 update unit test
    - migrate unit test to `rtl`
    - no-deltas-test

---
+++ integrates/front/src/scenes/Dashboard/containers/VulnerabilitiesView/ActionButtons/HandleAcceptanceButton/index.test.tsx
@@ -0,0 +1,72 @@
+import { PureAbility } from "@casl/ability";
+import { fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+import { HandleAcceptanceButton } from ".";
+import { authzPermissionsContext } from "utils/authz/config";
+
+describe("HandleAcceptanceButtons", (): void => {
+  it("should return a function", (): void => {
+    expect.hasAssertions();
+    expect(typeof HandleAcceptanceButton).toStrictEqual("function");
+  });
+
+  it("should render a component", (): void => {
+    expect.hasAssertions();
+
+    const { t } = useTranslation();
+    const openHandleAcceptance: jest.Mock = jest.fn();
+    const handleAcceptanceButtonText: string = t(
+      "searchFindings.tabVuln.buttons.handleAcceptance"
+    );
+    const mockedPermissions: PureAbility<string> = new PureAbility([
+      { action: "api_mutations_handle_vulnerabilities_acceptance_mutate" },
+    ]);
+    const { rerender } = render(
+      <authzPermissionsContext.Provider value={mockedPermissions}>
+        <HandleAcceptanceButton
+          areVulnerabilitiesPendingToAcceptance={true}
+          isEditing={true}
+          isRequestingReattack={false}
+          isVerifying={false}
+          openHandleAcceptance={openHandleAcceptance}
+        />
+      </authzPermissionsContext.Provider>
+    );
+
+    expect(screen.queryAllByText(handleAcceptanceButtonText)).toHaveLength(0);
+
+    rerender(
+      <authzPermissionsContext.Provider value={mockedPermissions}>
+        <HandleAcceptanceButton
+          areVulnerabilitiesPendingToAcceptance={false}
+          isEditing={false}
+          isRequestingReattack={false}
+          isVerifying={false}
+          openHandleAcceptance={openHandleAcceptance}
+        />
+      </authzPermissionsContext.Provider>
+    );
+
+    expect(screen.queryAllByText(handleAcceptanceButtonText)).toHaveLength(0);
+
+    rerender(
+      <authzPermissionsContext.Provider value={mockedPermissions}>
+        <HandleAcceptanceButton
+          areVulnerabilitiesPendingToAcceptance={true}
+          isEditing={false}
+          isRequestingReattack={false}
+          isVerifying={false}
+          openHandleAcceptance={openHandleAcceptance}
+        />
+      </authzPermissionsContext.Provider>
+    );
+
+    expect(screen.getByText(handleAcceptanceButtonText)).toBeInTheDocument();
+
+    fireEvent.click(screen.getByText(handleAcceptanceButtonText));
+
+    expect(openHandleAcceptance).toHaveBeenCalledTimes(1);
+  });
+});
