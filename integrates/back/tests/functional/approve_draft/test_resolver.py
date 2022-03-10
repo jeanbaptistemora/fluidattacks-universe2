e04b39ccff3338ce905b21e7d6d6bea25dc84085
Jonathan Mesa <jmesa@fluidattacks.com>
2022-03-10 16:39:05 -0500

    integrates\test(back): #6154.5 approve draft
    - complete functional testing for related indicators

--- integrates/back/tests/functional/approve_draft/test_resolver.py
+++ integrates/back/tests/functional/approve_draft/test_resolver.py
@@ -1,6 +1,20 @@
 from . import (
     get_result,
 )
+from dataloaders import (
+    Dataloaders,
+    get_new_context,
+)
+from db_model.findings.enums import (
+    FindingStateStatus,
+)
+from db_model.findings.types import (
+    Finding,
+    FindingUnreliableIndicators,
+)
+from db_model.vulnerabilities.types import (
+    Vulnerability,
+)
 import pytest
 from typing import (
     Any,
@@ -11,14 +25,22 @@ from typing import (
 @pytest.mark.asyncio
 @pytest.mark.resolver_test_group("approve_draft")
 @pytest.mark.parametrize(
-    ["email", "finding_id"],
+    ["email", "finding_id", "vuln_id"],
     [
-        ["admin@gmail.com", "3c475384-834c-47b0-ac71-a41a022e401c"],
-        ["reviewer@gmail.com", "475041520"],
+        [
+            "admin@gmail.com",
+            "3c475384-834c-47b0-ac71-a41a022e401c",
+            "be09edb7-cd5c-47ed-bee4-97c645acdce8",
+        ],
+        [
+            "reviewer@gmail.com",
+            "475041520",
+            "be09edb7-cd5c-47ed-bee4-97c645acdce8",
+        ],
     ],
 )
 async def test_approve_draft(
-    populate: bool, email: str, finding_id: str
+    populate: bool, email: str, finding_id: str, vuln_id: str
 ) -> None:
     assert populate
     result: Dict[str, Any] = await get_result(
@@ -27,6 +49,29 @@ async def test_approve_draft(
     assert "errors" not in result
     assert "success" in result["data"]["approveDraft"]
     assert result["data"]["approveDraft"]["success"]
+
+    loaders: Dataloaders = get_new_context()
+    finding: Finding = await loaders.finding.load(finding_id)
+    assert finding.state.status == FindingStateStatus.APPROVED
+
+    vuln: Vulnerability = await loaders.vulnerability.load(vuln_id)
+    approval_date: str = finding.approval.modified_date
+    assert vuln.unreliable_indicators.unreliable_report_date == approval_date
+    finding_indicators: FindingUnreliableIndicators = (
+        finding.unreliable_indicators
+    )
+    assert (
+        finding_indicators.unreliable_newest_vulnerability_report_date
+        == approval_date
+    )
+    assert (
+        finding_indicators.unreliable_oldest_open_vulnerability_report_date
+        == ""
+    )
+    assert (
+        finding_indicators.unreliable_oldest_vulnerability_report_date
+        == approval_date
+    )
 
 
 @pytest.mark.asyncio
