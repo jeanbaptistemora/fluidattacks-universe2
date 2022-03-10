758ad5673303fb01c026494fe56ed90faf30ae5e
Daniel Salazar <dsalazar@fluidattacks.com>
2022-03-10 16:57:46 -0500

    skims\feat(infra): #6153 update tf providers
    - Update providers
    - Update source code

--- skims/infra/s3_data.tf
+++ skims/infra/s3_data.tf
@@ -1,38 +1,60 @@
 resource "aws_s3_bucket" "skims_data" {
-  acl    = "private"
   bucket = "skims.data"
+
   tags = {
     "Name"               = "skims.data"
     "management:area"    = "cost"
     "management:product" = "skims"
     "management:type"    = "product"
   }
+}
 
-  lifecycle_rule {
-    abort_incomplete_multipart_upload_days = 7
-    enabled                                = true
-    id                                     = "skims_data_cache"
-    prefix                                 = "cache/"
+resource "aws_s3_bucket_acl" "skims_data" {
+  bucket = aws_s3_bucket.skims_data.id
+
+  acl = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "skims_data" {
+  bucket = aws_s3_bucket.skims_data.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "skims_data" {
+  bucket = aws_s3_bucket.skims_data.id
+
+  rule {
+    id     = "skims_data_cache"
+    status = "Enabled"
+
+    filter {
+      prefix = "cache/"
+    }
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = 7
+    }
 
     expiration {
       days = 7
     }
-
-    noncurrent_version_expiration {
-      days = 7
-    }
   }
+}
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
+resource "aws_s3_bucket_versioning" "skims_data" {
+  bucket = aws_s3_bucket.skims_data.id
 
-  versioning {
-    enabled    = true
-    mfa_delete = false
+  versioning_configuration {
+    status     = "Enabled"
+    mfa_delete = "Disabled"
   }
 }
