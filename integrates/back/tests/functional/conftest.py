babfb56fe69ff222abd903792baa6b6d2eefb349
Andres Aguirre <aaguirre@fluidattacks.com>
2022-03-10 21:43:40 +0000

    integrates\test(back): #6063.42 update stakeholder mobile
    - Add functional test to update the stakeholder mobile

--- integrates/back/tests/functional/conftest.py
+++ integrates/back/tests/functional/conftest.py
@@ -96,6 +96,7 @@ TEST_GROUPS: Set[str] = {
     "update_organization_policies",
     "update_organization_stakeholder",
     "update_severity",
+    "update_stakeholder_phone",
     "update_toe_input",
     "update_toe_lines_attacked_lines",
     "update_toe_lines_sorts",
--- makes/foss/modules/integrates/pipeline/makes.nix
+++ makes/foss/modules/integrates/pipeline/makes.nix
@@ -350,6 +350,7 @@ in {
             ["update_organization_policies"]
             ["update_organization_stakeholder"]
             ["update_severity"]
+            ["update_stakeholder_phone"]
             ["update_toe_input"]
             ["update_toe_lines_attacked_lines"]
             ["update_toe_lines_sorts"]
