395a136f768941deb7f7fbad728e8b273fa92af8
David Acevedo <dacevedo@fluidattacks.com>
2022-03-10 10:39:52 -0500

    integrates\feat(front): #6079 window size hooks
    - added new utility hook

--- integrates/front/src/utils/hooks/index.ts
+++ integrates/front/src/utils/hooks/index.ts
@@ -1,4 +1,5 @@
 import { useStoredState } from "./useStoredState";
 import { useTabTracking } from "./useTabTracking";
+import { useWindowSize } from "./useWindowSize";
 
-export { useStoredState, useTabTracking };
+export { useStoredState, useTabTracking, useWindowSize };
