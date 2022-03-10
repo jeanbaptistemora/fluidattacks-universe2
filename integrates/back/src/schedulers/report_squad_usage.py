2a11cc14a37ac92b1747ced1b324b71b3ce5fd71
Jonathan Mesa <jmesa@fluidattacks.com>
2022-03-09 20:07:39 -0500

    integrates\refac(back): #6108.47 group typed dataloader
    - modify code to use typed group

--- integrates/back/src/schedulers/report_squad_usage.py
+++ integrates/back/src/schedulers/report_squad_usage.py
@@ -5,11 +5,17 @@ from billing import (
     domain as billing_domain,
 )
 from custom_types import (
-    Group,
     Organization,
 )
 from dataloaders import (
+    Dataloaders,
     get_new_context,
+)
+from db_model.groups.enums import (
+    GroupTier,
+)
+from db_model.groups.types import (
+    Group,
 )
 from groups import (
     domain as groups_domain,
@@ -17,31 +23,43 @@ from groups import (
 from newutils import (
     bugsnag as bugsnag_utils,
 )
+from organizations import (
+    domain as orgs_domain,
+)
 from typing import (
     List,
+    Tuple,
 )
 
 bugsnag_utils.start_scheduler_session()
 
 
 async def main() -> None:
-    loader = get_new_context()
-    active_groups: List[str] = await groups_domain.get_active_groups()
-    groups: List[Group] = [
-        group
-        for group in await loader.group.load_many(active_groups)
-        if group["tier"] == "squad"
-    ]
-    orgs: List[Organization] = await loader.organization.load_many(
-        [group["organization"] for group in groups]
+    loaders: Dataloaders = get_new_context()
+    active_groups_names: Tuple[str, ...] = tuple(
+        await groups_domain.get_active_groups()
     )
-
+    active_groups: Tuple[Group, ...] = await loaders.group_typed.load_many(
+        active_groups_names
+    )
+    squad_groups: Tuple[Group, ...] = tuple(
+        group for group in active_groups if group.state.tier == GroupTier.SQUAD
+    )
+    squad_orgs_ids: List[str] = list(
+        await collect(
+            orgs_domain.get_id_by_name(group.organization_name)
+            for group in squad_groups
+        )
+    )
+    squad_orgs: List[Organization] = await loaders.organization.load_many(
+        squad_orgs_ids
+    )
     await collect(
         [
             billing_domain.report_subscription_usage(
-                group_name=group["name"],
+                group_name=group.name,
                 org_billing_customer=org["billing_customer"],
             )
-            for group, org in zip(groups, orgs)
+            for group, org in zip(squad_groups, squad_orgs)
         ]
     )
