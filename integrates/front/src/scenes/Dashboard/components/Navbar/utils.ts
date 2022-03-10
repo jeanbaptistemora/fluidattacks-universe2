e1d3e99fcd3e0a466ca3c21c481ef83971d08910
David Acevedo <dacevedo@fluidattacks.com>
2022-03-10 18:29:01 -0500

    integrates\fix(front): #6079 click outside
    - handle escape key pressed

--- integrates/front/src/scenes/Dashboard/components/Navbar/utils.ts
+++ integrates/front/src/scenes/Dashboard/components/Navbar/utils.ts
@@ -2,7 +2,10 @@ const clickedPortal = (event: Event): boolean => {
   const path = event.composedPath();
   const appRootIndex = 5;
 
-  return (path[path.length - appRootIndex] as HTMLElement).id !== "root";
+  return (
+    path.length < appRootIndex ||
+    (path[path.length - appRootIndex] as HTMLElement).id !== "root"
+  );
 };
 
 export { clickedPortal };
