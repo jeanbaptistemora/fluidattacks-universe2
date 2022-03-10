2a83eb5d61bbdbde82302124973338144500be66
David Acevedo <dacevedo@fluidattacks.com>
2022-03-10 11:44:57 -0500

    integrates\test(front): #6079 window size hook
    - added testcases for the recently added hook

--- integrates/front/src/utils/hooks/index.spec.tsx
+++ integrates/front/src/utils/hooks/index.spec.tsx
@@ -1,9 +1,11 @@
 import type { ReactWrapper, ShallowWrapper } from "enzyme";
 import { mount, shallow } from "enzyme";
 import mixpanel from "mixpanel-browser";
-import React, { createElement, useCallback } from "react";
+import React, { useCallback } from "react";
 import { act } from "react-dom/test-utils";
 import { MemoryRouter, useHistory } from "react-router-dom";
+
+import { useWindowSize } from "./useWindowSize";
 
 import { useStoredState, useTabTracking } from "utils/hooks";
 
@@ -13,10 +15,10 @@ describe("Custom utility hooks", (): void => {
       const [message, setMessage] = useStoredState("message", "fallback");
       const [sort, setSort] = useStoredState("sortOrder", { order: "asc" });
 
-      function handleClick(): void {
+      const handleClick = useCallback((): void => {
         setMessage("Hello world");
         setSort({ order: "none" });
-      }
+      }, [setMessage, setSort]);
 
       return (
         <React.Fragment>
@@ -35,7 +37,7 @@ describe("Custom utility hooks", (): void => {
     it("should render with fallback value", (): void => {
       expect.hasAssertions();
 
-      const wrapper: ShallowWrapper = shallow(createElement(TestComponent));
+      const wrapper: ShallowWrapper = shallow(<TestComponent />);
 
       expect(wrapper.find("p").at(0).text()).toStrictEqual("fallback");
     });
@@ -45,7 +47,7 @@ describe("Custom utility hooks", (): void => {
 
       sessionStorage.setItem("message", JSON.stringify("stored"));
       sessionStorage.setItem("sortOrder", JSON.stringify({ order: "dsc" }));
-      const wrapper: ShallowWrapper = shallow(createElement(TestComponent));
+      const wrapper: ShallowWrapper = shallow(<TestComponent />);
 
       expect(wrapper.find("p").at(0).text()).toStrictEqual("stored");
       expect(wrapper.find("p").at(1).text()).toStrictEqual("dsc");
@@ -110,6 +112,45 @@ describe("Custom utility hooks", (): void => {
       expect(trackMock).toHaveBeenCalledTimes(2);
 
       trackMock.mockReset();
+    });
+  });
+
+  describe("useWindowSize", (): void => {
+    // eslint-disable-next-line react/no-multi-comp
+    const TestComponent: React.FC = (): JSX.Element => {
+      const { height, width } = useWindowSize();
+
+      return (
+        <React.Fragment>
+          <p>{height}</p>
+          <p>{width}</p>
+        </React.Fragment>
+      );
+    };
+
+    it("should return a function", (): void => {
+      expect.hasAssertions();
+      expect(typeof useWindowSize).toStrictEqual("function");
+    });
+
+    it("should trigger on size change", (): void => {
+      expect.hasAssertions();
+
+      const wrapper: ReactWrapper = mount(<TestComponent />);
+
+      expect(wrapper.find("p").at(0).text()).toStrictEqual("768");
+      expect(wrapper.find("p").at(1).text()).toStrictEqual("1024");
+
+      // eslint-disable-next-line fp/no-mutating-methods
+      Object.defineProperty(window, "innerHeight", { value: 900 });
+      // eslint-disable-next-line fp/no-mutating-methods
+      Object.defineProperty(window, "innerWidth", { value: 1600 });
+      act((): void => {
+        window.dispatchEvent(new Event("resize"));
+      });
+
+      expect(wrapper.find("p").at(0).text()).toStrictEqual("900");
+      expect(wrapper.find("p").at(1).text()).toStrictEqual("1600");
     });
   });
 });
