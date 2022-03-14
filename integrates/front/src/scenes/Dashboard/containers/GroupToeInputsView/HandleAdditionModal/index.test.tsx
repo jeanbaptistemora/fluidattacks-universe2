e819dc7f8dd3373a7d726187b98e5cd290578b95
Sebastian Lizcano <slizcano@fluidattacks.com>
2022-03-14 09:09:15 -0500

    integrates\test(front): #6081.22 update unit test
    - migrate unit test to `rtl`
    - no-deltas-test

--- integrates/front/src/scenes/Dashboard/containers/GroupToeInputsView/HandleAdditionModal/index.test.tsx
+++ integrates/front/src/scenes/Dashboard/containers/GroupToeInputsView/HandleAdditionModal/index.test.tsx
@@ -1,12 +1,10 @@
 import type { MockedResponse } from "@apollo/client/testing";
 import { MockedProvider } from "@apollo/client/testing";
-import type { ReactWrapper } from "enzyme";
-import { mount } from "enzyme";
-import { Field } from "formik";
+import { PureAbility } from "@casl/ability";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import React from "react";
-import { act } from "react-dom/test-utils";
-import wait from "waait";
-import waitForExpect from "wait-for-expect";
+import { useTranslation } from "react-i18next";
 
 import { ADD_TOE_INPUT, GET_ROOTS } from "./queries";
 
@@ -18,7 +16,6 @@ jest.mock("../../../../../utils/notifications", (): Dictionary => {
   const mockedNotifications: Dictionary<() => Dictionary> = jest.requireActual(
     "../../../../../utils/notifications"
   );
-  jest.spyOn(mockedNotifications, "msgError").mockImplementation();
   jest.spyOn(mockedNotifications, "msgSuccess").mockImplementation();
 
   return mockedNotifications;
@@ -30,6 +27,7 @@ describe("handle toe inputs addition modal", (): void => {
 
     jest.clearAllMocks();
 
+    const { t } = useTranslation();
     const handleRefetchData: jest.Mock = jest.fn();
     const handleCloseModal: jest.Mock = jest.fn();
     const mocksMutation: MockedResponse[] = [
@@ -70,60 +68,43 @@ describe("handle toe inputs addition modal", (): void => {
       },
     };
 
-    const wrapper: ReactWrapper = mount(
-      <MockedProvider addTypename={false} mocks={[...mocksMutation, queryMock]}>
-        <HandleAdditionModal
-          groupName={"groupname"}
-          handleCloseModal={handleCloseModal}
-          refetchData={handleRefetchData}
-        />
-      </MockedProvider>,
-      {
-        wrappingComponent: authzPermissionsContext.Provider,
-      }
+    render(
+      <authzPermissionsContext.Provider value={new PureAbility([])}>
+        <MockedProvider
+          addTypename={false}
+          mocks={[...mocksMutation, queryMock]}
+        >
+          <HandleAdditionModal
+            groupName={"groupname"}
+            handleCloseModal={handleCloseModal}
+            refetchData={handleRefetchData}
+          />
+        </MockedProvider>
+      </authzPermissionsContext.Provider>
     );
 
-    await act(async (): Promise<void> => {
-      await wait(0);
+    await screen.findByPlaceholderText(
+      t("group.toe.inputs.addModal.fields.path").toString()
+    );
+    userEvent.type(
+      screen.getByPlaceholderText(
+        t("group.toe.inputs.addModal.fields.path").toString()
+      ),
+      "test/path"
+    );
+    userEvent.type(screen.getAllByRole("textbox")[2], "-");
+    userEvent.click(
+      screen.getByText(t("group.toe.inputs.addModal.procced").toString())
+    );
 
-      wrapper.update();
+    await waitFor((): void => {
+      expect(handleCloseModal).toHaveBeenCalledTimes(1);
     });
 
-    const componentFieldInput: ReactWrapper = wrapper
-      .find(Field)
-      .filter({ name: "path" })
-      .find("input");
-    componentFieldInput.simulate("change", {
-      target: {
-        name: "path",
-        value: "test/path",
-      },
-    });
-    const entryPointFieldInput: ReactWrapper = wrapper
-      .find(Field)
-      .filter({ name: "entryPoint" })
-      .find("input");
-    entryPointFieldInput.simulate("change", {
-      target: {
-        name: "entryPoint",
-        value: "-",
-      },
-    });
-
-    const form: ReactWrapper = wrapper.find("Formik");
-    form.at(0).simulate("submit");
-
-    await act(async (): Promise<void> => {
-      await waitForExpect((): void => {
-        wrapper.update();
-
-        expect(handleCloseModal).toHaveBeenCalledTimes(1);
-        expect(handleRefetchData).toHaveBeenCalledTimes(1);
-        expect(msgSuccess).toHaveBeenCalledWith(
-          "group.toe.inputs.addModal.alerts.success",
-          "groupAlerts.titleSuccess"
-        );
-      });
-    });
+    expect(handleRefetchData).toHaveBeenCalledTimes(1);
+    expect(msgSuccess).toHaveBeenCalledWith(
+      "group.toe.inputs.addModal.alerts.success",
+      "groupAlerts.titleSuccess"
+    );
   });
 });
