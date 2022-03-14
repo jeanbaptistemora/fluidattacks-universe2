8929043122d36e462abdfe12514689c01346b43d
Alejandro Trujillo <atrujillo@fluidattacks.com>
2022-03-14 14:40:31 -0500

    skims\fix(back): #6054.7 use required types
    - validate optional arguments

--- skims/skims/syntax_graph/syntax_readers/c_sharp/this_expression.py
+++ skims/skims/syntax_graph/syntax_readers/c_sharp/this_expression.py
@@ -7,6 +7,9 @@ from syntax_graph.syntax_nodes.this_expression import (
 from syntax_graph.types import (
     SyntaxGraphArgs,
 )
+from typing import (
+    cast,
+)
 from utils import (
     graph as g,
 )
@@ -15,5 +18,5 @@ from utils import (
 def reader(args: SyntaxGraphArgs) -> NId:
     pred = g.pred_ast(args.ast_graph, args.n_id)
     match = g.match_ast(args.ast_graph, pred[0], "identifier")
-    expression = match["identifier"]
+    expression = cast(str, match["identifier"])
     return build_this_expression_node(args, expression)
