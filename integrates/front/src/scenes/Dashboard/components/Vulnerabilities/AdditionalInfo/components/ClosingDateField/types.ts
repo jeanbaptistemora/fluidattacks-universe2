c359f321a0b5e12937e96d74fd60c8ed38db6790
David Acevedo <dacevedo@fluidattacks.com>
2022-03-16 07:09:33 -0500

    integrates\refac(front): #6079 layout cleanup
    - remove obsolete unused code
    - no-deltas-test

--- integrates/front/src/scenes/Dashboard/components/Vulnerabilities/AdditionalInfo/components/ClosingDateField/types.ts
+++
@@ -1,9 +0,0 @@
-import type { IVulnRowAttr } from "../../../types";
-import type { IVulnInfoAttr } from "../../types";
-
-interface IClosingDateFieldProps {
-  vulnerability: IVulnRowAttr;
-  vulnerabilityAdditionalInfo: IVulnInfoAttr;
-}
-
-export { IClosingDateFieldProps };
--- integrates/front/src/scenes/Dashboard/components/Vulnerabilities/AdditionalInfo/styles.ts
+++ integrates/front/src/scenes/Dashboard/components/Vulnerabilities/AdditionalInfo/styles.ts
@@ -1,56 +1,4 @@
-import type { StyledComponent } from "styled-components";
 import styled from "styled-components";
-
-const OuterRow: StyledComponent<
-  "div",
-  Record<string, unknown>
-> = styled.div.attrs<{
-  className: string;
-}>({
-  className: "flex flex-wrap pb1",
-})``;
-
-const Row: StyledComponent<"div", Record<string, unknown>> = styled.div.attrs<{
-  className: string;
-}>({
-  className: "flex flex-wrap-m pb1",
-})``;
-
-const LabelField: StyledComponent<
-  "div",
-  Record<string, unknown>
-> = styled.div.attrs<{
-  className: string;
-}>({
-  className: "pl1 pr0 w-30-l w-100-m w-100-ns w-auto",
-})``;
-
-const InfoField: StyledComponent<
-  "div",
-  Record<string, unknown>
-> = styled.div.attrs<{
-  className: string;
-}>({
-  className: "pl1 pr0 w-70-l w-100-m w-100-ns w-auto",
-})``;
-
-const Col100: StyledComponent<
-  "div",
-  Record<string, unknown>
-> = styled.div.attrs<{
-  className: string;
-}>({
-  className: "pl1 pr0 pb1 w-100",
-})``;
-
-const Col50: StyledComponent<
-  "div",
-  Record<string, unknown>
-> = styled.div.attrs<{
-  className: string;
-}>({
-  className: "w-50-l w-50-m w-100-ns",
-})``;
 
 const Field = styled.p.attrs({ className: "ma0 pv1" })``;
 
@@ -60,21 +8,4 @@ const Label = styled.span`
   }
 `;
 
-const Status: StyledComponent<
-  "span",
-  Record<string, unknown>
-> = styled.span.attrs({
-  className: "f2-5",
-})``;
-
-export {
-  OuterRow,
-  Row,
-  LabelField,
-  InfoField,
-  Col100,
-  Col50,
-  Field,
-  Label,
-  Status,
-};
+export { Field, Label };
