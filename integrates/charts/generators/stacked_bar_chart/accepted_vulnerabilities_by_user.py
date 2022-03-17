feb02a9d898c389d0e1b4950ab982df9ed862ea1
Sebastian Lizcano <slizcano@fluidattacks.com>
2022-03-17 13:47:21 -0500

    integrates\feat(back): #6178.1 add generator
    - add `accepted_vulnerabilities_by_user` stacked_bar_chart generator
    to have both `permanently` & `temporarily` stacked bars instead of
    a single one joined
    - no-deltas-test

---
+++ integrates/charts/generators/stacked_bar_chart/accepted_vulnerabilities_by_user.py
@@ -0,0 +1,210 @@
+from aioextensions import (
+    collect,
+    run,
+)
+from async_lru import (
+    alru_cache,
+)
+from charts.colors import (
+    TREATMENT,
+)
+from charts.generators.stacked_bar_chart.utils import (
+    MIN_PERCENTAGE,
+)
+from charts.utils import (
+    get_portfolios_groups,
+    json_dump,
+    iterate_groups,
+    iterate_organizations_and_groups,
+)
+from dataloaders import (
+    get_new_context,
+)
+from decimal import (
+    Decimal,
+)
+from db_model.findings.types import (
+    Finding,
+)
+from db_model.vulnerabilities.enums import (
+    VulnerabilityAcceptanceStatus as AcceptanceStatus,
+    VulnerabilityStateStatus as StateStatus,
+    VulnerabilityTreatmentStatus as TreatmentStatus,
+)
+from db_model.vulnerabilities.types import (
+    Vulnerability,
+)
+from typing import (
+    Any,
+    Counter,
+    Dict,
+    List,
+    Tuple,
+)
+
+
+@alru_cache(maxsize=None, typed=True)
+async def get_data_one_group(group: str) -> Counter[str]:
+    context = get_new_context()
+    group_findings: Tuple[Finding, ...] = await context.group_findings.load(
+        group.lower()
+    )
+
+    vulnerabilities: Tuple[
+        Vulnerability, ...
+    ] = await context.finding_vulnerabilities_nzr.load_many_chained(
+        tuple(finding.id for finding in group_findings)
+    )
+
+    temporarily = Counter(
+        [
+            f"{vuln.treatment.modified_by}/{TreatmentStatus.ACCEPTED}"
+            for vuln in vulnerabilities
+            if vuln.treatment.status == TreatmentStatus.ACCEPTED
+            and vuln.state.status == StateStatus.OPEN
+        ]
+    )
+    permanently = Counter(
+        [
+            (
+                f"{vuln.treatment.modified_by}"
+                "/"
+                f"{TreatmentStatus.ACCEPTED_UNDEFINED}"
+            )
+            for vuln in vulnerabilities
+            if vuln.treatment.status == TreatmentStatus.ACCEPTED_UNDEFINED
+            and vuln.treatment.acceptance_status == AcceptanceStatus.APPROVED
+            and vuln.state.status == StateStatus.OPEN
+        ]
+    )
+
+    return temporarily + permanently
+
+
+def format_vulnerabilities_by_data(
+    *, counters: Counter[str]
+) -> Dict[str, Any]:
+    translations: Dict[str, str] = {
+        "ACCEPTED_UNDEFINED": "Permanently accepted",
+        "ACCEPTED": "Temporarily accepted",
+    }
+    data: List[Tuple[str, int]] = Counter(
+        [
+            key.split("/")[0] for key in counters
+        ]
+    ).most_common(12)
+    accepted: List[int] = [
+        counters[f"{user}/ACCEPTED"] for user, _ in data
+    ]
+    accepted_undefined: List[int] = [
+        counters[f"{user}/ACCEPTED_UNDEFINED"] for user, _ in data
+    ]
+    max_acc_value: int = max(accepted) if accepted else 1
+    max_acc_undefined_value: int = (
+        max(accepted_undefined) if accepted_undefined else 1
+    )
+
+    max_accepted: List[str] = [
+        str(value)
+        if Decimal(value / max_acc_value) * Decimal("100.0") >= MIN_PERCENTAGE
+        else ""
+        for value in accepted
+    ]
+    max_accepted_undefined: List[str] = [
+        str(value)
+        if (
+            Decimal(value / max_acc_undefined_value) * Decimal("100.0")
+        ) >= MIN_PERCENTAGE
+        else ""
+        for value in accepted_undefined
+    ]
+
+    return dict(
+        data=dict(
+            colors={
+                "Permanently accepted": TREATMENT.more_passive,
+                "Temporarily accepted": TREATMENT.passive,
+            },
+            columns=[
+                [value, *[counters[f"{user}/{key}"] for user, _ in data]]
+                for key, value in translations.items()
+            ],
+            groups=[
+                list(translations.values())
+            ],
+            labels=dict(
+                format={
+                    "Permanently accepted": None
+                },
+            ),
+            order=None,
+            stack=dict(
+                normalize=False,
+            ),
+            type="bar",
+        ),
+        legend=dict(
+            position="bottom",
+        ),
+        axis=dict(
+            x=dict(
+                categories=[key for key, _ in data],
+                type="category",
+                tick=dict(
+                    rotate=12,
+                    multiline=False,
+                ),
+            ),
+            y=dict(
+                min=0,
+                padding=dict(
+                    bottom=0,
+                ),
+            ),
+        ),
+        maxValues={
+            "Permanently accepted": max_accepted_undefined,
+            "Temporarily accepted": max_accepted,
+        },
+        stackedBarChartYTickFormat=True,
+    )
+
+
+async def get_data_many_groups(groups: Tuple[str, ...]) -> Counter[str]:
+    groups_data = await collect(map(get_data_one_group, groups), workers=32)
+
+    return sum(groups_data, Counter())
+
+
+async def generate_all() -> None:
+    async for group in iterate_groups():
+        json_dump(
+            document=format_vulnerabilities_by_data(
+                counters=await get_data_one_group(group),
+            ),
+            entity="group",
+            subject=group,
+        )
+
+    async for org_id, _, org_groups in iterate_organizations_and_groups():
+        json_dump(
+            document=format_vulnerabilities_by_data(
+                counters=await get_data_many_groups(org_groups),
+            ),
+            entity="organization",
+            subject=org_id,
+        )
+
+    async for org_id, org_name, _ in iterate_organizations_and_groups():
+        for portfolio, groups in await get_portfolios_groups(org_name):
+            json_dump(
+                document=format_vulnerabilities_by_data(
+                    counters=await get_data_many_groups(tuple(groups)),
+                ),
+                entity="portfolio",
+                subject=f"{org_id}PORTFOLIO#{portfolio}",
+            )
+
+
+if __name__ == "__main__":
+    run(generate_all())
