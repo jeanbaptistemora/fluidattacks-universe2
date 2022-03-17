bc49bfc276845a6c3af5094d9f28ad738eeb71d6
Juan Chaves <jchaves@fluidattacks.com>
2022-03-17 20:50:33 +0000

    integrates\feat(back): #6084.4 add hacking certs
    - Added a function to format the remediation table of the
    cert

--- integrates/back/src/reports/certificate.py
+++ integrates/back/src/reports/certificate.py
@@ -1,4 +1,3 @@
-# FP: local testing
 from back.src.settings.logger import (
     LOGGING,
 )
@@ -24,6 +23,7 @@ from typing import (
     Tuple,
     TypedDict,
     Union,
+    ValuesView,
 )
 
 logging.config.dictConfig(LOGGING)  # NOSONAR
@@ -32,12 +32,6 @@ matplotlib.use("Agg")
 
 # Constants
 LOGGER = logging.getLogger(__name__)
-RemediationTable = TypedDict(
-    "RemediationTable",
-    {
-        "table": List[List[Union[float, int, str]]],
-    },
-)
 Context = TypedDict(
     "Context",
     {
@@ -51,11 +45,13 @@ Context = TypedDict(
         "customer": str,
         "version": str,
         "revdate": str,
-        "simpledate": str,
-        "remediation_table": RemediationTable,
+        "remediation_table": ValuesView[List[Union[float, int, str]]],
         "findings": Tuple[CertFindingInfo, ...],
         "accessVector": Optional[str],
         "cert_title": str,
+        "cert_body_part_1": str,
+        "cert_body_part_2": str,
+        "cert_signature": str,
         "crit_h": str,
         "crit_m": str,
         "crit_l": str,
@@ -92,6 +88,63 @@ async def format_finding(
         state=state,
         title=finding.title,
     )
+
+
+def _set_percentage(total_vulns: int, closed_vulns: int) -> str:
+    if total_vulns == 0:
+        return f"{closed_vulns * 100.0 / total_vulns:.1f}"
+    return "N/A"
+
+
+def make_remediation_table(
+    context_findings: Tuple[CertFindingInfo, ...], words: Dict[str, str]
+) -> ValuesView[List[Union[float, int, str]]]:
+    critical, high, medium, low = (
+        words["vuln_c"],
+        words["vuln_h"],
+        words["vuln_m"],
+        words["vuln_l"],
+    )
+    remediation_dict: Dict[str, List[Union[float, int, str]]] = {
+        #   Severity | Quantity | Total vulns | Closed vulns | Remediation %
+        critical: [critical, 0, 0, 0, "N/A"],
+        high: [high, 0, 0, 0, "N/A"],
+        medium: [medium, 0, 0, 0, "N/A"],
+        low: [low, 0, 0, 0, "N/A"],
+    }
+
+    for finding in context_findings:
+        label: str = ""
+        if 9.0 <= finding.severity_score <= 10.0:
+            label = critical
+        elif 7.0 <= finding.severity_score < 9.0:
+            label = high
+        elif 4.0 <= finding.severity_score < 7.0:
+            label = medium
+        else:
+            label = low
+        remediation_dict[label][1] = int(remediation_dict[label][1]) + 1
+        remediation_dict[label][2] = int(remediation_dict[label][2]) + (
+            finding.open_vulnerabilities + finding.closed_vulnerabilities
+        )
+        remediation_dict[label][3] = (
+            int(remediation_dict[label][3]) + finding.closed_vulnerabilities
+        )
+
+    remediation_dict[critical][4] = _set_percentage(
+        int(remediation_dict[critical][2]), int(remediation_dict[critical][3])
+    )
+    remediation_dict[high][4] = _set_percentage(
+        int(remediation_dict[high][2]), int(remediation_dict[high][3])
+    )
+    remediation_dict[medium][4] = _set_percentage(
+        int(remediation_dict[medium][2]), int(remediation_dict[medium][3])
+    )
+    remediation_dict[low][4] = _set_percentage(
+        int(remediation_dict[low][2]), int(remediation_dict[low][3])
+    )
+
+    return remediation_dict.values()
 
 
 class CertificateCreator(CreatorPdf):
