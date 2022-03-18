d041850f6edd4243ce7d791f054931c935e583dc
David Acevedo <dacevedo@fluidattacks.com>
2022-03-18 10:42:30 -0500

    integrates\feat(front): #6079 improve timeline
    - added responsive behaviour according to the new design
    - moved arrow and dot pseudoclasses to the container
    - matching the prototype

--- integrates/front/src/components/Timeline/Item.tsx
+++ integrates/front/src/components/Timeline/Item.tsx
@@ -4,16 +4,6 @@ import styled from "styled-components";
 const ItemContainer = styled.li.attrs({ className: "flex relative" })`
   padding: 10px 40px;
   width: 50%;
-`;
-
-const ItemContent = styled.article.attrs({ className: "bg-white" })`
-  border-radius: 6px;
-  box-shadow: 0px 8px 16px 0px rgb(92 92 112 / 20%);
-  padding: 20px 30px;
-
-  p {
-    color: #5c5c70;
-  }
 
   ::after {
     background-color: white;
@@ -32,6 +22,22 @@ const ItemContent = styled.article.attrs({ className: "bg-white" })`
     content: " ";
     position: absolute;
     top: 22px;
+  }
+
+  @media (max-width: 768px) {
+    padding-left: 70px;
+    padding-right: 25px;
+    width: 100%;
+  }
+`;
+
+const ItemContent = styled.article.attrs({ className: "bg-white" })`
+  border-radius: 6px;
+  box-shadow: 0px 8px 16px 0px rgb(92 92 112 / 20%);
+  padding: 20px 30px;
+
+  p {
+    color: #5c5c70;
   }
 `;
 
--- integrates/front/src/components/Timeline/index.tsx
+++ integrates/front/src/components/Timeline/index.tsx
@@ -10,14 +10,14 @@ const Timeline = styled.ol.attrs({
     justify-content: flex-end;
   }
 
-  li:nth-child(odd) article::after {
+  li:nth-child(odd)::after {
     right: -16px;
   }
 
-  li:nth-child(odd) article::before {
+  li:nth-child(odd)::before {
     border-color: transparent transparent transparent white;
     border-width: 10px 0 10px 10px;
-    right: 30px;
+    right: 31px;
   }
 
   li:nth-child(even) {
@@ -25,14 +25,14 @@ const Timeline = styled.ol.attrs({
     justify-content: flex-start;
   }
 
-  li:nth-child(even) article::after {
+  li:nth-child(even)::after {
     left: -16px;
   }
 
-  li:nth-child(even) article::before {
+  li:nth-child(even)::before {
     border-color: transparent white transparent transparent;
     border-width: 10px 10px 10px 0;
-    left: 30px;
+    left: 31px;
   }
 
   ::after {
@@ -42,6 +42,30 @@ const Timeline = styled.ol.attrs({
     left: calc(50% - 3px);
     position: absolute;
     width: 6px;
+  }
+
+  @media (max-width: 768px) {
+    li:nth-child(odd) {
+      align-self: flex-end;
+      justify-content: flex-start;
+    }
+
+    li:nth-child(odd)::after,
+    li:nth-child(even)::after {
+      left: 18px;
+    }
+
+    li:nth-child(odd)::before,
+    li:nth-child(even)::before {
+      border-color: transparent white transparent transparent;
+      border-width: 10px 10px 10px 0;
+      left: 60px;
+      right: unset;
+    }
+
+    ::after {
+      left: 31px;
+    }
   }
 `;
 
