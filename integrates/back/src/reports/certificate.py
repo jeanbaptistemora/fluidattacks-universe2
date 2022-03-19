df37f75bfa43b057fe24d33fcf5fc1795662bf7e
Juan Chaves <jchaves@fluidattacks.com>
2022-03-19 00:40:34 +0000

    integrates\refac(back): #6084.7 add hacking certs
    - Added a method to fill the cert context and the language
    options to fill it

--- integrates/back/src/reports/certificate.py
+++ integrates/back/src/reports/certificate.py
@@ -1,5 +1,11 @@
+from aioextensions import (
+    collect,
+)
 from back.src.settings.logger import (
     LOGGING,
+)
+from datetime import (
+    datetime,
 )
 from db_model.findings.types import (
     Finding,
@@ -9,11 +15,16 @@ from findings import (
 )
 import logging
 import matplotlib
+from newutils.datetime import (
+    get_from_str,
+    get_now,
+)
 from reports.pdf import (
     CreatorPdf,
 )
 from reports.typing import (
     CertFindingInfo,
+    CertInfoEs,
 )
 from typing import (
     Any,
@@ -32,33 +43,26 @@ matplotlib.use("Agg")
 
 # Constants
 LOGGER = logging.getLogger(__name__)
-Context = TypedDict(
-    "Context",
+CertContext = TypedDict(
+    "CertContext",
     {
         "business": str,
         "business_number": str,
-        "solution": str,
-        "start_date": str,
-        "report_date": str,
-        "team": str,
-        "team_mail": str,
         "customer": str,
-        "version": str,
-        "revdate": str,
+        "solution": str,
         "remediation_table": ValuesView[List[Union[float, int, str]]],
-        "findings": Tuple[CertFindingInfo, ...],
-        "accessVector": Optional[str],
+        "fluid_tpl": Dict[str, str],
         "cert_title": str,
         "cert_body_part_1": str,
         "cert_body_part_2": str,
-        "cert_signature": str,
-        "crit_h": str,
-        "crit_m": str,
-        "crit_l": str,
-        "field": str,
-        "user": str,
-        "date": str,
-        "link": str,
+        "signature_footer": str,
+        "start_day": str,
+        "start_month": str,
+        "start_year": str,
+        "report_day": str,
+        "report_month": str,
+        "report_year": str,
+        "words": Dict[str, str],
     },
 )
 
@@ -92,7 +96,7 @@ async def format_finding(
 
 def _set_percentage(total_vulns: int, closed_vulns: int) -> str:
     if total_vulns == 0:
-        return f"{closed_vulns * 100.0 / total_vulns:.1f}"
+        return f"{closed_vulns * 100.0 / total_vulns:.1f}%"
     return "N/A"
 
 
@@ -150,7 +154,70 @@ def make_remediation_table(
 class CertificateCreator(CreatorPdf):
     """Class to generate certificates in PDF."""
 
+    cert_context: Optional[CertContext] = None
+    cert_body: Dict[str, Dict[str, str]] = {}
+
     def __init__(self, lang: str, doctype: str, tempdir: str) -> None:
         "Class constructor"
         super().__init__(lang, doctype, tempdir)
         self.proj_tpl = "templates/pdf/certificate.adoc"
+        self.cert_support()
+
+    def cert_support(self) -> None:
+        """Define the dictionaries of accepted languages. For
+        the certificate body"""
+        self.cert_body = {}
+        self.cert_support_es()
+
+    def cert_support_es(self) -> None:
+        """Adds the English dictionary."""
+        self.wordlist["en"] = dict(zip(CertInfoEs.keys(), CertInfoEs.labels()))
+
+    def make_content(self, words: Dict[str, str]) -> Dict[str, str]:
+        """Gather Fluid-related content needed for the cert"""
+        base_adoc = "include::../templates/pdf/footer_{lang}.adoc[]"
+        return {
+            "signature_img": "image::../templates/pdf/signature.png[]",
+            "footer_adoc": base_adoc.format(lang=self.lang),
+        }
+
+    async def fill_context(  # noqa pylint: disable=too-many-arguments,too-many-locals
+        self,
+        findings: Tuple[Finding, ...],
+        group: str,
+        description: str,
+        user: str,
+        loaders: Any,
+    ) -> None:
+        """Fetch information and fill out the context"""
+        words = self.wordlist[self.lang]
+        cert_body_loc = self.cert_body[self.lang]
+        fluid_tpl_content = self.make_content(words)
+        context_findings = await collect(
+            [format_finding(loaders, finding, words) for finding in findings]
+        )
+        remediation_table = make_remediation_table(context_findings, words)
+        group_data: Dict[str, Any] = loaders.group.load((group))
+        start_date: datetime = get_from_str(
+            group_data["historic_configuration"][0]["date"]
+        )
+        current_date: datetime = get_now()
+        self.cert_context = {
+            "business": "",
+            "business_number": "",
+            "customer": user,
+            "fluid_tpl": fluid_tpl_content,
+            "remediation_table": remediation_table,
+            "cert_title": words["cert_title"],
+            "cert_body_part_1": cert_body_loc["cert_body_part_1"],
+            "cert_body_part_2": cert_body_loc["cert_body_part_2"],
+            "signature_footer": cert_body_loc["signature_footer"],
+            "start_day": str(start_date.day),
+            "start_month": start_date.strftime("%B"),
+            "start_year": str(start_date.year),
+            "report_day": str(current_date.day),
+            "report_month": current_date.strftime("%B"),
+            "report_year": str(current_date.year),
+            "solution": description,
+            "words": words,
+        }
