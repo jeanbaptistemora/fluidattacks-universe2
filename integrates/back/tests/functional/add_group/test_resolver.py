9793da569e9cb6d3caf01cbacb479616222b12be
Jonathan Mesa <jmesa@fluidattacks.com>
2022-03-29 17:10:45 +0000

    integrates\test(back): #6108.80 add group
    - complete functional test for this mutation

--- integrates/back/tests/functional/add_group/test_resolver.py
+++ integrates/back/tests/functional/add_group/test_resolver.py
@@ -1,10 +1,32 @@
 from . import (
     get_result,
 )
+from dataloaders import (
+    Dataloaders,
+    get_new_context,
+)
+from db_model.groups.enums import (
+    GroupLanguage,
+    GroupService,
+    GroupStateStatus,
+    GroupSubscriptionType,
+    GroupTier,
+)
+from db_model.groups.types import (
+    Group,
+)
+from group_access import (
+    domain as group_access_domain,
+)
+from names import (
+    domain as names_domain,
+)
+from organizations import (
+    domain as orgs_domain,
+)
 import pytest
 from typing import (
     Any,
-    Dict,
 )
 
 
@@ -20,12 +42,35 @@ async def test_add_group(populate: bool, email: str) -> None:
     assert populate
     org_name: str = "orgtest"
     group_name: str = "group1"
-    result: Dict[str, Any] = await get_result(
+    result: dict[str, Any] = await get_result(
         user=email, org=org_name, group=group_name
     )
     assert "errors" not in result
     assert "success" in result["data"]["addGroup"]
     assert result["data"]["addGroup"]["success"]
+
+    assert await names_domain.exists(group_name, "group") is False
+
+    loaders: Dataloaders = get_new_context()
+    group: Group = await loaders.group_typed.load(group_name)
+    assert group.agent_token is None
+    assert group.language == GroupLanguage.EN
+    assert group.organization_name == org_name
+    assert group.state.has_machine is True
+    assert group.state.has_squad is True
+    assert group.state.modified_by == email
+    assert group.state.service == GroupService.WHITE
+    assert group.state.status == GroupStateStatus.ACTIVE
+    assert group.state.tier == GroupTier.FREE
+    assert group.state.type == GroupSubscriptionType.CONTINUOUS
+
+    org_id = await orgs_domain.get_id_by_name(org_name)
+    org_groups: tuple[str, ...] = await orgs_domain.get_groups(org_id)
+    assert group_name in org_groups
+    assert await orgs_domain.has_user_access(org_id, email)
+    # Admins are not granted access to the group
+    group_users = await group_access_domain.get_group_users(group_name)
+    assert email not in group_users
 
 
 @pytest.mark.asyncio
@@ -48,7 +93,7 @@ async def test_add_group_fail(populate: bool, email: str) -> None:
     assert populate
     org_name: str = "orgtest"
     group_name: str = "group1"
-    result: Dict[str, Any] = await get_result(
+    result: dict[str, Any] = await get_result(
         user=email, org=org_name, group=group_name
     )
     assert "errors" in result
