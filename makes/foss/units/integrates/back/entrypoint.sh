7c9e1f165339f0ead23126a840ff2aa4592b0e8a
David Acevedo <dacevedo@fluidattacks.com>
2022-03-31 07:43:45 -0500

    integrates\refac(back): #5961 install datadog
    - use host ip from env vars to reach dogstatsd

--- makes/foss/units/integrates/back/entrypoint.sh
+++ makes/foss/units/integrates/back/entrypoint.sh
@@ -55,7 +55,7 @@ function serve {
         # The number of seconds to wait for requests on a Keep-Alive connection
         --keep-alive "${load_balancer_timeout}"
         # host:port of the statsd server to log to
-        --statsd-host localhost:8125
+        --statsd-host "${INSTANA_AGENT_HOST}":8125
         # The number of worker processes for handling requests
         --workers "${recommended_workers}"
       )
@@ -64,7 +64,7 @@ function serve {
         # The number of seconds to wait for requests on a Keep-Alive connection
         --keep-alive "${load_balancer_timeout}"
         # host:port of the statsd server to log to
-        --statsd-host localhost:8125
+        --statsd-host "${INSTANA_AGENT_HOST}":8125
         # The number of worker processes for handling requests
         --workers "${recommended_workers}"
       )
