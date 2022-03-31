f9477a2c2a92763313c864591a13d0217088dd6e
Frank Aristizabal <faristizabal@fluidattacks.com>
2022-03-31 14:13:51 -0500

    airs\refac(front): #6199.9 new page
    - Add portrait section for new product
    overview page.

---
+++ airs/front/src/components/ProductOverviewPage/Portrait/index.tsx
@@ -0,0 +1,46 @@
+/* eslint react/forbid-component-props: 0 */
+import { Link } from "gatsby";
+import React from "react";
+
+import {
+  InnerMainContentPortrait,
+  MainContentPortrait,
+  PortraitBigParagraph,
+  PortraitContainer,
+  PortraitImageContainer,
+  PortraitParagraph,
+} from "./styledComponents";
+
+import { translate } from "../../../utils/translations/translate";
+import { CloudImage } from "../../CloudImage";
+import { BigRegularRedButton } from "../styledComponents";
+
+const Portrait: React.FC = (): JSX.Element => (
+  <PortraitContainer>
+    <MainContentPortrait>
+      <InnerMainContentPortrait>
+        <PortraitBigParagraph>
+          {translate.t("productOverview.portrait.title")}
+        </PortraitBigParagraph>
+        <PortraitParagraph>
+          {translate.t("productOverview.portrait.paragraph")}
+        </PortraitParagraph>
+        <div className={"cf tl-l tc mt4 mb5"}>
+          <Link className={"no-underline"} to={"/contact-us-demo/"}>
+            <BigRegularRedButton>
+              {translate.t("productOverview.portrait.button")}
+            </BigRegularRedButton>
+          </Link>
+        </div>
+      </InnerMainContentPortrait>
+      <PortraitImageContainer>
+        <CloudImage
+          alt={"Attacks Surface Management Vulns"}
+          src={"/product-overview/portrait/product-overview-portrait.png"}
+        />
+      </PortraitImageContainer>
+    </MainContentPortrait>
+  </PortraitContainer>
+);
+
+export { Portrait };
