89229206f2314e5f38ca1c1dd56ed8e34bde9d61
David Acevedo <dacevedo@fluidattacks.com>
2022-04-01 14:57:20 -0500

    integrates\refac(back): #6230 boto config
    - we noticed that something calling ssl functions is having a high
    - resource consumption
    - lets disable ssl and monitor

--- integrates/back/src/dynamodb/operations.py
+++ integrates/back/src/dynamodb/operations.py
@@ -53,17 +53,19 @@ from typing import (
 )
 
 RESOURCE_OPTIONS = {
-    "service_name": "dynamodb",
     "aws_access_key_id": FI_AWS_DYNAMODB_ACCESS_KEY,
     "aws_secret_access_key": FI_AWS_DYNAMODB_SECRET_KEY,
     "aws_session_token": FI_AWS_SESSION_TOKEN,
-    "region_name": "us-east-1",
     "endpoint_url": (
         # FP: the endpoint is hosted in a local environment
         f"http://{FI_DYNAMODB_HOST}:{FI_DYNAMODB_PORT}"  # NOSONAR
         if FI_ENVIRONMENT == "development"
         else None
     ),
+    "region_name": "us-east-1",
+    "service_name": "dynamodb",
+    "use_ssl": False,
+    "verify": False,
 }
 SESSION = aioboto3.Session()
 
--- integrates/back/src/dynamodb/operations_legacy.py
+++ integrates/back/src/dynamodb/operations_legacy.py
@@ -29,23 +29,24 @@ from typing import (
     Any,
     Dict,
     List,
-    Optional,
 )
 
 # Constants
 LOGGER = logging.getLogger(__name__)
-RESOURCE_OPTIONS: Dict[str, Optional[str]] = {
-    "service_name": "dynamodb",
+RESOURCE_OPTIONS = {
     "aws_access_key_id": FI_AWS_DYNAMODB_ACCESS_KEY,
     "aws_secret_access_key": FI_AWS_DYNAMODB_SECRET_KEY,
     "aws_session_token": os.environ.get("AWS_SESSION_TOKEN"),
-    "region_name": "us-east-1",
     "endpoint_url": (
         # FP: the endpoint is hosted in a local environment
         f"http://{FI_DYNAMODB_HOST}:{FI_DYNAMODB_PORT}"  # NOSONAR
         if FI_ENVIRONMENT == "development"
         else None
     ),
+    "region_name": "us-east-1",
+    "service_name": "dynamodb",
+    "use_ssl": False,
+    "verify": False,
 }
 SESSION = aioboto3.Session()
 
