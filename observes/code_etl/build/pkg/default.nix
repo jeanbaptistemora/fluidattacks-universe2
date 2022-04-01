b02983aa979eaafae6caa982e0d2c41b589dd0e6
Daniel Murcia <dmurcia@fluidattacks.com>
2022-04-01 12:49:24 -0500

    observes\feat(build): #6250.2 nix build
    - build code etl with nix

---
+++ observes/code_etl/build/pkg/default.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  pythonPkgs,
+  src,
+  metadata,
+}: let
+  runtime_deps = [
+    pythonPkgs.click
+    pythonPkgs.fa_purity
+    pythonPkgs.GitPython
+    pythonPkgs.pathos
+    pythonPkgs.psycopg2
+    pythonPkgs.ratelimiter
+    pythonPkgs.requests
+    pythonPkgs.six
+    pythonPkgs.types-click
+    pythonPkgs.utils-logger
+  ];
+  dev_deps = [
+    pythonPkgs.import-linter
+    pythonPkgs.mypy
+    pythonPkgs.poetry
+    pythonPkgs.pytest
+    pythonPkgs.toml
+    pythonPkgs.types-toml
+  ];
+  pkg = (import ./build.nix) {
+    inherit lib src metadata;
+    nativeBuildInputs = dev_deps;
+    propagatedBuildInputs = runtime_deps;
+  };
+  build_env = extraLibs:
+    lib.buildEnv {
+      inherit extraLibs;
+      ignoreCollisions = false;
+    };
+in {
+  inherit pkg;
+  env.runtime = build_env runtime_deps;
+  env.dev = build_env (runtime_deps ++ dev_deps);
+}
