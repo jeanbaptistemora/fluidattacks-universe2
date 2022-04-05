5b22d59b5fc4477dd5807773452c88618b0ffb0d
Andres Aguirre <aaguirre@fluidattacks.com>
2022-04-05 17:11:15 +0000

    integrates\test(front): #6063.49 add new mobile
    - Should add a new mobile with the mobile modal

--- integrates/front/src/scenes/Dashboard/components/Navbar/UserProfile/MobileModal/index.test.tsx
+++ integrates/front/src/scenes/Dashboard/components/Navbar/UserProfile/MobileModal/index.test.tsx
@@ -1,11 +1,19 @@
 import type { MockedResponse } from "@apollo/client/testing";
 import { MockedProvider } from "@apollo/client/testing";
+import { PureAbility } from "@casl/ability";
 import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import React from "react";
 
-import { GET_STAKEHOLDER_PHONE } from "./queries";
+import {
+  GET_STAKEHOLDER_PHONE,
+  UPDATE_STAKEHOLDER_PHONE_MUTATION,
+  VERIFY_STAKEHOLDER_MUTATION,
+} from "./queries";
 
 import { MobileModal } from ".";
+import { authzPermissionsContext } from "utils/authz/config";
+import { msgSuccess } from "utils/notifications";
 
 jest.mock("../../../../../../utils/notifications", (): Dictionary => {
   const mockedNotifications: Dictionary<() => Dictionary> = jest.requireActual(
@@ -28,7 +36,7 @@ describe("Mobile modal", (): void => {
 
     const handleOnClose: jest.Mock = jest.fn();
 
-    const mockQueryFalse: MockedResponse[] = [
+    const mockQuery: MockedResponse[] = [
       {
         request: {
           query: GET_STAKEHOLDER_PHONE,
@@ -50,12 +58,128 @@ describe("Mobile modal", (): void => {
     ];
 
     render(
-      <MockedProvider addTypename={false} mocks={mockQueryFalse}>
+      <MockedProvider addTypename={false} mocks={mockQuery}>
         <MobileModal onClose={handleOnClose} />
       </MockedProvider>
     );
     await waitFor((): void => {
       expect(screen.getByDisplayValue("+1 (123) 454-5")).toBeInTheDocument();
+    });
+
+    expect(
+      screen.getByRole("button", { name: "profile.mobileModal.close" })
+    ).toBeInTheDocument();
+    expect(handleOnClose).toHaveBeenCalledTimes(0);
+  });
+
+  it("should add a new mobile", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    const handleOnClose: jest.Mock = jest.fn();
+
+    const mockedPermissions: PureAbility<string> = new PureAbility([
+      { action: "api_mutations_update_stakeholder_phone_mutate" },
+    ]);
+    const mockQuery: MockedResponse[] = [
+      {
+        request: {
+          query: GET_STAKEHOLDER_PHONE,
+        },
+        result: {
+          data: {
+            me: {
+              __typename: "Me",
+              phone: null,
+              userEmail: "test@fluidattacks.com",
+            },
+          },
+        },
+      },
+      {
+        request: {
+          query: GET_STAKEHOLDER_PHONE,
+        },
+        result: {
+          data: {
+            me: {
+              __typename: "Me",
+              phone: {
+                callingCountryCode: "57",
+                countryCode: "CO",
+                nationalNumber: "123456789",
+              },
+              userEmail: "test@fluidattacks.com",
+            },
+          },
+        },
+      },
+    ];
+    const mocksMutation: readonly MockedResponse[] = [
+      {
+        request: {
+          query: VERIFY_STAKEHOLDER_MUTATION,
+          variables: {
+            newPhone: {
+              callingCountryCode: "57",
+              nationalNumber: "123456789",
+            },
+          },
+        },
+        result: { data: { verifyStakeholder: { success: true } } },
+      },
+      {
+        request: {
+          query: UPDATE_STAKEHOLDER_PHONE_MUTATION,
+          variables: {
+            newPhone: { callingCountryCode: "57", nationalNumber: "123456789" },
+            verificationCode: "1234",
+          },
+        },
+        result: { data: { updateStakeholderPhone: { success: true } } },
+      },
+    ];
+
+    render(
+      <MockedProvider
+        addTypename={false}
+        mocks={mockQuery.concat(mocksMutation)}
+      >
+        <authzPermissionsContext.Provider value={mockedPermissions}>
+          <MobileModal onClose={handleOnClose} />
+        </authzPermissionsContext.Provider>
+      </MockedProvider>
+    );
+
+    userEvent.type(screen.getByRole("textbox"), "123456789");
+    userEvent.click(
+      screen.getByRole("button", { name: "profile.mobileModal.add" })
+    );
+    await waitFor((): void => {
+      expect(
+        screen.getByRole("button", { name: "profile.mobileModal.verify" })
+      ).toBeInTheDocument();
+      expect(msgSuccess).toHaveBeenCalledTimes(1);
+      expect(msgSuccess).toHaveBeenLastCalledWith(
+        "profile.mobileModal.alerts.sendNewMobileVerificationSuccess",
+        "groupAlerts.titleSuccess"
+      );
+    });
+
+    userEvent.type(
+      screen.getByRole("textbox", {
+        name: "newVerificationCode",
+      }),
+      "1234"
+    );
+    userEvent.click(
+      screen.getByRole("button", { name: "profile.mobileModal.verify" })
+    );
+    await waitFor((): void => {
+      expect(msgSuccess).toHaveBeenCalledTimes(2);
+      expect(msgSuccess).toHaveBeenLastCalledWith(
+        "profile.mobileModal.alerts.additionSuccess",
+        "groupAlerts.titleSuccess"
+      );
     });
 
     expect(
