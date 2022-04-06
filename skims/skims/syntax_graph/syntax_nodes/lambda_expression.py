40e998dc18011829de36e1e7c8718b05afc28bd7
Juan Echeverri <jecheverri@fluidattacks.com>
2022-04-06 17:12:55 -0500

    skims\refac(back): #6160.18 refac lambda exp cs
    - Refac syntax reader for lambda expression in C#

--- skims/skims/syntax_graph/syntax_nodes/lambda_expression.py
+++ skims/skims/syntax_graph/syntax_nodes/lambda_expression.py
@@ -4,21 +4,41 @@ from model.graph_model import (
 from syntax_graph.types import (
     SyntaxGraphArgs,
 )
+from typing import (
+    Optional,
+)
 
 
 def build_lambda_expression_node(
-    args: SyntaxGraphArgs, var_name: str, expression_node: str
+    args: SyntaxGraphArgs,
+    var_name: Optional[str],
+    parameters: Optional[str],
+    block_node: Optional[str],
 ) -> NId:
     args.syntax_graph.add_node(
         args.n_id,
-        variable=var_name,
         label_type="LambdaExpression",
     )
 
-    args.syntax_graph.add_edge(
-        args.n_id,
-        args.generic(args.fork_n_id(expression_node)),
-        label_ast="AST",
-    )
+    if var_name:
+        args.syntax_graph.add_edge(
+            args.n_id,
+            args.generic(args.fork_n_id(var_name)),
+            label_ast="AST",
+        )
+
+    if parameters:
+        args.syntax_graph.add_edge(
+            args.n_id,
+            args.generic(args.fork_n_id(parameters)),
+            label_ast="AST",
+        )
+
+    if block_node:
+        args.syntax_graph.add_edge(
+            args.n_id,
+            args.generic(args.fork_n_id(block_node)),
+            label_ast="AST",
+        )
 
     return args.n_id
--- skims/skims/syntax_graph/syntax_readers/c_sharp/lambda_expression.py
+++ skims/skims/syntax_graph/syntax_readers/c_sharp/lambda_expression.py
@@ -13,7 +13,10 @@ from utils.graph import (
 
 
 def reader(args: SyntaxGraphArgs) -> NId:
-    match_childs = match_ast(args.ast_graph, args.n_id)
-    var_name = args.ast_graph.nodes[match_childs["__0__"]]["label_text"]
-    expression_node = str(match_childs["__2__"])
-    return build_lambda_expression_node(args, var_name, expression_node)
+    match_childs = match_ast(
+        args.ast_graph, args.n_id, "parameter_list", "identifier", "block"
+    )
+    var_name = match_childs.get("identifier")
+    parameters = match_childs.get("parameter_list")
+    block_node = match_childs.get("block")
+    return build_lambda_expression_node(args, var_name, parameters, block_node)
