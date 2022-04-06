88b472442af33aa50e3bad4736f0fbf7c6685ff7
Daniel Murcia <dmurcia@fluidattacks.com>
2022-04-06 09:17:37 -0500

    observes\feat(back): #6247.3 commit first seen
    - add get_commit_first_seen_at

--- observes/code_etl/code_etl/compute_bills/_getter.py
+++ observes/code_etl/code_etl/compute_bills/_getter.py
@@ -1,3 +1,15 @@
+from datetime import (
+    datetime,
+)
+from fa_purity import (
+    Cmd,
+)
+from fa_purity.frozen import (
+    freeze,
+)
+from fa_purity.utils import (
+    raise_exception,
+)
 from functools import (
     lru_cache,
 )
@@ -5,6 +17,12 @@ import json
 import logging
 from ratelimiter import (  # type: ignore[import]
     RateLimiter,
+)
+from redshift_client.sql_client.core import (
+    SqlClient,
+)
+from redshift_client.sql_client.query import (
+    new_query,
 )
 import requests
 from typing import (
@@ -45,3 +63,19 @@ def _get_group_org(token: str, group: str) -> Optional[str]:
 
 def get_org(token: str, group: str) -> Optional[str]:
     return cast(Optional[str], _get_group_org(token, group))
+
+
+def get_commit_first_seen_at(client: SqlClient, fa_hash: str) -> Cmd[datetime]:
+    stm = """
+        SELECT seen_at FROM code.commits
+        WHERE fa_hash = %(fa_hash)s ORDER BY seen_at ASC LIMIT 1
+    """
+    return client.execute(
+        new_query(stm), freeze({"fa_hash": fa_hash})
+    ) + client.fetch_one().map(lambda i: i[0] if i else None).map(
+        lambda i: datetime.fromisoformat(i)
+        if isinstance(i, str)
+        else raise_exception(
+            Exception(f"Expected a datetime; got {str(i)} of type {type(i)}")
+        )
+    )
