6d817de2c73477d259662a8985fd842760536e26
Daniel Murcia <dmurcia@fluidattacks.com>
2022-04-08 15:48:54 -0500

    observes\feat(back): #6247.13 main procedure
    - add save_all and main cmds

--- observes/code_etl/code_etl/compute_bills/__init__.py
+++ observes/code_etl/code_etl/compute_bills/__init__.py
@@ -1,10 +1,17 @@
 from ._getter import (
     get_month_contributions,
+    get_month_repos,
+)
+from ._keeper import (
+    new_keeper,
 )
 from ._report import (
     extract_active_users,
     filter_by_fa_hash,
     final_reports,
+)
+from charset_normalizer import (
+    logging,
 )
 from code_etl.compute_bills.core import (
     ActiveUsersReport,
@@ -13,6 +20,10 @@ from code_etl.compute_bills.core import (
 from code_etl.objs import (
     GroupId,
 )
+from code_etl.utils import (
+    DB_CREDS,
+    DB_ID,
+)
 from datetime import (
     datetime,
 )
@@ -20,19 +31,32 @@ from fa_purity import (
     Cmd,
     FrozenDict,
 )
+from fa_purity.cmd import (
+    unsafe_unwrap,
+)
 from fa_purity.cmd.transform import (
     serial_merge,
 )
 from fa_purity.frozen import (
     freeze,
 )
+from pathlib import (
+    Path,
+)
+from redshift_client.sql_client.connection import (
+    connect,
+    IsolationLvl,
+)
 from redshift_client.sql_client.core import (
+    new_client,
     SqlClient,
 )
 from typing import (
     FrozenSet,
     Tuple,
 )
+
+LOG = logging.getLogger(__name__)
 
 
 def gen_final_reports(
@@ -54,4 +78,38 @@ def gen_final_reports(
     reports = tuple(map(process_group, groups))
     return (
         serial_merge(reports).map(lambda x: freeze(dict(x))).map(final_reports)
+    )
+
+
+def save_all(
+    token: str, folder: Path, data: FrozenDict[GroupId, FinalActiveUsersReport]
+) -> Cmd[None]:
+    def _action(grp: GroupId, report: FinalActiveUsersReport) -> None:
+        with open(
+            str(folder.joinpath(grp.name + ".csv")), "w", encoding="UTF-8"
+        ) as file:
+            unsafe_unwrap(new_keeper(file, token).save(grp, report))
+
+    def _save(items: Tuple[GroupId, FinalActiveUsersReport]) -> Cmd[None]:
+        return Cmd.from_cmd(lambda: _action(items[0], items[1]))
+
+    return serial_merge(tuple(map(_save, data.items()))).map(lambda _: None)
+
+
+def main(token: str, folder: Path, date: datetime) -> Cmd[None]:
+    connection = connect(DB_ID, DB_CREDS, True, IsolationLvl.AUTOCOMMIT)
+    client_1 = connection.bind(
+        lambda c: new_client(c, LOG.getChild("client_1"))
+    )
+    client_2 = connection.bind(
+        lambda c: new_client(c, LOG.getChild("client_2"))
+    )
+    return client_1.bind(
+        lambda c1: client_2.bind(
+            lambda c2: get_month_repos(c1, date).bind(
+                lambda groups: gen_final_reports(c1, c2, date, groups).bind(
+                    lambda d: save_all(token, folder, d)
+                )
+            )
+        )
     )
--- observes/code_etl/code_etl/utils.py
+++ observes/code_etl/code_etl/utils.py
@@ -1,7 +1,24 @@
 from datetime import (
     datetime,
 )
+from os import (
+    environ,
+)
+from redshift_client.sql_client.connection import (
+    Credentials,
+    DatabaseId,
+)
 
 COMMIT_HASH_SENTINEL: str = "-" * 40
 DATE_SENTINEL: datetime = datetime.utcfromtimestamp(0)
 DATE_NOW: datetime = datetime.utcnow()
+
+DB_ID = DatabaseId(
+    environ["REDSHIFT_DATABASE"],
+    environ["REDSHIFT_HOST"],
+    int(environ["REDSHIFT_PORT"]),
+)
+DB_CREDS = Credentials(
+    environ["REDSHIFT_USER"],
+    environ["REDSHIFT_PASSWORD"],
+)
