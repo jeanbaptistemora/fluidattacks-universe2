49f62d2b13c0c40fa4bd0434710713be2763f0fc
Frank Aristizabal <faristizabal@fluidattacks.com>
2022-04-08 14:33:35 -0500

    airs\refac(front): #6272.3 add plans
    - Add components and styles for
    new plan section in product
    overview page.

---
+++ airs/front/src/components/ProductOverviewPage/PlansSection/PlanCard/index.tsx
@@ -0,0 +1,76 @@
+/* eslint react/forbid-component-props: 0 */
+import { Link } from "gatsby";
+import React, { useCallback, useState } from "react";
+import { FiMinus, FiPlus } from "react-icons/fi";
+
+import {
+  CardContainer,
+  CardItem,
+  CardItemsContainer,
+  CardParagraph,
+  CardTitle,
+  CardTitleContainer,
+  MachineButton,
+  OpenButton,
+} from "./styledComponents";
+import type { IPlansCard } from "./types";
+
+import { CloudImage } from "../../../CloudImage";
+
+const PlanCard: React.FC<IPlansCard> = ({
+  description,
+  isMachine,
+  items,
+  title,
+}: IPlansCard): JSX.Element => {
+  const [isOpen, setOpen] = useState(false);
+  const handleOpenClose = useCallback((): void => {
+    setOpen(!isOpen);
+  }, [isOpen]);
+
+  return (
+    <div className={"w-100 mh3"}>
+      {isMachine ? (
+        <Link to={"/contact-us-demo/"}>
+          <MachineButton>{"Try for free 30 days"}</MachineButton>
+        </Link>
+      ) : undefined}
+      <CardContainer isMachine={isMachine}>
+        <CardTitleContainer>
+          <CardTitle>
+            <CloudImage
+              alt={`Plan ${title} Fluid Attacks`}
+              src={`airs/product-overview/plans-section/${
+                isMachine ? "machine" : "squad"
+              }`}
+              styles={"mr1"}
+            />
+            {title}
+            <OpenButton onClick={handleOpenClose}>
+              {isOpen ? <FiPlus /> : <FiMinus />}
+            </OpenButton>
+          </CardTitle>
+        </CardTitleContainer>
+        <CardItemsContainer isOpen={isOpen}>
+          <CardParagraph isOpen={isOpen}>{description}</CardParagraph>
+          {items.map((item): JSX.Element => {
+            return (
+              <CardItem key={`${item.text}`}>
+                <CloudImage
+                  alt={`plan-check`}
+                  src={`airs/product-overview/plans-section/${
+                    item.check ? "check" : "xmark"
+                  }`}
+                  styles={"mr3 check-icon"}
+                />
+                {item.text}
+              </CardItem>
+            );
+          })}
+        </CardItemsContainer>
+      </CardContainer>
+    </div>
+  );
+};
+
+export { PlanCard };
