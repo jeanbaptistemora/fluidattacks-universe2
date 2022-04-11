5b5716e0fac3e7f00491ca2af0dd56550257f7fd
Juan Echeverri <jecheverri@fluidattacks.com>
2022-04-11 18:52:19 -0500

    skims\feat(back): #6160.21 add field to lambda node cs
    - Add block_id field to lamba_expression C# node

--- skims/skims/syntax_graph/syntax_nodes/lambda_expression.py
+++ skims/skims/syntax_graph/syntax_nodes/lambda_expression.py
@@ -13,10 +13,11 @@ def build_lambda_expression_node(
     args: SyntaxGraphArgs,
     var_name: Optional[str],
     parameters: Optional[str],
-    block_node: Optional[str],
+    block_id: Optional[str],
 ) -> NId:
     args.syntax_graph.add_node(
         args.n_id,
+        block_id=block_id,
         label_type="LambdaExpression",
     )
 
@@ -34,10 +35,10 @@ def build_lambda_expression_node(
             label_ast="AST",
         )
 
-    if block_node:
+    if block_id:
         args.syntax_graph.add_edge(
             args.n_id,
-            args.generic(args.fork_n_id(block_node)),
+            args.generic(args.fork_n_id(block_id)),
             label_ast="AST",
         )
 
