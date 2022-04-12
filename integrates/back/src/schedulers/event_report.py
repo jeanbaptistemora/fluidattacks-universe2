4570ba7c9259a424002b0493c68066d9a9ec9781
Juan Villegas <jvillegas@fluidattacks.com>
2022-04-12 13:49:20 -0500

    integrates\fix(back): #5750.13 events notifications
    - Fixed events array to event report

--- integrates/back/src/schedulers/event_report.py
+++ integrates/back/src/schedulers/event_report.py
@@ -50,37 +50,36 @@ async def send_event_report() -> None:
     loaders = get_new_context()
 
     if FI_ENVIRONMENT == "production":
-        active_groups = await loaders.group_typed.load_many(groups_names)
         groups_names = [
-            group.name
-            for group in active_groups
-            if group.name not in FI_TEST_PROJECTS.split(",")
+            group
+            for group in groups_names
+            if group not in FI_TEST_PROJECTS.split(",")
         ]
 
-    unsolved_events: List[EventType] = [
-        await events_domain.get_unsolved_events(group)
+    unsolved_events = [
+        event
         for group in groups_names
+        for event in await events_domain.get_unsolved_events(group)
     ]
 
     events_filtered: List[EventType] = [
         event
         for event in unsolved_events
-        if event != []
-        and days_to_date(event[0]["historic_state"][-1]["date"]) in [7, 30]
+        if days_to_date(event["historic_state"][-1]["date"]) in [7, 30]
     ]
 
     if events_filtered:
         for event in events_filtered:
-            group_name = str(get_key_or_fallback(event[0], fallback=""))
-            event_type = event[0]["event_type"]
-            description = event[0]["detail"]
+            group_name = str(get_key_or_fallback(event, fallback=""))
+            event_type = event["event_type"]
+            description = event["detail"]
             report_date = datetime_utils.get_date_from_iso_str(
-                event[0]["historic_state"][0]["date"]
+                event["historic_state"][0]["date"]
             )
             await events_mail.send_mail_event_report(
                 loaders=loaders,
                 group_name=group_name,
-                event_id=event[0]["event_id"],
+                event_id=event["event_id"],
                 event_type=event_type,
                 description=description,
                 report_date=report_date,
