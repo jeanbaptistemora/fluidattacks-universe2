c2ce5651f577eda1ffdc3e64cd445ef585861322
David Acevedo <dacevedo@fluidattacks.com>
2022-04-12 16:29:21 -0500

    integrates\feat(front): #5982 welcome view
    - new onboarding experience

---
+++ integrates/front/src/scenes/Welcome/Onboarding/index.tsx
@@ -0,0 +1,61 @@
+import React, { useCallback } from "react";
+import { Redirect, Route, Switch, useHistory } from "react-router-dom";
+
+import { Container } from "./styles";
+
+import { Card, CardBody, CardHeader } from "components/Card";
+import { Col, Row } from "components/Layout";
+
+const Onboarding: React.FC = (): JSX.Element => {
+  const { push } = useHistory();
+
+  const goToTour = useCallback((): void => {
+    push("/welcome/tour");
+  }, [push]);
+
+  const goToDemo = useCallback((): void => {
+    push("/home");
+  }, [push]);
+
+  return (
+    <Container>
+      <Switch>
+        <Route exact={true} path={"/welcome"}>
+          <Row>
+            <Col>
+              <Row justify={"center"}>
+                <Col>
+                  <h1>{"Welcome to ASM"}</h1>
+                  <h2>{"Let's get started"}</h2>
+                </Col>
+              </Row>
+              <Row justify={"center"}>
+                <Col large={"30"} medium={"30"} small={"30"}>
+                  <Card onClick={goToTour}>
+                    <CardHeader>{"Platform tour"}</CardHeader>
+                    <CardBody>
+                      <p>
+                        {"Take our interactive tour to get familiar with ASM"}
+                      </p>
+                    </CardBody>
+                  </Card>
+                </Col>
+                <Col large={"30"} medium={"30"} small={"30"}>
+                  <Card onClick={goToDemo}>
+                    <CardHeader>{"Browse demo"}</CardHeader>
+                    <CardBody>
+                      <p>{"See ASM in action inside an interactive demo."}</p>
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Route>
+        <Redirect to={"/welcome"} />
+      </Switch>
+    </Container>
+  );
+};
+
+export { Onboarding };
