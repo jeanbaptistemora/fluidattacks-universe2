ca785df4a347ce8eb4f0948a6c9808e6a09e741b
Juan Echeverri <jecheverri@fluidattacks.com>
2022-04-11 19:11:39 -0500

    skims\feat(back): #6160.22 evaluate lambda expression cs
    - Add to symbolic_eval the lambda_expression evaluator

---
+++ skims/skims/symbolic_eval/cases/lambda_expression.py
@@ -0,0 +1,22 @@
+from model.core_model import (
+    FindingEnum,
+)
+from symbolic_eval.types import (
+    Evaluator,
+    SymbolicEvalArgs,
+)
+from typing import (
+    Dict,
+)
+
+FINDING_EVALUATORS: Dict[FindingEnum, Evaluator] = {}
+
+
+def evaluate(args: SymbolicEvalArgs) -> bool:
+    if pl_id := args.graph.nodes[args.n_id].get("block_id"):
+        args.evaluation[args.n_id] = args.generic(args.fork_n_id(pl_id))
+
+    if finding_evaluator := FINDING_EVALUATORS.get(args.finding):
+        args.evaluation[args.n_id] = finding_evaluator(args)
+
+    return args.evaluation[args.n_id]
