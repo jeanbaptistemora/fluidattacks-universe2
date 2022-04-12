9e854c60bcf51427490958a1d48f459e5c0c976f
Daniel Salazar <dsalazar@fluidattacks.com>
2022-04-12 12:32:35 -0500

    common\refac(build): #5427 pin makes
    - Pin makes to a specific commit
    for migration purposes

--- makes.lock.nix
+++ makes.lock.nix
@@ -1,6 +1,7 @@
 {
   makesSrc = builtins.fetchGit {
     url = "https://github.com/fluidattacks/makes";
-    ref = "refs/tags/22.04";
+    ref = "main";
+    rev = "a5ef5225c9fb9723958d7d980c89a7905c658712";
   };
 }
