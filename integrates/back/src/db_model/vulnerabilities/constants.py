8a2213aa7df70b65c1d32e5a74a56e6f59938a0d
Andres Aguirre <aaguirre@fluidattacks.com>
2022-04-13 21:55:00 +0000

    integrates\feat(back): #6121.6 vuln gsi filter design
    - Add design for the gsi that allows returning filtered vulns

--- integrates/back/src/db_model/vulnerabilities/constants.py
+++ integrates/back/src/db_model/vulnerabilities/constants.py
@@ -5,6 +5,11 @@ from dynamodb.types import (
     Facet,
 )
 
+FILTER_INDEX_METADATA = Facet(
+    attrs=TABLE.facets["vulnerability_metadata"].attrs,
+    pk_alias="FIN#finding_id",
+    sk_alias="VULN#vuln_id#DELETED#is_deleted#ZR#is_zero_risk#STATUS#status",
+)
 ROOT_INDEX_METADATA = Facet(
     attrs=TABLE.facets["vulnerability_metadata"].attrs,
     pk_alias="ROOT#root_id",
