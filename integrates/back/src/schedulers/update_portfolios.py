a5d18d6fb82b873f8de21e3eb639eccf2a4da098
Jonathan Mesa <jmesa@fluidattacks.com>
2022-04-18 19:22:21 +0000

    integrates\refac(back): #6108.142 update portfolios
    - improve logging for this schedule
    - modify static typing

--- integrates/back/src/schedulers/update_portfolios.py
+++ integrates/back/src/schedulers/update_portfolios.py
@@ -23,63 +23,49 @@ from decimal import (
 from findings.domain.core import (
     get_severity_score,
 )
-import logging
-import logging.config
 from organizations import (
     domain as orgs_domain,
 )
-from settings import (
-    LOGGING,
+from schedulers.common import (
+    error,
+    info,
 )
 from tags import (
     domain as tags_domain,
 )
 from typing import (
     Any,
-    cast,
-    Dict,
-    List,
-    Tuple,
     Union,
 )
-
-logging.config.dictConfig(LOGGING)
-LOGGER = logging.getLogger(__name__)
 
 
 def calculate_tag_indicators(
     tag: str,
-    tags_dict: Dict[str, List[Dict[str, Any]]],
-    indicator_list: List[str],
-) -> Dict[str, Union[Decimal, List[str]]]:
-    tag_info: Dict[str, Union[Decimal, List[str]]] = {}
+    tags_dict: dict[str, list[dict[str, Any]]],
+    indicator_list: list[str],
+) -> dict[str, Union[Decimal, list[str]]]:
+    tag_info: dict[str, Union[Decimal, list[str]]] = {}
     for indicator in indicator_list:
         if "max" in indicator:
             tag_info[indicator] = Decimal(
                 max(
-                    [
-                        cast(Decimal, group.get(indicator, Decimal("0.0")))
-                        for group in tags_dict[tag]
-                    ]
+                    Decimal(group.get(indicator, Decimal("0.0")))
+                    for group in tags_dict[tag]
                 )
             ).quantize(Decimal("0.1"))
         elif "mean" in indicator:
             tag_info[indicator] = Decimal(
                 sum(
-                    [
-                        cast(Decimal, group.get(indicator, Decimal("0.0")))
-                        for group in tags_dict[tag]
-                    ]
+                    Decimal(group.get(indicator, Decimal("0.0")))
+                    for group in tags_dict[tag]
                 )
                 / Decimal(len(tags_dict[tag]))
             ).quantize(Decimal("0.1"))
         else:
             tag_info[indicator] = Decimal(
                 min(
-                    [
-                        cast(Decimal, group.get(indicator, Decimal("inf")))
-                        for group in tags_dict[tag]
-                    ]
+                    Decimal(group.get(indicator, Decimal("inf")))
+                    for group in tags_dict[tag]
                 )
             ).quantize(Decimal("0.1"))
         tag_info["projects"] = [str(group["name"]) for group in tags_dict[tag]]
@@ -89,8 +75,8 @@ def calculate_tag_indicators(
 async def get_group_indicators_and_tags(
     loaders: Dataloaders,
     group: Group,
-    indicator_list: List[str],
-) -> Dict[str, Any]:
+    indicator_list: list[str],
+) -> dict[str, Any]:
     unreliable_indicators = await loaders.group_indicators_typed.load(
         group.name
     )
@@ -101,19 +87,19 @@ async def get_group_indicators_and_tags(
     }
 
     # This one is not present in group's unreliable_indicators
-    group_findings: Tuple[Finding, ...] = await loaders.group_findings.load(
+    group_findings: tuple[Finding, ...] = await loaders.group_findings.load(
         group.name
     )
-    filtered_indicators["max_severity"] = Decimal(
-        max(
-            [
+    filtered_indicators["max_severity"] = (
+        Decimal(
+            max(
                 float(get_severity_score(finding.severity))
                 for finding in group_findings
-            ]
-            if group_findings
-            else [0.0]
-        )
-    ).quantize(Decimal("0.1"))
+            )
+        ).quantize(Decimal("0.1"))
+        if group_findings
+        else Decimal("0.0")
+    )
 
     filtered_indicators["tag"] = group.tags
     filtered_indicators["name"] = group.name
@@ -123,11 +109,11 @@ async def get_group_indicators_and_tags(
 async def update_organization_indicators(
     loaders: Dataloaders,
     org_name: str,
-    groups: Tuple[Group, ...],
-) -> Tuple[bool, List[str]]:
-    success: List[bool] = []
-    updated_tags: List[str] = []
-    indicator_list: List[str] = [
+    groups: tuple[Group, ...],
+) -> tuple[bool, list[str]]:
+    success: list[bool] = []
+    updated_tags: list[str] = []
+    indicator_list: list[str] = [
         "max_open_severity",
         "max_severity",
         "mean_remediate",
@@ -143,7 +129,7 @@ async def update_organization_indicators(
             for group in groups
         )
     )
-    tags_dict: Dict[str, List[Dict[str, Any]]] = defaultdict(list)
+    tags_dict: dict[str, list[dict[str, Any]]] = defaultdict(list)
     for group_indicators in groups_indicators:
         for tag in group_indicators["tag"]:
             tags_dict[tag].append(group_indicators)
@@ -160,10 +146,14 @@ async def update_portfolios() -> None:
     async for _, org_name, org_group_names in (
         orgs_domain.iterate_organizations_and_groups()
     ):
-        org_groups: Tuple[Group, ...] = await loaders.group_typed.load_many(
+        info(
+            "[scheduler]: working on organization",
+            extra={"organization": org_name},
+        )
+        org_groups: tuple[Group, ...] = await loaders.group_typed.load_many(
             org_group_names
         )
-        tag_groups: Tuple[Group, ...] = tuple(
+        tag_groups: tuple[Group, ...] = tuple(
             group
             for group in org_groups
             if group.state.status == GroupStateStatus.ACTIVE and group.tags
@@ -172,9 +162,9 @@ async def update_portfolios() -> None:
             loaders, org_name, tag_groups
         )
         if not success:
-            LOGGER.error(
+            error(
                 "[scheduler]: error updating portfolio indicators",
-                extra={"extra": {"organization": org_name}},
+                extra={"organization": org_name},
             )
 
         org_tags = await loaders.organization_tags.load(org_name)
