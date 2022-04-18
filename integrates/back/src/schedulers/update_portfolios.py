743eed221f431ce35230203949fdf3f9ea25c678
Jonathan Mesa <jmesa@fluidattacks.com>
2022-04-18 18:14:28 +0000

    integrates\refac(back): #6108.141 update portfolios
    - migrate scheduler to use ew types
    - no-deltas-test

--- integrates/back/src/schedulers/update_portfolios.py
+++ integrates/back/src/schedulers/update_portfolios.py
@@ -4,9 +4,6 @@ from aioextensions import (
 from collections import (
     defaultdict,
 )
-from custom_types import (
-    Group as GroupType,
-)
 from dataloaders import (
     Dataloaders,
     get_new_context,
@@ -14,20 +11,20 @@ from dataloaders import (
 from db_model.findings.types import (
     Finding,
 )
+from db_model.groups.enums import (
+    GroupStateStatus,
+)
+from db_model.groups.types import (
+    Group,
+)
 from decimal import (
     Decimal,
 )
 from findings.domain.core import (
     get_severity_score,
 )
-from groups import (
-    domain as groups_domain,
-)
 import logging
 import logging.config
-from newutils.utils import (
-    get_key_or_fallback,
-)
 from organizations import (
     domain as orgs_domain,
 )
@@ -38,6 +35,7 @@ from tags import (
     domain as tags_domain,
 )
 from typing import (
+    Any,
     cast,
     Dict,
     List,
@@ -50,7 +48,9 @@ LOGGER = logging.getLogger(__name__)
 
 
 def calculate_tag_indicators(
-    tag: str, tags_dict: Dict[str, List[GroupType]], indicator_list: List[str]
+    tag: str,
+    tags_dict: Dict[str, List[Dict[str, Any]]],
+    indicator_list: List[str],
 ) -> Dict[str, Union[Decimal, List[str]]]:
     tag_info: Dict[str, Union[Decimal, List[str]]] = {}
     for indicator in indicator_list:
@@ -86,85 +86,104 @@ def calculate_tag_indicators(
     return tag_info
 
 
+async def get_group_indicators_and_tags(
+    loaders: Dataloaders,
+    group: Group,
+    indicator_list: List[str],
+) -> Dict[str, Any]:
+    unreliable_indicators = await loaders.group_indicators_typed.load(
+        group.name
+    )
+    filtered_indicators = {
+        indicator: getattr(unreliable_indicators, indicator)
+        for indicator in indicator_list
+        if getattr(unreliable_indicators, indicator) is not None
+    }
+
+    # This one is not present in group's unreliable_indicators
+    group_findings: Tuple[Finding, ...] = await loaders.group_findings.load(
+        group.name
+    )
+    filtered_indicators["max_severity"] = Decimal(
+        max(
+            [
+                float(get_severity_score(finding.severity))
+                for finding in group_findings
+            ]
+            if group_findings
+            else [0.0]
+        )
+    ).quantize(Decimal("0.1"))
+
+    filtered_indicators["tag"] = group.tags
+    filtered_indicators["name"] = group.name
+    return filtered_indicators
+
+
 async def update_organization_indicators(
-    loaders: Dataloaders, org_name: str, groups: List[str]
+    loaders: Dataloaders,
+    org_name: str,
+    groups: Tuple[Group, ...],
 ) -> Tuple[bool, List[str]]:
-    group_findings_loader = loaders.group_findings
     success: List[bool] = []
     updated_tags: List[str] = []
     indicator_list: List[str] = [
         "max_open_severity",
+        "max_severity",
         "mean_remediate",
         "mean_remediate_critical_severity",
         "mean_remediate_high_severity",
         "mean_remediate_low_severity",
         "mean_remediate_medium_severity",
-        "last_closing_date",
+        "last_closed_vulnerability_days",
     ]
-    tags_dict: Dict[str, List[GroupType]] = defaultdict(list)
-    groups_attrs = await collect(
-        groups_domain.get_attributes(group, indicator_list + ["tag"])
-        for group in groups
+    groups_indicators = list(
+        await collect(
+            get_group_indicators_and_tags(loaders, group.name, indicator_list)
+            for group in groups
+        )
     )
-    group_findings: Tuple[
-        Tuple[Finding, ...], ...
-    ] = await group_findings_loader.load_many(groups)
-    for index, group in enumerate(groups):
-        groups_attrs[index]["max_severity"] = Decimal(
-            max(
-                [
-                    float(get_severity_score(finding.severity))
-                    for finding in group_findings[index]
-                ]
-                if group_findings[index]
-                else [0.0]
-            )
-        ).quantize(Decimal("0.1"))
-        groups_attrs[index]["name"] = group
-        for tag in groups_attrs[index]["tag"]:
-            tags_dict[tag].append(groups_attrs[index])
+    tags_dict: Dict[str, List[Dict[str, Any]]] = defaultdict(list)
+    for group_indicators in groups_indicators:
+        for tag in group_indicators["tag"]:
+            tags_dict[tag].append(group_indicators)
     for tag in tags_dict:
         updated_tags.append(tag)
-        tag_info = calculate_tag_indicators(
-            tag, tags_dict, indicator_list + ["max_severity"]
-        )
+        tag_info = calculate_tag_indicators(tag, tags_dict, indicator_list)
         success.append(await tags_domain.update(org_name, tag, tag_info))
     return all(success), updated_tags
 
 
 async def update_portfolios() -> None:
-    """Update portfolios metrics"""
+    """Update portfolios metrics."""
     loaders: Dataloaders = get_new_context()
-    group_loader = loaders.group
-    async for _, org_name, org_groups in (
+    async for _, org_name, org_group_names in (
         orgs_domain.iterate_organizations_and_groups()
     ):
-        org_tags = await loaders.organization_tags.load(org_name)
-        org_groups_attrs = await group_loader.load_many(list(org_groups))
-        tag_groups: List[str] = [
-            str(group["name"])
-            for group in org_groups_attrs
-            if get_key_or_fallback(group, "group_status", "project_status")
-            == "ACTIVE"
-            and group["tags"]
-        ]
+        org_groups: Tuple[Group, ...] = await loaders.group_typed.load_many(
+            org_group_names
+        )
+        tag_groups: Tuple[Group, ...] = tuple(
+            group
+            for group in org_groups
+            if group.state.status == GroupStateStatus.ACTIVE and group.tags
+        )
         success, updated_tags = await update_organization_indicators(
             loaders, org_name, tag_groups
         )
-        if success:
-            deleted_tags = [
-                tag["tag"]
-                for tag in org_tags
-                if tag["tag"] not in updated_tags
-            ]
-            await collect(
-                tags_domain.delete(org_name, str(tag)) for tag in deleted_tags
-            )
-        else:
+        if not success:
             LOGGER.error(
                 "[scheduler]: error updating portfolio indicators",
                 extra={"extra": {"organization": org_name}},
             )
+
+        org_tags = await loaders.organization_tags.load(org_name)
+        deleted_tags = [
+            tag["tag"] for tag in org_tags if tag["tag"] not in updated_tags
+        ]
+        await collect(
+            tags_domain.delete(org_name, str(tag)) for tag in deleted_tags
+        )
 
 
 async def main() -> None:
