a7265bca4fab14bba7f283acaae1fb12fc3f9dda
Andres Aguirre <aaguirre@fluidattacks.com>
2022-04-19 21:53:42 +0000

    integrates\feat(back): #6121.15 zr filter statuses const
    - Define a set of statuses to filter zr vulns
    - Redefine the ZR index sort key

--- integrates/back/src/db_model/vulnerabilities/constants.py
+++ integrates/back/src/db_model/vulnerabilities/constants.py
@@ -1,15 +1,27 @@
 from db_model import (
     TABLE,
 )
+from db_model.vulnerabilities.enums import (
+    VulnerabilityZeroRiskStatus,
+)
 from dynamodb.types import (
     Facet,
 )
 
-FILTER_INDEX_METADATA = Facet(
+ZR_FILTER_STATUSES = {
+    VulnerabilityZeroRiskStatus.CONFIRMED,
+    VulnerabilityZeroRiskStatus.REQUESTED,
+}
+
+ZR_INDEX_METADATA = Facet(
     attrs=TABLE.facets["vulnerability_metadata"].attrs,
     pk_alias="FIN#finding_id",
-    sk_alias="VULN#vuln_id#DELETED#is_deleted#ZR#is_zero_risk#STATUS#status",
+    sk_alias=(
+        "VULN#DELETED#is_deleted#ZR#is_zero_risk#STATE#state_status#VERIF#"
+        "verification_status"
+    ),
 )
+
 ROOT_INDEX_METADATA = Facet(
     attrs=TABLE.facets["vulnerability_metadata"].attrs,
     pk_alias="ROOT#root_id",
