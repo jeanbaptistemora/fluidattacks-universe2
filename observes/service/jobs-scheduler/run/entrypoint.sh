8cfe4e8d0202c77793c779b96bdcda873e8fcd0b
Daniel Murcia <dmurcia@fluidattacks.com>
2022-04-20 08:45:51 -0500

    observes\refac(job): #6339.3 trigger jobs
    - trigger missing jobs

--- observes/service/jobs-scheduler/run/entrypoint.sh
+++ observes/service/jobs-scheduler/run/entrypoint.sh
@@ -2,4 +2,8 @@
 alias scheduler="observes-service-jobs-scheduler-bin"
 
 aws_login_prod 'observes' \
-  && scheduler run-job "CHECKLY"
+  && scheduler run-job "MIRROR" \
+  && scheduler run-job "ANNOUNCEKIT" \
+  && scheduler run-job "BUGSNAG" \
+  && scheduler run-job "CHECKLY" \
+  && scheduler run-job "DELIGHTED"
