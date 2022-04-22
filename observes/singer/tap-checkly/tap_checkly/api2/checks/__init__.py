c84dd90ed01721403a9e7cb4ee662f4be3657446
Daniel Murcia <dmurcia@fluidattacks.com>
2022-04-22 17:23:53 -0500

    observes\feat(back): #6340.6 list results
    - add list check results
    - def check result

--- observes/singer/tap-checkly/tap_checkly/api2/checks/__init__.py
+++ observes/singer/tap-checkly/tap_checkly/api2/checks/__init__.py
@@ -1,6 +1,12 @@
 from dataclasses import (
     dataclass,
 )
+from datetime import (
+    datetime,
+)
+from decimal import (
+    Decimal,
+)
 from fa_purity import (
     Cmd,
     FrozenList,
@@ -17,12 +23,39 @@ from tap_checkly.api2._raw import (
 )
 from tap_checkly.api2.checks.core import (
     CheckId,
+    CheckResult,
+    CheckRunId,
 )
 
 
 def _check_id_from_raw(raw: JsonObj) -> CheckId:
     _id = Unfolder(raw["id"]).to_primitive(str).unwrap()
     return CheckId(_id)
+
+
+def _check_result_from_raw(raw: JsonObj) -> CheckResult:
+    return CheckResult(
+        Unfolder(raw["apiCheckResult"]).to_json().unwrap(),
+        Unfolder(raw["browserCheckResult"]).to_json().unwrap(),
+        Unfolder(raw["attempts"]).to_primitive(int).unwrap(),
+        CheckRunId(Unfolder(raw["checkRunId"]).to_primitive(str).unwrap()),
+        datetime.fromisoformat(
+            Unfolder(raw["created_at"]).to_primitive(str).unwrap()
+        ),
+        Unfolder(raw["hasErrors"]).to_primitive(bool).unwrap(),
+        Unfolder(raw["hasFailures"]).to_primitive(bool).unwrap(),
+        Unfolder(raw["isDegraded"]).to_primitive(bool).unwrap(),
+        Unfolder(raw["name"]).to_primitive(str).unwrap(),
+        Unfolder(raw["overMaxResponseTime"]).to_primitive(bool).unwrap(),
+        Unfolder(raw["responseTime"]).to_primitive(Decimal).unwrap(),
+        Unfolder(raw["runLocation"]).to_primitive(str).unwrap(),
+        datetime.fromisoformat(
+            Unfolder(raw["startedAt"]).to_primitive(str).unwrap()
+        ),
+        datetime.fromisoformat(
+            Unfolder(raw["stoppedAt"]).to_primitive(str).unwrap()
+        ),
+    )
 
 
 @dataclass(frozen=True)
@@ -35,3 +68,11 @@ class ChecksClient:
             "/v1/checks",
             from_prim_dict({"limit": self._per_page, "page": page}),
         ).map(lambda l: tuple(map(_check_id_from_raw, l)))
+
+    def list_check_results(
+        self, check: CheckId, page: int
+    ) -> Cmd[FrozenList[CheckResult]]:
+        return self._raw.get_list(
+            "/v1/check-results/" + check.id_str,
+            from_prim_dict({"limit": self._per_page, "page": page}),
+        ).map(lambda l: tuple(map(_check_result_from_raw, l)))
--- observes/singer/tap-checkly/tap_checkly/api2/checks/core.py
+++ observes/singer/tap-checkly/tap_checkly/api2/checks/core.py
@@ -1,8 +1,40 @@
 from dataclasses import (
     dataclass,
 )
+from datetime import (
+    datetime,
+)
+from decimal import (
+    Decimal,
+)
+from fa_purity import (
+    JsonObj,
+)
 
 
 @dataclass(frozen=True)
 class CheckId:
     id_str: str
+
+
+@dataclass(frozen=True)
+class CheckRunId:
+    id_str: str
+
+
+@dataclass(frozen=True)
+class CheckResult:
+    api_result: JsonObj
+    browser_result: JsonObj
+    attempts: int
+    run_id: CheckRunId
+    created_at: datetime
+    has_errors: bool
+    has_failures: bool
+    is_degraded: bool
+    name: str
+    over_max_response_time: bool
+    response_time: Decimal
+    run_location: str
+    started_at: datetime
+    stopped_at: datetime
