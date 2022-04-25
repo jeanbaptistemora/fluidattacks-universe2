781fc633f3c3b36b4c539d501d8d6838b5c386ca
Juan Echeverri <jecheverri@fluidattacks.com>
2022-04-25 07:18:42 -0500

    skims\feat(back): #6160.35 add model f066
    - Add F066 entry to core_model.FindingEnum
    - Add F066 to json config and test files

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -358,6 +358,13 @@ class FindingEnum(Enum):
         execution_queue=ExecutionQueue.http,
         requirements=[75],
     )
+    F066: FindingMetadata = FindingMetadata.new(
+        auto_approve=False,
+        code="F066",
+        cwe=200,
+        execution_queue=ExecutionQueue.none,
+        requirements=[77, 176],
+    )
     F070: FindingMetadata = FindingMetadata.new(
         auto_approve=True,
         code="F070",
