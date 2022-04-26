410ee376565cddd3b357dcff70e7a68d502e7c13
Frank Aristizabal <faristizabal@fluidattacks.com>
2022-04-26 18:32:54 -0500

    airs\refac(front): #6380.1 add navbar
    - Add the upper menu present at the home
    and other valid pages to the 404 error page.

--- airs/front/src/pages/404.tsx
+++ airs/front/src/pages/404.tsx
@@ -3,6 +3,7 @@
 import { Link } from "gatsby";
 import React from "react";
 
+import { NavbarComponent } from "../components/Navbar";
 import {
   ButtonContainer,
   ErrorContainer,
@@ -13,17 +14,20 @@ import {
 } from "../styles/styledComponents";
 
 const Error404Page: React.FC = (): JSX.Element => (
-  <ErrorSection>
-    <ErrorContainer>
-      <ErrorTitle>{"404"}</ErrorTitle>
-      <ErrorDescription>{"Whoops! Nothing Found"}</ErrorDescription>
-      <ButtonContainer>
-        <Link to={"/"}>
-          <RegularRedButton>{"Go Home"}</RegularRedButton>
-        </Link>
-      </ButtonContainer>
-    </ErrorContainer>
-  </ErrorSection>
+  <React.Fragment>
+    <NavbarComponent />
+    <ErrorSection>
+      <ErrorContainer>
+        <ErrorTitle>{"404"}</ErrorTitle>
+        <ErrorDescription>{"Whoops! Nothing Found"}</ErrorDescription>
+        <ButtonContainer>
+          <Link to={"/"}>
+            <RegularRedButton>{"Go Home"}</RegularRedButton>
+          </Link>
+        </ButtonContainer>
+      </ErrorContainer>
+    </ErrorSection>
+  </React.Fragment>
 );
 
 export default Error404Page;
