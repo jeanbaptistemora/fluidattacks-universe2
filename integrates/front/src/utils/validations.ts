377138507e1c5a2665c854ec16c2bf92131635ef
Fabian Moreno <fmoreno@fluidattacks.com>
2022-04-26 20:57:38 -0500

    integrates\refac(front): #6381.1 modify text validation
    - Modified "Entry point" field text validation
    to allow '%' character

--- integrates/front/src/utils/validations.ts
+++ integrates/front/src/utils/validations.ts
@@ -128,7 +128,7 @@ const validTextField: Validator = (value: string): string | undefined => {
     const textMatch: RegExpMatchArray | null =
       // We use them for control character pattern matching.
       // eslint-disable-next-line no-control-regex
-      /[^a-zA-Z0-9ñáéíóúäëïöüÑÁÉÍÓÚÄËÏÖÜ\s(),./:;@_$#*=?-]/u.exec(value);
+      /[^a-zA-Z0-9ñáéíóúäëïöüÑÁÉÍÓÚÄËÏÖÜ\s(),./:;@_$%#*=?-]/u.exec(value);
     if (!_.isNull(textMatch)) {
       return translate.t("validations.invalidTextField", {
         chars: `'${textMatch[0]}'`,
