7428c1f1e33ed0f528d36a144526ff40be9f6428
Karen Camargo <kcamargo@fluidattacks.com>
2022-04-28 13:08:29 -0500

    integrates\refac(front): #6394.1 remove upload file
    - Remove upload file modal
    - no-deltas-test

--- integrates/front/src/scenes/Dashboard/components/Navbar/HelpWidget/index.tsx
+++ integrates/front/src/scenes/Dashboard/components/Navbar/HelpWidget/index.tsx
@@ -1,14 +1,10 @@
-import { useMutation } from "@apollo/client";
-import type { ApolloError } from "@apollo/client";
 import {
   faAngleDown,
   faComment,
   faHeadset,
   faQuestionCircle,
-  faUpload,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import type { GraphQLError } from "graphql";
 import React, { useCallback, useContext, useState } from "react";
 import { openPopupWidget } from "react-calendly";
 import { useDetectClickOutside } from "react-detect-click-outside";
@@ -19,29 +15,9 @@ import { UpgradeGroupsModal } from "./UpgradeGroupsModal";
 
 import { DropdownButton, DropdownMenu, NavbarButton } from "../styles";
 import { clickedPortal } from "../utils";
-import { TooltipWrapper } from "components/TooltipWrapper";
-import { AddFilesBasicModal } from "scenes/Dashboard/components/AddFilesBasicModal";
-import { SIGN_POST_URL_REQUESTER_MUTATION } from "scenes/Dashboard/containers/GroupSettingsView/queries";
 import type { IOrganizationGroups } from "scenes/Dashboard/types";
 import { authContext } from "utils/auth";
-import { Can } from "utils/authz/Can";
-import { Logger } from "utils/logger";
-import { msgError, msgSuccess } from "utils/notifications";
 import { toggleZendesk } from "utils/widgets";
-
-interface IAddFiles {
-  signPostUrlRequester: {
-    url: {
-      url: string;
-      fields: {
-        awsaccesskeyid: string;
-        key: string;
-        policy: string;
-        signature: string;
-      };
-    };
-  };
-}
 
 interface IHelpWidgetProps {
   groups: IOrganizationGroups["groups"];
@@ -101,80 +77,6 @@ export const HelpWidget: React.FC<IHelpWidgetProps> = ({
     }
   }, [groups, match, userEmail, userName]);
 
-  const [isButtonEnabled, setIsButtonEnabled] = useState(false);
-  const disableButton: () => void = useCallback((): void => {
-    setIsButtonEnabled(true);
-  }, []);
-
-  const enableButton: () => void = useCallback((): void => {
-    setIsButtonEnabled(false);
-  }, []);
-
-  const [uploadFile] = useMutation(SIGN_POST_URL_REQUESTER_MUTATION, {
-    onError: ({ graphQLErrors }: ApolloError): void => {
-      graphQLErrors.forEach((error: GraphQLError): void => {
-        msgError(t("groupAlerts.errorTextsad"));
-        Logger.warning("An error occurred uploading group files", error);
-      });
-    },
-    variables: {
-      filesData: userName,
-      groupName: userEmail,
-    },
-  });
-
-  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
-  const openAddModal: () => void = useCallback((): void => {
-    setIsAddModalOpen(true);
-  }, []);
-  const closeAddModal: () => void = useCallback((): void => {
-    setIsAddModalOpen(false);
-  }, []);
-
-  async function handleUpload(values: { file: FileList }): Promise<void> {
-    disableButton();
-    const results = await uploadFile({
-      variables: {
-        filesData: JSON.stringify([
-          {
-            fileName: values.file[0].name,
-          },
-        ]),
-        groupName: userEmail,
-      },
-    });
-
-    const { signPostUrlRequester }: IAddFiles = results.data;
-    const { url } = signPostUrlRequester;
-    const { awsaccesskeyid, key, policy, signature } = url.fields;
-
-    const formData = new FormData();
-    formData.append("acl", "private");
-    formData.append("AWSAccessKeyId", awsaccesskeyid);
-    formData.append("key", key);
-    formData.append("policy", policy);
-    formData.append("signature", signature);
-    formData.append("file", values.file[0], values.file[0].name);
-
-    const response = await fetch(url.url, {
-      body: formData,
-      method: "POST",
-    });
-
-    if (response.ok) {
-      msgSuccess(
-        t("searchFindings.tabResources.success"),
-        t("searchFindings.tabUsers.titleSuccess")
-      );
-    } else {
-      msgError(t("groupAlerts.errorTextsad"));
-      Logger.warning("An error occurred uploading User files", response.json);
-      enableButton();
-    }
-    enableButton();
-    closeAddModal();
-  }
-
   return (
     <div ref={ref}>
       <NavbarButton onClick={toggleDropdown}>
@@ -204,25 +106,6 @@ export const HelpWidget: React.FC<IHelpWidgetProps> = ({
               ) : undefined}
             </li>
           ) : undefined}
-          <Can do={"api_mutations_sign_post_url_requester_mutate"}>
-            <li>
-              <TooltipWrapper
-                id={"uploadFile"}
-                message={t("navbar.uploadFile.tooltip")}
-              >
-                <DropdownButton onClick={openAddModal}>
-                  <FontAwesomeIcon icon={faUpload} />
-                  {t("navbar.uploadFile.text")}
-                </DropdownButton>
-                <AddFilesBasicModal
-                  isOpen={isAddModalOpen}
-                  isUploading={isButtonEnabled}
-                  onClose={closeAddModal}
-                  onSubmit={handleUpload}
-                />
-              </TooltipWrapper>
-            </li>
-          </Can>
         </DropdownMenu>
       ) : undefined}
     </div>
