52cfda66b6ceb8aaad08236b2ea9665e97cdcf68
Juan Echeverri <jecheverri@fluidattacks.com>
2022-05-02 12:45:14 -0500

    skims\refac(back): #6409.4 refac f211
    - Refac F211 cs check to search objects more safely and efficiently

--- skims/skims/lib_root/f211/c_sharp.py
+++ skims/skims/lib_root/f211/c_sharp.py
@@ -1,7 +1,6 @@
-import itertools
 from lib_root.utilities.c_sharp import (
     get_variable_attribute,
-    yield_object_creation,
+    yield_shard_object_creation,
 )
 from lib_sast.types import (
     ShardDb,
@@ -28,46 +27,44 @@ def vuln_regular_expression(
     shard_db: ShardDb,  # pylint: disable=unused-argument
     graph_db: graph_model.GraphDB,
 ) -> core_model.Vulnerabilities:
+    c_sharp = graph_model.GraphShardMetadataLanguage.CSHARP
+
     def n_ids() -> graph_model.GraphShardNodes:
-        regex_methods = {"IsMatch", "Match", "Matches"}
-        for shard, node in itertools.chain(
-            yield_object_creation(
-                graph_db,
-                {
-                    "Regex",
-                },
-            ),
-        ):
-            _object = g.match_ast(
-                shard.graph, g.pred_ast(shard.graph, node, depth=2)[1]
-            )["__0__"]
-            name_object = shard.graph.nodes[_object].get("label_text")
-            for member in g.filter_nodes(
-                shard.graph,
-                nodes=shard.graph.nodes,
-                predicate=g.pred_has_labels(
-                    label_type="member_access_expression"
-                ),
-            ):
-                expression = node_to_str(shard.graph, member).split(".")
-                if (
-                    expression[0] == name_object
-                    and expression[1] in regex_methods
+        for shard in graph_db.shards_by_language(c_sharp):
+
+            regex_methods = {"IsMatch", "Match", "Matches"}
+
+            for node in yield_shard_object_creation(shard, {"Regex"}):
+                _object = g.match_ast(
+                    shard.graph, g.pred_ast(shard.graph, node, depth=2)[1]
+                )["__0__"]
+                name_object = shard.graph.nodes[_object].get("label_text")
+                for member in g.filter_nodes(
+                    shard.graph,
+                    nodes=shard.graph.nodes,
+                    predicate=g.pred_has_labels(
+                        label_type="member_access_expression"
+                    ),
                 ):
-                    invocation = g.pred_ast(shard.graph, member)[0]
-                    regex_param = g.get_ast_childs(
-                        shard.graph,
-                        invocation,
-                        "verbatim_string_literal",
-                        depth=3,
-                    )
-                    var_param = g.get_ast_childs(
-                        shard.graph, invocation, "identifier", depth=3
-                    )
-                    if regex_param or (
-                        var_param and vuln_attributes(shard, var_param)
+                    expression = node_to_str(shard.graph, member).split(".")
+                    if (
+                        expression[0] == name_object
+                        and expression[1] in regex_methods
                     ):
-                        yield shard, invocation
+                        invocation = g.pred_ast(shard.graph, member)[0]
+                        regex_param = g.get_ast_childs(
+                            shard.graph,
+                            invocation,
+                            "verbatim_string_literal",
+                            depth=3,
+                        )
+                        var_param = g.get_ast_childs(
+                            shard.graph, invocation, "identifier", depth=3
+                        )
+                        if regex_param or (
+                            var_param and vuln_attributes(shard, var_param)
+                        ):
+                            yield shard, invocation
 
     def vuln_attributes(
         shard: graph_model.GraphShard,
