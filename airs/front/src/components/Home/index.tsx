55f353c368d6bd8efb0583b0d582e0b79f0e5653
Frank Aristizabal <faristizabal@fluidattacks.com>
2022-05-04 17:51:07 -0500

    airs\refac(front): #6421.7 clean up code
    - Delete unused components.
    - no-deltas-test

--- airs/front/src/components/Home/index.tsx
+++ airs/front/src/components/Home/index.tsx
@@ -3,7 +3,6 @@ import { Helmet } from "react-helmet";
 
 import { ClientsSection } from "./ClientsSection";
 import { ContactSection } from "./ContactSection";
-import { EbookSection } from "./EbookSection";
 import { QualitySection } from "./QualitySection";
 
 const Home: React.FC = (): JSX.Element => (
@@ -14,7 +13,6 @@ const Home: React.FC = (): JSX.Element => (
         name={"facebook-domain-verification"}
       />
     </Helmet>
-    <EbookSection />
     <ClientsSection />
     <ContactSection />
     <QualitySection />
