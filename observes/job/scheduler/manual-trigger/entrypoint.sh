7dedda2097d02e4b4400d88790cc7d84d582fd5b
Daniel Murcia <dmurcia@fluidattacks.com>
2022-05-06 10:32:19 -0500

    observes\refac(job): #6436.4 trigger jobs
    - trigger upload jobs

--- observes/job/scheduler/manual-trigger/entrypoint.sh
+++ observes/job/scheduler/manual-trigger/entrypoint.sh
@@ -3,6 +3,4 @@
 aws_login_prod 'observes' \
   && sops_export_vars 'observes/secrets/prod.yaml' \
     bugsnag_notifier_key \
-  && observes-scheduler run-job "MAILCHIMP_ETL" \
-  && observes-scheduler run-job "MIRROR" \
-  && observes-scheduler run-job "DYNAMO_INTEGRATES_MAIN"
+  && observes-scheduler run-job "UPLOAD"
