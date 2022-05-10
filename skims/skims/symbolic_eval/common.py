e3386efedfef66d194f608a2016a6538c1759a83
Juan Echeverri <jecheverri@fluidattacks.com>
2022-05-10 15:52:43 -0500

    skims\feat(back): #5999.45 extra validation f008, f098 cs
    - Add extra validation to cover all the cases of http inputs

---
+++ skims/skims/symbolic_eval/common.py
@@ -0,0 +1,11 @@
+from typing import (
+    Set,
+)
+
+HTTP_INPUTS: Set[str] = {
+    "Request.Params",
+    "Request.Querystring",
+    "Request.Form",
+    "Request.Cookies",
+    "Request.ServerVariables",
+}
--- skims/skims/symbolic_eval/f098/member_access/c_sharp.py
+++ skims/skims/symbolic_eval/f098/member_access/c_sharp.py
@@ -1,3 +1,6 @@
+from symbolic_eval.common import (
+    HTTP_INPUTS,
+)
 from symbolic_eval.types import (
     SymbolicEvalArgs,
     SymbolicEvaluation,
@@ -7,8 +10,5 @@ from symbolic_eval.types import (
 def evaluate(args: SymbolicEvalArgs) -> SymbolicEvaluation:
     ma_attr = args.graph.nodes[args.n_id]
     member_access = f'{ma_attr["expression"]}.{ma_attr["member"]}'
-    args.evaluation[args.n_id] = member_access in {
-        "Request.Form",
-        "Request.Params",
-    }
+    args.evaluation[args.n_id] = member_access in HTTP_INPUTS
     return SymbolicEvaluation(args.evaluation[args.n_id], args.triggers)
