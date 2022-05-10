dffe65d6cdc5060c090021e6040121750f7eadf5
Jaime Hurtado <jhurtado@fluidattacks.com>
2022-05-10 14:45:27 -0500

    integrates\rever(front): #6452 welcome view
    - disabled welcome view until autoenrollment release
    - no-deltas-test

--- integrates/front/src/scenes/Welcome/index.tsx
+++ integrates/front/src/scenes/Welcome/index.tsx
@@ -1,15 +1,10 @@
 import { useMutation, useQuery } from "@apollo/client";
 import React, { useCallback, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 
 import { AUTOENROLL_DEMO, GET_USER_WELCOME } from "./queries";
-import { Container } from "./styles";
-import { Tour } from "./Tour";
 import type { IAutoenrollDemoResult, IGetUserWelcomeResult } from "./types";
 
-import { Card, CardBody, CardHeader } from "components/Card";
-import { Col, Row } from "components/Layout";
 import { Dashboard } from "scenes/Dashboard";
 import { Logger } from "utils/logger";
 import { msgError } from "utils/notifications";
@@ -18,11 +13,6 @@ const Welcome: React.FC = (): JSX.Element => {
   const { t } = useTranslation();
 
   const [browsingDemo, setBrowsingDemo] = useState(false);
-
-  const { push } = useHistory();
-  const goToTour = useCallback((): void => {
-    push("/welcome/tour");
-  }, [push]);
 
   const { data, loading } = useQuery<IGetUserWelcomeResult>(GET_USER_WELCOME, {
     onError: (error): void => {
@@ -72,46 +62,9 @@ const Welcome: React.FC = (): JSX.Element => {
   const isFirstTimeUser = organizations.length === 0;
 
   if (isFirstTimeUser && !browsingDemo) {
-    return (
-      <Container>
-        <Switch>
-          <Route exact={true} path={"/welcome"}>
-            <Row>
-              <Col>
-                <Row justify={"center"}>
-                  <Col>
-                    <h1>{t("welcome.title")}</h1>
-                    <h2>{t("welcome.subtitle")}</h2>
-                  </Col>
-                </Row>
-                <Row justify={"center"}>
-                  <Col large={"30"} medium={"30"} small={"30"}>
-                    <Card onClick={goToTour}>
-                      <CardHeader>{t("welcome.tour.title")}</CardHeader>
-                      <CardBody>
-                        <p>{t("welcome.tour.description")}</p>
-                      </CardBody>
-                    </Card>
-                  </Col>
-                  <Col large={"30"} medium={"30"} small={"30"}>
-                    <Card onClick={goToDemo}>
-                      <CardHeader>{t("welcome.demo.title")}</CardHeader>
-                      <CardBody>
-                        <p>{t("welcome.demo.description")}</p>
-                      </CardBody>
-                    </Card>
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-          </Route>
-          <Route exact={true} path={"/welcome/tour"}>
-            <Tour />
-          </Route>
-          <Redirect to={"/welcome"} />
-        </Switch>
-      </Container>
-    );
+    goToDemo();
+
+    return <div />;
   }
 
   return <Dashboard />;
