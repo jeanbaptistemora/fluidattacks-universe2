6277f1d06f9d2661193bd320e81b7a3cde626d46
Daniel Salazar <dsalazar@fluidattacks.com>
2022-05-09 19:00:30 -0500

    common\feat(build): #5231 aws login
    - Add required aws logins

--- common/users/infra/rotate/keys/entrypoint.sh
+++ common/users/infra/rotate/keys/entrypoint.sh
@@ -3,13 +3,15 @@
 function main {
   local day="${1:-}"
 
-  if [ "${day}" = "even" ]; then
-    taint-terraform-for-commonUsersKeys1
-  elif [ "${day}" = "odd" ]; then
-    taint-terraform-for-commonUsersKeys2
-  else
-    error "Either 'even' or 'odd' must be passed."
-  fi \
+  aws_login_prod "common" \
+    && if [ "${day}" = "even" ]; then
+      taint-terraform-for-commonUsersKeys1
+    elif [ "${day}" = "odd" ]; then
+      taint-terraform-for-commonUsersKeys2
+    else
+      error "Either 'even' or 'odd' must be passed."
+    fi \
+    && aws_login_prod "integrates" \
     && integrates-back-deploy-prod "${day}"
 }
 
--- common/users/infra/rotate/keys/main.nix
+++ common/users/infra/rotate/keys/main.nix
@@ -1,17 +1,18 @@
 {
   makeScript,
   outputs,
-  inputs,
   ...
 }:
 makeScript {
   name = "common-users-infra-rotate-keys";
   searchPaths = {
     bin = [
-      inputs.nixpkgs.which
       outputs."/integrates/back/deploy/prod"
       outputs."/taintTerraform/commonUsersKeys1"
       outputs."/taintTerraform/commonUsersKeys2"
+    ];
+    source = [
+      outputs."/common/utils/aws"
     ];
   };
   entrypoint = ./entrypoint.sh;
