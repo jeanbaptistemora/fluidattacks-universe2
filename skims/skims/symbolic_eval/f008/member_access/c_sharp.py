d7c3735955e2c2d25506b1ed2a77702704d74b93
Juan Echeverri <jecheverri@fluidattacks.com>
2022-05-10 10:38:29 -0500

    skims\feat(back): #5999.41 extra validation f008 cs
    - Add extra validation to cover the cases of Request.Parameters

--- skims/skims/symbolic_eval/f008/member_access/c_sharp.py
+++ skims/skims/symbolic_eval/f008/member_access/c_sharp.py
@@ -7,5 +7,8 @@ from symbolic_eval.types import (
 def evaluate(args: SymbolicEvalArgs) -> SymbolicEvaluation:
     ma_attr = args.graph.nodes[args.n_id]
     member_access = f'{ma_attr["expression"]}.{ma_attr["member"]}'
-    args.evaluation[args.n_id] = member_access == "Request.Form"
+    args.evaluation[args.n_id] = member_access in {
+        "Request.Form",
+        "Request.Params",
+    }
     return SymbolicEvaluation(args.evaluation[args.n_id], args.triggers)
