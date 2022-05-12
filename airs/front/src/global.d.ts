e8c09bcf1935694207677c94f24db10dad99e9ef
Frank Aristizabal <faristizabal@fluidattacks.com>
2022-05-12 13:37:33 -0500

    airs\rever(front): #6442.6 add mdx
    - Delete MDX types.

--- airs/front/src/global.d.ts
+++ airs/front/src/global.d.ts
@@ -44,44 +44,6 @@ interface IQueryData {
       };
       rawMarkdownBody: string;
     };
-    mdx: {
-      body: string;
-      fields: {
-        slug: string;
-      };
-      frontmatter: {
-        advise: string;
-        alt: string;
-        author: string;
-        authors: string;
-        banner: string;
-        category: string;
-        certificationid: string;
-        certificationsindex: string;
-        clientsindex: string;
-        codename: string;
-        cveid: string;
-        date: string;
-        defaux: string;
-        definition: string;
-        description: string;
-        image: string;
-        keywords: string;
-        partnersindex: string;
-        phrase: string;
-        product: string;
-        slug: string;
-        solution: string;
-        subtext: string;
-        subtitle: string;
-        product: string;
-        tags: string;
-        template: string;
-        title: string;
-        writer: string;
-      };
-      rawBody: string;
-    };
     site: {
       siteMetadata: {
         author: string;
@@ -118,46 +80,6 @@ interface IData {
             slug: string;
           };
           html: string;
-          frontmatter: {
-            advise: string;
-            alt: string;
-            author: string;
-            authors: string;
-            category: string;
-            certification: string;
-            certificationidd: string;
-            certificationlogo: string;
-            client: string;
-            clientlogo: string;
-            codename: string;
-            cveid: string;
-            date: string;
-            description: string;
-            filter: string;
-            image: string;
-            partner: string;
-            partnerlogo: string;
-            product: string;
-            severity: string;
-            slug: string;
-            spanish: string;
-            subtitle: string;
-            tags: string;
-            title: string;
-            writer: string;
-          };
-        };
-      }
-    ];
-  };
-  allMdx: {
-    edges: [
-      {
-        node: {
-          body: string;
-          fields: {
-            slug: string;
-          };
           frontmatter: {
             advise: string;
             alt: string;
