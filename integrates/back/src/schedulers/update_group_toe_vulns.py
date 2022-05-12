b89ba0fa65ac224e7dd02e57529ee10f197363fb
Andres Aguirre <aaguirre@fluidattacks.com>
2022-05-12 19:09:39 +0000

    integrates\feat(back): #6475.2 match entry point
    - Start to match vulnerability specific that starts
    with an entry point for vulnerabilities of input type

--- integrates/back/src/schedulers/update_group_toe_vulns.py
+++ integrates/back/src/schedulers/update_group_toe_vulns.py
@@ -104,7 +104,7 @@ async def process_toe_inputs(
             # ToeInput is not associated to a root_id
             # and vulnerability.root_id == toe_input.root_id
             and vulnerability.where.startswith(toe_input.component)
-            and vulnerability.specific == toe_input.entry_point
+            and vulnerability.specific.startswith(toe_input.entry_point)
             for vulnerability in vulnerabilities
         )
 
