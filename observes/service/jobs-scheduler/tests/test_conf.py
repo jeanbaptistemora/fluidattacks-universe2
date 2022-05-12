bd542bf457a4ce1b2a9b80b59b7505d90f37e668
Daniel Murcia <dmurcia@fluidattacks.com>
2022-05-12 10:27:00 -0500

    observes\test(back): #6474.3 schedule
    - add schedule test
    - refac version test

---
+++ observes/service/jobs-scheduler/tests/test_conf.py
@@ -0,0 +1,7 @@
+from jobs_scheduler.conf import (
+    SCHEDULE,
+)
+
+
+def test_schedule() -> None:
+    assert SCHEDULE
--- observes/service/jobs-scheduler/tests/test_version.py
+++ observes/service/jobs-scheduler/tests/test_version.py
@@ -1,6 +1,3 @@
-from importlib.metadata import (
-    version,
-)
 from jobs_scheduler import (
     __version__,
 )
@@ -11,4 +8,3 @@ def test_version() -> None:
     metadata = toml.load("./pyproject.toml")  # type: ignore[misc]
     current: str = metadata["tool"]["poetry"]["version"]  # type: ignore[misc]
     assert __version__ == current
-    assert version("jobs_scheduler") == current
