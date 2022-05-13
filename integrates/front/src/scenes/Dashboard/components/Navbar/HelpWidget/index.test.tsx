a7b1841a258a774159b67f12752a8178874103eb
Sebastian Lizcano <slizcano@fluidattacks.com>
2022-05-13 09:17:32 -0500

    integrates\test(front): #5965.5 add basic unit test
    - add basic unit test to verify which options are displayed

---
+++ integrates/front/src/scenes/Dashboard/components/Navbar/HelpWidget/index.test.tsx
@@ -0,0 +1,106 @@
+import { PureAbility } from "@casl/ability";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import React from "react";
+import { MemoryRouter, Route } from "react-router-dom";
+
+import { HelpModal } from "scenes/Dashboard/components/Navbar/HelpWidget/modal";
+import type { IOrganizationGroups } from "scenes/Dashboard/types";
+import { authContext } from "utils/auth";
+import { authzPermissionsContext } from "utils/authz/config";
+
+describe("HelpModal", (): void => {
+  const groups: IOrganizationGroups["groups"] = [
+    {
+      name: "unittesting",
+      permissions: [],
+      serviceAttributes: ["has_squad", "is_continuous"],
+    },
+  ];
+
+  it("should return a function", (): void => {
+    expect.hasAssertions();
+    expect(typeof HelpModal).toBe("function");
+  });
+
+  it("should render", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    jest.clearAllMocks();
+
+    render(
+      <authzPermissionsContext.Provider value={new PureAbility([])}>
+        <MemoryRouter initialEntries={["/orgs/okada/groups"]}>
+          <authContext.Provider
+            value={{
+              tours: {
+                newGroup: true,
+                newRoot: true,
+              },
+              userEmail: "test@fluidattacks.com",
+              userName: "",
+            }}
+          >
+            <Route path={"/orgs/:orgName/groups"}>
+              <HelpModal groups={groups} />
+            </Route>
+          </authContext.Provider>
+        </MemoryRouter>
+      </authzPermissionsContext.Provider>
+    );
+    await waitFor((): void => {
+      expect(screen.queryAllByRole("button")).toHaveLength(1);
+    });
+
+    userEvent.click(screen.getAllByRole("button")[0]);
+
+    await waitFor((): void => {
+      expect(screen.queryAllByRole("button")).toHaveLength(2);
+    });
+
+    userEvent.type(screen.getByText("navbar.help.support"), "{esc}");
+
+    await waitFor((): void => {
+      expect(screen.queryAllByRole("button")).toHaveLength(1);
+    });
+  });
+
+  it("should render talk expert too", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    jest.clearAllMocks();
+
+    render(
+      <authzPermissionsContext.Provider value={new PureAbility([])}>
+        <MemoryRouter
+          initialEntries={["/orgs/okada/groups/unittesting/events"]}
+        >
+          <authContext.Provider
+            value={{
+              tours: {
+                newGroup: true,
+                newRoot: true,
+              },
+              userEmail: "test@fluidattacks.com",
+              userName: "",
+            }}
+          >
+            <Route path={"/orgs/:orgName/groups/:groupName/events"}>
+              <HelpModal groups={groups} />
+            </Route>
+          </authContext.Provider>
+        </MemoryRouter>
+      </authzPermissionsContext.Provider>
+    );
+
+    await waitFor((): void => {
+      expect(screen.queryAllByRole("button")).toHaveLength(1);
+    });
+
+    userEvent.click(screen.getAllByRole("button")[0]);
+
+    await waitFor((): void => {
+      expect(screen.queryAllByRole("button")).toHaveLength(3);
+    });
+  });
+});
