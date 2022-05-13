eb86ff1f9ddf67636fef607f8449afa572277f5a
Juan Echeverri <jecheverri@fluidattacks.com>
2022-05-13 10:21:12 -0500

    skims\feat(back): #5999.57 f016 cs test
    - Add test to lib_path/f016
    - Update lib_path.csv

--- skims/skims/lib_root/f016/__init__.py
+++ skims/skims/lib_root/f016/__init__.py
@@ -1,4 +1,5 @@
 from lib_root.f016.c_sharp import (
+    service_point_manager_disabled as c_sharp_service_point_manager_disabled,
     weak_protocol as c_sharp_weak_protocol,
 )
 from model import (
@@ -7,4 +8,7 @@ from model import (
 )
 
 FINDING: core_model.FindingEnum = core_model.FindingEnum.F016
-QUERIES: graph_model.Queries = ((FINDING, c_sharp_weak_protocol),)
+QUERIES: graph_model.Queries = (
+    (FINDING, c_sharp_weak_protocol),
+    (FINDING, c_sharp_service_point_manager_disabled),
+)
