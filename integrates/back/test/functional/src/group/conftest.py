20d17541dce0fff186409a4f1be57c77b0b8e4ea
Sebastian Lizcano <slizcano@fluidattacks.com>
2022-05-19 13:04:53 -0500

    integrates\test(back): #6501.3 update functional test
    - update functional test by merging generic and conftest data to
    avoid posible error when populating data

--- integrates/back/test/functional/src/group/conftest.py
+++ integrates/back/test/functional/src/group/conftest.py
@@ -2,6 +2,9 @@
 from back.test import (
     db,
 )
+from collections import (
+    defaultdict,
+)
 from db_model.enums import (
     GitCloningStatus,
     Source,
@@ -615,4 +618,10 @@ async def populate(generic_data: dict[str, Any]) -> bool:
             },
         ],
     }
-    return await db.populate({**generic_data["db_data"], **data})
+
+    merge_dict = defaultdict(list)
+    for dict_data in (generic_data["db_data"], data):
+        for key, value in dict_data.items():
+            merge_dict[key].extend(value)
+
+    return await db.populate(merge_dict)
