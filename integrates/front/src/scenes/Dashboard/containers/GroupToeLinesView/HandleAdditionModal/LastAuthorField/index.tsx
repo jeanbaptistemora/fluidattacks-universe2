ea436e838e7612eac3f588bd21b7c778a910292b
Andres Aguirre <aaguirre@fluidattacks.com>
2022-05-19 16:09:07 -0500

    integrates\feat(front): #6431.17 last author field
    - Add field to enter the last commit author

---
+++ integrates/front/src/scenes/Dashboard/containers/GroupToeLinesView/HandleAdditionModal/LastAuthorField/index.tsx
@@ -0,0 +1,27 @@
+import { Field } from "formik";
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+import { ControlLabel, FormGroup } from "styles/styledComponents";
+import { FormikText } from "utils/forms/fields";
+import { composeValidators, required, validEmail } from "utils/validations";
+
+const LastAuthorField: React.FC = (): JSX.Element => {
+  const { t } = useTranslation();
+
+  return (
+    <FormGroup>
+      <ControlLabel>
+        <b>{t("group.toe.lines.addModal.fields.lastAuthor")} </b>
+      </ControlLabel>
+      <Field
+        component={FormikText}
+        name={"lastAuthor"}
+        type={"text"}
+        validate={composeValidators([required, validEmail])}
+      />
+    </FormGroup>
+  );
+};
+
+export { LastAuthorField };
