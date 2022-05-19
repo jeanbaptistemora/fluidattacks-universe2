f5bcbf889a3ffce5f46253dbedc500eb91593ca6
Andres Aguirre <aaguirre@fluidattacks.com>
2022-05-19 16:45:21 -0500

    integrates\feat(front): #6431.18 lines of code field
    - Add field to enter the lines of code

---
+++ integrates/front/src/scenes/Dashboard/containers/GroupToeLinesView/HandleAdditionModal/LinesOfCodeField/index.tsx
@@ -0,0 +1,44 @@
+import { Field } from "formik";
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+import { ControlLabel, FormGroup } from "styles/styledComponents";
+import { FormikText } from "utils/forms/fields";
+import {
+  composeValidators,
+  isOptionalInteger,
+  isPositive,
+  required,
+} from "utils/validations";
+
+const LinesOfCodeField: React.FC = (): JSX.Element => {
+  const { t } = useTranslation();
+  function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>): void {
+    if (
+      event.key.length > 1 ||
+      /\d/u.test(event.key) ||
+      event.key === "Control" ||
+      event.key.toLocaleLowerCase() === "v"
+    )
+      return;
+    event.preventDefault();
+  }
+
+  return (
+    <FormGroup>
+      <ControlLabel>
+        <b>{t("group.toe.lines.addModal.fields.loc")} </b>
+      </ControlLabel>
+      <Field
+        component={FormikText}
+        customKeyDown={handleKeyDown}
+        name={"loc"}
+        step={"1"}
+        type={"number"}
+        validate={composeValidators([required, isOptionalInteger, isPositive])}
+      />
+    </FormGroup>
+  );
+};
+
+export { LinesOfCodeField };
