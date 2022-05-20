50ea14c9f67d375e39d56c33e5652dd36bdb793f
Andres Aguirre <aaguirre@fluidattacks.com>
2022-05-20 12:14:30 -0500

    integrates\feat(front): #6431.21 filename field
    - Add field to enter the filename

---
+++ integrates/front/src/scenes/Dashboard/containers/GroupToeLinesView/HandleAdditionModal/FilenameField/index.tsx
@@ -0,0 +1,27 @@
+import { Field } from "formik";
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+import { ControlLabel, FormGroup } from "styles/styledComponents";
+import { FormikTextArea } from "utils/forms/fields";
+import { composeValidators, required, validTextField } from "utils/validations";
+
+const FilenameField: React.FC = (): JSX.Element => {
+  const { t } = useTranslation();
+
+  return (
+    <FormGroup>
+      <ControlLabel>
+        <b>{t("group.toe.lines.addModal.fields.filename")} </b>
+      </ControlLabel>
+      <Field
+        component={FormikTextArea}
+        name={"filename"}
+        type={"text"}
+        validate={composeValidators([required, validTextField])}
+      />
+    </FormGroup>
+  );
+};
+
+export { FilenameField };
