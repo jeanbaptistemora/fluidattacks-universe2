56d50f09e73b7cbb8225018d49ba1153febd4662
David Acevedo <dacevedo@fluidattacks.com>
2022-05-23 13:56:11 -0500

    integrates\feat(front): #6514 vulns table
    - added basic table to display vulns

--- integrates/front/src/scenes/Dashboard/containers/GroupVulnerabilitiesView/index.tsx
+++ integrates/front/src/scenes/Dashboard/containers/GroupVulnerabilitiesView/index.tsx
@@ -3,11 +3,37 @@ import { useParams } from "react-router-dom";
 
 import { useGroupVulnerabilities } from "./hooks";
 
+import { Table } from "components/Table";
+import type { IHeaderConfig } from "components/Table/types";
+
+const tableHeaders: IHeaderConfig[] = [
+  {
+    dataField: "where",
+    header: "Where",
+    wrapped: true,
+  },
+  {
+    dataField: "specific",
+    header: "Specific",
+  },
+];
+
 const GroupVulnerabilitiesView: React.FC = (): JSX.Element => {
   const { groupName } = useParams<{ groupName: string }>();
   const vulnerabilities = useGroupVulnerabilities(groupName);
 
-  return <div>{JSON.stringify(vulnerabilities)}</div>;
+  return (
+    <div>
+      <Table
+        dataset={vulnerabilities}
+        exportCsv={false}
+        headers={tableHeaders}
+        id={"tblVulnerabilities"}
+        pageSize={10}
+        search={true}
+      />
+    </div>
+  );
 };
 
 export { GroupVulnerabilitiesView };
