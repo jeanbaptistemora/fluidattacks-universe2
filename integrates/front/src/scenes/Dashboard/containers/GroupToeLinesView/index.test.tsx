476ae13860cafb87c2b162bb506c2d2f009fbbf0
Andres Aguirre <aaguirre@fluidattacks.com>
2022-05-24 10:25:12 -0500

    integrates\test(front): #6454.4 handle verify action
    - Should handle verify lines

--- integrates/front/src/scenes/Dashboard/containers/GroupToeLinesView/index.test.tsx
+++ integrates/front/src/scenes/Dashboard/containers/GroupToeLinesView/index.test.tsx
@@ -2,15 +2,27 @@ import { MockedProvider } from "@apollo/client/testing";
 import type { MockedResponse } from "@apollo/client/testing";
 import { PureAbility } from "@casl/ability";
 import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import React from "react";
 import { MemoryRouter, Route } from "react-router-dom";
 
-import { GET_TOE_LINES } from "./queries";
+import { GET_TOE_LINES, VERIFY_TOE_LINES } from "./queries";
 
 import { GroupToeLinesView } from ".";
 import { authzPermissionsContext } from "utils/authz/config";
+import { msgSuccess } from "utils/notifications";
 
-describe("GroupToeLinesView", (): void => {
+jest.mock("../../../../utils/notifications", (): Dictionary => {
+  const mockedNotifications: Dictionary<() => Dictionary> = jest.requireActual(
+    "../../../../utils/notifications"
+  );
+  jest.spyOn(mockedNotifications, "msgError").mockImplementation();
+  jest.spyOn(mockedNotifications, "msgSuccess").mockImplementation();
+
+  return mockedNotifications;
+});
+
+describe("groupToeLinesView", (): void => {
   it("should return a function", (): void => {
     expect.hasAssertions();
     expect(typeof GroupToeLinesView).toBe("function");
@@ -161,5 +173,157 @@ describe("GroupToeLinesView", (): void => {
         "comment 1",
       ].join("")
     );
+  });
+
+  it("should handle verify lines", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    jest.clearAllMocks();
+
+    const mocksMutation: MockedResponse[] = [
+      {
+        request: {
+          query: VERIFY_TOE_LINES,
+          variables: {
+            filename: "test/test#.config",
+            groupName: "unittesting",
+            rootId: "63298a73-9dff-46cf-b42d-9b2f01a56690",
+          },
+        },
+        result: { data: { updateToeLinesAttackedLines: { success: true } } },
+      },
+      {
+        request: {
+          query: VERIFY_TOE_LINES,
+          variables: {
+            filename: "test2/test.sh",
+            groupName: "unittesting",
+            rootId: "765b1d0f-b6fb-4485-b4e2-2c2cb1555b1a",
+          },
+        },
+        result: { data: { updateToeLinesAttackedLines: { success: true } } },
+      },
+    ];
+    const mockedToeLines: MockedResponse = {
+      request: {
+        query: GET_TOE_LINES,
+        variables: {
+          canGetAttackedAt: true,
+          canGetAttackedBy: true,
+          canGetAttackedLines: true,
+          canGetBePresentUntil: true,
+          canGetComments: true,
+          canGetFirstAttackAt: true,
+          first: 150,
+          groupName: "unittesting",
+        },
+      },
+      result: {
+        data: {
+          group: {
+            __typename: "Group",
+            name: "unittesting",
+            toeLines: {
+              __typename: "ToeLinesConnection",
+              edges: [
+                {
+                  node: {
+                    attackedAt: "2021-02-20T05:00:00+00:00",
+                    attackedBy: "test2@test.com",
+                    attackedLines: 4,
+                    bePresent: true,
+                    bePresentUntil: "",
+                    comments: "comment 1",
+                    filename: "test/test#.config",
+                    firstAttackAt: "2020-02-19T15:41:04+00:00",
+                    hasVulnerabilities: false,
+                    lastAuthor: "user@gmail.com",
+                    lastCommit: "f9e4beba70c4f34d6117c3b0c23ebe6b2bff66c2",
+                    loc: 8,
+                    modifiedDate: "2020-11-15T15:41:04+00:00",
+                    root: {
+                      id: "63298a73-9dff-46cf-b42d-9b2f01a56690",
+                      nickname: "product",
+                    },
+                    seenAt: "2020-02-01T15:41:04+00:00",
+                    sortsRiskLevel: 80,
+                    sortsSuggestions: null,
+                  },
+                },
+                {
+                  node: {
+                    attackedAt: "",
+                    attackedBy: "test@test.com",
+                    attackedLines: 120,
+                    bePresent: false,
+                    bePresentUntil: "2021-01-01T15:41:04+00:00",
+                    comments: "comment 2",
+                    filename: "test2/test.sh",
+                    firstAttackAt: "",
+                    hasVulnerabilities: true,
+                    lastAuthor: "user@gmail.com",
+                    lastCommit: "f9e4beba70c4f34d6117c3b0c23ebe6b2bff66c1",
+                    loc: 172,
+                    modifiedDate: "2020-11-16T15:41:04+00:00",
+                    root: {
+                      id: "765b1d0f-b6fb-4485-b4e2-2c2cb1555b1a",
+                      nickname: "integrates_1",
+                    },
+                    seenAt: "2020-01-01T15:41:04+00:00",
+                    sortsRiskLevel: 0,
+                    sortsSuggestions: null,
+                  },
+                },
+              ],
+              pageInfo: {
+                endCursor: "bnVsbA==",
+                hasNextPage: false,
+              },
+            },
+          },
+        },
+      },
+    };
+    const mockedPermissions: PureAbility<string> = new PureAbility([
+      { action: "api_resolvers_toe_lines_attacked_at_resolve" },
+      { action: "api_resolvers_toe_lines_attacked_by_resolve" },
+      { action: "api_resolvers_toe_lines_attacked_lines_resolve" },
+      { action: "api_resolvers_toe_lines_be_present_until_resolve" },
+      { action: "api_resolvers_toe_lines_comments_resolve" },
+      { action: "api_resolvers_toe_lines_first_attack_at_resolve" },
+      { action: "api_mutations_update_toe_lines_attacked_lines_mutate" },
+    ]);
+    render(
+      <MemoryRouter initialEntries={["/unittesting/surface/lines"]}>
+        <MockedProvider
+          addTypename={true}
+          mocks={[mockedToeLines, ...mocksMutation]}
+        >
+          <authzPermissionsContext.Provider value={mockedPermissions}>
+            <Route path={"/:groupName/surface/lines"}>
+              <GroupToeLinesView isInternal={true} />
+            </Route>
+          </authzPermissionsContext.Provider>
+        </MockedProvider>
+      </MemoryRouter>
+    );
+    const numberOfRows: number = 3;
+    await waitFor((): void => {
+      expect(screen.queryAllByRole("row")).toHaveLength(numberOfRows);
+    });
+
+    userEvent.click(screen.getAllByRole("checkbox")[1]);
+    userEvent.click(screen.getAllByRole("checkbox")[2]);
+
+    userEvent.click(
+      screen.getByText("group.toe.lines.actionButtons.verifyButton.text")
+    );
+
+    await waitFor((): void => {
+      expect(msgSuccess).toHaveBeenCalledWith(
+        "group.toe.lines.alerts.verifyToeLines.success",
+        "groupAlerts.updatedTitle"
+      );
+    });
   });
 });
