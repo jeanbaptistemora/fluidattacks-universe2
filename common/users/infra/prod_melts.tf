f53fb430a926857a24824561924db02dbaadbac4
John Perez <jperez@fluidattacks.com>
2022-05-26 16:41:44 -0500

    common\refac(infra): #6574.4 update permissions
    - Specify S3 permissions needed for `prod_melts` user
    - Remove KMS permissions from user

--- common/users/infra/prod_melts.tf
+++ common/users/infra/prod_melts.tf
@@ -8,24 +8,11 @@ locals {
             Sid    = "s3Write"
             Effect = "Allow"
             Action = ["*"]
+            // This buckets and resources list belog to melts, so the wildcard
+            // above is not dangerous
             Resource = [
               "arn:aws:s3:::fluidattacks-terraform-states-prod/melts*",
             ]
-          },
-          {
-            Sid    = "kmsRead"
-            Effect = "Allow"
-            Action = [
-              "kms:CreateAlias",
-              "kms:CreateKey",
-              "kms:Describe*",
-              "kms:Get*",
-              "kms:List*",
-              "kms:TagResource",
-              "kms:UntagResource",
-              "kms:UpdateAlias",
-            ]
-            Resource = ["*"]
           },
           {
             Sid    = "dynamoWrite"
