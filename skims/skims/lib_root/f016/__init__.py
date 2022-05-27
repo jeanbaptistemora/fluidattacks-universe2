4ae592a51c4e85fcfd614cd9fa3656512fd8a2cd
Juan Echeverri <jecheverri@fluidattacks.com>
2022-05-26 19:04:32 -0500

    skims\feat(back): #5999.73 f016 cs test
    - Add test to lib_path/f016
    - Update lib_path.csv

--- skims/skims/lib_root/f016/__init__.py
+++ skims/skims/lib_root/f016/__init__.py
@@ -1,4 +1,5 @@
 from lib_root.f016.c_sharp import (
+    httpclient_no_revocation_list as c_sharp_httpclient_no_revocation_list,
     insecure_shared_access_protocol as c_sharp_insecure_shared_access_protocol,
     service_point_manager_disabled as c_sharp_service_point_manager_disabled,
     weak_protocol as c_sharp_weak_protocol,
@@ -13,4 +14,5 @@ QUERIES: graph_model.Queries = (
     (FINDING, c_sharp_weak_protocol),
     (FINDING, c_sharp_service_point_manager_disabled),
     (FINDING, c_sharp_insecure_shared_access_protocol),
+    (FINDING, c_sharp_httpclient_no_revocation_list),
 )
