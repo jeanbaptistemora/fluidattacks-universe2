3e58e01cca6e37741a11fa081a96527cff25d4c5
Frank Aristizabal <faristizabal@fluidattacks.com>
2022-05-26 19:55:02 -0500

    airs\refac(front): #6516.9 update po
    - Update prouct overview to new version.
    - Add productSection.

---
+++ airs/front/src/components/ProductOverviewPage/ProductSection/InteractiveImage/index.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+import { Container } from "./styledComponents";
+
+import { CloudImage } from "../../../CloudImage";
+import { HotSpotButton } from "../../../HotSpotButton";
+
+interface IInteractiveProps {
+  image1: string;
+  image2: string;
+  isRight: boolean;
+}
+
+const InteractiveImage: React.FC<IInteractiveProps> = ({
+  image1,
+  image2,
+  isRight,
+}: IInteractiveProps): JSX.Element => {
+  const [isTouch, setIsTouch] = useState(false);
+
+  function onClick(): void {
+    setIsTouch(!isTouch);
+  }
+
+  return (
+    <Container>
+      <HotSpotButton isRight={isRight} onClick={onClick} />
+      <CloudImage alt={"Image Demo"} src={isTouch ? image2 : image1} />
+    </Container>
+  );
+};
+
+export { InteractiveImage };
---
+++ airs/front/src/components/ProductOverviewPage/ProductSection/InteractiveImage/styledComponents.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Container = styled.div.attrs({
+  className: `
+    center
+    dib
+    relative
+    bg-white
+  `,
+})``;
+
+export { Container };
