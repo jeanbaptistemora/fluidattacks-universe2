1d5ce9ef8db801d192c653e20183aa646e237fa4
Jonathan Mesa <jmesa@fluidattacks.com>
2022-05-31 16:40:19 +0000

    integrates\refac(back): #6109.45 orgs model
    - add formatting helpers for the new dataloader
    
    - complete model for orgs: facets/add/get/update/remove/utils
    - SPEED: 2 issues / 3 day = 0.6 issues/day
    - TODO: 5 fns
    - ETA: 5 / 0.6 = 8.3 days

--- integrates/back/src/db_model/organizations/utils.py
+++ integrates/back/src/db_model/organizations/utils.py
@@ -1,6 +1,17 @@
 from .constants import (
     ORGANIZATION_ID_PREFIX,
 )
+from .types import (
+    Organization,
+    OrganizationPolicies,
+    OrganizationState,
+)
+from db_model.organizations.enums import (
+    OrganizationStateStatus,
+)
+from dynamodb.types import (
+    Item,
+)
 
 
 def add_org_id_prefix(organization_id: str) -> str:
@@ -10,3 +21,35 @@ def add_org_id_prefix(organization_id: str) -> str:
 
 def remove_org_id_prefix(organization_id: str) -> str:
     return organization_id.lstrip(ORGANIZATION_ID_PREFIX)
+
+
+def format_organization(item: Item) -> Organization:
+    return Organization(
+        billing_customer=item.get("billing_customer"),
+        id=add_org_id_prefix(item["id"]),
+        name=item["name"],
+        policies=format_policies(item["policies"]),
+        state=format_state(item["state"]),
+    )
+
+
+def format_policies(policies: Item) -> OrganizationPolicies:
+    return OrganizationPolicies(
+        max_acceptance_days=int(policies["max_acceptance_days"]),
+        max_acceptance_severity=policies["max_acceptance_severity"],
+        max_number_acceptances=int(policies["max_number_acceptances"]),
+        min_acceptance_severity=policies["min_acceptance_severity"],
+        min_breaking_severity=policies["min_breaking_severity"],
+        modified_date=policies["modified_date"],
+        modified_by=policies["modified_by"],
+        vulnerability_grace_period=int(policies["vulnerability_grace_period"]),
+    )
+
+
+def format_state(state: Item) -> OrganizationState:
+    return OrganizationState(
+        status=OrganizationStateStatus[state["status"]],
+        modified_by=state["modified_by"],
+        modified_date=state["modified_date"],
+        pending_deletion_date=state.get("pending_deletion_date"),
+    )
