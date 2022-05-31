2f668a9bce53c1961d53569c7eba9fbd92eaec44
Jonathan Mesa <jmesa@fluidattacks.com>
2022-05-31 19:10:12 +0000

    integrates\refac(back): #6109.49 orgs model
    - add option to update org current state
    
    - complete model for orgs: facets/add/get/update/remove/utils
    - SPEED: 2 issues / 3 day = 0.6 issues/day
    - TODO: 5 fns
    - ETA: 5 / 0.6 = 8.3 days

--- integrates/back/src/db_model/organizations/__init__.py
+++ integrates/back/src/db_model/organizations/__init__.py
@@ -3,9 +3,11 @@ from .add import (
 )
 from .update import (
     update_metadata,
+    update_state,
 )
 
 __all__ = [
     "add",
     "update_metadata",
+    "update_state",
 ]
--- integrates/back/src/db_model/organizations/update.py
+++ integrates/back/src/db_model/organizations/update.py
@@ -1,5 +1,6 @@
 from .types import (
     OrganizationMetadataToUpdate,
+    OrganizationState,
 )
 from .utils import (
     format_metadata_item,
@@ -13,6 +14,9 @@ from custom_exceptions import (
 from db_model import (
     TABLE,
 )
+from db_model.organizations.enums import (
+    OrganizationStateStatus,
+)
 from db_model.organizations.utils import (
     remove_org_id_prefix,
 )
@@ -23,6 +27,7 @@ from dynamodb import (
 from dynamodb.exceptions import (
     ConditionalCheckFailedException,
 )
+import simplejson as json  # type: ignore
 
 
 async def update_metadata(
@@ -52,3 +57,60 @@ async def update_metadata(
             )
         except ConditionalCheckFailedException as ex:
             raise OrganizationNotFound() from ex
+
+
+async def update_state(
+    *,
+    organization_id: str,
+    organization_name: str,
+    state: OrganizationState,
+) -> None:
+    organization_id = remove_org_id_prefix(organization_id)
+    key_structure = TABLE.primary_key
+    state_item = json.loads(json.dumps(state))
+    state_item = {
+        key: None if not value and value is not False else value
+        for key, value in state_item.items()
+        if value is not None
+    }
+
+    try:
+        organization_key = keys.build_key(
+            facet=TABLE.facets["organization_metadata"],
+            values={
+                "id": organization_id,
+                "name": organization_name,
+            },
+        )
+        organization_item = {"state": state_item}
+        condition_expression = Attr(
+            key_structure.partition_key
+        ).exists() & Attr("state.status").ne(
+            OrganizationStateStatus.DELETED.value
+        )
+        await operations.update_item(
+            condition_expression=condition_expression,
+            item=organization_item,
+            key=organization_key,
+            table=TABLE,
+        )
+    except ConditionalCheckFailedException as ex:
+        raise OrganizationNotFound() from ex
+
+    historic_state_key = keys.build_key(
+        facet=TABLE.facets["organization_historic_state"],
+        values={
+            "id": organization_id,
+            "iso8601utc": state.modified_date,
+        },
+    )
+    historic_item = {
+        key_structure.partition_key: historic_state_key.partition_key,
+        key_structure.sort_key: historic_state_key.sort_key,
+        **state_item,
+    }
+    await operations.put_item(
+        facet=TABLE.facets["organization_historic_state"],
+        item=historic_item,
+        table=TABLE,
+    )
