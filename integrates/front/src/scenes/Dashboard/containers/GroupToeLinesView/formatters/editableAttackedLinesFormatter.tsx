77ae4480f46ad967060ed324fef8b6bccf5a167b
Andres Aguirre <aaguirre@fluidattacks.com>
2022-05-31 17:57:13 +0000

    integrates\refac(front): #6455.3 open edition mode
    - Add initial formatter that allows to edit a cell
    for the attacked lines

--- integrates/front/src/scenes/Dashboard/containers/GroupToeLinesView/formatters/editableAttackedLinesFormatter.tsx
+++ integrates/front/src/scenes/Dashboard/containers/GroupToeLinesView/formatters/editableAttackedLinesFormatter.tsx
@@ -1,9 +1,71 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-autofocus */
 import _ from "lodash";
-import React from "react";
+import React, { useState } from "react";
 
+import type { IToeLinesData } from "../types";
 import type { IHeaderConfig } from "components/Table/types";
 import { StyledInput } from "utils/forms/fields/styles";
+
+interface IEditableFormatterProps {
+  defaultValue: string;
+  row: IToeLinesData;
+  handleUpdateAttackedLines: (
+    rootId: string,
+    filename: string,
+    attackedLines: number
+  ) => Promise<void>;
+}
+
+const EditableFormatter: React.FC<IEditableFormatterProps> = ({
+  defaultValue,
+  row,
+  handleUpdateAttackedLines,
+}): JSX.Element => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  function handleOnDivClick(_event: React.MouseEvent<HTMLDivElement>): void {
+    setIsFocused(true);
+  }
+
+  function handleOnInputBlur(event: React.FocusEvent<HTMLInputElement>): void {
+    setIsFocused(false);
+    event.stopPropagation();
+  }
+
+  async function handleOnInputKeyUp(
+    event: React.KeyboardEvent<HTMLInputElement>
+  ): Promise<void> {
+    event.stopPropagation();
+    if (event.key === "Enter") {
+      await handleUpdateAttackedLines(
+        row.rootId,
+        row.filename,
+        _.toNumber(event.currentTarget.value)
+      );
+      setIsFocused(false);
+    }
+  }
+
+  return isFocused ? (
+    <StyledInput
+      autoFocus={true}
+      defaultValue={defaultValue}
+      onBlur={handleOnInputBlur}
+      onKeyUp={handleOnInputKeyUp}
+      type={"number"}
+    />
+  ) : (
+    <div
+      aria-checked={false}
+      onClick={handleOnDivClick}
+      role={"switch"}
+      tabIndex={0}
+    >
+      {defaultValue}
+    </div>
+  );
+};
 
 export const editableAttackedLinesFormatter: (
   handleUpdateAttackedLines: (
@@ -13,7 +75,7 @@ export const editableAttackedLinesFormatter: (
   ) => Promise<void>
 ) => (
   value: string,
-  row: Readonly<Record<string, string>>,
+  row: IToeLinesData,
   rowIndex: number,
   key: Readonly<IHeaderConfig>
 ) => JSX.Element = (
@@ -24,43 +86,26 @@ export const editableAttackedLinesFormatter: (
   ) => Promise<void>
 ): ((
   value: string,
-  row: Readonly<Record<string, string>>,
+  row: IToeLinesData,
   rowIndex: number,
   key: Readonly<IHeaderConfig>
 ) => JSX.Element) => {
   const formatter: (
     value: string,
-    row: Readonly<Record<string, string>>,
+    row: IToeLinesData,
     rowIndex: number,
     key: Readonly<IHeaderConfig>
   ) => JSX.Element = (
     value: string,
-    row: Readonly<Record<string, string>>,
+    row: IToeLinesData,
     _rowIndex: number,
     _key: Readonly<IHeaderConfig>
   ): JSX.Element => {
-    function handleOnBlur(event: React.FocusEvent<HTMLInputElement>): void {
-      event.stopPropagation();
-    }
-
-    function handleOnKeyUp(event: React.KeyboardEvent<HTMLInputElement>): void {
-      event.stopPropagation();
-      if (event.key === "Enter") {
-        void handleUpdateAttackedLines(
-          row.rootId,
-          row.filename,
-          _.toNumber(event.currentTarget.value)
-        );
-      }
-    }
-
     return (
-      <StyledInput
-        autoFocus={true}
+      <EditableFormatter
         defaultValue={value}
-        onBlur={handleOnBlur}
-        onKeyUp={handleOnKeyUp}
-        type={"number"}
+        handleUpdateAttackedLines={handleUpdateAttackedLines}
+        row={row}
       />
     );
   };
