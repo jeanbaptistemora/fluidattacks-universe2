8d569e18d10096c50f24a7d6962c2c10bd56493c
Andres Aguirre <aaguirre@fluidattacks.com>
2022-06-01 22:06:49 +0000

    integrates\feat(front): #6601.1 number input
    - Add new reusable component for cases where the user
    edits a value inside a table

---
+++ integrates/front/src/components/NumberInput/index.tsx
@@ -0,0 +1,143 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-autofocus */
+import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+import _ from "lodash";
+import React, { useEffect, useRef, useState } from "react";
+
+import {
+  Col50,
+  Row,
+  StyledFontAwesomeIcon,
+  StyledInput,
+  StyledInputContainer,
+  VerticalLine,
+} from "./styles";
+
+interface INumberInputProps {
+  defaultValue: string;
+  max: number;
+  min: number;
+  onEnter: (newValue: number | undefined) => void;
+}
+
+const NumberInput: React.FC<INumberInputProps> = ({
+  defaultValue,
+  max,
+  min,
+  onEnter,
+}): JSX.Element => {
+  const [value, setValue] = useState(defaultValue);
+  const inputReference: React.MutableRefObject<HTMLInputElement | null> =
+    useRef(null);
+
+  const getCurrentNumber = (): number => _.toNumber(value);
+
+  function handleOnInputChange(
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void {
+    setValue(event.target.value);
+    event.stopPropagation();
+  }
+
+  function handleOnInputContainerBlur(
+    event: React.FocusEvent<HTMLInputElement>
+  ): void {
+    if (!event.currentTarget.contains(event.relatedTarget)) {
+      setValue(defaultValue);
+    }
+    event.stopPropagation();
+  }
+
+  function handleOnInputFocus(event: React.FocusEvent<HTMLInputElement>): void {
+    event.stopPropagation();
+  }
+
+  function handleOnInputKeyUp(
+    event: React.KeyboardEvent<HTMLInputElement>
+  ): void {
+    event.stopPropagation();
+    if (event.key === "Enter" && !_.isEmpty(event.currentTarget.value)) {
+      const newValue = _.isEmpty(event.currentTarget.value)
+        ? undefined
+        : _.toNumber(event.currentTarget.value);
+      onEnter(newValue);
+    }
+  }
+
+  function handleInputKeyDown(
+    event: React.KeyboardEvent<HTMLInputElement>
+  ): void {
+    if (
+      event.key.length > 1 ||
+      /\d/u.test(event.key) ||
+      event.key === "Control" ||
+      event.key.toLocaleLowerCase() === "c"
+    )
+      return;
+    event.preventDefault();
+  }
+
+  function handleOnMinusClick(event: React.MouseEvent<SVGSVGElement>): void {
+    event.stopPropagation();
+    const newNumber = getCurrentNumber() - 1;
+    if (newNumber >= min && newNumber <= max) {
+      setValue(_.toString(newNumber));
+    }
+  }
+
+  function handleOnPlusClick(event: React.MouseEvent<SVGSVGElement>): void {
+    event.stopPropagation();
+    const newNumber = getCurrentNumber() + 1;
+    if (newNumber >= min && newNumber <= max) {
+      setValue(_.toString(newNumber));
+    }
+  }
+
+  useEffect((): void => {
+    if (inputReference.current !== null) {
+      inputReference.current.focus();
+    }
+  }, [value]);
+
+  return (
+    <StyledInputContainer onBlur={handleOnInputContainerBlur} tabIndex={-1}>
+      <Row>
+        <Col50>
+          <StyledInput
+            max={max}
+            min={min}
+            onChange={handleOnInputChange}
+            onFocus={handleOnInputFocus}
+            onKeyDown={handleInputKeyDown}
+            onKeyUp={handleOnInputKeyUp}
+            ref={inputReference}
+            step={"1"}
+            type={"number"}
+            value={value}
+          />
+        </Col50>
+        <Col50>
+          <Row>
+            <Col50>
+              <StyledFontAwesomeIcon
+                icon={faMinus}
+                onClick={handleOnMinusClick}
+                tabIndex={-1}
+              />
+            </Col50>
+            <VerticalLine />
+            <Col50>
+              <StyledFontAwesomeIcon
+                icon={faPlus}
+                onClick={handleOnPlusClick}
+                tabIndex={-1}
+              />
+            </Col50>
+          </Row>
+        </Col50>
+      </Row>
+    </StyledInputContainer>
+  );
+};
+
+export { NumberInput };
