a6d7e1af9d0c5c8536497239b8c6a3ad61cc8ace
Jonathan Mesa <jmesa@fluidattacks.com>
2022-06-01 23:05:13 +0000

    integrates\test(back): #6109.61 orgs model
    - migrate functional test mock data to typed version
    
    - complete model for orgs: facets/add/get/update/remove/utils/test
    - SPEED: 4 issues / 4 day = 1 issues/day
    - TODO: 3 fns
    - ETA: 3 / 1 = 3 days

--- integrates/back/test/functional/src/organization/conftest.py
+++ integrates/back/test/functional/src/organization/conftest.py
@@ -2,6 +2,17 @@
 from back.test import (
     db,
 )
+from db_model.organizations.enums import (
+    OrganizationStateStatus,
+)
+from db_model.organizations.types import (
+    Organization,
+    OrganizationPolicies,
+    OrganizationState,
+)
+from decimal import (
+    Decimal,
+)
 import pytest
 from typing import (
     Any,
@@ -13,9 +24,46 @@ from typing import (
 @pytest.fixture(autouse=True, scope="session")
 async def populate(generic_data: dict[str, Any]) -> bool:
     data: dict[str, Any] = {
-        "orgs": [
+        "organizations": [
             {
-                "name": "orgtest",
+                "organization": Organization(
+                    id="40f6da5f-4f66-4bf0-825b-a2d9748ad6db",
+                    name="orgtest",
+                    policies=OrganizationPolicies(
+                        modified_by=generic_data["global_vars"]["user_email"],
+                        modified_date="2019-11-22T20:07:57+00:00",
+                        max_acceptance_days=90,
+                        max_number_acceptances=4,
+                        max_acceptance_severity=Decimal("7.0"),
+                        min_acceptance_severity=Decimal("3.0"),
+                        min_breaking_severity=Decimal("2.0"),
+                        vulnerability_grace_period=5,
+                    ),
+                    state=OrganizationState(
+                        modified_by=generic_data["global_vars"]["user_email"],
+                        modified_date="2019-11-22T20:07:57+00:00",
+                        status=OrganizationStateStatus.ACTIVE,
+                    ),
+                ),
+            },
+            {
+                "organization": Organization(
+                    id="8a7c8089-92df-49ec-8c8b-ee83e4ff3256",
+                    name="acme",
+                    policies=OrganizationPolicies(
+                        modified_by=generic_data["global_vars"]["user_email"],
+                        modified_date="2019-11-22T20:07:57+00:00",
+                    ),
+                    state=OrganizationState(
+                        modified_by=generic_data["global_vars"]["user_email"],
+                        modified_date="2019-11-22T20:07:57+00:00",
+                        status=OrganizationStateStatus.ACTIVE,
+                    ),
+                ),
+            },
+        ],
+        "organization_users": [
+            {
                 "id": "40f6da5f-4f66-4bf0-825b-a2d9748ad6db",
                 "users": [
                     "admin@gmail.com",
@@ -29,36 +77,28 @@ async def populate(generic_data: dict[str, Any]) -> bool:
                     "resourcer@gmail.com",
                     "reviewer@gmail.com",
                 ],
-                "groups": [
-                    "group1",
-                ],
-                "policy": {
-                    "max_acceptance_days": 90,
-                    "max_number_acceptations": 4,
-                    "max_acceptance_severity": 7,
-                    "min_acceptance_severity": 3,
-                    "min_breaking_severity": 2,
-                    "vulnerability_grace_period": 5,
-                    "historic_max_number_acceptations": [
-                        {
-                            "date": "2019-11-22 15:07:57",
-                            "user": "test1@gmail.com",
-                            "max_number_acceptations": 4,
-                        },
-                    ],
-                },
             },
             {
-                "name": "acme",
                 "id": "8a7c8089-92df-49ec-8c8b-ee83e4ff3256",
                 "users": [
                     "admin@gmail.com",
                 ],
+            },
+        ],
+        "organization_groups": [
+            {
+                "id": "40f6da5f-4f66-4bf0-825b-a2d9748ad6db",
+                "groups": [
+                    "group1",
+                ],
+            },
+            {
+                "id": "8a7c8089-92df-49ec-8c8b-ee83e4ff3256",
                 "groups": [
                     "group2",
                 ],
-                "policy": {},
             },
         ],
+        "orgs": [],
     }
     return await db.populate({**generic_data["db_data"], **data})
