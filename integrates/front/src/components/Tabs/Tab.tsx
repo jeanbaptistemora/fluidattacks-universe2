43448f7ca0de0116d97917937c4914d76c1dabce
David Acevedo <dacevedo@fluidattacks.com>
2022-06-02 16:18:53 +0000

    integrates\refac(front): #6573 standardize tabs
    - title as children
    - allow tooltip to be optional

--- integrates/front/src/components/Tabs/Tab.tsx
+++ integrates/front/src/components/Tabs/Tab.tsx
@@ -7,18 +7,20 @@ import { TooltipWrapper } from "components/TooltipWrapper";
 interface ITabProps {
   id: string;
   link: string;
-  title: string;
-  tooltip: string;
+  tooltip?: string;
 }
 
-const Tab: React.FC<ITabProps> = (props: ITabProps): JSX.Element => {
-  const { tooltip, id, title, link } = props;
-
+const Tab: React.FC<ITabProps> = ({
+  children,
+  id,
+  link,
+  tooltip = "",
+}: Readonly<React.PropsWithChildren<ITabProps>>): JSX.Element => {
   return (
     <li>
       <TooltipWrapper id={`${id}Tooltip`} message={tooltip}>
         <TabLink id={id} to={link}>
-          {title}
+          {children}
         </TabLink>
       </TooltipWrapper>
     </li>
