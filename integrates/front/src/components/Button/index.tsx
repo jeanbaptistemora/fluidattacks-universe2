804d2e190249428fb2a7761bea7a111bf218daf9
Jaime Hurtado <jhurtado@fluidattacks.com>
2022-06-03 09:43:27 -0500

    integrates\feat(front): #6411.21 button component
    - added basic and gray button component variants to support
      autoenrollment styles
    - Speed: 3 task / 9.5 days = 0.32 task/day
    - TODO: 7 tasks
    - ETA: 7 / 0.32 = 21.9 days

--- integrates/front/src/components/Button/index.tsx
+++ integrates/front/src/components/Button/index.tsx
@@ -1,25 +1,45 @@
 import styled from "styled-components";
 
 interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant: "primary" | "secondary";
+  variant: "basic" | "gray" | "primary" | "secondary";
 }
 
 interface IVariant {
   backgroundColor: string;
+  borderColor: string;
   color: string;
   hoverBackgroundColor: string;
+  hoverColor: string;
 }
 
 const variants: Record<IButtonProps["variant"], IVariant> = {
+  basic: {
+    backgroundColor: "transparent",
+    borderColor: "transparent",
+    color: "#5c5c70",
+    hoverBackgroundColor: "transparent",
+    hoverColor: "black",
+  },
+  gray: {
+    backgroundColor: "#e9e9ed",
+    borderColor: "#e9e9ed",
+    color: "#5c5c70",
+    hoverBackgroundColor: "#e9e9ed",
+    hoverColor: "#5c5c70",
+  },
   primary: {
     backgroundColor: "#ff3435",
+    borderColor: "#ff3435",
     color: "#fff",
     hoverBackgroundColor: "#b80000",
+    hoverColor: "white",
   },
   secondary: {
     backgroundColor: "transparent",
+    borderColor: "#ff3435",
     color: "#ff3435",
     hoverBackgroundColor: "#ff3435",
+    hoverColor: "white",
   },
 };
 
@@ -30,7 +50,7 @@ const Button = styled.button.attrs<IButtonProps>(
 )<IButtonProps>`
   background-color: ${(props): string =>
     variants[props.variant].backgroundColor};
-  border-color: #ff3435;
+  border-color: ${(props): string => variants[props.variant].borderColor};
   border-radius: 4px;
   border-style: solid;
   border-width: 2px;
@@ -50,7 +70,7 @@ const Button = styled.button.attrs<IButtonProps>(
       variants[props.variant].hoverBackgroundColor};
     border-color: ${(props): string =>
       variants[props.variant].hoverBackgroundColor};
-    color: white;
+    color: ${(props): string => variants[props.variant].hoverColor};
     cursor: pointer;
   }
 `;
