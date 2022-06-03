7a07760d51f59cf4a3c0ffdbdc0cae767a9290ae
Sebastian Lizcano <slizcano@fluidattacks.com>
2022-06-03 18:17:20 -0500

    integrates\refac(back): #2228.44 add migration
    - add migration to remove group_access for group not found
    currently the migration it's just for finding information
    - no-deltas-test

---
+++ integrates/back/migrations/0223_remove_orphan_access.py
@@ -0,0 +1,145 @@
+# pylint: disable=invalid-name
+"""
+Remove FI_project_access access for groups
+that doesn't longer exist to avoid
+group_not_found exception
+
+Execution Time:
+Finalization Time:
+"""
+
+from aioextensions import (
+    collect,
+    run,
+)
+from authz.policy import (
+    _get_user_subject_policies,
+    get_group_level_roles,
+)
+from custom_exceptions import (
+    GroupNotFound,
+)
+from dataloaders import (
+    Dataloaders,
+    get_new_context,
+)
+from dynamodb import (
+    operations_legacy,
+)
+from group_access.dal import (
+    get_user_groups,
+)
+import logging
+import logging.config
+from settings import (
+    LOGGING,
+)
+import time
+
+logging.config.dictConfig(LOGGING)
+
+# Constants
+LOGGER_CONSOLE = logging.getLogger("console")
+USERS_TABLE = "FI_users"
+
+
+async def _process_user(
+    *,
+    email: str,
+    group_name: str,
+    loaders: Dataloaders,
+    user: dict[str, str],
+) -> None:
+    try:
+        await loaders.group.load(group_name)
+    except GroupNotFound as ex:
+        group_role = await get_group_level_roles(
+            email, [group_name], with_cache=False
+        )
+        LOGGER_CONSOLE.info(
+            ex,
+            extra={
+                "extra": {
+                    "email": email,
+                    "group_name": group_name,
+                    "user": user,
+                    "role": group_role,
+                }
+            },
+        )
+
+
+async def process_user(
+    *,
+    email: str,
+    loaders: Dataloaders,
+    progress: float,
+    user: dict[str, str],
+) -> None:
+    active, inactive = await collect(
+        (
+            get_user_groups(email, active=True),
+            get_user_groups(email, active=False),
+        )
+    )
+    groups = [
+        policy[1]
+        for policy in await _get_user_subject_policies(email)
+        if policy[2] == "group"
+    ]
+    user_groups = set(active + inactive + groups)
+
+    await collect(
+        tuple(
+            _process_user(
+                email=email,
+                group_name=group,
+                loaders=loaders,
+                user=user,
+            )
+            for group in user_groups
+        ),
+        workers=4,
+    )
+    LOGGER_CONSOLE.info(
+        "User processed",
+        extra={
+            "extra": {
+                "user": email,
+                "progress": round(progress, 2),
+            }
+        },
+    )
+
+
+async def main() -> None:
+    loaders: Dataloaders = get_new_context()
+    users = await operations_legacy.scan(USERS_TABLE, {})
+    LOGGER_CONSOLE.info(
+        "Users",
+        extra={"extra": {"user_len": len(users)}},
+    )
+
+    await collect(
+        tuple(
+            process_user(
+                email=user["email"],
+                loaders=loaders,
+                user=user,
+                progress=count / len(users),
+            )
+            for count, user in enumerate(users)
+        ),
+        workers=4,
+    )
+
+
+if __name__ == "__main__":
+    execution_time = time.strftime(
+        "Execution Time:    %Y-%m-%d at %H:%M:%S UTC"
+    )
+    run(main())
+    finalization_time = time.strftime(
+        "Finalization Time: %Y-%m-%d at %H:%M:%S UTC"
+    )
+    print(f"{execution_time}\n{finalization_time}")
