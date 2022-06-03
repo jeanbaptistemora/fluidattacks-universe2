8da5084f68aaaf1fb7fe96c92ade1ec178ab700c
Sebastian Lizcano <slizcano@fluidattacks.com>
2022-06-03 11:32:34 -0500

    integrates\feat(back): #6532.8 add generator
    - add generic js generator to append additional arrow text
    to text box graphics

---
+++ integrates/back/src/app/templates/static/graphics/generators/textBox/generic.js
@@ -0,0 +1,63 @@
+/* global d3 */
+
+const half = 0.5;
+const percentage = 100;
+
+function render(dataDocument, height, width) {
+  const fontSize = dataDocument.fontSizeRatio * Math.min(height, width);
+  const fontOffset = fontSize * half * half;
+
+  if (dataDocument.arrow) {
+    const arrowFontSize = dataDocument.arrowFontSizeRatio * Math.min(height, width);
+    const arrowFontOffset = 1.65;
+    const plusSign = parseFloat(dataDocument.text) < 0 ? '' : '+';
+
+    const svg = d3
+      .select('div')
+      .append('svg')
+      .attr('viewBox', `${ (-width / 2) + (fontSize * half) } ${ -height / 2 } ${ width } ${ height }`);
+
+    svg
+      .append('g')
+      .append('text')
+      .attr('font-size', `${ fontSize }`)
+      .attr('font-family', 'Arial, Helvetica, sans-serif')
+      .attr('font-weight', 'bold')
+      .attr('text-anchor', 'middle')
+      .attr('transform', `translate(0, ${ fontOffset })`)
+      .text(`${ plusSign }${ parseFloat(dataDocument.text * percentage).toFixed(0) }%`);
+
+    svg
+      .append('text')
+      .attr('font-size', `${ arrowFontSize }`)
+      .attr('font-family', 'Arial, Helvetica, sans-serif')
+      .attr('text-anchor', 'middle')
+      .attr('transform', `translate(${ fontSize * arrowFontOffset }, ${ fontOffset })`)
+      .style('fill', dataDocument.color)
+      .text(dataDocument.arrow);
+  } else {
+    const svg = d3
+      .select('div')
+      .append('svg')
+      .attr('viewBox', `${ -width / 2 } ${ -height / 2 } ${ width } ${ height }`);
+
+    svg
+      .append('g')
+      .append('text')
+      .attr('font-size', `${ fontSize }`)
+      .attr('font-family', 'Arial, Helvetica, sans-serif')
+      .attr('font-weight', 'bold')
+      .attr('text-anchor', 'middle')
+      .attr('transform', `translate(0, ${ fontOffset })`)
+      .text(`${ parseFloat(dataDocument.text * percentage).toFixed(0) }%`);
+  }
+}
+
+function load() {
+  const args = JSON.parse(document.getElementById('args').textContent.replace(/'/g, '"'));
+  const dataDocument = JSON.parse(args.data);
+
+  render(dataDocument, args.height, args.width);
+}
+
+window.load = load;
