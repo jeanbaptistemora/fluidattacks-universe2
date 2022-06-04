4b26af111782d7423c88b2bd2ac30e8e6ab8b3f5
Frank Aristizabal <faristizabal@fluidattacks.com>
2022-06-04 13:49:38 -0500

    airs\refac(front): #6516.24 update po
    - Update prouct overview to new version.
    - Add plans banner.

---
+++ airs/front/src/components/ProductOverviewPage/PlansBanner/styledComponents.tsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+const Container = styled.div.attrs({
+  className: `
+    bg-white
+    flex
+    ph-body
+  `,
+})``;
+
+const CardContainer = styled.div.attrs({
+  className: `
+    br2
+    ba
+    pv5
+    ph6
+    mv5
+    w-100
+    center
+    mw-1366
+  `,
+})`
+  border-color: #ff3435;
+  box-shadow: 0px 0px 6px 3px rgb(0 0 0 / 8%);
+`;
+
+const CardButton = styled.div.attrs({
+  className: `
+    flex
+    t-all-3-eio
+  `,
+})`
+  > p {
+    transition: all 0.3s ease-in-out;
+  }
+
+  > p + svg {
+    color: #ff3435;
+    font-size: 25px;
+  }
+
+  :hover > p {
+    color: #ff3435;
+    margin-right: 5px;
+  }
+`;
+
+export { CardContainer, CardButton, Container };
