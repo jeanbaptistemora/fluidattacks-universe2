289a55c2c20e0c9730c67242bed1f11c57aa4e10
Daniel Murcia <dmurcia@fluidattacks.com>
2022-06-06 13:29:45 -0500

    observes\refac(back): #6432.7 obj clients
    - move clients to its own modules

--- observes/singer/tap-checkly/tap_checkly/api2/alert_channels/__init__.py
+++ observes/singer/tap-checkly/tap_checkly/api2/alert_channels/__init__.py
@@ -1,78 +1,13 @@
-from __future__ import (
-    annotations,
-)
-
-from . import (
-    _decode,
+from ._client import (
+    AlertChannelsClient,
 )
 from ._core import (
     AlertChannel,
     AlertChannelObj,
 )
-from dataclasses import (
-    dataclass,
-)
-from fa_purity import (
-    Cmd,
-    FrozenList,
-    Stream,
-)
-from fa_purity.json.factory import (
-    from_prim_dict,
-)
-from fa_purity.pure_iter.factory import (
-    from_flist,
-    infinite_range,
-)
-from fa_purity.stream.factory import (
-    from_piter,
-)
-from fa_purity.stream.transform import (
-    chain,
-    until_none,
-)
-from tap_checkly.api2._raw import (
-    Credentials,
-    RawClient,
-)
-
-
-@dataclass(frozen=True)
-class AlertChannelsClient:
-    _client: RawClient
-    _per_page: int
-
-    def _get_page(self, page: int) -> Cmd[FrozenList[AlertChannelObj]]:
-        return self._client.get_list(
-            "/v1/alert-channels",
-            from_prim_dict(
-                {
-                    "limit": self._per_page,
-                    "page": page,
-                }
-            ),
-        ).map(lambda l: tuple(map(_decode.from_raw_obj, l)))
-
-    def list_all(self) -> Stream[AlertChannelObj]:
-        return (
-            infinite_range(1, 1)
-            .map(self._get_page)
-            .transform(lambda x: from_piter(x))
-            .map(lambda i: i if bool(i) else None)
-            .transform(lambda x: until_none(x))
-            .map(lambda x: from_flist(x))
-            .transform(lambda x: chain(x))
-        )
-
-    @staticmethod
-    def new(
-        auth: Credentials,
-        per_page: int,
-    ) -> AlertChannelsClient:
-        return AlertChannelsClient(RawClient(auth), per_page)
-
 
 __all__ = [
+    "AlertChannelsClient",
     "AlertChannel",
     "AlertChannelObj",
 ]
--- observes/singer/tap-checkly/tap_checkly/api2/checks/__init__.py
+++ observes/singer/tap-checkly/tap_checkly/api2/checks/__init__.py
@@ -1,82 +1,13 @@
-from __future__ import (
-    annotations,
-)
-
-from . import (
-    _decode,
+from ._client import (
+    ChecksClient,
 )
 from ._core import (
     CheckId,
     CheckObj,
 )
-from .results import (
-    CheckResultClient,
-    CheckResultObj,
-)
-from dataclasses import (
-    dataclass,
-)
-from datetime import (
-    datetime,
-)
-from fa_purity import (
-    Cmd,
-    FrozenList,
-    Stream,
-)
-from fa_purity.json.factory import (
-    from_prim_dict,
-)
-from tap_checkly.api2 import (
-    _utils,
-)
-from tap_checkly.api2._raw import (
-    Credentials,
-    RawClient,
-)
-
-
-@dataclass(frozen=True)
-class ChecksClient:
-    _raw: RawClient
-    _per_page: int
-    _from_date: datetime
-    _to_date: datetime
-
-    def _list_ids(self, page: int) -> Cmd[FrozenList[CheckId]]:
-        return self._raw.get_list(
-            "/v1/checks",
-            from_prim_dict({"limit": self._per_page, "page": page}),
-        ).map(lambda l: tuple(map(_decode.id_from_raw, l)))
-
-    def list_ids(self) -> Stream[CheckId]:
-        return _utils.paginate_all(self._list_ids)
-
-    def _list_checks(self, page: int) -> Cmd[FrozenList[CheckObj]]:
-        return self._raw.get_list(
-            "/v1/checks",
-            from_prim_dict({"limit": self._per_page, "page": page}),
-        ).map(lambda l: tuple(map(_decode.from_raw_obj, l)))
-
-    def list_checks(self) -> Stream[CheckObj]:
-        return _utils.paginate_all(self._list_checks)
-
-    def list_check_results(self, check: CheckId) -> Stream[CheckResultObj]:
-        _client = CheckResultClient(
-            self._raw, check, self._per_page, self._from_date, self._to_date
-        )
-        return _client.list_all()
-
-    @staticmethod
-    def new(
-        auth: Credentials,
-        per_page: int,
-        from_date: datetime,
-        to_date: datetime,
-    ) -> ChecksClient:
-        return ChecksClient(RawClient(auth), per_page, from_date, to_date)
-
 
 __all__ = [
+    "ChecksClient",
     "CheckId",
+    "CheckObj",
 ]
