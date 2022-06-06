014527c335cef535771c0997d3ede4bc613e8316
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-06-06 11:13:13 -0500

    skims\refac(back): #6074.10 add method
    - Minor changes for f065
    - no-deltas-test

--- skims/skims/lib_path/f065/__init__.py
+++ skims/skims/lib_path/f065/__init__.py
@@ -2,23 +2,25 @@ from lib_path.common import (
     SHIELD_BLOCKING,
 )
 from lib_path.f065.html import (
-    has_not_autocomplete,
+    has_autocomplete,
 )
 from model.core_model import (
     Vulnerabilities,
 )
 from typing import (
+    Callable,
     Tuple,
 )
 
 
 @SHIELD_BLOCKING
-def run_has_not_autocomplete(path: str) -> Vulnerabilities:
-    return has_not_autocomplete(path)
+def run_has_autocomplete(content: str, path: str) -> Vulnerabilities:
+    return has_autocomplete(content=content, path=path)
 
 
 @SHIELD_BLOCKING
 def analyze(
+    content_generator: Callable[[], str],
     file_extension: str,
     path: str,
     **_: None,
@@ -26,6 +28,6 @@ def analyze(
     results: Tuple[Vulnerabilities, ...] = ()
 
     if file_extension == "html":
-        results = (run_has_not_autocomplete(path),)
+        results = (run_has_autocomplete(content_generator(), path),)
 
     return results
--- skims/skims/lib_path/f065/html.py
+++ skims/skims/lib_path/f065/html.py
@@ -1,82 +1,68 @@
 from bs4 import (
     BeautifulSoup,
 )
-from bs4.element import (
-    Tag,
+from lib_path.common import (
+    get_vulnerabilities_from_iterator_blocking,
 )
-import os
+from model.core_model import (
+    MethodsEnum,
+    Vulnerabilities,
+)
+from typing import (
+    Iterator,
+    Tuple,
+)
 
 
-def _get_xpath(tag: Tag) -> str:
-    """Return the xpath of a BeautifulSoup Tag."""
-    # Inspiration from https://gist.github.com/ergoithz/6cf043e3fdedd1b94fcf
-    components = []
-    child = tag if tag.name else tag.parent
-    for parent in child.parents:
-        siblings = parent.find_all(child.name, recursive=False)
-        if len(siblings) == 1:
-            components.append(child.name)
-        else:
-            for sibling_no, sibling in enumerate(siblings, 1):
-                if sibling is child:
-                    components.append(sibling_no)
-                    break
-        child = parent
-    components.reverse()
-    return "/" + "/".join(components)
+def has_autocomplete(content: str, path: str) -> Vulnerabilities:
+    def iterator() -> Iterator[Tuple[int, int]]:
+        """
+        Check if *input* or *form* tags have *autocomplete*
+        attribute set to *off*.
 
+        It's known that *form* tags may have the *autocomplete* attribute set
+        to *on* and specific *input* tags have it set to *off*. However, this
+        check enforces a defensive and explicit approach,
+        forcing every *input* and *form* tag to have the *autocomplete*
+        attribute set to *off*.
+        """
 
-def has_not_autocomplete(filename: str) -> tuple:
-    """
-    Check if *input* or *form* tags have *autocomplete* attribute set to *off*.
+        html_obj = BeautifulSoup(content, features="html.parser")
 
-    It's known that *form* tags may have the *autocomplete* attribute set
-    to *on* and specific *input* tags have it set to *off*. However, this
-    check enforces a defensive and explicit approach,
-    forcing every *input* and *form* tag to have the *autocomplete* attribute
-    set to *off* in order to mark the result as CLOSED.
+        for obj in html_obj("input"):
+            autocomplete_enabled: bool = obj.get("autocomplete", "on") != "off"
+            is_input_enabled: bool = obj.get("disabled", "") != ""
+            is_input_type_sensitive: bool = obj.get("type", "text") in (
+                # autocomplete only works with these:
+                #   https://www.w3schools.com/tags/att_input_autocomplete.asp
+                "checkbox",
+                "date",
+                "datetime-local",
+                "email",
+                "month",
+                "password",
+                "search",
+                "tel",
+                "text",
+                "time",
+                "url",
+                "week",
+            )
+            if (
+                autocomplete_enabled
+                and is_input_type_sensitive
+                and is_input_enabled
+            ):
+                yield obj.sourceline, obj.sourcepos
 
-    :param filename: Path to the *HTML* source.
-    :returns: True if ALL tags *form* and *input* have attribute
-              *autocomplete* set to *off* (*on* is de default value),
-              False otherwise.
-    :rtype: :class:`fluidasserts.Result`
-    """
-    if not os.path.exists(filename):
-        return "UNKNOWN", "File does not exist"
+        for obj in html_obj("form"):
+            if obj.attrs.get("autocomplete", "on") != "off":
+                yield obj.sourceline, obj.sourcepos
 
-    with open(filename, "r", encoding="latin-1") as file_desc:
-        html_obj = BeautifulSoup(file_desc.read(), features="html.parser")
-
-    vulnerabilities: list = []
-    for obj in html_obj("input"):
-        autocomplete_enabled: bool = obj.get("autocomplete", "on") != "off"
-        is_input_enabled: bool = obj.get("disabled", "") != ""
-        is_input_type_sensitive: bool = obj.get("type", "text") in (
-            # autocomplete only works with these:
-            #   https://www.w3schools.com/tags/att_input_autocomplete.asp
-            "checkbox",
-            "date",
-            "datetime-local",
-            "email",
-            "month",
-            "password",
-            "search",
-            "tel",
-            "text",
-            "time",
-            "url",
-            "week",
-        )
-        if (
-            autocomplete_enabled
-            and is_input_enabled
-            and is_input_type_sensitive
-        ):
-            vulnerabilities.append(_get_xpath(obj))
-
-    for obj in html_obj("form"):
-        if obj.attrs.get("autocomplete", "on") != "off":
-            vulnerabilities.append(_get_xpath(obj))
-
-    return "Null", "Testing"
+    return get_vulnerabilities_from_iterator_blocking(
+        content=content,
+        description_key="lib_path.f065.has_autocomplete",
+        iterator=iterator(),
+        path=path,
+        method=MethodsEnum.HAS_AUTOCOMPLETE,
+    )
