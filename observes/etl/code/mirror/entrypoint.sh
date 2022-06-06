237310ff74f9aeb578c147042c3f9d87eeb20d59
Daniel Murcia <dmurcia@fluidattacks.com>
2022-06-06 11:28:20 -0500

    observes\refac(job): #6610.1 services repo
    - remove use_git_repo_services use
    - remove services token use

--- observes/etl/code/mirror/entrypoint.sh
+++ observes/etl/code/mirror/entrypoint.sh
@@ -2,16 +2,16 @@
 
 function job_code_mirror {
   local group="${1}"
+  local working_dir
 
-  true \
+  working_dir=$(mktemp -d) \
     && if test -z "${group}"; then
       abort '[INFO] Please set the first argument to the group name'
     fi \
     && aws_login_prod 'observes' \
     && ensure_gitlab_env_vars \
       INTEGRATES_API_TOKEN \
-      SERVICES_API_TOKEN \
-    && use_git_repo_services \
+    && pushd "${working_dir}" \
     && echo "[INFO] Working on ${group}" \
     && echo "[INFO] Cloning ${group} from source Git repository" \
     && export CI='true' \
@@ -19,10 +19,11 @@ function job_code_mirror {
     && export PROD_AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
     && export PROD_AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
     && { USER=nobody melts resources --clone-from-customer-git "${group}" || true; } \
-    && if find "groups/${group}/fusion/"* -maxdepth 0 -type d; then
+    && if find "./groups/${group}/fusion/"* -maxdepth 0 -type d; then
       echo '[INFO] Pushing repositories to S3' \
         && USER=nobody melts drills --push-repos "${group}" \
-        && rm -rf "groups/${group}/fusion/"
+        && chmod -R 777 groups \
+        && rm -r groups
     else
       echo '[WARNING] Unable to clone repositories from source' \
         && echo '[WARNING] Connection failed or repo has not been updated' \
