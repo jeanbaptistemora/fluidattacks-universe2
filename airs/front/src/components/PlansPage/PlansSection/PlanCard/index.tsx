8a349e458056480e9845b8f6e8cca4dc9bfbe48c
Frank Aristizabal <faristizabal@fluidattacks.com>
2022-06-06 13:05:07 -0500

    airs\refac(front): #6625.1 update plans
    - Add components for new plans page.
    - no-deltas-test

---
+++ airs/front/src/components/PlansPage/PlansSection/PlanCard/index.tsx
@@ -0,0 +1,78 @@
+/* eslint react/forbid-component-props: 0 */
+import { Link } from "gatsby";
+import React from "react";
+
+import {
+  CardContainer,
+  CardItem,
+  CardItemsContainer,
+  CardTitleContainer,
+  MachineHead,
+} from "./styledComponents";
+import type { IPlansCard } from "./types";
+
+import {
+  NewRegularRedButton,
+  PhantomRegularRedButton,
+} from "../../../../styles/styledComponents";
+import { CloudImage } from "../../../CloudImage";
+import { Paragraph, Title } from "../../../Texts";
+
+const PlanCard: React.FC<IPlansCard> = ({
+  description,
+  isMachine,
+  items,
+  title,
+}: IPlansCard): JSX.Element => {
+  return (
+    <div className={"mh3"}>
+      {isMachine ? (
+        <MachineHead>{"Try it free for 30 days"}</MachineHead>
+      ) : undefined}
+      <CardContainer isMachine={isMachine}>
+        <CardTitleContainer>
+          <CloudImage
+            alt={`Plan ${title} Fluid Attacks`}
+            src={`airs/plans/${isMachine ? "machine" : "squad"}`}
+            styles={"mr1"}
+          />
+          <Title fColor={"#24252d"} fSize={"32"} marginTop={"1"}>
+            {title}
+          </Title>
+        </CardTitleContainer>
+        <CardItemsContainer>
+          <Paragraph fColor={"#5c5c70"} fSize={"20"}>
+            {description}
+          </Paragraph>
+          {isMachine ? (
+            <Link to={"/contact-us"}>
+              <NewRegularRedButton className={"mv4 w-100"}>
+                {"Contact sales"}
+              </NewRegularRedButton>
+            </Link>
+          ) : (
+            <Link to={"/contact-us"}>
+              <PhantomRegularRedButton className={"mv4 w-100"}>
+                {"Contact sales"}
+              </PhantomRegularRedButton>
+            </Link>
+          )}
+          {items.map((item): JSX.Element => {
+            return (
+              <CardItem key={`${item.text}`}>
+                <CloudImage
+                  alt={`plan-check`}
+                  src={`airs/plans/${item.check ? "check" : "xmark"}`}
+                  styles={"mr3 check-icon"}
+                />
+                {item.text}
+              </CardItem>
+            );
+          })}
+        </CardItemsContainer>
+      </CardContainer>
+    </div>
+  );
+};
+
+export { PlanCard };
---
+++ airs/front/src/components/PlansPage/PlansSection/PlanCard/styledComponents.tsx
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+
+const CardContainer = styled.div.attrs({
+  className: `
+    mh3
+    mb4
+    pv4
+    ph4
+    tc
+    br4
+    w-100
+    center
+  `,
+})<{ isMachine: boolean }>`
+  max-width: 550px;
+  margin-top: ${({ isMachine }): string => (isMachine ? "0" : "47px")};
+  background-color: ${({ isMachine }): string =>
+    isMachine ? "#ffffff" : "#f4f4f6"};
+  border-top-left-radius: ${({ isMachine }): string =>
+    isMachine ? "unset !important" : "1rem"};
+  border-top-right-radius: ${({ isMachine }): string =>
+    isMachine ? "unset !important" : "1rem"};
+  box-shadow: 0px 0px 6px 3px rgba(0, 0, 0, 0.06);
+`;
+
+const CardTitleContainer = styled.div.attrs({
+  className: `
+    mb3
+    tl
+  `,
+})``;
+
+const CardItemsContainer = styled.div.attrs({
+  className: `
+    center
+    tl
+  `,
+})``;
+
+const CardItem = styled.div.attrs({
+  className: `
+    f5
+    flex
+    pb3
+    mb3
+    c-black-gray
+    roboto
+    ma0
+    tl
+  `,
+})``;
+
+const MachineHead = styled.div.attrs({
+  className: `
+    white
+    f4
+    dib
+    roboto
+    w-100
+    mh3
+    center
+  `,
+})`
+  max-width: 550px;
+  padding: 10px 16px;
+  background-color: #b0b0bf;
+  border-top-left-radius: 1rem;
+  border-top-right-radius: 1rem;
+`;
+
+export {
+  CardContainer,
+  CardItem,
+  CardItemsContainer,
+  CardTitleContainer,
+  MachineHead,
+};
