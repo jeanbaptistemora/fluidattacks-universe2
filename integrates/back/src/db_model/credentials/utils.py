0b23074dadeaabb0564d4bece7acab2014db5ec3
Andres Aguirre <aaguirre@fluidattacks.com>
2022-06-07 20:58:17 +0000

    integrates\feat(back): #6579.4 credential formatters
    - Add formatters to transform the db item into nametuples

---
+++ integrates/back/src/db_model/credentials/utils.py
@@ -0,0 +1,47 @@
+from db_model.credentials.types import (
+    Credential,
+    CredentialNewState,
+    HttpsPatSecret,
+    HttpsSecret,
+    SshSecret,
+)
+from db_model.enums import (
+    CredentialType,
+)
+from dynamodb.types import (
+    Item,
+)
+from typing import (
+    Union,
+)
+
+
+def format_secret(
+    credential_type: CredentialType, item: Item
+) -> Union[HttpsPatSecret, HttpsSecret, SshSecret]:
+    if credential_type is CredentialType.HTTPS:
+        if "token" in item:
+            return HttpsPatSecret(token=item["token"])
+
+        return HttpsSecret(
+            user=item["user"],
+            password=item["password"],
+        )
+
+    return SshSecret(key=item["key"])
+
+
+def format_credential(item: Item) -> Credential:
+    credential_type = CredentialType(item["state"]["type"])
+    return Credential(
+        id=item["id"],
+        organization_id=item["organization_id"],
+        owner=item["owner"],
+        state=CredentialNewState(
+            modified_by=item["state"]["modified_by"],
+            modified_date=item["state"]["modified_date"],
+            name=item["state"]["name"],
+            secret=format_secret(credential_type, item["state"]["secret"]),
+            type=credential_type,
+        ),
+    )
