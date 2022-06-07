915117979d85cde318efd2f36a6244dae6af5e8e
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-06-07 09:08:19 -0500

    skims\refac(back): #6074.15 add method
    - Add html method from asserts to skims
    - no-deltas-test

--- skims/skims/lib_path/f065/html.py
+++ skims/skims/lib_path/f065/html.py
@@ -8,6 +8,11 @@ from model.core_model import (
     MethodsEnum,
     Vulnerabilities,
 )
+from pyparsing import (
+    CaselessKeyword,
+    makeHTMLTags,
+    ParseException,
+)
 from typing import (
     Iterator,
     Tuple,
@@ -66,3 +71,65 @@ def has_autocomplete(content: str, path: str) -> Vulnerabilities:
         path=path,
         method=MethodsEnum.HAS_AUTOCOMPLETE,
     )
+
+
+def _has_attributes(content: str, tag: str, attrs: dict) -> bool:
+    """
+    Check ``HTML`` attributes` values.
+
+    This method checks whether the tag (``tag``) inside the code file
+    (``filename``) has attributes (``attr``) with the specific values.
+
+    :param filename: Path to the ``HTML`` source.
+    :param tag: ``HTML`` tag to search.
+    :param attrs: Attributes with values to search.
+    :returns: True if attribute set as specified, False otherwise.
+    """
+
+    tag_s, _ = makeHTMLTags(tag)
+    tag_expr = tag_s
+
+    result = False
+
+    for expr in tag_expr.searchString(content):
+        for attr, value in attrs.items():
+            try:
+                value.parseString(getattr(expr, attr))
+                result = True
+            except ParseException:
+                result = False
+                break
+        if result:
+            break
+
+    return result
+
+
+def is_cacheable(content: str) -> tuple:
+    """Check if cache is possible.
+
+    Verifies if the file has the tags::
+       <META HTTP-EQUIV="Pragma" CONTENT="no-cache"> and
+       <META HTTP-EQUIV="Expires" CONTENT="-1">
+
+    :param filename: Path to the ``HTML`` source.
+    :returns: True if tag ``meta`` have attributes ``http-equiv``
+              and ``content`` set as specified, False otherwise.
+    :rtype: :class:`fluidasserts.Result`
+    """
+
+    tag = "meta"
+    tk_pragma = CaselessKeyword("pragma")
+    tk_nocache = CaselessKeyword("no-cache")
+    pragma_attrs = {"http-equiv": tk_pragma, "content": tk_nocache}
+
+    tk_expires = CaselessKeyword("expires")
+    tk_minusone = CaselessKeyword("-1")
+    expires_attrs = {"http-equiv": tk_expires, "content": tk_minusone}
+
+    has_pragma = _has_attributes(content, tag, pragma_attrs)
+    has_expires = _has_attributes(content, tag, expires_attrs)
+
+    vulnerable: bool = not has_pragma or not has_expires  # noqa
+
+    return "test", "test"
