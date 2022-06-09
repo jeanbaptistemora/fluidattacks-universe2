b682ca9939dec3309fa60dc1e0b9f0699962cd05
Daniel Murcia <dmurcia@fluidattacks.com>
2022-06-09 15:52:46 -0500

    observes\feat(build): #6649.2 build with nix
    - build pkg with nix init

---
+++ observes/singer/target-redshift/build/deps/pkg_override.nix
@@ -0,0 +1,22 @@
+let
+  pkg_override = is_pkg: new_pkg: let
+    override = x:
+      if x ? overridePythonAttrs && is_pkg x
+      then new_pkg
+      else pkg_override is_pkg new_pkg x;
+  in
+    pkg:
+      if pkg ? overridePythonAttrs
+      then
+        pkg.overridePythonAttrs (
+          old: {
+            checkInputs = map override (old.checkInputs or []);
+            buildInputs = map override (old.buildInputs or []);
+            nativeBuildInputs = map override (old.nativeBuildInputs or []);
+            propagatedBuildInputs = map override (old.propagatedBuildInputs or []);
+            pythonPath = map override (old.pythonPath or []);
+          }
+        )
+      else pkg;
+in
+  pkg_override
---
+++ observes/singer/target-redshift/build/pkg/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  metadata,
+  python_pkgs,
+  src,
+}: let
+  runtime_deps = with python_pkgs; [
+    boto3
+    click
+    jsonschema
+    psycopg2
+    returns
+  ];
+  build_deps = with python_pkgs; [poetry];
+  test_deps = with python_pkgs; [
+    import-linter
+    mypy
+    pytest
+    toml
+    types-toml
+  ];
+  pkg = (import ./build.nix) {
+    inherit lib src metadata runtime_deps build_deps test_deps;
+  };
+  build_env = extraLibs:
+    lib.buildEnv {
+      inherit extraLibs;
+      ignoreCollisions = false;
+    };
+in {
+  inherit pkg;
+  env.runtime = build_env runtime_deps;
+  env.dev = build_env (runtime_deps ++ test_deps ++ build_deps);
+  env.bin = build_env [pkg];
+}
