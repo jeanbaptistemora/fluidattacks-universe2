8d149c88122a4ff0470b7820cbc594b498a868b8
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-06-09 09:24:54 -0500

    skims\feat(back): #6074.25 add method
    - Add init for f097
    - no-deltas-test

---
+++ skims/skims/lib_path/f097/__init__.py
@@ -0,0 +1,36 @@
+from lib_path.common import (
+    SHIELD_BLOCKING,
+)
+from lib_path.f097.html import (
+    has_reverse_tabnabbing,
+)
+from model.core_model import (
+    Vulnerabilities,
+)
+from typing import (
+    Callable,
+    Tuple,
+)
+
+
+@SHIELD_BLOCKING
+def run_has_reverse_tabnabbing(content: str, path: str) -> Vulnerabilities:
+    return has_reverse_tabnabbing(content=content, path=path)
+
+
+@SHIELD_BLOCKING
+def analyze(
+    content_generator: Callable[[], str],
+    file_extension: str,
+    path: str,
+    **_: None,
+) -> Tuple[Vulnerabilities, ...]:
+    results: Tuple[Vulnerabilities, ...] = ()
+
+    if file_extension == "html":
+        results = (
+            *results,
+            run_has_reverse_tabnabbing(content_generator(), path),
+        )
+
+    return results
