480301877164e1841c6f1fe46930d992d4962483
Andres Aguirre <aaguirre@fluidattacks.com>
2022-06-10 20:12:44 +0000

    integrates\feat(back): #6579.16 credentials in org deletion
    - Remove the organization credentials when the organization is deleted

--- integrates/back/src/schedulers/delete_obsolete_orgs.py
+++ integrates/back/src/schedulers/delete_obsolete_orgs.py
@@ -12,6 +12,9 @@ from dataloaders import (
     Dataloaders,
     get_new_context,
 )
+from db_model.credentials.types import (
+    Credential,
+)
 from db_model.groups.enums import (
     GroupStateRemovalJustification,
 )
@@ -73,6 +76,20 @@ async def _remove_organization(
     group_names = await orgs_domain.get_group_names(loaders, organization_id)
     await collect(
         _remove_group(loaders, group, modified_by) for group in group_names
+    )
+    organization_credentials: tuple[
+        Credential, ...
+    ] = await loaders.organization_credentials_new.load(organization_id)
+    await collect(
+        tuple(
+            orgs_domain.remove_credential(
+                loaders=loaders,
+                organization_id=organization_id,
+                credential_id=credential.id,
+                modified_by=modified_by,
+            )
+            for credential in organization_credentials
+        )
     )
     if success:
         await orgs_domain.update_state(
