714da741e19ac1d320cd0bb91756c69e528d6d61
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-06-13 11:57:18 -0500

    skims\feat(back): #6074.35 add method
    - Migrate assert method to skims
    - Add a method for F086
    - no-deltas-test

---
+++ skims/skims/lib_path/f086/__init__.py
@@ -0,0 +1,38 @@
+from lib_path.common import (
+    SHIELD_BLOCKING,
+)
+from lib_path.f086.html import (
+    has_not_subresource_integrity,
+)
+from model.core_model import (
+    Vulnerabilities,
+)
+from typing import (
+    Callable,
+    Tuple,
+)
+
+
+@SHIELD_BLOCKING
+def run_has_not_subresource_integrity(
+    content: str, path: str
+) -> Vulnerabilities:
+    return has_not_subresource_integrity(content=content, path=path)
+
+
+@SHIELD_BLOCKING
+def analyze(
+    content_generator: Callable[[], str],
+    file_extension: str,
+    path: str,
+    **_: None,
+) -> Tuple[Vulnerabilities, ...]:
+    results: Tuple[Vulnerabilities, ...] = ()
+
+    if file_extension == "html":
+        results = (
+            *results,
+            run_has_not_subresource_integrity(content_generator(), path),
+        )
+
+    return results
---
+++ skims/skims/lib_path/f086/html.py
@@ -0,0 +1,55 @@
+from bs4 import (
+    BeautifulSoup,
+)
+from lib_path.common import (
+    get_vulnerabilities_from_iterator_blocking,
+)
+from model.core_model import (
+    MethodsEnum,
+    Vulnerabilities,
+)
+from typing import (
+    Iterator,
+    Tuple,
+)
+
+
+def has_not_subresource_integrity(content: str, path: str) -> Vulnerabilities:
+    def iterator() -> Iterator[Tuple[int, int]]:
+        r"""
+        Check if elements fetched by the provided HTML have `SRI`.
+
+        See: `Documentation <https://developer.mozilla.org/en-US/
+        docs/Web/Security/Subresource_Integrity>`_.
+
+        :param path: Path to the ``HTML`` source.
+        :rtype: :class:`fluidasserts.Result`
+        """
+
+        soup = BeautifulSoup(content, features="html.parser")
+
+        for elem_types in ("link", "script"):
+            for elem in soup(elem_types):
+                does_not_have_integrity: bool = elem.get("integrity") is None
+
+                if elem_types == "link":
+                    references_external_resource: bool = elem.get(
+                        "href", ""
+                    ).startswith("http")
+                elif elem_types == "script":
+                    references_external_resource = elem.get(
+                        "src", ""
+                    ).startswith("http")
+
+                if does_not_have_integrity and references_external_resource:
+                    yield elem.sourceline, elem.sourcepos
+
+    return get_vulnerabilities_from_iterator_blocking(
+        content=content,
+        description_key=(
+            "lib_path.f086.sub_resource_integrity.missing_integrity"
+        ),
+        iterator=iterator(),
+        path=path,
+        method=MethodsEnum.HTML_HAS_NOT_SUB_RESOURCE_INTEGRITY,
+    )
