1002cfb71909ed495ed618c0dbefff59a7af7097
David Acevedo <dacevedo@fluidattacks.com>
2022-06-14 09:00:08 -0500

    integrates\feat(front): #6330 storybook a11y
    - installed accessibility addon

--- integrates/front/.storybook/main.ts
+++ integrates/front/.storybook/main.ts
@@ -4,7 +4,11 @@ import { commonConfig } from "../webpack.common.config";
 const config: StorybookConfig = {
   core: { builder: "webpack5" },
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-a11y",
+    "@storybook/addon-essentials",
+    "@storybook/addon-links",
+  ],
   webpackFinal: (config) => ({
     ...config,
     resolve: {
