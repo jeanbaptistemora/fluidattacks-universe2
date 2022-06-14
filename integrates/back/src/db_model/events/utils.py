51fa66a51f6d0df565c65337e8a2cd9ee5ff590f
Jonathan Mesa <jmesa@fluidattacks.com>
2022-06-14 20:31:14 +0000

    integrates\feat(back): #6114.16 events model
    - add formatting helper for event item

--- integrates/back/src/db_model/events/utils.py
+++ integrates/back/src/db_model/events/utils.py
@@ -1,10 +1,79 @@
 from .types import (
     Event,
+    EventEvidence,
+    EventEvidences,
+    EventState,
+)
+from db_model.events.enums import (
+    EventAccessibility,
+    EventActionsAfterBlocking,
+    EventActionsBeforeBlocking,
+    EventAffectedComponents,
+    EventStateStatus,
+    EventType,
 )
 from dynamodb.types import (
     Item,
 )
 import simplejson as json  # type: ignore
+
+
+def format_evidences(evidences: Item) -> EventEvidences:
+    return EventEvidences(
+        file=EventEvidence(
+            file_name=evidences["file"]["file_name"],
+            modified_date=evidences["file"]["modified_date"],
+        )
+        if evidences.get("file")
+        else None,
+        image=EventEvidence(
+            file_name=evidences["image"]["file_name"],
+            modified_date=evidences["image"]["modified_date"],
+        )
+        if evidences.get("image")
+        else None,
+    )
+
+
+def format_event(item: Item) -> Event:
+    return Event(
+        action_after_blocking=EventActionsAfterBlocking[
+            item["action_after_blocking"]
+        ]
+        if item.get("action_after_blocking")
+        else None,
+        action_before_blocking=EventActionsBeforeBlocking[
+            item["action_before_blocking"]
+        ]
+        if item.get("action_before_blocking")
+        else None,
+        accessibility=set(
+            EventAccessibility[acc_item] for acc_item in item["accessibility"]
+        )
+        if item.get("accessibility")
+        else None,
+        affected_components=set(
+            EventAffectedComponents[aff_item]
+            for aff_item in item["affected_components"]
+        )
+        if item.get("affected_components")
+        else None,
+        client=item["client"],
+        context=item.get("context"),
+        description=item["description"],
+        evidences=format_evidences(item["evidences"]),
+        group_name=item["group_name"],
+        hacker=item["hacker"],
+        id=item["id"],
+        report_date=item["report_date"],
+        root_id=item.get("root_id"),
+        state=EventState(
+            modified_by=item["state"]["modified_by"],
+            modified_date=item["state"]["modified_date"],
+            status=EventStateStatus[item["state"]["status"]],
+        ),
+        type=EventType[item["type"]],
+    )
 
 
 def format_event_item(event: Event) -> Item:
