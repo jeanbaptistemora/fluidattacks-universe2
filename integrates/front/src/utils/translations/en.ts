40c0d82c39915146d2aa38da5471b3b668ba3a4c
David Acevedo <dacevedo@fluidattacks.com>
2022-06-16 08:31:53 -0500

    integrates\refac(front): #5171 rename button
    - updated button text

--- integrates/front/src/utils/translations/en.ts
+++ integrates/front/src/utils/translations/en.ts
@@ -1580,7 +1580,7 @@ export const enTranslations: ResourceKey = {
           name: "Group name",
           new: {
             group: "New group",
-            text: "New group",
+            text: "New project",
             tooltip: "Create a new group",
           },
           organization: {
