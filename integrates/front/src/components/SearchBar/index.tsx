6a066899d7efb40e5f735083c247e0ac26190bda
David Acevedo <dacevedo@fluidattacks.com>
2022-06-17 14:13:41 -0500

    integrates\feat(front): #6514 prepare search bar
    - added base component

---
+++ integrates/front/src/components/SearchBar/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import type { ISearchBarProps } from "./types";
+
+const SearchBar: React.FC<ISearchBarProps> = (): JSX.Element => {
+  return <div />;
+};
+
+export type { ISearchBarProps };
+export { SearchBar };
