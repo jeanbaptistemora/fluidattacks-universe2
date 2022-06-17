c7fb97e9dc8b999762ea92fbf122c849534800d4
Juan Echeverri <jecheverri@fluidattacks.com>
2022-06-17 09:39:07 -0500

    skims\feat(back): #6576.3 cover all possible method paths
    - Cover all possible method paths of f004 C# check

--- skims/skims/symbolic_eval/f004/member_access/c_sharp.py
+++ skims/skims/symbolic_eval/f004/member_access/c_sharp.py
@@ -2,12 +2,16 @@ from symbolic_eval.types import (
     SymbolicEvalArgs,
     SymbolicEvaluation,
 )
+from utils import (
+    string,
+)
 
 
 def cs_remote_command_execution(args: SymbolicEvalArgs) -> SymbolicEvaluation:
     ma_attr = args.graph.nodes[args.n_id]
     args.evaluation[args.n_id] = (
-        ma_attr["expression"] == "Diagnostics.Process"
+        ma_attr["expression"]
+        in string.build_attr_paths("System.Diagnostics.Process")
         and ma_attr["member"] == "Start"
     ) or ma_attr["member"] == "Execute"
     return SymbolicEvaluation(args.evaluation[args.n_id], args.triggers)
