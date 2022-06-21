c65414958f208760bb452229b303652fe1c612fc
Estid Lozano <elozano@fluidattacks.com>
2022-06-21 10:13:33 -0500

    integrates\feat(front): #6185.2 techinfo dropdown
    - TechnicalInfo in navbar uses Dropdown component

--- integrates/front/src/scenes/Dashboard/components/Navbar/TechnicalInfo/index.tsx
+++ integrates/front/src/scenes/Dashboard/components/Navbar/TechnicalInfo/index.tsx
@@ -1,13 +1,12 @@
 import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useCallback, useState } from "react";
-import { useDetectClickOutside } from "react-detect-click-outside";
+import React from "react";
 import { useTranslation } from "react-i18next";
 
-import { clickedPortal } from "../utils";
 import { ButtonOpacity } from "components/Button";
+import { Dropdown } from "components/Dropdown";
 import { ExternalLink } from "components/ExternalLink";
-import { Menu, MenuItem } from "components/Menu";
+import { MenuItem } from "components/Menu";
 import {
   ASM_DEPLOYMENT_DATE,
   CI_COMMIT_SHA,
@@ -17,44 +16,33 @@ import {
 export const TechnicalInfo: React.FC = (): JSX.Element => {
   const { t } = useTranslation();
 
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const toggleDropdown = useCallback((): void => {
-    setIsDropdownOpen((currentValue): boolean => !currentValue);
-  }, []);
-  const ref = useDetectClickOutside({
-    onTriggered: (event): void => {
-      // Exclude clicks in portals to prevent modals from closing the dropdown
-      if (!clickedPortal(event)) {
-        setIsDropdownOpen(false);
-      }
-    },
-  });
-
   return (
-    <div ref={ref}>
-      <ButtonOpacity onClick={toggleDropdown}>
-        <FontAwesomeIcon color={"#2e2e38"} icon={faInfoCircle} />
-      </ButtonOpacity>
-      {isDropdownOpen ? (
-        <Menu align={"right"}>
-          <MenuItem>
-            <button>
-              <div>
-                {t("info.commit")}&nbsp;
-                <ExternalLink
-                  href={`https://gitlab.com/fluidattacks/product/-/tree/${CI_COMMIT_SHA}`}
-                >
-                  {CI_COMMIT_SHORT_SHA}
-                </ExternalLink>
-              </div>
-              <small>
-                {t("info.deploymentDate")}&nbsp;
-                {ASM_DEPLOYMENT_DATE}
-              </small>
-            </button>
-          </MenuItem>
-        </Menu>
-      ) : undefined}
-    </div>
+    <Dropdown
+      align={"left"}
+      button={
+        <ButtonOpacity>
+          <FontAwesomeIcon color={"#2e2e38"} icon={faInfoCircle} />
+        </ButtonOpacity>
+      }
+    >
+      <MenuItem>
+        <button>
+          <p className={"f5 ma0"}>
+            {t("info.commit")}
+            &nbsp;
+            <ExternalLink
+              href={`https://gitlab.com/fluidattacks/product/-/tree/${CI_COMMIT_SHA}`}
+            >
+              {CI_COMMIT_SHORT_SHA}
+            </ExternalLink>
+          </p>
+          <p className={"f6 ma0"}>
+            {t("info.deploymentDate")}
+            &nbsp;
+            {ASM_DEPLOYMENT_DATE}
+          </p>
+        </button>
+      </MenuItem>
+    </Dropdown>
   );
 };
