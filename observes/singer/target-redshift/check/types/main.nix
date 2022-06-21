546ccc856581fad0e05e538340511a76c09733ce
Daniel Murcia <dmurcia@fluidattacks.com>
2022-06-21 10:13:09 -0500

    observes\feat(cross): #6649.15 check jobs
    - add tests and test check
    - add arch check
    - refac lint check

---
+++ observes/singer/target-redshift/check/types/main.nix
@@ -0,0 +1,18 @@
+{
+  fetchNixpkgs,
+  inputs,
+  makeScript,
+  projectPath,
+  ...
+}: let
+  root = projectPath inputs.observesIndex.target.redshift.root;
+  pkg = import "${root}/entrypoint.nix" fetchNixpkgs projectPath inputs.observesIndex;
+  check = pkg.check.types;
+in
+  makeScript {
+    searchPaths = {
+      bin = [check];
+    };
+    name = "observes-singer-target-redshift-check-types";
+    entrypoint = "";
+  }
