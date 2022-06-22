2a52b60f2691fc6e2e1bd7859b3e5212bc90896f
Juan Villegas <jvillegas@fluidattacks.com>
2022-06-22 15:50:59 -0500

    integrates\test(back): #6629.2 event notifications
    - Added fn test to send event report email

--- integrates/back/test/unit/src/domain_test/test_mailer.py
+++ integrates/back/test/unit/src/domain_test/test_mailer.py
@@ -1,3 +1,4 @@
+# pylint: disable=too-many-arguments
 from aioextensions import (
     collect,
 )
@@ -8,6 +9,9 @@ from dataloaders import (
     Dataloaders,
     get_new_context,
 )
+from datetime import (
+    datetime,
+)
 from group_access.domain import (
     get_group_users,
 )
@@ -16,6 +20,9 @@ from groups.domain import (
 )
 from mailer.common import (
     get_recipient_first_name,
+)
+from mailer.events import (
+    send_mail_event_report,
 )
 import pytest
 
@@ -59,4 +66,67 @@ async def test_get_recipient_first_name() -> None:
 
     assert len([recipient for recipient in recipients if recipient]) < len(
         users
+    )
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    [
+        "group_name",
+        "event_id",
+        "event_type",
+        "description",
+        "reason",
+        "other",
+        "is_closed",
+    ],
+    [
+        [
+            "unittesting",
+            "538745942",
+            "AUTHORIZATION_SPECIAL_ATTACK",
+            "Test",
+            None,
+            None,
+            False,
+        ],
+        [
+            "unittesting",
+            "538745942",
+            "AUTHORIZATION_SPECIAL_ATTACK",
+            "Test",
+            "PROBLEM_SOLVED",
+            None,
+            True,
+        ],
+        [
+            "unittesting",
+            "538745942",
+            "AUTHORIZATION_SPECIAL_ATTACK",
+            "Test",
+            "OTHER",
+            "Test",
+            True,
+        ],
+    ],
+)
+async def test_send_event_report(
+    group_name: str,
+    event_id: str,
+    event_type: str,
+    description: str,
+    reason: str,
+    other: str,
+    is_closed: bool,
+) -> None:
+    await send_mail_event_report(
+        loaders=get_new_context(),
+        group_name=group_name,
+        event_id=event_id,
+        event_type=event_type,
+        description=description,
+        reason=reason,
+        other=other,
+        is_closed=is_closed,
+        report_date=datetime(2022, 6, 16).date(),
     )
