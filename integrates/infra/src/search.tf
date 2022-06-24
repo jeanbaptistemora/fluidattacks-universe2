2788893694685a0b322f42b13386354a210175c7
David Acevedo <dacevedo@fluidattacks.com>
2022-06-24 15:51:45 -0500

    integrates\feat(infra): #6514 prepare opensearch
    - added iam role for the upcoming lambda

--- integrates/infra/src/search.tf
+++ integrates/infra/src/search.tf
@@ -17,3 +17,30 @@ resource "aws_opensearch_domain" "integrates" {
     "management:type"    = "product"
   }
 }
+
+resource "aws_iam_role" "opensearch_dynamodb_lambda_role" {
+  name = "opensearch_dynamodb_lambda_role"
+
+  assume_role_policy = <<-EOF
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Effect": "Allow",
+          "Principal": {
+            "Service": "lambda.amazonaws.com"
+          },
+          "Sid": ""
+        }
+      ]
+    }
+  EOF
+
+  tags = {
+    "Name"               = "integrates-opensearch"
+    "management:area"    = "cost"
+    "management:product" = "integrates"
+    "management:type"    = "product"
+  }
+}
