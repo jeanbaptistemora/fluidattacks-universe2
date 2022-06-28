3eba39d6dc0e785cc7533d5afbe13efc044194a3
Daniel Murcia <dmurcia@fluidattacks.com>
2022-06-28 09:20:19 -0500

    integrates\feat(back): #6733.2 last update client
    - add last update client

---
+++ integrates/back/src/redshift/last_sync/__init__.py
@@ -0,0 +1,48 @@
+from . import (
+    _queries,
+)
+from ._queries import (
+    RepoId,
+)
+from context import (
+    FI_ENVIRONMENT,
+)
+from dataclasses import (
+    dataclass,
+)
+import logging
+from psycopg2.extensions import (
+    cursor as cursor_cls,
+)
+
+LOGGER = logging.getLogger(__name__)
+
+
+@dataclass(frozen=True)
+class LastUpdateClient:
+    cursor: cursor_cls
+
+    def exist(self, repo: RepoId) -> bool:
+        if FI_ENVIRONMENT == "production":
+            query = _queries.get(repo)
+            self.cursor.execute(query.query, query.values)
+            result = self.cursor.fetchall()
+            return bool(result)
+        LOGGER.debug("DRY-RUN: exist last update indicator for %s", repo)
+        return False
+
+    def upsert_indicator(self, repo: RepoId) -> None:
+        if FI_ENVIRONMENT == "production":
+            query = (
+                _queries.update(repo)
+                if self.exist(repo)
+                else _queries.insert(repo)
+            )
+            self.cursor.execute(query.query, query.values)
+        else:
+            LOGGER.debug("DRY-RUN: refresh last update indicator for %s", repo)
+
+
+__all__ = [
+    "RepoId",
+]
