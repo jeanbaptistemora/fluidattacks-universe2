898374b582e756b65d96406b2c099d4013f38481
Daniel Murcia <dmurcia@fluidattacks.com>
2022-06-28 08:22:00 -0500

    integrates\feat(back): #6733.1 last update queries
    - add las update queries

---
+++ integrates/back/src/redshift/last_sync/_queries.py
@@ -0,0 +1,42 @@
+from dataclasses import (
+    dataclass,
+)
+from typing import (
+    Dict,
+)
+
+
+@dataclass(frozen=True)
+class RepoId:
+    group: str
+    name: str
+
+
+@dataclass(frozen=True)
+class Query:
+    query: str
+    values: Dict[str, str]
+
+
+def insert(repo: RepoId) -> Query:
+    query = (
+        "INSERT INTO last_update.repos "
+        "(group, repo, updated_at) VALUES ('%(group)s', '%(repo)s', getdate())"
+    )
+    return Query(query, {"group": repo.group, "repo": repo.name})
+
+
+def update(repo: RepoId) -> Query:
+    query = (
+        "UPDATE last_update.repos "
+        "set updated_at=getdate() WHERE group='%(group)s' AND repo='%(repo)s'"
+    )
+    return Query(query, {"group": repo.group, "repo": repo.name})
+
+
+def get(repo: RepoId) -> Query:
+    query = (
+        "SELECT * FROM last_update.repos"
+        " WHERE group='%(group)s' AND repo='%(repo)s'"
+    )
+    return Query(query, {"group": repo.group, "repo": repo.name})
