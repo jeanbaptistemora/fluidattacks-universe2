62bc5a59cac67321d54aaa7823c38bbea23ebe64
Daniel Murcia <dmurcia@fluidattacks.com>
2022-07-01 10:54:19 -0500

    observes\feat(infra): #6786.2 cache bucket
    - add cache s3 bucket
    - remove migration bucket

---
+++ observes/infra/src/cache.tf
@@ -0,0 +1,26 @@
+resource "aws_s3_bucket" "observes_cache" {
+  bucket = "observes.cache"
+
+  tags = {
+    "Name"               = "observes.cache"
+    "management:area"    = "cost"
+    "management:product" = "observes"
+    "management:type"    = "product"
+  }
+}
+
+resource "aws_s3_bucket_acl" "observes_cache" {
+  bucket = aws_s3_bucket.observes_cache.id
+
+  acl = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "observes_cache" {
+  bucket = aws_s3_bucket.observes_cache.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
