8a96e28189773f72a024119289f20a49f364bf91
John Perez <jperez@fluidattacks.com>
2022-07-06 14:24:42 -0500

    airs\refac(cross): #6788.6 update node
    - Return to react-share
    - Use overrides to update the react version on react-share
    - no-deltas-test

--- airs/front/src/components/BlogFooter/SocialNetworkList/index.tsx
+++ airs/front/src/components/BlogFooter/SocialNetworkList/index.tsx
@@ -1,33 +1,40 @@
 /* eslint react/forbid-component-props: 0 */
 import React from "react";
+import { FaFacebookF, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 import {
-  FaEllipsisH,
-  FaFacebookF,
-  FaLinkedinIn,
-  FaTwitter,
-  FaWhatsapp,
-} from "react-icons/fa";
-import { RWebShare } from "react-web-share";
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+} from "react-share";
 
 interface IProps {
   slug: string;
 }
 
 const SocialNetworkList: React.FC<IProps> = ({ slug }: IProps): JSX.Element => (
-  <RWebShare
-    data={{
-      title: "Share this post",
-      url: `https://fluidattacks.com/blog/${slug}`,
-    }}
-  >
-    <button className={"blog-link br3 ma1 pointer bg-transparent"}>
+  <React.Fragment>
+    <FacebookShareButton
+      className={"blog-link br3 ma1"}
+      title={"Share on Facebook"}
+      url={`https://fluidattacks.com/blog/${slug}`}
+    >
       <FaFacebookF className={"pa2 nb1 f4 black"} />
+    </FacebookShareButton>
+    <LinkedinShareButton
+      className={"blog-link br3 ma1"}
+      title={"Share on LinkedIn"}
+      url={`https://fluidattacks.com/blog/${slug}`}
+    >
       <FaLinkedinIn className={"pa2 nb1 f4 black"} />
+    </LinkedinShareButton>
+    <TwitterShareButton
+      className={"blog-link br3 ma1"}
+      title={"Share on Twitter"}
+      url={`https://fluidattacks.com/blog/${slug}`}
+    >
       <FaTwitter className={"pa2 nb1 f4 black"} />
-      <FaWhatsapp className={"pa2 nb1 f4 black"} />
-      <FaEllipsisH className={"pa2 nb1 f4 black"} />
-    </button>
-  </RWebShare>
+    </TwitterShareButton>
+  </React.Fragment>
 );
 
 export { SocialNetworkList };
