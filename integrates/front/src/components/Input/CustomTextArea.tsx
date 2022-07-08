5926e01723cc45d97dca425970e378bbc374015b
Estid Lozano <elozano@fluidattacks.com>
2022-07-08 13:23:47 -0500

    integrates\refac(front): #6573 textarea input
    - Added a separate TextArea input component
    - no-deltas-test

---
+++ integrates/front/src/components/Input/CustomTextArea.tsx
@@ -0,0 +1,71 @@
+import type { FieldProps } from "formik";
+import type { FC, FocusEvent, KeyboardEvent } from "react";
+import React, { useCallback } from "react";
+
+import type { IInputBase } from "./InputBase";
+import { InputBase } from "./InputBase";
+import { StyledTextArea } from "./styles";
+
+interface ITextAreaProps extends IInputBase {
+  onBlur?: (event: FocusEvent<HTMLTextAreaElement>) => void;
+  onFocus?: (event: FocusEvent<HTMLTextAreaElement>) => void;
+  onKeyDown?: (event: KeyboardEvent<HTMLTextAreaElement>) => void;
+  placeholder?: string;
+  rows?: number;
+}
+
+type TTextAreaProps = FieldProps<string, Record<string, string>> &
+  ITextAreaProps;
+
+const CustomTextArea: FC<TTextAreaProps> = ({
+  disabled,
+  field,
+  form,
+  id,
+  label,
+  onBlur,
+  onFocus,
+  onKeyDown,
+  placeholder,
+  rows = 3,
+  variant,
+}: Readonly<TTextAreaProps>): JSX.Element => {
+  const { name, onBlur: onBlurField, onChange, value } = field;
+  const alert = form.errors[name];
+
+  const handleBlur = useCallback(
+    (ev: FocusEvent<HTMLTextAreaElement>): void => {
+      onBlurField(ev);
+      onBlur?.(ev);
+    },
+    [onBlur, onBlurField]
+  );
+
+  return (
+    <InputBase
+      alert={alert}
+      id={id}
+      label={label}
+      name={name}
+      variant={variant}
+    >
+      <StyledTextArea
+        aria-label={name}
+        autoComplete={"off"}
+        disabled={disabled}
+        id={id}
+        name={name}
+        onBlur={handleBlur}
+        onChange={onChange}
+        onFocus={onFocus}
+        onKeyDown={onKeyDown}
+        placeholder={placeholder}
+        rows={rows}
+        value={value}
+      />
+    </InputBase>
+  );
+};
+
+export type { ITextAreaProps };
+export { CustomTextArea };
---
+++ integrates/front/src/components/Input/TextArea.tsx
@@ -0,0 +1,32 @@
+import { Field } from "formik";
+import type { FC } from "react";
+import React from "react";
+
+import type { ITextAreaProps } from "./CustomTextArea";
+import { CustomTextArea } from "./CustomTextArea";
+
+const TextArea: FC<ITextAreaProps> = ({
+  disabled,
+  id,
+  label,
+  name,
+  onBlur,
+  onFocus,
+  onKeyDown,
+  variant,
+}: Readonly<ITextAreaProps>): JSX.Element => (
+  <Field
+    component={CustomTextArea}
+    disabled={disabled}
+    id={id}
+    label={label}
+    name={name}
+    onBlur={onBlur}
+    onFocus={onFocus}
+    onKeyDown={onKeyDown}
+    type={"text"}
+    variant={variant}
+  />
+);
+
+export { TextArea };
