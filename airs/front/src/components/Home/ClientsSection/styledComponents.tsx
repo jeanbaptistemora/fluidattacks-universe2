23e3b8faf722dc8ce97c92a2115c80c4059ed650
Frank Aristizabal <faristizabal@fluidattacks.com>
2022-07-08 14:49:53 -0500

    airs\refac(front): #6847.1 update home
    - Update clients carousel.

--- airs/front/src/components/Home/ClientsSection/styledComponents.tsx
+++ airs/front/src/components/Home/ClientsSection/styledComponents.tsx
@@ -2,11 +2,11 @@ import styled from "styled-components";
 
 const Container = styled.div.attrs({
   className: `
+    db
     tc
     pv5
-    h-500
-    bg-darker-blue
     center
+    ph-body
   `,
 })<{ bgColor: string }>`
   background-color: ${({ bgColor }): string => bgColor};
@@ -15,51 +15,49 @@ const Container = styled.div.attrs({
 const ClientsContainer = styled.div.attrs({
   className: `
     w-100
-    mw-1920
     relative
     center
     overflow-hidden
   `,
 })<{ gradientColor: string }>`
   z-index: 0;
+  max-width: 1135px;
 
-  @media screen and (min-width: 30em) {
-    &::before,
-    &::after {
-      background: linear-gradient(
-        to left,
-        transparent,
-        ${({ gradientColor }): string => gradientColor}
-      );
-      content: "";
-      height: 100%;
-      position: absolute;
-      width: 200px;
-      z-index: 2;
-    }
+  &::before,
+  &::after {
+    background: linear-gradient(
+      to left,
+      transparent,
+      ${({ gradientColor }): string => gradientColor}
+    );
+    content: "";
+    height: 100%;
+    position: absolute;
+    width: 100px;
+    z-index: 2;
+  }
 
-    &::after {
-      right: 0;
-      top: 0;
-      transform: rotateZ(180deg);
-    }
+  &::after {
+    right: 0;
+    top: 0;
+    transform: rotateZ(180deg);
+  }
 
-    &::before {
-      left: 0;
-      top: 0;
-    }
+  &::before {
+    left: 0;
+    top: 0;
   }
 `;
 
 const SlideShow = styled.div.attrs({
   className: `
-    home-slide-track
+    product-slide-track
     flex
   `,
 })`
   > img {
-    max-width: 382px;
-    max-height: 200px;
+    max-width: 163px;
+    max-height: 85px;
   }
 `;
 
