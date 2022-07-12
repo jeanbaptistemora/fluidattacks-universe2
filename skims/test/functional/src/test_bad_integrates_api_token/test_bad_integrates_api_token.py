6c2109fe001388855de436daa70240b8577d15f4
Flor Calderon <fcalderon@fluidattacks.com>
2022-07-12 17:42:00 -0500

    skims\feat(back): #6798.6  functional tests fix
    -Adding test package for test_client and test_bad_integrates_api_token
    
    - Adding new test_groups
    
    - Delete test carpet 'group1'

---
+++ skims/test/functional/src/test_bad_integrates_api_token/test_bad_integrates_api_token.py
@@ -0,0 +1,52 @@
+from cli import (
+    cli,
+)
+from contextlib import (
+    redirect_stderr,
+    redirect_stdout,
+)
+import io
+import pytest
+from typing import (
+    Tuple,
+)
+from utils.logs import (
+    configure,
+)
+
+
+def skims(*args: str) -> Tuple[int, str, str]:
+    out_buffer, err_buffer = io.StringIO(), io.StringIO()
+
+    with redirect_stdout(out_buffer), redirect_stderr(err_buffer):
+        try:
+            configure()
+            cli.main(args=list(args), prog_name="skims")
+        except SystemExit as exc:  # NOSONAR
+            code: int = exc.code
+
+    try:
+        return code, out_buffer.getvalue(), err_buffer.getvalue()
+    finally:
+        del out_buffer
+        del err_buffer
+
+
+def get_suite_config(suite: str) -> str:
+    return f"skims/test/data/config/{suite}.yaml"
+
+
+@pytest.mark.skims_test_group("test_bad_integrates_api_token")
+def test_bad_integrates_api_token(test_group: str) -> None:
+    suite: str = "nothing_to_do"
+    code, stdout, stderr = skims(
+        "scan",
+        "--token",
+        "123",
+        "--group",
+        test_group,
+        get_suite_config(suite),
+    )
+    assert code == 1
+    assert "StopRetrying: Invalid API token" in stdout, stdout
+    assert not stderr, stderr
---
+++ skims/test/functional/src/test_client/test_client.py
@@ -0,0 +1,18 @@
+from integrates.graphql import (
+    client as graphql_client,
+)
+import pytest
+
+
+@pytest.mark.asyncio
+@pytest.mark.skims_test_group("test_client")
+@pytest.mark.usefixtures("test_integrates_session")
+async def test_client(
+    test_integrates_api_token: str,
+) -> None:
+    async with graphql_client() as client:
+        # pylint: disable=protected-access
+        assert client.session._default_headers == {
+            "authorization": f"Bearer {test_integrates_api_token}",
+            "x-integrates-source": "skims",
+        }
