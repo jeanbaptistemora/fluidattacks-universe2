783a8de964e5b049a9ef7f0fe1e028c500284a5c
asaldarriagaatfluid <asaldarriaga@fluidattacks.com>
2022-07-15 11:23:50 -0500

    skims\feat(back): #6798.11 functional tests fix
    - Add utils.py file with functions that the tests need

--- skims/test/functional/src/bad_api_token/test_script.py
+++ skims/test/functional/src/bad_api_token/test_script.py
@@ -1,39 +1,8 @@
-from cli import (
-    cli,
-)
-from contextlib import (
-    redirect_stderr,
-    redirect_stdout,
-)
-import io
 import pytest
-from typing import (
-    Tuple,
+from utils_test import (
+    get_suite_config,
+    skims,
 )
-from utils.logs import (
-    configure,
-)
-
-
-def skims(*args: str) -> Tuple[int, str, str]:
-    out_buffer, err_buffer = io.StringIO(), io.StringIO()
-
-    with redirect_stdout(out_buffer), redirect_stderr(err_buffer):
-        try:
-            configure()
-            cli.main(args=list(args), prog_name="skims")
-        except SystemExit as exc:  # NOSONAR
-            code: int = exc.code
-
-    try:
-        return code, out_buffer.getvalue(), err_buffer.getvalue()
-    finally:
-        del out_buffer
-        del err_buffer
-
-
-def get_suite_config(suite: str) -> str:
-    return f"skims/test/data/config/{suite}.yaml"
 
 
 @pytest.mark.skims_test_group("bad_api_token")
