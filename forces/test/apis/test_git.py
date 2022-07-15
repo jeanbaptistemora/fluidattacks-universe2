faf296013a0b2c9ee6baa98d7cc1cd1cdc736438
Daniel Salazar <dsalazar@fluidattacks.com>
2022-07-14 19:18:48 -0500

    forces\refac(back): #6820 rename repo
    - Rename product to universe

--- forces/test/apis/test_git.py
+++ forces/test/apis/test_git.py
@@ -17,32 +17,32 @@ import pytest
 @pytest.mark.asyncio
 def test_get_repository_metadata_test() -> None:
     result = get_repository_metadata(repo_path=".")
-    assert result["git_repo"] == "product"
-    assert "fluidattacks/product" in result["git_origin"]
+    assert result["git_repo"] == "universe"
+    assert "fluidattacks/universe" in result["git_origin"]
     assert result["git_branch"] != "master"
 
 
 @pytest.mark.asyncio
 async def test_check_remotes(test_token: str) -> None:
     set_api_token(test_token)
-    config = ForcesConfig(group="herrin", repository_name="product")
+    config = ForcesConfig(group="herrin", repository_name="universe")
     assert await check_remotes(config)
 
-    bad_config = ForcesConfig(group="herrin", repository_name="products")
+    bad_config = ForcesConfig(group="herrin", repository_name="universes")
     assert not await check_remotes(bad_config)
 
 
 def test_get_repo_name_from_vars() -> None:
-    os.environ["REPO_NAME"] = "product"
-    assert get_repo_name_from_vars() == "product"
+    os.environ["REPO_NAME"] = "universe"
+    assert get_repo_name_from_vars() == "universe"
 
 
 def test_extract_repo_name() -> None:
     assert (
-        extract_repo_name("git@gitlab.com:fluidattacks/product.git")
-        == "product"
+        extract_repo_name("git@gitlab.com:fluidattacks/universe.git")
+        == "universe"
     )
     assert (
-        extract_repo_name("https://127.0.0..1:8080/fluidattacks/product")
-        == "product"
+        extract_repo_name("https://127.0.0..1:8080/fluidattacks/universe")
+        == "universe"
     )
