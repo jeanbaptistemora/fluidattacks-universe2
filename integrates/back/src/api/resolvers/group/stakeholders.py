fbb1fb5bcfc8ce9185739341ebd9235b307b2e0a
Jonathan Mesa <jmesa@fluidattacks.com>
2022-07-18 17:29:52 +0000

    integrates\refac(back): #6110.72 stakeholders resolvers
    - rm redis for these resolvers
    - rm unneeded formatting

--- integrates/back/src/api/resolvers/group/stakeholders.py
+++ integrates/back/src/api/resolvers/group/stakeholders.py
@@ -12,27 +12,14 @@ from decorators import (
     enforce_group_level_auth_async,
     require_asm,
 )
-from functools import (
-    partial,
-)
 from graphql.type.definition import (
     GraphQLResolveInfo,
 )
 from newutils import (
     token as token_utils,
 )
-from newutils.stakeholders import (
-    format_stakeholder,
-    format_stakeholder_item,
-)
-from redis_cluster.operations import (
-    redis_get_or_set_entity_attr,
-)
 from stakeholders import (
     domain as stakeholders_domain,
-)
-from typing import (
-    Any,
 )
 
 
@@ -43,45 +30,25 @@ from typing import (
 async def resolve(
     parent: Group,
     info: GraphQLResolveInfo,
-    **kwargs: None,
-) -> list[Stakeholder]:
+    **_kwargs: None,
+) -> tuple[Stakeholder, ...]:
+    loaders: Dataloaders = info.context.loaders
     # The store is needed to resolve stakeholder's role
     request_store = token_utils.get_request_store(info.context)
     request_store["entity"] = "GROUP"
     request_store["group_name"] = parent.name
     user_data: dict[str, str] = await token_utils.get_jwt_content(info.context)
     user_email: str = user_data["user_email"]
+    stakeholders: tuple[
+        Stakeholder, ...
+    ] = await loaders.group_stakeholders.load(parent.name)
 
-    response: list[dict[str, Any]] = await redis_get_or_set_entity_attr(
-        partial(resolve_no_cache, parent, info, **kwargs),
-        entity="group",
-        attr="stakeholders",
-        name=parent.name,
-    )
-    stakeholders: list[Stakeholder] = [
-        format_stakeholder(item_legacy=stakeholder, item_vms=None)
-        for stakeholder in response
-    ]
     exclude_fluid_staff = not stakeholders_domain.is_fluid_staff(user_email)
     if exclude_fluid_staff:
-        stakeholders = [
+        stakeholders = tuple(
             stakeholder
             for stakeholder in stakeholders
             if not stakeholders_domain.is_fluid_staff(stakeholder.email)
-        ]
+        )
+
     return stakeholders
-
-
-async def resolve_no_cache(
-    parent: Group,
-    info: GraphQLResolveInfo,
-    **_kwargs: None,
-) -> list[dict[str, Any]]:
-    loaders: Dataloaders = info.context.loaders
-    group_name: str = parent.name
-    group_stakeholders = await loaders.group_stakeholders.load(group_name)
-    group_stakeholders_item = [
-        format_stakeholder_item(group_stakeholder)
-        for group_stakeholder in group_stakeholders
-    ]
-    return group_stakeholders_item
--- integrates/back/src/api/resolvers/organization/stakeholders.py
+++ integrates/back/src/api/resolvers/organization/stakeholders.py
@@ -10,24 +10,11 @@ from db_model.stakeholders.types import (
 from decorators import (
     enforce_organization_level_auth_async,
 )
-from functools import (
-    partial,
-)
 from graphql.type.definition import (
     GraphQLResolveInfo,
 )
 from newutils import (
     token as token_utils,
-)
-from newutils.stakeholders import (
-    format_stakeholder,
-    format_stakeholder_item,
-)
-from redis_cluster.operations import (
-    redis_get_or_set_entity_attr,
-)
-from typing import (
-    Any,
 )
 
 
@@ -35,37 +22,12 @@ from typing import (
 async def resolve(
     parent: Organization,
     info: GraphQLResolveInfo,
-    **kwargs: None,
-) -> list[Stakeholder]:
+    **_kwargs: None,
+) -> tuple[Stakeholder, ...]:
+    loaders: Dataloaders = info.context.loaders
     # The store is needed to resolve stakeholder's role
     request_store = token_utils.get_request_store(info.context)
     request_store["entity"] = "ORGANIZATION"
     request_store["organization_id"] = parent.id
 
-    response: list[dict[str, Any]] = await redis_get_or_set_entity_attr(
-        partial(resolve_no_cache, parent, info, **kwargs),
-        entity="organization",
-        attr="stakeholders",
-        id=parent.id,
-    )
-    stakeholders: list[Stakeholder] = [
-        format_stakeholder(item_legacy=stakeholder, item_vms=None)
-        for stakeholder in response
-    ]
-    return stakeholders
-
-
-async def resolve_no_cache(
-    parent: Organization,
-    info: GraphQLResolveInfo,
-    **_kwargs: None,
-) -> list[dict[str, Any]]:
-    loaders: Dataloaders = info.context.loaders
-    org_stakeholders: tuple[
-        Stakeholder, ...
-    ] = await loaders.organization_stakeholders.load(parent.id)
-    org_stakeholders_item = [
-        format_stakeholder_item(group_stakeholder)
-        for group_stakeholder in org_stakeholders
-    ]
-    return org_stakeholders_item
+    return await loaders.organization_stakeholders.load(parent.id)
