4a7556590d20c16189a6fabd29d00b4e9623291c
Diego Restrepo <drestrepo@fluidattacks.com>
2022-07-18 18:35:27 -0500

    skims\refac(back): #6866.6 move server to skims
    - add dependencies

--- skims/config/runtime/pypi/main.nix
+++ skims/config/runtime/pypi/main.nix
@@ -7,11 +7,16 @@ makePythonPypiEnvironment rec {
   name = "skims-runtime";
   searchPathsBuild = {
     bin = [
+      inputs.nixpkgs.curl
       inputs.nixpkgs.gcc
       inputs.nixpkgs.gnutar
       inputs.nixpkgs.gzip
+      inputs.nixpkgs.python38Packages.pycurl
     ];
-    pythonPackage38 = [inputs.nixpkgs.python38Packages.pygraphviz];
+    pythonPackage38 = [
+      inputs.nixpkgs.python38Packages.pygraphviz
+      inputs.nixpkgs.python38Packages.pycurl
+    ];
   };
   searchPathsRuntime = searchPathsBuild;
   sourcesYaml = ./sources.yaml;
