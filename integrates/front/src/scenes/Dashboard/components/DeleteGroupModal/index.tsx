912c4f23f5764b970da73c65635926dd6b78d66b
Estid Lozano <elozano@fluidattacks.com>
2022-07-20 08:47:56 -0500

    integrates\refac(front): #6686 deletegroup inputs
    - Replace Formik Inputs, ControlLabels, etc. by Input and Select
    - Clean code
    - no-deltas-test

--- integrates/front/src/scenes/Dashboard/components/DeleteGroupModal/index.tsx
+++ integrates/front/src/scenes/Dashboard/components/DeleteGroupModal/index.tsx
@@ -1,11 +1,12 @@
 import { Field, Formik } from "formik";
-import React from "react";
+import type { FC } from "react";
+import React, { Fragment, StrictMode } from "react";
 import { useTranslation } from "react-i18next";
 
+import { Select } from "components/Input";
 import { Modal, ModalConfirm } from "components/Modal";
-import { Tooltip } from "components/Tooltip";
-import { Alert, ControlLabel, FormGroup } from "styles/styledComponents";
-import { FormikDropdown, FormikText } from "utils/forms/fields";
+import { Alert, ControlLabel } from "styles/styledComponents";
+import { FormikText } from "utils/forms/fields";
 import { required } from "utils/validations";
 
 interface IDeleteGroupModalProps {
@@ -15,7 +16,7 @@ interface IDeleteGroupModalProps {
   onSubmit: (values: { confirmation: string; reason: string }) => void;
 }
 
-const DeleteGroupModal: React.FC<IDeleteGroupModalProps> = ({
+const DeleteGroupModal: FC<IDeleteGroupModalProps> = ({
   groupName,
   isOpen,
   onClose,
@@ -38,7 +39,7 @@ const DeleteGroupModal: React.FC<IDeleteGroupModalProps> = ({
   }
 
   return (
-    <React.StrictMode>
+    <StrictMode>
       <Modal
         open={isOpen}
         title={t("searchFindings.servicesTable.deleteGroup.deleteGroup")}
@@ -53,77 +54,66 @@ const DeleteGroupModal: React.FC<IDeleteGroupModalProps> = ({
           validate={formValidations}
         >
           {({ submitForm, isValid, dirty }): JSX.Element => (
-            <React.Fragment>
+            <Fragment>
               <ControlLabel>
                 {t("searchFindings.servicesTable.deleteGroup.warningTitle")}
               </ControlLabel>
               <Alert>
                 {t("searchFindings.servicesTable.deleteGroup.warningBody")}
               </Alert>
-              <FormGroup>
-                <ControlLabel>
-                  {t("searchFindings.servicesTable.deleteGroup.typeGroupName")}
-                </ControlLabel>
-                <Field
-                  component={FormikText}
-                  name={"confirmation"}
-                  placeholder={groupName.toLowerCase()}
-                  type={"text"}
-                  validate={required}
-                />
-              </FormGroup>
-              <FormGroup>
-                <ControlLabel>
-                  {t("searchFindings.servicesTable.deleteGroup.reason.title")}
-                </ControlLabel>
-                <Tooltip
-                  id={"searchFindings.servicesTable.deleteGroup.reason.tooltip"}
-                  place={"top"}
-                  tip={t(
-                    "searchFindings.servicesTable.deleteGroup.reason.tooltip"
+              <ControlLabel>
+                {t("searchFindings.servicesTable.deleteGroup.typeGroupName")}
+              </ControlLabel>
+              <Field
+                component={FormikText}
+                name={"confirmation"}
+                placeholder={groupName.toLowerCase()}
+                type={"text"}
+                validate={required}
+              />
+              <Select
+                label={t(
+                  "searchFindings.servicesTable.deleteGroup.reason.title"
+                )}
+                name={"reason"}
+                tooltip={t(
+                  "searchFindings.servicesTable.deleteGroup.reason.tooltip"
+                )}
+              >
+                <option value={"NO_SYSTEM"}>
+                  {t(
+                    "searchFindings.servicesTable.deleteGroup.reason.noSystem"
                   )}
-                >
-                  <FormGroup>
-                    <Field component={FormikDropdown} name={"reason"}>
-                      <option value={"NO_SYSTEM"}>
-                        {t(
-                          "searchFindings.servicesTable.deleteGroup.reason.noSystem"
-                        )}
-                      </option>
-                      <option value={"NO_SECTST"}>
-                        {t(
-                          "searchFindings.servicesTable.deleteGroup.reason.noSectst"
-                        )}
-                      </option>
-                      <option value={"DIFF_SECTST"}>
-                        {t(
-                          "searchFindings.servicesTable.deleteGroup.reason.diffSectst"
-                        )}
-                      </option>
-                      <option value={"MIGRATION"}>
-                        {t(
-                          "searchFindings.servicesTable.deleteGroup.reason.migration"
-                        )}
-                      </option>
-                      <option value={"OTHER"}>
-                        {t(
-                          "searchFindings.servicesTable.deleteGroup.reason.other"
-                        )}
-                      </option>
-                    </Field>
-                  </FormGroup>
-                </Tooltip>
-              </FormGroup>
+                </option>
+                <option value={"NO_SECTST"}>
+                  {t(
+                    "searchFindings.servicesTable.deleteGroup.reason.noSectst"
+                  )}
+                </option>
+                <option value={"DIFF_SECTST"}>
+                  {t(
+                    "searchFindings.servicesTable.deleteGroup.reason.diffSectst"
+                  )}
+                </option>
+                <option value={"MIGRATION"}>
+                  {t(
+                    "searchFindings.servicesTable.deleteGroup.reason.migration"
+                  )}
+                </option>
+                <option value={"OTHER"}>
+                  {t("searchFindings.servicesTable.deleteGroup.reason.other")}
+                </option>
+              </Select>
               <ModalConfirm
                 disabled={!dirty || !isValid}
                 onCancel={onClose}
                 onConfirm={submitForm}
               />
-            </React.Fragment>
+            </Fragment>
           )}
         </Formik>
       </Modal>
-    </React.StrictMode>
+    </StrictMode>
   );
 };
 
