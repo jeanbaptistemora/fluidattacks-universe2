25deacb58702d9456ab7c984c53c33f0cec55652
Andres Cuberos <acuberos@fluidattacks.com>
2022-07-21 13:30:23 -0500

    integrates\refac(front): #6946.2 change button text
    - This reverts commit f7893dbf618e6120abd19bd8b0590daa157254b5.

--- integrates/front/src/utils/translations/en.ts
+++ integrates/front/src/utils/translations/en.ts
@@ -1623,7 +1623,7 @@ export const enTranslations: ResourceKey = {
           name: "Group name",
           new: {
             group: "New group",
-            text: "New project",
+            text: "New group",
             tooltip: "Create a new group",
           },
           organization: {
