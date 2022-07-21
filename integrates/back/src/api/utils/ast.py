fdb7a2012aa7a4641921c9317bc207a70f52759f
Juan Chaves <jchaves@fluidattacks.com>
2022-07-21 22:46:13 +0000

    integrates\feat(back): #6833.2 add deprecation utils
    - Added support to check the input and argument nodes
    - The results will be kept in several dicts, pending the
    adding of docs to explain their structure

--- integrates/back/src/api/utils/ast.py
+++ integrates/back/src/api/utils/ast.py
@@ -1,18 +1,24 @@
 from api.schema import (
     SDL_CONTENT,
 )
+from api.utils.types import (
+    ApiDeprecation,
+)
 from graphql import (
     DirectiveNode,
+    DocumentNode,
     EnumTypeDefinitionNode,
+    InputObjectTypeDefinitionNode,
     ObjectTypeDefinitionNode,
     parse,
+    TypeDefinitionNode,
 )
 from typing import (
     Optional,
 )
 
 
-def get_deprecation_reason(directives: list[DirectiveNode]) -> Optional[str]:
+def _get_deprecation_reason(directives: list[DirectiveNode]) -> Optional[str]:
     return next(
         (
             directive.arguments[0].value.value
@@ -23,27 +29,64 @@ def get_deprecation_reason(directives: list[DirectiveNode]) -> Optional[str]:
     )
 
 
+def _search_directives(
+    definition: TypeDefinitionNode,
+    deprecation_dict: dict[str, set[ApiDeprecation]],
+) -> dict[str, set[ApiDeprecation]]:
+
+    if isinstance(definition, EnumTypeDefinitionNode):
+        fields = definition.values
+    elif isinstance(definition, ObjectTypeDefinitionNode):
+        fields = definition.fields
+    elif isinstance(definition, InputObjectTypeDefinitionNode):
+        fields = definition.fields
+    else:
+        fields = []
+
+    for value in fields:
+        deprecation_reason = _get_deprecation_reason(value.directives)
+        if deprecation_reason:
+            deprecation_dict.setdefault(definition.name.value, set()).add(
+                ApiDeprecation(
+                    parent=definition.name.value,
+                    field=value.name.value,
+                    reason=deprecation_reason.replace("\n", " "),
+                )
+            )
+    return deprecation_dict
+
+
+def _search_field_arguments(
+    definition: ObjectTypeDefinitionNode,
+    deprecation_dict: dict[str, set[ApiDeprecation]],
+) -> dict[str, set[ApiDeprecation]]:
+    for field in definition.fields:
+        for argument in field.arguments:
+            arg_deprecation = _get_deprecation_reason(argument.directives)
+            if arg_deprecation:
+                deprecation_dict.setdefault(definition.name.value, set()).add(
+                    ApiDeprecation(
+                        parent=definition.name.value,
+                        field=argument.name.value,
+                        reason=arg_deprecation.replace("\n", " "),
+                    )
+                )
+    return deprecation_dict
+
+
 def parse_schema_deprecations() -> None:
-    schema_ast = parse(SDL_CONTENT)
+    schema_ast: DocumentNode = parse(SDL_CONTENT)
+    arguments: dict[str, set[ApiDeprecation]] = {}
+    enums: dict[str, set[ApiDeprecation]] = {}
+    operations: dict[str, set[ApiDeprecation]] = {}
+
     for definition in schema_ast.definitions:
         if isinstance(definition, EnumTypeDefinitionNode):
-            for value in definition.values:
-                deprecation_reason = get_deprecation_reason(value.directives)
-                if deprecation_reason:
-                    print(
-                        definition.kind,
-                        definition.name.value,
-                        value.name.value,
-                        deprecation_reason,
-                    )
+            _search_directives(definition, enums)
 
         if isinstance(definition, ObjectTypeDefinitionNode):
-            for field in definition.fields:
-                deprecation_reason = get_deprecation_reason(field.directives)
-                if deprecation_reason:
-                    print(
-                        definition.kind,
-                        definition.name.value,
-                        field.name.value,
-                        deprecation_reason,
-                    )
+            _search_directives(definition, operations)
+            _search_field_arguments(definition, arguments)
+
+        if isinstance(definition, InputObjectTypeDefinitionNode):
+            _search_directives(definition, arguments)
---
+++ integrates/back/src/api/utils/types.py
@@ -0,0 +1,9 @@
+from typing import (
+    NamedTuple,
+)
+
+
+class ApiDeprecation(NamedTuple):
+    parent: str
+    field: str
+    reason: str
