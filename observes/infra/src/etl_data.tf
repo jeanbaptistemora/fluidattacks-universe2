7ac25f8e4030318bb99d942aa155fbc56f35be48
Daniel Murcia <dmurcia@fluidattacks.com>
2022-07-22 14:25:58 -0500

    observes\feat(infra): #6944.10 etl data
    - add bucket for temp store of etl data

---
+++ observes/infra/src/etl_data.tf
@@ -0,0 +1,26 @@
+resource "aws_s3_bucket" "etl_data" {
+  bucket = "observes.etl_data"
+
+  tags = {
+    "Name"               = "observes.etl_data"
+    "management:area"    = "cost"
+    "management:product" = "observes"
+    "management:type"    = "product"
+  }
+}
+
+resource "aws_s3_bucket_acl" "etl_data" {
+  bucket = aws_s3_bucket.etl_data.id
+
+  acl = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "etl_data" {
+  bucket = aws_s3_bucket.etl_data.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
