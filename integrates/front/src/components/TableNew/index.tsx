c865723916cdc5b3a1f8739c0db62c06b75a0aaf
Nicolas Carvajal <ncarvajal@fluidattacks.com>
2022-07-22 12:31:57 -0500

    integrates\feat(front): #6331 add pagination
    - added simple pagination

--- integrates/front/src/components/TableNew/index.tsx
+++ integrates/front/src/components/TableNew/index.tsx
@@ -1,10 +1,12 @@
 import {
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 import type { ColumnDef } from "@tanstack/react-table";
-import type { ReactElement } from "react";
+import type { ChangeEvent, ReactElement } from "react";
 import React, { useState } from "react";
 
 import { ToggleFunction } from "./columnToggle";
@@ -26,11 +28,33 @@ export const Tables = <TData extends Record<string, unknown>>(
     columns,
     data,
     getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
     onColumnVisibilityChange: setColumnVisibility,
     state: {
       columnVisibility,
     },
   });
+
+  function firstPage(): void {
+    table.setPageIndex(0);
+  }
+  function prevPage(): void {
+    table.previousPage();
+  }
+  function nextPage(): void {
+    table.nextPage();
+  }
+  function lastPage(): void {
+    table.setPageIndex(table.getPageCount() - 1);
+  }
+  function pageIndex(event: ChangeEvent<HTMLInputElement>): void {
+    const page = event.target.value ? Number(event.target.value) - 1 : 0;
+    table.setPageIndex(page);
+  }
+  function pageRecords(event: ChangeEvent<HTMLSelectElement>): void {
+    table.setPageSize(Number(event.target.value));
+  }
 
   return (
     <div className={"w-100"} id={id}>
@@ -75,6 +99,48 @@ export const Tables = <TData extends Record<string, unknown>>(
           )}
         </tbody>
       </table>
+      <div>
+        <button disabled={!table.getCanPreviousPage()} onClick={firstPage}>
+          {"<<"}
+        </button>
+        <button disabled={!table.getCanPreviousPage()} onClick={prevPage}>
+          {"<"}
+        </button>
+        <button disabled={!table.getCanNextPage()} onClick={nextPage}>
+          {">"}
+        </button>
+        <button disabled={!table.getCanPreviousPage()} onClick={lastPage}>
+          {">>"}
+        </button>
+        <span>
+          <strong>
+            {`${
+              table.getState().pagination.pageIndex + 1
+            } of ${table.getPageCount()}`}
+          </strong>
+        </span>
+        <span>
+          {"Page: "}
+          <input
+            defaultValue={table.getState().pagination.pageIndex + 1}
+            onChange={pageIndex}
+            type={"number"}
+          />
+        </span>
+        <select
+          onChange={pageRecords}
+          value={table.getState().pagination.pageSize}
+        >
+          {[10, 20].map(
+            (pageSize): ReactElement => (
+              <option key={pageSize} value={pageSize}>
+                {`Show ${pageSize}`}
+              </option>
+            )
+          )}
+        </select>
+      </div>
+      {`${table.getRowModel().rows.length} Rows`}
     </div>
   );
 };
