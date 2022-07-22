7886abd55d1c01bcf47d7083cd02e6606661710e
Daniel Murcia <dmurcia@fluidattacks.com>
2022-07-22 10:40:53 -0500

    observes\fix(back): #6944.6 lower case
    - add lower case for schema ids
    - add logger conf
    - add info msgs

--- observes/etl/dynamo_etl_conf/dynamo_etl_conf/__init__.py
+++ observes/etl/dynamo_etl_conf/dynamo_etl_conf/__init__.py
@@ -1,1 +1,7 @@
-__version__ = "1.0.0"
+from ._utils import (
+    get_log,
+)
+import logging
+
+__version__ = "1.0.1"
+LOG = get_log(__name__, logging.DEBUG)
--- observes/etl/dynamo_etl_conf/dynamo_etl_conf/centralize.py
+++ observes/etl/dynamo_etl_conf/dynamo_etl_conf/centralize.py
@@ -14,6 +14,7 @@ from fa_purity.stream.transform import (
     filter_maybe,
     until_empty,
 )
+import logging
 from redshift_client.id_objs import (
     SchemaId,
 )
@@ -22,7 +23,16 @@ from redshift_client.schema.client import (
 )
 from typing import (
     FrozenSet,
+    TypeVar,
 )
+
+_T = TypeVar("_T")
+LOG = logging.getLogger(__name__)
+
+
+def _print(item: _T, msg: str) -> _T:
+    LOG.info(msg)
+    return item
 
 
 def _exist(client: SchemaClient, schema: SchemaId) -> Cmd[Maybe[SchemaId]]:
@@ -36,7 +46,11 @@ def merge_parts(
 ) -> Cmd[None]:
     schemas = (
         infinite_range(0, 1)
-        .map(lambda i: _exist(client, SchemaId(f"{schema_part_prefix}{i}")))
+        .map(
+            lambda i: _exist(
+                client, SchemaId(f"{schema_part_prefix.lower()}{i}")
+            )
+        )
         .transform(lambda p: until_empty(from_piter(p)))
     )
     return consume(schemas.map(lambda s: client.move(s, target)))
@@ -47,7 +61,20 @@ def merge_dynamo_tables(
 ) -> Cmd[None]:
     schemas = (
         from_flist(tuple(tables))
-        .map(lambda table: _exist(client, SchemaId(f"dynamodb_{table}")))
-        .transform(lambda p: filter_maybe(from_piter(p)))
+        .map(lambda table: SchemaId(f"dynamodb_{table.lower()}"))
+        .map(
+            lambda s: _exist(client, s).map(
+                lambda m: m.to_result().alt(lambda _: s)
+            )
+        )
+        .transform(lambda p: from_piter(p))
     )
-    return consume(schemas.map(lambda s: client.migrate(s, target)))
+    nothing = Cmd.from_cmd(lambda: None)
+    return consume(
+        schemas.map(
+            lambda r: r.map(lambda s: _print(s, f"Migrating {s} -> {target}"))
+            .map(lambda s: client.migrate(s, target))
+            .alt(lambda s: _print(nothing, f"Ignoring non-existent {s}"))
+            .to_union()
+        )
+    )
