ec33d81fc7c6cfe9d70e1d6f9cdb43082209edd6
Nicolas Carvajal <ncarvajal@fluidattacks.com>
2022-07-22 14:10:25 -0500

    integrates\refac(front): #6331 refactor pagination
    - no-deltas-test
    - add pagination component
    - refactor table component

--- integrates/front/src/components/TableNew/index.tsx
+++ integrates/front/src/components/TableNew/index.tsx
@@ -6,10 +6,11 @@ import {
   useReactTable,
 } from "@tanstack/react-table";
 import type { ColumnDef } from "@tanstack/react-table";
-import type { ChangeEvent, ReactElement } from "react";
+import type { ReactElement } from "react";
 import React, { useState } from "react";
 
 import { ToggleFunction } from "./columnToggle";
+import { PagMenu } from "./paginationMenu";
 
 export interface ITableProps<TData> {
   id: string;
@@ -35,26 +36,6 @@ export const Tables = <TData extends Record<string, unknown>>(
       columnVisibility,
     },
   });
-
-  function firstPage(): void {
-    table.setPageIndex(0);
-  }
-  function prevPage(): void {
-    table.previousPage();
-  }
-  function nextPage(): void {
-    table.nextPage();
-  }
-  function lastPage(): void {
-    table.setPageIndex(table.getPageCount() - 1);
-  }
-  function pageIndex(event: ChangeEvent<HTMLInputElement>): void {
-    const page = event.target.value ? Number(event.target.value) - 1 : 0;
-    table.setPageIndex(page);
-  }
-  function pageRecords(event: ChangeEvent<HTMLSelectElement>): void {
-    table.setPageSize(Number(event.target.value));
-  }
 
   return (
     <div className={"w-100"} id={id}>
@@ -99,48 +80,7 @@ export const Tables = <TData extends Record<string, unknown>>(
           )}
         </tbody>
       </table>
-      <div>
-        <button disabled={!table.getCanPreviousPage()} onClick={firstPage}>
-          {"<<"}
-        </button>
-        <button disabled={!table.getCanPreviousPage()} onClick={prevPage}>
-          {"<"}
-        </button>
-        <button disabled={!table.getCanNextPage()} onClick={nextPage}>
-          {">"}
-        </button>
-        <button disabled={!table.getCanPreviousPage()} onClick={lastPage}>
-          {">>"}
-        </button>
-        <span>
-          <strong>
-            {`${
-              table.getState().pagination.pageIndex + 1
-            } of ${table.getPageCount()}`}
-          </strong>
-        </span>
-        <span>
-          {"Page: "}
-          <input
-            defaultValue={table.getState().pagination.pageIndex + 1}
-            onChange={pageIndex}
-            type={"number"}
-          />
-        </span>
-        <select
-          onChange={pageRecords}
-          value={table.getState().pagination.pageSize}
-        >
-          {[10, 20].map(
-            (pageSize): ReactElement => (
-              <option key={pageSize} value={pageSize}>
-                {`Show ${pageSize}`}
-              </option>
-            )
-          )}
-        </select>
-      </div>
-      {`${table.getRowModel().rows.length} Rows`}
+      <PagMenu table={table} />
     </div>
   );
 };
---
+++ integrates/front/src/components/TableNew/paginationMenu.tsx
@@ -0,0 +1,78 @@
+import type { Table } from "@tanstack/react-table";
+import type { ChangeEvent, ReactElement } from "react";
+import React from "react";
+
+export interface IPagMenuProps<TData> {
+  table: Table<TData>;
+}
+
+export const PagMenu = <TData extends Record<string, unknown>>(
+  props: IPagMenuProps<TData>
+): JSX.Element => {
+  const { table } = props;
+
+  function firstPage(): void {
+    table.setPageIndex(0);
+  }
+  function prevPage(): void {
+    table.previousPage();
+  }
+  function nextPage(): void {
+    table.nextPage();
+  }
+  function lastPage(): void {
+    table.setPageIndex(table.getPageCount() - 1);
+  }
+  function pageIndex(event: ChangeEvent<HTMLInputElement>): void {
+    const page = event.target.value ? Number(event.target.value) - 1 : 0;
+    table.setPageIndex(page);
+  }
+  function pageRecords(event: ChangeEvent<HTMLSelectElement>): void {
+    table.setPageSize(Number(event.target.value));
+  }
+
+  return (
+    <div>
+      <button disabled={!table.getCanPreviousPage()} onClick={firstPage}>
+        {"<<"}
+      </button>
+      <button disabled={!table.getCanPreviousPage()} onClick={prevPage}>
+        {"<"}
+      </button>
+      <button disabled={!table.getCanNextPage()} onClick={nextPage}>
+        {">"}
+      </button>
+      <button disabled={!table.getCanPreviousPage()} onClick={lastPage}>
+        {">>"}
+      </button>
+      <span>
+        <strong>
+          {`${
+            table.getState().pagination.pageIndex + 1
+          } of ${table.getPageCount()}`}
+        </strong>
+      </span>
+      <span>
+        {"Page: "}
+        <input
+          defaultValue={table.getState().pagination.pageIndex + 1}
+          onChange={pageIndex}
+          type={"number"}
+        />
+      </span>
+      <select
+        onChange={pageRecords}
+        value={table.getState().pagination.pageSize}
+      >
+        {[10, 20].map(
+          (pageSize): ReactElement => (
+            <option key={pageSize} value={pageSize}>
+              {`Show ${pageSize}`}
+            </option>
+          )
+        )}
+      </select>
+      {`${table.getRowModel().rows.length} Rows`}
+    </div>
+  );
+};
