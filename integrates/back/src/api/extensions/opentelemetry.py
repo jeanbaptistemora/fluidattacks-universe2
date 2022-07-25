abd7559ec6fcc003dcb4b7241ad96debbbec3964
David Acevedo <dacevedo@fluidattacks.com>
2022-07-25 09:22:23 -0500

    integrates\perf(back): #5961 telemetry extension
    - use a faster function to check for async resolvers

--- integrates/back/src/api/extensions/opentelemetry.py
+++ integrates/back/src/api/extensions/opentelemetry.py
@@ -12,6 +12,9 @@ from ariadne.types import (
 from graphql import (
     GraphQLResolveInfo,
 )
+from graphql.pyutils import (
+    is_awaitable,
+)
 from opentelemetry import (
     trace,
 )
@@ -20,7 +23,28 @@ from typing import (
 )
 
 
-class OpenTelemetryExtension(Extension):
+class FastExtension(Extension):
+    """
+    Resolve using a faster function to check for async resolvers
+
+    Pending contribution to upstream
+    https://github.com/graphql-python/graphql-core/issues/54
+    """
+
+    async def resolve(
+        self,
+        next_: Resolver,
+        parent: Any,
+        info: GraphQLResolveInfo,
+        **kwargs: Any,
+    ) -> Any:
+        result = next_(parent, info, **kwargs)
+        if is_awaitable(result):
+            result = await result
+        return result
+
+
+class OpenTelemetryExtension(FastExtension):
     """
     OpenTelemetry extension for ariadne
 
