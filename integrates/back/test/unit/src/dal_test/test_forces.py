13960080e242e95473dc579606fec50dd342390c
Daniel Betancur <cbetancur@fluidattacks.com>
2022-07-26 11:02:53 -0500

    integrates\test(back): #4944.8 modify unit test
    - modify unit test to mock db calls

--- integrates/back/test/unit/src/dal_test/test_forces.py
+++ integrates/back/test/unit/src/dal_test/test_forces.py
@@ -5,17 +5,30 @@ from newutils import (
     datetime as datetime_utils,
 )
 import pytest
+from unittest import (
+    mock,
+)
 
 pytestmark = pytest.mark.asyncio
 
 
 @pytest.mark.changes_db
 async def test_create_execution() -> None:
+    def side_effect(table_name: str, execution_attributes: dict) -> bool:
+        return bool(table_name and ("" not in execution_attributes.values()))
+
     group = "unittesting"
     execution_id = "random_id"
     now = datetime_utils.get_now()
-    assert await add_execution(
-        group_name=group,
-        execution_id=execution_id,
-        date=now,
-    )
+    with mock.patch("forces.dal.dynamodb_ops.put_item") as mock_put:
+        mock_put.side_effect = side_effect
+        assert await add_execution(
+            group_name=group,
+            execution_id=execution_id,
+            date=now,
+        )
+        assert not await add_execution(
+            group_name="",
+            execution_id=execution_id,
+            date=now,
+        )
