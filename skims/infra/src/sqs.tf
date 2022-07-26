783687e3a64407d096e1ace1a207d11be555fbb3
Diego Restrepo <drestrepo@fluidattacks.com>
2022-07-26 15:41:02 -0500

    skims\fix(back): #6866.38 increase retention time
    - increase the retention time, there may be problems

--- skims/infra/src/sqs.tf
+++ skims/infra/src/sqs.tf
@@ -2,7 +2,7 @@ resource "aws_sqs_queue" "skims_report_queue" {
   name                      = "skims-report-queue"
   delay_seconds             = 5
   max_message_size          = 2048
-  message_retention_seconds = 86400
+  message_retention_seconds = 259200
   receive_wait_time_seconds = 10
 
   tags = {
