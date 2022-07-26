7c96dd73bee7d9d5707a5526883760951640121b
David Acevedo <dacevedo@fluidattacks.com>
2022-07-26 12:30:59 -0500

    integrates\feat(back): #6514 prepare search
    - added support for exact filters
    - the group vulns resolver now talks to opensearch

--- integrates/back/src/api/resolvers/group/vulnerabilities.py
+++ integrates/back/src/api/resolvers/group/vulnerabilities.py
@@ -3,6 +3,10 @@ from db_model.groups.types import (
 )
 from db_model.vulnerabilities.types import (
     VulnerabilitiesConnection,
+    VulnerabilityEdge,
+)
+from db_model.vulnerabilities.utils import (
+    format_vulnerability,
 )
 from dynamodb.types import (
     PageInfo,
@@ -10,17 +14,45 @@ from dynamodb.types import (
 from graphql import (
     GraphQLResolveInfo,
 )
+import logging
+from search.operations import (
+    search,
+)
 from typing import (
     Any,
 )
 
+LOGGER = logging.getLogger(__name__)
+
 
 async def resolve(
-    _parent: Group,
+    parent: Group,
     _info: GraphQLResolveInfo,
-    **_kwargs: Any,
+    **kwargs: Any,
 ) -> VulnerabilitiesConnection:
+    query: str = kwargs.get("search", "")
+    results = await search(
+        exact_filters={"group_name": parent.name},
+        query=query,
+    )
+    edges = tuple(
+        VulnerabilityEdge(
+            cursor="",
+            node=format_vulnerability(result),
+        )
+        for result in results
+        if result["group_name"] == parent.name
+    )
+
+    # Hopefully not needed, but we must ensure security with a new piece of
+    # the stack
+    if len(edges) != len(results):
+        LOGGER.critical(
+            "Exact filters mismatch",
+            extra={"extra": {"group_name": parent.name, "query": query}},
+        )
+
     return VulnerabilitiesConnection(
-        edges=tuple(),
+        edges=edges,
         page_info=PageInfo(has_next_page=False, end_cursor=""),
     )
--- integrates/back/src/search/operations.py
+++ integrates/back/src/search/operations.py
@@ -6,15 +6,30 @@ from typing import (
 )
 
 
-async def search(*, query: str) -> tuple[dict[str, Any], ...]:
+async def search(
+    *,
+    exact_filters: dict[str, Any],
+    query: str,
+) -> tuple[dict[str, Any], ...]:
     """
     Searches for items matching the arbitrary user input
 
     https://opensearch-project.github.io/opensearch-py/api-ref/client.html#opensearchpy.OpenSearch.search
     """
     client = await get_client()
-    response: dict[str, Any] = await client.search(
-        body={"query": {"multi_match": {"query": query}}}
-    )
+    term_queries = [
+        {"term": {key: value}} for key, value in exact_filters.items()
+    ]
+    body = {
+        "query": {
+            "bool": {
+                "must": [
+                    {"multi_match": {"query": query}},
+                    *term_queries,
+                ],
+            }
+        }
+    }
+    response: dict[str, Any] = await client.search(body=body)
 
     return tuple(hit["_source"] for hit in response["hits"]["hits"])
