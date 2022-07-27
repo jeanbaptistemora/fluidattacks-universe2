35a84a7ddb7f10d3ab012666b5fb2cb7d2c74b12
Daniel Betancur <cbetancur@fluidattacks.com>
2022-07-27 16:47:13 -0500

    integrates\test(back): #4944.9 add conftest
    - add conftest with default credentials

---
+++ integrates/back/test/unit/src/batch/conftest.py
@@ -0,0 +1,10 @@
+import os
+import pytest
+
+
+@pytest.fixture(scope="function", autouse=True)
+def aws_credentials() -> None:
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
