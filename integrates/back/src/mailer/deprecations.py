6ed5a0bd63f0f565ace5c2a9a457acf0df303604
Juan Chaves <jchaves@fluidattacks.com>
2022-07-27 17:23:01 +0000

    integrates\feat(back): #6832.3 add deprecation notice
    - Added a method to format the deprecation dicts to a
    mail-friendly format

--- integrates/back/src/mailer/deprecations.py
+++ integrates/back/src/mailer/deprecations.py
@@ -2,9 +2,6 @@ from .common import (
     GENERAL_TAG,
     send_mails_async,
 )
-from datetime import (
-    datetime,
-)
 from newutils.datetime import (
     get_now_plus_delta,
 )
@@ -13,17 +10,31 @@ from typing import (
 )
 
 
+def _format_deprecation_for_mail(
+    deprecations: dict[str, list[Any]]
+) -> dict[str, str]:
+    """
+    Translates the deprecation dict values to a more readable mail format
+    """
+    depr_mail: dict[str, str] = {}
+    for key, deprecated_fields in deprecations.items():
+        depr_mail[key] = ", ".join(
+            [field.field for field in deprecated_fields]
+        )
+
+    return depr_mail
+
+
 async def send_mail_deprecation_notice(
     *,
     loaders: Any,
-    deprecations: dict[str, str],
+    deprecations: dict[str, list[Any]],
     email_to: list[str],
 ) -> None:
     # These mails are meant to anticipate next month's deprecations
-    date: datetime = get_now_plus_delta(weeks=4)
-    month: str = date.strftime("%B")
+    month: str = get_now_plus_delta(weeks=4).strftime("%B")
     email_context: dict[str, dict[str, str]] = {
-        "deprecations": deprecations,
+        "deprecations": _format_deprecation_for_mail(deprecations),
     }
     await send_mails_async(
         loaders,
