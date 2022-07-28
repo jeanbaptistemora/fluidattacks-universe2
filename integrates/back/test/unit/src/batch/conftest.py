52a8fa30f038ca1378ff5dd03df21bb2a43d68d6
Daniel Betancur <cbetancur@fluidattacks.com>
2022-07-28 18:33:17 -0500

    integrates\test(back): #4944.10 add dynamo fixture
    - add fixture to mock dynamodb calls

--- integrates/back/test/unit/src/batch/conftest.py
+++ integrates/back/test/unit/src/batch/conftest.py
@@ -1,5 +1,12 @@
+import boto3
+from moto.dynamodb2 import (
+    mock_dynamodb2,
+)
 import os
 import pytest
+from typing import (
+    Generator,
+)
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -8,3 +15,10 @@ def aws_credentials() -> None:
     os.environ["AWS_ACCESS_KEY_ID"] = "testing"
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
     os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+
+
+@pytest.fixture(scope="module")
+def dynamodb() -> Generator:
+    """Mocked DynamoDB Fixture."""
+    with mock_dynamodb2():
+        yield boto3.resource("dynamodb")
