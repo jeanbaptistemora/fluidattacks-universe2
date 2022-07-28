c97dace0ba27e7604812d17e5ad4314043f910d9
Fabio Lagos <flagos@fluidattacks.com>
2022-07-28 15:23:50 -0500

    skims\fix(back): #7020.1 refactored f016
    - refactor f016 weak protocol to use syntax graph

--- skims/skims/lib_root/f016/c_sharp.py
+++ skims/skims/lib_root/f016/c_sharp.py
@@ -44,27 +44,28 @@ def weak_protocol(
         for shard in graph_db.shards_by_language(
             GraphShardMetadataLanguage.CSHARP,
         ):
-            for member in g.filter_nodes(
-                shard.graph,
-                nodes=shard.graph.nodes,
-                predicate=g.pred_has_labels(
-                    label_type="member_access_expression"
-                ),
+            if shard.syntax_graph is None:
+                continue
+            graph = shard.syntax_graph
+            for nid in g.filter_nodes(
+                graph,
+                nodes=graph.nodes,
+                predicate=g.pred_has_labels(label_type="MemberAccess"),
             ):
-                c_ident = g.get_ast_childs(shard.graph, member, "identifier")
-                if (
-                    len(c_ident) == 2
-                    and shard.graph.nodes[c_ident[0]]["label_text"]
-                    == "SecurityProtocolType"
-                    and shard.graph.nodes[c_ident[1]]["label_text"]
-                    in {"Ssl3", "Tls", "Tls11", "None"}
-                ):
-                    protocol = shard.graph.nodes[c_ident[1]]["label_text"]
 
+                protocol = graph.nodes[nid].get("member")
+                if graph.nodes[nid].get(
+                    "expression"
+                ) == "SecurityProtocolType" and protocol in [
+                    "Ssl3",
+                    "Tls",
+                    "Tls11",
+                    "None",
+                ]:
                     metadata["what"] = protocol
                     metadata["desc_params"] = {"protocol": protocol}
 
-                    yield shard, member, metadata
+                    yield shard, nid, metadata
 
     return get_vulnerabilities_from_n_ids_metadata(
         desc_key="src.lib_root.f016.csharp_weak_protocol",
