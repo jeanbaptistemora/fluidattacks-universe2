80a1b5b933183da03dc164c4f0601fc765e40dcd
Daniel Murcia <dmurcia@fluidattacks.com>
2022-07-28 15:07:42 -0500

    observes\feat(back): #6971.10 csv save
    - add csv save module

---
+++ observes/singer/target-s3/target_s3/csv.py
@@ -0,0 +1,59 @@
+import csv
+from fa_purity import (
+    Cmd,
+    FrozenList,
+    PureIter,
+)
+from fa_purity.json.primitive.core import (
+    Primitive,
+)
+from fa_purity.pure_iter.factory import (
+    from_flist,
+)
+from fa_purity.stream.factory import (
+    from_piter,
+)
+from target_s3.complete_record import (
+    CompletePlainRecord,
+)
+from target_s3.record_group import (
+    RecordGroup,
+)
+from target_s3.ro_file import (
+    TempReadOnlyFile,
+)
+from typing import (
+    IO,
+    Tuple,
+)
+
+
+def _save(data: PureIter[FrozenList[Primitive]]) -> Cmd[TempReadOnlyFile]:
+    def write_cmd(file: IO[str]) -> Cmd[None]:
+        def _action() -> None:
+            writer = csv.writer(
+                file, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+            )
+            writer.writerows(data)
+
+        return Cmd.from_cmd(_action)
+
+    return TempReadOnlyFile.from_cmd(write_cmd)
+
+
+def _ordered_data(record: CompletePlainRecord) -> FrozenList[Primitive]:
+    def _key(item: Tuple[str, Primitive]) -> str:
+        return item[0]
+
+    items = tuple(record.record.record.items())
+    ordered = sorted(items, key=_key)
+    return tuple(i[1] for i in ordered)
+
+
+def save(records: PureIter[RecordGroup]) -> Cmd[PureIter[TempReadOnlyFile]]:
+    files = (
+        records.map(lambda g: g.records.map(_ordered_data).transform(_save))
+        .transform(lambda x: from_piter(x))
+        .to_list()
+    )
+    return files.map(lambda f: from_flist(f))
