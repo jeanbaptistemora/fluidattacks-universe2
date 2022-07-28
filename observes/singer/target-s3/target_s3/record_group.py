6f33e7ce35a6f2eb895449d76341388244337261
Daniel Murcia <dmurcia@fluidattacks.com>
2022-07-28 09:09:46 -0500

    observes\feat(back): #6971.9 record group
    - redefine RecordGroup using CompletePlainRecord
    - explicit error on new builder for RecordGroup

--- observes/singer/target-s3/target_s3/record_group.py
+++ observes/singer/target-s3/target_s3/record_group.py
@@ -8,50 +8,68 @@ from dataclasses import (
 from fa_purity import (
     PureIter,
     Result,
+    ResultE,
 )
-from target_s3.plain_record import (
-    PlainRecord,
+from fa_purity.pure_iter.factory import (
+    from_flist,
+)
+from fa_purity.result.transform import (
+    all_ok,
+)
+from fa_singer_io.singer import (
+    SingerSchema,
+)
+from target_s3.complete_record import (
+    CompletePlainRecord,
 )
 
 
 @dataclass(frozen=True)
 class _RecordGroup:
-    stream: str
-    records: PureIter[PlainRecord]
+    schema: SingerSchema
+    records: PureIter[CompletePlainRecord]
 
 
 @dataclass(frozen=True)
 class RecordGroup:
     """
-    Group of PlainRecord that share same stream
+    Group of CompletePlainRecord that share same schema
     """
 
     _inner: _RecordGroup
 
     @property
-    def stream(self) -> str:
-        return self._inner.stream
+    def schema(self) -> SingerSchema:
+        return self._inner.schema
 
     @property
-    def records(self) -> PureIter[PlainRecord]:
+    def records(self) -> PureIter[CompletePlainRecord]:
         return self._inner.records
 
     @staticmethod
-    def filter(stream: str, records: PureIter[PlainRecord]) -> RecordGroup:
-        items = records.filter(lambda r: r.stream == stream)
-        return RecordGroup(_RecordGroup(stream, items))
+    def filter(
+        schema: SingerSchema, records: PureIter[CompletePlainRecord]
+    ) -> RecordGroup:
+        items = records.filter(lambda r: r.schema == schema)
+        return RecordGroup(_RecordGroup(schema, items))
 
     @staticmethod
-    def new(stream: str, records: PureIter[PlainRecord]) -> RecordGroup:
+    def new(
+        schema: SingerSchema, records: PureIter[CompletePlainRecord]
+    ) -> ResultE[RecordGroup]:
         items = records.map(
-            lambda r: Result.success(r, PlainRecord)
-            if r.stream == stream
-            else Result.failure(r, PlainRecord)
+            lambda r: Result.success(r, CompletePlainRecord)
+            if r.schema == schema
+            else Result.failure(r, CompletePlainRecord)
         ).map(
             lambda r: r.alt(
                 lambda x: ValueError(
-                    f"A record does not belong to the RecordGroup. Expected stream `{stream}` but got `{x.stream}`"
+                    f"A record does not belong to the RecordGroup. Expected schema `{schema}` but got `{x.schema}`"
                 )
-            ).unwrap()
+            )
         )
-        return RecordGroup(_RecordGroup(stream, items))
+        return (
+            all_ok(items.to_list())
+            .map(lambda l: RecordGroup(_RecordGroup(schema, from_flist(l))))
+            .alt(Exception)
+        )
