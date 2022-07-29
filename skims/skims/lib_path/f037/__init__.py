4006624037fdc1eec129cdf60d3286fc632c3173
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-07-29 13:57:02 -0500

    skims\feat(back): #6074.75 add method f037
    - Add method for F037

---
+++ skims/skims/lib_path/f037/__init__.py
@@ -0,0 +1,36 @@
+from lib_path.common import (
+    SHIELD_BLOCKING,
+)
+from lib_path.f037.dotnetconfig import (
+    not_suppress_vuln_header,
+)
+from model.core_model import (
+    Vulnerabilities,
+)
+from typing import (
+    Callable,
+    Tuple,
+)
+
+
+@SHIELD_BLOCKING
+def run_not_suppress_vuln_header(content: str, path: str) -> Vulnerabilities:
+    return not_suppress_vuln_header(content=content, path=path)
+
+
+@SHIELD_BLOCKING
+def analyze(
+    content_generator: Callable[[], str],
+    file_extension: str,
+    path: str,
+    **_: None,
+) -> Tuple[Vulnerabilities, ...]:
+    results: Tuple[Vulnerabilities, ...] = ()
+
+    if file_extension == "config":
+        results = (
+            *results,
+            run_not_suppress_vuln_header(content_generator(), path),
+        )
+
+    return results
---
+++ skims/skims/lib_path/f037/dotnetconfig.py
@@ -0,0 +1,47 @@
+from bs4 import (
+    BeautifulSoup,
+)
+from bs4.element import (
+    Tag,
+)
+from lib_path.common import (
+    get_vulnerabilities_from_iterator_blocking,
+)
+from model.core_model import (
+    MethodsEnum,
+    Vulnerabilities,
+)
+from typing import (
+    Iterator,
+    Tuple,
+)
+
+
+def not_suppress_vuln_header(content: str, path: str) -> Vulnerabilities:
+    def iterator() -> Iterator[Tuple[int, int]]:
+        """
+        Search for X-Powered-By headers in a Web.config source file or package.
+        """
+        soup = BeautifulSoup(content, features="xml")
+        has_remove_banner: bool = False
+
+        for custom_headers in soup("customHeaders"):
+            for tag in custom_headers.contents:
+                if isinstance(tag, Tag):
+                    tag_name = tag.name
+                    tag_value = tag.attrs.get("name")
+                    if tag_name == "remove" and tag_value == "X-Powered-By":
+                        has_remove_banner = True
+
+        if not has_remove_banner:
+            line_no: int = 0
+            col_no: int = 0
+            yield line_no, col_no
+
+    return get_vulnerabilities_from_iterator_blocking(
+        content=content,
+        description_key="lib_path.dotnetconfig.not_suppress_vuln_header",
+        iterator=iterator(),
+        path=path,
+        method=MethodsEnum.DOTNETCONFIG_NOT_SUPPRESS_VULN_HEADER,
+    )
