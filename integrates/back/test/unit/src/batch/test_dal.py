1e6888eca901f65df7357c458e9b4a65e59d4965
Daniel Betancur <cbetancur@fluidattacks.com>
2022-08-01 10:46:42 -0500

    integrates\test(back): #4944.12 modify unit tests
    - modify unit tests to mock db calls
    - no-deltas-test

--- integrates/back/test/unit/src/batch/test_dal.py
+++ integrates/back/test/unit/src/batch/test_dal.py
@@ -11,6 +11,12 @@ from batch.types import (
     BatchProcessing,
 )
 import json
+from moto.dynamodb2 import (
+    dynamodb_backend2,
+)
+from mypy_boto3_dynamodb import (
+    DynamoDBServiceResource as ServiceResource,
+)
 from newutils.datetime import (
     get_as_epoch,
     get_now,
@@ -27,206 +33,203 @@ pytestmark = [
     pytest.mark.asyncio,
 ]
 
-scan_actions_result = {
-    "Items": [
-        {
-            "additional_info": json.dumps(
-                {
-                    "report_type": "PDF",
-                    "treatments": [
-                        "ACCEPTED",
-                        "ACCEPTED_UNDEFINED",
-                        "IN_PROGRESS",
-                        "NEW",
-                    ],
-                    "states": ["CLOSED", "OPEN"],
-                    "verifications": [],
-                    "closing_date": None,
-                }
-            ),
-            "subject": "unittesting@fluidattacks.com",
-            "action_name": "report",
-            "pk": (
-                "1bc77999477bfcc84cd111ac745407f2b9ff21e930f59b097b414bbe34f2"
-                "9b46"
-            ),
-            "time": "1616116348",
-            "entity": "unittesting",
-            "queue": "small",
-        },
-        {
-            "additional_info": json.dumps(
-                {
-                    "report_type": "XLS",
-                    "treatments": [
-                        "ACCEPTED",
-                        "ACCEPTED_UNDEFINED",
-                        "IN_PROGRESS",
-                        "NEW",
-                    ],
-                    "states": ["CLOSED", "OPEN"],
-                    "verifications": [],
-                    "closing_date": None,
-                }
-            ),
-            "subject": "unittesting@fluidattacks.com",
-            "action_name": "report",
-            "pk": (
-                "7eda9da492308050bee1bb70b386ffcb3fc9bcabe5b41185113706fe6a2d"
-                "490c"
-            ),
-            "time": "1615834776",
-            "entity": "unittesting",
-            "queue": "small",
-        },
-        {
-            "additional_info": json.dumps(
-                {
-                    "report_type": "DATA",
-                    "treatments": [
-                        "ACCEPTED",
-                        "ACCEPTED_UNDEFINED",
-                        "IN_PROGRESS",
-                        "NEW",
-                    ],
-                    "states": ["CLOSED", "OPEN"],
-                    "verifications": [],
-                    "closing_date": None,
-                }
-            ),
-            "subject": "unittesting@fluidattacks.com",
-            "action_name": "report",
-            "pk": (
-                "71992ff157b46d63fe5bb9dd37176cdb0e27854009b0529d648375d9bfb3"
-                "8977"
-            ),
-            "time": "1616116348",
-            "entity": "unittesting",
-            "queue": "small",
-        },
-        {
-            "additional_info": json.dumps(
-                {
-                    "report_type": "XLS",
-                    "treatments": ["ACCEPTED", "NEW"],
-                    "states": ["OPEN"],
-                    "verifications": ["REQUESTED"],
-                    "closing_date": None,
-                }
-            ),
-            "subject": "unittesting@fluidattacks.com",
-            "action_name": "report",
-            "pk": (
-                "0b60f7743b70ef85c0bc62d49483cef23a883ef03aac17d8921021214f08"
-                "2ad6"
-            ),
-            "time": "1615834776",
-            "entity": "unittesting",
-            "queue": "small",
-        },
-        {
-            "additional_info": json.dumps(
-                {
-                    "report_type": "XLS",
-                    "treatments": [
-                        "ACCEPTED",
-                        "ACCEPTED_UNDEFINED",
-                        "IN_PROGRESS",
-                        "NEW",
-                    ],
-                    "states": ["CLOSED"],
-                    "verifications": ["VERIFIED"],
-                    "closing_date": "2020-06-01T05:00:00+00:00",
-                }
-            ),
-            "subject": "unittesting@fluidattacks.com",
-            "action_name": "report",
-            "pk": (
-                "5ae92b4b4437e3e004fb668bed37472fe4615d92767e3a31d81a7d9ea7c7"
-                "d999"
-            ),
-            "time": "1656429212",
-            "entity": "unittesting",
-            "queue": "small",
-        },
-    ],
-}
+TABLE_NAME = "fi_async_processing"
+key_schema = [{"AttributeName": "pk", "KeyType": "HASH"}]
+attribute_definitions = [{"AttributeName": "pk", "AttributeType": "S"}]
+
+TIME = str(get_as_epoch(get_now()))
+
+data = [
+    dict(
+        action_name="report",
+        entity="oneshottest",
+        subject="unittesting@fluidattacks.com",
+        time=TIME,
+        additional_info=json.dumps(
+            {
+                "report_type": "PDF",
+                "treatments": [
+                    "ACCEPTED",
+                    "ACCEPTED_UNDEFINED",
+                    "IN_PROGRESS",
+                    "NEW",
+                ],
+                "states": ["CLOSED", "OPEN"],
+                "verifications": [],
+                "closing_date": None,
+            }
+        ),
+    ),
+    dict(
+        action_name="report",
+        entity="unittesting",
+        subject="unittesting@fluidattacks.com",
+        time=TIME,
+        additional_info=json.dumps(
+            {
+                "report_type": "XLS",
+                "treatments": [
+                    "ACCEPTED",
+                    "ACCEPTED_UNDEFINED",
+                    "IN_PROGRESS",
+                    "NEW",
+                ],
+                "states": ["CLOSED", "OPEN"],
+                "verifications": [],
+                "closing_date": None,
+            }
+        ),
+    ),
+    dict(
+        action_name="report",
+        entity="unittesting",
+        subject="unittesting@fluidattacks.com",
+        time=TIME,
+        additional_info=json.dumps(
+            {
+                "report_type": "DATA",
+                "treatments": [
+                    "ACCEPTED",
+                    "ACCEPTED_UNDEFINED",
+                    "IN_PROGRESS",
+                    "NEW",
+                ],
+                "states": ["CLOSED", "OPEN"],
+                "verifications": [],
+                "closing_date": None,
+            }
+        ),
+    ),
+    dict(
+        action_name="report",
+        entity="unittesting",
+        subject="unittesting@fluidattacks.com",
+        time=TIME,
+        additional_info=json.dumps(
+            {
+                "report_type": "XLS",
+                "treatments": [
+                    "ACCEPTED",
+                    "NEW",
+                ],
+                "states": ["OPEN"],
+                "verifications": [],
+                "closing_date": None,
+            }
+        ),
+    ),
+    dict(
+        action_name="report",
+        entity="unittesting",
+        subject="unittesting@fluidattacks.com",
+        time=TIME,
+        additional_info=json.dumps(
+            {
+                "report_type": "XLS",
+                "treatments": [
+                    "ACCEPTED",
+                    "ACCEPTED_UNDEFINED",
+                    "IN_PROGRESS",
+                    "NEW",
+                ],
+                "states": ["CLOSED"],
+                "verifications": ["VERIFIED"],
+                "closing_date": "2020-06-01T05:00:00+00:00",
+            }
+        ),
+    ),
+]
 
 
-async def test_get_actions() -> None:
+async def test_put_action_to_dynamodb(dynamodb: ServiceResource) -> None:
+    def side_effect(item: dict, table: str) -> bool:
+        if bool(table and item):
+            return dynamodb.Table(TABLE_NAME).put_item(Item=item)
+        return False
+
+    dynamodb.create_table(
+        TableName=TABLE_NAME,
+        KeySchema=key_schema,
+        AttributeDefinitions=attribute_definitions,
+    )
+    assert TABLE_NAME in dynamodb_backend2.tables
+    assert len(dynamodb.Table(TABLE_NAME).scan()["Items"]) == 0
+    with mock.patch("batch.dal.dynamodb_ops.put_item") as mock_put_item:
+        mock_put_item.side_effect = side_effect
+        for item in data:
+            await batch_dal.put_action_to_dynamodb(**item)
+    assert len(dynamodb.Table(TABLE_NAME).scan()["Items"]) == 5
+    assert (
+        dynamodb.Table(TABLE_NAME).scan()["Items"][0]["entity"]
+        == "oneshottest"
+    )
+
+
+async def test_get_actions(dynamodb: ServiceResource) -> None:
     with mock.patch("batch.dal.dynamodb_ops.scan") as mock_scan:
-        mock_scan.return_value = scan_actions_result["Items"]
+        mock_scan.return_value = dynamodb.Table(TABLE_NAME).scan()["Items"]
         all_actions = await batch_dal.get_actions()
     assert isinstance(all_actions, list)
     assert len(all_actions) == 5
 
 
-async def test_get_action() -> None:
-    def side_effect(table: str, key_condition: dict) -> list:
-        for item in scan_actions_result["Items"]:
-            if item["pk"] in key_condition.values() and bool(table):
-                return [item]
-        return []
+async def test_get_action(dynamodb: ServiceResource) -> None:
+    with mock.patch("batch.dal.dynamodb_ops.query") as mock_query:
+        item_1 = dict(
+            action="report",
+            entity="unittesting",
+            subject="unittesting@fluidattacks.com",
+            additional_info=json.dumps(
+                {
+                    "report_type": "XLS",
+                    "treatments": ["ACCEPTED", "NEW"],
+                    "states": ["OPEN"],
+                    "verifications": [],
+                    "closing_date": None,
+                }
+            ),
+        )
+        key = batch_dal.mapping_to_key(
+            [
+                item_1["action"],
+                item_1["entity"],
+                item_1["subject"],
+                item_1["additional_info"],
+            ]
+        )
+        mock_query.return_value = [
+            dynamodb.Table(TABLE_NAME).get_item(Key={"pk": key})["Item"]
+        ]
+        action_in_db = await batch_dal.get_action(action_dynamo_pk=key)
+        assert bool(action_in_db)
 
-    with mock.patch(
-        "batch.dal.boto3.dynamodb.conditions.Equals"
-    ) as mock_key_condition:
-        with mock.patch("batch.dal.dynamodb_ops.query") as mock_query:
-            mock_query.side_effect = side_effect
-            item_1 = dict(
-                action="report",
-                additional_info=json.dumps(
-                    {
-                        "report_type": "XLS",
-                        "treatments": list(sorted(["ACCEPTED", "NEW"])),
-                        "states": ["OPEN"],
-                        "verifications": ["REQUESTED"],
-                        "closing_date": None,
-                    }
-                ),
-                entity="unittesting",
-                subject="integratesmanager@gmail.com",
-                time="1615834776",
-            )
-            key = batch_dal.mapping_to_key(
-                [
-                    item_1["action"],
-                    item_1["additional_info"],
-                    item_1["entity"],
-                    item_1["subject"],
-                    item_1["time"],
-                ]
-            )
-            mock_key_condition.return_value = key
-            action = await batch_dal.get_action(action_dynamo_pk=key)
-            assert bool(action)
-
-            item_2 = dict(
-                action="report",
-                additional_info="PDF",
-                entity="continuoustesting",
-                subject="integratesmanager@gmail.com",
-                time="1615834776",
-            )
-            key_2 = batch_dal.mapping_to_key(
-                [
-                    item_2["action"],
-                    item_2["additional_info"],
-                    item_2["entity"],
-                    item_2["subject"],
-                    item_2["time"],
-                ]
-            )
-            mock_key_condition.return_value = key_2
-            optional_action = await batch_dal.get_action(
-                action_dynamo_pk=key_2
-            )
-            assert not bool(optional_action)
+        item_2 = dict(
+            action="report",
+            entity="continuoustesting",
+            subject="integratesmanager@gmail.com",
+            additional_info="PDF",
+        )
+        key_2 = batch_dal.mapping_to_key(
+            [
+                item_2["action"],
+                item_2["entity"],
+                item_2["subject"],
+                item_2["additional_info"],
+            ]
+        )
+        try:
+            mock_query.return_value = [
+                dynamodb.Table(TABLE_NAME).get_item(Key={"pk": key_2})["Item"]
+            ]
+        except KeyError:
+            mock_query.return_value = []
+        action_not_in_db = await batch_dal.get_action(action_dynamo_pk=key_2)
+        assert not bool(action_not_in_db)
 
 
-async def test_requeue_actions() -> None:
+async def test_requeue_actions(dynamodb: ServiceResource) -> None:
     with mock.patch("batch.dal.dynamodb_ops.scan") as mock_scan:
-        mock_scan.return_value = scan_actions_result["Items"]
+        mock_scan.return_value = dynamodb.Table(TABLE_NAME).scan()["Items"]
         pending_actions: List[BatchProcessing] = await batch_dal.get_actions()
 
     assert all(
@@ -251,39 +254,14 @@ async def test_requeue_actions() -> None:
     )
 
 
-@pytest.mark.changes_db
-async def test_put_action_to_dynamodb() -> None:
-    def side_effect(item: dict, table: str) -> bool:
-        if item and table:
-            scan_actions_result["Items"].append(item)
-            return True
-        return False
-
-    with mock.patch("batch.dal.dynamodb_ops.put_item") as mock_put_item:
-        mock_put_item.side_effect = side_effect
-        time = str(get_as_epoch(get_now()))
-        item = dict(
-            action_name="report",
-            entity="oneshottest",
-            subject="integratesmanager@gmail.com",
-            time=time,
-            additional_info="XLS",
-        )
-        key = await batch_dal.put_action_to_dynamodb(**item)
-    new_length = len(scan_actions_result["Items"])
-    added_action = scan_actions_result["Items"][new_length - 1]
-    assert new_length == 6
-    assert key == added_action["pk"]
-    assert added_action["queue"] == "small"
-
-
-async def test_delete_action() -> None:
+async def test_delete_action(dynamodb: ServiceResource) -> None:
+    key = "6da32eec4b0f7dba634335a67386983e090cc6c25f58c12244c2d6685a8f5126"
     with mock.patch("batch.dal.dynamodb_ops.delete_item") as mock_delete_item:
-        mock_delete_item.return_value = True
-        key = (
-            "7e1955e6a576d23fbb4085eae499b8d84279cd214dbc4c87d0f8447b12023156"
+        mock_delete_item.return_value = dynamodb.Table(TABLE_NAME).delete_item(
+            Key={"pk": key}
         )
         assert await batch_dal.delete_action(dynamodb_pk=key)
+    assert len(dynamodb.Table(TABLE_NAME).scan()["Items"]) == 4
     with pytest.raises(Exception) as delete_exception:
         await batch_dal.delete_action()
     assert "you must supply the dynamodb pk" in str(delete_exception.value)
