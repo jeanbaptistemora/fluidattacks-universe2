e99157af164f2817b909494769ee176145469acd
Andres Aguirre <aaguirre@fluidattacks.com>
2022-08-02 13:50:47 +0000

    integrates\feat(back): #6998.2 event index in creation
    - Include the event index during the event creation

--- integrates/back/src/db_model/events/add.py
+++ integrates/back/src/db_model/events/add.py
@@ -7,6 +7,12 @@ from custom_exceptions import (
 from db_model import (
     TABLE,
 )
+from db_model.events.constants import (
+    GSI_2_FACET,
+)
+from db_model.events.enums import (
+    EventStateStatus,
+)
 from db_model.events.utils import (
     format_event_item,
 )
@@ -20,11 +26,21 @@ import simplejson as json  # type: ignore
 async def add(*, event: Event) -> None:
     items = []
     key_structure = TABLE.primary_key
+    gsi_2_index = TABLE.indexes["gsi_2"]
     primary_key = keys.build_key(
         facet=TABLE.facets["event_metadata"],
         values={
             "id": event.id,
             "name": event.group_name,
+        },
+    )
+    gsi_2_key = keys.build_key(
+        facet=GSI_2_FACET,
+        values={
+            "is_solved": str(
+                event.state.status is EventStateStatus.SOLVED
+            ).lower(),
+            "group_name": event.group_name,
         },
     )
 
@@ -39,6 +55,8 @@ async def add(*, event: Event) -> None:
     item = {
         key_structure.partition_key: primary_key.partition_key,
         key_structure.sort_key: primary_key.sort_key,
+        gsi_2_index.primary_key.sort_key: gsi_2_key.sort_key,
+        gsi_2_index.primary_key.partition_key: gsi_2_key.partition_key,
         **format_event_item(event),
     }
     items.append(item)
