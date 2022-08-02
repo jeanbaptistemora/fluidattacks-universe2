8d77543c6054d0980cc0b5e9855997b7c4f2fa3e
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-08-02 17:43:33 -0500

    skims\refac(back): #6074.81 add method
    - Add method for F239
    - Speed: 3 method / 2 days = 1.5 method/day
    - TODO: 2 methods
    - ETA: 2 / 1.5 = 1.33 days

---
+++ skims/skims/lib_path/f239/__init__.py
@@ -0,0 +1,36 @@
+from lib_path.common import (
+    SHIELD_BLOCKING,
+)
+from lib_path.f239.dotnetconfig import (
+    not_custom_errors,
+)
+from model.core_model import (
+    Vulnerabilities,
+)
+from typing import (
+    Callable,
+    Tuple,
+)
+
+
+@SHIELD_BLOCKING
+def run_not_custom_errors(content: str, path: str) -> Vulnerabilities:
+    return not_custom_errors(content=content, path=path)
+
+
+@SHIELD_BLOCKING
+def analyze(
+    content_generator: Callable[[], str],
+    file_extension: str,
+    path: str,
+    **_: None,
+) -> Tuple[Vulnerabilities, ...]:
+    results: Tuple[Vulnerabilities, ...] = ()
+
+    if file_extension == "config":
+        results = (
+            *results,
+            run_not_custom_errors(content_generator(), path),
+        )
+
+    return results
