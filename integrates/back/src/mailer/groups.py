526f856f7cb733719d72d3c89dc4da73195fcd30
Mafe Toro <mtoro@fluidattacks.com>
2022-08-02 07:35:03 -0500

    integrates\refac(back): #7047.1 change of subject
    - change of the subject

--- integrates/back/src/mailer/groups.py
+++ integrates/back/src/mailer/groups.py
@@ -677,7 +677,7 @@ async def send_mail_missing_environment_alert(
         loaders=loaders,
         email_to=email_to,
         tags=GENERAL_TAG,
-        subject=f"[ASM] Missing {group_name} registered environments",
+        subject=f"[ASM] ACTION NEEDED: Your group {group_name} is incomplete",
         context=context,
         template_name="missing_environment_alert",
     )
