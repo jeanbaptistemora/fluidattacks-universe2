05c0a94604a71a63729eb5b90cb365f777201dea
John Perez <jperez@fluidattacks.com>
2022-08-02 17:24:29 -0500

    integrates\refac(back): #7059.4 use global client
    - Propagate the use of the global S3 client through the app

--- integrates/back/migrations/0034_delete_duplicated_evidences.py
+++ integrates/back/migrations/0034_delete_duplicated_evidences.py
@@ -5,7 +5,6 @@ for unittesting
 Execution Time:    2020-11-13 at 12:08:40 UTC-05
 Finalization Time: 2020-11-13 at 12:31:53 UTC-05
 """
-import aioboto3
 from aioextensions import (
     collect,
     in_thread,
@@ -14,14 +13,12 @@ from aioextensions import (
 from botocore.exceptions import (
     ClientError,
 )
-from context import (
-    FI_AWS_S3_ACCESS_KEY,
-    FI_AWS_S3_SECRET_KEY,
-)
-import contextlib
 import logging
 import logging.config
 import os
+from s3.resource import (
+    get_s3_resource,
+)
 from settings import (
     LOGGING,
 )
@@ -35,52 +32,39 @@ logging.config.dictConfig(LOGGING)
 # Constants
 BUCKET = "fluidintegrates.evidences"
 LOGGER = logging.getLogger(__name__)
-OPTIONS = dict(
-    aws_access_key_id=FI_AWS_S3_ACCESS_KEY,
-    aws_secret_access_key=FI_AWS_S3_SECRET_KEY,
-    aws_session_token=os.environ.get("AWS_SESSION_TOKEN"),
-    region_name="us-east-1",
-    service_name="s3",
-)
 STAGE = os.environ["STAGE"]
 
 
-@contextlib.asynccontextmanager
-async def aio_client():  # type: ignore
-    async with aioboto3.client(**OPTIONS) as client:
-        yield client
-
-
 async def get_all_project_evidences(name: str) -> List[str]:
-    async with aio_client() as client:
-        continuation_token = None
-        key_list: List[str] = []
-        while True:
-            if continuation_token:
-                resp = await client.list_objects_v2(
-                    Bucket=BUCKET,
-                    Prefix=name,
-                    ContinuationToken=continuation_token,
-                )
-            else:
-                resp = await client.list_objects_v2(Bucket=BUCKET, Prefix=name)
-            key_list += [item["Key"] for item in resp.get("Contents", [])]
-            if not resp.get("IsTruncated"):
-                break
-            continuation_token = resp.get("NextContinuationToken")
+    client = await get_s3_resource()
+    continuation_token = None
+    key_list: List[str] = []
+    while True:
+        if continuation_token:
+            resp = await client.list_objects_v2(
+                Bucket=BUCKET,
+                Prefix=name,
+                ContinuationToken=continuation_token,
+            )
+        else:
+            resp = await client.list_objects_v2(Bucket=BUCKET, Prefix=name)
+        key_list += [item["Key"] for item in resp.get("Contents", [])]
+        if not resp.get("IsTruncated"):
+            break
+        continuation_token = resp.get("NextContinuationToken")
     return key_list
 
 
 async def remove_file(name: str) -> int:
     print(f"[INFO] Deleting evidence {name}")
     success = False
-    async with aio_client() as client:
-        try:
-            response = await client.delete_object(Bucket=BUCKET, Key=name)
-            resp_code = response["ResponseMetadata"]["HTTPStatusCode"]
-            success = resp_code in [200, 204]
-        except ClientError as ex:
-            LOGGER.exception(ex, extra={"extra": locals()})
+    client = await get_s3_resource()
+    try:
+        response = await client.delete_object(Bucket=BUCKET, Key=name)
+        resp_code = response["ResponseMetadata"]["HTTPStatusCode"]
+        success = resp_code in [200, 204]
+    except ClientError as ex:
+        LOGGER.exception(ex, extra={"extra": locals()})
     return success
 
 
