59ce180e4863b58ed2661329d1dcbc713a57e0eb
Daniel Betancur <cbetancur@fluidattacks.com>
2022-08-03 11:58:16 -0500

    integrates\test(back): #4944.14 add conftest to module
    - add conftest to authz_test
    - add fixtures to mock db calls

---
+++ integrates/back/test/unit/src/authz_test/conftest.py
@@ -0,0 +1,68 @@
+import boto3
+from moto.dynamodb2 import (
+    mock_dynamodb2,
+)
+from mypy_boto3_dynamodb import (
+    DynamoDBServiceResource as ServiceResource,
+)
+import os
+import pytest
+
+
+@pytest.fixture(scope="function", autouse=True)
+def aws_credentials() -> None:
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+
+
+@pytest.fixture(scope="module")
+def dynamodb() -> ServiceResource:
+    """Mocked DynamoDB Fixture."""
+    with mock_dynamodb2():
+        yield boto3.resource("dynamodb")
+
+
+@pytest.fixture(scope="module", autouse=True)
+def create_table(dynamodb: ServiceResource) -> None:
+    table_name = "fi_authz"
+    key_schema = [
+        {"AttributeName": "object", "KeyType": "HASH"},
+        {"AttributeName": "object", "KeyType": "RANGE"},
+    ]
+    attribute_definitions = [{"AttributeName": "object", "AttributeType": "S"}]
+    data = [
+        dict(
+            level="group",
+            object="oneshottest",
+            role="reattacker",
+            subject="integrateshacker@fluidattacks.com",
+        ),
+        dict(
+            level="group",
+            object="unittesting",
+            role="hacker",
+            subject="integrateshacker@fluidattacks.com",
+        ),
+        dict(
+            level="user",
+            object="unittesting",
+            role="user",
+            subject="integratesuser2@gmail.com",
+        ),
+        dict(
+            level="organization",
+            object="org#38eb8f25-7945-4173-ab6e-0af4ad8b7ef3",
+            role="customer_manager",
+            subject="unittest2@fluidattacks.com",
+        ),
+    ]
+
+    dynamodb.create_table(
+        TableName=table_name,
+        KeySchema=key_schema,
+        AttributeDefinitions=attribute_definitions,
+    )
+    for item in data:
+        dynamodb.Table(table_name).put_item(Item=item)
