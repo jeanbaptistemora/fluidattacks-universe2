209eb1a8e2056cc0c5b948dd44f4c6827d68609a
John Perez <jperez@fluidattacks.com>
2022-08-03 10:27:49 -0500

    teaches\refac(infra): #7076.5 enable https
    - Enable only HTTPS operations on the `ultra-secure-app-backup` bucket

--- teaches/attacking-aws/infra/s3/main.tf
+++ teaches/attacking-aws/infra/s3/main.tf
@@ -15,3 +15,30 @@ resource "aws_s3_bucket" "ultra-secure-app-backup" {
   }
 }
 
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.ultra-secure-app-backup.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "BUCKET-POLICY"
+    Statement = [
+      {
+        Sid       = "EnforceTls"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          "${aws_s3_bucket.ultra-secure-app-backup.arn}/*",
+          "${aws_s3_bucket.ultra-secure-app-backup.arn}",
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+          NumericLessThan = {
+            "s3:TlsVersion" : 1.2
+          }
+        }
+      },
+    ]
+  })
+}
