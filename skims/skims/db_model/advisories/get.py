c57443d00683b55035a65f50e47f58814ae6a4de
Alejandro Trujillo <atrujillo@fluidattacks.com>
2022-08-04 14:37:59 -0500

    skims\feat(back): #6807.31 dataloader
    - advisory dataloader
    - added dependencies
    - no-deltas-test

--- skims/skims/db_model/advisories/get.py
+++ skims/skims/db_model/advisories/get.py
@@ -4,6 +4,12 @@ from .types import (
 from .utils import (
     format_advisory,
 )
+from aiodataloader import (
+    DataLoader,
+)
+from aioextensions import (
+    collect,
+)
 from boto3.dynamodb.conditions import (
     Key,
 )
@@ -15,6 +21,7 @@ from dynamodb import (
     operations,
 )
 from typing import (
+    Iterable,
     Tuple,
 )
 
@@ -37,3 +44,16 @@ async def _get_advisories(
     )
 
     return tuple(format_advisory(item) for item in response.items)
+
+
+class AdvisoriesLoader(DataLoader):
+    # pylint: disable=no-self-use,method-hidden
+    async def batch_load_fn(
+        self, ad_keys: Iterable[Tuple[str, str]]
+    ) -> Tuple[Advisory, ...]:
+        return await collect(
+            tuple(
+                _get_advisories(platform=platform, package_name=package_name)
+                for platform, package_name in ad_keys
+            )
+        )
