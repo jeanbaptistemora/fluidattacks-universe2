261f81ef709e0d66a430559f5c20a273d1dab6a3
Daniel Murcia <dmurcia@fluidattacks.com>
2022-08-05 18:29:33 -0500

    observes\fix(back): #7097.2 reformat datetime
    - reformat datetime objects

--- observes/singer/target-s3/target_s3/csv.py
+++ observes/singer/target-s3/target_s3/csv.py
@@ -2,28 +2,48 @@ from . import (
     _utils,
 )
 import csv
+from dateutil.parser import (
+    isoparse,
+)
 from fa_purity import (
     Cmd,
+    FrozenDict,
     FrozenList,
+    JsonValue,
+    Maybe,
     PureIter,
+    Result,
+    ResultE,
 )
 from fa_purity.json.primitive.core import (
     Primitive,
 )
+from fa_purity.json.primitive.factory import (
+    to_opt_primitive,
+)
+from fa_purity.json.value.transform import (
+    Unfolder,
+)
+from fa_purity.pure_iter.factory import (
+    from_flist,
+)
+from fa_purity.result.transform import (
+    all_ok,
+)
 import logging
 from target_s3.core import (
     CompletePlainRecord,
+    PlainRecord,
     RecordGroup,
     TempReadOnlyFile,
 )
 from typing import (
+    FrozenSet,
     IO,
     Tuple,
-    TypeVar,
 )
 
 LOG = logging.getLogger(__name__)
-_T = TypeVar("_T")
 
 
 def _save(data: PureIter[FrozenList[Primitive]]) -> Cmd[TempReadOnlyFile]:
@@ -48,21 +68,104 @@ def _ordered_data(record: CompletePlainRecord) -> FrozenList[Primitive]:
     return tuple(i[1] for i in ordered)
 
 
+def _is_datetime(schema: JsonValue) -> ResultE[bool]:
+    def _inner(data: FrozenDict[str, Unfolder]) -> ResultE[bool]:
+        _type = (
+            Maybe.from_optional(data.get("type"))
+            .to_result()
+            .alt(lambda _: Exception("Missing `type` key"))
+            .bind(lambda u: u.to_primitive(str).alt(Exception))
+        )
+        _format: ResultE[Maybe[str]] = (
+            Maybe.from_optional(data.get("format"))
+            .map(
+                lambda u: u.to_primitive(str)
+                .alt(lambda _: Exception("Expected `str` at `format` key"))
+                .map(lambda x: Maybe.from_value(x))
+            )
+            .value_or(Result.success(Maybe.empty()))
+        )
+        return _type.bind(
+            lambda t: _format.map(
+                lambda f: t == "string" and f.value_or(None) == "date-time"
+            )
+        )
+
+    return Unfolder(schema).to_unfolder_dict().alt(Exception).bind(_inner)
+
+
+def _reformat_group_records(
+    datetime_props: FrozenSet[str], record: CompletePlainRecord
+) -> ResultE[CompletePlainRecord]:
+    def _to_prim(item: str) -> Primitive:
+        return item
+
+    def _adjust(key: str, value: Primitive) -> ResultE[Primitive]:
+        if key in datetime_props:
+            return (
+                to_opt_primitive(value, str)
+                .map(
+                    lambda x: _to_prim(
+                        isoparse(x).strftime("%Y-%m-%d %H:%M:%S")
+                    ) if x else None
+                )
+                .alt(Exception)
+            )
+        return Result.success(value)
+
+    _records = all_ok(
+        tuple(
+            _adjust(k, v).map(lambda a: (k, a))
+            for k, v in record.record.record.items()
+        )
+    ).map(lambda l: FrozenDict(dict(l)))
+    return _records.bind(
+        lambda r: CompletePlainRecord.new(
+            record.schema, PlainRecord.new(record.schema.stream, r)
+        )
+    )
+
+
+def _reformat_group(group: RecordGroup) -> RecordGroup:
+    datetime_properties = (
+        Maybe.from_optional(group.schema.schema.encode().get("properties"))
+        .to_result()
+        .alt(lambda _: Exception("Missing `properties` key"))
+        .bind(lambda j: Unfolder(j).to_json().alt(Exception))
+        .map(lambda d: tuple(d.items()))
+        .map(
+            lambda p: tuple(
+                _is_datetime(i[1]).map(lambda b: (i[0], b)) for i in p
+            )
+        )
+        .bind(lambda x: all_ok(x).map(lambda y: from_flist(y)))
+        .map(lambda p: p.filter(lambda t: t[1]).map(lambda x: x[0]))
+        .map(lambda x: frozenset(x))
+    )
+    records = datetime_properties.map(
+        lambda p: group.records.map(
+            lambda c: _reformat_group_records(p, c).unwrap()
+        )
+    ).unwrap()
+    return RecordGroup.new(group.schema, records).unwrap()
+
+
 def save(
     group: RecordGroup,
 ) -> Cmd[TempReadOnlyFile]:
+    _group = _reformat_group(group)
     msg = _utils.log_cmd(
         lambda: LOG.info(
-            "Saving stream `%s` data into temp", group.schema.stream
+            "Saving stream `%s` data into temp", _group.schema.stream
         ),
         None,
     )
-    return msg + group.records.map(_ordered_data).transform(
+    return msg + _group.records.map(_ordered_data).transform(
         lambda x: _save(x)
     ).bind(
         lambda t: _utils.log_cmd(
             lambda: LOG.info(
-                "Stream `%s` saved into temp file!", group.schema.stream
+                "Stream `%s` saved into temp file!", _group.schema.stream
             ),
             t,
         )
