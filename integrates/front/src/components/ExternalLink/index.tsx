cfa50baa02b929bc450536aa7891b4ff64782e73
Estid Lozano <elozano@fluidattacks.com>
2022-08-05 12:04:35 -0500

    integrates\feat(front): #6573 externallink design
    - Add hover effect
    - Add underline
    - Add identifiable class
    - Other minor styles

--- integrates/front/src/components/ExternalLink/index.tsx
+++ integrates/front/src/components/ExternalLink/index.tsx
@@ -7,11 +7,20 @@ type ExternalLinkProps = Omit<
 >;
 
 const ExternalLink = styled.a.attrs<ExternalLinkProps>({
-  className: "link",
+  className: "comp-ext-link f6 link",
   // https://owasp.org/www-community/attacks/Reverse_Tabnabbing
   rel: "nofollow noopener noreferrer",
   target: "_blank",
-})``;
+})`
+  border-radius: 4px;
+  color: #5c5c70;
+  padding: 9px 6px;
+  text-decoration: underline;
+  :hover {
+    background-color: #dddde3;
+    color: #121216;
+  }
+`;
 
 export type { ExternalLinkProps };
 export { ExternalLink };
