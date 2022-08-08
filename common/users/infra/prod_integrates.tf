7dc4fad403c0784bc8329bad2d5610fbf67be928
Diego Restrepo <drestrepo@fluidattacks.com>
2022-08-08 09:34:04 -0500

    common\feat(infra): #7095.3 add permissions
    - allow to get skims results

--- common/users/infra/prod_integrates.tf
+++ common/users/infra/prod_integrates.tf
@@ -65,6 +65,8 @@ locals {
             Resource = [
               "arn:aws:s3:::continuous-data",
               "arn:aws:s3:::continuous-data/*",
+              "arn:aws:s3:::skims.data",
+              "arn:aws:s3:::skims.data/*",
             ]
           },
           {
