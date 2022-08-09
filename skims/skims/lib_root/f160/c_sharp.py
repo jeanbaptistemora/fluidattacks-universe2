cd33ae5147c66ce8058b5e70885114869851e5d3
Flor Calderon <fcalderon@fluidattacks.com>
2022-08-09 15:21:17 -0500

    skims\refac(back): #7020.10 refac f160
    - modified function create temp file to use syntax graph

--- skims/skims/lib_root/f160/c_sharp.py
+++ skims/skims/lib_root/f160/c_sharp.py
@@ -1,6 +1,3 @@
-from lib_root.utilities.c_sharp import (
-    yield_invocation_expression,
-)
 from lib_sast.types import (
     ShardDb,
 )
@@ -11,6 +8,9 @@ from model import (
 from sast.query import (
     get_vulnerabilities_from_n_ids,
 )
+from utils import (
+    graph as g,
+)
 from utils.string import (
     complete_attrs_on_set,
 )
@@ -20,24 +20,34 @@ def c_sharp_file_create_temp_file(
     shard_db: ShardDb,  # pylint: disable=unused-argument
     graph_db: graph_model.GraphDB,
 ) -> core_model.Vulnerabilities:
+    method = core_model.MethodsEnum.CS_CREATE_TEMP_FILE
+    c_sharp = graph_model.GraphShardMetadataLanguage.CSHARP
+
     def n_ids() -> graph_model.GraphShardNodes:
-        danger_methods = complete_attrs_on_set(
-            {"System.IO.Path.GetTempFileName"}
-        )
-        for (
-            shard,
-            method_id,
-            method_name,
-        ) in yield_invocation_expression(graph_db):
-            if method_name in danger_methods:
-                yield shard, method_id
+
+        for shard in graph_db.shards_by_language(c_sharp):
+            if shard.syntax_graph is None:
+                continue
+
+            graph = shard.syntax_graph
+            danger_methods = complete_attrs_on_set(
+                {"System.IO.Path.GetTempFileName"}
+            )
+            for nid in g.filter_nodes(
+                graph,
+                nodes=graph.nodes,
+                predicate=g.pred_has_labels(label_type="MethodInvocation"),
+            ):
+                method_name = graph.nodes[nid].get("expression")
+                if method_name in danger_methods:
+                    yield shard, nid
 
     translation_key = (
         "src.lib_path.f160.c_sharp_file_create_temp_file.description"
     )
     return get_vulnerabilities_from_n_ids(
         desc_key=translation_key,
-        desc_params=dict(lang="C#"),
+        desc_params={},
         graph_shard_nodes=n_ids(),
-        method=core_model.MethodsEnum.CS_CREATE_TEMP_FILE,
+        method=method,
     )
