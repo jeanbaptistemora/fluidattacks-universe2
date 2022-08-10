114dcf8d534a639dc86e173d6d779fe53259c900
Flor Calderon <fcalderon@fluidattacks.com>
2022-08-10 13:50:05 -0500

    skims\refac(back): #7020.12 named argument evaluator
    - add test to evaluate NamedArgument node type

---
+++ skims/skims/symbolic_eval/cases/named_argument.py
@@ -0,0 +1,21 @@
+from model.core_model import (
+    FindingEnum,
+)
+from symbolic_eval.types import (
+    Evaluator,
+    SymbolicEvalArgs,
+    SymbolicEvaluation,
+)
+from typing import (
+    Dict,
+)
+
+FINDING_EVALUATORS: Dict[FindingEnum, Evaluator] = {}
+
+
+def evaluate(args: SymbolicEvalArgs) -> SymbolicEvaluation:
+    args.evaluation[args.n_id] = False
+    if finding_evaluator := FINDING_EVALUATORS.get(args.method.value.finding):
+        args.evaluation[args.n_id] = finding_evaluator(args).danger
+
+    return SymbolicEvaluation(args.evaluation[args.n_id], args.triggers)
