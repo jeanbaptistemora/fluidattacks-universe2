41042730941e71f08f6a0f82119d83da60cc449c
Daniel Murcia <dmurcia@fluidattacks.com>
2022-08-10 11:15:21 -0500

    observes\fix(job): #6624.61 missing region
    - add missing region on scheduler

--- observes/job/scheduler/entrypoint.sh
+++ observes/job/scheduler/entrypoint.sh
@@ -1,6 +1,7 @@
 # shellcheck shell=bash
 
 : \
+  && export AWS_DEFAULT_REGION="us-east-1" \
   && sops_export_vars 'observes/secrets/prod.yaml' \
     bugsnag_notifier_key \
   && observes-scheduler run-schedule
