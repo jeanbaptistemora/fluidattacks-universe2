62e148434b5efdbabb0627952f6e0307eec69c2a
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-08-11 14:33:48 -0500

    skims\feat(back): #6769.3 add method
    - Add method for F024
    - Speed: 2 method / 1 days = 2 method/day
    - TODO: 8 methods
    - ETA: 8 / 1 = 8 days

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -1754,7 +1754,15 @@ class MethodsEnum(Enum):
     )
     AWS_SEC_GROUPS_RFC1918 = MethodInfo(
         file_name="aws",
-        name="unrestricted_ip_protocols",
+        name="security_groups_ip_ranges_in_rfc1918",
+        module="dast",
+        finding=FindingEnum.F024,
+        developer=DeveloperEnum.LUIS_SAAVEDRA,
+        technique=TechniqueEnum.DAST,
+    )
+    AWS_UNRESTRICTED_DNS_ACCESS = MethodInfo(
+        file_name="aws",
+        name="unrestricted_dns_access",
         module="dast",
         finding=FindingEnum.F024,
         developer=DeveloperEnum.LUIS_SAAVEDRA,
