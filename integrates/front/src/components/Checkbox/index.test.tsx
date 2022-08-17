6503be66a8fffe58d31f977beade1efd7fa30b56
Estid Lozano <elozano@fluidattacks.com>
2022-08-17 14:17:35 -0500

    integrates\refac(front): #6573 cleanup old checkbox
    - Remove files of old Checkbox component

--- integrates/front/src/components/Checkbox/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import { Checkbox } from ".";
-
-describe("Checkbox", (): void => {
-  it("should return a function", (): void => {
-    expect.hasAssertions();
-    expect(typeof Checkbox).toBe("function");
-  });
-});
