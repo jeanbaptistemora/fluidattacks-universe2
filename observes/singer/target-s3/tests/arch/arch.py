ed238dc005c20d03d44e9ee141d415b16d67062c
Daniel Murcia <dmurcia@fluidattacks.com>
2022-08-18 17:28:22 -0500

    observes\test(back): #7141.8 target s3 arch
    - use new lib for target s3 arch test

---
+++ observes/singer/target-s3/tests/arch/arch.py
@@ -0,0 +1,50 @@
+from arch_lint.dag.core import (
+    DAG,
+    new_dag,
+)
+from arch_lint.graph import (
+    FullPathModule,
+)
+from typing import (
+    Dict,
+    FrozenSet,
+    Tuple,
+    Union,
+)
+
+_dag: Dict[str, Tuple[Union[Tuple[str, ...], str], ...]] = {
+    "target_s3": (
+        "_cli",
+        "loader",
+        "upload",
+        "csv",
+        "_splitter",
+        "core",
+        "in_buffer",
+        "_utils",
+        "_parallel",
+        "_logger",
+    ),
+    "target_s3.core": (
+        "_record_group",
+        "_complete_record",
+        "_plain_record",
+        "_ro_file",
+    ),
+}
+
+
+def project_dag() -> DAG:
+    return new_dag(_dag)
+
+
+def forbidden_allowlist() -> Dict[FullPathModule, FrozenSet[FullPathModule]]:
+    _raw: Dict[str, FrozenSet[str]] = {
+        "pathos": frozenset(["target_s3._parallel"]),
+    }
+    return {
+        FullPathModule.from_raw(k): frozenset(
+            FullPathModule.from_raw(i) for i in v
+        )
+        for k, v in _raw.items()
+    }
