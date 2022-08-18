9219124c4186a63b6077c884d4e444ee5fb57bcc
Carlos Betancur <cbetancur@fluidattacks.com>
2022-08-18 17:43:36 -0500

    integrates\test(back): #4944.23 mock s3 calls
    - modify tests to mock s3 calls
    - add conftest with mock s3 fixture
    - add csv file
    - no-deltas-test

---
+++ integrates/back/test/unit/src/findings/domain/conftest.py
@@ -0,0 +1,28 @@
+import boto3
+from moto import (
+    mock_s3,
+)
+from mypy_boto3_s3 import (
+    S3Client,
+)
+import os
+import pytest
+
+BUCKET_NAME = "test_bucket"
+
+
+@pytest.fixture(scope="function", autouse=True)
+def aws_credentials() -> None:
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+
+
+@pytest.fixture(scope="module")
+def s3_mock() -> S3Client:
+    """Mocked S3 Fixture."""
+    with mock_s3():
+        s3_client = boto3.client("s3")
+        s3_client.create_bucket(Bucket=BUCKET_NAME)
+        yield s3_client
--- integrates/back/test/unit/src/findings/domain/test_evidence.py
+++ integrates/back/test/unit/src/findings/domain/test_evidence.py
@@ -6,22 +6,82 @@ from findings.domain import (
     get_records_from_file,
     validate_evidence,
 )
+from mypy_boto3_s3 import (
+    S3Client,
+)
 import os
 import pytest
 from starlette.datastructures import (
     UploadFile,
+)
+from unittest import (
+    mock,
 )
 
 pytestmark = [
     pytest.mark.asyncio,
 ]
 
+BUCKET_NAME = "test_bucket"
 
-async def test_download_evidence_file() -> None:
+
+async def test_create_test_bucket(s3_mock: S3Client) -> None:
+    result = s3_mock.list_buckets()
+    assert len(result["Buckets"]) == 1
+    assert result["Buckets"][0]["Name"] == "test_bucket"
+
+
+@pytest.mark.parametrize(
+    [
+        "group_name",
+        "file_name",
+        "finding_id",
+    ],
+    [
+        [
+            "unittesting",
+            "unittesting-422286126-evidence_route_1.png",
+            "422286126",
+        ],
+        [
+            "unittesting",
+            "unittesting-422286126-evidence_file.csv",
+            "422286126",
+        ],
+    ],
+)
+async def test_upload_test_file(
+    group_name: str, file_name: str, finding_id: str, s3_mock: S3Client
+) -> None:
+    file_location = os.path.dirname(os.path.abspath(__file__))
+    file_location = os.path.join(file_location, "mock/" + file_name)
+    file_name = "/".join([group_name.lower(), finding_id, file_name])
+    with open(file_location, "rb") as data:
+        s3_mock.upload_fileobj(data, BUCKET_NAME, file_name)
+    assert bool(s3_mock.get_object(Bucket=BUCKET_NAME, Key=file_name))
+
+
+async def test_download_evidence_file(s3_mock: S3Client) -> None:
+    def side_effect(bucket: str, file_name: str, file_path: str) -> None:
+        if bool(bucket and file_name and file_path):
+            s3_mock.download_file(BUCKET_NAME, file_name, file_path)
+
     group_name = "unittesting"
     finding_id = "422286126"
     file_name = "unittesting-422286126-evidence_route_1.png"
-    test_data = await download_evidence_file(group_name, finding_id, file_name)
+    file_key = "/".join([group_name.lower(), finding_id, file_name])
+    with mock.patch("s3.operations.download_file") as mock_download:
+        mock_download.side_effect = side_effect
+        with mock.patch(
+            "findings.storage.search_evidence"
+        ) as mock_search_evidence:
+            mock_search_evidence.return_value = bool(
+                s3_mock.get_object(Bucket=BUCKET_NAME, Key=file_key)
+            )
+            test_data = await download_evidence_file(
+                group_name, finding_id, file_name
+            )
+
     expected_output = os.path.abspath(
         # FP: local testing
         "/tmp/unittesting-422286126-evidence_route_1.png"  # NOSONAR
@@ -29,11 +89,26 @@ async def test_download_evidence_file() -> None:
     assert test_data == expected_output
 
 
-async def test_get_records_from_file() -> None:
+async def test_get_records_from_file(s3_mock: S3Client) -> None:
+    def side_effect(bucket: str, file_name: str, file_path: str) -> None:
+        if bool(bucket and file_name and file_path):
+            s3_mock.download_file(BUCKET_NAME, file_name, file_path)
+
     group_name = "unittesting"
     finding_id = "422286126"
     file_name = "unittesting-422286126-evidence_file.csv"
-    test_data = await get_records_from_file(group_name, finding_id, file_name)
+    file_key = "/".join([group_name.lower(), finding_id, file_name])
+    with mock.patch("s3.operations.download_file") as mock_download:
+        mock_download.side_effect = side_effect
+        with mock.patch(
+            "findings.storage.search_evidence"
+        ) as mock_search_evidence:
+            mock_search_evidence.return_value = bool(
+                s3_mock.get_object(Bucket=BUCKET_NAME, Key=file_key)
+            )
+            test_data = await get_records_from_file(
+                group_name, finding_id, file_name
+            )
     expected_output = [
         OrderedDict(
             [
@@ -46,11 +121,11 @@ async def test_get_records_from_file() -> None:
             [
                 ("song", "heart shaped box"),
                 ("artist", "nirvana"),
-                ("year", "1992"),
+                ("year", "nirvana"),
             ]
         ),
         OrderedDict(
-            [("song", "zenith"), ("artist", "ghost"), ("year", "2015")]
+            [("song", "zenith"), ("artist", "zenith"), ("year", "2015")]
         ),
         OrderedDict(
             [("song", "hysteria"), ("artist", "def leppard"), ("year", "1987")]
