24937d83bae1f036dd58c3afcfec5fe19594b346
Daniel Murcia <dmurcia@fluidattacks.com>
2022-08-18 17:09:17 -0500

    observes\test(back): #7141.7 tap dynamo arch
    - def project dag
    - test arch using new lib

---
+++ observes/singer/tap-dynamo/tests/arch/arch.py
@@ -0,0 +1,40 @@
+from arch_lint.dag.core import (
+    DAG,
+    new_dag,
+)
+from arch_lint.graph import (
+    FullPathModule,
+)
+from typing import (
+    Dict,
+    FrozenSet,
+    Tuple,
+    Union,
+)
+
+_dag: Dict[str, Tuple[Union[Tuple[str, ...], str], ...]] = {
+    "tap_dynamo": ("cli", "extractor", ("client", "dynamo")),
+    "tap_dynamo.dynamo": ("item_factory", "core"),
+}
+
+
+def project_dag() -> DAG:
+    return new_dag(_dag)
+
+
+def forbidden_allowlist() -> Dict[FullPathModule, FrozenSet[FullPathModule]]:
+    _raw: Dict[str, FrozenSet[str]] = {
+        "boto3": frozenset(
+            [
+                "tap_dynamo.client",
+                "tap_dynamo.dynamo.item_factory",
+                "tap_dynamo.dynamo.core",
+            ]
+        ),
+    }
+    return {
+        FullPathModule.from_raw(k): frozenset(
+            FullPathModule.from_raw(i) for i in v
+        )
+        for k, v in _raw.items()
+    }
