700faeca602eb48dca8a5e8f9f09f3c46c999a55
Estid Lozano <elozano@fluidattacks.com>
2022-08-19 14:38:12 -0500

    integrates\feat(front): #6573 badge design
    - Add border
    - Slightly change some bgColors and colors
    - Props destructure
    - Separate TVariant from Ivariant

--- integrates/front/src/components/Badge/index.tsx
+++ integrates/front/src/components/Badge/index.tsx
@@ -1,39 +1,53 @@
 import styled from "styled-components";
 
+type TVariant = "gray" | "green" | "orange" | "red";
+
 interface IBadgeProps {
-  variant: "gray" | "green" | "orange" | "red";
+  variant: TVariant;
 }
 
 interface IVariant {
   bgColor: string;
+  borderColor: string;
   color: string;
 }
 
-const variants: Record<IBadgeProps["variant"], IVariant> = {
+const variants: Record<TVariant, IVariant> = {
   gray: {
     bgColor: "#e9e9ed",
+    borderColor: "#c7c7d1",
     color: "#2e2e38",
   },
   green: {
-    bgColor: "#c2ffd4",
+    bgColor: "#dbffdb",
+    borderColor: "#afd8b5",
     color: "#009245",
   },
   orange: {
-    bgColor: "#ffebd6",
-    color: "#ff961e",
+    bgColor: "#ffeecc",
+    borderColor: "#ffdca9",
+    color: "#d88218",
   },
   red: {
-    bgColor: "#ffd6d6",
-    color: "#bf0b1a",
+    bgColor: "#fdd8da",
+    borderColor: "#fbb1b5",
+    color: "#cc8080",
   },
 };
 
 const Badge = styled.span<IBadgeProps>`
-  background-color: ${({ variant }): string => variants[variant].bgColor};
   border-radius: 50px;
-  color: ${({ variant }): string => variants[variant].color};
   font-weight: 400;
   padding: 4px 12px;
+  ${({ variant }): string => {
+    const { bgColor, borderColor, color } = variants[variant];
+
+    return `
+      background-color: ${bgColor};
+      border: 1px solid ${borderColor};
+      color: ${color};
+    `;
+  }}
 `;
 
 export type { IBadgeProps };
