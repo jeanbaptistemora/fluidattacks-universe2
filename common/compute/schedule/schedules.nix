e991d10ea2ccedfaa961bd1cda79bd554395dcc6
Diego Restrepo <drestrepo@fluidattacks.com>
2022-08-19 07:55:44 -0500

    common\feat(infra): #7095.26 add schedule
    - run report machine schedule

--- common/compute/schedule/schedules.nix
+++ common/compute/schedule/schedules.nix
@@ -430,6 +430,35 @@
       "management:type" = "product";
     };
   };
+  integrates_machine_report = {
+    enabled = true;
+    command = [
+      "m"
+      "gitlab:fluidattacks/universe@trunk"
+      "/integrates/utils/scheduler"
+      "prod"
+      "schedulers.report_machine.main"
+    ];
+
+    schedule_expression = "cron(0 7-21 ? * 2-6 *)";
+    size = "nano";
+    awsRole = "prod_integrates";
+    attempts = 3;
+    timeout = 3600;
+    parallel = 1;
+
+    environment = [
+      "CACHIX_AUTH_TOKEN"
+      "UNIVERSE_API_TOKEN"
+    ];
+
+    tags = {
+      "Name" = "integrates_machine_report";
+      "management:area" = "cost";
+      "management:product" = "integrates";
+      "management:type" = "product";
+    };
+  };
   integrates_refresh_toe_lines = {
     enabled = true;
     command = [
