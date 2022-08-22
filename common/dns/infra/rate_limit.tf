2d96d9768861fb108e9770205dbdb9414fd1ccd0
Daniel Salazar <dsalazar@fluidattacks.com>
2022-08-22 14:15:32 -0500

    common\refac(infra): #7214 unify rate limits
    - Use only one generic rate limit
    - Remove api specific one

--- common/dns/infra/rate_limit.tf
+++ common/dns/infra/rate_limit.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_rate_limit" "integrates_production" {
   zone_id             = cloudflare_zone.fluidattacks_com.id
-  threshold           = 400
+  threshold           = 200
   period              = 60
   disabled            = false
   description         = "Integrates production"
@@ -11,42 +11,6 @@ resource "cloudflare_rate_limit" "integrates_production" {
       url_pattern = "app.${cloudflare_zone.fluidattacks_com.zone}/*"
       schemes     = ["_ALL_"]
       methods     = ["_ALL_"]
-    }
-    response {
-      origin_traffic = true
-
-      headers = [
-        {
-          "name"  = "Cf-Cache-Status"
-          "op"    = "ne"
-          "value" = "HIT"
-        },
-      ]
-    }
-  }
-
-  action {
-    mode = "js_challenge"
-  }
-
-  correlate {
-    by = "nat"
-  }
-}
-
-resource "cloudflare_rate_limit" "integrates_production_api" {
-  zone_id             = cloudflare_zone.fluidattacks_com.id
-  threshold           = 200
-  period              = 60
-  disabled            = false
-  description         = "Integrates production API"
-  bypass_url_patterns = []
-
-  match {
-    request {
-      url_pattern = "app.${cloudflare_zone.fluidattacks_com.zone}/api"
-      schemes     = ["_ALL_"]
-      methods     = ["POST"]
     }
     response {
       origin_traffic = true
