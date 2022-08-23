b35cdd566ddfd31261901594743d8e4f82743565
Jhon Romero <jromero@fluidattacks.com>
2022-08-23 20:35:32 +0000

    integrates\feat(back): #6115.7 forces executions query
    - forces execution query resolver switched and formatted

--- integrates/back/src/api/resolvers/query/forces_execution.py
+++ integrates/back/src/api/resolvers/query/forces_execution.py
@@ -1,20 +1,26 @@
 from ariadne.utils import (
     convert_kwargs_to_snake_case,
 )
+from dataloaders import (
+    Dataloaders,
+)
+from db_model.forces.types import (
+    ForcesExecution,
+)
 from decorators import (
     concurrent_decorators,
     enforce_group_level_auth_async,
     require_asm,
     require_login,
 )
-from forces import (
-    domain as forces_domain,
-)
 from functools import (
     partial,
 )
 from graphql.type.definition import (
     GraphQLResolveInfo,
+)
+from newutils.forces import (
+    format_forces_to_resolve,
 )
 from newutils.utils import (
     get_key_or_fallback,
@@ -48,10 +54,12 @@ async def resolve(
 
 
 async def resolve_no_cache(
-    _parent: None, _info: GraphQLResolveInfo, **kwargs: str
+    _parent: None, info: GraphQLResolveInfo, **kwargs: str
 ) -> Dict[str, Any]:
     execution_id: str = kwargs["execution_id"]
     group_name: str = get_key_or_fallback(kwargs)
-    return await forces_domain.get_execution(
-        execution_id=execution_id, group_name=group_name.lower()
+    loaders: Dataloaders = info.context.loaders
+    execution: ForcesExecution = await loaders.forces_execution.load(
+        (group_name, execution_id)
     )
+    return format_forces_to_resolve(execution=execution)
--- integrates/back/src/newutils/forces.py
+++ integrates/back/src/newutils/forces.py
@@ -63,3 +63,58 @@ def format_forces(item: Item) -> ForcesExecution:
         severity_threshold=int(item["severity_threshold"]),
         vulnerabilities=format_forces_vulnerabilities(item["vulnerabilities"]),
     )
+
+
+def format_explotability(execution: Item) -> Item:
+    for _, vulnerabilities in execution.get("vulnerabilities", {}).items():
+        if not isinstance(vulnerabilities, list):
+            continue
+
+        for vuln in vulnerabilities:
+            explot = {
+                "0.91": "Unproven",
+                "0.94": "Proof of concept",
+                "0.97": "Functional",
+                "1.0": "High",
+                "1": "High",
+            }.get(str(vuln.get("exploitability", 0)), "-")
+            vuln["exploitability"] = explot
+    return execution
+
+
+def format_forces_to_resolve(execution: ForcesExecution) -> Item:
+    item = {
+        "execution_id": execution.id,
+        "group_name": execution.group_name,
+        "date": execution.date,
+        "git_commit": execution.commit,
+        "git_repo": execution.repo,
+        "git_branch": execution.branch,
+        "kind": execution.kind,
+        "exit_code": execution.exit_code,
+        "strictness": execution.strictness,
+        "git_origin": execution.origin,
+        "grace_period": execution.grace_period,
+        "severity_threshold": execution.severity_threshold,
+        "vulnerabilities": {
+            "num_of_accepted_vulnerabilities": (
+                execution.vulnerabilities.num_of_accepted_vulnerabilities
+            ),
+            "num_of_open_vulnerabilities": (
+                execution.vulnerabilities.num_of_open_vulnerabilities
+            ),
+            "num_of_closed_vulnerabilities": (
+                execution.vulnerabilities.num_of_closed_vulnerabilities
+            ),
+            "open": list(execution.vulnerabilities.open)
+            if execution.vulnerabilities.open
+            else [],
+            "closed": list(execution.vulnerabilities.closed)
+            if execution.vulnerabilities.closed
+            else [],
+            "accepted": list(execution.vulnerabilities.accepted)
+            if execution.vulnerabilities.accepted
+            else [],
+        },
+    }
+    return format_explotability(item)
