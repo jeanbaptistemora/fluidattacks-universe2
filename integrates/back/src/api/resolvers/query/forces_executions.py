3caf1553ab4b2bbed6f4c1f07495c04251d4cf15
Jhon Romero <jromero@fluidattacks.com>
2022-08-23 21:16:46 +0000

    integrates\feat(back): #6115.8 forces executions resolver
    - forces executions resolver switched and formatted

--- integrates/back/src/api/resolvers/query/forces_executions.py
+++ integrates/back/src/api/resolvers/query/forces_executions.py
@@ -1,17 +1,23 @@
 from ariadne.utils import (
     convert_kwargs_to_snake_case,
 )
+from dataloaders import (
+    Dataloaders,
+)
+from db_model.forces.types import (
+    ForcesExecution,
+)
 from decorators import (
     concurrent_decorators,
     enforce_group_level_auth_async,
     require_asm,
     require_login,
 )
-from forces import (
-    domain as forces_domain,
-)
 from graphql.type.definition import (
     GraphQLResolveInfo,
+)
+from newutils.forces import (
+    format_forces_to_resolve,
 )
 from newutils.utils import (
     get_key_or_fallback,
@@ -19,8 +25,6 @@ from newutils.utils import (
 )
 from typing import (
     Any,
-    Dict,
-    List,
 )
 
 
@@ -31,24 +35,19 @@ from typing import (
     require_asm,
 )
 async def resolve(
-    _parent: None, _info: GraphQLResolveInfo, **kwargs: Any
-) -> Dict[str, Any]:
+    _parent: None, info: GraphQLResolveInfo, **kwargs: Any
+) -> dict[str, Any]:
     # Compatibility with old API
     group_name: str = get_key_or_fallback(kwargs).lower()
     group_name_key: str = get_present_key(kwargs)
+    loaders: Dataloaders = info.context.loaders
 
-    executions: List[Dict[str, Any]] = []
-    limit = 100
-    counter = 0
-    async for execution in forces_domain.get_executions(
-        group_name=group_name,
-        group_name_key=group_name_key,
-    ):
-        executions.append(execution)
-        counter += 1
-        if counter == limit:
-            break
-
+    executions_typed: tuple[
+        ForcesExecution, ...
+    ] = await loaders.forces_executions.load(group_name)
+    executions = [
+        format_forces_to_resolve(execution) for execution in executions_typed
+    ]
     return {
         "executions": executions,
         f"{group_name_key}": group_name,
