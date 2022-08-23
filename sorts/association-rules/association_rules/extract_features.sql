442a2589243e93149b32f60fe5e41d14131488a8
Karen Camargo <kcamargo@fluidattacks.com>
2022-08-23 22:29:41 +0000

    sorts\refac(back): #7060 decrease tech debt
    - set variable
    - Solved 2 smells in 1 hrs (2 smells/hr)
    - Remaining: 133 smells, 66.5 hrs

--- sorts/association-rules/association_rules/extract_features.sql
+++ sorts/association-rules/association_rules/extract_features.sql
@@ -1,8 +1,14 @@
+WITH tmp_variables AS (
+SELECT
+        'VULN#%' AS PKAlias_vms,
+        'FIN#%' AS SKAlias_vms
+)
+
 WITH vulnerabilities AS (
 SELECT "Integrates Vms"."finding_id_str" AS "Finding Id Str",
        "Integrates Vms"."id_str" AS "Uuid Str",
        CASE WHEN CHARINDEX(' ', "Integrates Vms"."where_str") > 0
-       THEN SUBSTRING("Integrates Vms"."where_str", 1, CHARINDEX(' ', "Integrates Vms"."where_str")-1) 
+       THEN SUBSTRING("Integrates Vms"."where_str", 1, CHARINDEX(' ', "Integrates Vms"."where_str")-1)
        ELSE "Integrates Vms"."where_str" END AS "Where Str",
        "Integrates Vms"."where_str" AS "Raw_File",
        "Integrates Vms"."specific_str" AS "Specific Str",
@@ -16,8 +22,8 @@ SELECT "Integrates Vms"."finding_id_str" AS "Finding Id Str",
        "Integrates Vms"."skims_method_str" AS "Skims Method str",
        "Integrates Vms"."developer_str" AS "Developer Str"
 FROM "dynamodb"."integrates_vms" AS "Integrates Vms"
-WHERE ("Integrates Vms"."pk_str" LIKE 'VULN#%'
-       AND "Integrates Vms"."sk_str" LIKE 'FIN#%')
+WHERE ("Integrates Vms"."pk_str" LIKE (SELECT PKAlias_vms FROM tmp_variables)
+       AND "Integrates Vms"."sk_str" LIKE (SELECT SKAlias_vms FROM tmp_variables))
 ),
 
 findings AS (
@@ -58,13 +64,13 @@ SELECT vms.severity__integrity_impact_float,
         vms.severity__availability_impact_float,
         vms.severity__confidentiality_requirement_float
 FROM "dynamodb"."integrates_vms" AS vms
-WHERE vms.pk_str LIKE 'FIN#%' AND
+WHERE vms.pk_str LIKE (SELECT SKAlias_vms FROM tmp_variables) AND
 vms.sk_str LIKE 'GROUP#%' AND
 vms.title_str IS NOT NULL
 ),
 
 findings_historic_state AS (
-SELECT 
+SELECT
         ROW_NUMBER() OVER(PARTITION BY vms.pk_str ORDER BY vms.modified_date_datetime ASC)-1 AS forward_index_int,
         ROW_NUMBER() OVER(PARTITION BY vms.pk_str ORDER BY vms.modified_date_datetime DESC)-1 AS backward_index_int,
         vms.modified_date_datetime,
@@ -74,7 +80,7 @@ SELECT
         vms.status_str,
         SUBSTRING(vms.pk_str, 5) AS id_str
 FROM "dynamodb"."integrates_vms" AS vms
-WHERE vms.pk_str LIKE 'FIN#%'
+WHERE vms.pk_str LIKE (SELECT SKAlias_vms FROM tmp_variables)
 AND vms.sk_str LIKE 'STATE#%'
 ),
 
@@ -89,12 +95,12 @@ v AS (
        "Integrates Vms"."commit_str" AS "Commit Str",
        "Integrates Vms"."root_id_str" AS "Root Id Str"
 FROM "dynamodb"."integrates_vms" AS "Integrates Vms"
-WHERE ("Integrates Vms"."pk_str" LIKE 'VULN#%'
-       AND "Integrates Vms"."sk_str" LIKE 'FIN#%')
+WHERE ("Integrates Vms"."pk_str" LIKE (SELECT PKAlias_vms FROM tmp_variables)
+       AND "Integrates Vms"."sk_str" LIKE (SELECT SKAlias_vms FROM tmp_variables))
   ),
-  
+
 s AS (SELECT * FROM(
-SELECT 
+SELECT
         ROW_NUMBER() OVER(PARTITION BY "Integrates Vms"."pk_str" ORDER BY "Integrates Vms"."modified_date_datetime" ASC)-1 AS "Forward Index Int",
         ROW_NUMBER() OVER(PARTITION BY "Integrates Vms"."pk_str" ORDER BY "Integrates Vms"."modified_date_datetime" DESC)-1 AS "Backward Index Int",
         SUBSTRING("Integrates Vms"."pk_str", 6) AS "Uuid",
@@ -103,14 +109,14 @@ SELECT
         "Integrates Vms"."status_str" AS "Status Str",
         "Integrates Vms"."comment_id_str" AS "Comment Id Str"
 FROM "dynamodb"."integrates_vms" AS "Integrates Vms"
-WHERE ("Integrates Vms"."pk_str" LIKE 'VULN#%'
+WHERE ("Integrates Vms"."pk_str" LIKE (SELECT PKAlias_vms FROM tmp_variables)
        AND "Integrates Vms"."sk_str" LIKE 'ZERORISK#%')) prov
     WHERE prov."Backward Index Int" = 0
 ),
 
 vulnerabilities_ZR AS (
-SELECT 
-    COALESCE(v."Uuid Str", s."Uuid") AS "Uuid Str", 
+SELECT
+    COALESCE(v."Uuid Str", s."Uuid") AS "Uuid Str",
     COALESCE(s."Status Str", 'NOT REQUESTED') AS "Status Str"
 FROM v FULL OUTER JOIN s
 ON v."Uuid Str" = s."Uuid"
@@ -130,9 +136,9 @@ WHERE organizations.sk_str ILIKE 'info%'
 
 findings_last_state AS (
 SELECT f.id_str, f.title_str, f.group_name_str
-FROM findings f 
+FROM findings f
     INNER JOIN (SELECT * FROM findings_historic_state WHERE backward_index_int = 0 AND status_str = 'APPROVED') h
-    on f.id_str = h.id_str    
+    on f.id_str = h.id_str
 ),
 
 vuln_find AS (
@@ -149,8 +155,8 @@ ON vuln."Uuid Str" = zr."Uuid Str"
 ),
 
 definitive AS (
-SELECT vulns.*, g.company 
-FROM vulns_enriched vulns 
+SELECT vulns.*, g.company
+FROM vulns_enriched vulns
 INNER JOIN (SELECT * FROM group_company WHERE "company" NOT IN ('imamura', 'okada', 'orgtest')) g
 ON vulns.group_name_str = g.group_name
 )
