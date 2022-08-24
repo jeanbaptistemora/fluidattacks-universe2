96ec2ea18e8e5b366e193c07901edfb3a32f70c6
Karen Camargo <kcamargo@fluidattacks.com>
2022-08-24 12:51:55 +0000

    airs\refac(front): #7060 decrease tech debt
    - Declare explicitly variables
    - Solved 4 smells in 0.33 hrs (12.12 smells/hr)
    - Remaining: 129 smells, 10.64 hrs

--- airs/front/static/zohoLiveChat.js
+++ airs/front/static/zohoLiveChat.js
@@ -1,4 +1,4 @@
-var $zoho = $zoho || {};
+let $zoho = $zoho || {};
 $zoho.salesiq = $zoho.salesiq || {
   widgetcode:
     "50094df2b302da078befa3cee2e8de00943ab0089a71d1aa34f6df9b6cb54ae5feb79731b60e202192c2895c9acefb61",
@@ -7,11 +7,11 @@ $zoho.salesiq = $zoho.salesiq || {
     // Intentionally left blank
   },
 };
-var d = document;
-var s = d.createElement("script");
+let d = document;
+let s = d.createElement("script");
 s.type = "text/javascript";
 s.id = "zsiqscript";
 s.defer = true;
 s.src = "https://salesiq.zoho.com/widget";
-var t = d.getElementsByTagName("script")[0];
+let t = d.getElementsByTagName("script")[0];
 t.parentNode.insertBefore(s, t);
