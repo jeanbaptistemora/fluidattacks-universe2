f1cc671b39e65db556c8765a350d512da0ce96ec
Flor Calderon <fcalderon@fluidattacks.com>
2022-08-26 12:32:18 -0500

    skims\refac(back): #7020.58 modified java reader
    - Modified argument list syntax reader to notate missing nodes

--- skims/skims/syntax_graph/syntax_readers/java/argument_list.py
+++ skims/skims/syntax_graph/syntax_readers/java/argument_list.py
@@ -8,11 +8,18 @@ from syntax_graph.types import (
     SyntaxGraphArgs,
 )
 from utils.graph import (
+    adj_ast,
     match_ast_group_d,
 )
 
 
 def reader(args: SyntaxGraphArgs) -> NId:
     graph = args.ast_graph
-    c_ids = match_ast_group_d(graph, args.n_id, "identifier")
-    return build_argument_list_node(args, iter(c_ids))
+    _, *c_add, _ = adj_ast(graph, args.n_id)  # do not consider ( )
+    c_ids = set(match_ast_group_d(graph, args.n_id, "identifier"))
+    c_ids.update(filter(None, c_add))
+
+    return build_argument_list_node(
+        args,
+        c_ids=(_id for _id in c_ids if graph.nodes[_id]["label_type"] != ","),
+    )
