d493aaab5cc613857612fb426c1dcd83b2ea08de
Daniel Murcia <dmurcia@fluidattacks.com>
2022-08-26 17:57:28 -0500

    observes\feat(job): #7153.26 mandrill etl
    - add mandrill etl job
    - add api key secret

---
+++ observes/etl/mandrill/entrypoint.sh
@@ -0,0 +1,27 @@
+# shellcheck shell=bash
+
+alias target-redshift="observes-target-redshift"
+
+function start_etl {
+  local db_creds
+  local mailchimp_creds
+
+  db_creds=$(mktemp) \
+    && mailchimp_creds=$(mktemp) \
+    && export_notifier_key \
+    && sops_export_vars 'observes/secrets/prod.yaml' \
+      mandrill_api_key \
+    && json_db_creds "${db_creds}" \
+    && echo '[INFO] Running tap' \
+    && tap-mandrill stream "activity" \
+      --api-key "${mandrill_api_key}" \
+      > .singer \
+    && echo '[INFO] Running target' \
+    && target-redshift \
+      --auth "${db_creds}" \
+      --drop-schema \
+      --schema-name 'mandrill' \
+      < .singer
+}
+
+start_etl
---
+++ observes/etl/mandrill/main.nix
@@ -0,0 +1,21 @@
+{
+  inputs,
+  makeScript,
+  outputs,
+  ...
+}:
+makeScript {
+  searchPaths = {
+    bin = [
+      outputs."${inputs.observesIndex.target.redshift.bin}"
+      outputs."${inputs.observesIndex.tap.mandrill.bin}"
+    ];
+    source = [
+      outputs."${inputs.observesIndex.service.job_last_success.bin}"
+      outputs."/common/utils/sops"
+      outputs."/observes/common/db-creds"
+    ];
+  };
+  name = "observes-etl-mandrill";
+  entrypoint = ./entrypoint.sh;
+}
