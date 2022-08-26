9877d887c7f67fdd99f881396ef807eb4b5b21e1
David Acevedo <dacevedo@fluidattacks.com>
2022-08-26 08:08:33 -0500

    integrates\fix(back): #7255 hooks poc
    - show 1 link per finding to reduce spam

--- integrates/streams/src/dynamodb/processor.py
+++ integrates/streams/src/dynamodb/processor.py
@@ -76,6 +76,7 @@ def _replicate_on_opensearch(records: tuple[Record, ...]) -> None:
 
 
 def _trigger_webhooks(records: tuple[Record, ...]) -> None:
+    """Notifies external integrations"""
     items_to_notify = tuple(
         record.item
         for record in records
@@ -89,10 +90,17 @@ def _trigger_webhooks(records: tuple[Record, ...]) -> None:
     )
 
     for group_name, items in items_by_group:
+        items_by_finding = itertools.groupby(
+            sorted(items, key=lambda item: item["sk"].split("#")[1]),
+            key=lambda item: item["sk"].split("#")[1],
+        )
         text = "\n".join(
             [
                 f"🏹 New vulnerabilities reported on group {group_name}:",
-                *[f"- {item['where']} | {item['specific']}" for item in items],
+                *[
+                    f"- https://app.fluidattacks.com/groups/{group_name}/vulns/{finding_id}/locations"
+                    for finding_id, _ in items_by_finding
+                ],
             ]
         )
         requests.post(FI_GOOGLE_CHAT_WEBOOK_URL, json={"text": text})
