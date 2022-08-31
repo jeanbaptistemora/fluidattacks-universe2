c161c3676f608bda458bea71ba10be6f66ea6b51
Daniel Salazar <dsalazar@fluidattacks.com>
2022-08-31 14:08:56 -0500

    common\feat(infra): #7214 two rate limits
    - Add rate limit for /orgs
    - Add rate limit for /api

--- common/dns/infra/rate_limit.tf
+++ common/dns/infra/rate_limit.tf
@@ -8,9 +8,45 @@ resource "cloudflare_rate_limit" "integrates_production" {
 
   match {
     request {
-      url_pattern = "app.${cloudflare_zone.fluidattacks_com.zone}/*"
+      url_pattern = "app.${cloudflare_zone.fluidattacks_com.zone}/orgs/*"
       schemes     = ["_ALL_"]
       methods     = ["_ALL_"]
+    }
+    response {
+      origin_traffic = true
+
+      headers = [
+        {
+          "name"  = "Cf-Cache-Status"
+          "op"    = "ne"
+          "value" = "HIT"
+        },
+      ]
+    }
+  }
+
+  action {
+    mode = "js_challenge"
+  }
+
+  correlate {
+    by = "nat"
+  }
+}
+
+resource "cloudflare_rate_limit" "integrates_production_api" {
+  zone_id             = cloudflare_zone.fluidattacks_com.id
+  threshold           = 200
+  period              = 60
+  disabled            = false
+  description         = "Integrates production API"
+  bypass_url_patterns = []
+
+  match {
+    request {
+      url_pattern = "app.${cloudflare_zone.fluidattacks_com.zone}/api"
+      schemes     = ["_ALL_"]
+      methods     = ["POST"]
     }
     response {
       origin_traffic = true
