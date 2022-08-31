289101759c9fc8ccaa8ffc50ab314924544f7566
Daniel Salazar <dsalazar@fluidattacks.com>
2022-08-31 15:10:32 -0500

    common\refac(infra): #7214 only api rate limit
    - Only use api rate limit

--- common/dns/infra/rate_limit.tf
+++ common/dns/infra/rate_limit.tf
@@ -1,39 +1,3 @@
-resource "cloudflare_rate_limit" "integrates_production" {
-  zone_id             = cloudflare_zone.fluidattacks_com.id
-  threshold           = 400
-  period              = 60
-  disabled            = false
-  description         = "Integrates production"
-  bypass_url_patterns = []
-
-  match {
-    request {
-      url_pattern = "app.${cloudflare_zone.fluidattacks_com.zone}/orgs/*"
-      schemes     = ["_ALL_"]
-      methods     = ["_ALL_"]
-    }
-    response {
-      origin_traffic = true
-
-      headers = [
-        {
-          "name"  = "Cf-Cache-Status"
-          "op"    = "ne"
-          "value" = "HIT"
-        },
-      ]
-    }
-  }
-
-  action {
-    mode = "js_challenge"
-  }
-
-  correlate {
-    by = "nat"
-  }
-}
-
 resource "cloudflare_rate_limit" "integrates_production_api" {
   zone_id             = cloudflare_zone.fluidattacks_com.id
   threshold           = 200
