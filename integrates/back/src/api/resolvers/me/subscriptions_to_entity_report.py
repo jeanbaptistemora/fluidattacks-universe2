d0583268d663a4bb2157ff778e73e9791ce95926
Jhon Romero <jromero@fluidattacks.com>
2022-08-31 13:49:28 +0000

    integrates\feat(back): #6113.62 subscriptions resolver
    - switch subscriptions resolver to the new type

--- integrates/back/src/api/resolvers/me/subscriptions_to_entity_report.py
+++ integrates/back/src/api/resolvers/me/subscriptions_to_entity_report.py
@@ -1,11 +1,8 @@
+from db_model.subscriptions.types import (
+    Subscription,
+)
 from graphql.type.definition import (
     GraphQLResolveInfo,
-)
-from newutils import (
-    subscriptions as subscriptions_utils,
-)
-from subscriptions import (
-    domain as subscriptions_domain,
 )
 from typing import (
     Any,
@@ -13,27 +10,25 @@ from typing import (
 
 
 async def _format_subscriptions(
-    subscriptions: list[dict[str, Any]],
+    subscriptions: list[Subscription],
 ) -> list[dict[str, str]]:
     return [
         {
-            "entity": subscription["sk"]["entity"],
-            "frequency": subscriptions_utils.period_to_frequency(
-                period=subscription["period"]
-            ),
-            "subject": subscription["sk"]["subject"],
+            "entity": subscription.entity,
+            "frequency": subscription.frequency,
+            "subject": subscription.subject,
         }
         for subscription in subscriptions
-        if subscription["sk"]["meta"] == "entity_report"
     ]
 
 
 async def resolve(
     parent: dict[str, Any],
-    _info: GraphQLResolveInfo,
+    info: GraphQLResolveInfo,
     **_kwargs: None,
 ) -> list[dict[str, str]]:
     email = str(parent["user_email"])
-    subscriptions = await subscriptions_domain.get_user_subscriptions(email)
+    loaders = info.context.loaders
+    subscriptions = await loaders.stakeholder_subscriptions.load(email)
 
     return await _format_subscriptions(subscriptions)
