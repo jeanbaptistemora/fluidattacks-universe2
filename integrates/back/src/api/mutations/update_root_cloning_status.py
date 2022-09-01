9d1f5f71a96bb40e9c34081b8af9838c2726fc9a
Andres Cuberos <acuberos@fluidattacks.com>
2022-09-01 18:57:21 +0000

    integrates\fix(back): #7330.1 queue machine root update api
    - When updating the cloning status over the API,
      check if the commit is different from the last one saved
      and queue a Machine execution if so.
    - Observes schedules use Melts to keep the mirrors updated
      and use the API to notify these changes,
      but Machine was not queued if there were new changes.

--- integrates/back/src/api/mutations/update_root_cloning_status.py
+++ integrates/back/src/api/mutations/update_root_cloning_status.py
@@ -4,8 +4,15 @@ from ariadne.utils import (
 from custom_types import (
     SimplePayload,
 )
+from dataloaders import (
+    Dataloaders,
+)
 from db_model.enums import (
     GitCloningStatus,
+)
+from db_model.roots.types import (
+    GitRoot,
+    Root,
 )
 from decorators import (
     concurrent_decorators,
@@ -15,11 +22,17 @@ from decorators import (
 from graphql.type.definition import (
     GraphQLResolveInfo,
 )
+from machine.jobs import (
+    FINDINGS,
+    queue_job_new,
+)
 from roots import (
     domain as roots_domain,
 )
 from typing import (
     Any,
+    Optional,
+    Tuple,
 )
 
 
@@ -33,14 +46,31 @@ async def mutate(
     info: GraphQLResolveInfo,
     **kwargs: Any,
 ) -> SimplePayload:
+    loaders: Dataloaders = info.context.loaders
+    group_name: str = kwargs["group_name"]
+    root_id: str = kwargs["id"]
+    commit: Optional[str] = kwargs.get("commit")
+
+    root: Tuple[Root] = loaders.root.load((group_name, root_id))
+    if (
+        commit is not None
+        and isinstance(root, GitRoot)
+        and commit != root.cloning.commit
+    ):
+        await queue_job_new(
+            group_name=group_name,
+            dataloaders=loaders,
+            finding_codes=tuple(FINDINGS.keys()),
+            roots=[root.state.nickname],
+        )
 
     await roots_domain.update_root_cloning_status(
-        loaders=info.context.loaders,
-        group_name=kwargs["group_name"],
-        root_id=kwargs["id"],
+        loaders=loaders,
+        group_name=group_name,
+        root_id=root_id,
         status=GitCloningStatus(kwargs["status"]),
         message=kwargs["message"],
-        commit=kwargs.get("commit"),
+        commit=commit,
     )
 
     return SimplePayload(success=True)
