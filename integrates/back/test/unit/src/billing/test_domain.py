0076db486008e182c95d41144f767bbc3217318e
Carlos Betancur <cbetancur@fluidattacks.com>
2022-09-05 14:44:01 -0500

    integrates\test(back): #7323.4 add unit test
    - add billing unit test
    - add file used in test

---
+++ integrates/back/test/unit/src/billing/test_domain.py
@@ -0,0 +1,28 @@
+from billing.domain import (
+    save_file,
+)
+from context import (
+    FI_AWS_S3_RESOURCES_BUCKET,
+)
+import os
+import pytest
+from s3.operations import (
+    list_files,
+)
+from starlette.datastructures import (
+    UploadFile,
+)
+
+pytestmark = [
+    pytest.mark.asyncio,
+]
+
+
+async def test_save_file() -> None:
+    file_name = "billing-test-file.png"
+    file_location = os.path.dirname(os.path.abspath(__file__))
+    file_location = os.path.join(file_location, "mock/" + file_name)
+    with open(file_location, "rb") as data:
+        test_file = UploadFile(data)
+        await save_file(test_file, file_name)
+    assert file_name in await list_files(FI_AWS_S3_RESOURCES_BUCKET, file_name)
