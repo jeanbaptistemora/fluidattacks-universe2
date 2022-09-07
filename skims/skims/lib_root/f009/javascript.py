8e6e46437d992472ce2d54a4b9e32b8b521dafdb
Fabio Lagos <flagos@fluidattacks.com>
2022-09-07 10:43:28 -0500

    skims\refac(back): #7020.84 f009 refac
    - refac f009 to use syntax graph
    - Speed: 2 findings/day
    - ToDo: 6 findings
    - ETA: 3 days

--- skims/skims/lib_root/f009/javascript.py
+++ skims/skims/lib_root/f009/javascript.py
@@ -11,9 +11,6 @@ from sast.query import (
 from utils import (
     graph as g,
 )
-from utils.graph.text_nodes import (
-    node_to_str,
-)
 from utils.string import (
     complete_attrs_on_set,
 )
@@ -23,46 +20,44 @@ def crypto_js_credentials(
     shard_db: ShardDb,  # pylint: disable=unused-argument
     graph_db: graph_model.GraphDB,
 ) -> core_model.Vulnerabilities:
+    danger_methods = complete_attrs_on_set(
+        {
+            "CryptoJS.enc.Base64.parse",
+            "CryptoJS.enc.Utf16.parse",
+            "CryptoJS.enc.Utf16LE.parse",
+            "CryptoJS.enc.Hex.parse",
+            "CryptoJS.enc.Latin1.parse",
+            "CryptoJS.enc.Utf8.parse",
+        }
+    )
+
     def n_ids() -> graph_model.GraphShardNodes:
-        danger_methods = complete_attrs_on_set(
-            {
-                "CryptoJS.enc.Base64.parse",
-                "CryptoJS.enc.Utf16.parse",
-                "CryptoJS.enc.Utf16LE.parse",
-                "CryptoJS.enc.Hex.parse",
-                "CryptoJS.enc.Latin1.parse",
-                "CryptoJS.enc.Utf8.parse",
-            }
-        )
         for shard in graph_db.shards_by_language(
             graph_model.GraphShardMetadataLanguage.JAVASCRIPT,
         ):
-            graph = shard.graph
-            for call_expression in g.filter_nodes(
+            if shard.syntax_graph is None:
+                continue
+            graph = shard.syntax_graph
+            for n_id in g.filter_nodes(
                 graph,
                 nodes=graph.nodes,
-                predicate=g.pred_has_labels(label_type="call_expression"),
+                predicate=g.pred_has_labels(label_type="CallExpression"),
             ):
-                match = g.match_ast(
-                    graph,
-                    call_expression,
-                    "member_expression",
-                    "arguments",
-                )
-                if member_expression_id := match["member_expression"]:
-                    method_name = node_to_str(graph, member_expression_id)
-                    if method_name in danger_methods:
-                        match_arguments = g.match_ast_group(
-                            graph,
-                            str(match["arguments"]),
-                            "string",
-                            "template_string",
+                if (
+                    graph.nodes[n_id]["function_name"] in danger_methods
+                    and (
+                        al_id := g.match_ast(graph, n_id, "ArgumentList").get(
+                            "ArgumentList"
                         )
-                        if (
-                            match_arguments["string"]
-                            or match_arguments["template_string"]
-                        ):
-                            yield shard, call_expression
+                    )
+                    and (
+                        child := g.match_ast(graph, al_id, "Literal").get(
+                            "Literal"
+                        )
+                    )
+                    and graph.nodes[child]["value_type"] == "string"
+                ):
+                    yield shard, n_id
 
     return get_vulnerabilities_from_n_ids(
         desc_key="src.lib_path.f009.crypto_js_credentials.description",
