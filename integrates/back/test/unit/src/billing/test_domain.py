8a9eaee26843eb44591ce2cc894a8b2131314a5d
Carlos Betancur <cbetancur@fluidattacks.com>
2022-09-07 09:52:11 -0500

    integrates\test(back): #7323.10 add unit tests
    - add billing domain unit tests

--- integrates/back/test/unit/src/billing/test_domain.py
+++ integrates/back/test/unit/src/billing/test_domain.py
@@ -3,7 +3,9 @@
 # SPDX-License-Identifier: MPL-2.0
 
 from billing.domain import (
+    remove_file,
     save_file,
+    search_file,
 )
 from context import (
     FI_AWS_S3_RESOURCES_BUCKET,
@@ -30,3 +32,22 @@ async def test_save_file() -> None:
         test_file = UploadFile(data)
         await save_file(test_file, file_name)
     assert file_name in await list_files(FI_AWS_S3_RESOURCES_BUCKET, file_name)
+
+
+async def test_search_file() -> None:
+    file_name = "unittesting-test-file.csv"
+    file_location = os.path.dirname(os.path.abspath(__file__))
+    file_location = os.path.join(file_location, "mock/" + file_name)
+    with open(file_location, "rb") as data:
+        test_file = UploadFile(data)
+        await save_file(test_file, file_name)
+    assert file_name in await list_files(FI_AWS_S3_RESOURCES_BUCKET, file_name)
+    assert file_name in await search_file(file_name)
+
+
+async def test_remove_file() -> None:
+    file_name = "unittesting-test-file.csv"
+    await remove_file(file_name)
+    assert file_name not in await list_files(
+        FI_AWS_S3_RESOURCES_BUCKET, file_name
+    )
