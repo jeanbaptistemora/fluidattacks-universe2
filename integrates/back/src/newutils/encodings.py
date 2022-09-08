2ecdd463d8ae5d58d6c177f77a69ac97225514a7
Jonathan Mesa <jmesa@fluidattacks.com>
2022-09-08 16:06:15 +0000

    integrates\refac(back): #6113.81 cleanup subs
    - rm unneeded code and mock data
    - no-deltas-test

--- integrates/back/src/newutils/encodings.py
+++ integrates/back/src/newutils/encodings.py
@@ -12,7 +12,6 @@ from newutils import (
 from typing import (
     Any,
     cast,
-    Dict,
 )
 
 
@@ -26,44 +25,7 @@ def safe_decode(hexstr: str) -> str:
     return bytes.fromhex(hexstr).decode("utf-8")
 
 
-def mapping_to_key(mapping: Dict[str, str]) -> str:
-    """Turn a mapping of str to str into a str suitable for use in DynamoDB."""
-    if not mapping:
-        raise ValueError("Empty parameters")
-
-    if not all(
-        isinstance(obj, str)
-        for arguments in mapping.items()
-        for obj in arguments
-    ):
-        raise TypeError(f"Expected Dict[str, str], got: {type(mapping)}")
-
-    return "/".join(
-        [
-            ":".join(
-                [
-                    attribute_name,
-                    safe_encode(attribute_value),
-                ]
-            )
-            for attribute_name, attribute_value in mapping.items()
-        ]
-    )
-
-
-def key_to_mapping(key: str) -> Dict[str, str]:
-    """Inverse of dict_to_key."""
-    if not isinstance(key, str):
-        raise TypeError(f"Expected str, got: {type(key)}")
-
-    return {
-        attribute_name: safe_decode(attribute_value)
-        for attribute in key.split("/")
-        for attribute_name, attribute_value in [attribute.split(":")]
-    }
-
-
-def jwt_payload_encode(payload: Dict[str, Any]) -> str:
+def jwt_payload_encode(payload: dict[str, Any]) -> str:
     def hook(obj: object) -> str:
         # special cases where json encoder does not handle the object type
         # or a special format is needed
@@ -78,8 +40,8 @@ def jwt_payload_encode(payload: Dict[str, Any]) -> str:
     return encoder.encode(payload)
 
 
-def jwt_payload_decode(payload: str) -> Dict[str, Any]:
-    def hook(jwt_payload: Dict[str, Any]) -> Dict[str, Any]:
+def jwt_payload_decode(payload: str) -> dict[str, Any]:
+    def hook(jwt_payload: dict[str, Any]) -> dict[str, Any]:
         if "exp" in jwt_payload:
             exp = jwt_payload["exp"]
             if isinstance(exp, int):
@@ -90,4 +52,4 @@ def jwt_payload_decode(payload: str) -> Dict[str, Any]:
         return jwt_payload
 
     decoder = json.JSONDecoder(object_hook=hook)
-    return cast(Dict[str, Any], decoder.decode(payload))
+    return cast(dict[str, Any], decoder.decode(payload))
