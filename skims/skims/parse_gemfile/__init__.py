9aba864e72d5c375bf59adfd8dd3c29a6c341333
Alejandro Trujillo <atrujillo@fluidattacks.com>
2022-09-09 14:20:41 -0500

    skims\fix(back): #6807.59 gemfile parser
    - method to parse lines from gemfiles
    - added dependence
    - no-deltas-test

---
+++ skims/skims/parse_gemfile/__init__.py
@@ -0,0 +1,41 @@
+# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from gemfileparser import (
+    Dependency,
+    GemfileParser,
+)
+from typing import (
+    Any,
+    Dict,
+)
+
+
+def parse_line(in_line: str) -> Dict[str, Any]:
+    line = in_line.split(",")
+    column_list = []
+    for column in line:
+        stripped_column = (
+            column.replace("'", "")
+            .replace('"', "")
+            .replace("%q<", "")
+            .replace("(", "")
+            .replace(")", "")
+            .replace("[", "")
+            .replace("]", "")
+            .strip()
+        )
+        column_list.append(stripped_column)
+
+    dep = Dependency()
+    for column in column_list:
+        for criteria, criteria_regex in GemfileParser.gemfile_regexes.items():
+            match = criteria_regex.match(column)
+            if match:
+                if criteria == "requirement":
+                    dep.requirement.append(match.group(criteria))
+                else:
+                    setattr(dep, criteria, match.group(criteria))
+                break
+    return dep.to_dict()
