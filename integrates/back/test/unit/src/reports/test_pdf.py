98b2db2cd674a30ad1a8362a6633fc6e2868540c
Carlos Betancur <cbetancur@fluidattacks.com>
2022-09-12 09:22:30 -0500

    integrates\test(back): #7323.14 add unit test
    - add reports unit test

---
+++ integrates/back/test/unit/src/reports/test_pdf.py
@@ -0,0 +1,35 @@
+# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from decimal import (
+    Decimal,
+)
+import pytest
+from reports.pdf import (
+    get_severity,
+)
+
+
+@pytest.mark.parametrize(
+    ["metric", "metric_value", "expected_result"],
+    [
+        ["access_vector", "0.395", "Local"],
+        ["attack_vector", "0.62", "Red adyacente"],
+        ["confidentiality_impact", "0.66", "Completo"],
+        ["integrity_impact", "0.0", "Ninguno"],
+        ["availability_impact", "0.275", "Parcial"],
+        ["authentication", "0.704", "Ninguna"],
+        ["exploitability", "1.0", "Alta"],
+        ["confidence_level", "0.9", "No confirmado"],
+        ["resolution_level", "0.9", "Temporal"],
+        ["access_complexity", "0.71", "Bajo"],
+    ],
+)
+def test_get_severity(
+    metric: str,
+    metric_value: str,
+    expected_result: str,
+) -> None:
+    result = get_severity(metric, Decimal(metric_value))
+    assert result == expected_result
