c483b26e777baf2e97f3f6c627aaf3de15225cef
Carlos Betancur <cbetancur@fluidattacks.com>
2022-09-13 15:32:56 -0500

    integrates\test(back): #7323.16 add sms unit test
    - add sms service unit test

--- integrates/back/test/unit/src/sms/test_common.py
+++ integrates/back/test/unit/src/sms/test_common.py
@@ -6,6 +6,9 @@ import pytest
 from sms.common import (
     send_sms_notification,
 )
+from unittest import (
+    mock,
+)
 
 # Constants
 pytestmark = [
@@ -21,3 +24,14 @@ async def test_send_sms_notification() -> None:
         message_body=test_message,
     )
     assert test_result is None
+
+    expected_sid = "SM87105da94bff44b999e4e6eb90d8eb6a"
+    with mock.patch("sms.common.FI_ENVIRONMENT", "production"):
+        with mock.patch("sms.common.client.messages.create") as mock_twilio:
+            mock_twilio.return_value = expected_sid
+            mock_result = await send_sms_notification(
+                phone_number=test_phone_number,
+                message_body=test_message,
+            )
+        assert mock_twilio.called is True
+        assert mock_result is None
