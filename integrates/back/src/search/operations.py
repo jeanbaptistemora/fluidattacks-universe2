7a98203fae6999f5413c6f92df06b020fc285762
Jhon Romero <jromero@fluidattacks.com>
2022-09-13 17:40:14 +0000

    integrates\feat(back): #6984.5 opensearch operations
    - new fields created for improve the search opetarions

--- integrates/back/src/search/operations.py
+++ integrates/back/src/search/operations.py
@@ -21,11 +21,13 @@ SCROLL_TIME = "10m"
 
 async def search(
     *,
-    exact_filters: dict[str, Any],
+    exact_filters: Optional[dict[str, Any]] = None,
     index: str,
     limit: int,
     after: Optional[str] = None,
     query: Optional[str] = None,
+    should_filters: Optional[list[dict[str, Any]]] = None,
+    must_filters: Optional[list[dict[str, Any]]] = None,
 ) -> SearchResponse:
     """
     Searches for items matching both the user input (full-text)
@@ -35,16 +37,39 @@ async def search(
     https://opensearch-project.github.io/opensearch-py/api-ref/client.html#opensearchpy.OpenSearch.search
     """
     client = await get_client()
+
+    full_and_filters = []
+    full_or_filters = []
+
+    if must_filters:
+        full_and_filters = [
+            {"match": {key: {"query": value, "operator": "and"}}}
+            for attrs in must_filters
+            for key, value in attrs.items()
+        ]
+
+    if should_filters:
+        full_or_filters = [
+            {"match": {key: value}}
+            for attrs in should_filters
+            for key, value in attrs.items()
+        ]
     full_text_queries = [{"multi_match": {"query": query}}] if query else []
-    term_queries = [
-        {"term": {key: value}} for key, value in exact_filters.items()
-    ]
+    term_queries = (
+        [{"term": {key: value}} for key, value in exact_filters.items()]
+        if exact_filters
+        else {}
+    )
     body = {
         "query": {
             "bool": {
                 "must": [
+                    *full_and_filters,
                     *full_text_queries,
                     *term_queries,
+                ],
+                "should": [
+                    *full_or_filters,
                 ],
             }
         }
