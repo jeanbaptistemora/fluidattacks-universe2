1bc74583395b27043f7795a7ece3d6c5b0d8f38a
Sebastian Lizcano <slizcano@fluidattacks.com>
2022-09-14 09:58:16 -0500

    integrates\test(back): #7388.2 update functional test
    - update solve_event functional test to verify last_reattack_requester
    keeps reattacker instead of replacing after closing a event
    with on_hold status
    - no-deltas-test

--- integrates/back/test/functional/src/solve_event/conftest.py
+++ integrates/back/test/functional/src/solve_event/conftest.py
@@ -6,6 +6,9 @@
 from back.test import (
     db,
 )
+from db_model.enums import (
+    Source,
+)
 from db_model.events.enums import (
     EventStateStatus,
     EventType,
@@ -15,6 +18,34 @@ from db_model.events.types import (
     EventEvidence,
     EventEvidences,
     EventState,
+)
+from db_model.findings.enums import (
+    FindingStateStatus,
+    FindingVerificationStatus,
+)
+from db_model.findings.types import (
+    Finding,
+    Finding31Severity,
+    FindingEvidences,
+    FindingState,
+    FindingUnreliableIndicatorsToUpdate,
+    FindingVerification,
+)
+from db_model.vulnerabilities.enums import (
+    VulnerabilityStateStatus,
+    VulnerabilityTreatmentStatus,
+    VulnerabilityType,
+    VulnerabilityVerificationStatus,
+)
+from db_model.vulnerabilities.types import (
+    Vulnerability,
+    VulnerabilityState,
+    VulnerabilityTreatment,
+    VulnerabilityUnreliableIndicators,
+    VulnerabilityVerification,
+)
+from decimal import (
+    Decimal,
 )
 import pytest
 from typing import (
@@ -232,6 +263,141 @@ async def populate(generic_data: dict[str, Any]) -> bool:
                         status=EventStateStatus.CREATED,
                     ),
                 ],
+            },
+        ],
+        "findings": [
+            {
+                "finding": Finding(
+                    id="3c475384-834c-47b0-ac71-a41a022e401c",
+                    group_name="unittesting",
+                    state=FindingState(
+                        modified_by="test1@gmail.com",
+                        modified_date="2017-04-08T00:45:11+00:00",
+                        source=Source.ASM,
+                        status=FindingStateStatus.CREATED,
+                    ),
+                    title="001. SQL injection - C Sharp SQL API",
+                    recommendation="Updated recommendation",
+                    description="I just have updated the description",
+                    hacker_email="test1@gmail.com",
+                    severity=Finding31Severity(
+                        attack_complexity=Decimal("0.44"),
+                        attack_vector=Decimal("0.2"),
+                        availability_impact=Decimal("0.22"),
+                        availability_requirement=Decimal("1.5"),
+                        confidentiality_impact=Decimal("0.22"),
+                        confidentiality_requirement=Decimal("0.5"),
+                        exploitability=Decimal("0.94"),
+                        integrity_impact=Decimal("0.22"),
+                        integrity_requirement=Decimal("1"),
+                        modified_availability_impact=Decimal("0.22"),
+                        modified_user_interaction=Decimal("0.62"),
+                        modified_integrity_impact=Decimal("0"),
+                        modified_attack_complexity=Decimal("0.44"),
+                        modified_severity_scope=Decimal("0"),
+                        modified_privileges_required=Decimal("0.27"),
+                        modified_attack_vector=Decimal("0.85"),
+                        modified_confidentiality_impact=Decimal("0.22"),
+                        privileges_required=Decimal("0.62"),
+                        severity_scope=Decimal("1.2"),
+                        remediation_level=Decimal("0.95"),
+                        report_confidence=Decimal("1"),
+                        user_interaction=Decimal("0.85"),
+                    ),
+                    requirements="REQ.0132. Passwords (phrase type) "
+                    "must be at least 3 words long.",
+                    threat="Updated threat",
+                    attack_vector_description=(
+                        "This is an updated attack vector"
+                    ),
+                    evidences=FindingEvidences(),
+                ),
+                "historic_state": [
+                    FindingState(
+                        modified_by="test1@gmail.com",
+                        modified_date="2017-04-08T00:45:12+00:00",
+                        source=Source.ASM,
+                        status=FindingStateStatus.SUBMITTED,
+                    ),
+                    FindingState(
+                        modified_by="test1@gmail.com",
+                        modified_date="2017-04-08T00:45:13+00:00",
+                        source=Source.ASM,
+                        status=FindingStateStatus.REJECTED,
+                    ),
+                    FindingState(
+                        modified_by="test1@gmail.com",
+                        modified_date="2017-04-08T00:45:14+00:00",
+                        source=Source.ASM,
+                        status=FindingStateStatus.SUBMITTED,
+                    ),
+                    FindingState(
+                        modified_by="test1@gmail.com",
+                        modified_date="2018-04-08T00:45:11+00:00",
+                        source=Source.ASM,
+                        status=FindingStateStatus.APPROVED,
+                    ),
+                ],
+                "historic_verification": [
+                    FindingVerification(
+                        comment_id="42343434",
+                        modified_by="test2@gmail.com",
+                        modified_date="2022-02-01T00:45:12+00:00",
+                        status=FindingVerificationStatus.REQUESTED,
+                        vulnerability_ids={
+                            "4dbc03e0-4cfc-4b33-9b70-bb7566c460bd",
+                        },
+                    ),
+                    FindingVerification(
+                        comment_id="42343435",
+                        modified_by="customer_manager@fluidattacks.com",
+                        modified_date="2022-02-04T00:45:15+00:00",
+                        status=FindingVerificationStatus.ON_HOLD,
+                        vulnerability_ids={
+                            "4dbc03e0-4cfc-4b33-9b70-bb7566c460bd",
+                        },
+                    ),
+                ],
+                "unreliable_indicator": FindingUnreliableIndicatorsToUpdate(
+                    unreliable_closed_vulnerabilities=0,
+                    unreliable_open_vulnerabilities=1,
+                ),
+            },
+        ],
+        "vulnerabilities": [
+            {
+                "vulnerability": Vulnerability(
+                    finding_id="3c475384-834c-47b0-ac71-a41a022e401c",
+                    group_name="unittesting",
+                    hacker_email="test1@gmail.com",
+                    id="4dbc03e0-4cfc-4b33-9b70-bb7566c460bd",
+                    specific="5",
+                    state=VulnerabilityState(
+                        modified_by="machine@fluidattacks.com",
+                        modified_date="2022-09-01T00:45:11+00:00",
+                        source=Source.MACHINE,
+                        status=VulnerabilityStateStatus.OPEN,
+                    ),
+                    treatment=VulnerabilityTreatment(
+                        modified_date="2022-09-01T00:45:11+00:00",
+                        status=VulnerabilityTreatmentStatus.NEW,
+                    ),
+                    type=VulnerabilityType.LINES,
+                    unreliable_indicators=VulnerabilityUnreliableIndicators(
+                        unreliable_report_date="2022-09-01T00:45:11+00:00",
+                        unreliable_source=Source.MACHINE,
+                        unreliable_treatment_changes=0,
+                        unreliable_last_reattack_requester="test2@gmail.com",
+                    ),
+                    verification=VulnerabilityVerification(
+                        modified_date="2022-09-01T00:45:11+00:00",
+                        status=VulnerabilityVerificationStatus.ON_HOLD,
+                        event_id="418900975",
+                    ),
+                    where="nickname1/README.md",
+                    root_id="77637717-41d4-4242-854a-db8ff7fe5ed0",
+                    commit="15ab18899a617e5b18c5c0ad1e7ad7352615d5a3",
+                ),
             },
         ],
     }
--- integrates/back/test/functional/src/solve_event/test_resolver.py
+++ integrates/back/test/functional/src/solve_event/test_resolver.py
@@ -15,9 +15,30 @@ from db_model.events.enums import (
 from db_model.events.types import (
     Event,
 )
+from db_model.finding_comments.enums import (
+    CommentType,
+)
+from db_model.finding_comments.types import (
+    FindingComment,
+)
+from db_model.findings.enums import (
+    FindingVerificationStatus,
+)
+from db_model.findings.types import (
+    Finding,
+)
+from db_model.vulnerabilities.enums import (
+    VulnerabilityVerificationStatus,
+)
+from db_model.vulnerabilities.types import (
+    Vulnerability,
+)
 import pytest
 from typing import (
     Any,
+)
+from vulnerabilities.domain.core import (
+    get_reattack_requester,
 )
 
 
@@ -30,7 +51,6 @@ from typing import (
         ["hacker@gmail.com", "418900972"],
         ["reattacker@gmail.com", "418900973"],
         ["resourcer@gmail.com", "418900974"],
-        ["customer_manager@fluidattacks.com", "418900975"],
     ],
 )
 async def test_solve_event(populate: bool, email: str, event_id: str) -> None:
@@ -66,3 +86,66 @@ async def test_solve_event_fail(populate: bool, email: str) -> None:
     result: dict[str, Any] = await get_result(user=email, event=event_id)
     assert "errors" in result
     assert result["errors"][0]["message"] == "Access denied"
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group("solve_event")
+@pytest.mark.parametrize(
+    ["email", "event_id"],
+    [
+        ["customer_manager@fluidattacks.com", "418900975"],
+    ],
+)
+async def test_solve_event_on_hold(
+    populate: bool, email: str, event_id: str
+) -> None:
+    assert populate
+    loaders: Dataloaders = get_new_context()
+    event: Event = await loaders.event.load(event_id)
+    finding: Finding = await loaders.finding.load(
+        "3c475384-834c-47b0-ac71-a41a022e401c"
+    )
+    vulnerability: Vulnerability = await loaders.vulnerability.load(
+        "4dbc03e0-4cfc-4b33-9b70-bb7566c460bd"
+    )
+    assert event.state.status == EventStateStatus.CREATED
+    assert finding.verification.status == FindingVerificationStatus.ON_HOLD
+    assert finding.verification.modified_by == email
+    assert (
+        vulnerability.verification.status
+        == VulnerabilityVerificationStatus.ON_HOLD
+    )
+
+    result: dict[str, Any] = await get_result(user=email, event=event_id)
+    assert "errors" not in result
+    assert "success" in result["data"]["solveEvent"]
+
+    loaders = get_new_context()
+    event = await loaders.event.load(event_id)
+    finding = await loaders.finding.load(
+        "3c475384-834c-47b0-ac71-a41a022e401c"
+    )
+    vulnerability = await loaders.vulnerability.load(
+        "4dbc03e0-4cfc-4b33-9b70-bb7566c460bd"
+    )
+    requester = await get_reattack_requester(loaders, vulnerability)
+    solve_consult: str = "The reattacks are back to"
+    consults: tuple[FindingComment, ...] = await loaders.finding_comments.load(
+        (CommentType.COMMENT, finding.id)
+    )
+    assert event.state.status == EventStateStatus.SOLVED
+    assert event.state.modified_by == email
+    assert any(solve_consult in consult.content for consult in consults)
+    assert finding.verification.status == FindingVerificationStatus.REQUESTED
+    assert finding.verification.modified_by != email
+    assert finding.verification.modified_by == requester
+    assert (
+        vulnerability.unreliable_indicators.unreliable_last_reattack_requester
+        == requester
+    )
+    assert (
+        vulnerability.verification.status
+        == VulnerabilityVerificationStatus.REQUESTED
+    )
+    assert any(email in consult.email for consult in consults)
+    assert not any(requester in consult.email for consult in consults)
