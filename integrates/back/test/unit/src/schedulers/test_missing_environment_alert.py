94f5180d0e3a75a074aaaafdfc1b6ba3b718fee8
Andres Saldarriaga <asaldarriaga@fluidattacks.com>
2022-09-15 21:45:09 +0000

    integrates\test(back): #7323.21 edit unit tests
    - edit unit test to increase coverage

--- integrates/back/test/unit/src/schedulers/test_missing_environment_alert.py
+++ integrates/back/test/unit/src/schedulers/test_missing_environment_alert.py
@@ -5,11 +5,13 @@
 from dataloaders import (
     get_new_context,
 )
+import pytest
 from schedulers.missing_environment_alert import (
     _send_mail_report as send_mail_missing_environment,
 )
 
 
+@pytest.mark.asyncio
 async def test_send_mail_missing_environment() -> None:
     await send_mail_missing_environment(
         loaders=get_new_context(),
