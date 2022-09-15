765aef22c50a2b993b90638e7e2f9b918a145f96
Jonathan Mesa <jmesa@fluidattacks.com>
2022-09-15 19:27:17 +0000

    integrates\feat(build): #7329 redshift related deps
    - add deps for psycopg2 and postgresql

--- integrates/streams/runtime/main.nix
+++ integrates/streams/runtime/main.nix
@@ -1,8 +1,21 @@
 # SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
 #
 # SPDX-License-Identifier: MPL-2.0
-{makePythonPypiEnvironment, ...}:
+{
+  makePythonPypiEnvironment,
+  inputs,
+  ...
+}:
 makePythonPypiEnvironment {
   name = "integrates-streams-runtime";
   sourcesYaml = ./pypi-sources.yaml;
+  searchPathsBuild = {
+    bin = [inputs.nixpkgs.gcc inputs.nixpkgs.postgresql];
+  };
+  searchPathsRuntime = {
+    bin = [
+      inputs.nixpkgs.gcc
+      inputs.nixpkgs.postgresql
+    ];
+  };
 }
