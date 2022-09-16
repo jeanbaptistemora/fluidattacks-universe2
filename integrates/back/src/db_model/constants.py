dd0116086225854ac83e8046c11faf5aca2c1791
Jaime Hurtado <jhurtado@fluidattacks.com>
2022-09-16 00:30:59 +0000

    integrates\refac(back): #7311.27 improve notifications
    - revised updated policies email template

--- integrates/back/src/db_model/constants.py
+++ integrates/back/src/db_model/constants.py
@@ -15,16 +15,17 @@ POLICIES_FORMATTED = {
         "can be temporarily accepted"
     ),
     "max_acceptance_severity": (
-        "Maximum temporal CVSS 3.1 score range "
-        "between which a finding can be accepted"
+        "Maximum temporary CVSS 3.1 score for the range "
+        "within which a finding can be accepted"
     ),
     "min_breaking_severity": (
         "Minimum CVSS 3.1 score of an open "
-        "vulnerability for DevSecOps to break the build in strict mode"
+        "vulnerability for the DevSecOps agent "
+        "to break the build in strict mode"
     ),
     "min_acceptance_severity": (
-        "Minimum temporal CVSS 3.1 score range "
-        "between which a finding can be accepted"
+        "Minimum temporary CVSS 3.1 score for the range "
+        "within which a finding can be accepted"
     ),
     "vulnerability_grace_period": (
         "Grace period in days where newly "
