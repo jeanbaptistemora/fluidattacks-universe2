2477a75dc00d3561587a954ade7591d0b59999df
Kevin Amado <kamado@fluidattacks.com>
2022-09-16 10:11:33 -0600

    skims\feat(build): #7490.2 fail if cvg drops
    - Set a coverage threshold, if the coverage drops
      below this number, the pipeline will fail

--- skims/coverage/entrypoint.sh
+++ skims/coverage/entrypoint.sh
@@ -5,6 +5,7 @@
 # shellcheck shell=bash
 
 function main {
+  local coverage_target=55.00
   local coverage_args=(
     --skip-empty
     -i
@@ -13,13 +14,16 @@ function main {
   : \
     && pushd skims \
     && coverage combine \
-    && coverage report "${coverage_args[@]}" \
+    && coverage report \
+      --fail-under="${coverage_target}" \
+      --precision=2 \
+      "${coverage_args[@]}" \
     && coverage html "${coverage_args[@]}" -d build \
     && coverage xml "${coverage_args[@]}" \
-    && aws_login "dev" "3600" \
-    && sops_export_vars __argSecretsDev__ CODECOV_TOKEN \
     && if test "${CI_COMMIT_REF_NAME}" = trunk; then
-      codecov -B skims -C "${CI_COMMIT_SHA}" -F skims
+      aws_login "dev" "3600" \
+        && sops_export_vars __argSecretsDev__ CODECOV_TOKEN \
+        && codecov -B skims -C "${CI_COMMIT_SHA}" -F skims
     fi \
     && popd \
     || return 1
