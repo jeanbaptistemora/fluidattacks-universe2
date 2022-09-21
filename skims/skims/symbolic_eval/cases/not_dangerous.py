73dd0e5628afbb091bfa3d8f225b72f4eea147d8
Fabio Lagos <flagos@fluidattacks.com>
2022-09-21 08:40:41 -0500

    skims\refac(back): #7020.107 add evaluators
    - Add general evaluator for nodes that do not affect symbol eval
    - Assign general evaluator to Break, Comment, Continue, This and Yield
    - Add ReturnNode, Try and Using Statement evaluators
    - no-deltas-test

---
+++ skims/skims/symbolic_eval/cases/not_dangerous.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from symbolic_eval.types import (
+    SymbolicEvalArgs,
+    SymbolicEvaluation,
+)
+
+
+def evaluate(args: SymbolicEvalArgs) -> SymbolicEvaluation:
+    args.evaluation[args.n_id] = False
+    return SymbolicEvaluation(args.evaluation[args.n_id], args.triggers)
---
+++ skims/skims/symbolic_eval/cases/using_statement.py
@@ -0,0 +1,32 @@
+# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from model.core_model import (
+    FindingEnum,
+)
+from symbolic_eval.types import (
+    Evaluator,
+    SymbolicEvalArgs,
+    SymbolicEvaluation,
+)
+from typing import (
+    Dict,
+)
+
+FINDING_EVALUATORS: Dict[FindingEnum, Evaluator] = {}
+
+
+def evaluate(args: SymbolicEvalArgs) -> SymbolicEvaluation:
+    block_id = args.graph.nodes[args.n_id]["block_id"]
+    block_danger = args.generic(args.fork_n_id(block_id)).danger
+
+    declaration_danger = False
+    if decl_id := args.graph.nodes[args.n_id].get("declaration_id"):
+        declaration_danger = args.generic(args.fork_n_id(decl_id)).danger
+
+    args.evaluation[args.n_id] = block_danger or declaration_danger
+    if finding_evaluator := FINDING_EVALUATORS.get(args.method.value.finding):
+        args.evaluation[args.n_id] = finding_evaluator(args).danger
+
+    return SymbolicEvaluation(args.evaluation[args.n_id], args.triggers)
