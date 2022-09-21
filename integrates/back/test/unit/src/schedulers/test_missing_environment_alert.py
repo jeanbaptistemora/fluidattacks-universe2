47c8e69e13d2bb1aa98f5ed25a24606495f837f6
Andres Saldarriaga <asaldarriaga@fluidattacks.com>
2022-09-21 00:01:47 +0000

    integrates\test(back): #7323.25 add unit tests
    - add unit test in schedulers file

--- integrates/back/test/unit/src/schedulers/test_missing_environment_alert.py
+++ integrates/back/test/unit/src/schedulers/test_missing_environment_alert.py
@@ -8,6 +8,7 @@ from dataloaders import (
 import pytest
 from schedulers.missing_environment_alert import (
     _send_mail_report as send_mail_missing_environment,
+    has_environment,
 )
 
 
@@ -18,3 +19,11 @@ async def test_send_mail_missing_environment() -> None:
         group="unittesting",
         group_date_delta=3,
     )
+
+
+@pytest.mark.asyncio
+async def test_has_environment() -> None:
+    loaders = get_new_context()
+    group = "unittesting"
+    test = await has_environment(loaders, group)
+    assert test is True
