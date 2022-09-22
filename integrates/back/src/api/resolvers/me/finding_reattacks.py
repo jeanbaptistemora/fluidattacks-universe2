7e80a4d76a2f1d8b3a876a2790686cf5f89560e5
Jhon Romero <jromero@fluidattacks.com>
2022-09-22 23:48:55 +0000

    integrates\refac(back): #7504.7 todo reattacks
    - test group not in reattack view

--- integrates/back/src/api/resolvers/me/finding_reattacks.py
+++ integrates/back/src/api/resolvers/me/finding_reattacks.py
@@ -2,10 +2,14 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
+from dataloaders import (
+    Dataloaders,
+)
 from db_model.findings.types import (
     Finding,
 )
 from db_model.findings.utils import (
+    filter_non_in_test_orgs,
     format_finding,
 )
 from decorators import (
@@ -25,22 +29,32 @@ from typing import (
 @require_login
 async def resolve(
     _parent: dict[str, Any],
-    _info: GraphQLResolveInfo,
+    info: GraphQLResolveInfo,
     **_kwargs: None,
 ) -> tuple[Finding, ...]:
 
     not_zero_requested = {
         "unreliable_indicators.unreliable_verification_summary.requested": 0
     }
-
     results = await search(
         must_filters=[
             {"verification.status": "REQUESTED"},
             {"unreliable_indicators.unreliable_status": "OPEN"},
         ],
-        must_not_filters=[{"group_name": "narrabri"}, not_zero_requested],
+        must_not_filters=[not_zero_requested],
         index="findings",
         limit=100,
     )
 
-    return tuple(format_finding(result) for result in results.items)
+    loaders: Dataloaders = info.context.loaders
+    test_group_orgs = await loaders.organization_groups.load_many(
+        (
+            "0d6d8f9d-3814-48f8-ba2c-f4fb9f8d4ffa",
+            "a23457e2-f81f-44a2-867f-230082af676c",
+        )
+    )
+
+    return filter_non_in_test_orgs(
+        test_group_orgs,
+        tuple(format_finding(result) for result in results.items),
+    )
--- integrates/back/src/db_model/findings/utils.py
+++ integrates/back/src/db_model/findings/utils.py
@@ -27,6 +27,9 @@ from db_model.enums import (
     Source,
     StateRemovalJustification,
 )
+from db_model.groups.types import (
+    Group,
+)
 from decimal import (
     Decimal,
 )
@@ -37,6 +40,22 @@ from typing import (
     Optional,
     Union,
 )
+
+
+def filter_non_in_test_orgs(
+    test_group_orgs: tuple[tuple[Group, ...], ...],
+    findings: tuple[Finding, ...],
+) -> tuple[Finding, ...]:
+    test_group_names = tuple(
+        tuple(group.name for group in groups) for groups in test_group_orgs
+    )
+    return tuple(
+        finding
+        for finding in findings
+        if not any(
+            finding.group_name in group_name for group_name in test_group_names
+        )
+    )
 
 
 def filter_non_state_status_findings(
