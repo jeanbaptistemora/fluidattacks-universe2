172a8f8e807b054ebdf04ddb06b703f9578f1073
Andres Aguirre <aaguirre@fluidattacks.com>
2022-09-23 16:40:04 +0000

    integrates\feat(back): #7337.7 calculate org non compliance
    - Add function that calculate the non compliance for an organization
    - no-deltas-test

--- integrates/back/src/schedulers/update_compliance.py
+++ integrates/back/src/schedulers/update_compliance.py
@@ -2,9 +2,15 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
+from aioextensions import (
+    collect,
+)
 from dataloaders import (
     Dataloaders,
     get_new_context,
+)
+from db_model.findings.types import (
+    Finding,
 )
 from db_model.groups.types import (
     Group,
@@ -12,8 +18,26 @@ from db_model.groups.types import (
 from db_model.organizations.types import (
     Organization,
 )
+from db_model.vulnerabilities.enums import (
+    VulnerabilityStateStatus,
+)
+from db_model.vulnerabilities.types import (
+    FindingVulnerabilitiesZrRequest,
+    VulnerabilitiesConnection,
+    Vulnerability,
+)
+from decimal import (
+    Decimal,
+)
 from newutils import (
     organizations as orgs_utils,
+)
+from newutils.compliance import (
+    get_compliance_file,
+)
+from newutils.findings import (
+    get_requirements_file,
+    get_vulns_file,
 )
 from organizations import (
     domain as orgs_domain,
@@ -21,26 +45,118 @@ from organizations import (
 from schedulers.common import (
     info,
 )
+from typing import (
+    Any,
+)
 
 
-async def update_organization_compliance(
-    loaders: Dataloaders, organization: Organization
-) -> None:
-    info(f"Working on organization {organization.name}")
+async def get_open_vulnerabilities(
+    loaders: Dataloaders,
+    finding: Finding,
+) -> tuple[Vulnerability, ...]:
+    connections: VulnerabilitiesConnection = (
+        await loaders.finding_vulnerabilities_nzr_c.load(
+            FindingVulnerabilitiesZrRequest(
+                finding_id=finding.id,
+                paginate=False,
+                state_status=VulnerabilityStateStatus.OPEN,
+            )
+        )
+    )
+    return tuple(edge.node for edge in connections.edges)
+
+
+async def get_organization_non_compliance_level(
+    loaders: Dataloaders,
+    organization: Organization,
+    compliance_file: dict[str, Any],
+    requirements_file: dict[str, Any],
+    vulnerabilities_file: dict[str, Any],
+) -> Decimal:
     org_groups: tuple[Group, ...] = await loaders.organization_groups.load(
         organization.id
     )
-    info(f"Groups {len(org_groups)}")
+    findings: tuple[
+        Finding, ...
+    ] = await loaders.group_findings.load_many_chained(
+        tuple(group.name for group in org_groups)
+    )
+    findings_open_vulnerabilities = await collect(
+        tuple(
+            get_open_vulnerabilities(loaders, finding) for finding in findings
+        ),
+        workers=100,
+    )
+    open_findings: list[Finding] = []
+    for finding, open_vulnerabilities in zip(
+        findings, findings_open_vulnerabilities
+    ):
+        if open_vulnerabilities:
+            open_findings.append(finding)
+
+    requirements_by_finding = tuple(
+        vulnerabilities_file[finding.title[:3]]["requirements"]
+        for finding in open_findings
+    )
+    compliances_by_finding = tuple(
+        set(
+            reference
+            for requirement in requirements
+            for reference in requirements_file[requirement]["references"]
+        )
+        for requirements in requirements_by_finding
+    )
+    org_non_compliance = (
+        set.union(*compliances_by_finding) if compliances_by_finding else set()
+    )
+    all_compliances = set(
+        f"{name.lower()}.{definition}"
+        for name, standard in compliance_file.items()
+        for definition in standard["definitions"]
+    )
+    return (
+        Decimal(
+            len(org_non_compliance.intersection(all_compliances))
+            / len(all_compliances)
+        ).quantize(Decimal("0.01"))
+        if all_compliances
+        else Decimal("0.0")
+    )
+
+
+async def update_organization_compliance(
+    loaders: Dataloaders,
+    organization: Organization,
+    compliance_file: dict[str, Any],
+    requirements_file: dict[str, Any],
+    vulnerabilities_file: dict[str, Any],
+) -> None:
+    info(f"Working on organization {organization.name}")
+    non_compliance_level = await get_organization_non_compliance_level(
+        loaders=loaders,
+        organization=organization,
+        compliance_file=compliance_file,
+        requirements_file=requirements_file,
+        vulnerabilities_file=vulnerabilities_file,
+    )
+    info(f"non_compliance_level {non_compliance_level}")
 
 
 async def update_compliance() -> None:
     loaders: Dataloaders = get_new_context()
+    compliance_file = await get_compliance_file()
+    requirements_file = await get_requirements_file()
+    vulnerabilities_file = await get_vulns_file()
     async for organization in orgs_domain.iterate_organizations():
         if orgs_utils.is_deleted(organization):
             continue
 
         await update_organization_compliance(
-            loaders=loaders, organization=organization
+            loaders=loaders,
+            organization=organization,
+            compliance_file=compliance_file,
+            requirements_file=requirements_file,
+            vulnerabilities_file=vulnerabilities_file,
         )
 
 
