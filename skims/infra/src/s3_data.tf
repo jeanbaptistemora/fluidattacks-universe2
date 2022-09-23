1f384693c16a3564cb64a772da0460a42641a0d6
Alejandro Trujillo <atrujillo@fluidattacks.com>
2022-09-23 11:17:41 -0500

    skims\feat(back): #7389.8 s3 permissions
    - policy for bucket

--- skims/infra/src/s3_data.tf
+++ skims/infra/src/s3_data.tf
@@ -38,6 +38,32 @@ resource "aws_s3_bucket_acl" "skims_sca" {
   acl = "public-read"
 }
 
+
+data "aws_iam_policy_document" "skims_sca" {
+  statement {
+    sid    = "AllowScaPublicRead"
+    effect = "Allow"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions = [
+      "s3:GetObject",
+    ]
+    resources = [
+      "${aws_s3_bucket.skims_sca.arn}/*",
+    ]
+  }
+}
+
+
+resource "aws_s3_bucket_policy" "skims_sca" {
+  bucket = aws_s3_bucket.skims_sca.id
+  policy = data.aws_iam_policy_document.skims_sca.json
+}
+
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "skims_data" {
   bucket = aws_s3_bucket.skims_data.id
 
