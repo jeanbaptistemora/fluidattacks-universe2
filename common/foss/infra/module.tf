ce6d622c1ab1354f7f9432d6601bdab5fcf43339
Kevin Amado <kamado@fluidattacks.com>
2022-09-24 10:14:34 -0600

    common\feat(infra): #7578.1 enable projects
    - Enable projects on github FOSS repositories
      so that we can create a roadmap as OpenSSF
      suggests
    - Add a homepage to makes

--- common/foss/infra/module.tf
+++ common/foss/infra/module.tf
@@ -6,7 +6,7 @@ module "github_makes" {
   source = "./modules/github"
 
   description = "A DevSecOps framework powered by Nix."
-  homepage    = null
+  homepage    = "https://fluidattacks.github.io/makes/"
   name        = "makes"
   token       = var.githubToken
   topics      = ["build", "cd", "ci", "devops", "devsecops", "nix"]
--- common/foss/infra/modules/github/resource.tf
+++ common/foss/infra/modules/github/resource.tf
@@ -36,7 +36,7 @@ resource "github_repository" "repo" {
   description            = var.description
   has_downloads          = false
   has_issues             = true
-  has_projects           = false
+  has_projects           = true
   has_wiki               = false
   homepage_url           = var.homepage
   is_template            = false
