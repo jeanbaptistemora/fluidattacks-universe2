e29667246e80983c6e792c5680c669701b052b11
Carlos Betancur <cbetancur@fluidattacks.com>
2022-09-26 14:53:48 -0500

    integrates\test(back): #4944.40 populate db from conftest
    - modify batch conftest to populate db from it
    - add mapping_to_key unit test
    - rename unit test
    - no-deltas-test

--- integrates/back/test/unit/src/batch/test_dal.py
+++ integrates/back/test/unit/src/batch/test_dal.py
@@ -5,8 +5,13 @@
 from aioextensions import (
     collect,
 )
-from batch import (
-    dal as batch_dal,
+from batch.dal import (
+    delete_action,
+    get_action,
+    get_actions,
+    mapping_to_key,
+    put_action_to_batch,
+    put_action_to_dynamodb,
 )
 from batch.enums import (
     Product,
@@ -38,96 +43,36 @@ pytestmark = [
 ]
 
 TABLE_NAME = "fi_async_processing"
-key_schema = [{"AttributeName": "pk", "KeyType": "HASH"}]
-attribute_definitions = [{"AttributeName": "pk", "AttributeType": "S"}]
 
-TIME = str(get_as_epoch(get_now()))
 
-data = [
-    dict(
-        action_name="report",
-        entity="oneshottest",
-        subject="unittesting@fluidattacks.com",
-        time=TIME,
-        additional_info=json.dumps(
-            {
-                "report_type": "PDF",
-                "treatments": [
-                    "ACCEPTED",
-                    "ACCEPTED_UNDEFINED",
-                    "IN_PROGRESS",
-                    "NEW",
-                ],
-                "states": ["CLOSED", "OPEN"],
-                "verifications": [],
-                "closing_date": None,
-                "finding_title": "",
-                "age": None,
-                "min_severity": None,
-                "max_severity": None,
-            }
-        ),
-    ),
-    dict(
+def test_create_table(dynamodb: ServiceResource, populate_db: bool) -> None:
+    assert populate_db
+    assert TABLE_NAME in dynamodb_backend2.tables
+    assert len(dynamodb.Table(TABLE_NAME).scan()["Items"]) == 5
+
+
+async def test_delete_action(dynamodb: ServiceResource) -> None:
+    key = "44aa89bddf5e0a5b1aca2551799b71ff593c95a89f4402b84697e9b29f652110"
+    with mock.patch("batch.dal.dynamodb_ops.delete_item") as mock_delete_item:
+        mock_delete_item.return_value = dynamodb.Table(TABLE_NAME).delete_item(
+            Key={"pk": key}
+        )
+        assert await delete_action(dynamodb_pk=key)
+    assert len(dynamodb.Table(TABLE_NAME).scan()["Items"]) == 4
+    with pytest.raises(Exception) as delete_exception:
+        await delete_action()
+    assert "you must supply the dynamodb pk" in str(delete_exception.value)
+
+
+async def test_get_action(dynamodb: ServiceResource) -> None:
+    item_1 = dict(
         action_name="report",
         entity="unittesting",
         subject="unittesting@fluidattacks.com",
-        time=TIME,
         additional_info=json.dumps(
             {
                 "report_type": "XLS",
-                "treatments": [
-                    "ACCEPTED",
-                    "ACCEPTED_UNDEFINED",
-                    "IN_PROGRESS",
-                    "NEW",
-                ],
-                "states": ["CLOSED", "OPEN"],
-                "verifications": [],
-                "closing_date": None,
-                "finding_title": "",
-                "age": None,
-                "min_severity": None,
-                "max_severity": None,
-            }
-        ),
-    ),
-    dict(
-        action_name="report",
-        entity="unittesting",
-        subject="unittesting@fluidattacks.com",
-        time=TIME,
-        additional_info=json.dumps(
-            {
-                "report_type": "DATA",
-                "treatments": [
-                    "ACCEPTED",
-                    "ACCEPTED_UNDEFINED",
-                    "IN_PROGRESS",
-                    "NEW",
-                ],
-                "states": ["CLOSED", "OPEN"],
-                "verifications": [],
-                "closing_date": None,
-                "finding_title": "",
-                "age": None,
-                "min_severity": None,
-                "max_severity": None,
-            }
-        ),
-    ),
-    dict(
-        action_name="report",
-        entity="unittesting",
-        subject="unittesting@fluidattacks.com",
-        time=TIME,
-        additional_info=json.dumps(
-            {
-                "report_type": "XLS",
-                "treatments": [
-                    "ACCEPTED",
-                    "NEW",
-                ],
+                "treatments": ["ACCEPTED", "NEW"],
                 "states": ["OPEN"],
                 "verifications": [],
                 "closing_date": None,
@@ -137,12 +82,127 @@ data = [
                 "max_severity": "6.4",
             }
         ),
-    ),
-    dict(
+    )
+    key = mapping_to_key(
+        [
+            item_1["action_name"],
+            item_1["entity"],
+            item_1["subject"],
+            item_1["additional_info"],
+        ]
+    )
+    with mock.patch("batch.dal.dynamodb_ops.query") as mock_query:
+        mock_query.return_value = [
+            dynamodb.Table(TABLE_NAME).get_item(Key={"pk": key})["Item"]
+        ]
+        action_in_db = await get_action(action_dynamo_pk=key)
+    assert bool(action_in_db)
+
+    item_2 = dict(
+        action_name="report",
+        entity="continuoustesting",
+        subject="integratesmanager@gmail.com",
+        additional_info="PDF",
+    )
+    key_2 = mapping_to_key(
+        [
+            item_2["action_name"],
+            item_2["entity"],
+            item_2["subject"],
+            item_2["additional_info"],
+        ]
+    )
+    with mock.patch("batch.dal.dynamodb_ops.query") as mock_query:
+        try:
+            mock_query.return_value = [
+                dynamodb.Table(TABLE_NAME).get_item(Key={"pk": key_2})["Item"]
+            ]
+        except KeyError:
+            mock_query.return_value = []
+        action_not_in_db = await get_action(action_dynamo_pk=key_2)
+    assert not bool(action_not_in_db)
+
+
+async def test_get_actions(dynamodb: ServiceResource) -> None:
+    with mock.patch("batch.dal.dynamodb_ops.scan") as mock_scan:
+        mock_scan.return_value = dynamodb.Table(TABLE_NAME).scan()["Items"]
+        all_actions = await get_actions()
+    assert isinstance(all_actions, list)
+    assert len(all_actions) == 4
+
+
+def test_mapping_to_key(dynamodb: ServiceResource) -> None:
+    item_1 = dict(
         action_name="report",
         entity="unittesting",
         subject="unittesting@fluidattacks.com",
-        time=TIME,
+        additional_info=json.dumps(
+            {
+                "report_type": "XLS",
+                "treatments": ["ACCEPTED", "NEW"],
+                "states": ["OPEN"],
+                "verifications": [],
+                "closing_date": None,
+                "finding_title": "038",
+                "age": 1100,
+                "min_severity": "2.4",
+                "max_severity": "6.4",
+            }
+        ),
+    )
+    key = mapping_to_key(
+        [
+            item_1["action_name"],
+            item_1["entity"],
+            item_1["subject"],
+            item_1["additional_info"],
+        ]
+    )
+
+    assert dynamodb.Table(TABLE_NAME).get_item(Key={"pk": key})["Item"]
+
+
+async def test_put_action_to_batch(dynamodb: ServiceResource) -> None:
+    with mock.patch("batch.dal.dynamodb_ops.scan") as mock_scan:
+        mock_scan.return_value = dynamodb.Table(TABLE_NAME).scan()["Items"]
+        pending_actions: List[BatchProcessing] = await get_actions()
+
+    assert all(
+        result is None
+        for result in await collect(
+            [
+                put_action_to_batch(
+                    entity=action.entity,
+                    action_name=action.action_name,
+                    action_dynamo_pk=action.key,
+                    queue=action.queue,
+                    product_name=(
+                        Product.SKIMS
+                        if action.action_name == "execute-machine"
+                        else Product.INTEGRATES
+                    ).value,
+                )
+                for action in pending_actions
+            ],
+            workers=20,
+        )
+    )
+
+
+async def test_put_action_to_dynamodb(dynamodb: ServiceResource) -> None:
+    def side_effect(item: dict, table: str) -> bool:
+        if bool(table and item):
+            return dynamodb.Table(TABLE_NAME).put_item(  # type: ignore
+                Item=item
+            )
+        return False
+
+    time = str(get_as_epoch(get_now()))
+    item = dict(
+        action_name="report",
+        entity="unittesting",
+        subject="unittesting@fluidattacks.com",
+        time=time,
         additional_info=json.dumps(
             {
                 "report_type": "XLS",
@@ -161,137 +221,23 @@ data = [
                 "max_severity": None,
             }
         ),
-    ),
-]
-
-
-async def test_put_action_to_dynamodb(dynamodb: ServiceResource) -> None:
-    def side_effect(item: dict, table: str) -> bool:
-        if bool(table and item):
-            return dynamodb.Table(TABLE_NAME).put_item(  # type: ignore
-                Item=item
-            )
-        return False
-
-    dynamodb.create_table(
-        TableName=TABLE_NAME,
-        KeySchema=key_schema,  # type: ignore
-        AttributeDefinitions=attribute_definitions,  # type: ignore
     )
-    assert TABLE_NAME in dynamodb_backend2.tables
-    assert len(dynamodb.Table(TABLE_NAME).scan()["Items"]) == 0
+    key = mapping_to_key(
+        [
+            item["action_name"],
+            item["entity"],
+            item["subject"],
+            item["additional_info"],
+        ]
+    )
+    assert len(dynamodb.Table(TABLE_NAME).scan()["Items"]) == 4
     with mock.patch("batch.dal.dynamodb_ops.put_item") as mock_put_item:
         mock_put_item.side_effect = side_effect
-        for item in data:
-            await batch_dal.put_action_to_dynamodb(**item)
+        await put_action_to_dynamodb(**item)
     assert len(dynamodb.Table(TABLE_NAME).scan()["Items"]) == 5
     assert (
-        dynamodb.Table(TABLE_NAME).scan()["Items"][0]["entity"]
-        == "oneshottest"
+        key
+        in dynamodb.Table(TABLE_NAME)
+        .get_item(Key={"pk": key})["Item"]
+        .values()
     )
-
-
-async def test_get_actions(dynamodb: ServiceResource) -> None:
-    with mock.patch("batch.dal.dynamodb_ops.scan") as mock_scan:
-        mock_scan.return_value = dynamodb.Table(TABLE_NAME).scan()["Items"]
-        all_actions = await batch_dal.get_actions()
-    assert isinstance(all_actions, list)
-    assert len(all_actions) == 5
-
-
-async def test_get_action(dynamodb: ServiceResource) -> None:
-    with mock.patch("batch.dal.dynamodb_ops.query") as mock_query:
-        item_1 = dict(
-            action="report",
-            entity="unittesting",
-            subject="unittesting@fluidattacks.com",
-            additional_info=json.dumps(
-                {
-                    "report_type": "XLS",
-                    "treatments": ["ACCEPTED", "NEW"],
-                    "states": ["OPEN"],
-                    "verifications": [],
-                    "closing_date": None,
-                    "finding_title": "038",
-                    "age": 1100,
-                    "min_severity": "2.4",
-                    "max_severity": "6.4",
-                }
-            ),
-        )
-        key = batch_dal.mapping_to_key(
-            [
-                item_1["action"],
-                item_1["entity"],
-                item_1["subject"],
-                item_1["additional_info"],
-            ]
-        )
-        mock_query.return_value = [
-            dynamodb.Table(TABLE_NAME).get_item(Key={"pk": key})["Item"]
-        ]
-        action_in_db = await batch_dal.get_action(action_dynamo_pk=key)
-        assert bool(action_in_db)
-
-        item_2 = dict(
-            action="report",
-            entity="continuoustesting",
-            subject="integratesmanager@gmail.com",
-            additional_info="PDF",
-        )
-        key_2 = batch_dal.mapping_to_key(
-            [
-                item_2["action"],
-                item_2["entity"],
-                item_2["subject"],
-                item_2["additional_info"],
-            ]
-        )
-        try:
-            mock_query.return_value = [
-                dynamodb.Table(TABLE_NAME).get_item(Key={"pk": key_2})["Item"]
-            ]
-        except KeyError:
-            mock_query.return_value = []
-        action_not_in_db = await batch_dal.get_action(action_dynamo_pk=key_2)
-        assert not bool(action_not_in_db)
-
-
-async def test_requeue_actions(dynamodb: ServiceResource) -> None:
-    with mock.patch("batch.dal.dynamodb_ops.scan") as mock_scan:
-        mock_scan.return_value = dynamodb.Table(TABLE_NAME).scan()["Items"]
-        pending_actions: List[BatchProcessing] = await batch_dal.get_actions()
-
-    assert all(
-        result is None
-        for result in await collect(
-            [
-                batch_dal.put_action_to_batch(
-                    entity=action.entity,
-                    action_name=action.action_name,
-                    action_dynamo_pk=action.key,
-                    queue=action.queue,
-                    product_name=(
-                        Product.SKIMS
-                        if action.action_name == "execute-machine"
-                        else Product.INTEGRATES
-                    ).value,
-                )
-                for action in pending_actions
-            ],
-            workers=20,
-        )
-    )
-
-
-async def test_delete_action(dynamodb: ServiceResource) -> None:
-    key = "44aa89bddf5e0a5b1aca2551799b71ff593c95a89f4402b84697e9b29f652110"
-    with mock.patch("batch.dal.dynamodb_ops.delete_item") as mock_delete_item:
-        mock_delete_item.return_value = dynamodb.Table(TABLE_NAME).delete_item(
-            Key={"pk": key}
-        )
-        assert await batch_dal.delete_action(dynamodb_pk=key)
-    assert len(dynamodb.Table(TABLE_NAME).scan()["Items"]) == 4
-    with pytest.raises(Exception) as delete_exception:
-        await batch_dal.delete_action()
-    assert "you must supply the dynamodb pk" in str(delete_exception.value)
