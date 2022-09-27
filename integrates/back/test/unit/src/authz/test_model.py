da00beff043690af79309846420a71375cc00162
Carlos Betancur <cbetancur@fluidattacks.com>
2022-09-27 11:14:47 -0500

    integrates\test(back): #7323.33 add authz unit test
    - add authz.model unit test
    - no-deltas-test

--- integrates/back/test/unit/src/authz/test_model.py
+++ integrates/back/test/unit/src/authz/test_model.py
@@ -3,11 +3,15 @@
 # SPDX-License-Identifier: MPL-2.0
 
 import authz
+from authz.model import (
+    get_user_level_roles_model,
+)
 import pytest
 from typing import (
     Any,
     Dict,
     List,
+    Set,
 )
 
 # Constants
@@ -116,3 +120,118 @@ def test_model_integrity_roles(
     roles_common: Dict[str, Any], roles_fluid: Dict[str, Any]
 ) -> None:
     assert sorted(roles_common.keys()) == sorted(roles_fluid.keys())
+
+
+@pytest.mark.parametrize(
+    ["email", "expected_output"],
+    [
+        [
+            "continuoushacking@gmail.com",
+            {
+                "admin": {
+                    "actions": {
+                        "api_resolvers_query_list_user_groups_resolve",
+                        "api_resolvers_query_vulnerabilities_to_reattack_resolve",  # noqa: E501
+                        "api_resolvers_query_groups_with_forces_resolve",
+                        "api_resolvers_query_groups_resolve",
+                        "api_mutations_add_organization_mutate",
+                        "api_mutations_update_stakeholder_phone_mutate",
+                        "grant_user_level_role:hacker",
+                        "api_mutations_start_machine_execution_mutate",
+                        "api_mutations_add_group_mutate",
+                        "grant_user_level_role:user",
+                        "front_can_use_groups_searchbar",
+                        "grant_user_level_role:admin",
+                        "api_mutations_add_machine_execution_mutate",
+                        "api_mutations_finish_machine_execution_mutate",
+                        "api_mutations_submit_group_machine_execution_mutate",
+                        "api_mutations_verify_stakeholder_mutate",
+                        "api_mutations_add_stakeholder_mutate",
+                        "api_mutations_invalidate_cache_mutate",
+                    },
+                    "tags": set(),
+                },
+                "hacker": {
+                    "actions": {
+                        "api_mutations_verify_stakeholder_mutate",
+                        "api_mutations_update_stakeholder_phone_mutate",
+                    },
+                    "tags": {"drills"},
+                },
+                "user": {
+                    "actions": {
+                        "api_mutations_update_stakeholder_phone_mutate",
+                        "api_mutations_verify_stakeholder_mutate",
+                        "api_mutations_add_organization_mutate",
+                    },
+                    "tags": set(),
+                },
+            },
+        ],
+        [
+            "integrateshacker@fluidattacks.com",
+            {
+                "admin": {
+                    "actions": {
+                        "api_resolvers_query_list_user_groups_resolve",
+                        "api_resolvers_query_vulnerabilities_to_reattack_resolve",  # noqa: E501
+                        "front_can_edit_credentials_secrets_in_bulk",
+                        "api_resolvers_query_groups_with_forces_resolve",
+                        "api_resolvers_query_groups_resolve",
+                        "api_mutations_add_organization_mutate",
+                        "front_can_retrieve_todo_drafts",
+                        "api_mutations_update_stakeholder_phone_mutate",
+                        "grant_user_level_role:hacker",
+                        "front_can_retrieve_todo_reattacks",
+                        "api_mutations_start_machine_execution_mutate",
+                        "api_mutations_add_group_mutate",
+                        "grant_user_level_role:user",
+                        "front_can_use_groups_searchbar",
+                        "grant_user_level_role:admin",
+                        "api_mutations_add_machine_execution_mutate",
+                        "api_mutations_finish_machine_execution_mutate",
+                        "api_resolvers_finding_hacker_resolve",
+                        "api_mutations_submit_group_machine_execution_mutate",
+                        "api_mutations_verify_stakeholder_mutate",
+                        "api_mutations_add_stakeholder_mutate",
+                        "api_mutations_invalidate_cache_mutate",
+                    },
+                    "tags": set(),
+                },
+                "hacker": {
+                    "actions": {
+                        "api_resolvers_query_list_user_groups_resolve",
+                        "api_mutations_add_group_mutate",
+                        "front_can_use_groups_searchbar",
+                        "front_can_edit_credentials_secrets_in_bulk",
+                        "api_mutations_add_organization_mutate",
+                        "front_can_retrieve_todo_drafts",
+                        "api_resolvers_finding_hacker_resolve",
+                        "api_mutations_update_stakeholder_phone_mutate",
+                        "front_can_retrieve_todo_reattacks",
+                        "api_mutations_verify_stakeholder_mutate",
+                    },
+                    "tags": {"drills"},
+                },
+                "user": {
+                    "actions": {
+                        "api_mutations_add_group_mutate",
+                        "front_can_use_groups_searchbar",
+                        "front_can_edit_credentials_secrets_in_bulk",
+                        "api_mutations_add_organization_mutate",
+                        "front_can_retrieve_todo_drafts",
+                        "api_resolvers_finding_hacker_resolve",
+                        "api_mutations_update_stakeholder_phone_mutate",
+                        "front_can_retrieve_todo_reattacks",
+                        "api_mutations_verify_stakeholder_mutate",
+                    },
+                    "tags": set(),
+                },
+            },
+        ],
+    ],
+)
+def test_get_user_level_roles_model(
+    email: str, expected_output: Dict[str, Dict[str, Set[str]]]
+) -> None:
+    assert get_user_level_roles_model(email) == expected_output
