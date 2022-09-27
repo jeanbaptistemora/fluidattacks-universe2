cca3c2ded2136a7578daa7d92b96007b8ed4efd5
Sebastian Lizcano <slizcano@fluidattacks.com>
2022-09-27 13:15:21 -0500

    integrates\refac(front): #7544.3 remove unused code
    - remove unnecessary container as changed order was released directly
    instead of using feature preview
    - no-deltas-test

--- integrates/front/src/scenes/Dashboard/containers/ChartsGenericView/index.test.tsx
+++ integrates/front/src/scenes/Dashboard/containers/ChartsGenericView/index.test.tsx
@@ -11,7 +11,6 @@ import React from "react";
 
 import { SUBSCRIPTIONS_TO_ENTITY_REPORT } from "./queries";
 
-import { ChartsGenericView } from "scenes/Dashboard/containers/ChartsGenericView";
 import { ChartsChangedOrderView } from "scenes/Dashboard/containers/ChartsGenericView/newOrderIndex";
 
 describe("ChartsGenericView", (): void => {
@@ -32,72 +31,10 @@ describe("ChartsGenericView", (): void => {
 
   it("should return an function", (): void => {
     expect.hasAssertions();
-    expect(typeof ChartsGenericView).toBe("function");
-  });
-
-  it("should return an function for changed order", (): void => {
-    expect.hasAssertions();
     expect(typeof ChartsChangedOrderView).toBe("function");
   });
 
   it("should render a component and number of graphics of entity", async (): Promise<void> => {
-    expect.hasAssertions();
-
-    const groupGraphics: number = 41;
-    const organizationAndPortfolioGraphics: number = 46;
-
-    const { container, rerender } = render(
-      <MockedProvider addTypename={true} mocks={[mocks]}>
-        <ChartsGenericView
-          bgChange={false}
-          entity={"organization"}
-          reportMode={false}
-          subject={"subject"}
-        />
-      </MockedProvider>
-    );
-
-    await waitFor((): void => {
-      expect(container.getElementsByClassName("frame")).toHaveLength(
-        organizationAndPortfolioGraphics
-      );
-    });
-
-    rerender(
-      <MockedProvider addTypename={true} mocks={[mocks]}>
-        <ChartsGenericView
-          bgChange={false}
-          entity={"group"}
-          reportMode={false}
-          subject={"subject"}
-        />
-      </MockedProvider>
-    );
-
-    await waitFor((): void => {
-      expect(container.getElementsByClassName("frame")).toHaveLength(
-        groupGraphics
-      );
-    });
-
-    rerender(
-      <MockedProvider addTypename={true} mocks={[mocks]}>
-        <ChartsGenericView
-          bgChange={false}
-          entity={"portfolio"}
-          reportMode={false}
-          subject={"subject"}
-        />
-      </MockedProvider>
-    );
-    await waitFor((): void => {
-      expect(container.getElementsByClassName("frame")).toHaveLength(
-        organizationAndPortfolioGraphics
-      );
-    });
-  });
-
-  it("should render a component and number of graphics of entity for changed order", async (): Promise<void> => {
     expect.hasAssertions();
 
     const groupGraphics: number = 41;
