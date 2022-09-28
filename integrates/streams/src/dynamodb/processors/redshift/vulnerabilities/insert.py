ddd116dcc0d64d2159b5ae08802485d88cc12e86
Jonathan Mesa <jmesa@fluidattacks.com>
2022-09-28 19:22:56 +0000

    integrates\feat(back): #7329 redshift vulns
    - migrate insert_historic ops for state and treatment

--- integrates/streams/src/dynamodb/processors/redshift/vulnerabilities/insert.py
+++ integrates/streams/src/dynamodb/processors/redshift/vulnerabilities/insert.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: MPL-2.0
 
 from ..queries import (
+    SQL_INSERT_HISTORIC,
     SQL_INSERT_METADATA,
 )
 from ..utils import (
@@ -10,12 +11,18 @@ from ..utils import (
 )
 from .initialize import (
     METADATA_TABLE,
+    STATE_TABLE,
+    TREATMENT_TABLE,
 )
 from .types import (
     MetadataTableRow,
+    StateTableRow,
+    TreatmentTableRow,
 )
 from .utils import (
     format_row_metadata,
+    format_row_state,
+    format_row_treatment,
 )
 from dynamodb.types import (
     Item,
@@ -35,6 +42,49 @@ def insert_metadata(
     cursor.execute(  # nosec
         SQL_INSERT_METADATA.substitute(
             table=METADATA_TABLE,
+            fields=_fields,
+            values=values,
+        ),
+        sql_values,
+    )
+
+
+def insert_historic_state(
+    *,
+    cursor: cursor_cls,
+    vulnerability_id: str,
+    historic_state: tuple[Item, ...],
+) -> None:
+    _fields, values = format_query_fields(StateTableRow)
+    sql_values = [
+        format_row_state(vulnerability_id, state) for state in historic_state
+    ]
+    cursor.executemany(  # nosec
+        SQL_INSERT_HISTORIC.substitute(
+            table_metadata=METADATA_TABLE,
+            table_historic=STATE_TABLE,
+            fields=_fields,
+            values=values,
+        ),
+        sql_values,
+    )
+
+
+def insert_historic_treatment(
+    *,
+    cursor: cursor_cls,
+    vulnerability_id: str,
+    historic_treatment: tuple[Item, ...],
+) -> None:
+    _fields, values = format_query_fields(TreatmentTableRow)
+    sql_values = [
+        format_row_treatment(vulnerability_id, treatment)
+        for treatment in historic_treatment
+    ]
+    cursor.executemany(  # nosec
+        SQL_INSERT_HISTORIC.substitute(
+            table_metadata=METADATA_TABLE,
+            table_historic=TREATMENT_TABLE,
             fields=_fields,
             values=values,
         ),
