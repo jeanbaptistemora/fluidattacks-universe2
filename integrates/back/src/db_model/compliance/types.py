3d55b87e6b2b602d1fbaceb2fe2e3444ff2ff38d
Andres Aguirre <aaguirre@fluidattacks.com>
2022-09-28 20:28:02 +0000

    integrates\feat(back): #7337.29 format compliance indicators
    - Add formatters for the compliance indicators
    - Add the related types
    - no-deltas-test

---
+++ integrates/back/src/db_model/compliance/types.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from decimal import (
+    Decimal,
+)
+from typing import (
+    NamedTuple,
+    Optional,
+)
+
+
+class ComplianceStandard(NamedTuple):
+    avg_organization_non_compliance_level: Decimal
+    best_organization_non_compliance_level: Decimal
+    standard_name: str
+    worst_organization_non_compliance_level: Decimal
+
+
+class ComplianceUnreliableIndicators(NamedTuple):
+    standards: Optional[list[ComplianceStandard]] = None
