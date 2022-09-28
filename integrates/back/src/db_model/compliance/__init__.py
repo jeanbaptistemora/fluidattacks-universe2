8c167179011f05ca00646a8460d4ce8a4c4a2efe
Andres Aguirre <aaguirre@fluidattacks.com>
2022-09-28 22:03:01 +0000

    integrates\feat(back): #7337.31 update compliance indicators
    - Add model function to update the compliance indicators

--- integrates/back/src/db_model/compliance/__init__.py
+++ integrates/back/src/db_model/compliance/__init__.py
@@ -1,3 +1,11 @@
 # SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
 #
 # SPDX-License-Identifier: MPL-2.0
+
+from .update import (
+    update_unreliable_indicators,
+)
+
+__all__ = [
+    "update_unreliable_indicators",
+]
