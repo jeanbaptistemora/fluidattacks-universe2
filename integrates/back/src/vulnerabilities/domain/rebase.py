e97047f59c00cbf87f308727672b31d601d4cb37
Diego Restrepo <drestrepo@fluidattacks.com>
2022-09-28 14:31:50 -0500

    integrates\feat(back): #7300.16 log rebase problems
    - show vulnerabilities that could conflict with rebase

--- integrates/back/src/vulnerabilities/domain/rebase.py
+++ integrates/back/src/vulnerabilities/domain/rebase.py
@@ -15,8 +15,16 @@ from db_model.vulnerabilities.types import (
     Vulnerability,
     VulnerabilityMetadataToUpdate,
 )
+import logging
+from settings.logger import (
+    LOGGING,
+)
 from typing import (
     Tuple,
+)
+from vulnerabilities.domain.utils import (
+    get_hash,
+    get_path_from_integrates_vulnerability,
 )
 from vulnerabilities.domain.validations import (
     validate_commit_hash,
@@ -24,6 +32,11 @@ from vulnerabilities.domain.validations import (
     validate_uniqueness,
     validate_where,
 )
+
+logging.config.dictConfig(LOGGING)
+
+# Constants
+LOGGER = logging.getLogger(__name__)
 
 
 async def rebase(
@@ -41,6 +54,47 @@ async def rebase(
 
     validate_commit_hash(vulnerability_commit)
     validate_specific(vulnerability_specific)
+    current_vuln: Vulnerability = next(
+        vuln for vuln in finding_vulns_data if vuln.id == vulnerability_id
+    )
+    current_vuln_hash = get_hash(
+        specific=current_vuln.specific,
+        type_=current_vuln.type,
+        where=get_path_from_integrates_vulnerability(
+            current_vuln.where, current_vuln.type
+        )[1],
+        root_id=current_vuln.root_id,
+    )
+    for vuln in finding_vulns_data:
+        vuln_hash: int = get_hash(
+            specific=vuln.specific,
+            type_=vuln.type,
+            where=get_path_from_integrates_vulnerability(
+                vuln.where, vuln.type
+            )[1],
+            root_id=vuln.root_id,
+        )
+        if vuln_hash == current_vuln_hash:
+            LOGGER.warning(
+                "there is a problem with the rebase vulnerability",
+                extra={
+                    "extra": {
+                        "vuln_to_rebase": {
+                            "id": current_vuln.id,
+                            "path": current_vuln.where,
+                            "line": current_vuln.specific,
+                            "root_id": current_vuln.root_id,
+                        },
+                        "vuln_overwrite": {
+                            "id": vuln.id,
+                            "path": vuln.where,
+                            "line": vuln.specific,
+                            "root_id": vuln.root_id,
+                        },
+                    }
+                },
+            )
+
     validate_uniqueness(
         finding_vulns_data=finding_vulns_data,
         vulnerability_where=vulnerability_where,
