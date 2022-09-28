58eb8d8a686d131cca31e3ea89ef93ffcbae32cc
Frank Aristizabal <faristizabal@fluidattacks.com>
2022-09-28 10:40:18 -0500

    airs\refac(cross): #7489 add form
    - Add new form to events.
    - no-deltas-test

---
+++ airs/front/src/templates/qrEventTemplate.tsx
@@ -0,0 +1,105 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+/* eslint react/no-danger:0 */
+/* eslint import/no-default-export:0 */
+/* eslint @typescript-eslint/no-invalid-void-type:0 */
+/* eslint @typescript-eslint/no-confusing-void-expression:0 */
+import { graphql } from "gatsby";
+import type { StaticQueryDocument } from "gatsby";
+import { Breadcrumb } from "gatsby-plugin-breadcrumb";
+import React from "react";
+
+import { Layout } from "../components/Layout";
+import { NavbarComponent } from "../components/Navbar";
+import { PageHeader } from "../components/PageHeader";
+import { Seo } from "../components/Seo";
+import { IframeContainer, PageArticle } from "../styles/styledComponents";
+import { capitalizeObject, capitalizePlainString } from "../utils/utilities";
+
+const QrEventIndex: React.FC<IQueryData> = ({
+  data,
+  pageContext,
+}: IQueryData): JSX.Element => {
+  const {
+    breadcrumb: { crumbs },
+  } = pageContext;
+
+  const { description, keywords, slug, subtext, subtitle, title } =
+    data.markdownRemark.frontmatter;
+
+  return (
+    <React.Fragment>
+      <Seo
+        description={description}
+        image={
+          "https://res.cloudinary.com/fluid-attacks/image/upload/v1619631770/airs/contact-us/bg-contact-us_cpcyoj.png"
+        }
+        keywords={keywords}
+        title={`${title} | Fluid Attacks`}
+        url={slug}
+      />
+
+      <Layout>
+        <div>
+          <NavbarComponent />
+          <Breadcrumb
+            crumbLabel={capitalizePlainString(title)}
+            crumbSeparator={" / "}
+            crumbs={capitalizeObject(crumbs)}
+          />
+
+          <PageArticle bgColor={"#f9f9f9"}>
+            <PageHeader
+              banner={"contact-bg"}
+              pageWithBanner={true}
+              slug={slug}
+              subtext={subtext}
+              subtitle={subtitle}
+              title={title}
+            />
+
+            <IframeContainer>
+              <iframe
+                sandbox={
+                  "allow-forms allow-top-navigation allow-same-origin allow-scripts allow-popups"
+                }
+                src={
+                  "https://forms.zohopublic.com/fluidattacks1/form/QREvents/formperma/DrJym0UgnGezDSs8wr46hykRnWlDKHYuZZfz5Ig8YiE"
+                }
+                style={{
+                  border: "0",
+                  height: "900px",
+                  marginBottom: "-7px",
+                  width: "100%",
+                }}
+                title={"QR Event Form"}
+              />
+            </IframeContainer>
+          </PageArticle>
+        </div>
+      </Layout>
+    </React.Fragment>
+  );
+};
+
+export default QrEventIndex;
+
+export const query: StaticQueryDocument = graphql`
+  query QrEventIndex($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      fields {
+        slug
+      }
+      frontmatter {
+        description
+        keywords
+        title
+        subtitle
+      }
+    }
+  }
+`;
