c011cd3a61f3aca49a9299934767dcd35683352f
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-10-03 13:21:28 -0500

    skims\feat(back): #6769.36 add method to model
    - Add new method to model

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -1836,6 +1836,14 @@ class MethodsEnum(Enum):
         developer=DeveloperEnum.LUIS_SAAVEDRA,
         technique=TechniqueEnum.DAST,
     )
+    AWS_INSECURE_PROTOCOLS = MethodInfo(
+        file_name="aws",
+        name="serves_content_over_insecure_protocols",
+        module="dast",
+        finding=FindingEnum.F016,
+        developer=DeveloperEnum.LUIS_SAAVEDRA,
+        technique=TechniqueEnum.DAST,
+    )
     CFN_EC2_SEC_GROUPS_RFC1918 = MethodInfo(
         file_name="cloudformation",
         name="cfn_ec2_has_security_groups_ip_ranges_in_rfc1918",
