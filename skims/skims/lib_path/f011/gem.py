5664de3b1a4abf8968b402c4d41d55a06085aabe
Lewis Contreras <lcontreras@fluidattacks.com>
2022-10-03 11:33:28 -0500

    skims\feat(back): #6796.21 mod dep resolver gem
    - Modify dep resolver for gem f011
    - Modify patterns
    - Update lib_path.csv
    - no-deltas-test

--- skims/skims/lib_path/f011/gem.py
+++ skims/skims/lib_path/f011/gem.py
@@ -19,18 +19,18 @@ from typing import (
     Tuple,
 )
 
+GEMFILE_DEP: Pattern[str] = re.compile(
+    r'\s*gem\s*"(.*)",\s*?"=?\s?([\d+\.?]+)'
+)
+NOT_PROD_DEP: Pattern[str] = re.compile(
+    r":group => \[?[:\w\-, ]*(:development|:test)"
+)
+NOT_PROD_GROUP: Pattern[str] = re.compile(r"(\s*)group :(test|development)")
+GEM_LOCK_DEP: Pattern[str] = re.compile(r"\s+(\S+)\s+\(=?\s?([^><~,]+)\)")
+
 
 @pkg_deps_to_vulns(Platform.GEM, MethodsEnum.GEM_GEMFILE)
 def gem_gemfile(gem_info: Tuple[str, str]) -> Iterator[DependencyType]:
-    require_patt: Pattern[str] = re.compile(
-        r'\s*gem\s*"(.*)",\s*?"=?\s?([\d+\.?]+)'
-    )
-    not_prod_patt: Pattern[str] = re.compile(
-        r":group => \[?[:\w\-, ]*(:development|:test)"
-    )
-    test_dev_group_patt: Pattern[str] = re.compile(
-        r"(\s*)group :(test|development)"
-    )
     line_group: bool = False
     end_line: str = ""
     for line_number, line in enumerate(gem_info[0].splitlines(), 1):
@@ -38,12 +38,12 @@ def gem_gemfile(gem_info: Tuple[str, str]) -> Iterator[DependencyType]:
             if line == end_line:
                 line_group = False
                 end_line = ""
-        elif match_group := re.search(test_dev_group_patt, line):
+        elif match_group := re.search(NOT_PROD_GROUP, line):
             line_group = True
             blank = match_group.group(1)
             end_line = f"{blank}end"
-        elif matched := re.search(require_patt, line):
-            if re.search(not_prod_patt, line):
+        elif matched := re.search(GEMFILE_DEP, line):
+            if re.search(NOT_PROD_DEP, line):
                 continue
             pkg_name = matched.group(1)
             version = matched.group(2)
@@ -53,14 +53,13 @@ def gem_gemfile(gem_info: Tuple[str, str]) -> Iterator[DependencyType]:
 @pkg_deps_to_vulns(Platform.GEM, MethodsEnum.GEM_GEMFILE_LOCK)
 def gem_gemfile_lock(gem_info: Tuple[str, str]) -> Iterator[DependencyType]:
     line_gem: bool = False
-    form_dep: Pattern[str] = re.compile(r"\s+(\S+)\s+\(=?\s?([^><~,]+)\)")
     match_arr: List[str] = []
     for line_number, line in enumerate(gem_info[0].splitlines(), 1):
         if line.startswith("GEM"):
             line_gem = True
         elif not line_gem:
             continue
-        elif matched := re.match(form_dep, line):
+        elif matched := re.match(GEM_LOCK_DEP, line):
             pkg_name = matched.group(1)
             if pkg_name in match_arr:
                 continue
