7d7f0fdb0657ab937c60b00d14cd629e1e38ffad
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-10-05 11:54:29 -0500

    skims\feat(back): #6769.40 add method to model
    - Add method dast for F070 to model

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -1851,6 +1851,14 @@ class MethodsEnum(Enum):
         developer=DeveloperEnum.LUIS_SAAVEDRA,
         technique=TechniqueEnum.DAST,
     )
+    AWS_USES_INSECURE_SECURITY_POLICY = MethodInfo(
+        file_name="aws",
+        name="uses_insecure_security_policy",
+        module="dast",
+        finding=FindingEnum.F070,
+        developer=DeveloperEnum.LUIS_SAAVEDRA,
+        technique=TechniqueEnum.DAST,
+    )
     CFN_EC2_SEC_GROUPS_RFC1918 = MethodInfo(
         file_name="cloudformation",
         name="cfn_ec2_has_security_groups_ip_ranges_in_rfc1918",
