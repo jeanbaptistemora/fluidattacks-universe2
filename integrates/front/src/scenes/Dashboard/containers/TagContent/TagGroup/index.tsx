5c969d9cd5fded3531f23512195e3fa3d8545dde
Nicolas Carvajal <ncarvajal@fluidattacks.com>
2022-10-06 10:30:07 -0500

    integrates\refac(front): #6331 migrate tag groups table
    - migrate tag groups table

--- integrates/front/src/scenes/Dashboard/containers/TagContent/TagGroup/index.tsx
+++ integrates/front/src/scenes/Dashboard/containers/TagContent/TagGroup/index.tsx
@@ -6,15 +6,14 @@
 
 import { useQuery } from "@apollo/client";
 import type { ApolloError } from "@apollo/client";
+import type { ColumnDef, Row as TableRow } from "@tanstack/react-table";
 import type { GraphQLError } from "graphql";
 import _ from "lodash";
-import React, { useState } from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
 import { useHistory, useParams } from "react-router-dom";
 
-import { Table } from "components/Table";
-import type { IHeaderConfig } from "components/Table/types";
-import { filterSearchText } from "components/Table/utils";
+import { Table } from "components/TableNew";
 import { PORTFOLIO_GROUP_QUERY } from "scenes/Dashboard/containers/TagContent/TagGroup/queries";
 import { Row } from "styles/styledComponents";
 import { Logger } from "utils/logger";
@@ -40,54 +39,33 @@ const TagsGroup: React.FC = (): JSX.Element => {
     variables: { tag: tagName },
   });
   const { push } = useHistory();
-  const [searchTextFilter, setSearchTextFilter] = useState("");
 
-  function onSearchTextChange(
-    event: React.ChangeEvent<HTMLInputElement>
-  ): void {
-    setSearchTextFilter(event.target.value);
-  }
-
-  const tableHeaders: IHeaderConfig[] = [
-    { dataField: "name", header: "Group Name" },
-    { dataField: "description", header: "Description" },
+  const columns: ColumnDef<{ description: string; name: string }>[] = [
+    { accessorKey: "name", header: "Group Name" },
+    { accessorKey: "description", header: "Description" },
   ];
 
-  const handleRowTagClick: (
-    event: React.FormEvent<HTMLButtonElement>,
-    rowInfo: { name: string }
-  ) => void = (
-    _0: React.FormEvent<HTMLButtonElement>,
-    rowInfo: { name: string }
-  ): void => {
-    push(`/groups/${rowInfo.name.toLowerCase()}/analytics`);
-  };
+  function handleRowTagClick(
+    rowInfo: TableRow<{ description: string; name: string }>
+  ): (event: React.FormEvent) => void {
+    return (event: React.FormEvent): void => {
+      push(`/groups/${rowInfo.original.name.toLowerCase()}/analytics`);
+      event.preventDefault();
+    };
+  }
 
   if (_.isUndefined(data) || _.isEmpty(data)) {
     return <div />;
   }
-  const filterSearchTextDataset: IPortfolio["tag"]["groups"] = filterSearchText(
-    data.tag.groups,
-    searchTextFilter
-  );
 
   return (
     <div>
       <Row>
         <Table
-          customSearch={{
-            customSearchDefault: searchTextFilter,
-            isCustomSearchEnabled: true,
-            onUpdateCustomSearch: onSearchTextChange,
-            position: "right",
-          }}
-          dataset={filterSearchTextDataset}
-          exportCsv={false}
-          headers={tableHeaders}
+          columns={columns}
+          data={data.tag.groups}
           id={"tblGroupsTag"}
-          pageSize={10}
-          rowEvents={{ onClick: handleRowTagClick }}
-          search={false}
+          onRowClick={handleRowTagClick}
         />
       </Row>
     </div>
