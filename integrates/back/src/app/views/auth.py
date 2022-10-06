8355c9f9830d6f75e43d252c596dc7d932242245
Jaime Hurtado <jhurtado@fluidattacks.com>
2022-10-06 23:20:33 +0000

    integrates\fix(back): #7716 invited user info
    - fixed bug in user data when the user is invited to a
      group or organization and logged in for first time
      in the ARM

--- integrates/back/src/app/views/auth.py
+++ integrates/back/src/app/views/auth.py
@@ -70,6 +70,26 @@ from typing import (
 import uuid
 
 LOGGER = logging.getLogger(__name__)
+SUBSCRIPTIONS = [
+    "ACCESS_GRANTED",
+    "AGENT_TOKEN",
+    "CHARTS_REPORT",
+    "EVENT_REPORT",
+    "FILE_UPDATE",
+    "GROUP_INFORMATION",
+    "GROUP_REPORT",
+    "NEW_COMMENT",
+    "NEW_DRAFT",
+    "PORTFOLIO_UPDATE",
+    "REMEDIATE_FINDING",
+    "REMINDER_NOTIFICATION",
+    "ROOT_UPDATE",
+    "SERVICE_UPDATE",
+    "UNSUBSCRIPTION_ALERT",
+    "UPDATED_TREATMENT",
+    "VULNERABILITY_ASSIGNED",
+    "VULNERABILITY_REPORT",
+]
 
 
 @retry_on_exceptions(
@@ -172,6 +192,7 @@ async def autoenroll_stakeholder(
     first_name: str,
     last_name: str,
 ) -> None:
+    await analytics.mixpanel_track(email, "Register")
     await orgs_domain.add_without_group(
         email=email,
         role="user",
@@ -186,26 +207,28 @@ async def autoenroll_stakeholder(
             last_name=last_name,
             registration_date=today,
             notifications_preferences=NotificationsPreferences(
-                email=[
-                    "ACCESS_GRANTED",
-                    "AGENT_TOKEN",
-                    "CHARTS_REPORT",
-                    "EVENT_REPORT",
-                    "FILE_UPDATE",
-                    "GROUP_INFORMATION",
-                    "GROUP_REPORT",
-                    "NEW_COMMENT",
-                    "NEW_DRAFT",
-                    "PORTFOLIO_UPDATE",
-                    "REMEDIATE_FINDING",
-                    "REMINDER_NOTIFICATION",
-                    "ROOT_UPDATE",
-                    "SERVICE_UPDATE",
-                    "UNSUBSCRIPTION_ALERT",
-                    "UPDATED_TREATMENT",
-                    "VULNERABILITY_ASSIGNED",
-                    "VULNERABILITY_REPORT",
-                ]
+                email=SUBSCRIPTIONS
+            ),
+        ),
+    )
+
+
+async def invited_stakeholder(
+    email: str,
+    first_name: str,
+    last_name: str,
+) -> None:
+    await analytics.mixpanel_track(email, "Register")
+    today = datetime_utils.get_iso_date()
+    await stakeholders_model.update_metadata(
+        email=email,
+        metadata=StakeholderMetadataToUpdate(
+            first_name=first_name,
+            last_login_date=today,
+            last_name=last_name,
+            registration_date=today,
+            notifications_preferences=NotificationsPreferences(
+                email=SUBSCRIPTIONS
             ),
         ),
     )
@@ -215,13 +238,14 @@ async def log_stakeholder_in(
     loaders: Dataloaders, stakeholder: UserAccessInfo
 ) -> None:
     email = stakeholder.user_email.lower()
+    first_name = stakeholder.first_name[:29]
+    last_name = stakeholder.last_name[:29]
     if await stakeholders_domain.exists(loaders, email):
         stakeholder_in_db: Stakeholder = await loaders.stakeholder.load(email)
+        if not stakeholder_in_db.registration_date:
+            await invited_stakeholder(email, first_name, last_name)
         if not stakeholder_in_db.is_registered:
             await stakeholders_domain.register(email)
         await stakeholders_domain.update_last_login(email)
     else:
-        first_name = stakeholder.first_name[:29]
-        last_name = stakeholder.last_name[:29]
-        await analytics.mixpanel_track(email, "Register")
         await autoenroll_stakeholder(email, first_name, last_name)
