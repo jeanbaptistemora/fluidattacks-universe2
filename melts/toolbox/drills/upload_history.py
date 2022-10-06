330698a7aa1b9bd1aa77131fd0999e37b1537134
John Perez <jperez@fluidattacks.com>
2022-10-05 21:42:27 -0500

    melts\refac(back): #7036.14 continuous-repositories
    - Update all references to `continuous-repositories` in melts

--- melts/toolbox/drills/upload_history.py
+++ melts/toolbox/drills/upload_history.py
@@ -26,16 +26,17 @@ def main() -> None:
     1. Have their repos on s3
     2. Their repos were last uploaded at least 1 day ago
     """
-    bucket: str = "continuous-repositories"
+    bucket: str = "integrates"
+    bucket_path: str = "continuous-repositories"
     subs_names: List[str] = listdir("groups")
     table_format: str = "{:<25} {:<25}"
 
     print(table_format.format("GROUP", "DAYS SINCE LAST UPLOAD"))
     print("---------------------------------------------------")
     for subs in subs_names:
-        if drills_generic.s3_path_exists(bucket, f"{subs}/"):
+        if drills_generic.s3_path_exists(bucket, f"{bucket_path}/{subs}/"):
             last_upload_date: datetime = drills_generic.get_last_upload(
-                bucket, f"{subs}/"
+                bucket, f"{bucket_path}/{subs}/"
             )
             days: int = drills_generic.calculate_days_ago(last_upload_date)
             if days != 0:
