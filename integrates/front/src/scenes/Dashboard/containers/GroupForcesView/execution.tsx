a8da5874885314595da6ed6eb82003960c7fcabe
Jhon Romero <jromero@fluidattacks.com>
2022-10-07 23:15:35 +0000

    integrates\feat(front): #7600.11 execution filters
    - migrate execution table
    
    - no-deltas-test

--- integrates/front/src/scenes/Dashboard/containers/GroupForcesView/execution.tsx
+++ integrates/front/src/scenes/Dashboard/containers/GroupForcesView/execution.tsx
@@ -5,19 +5,15 @@
  */
 
 import { useQuery } from "@apollo/client";
+import type { ColumnDef } from "@tanstack/react-table";
 import _ from "lodash";
-import type { ReactElement } from "react";
-import React, { useCallback, useState } from "react";
+import React from "react";
 import ReactAnsi from "react-ansi";
-import { selectFilter } from "react-bootstrap-table2-filter";
 import { useTranslation } from "react-i18next";
 import { MemoryRouter, Route, Switch } from "react-router-dom";
 
-import { Table } from "components/Table";
-import type { IHeaderConfig } from "components/Table/types";
-import { filterSearchText } from "components/Table/utils";
+import { Table } from "components/TableNew";
 import { Tab, Tabs } from "components/Tabs";
-import { statusFormatter } from "scenes/Dashboard/components/Vulnerabilities/Formatter/index";
 import styles from "scenes/Dashboard/containers/GroupForcesView/index.css";
 import { GET_FORCES_EXECUTION } from "scenes/Dashboard/containers/GroupForcesView/queries";
 import type {
@@ -25,10 +21,8 @@ import type {
   IExploitResult,
   IFoundVulnerabilities,
   IGetForcesExecution,
-  IVulnerabilities,
 } from "scenes/Dashboard/containers/GroupForcesView/types";
 import { Col33, Row, TabContent } from "styles/styledComponents";
-import { useStoredState } from "utils/hooks";
 
 const Execution: React.FC<IExecution> = (
   props: Readonly<IExecution>
@@ -36,12 +30,6 @@ const Execution: React.FC<IExecution> = (
   const { t } = useTranslation();
   const { log, executionId, groupName } = props;
   const isOld: boolean = log !== undefined;
-
-  const [isFilterEnabled, setFilterEnabled] = useStoredState<boolean>(
-    "forcesExecutionFilters",
-    false
-  );
-  const [searchTextFilter, setSearchTextFilter] = useState("");
 
   const { loading, data } = useQuery<IGetForcesExecution>(
     GET_FORCES_EXECUTION,
@@ -54,45 +42,13 @@ const Execution: React.FC<IExecution> = (
     }
   );
 
-  const handleUpdateFilter: () => void = useCallback((): void => {
-    setFilterEnabled(!isFilterEnabled);
-  }, [isFilterEnabled, setFilterEnabled]);
-
   if (loading && !isOld) {
     return <p>{"Loading ..."}</p>;
   }
 
-  const selectOptionsStatus = { Secure: "Secure", Vulnerable: "Vulnerable" };
-  const selectOptionsKind = { DAST: "DAST", SAST: "SAST" };
-  const selectOptionsExploitability = {
-    Functional: "Functional",
-    High: "High",
-    "Proof of concept": "Proof of concept",
-    Unproven: "Unproven",
-  };
-
   const execution: IExecution = isOld
     ? props
     : { ...props, ...data?.forcesExecution };
-
-  const onFilterStatus: (filterVal: string) => void = (
-    filterVal: string
-  ): void => {
-    sessionStorage.setItem("statusExecutionFilter", filterVal);
-  };
-  const onFilterKind: (filterVal: string) => void = (
-    filterVal: string
-  ): void => {
-    sessionStorage.setItem("kindExecutionFilter", filterVal);
-  };
-  const onFilterExploitability: (filterVal: string) => void = (
-    filterVal: string
-  ): void => {
-    sessionStorage.setItem("exploitabilityForcesFilter", filterVal);
-  };
-  const formatText: (text: string) => ReactElement<Text> = (
-    text: string
-  ): ReactElement<Text> => <p className={styles.wrapped}>{text}</p>;
 
   const stateResolve: (status: string) => string = (status: string): string => {
     switch (status) {
@@ -127,85 +83,54 @@ const Execution: React.FC<IExecution> = (
     return `${openStr}, ${acceptedStr}, ${closedStr}, ${totalStr}`;
   };
 
-  const getDatasetFromVulnerabilities: (
-    vulnerabilities: IVulnerabilities | null
-  ) => Record<string, unknown>[] = (
-    vulnerabilities: IVulnerabilities | null
-  ): Record<string, unknown>[] => {
-    const vulns: IExploitResult[] = _.isNull(vulnerabilities)
-      ? []
-      : vulnerabilities.open.concat(
-          vulnerabilities.closed.concat(vulnerabilities.accepted)
-        );
+  const vulns: IExploitResult[] = _.isNull(execution.vulnerabilities)
+    ? []
+    : execution.vulnerabilities.open.concat(
+        execution.vulnerabilities.closed.concat(
+          execution.vulnerabilities.accepted
+        )
+      );
 
-    return vulns.map(
-      (elem: IExploitResult): Record<string, unknown> => ({
-        ...elem,
-        state: stateResolve(elem.state),
-      })
-    );
-  };
+  const datset = vulns.map(
+    (elem: IExploitResult): IExploitResult => ({
+      ...elem,
+      state: stateResolve(elem.state),
+    })
+  );
 
-  const headersCompromisedToeTable: IHeaderConfig[] = [
+  const columns: ColumnDef<IExploitResult>[] = [
     {
-      dataField: "exploitability",
-      filter: selectFilter({
-        defaultValue: _.get(sessionStorage, "exploitabilityForcesFilter"),
-        onFilter: onFilterExploitability,
-        options: selectOptionsExploitability,
-      }),
-      formatter: formatText,
+      accessorFn: (row): number => row.exploitability,
       header: t("group.forces.compromisedToe.exploitability"),
-      width: "15%",
-      wrapped: true,
+      id: "explotability",
+      meta: { filterType: "select" },
     },
     {
-      dataField: "state",
-      filter: selectFilter({
-        defaultValue: _.get(sessionStorage, "statusExecutionFilter"),
-        onFilter: onFilterStatus,
-        options: selectOptionsStatus,
-      }),
-      formatter: statusFormatter,
+      accessorFn: (row): string => row.state,
       header: t("group.forces.compromisedToe.status"),
-      width: "105px",
-      wrapped: true,
+      id: "state",
+      meta: { filterType: "select" },
     },
     {
-      dataField: "kind",
-      filter: selectFilter({
-        defaultValue: _.get(sessionStorage, "kindExecutionFilter"),
-        onFilter: onFilterKind,
-        options: selectOptionsKind,
-      }),
-      formatter: formatText,
+      accessorFn: (row): string => row.kind,
+
       header: t("group.forces.compromisedToe.type"),
-      width: "10%",
-      wrapped: true,
+      id: "kind",
+      meta: { filterType: "select" },
     },
     {
-      dataField: "who",
-      formatter: formatText,
+      accessorFn: (row): string => row.who,
       header: t("group.forces.compromisedToe.what"),
-      wrapped: true,
+      id: "who",
+      meta: { filterType: "select" },
     },
     {
-      dataField: "where",
-      formatter: formatText,
+      accessorFn: (row): string => row.where,
       header: t("group.forces.compromisedToe.where"),
-      wrapped: true,
+      id: "where",
+      meta: { filterType: "select" },
     },
   ];
-
-  const filterSearchtextResult: Record<string, unknown>[] = filterSearchText(
-    getDatasetFromVulnerabilities(execution.vulnerabilities),
-    searchTextFilter
-  );
-  function onSearchTextChange(
-    event: React.ChangeEvent<HTMLInputElement>
-  ): void {
-    setSearchTextFilter(event.target.value);
-  }
 
   return (
     <div>
@@ -333,20 +258,12 @@ const Execution: React.FC<IExecution> = (
             <Route path={"/summary"}>
               <Table
                 columnToggle={true}
-                customSearch={{
-                  customSearchDefault: searchTextFilter,
-                  isCustomSearchEnabled: true,
-                  onUpdateCustomSearch: onSearchTextChange,
-                  position: "right",
-                }}
-                dataset={filterSearchtextResult}
+                columns={columns}
+                data={datset}
+                enableColumnFilters={true}
+                enableSearchBar={true}
                 exportCsv={false}
-                headers={headersCompromisedToeTable}
                 id={"tblCompromisedToe"}
-                isFilterEnabled={isFilterEnabled}
-                onUpdateEnableFilter={handleUpdateFilter}
-                pageSize={100}
-                search={false}
               />
             </Route>
             <Route path={"/log"}>
