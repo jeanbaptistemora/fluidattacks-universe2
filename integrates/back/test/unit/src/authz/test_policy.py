5083526bff50d52922cb406c4249b4d25a6bc80c
Carlos Betancur <cbetancur@fluidattacks.com>
2022-10-07 11:13:00 -0500

    integrates\test(back): #4944.51 mock db calls
    - modify test to mock dynamodb calls
    - run test using parameters
    - rename functions to be more clear

--- integrates/back/test/unit/src/authz/test_policy.py
+++ integrates/back/test/unit/src/authz/test_policy.py
@@ -65,14 +65,16 @@ def test_create_tables(
 async def test_get_user_level_role(
     email: str, result: str, dynamo_resource: ServiceResource
 ) -> None:
-    def side_effect(**kwargs: Any) -> Any:
+    def mock_batch_get_item(**kwargs: Any) -> Any:
         return dynamo_resource.batch_get_item(**kwargs)
 
     loaders: Dataloaders = get_new_context()
     with mock.patch(
         "dynamodb.operations.get_resource", new_callable=mock.AsyncMock
     ) as mock_resource:
-        mock_resource.return_value.batch_get_item.side_effect = side_effect
+        mock_resource.return_value.batch_get_item.side_effect = (
+            mock_batch_get_item
+        )
         user_level_role = await get_user_level_role(loaders, email)
     assert mock_resource.called is True
     assert user_level_role == result
@@ -100,7 +102,7 @@ async def test_get_group_service_policies(
     result: list,
     dynamo_resource: ServiceResource,
 ) -> None:
-    def side_effect(**kwargs: Any) -> Any:
+    def mock_batch_get_item(**kwargs: Any) -> Any:
         table_name = "integrates_vms"
         return dynamo_resource.Table(table_name).query(**kwargs)
 
@@ -108,7 +110,9 @@ async def test_get_group_service_policies(
     with mock.patch(
         "dynamodb.operations.get_table_resource", new_callable=mock.AsyncMock
     ) as mock_table_resource:
-        mock_table_resource.return_value.query.side_effect = side_effect
+        mock_table_resource.return_value.query.side_effect = (
+            mock_batch_get_item
+        )
         group_policies = get_group_service_policies(
             await loaders.group.load(group)
         )
@@ -131,43 +135,63 @@ async def test_get_group_level_role(
     group: str,
     result: str,
 ) -> None:
-    def side_effect(**kwargs: Any) -> Any:
+    def mock_batch_get_item(**kwargs: Any) -> Any:
         return dynamo_resource.batch_get_item(**kwargs)
 
     loaders: Dataloaders = get_new_context()
     with mock.patch(
         "dynamodb.operations.get_resource", new_callable=mock.AsyncMock
     ) as mock_resource:
-        mock_resource.return_value.batch_get_item.side_effect = side_effect
+        mock_resource.return_value.batch_get_item.side_effect = (
+            mock_batch_get_item
+        )
         test_role = await get_group_level_role(loaders, email, group)
         assert mock_resource.called is True
         assert test_role == result
 
 
+@pytest.mark.parametrize(
+    ["email", "formatted_email", "role"],
+    [
+        ["TEST_EMAIL@TEST.COM", "test_email@test.com", "user"],
+        ["TEST_EmAiL@tEsT.com", "test_email@test.com", "admin"],
+    ],
+)
 @pytest.mark.changes_db
-async def test_grant_user_level_role() -> None:
-    await grant_user_level_role("..TEST@gmail.com", "user")
-    assert (
-        await get_user_level_role(get_new_context(), "..test@gmail.com")
-        == "user"
-    )
-    assert (
-        await get_user_level_role(get_new_context(), "..tEst@gmail.com")
-        == "user"
-    )
-    await grant_user_level_role("..TEST@gmail.com", "admin")
-    assert (
-        await get_user_level_role(get_new_context(), "..test@gmail.com")
-        == "admin"
-    )
-    assert (
-        await get_group_level_role(
-            get_new_context(), "..tEst@gmail.com", "a-group"
+async def test_grant_user_level_role(
+    dynamo_resource: ServiceResource,
+    email: str,
+    formatted_email: str,
+    role: str,
+) -> None:
+    def mock_update_item(**kwargs: Any) -> Any:
+        table_name = "integrates_vms"
+        return dynamo_resource.Table(table_name).update_item(**kwargs)
+
+    def mock_batch_get_item(**kwargs: Any) -> Any:
+        return dynamo_resource.batch_get_item(**kwargs)
+
+    with mock.patch(
+        "dynamodb.operations.get_table_resource", new_callable=mock.AsyncMock
+    ) as mock_table_resource:
+        mock_table_resource.return_value.update_item.side_effect = (
+            mock_update_item
         )
-        == "admin"
-    )
+        await grant_user_level_role(email, role)
+
+    with mock.patch(
+        "dynamodb.operations.get_resource", new_callable=mock.AsyncMock
+    ) as mock_resource:
+        mock_resource.return_value.batch_get_item.side_effect = (
+            mock_batch_get_item
+        )
+        assert (
+            await get_user_level_role(get_new_context(), formatted_email)
+            == role
+        )
+    assert mock_table_resource.called is True
     with pytest.raises(ValueError) as test_raised_err:
-        await grant_user_level_role("..TEST@gmail.com", "breakall")
+        await grant_user_level_role(email, "breakall")
     assert str(test_raised_err.value) == "Invalid role value: breakall"
 
 
