0503e2c4a4e5e14258f2d9b0edac5e1015439fa0
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-10-07 12:07:19 -0500

    skims\feat(back): #6769.49 add method to model
    - add method dast F099 to model

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -1905,6 +1905,14 @@ class MethodsEnum(Enum):
         developer=DeveloperEnum.LUIS_SAAVEDRA,
         technique=TechniqueEnum.DAST,
     )
+    AWS_UNENCRYPTED_BUCKETS = MethodInfo(
+        file_name="aws",
+        name="unencrypted_buckets",
+        module="dast",
+        finding=FindingEnum.F099,
+        developer=DeveloperEnum.LUIS_SAAVEDRA,
+        technique=TechniqueEnum.DAST,
+    )
     CFN_EC2_SEC_GROUPS_RFC1918 = MethodInfo(
         file_name="cloudformation",
         name="cfn_ec2_has_security_groups_ip_ranges_in_rfc1918",
