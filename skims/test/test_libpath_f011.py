05cfd030de6ec5e29e9c1338ea351ea2d95cc403
Lewis Contreras <lcontreras@fluidattacks.com>
2022-10-10 11:05:37 -0500

    skims\feat(back): #6796.32 add test gem f393
    - Add test for gemfile f393 method

--- skims/test/test_libpath_f011.py
+++ skims/test/test_libpath_f011.py
@@ -9,6 +9,9 @@ from lib_path.f011.gem import (
     gem_gemfile,
     gem_gemfile_lock,
 )
+from lib_path.f393.gem import (
+    gem_gemfile_dev,
+)
 from operator import (
     itemgetter,
 )
@@ -18,6 +21,7 @@ from typing import (
     Iterator,
     List,
     Pattern,
+    Tuple,
 )
 
 
@@ -97,3 +101,37 @@ def test_gem_gemfile_lock() -> None:
                     break
 
         assert assertion
+
+
+@pytest.mark.skims_test_group("unittesting")
+def test_gem_gemfile_dev() -> None:
+    path: str = "skims/test/data/lib_path/f011/Gemfile"
+
+    with open(
+        path,
+        mode="r",
+        encoding="latin-1",
+    ) as file_handle:
+        file_contents: str = file_handle.read(-1)
+        gem_gemfile_unwrapped = gem_gemfile_dev.__wrapped__  # type: ignore
+
+    expected: Tuple[Tuple[str, str, int], ...] = (
+        ("minitest-bisect", "1.0.0", 118),
+        ("stackprof", "1.4.2", 123),
+        ("devise", "1.4.2", 132),
+        ("rubocop", "1.35.1", 133),
+        ("pry", "1.0.1", 145),
+    )
+    reports: Iterator[DependencyType] = gem_gemfile_unwrapped(
+        file_contents, path
+    )
+    assertion: bool = True
+    for num, report in enumerate(reports):
+        product = report[0].get("item")
+        line = report[0].get("line")
+        version = report[1].get("item")
+        if (product, version, line) != expected[num]:
+            assertion = not assertion
+            break
+
+    assert assertion
