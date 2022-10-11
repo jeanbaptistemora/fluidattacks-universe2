9270de448c4bd8c86696c5d73356465bf32f0ab0
Andres Aguirre <aaguirre@fluidattacks.com>
2022-10-11 20:04:15 +0000

    integrates\feat(front): #7337.72 number of unf standards
    - Add the number of unfulfilled standards by group

--- integrates/front/src/scenes/Dashboard/containers/OrganizationComplianceStandardsView copy/index.tsx
+++ integrates/front/src/scenes/Dashboard/containers/OrganizationComplianceStandardsView copy/index.tsx
@@ -9,12 +9,14 @@ import { useQuery } from "@apollo/client";
 import { Formik } from "formik";
 import type { GraphQLError } from "graphql";
 import _ from "lodash";
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 
+import { GET_GROUP_UNFULFILLED_STANDARDS } from "./queries";
 import type {
   IGroupAttr,
   IOrganizationComplianceStandardsProps,
+  IUnfulfilledStandardAttr,
 } from "./types";
 
 import { Select } from "components/Input";
@@ -29,6 +31,11 @@ const OrganizationComplianceStandardsView: React.FC<IOrganizationComplianceStand
   (props: IOrganizationComplianceStandardsProps): JSX.Element => {
     const { organizationId } = props;
     const { t } = useTranslation();
+
+    // Handle state
+    const [selectedGroupName, setSelectedGroupName] = useState<
+      string | undefined
+    >(undefined);
 
     // GraphQL queries
     const { data: groupsData } = useQuery<{
@@ -47,10 +54,48 @@ const OrganizationComplianceStandardsView: React.FC<IOrganizationComplianceStand
         organizationId,
       },
     });
-    const groups = _.isUndefined(groupsData)
-      ? []
-      : groupsData.organization.groups;
+    const { data: unfulfilledStandardsData } = useQuery<{
+      group: {
+        compliance: { unfulfilledStandards: IUnfulfilledStandardAttr[] };
+      };
+    }>(GET_GROUP_UNFULFILLED_STANDARDS, {
+      onError: ({ graphQLErrors }: ApolloError): void => {
+        graphQLErrors.forEach((error: GraphQLError): void => {
+          msgError(t("groupAlerts.errorTextsad"));
+          Logger.warning(
+            "An error occurred loading group unfulfilled standards",
+            error
+          );
+        });
+      },
+      skip: _.isUndefined(selectedGroupName) || _.isEmpty(selectedGroupName),
+      variables: {
+        groupName: selectedGroupName,
+      },
+    });
 
+    // Format data
+    const groups = useMemo(
+      (): IGroupAttr[] =>
+        _.isUndefined(groupsData) ? [] : groupsData.organization.groups,
+      [groupsData]
+    );
+    const unfulfilledStandards = _.isUndefined(unfulfilledStandardsData)
+      ? []
+      : unfulfilledStandardsData.group.compliance.unfulfilledStandards;
+
+    // Handle effects
+    useEffect((): void => {
+      if (_.isUndefined(selectedGroupName) && groups.length > 0) {
+        setSelectedGroupName(groups[0].name);
+      }
+    }, [groups, selectedGroupName]);
+
+    // Handle actions
+    function onGroupChange(event: React.ChangeEvent<HTMLSelectElement>): void {
+      event.preventDefault();
+      setSelectedGroupName(event.target.value);
+    }
     function onSubmit(): void {
       // OnSubmit
     }
@@ -61,22 +106,25 @@ const OrganizationComplianceStandardsView: React.FC<IOrganizationComplianceStand
           <Col lg={50} md={50} sm={50}>
             <Formik
               initialValues={{
-                groupName: "",
+                groupName: selectedGroupName,
               }}
               name={"selectGroup"}
               onSubmit={onSubmit}
             >
-              <div className={"flex flex-row  justify-start"}>
+              <div className={"flex flex-row  justify-start items-end "}>
                 <div>
                   <Text disp={"inline"} fw={7} mb={3} mt={2} size={"big"}>
                     {t(
                       "organization.tabs.compliance.tabs.standards.unfulfilledStandards.title"
                     )}
+                    {_.isUndefined(unfulfilledStandardsData)
+                      ? undefined
+                      : ` (${unfulfilledStandards.length})`}
                   </Text>
                 </div>
                 &emsp;
                 <div>
-                  <Select name={"groupName"}>
+                  <Select name={"groupName"} onChange={onGroupChange}>
                     {groups.map(
                       (group: IGroupAttr): JSX.Element => (
                         <option key={group.name} value={group.name}>
