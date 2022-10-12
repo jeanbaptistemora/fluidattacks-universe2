69f53ca31905a2459730b7c54c8fc6b9eeec0d2b
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-10-12 13:59:57 -0500

    skims\feat(back): #6769.64 add method to model
    - add new method for f246 to model

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -1952,6 +1952,14 @@ class MethodsEnum(Enum):
         developer=DeveloperEnum.LUIS_SAAVEDRA,
         technique=TechniqueEnum.DAST,
     )
+    AWS_RDS_HAS_UNENCRYPTED_STORAGE = MethodInfo(
+        file_name="aws",
+        name="rds_has_unencrypted_storage",
+        module="dast",
+        finding=FindingEnum.F246,
+        developer=DeveloperEnum.LUIS_SAAVEDRA,
+        technique=TechniqueEnum.DAST,
+    )
     CFN_EC2_SEC_GROUPS_RFC1918 = MethodInfo(
         file_name="cloudformation",
         name="cfn_ec2_has_security_groups_ip_ranges_in_rfc1918",
