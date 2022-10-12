e8a8ac5239658ca03dc44c651982d71cebbfe83d
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-10-12 10:58:00 -0500

    skims\feat(back): #6769.61 add method to model
    - Add method dast to model f203

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -1944,6 +1944,14 @@ class MethodsEnum(Enum):
         developer=DeveloperEnum.LUIS_SAAVEDRA,
         technique=TechniqueEnum.DAST,
     )
+    AWS_ACL_PUBLIC_BUCKETS = MethodInfo(
+        file_name="aws",
+        name="acl_public_buckets",
+        module="dast",
+        finding=FindingEnum.F203,
+        developer=DeveloperEnum.LUIS_SAAVEDRA,
+        technique=TechniqueEnum.DAST,
+    )
     CFN_EC2_SEC_GROUPS_RFC1918 = MethodInfo(
         file_name="cloudformation",
         name="cfn_ec2_has_security_groups_ip_ranges_in_rfc1918",
