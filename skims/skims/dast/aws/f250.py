d0c98024581b27420cb70a0f6c8c4c6fe8646d20
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-10-13 13:46:49 -0500

    skims\feat(back): #6769.67 add method
    - add new method dast for f250
    - no-deltas-test

---
+++ skims/skims/dast/aws/f250.py
@@ -0,0 +1,69 @@
+# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from dast.aws.types import (
+    Location,
+)
+from dast.aws.utils import (
+    build_vulnerabilities,
+    run_boto3_fun,
+)
+from model import (
+    core_model,
+)
+from model.core_model import (
+    AwsCredentials,
+    Vulnerability,
+)
+from typing import (
+    Any,
+    Callable,
+    Coroutine,
+    Dict,
+    List,
+    Tuple,
+)
+from zone import (
+    t,
+)
+
+
+async def ebs_is_encryption_disabled(
+    credentials: AwsCredentials,
+) -> core_model.Vulnerabilities:
+    response: Dict[str, Any] = await run_boto3_fun(
+        credentials, service="ec2", function="describe_volumes"
+    )
+    volumes = response.get("Volumes", []) if response else []
+    method = core_model.MethodsEnum.AWS_EBS_IS_ENCRYPTION_DISABLED
+    vulns: core_model.Vulnerabilities = ()
+    if volumes:
+        for volume in volumes:
+            locations: List[Location] = []
+            if not volume.get("Encrypted"):
+                locations = [
+                    Location(
+                        access_patterns=("/Encrypted",),
+                        arn=(f"arn:aws:ec2:::VolumeId/{volume['VolumeId']}"),
+                        values=(volume.get("Encrypted"),),
+                        description=t(
+                            "src.lib_path.f250.ec2_has_unencrypted_volumes"
+                        ),
+                    )
+                ]
+            vulns = (
+                *vulns,
+                *build_vulnerabilities(
+                    locations=locations,
+                    method=(method),
+                    aws_response=volume,
+                ),
+            )
+    return vulns
+
+
+CHECKS: Tuple[
+    Callable[[AwsCredentials], Coroutine[Any, Any, Tuple[Vulnerability, ...]]],
+    ...,
+] = (ebs_is_encryption_disabled,)
