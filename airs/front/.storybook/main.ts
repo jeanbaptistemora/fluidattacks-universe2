452f29196f77a476233bf265a5062c99d0e51ec4
Frank Aristizabal <faristizabal@fluidattacks.com>
2022-10-13 18:08:12 -0500

    airs\refac(front): #7727 change styles
    - Add storybook config.

---
+++ airs/front/.storybook/main.ts
@@ -0,0 +1,22 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+/* eslint-disable react/forbid-component-props */
+import { StorybookConfig } from "@storybook/core-common";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+};
+
+export = config;
---
+++ airs/front/.storybook/preview.ts
@@ -0,0 +1,19 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+/* eslint-disable react/forbid-component-props */
+import { Parameters } from "@storybook/api";
+
+const parameters: Parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export { parameters };
