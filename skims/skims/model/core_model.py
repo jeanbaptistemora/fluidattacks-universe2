2d43b83dd2adc0e1019806366614e7881d74b753
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-10-14 11:34:46 -0500

    skims\feat(back): #6769.68 add method to model
    - add new method dast for f256 to the model

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -1993,6 +1993,14 @@ class MethodsEnum(Enum):
         developer=DeveloperEnum.LUIS_SAAVEDRA,
         technique=TechniqueEnum.DAST,
     )
+    AWS_RDS_HAS_NOT_AUTOMATED_BACKUPS = MethodInfo(
+        file_name="aws",
+        name="rds_has_not_automated_backups",
+        module="dast",
+        finding=FindingEnum.F256,
+        developer=DeveloperEnum.LUIS_SAAVEDRA,
+        technique=TechniqueEnum.DAST,
+    )
     CFN_EC2_SEC_GROUPS_RFC1918 = MethodInfo(
         file_name="cloudformation",
         name="cfn_ec2_has_security_groups_ip_ranges_in_rfc1918",
