e058181879bdf7ebcb605bb8ddcffa34cb0faa52
Jonathan Mesa <jmesa@fluidattacks.com>
2022-10-18 19:19:17 +0000

    integrates\feat(back): #7329 archive toe inputs
    - start archiving toe inputs directly from back
    - this sync as previous to the migration of deleted groups

--- integrates/back/src/db_model/toe_inputs/get.py
+++ integrates/back/src/db_model/toe_inputs/get.py
@@ -39,10 +39,39 @@ from dynamodb.exceptions import (
 from dynamodb.model import (
     TABLE,
 )
+from dynamodb.types import (
+    Item,
+)
 from typing import (
     List,
     Tuple,
 )
+
+
+async def get_toe_input_item(request: ToeInputRequest) -> Item:
+    primary_key = keys.build_key(
+        facet=TABLE.facets["toe_input_metadata"],
+        values={
+            "component": request.component,
+            "entry_point": request.entry_point,
+            "group_name": request.group_name,
+            "root_id": request.root_id,
+        },
+    )
+    key_structure = TABLE.primary_key
+    response = await operations.query(
+        condition_expression=(
+            Key(key_structure.partition_key).eq(primary_key.partition_key)
+            & Key(key_structure.sort_key).eq(primary_key.sort_key)
+        ),
+        facets=(TABLE.facets["toe_input_metadata"],),
+        table=TABLE,
+        limit=1,
+    )
+    if not response.items:
+        raise ToeInputNotFound()
+
+    return {**response.items[0], "group_name": request.group_name}
 
 
 async def _get_toe_input(request: ToeInputRequest) -> ToeInput:
