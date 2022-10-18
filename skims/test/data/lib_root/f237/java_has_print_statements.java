81834e06afb28cdb114e5cdd56abd382c5dbae3f
Luis Patino <lpatino@fluidattacks.com>
2022-10-18 11:35:15 -0500

    skims\feat(back): #5154.15 add f237 method
    - Tests for F237 method.

---
+++ skims/test/data/lib_root/f237/java_has_print_statements.java
@@ -0,0 +1,20 @@
+import System.out;
+
+class printer {
+    public static void main(String[] args) {
+        String message = "Variable";
+        System.out.print("String Literal");
+        out.print(message);
+        print("Have" + message + "concatenation");
+        System.out.print();
+        System.out.print("");
+    }
+    public static void print(String[] args) {
+        String message = "Variable";
+        System.out.println("String Literal");
+        out.println(message);
+        println("Have" + message + "concatenation");
+        System.out.println();
+        System.out.println("");
+    }
+}
