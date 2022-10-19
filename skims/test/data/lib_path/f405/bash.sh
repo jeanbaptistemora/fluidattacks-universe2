20911cc081ade2b63c4215b24d1e84fdf0dd606c
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-10-19 11:05:21 -0500

    skims\feat(back): #7662.6 add f405 to analyze
    - add method f405 to analyze
    - update results file
    - add test file
    - no-deltas-test

---
+++ skims/test/data/lib_path/f405/bash.sh
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+# shellcheck shell=bash
+
+chmod 750 myfile
+cp text.txt ground.txt
+chmod 754 myfile
