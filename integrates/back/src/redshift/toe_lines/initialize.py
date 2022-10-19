b7ab63338304ffe241fb435b4c23444e0ccefeb3
Jonathan Mesa <jmesa@fluidattacks.com>
2022-10-19 01:04:50 +0000

    integrates\feat(back): #7329 archive toe lines
    - support archiving toe lines directly from the back
    - this prior to running the needed migration
    - no-deltas-test

---
+++ integrates/back/src/redshift/toe_lines/initialize.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from ..operations import (
+    SCHEMA_NAME,
+)
+
+METADATA_TABLE: str = f"{SCHEMA_NAME}.toe_lines_metadata"
---
+++ integrates/back/src/redshift/toe_lines/types.py
@@ -0,0 +1,33 @@
+# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from dataclasses import (
+    dataclass,
+)
+from datetime import (
+    datetime,
+)
+from typing import (
+    Optional,
+)
+
+
+@dataclass(frozen=True)
+class MetadataTableRow:
+    # pylint: disable=invalid-name,too-many-instance-attributes
+    id: str
+    attacked_at: Optional[datetime]
+    attacked_by: str
+    attacked_lines: int
+    be_present: bool
+    be_present_until: Optional[datetime]
+    first_attack_at: Optional[datetime]
+    group_name: str
+    has_vulnerabilities: bool
+    loc: int
+    modified_date: datetime
+    root_id: str
+    seen_at: datetime
+    seen_first_time_by: Optional[str]
+    sorts_risk_level: int
