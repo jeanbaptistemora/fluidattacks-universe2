d97bb20db3c886dbb1d8b8b59cb307193500b6ee
Lewis Contreras <lcontreras@fluidattacks.com>
2022-10-20 13:46:47 -0500

    skims\feat(back): #7847.4 add go resolver f011
    - Add go.mod dependencies resolver for f011

---
+++ skims/skims/lib_path/f011/go.py
@@ -0,0 +1,32 @@
+# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from lib_path.common import (
+    DependencyType,
+    format_pkg_dep,
+    pkg_deps_to_vulns,
+)
+from model.core_model import (
+    MethodsEnum,
+    Platform,
+)
+import re
+from typing import (
+    Iterator,
+    Pattern,
+)
+
+GO_DEP: Pattern[str] = re.compile(
+    r"\s+.+/(?P<product>[\w\-\.~]+)(/v.+)?\sv(?P<version>\S+)"
+)
+
+
+# pylint: disable=unused-argument
+@pkg_deps_to_vulns(Platform.GO, MethodsEnum.GO_MOD)
+def go_mod(content: str, path: str) -> Iterator[DependencyType]:
+    for line_number, line in enumerate(content.splitlines(), 1):
+        if matched := re.search(GO_DEP, line):
+            product = matched.group("product")
+            version = matched.group("version")
+            yield format_pkg_dep(product, version, line_number, line_number)
