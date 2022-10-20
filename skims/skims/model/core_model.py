afd42e3994ef8f089be13649a154dfee25c4295b
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-10-20 10:49:55 -0500

    skims\feat(back): #6769.74 add new method to the model
    - Add new method to the model for f257

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -2054,6 +2054,14 @@ class MethodsEnum(Enum):
         developer=DeveloperEnum.LUIS_SAAVEDRA,
         technique=TechniqueEnum.DAST,
     )
+    AWS_EC2_HAS_NOT_TERMINATION_PROTECTION = MethodInfo(
+        file_name="aws",
+        name="ec2_has_not_termination_protection",
+        module="dast",
+        finding=FindingEnum.F257,
+        developer=DeveloperEnum.LUIS_SAAVEDRA,
+        technique=TechniqueEnum.DAST,
+    )
     CFN_EC2_SEC_GROUPS_RFC1918 = MethodInfo(
         file_name="cloudformation",
         name="cfn_ec2_has_security_groups_ip_ranges_in_rfc1918",
