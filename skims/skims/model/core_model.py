b867887d05ae47c3725bbd64a62d739353dd3d9e
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-10-20 13:12:06 -0500

    skims\feat(back): #6769.75 add new method to model
    - Add new method for f258 to the model
    - Speed: 1 methods / 1 days = 1 method/day
    - TODO: 26 methods
    - ETA: 26 / 1 = 26 days

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -2062,6 +2062,14 @@ class MethodsEnum(Enum):
         developer=DeveloperEnum.LUIS_SAAVEDRA,
         technique=TechniqueEnum.DAST,
     )
+    AWS_ELB2_HAS_NOT_DELETION_PROTECTION = MethodInfo(
+        file_name="aws",
+        name="elb2_has_not_deletion_protection",
+        module="dast",
+        finding=FindingEnum.F258,
+        developer=DeveloperEnum.LUIS_SAAVEDRA,
+        technique=TechniqueEnum.DAST,
+    )
     CFN_EC2_SEC_GROUPS_RFC1918 = MethodInfo(
         file_name="cloudformation",
         name="cfn_ec2_has_security_groups_ip_ranges_in_rfc1918",
