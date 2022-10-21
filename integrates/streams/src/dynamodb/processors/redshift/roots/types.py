d14cd0d277e5cbeafd30aaefd5523e19473ba153
Jonathan Mesa <jmesa@fluidattacks.com>
2022-10-21 19:35:17 +0000

    integrates\feat(back): #7329 redshift roots
    - add types definitions for roots

---
+++ integrates/streams/src/dynamodb/processors/redshift/roots/types.py
@@ -0,0 +1,40 @@
+# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from dataclasses import (
+    dataclass,
+)
+from datetime import (
+    datetime,
+)
+from typing import (
+    Optional,
+)
+
+
+@dataclass(frozen=True)
+class MetadataTableRow:
+    # pylint: disable=invalid-name
+    id: str
+    created_date: str
+    group_name: str
+    organization_name: str
+    type: str
+
+
+@dataclass(frozen=True)
+class EnvironmentUrlTableRow:
+    # pylint: disable=invalid-name
+    id: str
+    cloud_name: Optional[str]
+    created_at: datetime
+    url_type: Optional[str]
+
+
+@dataclass(frozen=True)
+class RootCodeLanguagesTableRow:
+    # pylint: disable=invalid-name
+    id: str
+    language: str
+    loc: int
