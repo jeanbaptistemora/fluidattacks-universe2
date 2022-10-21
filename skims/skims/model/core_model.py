b45ccbe6470398787d7a604d88fb04d6bfb6a665
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-10-21 10:16:23 -0500

    skims\feat(back): #6769.76 add new method to the model
    - Add new method for f259 to the model
    - Speed: 2 methods / 1 days = 2 method/day
    - TODO: 25 methods
    - ETA: 25 / 2 = 12.5 days

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -2092,6 +2092,14 @@ class MethodsEnum(Enum):
         developer=DeveloperEnum.LUIS_SAAVEDRA,
         technique=TechniqueEnum.DAST,
     )
+    AWS_DYNAMODB_HAS_NOT_POINT_IN_TIME_RECOVERY = MethodInfo(
+        file_name="aws",
+        name="dynamodb_has_not_point_in_time_recovery",
+        module="dast",
+        finding=FindingEnum.F259,
+        developer=DeveloperEnum.LUIS_SAAVEDRA,
+        technique=TechniqueEnum.DAST,
+    )
     CFN_EC2_SEC_GROUPS_RFC1918 = MethodInfo(
         file_name="cloudformation",
         name="cfn_ec2_has_security_groups_ip_ranges_in_rfc1918",
