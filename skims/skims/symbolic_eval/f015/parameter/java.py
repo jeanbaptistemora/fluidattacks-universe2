f5a303e67d1716fe9eb10b2e8cb4c8172cb2358d
Luis Patino <lpatino@fluidattacks.com>
2022-10-24 13:43:08 -0500

    skims\feat(back): #5154.20 add f015 method
    - Symbolic eval files for f015.
    - no-deltas-test

---
+++ skims/skims/symbolic_eval/f015/parameter/java.py
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from symbolic_eval.types import (
+    SymbolicEvalArgs,
+    SymbolicEvaluation,
+)
+
+
+def java_insecure_authentication(args: SymbolicEvalArgs) -> SymbolicEvaluation:
+    if args.graph.nodes[args.n_id]["variable_type"] == "HttpHeaders":
+        args.evaluation[args.n_id] = True
+        args.triggers.add("Parameter")
+    return SymbolicEvaluation(args.evaluation[args.n_id], args.triggers)
