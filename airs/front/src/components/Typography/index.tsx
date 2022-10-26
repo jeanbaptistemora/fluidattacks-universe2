37ac1fbf5fc7deb075e9708eb6c14a1074a3af97
Frank Aristizabal <faristizabal@fluidattacks.com>
2022-10-26 15:41:15 -0500

    airs\refac(front): #7727 change styles
    - Add typography components.

---
+++ airs/front/src/components/Typography/index.tsx
@@ -0,0 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { Text } from "./Text";
+import { Title } from "./Title";
+
+export { Text, Title };
