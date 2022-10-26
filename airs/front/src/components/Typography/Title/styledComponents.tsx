fb9c2c15eb04c902fbf28d3f6d62f357c6f8c336
Frank Aristizabal <faristizabal@fluidattacks.com>
2022-10-26 14:24:17 -0500

    airs\refac(front): #7727 change styles
    - Add typography components.

---
+++ airs/front/src/components/Typography/Title/styledComponents.tsx
@@ -0,0 +1,73 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import styled from "styled-components";
+
+import type { ISize, ITypographyProps, TSize, TStyle, TWeight } from "../types";
+
+const fontStyles: Record<TStyle, string> = {
+  i: "italic",
+  no: "normal",
+};
+
+const fontWeights: Record<TWeight, number> = {
+  bold: 7,
+  regular: 4,
+  semibold: 6,
+};
+
+const variants: Record<TSize, { sizes: ISize; weight: TWeight }> = {
+  big: { sizes: { fontSize: "1", lineHeight: "56" }, weight: "bold" },
+  medium: {
+    sizes: { fontSize: "2", lineHeight: "48" },
+    weight: "bold",
+  },
+  small: {
+    sizes: { fontSize: "3", lineHeight: "40" },
+    weight: "semibold",
+  },
+  xs: {
+    sizes: { fontSize: "4", lineHeight: "32" },
+    weight: "semibold",
+  },
+};
+
+const StyledTitle = styled.p.attrs(
+  ({
+    mb = 0,
+    ml = 0,
+    mr = 0,
+    mt = 0,
+    size = "medium",
+  }: ITypographyProps): {
+    className: string;
+  } => ({
+    className: `f${variants[size].sizes.fontSize} fw${
+      fontWeights[variants[size].weight]
+    } mb${mb} ml${ml} mr${mr} mt${mt}`,
+  })
+)<ITypographyProps>`
+  ${({
+    color,
+    hColor = color,
+    display = "block",
+    fontStyle = "no",
+    textAlign = "start",
+    size = "medium",
+  }): string => `
+        color: ${color};
+        display: ${display};
+        font-style: ${fontStyles[fontStyle]};
+        line-height: ${variants[size].sizes.lineHeight}px;
+        text-align: ${textAlign};
+        width: ${display === "block" ? "100%" : "auto"};
+        :hover {
+          color: ${hColor};
+        }
+      `}
+`;
+
+export { StyledTitle };
