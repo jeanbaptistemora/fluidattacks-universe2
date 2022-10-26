17a41f399a68f67feb5c2e39281885765475ca6d
David Acevedo <dacevedo@fluidattacks.com>
2022-10-26 21:26:52 +0000

    integrates\refac(back): #7531 scheduler
    - changed parametrize strategy
    - to run the scheduler function only once

--- integrates/back/test/functional/src/expire_free_trial/test_schedule.py
+++ integrates/back/test/functional/src/expire_free_trial/test_schedule.py
@@ -25,60 +25,47 @@ from schedulers import (
 
 @pytest.mark.asyncio
 @pytest.mark.resolver_test_group("expire_free_trial")
-@pytest.mark.parametrize(
-    [
-        "group_name",
-        "email",
-        "completed_before",
-        "managed_before",
-        "completed_after",
-        "managed_after",
-    ],
-    [
-        # Reached trial limit
-        [
-            "testgroup",
-            "johndoe@fluidattacks.com",
-            False,
-            GroupManaged.TRIAL,
-            True,
-            GroupManaged.UNDER_REVIEW,
-        ],
-        # Still has remaining days
-        [
-            "testgroup2",
-            "janedoe@fluidattacks.com",
-            False,
-            GroupManaged.TRIAL,
-            False,
-            GroupManaged.TRIAL,
-        ],
-    ],
-)
 @freeze_time("2022-11-11T15:58:31.280182")
-async def test_expire_free_trial(
-    *,
-    populate: bool,
-    group_name: str,
-    email: str,
-    completed_before: bool,
-    managed_before: GroupManaged,
-    completed_after: bool,
-    managed_after: GroupManaged,
-) -> None:
+async def test_expire_free_trial(*, populate: bool) -> None:
     assert populate
     loaders = get_new_context()
+    cases = [
+        # Reached trial limit
+        {
+            "email": "johndoe@fluidattacks.com",
+            "group_name": "testgroup",
+            "completed_before": False,
+            "managed_before": GroupManaged.TRIAL,
+            "completed_after": True,
+            "managed_after": GroupManaged.UNDER_REVIEW,
+        },
+        # Still has remaining days
+        {
+            "email": "janedoe@fluidattacks.com",
+            "group_name": "testgroup2",
+            "completed_before": False,
+            "managed_before": GroupManaged.TRIAL,
+            "completed_after": False,
+            "managed_after": GroupManaged.TRIAL,
+        },
+    ]
 
-    enrollment_before: Enrollment = await loaders.enrollment.load(email)
-    assert enrollment_before.trial.completed == completed_before
-    group_before: Group = await loaders.group.load(group_name)
-    assert group_before.state.managed == managed_before
+    for case in cases:
+        enrollment_before: Enrollment = await loaders.enrollment.load(
+            case["email"]
+        )
+        group_before: Group = await loaders.group.load(case["group_name"])
+        assert enrollment_before.trial.completed == case["completed_before"]
+        assert group_before.state.managed == case["managed_before"]
 
     await expire_free_trial.main()
     loaders.enrollment.clear_all()
     loaders.group.clear_all()
 
-    enrollment_after: Enrollment = await loaders.enrollment.load(email)
-    assert enrollment_after.trial.completed == completed_after
-    group_after: Group = await loaders.group.load(group_name)
-    assert group_after.state.managed == managed_after
+    for case in cases:
+        enrollment_after: Enrollment = await loaders.enrollment.load(
+            case["email"]
+        )
+        group_after: Group = await loaders.group.load(case["group_name"])
+        assert enrollment_after.trial.completed == case["completed_after"]
+        assert group_after.state.managed == case["managed_after"]
