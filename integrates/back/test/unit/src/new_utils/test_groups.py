2171d7778ab5cd0a84a44d18084f6dd4c00158e3
David Acevedo <dacevedo@fluidattacks.com>
2022-10-27 14:48:31 +0000

    integrates\test(back): #7531 scheduler
    - Groups under review should not be considered active, thus excluded
    - from other scheduled tasks

---
+++ integrates/back/test/unit/src/new_utils/test_groups.py
@@ -0,0 +1,57 @@
+# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from db_model.groups.enums import (
+    GroupLanguage,
+    GroupManaged,
+    GroupService,
+    GroupStateStatus,
+    GroupSubscriptionType,
+    GroupTier,
+)
+from db_model.groups.types import (
+    Group,
+    GroupState,
+)
+from newutils.groups import (
+    filter_active_groups,
+)
+import pytest
+
+
+@pytest.mark.parametrize(
+    ["managed", "status", "expected"],
+    [
+        # Active
+        (GroupManaged.MANAGED, GroupStateStatus.ACTIVE, True),
+        # Inactive
+        (GroupManaged.MANAGED, GroupStateStatus.DELETED, False),
+        # Active but under review
+        (GroupManaged.UNDER_REVIEW, GroupStateStatus.ACTIVE, False),
+    ],
+)
+def test_filter_active_groups(
+    managed: GroupManaged, status: GroupStateStatus, expected: bool
+) -> None:
+    group = Group(
+        created_by="johndoe@fluidattacks.com",
+        created_date="2022-10-21T15:58:31.280182",
+        description="test description",
+        language=GroupLanguage.EN,
+        name="testgroup",
+        organization_id="",
+        state=GroupState(
+            has_machine=True,
+            has_squad=False,
+            managed=managed,
+            modified_by="johndoe@fluidattacks.com",
+            modified_date="2022-10-21T15:58:31.280182",
+            service=GroupService.WHITE,
+            status=status,
+            tier=GroupTier.FREE,
+            type=GroupSubscriptionType.CONTINUOUS,
+        ),
+    )
+    filtered = filter_active_groups((group,))
+    assert bool(filtered) == expected
