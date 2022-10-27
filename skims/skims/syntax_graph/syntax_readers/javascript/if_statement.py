8ab061aed9c68f2dfa0fedbd0874ab4727823759
Flor Calderon <fcalderon@fluidattacks.com>
2022-10-27 11:50:41 -0500

    skims\refac(back): #4846.55 modify syntax reader
    - modify if statement to check the correct condition
    - update json results

--- skims/skims/syntax_graph/syntax_readers/javascript/if_statement.py
+++ skims/skims/syntax_graph/syntax_readers/javascript/if_statement.py
@@ -11,10 +11,18 @@ from syntax_graph.syntax_nodes.if_statement import (
 from syntax_graph.types import (
     SyntaxGraphArgs,
 )
+from utils.graph import (
+    match_ast,
+)
 
 
 def reader(args: SyntaxGraphArgs) -> NId:
     condition_id = args.ast_graph.nodes[args.n_id]["label_field_condition"]
+    if (
+        args.ast_graph.nodes[condition_id]["label_type"]
+        == "parenthesized_expression"
+    ):
+        condition_id = match_ast(args.ast_graph, condition_id).get("__1__")
     true_id = args.ast_graph.nodes[args.n_id]["label_field_consequence"]
     false_id = args.ast_graph.nodes[args.n_id].get("label_field_alternative")
     return build_if_node(args, condition_id, true_id, false_id)
