bc9c1b6fb3951d8ed176eff8733fdc5253216ade
Jonathan Mesa <jmesa@fluidattacks.com>
2022-10-27 17:57:52 +0000

    integrates\feat(back): #7329 archive events
    - add type for event metadata to be archived

---
+++ integrates/back/src/redshift/events/types.py
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from dataclasses import (
+    dataclass,
+)
+from datetime import (
+    datetime,
+)
+from typing import (
+    Optional,
+)
+
+
+@dataclass(frozen=True)
+class MetadataTableRow:
+    # pylint: disable=invalid-name,too-many-instance-attributes
+    id: str
+    created_by: str
+    created_date: datetime
+    event_date: datetime
+    group_name: str
+    hacker: str
+    root_id: Optional[str]
+    solving_date: Optional[datetime]
+    status: str
+    type: str
