ca66a4ad8972e06b3bf05852490d01a9527dc181
Juan Chaves <jchaves@fluidattacks.com>
2022-10-28 01:05:04 +0000

    integrates\feat(back): #7566.7 add historic subscriptions
    - `remove` will now remove historical subs as well

--- integrates/back/src/db_model/subscriptions/remove.py
+++ integrates/back/src/db_model/subscriptions/remove.py
@@ -2,6 +2,9 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
+from boto3.dynamodb.conditions import (
+    Key,
+)
 from db_model import (
     TABLE,
 )
@@ -12,7 +15,12 @@ from dynamodb import (
     keys,
 )
 from dynamodb.operations import (
+    batch_delete_item,
     delete_item,
+    query,
+)
+from dynamodb.types import (
+    PrimaryKey,
 )
 
 
@@ -32,3 +40,30 @@ async def remove(
     )
 
     await delete_item(key=primary_key, table=TABLE)
+
+    historic_sub_key = keys.build_key(
+        facet=TABLE.facets["stakeholder_historic_subscription"],
+        values={
+            "email": email,
+            "entity": entity.lower(),
+            "subject": subject,
+        },
+    )
+    key_structure = TABLE.primary_key
+    response = await query(
+        condition_expression=(
+            Key(key_structure.partition_key).eq(historic_sub_key.partition_key)
+        ),
+        facets=(TABLE.facets["stakeholder_historic_subscription"],),
+        table=TABLE,
+    )
+    await batch_delete_item(
+        keys=tuple(
+            PrimaryKey(
+                partition_key=item[key_structure.partition_key],
+                sort_key=item[key_structure.sort_key],
+            )
+            for item in response.items
+        ),
+        table=TABLE,
+    )
