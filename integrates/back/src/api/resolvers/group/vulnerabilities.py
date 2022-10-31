5c229418802e2383aa1850b9bd2a940b3c270667
Juan Villegas <jvillegas@fluidattacks.com>
2022-10-31 10:16:40 -0500

    integrates\feat(back): #7396.10 vuln filter
    - zeroRisk attr has been added to filter zero risk requested
    
    - vulns zeroRisk enum has been added

--- integrates/back/src/api/resolvers/group/vulnerabilities.py
+++ integrates/back/src/api/resolvers/group/vulnerabilities.py
@@ -2,8 +2,14 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
+from dataloaders import (
+    Dataloaders,
+)
 from db_model.groups.types import (
     Group,
+)
+from db_model.vulnerabilities.enums import (
+    VulnerabilityZeroRiskStatus,
 )
 from db_model.vulnerabilities.types import (
     VulnerabilitiesConnection,
@@ -21,6 +27,8 @@ from search.operations import (
 )
 from typing import (
     Any,
+    Dict,
+    List,
 )
 
 LOGGER = logging.getLogger(__name__)
@@ -31,30 +39,19 @@ async def resolve(
     info: GraphQLResolveInfo,
     **kwargs: Any,
 ) -> VulnerabilitiesConnection:
-    after = kwargs.get("after")
-    first = kwargs.get("first", 10)
-    query = kwargs.get("search")
-
-    must_filters = []
-    if treatment := kwargs.get("treatment"):
-        must_filters.append({"treatment.status": str(treatment).upper()})
-
-    if state := kwargs.get("stateStatus"):
-        must_filters.append({"state.status": str(state).upper()})
-
-    if verification := kwargs.get("verificationStatus"):
-        must_filters.append({"verification.status": str(verification).upper()})
+    vulnerabilities_filters: Dict[str, Any] = vulnerabilities_filter(**kwargs)
 
     results = await search(
-        after=after,
+        after=kwargs.get("after"),
         exact_filters={"group_name": parent.name},
-        must_filters=must_filters,
-        must_not_filters=[{"zero_risk.status": "CONFIRMED"}],
+        must_filters=vulnerabilities_filters["must_filters"],
+        must_not_filters=vulnerabilities_filters["must_not_filters"],
         index="vulnerabilities",
-        limit=first,
-        query=query,
+        limit=kwargs.get("first", 10),
+        query=kwargs.get("search"),
     )
-    loaders = info.context.loaders
+
+    loaders: Dataloaders = info.context.loaders
     draft_ids = tuple(
         draft.id for draft in await loaders.group_drafts.load(parent.name)
     )
@@ -74,3 +71,45 @@ async def resolve(
         page_info=results.page_info,
         total=results.total,
     )
+
+
+def vulnerabilities_filter(**kwargs: Any) -> Dict[str, Any]:
+    vulns_must_filters: List[Dict[str, Any]] = must_filter(**kwargs)
+    vulns_must_not_filters: List[Dict[str, Any]] = must_not_filter()
+
+    if zero_risk := kwargs.get("zeroRisk"):
+        vulns_must_filters.append({"zero_risk.status": zero_risk})
+    else:
+        vulns_must_not_filters.append(
+            {"zero_risk.status": VulnerabilityZeroRiskStatus.REQUESTED}
+        )
+
+    filters: Dict[str, Any] = {
+        "must_filters": vulns_must_filters,
+        "must_not_filters": vulns_must_not_filters,
+    }
+
+    return filters
+
+
+def must_filter(**kwargs: Any) -> List[Dict[str, Any]]:
+    must_filters = []
+
+    if treatment := kwargs.get("treatment"):
+        must_filters.append({"treatment.status": str(treatment).upper()})
+
+    if state := kwargs.get("stateStatus"):
+        must_filters.append({"state.status": str(state).upper()})
+
+    if verification := kwargs.get("verificationStatus"):
+        must_filters.append({"verification.status": str(verification).upper()})
+
+    return must_filters
+
+
+def must_not_filter() -> List[Dict[str, Any]]:
+    must_not_filters = [
+        {"zero_risk.status": VulnerabilityZeroRiskStatus.CONFIRMED}
+    ]
+
+    return must_not_filters
