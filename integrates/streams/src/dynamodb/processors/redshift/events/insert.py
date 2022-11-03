d960655cdaaaad21b2ea9e98333a066595ac40d3
Jonathan Mesa <jmesa@fluidattacks.com>
2022-11-03 01:44:53 +0000

    integrates\feat(back): #7329 archive events
    - add support for archiving events metadata in streams
    - processing logic still not in place
    - no-deltas-test

---
+++ integrates/streams/src/dynamodb/processors/redshift/events/insert.py
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from ..utils import (
+    format_sql_query_metadata,
+    get_query_fields,
+)
+from .initialize import (
+    METADATA_TABLE,
+)
+from .types import (
+    MetadataTableRow,
+)
+from .utils import (
+    format_row_metadata,
+)
+from dynamodb.types import (
+    Item,
+)
+from psycopg2.extensions import (
+    cursor as cursor_cls,
+)
+
+
+def insert_metadata(
+    *,
+    cursor: cursor_cls,
+    item: Item,
+) -> None:
+    sql_fields = get_query_fields(MetadataTableRow)
+    sql_values = format_row_metadata(item)
+    cursor.execute(
+        format_sql_query_metadata(METADATA_TABLE, sql_fields),
+        sql_values,
+    )
---
+++ integrates/streams/src/dynamodb/processors/redshift/events/utils.py
@@ -0,0 +1,44 @@
+# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from datetime import (
+    datetime,
+)
+from dynamodb.types import (
+    Item,
+)
+from typing import (
+    Optional,
+)
+
+FLUID_IDENTIFIER = "@fluidattacks.com"
+
+
+def format_row_metadata(
+    item: Item,
+) -> Item:
+    unreliable_solving_date: Optional[str] = item.get(
+        "unreliable_indicators", {}
+    ).get("unreliable_solving_date", None)
+    return dict(
+        id=item["id"],
+        created_by=item["created_by"]
+        if str(item["created_by"]).endswith(FLUID_IDENTIFIER)
+        else None,
+        created_date=datetime.fromisoformat(item["created_date"])
+        if item.get("created_date")
+        else None,
+        event_date=datetime.fromisoformat(item["event_date"])
+        if item.get("event_date")
+        else None,
+        group_name=item["group_name"],
+        hacker=item["hacker"],
+        root_id=item.get("root_id"),
+        solution_reason=item["state"].get("reason"),
+        solving_date=datetime.fromisoformat(unreliable_solving_date)
+        if unreliable_solving_date
+        else None,
+        status=item["state"]["status"],
+        type=item["type"],
+    )
