6afb9021ed9d306cc092e4358466e2248a786c17
Daniel Salazar <dsalazar@fluidattacks.com>
2022-11-04 03:58:32 +0000

    integrates\refac(build): #7959 schema lint
    - Enable schema lint failure

--- integrates/pipeline/makes.nix
+++ integrates/pipeline/makes.nix
@@ -503,11 +503,7 @@ in {
           }
           {
             output = "/integrates/back/lint/schema";
-            gitlabExtra =
-              gitlabLint
-              // {
-                allow_failure = true;
-              };
+            gitlabExtra = gitlabLint;
           }
           {
             output = "/integrates/back/lint/schema/deprecations";
