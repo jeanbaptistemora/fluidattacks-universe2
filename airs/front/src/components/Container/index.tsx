407af8562ff88c143dc68be07eee35a71f21b4ef
Frank Aristizabal <faristizabal@fluidattacks.com>
2022-11-08 13:38:57 -0500

    airs\refac(front): #7727 change styles
    - Add container component.

---
+++ airs/front/src/components/Container/index.tsx
@@ -0,0 +1,77 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import React from "react";
+
+import { StyledContainer } from "./styledComponents";
+import type { IContainerProps } from "./types";
+
+const Container: React.FC<IContainerProps> = ({
+  align,
+  bgColor,
+  br,
+  center,
+  children,
+  direction,
+  display,
+  height,
+  justify,
+  maxWidth,
+  mb,
+  mh,
+  minHeight,
+  minWidth,
+  ml,
+  mr,
+  mt,
+  mv,
+  pb,
+  ph,
+  pl,
+  pr,
+  pt,
+  pv,
+  width,
+  widthMd,
+  widthSm,
+  wrap,
+}): JSX.Element => {
+  return (
+    <StyledContainer
+      align={align}
+      bgColor={bgColor}
+      br={br}
+      center={center}
+      direction={direction}
+      display={display}
+      height={height}
+      justify={justify}
+      maxWidth={maxWidth}
+      mb={mb}
+      mh={mh}
+      minHeight={minHeight}
+      minWidth={minWidth}
+      ml={ml}
+      mr={mr}
+      mt={mt}
+      mv={mv}
+      pb={pb}
+      ph={ph}
+      pl={pl}
+      pr={pr}
+      pt={pt}
+      pv={pv}
+      width={width}
+      widthMd={widthMd}
+      widthSm={widthSm}
+      wrap={wrap}
+    >
+      {children}
+    </StyledContainer>
+  );
+};
+
+export { Container };
