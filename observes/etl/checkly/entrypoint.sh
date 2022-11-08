5d40d084e1e15c145f7acc58618f5659d9809586
Daniel Murcia <dmurcia@fluidattacks.com>
2022-11-08 08:11:09 -0500

    observes\fix(job): #7443.19 checkly schema
    - change target checkly etl schema
    - rm rolled results etl
    
    Closes #7443

--- observes/etl/checkly/entrypoint.sh
+++ observes/etl/checkly/entrypoint.sh
@@ -32,7 +32,7 @@ function start_etl {
       > .singer \
     && echo '[INFO] Running target' \
     && target-redshift destroy-and-upload \
-      --schema-name 'new_checkly' \
+      --schema-name 'checkly' \
       --s3-state "${state_folder}/${state_file}" \
       --persistent-tables "check_results,check_results_api,check_results_browser,check_results_browser_pages" \
       < .singer \
