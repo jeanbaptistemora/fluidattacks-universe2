b7fbf362301d404d175cbcd19b4812ceaedf488b
Julian Gomez <ugomez@fluidattacks.com>
2022-11-09 12:39:08 -0500

    skims\refac(back): #7423.53 implement missing case
    - implement logic to cover missing case
    - update test results

--- skims/skims/lib_root/f143/common.py
+++ skims/skims/lib_root/f143/common.py
@@ -2,6 +2,9 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
+from itertools import (
+    chain,
+)
 from model.graph_model import (
     Graph,
     NId,
@@ -27,13 +30,23 @@ def has_eval(graph: Graph) -> List[NId]:
     vuln_nodes: List[NId] = []
     sensitive_methods = {"eval", "Function"}
 
-    for member in g.filter_nodes(
-        graph,
-        graph.nodes,
-        predicate=g.pred_has_labels(label_type="MethodInvocation"),
+    for member in chain(
+        g.filter_nodes(
+            graph,
+            graph.nodes,
+            g.pred_has_labels(label_type="MethodInvocation"),
+        ),
+        g.filter_nodes(
+            graph,
+            graph.nodes,
+            g.pred_has_labels(label_type="ObjectCreation"),
+        ),
     ):
         if (args_id := graph.nodes[member].get("arguments_id")) and (
-            graph.nodes[member].get("expression") in sensitive_methods
+            (
+                graph.nodes[member].get("expression") in sensitive_methods
+                or graph.nodes[member].get("name") in sensitive_methods
+            )
             and not is_argument_literal(graph, args_id)
         ):
             vuln_nodes.append(member)
