cc23ff4cbbdd7281620f8be9aab58cb5420247ed
Juan Villegas <jvillegas@fluidattacks.com>
2022-11-09 06:14:30 -0500

    integrates\test(front): #7396.16 vuln filter
    - provide mock for test
    
    - render mock
    
    - expect table case
    
    - no-deltas-test

--- integrates/front/src/scenes/Dashboard/containers/GroupVulnerabilitiesView/index.test.tsx
+++ integrates/front/src/scenes/Dashboard/containers/GroupVulnerabilitiesView/index.test.tsx
@@ -4,11 +4,203 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
+import { MockedProvider } from "@apollo/client/testing";
+import type { MockedResponse } from "@apollo/client/testing";
+import { render, screen, waitFor } from "@testing-library/react";
+import React from "react";
+import { MemoryRouter, Route } from "react-router-dom";
+
+import { GET_GROUP_VULNERABILITIES } from "./queries";
+import type { IGroupVulnerabilities } from "./types";
+
 import { GroupVulnerabilitiesView } from ".";
+import { getCache } from "utils/apollo";
 
 describe("GroupVulnerabilitiesView", (): void => {
+  const mockGroupVulnerabilities: IGroupVulnerabilities = {
+    group: {
+      name: "unittesting",
+      vulnerabilities: {
+        edges: [
+          {
+            node: {
+              assigned: "",
+              currentState: "open",
+              currentStateCapitalized: "Open",
+              externalBugTrackingSystem: null,
+              findingId: "438679960",
+              groupName: "unittesting",
+              historicTreatment: [
+                {
+                  acceptanceDate: "",
+                  acceptanceStatus: "",
+                  assigned: "assigned-user-1",
+                  date: "2019-07-05 09:56:40",
+                  justification: "test progress justification",
+                  treatment: "IN PROGRESS",
+                  user: "usertreatment@test.test",
+                },
+              ],
+              id: "89521e9a-b1a3-4047-a16e-15d530dc1340",
+              lastTreatmentDate: "2019-07-05 09:56:40",
+              lastVerificationDate: null,
+              organizationName: "test",
+              remediated: false,
+              reportDate: "",
+              rootNickname: "https:",
+              severity: "3",
+              specific: "specific-1",
+              stream: null,
+              tag: "tag-1, tag-2",
+              treatment: "",
+              treatmentAcceptanceDate: "",
+              treatmentAcceptanceStatus: "",
+              treatmentAssigned: "assigned-user-1",
+              treatmentDate: "2019-07-05 09:56:40",
+              treatmentJustification: "test progress justification",
+              treatmentUser: "usertreatment@test.test",
+              verification: "Requested",
+              vulnerabilityType: "inputs",
+              where: "https://example.com/inputs",
+              zeroRisk: "Requested",
+            },
+          },
+          {
+            node: {
+              assigned: "",
+              currentState: "closed",
+              currentStateCapitalized: "Closed",
+              externalBugTrackingSystem: null,
+              findingId: "438679960",
+              groupName: "unittesting",
+              historicTreatment: [
+                {
+                  acceptanceDate: "",
+                  acceptanceStatus: "",
+                  assigned: "assigned-user-3",
+                  date: "2019-07-05 09:56:40",
+                  justification: "test progress justification",
+                  treatment: "IN PROGRESS",
+                  user: "usertreatment@test.test",
+                },
+              ],
+              id: "a09c79fc-33fb-4abd-9f20-f3ab1f500bd0",
+              lastTreatmentDate: "2019-07-05 09:56:40",
+              lastVerificationDate: null,
+              organizationName: "test",
+              remediated: false,
+              reportDate: "",
+              rootNickname: "https:",
+              severity: "1",
+              specific: "specific-2",
+              stream: null,
+              tag: "tag-3, tag-4",
+              treatment: "",
+              treatmentAcceptanceDate: "",
+              treatmentAcceptanceStatus: "",
+              treatmentAssigned: "assigned-user-2",
+              treatmentDate: "2019-07-05 09:56:40",
+              treatmentJustification: "test progress justification",
+              treatmentUser: "usertreatment@test.test",
+              verification: "Verified",
+              vulnerabilityType: "lines",
+              where: "https://example.com/lines",
+              zeroRisk: null,
+            },
+          },
+          {
+            node: {
+              assigned: "",
+              currentState: "open",
+              currentStateCapitalized: "Open",
+              externalBugTrackingSystem: null,
+              findingId: "438679960",
+              groupName: "unittesting",
+              historicTreatment: [
+                {
+                  acceptanceDate: "",
+                  acceptanceStatus: "",
+                  assigned: "assigned-user-4",
+                  date: "2019-07-05 09:56:40",
+                  justification: "test progress justification",
+                  treatment: "IN PROGRESS",
+                  user: "usertreatment@test.test",
+                },
+              ],
+              id: "af7a48b8-d8fc-41da-9282-d424fff563f0",
+              lastTreatmentDate: "2019-07-05 09:56:40",
+              lastVerificationDate: "2019-07-05 09:56:40",
+              organizationName: "test",
+              remediated: false,
+              reportDate: "",
+              rootNickname: "https:",
+              severity: "1",
+              specific: "specific-3",
+              stream: null,
+              tag: "tag-5, tag-6",
+              treatment: "IN_PROGRESS",
+              treatmentAcceptanceDate: "",
+              treatmentAcceptanceStatus: "",
+              treatmentAssigned: "assigned-user-3",
+              treatmentDate: "2019-07-05 09:56:40",
+              treatmentJustification: "test progress justification",
+              treatmentUser: "usertreatment@test.test",
+              verification: "Verified",
+              vulnerabilityType: "lines",
+              where: "https://example.com/lines",
+              zeroRisk: null,
+            },
+          },
+        ],
+        pageInfo: {
+          endCursor: "bnVsbA==",
+          hasNextPage: false,
+        },
+        total: 3,
+      },
+    },
+  };
+
   it("should return a function", (): void => {
     expect.hasAssertions();
     expect(typeof GroupVulnerabilitiesView).toBe("function");
+  });
+
+  it("should render in group vulnerabilities", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    const queryMock: MockedResponse = {
+      request: {
+        query: GET_GROUP_VULNERABILITIES,
+        variables: {
+          after: "",
+          first: 100,
+          groupName: "unittesting",
+          search: "",
+          stateStatus: "",
+          treatment: "",
+          verificationStatus: "",
+          zeroRisk: "",
+        },
+      },
+      result: {
+        data: mockGroupVulnerabilities,
+      },
+    };
+
+    render(
+      <MemoryRouter initialEntries={["/orgs/okada/groups/unittesting/vulns"]}>
+        <MockedProvider cache={getCache()} mocks={[queryMock]}>
+          <Route
+            component={GroupVulnerabilitiesView}
+            path={"/orgs/:organizationName/groups/:groupName/vulns"}
+          />
+        </MockedProvider>
+      </MemoryRouter>
+    );
+
+    await waitFor((): void => {
+      expect(screen.queryByRole("table")).toBeInTheDocument();
+    });
   });
 });
