0aae52536db3ddd3e2cf7260d22d2e8d0fa45bb8
Jonathan Mesa <jmesa@fluidattacks.com>
2022-11-09 21:40:48 +0000

    integrates\feat(back): #7329 archive groups
    - add fns to create the needed tables in redshift
    - no-deltas-test

---
+++ integrates/streams/src/dynamodb/processors/redshift/groups/initialize.py
@@ -0,0 +1,134 @@
+# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from ..operations import (
+    execute,
+    initialize_schema,
+    SCHEMA_NAME,
+)
+from psycopg2 import (
+    sql,
+)
+
+CODE_LANGUAGES_TABLE = "groups_code_languages"
+METADATA_TABLE = "groups_metadata"
+STATE_TABLE = "groups_state"
+UNFULFILLED_STANDARDS_TABLE = "groups_unfulfilled_standards"
+
+
+def _initialize_code_languages_table() -> None:
+    execute(
+        sql.SQL(
+            """
+            CREATE TABLE IF NOT EXISTS {table} (
+                id VARCHAR,
+                group_name VARCHAR,
+                language VARCHAR,
+                loc INTEGER,
+
+                UNIQUE (
+                    id
+                ),
+                PRIMARY KEY (
+                    id
+                )
+            )
+        """
+        ).format(
+            table=sql.Identifier(SCHEMA_NAME, CODE_LANGUAGES_TABLE),
+        ),
+    )
+
+
+def _initialize_metadata_table() -> None:
+    execute(
+        sql.SQL(
+            """
+            CREATE TABLE IF NOT EXISTS {table} (
+                id VARCHAR,
+                created_by VARCHAR,
+                created_date TIMESTAMPTZ,
+                language VARCHAR,
+                name VARCHAR,
+                organization_id VARCHAR,
+                sprint_duration INTEGER,
+                sprint_start_date TIMESTAMPTZ,
+
+                UNIQUE (
+                    id
+                ),
+                PRIMARY KEY (
+                    id
+                )
+            )
+        """
+        ).format(
+            table=sql.Identifier(SCHEMA_NAME, METADATA_TABLE),
+        ),
+    )
+
+
+def _initialize_state_table() -> None:
+    execute(
+        sql.SQL(
+            """
+            CREATE TABLE IF NOT EXISTS {table} (
+                id VARCHAR,
+                has_machine BOOLEAN,
+                has_squad BOOLEAN,
+                justification VARCHAR,
+                managed VARCHAR,
+                modified_by VARCHAR,
+                modified_date TIMESTAMPTZ,
+                pending_deletion_date TIMESTAMPTZ,
+                service VARCHAR,
+                status VARCHAR,
+                tier VARCHAR,
+                type VARCHAR,
+
+                PRIMARY KEY (
+                    id,
+                    modified_date
+                ),
+                FOREIGN KEY (id)
+                    REFERENCES {reference_table}(id)
+            )
+        """,
+        ).format(
+            table=sql.Identifier(SCHEMA_NAME, STATE_TABLE),
+            reference_table=sql.Identifier(SCHEMA_NAME, METADATA_TABLE),
+        ),
+    )
+
+
+def _initialize_unfulfilled_standards_table() -> None:
+    execute(
+        sql.SQL(
+            """
+            CREATE TABLE IF NOT EXISTS {table} (
+                id VARCHAR,
+                group_name VARCHAR,
+                name VARCHAR,
+                requirement VARCHAR,
+
+                UNIQUE (
+                    id
+                ),
+                PRIMARY KEY (
+                    id
+                )
+            )
+        """
+        ).format(
+            table=sql.Identifier(SCHEMA_NAME, UNFULFILLED_STANDARDS_TABLE),
+        ),
+    )
+
+
+async def initialize_tables() -> None:
+    initialize_schema()
+    _initialize_metadata_table()
+    _initialize_code_languages_table()
+    _initialize_state_table()
+    _initialize_unfulfilled_standards_table()
