da1a27d8cd9e6c677bd8301688e1a246a00da6f3
David Acevedo <dacevedo@fluidattacks.com>
2022-11-11 15:24:55 +0000

    integrates\feat(build): #8084 deps linter
    - check for presence of semver range characters

--- integrates/front/lint/entrypoint.sh
+++ integrates/front/lint/entrypoint.sh
@@ -14,7 +14,10 @@ function main {
     && if integrates-front-lint-eslint; then
       success=$((success + 1))
     fi \
-    && if test "${success}" -eq 2; then
+    && if lint_npm_deps integrates/front/package.json; then
+      success=$((success + 1))
+    fi \
+    && if test "${success}" -eq 3; then
       info "Congratulations! Your code comply with the suggested style"
     else
       critical "Your code doesn't comply with the suggested style"
