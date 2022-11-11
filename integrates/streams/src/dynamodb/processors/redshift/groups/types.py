9ec401f6083405391dc1dad5f31de7fa5ba5664b
Jonathan Mesa <jmesa@fluidattacks.com>
2022-11-11 15:51:04 +0000

    integrates\refac(back): #7329 archive groups
    - replicate groups support at streams
    - no-deltas-test

---
+++ integrates/streams/src/dynamodb/processors/redshift/groups/types.py
@@ -0,0 +1,52 @@
+# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from dataclasses import (
+    dataclass,
+)
+from datetime import (
+    datetime,
+)
+from typing import (
+    Optional,
+)
+
+
+@dataclass(frozen=True)
+class CodeLanguagesTableRow:
+    # pylint: disable=invalid-name
+    id: str
+    group_name: str
+    language: str
+    loc: int
+
+
+@dataclass(frozen=True)
+class MetadataTableRow:
+    # pylint: disable=invalid-name,too-many-instance-attributes
+    id: str
+    created_by: str
+    created_date: str
+    language: str
+    name: str
+    organization_id: str
+    sprint_duration: Optional[int]
+    sprint_start_date: Optional[datetime]
+
+
+@dataclass(frozen=True)
+class StateTableRow:
+    # pylint: disable=invalid-name,too-many-instance-attributes
+    id: str
+    has_machine: bool
+    has_squad: bool
+    justification: Optional[str]
+    managed: str
+    modified_by: str
+    modified_date: datetime
+    pending_deletion_date: Optional[datetime]
+    service: Optional[str]
+    status: str
+    tier: str
+    type: str
