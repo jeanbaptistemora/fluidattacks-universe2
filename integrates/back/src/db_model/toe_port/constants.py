43c1b295a477b58316c05d48211aa40c9cb97ecc
Andres Aguirre <aaguirre@fluidattacks.com>
2022-11-15 20:13:51 +0000

    integrates\feat(back): #8083.4 ports gsi 2 facet
    - Add facet to filter the toe ports

---
+++ integrates/back/src/db_model/toe_port/constants.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from db_model import (
+    TABLE,
+)
+from dynamodb.types import (
+    Facet,
+)
+
+GSI_2_FACET = Facet(
+    attrs=TABLE.facets["toe_port_metadata"].attrs,
+    pk_alias="GROUP#group_name",
+    sk_alias="PORTS#PRESENT#be_present#ROOT#root_id#IP#ip#PORT#port",
+)
