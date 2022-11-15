7eaeb5872776c0cce74e85b2557c682cde31a3fc
Andres Aguirre <aaguirre@fluidattacks.com>
2022-11-15 18:43:32 +0000

    integrates\feat(back): #8083.2 toe port types
    - Add type for the toe ports

---
+++ integrates/back/src/db_model/toe_port/types.py
@@ -0,0 +1,83 @@
+# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from datetime import (
+    datetime,
+)
+from dynamodb.types import (
+    PageInfo,
+)
+from typing import (
+    NamedTuple,
+    Optional,
+    Tuple,
+)
+
+
+class ToePort(NamedTuple):
+    attacked_at: Optional[datetime]
+    attacked_by: Optional[str]
+    be_present: bool
+    be_present_until: Optional[datetime]
+    first_attack_at: Optional[datetime]
+    group_name: str
+    has_vulnerabilities: bool
+    ip: str
+    port: str
+    root_id: str
+    seen_at: Optional[datetime]
+    seen_first_time_by: Optional[str]
+
+    def get_hash(self) -> int:
+        return hash((self.group_name, self.ip, self.port))
+
+
+class ToePortEdge(NamedTuple):
+    node: ToePort
+    cursor: str
+
+
+class ToePortsConnection(NamedTuple):
+    edges: Tuple[ToePortEdge, ...]
+    page_info: PageInfo
+
+
+class ToePortRequest(NamedTuple):
+    group_name: str
+    ip: str
+    port: str
+    root_id: str
+
+
+class GroupToePortsRequest(NamedTuple):
+    group_name: str
+    after: Optional[str] = None
+    be_present: Optional[bool] = None
+    first: Optional[int] = None
+    paginate: bool = False
+
+
+class RootToePortsRequest(NamedTuple):
+    group_name: str
+    root_id: str
+    after: Optional[str] = None
+    be_present: Optional[bool] = None
+    first: Optional[int] = None
+    paginate: bool = False
+
+
+class ToePortMetadataToUpdate(NamedTuple):
+    attacked_at: Optional[datetime] = None
+    attacked_by: Optional[str] = None
+    be_present: Optional[bool] = None
+    be_present_until: Optional[datetime] = None
+    first_attack_at: Optional[datetime] = None
+    has_vulnerabilities: Optional[bool] = None
+    seen_at: Optional[datetime] = None
+    seen_first_time_by: Optional[str] = None
+    root_id: Optional[str] = None
+    clean_attacked_at: bool = False
+    clean_be_present_until: bool = False
+    clean_first_attack_at: bool = False
+    clean_seen_at: bool = False
