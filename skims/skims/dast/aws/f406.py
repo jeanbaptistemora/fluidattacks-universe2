98a3bead02964876f50b8a66b21d4413c223b83a
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-11-15 12:35:58 -0500

    skims\feat(back): #6769.100 add method
    - Add method for f406
    - Speed: 13 methods / 11 days = 1.18 method/day
    - TODO: 14 methods
    - ETA: 14 / 1.18 = 11.86 days
    - no-deltas-test

---
+++ skims/skims/dast/aws/f406.py
@@ -0,0 +1,89 @@
+# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from dast.aws.types import (
+    Location,
+)
+from dast.aws.utils import (
+    build_vulnerabilities,
+    run_boto3_fun,
+)
+from model import (
+    core_model,
+)
+from model.core_model import (
+    AwsCredentials,
+    Vulnerability,
+)
+from typing import (
+    Any,
+    Callable,
+    Coroutine,
+    Dict,
+    List,
+    Tuple,
+)
+from zone import (
+    t,
+)
+
+
+async def get_paginated_items(
+    credentials: AwsCredentials,
+    token_name: str,
+    next_token_name: str = "",
+) -> List:
+    """Get all items in paginated API calls."""
+    pools = []
+    args: Dict[str, Any] = {
+        "credentials": credentials,
+        "service": "efs",
+        "function": "describe_file_systems",
+        "parameters": {"MaxItems": 50},
+    }
+    object_name = "FileSystems"
+    data = await run_boto3_fun(**args)
+    pools += data.get(object_name, [])
+    next_token = data.get(token_name, "")
+    args[next_token_name if next_token_name else token_name] = next_token
+    while next_token:
+        data = await run_boto3_fun(**args)
+        pools += data[object_name]
+        next_token = data.get(next_token_name, "")
+    return pools
+
+
+async def efs_is_encryption_disabled(
+    credentials: AwsCredentials,
+) -> core_model.Vulnerabilities:
+    method = core_model.MethodsEnum.AWS_EFS_IS_ENCRYPTION_DISABLED
+    vulns: core_model.Vulnerabilities = ()
+    filesystems = await get_paginated_items(credentials, "Marker")
+    for filesystem in filesystems:
+        locations: List[Location] = []
+        if not filesystem.get("Encrypted"):
+            locations = [
+                Location(
+                    arn=(filesystem["FileSystemArn"]),
+                    description=t("lib_path.f406.aws_efs_unencrypted"),
+                    values=(filesystem["Encrypted"],),
+                    access_patterns=("/Encrypted",),
+                ),
+            ]
+            vulns = (
+                *vulns,
+                *build_vulnerabilities(
+                    locations=locations,
+                    method=(method),
+                    aws_response=filesystem,
+                ),
+            )
+
+    return vulns
+
+
+CHECKS: Tuple[
+    Callable[[AwsCredentials], Coroutine[Any, Any, Tuple[Vulnerability, ...]]],
+    ...,
+] = (efs_is_encryption_disabled,)
