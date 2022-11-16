e1686f05b8cae8a76faa9dc3b28668926448cfc3
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-11-16 08:15:32 -0500

    skims\feat(back): #6769.101 add method to the model
    - Add method for f407 to the model
    - Speed: 13 methods / 11 days = 1.18 method/day
    - TODO: 14 methods
    - ETA: 14 / 1.18 = 11.86 days
    - no-deltas-test

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -2310,6 +2310,14 @@ class MethodsEnum(Enum):
         developer=DeveloperEnum.LUIS_SAAVEDRA,
         technique=TechniqueEnum.DAST,
     )
+    AWS_EBS_HAS_ENCRYPTION_DISABLED = MethodInfo(
+        file_name="aws",
+        name="ebs_has_encryption_disabled",
+        module="dast",
+        finding=FindingEnum.F407,
+        developer=DeveloperEnum.LUIS_SAAVEDRA,
+        technique=TechniqueEnum.DAST,
+    )
     CFN_EC2_SEC_GROUPS_RFC1918 = MethodInfo(
         file_name="cloudformation",
         name="cfn_ec2_has_security_groups_ip_ranges_in_rfc1918",
