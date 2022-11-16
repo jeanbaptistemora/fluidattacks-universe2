536412a11b6081a5e732cdb5e08897a3901f9672
Frank Aristizabal <faristizabal@fluidattacks.com>
2022-11-16 06:38:42 -0500

    airs\refac(front): #8064.8 update internals
    - Update internal pages of solution.

---
+++ airs/front/src/scenes/SolutionPage/MainSection/components/GridContainer.tsx
@@ -0,0 +1,27 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import React from "react";
+
+import { Container } from "../../../../components/Container";
+import type { IContainerProps } from "../../../../components/Container/types";
+
+const GridContainer: React.FC<IContainerProps> = ({
+  children,
+}): JSX.Element => (
+  <Container
+    center={true}
+    direction={"row"}
+    display={"flex"}
+    justify={"center"}
+    maxWidth={"1440px"}
+    wrap={"wrap"}
+  >
+    {children}
+  </Container>
+);
+
+export { GridContainer };
---
+++ airs/front/src/scenes/SolutionPage/MainSection/components/SolutionCard.tsx
@@ -0,0 +1,29 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import React from "react";
+
+import { SimpleCard } from "../../../../components/SimpleCard";
+import type { ISimpleCardProps } from "../../../../components/SimpleCard/types";
+
+const SolutionCard: React.FC<ISimpleCardProps> = ({
+  description,
+  image,
+  title,
+}): JSX.Element => (
+  <SimpleCard
+    description={description}
+    descriptionColor={"#535365"}
+    image={image}
+    title={title}
+    titleColor={"#2e2e38"}
+    titleMinHeight={"64px"}
+    width={"464px"}
+    widthSm={"100%"}
+  />
+);
+
+export { SolutionCard };
