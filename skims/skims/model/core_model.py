0dc8dc293fa5934be3ecaee2a82c547af10583f2
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-11-16 13:12:40 -0500

    skims\feat(back): #6769.103 add method to model
    - Add method for f372 to the model
    - Speed: 14 methods / 12 days = 1.16 method/day
    - TODO: 13 methods
    - ETA: 13 / 1.16 = 11.20 days
    - no-deltas-test

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -2318,6 +2318,14 @@ class MethodsEnum(Enum):
         developer=DeveloperEnum.LUIS_SAAVEDRA,
         technique=TechniqueEnum.DAST,
     )
+    AWS_ELB2_HAS_NOT_HTTPS = MethodInfo(
+        file_name="aws",
+        name="elbv2_listeners_not_using_https",
+        module="dast",
+        finding=FindingEnum.F372,
+        developer=DeveloperEnum.LUIS_SAAVEDRA,
+        technique=TechniqueEnum.DAST,
+    )
     CFN_EC2_SEC_GROUPS_RFC1918 = MethodInfo(
         file_name="cloudformation",
         name="cfn_ec2_has_security_groups_ip_ranges_in_rfc1918",
