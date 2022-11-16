750a49200a3cf2e9502c8538cfe5bf9fc611a169
Andres Aguirre <aaguirre@fluidattacks.com>
2022-11-16 15:59:56 +0000

    integrates\feat(back): #8083.12 remove group toe ports
    - Add model function to remove all toe ports for a group

--- integrates/back/src/db_model/toe_port/remove.py
+++ integrates/back/src/db_model/toe_port/remove.py
@@ -3,12 +3,18 @@
 # SPDX-License-Identifier: MPL-2.0
 
 
+from boto3.dynamodb.conditions import (
+    Key,
+)
 from dynamodb import (
     keys,
     operations,
 )
 from dynamodb.model import (
     TABLE,
+)
+from dynamodb.types import (
+    PrimaryKey,
 )
 
 
@@ -30,3 +36,35 @@ async def remove(
         },
     )
     await operations.delete_item(key=toe_port_key, table=TABLE)
+
+
+async def remove_group_toe_ports(
+    *,
+    group_name: str,
+) -> None:
+    facet = TABLE.facets["toe_port_metadata"]
+    primary_key = keys.build_key(
+        facet=facet,
+        values={"group_name": group_name},
+    )
+    key_structure = TABLE.primary_key
+    response = await operations.query(
+        condition_expression=(
+            Key(key_structure.partition_key).eq(primary_key.partition_key)
+            & Key(key_structure.sort_key).begins_with(
+                primary_key.sort_key.replace("#ROOT#IP#PORT", "")
+            )
+        ),
+        facets=(TABLE.facets["toe_port_metadata"],),
+        table=TABLE,
+    )
+    await operations.batch_delete_item(
+        keys=tuple(
+            PrimaryKey(
+                partition_key=item["pk"],
+                sort_key=item["sk"],
+            )
+            for item in response.items
+        ),
+        table=TABLE,
+    )
