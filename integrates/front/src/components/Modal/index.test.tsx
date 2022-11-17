6f029ef8a53b99edfeb93211fc1eee9b65344141
Laura Naranjo <lnaranjo@fluidattacks.com>
2022-11-17 17:29:02 -0500

    integrates\test(front): #7323.47 increase coverage
    - modify unit test

--- integrates/front/src/components/Modal/index.test.tsx
+++ integrates/front/src/components/Modal/index.test.tsx
@@ -28,11 +28,11 @@ describe("Modal", (): void => {
     expect(screen.queryByText("Unit test title")).toBeInTheDocument();
   });
 
-  it("should close on press esc", async (): Promise<void> => {
+  it("should call onClose and close on press esc", async (): Promise<void> => {
     expect.hasAssertions();
 
     const handleClose: jest.Mock = jest.fn();
-    const { container } = render(
+    const { container, rerender } = render(
       <Modal onClose={handleClose} open={true} title={"Unit test title"}>
         <p>{"Unit 2 modal content"}</p>
       </Modal>
@@ -41,6 +41,14 @@ describe("Modal", (): void => {
     fireEvent.keyDown(container, { key: "Escape" });
     await waitFor((): void => {
       expect(handleClose).toHaveBeenCalledTimes(1);
+
+      rerender(
+        <Modal open={false} title={"Unit test title"}>
+          <p>{"Unit 2 modal content"}</p>
+        </Modal>
+      );
     });
+
+    expect(screen.queryByText("Unit 2 modal content")).not.toBeInTheDocument();
   });
 });
