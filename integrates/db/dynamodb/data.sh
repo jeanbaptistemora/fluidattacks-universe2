51bc7a188cae8793c9c6d2e914034c84882f85ab
David Acevedo <dacevedo@fluidattacks.com>
2022-11-17 11:56:10 -0500

    integrates\refac(build): #8081 db facets
    - updated local data
    - migration is done, lets include all facets automatically
    - no-deltas-test

--- integrates/db/dynamodb/data.sh
+++ integrates/db/dynamodb/data.sh
@@ -12,70 +12,13 @@ function main {
   local email="${GITLAB_USER_EMAIL:-$(get_git_email)}"
   local out="integrates/db/.data"
   local i=0
-  local included_facets=(
-    company_metadata
-    compliance_unreliable_indicators
-    credentials_historic_state
-    credentials_metadata
-    credentials_state
-    enrollment_metadata
-    event_comment
-    event_historic_state
-    event_metadata
-    finding_approval
-    finding_comment
-    finding_creation
-    finding_historic_state
-    finding_historic_verification
-    finding_id
-    finding_metadata
-    finding_state
-    finding_submission
-    finding_unreliable_indicators
-    finding_verification
-    forces_execution
-    git_root_historic_cloning
-    git_root_historic_state
-    git_root_metadata
-    group_access
-    group_comment
-    group_historic_policies
-    group_historic_state
-    group_metadata
-    group_unreliable_indicators
-    ip_root_historic_state
-    ip_root_metadata
-    machine_git_root_execution
-    organization_access
-    organization_historic_policies
-    organization_historic_state
-    organization_metadata
-    organization_unreliable_indicators
-    portfolio_metadata
-    root_environment_secret
-    root_environment_url
-    root_secret
-    root_services_toe_lines
-    stakeholder_metadata
-    stakeholder_subscription
-    toe_input_metadata
-    toe_lines_metadata
-    url_root_historic_state
-    url_root_metadata
-    vulnerability_historic_state
-    vulnerability_historic_treatment
-    vulnerability_historic_verification
-    vulnerability_historic_zero_risk
-    vulnerability_metadata
-  )
-  local facets=''
 
-  facets=$(echo "${included_facets[@]}" | jq -R 'split(" ")') \
+  : \
     && rm -rf "${out}" \
     && mkdir -p "${out}" \
-    && echo '[INFO] Populating from new database design...' \
-    && jq -c --arg facets "${facets}" \
-      '{integrates_vms: [.DataModel[].TableFacets[] | select(.FacetName as $fn | $facets | index($fn) ) | {PutRequest: {Item: .TableData[]}}]}' \
+    && echo '[INFO] Populating from database design...' \
+    && jq -c \
+      '{integrates_vms: [.DataModel[].TableFacets[] | select(.FacetName) | {PutRequest: {Item: .TableData[]}}]}' \
       "__argNewDbDesign__" > "${out}/database-design" \
     && items_len=$(jq '.integrates_vms | length' "${out}/database-design") \
     && echo "items qy: ${items_len}" \
