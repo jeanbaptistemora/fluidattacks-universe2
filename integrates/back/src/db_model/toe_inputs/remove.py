c9c87dc7417f5e15ae0f934e1c019b39816104db
Juan Chaves <jchaves@fluidattacks.com>
2022-11-21 19:40:06 +0000

    integrates\feat(back): #7494.29 add historic toe inputs
    - Deleting a `toe_input` will delete its historic
    metadata as well

--- integrates/back/src/db_model/toe_inputs/remove.py
+++ integrates/back/src/db_model/toe_inputs/remove.py
@@ -36,6 +36,36 @@ async def remove(
     )
     await operations.delete_item(key=toe_input_key, table=TABLE)
 
+    historic_toe_input_key = keys.build_key(
+        facet=TABLE.facets["toe_input_historic_metadata"],
+        values={
+            "component": component,
+            "entry_point": entry_point,
+            "group_name": group_name,
+            "root_id": root_id,
+        },
+    )
+    key_structure = TABLE.primary_key
+    response = await operations.query(
+        condition_expression=(
+            Key(key_structure.partition_key).eq(
+                historic_toe_input_key.partition_key
+            )
+        ),
+        facets=(TABLE.facets["toe_input_historic_metadata"],),
+        table=TABLE,
+    )
+    await operations.batch_delete_item(
+        keys=tuple(
+            PrimaryKey(
+                partition_key=item[key_structure.partition_key],
+                sort_key=item[key_structure.sort_key],
+            )
+            for item in response.items
+        ),
+        table=TABLE,
+    )
+
 
 async def remove_group_toe_inputs(
     *,
