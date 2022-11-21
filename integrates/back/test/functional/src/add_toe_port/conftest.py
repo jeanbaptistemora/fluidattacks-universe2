b09dead3412a3673ef5426c77192a69096421b3b
Andres Aguirre <aaguirre@fluidattacks.com>
2022-11-21 18:01:45 +0000

    integrates\test(back): #8083.31 error to add toe port
    - Test the error cases to add a new toe port
    - no-deltas-test

--- integrates/back/test/functional/src/add_toe_port/conftest.py
+++ integrates/back/test/functional/src/add_toe_port/conftest.py
@@ -6,12 +6,18 @@
 from back.test import (
     db,
 )
+from datetime import (
+    datetime,
+)
 from db_model.roots.enums import (
     RootType,
 )
 from db_model.roots.types import (
     IPRoot,
     IPRootState,
+)
+from db_model.toe_ports.types import (
+    ToePort,
 )
 import pytest
 from typing import (
@@ -71,7 +77,48 @@ async def populate(generic_data: Dict[str, Any]) -> bool:
                 ),
                 "historic_state": [],
             },
+            {
+                "root": IPRoot(
+                    created_by=test_email,
+                    created_date=test_date,
+                    group_name="group1",
+                    id="7a9759ad-218a-4a98-9210-31dd78d61580",
+                    organization_name="orgtest",
+                    state=IPRootState(
+                        address="192.168.1.2",
+                        modified_by=test_email,
+                        modified_date=test_date,
+                        nickname="ip_3",
+                        other=None,
+                        port="8080",
+                        reason=None,
+                        status=test_status,  # type: ignore
+                    ),
+                    type=RootType.IP,
+                ),
+                "historic_state": [],
+            },
         ],
+        "toe_ports": (
+            ToePort(
+                attacked_at=datetime.fromisoformat(
+                    "2020-01-02T05:00:00+00:00"
+                ),
+                attacked_by="admin@gmail.com",
+                be_present=True,
+                be_present_until=None,
+                address="192.168.1.2",
+                port="8080",
+                first_attack_at=datetime.fromisoformat(
+                    "2020-01-02T05:00:00+00:00"
+                ),
+                group_name="group1",
+                has_vulnerabilities=True,
+                seen_at=datetime.fromisoformat("2000-01-01T05:00:00+00:00"),
+                seen_first_time_by="test1@test.com",
+                root_id="7a9759ad-218a-4a98-9210-31dd78d61580",
+            ),
+        ),
     }
 
     return await db.populate({**generic_data["db_data"], **data})
--- integrates/back/test/functional/src/add_toe_port/test_resolver.py
+++ integrates/back/test/functional/src/add_toe_port/test_resolver.py
@@ -50,3 +50,155 @@ async def test_add_toe_port(
     assert "errors" not in result
     assert "success" in result["data"]["addToePort"]
     assert result["data"]["addToePort"]["success"]
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group("add_toe_port")
+@pytest.mark.parametrize(
+    ["email", "address", "port", "root_id"],
+    [
+        [
+            "admin@fluidattacks.com",
+            "192.168.1.2",
+            "8080",
+            "7a9759ad-218a-4a98-9210-31dd78d61580",
+        ],
+    ],
+)
+async def test_add_toe_port_already_exists(
+    populate: bool,
+    email: str,
+    address: str,
+    port: str,
+    root_id: str,
+) -> None:
+    assert populate
+    group_name: str = "group1"
+    result: Dict[str, Any] = await get_result(
+        address=address,
+        port=port,
+        group_name=group_name,
+        root_id=root_id,
+        user=email,
+    )
+    assert "errors" in result
+    assert (
+        result["errors"][0]["message"] == "Exception - Toe port already exists"
+    )
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group("add_toe_port")
+@pytest.mark.parametrize(
+    ["email", "address", "port", "root_id"],
+    [
+        [
+            "admin@fluidattacks.com",
+            "192.168.1.1",
+            "8080",
+            "13cadbdc-23f3-463a-9421-f50f8d0cb1e5",
+        ],
+        [
+            "admin@fluidattacks.com",
+            "192.168.1.1",
+            "8081",
+            "13cadbdc-23f3-463a-9421-f50f8d0cb1e6",
+        ],
+    ],
+)
+async def test_add_toe_port_root_not_found(
+    populate: bool,
+    email: str,
+    address: str,
+    port: str,
+    root_id: str,
+) -> None:
+    assert populate
+    group_name: str = "group1"
+    result: Dict[str, Any] = await get_result(
+        address=address,
+        port=port,
+        group_name=group_name,
+        root_id=root_id,
+        user=email,
+    )
+    assert "errors" in result
+    assert (
+        result["errors"][0]["message"]
+        == "Exception - Access denied or root not found"
+    )
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group("add_toe_port")
+@pytest.mark.parametrize(
+    ["email", "address", "port", "root_id"],
+    [
+        [
+            "admin@fluidattacks.com",
+            "192.168.1.2",
+            "8080",
+            "83cadbdc-23f3-463a-9421-f50f8d0cb1e5",
+        ],
+        [
+            "admin@fluidattacks.com",
+            "192.168.1.1",
+            "8082",
+            "83cadbdc-23f3-463a-9421-f50f8d0cb1e6",
+        ],
+    ],
+)
+async def test_add_toe_port_ip_and_port_do_not_exist(
+    populate: bool,
+    email: str,
+    address: str,
+    port: str,
+    root_id: str,
+) -> None:
+    assert populate
+    group_name: str = "group1"
+    result: Dict[str, Any] = await get_result(
+        address=address,
+        port=port,
+        group_name=group_name,
+        root_id=root_id,
+        user=email,
+    )
+    assert "errors" in result
+    assert (
+        result["errors"][0]["message"]
+        == "Exception - The root does not have the IP and the port"
+    )
+
+
+@pytest.mark.asyncio
+@pytest.mark.resolver_test_group("add_toe_port")
+@pytest.mark.parametrize(
+    ["email", "address", "port", "root_id"],
+    [
+        [
+            "user@gmail.com",
+            "192.168.1.1",
+            "8080",
+            "83cadbdc-23f3-463a-9421-f50f8d0cb1e5",
+        ],
+    ],
+)
+async def test_add_toe_port_access_denied(
+    populate: bool,
+    email: str,
+    address: str,
+    port: str,
+    root_id: str,
+) -> None:
+    assert populate
+    group_name: str = "group1"
+    result: Dict[str, Any] = await get_result(
+        address=address,
+        port=port,
+        group_name=group_name,
+        root_id=root_id,
+        user=email,
+    )
+    assert "errors" in result
+    assert result["errors"][0]["message"] == "Access denied"
