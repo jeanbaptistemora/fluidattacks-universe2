66705dca98cb09549a6470ecbd29b7ab93a14067
Daniel Murcia <dmurcia@fluidattacks.com>
2022-11-22 13:07:00 -0500

    observes\feat(back): #8090.12 relation factory
    - add relation factory

---
+++ observes/etl/code/code_etl/client/_raw_file_commit/_factory.py
@@ -0,0 +1,39 @@
+# SPDX-FileCopyrightText: 2022 "Fluid Attacks <development@fluidattacks.com>"
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from code_etl.client._raw_objs import (
+    RawFileCommitRelation,
+)
+from code_etl.objs import (
+    CommitStamp,
+)
+from dataclasses import (
+    dataclass,
+)
+from fa_purity import (
+    PureIter,
+)
+from fa_purity.pure_iter.factory import (
+    from_flist,
+)
+
+
+@dataclass(frozen=True)
+class FileRelationFactory:
+    @staticmethod
+    def extract_relations(
+        stamp: CommitStamp,
+    ) -> PureIter[RawFileCommitRelation]:
+        def _build(file: str) -> RawFileCommitRelation:
+            return RawFileCommitRelation(
+                stamp.commit.commit_id.repo.namespace,
+                stamp.commit.commit_id.repo.repository,
+                stamp.commit.commit_id.hash.hash,
+                file,
+            )
+
+        empty: PureIter[RawFileCommitRelation] = from_flist(tuple())
+        return stamp.commit.data.files.map(
+            lambda f: from_flist(tuple(f)).map(_build)
+        ).value_or(empty)
