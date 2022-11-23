297982af459dd110ba75a571a4b5a872c30b053d
Jonathan Mesa <jmesa@fluidattacks.com>
2022-11-23 19:30:06 +0000

    integrates\refac(back): #7417 org finding policy model
    - migrate batch_dispatch to new org_finding_policy type

--- integrates/back/src/batch_dispatch/handle_finding_policy.py
+++ integrates/back/src/batch_dispatch/handle_finding_policy.py
@@ -15,6 +15,13 @@ from dataloaders import (
 from db_model.groups.types import (
     Group,
 )
+from db_model.organization_finding_policies.enums import (
+    PolicyStateStatus,
+)
+from db_model.organization_finding_policies.types import (
+    OrgFindingPolicy,
+    OrgFindingPolicyRequest,
+)
 from db_model.organizations.types import (
     Organization,
 )
@@ -24,7 +31,6 @@ from newutils import (
     groups as groups_utils,
 )
 from organizations_finding_policies.domain import (
-    get_finding_policy,
     update_finding_policy_in_groups,
 )
 from settings import (
@@ -52,15 +58,18 @@ async def handle_finding_policy(*, item: BatchProcessing) -> None:
     LOGGER_TRANSACTIONAL.info(":".join([item.subject, message]))
 
     organization_name = item.additional_info
-    finding_policy = await get_finding_policy(
-        org_name=organization_name, finding_policy_id=item.entity
+    loaders: Dataloaders = get_new_context()
+    finding_policy: OrgFindingPolicy = (
+        await loaders.organization_finding_policy.load(
+            OrgFindingPolicyRequest(
+                organization_name=organization_name, policy_id=item.entity
+            )
+        )
     )
-
     if finding_policy.state.status in {
-        "APPROVED",
-        "INACTIVE",
+        PolicyStateStatus.APPROVED,
+        PolicyStateStatus.INACTIVE,
     }:
-        loaders: Dataloaders = get_new_context()
         organization: Organization = await loaders.organization.load(
             organization_name
         )
@@ -70,7 +79,7 @@ async def handle_finding_policy(*, item: BatchProcessing) -> None:
         )
         active_groups = groups_utils.filter_active_groups(groups)
         active_group_names = [group.name for group in active_groups]
-        finding_name: str = finding_policy.metadata.name.lower()
+        finding_name: str = finding_policy.name.lower()
         (
             updated_finding_ids,
             updated_vuln_ids,
@@ -78,9 +87,9 @@ async def handle_finding_policy(*, item: BatchProcessing) -> None:
             loaders=loaders,
             finding_name=finding_name,
             group_names=active_group_names,
-            status=finding_policy.state.status,
+            status=finding_policy.state.status.value,
             user_email=item.subject,
-            tags=set(finding_policy.metadata.tags),
+            tags=set(finding_policy.tags),
         )
         await update_unreliable_indicators_by_deps(
             EntityDependency.handle_finding_policy,
