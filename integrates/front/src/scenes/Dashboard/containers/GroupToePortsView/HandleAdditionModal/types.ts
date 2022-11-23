2f77d544fdbf623812ef83242d8831601b1d743b
Andres Aguirre <aaguirre@fluidattacks.com>
2022-11-23 15:32:49 +0000

    integrates\feat(front): #8083.47 types to add toe port
    - Add all related types to add a new toe port with a modal

---
+++ integrates/front/src/scenes/Dashboard/containers/GroupToePortsView/HandleAdditionModal/types.ts
@@ -0,0 +1,52 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+interface IFormValues {
+  address: string;
+  port: string;
+  rootId: string;
+}
+
+interface IAddToePortResultAttr {
+  addToePort: {
+    success: boolean;
+  };
+}
+
+interface IHandleAdditionModalProps {
+  groupName: string;
+  handleCloseModal: () => void;
+  refetchData: () => void;
+}
+
+interface IGitRootAttr {
+  __typename: "GitRoot";
+}
+
+interface IIPRootAttr {
+  __typename: "IPRoot";
+  address: string;
+  id: string;
+  nickname: string;
+  port: number;
+  state: "ACTIVE" | "INACTIVE";
+}
+
+interface IURLRootAttr {
+  __typename: "URLRoot";
+}
+
+type Root = IGitRootAttr | IIPRootAttr | IURLRootAttr;
+
+export type {
+  IFormValues,
+  IHandleAdditionModalProps,
+  IAddToePortResultAttr,
+  Root,
+  IGitRootAttr,
+  IIPRootAttr,
+  IURLRootAttr,
+};
