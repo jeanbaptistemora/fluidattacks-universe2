67cc204ef58502f1af084ff0995124e46280b791
Luis Patino <lpatino@fluidattacks.com>
2022-11-24 18:43:41 -0500

    skims\feat(back): #8159.9 f337 csfr for c sharp
    - Method working correctly.
    - Licenses deleted for f280 files.

--- skims/skims/lib_root/f337/c_sharp.py
+++ skims/skims/lib_root/f337/c_sharp.py
@@ -1,7 +1,3 @@
-# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
-#
-# SPDX-License-Identifier: MPL-2.0
-
 from lib_sast.types import (
     ShardDb,
 )
@@ -10,22 +6,66 @@ from model.core_model import (
     Vulnerabilities,
 )
 from model.graph_model import (
+    Graph,
     GraphDB,
+    GraphShard,
     GraphShardMetadataLanguage as GraphLanguage,
     GraphShardNode,
+    NId,
 )
 from sast.query import (
     get_vulnerabilities_from_n_ids,
 )
 from typing import (
     Iterable,
+    List,
 )
 from utils import (
     graph as g,
 )
 
 
-def has_not_csfr_protection(
+def get_inner_nodes(graph: Graph, node: NId, label_type: str) -> List[NId]:
+    return g.match_ast_group(graph, node, label_type, depth=-1)[label_type]
+
+
+def controller_class_is_safe(shard: GraphShard, n_id: NId) -> bool:
+    return bool(
+        shard.syntax_graph
+        and (
+            class_filter := g.match_ast(
+                shard.syntax_graph, n_id, "Attribute", depth=2
+            )["Attribute"]
+        )
+        and (attribute_node := shard.syntax_graph.nodes[class_filter])
+        and (attribute_node["name"] == "AutoValidateAntiforgeryToken")
+    )
+
+
+def get_vulns(graph: Graph, n_id: NId) -> Iterable[NId]:
+    unsafe_attributes: List[str] = [
+        "HttpPost",
+        "HttpPut",
+        "HttpDelete",
+        "HttpPatch",
+    ]
+
+    method_declarations: List[NId] = get_inner_nodes(
+        graph, n_id, "MethodDeclaration"
+    )
+
+    for method_node in method_declarations:
+        if attr_node := graph.nodes[method_node].get("attributes_id"):
+            attrs = get_inner_nodes(graph, attr_node, "Attribute")
+            names = [graph.nodes[attr].get("name") for attr in attrs]
+            if (
+                any(unsafe_attr in names for unsafe_attr in unsafe_attributes)
+                and "ValidateAntiForgeryToken" not in names
+            ):
+                yield method_node
+
+
+def has_not_csfr_protection(  # NOSONAR
     shard_db: ShardDb,  # NOSONAR # pylint: disable=unused-argument
     graph_db: GraphDB,
 ) -> Vulnerabilities:
@@ -35,30 +75,21 @@ def has_not_csfr_protection(
         for shard in graph_db.shards_by_language(
             GraphLanguage.CSHARP,
         ):
+
             if shard.syntax_graph is None:
                 continue
-            graph = shard.syntax_graph
+            graph: Graph = shard.syntax_graph
 
             for n_id in g.filter_nodes(
                 graph,
                 graph.nodes,
                 g.pred_has_labels(label_type="Class"),
             ):
-                if (
-                    (
-                        class_filter := g.match_ast(
-                            shard.syntax_graph, n_id, "Attribute", depth=2
-                        )["Attribute"]
-                    )
-                    and (attribute_node := graph.nodes[class_filter])
-                    and (
-                        attribute_node["name"]
-                        == "AutoValidateAntiforgeryToken"
-                    )
-                ):
+                if controller_class_is_safe(shard, n_id):
                     continue
 
-                yield shard, n_id
+                for vuln in get_vulns(graph, n_id):
+                    yield shard, vuln
 
     return get_vulnerabilities_from_n_ids(
         desc_key="lib_root.f337.cs_has_not_csfr_protection",
