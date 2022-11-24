2e27673bf20b6c0ed459d541efb9696a3d8fb8ce
Daniel Salazar <dsalazar@fluidattacks.com>
2022-11-23 19:31:37 -0500

    reviews\refac(cross): #5226 rm license headers
    - Remove license headers as we now use dep5
    - no-deltas-test

--- reviews/pipeline/makes.nix
+++ reviews/pipeline/makes.nix
@@ -1,6 +1,3 @@
-# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
-#
-# SPDX-License-Identifier: MPL-2.0
 {gitlabCi, ...}: let
   gitlabBranchNotTrunk = gitlabCi.rules.branchNot "trunk";
 
