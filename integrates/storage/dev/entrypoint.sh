9bd6e38f6efae7dafc867c0bc981c28a7c585e67
Daniel Salazar <dsalazar@fluidattacks.com>
2022-11-24 16:44:12 -0500

    integrates\feat(build): #5226 populate bucket
    - Prepare data for syncing with s3
    - Adapt infra to receive branch

--- integrates/storage/dev/entrypoint.sh
+++ integrates/storage/dev/entrypoint.sh
@@ -1,16 +1,38 @@
-# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
-#
-# SPDX-License-Identifier: MPL-2.0
-
 # shellcheck shell=bash
+
+function _prepare_data {
+  local data="${1}"
+  local branch="${2}"
+  local bill_year
+  local bill_month
+
+  : \
+    && bill_year="$(date +'%Y')" \
+    && bill_month="$(date +'%m')" \
+    && mv \
+      "${data}/analytics/branch" \
+      "${data}/analytics/${branch}" \
+    && mv \
+      "${data}/continuous-data/bills/year" \
+      "${data}/continuous-data/bills/${bill_year}" \
+    && mv \
+      "${data}/continuous-data/bills/${bill_year}/month" \
+      "${data}/continuous-data/bills/${bill_year}/${bill_month}"
+}
 
 function main {
   local data="__argData__"
+  local tmp
+  local branch
   local endpoint
 
   : \
+    && branch="${CI_COMMIT_REF_NAME}" \
     && deploy-terraform-for-integratesStorageDev \
-    && endpoint="${CI_COMMIT_REF_NAME}.integrates" \
+    && tmp="$(mktemp -d)" \
+    && copy "${data}" "${tmp}" \
+    && _prepare_data "${tmp}" "${branch}" \
+    && endpoint="integrates.${branch}" \
     && aws_s3_sync \
       "${data}" \
       "s3://${endpoint}" \
--- integrates/storage/infra/src/storage.tf
+++ integrates/storage/infra/src/storage.tf
@@ -1,11 +1,7 @@
-# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
-#
-# SPDX-License-Identifier: MPL-2.0
-
 variable "branch" {}
 
 locals {
-  endpoint = var.branch == "trunk" ? "prod.integrates" : "${var.branch}.integrates"
+  endpoint = "integrates.${var.branch}"
   tags = {
     area = var.branch == "trunk" ? "cost" : "innovation"
   }
