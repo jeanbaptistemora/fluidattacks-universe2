a07b29c4c43f2ba9c0051a9facd1c580aac8aab9
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-11-24 15:03:30 -0500

    skims\feat(back): #6769.120 add method to model
    - Add method for f400 to the model
    - Speed: 23 methods / 17 days = 1.35 method/day
    - TODO: 4 methods
    - ETA: 4 / 1.35 = 2.96 days
    - no-deltas-test

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -2416,6 +2416,14 @@ class MethodsEnum(Enum):
         developer=DeveloperEnum.LUIS_SAAVEDRA,
         technique=TechniqueEnum.DAST,
     )
+    AWS_CLOUDTRAIL_FILES_NOT_VALIDATED = MethodInfo(
+        file_name="aws",
+        name="cloudtrail_files_not_validated",
+        module="dast",
+        finding=FindingEnum.F400,
+        developer=DeveloperEnum.LUIS_SAAVEDRA,
+        technique=TechniqueEnum.DAST,
+    )
     CFN_EC2_SEC_GROUPS_RFC1918 = MethodInfo(
         file_name="cloudformation",
         name="cfn_ec2_has_security_groups_ip_ranges_in_rfc1918",
