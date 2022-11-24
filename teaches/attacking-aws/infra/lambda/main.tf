f5da39578bf2c129c0830136ce41fbc06cc7cbf1
Daniel Salazar <dsalazar@fluidattacks.com>
2022-11-23 19:21:44 -0500

    teaches\refac(cross): #5226 rm license headers
    - Remove license headers as we now use dep5
    - no-deltas-test

--- teaches/attacking-aws/infra/lambda/main.tf
+++ teaches/attacking-aws/infra/lambda/main.tf
@@ -1,7 +1,3 @@
-# SPDX-FileCopyrightText: 2022 Fluid Attacks <development@fluidattacks.com>
-#
-# SPDX-License-Identifier: MPL-2.0
-
 resource "aws_lambda_function" "secure-function" {
   filename      = "lambda/secure_function.zip"
   function_name = "secure-function"
