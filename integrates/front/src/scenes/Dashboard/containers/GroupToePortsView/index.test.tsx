2932d6910b798f74393e8f7682270045a7ef0998
Andres Aguirre <aaguirre@fluidattacks.com>
2022-11-24 14:05:39 -0500

    integrates\test(front): #8083.55 edit be present on cell
    - Should edit be present on cell
    - no-deltas-test

--- integrates/front/src/scenes/Dashboard/containers/GroupToePortsView/index.test.tsx
+++ integrates/front/src/scenes/Dashboard/containers/GroupToePortsView/index.test.tsx
@@ -1,15 +1,17 @@
 import { MockedProvider } from "@apollo/client/testing";
 import type { MockedResponse } from "@apollo/client/testing";
 import { PureAbility } from "@casl/ability";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor, within } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import React from "react";
 import { MemoryRouter, Route } from "react-router-dom";
 
-import { GET_TOE_PORTS } from "./queries";
+import { GET_TOE_PORTS, UPDATE_TOE_PORT } from "./queries";
 import type { IToePortsConnection } from "./types";
 
 import { GroupToePortsView } from ".";
 import { authzPermissionsContext } from "utils/authz/config";
+import { msgSuccess } from "utils/notifications";
 
 jest.mock("../../../../utils/notifications", (): Record<string, unknown> => {
   const mockedNotifications: Record<string, () => Record<string, unknown>> =
@@ -196,5 +198,182 @@ describe("groupToePortsView", (): void => {
         .join("")
         .replace(/[^a-zA-Z ]/gu, "")
     );
+  });
+
+  it("should edit be present on cell", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    const mocksMutation: MockedResponse[] = [
+      {
+        request: {
+          query: UPDATE_TOE_PORT,
+          variables: {
+            address: "127.0.0.1",
+            bePresent: false,
+            canGetAttackedAt: true,
+            canGetAttackedBy: true,
+            canGetBePresentUntil: true,
+            canGetFirstAttackAt: true,
+            canGetSeenFirstTimeBy: true,
+            groupName: "unittesting",
+            hasRecentAttack: undefined,
+            port: 8080,
+            rootId: "1a32cab8-7b4c-4761-a0a5-85cb8b64ce68",
+            shouldGetNewToePort: true,
+          },
+        },
+        result: {
+          data: {
+            updateToePort: {
+              success: true,
+              toePort: {
+                __typename: "ToePort",
+                address: "127.0.0.1",
+                attackedAt: "2020-01-02T00:00:00-05:00",
+                attackedBy: "hacker@test.com",
+                bePresent: false,
+                bePresentUntil: "2022-01-02T00:00:00-05:00",
+                firstAttackAt: "2020-02-19T15:41:04+00:00",
+                hasVulnerabilities: false,
+                port: 8080,
+                root: {
+                  __typename: "IPRoot",
+                  id: "1a32cab8-7b4c-4761-a0a5-85cb8b64ce68",
+                  nickname: "test_nickname",
+                },
+                seenAt: "2000-01-01T05:00:00+00:00",
+                seenFirstTimeBy: "",
+              },
+            },
+          },
+        },
+      },
+    ];
+    const mockedToePorts: MockedResponse<{
+      group: { name: string; toePorts: IToePortsConnection };
+    }> = {
+      request: {
+        query: GET_TOE_PORTS,
+        variables: {
+          canGetAttackedAt: true,
+          canGetAttackedBy: true,
+          canGetBePresentUntil: true,
+          canGetFirstAttackAt: true,
+          canGetSeenFirstTimeBy: true,
+          first: 150,
+          groupName: "unittesting",
+        },
+      },
+      result: {
+        data: {
+          group: {
+            name: "unittesting",
+            toePorts: {
+              __typename: "ToePortsConnection",
+              edges: [
+                {
+                  node: {
+                    __typename: "ToePort",
+                    address: "127.0.0.1",
+                    attackedAt: "2020-01-02T00:00:00-05:00",
+                    attackedBy: "hacker@test.com",
+                    bePresent: true,
+                    bePresentUntil: null,
+                    firstAttackAt: "2020-02-19T15:41:04+00:00",
+                    hasVulnerabilities: false,
+                    port: 8080,
+                    root: {
+                      __typename: "IPRoot",
+                      id: "1a32cab8-7b4c-4761-a0a5-85cb8b64ce68",
+                      nickname: "test_nickname",
+                    },
+                    seenAt: "2000-01-01T05:00:00+00:00",
+                    seenFirstTimeBy: "",
+                  },
+                },
+              ],
+              pageInfo: {
+                endCursor: "bnVsbA==",
+                hasNextPage: false,
+              },
+            },
+          },
+        },
+      },
+    };
+    const mockedPermissions = new PureAbility<string>([
+      { action: "api_resolvers_toe_port_attacked_at_resolve" },
+      { action: "api_resolvers_toe_port_attacked_by_resolve" },
+      { action: "api_resolvers_toe_port_be_present_until_resolve" },
+      { action: "api_resolvers_toe_port_first_attack_at_resolve" },
+      { action: "api_resolvers_toe_port_seen_first_time_by_resolve" },
+      { action: "api_mutations_update_toe_port_mutate" },
+    ]);
+    render(
+      <MemoryRouter initialEntries={["/unittesting/surface/ports"]}>
+        <MockedProvider
+          addTypename={true}
+          mocks={[mockedToePorts, ...mocksMutation]}
+        >
+          <authzPermissionsContext.Provider value={mockedPermissions}>
+            <Route path={"/:groupName/surface/ports"}>
+              <GroupToePortsView isInternal={true} />
+            </Route>
+          </authzPermissionsContext.Provider>
+        </MockedProvider>
+      </MemoryRouter>
+    );
+    await waitFor((): void => {
+      expect(screen.queryByRole("table")).toBeInTheDocument();
+    });
+
+    await userEvent.click(
+      screen.getByRole("button", { name: "group.findings.tableSet.btn.text" })
+    );
+
+    await userEvent.click(
+      screen.getByRole("checkbox", {
+        checked: false,
+        name: "bePresent",
+      })
+    );
+    await userEvent.click(
+      screen.getByRole("checkbox", {
+        checked: false,
+        name: "bePresentUntil",
+      })
+    );
+
+    expect(
+      screen.getAllByRole("row")[0].textContent?.replace(/[^a-zA-Z ]/gu, "")
+    ).toStrictEqual(
+      [
+        "group.toe.ports.root",
+        "group.toe.ports.port",
+        "group.toe.ports.hasVulnerabilities",
+        "group.toe.ports.seenAt",
+        "group.toe.ports.bePresent",
+        "group.toe.ports.attackedAt",
+        "group.toe.ports.seenFirstTimeBy",
+        "group.toe.ports.bePresentUntil",
+      ]
+        .join("")
+        .replace(/[^a-zA-Z ]/gu, "")
+    );
+
+    const row = screen.getByRole("row", {
+      name: "test_nickname 8080 group.toe.ports.no 2000-01-01 bePresentSwitch Yes 2020-01-02",
+    });
+
+    await userEvent.click(
+      within(row).getByRole("checkbox", { name: "bePresentSwitch" })
+    );
+
+    await waitFor((): void => {
+      expect(msgSuccess).toHaveBeenCalledWith(
+        "group.toe.ports.alerts.updatePort",
+        "groupAlerts.updatedTitle"
+      );
+    });
   });
 });
