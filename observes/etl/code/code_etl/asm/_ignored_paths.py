f03adbc7aa36ac4d29afb98b5d41c6e06eaa5d22
Daniel Murcia <dmurcia@fluidattacks.com>
2022-11-25 13:22:34 -0500

    observes\feat(back): #8082.4 ignored paths
    - add get ignored paths cmd

---
+++ observes/etl/code/code_etl/asm/_ignored_paths.py
@@ -0,0 +1,94 @@
+from ._raw_client import (
+    GraphQlAsmClient,
+)
+from dataclasses import (
+    dataclass,
+)
+from fa_purity import (
+    Cmd,
+    FrozenList,
+    JsonObj,
+    JsonValue,
+    ResultE,
+)
+from fa_purity.frozen import (
+    freeze,
+)
+from fa_purity.json.value.transform import (
+    Unfolder,
+)
+from fa_purity.pure_iter.factory import (
+    from_flist,
+)
+from fa_purity.result.transform import (
+    all_ok,
+)
+
+
+@dataclass(frozen=True)
+class IgnoredPath:
+    nickname: str
+    file_path: str
+
+
+def _decode_ignored_paths(raw: JsonObj) -> ResultE[FrozenList[IgnoredPath]]:
+    nickname = (
+        Unfolder(JsonValue(raw))
+        .uget("nickname")
+        .bind(lambda u: u.to_primitive(str).alt(Exception))
+    )
+    files = (
+        Unfolder(JsonValue(raw))
+        .uget("gitignore")
+        .bind(lambda u: u.to_list_of(str).alt(Exception))
+    )
+    return nickname.bind(
+        lambda n: files.map(lambda fs: tuple(IgnoredPath(n, f) for f in fs))
+    ).alt(lambda e: Exception(f"decode_ignored_paths failed i.e. {e}"))
+
+
+def _decode_raw_ignored_paths(
+    raw: JsonObj,
+) -> ResultE[FrozenList[IgnoredPath]]:
+    return (
+        Unfolder(JsonValue(raw))
+        .uget("group")
+        .bind(
+            lambda u: u.uget("roots").bind(
+                lambda uu: uu.to_unfolder_list()
+                .alt(Exception)
+                .bind(
+                    lambda q: from_flist(q)
+                    .map(
+                        lambda i: i.to_json()
+                        .alt(Exception)
+                        .bind(_decode_ignored_paths)
+                    )
+                    .transform(lambda p: all_ok(p.to_list()))
+                )
+            )
+        )
+        .map(lambda i: from_flist(i).bind(lambda x: from_flist(x)).to_list())
+        .alt(lambda e: Exception(f"decode_raw_ignored_paths failed i.e. {e}"))
+    )
+
+
+def get_ignored_paths(
+    client: GraphQlAsmClient, group: str
+) -> Cmd[FrozenList[IgnoredPath]]:
+    query = """
+    query IgnoredPaths($groupName: String!){
+        group(groupName: $groupName){
+            roots {
+                ...on GitRoot{
+                    nickname
+                    gitignore
+                }
+            }
+        }
+    }
+    """
+    values = {"groupName": group}
+    return client.get(query, freeze(values)).map(
+        lambda j: _decode_raw_ignored_paths(j).unwrap()
+    )
