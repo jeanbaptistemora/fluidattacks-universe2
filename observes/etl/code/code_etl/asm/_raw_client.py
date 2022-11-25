47fb405c0dba4ef8a04848b29ca9fff06b34ae95
Daniel Murcia <dmurcia@fluidattacks.com>
2022-11-25 12:41:05 -0500

    observes\feat(back): #8082.3 asm client
    - add asm raw client

---
+++ observes/etl/code/code_etl/asm/_raw_client.py
@@ -0,0 +1,52 @@
+from __future__ import (
+    annotations,
+)
+
+from dataclasses import (
+    dataclass,
+)
+from fa_purity import (
+    Cmd,
+    JsonObj,
+)
+from fa_purity.frozen import (
+    FrozenDict,
+)
+from fa_purity.json.factory import (
+    from_any,
+)
+from gql import (
+    Client,
+    gql,
+)
+from gql.transport.requests import (
+    RequestsHTTPTransport,
+)
+from typing import (
+    Dict,
+)
+
+API_ENDPOINT = "https://app.fluidattacks.com/api"
+
+
+@dataclass(frozen=True)
+class _GraphQlAsmClient:
+    client: Client
+
+
+@dataclass(frozen=True)
+class GraphQlAsmClient:
+    _inner: _GraphQlAsmClient
+
+    @staticmethod
+    def new(token: str) -> GraphQlAsmClient:
+        headers: Dict[str, str] = {"Authorization": f"Bearer {token}"}
+        transport = RequestsHTTPTransport(API_ENDPOINT, headers)
+        client = Client(transport=transport, fetch_schema_from_transport=True)
+        return GraphQlAsmClient(_GraphQlAsmClient(client))
+
+    def get(self, query: str, values: FrozenDict[str, str]) -> Cmd[JsonObj]:
+        def _action() -> JsonObj:
+            return from_any(self._inner.client.execute(gql(query), dict(values))).unwrap()  # type: ignore[misc]
+
+        return Cmd.from_cmd(_action)
