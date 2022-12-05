e091e148af8b9645cf5e9949493e2427f979683a
Daniel Salazar <dsalazar@fluidattacks.com>
2022-12-05 16:29:04 -0500

    common\refac(build): #8268 rm issue part
    - Remove issue part as it is useless now

--- .lint-git-commit-msg/parser.js
+++ .lint-git-commit-msg/parser.js
@@ -1,6 +1,7 @@
 module.exports = {
   parserOpts: {
-    headerPattern: /^(airs|all|asserts|common|docs|forces|integrates|melts|observes|reviews|skims|sorts|teaches)\\(\w*)\((\w*)\):\s(#[1-9]\d*)(.\d+)?\s(.*)$/,
-    headerCorrespondence: [ 'product', 'type', 'scope', 'ticket', 'part', 'subject' ],
+    headerPattern:
+      /^(airs|all|asserts|common|docs|forces|integrates|melts|observes|reviews|skims|sorts|teaches)\\(\w*)\((\w*)\):\s(#[1-9]\d*)\s(.*)$/,
+    headerCorrespondence: ["product", "type", "scope", "ticket", "subject"],
   },
 };
