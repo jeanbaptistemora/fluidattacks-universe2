8a73d8d9d755b80be42bfc429833481293fbd4eb
Andres Saldarriaga <asaldarriaga@fluidattacks.com>
2022-12-05 18:06:37 +0000

    integrates\feat(front): #8225.3 update login view
    - add some useful resources

--- integrates/front/src/resources/index.tsx
+++ integrates/front/src/resources/index.tsx
@@ -1,4 +1,19 @@
 import lotCircleXMark from "./circleXMark.json";
+import loginBGR from "./loginBGR.png";
+import loginBitBucketLogo from "./loginBitBucketLogo.png";
+import loginGoogleLogo from "./loginGoogleLogo.png";
+import loginLogo from "./loginLogo.png";
+import loginMicrosoftLogo from "./loginMicrosoftLogo.png";
+import loginNewFeat from "./loginNewFeat.png";
 import lotScan from "./scan.json";
 
-export { lotCircleXMark, lotScan };
+export {
+  lotCircleXMark,
+  lotScan,
+  loginBGR,
+  loginNewFeat,
+  loginLogo,
+  loginGoogleLogo,
+  loginMicrosoftLogo,
+  loginBitBucketLogo,
+};
