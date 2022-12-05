da5118703429fb38db62e5a5c66dedc8d3a2e5e8
Jhon Romero <jromero@fluidattacks.com>
2022-12-05 12:07:30 -0500

    skims\feat(back): #8220.3 f428 json use of comments
    - fix report fp cases
    
    - no-deltas-test

--- skims/skims/lib_path/f428/json.py
+++ skims/skims/lib_path/f428/json.py
@@ -16,14 +16,14 @@ def _json_line_comments(
     content: str,
 ) -> Iterator[Any]:
     for line_number, line in enumerate(content.splitlines(), start=1):
-        if re.search(r"(([^\\:]|^)\/\/.*$)", line):
+        if re.search(r"(([^\S]|^)\/\/.*$)", line):
             yield (line_number, 0)
 
 
 def json_unapropiated_comment(content: str, path: str) -> Vulnerabilities:
     return get_vulnerabilities_from_iterator_blocking(
         content=content,
-        description_key="lib_path.f060.json_allowed_hosts",
+        description_key="lib_path.f428.json_unapropiated_comment",
         iterator=_json_line_comments(
             content=content,
         ),
