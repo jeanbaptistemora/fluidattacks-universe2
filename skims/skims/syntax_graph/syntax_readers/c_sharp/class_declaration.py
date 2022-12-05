de6728505e43d13288f1e2d8a54acc4d81f3db0a
Luis Patino <lpatino@fluidattacks.com>
2022-12-05 16:02:56 -0500

    skims\fix(back): #8269.1 c_sharp readers class declaration line
    - Class declaration node in syntax graph now have the correct line
    even when there are Action Filters.
    - .json unittest output updated.

--- skims/skims/syntax_graph/syntax_readers/c_sharp/class_declaration.py
+++ skims/skims/syntax_graph/syntax_readers/c_sharp/class_declaration.py
@@ -9,6 +9,7 @@ from syntax_graph.types import (
 )
 from utils.graph import (
     match_ast,
+    match_ast_d,
 )
 from utils.graph.text_nodes import (
     node_to_str,
@@ -17,10 +18,13 @@ from utils.graph.text_nodes import (
 
 def reader(args: SyntaxGraphArgs) -> NId:
     class_node = args.ast_graph.nodes[args.n_id]
+    declaration_n_id = match_ast_d(args.ast_graph, args.n_id, "identifier")
+    declaration_node = args.ast_graph.nodes[declaration_n_id]
+    declaration_line = declaration_node["label_l"]
+    args.ast_graph.nodes[args.n_id]["label_l"] = declaration_line
     name_id = class_node["label_field_name"]
     block_id = class_node["label_field_body"]
     name = node_to_str(args.ast_graph, name_id)
-
     match_childs = match_ast(args.ast_graph, args.n_id, "attribute_list")
     attributes = match_childs.get("attribute_list")
     return build_class_node(args, name, block_id, attributes)
