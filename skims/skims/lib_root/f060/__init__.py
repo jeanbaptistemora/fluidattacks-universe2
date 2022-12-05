a8b4ea78e8e2b6ffb9400a03da536d06108da2bb
Julian Gomez <ugomez@fluidattacks.com>
2022-12-05 16:57:22 -0500

    skims\feat(back): #8281 add method js f060
    - add js method for f060
    - add test
    - update test results

--- skims/skims/lib_root/f060/__init__.py
+++ skims/skims/lib_root/f060/__init__.py
@@ -1,6 +1,9 @@
 from lib_root.f060.c_sharp import (
     insecure_certificate_validation as csharp_insecure_certificate_validation,
 )
+from lib_root.f060.javascript import (
+    unsafe_origin as js_unsafe_origin,
+)
 from lib_root.f060.typescript import (
     unsafe_origin as ts_unsafe_origin,
 )
@@ -13,4 +16,5 @@ FINDING: core_model.FindingEnum = core_model.FindingEnum.F060
 QUERIES: graph_model.Queries = (
     (FINDING, csharp_insecure_certificate_validation),
     (FINDING, ts_unsafe_origin),
+    (FINDING, js_unsafe_origin),
 )
---
+++ skims/skims/lib_root/f060/javascript.py
@@ -0,0 +1,45 @@
+from lib_root.f060.common import (
+    has_dangerous_param,
+)
+from lib_sast.types import (
+    ShardDb,
+)
+from model.core_model import (
+    MethodsEnum,
+    Vulnerabilities,
+)
+from model.graph_model import (
+    GraphDB,
+    GraphShardMetadataLanguage as GraphLanguage,
+    GraphShardNode,
+)
+from sast.query import (
+    get_vulnerabilities_from_n_ids,
+)
+from typing import (
+    Iterable,
+)
+
+
+def unsafe_origin(
+    shard_db: ShardDb,  # NOSONAR # pylint: disable=unused-argument
+    graph_db: GraphDB,
+) -> Vulnerabilities:
+    method = MethodsEnum.JS_UNSAFE_ORIGIN
+
+    def n_ids() -> Iterable[GraphShardNode]:
+        for shard in graph_db.shards_by_language(
+            GraphLanguage.JAVASCRIPT,
+        ):
+            if shard.syntax_graph is None:
+                continue
+            graph = shard.syntax_graph
+            for n_id in has_dangerous_param(graph):
+                yield shard, n_id
+
+    return get_vulnerabilities_from_n_ids(
+        desc_key="lib_root.f060.common_unsafe_origin",
+        desc_params={},
+        graph_shard_nodes=n_ids(),
+        method=method,
+    )
