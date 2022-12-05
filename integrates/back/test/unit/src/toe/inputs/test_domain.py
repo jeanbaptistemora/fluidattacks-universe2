d20d522205babbd232eabe301702c243ba8ae4f8
Juan Chaves <jchaves@fluidattacks.com>
2022-12-05 18:17:29 +0000

    integrates\test(back): #7494.31 add historic toe inputs
    - Added a test to check for the deletion of historic toe
    input data

--- integrates/back/test/unit/src/toe/inputs/test_domain.py
+++ integrates/back/test/unit/src/toe/inputs/test_domain.py
@@ -1,3 +1,6 @@
+from custom_exceptions import (
+    ToeInputNotFound,
+)
 from dataloaders import (
     get_new_context,
 )
@@ -89,12 +92,48 @@ async def test_delete() -> None:
             root_id="",
         )
     )
+    historic_value = await loaders.toe_input_historic.load(
+        ToeInputRequest(
+            component="https://test.com/test/new.aspx",
+            entry_point="btnTest",
+            group_name=group_name,
+            root_id="",
+        )
+    )
+    assert historic_value == (
+        ToeInput(
+            attacked_at=datetime.fromisoformat("2021-02-12T05:00:00+00:00"),
+            attacked_by="test@test.com",
+            be_present=True,
+            be_present_until=None,
+            component="https://test.com/test/new.aspx",
+            entry_point="btnTest",
+            first_attack_at=datetime.fromisoformat(
+                "2021-02-12T05:00:00+00:00"
+            ),
+            has_vulnerabilities=False,
+            group_name="unittesting",
+            seen_at=datetime.fromisoformat("2000-01-01T05:00:00+00:00"),
+            seen_first_time_by="new@test.com",
+            unreliable_root_id="",
+            state=ToeInputState(modified_date="2022-11-11T05:00:00+00:00"),
+        ),
+    )
+
     await toe_inputs_domain.remove(current_value)
-    loaders = get_new_context()
-    group_toe_inputs = await loaders.group_toe_inputs.load_nodes(
+    group_toe_inputs = await loaders.group_toe_inputs.clear_all().load_nodes(
         GroupToeInputsRequest(group_name=group_name)
     )
     assert len(group_toe_inputs) == 5
+    with pytest.raises(ToeInputNotFound):
+        await loaders.toe_input_historic.clear_all().load(
+            ToeInputRequest(
+                component="https://test.com/test/new.aspx",
+                entry_point="btnTest",
+                group_name=group_name,
+                root_id="",
+            )
+        )
 
 
 @pytest.mark.changes_db
