ebda148b3e61cceba77de94a3ab5d7ff7fe3230e
David Acevedo <dacevedo@fluidattacks.com>
2022-12-05 17:08:52 +0000

    integrates\fix(back): #8081 company trial
    - added fall back while data is migrated

--- integrates/back/src/api/resolvers/organization/company.py
+++ integrates/back/src/api/resolvers/organization/company.py
@@ -3,6 +3,7 @@ from dataloaders import (
 )
 from db_model.companies.types import (
     Company,
+    Trial,
 )
 from db_model.organizations.types import (
     Organization,
@@ -24,4 +25,15 @@ async def resolve(
     domain = parent.created_by.split("@")[1]
     company: Optional[Company] = await loaders.company.load(domain)
 
-    return company
+    if company:
+        return company
+
+    return Company(
+        domain=domain,
+        trial=Trial(
+            completed=False,
+            extension_date="",
+            extension_days=0,
+            start_date="",
+        ),
+    )
