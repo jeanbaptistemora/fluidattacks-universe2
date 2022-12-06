be23cb22693d4c92f4a261444a8e8565f82a8b2a
Daniel Salazar <dsalazar@fluidattacks.com>
2022-12-06 18:46:02 -0500

    integrates\feat(build): #5226 deploy infra param
    - Allow user to decide whether or not to deploy infra

--- integrates/storage/dev/lib/populate/template.sh
+++ integrates/storage/dev/lib/populate/template.sh
@@ -1,5 +1,17 @@
 # shellcheck shell=bash
 
+function _deploy_infra {
+  deploy_infra="${1}"
+
+  if [ "${deploy_infra}" == "true" ]; then
+    deploy-terraform-for-integratesStorageDev
+  elif [ "${deploy_infra}" == "false" ]; then
+    info "Skipping infra deployment."
+  else
+    error "Must provide either 'true' or 'false'."
+  fi
+}
+
 function _prepare_data {
   local data="${1}"
   local branch="${2}"
@@ -21,7 +33,8 @@ function _prepare_data {
 }
 
 function populate {
-  local sync_path="${1:-}"
+  local deploy_infra="${1:-true}"
+  local sync_path="${2:-}"
   local data="__argData__"
   local mutable_data
   local branch
@@ -29,7 +42,7 @@ function populate {
 
   : \
     && branch="${CI_COMMIT_REF_NAME}" \
-    && deploy-terraform-for-integratesStorageDev \
+    && _deploy_infra "${deploy_infra}" \
     && mutable_data="$(mktemp -d)" \
     && copy "${data}" "${mutable_data}" \
     && _prepare_data "${mutable_data}" "${branch}" \
