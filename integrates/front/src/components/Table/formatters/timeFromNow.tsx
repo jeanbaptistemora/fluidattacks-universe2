c7a10fc334057d265399ee208ee3a5a2fab84f1d
Nicolas Carvajal <ncarvajal@fluidattacks.com>
2022-12-06 11:19:21 -0500

    integrates\feat(front): #7952 replace moment function
    - replace moment function

--- integrates/front/src/components/Table/formatters/timeFromNow.tsx
+++ integrates/front/src/components/Table/formatters/timeFromNow.tsx
@@ -1,9 +1,29 @@
-import moment from "moment";
-
 export const timeFromNow: (value: string) => string = (
   value: string
 ): string => {
-  const result: string = moment(value, "YYYY-MM-DD hh:mm:ss").fromNow();
+  const date = new Date(value);
 
-  return result === "Invalid date" ? "-" : result;
+  if (isNaN(date.getTime())) return "-";
+
+  const dateDiff = new Date().getTime() - date.getTime();
+
+  switch (true) {
+    case dateDiff < 60000:
+      return `${Math.floor(dateDiff / 1000)} seconds ago`;
+
+    case dateDiff < 60000 * 60:
+      return `${Math.floor(dateDiff / 60000)} minutes ago`;
+
+    case dateDiff < 60000 * 60 * 24:
+      return `${Math.floor(dateDiff / (60000 * 60))} hours ago`;
+
+    case dateDiff < 60000 * 60 * 24 * 30:
+      return `${Math.floor(dateDiff / (60000 * 60 * 24))} days ago`;
+
+    case dateDiff < 60000 * 60 * 24 * 30 * 12:
+      return `${Math.floor(dateDiff / (60000 * 60 * 24 * 30))} months ago`;
+
+    default:
+      return `${Math.floor(dateDiff / (60000 * 60 * 24 * 30 * 12))} years ago`;
+  }
 };
