ad175fc90fc4d7776f76bb452b9a01d359eee4c5
Julian Gomez <ugomez@fluidattacks.com>
2022-12-06 18:37:41 -0500

    skims\feat(back): #8297 enable method ts f004
    - enable method to find vulns
    - add test
    - update test results

--- skims/skims/lib_root/f004/__init__.py
+++ skims/skims/lib_root/f004/__init__.py
@@ -1,10 +1,16 @@
 from lib_root.f004.c_sharp import (
     remote_command_execution as c_sharp_remote_command_execution,
 )
+from lib_root.f004.typescript import (
+    remote_command_execution as ts_remote_command_execution,
+)
 from model import (
     core_model,
     graph_model,
 )
 
 FINDING: core_model.FindingEnum = core_model.FindingEnum.F004
-QUERIES: graph_model.Queries = ((FINDING, c_sharp_remote_command_execution),)
+QUERIES: graph_model.Queries = (
+    (FINDING, c_sharp_remote_command_execution),
+    (FINDING, ts_remote_command_execution),
+)
