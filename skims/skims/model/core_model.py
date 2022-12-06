ffddc6ec29b670c9b3e6e80be4fb190f5ece60cb
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-12-06 15:36:40 -0500

    skims\feat(back): #6769 add new method to model
    - Add method for f325 to the model

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -2432,6 +2432,14 @@ class MethodsEnum(Enum):
         developer=DeveloperEnum.LUIS_SAAVEDRA,
         technique=TechniqueEnum.DAST,
     )
+    AWS_IAM_IS_POLICY_MISS_CONFIGURED = MethodInfo(
+        file_name="aws",
+        name="iam_is_policy_miss_configured",
+        module="dast",
+        finding=FindingEnum.F325,
+        developer=DeveloperEnum.LUIS_SAAVEDRA,
+        technique=TechniqueEnum.DAST,
+    )
     CFN_EC2_SEC_GROUPS_RFC1918 = MethodInfo(
         file_name="cloudformation",
         name="cfn_ec2_has_security_groups_ip_ranges_in_rfc1918",
