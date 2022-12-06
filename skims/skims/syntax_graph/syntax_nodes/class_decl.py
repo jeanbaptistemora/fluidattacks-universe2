7d7469fda13927138a93609f3cc20ba3c0957fe1
Luis Patino <lpatino@fluidattacks.com>
2022-12-06 15:05:54 -0500

    skims\fix(back): #8296 multiple class level action filters fix
    - All class levels action filters are now took in account.
    - Updates .json output

--- skims/skims/syntax_graph/syntax_nodes/class_decl.py
+++ skims/skims/syntax_graph/syntax_nodes/class_decl.py
@@ -5,15 +5,48 @@ from syntax_graph.types import (
     SyntaxGraphArgs,
 )
 from typing import (
-    Optional,
+    Any,
 )
+from utils.graph import (
+    match_ast_d,
+)
+
+
+def add_main_node(
+    args: SyntaxGraphArgs, name_node: str, n_id: Any
+) -> SyntaxGraphArgs:
+    args.syntax_graph.nodes[args.n_id][name_node] = n_id
+    args.syntax_graph.add_edge(
+        args.n_id,
+        args.generic(args.fork_n_id(n_id)),
+        label_ast="AST",
+    )
+    return args
+
+
+def add_attributes(
+    args: SyntaxGraphArgs,
+    name_node: str,
+    childs: Any,
+) -> SyntaxGraphArgs:
+    args = add_main_node(args, name_node, childs[0])
+
+    for attribute_branch in childs[1:]:
+        attribute = match_ast_d(args.ast_graph, attribute_branch, "attribute")
+        if attribute:
+            args.syntax_graph.add_edge(
+                childs[0],
+                args.generic(args.fork_n_id(attribute)),
+                label_ast="AST",
+            )
+    return args
 
 
 def build_class_node(
     args: SyntaxGraphArgs,
     name: str,
     block_id: NId,
-    attributes_id: Optional[NId],
+    attributes_id: Any,
 ) -> NId:
     args.syntax_graph.add_node(
         args.n_id,
@@ -29,10 +62,6 @@ def build_class_node(
     )
 
     if attributes_id:
-        args.syntax_graph.add_edge(
-            args.n_id,
-            args.generic(args.fork_n_id(attributes_id)),
-            label_ast="AST",
-        )
+        args = add_attributes(args, name, attributes_id)
 
     return args.n_id
--- skims/skims/syntax_graph/syntax_readers/c_sharp/class_declaration.py
+++ skims/skims/syntax_graph/syntax_readers/c_sharp/class_declaration.py
@@ -8,8 +8,8 @@ from syntax_graph.types import (
     SyntaxGraphArgs,
 )
 from utils.graph import (
-    match_ast,
     match_ast_d,
+    match_ast_group,
 )
 from utils.graph.text_nodes import (
     node_to_str,
@@ -25,6 +25,6 @@ def reader(args: SyntaxGraphArgs) -> NId:
     name_id = class_node["label_field_name"]
     block_id = class_node["label_field_body"]
     name = node_to_str(args.ast_graph, name_id)
-    match_childs = match_ast(args.ast_graph, args.n_id, "attribute_list")
+    match_childs = match_ast_group(args.ast_graph, args.n_id, "attribute_list")
     attributes = match_childs.get("attribute_list")
     return build_class_node(args, name, block_id, attributes)
