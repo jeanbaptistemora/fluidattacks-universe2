8d797bf1fc31e09ee799c849924183f34012951d
Nicolas Carvajal <ncarvajal@fluidattacks.com>
2022-12-07 11:39:32 -0500

    integrates\feat(front): #7952 replace function with dayjs
    - replace function with dayjs

--- integrates/front/src/components/Table/formatters/timeFromNow.tsx
+++ integrates/front/src/components/Table/formatters/timeFromNow.tsx
@@ -1,29 +1,14 @@
+import dayjs, { extend } from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import _ from "lodash";
+
 export const timeFromNow: (value: string) => string = (
   value: string
 ): string => {
   const date = new Date(value);
+  if (_.isEmpty(value) || isNaN(date.getTime())) return "-";
 
-  if (isNaN(date.getTime())) return "-";
+  extend(relativeTime);
 
-  const dateDiff = new Date().getTime() - date.getTime();
-
-  switch (true) {
-    case dateDiff < 60000:
-      return `${Math.floor(dateDiff / 1000)} seconds ago`;
-
-    case dateDiff < 60000 * 60:
-      return `${Math.floor(dateDiff / 60000)} minutes ago`;
-
-    case dateDiff < 60000 * 60 * 24:
-      return `${Math.floor(dateDiff / (60000 * 60))} hours ago`;
-
-    case dateDiff < 60000 * 60 * 24 * 30:
-      return `${Math.floor(dateDiff / (60000 * 60 * 24))} days ago`;
-
-    case dateDiff < 60000 * 60 * 24 * 30 * 12:
-      return `${Math.floor(dateDiff / (60000 * 60 * 24 * 30))} months ago`;
-
-    default:
-      return `${Math.floor(dateDiff / (60000 * 60 * 24 * 30 * 12))} years ago`;
-  }
+  return dayjs(value, "YYYY-MM-DD hh:mm:ss").fromNow();
 };
