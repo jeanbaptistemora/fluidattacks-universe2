926e1a4d587757348fe17ad091a4318ccc6ac418
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-12-09 13:36:35 -0500

    skims\refac(back): #7612 remove migrated methods
    - remove migrated methods ebs
    - no-deltas-test

--- asserts/fluidasserts/cloud/aws/ebs.py
+++ asserts/fluidasserts/cloud/aws/ebs.py
@@ -40,45 +40,6 @@ def _get_snapshots(key_id, retry, secret, session_token, acc_id):
 
 @api(risk=HIGH, kind=DAST)
 @unknown_if(BotoCoreError, RequestException)
-def is_encryption_disabled(
-    key_id: str, secret: str, session_token: str = None, retry: bool = True
-) -> tuple:
-    """Check if an ``EBS volume`` has encryption disabled.
-
-    :param key_id: AWS Key Id
-    :param secret: AWS Key Secret
-    """
-    volumes = aws.run_boto3_func(
-        key_id=key_id,
-        secret=secret,
-        boto3_client_kwargs={"aws_session_token": session_token},
-        service="ec2",
-        func="describe_volumes",
-        param="Volumes",
-        retry=retry,
-    )
-
-    msg_open: str = "EBS Volumes have encryption disabled"
-    msg_closed: str = "EBS Volumes have encryption enabled"
-
-    vulns, safes = [], []
-    for volume in volumes:
-        (vulns if not volume.get("Encrypted") else safes).append(
-            (volume["VolumeId"], "has encryption disabled")
-        )
-
-    return _get_result_as_tuple(
-        service="EBS",
-        objects="Volumes",
-        msg_open=msg_open,
-        msg_closed=msg_closed,
-        vulns=vulns,
-        safes=safes,
-    )
-
-
-@api(risk=HIGH, kind=DAST)
-@unknown_if(BotoCoreError, RequestException)
 def uses_default_kms_key(
     key_id: str, secret: str, session_token: str = None, retry: bool = True
 ) -> tuple:
