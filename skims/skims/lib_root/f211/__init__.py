7cd51b8339c6fc8df0195c2250f3c20adba173bb
Julian Gomez <ugomez@fluidattacks.com>
2022-12-12 18:01:04 -0500

    skims\feat(back): #8316 add method ts
    - add method for ts
    - add test
    - update test results

--- skims/skims/lib_root/f211/__init__.py
+++ skims/skims/lib_root/f211/__init__.py
@@ -2,6 +2,9 @@ from lib_root.f211.c_sharp import (
     regex_injection as csharp_regex_injection,
     vuln_regular_expression as csharp_vuln_regular_expression,
 )
+from lib_root.f211.typescript import (
+    regex_injection as ts_regex_injection,
+)
 from model import (
     core_model,
     graph_model,
@@ -11,4 +14,5 @@ FINDING: core_model.FindingEnum = core_model.FindingEnum.F211
 QUERIES: graph_model.Queries = (
     (FINDING, csharp_vuln_regular_expression),
     (FINDING, csharp_regex_injection),
+    (FINDING, ts_regex_injection),
 )
