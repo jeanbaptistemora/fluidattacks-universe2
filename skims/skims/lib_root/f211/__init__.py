888297313fe66ae4a26cce60943edb878e786ebd
Jhon Romero <jromero@fluidattacks.com>
2022-12-12 21:30:11 -0500

    skims\feat(back): #8318 java regex vuln
    - add java ReDoS method
    
    - no-deltas-test

--- skims/skims/lib_root/f211/__init__.py
+++ skims/skims/lib_root/f211/__init__.py
@@ -2,14 +2,14 @@ from lib_root.f211.c_sharp import (
     regex_injection as csharp_regex_injection,
     vuln_regular_expression as csharp_vuln_regular_expression,
 )
+from lib_root.f211.java import (
+    java_vuln_regular_expression,
+)
 from lib_root.f211.javascript import (
     regex_injection as js_regex_injection,
 )
 from lib_root.f211.typescript import (
     regex_injection as ts_regex_injection,
-)
-from lib_root.f211.java import (
-    java_vuln_regular_expression,
 )
 from model import (
     core_model,
