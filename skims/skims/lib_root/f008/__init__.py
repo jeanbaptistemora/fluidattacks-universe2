d0be502ca78a51a0ea6899f297943cf582e2f351
Julian Gomez <ugomez@fluidattacks.com>
2022-12-13 16:05:44 -0500

    skims\feat(back): #8325 add js method f008
    - add js method for f008
    - add a test file
    - update test results

--- skims/skims/lib_root/f008/__init__.py
+++ skims/skims/lib_root/f008/__init__.py
@@ -1,3 +1,6 @@
+from lib_root.f008.javascript import (
+    reflected_xss as js_reflected_xss,
+)
 from lib_root.f008.typescript import (
     reflected_xss as ts_reflected_xss,
 )
@@ -7,4 +10,7 @@ from model import (
 )
 
 FINDING: core_model.FindingEnum = core_model.FindingEnum.F008
-QUERIES: graph_model.Queries = ((FINDING, ts_reflected_xss),)
+QUERIES: graph_model.Queries = (
+    (FINDING, js_reflected_xss),
+    (FINDING, ts_reflected_xss),
+)
