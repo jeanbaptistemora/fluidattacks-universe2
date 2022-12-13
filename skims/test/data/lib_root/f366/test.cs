6fa8c0ac0574b1553c2497c08296644cb885e79e
Luis Patino <lpatino@fluidattacks.com>
2022-12-13 12:45:10 -0500

    skims\test(back): #8294 f366 multiple filters handled
    - New capability tests.
    - Updates .csv output.

--- skims/test/data/lib_root/f366/test.cs
+++ skims/test/data/lib_root/f366/test.cs
@@ -3,16 +3,59 @@ using System.Security;
 
 namespace MyLibrary
 {
-
+    // Should fail pointing to lines 12 and 19
+    [FilterOne]
     [SecurityCritical]
-    public class Foo
+    [LastFilter]
+    public class HaveFails
     {
         [SecuritySafeCritical]
+        [Fone]
+        [Hello]
+        public void FailsFirsFilter()
+        {
+        }
+
+        [SecuritySafeCritical]
+        public void MethodWhithOneFilter()
+        {
+        }
+
+        public void MethodWithoutFilters()
+        {
+        }
+
+        [FilterOne]
+        [OtherFilter]
+        public void MethodWithSomeSafeFilters()
+        {
+        }
+
+    }
+
+    // Must not fail cause class has no insecure filter
+    [FilterOne]
+    [LastFilter]
+    public class MustNotFail
+    {
+        [Fone]
+        [SecuritySafeCritical]
+        [Hello]
         public void Bar()
         {
         }
 
-        public void Testeo()
+        [SecuritySafeCritical]
+        public void MethodWhithoutFilters()
+        {
+        }
+
+    }
+
+    // Chacks having no filters is not an issue
+    public class ControlClass
+    {
+        public void Bar()
         {
         }
     }
