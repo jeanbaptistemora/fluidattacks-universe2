aa0ce223f3cb9bfae6fc33a9bbd1a3513fe93d15
Daniel Murcia <dmurcia@fluidattacks.com>
2022-12-14 22:39:09 +0000

    observes\feat(back): #8082 filter stamps
    - add upload_filtered_stamps cmd

--- observes/etl/code/code_etl/upload_repo/actions.py
+++ observes/etl/code/code_etl/upload_repo/actions.py
@@ -23,6 +23,13 @@ from fa_purity.pure_iter.core import (
 from fa_purity.pure_iter.transform import (
     consume,
 )
+from fa_purity.stream.factory import (
+    from_piter,
+)
+from fa_purity.stream.transform import (
+    consume as consume_stream,
+    filter_maybe,
+)
 
 
 def register(client: Client, reg: Maybe[RepoRegistration]) -> Cmd[None]:
@@ -34,10 +41,24 @@ def register(client: Client, reg: Maybe[RepoRegistration]) -> Cmd[None]:
 
 
 def filter_ignored(
-    client: ArmClient, group: str, stamp: CommitStamp
+    client: ArmClient, stamp: CommitStamp
 ) -> Cmd[Maybe[CommitStamp]]:
-    ignored_paths = client.get_ignored_paths(group)
+    ignored_paths = client.get_ignored_paths(
+        stamp.commit.commit_id.repo.namespace
+    )
     return ignored_paths.map(lambda i: IgnoredFilter(i).filter_stamp(stamp))
+
+
+def upload_filtered_stamps(
+    client: Client, arm_client: ArmClient, stamps: PureIter[CommitStamp]
+) -> Cmd[None]:
+    _stamps = (
+        stamps.map(lambda s: filter_ignored(arm_client, s))
+        .transform(lambda x: from_piter(x))
+        .transform(lambda x: filter_maybe(x))
+    )
+    actions = _stamps.chunked(2000).map(client.insert_stamps)
+    return consume_stream(actions)
 
 
 def upload_stamps(client: Client, stamps: PureIter[CommitStamp]) -> Cmd[None]:
