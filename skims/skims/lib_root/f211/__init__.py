2fad285407a035d694d7b2bfa504d4b8d6b53d6d
Jhon Romero <jromero@fluidattacks.com>
2022-12-14 10:11:25 -0500

    skims\feat(back): #8318 java regex vuln
    - deactivate method

--- skims/skims/lib_root/f211/__init__.py
+++ skims/skims/lib_root/f211/__init__.py
@@ -2,9 +2,6 @@ from lib_root.f211.c_sharp import (
     regex_injection as csharp_regex_injection,
     vuln_regular_expression as csharp_vuln_regular_expression,
 )
-from lib_root.f211.java import (
-    java_vuln_regular_expression,
-)
 from lib_root.f211.javascript import (
     regex_injection as js_regex_injection,
 )
@@ -22,5 +19,4 @@ QUERIES: graph_model.Queries = (
     (FINDING, csharp_regex_injection),
     (FINDING, js_regex_injection),
     (FINDING, ts_regex_injection),
-    (FINDING, java_vuln_regular_expression),
 )
