119297799bc9940b1109697c25cbdf7c8d7ee277
Daniel Salazar <dsalazar@fluidattacks.com>
2022-12-15 16:32:33 +0000

    integrates\feat(infra): #5226 centralize infra
    - Create one single development bucket for all storage

--- integrates/infra/src/storage.tf
+++ integrates/infra/src/storage.tf
@@ -1,5 +1,4 @@
-# Integrates main bucket
-
+# Production
 resource "aws_s3_bucket" "integrates" {
   bucket = "integrates"
 
@@ -12,7 +11,7 @@ resource "aws_s3_bucket" "integrates" {
 }
 
 resource "aws_s3_bucket_acl" "integrates" {
-  bucket = "integrates"
+  bucket = aws_s3_bucket.integrates.id
 
   acl = "private"
 }
@@ -80,6 +79,93 @@ resource "aws_s3_bucket_cors_configuration" "integrates" {
     allowed_headers = ["*"]
     allowed_methods = ["PUT", "POST"]
     allowed_origins = ["https://app.fluidattacks.com", "https://localhost:*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
+
+# Development
+
+resource "aws_s3_bucket" "storage_dev" {
+  bucket = "integrates.dev"
+
+  tags = {
+    "Name"               = "integrates.dev"
+    "management:area"    = "innovation"
+    "management:product" = "integrates"
+    "management:type"    = "product"
+  }
+}
+
+resource "aws_s3_bucket_acl" "storage_dev" {
+  bucket = aws_s3_bucket.storage_dev.id
+
+  acl = "private"
+}
+
+resource "aws_s3_bucket_logging" "storage_dev" {
+  bucket = aws_s3_bucket.storage_dev.id
+
+  target_bucket = "common.logging"
+  target_prefix = "log/integrates.dev"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "storage_dev" {
+  bucket = aws_s3_bucket.storage_dev.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "storage_dev" {
+  bucket = aws_s3_bucket.storage_dev.id
+
+  rule {
+    id     = "analytics"
+    status = "Enabled"
+
+    filter {
+      prefix = "analytics/"
+    }
+    noncurrent_version_expiration {
+      noncurrent_days = 14
+    }
+    expiration {
+      days = 14
+    }
+  }
+  rule {
+    id     = "reports"
+    status = "Enabled"
+
+    filter {
+      prefix = "reports/"
+    }
+    expiration {
+      # 1 month + some timezone skews
+      days = 32
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "storage_dev" {
+  bucket = aws_s3_bucket.storage_dev.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_cors_configuration" "storage_dev" {
+  bucket = aws_s3_bucket.storage_dev.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["https://*.app.fluidattacks.com", "https://localhost:*"]
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
   }
