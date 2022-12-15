fd1c017c7272e5f5c1b45b895bdd4c6a207b3be7
David Acevedo <dacevedo@fluidattacks.com>
2022-12-15 17:48:38 +0000

    integrates\refac(front): #6080 rm css modules
    - now using styledcomponents for global styles

--- integrates/front/src/styles.ts
+++ integrates/front/src/styles.ts
@@ -46,6 +46,69 @@ const GlobalStyle = createGlobalStyle`
     content: "/";
     padding: 0 4px;
   }
+
+  .g-btn {
+    color: #333;
+    border: 1px solid transparent;
+    border-color: #ccc;
+  }
+
+  .g-a,
+  .g-a:hover,
+  .g-a:focus,
+  .g-a:visited {
+    color: #333;
+    text-decoration: none;
+  }
+
+  .questionBtn {
+    border: 0 !important;
+    color: #ff3435;
+  }
+
+  .questionBtn:hover {
+    background-color: unset !important;
+    color: #272727 !important;
+  }
+
+  .panel-cb {
+    border-color: #ddd;
+  }
+
+  .panel-ch {
+    color: #333;
+    background-color: #f5f5f5;
+    border-color: #ddd;
+  }
+
+  .g1 {
+    border: 1px solid transparent;
+    border-color: #ddd;
+    height: 402px;
+  }
+
+  .g2 {
+    border: 1px solid transparent;
+    border-color: #ddd;
+    height: 242px;
+  }
+
+  .g3 {
+    border: 1px solid transparent;
+    border-color: #ddd;
+    height: 162px;
+  }
+
+  .upload-file {
+    margin-right: 40px;
+    margin-left: 40px;
+  }
+
+  @media (max-width: 479px) {
+    .upload-file {
+      margin-top: 20px;
+    }
+  }
 `;
 
 export { GlobalStyle };
