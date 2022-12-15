53eadaf104569ec0729cb6191596d920b9e7226e
Julian Gomez <ugomez@fluidattacks.com>
2022-12-15 17:44:36 -0500

    skims\feat(back): #8349 add common file
    - add common file that stores the logic for both js an ts methods
    - refac name of translation

---
+++ skims/skims/lib_root/f297/common.py
@@ -0,0 +1,53 @@
+from model.core_model import (
+    MethodsEnum,
+)
+from model.graph_model import (
+    Graph,
+    NId,
+)
+from symbolic_eval.evaluate import (
+    evaluate,
+)
+from symbolic_eval.utils import (
+    get_backward_paths,
+)
+from typing import (
+    List,
+    Tuple,
+)
+from utils import (
+    graph as g,
+)
+from utils.string import (
+    split_on_last_dot,
+)
+
+
+def get_vuln_nodes(graph: Graph, method: MethodsEnum) -> List[str]:
+    vuln_nodes: List[str] = []
+    for nid in g.filter_nodes(
+        graph,
+        graph.nodes,
+        predicate=g.pred_has_labels(label_type="MethodInvocation"),
+    ):
+        f_name: Tuple[str, str] = split_on_last_dot(
+            graph.nodes[nid]["expression"]
+        )
+        if f_name[-1] == "query" and is_argument_vuln(graph, nid, method):
+            vuln_nodes.append(nid)
+
+    return vuln_nodes
+
+
+def is_argument_vuln(
+    graph: Graph,
+    n_id: NId,
+    method: MethodsEnum,
+) -> bool:
+    danger = {"UserConnection", "NonParametrizedQuery"}
+
+    for path in get_backward_paths(graph, n_id):
+        evaluation = evaluate(method, graph, path, n_id)
+        if evaluation and evaluation.triggers == danger:
+            return True
+    return False
