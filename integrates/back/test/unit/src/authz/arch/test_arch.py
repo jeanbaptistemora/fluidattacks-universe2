268767481f7c94ca801f0a9ab17a7d761be0b18b
Andres Uribe <auribe@fluidattacks.com>
2022-12-16 23:08:11 +0000

    integrates\feat(back): #8263 refact modules tests
    - refact api module test.
    - refacr authz module test.

--- integrates/back/test/unit/src/authz/arch/test_arch.py
+++ integrates/back/test/unit/src/authz/arch/test_arch.py
@@ -1,20 +1,29 @@
 from .arch import (
     project_dag,
 )
+from arch_lint import (
+    FullPathModule,
+)
 from arch_lint.dag.check import (
     check_dag_map,
+    dag_map_completeness,
 )
 from arch_lint.graph import (
     ImportGraph,
 )
 
-ROOT = "authz"
+root = FullPathModule.assert_module("authz")
 
 
 def test_dag_creation() -> None:
     project_dag()
 
 
+def test_dag_completeness() -> None:
+    graph = ImportGraph.from_modules(root, True)
+    dag_map_completeness(project_dag(), graph, root)
+
+
 def test_dag() -> None:
-    graph = ImportGraph.build_graph(ROOT, False)
+    graph = ImportGraph.from_modules(root, True)
     check_dag_map(project_dag(), graph)
