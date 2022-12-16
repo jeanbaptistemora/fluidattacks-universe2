d3977c9f4e2d9e040ec4334980aa151fb5fc625b
Frank Aristizabal <faristizabal@fluidattacks.com>
2022-12-15 19:00:04 -0500

    airs\refac(front): #8075 new blog index
    - Update components for new blogs pages.

---
+++ airs/front/src/scenes/BlogsToFilterPage/BlogsCta/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { Container } from "../../../components/Container";
+import { CtaBanner } from "../../../components/CtaBanner";
+import { translate } from "../../../utils/translations/translate";
+
+const BlogsCta: React.FC = (): JSX.Element => {
+  return (
+    <Container bgColor={"#dddde3"} ph={4} pv={5}>
+      <CtaBanner
+        button1Link={"/free-trial/"}
+        button1Text={"Start free trial"}
+        button2Link={"/services/continuous-hacking/"}
+        button2Text={"Learn more"}
+        image={"/airs/blogs/blogs-cta"}
+        matomoAction={"Blogs-free-trial"}
+        paragraph={translate.t("blog.ctaDescription")}
+        size={"big"}
+        sizeSm={"medium"}
+        title={translate.t("blog.ctaTitle")}
+      />
+    </Container>
+  );
+};
+
+export { BlogsCta };
