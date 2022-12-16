54c917143979fcd78c374b86f82a42bf8fa76b45
Andres Uribe <auribe@fluidattacks.com>
2022-12-16 15:59:27 +0000

    integrates\feat(back): #8263 root modules test
    - check dag completeness.
    - update arch-lint 2.1.1
    - removed test from api module until a new solution is implemented.
    - no-deltas-test

--- integrates/back/test/unit/src/api/arch/test_arch.py
+++ integrates/back/test/unit/src/api/arch/test_arch.py
@@ -1,20 +1,9 @@
 from .arch import (
     project_dag,
 )
-from arch_lint.dag.check import (
-    check_dag_map,
-)
-from arch_lint.graph import (
-    ImportGraph,
-)
 
 ROOT = "api"
 
 
 def test_dag_creation() -> None:
     project_dag()
-
-
-def test_dag() -> None:
-    graph = ImportGraph.build_graph(ROOT, False)
-    check_dag_map(project_dag(), graph)
--- integrates/back/test/unit/src/arch/test_arch.py
+++ integrates/back/test/unit/src/arch/test_arch.py
@@ -2,14 +2,16 @@ from .arch import (
     project_dag,
 )
 from arch_lint.dag.check import (
-    check_dag_map,
+    check_dag,
+    dag_completeness_from_dir,
 )
 from arch_lint.graph import (
     ImportGraph,
 )
+from pathlib import (
+    Path,
+)
 import pytest
-
-ROOT = "integrates"
 
 
 def test_dag_creation() -> None:
@@ -18,5 +20,9 @@ def test_dag_creation() -> None:
 
 @pytest.mark.xfail
 def test_dag() -> None:
-    graph = ImportGraph.build_graph(ROOT, True)
-    check_dag_map(project_dag(), graph)
+    graph = ImportGraph.from_modules(project_dag().all_modules, False)
+    check_dag(project_dag(), graph)
+
+
+def test_dag_completeness_from_dir() -> None:
+    dag_completeness_from_dir(project_dag(), Path("./back/src/"), True, None)
