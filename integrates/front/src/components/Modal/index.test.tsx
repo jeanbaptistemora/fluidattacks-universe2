59f5dd25a3012dc3e2a34c2e6a557a819bf0b9ba
Laura Naranjo <lnaranjo@fluidattacks.com>
2022-12-19 12:18:06 -0500

    integrates\test(front): #7323 increase coverage
    - modify test d non-functional part
    - add unit test

--- integrates/front/src/components/Modal/index.test.tsx
+++ integrates/front/src/components/Modal/index.test.tsx
@@ -1,4 +1,5 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import React from "react";
 
 import { Modal } from ".";
@@ -22,11 +23,28 @@ describe("Modal", (): void => {
     expect(screen.queryByText("Unit test title")).toBeInTheDocument();
   });
 
-  it("should call onClose and close on press esc", async (): Promise<void> => {
+  it("should call onClose after click button", async (): Promise<void> => {
     expect.hasAssertions();
 
     const handleClose: jest.Mock = jest.fn();
-    const { container, rerender } = render(
+    const user = userEvent.setup();
+    render(
+      <Modal onClose={handleClose} open={true} title={"Unit test title"}>
+        <p>{"Unit 2 modal content"}</p>
+      </Modal>
+    );
+
+    await user.click(screen.getByRole("button"));
+    await waitFor((): void => {
+      expect(handleClose).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it("should call onClose after press esc", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    const handleClose: jest.Mock = jest.fn();
+    const { container } = render(
       <Modal onClose={handleClose} open={true} title={"Unit test title"}>
         <p>{"Unit 2 modal content"}</p>
       </Modal>
@@ -35,14 +53,6 @@ describe("Modal", (): void => {
     fireEvent.keyDown(container, { key: "Escape" });
     await waitFor((): void => {
       expect(handleClose).toHaveBeenCalledTimes(1);
-
-      rerender(
-        <Modal open={false} title={"Unit test title"}>
-          <p>{"Unit 2 modal content"}</p>
-        </Modal>
-      );
     });
-
-    expect(screen.queryByText("Unit 2 modal content")).not.toBeInTheDocument();
   });
 });
