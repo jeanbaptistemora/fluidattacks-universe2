8bd540abc9ef6a05ef40bfc01f1900965d172b44
Andres Uribe <auribe@fluidattacks.com>
2022-12-19 17:08:51 +0000

    integrates\feat(back): #8263 add module test
    - added test to findings, machine.

---
+++ integrates/back/test/unit/src/findings/arch/arch.py
@@ -0,0 +1,28 @@
+from arch_lint.dag import (
+    DagMap,
+)
+from typing import (
+    Dict,
+    Tuple,
+    Union,
+)
+
+_dag: Dict[str, Tuple[Union[Tuple[str, ...], str], ...]] = {
+    "findings": (
+        "domain",
+        "storage",
+        "types",
+    ),
+    "findings.domain": (
+        "draft",
+        "core",
+        "evidence",
+    ),
+}
+
+
+def project_dag() -> DagMap:
+    result = DagMap.new(_dag)
+    if isinstance(result, Exception):
+        raise result  # pylint: disable=raising-bad-type
+    return result
---
+++ integrates/back/test/unit/src/machine/arch/arch.py
@@ -0,0 +1,22 @@
+from arch_lint.dag import (
+    DagMap,
+)
+from typing import (
+    Dict,
+    Tuple,
+    Union,
+)
+
+_dag: Dict[str, Tuple[Union[Tuple[str, ...], str], ...]] = {
+    "machine": (
+        "availability",
+        "jobs",
+    ),
+}
+
+
+def project_dag() -> DagMap:
+    result = DagMap.new(_dag)
+    if isinstance(result, Exception):
+        raise result  # pylint: disable=raising-bad-type
+    return result
