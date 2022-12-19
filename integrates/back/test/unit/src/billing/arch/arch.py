697ea22efa5c20447e29320313d8ec6a2d8e1326
Andres Uribe <auribe@fluidattacks.com>
2022-12-19 12:17:44 +0000

    integrates\feat(back): #8263 add module test
    - added tests to batch, billing and cli modules

---
+++ integrates/back/test/unit/src/billing/arch/arch.py
@@ -0,0 +1,19 @@
+from arch_lint.dag import (
+    DagMap,
+)
+from typing import (
+    Dict,
+    Tuple,
+    Union,
+)
+
+_dag: Dict[str, Tuple[Union[Tuple[str, ...], str], ...]] = {
+    "billing": ("domain", "dal", "types"),
+}
+
+
+def project_dag() -> DagMap:
+    result = DagMap.new(_dag)
+    if isinstance(result, Exception):
+        raise result  # pylint: disable=raising-bad-type
+    return result
