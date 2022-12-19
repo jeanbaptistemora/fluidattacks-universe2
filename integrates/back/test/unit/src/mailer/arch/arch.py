8ebfbc77c24974e35481599476966f960e60d760
Andres Uribe <auribe@fluidattacks.com>
2022-12-19 19:52:04 +0000

    integrates\feat(back): #8263 add module test
    - added test to mailer, newutils.

---
+++ integrates/back/test/unit/src/mailer/arch/arch.py
@@ -0,0 +1,29 @@
+from arch_lint.dag import (
+    DagMap,
+)
+from typing import (
+    Dict,
+    Tuple,
+    Union,
+)
+
+_dag: Dict[str, Tuple[Union[Tuple[str, ...], str], ...]] = {
+    "mailer": (
+        "analytics",
+        "findings",
+        "vulnerabilities",
+        "groups",
+        "types",
+        "events",
+        "deprecations",
+        "common",
+        "utils",
+    ),
+}
+
+
+def project_dag() -> DagMap:
+    result = DagMap.new(_dag)
+    if isinstance(result, Exception):
+        raise result  # pylint: disable=raising-bad-type
+    return result
---
+++ integrates/back/test/unit/src/new_utils/arch/arch.py
@@ -0,0 +1,52 @@
+from arch_lint.dag import (
+    DagMap,
+)
+from typing import (
+    Dict,
+    Tuple,
+    Union,
+)
+
+_dag: Dict[str, Tuple[Union[Tuple[str, ...], str], ...]] = {
+    "newutils": (
+        "organization_access",
+        "finding_comments",
+        "env",
+        "forms",
+        "compliance",
+        "string",
+        "encodings",
+        "requests",
+        "event_comments",
+        "group_comments",
+        "stakeholders",
+        "subscriptions",
+        "validations",
+        "git_self",
+        "organizations",
+        "vulnerabilities",
+        "templates",
+        "resources",
+        "reports",
+        "bugsnag",
+        "analytics",
+        "deprecations",
+        "logs",
+        "group_access",
+        "files",
+        "groups",
+        "utils",
+        "findings",
+        "forces",
+        "cvss",
+        "datetime",
+    ),
+    "newutils.deprecations": ("ast", "filters", "types"),
+}
+
+
+def project_dag() -> DagMap:
+    result = DagMap.new(_dag)
+    if isinstance(result, Exception):
+        raise result  # pylint: disable=raising-bad-type
+    return result
