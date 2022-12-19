bf0160da3300dce9a76488b3d1735a064691ce20
Julian Gomez <ugomez@fluidattacks.com>
2022-12-19 14:39:20 -0500

    skims\feat(back): #8351 add method js
    - add js method for f063
    - add test file
    - update test results
    - refac ts test file to cover vuln and safe cases

--- skims/skims/lib_root/f063/__init__.py
+++ skims/skims/lib_root/f063/__init__.py
@@ -6,6 +6,7 @@ from lib_root.f063.java import (
 )
 from lib_root.f063.javascript import (
     javascript_insecure_path_traversal as js_insecure_path_traversal,
+    zip_slip_injection as js_zip_slip_injection,
 )
 from lib_root.f063.typescript import (
     typescript_insecure_path_traversal,
@@ -21,6 +22,7 @@ QUERIES: graph_model.Queries = (
     (FINDING, c_sharp_open_redirect),
     (FINDING, java_zip_slip_injection),
     (FINDING, js_insecure_path_traversal),
+    (FINDING, js_zip_slip_injection),
     (FINDING, ts_zip_slip_injection),
     (FINDING, typescript_insecure_path_traversal),
 )
