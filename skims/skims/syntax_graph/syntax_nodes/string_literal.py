6aa539fee10f2712ab9aa12333340f70c07baba7
Fabio Lagos <flagos@fluidattacks.com>
2022-12-20 09:09:28 -0500

    skims\feat(back): #8353 template str add
    - generate child nodes for template string variables
    - adjust unittest results

--- skims/skims/syntax_graph/syntax_nodes/string_literal.py
+++ skims/skims/syntax_graph/syntax_nodes/string_literal.py
@@ -6,11 +6,16 @@ from syntax_graph.types import (
 )
 from typing import (
     Iterator,
+    List,
+    Optional,
 )
 
 
 def build_string_literal_node(
-    args: SyntaxGraphArgs, value: str, c_ids: Iterator
+    args: SyntaxGraphArgs,
+    value: str,
+    c_ids: Optional[Iterator] = None,
+    declaration_vars: Optional[List[str]] = None,
 ) -> NId:
     args.syntax_graph.add_node(
         args.n_id,
@@ -19,11 +24,31 @@ def build_string_literal_node(
         label_type="Literal",
     )
 
-    for c_id in c_ids:
-        args.syntax_graph.add_edge(
-            args.n_id,
-            args.generic(args.fork_n_id(c_id)),
-            label_ast="AST",
-        )
+    if c_ids:
+        for c_id in c_ids:
+            args.syntax_graph.add_edge(
+                args.n_id,
+                args.generic(args.fork_n_id(c_id)),
+                label_ast="AST",
+            )
+
+    if declaration_vars:
+        new_id = len(args.ast_graph) + 1
+        while args.syntax_graph.nodes.get(str(new_id)):
+            new_id = new_id + 1
+
+        for var_name in declaration_vars:
+            _id = str(new_id)
+            args.syntax_graph.add_node(
+                _id,
+                symbol=var_name,
+                label_type="SymbolLookup",
+            )
+            args.syntax_graph.add_edge(
+                args.n_id,
+                _id,
+                label_ast="AST",
+            )
+            new_id = new_id + 1
 
     return args.n_id
