af939c0da2a2ad54942bf65c40864a6ec8676621
Luis Saavedra <lsaavedra@fluidattacks.com>
2022-12-20 11:14:27 -0500

    skims\feat(back): #7612 add method to the model
    - Add method to the model for F200

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -2525,6 +2525,14 @@ class MethodsEnum(Enum):
         developer=DeveloperEnum.LUIS_SAAVEDRA,
         technique=TechniqueEnum.DAST,
     )
+    AWS_VPC_WITHOUT_FLOWLOG = MethodInfo(
+        file_name="aws",
+        name="vpcs_without_flowlog",
+        module="dast",
+        finding=FindingEnum.F200,
+        developer=DeveloperEnum.LUIS_SAAVEDRA,
+        technique=TechniqueEnum.DAST,
+    )
     CFN_EC2_SEC_GROUPS_RFC1918 = MethodInfo(
         file_name="cloudformation",
         name="cfn_ec2_has_security_groups_ip_ranges_in_rfc1918",
