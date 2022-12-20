7a6692f8299c6d5ad6bbcf8d9cf408f5a8c0e60d
Andres Uribe <auribe@fluidattacks.com>
2022-12-20 19:49:15 +0000

    integrates\feat(back): #8263 add module test
    - added module test to organizations and reports

---
+++ integrates/back/test/unit/src/reports/arch/arch.py
@@ -0,0 +1,34 @@
+from arch_lint.dag import (
+    DagMap,
+)
+from typing import (
+    Dict,
+    Tuple,
+    Union,
+)
+
+_dag: Dict[str, Tuple[Union[Tuple[str, ...], str], ...]] = {
+    "reports": (
+        "domain",
+        "report_types",
+        "standard_report",
+        "certificate",
+        "pdf",
+        "secure_pdf",
+        "it_report",
+        "typing",
+    ),
+    "reports.report_types": (
+        "data",
+        "technical",
+        "certificate",
+        "unfulfilled_standards",
+    ),
+}
+
+
+def project_dag() -> DagMap:
+    result = DagMap.new(_dag)
+    if isinstance(result, Exception):
+        raise result  # pylint: disable=raising-bad-type
+    return result
