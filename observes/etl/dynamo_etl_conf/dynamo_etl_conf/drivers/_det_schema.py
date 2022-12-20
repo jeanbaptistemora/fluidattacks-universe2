a80d10fd5d1ec5440644da0406b46ab1c0a6bf4a
Daniel Murcia <dmurcia@fluidattacks.com>
2022-12-20 12:53:16 -0500

    observes\feat(cross): #8391 determine schema
    - add determine schema driver
    - move job

---
+++ observes/etl/dynamo_etl_conf/dynamo_etl_conf/drivers/_det_schema.py
@@ -0,0 +1,27 @@
+from ._bins import (
+    BinPaths,
+)
+from dataclasses import (
+    dataclass,
+)
+from dynamo_etl_conf._run import (
+    external_run,
+)
+from fa_purity.cmd import (
+    Cmd,
+)
+from typing import (
+    FrozenSet,
+)
+
+
+def determine_schemas(
+    tables: FrozenSet[str], segments: int, cache_bucket: str
+) -> Cmd[None]:
+    args = (
+        BinPaths.DETERMINE_SCHEMAS.value,
+        " ".join(tables),
+        str(segments),
+        cache_bucket,
+    )
+    return external_run(args)
