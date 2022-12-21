837e978868578ae5da4fcec9b2b7466e4bc945f1
Jonathan Mesa <jmesa@fluidattacks.com>
2022-12-21 20:22:05 +0000

    integrates\fix(back): #7643 rm org access historics
    - include histotic related items in removal op for org access
    - these items are being left behind on revoking access

--- integrates/back/src/db_model/organization_access/remove.py
+++ integrates/back/src/db_model/organization_access/remove.py
@@ -1,3 +1,6 @@
+from boto3.dynamodb.conditions import (
+    Key,
+)
 from db_model import (
     TABLE,
 )
@@ -8,7 +11,12 @@ from dynamodb import (
     keys,
 )
 from dynamodb.operations import (
+    batch_delete_item,
     delete_item,
+    query,
+)
+from dynamodb.types import (
+    PrimaryKey,
 )
 
 
@@ -23,3 +31,29 @@ async def remove(*, email: str, organization_id: str) -> None:
     )
 
     await delete_item(key=primary_key, table=TABLE)
+
+    historic_key = keys.build_key(
+        facet=TABLE.facets["organization_historic_access"],
+        values={
+            "email": email,
+            "id": remove_org_id_prefix(organization_id),
+        },
+    )
+    key_structure = TABLE.primary_key
+    response = await query(
+        condition_expression=(
+            Key(key_structure.partition_key).eq(historic_key.partition_key)
+        ),
+        facets=(TABLE.facets["organization_historic_access"],),
+        table=TABLE,
+    )
+    await batch_delete_item(
+        keys=tuple(
+            PrimaryKey(
+                partition_key=item[key_structure.partition_key],
+                sort_key=item[key_structure.sort_key],
+            )
+            for item in response.items
+        ),
+        table=TABLE,
+    )
