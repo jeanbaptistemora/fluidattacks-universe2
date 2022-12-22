374533762729dadc261cda8e6819d2c3bbb13dde
Frank Aristizabal <faristizabal@fluidattacks.com>
2022-12-22 10:07:41 -0500

    airs\refac(front): #8075 new blog index
    - Add components for new blog index.

---
+++ airs/front/src/scenes/BlogPage/ContentSection/components/styledComponents.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const ImageBlockContainer = styled.div`
+  img {
+    display: block;
+    margin: auto;
+  }
+
+  p {
+    font-size: 0.75rem;
+    text-align: center;
+  }
+`;
+
+export { ImageBlockContainer };
