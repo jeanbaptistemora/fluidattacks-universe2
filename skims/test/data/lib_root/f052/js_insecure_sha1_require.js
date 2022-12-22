247f735c36105784c06022f1c768f481170a88de
Luis Patino <lpatino@fluidattacks.com>
2022-12-22 16:41:58 -0500

    skims\feat(back): #8408 f052 add js sha1 library use
    - translations fix.
    - Tests for new method.

---
+++ skims/test/data/lib_root/f052/js_insecure_sha1_require.js
@@ -0,0 +1,9 @@
+const anyName = require("js-sha1")
+
+// As js-sha1 library uses sha-1 algorithm all its uses must be marked.
+
+anyName("");
+anyName.hex("");
+anyName.array("");
+anyName.digest("");
+anyName.arrayBuffer('');
