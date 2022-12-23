596f5c929b7ef379a1bad02b6b06bd6101fe9cfe
Frank Aristizabal <faristizabal@fluidattacks.com>
2022-12-23 11:30:13 -0500

    airs\refac(front): #8075 new blog index
    - Add components for new blog index.

--- airs/front/src/scenes/BlogPage/ContentSection/components/styledComponents.tsx
+++ airs/front/src/scenes/BlogPage/ContentSection/components/styledComponents.tsx
@@ -12,4 +12,47 @@ const ImageBlockContainer = styled.div`
   }
 `;
 
-export { ImageBlockContainer };
+const TableBlockContainer = styled.div`
+  overflow-x: auto;
+
+  p {
+    font-size: 0.75rem;
+    text-align: center;
+  }
+
+  table {
+    border-collapse: collapse;
+    overflow-x: auto;
+    border-spacing: 0;
+    text-align: left;
+    width: 100%;
+
+    thead {
+      border-bottom: solid 2px #535365;
+    }
+
+    thead th {
+      color: #2e2e38;
+      font-size: 20px;
+      font-weight: 700;
+      padding: 1rem;
+    }
+
+    tbody tr {
+      background-color: #fff;
+      position: relative;
+    }
+
+    tbody tr:nth-child(even) {
+      background-color: #f4f4f6;
+    }
+
+    tbody td {
+      border-bottom: solid 1px #b0b0bf;
+      color: #535365;
+      padding: 1rem;
+    }
+  }
+`;
+
+export { ImageBlockContainer, TableBlockContainer };
