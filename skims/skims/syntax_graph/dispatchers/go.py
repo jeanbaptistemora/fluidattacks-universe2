8b6dd5075cd494f2ac2fd2662f383d7963e45e36
Julian Gomez <ugomez@fluidattacks.com>
2022-12-24 19:57:00 -0500

    skims\feat(back): #8407 go readers
    - add go readers

--- skims/skims/syntax_graph/dispatchers/go.py
+++ skims/skims/syntax_graph/dispatchers/go.py
@@ -1,4 +1,7 @@
 from syntax_graph.syntax_readers.go import (
+    block as go_block,
+    function_declaration as go_function_declaration,
+    identifier as go_identifier,
     import_declaration as go_import_declaration,
     package_clause as go_package_clause,
     source_file as go_source_file,
@@ -9,6 +12,24 @@ from syntax_graph.types import (
 )
 
 GO_DISPATCHERS: Dispatchers = (
+    Dispatcher(
+        applicable_types={
+            "block",
+        },
+        syntax_reader=go_block.reader,
+    ),
+    Dispatcher(
+        applicable_types={
+            "function_declaration",
+        },
+        syntax_reader=go_function_declaration.reader,
+    ),
+    Dispatcher(
+        applicable_types={
+            "identifier",
+        },
+        syntax_reader=go_identifier.reader,
+    ),
     Dispatcher(
         applicable_types={
             "import_declaration",
