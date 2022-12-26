7044f57f2362695dbdcddbefaba6183264de9c74
Julian Gomez <ugomez@fluidattacks.com>
2022-12-26 12:38:14 -0500

    skims\feat(back): #8407 go readers
    - add go readers

--- skims/skims/syntax_graph/dispatchers/go.py
+++ skims/skims/syntax_graph/dispatchers/go.py
@@ -1,11 +1,13 @@
 from syntax_graph.syntax_readers.go import (
     assignment_statement as go_assignment_statement,
     block as go_block,
+    call_expression as go_call_expression,
     expression_list as go_expression_list,
     function_declaration as go_function_declaration,
     identifier as go_identifier,
     import_declaration as go_import_declaration,
     package_clause as go_package_clause,
+    selector_expression as go_selector_expression,
     source_file as go_source_file,
 )
 from syntax_graph.types import (
@@ -29,6 +31,12 @@ GO_DISPATCHERS: Dispatchers = (
     ),
     Dispatcher(
         applicable_types={
+            "call_expression",
+        },
+        syntax_reader=go_call_expression.reader,
+    ),
+    Dispatcher(
+        applicable_types={
             "expression_list",
         },
         syntax_reader=go_expression_list.reader,
@@ -41,7 +49,10 @@ GO_DISPATCHERS: Dispatchers = (
     ),
     Dispatcher(
         applicable_types={
+            "blank_identifier",
             "identifier",
+            "field_identifier",
+            "package_identifier",
         },
         syntax_reader=go_identifier.reader,
     ),
@@ -56,6 +67,12 @@ GO_DISPATCHERS: Dispatchers = (
             "package_clause",
         },
         syntax_reader=go_package_clause.reader,
+    ),
+    Dispatcher(
+        applicable_types={
+            "selector_expression",
+        },
+        syntax_reader=go_selector_expression.reader,
     ),
     Dispatcher(
         applicable_types={
