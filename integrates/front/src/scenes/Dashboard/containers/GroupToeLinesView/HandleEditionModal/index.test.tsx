185076824d79051ba3a18239161d7caeed722166
Laura Naranjo <lnaranjo@fluidattacks.com>
2022-12-27 15:04:13 -0500

    integrates\test(front): #7323 increase coverage
    - modify unit test
    - add unit test to handle error

--- integrates/front/src/scenes/Dashboard/containers/GroupToeLinesView/HandleEditionModal/index.test.tsx
+++ integrates/front/src/scenes/Dashboard/containers/GroupToeLinesView/HandleEditionModal/index.test.tsx
@@ -1,15 +1,15 @@
 import type { MockedResponse } from "@apollo/client/testing";
 import { MockedProvider } from "@apollo/client/testing";
-import { PureAbility } from "@casl/ability";
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { GraphQLError } from "graphql";
 import React from "react";
+import { MemoryRouter, Route } from "react-router-dom";
 
 import { UPDATE_TOE_LINES_ATTACKED_LINES } from "./queries";
 
 import { HandleEditionModal } from ".";
 import type { IToeLinesData } from "../types";
-import { authzPermissionsContext } from "utils/authz/config";
 import { msgSuccess } from "utils/notifications";
 
 jest.mock("../../../../../utils/notifications", (): Record<string, unknown> => {
@@ -74,20 +74,22 @@ describe("handle toe lines edition modal", (): void => {
       },
     ];
     render(
-      <authzPermissionsContext.Provider value={new PureAbility([])}>
+      <MemoryRouter initialEntries={["/unittesting/surface/lines"]}>
         <MockedProvider
           addTypename={false}
           mocks={[...mocksMutation, ...mocksMutation, ...mocksMutation]}
         >
-          <HandleEditionModal
-            groupName={"groupname"}
-            handleCloseModal={handleCloseModal}
-            refetchData={handleRefetchData}
-            selectedToeLinesDatas={mokedToeLines}
-            setSelectedToeLinesDatas={jest.fn()}
-          />
+          <Route path={"/:groupName/surface/lines"}>
+            <HandleEditionModal
+              groupName={"groupname"}
+              handleCloseModal={handleCloseModal}
+              refetchData={handleRefetchData}
+              selectedToeLinesDatas={mokedToeLines}
+              setSelectedToeLinesDatas={jest.fn()}
+            />
+          </Route>
         </MockedProvider>
-      </authzPermissionsContext.Provider>
+      </MemoryRouter>
     );
 
     await userEvent.clear(screen.getByRole("spinbutton"));
@@ -108,5 +110,96 @@ describe("handle toe lines edition modal", (): void => {
       "group.toe.lines.editModal.alerts.success",
       "groupAlerts.updatedTitle"
     );
+  });
+
+  it("should handle error attacked lines edition", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    jest.clearAllMocks();
+
+    const handleRefetchData: jest.Mock = jest.fn();
+    const handleCloseModal: jest.Mock = jest.fn();
+    const mocksMutation: MockedResponse[] = [
+      {
+        request: {
+          query: UPDATE_TOE_LINES_ATTACKED_LINES,
+          variables: {
+            attackedLines: 9,
+            comments: "This is a test of updating toe lines",
+            filename: "test/test#.config",
+            groupName: "groupname",
+            rootId: "63298a73-9dff-46cf-b42d-9b2f01a56690",
+          },
+        },
+        result: {
+          errors: [
+            new GraphQLError(
+              "Exception - The attacked lines must be between 0 and the loc (lines of code)"
+            ),
+          ],
+        },
+      },
+    ];
+    const mokedToeLines: IToeLinesData[] = [
+      {
+        attackedAt: new Date("2021-02-20T05:00:00+00:00"),
+        attackedBy: "test2@test.com",
+        attackedLines: 4,
+        bePresent: true,
+        bePresentUntil: undefined,
+        comments: "comment 1",
+        coverage: 0.1,
+        daysToAttack: 4,
+        extension: "config",
+        filename: "test/test#.config",
+        firstAttackAt: new Date("2020-02-19T15:41:04+00:00"),
+        hasVulnerabilities: true,
+        lastAuthor: "user@gmail.com",
+        lastCommit: "f9e4beba70c4f34d6117c3b0c23ebe6b2bff66c2",
+        loc: 8,
+        modifiedDate: new Date("2020-11-15T15:41:04+00:00"),
+        root: {
+          id: "63298a73-9dff-46cf-b42d-9b2f01a56690",
+          nickname: "universe",
+        },
+        rootId: "63298a73-9dff-46cf-b42d-9b2f01a56690",
+        rootNickname: "universe",
+        seenAt: new Date("2020-02-01T15:41:04+00:00"),
+        sortsRiskLevel: 80,
+        sortsSuggestions: null,
+      },
+    ];
+    render(
+      <MemoryRouter initialEntries={["/unittesting/surface/lines"]}>
+        <MockedProvider
+          addTypename={false}
+          mocks={[...mocksMutation, ...mocksMutation, ...mocksMutation]}
+        >
+          <Route path={"/:groupName/surface/lines"}>
+            <HandleEditionModal
+              groupName={"groupname"}
+              handleCloseModal={handleCloseModal}
+              refetchData={handleRefetchData}
+              selectedToeLinesDatas={mokedToeLines}
+              setSelectedToeLinesDatas={jest.fn()}
+            />
+          </Route>
+        </MockedProvider>
+      </MemoryRouter>
+    );
+
+    await userEvent.clear(screen.getByRole("spinbutton"));
+    await userEvent.type(screen.getByRole("spinbutton"), "9");
+
+    await userEvent.type(
+      screen.getByRole("textbox"),
+      "This is a test of error in updating toe lines"
+    );
+
+    await userEvent.click(screen.getByText("components.modal.confirm"));
+
+    expect(
+      screen.getByText("This value must be between 0 and 8")
+    ).toBeInTheDocument();
   });
 });
