b0fc53c1630999dcaee82e8cfe70de813625b97a
Andres Uribe <auribe@fluidattacks.com>
2022-12-27 21:30:53 +0000

    integrates\feat(back): #8263 add module test
    - added module test to sessions, settings.

---
+++ integrates/back/test/unit/src/settings/arch/arch.py
@@ -0,0 +1,34 @@
+from arch_lint.dag import (
+    DagMap,
+)
+from typing import (
+    Dict,
+    Tuple,
+    Union,
+)
+
+_dag: Dict[str, Tuple[Union[Tuple[str, ...], str], ...]] = {
+    "settings": (
+        "jwt",
+        "uvicorn",
+        "session",
+        "logger",
+        "various",
+        "analytics",
+        "api",
+        "auth",
+        "statics",
+    ),
+    "settings.auth": (
+        "google",
+        "bitbucket",
+        "azure",
+    ),
+}
+
+
+def project_dag() -> DagMap:
+    result = DagMap.new(_dag)
+    if isinstance(result, Exception):
+        raise result  # pylint: disable=raising-bad-type
+    return result
