76463a908ca2315ae1af0692a99ee6481074af7a
Andres Uribe <auribe@fluidattacks.com>
2022-12-27 16:24:39 +0000

    integrates\feat(back): #8263 add module test
    - added module test to app, batch_dispatch.

---
+++ integrates/back/test/unit/src/app/arch/arch.py
@@ -0,0 +1,20 @@
+from arch_lint.dag import (
+    DagMap,
+)
+from typing import (
+    Dict,
+    Tuple,
+    Union,
+)
+
+_dag: Dict[str, Tuple[Union[Tuple[str, ...], str], ...]] = {
+    "app": ("app", "views", "middleware", "utils"),
+    "app.views": ("charts", "evidence", "oauth", "auth"),
+}
+
+
+def project_dag() -> DagMap:
+    result = DagMap.new(_dag)
+    if isinstance(result, Exception):
+        raise result  # pylint: disable=raising-bad-type
+    return result
