6840a455f7fdf81a4392357b00f2ab9135a17e1c
Andres Uribe <auribe@fluidattacks.com>
2022-12-27 16:59:22 +0000

    integrates\feat(back): #8263 add module test
    - added module test to companies, decorators, event_comments.

---
+++ integrates/back/test/unit/src/event_comments/arch/arch.py
@@ -0,0 +1,19 @@
+from arch_lint.dag import (
+    DagMap,
+)
+from typing import (
+    Dict,
+    Tuple,
+    Union,
+)
+
+_dag: Dict[str, Tuple[Union[Tuple[str, ...], str], ...]] = {
+    "event_comments": (),
+}
+
+
+def project_dag() -> DagMap:
+    result = DagMap.new(_dag)
+    if isinstance(result, Exception):
+        raise result  # pylint: disable=raising-bad-type
+    return result
