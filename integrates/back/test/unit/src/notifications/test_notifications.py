d2fae40c8875b10ea8357aa2f51ed482ec6c8567
Andres Uribe <auribe@fluidattacks.com>
2022-12-28 16:51:59 +0000

    integrates\feat(back): #8263 add module test
    - added module test to dataloaders, group_comments, notifications, sms.
    - no-deltas-test.

--- integrates/back/test/unit/src/notifications/test_notifications.py
+++ integrates/back/test/unit/src/notifications/test_notifications.py
@@ -19,4 +19,4 @@ async def test_recipient_first_name() -> None:
     assert first_name == "Integrates"
     email_no_name: str = "forces.unittesting@fluidattacks.com"
     first_name = await _get_recipient_first_name_async(loaders, email_no_name)
-    assert first_name == email_no_name.split("@")[0]
+    assert first_name == email_no_name.split("@", maxsplit=1)[0]
