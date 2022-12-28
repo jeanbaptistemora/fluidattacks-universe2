32f90e33cce96158beedb79cfd312051d1fe8e0a
Jonathan Mesa <jmesa@fluidattacks.com>
2022-12-28 19:19:40 +0000

    integrates\refac(back): #7473 rm stakeholder all orgs
    - modify scheduler by iterating over stakeholders to ease
    transition to all orgs
    - at this point we will continue to check only imamura
    - rm stakeholder using remove_stakeholder.domain

--- integrates/back/src/schedulers/remove_inactive_stakeholders.py
+++ integrates/back/src/schedulers/remove_inactive_stakeholders.py
@@ -17,6 +17,9 @@ from newutils import (
 from organizations import (
     domain as orgs_domain,
 )
+from remove_stakeholder.domain import (
+    remove_stakeholder_all_organizations,
+)
 from schedulers.common import (
     info,
 )
@@ -24,71 +27,57 @@ from schedulers.common import (
 INACTIVITY_DAYS = 90
 
 
-async def remove_stakeholder(
-    email: str,
+async def process_stakeholder(
+    loaders: Dataloaders,
     modified_by: str,
-    organization_id: str,
+    stakeholder: Stakeholder,
 ) -> None:
-    await orgs_domain.remove_access(
-        loaders=get_new_context(),
-        email=email,
+    if stakeholder.last_login_date is None:
+        return
+
+    inactivity_days = (
+        datetime_utils.get_utc_now() - stakeholder.last_login_date
+    ).days
+    if inactivity_days < INACTIVITY_DAYS:
+        return
+
+    await remove_stakeholder_all_organizations(
+        loaders=loaders,
+        email=stakeholder.email,
         modified_by=modified_by,
-        organization_id=organization_id,
     )
-    info("Inactive stakeholder removed", extra={"email": email})
+    info(
+        "Inactive stakeholder removed",
+        extra={
+            "email": stakeholder.email,
+            "inactivity_days": inactivity_days,
+        },
+    )
 
 
 async def remove_inactive_stakeholders() -> None:
     """
-    Remove stakeholders if only have access to imamura,
-    and there are no logins in the defined period.
+    Remove stakeholders for inactivity (no logins) in the defined period.
     """
-    modified_by = "integrates@fluidattacks.com"
     loaders: Dataloaders = get_new_context()
+    modified_by = "integrates@fluidattacks.com"
     org_name = "imamura"
     organization: Organization = await loaders.organization.load(org_name)
-    org_stakeholders: tuple[
+    all_stakeholders: tuple[
         Stakeholder, ...
     ] = await orgs_domain.get_stakeholders(loaders, organization.id)
-    inactive_stakeholders = [
-        stakeholder
-        for stakeholder in org_stakeholders
-        if (
-            stakeholder.last_login_date
-            and (
-                datetime_utils.get_plus_delta(
-                    stakeholder.last_login_date,
-                    days=INACTIVITY_DAYS,
-                )
-                < datetime_utils.get_utc_now()
-            )
-        )
-    ]
-    inactive_stakeholder_orgs = [
-        await loaders.stakeholder_organizations_access.load(
-            inactive_stakeholder.email
-        )
-        for inactive_stakeholder in inactive_stakeholders
-    ]
-    stakeholders_to_remove = [
-        inactive_stakeholder
-        for inactive_stakeholder, orgs in zip(
-            inactive_stakeholders, inactive_stakeholder_orgs
-        )
-        if len(orgs) == 1
-    ]
+    info("Stakeholders to process", extra={"item": len(all_stakeholders)})
     await collect(
         tuple(
-            remove_stakeholder(
-                email=stakeholder_to_remove.email,
-                organization_id=organization.id,
+            process_stakeholder(
+                loaders=loaders,
                 modified_by=modified_by,
+                stakeholder=stakeholder,
             )
-            for stakeholder_to_remove in stakeholders_to_remove
+            for stakeholder in all_stakeholders
         ),
-        workers=16,
+        workers=1,
     )
-    info("Stakeholders processed", extra={"len": len(stakeholders_to_remove)})
 
 
 async def main() -> None:
