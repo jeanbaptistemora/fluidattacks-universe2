c6040a2220d2e5e8a38307f7dc65c58eebd92f1b
Fabio Lagos <flagos@fluidattacks.com>
2022-12-29 09:11:50 -0500

    skims\refac(back): #8457 readers adjustments
    - adjust js readers to avoid empty attrs

--- skims/skims/syntax_graph/syntax_readers/javascript/for_each_statement.py
+++ skims/skims/syntax_graph/syntax_readers/javascript/for_each_statement.py
@@ -8,16 +8,23 @@ from syntax_graph.types import (
     MissingCaseHandling,
     SyntaxGraphArgs,
 )
+from utils.graph import (
+    match_ast,
+)
 
 
 def reader(args: SyntaxGraphArgs) -> NId:
-    node = args.ast_graph.nodes[args.n_id]
+    graph = args.ast_graph
+    node = graph.nodes[args.n_id]
     var_node = node.get("label_field_left")
     iterable_item = node.get("label_field_right")
     block_id = node.get("label_field_body")
 
     if not (var_node and iterable_item and block_id):
         raise MissingCaseHandling(f"Bad for statement handling in {args.n_id}")
+
+    if graph.nodes[block_id]["label_type"] == "expression_statement":
+        block_id = match_ast(graph, block_id)["__0__"]
 
     return build_for_each_statement_node(
         args, var_node, iterable_item, block_id
--- skims/skims/syntax_graph/syntax_readers/javascript/while_statement.py
+++ skims/skims/syntax_graph/syntax_readers/javascript/while_statement.py
@@ -5,21 +5,32 @@ from syntax_graph.syntax_nodes.while_statement import (
     build_while_statement_node,
 )
 from syntax_graph.types import (
+    MissingCaseHandling,
     SyntaxGraphArgs,
 )
 from utils.graph import (
+    adj_ast,
     match_ast,
 )
 
 
 def reader(args: SyntaxGraphArgs) -> NId:
+    graph = args.ast_graph
     match_childs = match_ast(
         args.ast_graph,
         args.n_id,
         "parenthesized_expression",
         "statement_block",
     )
-    block_node = str(match_childs["statement_block"])
-    conditional_node = str(match_childs["parenthesized_expression"])
+    block_id = match_childs.get("statement_block")
+    if not block_id:
+        raise MissingCaseHandling(
+            f"Bad while statement handling in {args.n_id}"
+        )
 
-    return build_while_statement_node(args, block_node, conditional_node)
+    if graph.nodes[block_id]["label_type"] == "expression_statement":
+        block_id = str(adj_ast(graph, block_id)[0])
+
+    conditional_id = match_childs.get("parenthesized_expression")
+
+    return build_while_statement_node(args, block_id, conditional_id)
