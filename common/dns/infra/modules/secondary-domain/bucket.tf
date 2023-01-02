efd3586e33f86c0b01fe2e21a6305f00daf920b4
Daniel Salazar <dsalazar@fluidattacks.com>
2023-01-02 23:57:51 +0000

    common\feat(infra): #8073 secondary domain module
    - Add tf module for secondary domains
    - no-deltas-test

---
+++ common/dns/infra/modules/secondary-domain/bucket.tf
@@ -0,0 +1,28 @@
+# Bucket to avoid domain takeover
+# https://community.cloudflare.com/t/cloudflare-s3-bucket-with-different-name-bucket-and-domain/193301
+
+resource "aws_s3_bucket" "main" {
+  bucket = var.domain
+}
+
+resource "aws_s3_bucket_acl" "main" {
+  bucket = aws_s3_bucket.main.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "main" {
+  bucket = aws_s3_bucket.main.id
+  versioning_configuration {
+    status = "Suspended"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
---
+++ common/dns/infra/modules/secondary-domain/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = "~> 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.4.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.10.0"
+    }
+  }
+}
+
+variable "domain" {}
