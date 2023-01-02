ae4b3feebe41ff141ecb3d0fea3e8fb28550e8af
Lewis Contreras <lcontreras@fluidattacks.com>
2023-01-02 12:03:23 -0500

    skims\feat(back): #8102 add php dev deps parser
    - Add composer_json_dev method for sca

---
+++ skims/skims/lib_path/f393/composer.py
@@ -0,0 +1,31 @@
+from lib_path.common import (
+    DependencyType,
+    pkg_deps_to_vulns,
+)
+from model.core_model import (
+    MethodsEnum,
+    Platform,
+)
+from parse_json import (
+    loads_blocking as json_loads_blocking,
+)
+from typing import (
+    Iterator,
+)
+
+
+# pylint: disable=unused-argument
+@pkg_deps_to_vulns(Platform.COMPOSER, MethodsEnum.COMPOSER_JSON_DEV)
+def composer_json_dev(  # NOSONAR
+    content: str, path: str
+) -> Iterator[DependencyType]:
+    content_json = json_loads_blocking(content, default={})
+
+    dependencies: Iterator[DependencyType] = (
+        (product, version)
+        for key in content_json
+        if key["item"] == "require-dev"
+        for product, version in content_json[key].items()
+    )
+
+    return dependencies
