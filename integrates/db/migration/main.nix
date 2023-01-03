bc40b7ffd848a79ec16aefa326ca28536abda502
Sebastian Lizcano <slizcano@fluidattacks.com>
2023-01-03 09:49:34 -0500

    integrates\refac(build): #8323 remove unnecessary code
    - remove not longer needed bin on migration

--- integrates/db/migration/main.nix
+++ integrates/db/migration/main.nix
@@ -1,17 +1,11 @@
 {
   makeScript,
   outputs,
-  inputs,
   ...
 }:
 makeScript {
   replace = {
     __argIntegratesBackEnv__ = outputs."/integrates/back/env";
-  };
-  searchPaths = {
-    bin = [
-      inputs.nixpkgs.ffmpeg
-    ];
   };
   name = "integrates-db-migration";
   entrypoint = ./entrypoint.sh;
