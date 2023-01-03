13c92d66c444fb2c43aca279614958ea4fe96d86
Daniel Murcia <dmurcia@fluidattacks.com>
2023-01-03 08:28:38 -0500

    observes\feat(back): #7982 dev env
    - add automatic conf setup at dev env

--- observes/etl/dynamo_etl_conf/env/dev/main.nix
+++ observes/etl/dynamo_etl_conf/env/dev/main.nix
@@ -5,12 +5,18 @@
   ...
 }: let
   system = "x86_64-linux";
-  pkg = (inputs.flakeAdapter {src = projectPath inputs.observesIndex.etl.dynamo.root;}).defaultNix;
-  env = pkg.outputs.packages."${system}".env.dev;
+  pkg = (inputs.flakeAdapter {src = projectPath inputs.observesIndex.etl.dynamo.root;}).defaultNix.outputs.packages."${system}";
+  env = pkg.env.dev;
 in
   makeTemplate {
+    name = "observes-etl-dynamo-etl-conf-env-dev";
     searchPaths = {
       bin = [env];
     };
-    name = "observes-etl-dynamo-etl-conf-env-dev";
+    replace = {
+      __argPython__ = inputs.nixpkgs.python310;
+      __argPythonEnv__ = env;
+      __argPythonEntry__ = ./vs_settings.py;
+    };
+    template = ./template.sh;
   }
