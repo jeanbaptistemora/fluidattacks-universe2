6e88a41baf6920dcfa03f3f685d4053f39ed6074
Luis Saavedra <lsaavedra@fluidattacks.com>
2023-01-04 10:13:30 -0500

    skims\feat(back): #7612 add method
    - Add method for f363

--- skims/skims/dast/aws/f363.py
+++ skims/skims/dast/aws/f363.py
@@ -53,7 +53,42 @@ async def not_requires_uppercase(
     return vulns
 
 
+async def not_requires_lowercase(
+    credentials: AwsCredentials,
+) -> core_model.Vulnerabilities:
+
+    response: Dict[str, Any] = await run_boto3_fun(
+        credentials, service="iam", function="get_account_password_policy"
+    )
+    user = await run_boto3_fun(credentials, service="iam", function="get_user")
+    vulns: core_model.Vulnerabilities = ()
+    password_policy = response.get("PasswordPolicy", [])
+    if not password_policy["RequireLowercaseCharacters"]:
+        locations = [
+            Location(
+                access_patterns=("/RequireLowercaseCharacters",),
+                arn=(f"{user['User']['Arn']}"),
+                values=(password_policy["RequireLowercaseCharacters"],),
+                description=("src.lib_path.f363.not_requires_lowercase"),
+            ),
+        ]
+
+        vulns = (
+            *vulns,
+            *build_vulnerabilities(
+                locations=locations,
+                method=(core_model.MethodsEnum.AWS_IAM_NOT_REQUIRES_LOWERCASE),
+                aws_response=password_policy,
+            ),
+        )
+
+    return vulns
+
+
 CHECKS: Tuple[
     Callable[[AwsCredentials], Coroutine[Any, Any, Tuple[Vulnerability, ...]]],
     ...,
-] = (not_requires_uppercase,)
+] = (
+    not_requires_uppercase,
+    not_requires_lowercase,
+)
