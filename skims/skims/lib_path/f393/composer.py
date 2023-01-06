59266cfc2dae8b43ad3de33237dad7ab009f8546
Lewis Contreras <lcontreras@fluidattacks.com>
2023-01-06 14:47:54 -0500

    skims\feat(back): #8102 add php method sca
    - Add composer_lock_dev method for sca

--- skims/skims/lib_path/f393/composer.py
+++ skims/skims/lib_path/f393/composer.py
@@ -29,3 +29,23 @@ def composer_json_dev(  # NOSONAR
     )
 
     return dependencies
+
+
+# pylint: disable=unused-argument
+@pkg_deps_to_vulns(Platform.COMPOSER, MethodsEnum.COMPOSER_LOCK)
+def composer_lock_dev(  # NOSONAR
+    content: str, path: str
+) -> Iterator[DependencyType]:
+    content_json = json_loads_blocking(content, default={})
+    for key in content_json:
+        if key["item"] == "packages-dev":
+            for line in content_json[key]["item"]:
+                cont = 0
+                info = []
+                for product in line.values():
+                    if cont >= 2:
+                        cont = 0
+                        break
+                    cont += 1
+                    info.append(product)
+                yield tuple(info)  # type: ignore
