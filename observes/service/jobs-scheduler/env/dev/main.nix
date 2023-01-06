aed33690d2c8d12308c86aa299c8b47157d352ac
Daniel Murcia <dmurcia@fluidattacks.com>
2023-01-06 08:30:45 -0500

    observes\feat(build): #8522 auto dev conf
    - add auto dev conf in jobs scheduler

--- observes/service/jobs-scheduler/env/dev/main.nix
+++ observes/service/jobs-scheduler/env/dev/main.nix
@@ -16,4 +16,10 @@ in
       bin = [env];
       export = import ../bins.nix {inherit outputs;};
     };
+    replace = {
+      __argPython__ = inputs.nixpkgs.python310;
+      __argPythonEnv__ = env;
+      __argPythonEntry__ = ./vs_settings.py;
+    };
+    template = ./template.sh;
   }
