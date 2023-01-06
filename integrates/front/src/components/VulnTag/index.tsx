596337d22233a1742ab8dcfb1c9a3f86406808c7
Nicolas Carvajal <ncarvajal@fluidattacks.com>
2023-01-06 09:58:53 -0500

    integrates\feat(front): #8459 change styles of vulntag component
    - change styles of vulntag component
    - add a separate file for vulntag component styles
    - no-deltas-test

--- integrates/front/src/components/VulnTag/index.tsx
+++ integrates/front/src/components/VulnTag/index.tsx
@@ -2,24 +2,83 @@ import _ from "lodash";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
-import { Tag } from "components/Tag";
-import { getBgColor } from "utils/colors";
+import type { ITagProps } from "./styles";
+import { NumberDisplay, Tag, VulnDisplay } from "./styles";
+
+import { Tooltip } from "components/Tooltip";
 
 interface IStatus {
-  value: string | undefined;
+  value: number | undefined;
 }
 
 const VulnTag: React.FC<IStatus> = ({ value }: IStatus): JSX.Element => {
   const { t } = useTranslation();
-  const formatedStatus: string = _.capitalize(value);
-  const currentStateBgColor = getBgColor(_.capitalize(value));
+  const SEVERITY_THRESHOLD_CRITICAL: number = 9;
+  const SEVERITY_THRESHOLD_HIGH: number = 6.9;
+  const SEVERITY_THRESHOLD_MED: number = 3.9;
+  const SEVERITY_THRESHOLD_LOW: number = 0.1;
+
+  function setSeverityLevel(
+    severity?: number
+  ): ["CRITICAL" | "HIGH" | "LOW" | "MED" | "NONE", string] {
+    if (severity === undefined) {
+      return ["NONE", t("searchFindings.header.severity.level.none")];
+    }
+    if (severity >= SEVERITY_THRESHOLD_CRITICAL) {
+      return ["CRITICAL", t("searchFindings.header.severity.level.critical")];
+    }
+    if (severity > SEVERITY_THRESHOLD_HIGH) {
+      return ["HIGH", t("searchFindings.header.severity.level.high")];
+    }
+    if (severity > SEVERITY_THRESHOLD_MED) {
+      return ["MED", t("searchFindings.header.severity.level.medium")];
+    }
+    if (severity >= SEVERITY_THRESHOLD_LOW) {
+      return ["LOW", t("searchFindings.header.severity.level.low")];
+    }
+
+    return ["NONE", t("searchFindings.header.severity.level.none")];
+  }
+
+  const severityConfigs: Record<
+    string,
+    { color: ITagProps["variant"]; text: string }
+  > = {
+    CRITICAL: {
+      color: "deepRed",
+      text: t("searchFindings.criticalSeverity"),
+    },
+    HIGH: {
+      color: "red",
+      text: t("searchFindings.highSeverity"),
+    },
+    LOW: { color: "orange", text: t("searchFindings.lowSeverity") },
+    MED: {
+      color: "yellow",
+      text: t("searchFindings.mediumSeverity"),
+    },
+    NONE: {
+      color: "none",
+      text: t("searchFindings.noneSeverity"),
+    },
+  };
+
+  const [severityLevel, severityLevelTooltip] = setSeverityLevel(value);
+  const { color, text: severityText } = severityConfigs[severityLevel];
+  const formatedStatus: string = _.capitalize(String(value));
 
   return (
-    <Tag variant={currentStateBgColor}>
-      {formatedStatus === "On_hold"
-        ? t("searchFindings.tabVuln.onHold")
-        : formatedStatus.split(" ")[0]}
-    </Tag>
+    <Tooltip
+      id={"severityTooltip"}
+      tip={t("searchFindings.header.severity.tooltip") + severityLevelTooltip}
+    >
+      <Tag>
+        <NumberDisplay variant={color}>
+          {formatedStatus.split(" ")[0]}
+        </NumberDisplay>
+        <VulnDisplay variant={color}>{severityText}</VulnDisplay>
+      </Tag>
+    </Tooltip>
   );
 };
 
