f39735f022215668e62fc24e46c2635c537f0b40
Daniel Murcia <dmurcia@fluidattacks.com>
2023-01-06 11:21:29 -0500

    observes\feat(job): #8523 retry final phase
    - retry final phase of the dynamo segment ETL

---
+++ observes/etl/dynamo/retry-redshift-upload/entrypoint.sh
@@ -0,0 +1,43 @@
+# shellcheck shell=bash
+
+alias tap-dynamo="observes-singer-tap-dynamo-bin"
+
+function dynamodb_etl {
+  local segment="${1}"
+
+  local db_creds
+  local schemas
+  local cache_bucket_folder
+  export AWS_DEFAULT_REGION="us-east-1"
+
+  if test "${segment}" == "auto"; then
+    segment="${AWS_BATCH_JOB_ARRAY_INDEX}"
+  fi \
+    && echo "[INFO] worker at segment: ${segment}" \
+    && db_creds=$(mktemp) \
+    && schemas=$(mktemp -d) \
+    && cache_bucket_folder="s3://observes.cache/dynamoEtl/vms_schema" \
+    && aws_login "prod_observes" "3600" \
+    && echo '[INFO] Generating secret files' \
+    && json_db_creds "${db_creds}" \
+    && export_notifier_key \
+    && echo "[INFO] Getting schemas cache at ${cache_bucket_folder}" \
+    && aws_s3_sync "${cache_bucket_folder}" "${schemas}" \
+    && echo '[INFO] Running Last ETL phase' \
+    && echo '{"type": "STATE", "value": {}}' > .input \
+    && tap-json \
+      --date-formats '%Y-%m-%d %H:%M:%S' \
+      --schema-folder "${schemas}" \
+      --schema-cache \
+      < .input \
+      > .singer \
+    && target-redshift from-s3 \
+      --schema-name "dynamodb_integrates_vms_part_${segment}" \
+      --bucket 'observes.etl-data' \
+      --prefix "dynamodb/part_${segment}/" \
+      --role 'arn:aws:iam::205810638802:role/redshift-role' \
+      < .singer \
+    && rm .singer
+}
+
+dynamodb_etl "${@}"
---
+++ observes/etl/dynamo/retry-redshift-upload/main.nix
@@ -0,0 +1,21 @@
+{
+  inputs,
+  makeScript,
+  outputs,
+  ...
+}:
+makeScript {
+  searchPaths = {
+    bin = [
+      outputs."${inputs.observesIndex.service.success_indicators.bin}"
+      outputs."${inputs.observesIndex.tap.json.bin}"
+      outputs."${inputs.observesIndex.target.redshift_2.bin}"
+    ];
+    source = [
+      outputs."/common/utils/aws"
+      outputs."/observes/common/db-creds"
+    ];
+  };
+  name = "observes-etl-dynamo-parallel";
+  entrypoint = ./entrypoint.sh;
+}
