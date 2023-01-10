5fcdc3c4474f5813ce8a6d1bcf1a6c81f102ebdf
Juan Chaves <jchaves@fluidattacks.com>
2023-01-10 18:40:21 +0000

    common\refac(build): #8420 remove forces from commitlint
    - Commits labeled with forces will be considered invalid

--- .lint-git-commit-msg/parser.js
+++ .lint-git-commit-msg/parser.js
@@ -1,7 +1,7 @@
 module.exports = {
   parserOpts: {
     headerPattern:
-      /^(airs|all|asserts|common|docs|forces|integrates|melts|observes|reviews|skims|sorts|teaches)\\(\w*)\((\w*)\):\s(#[1-9]\d*)\s(.*)$/,
+      /^(airs|all|asserts|common|docs|integrates|melts|observes|reviews|skims|sorts|teaches)\\(\w*)\((\w*)\):\s(#[1-9]\d*)\s(.*)$/,
     headerCorrespondence: ["product", "type", "scope", "ticket", "subject"],
   },
 };
