7876d14b4c80469e4d48be2af2a3653e12c91793
Karen Camargo <kcamargo@fluidattacks.com>
2023-01-10 16:36:27 +0000

    integrates\refac(front): #8516 payment methods
    - add credit card and other payment methods query

--- integrates/front/src/scenes/Dashboard/containers/Organization-Content/OrganizationBillingView/queries.ts
+++ integrates/front/src/scenes/Dashboard/containers/Organization-Content/OrganizationBillingView/queries.ts
@@ -118,6 +118,54 @@ const ADD_PAYMENT_METHOD: DocumentNode = gql`
   }
 `;
 
+const ADD_CREDIT_CARD_PAYMENT_METHOD: DocumentNode = gql`
+  mutation addCreditCardPaymentMethod(
+    $organizationId: String!
+    $cardNumber: String!
+    $cardExpirationMonth: String!
+    $cardExpirationYear: String!
+    $cardCvc: String!
+    $makeDefault: Boolean!
+  ) {
+    addCreditCardPaymentMethod(
+      organizationId: $organizationId
+      cardNumber: $cardNumber
+      cardExpirationMonth: $cardExpirationMonth
+      cardExpirationYear: $cardExpirationYear
+      cardCvc: $cardCvc
+      makeDefault: $makeDefault
+    ) {
+      success
+    }
+  }
+`;
+
+const ADD_OTHER_PAYMENT_METHOD: DocumentNode = gql`
+  mutation addOtherPaymentMethod(
+    $organizationId: String!
+    $businessName: String!
+    $email: String!
+    $country: String!
+    $state: String!
+    $city: String!
+    $rut: Upload
+    $taxId: Upload
+  ) {
+    addOtherPaymentMethod(
+      organizationId: $organizationId
+      businessName: $businessName
+      email: $email
+      country: $country
+      state: $state
+      city: $city
+      rut: $rut
+      taxId: $taxId
+    ) {
+      success
+    }
+  }
+`;
+
 const REMOVE_PAYMENT_METHOD: DocumentNode = gql`
   mutation removePaymentMethod(
     $organizationId: String!
@@ -201,6 +249,8 @@ const UPDATE_GROUP_MUTATION: DocumentNode = gql`
 `;
 
 export {
+  ADD_CREDIT_CARD_PAYMENT_METHOD,
+  ADD_OTHER_PAYMENT_METHOD,
   ADD_PAYMENT_METHOD,
   DOWNLOAD_FILE_MUTATION,
   GET_ORGANIZATION_BILLING,
