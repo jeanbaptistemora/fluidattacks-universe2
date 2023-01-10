482ef6785084540f97d24c47c6ef99e1b60a38a0
Jonathan Mesa <jmesa@fluidattacks.com>
2023-01-10 22:02:33 +0000

    integrates\fix(back): #8490 scheduler reset accepted
    - process vulns per finding to avoid too much concurrency
    during treatment updates and unreliable indicators
    processing
    - limit workers at processing groups and findings

--- integrates/back/src/schedulers/reset_expired_accepted_findings.py
+++ integrates/back/src/schedulers/reset_expired_accepted_findings.py
@@ -8,9 +8,6 @@ from dataloaders import (
     Dataloaders,
     get_new_context,
 )
-from datetime import (
-    datetime,
-)
 from db_model.findings.types import (
     Finding,
 )
@@ -57,64 +54,70 @@ async def _reset_expired_accepted_vuln(
         )
 
 
-async def reset_group_expired_accepted_findings(
-    loaders: Dataloaders, group_name: str, today: datetime
-) -> None:
-    group_findings: tuple[Finding] = await loaders.group_findings.load(
-        group_name
-    )
-    vulns = await loaders.finding_vulnerabilities.load_many_chained(
-        [finding.id for finding in group_findings]
-    )
-    updated_finding_ids: set[str] = set()
+async def _process_finding(loaders: Dataloaders, finding_id: str) -> None:
+    today = datetime_utils.get_utc_now()
+    vulnerabilities: tuple[
+        Vulnerability, ...
+    ] = await loaders.finding_vulnerabilities.load(finding_id)
     updated_vuln_ids: set[str] = set()
 
-    for vuln in vulns:
-        finding_id = vuln.finding_id
+    for vulnerability in vulnerabilities:
         is_accepted_expired = (
-            vuln.treatment.accepted_until < today
-            if vuln.treatment and vuln.treatment.accepted_until
+            vulnerability.treatment.accepted_until < today
+            if vulnerability.treatment
+            and vulnerability.treatment.accepted_until
             else False
         )
         is_undefined_accepted_expired = (
-            vuln.treatment
-            and vuln.treatment.status
+            vulnerability.treatment
+            and vulnerability.treatment.status
             == VulnerabilityTreatmentStatus.ACCEPTED_UNDEFINED
-            and vuln.treatment.acceptance_status
+            and vulnerability.treatment.acceptance_status
             == VulnerabilityAcceptanceStatus.SUBMITTED
             and datetime_utils.get_plus_delta(
-                vuln.treatment.modified_date,
+                vulnerability.treatment.modified_date,
                 days=5,
             )
             <= today
         )
         if (
-            vuln.treatment
-            and vuln.treatment.modified_by
+            vulnerability.treatment
+            and vulnerability.treatment.modified_by
             and (is_accepted_expired or is_undefined_accepted_expired)
         ):
-            await _reset_expired_accepted_vuln(loaders, vuln.id)
-            updated_finding_ids.add(finding_id)
-            updated_vuln_ids.add(vuln.id)
+            await _reset_expired_accepted_vuln(loaders, vulnerability.id)
+            updated_vuln_ids.add(vulnerability.id)
 
-    await update_unreliable_indicators_by_deps(
-        EntityDependency.reset_expired_accepted_findings,
-        finding_ids=list(updated_finding_ids),
-        vulnerability_ids=list(updated_vuln_ids),
+    if updated_vuln_ids:
+        await update_unreliable_indicators_by_deps(
+            EntityDependency.reset_expired_accepted_findings,
+            finding_ids=[finding_id],
+            vulnerability_ids=list(updated_vuln_ids),
+        )
+
+
+async def _process_group(loaders: Dataloaders, group_name: str) -> None:
+    group_findings: tuple[Finding, ...] = await loaders.group_findings.load(
+        group_name
+    )
+    await collect(
+        tuple(
+            _process_finding(loaders, finding_id)
+            for finding_id in tuple(finding.id for finding in group_findings)
+        ),
+        workers=4,
     )
 
 
 async def reset_expired_accepted_findings() -> None:
     """Update treatment if acceptance date expires."""
-    today: datetime = datetime_utils.get_now()
     loaders: Dataloaders = get_new_context()
     group_names = await orgs_domain.get_all_active_group_names(loaders)
     await collect(
         tuple(
-            reset_group_expired_accepted_findings(loaders, group_name, today)
-            for group_name in group_names
+            _process_group(loaders, group_name) for group_name in group_names
         ),
-        workers=16,
+        workers=4,
     )
 
 
