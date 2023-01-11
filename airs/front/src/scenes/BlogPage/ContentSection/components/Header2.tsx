80b6f36578509fedecc86a8f809422cfee082f38
Frank Aristizabal <faristizabal@fluidattacks.com>
2023-01-11 13:06:57 -0500

    airs\refac(front): #8492 update blog
    - update components of blog index.

--- airs/front/src/scenes/BlogPage/ContentSection/components/Header2.tsx
+++ airs/front/src/scenes/BlogPage/ContentSection/components/Header2.tsx
@@ -6,7 +6,7 @@ import type { ITitleProps } from "../../../../components/Typography";
 
 const Header2: React.FC<ITitleProps> = ({ children }): JSX.Element => (
   <Container pv={3}>
-    <Title color={"#2e2e38"} level={2}>
+    <Title color={"#2e2e38"} level={2} size={"small"}>
       {children}
     </Title>
   </Container>
--- airs/front/src/scenes/BlogPage/ContentSection/components/Header3.tsx
+++ airs/front/src/scenes/BlogPage/ContentSection/components/Header3.tsx
@@ -6,7 +6,7 @@ import type { ITitleProps } from "../../../../components/Typography";
 
 const Header3: React.FC<ITitleProps> = ({ children }): JSX.Element => (
   <Container pv={3}>
-    <Title color={"#2e2e38"} level={3}>
+    <Title color={"#2e2e38"} level={3} size={"xs"}>
       {children}
     </Title>
   </Container>
