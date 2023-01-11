d3030d64294a4dc7c94b15c16f36c73ed6bb599d
Karen Camargo <kcamargo@fluidattacks.com>
2023-01-11 22:58:48 +0000

    integrates\refac(back): #8516 payment methods
    - rm add payment method mutation

--- integrates/back/test/unit/src/api/arch/arch.py
+++ integrates/back/test/unit/src/api/arch/arch.py
@@ -417,7 +417,6 @@ _dag: Dict[str, Tuple[Union[Tuple[str, ...], str], ...]] = {
         "remove_stakeholder_access",
         "confirm_vulnerabilities_zero_risk",
         "add_credit_card_payment_method",
-        "add_payment_method",
         "add_other_payment_method",
         "update_finding_description",
         "update_tours",
