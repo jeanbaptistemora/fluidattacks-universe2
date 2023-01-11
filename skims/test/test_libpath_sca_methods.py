10f605e93fb29131fcefb2fc5c9b26500f52e903
Lewis Contreras <lcontreras@fluidattacks.com>
2023-01-11 14:10:09 -0500

    skims\feat(back): #8102 add php test sca
    - Add test for composer_lock method for sca

--- skims/test/test_libpath_sca_methods.py
+++ skims/test/test_libpath_sca_methods.py
@@ -4,6 +4,7 @@ from lib_path.common import (
 )
 from lib_path.f011.composer import (
     composer_json,
+    composer_lock,
 )
 from lib_path.f011.gem import (
     gem_gemfile,
@@ -303,5 +304,43 @@ def test_composer_json_dev() -> None:
             if not equal_props:
                 assertion = not assertion
                 break
+
+    assert assertion
+
+
+@pytest.mark.skims_test_group("unittesting")
+def test_composer_lock() -> None:
+    patt_info: Pattern[str] = re.compile(r'".*?": "(?P<info>.*?)"')
+    path: str = "skims/test/data/lib_path/f011/composer.lock"
+    with open(
+        path,
+        mode="r",
+        encoding="latin-1",
+    ) as file_handle:
+        file_contents: str = file_handle.read(-1)
+    content: List[str] = file_contents.splitlines()
+    generator_dep = composer_lock.__wrapped__(  # type: ignore
+        file_contents, path
+    )
+    assertion: bool = True
+    for line_num in (9, 85, 165, 241):
+        if (pkg_name_match := patt_info.search(content[line_num])) and (
+            version_match := patt_info.search(content[line_num + 1])
+        ):
+            pkg_name = pkg_name_match.group("info")
+            version = version_match.group("info")
+            try:
+                next_dep = next(generator_dep)
+                pkg_item = itemgetter("item")(next_dep[0])
+                item_ver = itemgetter("item")(next_dep[1])
+            except StopIteration:
+                assertion = not assertion
+                break
+            if not (pkg_item == pkg_name and version == item_ver):
+                assertion = not assertion
+                break
+        else:
+            assertion = not assertion
+            break
 
     assert assertion
