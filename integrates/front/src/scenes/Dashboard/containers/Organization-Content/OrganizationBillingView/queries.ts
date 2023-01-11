0be4adadbacb89feddf1ec59611fe1f246909e0a
Karen Camargo <kcamargo@fluidattacks.com>
2023-01-11 22:03:24 +0000

    integrates\refac(cross): #8516 payment methods
    - rm add payment method permissions

--- integrates/front/src/scenes/Dashboard/containers/Organization-Content/OrganizationBillingView/queries.ts
+++ integrates/front/src/scenes/Dashboard/containers/Organization-Content/OrganizationBillingView/queries.ts
@@ -82,42 +82,6 @@ const GET_ORGANIZATION_BILLING: DocumentNode = gql`
   }
 `;
 
-const ADD_PAYMENT_METHOD: DocumentNode = gql`
-  mutation addPaymentMethod(
-    $organizationId: String!
-    $cardNumber: String!
-    $cardExpirationMonth: String!
-    $cardExpirationYear: String!
-    $cardCvc: String!
-    $makeDefault: Boolean!
-    $businessName: String!
-    $email: String!
-    $country: String!
-    $state: String!
-    $city: String!
-    $rut: Upload
-    $taxId: Upload
-  ) {
-    addPaymentMethod(
-      organizationId: $organizationId
-      cardNumber: $cardNumber
-      cardExpirationMonth: $cardExpirationMonth
-      cardExpirationYear: $cardExpirationYear
-      cardCvc: $cardCvc
-      makeDefault: $makeDefault
-      businessName: $businessName
-      email: $email
-      country: $country
-      state: $state
-      city: $city
-      rut: $rut
-      taxId: $taxId
-    ) {
-      success
-    }
-  }
-`;
-
 const ADD_CREDIT_CARD_PAYMENT_METHOD: DocumentNode = gql`
   mutation addCreditCardPaymentMethod(
     $organizationId: String!
@@ -251,7 +215,6 @@ const UPDATE_GROUP_MUTATION: DocumentNode = gql`
 export {
   ADD_CREDIT_CARD_PAYMENT_METHOD,
   ADD_OTHER_PAYMENT_METHOD,
-  ADD_PAYMENT_METHOD,
   DOWNLOAD_FILE_MUTATION,
   GET_ORGANIZATION_BILLING,
   REMOVE_PAYMENT_METHOD,
