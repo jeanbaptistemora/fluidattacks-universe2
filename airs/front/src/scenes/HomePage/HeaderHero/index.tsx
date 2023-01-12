12f203bbdc7b389c4cfb04303fbf93da617b317e
Andres Saldarriaga <asaldarriaga@fluidattacks.com>
2023-01-12 13:49:39 -0500

    airs\refac(front): #8393 new homepage design
    - add new home hero

---
+++ airs/front/src/scenes/HomePage/HeaderHero/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { Hero } from "../../../components/Hero";
+import { translate } from "../../../utils/translations/translate";
+
+const HeaderHero: React.FC = (): JSX.Element => {
+  return (
+    <Hero
+      button1Link={"/free-trial/"}
+      button1Text={"Start free trial"}
+      button2Link={"/contact-us/"}
+      button2Text={"Contact now"}
+      image={"airs/home/HeaderHero/hero-image"}
+      matomoAction={"Home"}
+      paragraph={translate.t("home.hero.paragraph")}
+      size={"big"}
+      sizeSm={"medium"}
+      title={translate.t("home.hero.title")}
+      tone={"dark"}
+      variant={"right"}
+    />
+  );
+};
+
+export { HeaderHero };
