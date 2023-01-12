3e3d4b02114fe011a8f17ccfe52b823897607a0f
Laura Naranjo <lnaranjo@fluidattacks.com>
2023-01-12 11:10:01 -0500

    integrates\fix(back): #8520 linting errors
    - fix linting errors in sessions module

--- integrates/back/src/settings/jwt.py
+++ integrates/back/src/settings/jwt.py
@@ -2,8 +2,11 @@ from context import (
     FI_JWT_SECRET,
     FI_JWT_SECRET_API,
 )
+from typing import (
+    Literal,
+)
 
 JWT_COOKIE_NAME = "integrates_session"
-JWT_COOKIE_SAMESITE = "Lax"
+JWT_COOKIE_SAMESITE: Literal["lax"] = "lax"
 JWT_SECRET = FI_JWT_SECRET
 JWT_SECRET_API = FI_JWT_SECRET_API
