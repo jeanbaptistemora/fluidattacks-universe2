f810225c7719cb8b2315e927089a23e33f3c9916
Jonathan Mesa <jmesa@fluidattacks.com>
2023-01-12 00:43:08 +0000

    integrates\test(back): #8490 scheduler reset expired acc
    - test for no change in a still not expired accepted vuln
    - update mock data
    - no-deltas-test

--- integrates/back/test/unit/src/schedulers/test_reset_expired_accepted_findings.py
+++ integrates/back/test/unit/src/schedulers/test_reset_expired_accepted_findings.py
@@ -30,10 +30,11 @@ async def test_reset_expired_accepted_findings() -> None:
     finding_id = "818828206"
     vuln_accepted_id = "429bd683-3654-4dad-9eeb-3f8a2de9afd4"
     vuln_submitted_undefined_id = "1f0ee1d0-9816-49c6-8687-f91de722681b"
+    vuln_accepted_no_change_id = "f54d583b-cb1e-4b26-9b88-42baddab33ad"
 
     loaders: Dataloaders = get_new_context()
     vulns = await loaders.finding_vulnerabilities.load(finding_id)
-    assert len(vulns) == 2
+    assert len(vulns) == 3
     vuln_accepted: Vulnerability = await loaders.vulnerability.load(
         vuln_accepted_id
     )
@@ -60,6 +61,19 @@ async def test_reset_expired_accepted_findings() -> None:
         == VulnerabilityAcceptanceStatus.SUBMITTED
     )
     assert vuln_submitted_undefined.treatment.accepted_until is None
+    vuln_accepted_no_change: Vulnerability = await loaders.vulnerability.load(
+        vuln_accepted_no_change_id
+    )
+    assert (
+        vuln_accepted_no_change.state.status
+        == VulnerabilityStateStatus.VULNERABLE
+    )
+    assert vuln_accepted_no_change.treatment
+    assert (
+        vuln_accepted_no_change.treatment.status
+        == VulnerabilityTreatmentStatus.ACCEPTED
+    )
+    assert vuln_accepted_no_change.treatment.accepted_until
 
     await reset_expired_accepted_findings.process_group(
         loaders=loaders, group_name=group_name, progress=0.0
@@ -67,7 +81,7 @@ async def test_reset_expired_accepted_findings() -> None:
 
     loaders = get_new_context()
     vulns = await loaders.finding_vulnerabilities.load(finding_id)
-    assert len(vulns) == 2
+    assert len(vulns) == 3
     vuln_accepted = await loaders.vulnerability.load(vuln_accepted_id)
     assert vuln_accepted.state.status == VulnerabilityStateStatus.VULNERABLE
     assert vuln_accepted.treatment
@@ -90,3 +104,16 @@ async def test_reset_expired_accepted_findings() -> None:
     )
     assert vuln_submitted_undefined.treatment.acceptance_status is None
     assert vuln_submitted_undefined.treatment.accepted_until is None
+    vuln_accepted_no_change = await loaders.vulnerability.load(
+        vuln_accepted_no_change_id
+    )
+    assert (
+        vuln_accepted_no_change.state.status
+        == VulnerabilityStateStatus.VULNERABLE
+    )
+    assert vuln_accepted_no_change.treatment
+    assert (
+        vuln_accepted_no_change.treatment.status
+        == VulnerabilityTreatmentStatus.ACCEPTED
+    )
+    assert vuln_accepted_no_change.treatment.accepted_until
