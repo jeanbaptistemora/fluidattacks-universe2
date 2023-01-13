6f8b2e9a0a8f0c81130f5bf8a46edd0c6f86768e
Juan Villegas <jvillegas@fluidattacks.com>
2023-01-13 10:46:06 -0500

    integrates\test(back): #7819 toe lines
    - refac to data assert in test
    
    - no-deltas-test

--- integrates/back/test/functional/src/update_toe_lines_sorts/test_resolver.py
+++ integrates/back/test/functional/src/update_toe_lines_sorts/test_resolver.py
@@ -62,90 +62,25 @@ async def test_update_toe_lines_sorts(
     )
     assert result["data"]["updateToeLinesSorts"]["success"]
     result = await query_get(user=user_email, group_name="group1")
-    assert result["data"]["group"]["toeLines"] == {
-        "edges": [
-            {
-                "node": {
-                    "attackedAt": "2021-01-20T05:00:00+00:00",
-                    "attackedBy": "test@test.com",
-                    "attackedLines": 23,
-                    "bePresent": False,
-                    "bePresentUntil": "2021-01-19T15:41:04+00:00",
-                    "comments": "comment 1",
-                    "lastAuthor": "customer1@gmail.com",
-                    "filename": "test/test#.config",
-                    "firstAttackAt": "2020-01-19T15:41:04+00:00",
-                    "loc": 4324,
-                    "lastCommit": "f9e4beba70c4f34d6117c3b0c23ebe6b2bff66c1",
-                    "modifiedDate": "2020-11-16T15:41:04+00:00",
-                    "root": {"nickname": "universe"},
-                    "seenAt": "2020-01-01T15:41:04+00:00",
-                    "sortsRiskLevel": 0,
-                    "sortsRiskLevelDate": None,
-                    "sortsSuggestions": None,
-                },
-                "cursor": "eyJwayI6ICJHUk9VUCNncm91cDEiLCAic2siOiAiTElORVMjUk9"
-                "PVCM2MzI5OGE3My05ZGZmLTQ2Y2YtYjQyZC05YjJmMDFhNTY2OT"
-                "AjRklMRU5BTUUjdGVzdC90ZXN0Iy5jb25maWcifQ==",
-            },
-            {
-                "node": {
-                    "attackedAt": "2021-02-20T05:00:00+00:00",
-                    "attackedBy": "test2@test.com",
-                    "attackedLines": 4,
-                    "bePresent": True,
-                    "bePresentUntil": None,
-                    "comments": "comment 2",
-                    "lastAuthor": "customer2@gmail.com",
-                    "filename": "test2/test.sh",
-                    "firstAttackAt": "2020-02-19T15:41:04+00:00",
-                    "loc": 180,
-                    "lastCommit": "f9e4beba70c4f34d6117c3b0c23ebe6b2bff66c2",
-                    "modifiedDate": "2020-11-15T15:41:04+00:00",
-                    "root": {"nickname": "asm_1"},
-                    "seenAt": "2020-02-01T15:41:04+00:00",
-                    "sortsRiskLevel": sorts_risk_level,
-                    "sortsRiskLevelDate": (
-                        f"{sorts_risk_level_date}T00:00:00+00:00"
-                    ),
-                    "sortsSuggestions": sorts_suggestions,
-                },
-                "cursor": "eyJwayI6ICJHUk9VUCNncm91cDEiLCAic2siOiAiTElORVMjUk9"
-                "PVCM3NjViMWQwZi1iNmZiLTQ0ODUtYjRlMi0yYzJjYjE1NTViMW"
-                "EjRklMRU5BTUUjdGVzdDIvdGVzdC5zaCJ9",
-            },
-            {
-                "node": {
-                    "attackedAt": "2021-01-20T05:00:00+00:00",
-                    "attackedBy": "test3@test.com",
-                    "attackedLines": 120,
-                    "bePresent": True,
-                    "bePresentUntil": None,
-                    "comments": "comment 3",
-                    "lastAuthor": "customer3@gmail.com",
-                    "filename": "test3/test.config",
-                    "firstAttackAt": "2020-01-14T15:41:04+00:00",
-                    "loc": 243,
-                    "lastCommit": "f9e4beba70c4f34d6117c3b0c23ebe6b2bff66c3",
-                    "modifiedDate": "2020-11-16T15:41:04+00:00",
-                    "root": {"nickname": "asm_1"},
-                    "seenAt": "2019-01-01T15:41:04+00:00",
-                    "sortsRiskLevel": 0,
-                    "sortsRiskLevelDate": None,
-                    "sortsSuggestions": None,
-                },
-                "cursor": "eyJwayI6ICJHUk9VUCNncm91cDEiLCAic2siOiAiTElORVMjUk9"
-                "PVCM3NjViMWQwZi1iNmZiLTQ0ODUtYjRlMi0yYzJjYjE1NTViMW"
-                "EjRklMRU5BTUUjdGVzdDMvdGVzdC5jb25maWcifQ==",
-            },
-        ],
-        "pageInfo": {
-            "hasNextPage": False,
-            "endCursor": "eyJwayI6ICJHUk9VUCNncm91cDEiLCAic2siOiAiTElORVMjUk9P"
-            "VCM3NjViMWQwZi1iNmZiLTQ0ODUtYjRlMi0yYzJjYjE1NTViMWEj"
-            "RklMRU5BTUUjdGVzdDMvdGVzdC5jb25maWcifQ==",
-        },
-    }
+    lines = result["data"]["group"]["toeLines"]["edges"]
+    assert lines[0]["node"]["attackedAt"] == "2021-01-20T05:00:00+00:00"
+    assert lines[0]["node"]["attackedBy"] == "test@test.com"
+    assert lines[0]["node"]["attackedLines"] == 23
+    assert lines[0]["node"]["bePresent"] is False
+    assert lines[0]["node"]["bePresentUntil"] == "2021-01-19T15:41:04+00:00"
+    assert lines[0]["node"]["comments"] == "comment 1"
+    assert lines[0]["node"]["filename"] == "test/test#.config"
+    assert lines[0]["node"]["firstAttackAt"] == "2020-01-19T15:41:04+00:00"
+    assert lines[0]["node"]["lastAuthor"] == "customer1@gmail.com"
+    assert (
+        lines[0]["node"]["lastCommit"]
+        == "f9e4beba70c4f34d6117c3b0c23ebe6b2bff66c1"
+    )
+    assert lines[0]["node"]["loc"] == 4324
+    assert lines[0]["node"]["modifiedDate"] == "2020-11-16T15:41:04+00:00"
+    assert lines[0]["node"]["root"]["nickname"] == "universe"
+    assert lines[0]["node"]["seenAt"] == "2020-01-01T15:41:04+00:00"
+    assert lines[0]["node"]["sortsRiskLevel"] == 0
 
 
 @pytest.mark.asyncio
