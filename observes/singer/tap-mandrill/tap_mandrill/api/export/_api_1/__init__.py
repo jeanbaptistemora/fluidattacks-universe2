d9af25b1e6d8e79f33d20ac75f9cbd4dee14d867
Daniel Murcia <dmurcia@fluidattacks.com>
2023-01-13 14:24:52 -0500

    observes\feat(back): #8534 switch export activity
    - switch to new export activity

--- observes/singer/tap-mandrill/tap_mandrill/api/export/_api_1/__init__.py
+++ observes/singer/tap-mandrill/tap_mandrill/api/export/_api_1/__init__.py
@@ -20,7 +20,7 @@ def export_api_1(key: ApiKey) -> ExportApi:
     get_jobs = _get_job.get_jobs(client)  # type: ignore[misc]
     return ExportApi.new(
         get_jobs,
-        _activity.export_activity(client),  # type: ignore[misc]
+        _activity.export_activity_2(key),
         lambda j, interval, retries: _download.until_finish(
             get_jobs, j, interval, retries
         ),
