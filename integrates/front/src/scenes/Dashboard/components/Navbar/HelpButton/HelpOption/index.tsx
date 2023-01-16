b7c3f46bee25dffe5fcc7735d9cf6adcf503c951
Frank Aristizabal <faristizabal@fluidattacks.com>
2023-01-16 13:02:20 -0500

    integrates\refac(front): #8329 update help button
    - Update navbar help button.

---
+++ integrates/front/src/scenes/Dashboard/components/Navbar/HelpButton/HelpOption/index.tsx
@@ -0,0 +1,44 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import type { FC } from "react";
+
+import type { IHelpOptionProps } from "./types";
+
+import { Button } from "components/Button";
+import { Col, Row } from "components/Layout";
+import { Text } from "components/Text";
+
+const HelpOption: FC<IHelpOptionProps> = ({
+  description,
+  icon,
+  onClick,
+  title,
+}): JSX.Element => {
+  return (
+    <Button onClick={onClick}>
+      <Row>
+        <Col lg={10}>
+          <Text size={"xs"}>
+            <FontAwesomeIcon icon={icon} />
+          </Text>
+        </Col>
+        <Col lg={90}>
+          <Row>
+            <Col>
+              <Text>{title}</Text>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Text bright={9} mt={1} size={"xs"} tone={"light"}>
+                {description}
+              </Text>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Button>
+  );
+};
+
+export { HelpOption };
