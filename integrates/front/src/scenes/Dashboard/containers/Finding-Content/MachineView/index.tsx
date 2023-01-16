b6fade8cb32cfcaa623a359a31c5e7a99fd1f47d
Diego Restrepo <drestrepo@fluidattacks.com>
2023-01-16 15:36:59 -0500

    integrates\refac(front): #8569 remove table
    - remove table with machine executions
    - remove associated code
    - no-deltas-test

--- integrates/front/src/scenes/Dashboard/containers/Finding-Content/MachineView/index.tsx
+++ integrates/front/src/scenes/Dashboard/containers/Finding-Content/MachineView/index.tsx
@@ -2,22 +2,14 @@ import { NetworkStatus, useMutation, useQuery } from "@apollo/client";
 import type { ApolloError, FetchResult } from "@apollo/client";
 import { faClock, faRocket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import type { ColumnDef, Row } from "@tanstack/react-table";
 import type { GraphQLError } from "graphql";
 import _ from "lodash";
 import React, { useCallback, useState } from "react";
 import { useParams } from "react-router-dom";
 
-import { Execution } from "./execution";
-import {
-  GET_FINDING_MACHINE_JOBS,
-  GET_ROOTS,
-  SUBMIT_MACHINE_JOB,
-} from "./queries";
+import { GET_ROOTS, SUBMIT_MACHINE_JOB } from "./queries";
 import { Queue } from "./queue";
 import type {
-  IExecution,
-  IFindingMachineJob,
   IFindingMachineJobs,
   IGroupRoot,
   ISubmitMachineJobResult,
@@ -25,71 +17,22 @@ import type {
 
 import { Button } from "components/Button";
 import { Modal } from "components/Modal";
-import { Table } from "components/Table";
-import type { ICellHelper } from "components/Table/types";
 import { ButtonToolbarCenter } from "styles/styledComponents";
-import { formatDate, formatDuration } from "utils/formatHelpers";
 import { Logger } from "utils/logger";
 import { msgError, msgSuccess } from "utils/notifications";
 import { translate } from "utils/translations/translate";
 
 const MachineView: React.FC = (): JSX.Element => {
-  const { findingId, groupName } =
-    useParams<{ findingId: string; groupName: string }>();
-
-  // States
-  const defaultCurrentRow: IExecution = {
-    createdAt: "",
-    duration: 0,
-    jobId: "",
-    name: "",
-    priority: "",
-    queue: "",
-    rootId: "",
-    rootNickname: "",
-    startedAt: "",
-    status: "",
-    stoppedAt: "",
-    vulnerabilities: null,
-  };
-  const [currentRow, setCurrentRow] = useState(defaultCurrentRow);
-  const [isExecutionDetailsModalOpen, setIsExecutionDetailsModalOpen] =
-    useState(false);
-
-  const closeSeeExecutionDetailsModal: () => void = useCallback((): void => {
-    setIsExecutionDetailsModalOpen(false);
-  }, []);
-
-  function openSeeExecutionDetailsModal(
-    rowInfo: Row<IExecution>
-  ): (event: React.FormEvent) => void {
-    return (event: React.FormEvent): void => {
-      setCurrentRow(rowInfo.original);
-      setIsExecutionDetailsModalOpen(true);
-      event.stopPropagation();
-    };
-  }
+  const { findingId, groupName } = useParams<{
+    findingId: string;
+    groupName: string;
+  }>();
 
   // GraphQL operations
-  const {
-    data,
-    refetch,
-    networkStatus: dataNS,
-  } = useQuery<IFindingMachineJobs>(GET_FINDING_MACHINE_JOBS, {
-    fetchPolicy: "no-cache",
-    notifyOnNetworkStatusChange: true,
-    onError: ({ graphQLErrors }: ApolloError): void => {
-      graphQLErrors.forEach((error: GraphQLError): void => {
-        msgError(translate.t("groupAlerts.errorTextsad"));
-        Logger.warning("An error occurred loading machine jobs", error);
-      });
-    },
-    variables: { findingId },
-  });
+
   const handleOnSuccess = (result: ISubmitMachineJobResult): void => {
     if (!_.isUndefined(result)) {
       if (result.submitMachineJob.success) {
-        void refetch();
         msgSuccess(
           translate.t("searchFindings.tabMachine.submitJobSuccess"),
           translate.t("searchFindings.tabMachine.success")
@@ -104,17 +47,18 @@ const MachineView: React.FC = (): JSX.Element => {
       }
     }
   };
-  const { data: dataRoots } = useQuery<IFindingMachineJobs>(GET_ROOTS, {
-    fetchPolicy: "no-cache",
-    notifyOnNetworkStatusChange: true,
-    onError: ({ graphQLErrors }: ApolloError): void => {
-      graphQLErrors.forEach((error: GraphQLError): void => {
-        msgError(translate.t("groupAlerts.errorTextsad"));
-        Logger.warning("An error occurred loading roots", error);
-      });
-    },
-    variables: { groupName },
-  });
+  const { data: dataRoots, networkStatus: dataNS } =
+    useQuery<IFindingMachineJobs>(GET_ROOTS, {
+      fetchPolicy: "no-cache",
+      notifyOnNetworkStatusChange: true,
+      onError: ({ graphQLErrors }: ApolloError): void => {
+        graphQLErrors.forEach((error: GraphQLError): void => {
+          msgError(translate.t("groupAlerts.errorTextsad"));
+          Logger.warning("An error occurred loading roots", error);
+        });
+      },
+      variables: { groupName },
+    });
 
   const handleOnError: ({ graphQLErrors }: ApolloError) => void = ({
     graphQLErrors,
@@ -152,10 +96,7 @@ const MachineView: React.FC = (): JSX.Element => {
   const isLoading: boolean =
     submittingMachineJob || dataNS === NetworkStatus.refetch;
 
-  if (
-    (_.isUndefined(data) || _.isEmpty(data)) &&
-    (_.isUndefined(dataRoots) || _.isEmpty(dataRoots))
-  ) {
+  if (_.isUndefined(dataRoots) || _.isEmpty(dataRoots)) {
     return <div />;
   }
 
@@ -167,56 +108,6 @@ const MachineView: React.FC = (): JSX.Element => {
   const rootNicknames: string[] = rootNicknamesSorted
     .filter((root: IGroupRoot): boolean => root.state === "ACTIVE")
     .map((root: IGroupRoot): string => root.nickname);
-
-  const headers: ColumnDef<IExecution>[] = [
-    {
-      accessorKey: "status",
-      header: translate.t("searchFindings.tabMachine.headerStatus"),
-    },
-    {
-      accessorKey: "startedAt",
-      cell: (cell: ICellHelper<IExecution>): string =>
-        formatDate(parseFloat(cell.getValue())),
-      header: translate.t("searchFindings.tabMachine.headerStartedAt"),
-    },
-    {
-      accessorKey: "duration",
-      cell: (cell: ICellHelper<IExecution>): string =>
-        formatDuration(cell.getValue()),
-      header: translate.t("searchFindings.tabMachine.headerDuration"),
-    },
-    {
-      accessorKey: "rootNickname",
-      header: translate.t("searchFindings.tabMachine.headerRoot"),
-    },
-    {
-      accessorKey: "priority",
-      header: translate.t("searchFindings.tabMachine.headerPriority"),
-    },
-  ];
-
-  const tableDataset: IExecution[] =
-    _.isUndefined(data) || _.isUndefined(data.finding.machineJobs)
-      ? []
-      : data.finding.machineJobs.map(
-          (job: IFindingMachineJob): IExecution => ({
-            ...job,
-            duration:
-              job.startedAt === null || job.stoppedAt === null
-                ? -1
-                : parseFloat(job.stoppedAt) - parseFloat(job.startedAt),
-            jobId: job.id,
-            priority: job.queue.endsWith("_soon")
-              ? translate.t("searchFindings.tabMachine.priorityHigh")
-              : translate.t("searchFindings.tabMachine.priorityNormal"),
-            rootId: job.rootNickname,
-            rootNickname: job.rootNickname,
-            startedAt: (job.startedAt === null
-              ? -1
-              : parseFloat(job.startedAt)
-            ).toString(),
-          })
-        );
 
   async function submitJobOnClick(
     roots: string[]
@@ -255,38 +146,6 @@ const MachineView: React.FC = (): JSX.Element => {
               onClose={closeQueueModal}
               onSubmit={submitJobOnClick}
               rootNicknames={rootNicknames}
-            />
-          </Modal>
-        </React.StrictMode>
-      )}
-      {_.isUndefined(data) || _.isEmpty(data) ? (
-        <div />
-      ) : (
-        <React.StrictMode>
-          <Table
-            columns={headers}
-            data={tableDataset}
-            id={"tblMachineJobs"}
-            onRowClick={openSeeExecutionDetailsModal}
-          />
-          <Modal
-            onClose={closeSeeExecutionDetailsModal}
-            open={isExecutionDetailsModalOpen}
-            title={translate.t("group.machine.executionDetailsModal.title")}
-          >
-            <Execution
-              createdAt={currentRow.createdAt}
-              duration={currentRow.duration}
-              jobId={currentRow.jobId}
-              name={currentRow.name}
-              priority={currentRow.priority}
-              queue={currentRow.queue}
-              rootId={currentRow.rootId}
-              rootNickname={currentRow.rootNickname}
-              startedAt={currentRow.startedAt}
-              status={currentRow.status}
-              stoppedAt={currentRow.stoppedAt}
-              vulnerabilities={currentRow.vulnerabilities}
             />
           </Modal>
         </React.StrictMode>
--- integrates/front/src/scenes/Dashboard/containers/Finding-Content/MachineView/queries.ts
+++ integrates/front/src/scenes/Dashboard/containers/Finding-Content/MachineView/queries.ts
@@ -1,29 +1,6 @@
 import { gql } from "@apollo/client";
 import type { DocumentNode } from "graphql";
 
-const GET_FINDING_MACHINE_JOBS: DocumentNode = gql`
-  query GetFindingMachineJobs($findingId: String!) {
-    finding(identifier: $findingId) {
-      id
-      machineJobs {
-        createdAt
-        exitCode
-        exitReason
-        id
-        name
-        queue
-        rootNickname
-        startedAt
-        stoppedAt
-        status
-        vulnerabilities {
-          open
-          modified
-        }
-      }
-    }
-  }
-`;
 const GET_ROOTS: DocumentNode = gql`
   query GetRoots($groupName: String!) {
     group(groupName: $groupName) {
@@ -47,4 +24,4 @@ const SUBMIT_MACHINE_JOB: DocumentNode = gql`
   }
 `;
 
-export { GET_FINDING_MACHINE_JOBS, SUBMIT_MACHINE_JOB, GET_ROOTS };
+export { SUBMIT_MACHINE_JOB, GET_ROOTS };
