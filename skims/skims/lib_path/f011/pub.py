b5479debf767ce334f92c13934149def475e5f1d
Lewis Contreras <lcontreras@fluidattacks.com>
2023-01-16 11:14:31 -0500

    skims\feat(back): #8103 add pub method sca
    - Create pubspec_yaml deps resolver for sca

---
+++ skims/skims/lib_path/f011/pub.py
@@ -0,0 +1,28 @@
+from lib_path.common import (
+    DependencyType,
+    format_pkg_dep,
+    pkg_deps_to_vulns,
+)
+from model.core_model import (
+    MethodsEnum,
+    Platform,
+)
+from typing import (
+    Iterator,
+)
+import yaml
+
+
+# pylint: disable=unused-argument
+@pkg_deps_to_vulns(Platform.COMPOSER, MethodsEnum.COMPOSER_JSON)
+def pubspec_yaml(  # NOSONAR
+    content: str, path: str
+) -> Iterator[DependencyType]:
+    dict_yaml = yaml.safe_load(content)
+    if "dependencies" in dict_yaml:
+        for dep, version in dict_yaml["dependencies"].items():
+            if dep == "flutter":
+                continue
+            yield format_pkg_dep(dep, version, 1, 1)
+
+    return iter([({}, {})])
