b6922f8940cdfc1c24eae47d39d8451aff080482
Luis Saavedra <lsaavedra@fluidattacks.com>
2023-01-17 08:47:24 -0500

    skims\feat(back): #7612 add method to the model
    - Add method to the model
    - Add translations for F363

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -2540,6 +2540,14 @@ class MethodsEnum(Enum):
         developer=DeveloperEnum.LUIS_SAAVEDRA,
         technique=TechniqueEnum.DAST,
     )
+    AWS_IAM_HAS_OLD_CREDS_ENABLED = MethodInfo(
+        file_name="aws",
+        name="have_old_creds_enabled",
+        module="dast",
+        finding=FindingEnum.F363,
+        developer=DeveloperEnum.LUIS_SAAVEDRA,
+        technique=TechniqueEnum.DAST,
+    )
     AWS_IAM_NOT_REQUIRES_LOWERCASE = MethodInfo(
         file_name="aws",
         name="not_requires_lowercase",
