3f3d87542b37dcc638d70ab833045cb3654bc29b
Juan Chaves <jchaves@fluidattacks.com>
2023-01-18 23:38:10 +0000

    integrates\feat(back): #8336 standardize toe lines
    - Added a migration to standardize the shape of `ToeLines`

---
+++ integrates/back/migrations/0355_standardize_toe_lines_structure.py
@@ -0,0 +1,142 @@
+# pylint: disable=invalid-name
+"""
+Refresh toe inputs metadata and state when an empty string is in an
+attribute that would hold a date. These empty strings are causing an
+indexation error in opensearch. The attribute will be removed instead.
+
+Execution Time:
+Execution Time:
+"""
+from aioextensions import (
+    collect,
+    run,
+)
+from boto3.dynamodb.conditions import (
+    Attr,
+    Key,
+)
+from dataloaders import (
+    Dataloaders,
+    get_new_context,
+)
+from dynamodb import (
+    keys,
+    operations,
+)
+from dynamodb.model import (
+    TABLE,
+)
+from dynamodb.types import (
+    Item,
+    PrimaryKey,
+)
+from organizations import (
+    domain as orgs_domain,
+)
+import time
+
+
+async def get_toe_lines_by_group(
+    group_name: str,
+) -> tuple[Item, ...]:
+    facet = TABLE.facets["toe_lines_metadata"]
+    primary_key = keys.build_key(
+        facet=facet,
+        values={"group_name": group_name},
+    )
+    key_structure = TABLE.primary_key
+    response = await operations.query(
+        condition_expression=(
+            Key(key_structure.partition_key).eq(primary_key.partition_key)
+            & Key(key_structure.sort_key).begins_with(
+                primary_key.sort_key.replace("#ROOT#FILENAME", "")
+            )
+        ),
+        facets=(TABLE.facets["toe_lines_metadata"],),
+        index=None,
+        table=TABLE,
+    )
+
+    return response.items
+
+
+async def process_toe_lines_item(item: Item) -> None:
+    state_item: Item = item["state"]
+    migrated_attrs = {
+        "loc",
+        "comments",
+        "sorts_risk_level",
+        "sorts_risk_level_date",
+        "last_commit",
+        "last_author",
+        "has_vulnerabilities",
+        "attacked_at",
+        "first_attack_at",
+        "be_present",
+        "attacked_by",
+        "attacked_lines",
+        "seen_at",
+        "sorts_suggestions",
+    }
+    keys_to_update = (migrated_attrs & item.keys()) - state_item.keys()
+
+    if not keys_to_update:
+        return
+    to_update: Item = {key: item[key] for key in keys_to_update}
+
+    key_structure = TABLE.primary_key
+    primary_key = PrimaryKey(
+        partition_key=item[TABLE.primary_key.partition_key],
+        sort_key=item[TABLE.primary_key.sort_key],
+    )
+    condition_expression = Attr(key_structure.partition_key).exists()
+    await operations.update_item(
+        condition_expression=condition_expression,
+        item=to_update,
+        key=primary_key,
+        table=TABLE,
+    )
+
+
+async def process_group(group_name: str, progress: float) -> None:
+    group_toe_lines = await get_toe_lines_by_group(group_name)
+    print(
+        f"Working on {group_name=}, {len(group_toe_lines)=}, "
+        f"progress: {round(progress, 2)}"
+    )
+    if not group_toe_lines:
+        return
+
+    await collect(
+        tuple(process_toe_lines_item(item) for item in group_toe_lines),
+        workers=64,
+    )
+
+
+async def main() -> None:
+    loaders: Dataloaders = get_new_context()
+    group_names = sorted(
+        await orgs_domain.get_all_group_names(loaders=loaders)
+    )
+    print(f"{len(group_names)=}")
+    await collect(
+        tuple(
+            process_group(
+                group_name=group_name,
+                progress=count / len(group_names),
+            )
+            for count, group_name in enumerate(group_names)
+        ),
+        workers=1,
+    )
+
+
+if __name__ == "__main__":
+    execution_time = time.strftime(
+        "Execution Time:    %Y-%m-%d at %H:%M:%S %Z"
+    )
+    run(main())
+    finalization_time = time.strftime(
+        "Finalization Time: %Y-%m-%d at %H:%M:%S %Z"
+    )
+    print(f"{execution_time}\n{finalization_time}")
