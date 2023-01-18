3ea40bd156a564ba722b8c9d4ddfcab861cc4eb9
Jonathan Mesa <jmesa@fluidattacks.com>
2023-01-18 17:44:06 -0500

    integrates\fix(back): #7474 toe lines key error
    - fix key_error in removed toe lines items
    - several attrs are now part of a state map

--- integrates/streams/src/dynamodb/processors/redshift/toe_inputs/utils.py
+++ integrates/streams/src/dynamodb/processors/redshift/toe_inputs/utils.py
@@ -5,6 +5,13 @@ from dynamodb.types import (
     Item,
 )
 import hashlib
+from typing import (
+    Any,
+)
+
+
+def get_key(item: Item, key: str) -> Any:
+    return item.get(key) or item.get("state", {}).get(key)
 
 
 def format_row_metadata(
@@ -12,22 +19,26 @@ def format_row_metadata(
 ) -> Item:
     return dict(
         id=hashlib.sha256(item["sk"].encode("utf-8")).hexdigest(),
-        attacked_at=datetime.fromisoformat(item["attacked_at"])
-        if item.get("attacked_at")
+        attacked_at=datetime.fromisoformat(get_key(item, "attacked_at"))
+        if get_key(item, "attacked_at")
         else None,
-        attacked_by=item["attacked_by"],
-        be_present=bool(item["be_present"]),
-        be_present_until=datetime.fromisoformat(item["be_present_until"])
-        if item.get("be_present_until")
+        attacked_by=get_key(item, "attacked_by"),
+        be_present=bool(get_key(item, "be_present")),
+        be_present_until=datetime.fromisoformat(
+            get_key(item, "be_present_until")
+        )
+        if get_key(item, "be_present_until")
         else None,
-        first_attack_at=datetime.fromisoformat(item["first_attack_at"])
-        if item.get("first_attack_at")
+        first_attack_at=datetime.fromisoformat(
+            get_key(item, "first_attack_at")
+        )
+        if get_key(item, "first_attack_at")
         else None,
         group_name=item["group_name"],
-        has_vulnerabilities=bool(item.get("has_vulnerabilities", False)),
-        root_id=item["unreliable_root_id"],
-        seen_at=datetime.fromisoformat(item["seen_at"])
-        if item.get("seen_at")
+        has_vulnerabilities=bool(get_key(item, "has_vulnerabilities")),
+        root_id=get_key(item, "unreliable_root_id"),
+        seen_at=datetime.fromisoformat(get_key(item, "seen_at"))
+        if get_key(item, "seen_at")
         else None,
-        seen_first_time_by=item["seen_first_time_by"],
+        seen_first_time_by=get_key(item, "seen_first_time_by"),
     )
