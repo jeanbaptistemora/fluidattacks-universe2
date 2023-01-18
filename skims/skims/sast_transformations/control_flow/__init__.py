7a682c9433c915016eab62b575db2c81e2a44d59
Fabio Lagos <flagos@fluidattacks.com>
2023-01-18 08:36:11 -0500

    skims\refac(back): #8407 remove deprecated code
    - remove deprecated sast code
    - adjust unittesting json results
    - no-deltas-test

--- skims/skims/sast_transformations/control_flow/__init__.py
+++ skims/skims/sast_transformations/control_flow/__init__.py
@@ -32,32 +32,9 @@ def c_sharp_add(graph: Graph) -> None:
         generic(args=CfgArgs(generic, graph, n_id, language), stack=[])
 
 
-def java_add(graph: Graph) -> None:
-    def _predicate(n_attrs: NAttrs) -> bool:
-        return g.pred_has_labels(label_type="method_declaration",)(
-            n_attrs
-        ) or g.pred_has_labels(label_type="constructor_declaration")(n_attrs)
-
-    language = GraphShardMetadataLanguage.JAVA
-
-    for n_id in g.filter_nodes(
-        graph,
-        graph.nodes,
-        predicate=_predicate,
-    ):
-        generic(args=CfgArgs(generic, graph, n_id, language), stack=[])
-
-
-def javascript_add(graph: Graph) -> None:
-    language = GraphShardMetadataLanguage.JAVASCRIPT
-    generic(args=CfgArgs(generic, graph, g.ROOT_NODE, language), stack=[])
-
-
 def add(
     graph: Graph,
     language: GraphShardMetadataLanguage,
 ) -> None:
-    if language == GraphShardMetadataLanguage.JAVA:
-        java_add(graph)
-    elif language == GraphShardMetadataLanguage.CSHARP:
+    if language == GraphShardMetadataLanguage.CSHARP:
         c_sharp_add(graph)
--- skims/skims/sast_transformations/control_flow/walkers/__init__.py
+++ skims/skims/sast_transformations/control_flow/walkers/__init__.py
@@ -7,14 +7,10 @@ from sast_transformations.control_flow.types import (
 from sast_transformations.control_flow.walkers.c_sharp import (
     CSHARP_WALKERS,
 )
-from sast_transformations.control_flow.walkers.java import (
-    JAVA_WALKERS,
-)
 from typing import (
     Dict,
 )
 
 WALKERS_BY_LANG: Dict[GraphShardMetadataLanguage, Walkers] = {
     GraphShardMetadataLanguage.CSHARP: CSHARP_WALKERS,
-    GraphShardMetadataLanguage.JAVA: JAVA_WALKERS,
 }
