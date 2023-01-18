d3bb687dbd6f875d46e7d92770d3fc05aa444e38
Andres Cuberos <acuberos@fluidattacks.com>
2023-01-18 14:25:31 -0500

    common\feat(infra): #8078 vpc endpoint for amp
    - Create VPC endpoint for Prometheus workspace
      so the traffic from the K8s cluster to AMP
      does not leave the VPC.

--- common/monitoring/infra/prometheus.tf
+++ common/monitoring/infra/prometheus.tf
@@ -1,15 +1,36 @@
-resource "aws_prometheus_workspace" "monitoring" {
-  alias = "common-monitoring"
-
-  logging_configuration {
-    log_group_arn = aws_cloudwatch_log_group.monitoring.arn
-  }
-
+locals {
   tags = {
     "Name"               = "prometheus"
     "management:area"    = "cost"
     "management:product" = "common"
     "management:type"    = "product"
     "Access"             = "private"
+  }
+}
+
+data "aws_subnet" "k8s_subnets" { # common/vpc/infra/subnets.tf
+  count = 3
+  filter {
+    name   = "tag:Name"
+    values = ["k8s_${count.index + 1}"]
+  }
+}
+
+resource "aws_vpc_endpoint" "prometheus_endpoint" {
+  service_name = "com.amazonaws.us-east-1.aps-workspaces"
+  vpc_id       = data.aws_subnet.k8s_subnets[0].vpc_id
+  subnet_ids = [
+    for subnet in data.aws_subnet.k8s_subnets : subnet.id
+  ]
+  vpc_endpoint_type = "Interface"
+  tags              = local.tags
+}
+
+resource "aws_prometheus_workspace" "monitoring" {
+  alias = "common-monitoring"
+  tags  = local.tags
+
+  logging_configuration {
+    log_group_arn = "${aws_cloudwatch_log_group.monitoring.arn}:*"
   }
 }
