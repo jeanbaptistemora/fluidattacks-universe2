78096689d5a3bbd34f9cc714313eca2a167c9d85
Sebastian Lizcano <slizcano@fluidattacks.com>
2023-01-19 15:42:33 -0500

    integrates\refac(back): #8006 update resolver
    - update resolver to take into account new types

--- integrates/back/src/api/resolvers/credentials/token.py
+++ integrates/back/src/api/resolvers/credentials/token.py
@@ -1,6 +1,8 @@
 from db_model.credentials.types import (
     Credentials,
     HttpsPatSecret,
+    OauthGithubSecret,
+    OauthGitlabSecret,
 )
 from decorators import (
     enforce_owner,
@@ -8,15 +10,34 @@ from decorators import (
 from graphql.type.definition import (
     GraphQLResolveInfo,
 )
+from newutils.datetime import (
+    get_utc_now,
+)
+from oauth.gitlab import (
+    get_token,
+)
 from typing import (
     Optional,
 )
 
 
 @enforce_owner
-def resolve(parent: Credentials, _info: GraphQLResolveInfo) -> Optional[str]:
-    return (
-        parent.state.secret.token
-        if isinstance(parent.state.secret, HttpsPatSecret)
-        else None
-    )
+async def resolve(
+    parent: Credentials, info: GraphQLResolveInfo
+) -> Optional[str]:
+    if isinstance(parent.state.secret, HttpsPatSecret):
+        return parent.state.secret.token
+
+    if isinstance(parent.state.secret, OauthGithubSecret):
+        return parent.state.secret.access_token
+
+    if isinstance(parent.state.secret, OauthGitlabSecret):
+        if parent.state.secret.valid_until <= get_utc_now():
+            return await get_token(
+                credential=parent,
+                loaders=info.context.loaders,
+            )
+
+        return parent.state.secret.access_token
+
+    return None
