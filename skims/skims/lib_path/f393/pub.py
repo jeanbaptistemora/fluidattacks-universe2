3a5bbdf2a5f8d764024da3a47a5c1363fe89e288
Lewis Contreras <lcontreras@fluidattacks.com>
2023-01-19 10:18:25 -0500

    skims\feat(back): #8103 add pub method f393
    - Add pub_pubspec_yaml_dev method for f393
    - Update lib_path.csv
    - no-deltas-test

---
+++ skims/skims/lib_path/f393/pub.py
@@ -0,0 +1,40 @@
+from lib_path.common import (
+    DependencyType,
+    format_pkg_dep,
+    pkg_deps_to_vulns,
+)
+from model.core_model import (
+    MethodsEnum,
+    Platform,
+)
+import re
+from typing import (
+    Iterator,
+    Pattern,
+)
+
+PUB_DEV_DEP: Pattern[str] = re.compile(
+    r"^\s{2}(?P<pkg>[^\s]+):\s(?P<version>[^\s]*)$"
+)
+
+
+# pylint: disable=unused-argument
+@pkg_deps_to_vulns(Platform.PUB, MethodsEnum.PUB_PUBSPEC_YAML_DEV)
+def pub_pubspec_yaml_dev(  # NOSONAR
+    content: str, path: str
+) -> Iterator[DependencyType]:
+    line_deps: bool = False
+    for line_number, line in enumerate(content.splitlines(), 1):
+        if line.startswith("dev_dependencies:"):
+            line_deps = True
+        elif line_deps:
+            if matched := re.match(PUB_DEV_DEP, line):
+                pkg_name = matched.group("pkg")
+                pkg_version = matched.group("version")
+                yield format_pkg_dep(
+                    pkg_name, pkg_version, line_number, line_number
+                )
+            elif not line:
+                break
+
+    return iter([({}, {})])
