09d18969d6977ae3eb0161e6897392291f44ab42
Juan Villegas <jvillegas@fluidattacks.com>
2023-01-19 14:13:31 -0500

    integrates\feat(front): #8368 to do draft
    - organization to drafts

--- integrates/front/src/scenes/Dashboard/containers/Tasks-Content/Drafts/index.tsx
+++ integrates/front/src/scenes/Dashboard/containers/Tasks-Content/Drafts/index.tsx
@@ -1,8 +1,11 @@
 import { useQuery } from "@apollo/client";
 import type { ColumnDef, Row } from "@tanstack/react-table";
 import _ from "lodash";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
+import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
+
+import { formatDrafts } from "./utils";
 
 import type { IFilter } from "components/Filter";
 import { Filters, useFilters } from "components/Filter";
@@ -15,10 +18,13 @@ import type {
   IGetTodoDrafts,
   ITodoDraftAttr,
 } from "scenes/Dashboard/containers/Tasks-Content/Drafts/types";
+import { GET_USER_ORGANIZATIONS_GROUPS } from "scenes/Dashboard/queries";
+import type { IGetUserOrganizationsGroups } from "scenes/Dashboard/types";
 import { Logger } from "utils/logger";
 
 export const TasksDrafts: React.FC = (): JSX.Element => {
   const { push } = useHistory();
+  const { t } = useTranslation();
 
   const hackerFormatter = (value: string): JSX.Element => {
     return <div className={`tl truncate`}>{value}</div>;
@@ -47,6 +53,10 @@ export const TasksDrafts: React.FC = (): JSX.Element => {
       header: "Group Name",
     },
     {
+      accessorKey: "organizationName",
+      header: t("todoList.tabs.drafts.organization"),
+    },
+    {
       accessorKey: "hacker",
       cell: (cell: ICellHelper<ITodoDraftAttr>): JSX.Element =>
         hackerFormatter(cell.getValue()),
@@ -70,6 +80,18 @@ export const TasksDrafts: React.FC = (): JSX.Element => {
     },
   ]);
 
+  const { data: userData } = useQuery<IGetUserOrganizationsGroups>(
+    GET_USER_ORGANIZATIONS_GROUPS,
+    {
+      fetchPolicy: "cache-first",
+      onError: ({ graphQLErrors }): void => {
+        graphQLErrors.forEach((error): void => {
+          Logger.warning("An error occurred fetching user groups", error);
+        });
+      },
+    }
+  );
+
   const { data } = useQuery<IGetTodoDrafts>(GET_TODO_DRAFTS, {
     onError: ({ graphQLErrors }): void => {
       graphQLErrors.forEach((error): void => {
@@ -81,10 +103,16 @@ export const TasksDrafts: React.FC = (): JSX.Element => {
     },
   });
 
-  const dataset: ITodoDraftAttr[] =
-    _.isUndefined(data) || _.isEmpty(data) ? [] : _.flatten(data.me.drafts);
+  const drafts: ITodoDraftAttr[] = useMemo(
+    (): ITodoDraftAttr[] =>
+      formatDrafts(
+        _.isNil(data) ? [] : data.me.drafts,
+        userData === undefined ? [] : userData.me.organizations
+      ),
+    [data, userData]
+  );
 
-  const filteredDataset = useFilters(dataset, filters);
+  const filteredDataset = useFilters(drafts, filters);
 
   const goToDraft = useCallback(
     (rowInfo: Row<ITodoDraftAttr>): ((event: React.FormEvent) => void) => {
