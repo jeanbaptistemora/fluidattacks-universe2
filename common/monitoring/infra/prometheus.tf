3a2fc0f38ba3ac90f011ac7eedb9576da3e1365f
Andres Cuberos <acuberos@fluidattacks.com>
2023-01-19 14:18:16 -0500

    common\feat(infra): #8078 iam k8s permissions
    - Create K8s service account and IAM role with permissions
      to scrape data from cluster resources and send them to AMP.

--- common/monitoring/infra/prometheus.tf
+++ common/monitoring/infra/prometheus.tf
@@ -1,4 +1,5 @@
 locals {
+  role_name = "monitoring"
   tags = {
     "Name"               = "prometheus"
     "management:area"    = "cost"
@@ -6,6 +7,8 @@ locals {
     "management:type"    = "product"
   }
 }
+
+data "aws_caller_identity" "current" {}
 
 data "aws_eks_cluster" "k8s_cluster" {
   name = "common"
@@ -73,4 +76,109 @@ resource "helm_release" "adot_operator" {
   depends_on = [
     helm_release.cert_manager
   ]
+}
+
+data "aws_iam_policy_document" "prometheus_permissions" {
+  statement {
+    actions = [
+      "aps:GetLabels",
+      "aps:GetMetricMetadata",
+      "aps:GetSeries",
+      "aps:RemoteWrite"
+    ]
+    resources = [
+      aws_prometheus_workspace.monitoring.arn
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "k8s_oidc" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test     = "StringEquals"
+      values   = ["system:serviceaccount:${kubernetes_namespace.monitoring.metadata[0].name}:${local.role_name}"]
+      variable = "${data.aws_eks_cluster.k8s_cluster.identity[0].oidc[0].issuer}:sub"
+    }
+
+    principals {
+      type        = "Federated"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${data.aws_eks_cluster.k8s_cluster.identity[0].oidc[0].issuer}"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "prometheus" {
+  name        = "EKSPrometheusAccess"
+  description = "Permissions required for ADOT Collector to send scraped metrics to AMP"
+  policy      = data.aws_iam_policy_document.prometheus_permissions.json
+}
+
+resource "aws_iam_role" "monitoring" {
+  name               = local.role_name
+  assume_role_policy = data.aws_iam_policy_document.k8s_oidc.json
+}
+
+resource "aws_iam_role_policy_attachment" "prometheus_access" {
+  role       = aws_iam_role.monitoring.name
+  policy_arn = aws_iam_policy.prometheus.arn
+}
+
+resource "kubernetes_service_account" "monitoring" {
+  automount_service_account_token = true
+  metadata {
+    name      = local.role_name
+    namespace = kubernetes_namespace.monitoring.metadata[0].name
+
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.monitoring.arn
+    }
+  }
+}
+
+resource "kubernetes_cluster_role" "monitoring" {
+  metadata {
+    name = local.role_name
+  }
+
+  rule {
+    api_groups = [""]
+    resources = [
+      "endpoints",
+      "nodes",
+      "nodes/metrics",
+      "nodes/proxy",
+      "pods",
+      "services"
+    ]
+    verbs = [
+      "get",
+      "list",
+      "watch"
+    ]
+  }
+
+  rule {
+    non_resource_urls = ["/metrics"]
+    verbs             = ["get"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "monitoring" {
+  metadata {
+    name = local.role_name
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.monitoring.metadata[0].name
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.monitoring.metadata[0].name
+    namespace = kubernetes_namespace.monitoring.metadata[0].name
+  }
 }
