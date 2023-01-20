6d2f538e678fa92195c5eeddf66d17c3b2cb4f9a
Luis Saavedra <lsaavedra@fluidattacks.com>
2023-01-20 10:17:25 -0500

    skims\feat(back): #7612 add method
    - Add method for F165

--- skims/skims/dast/aws/f165.py
+++ skims/skims/dast/aws/f165.py
@@ -1,9 +1,16 @@
+from contextlib import (
+    suppress,
+)
+import csv
 from dast.aws.types import (
     Location,
 )
 from dast.aws.utils import (
     build_vulnerabilities,
     run_boto3_fun,
+)
+from io import (
+    StringIO,
 )
 from model import (
     core_model,
@@ -76,7 +83,51 @@ async def users_with_multiple_access_keys(
     return vulns
 
 
+async def root_has_access_keys(
+    credentials: AwsCredentials,
+) -> core_model.Vulnerabilities:
+    await run_boto3_fun(
+        credentials, service="iam", function="generate_credential_report"
+    )
+    response: Dict[str, Any] = await run_boto3_fun(
+        credentials, service="iam", function="get_credential_report"
+    )
+    locations: List[Location] = []
+    vulns: core_model.Vulnerabilities = ()
+    users_csv = StringIO(response.get("Content", b"").decode())
+    credentials_report = tuple(csv.DictReader(users_csv, delimiter=","))
+    root_user = credentials_report[0]
+    root_arn = root_user["arn"]
+
+    key_names = ("access_key_1_active", "access_key_2_active")
+    with suppress(KeyError):
+        for index, name in enumerate(key_names):
+            if root_user[name] == "true":
+                locations = [
+                    *locations,
+                    Location(
+                        access_patterns=(f"/{key_names[index]}",),
+                        arn=(f"{root_arn}"),
+                        values=(root_user[key_names[index]],),
+                        description=("src.lib_path.f165.root_has_access_keys"),
+                    ),
+                ]
+    vulns = (
+        *vulns,
+        *build_vulnerabilities(
+            locations=locations,
+            method=(core_model.MethodsEnum.AWS_IAM_ROOT_HAS_ACCESS_KEYS),
+            aws_response=root_user,
+        ),
+    )
+
+    return vulns
+
+
 CHECKS: Tuple[
     Callable[[AwsCredentials], Coroutine[Any, Any, Tuple[Vulnerability, ...]]],
     ...,
-] = (users_with_multiple_access_keys,)
+] = (
+    users_with_multiple_access_keys,
+    root_has_access_keys,
+)
