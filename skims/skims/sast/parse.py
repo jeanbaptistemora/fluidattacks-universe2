d11b4f82deac58a6d51d521ef6883d3961e90336
Juan Echeverri <jecheverri@fluidattacks.com>
2023-01-23 18:41:59 -0500

    skims\feat(back): #8627 add syntax readers
    - Add python syntax readers

--- skims/skims/sast/parse.py
+++ skims/skims/sast/parse.py
@@ -294,7 +294,6 @@ def _parse_one_cached(
         GraphShardMetadataLanguage.JAVASCRIPT,
         GraphShardMetadataLanguage.JSON,
         GraphShardMetadataLanguage.KOTLIN,
-        GraphShardMetadataLanguage.PYTHON,
         GraphShardMetadataLanguage.TYPESCRIPT,
         GraphShardMetadataLanguage.YAML,
     }
--- skims/skims/syntax_graph/dispatchers/python.py
+++ skims/skims/syntax_graph/dispatchers/python.py
@@ -1,4 +1,6 @@
 from syntax_graph.syntax_readers.python import (
+    function_definition as python_function_definition,
+    identifier as python_identifier,
     import_statement as python_import_statement,
     module as python_module,
 )
@@ -10,14 +12,26 @@ from syntax_graph.types import (
 PYTHON_DISPATCHERS: Dispatchers = (
     Dispatcher(
         applicable_types={
-            "module",
+            "function_definition",
         },
-        syntax_reader=python_module.reader,
+        syntax_reader=python_function_definition.reader,
+    ),
+    Dispatcher(
+        applicable_types={
+            "identifier",
+        },
+        syntax_reader=python_identifier.reader,
     ),
     Dispatcher(
         applicable_types={
             "import_statement",
         },
         syntax_reader=python_import_statement.reader,
+    ),
+    Dispatcher(
+        applicable_types={
+            "module",
+        },
+        syntax_reader=python_module.reader,
     ),
 )
