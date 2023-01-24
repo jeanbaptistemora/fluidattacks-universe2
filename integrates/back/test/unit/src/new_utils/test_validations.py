856074aa7fdf2f91dc1baf88967f0d17b8f538aa
Andres Uribe <auribe@fluidattacks.com>
2023-01-24 20:48:51 +0000

    integrates\refac(back): #8264 test validators
    - add missing tests in validations

--- integrates/back/test/unit/src/new_utils/test_validations.py
+++ integrates/back/test/unit/src/new_utils/test_validations.py
@@ -10,6 +10,7 @@ from custom_exceptions import (
     InvalidFieldChange,
     InvalidFieldLength,
     InvalidMarkdown,
+    InvalidMinTimeToRemediate,
     InvalidReportFilter,
     InvalidSeverityUpdateValues,
     InvalidSpacesField,
@@ -805,6 +806,23 @@ def test_validate_string_length_between_deco() -> None:
         decorated_func_obj(test_obj=test_obj_fail)
 
 
+def test_validate_title_change() -> None:
+    # Test valid input
+    assert validations.validate_finding_title_change_policy(
+        old_title="old_title",
+        new_title="new_title",
+        status=FindingStateStatus.CREATED,
+    )
+
+    # Test invalid input
+    with pytest.raises(InvalidFieldChange):
+        validations.validate_finding_title_change_policy(
+            old_title="old_title",
+            new_title="new_title",
+            status=FindingStateStatus.APPROVED,
+        )
+
+
 def test_validate_title_change_deco() -> None:
     @validations.validate_finding_title_change_policy_deco(
         "old_title_field",
@@ -856,6 +874,17 @@ def test_validate_title_change_deco() -> None:
         decorated_func_obj(
             test_obj=test_obj, status_field=FindingStateStatus.APPROVED
         )
+
+
+def test_validate_commit_hash() -> None:
+    validations.validate_commit_hash(
+        "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+    )
+    validations.validate_commit_hash(
+        "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    )
+    with pytest.raises(InvalidCommitHash):
+        validations.validate_commit_hash("invalid Hash")
 
 
 def test_validate_commit_hash_deco() -> None:
@@ -1024,6 +1053,89 @@ def test_validate_markdown_deco() -> None:
 
     with pytest.raises(InvalidMarkdown):
         decorated_func(text="<span>Example Text</span>")
+
+
+def test_validate_no_duplicate_drafts() -> None:
+    test_finding = (
+        Finding(
+            id="3c475384-834c-47b0-ac71-a41a022e401c",
+            group_name="group1",
+            state=FindingState(
+                modified_by="test1@gmail.com",
+                modified_date=datetime.fromisoformat(
+                    "2017-04-08T00:45:11+00:00"
+                ),
+                source=Source.ASM,
+                status=FindingStateStatus.CREATED,
+            ),
+            title="001. SQL injection - C Sharp SQL API",
+            recommendation="Updated recommendation",
+            description="I just have updated the description",
+            hacker_email="test1@gmail.com",
+            severity=Finding31Severity(
+                attack_complexity=Decimal("0.44"),
+                attack_vector=Decimal("0.2"),
+                availability_impact=Decimal("0.22"),
+                availability_requirement=Decimal("1.5"),
+                confidentiality_impact=Decimal("0.22"),
+                confidentiality_requirement=Decimal("0.5"),
+                exploitability=Decimal("0.94"),
+                integrity_impact=Decimal("0.22"),
+                integrity_requirement=Decimal("1"),
+                modified_availability_impact=Decimal("0.22"),
+                modified_user_interaction=Decimal("0.62"),
+                modified_integrity_impact=Decimal("0"),
+                modified_attack_complexity=Decimal("0.44"),
+                modified_severity_scope=Decimal("0"),
+                modified_privileges_required=Decimal("0.27"),
+                modified_attack_vector=Decimal("0.85"),
+                modified_confidentiality_impact=Decimal("0.22"),
+                privileges_required=Decimal("0.62"),
+                severity_scope=Decimal("1.2"),
+                remediation_level=Decimal("0.95"),
+                report_confidence=Decimal("1"),
+                user_interaction=Decimal("0.85"),
+            ),
+            requirements=(
+                "REQ.0132. Passwords (phrase type) "
+                "must be at least 3 words long."
+            ),
+            threat="Updated threat",
+            attack_vector_description=("This is an updated attack vector"),
+            evidences=FindingEvidences(
+                evidence1=FindingEvidence(
+                    description="evidence1",
+                    url="group1-3c475384-834c-47b0-ac71-a41a022e401c-"
+                    "evidence1",
+                    modified_date=datetime.fromisoformat(
+                        "2020-11-19T13:37:10+00:00"
+                    ),
+                ),
+                records=FindingEvidence(
+                    description="records",
+                    url="group1-3c475384-834c-47b0-ac71-a41a022e401c-"
+                    "records",
+                    modified_date=datetime.fromisoformat(
+                        "2111-11-19T13:37:10+00:00"
+                    ),
+                ),
+            ),
+        ),
+    )
+
+    assert validations.validate_no_duplicate_drafts(
+        new_title="New Title", drafts=(), findings=test_finding
+    )
+    assert validations.validate_no_duplicate_drafts(
+        new_title="New Title", drafts=test_finding, findings=()
+    )
+
+    with pytest.raises(DuplicateDraftFound):
+        validations.validate_no_duplicate_drafts(
+            new_title="001. SQL injection - C Sharp SQL API",
+            drafts=(),
+            findings=test_finding,
+        )
 
 
 def test_validate_no_duplicate_drafts_deco() -> None:
@@ -1299,3 +1411,12 @@ def test_validate_chart_field() -> None:
     validations.validate_chart_field("content", "field")
     with pytest.raises(InvalidChar):
         validations.validate_chart_field("content!", "field")
+
+
+def test_check_and_set_min_time_to_remediate() -> None:
+    assert validations.check_and_set_min_time_to_remediate(None) is None
+    assert validations.check_and_set_min_time_to_remediate("5") == 5
+    assert validations.check_and_set_min_time_to_remediate("10") == 10
+    with pytest.raises(InvalidMinTimeToRemediate):
+        validations.check_and_set_min_time_to_remediate("-5")
+        validations.check_and_set_min_time_to_remediate("abc")
