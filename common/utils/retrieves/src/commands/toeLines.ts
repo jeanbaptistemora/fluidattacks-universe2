e0438b6ceacb8eefb36ed6b682edc2bbc15a8f8f
Diego Restrepo <drestrepo@fluidattacks.com>
2023-01-24 09:13:26 -0500

    common\feat(front): #8440 get toe lines
    - get toe lines from api
    - register toe lines command
    - no-deltas-test

---
+++ common/utils/retrieves/src/commands/toeLines.ts
@@ -0,0 +1,79 @@
+import { join } from "path";
+
+import type { ExtensionContext } from "vscode";
+// eslint-disable-next-line import/no-unresolved
+import { workspace } from "vscode";
+
+import { ToeLinesPanel } from "../panels/ToelinesPanel";
+import { GET_TOE_LINES } from "../queries";
+import type { GitRootTreeItem } from "../treeItems/gitRoot";
+import type { IEdge, IToeLinesPaginator } from "../types";
+import { getClient } from "../utils/apollo";
+
+async function getToeLines(
+  groupName: string,
+  rootId: string
+): Promise<IEdge[]> {
+  const result = await Promise.resolve(
+    getClient()
+      .query({
+        query: GET_TOE_LINES,
+        variables: { first: 500, groupName, rootId },
+      })
+      .then((_result): IToeLinesPaginator => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
+        return _result.data.group.toeLines;
+      })
+  );
+
+  let { edges } = result;
+  let { hasNextPage } = result.pageInfo;
+  while (hasNextPage) {
+    // eslint-disable-next-line no-await-in-loop
+    const next = await Promise.resolve(
+      getClient()
+        .query({
+          query: GET_TOE_LINES,
+          variables: {
+            after: result.pageInfo.endCursor,
+            first: 500,
+            groupName,
+            rootId,
+          },
+        })
+        .then((_result): IToeLinesPaginator => {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
+          return _result.data.group.toeLines;
+        })
+    );
+    // eslint-disable-next-line fp/no-mutation
+    edges = [...edges, ...next.edges];
+    // eslint-disable-next-line fp/no-mutation, prefer-destructuring
+    hasNextPage = next.pageInfo.hasNextPage;
+  }
+
+  return edges;
+}
+
+function toeLines(context: ExtensionContext, node: GitRootTreeItem): void {
+  Promise.resolve(getToeLines(node.groupName, node.rootId))
+    .then((_result): void => {
+      if (!workspace.workspaceFolders) {
+        return;
+      }
+      const servicePath = workspace.workspaceFolders[0].uri.path;
+      const fusionPath = join(
+        servicePath,
+        "groups",
+        node.groupName,
+        "fusion",
+        node.nickname
+      );
+      ToeLinesPanel.render(context.extensionUri, fusionPath, _result);
+    })
+    .catch((_err): [] => {
+      return [];
+    });
+}
+
+export { toeLines };
--- common/utils/retrieves/src/extension.ts
+++ common/utils/retrieves/src/extension.ts
@@ -4,13 +4,23 @@ import type { ExtensionContext } from "vscode";
 import { commands, window, workspace } from "vscode";
 
 import { clone } from "./commands/clone";
+import { toeLines } from "./commands/toeLines";
 import { GroupsProvider } from "./providers/groups";
+import type { GitRootTreeItem } from "./treeItems/gitRoot";
 
 function activate(_context: ExtensionContext): void {
   if (!workspace.workspaceFolders) {
     return;
   }
 
+  _context.subscriptions.push(
+    commands.registerCommand(
+      "retrieves.lines",
+      (node: GitRootTreeItem): void => {
+        toeLines(_context, node);
+      }
+    )
+  );
   const repo = simpleGit(workspace.workspaceFolders[0].uri.path);
   void repo.listRemote(["--get-url"], (err, data): void => {
     if (err) {
