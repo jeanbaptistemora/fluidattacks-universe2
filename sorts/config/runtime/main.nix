c9964c3486fe34b9fcbfddb060619feb0a4945ea
Daniel Salazar <dsalazar@fluidattacks.com>
2023-01-26 16:52:53 -0500

    sorts\feat(build): #8514 use libxcrypt
    - Use libxcrypt in order to properly compile
    psycopg2 on newer gcc versions

--- sorts/config/runtime/main.nix
+++ sorts/config/runtime/main.nix
@@ -16,6 +16,10 @@
       inputs.nixpkgs.postgresql
     ];
     sourcesYaml = ./pypi-sources.yaml;
+
+    # Required when using psycopg2 on Python3.8
+    # Can be removed once we upgrade to Python3.9
+    searchPathsBuild.export = [["CPATH" inputs.nixpkgs.libxcrypt "/include"]];
   };
 in
   makeTemplate {
