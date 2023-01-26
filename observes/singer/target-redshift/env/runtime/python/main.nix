39935869d51f7b3aaadf5dc8b57ea053d805fa5b
Daniel Salazar <dsalazar@fluidattacks.com>
2023-01-26 16:27:26 -0500

    observes\feat(build): #8514 use libxcrypt
    - Use libxcrypt in order to properly compile
    psycopg2 on newer gcc versions

--- observes/singer/target-redshift/env/runtime/python/main.nix
+++ observes/singer/target-redshift/env/runtime/python/main.nix
@@ -14,4 +14,8 @@ makePythonPypiEnvironment {
     inputs.nixpkgs.postgresql
   ];
   sourcesYaml = ./pypi-sources.yaml;
+
+  # Required when using psycopg2 on Python3.8
+  # Can be removed once we upgrade to Python3.9
+  searchPathsBuild.export = [["CPATH" inputs.nixpkgs.libxcrypt "/include"]];
 }
