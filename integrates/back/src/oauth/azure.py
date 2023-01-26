7d4c2ea04ad855b7fed00fe9961afbfc0fe38b54
Sebastian Lizcano <slizcano@fluidattacks.com>
2023-01-25 23:33:17 -0500

    integrates\refac(back): #8006 get refresh token
    - refresh oauth2 access_token from provider

--- integrates/back/src/oauth/azure.py
+++ integrates/back/src/oauth/azure.py
@@ -10,13 +10,31 @@ from context import (
     FI_AZURE_OAUTH2_REPOSITORY_APP_ID,
     FI_AZURE_OAUTH2_REPOSITORY_SECRET,
 )
+from datetime import (
+    datetime,
+)
+from db_model.credentials.types import (
+    Credentials,
+    CredentialsState,
+    OauthAzureSecret,
+)
+from db_model.credentials.update import (
+    update_credential_state,
+)
 import json
 import logging
 import logging.config
+from newutils.datetime import (
+    get_minus_delta,
+    get_plus_delta,
+    get_utc_now,
+)
+import pytz
 from settings import (
     LOGGING,
 )
 from typing import (
+    Any,
     Optional,
 )
 
@@ -76,5 +94,84 @@ async def get_azure_refresh_token(
                     continue
 
                 return result
+
+    return None
+
+
+async def get_azure_token(
+    *,
+    credential: Credentials,
+    loaders: Any,
+) -> Optional[str]:
+    if not isinstance(credential.state.secret, OauthAzureSecret):
+        return None
+
+    request_parameters: dict[str, str] = dict(
+        client_assertion_type=(
+            "urn:ietf:params:oauth:client-assertion-type:jwt-bearer"
+        ),
+        grant_type="refresh_token",
+        client_assertion=FI_AZURE_OAUTH2_REPOSITORY_SECRET,
+        assertion=credential.state.secret.arefresh_token,
+        redirect_uri=credential.state.secret.redirect_uri,
+    )
+    data = FormData()
+    for key, value in request_parameters.items():
+        data.add_field(key, value)
+    retries: int = 0
+    retry: bool = True
+    async with ClientSession(
+        headers={"Content-Type": "application/x-www-form-urlencoded"}
+    ) as session:
+        while retry and retries < 5:
+            retry = False
+            async with session.post(
+                AZURE_REFRESH_URL,
+                data=data,
+            ) as response:
+                try:
+                    result = await response.json()
+                except (
+                    json.decoder.JSONDecodeError,
+                    ClientError,
+                ) as exc:
+                    LOGGER.exception(exc, extra=dict(extra=locals()))
+                    break
+                if not response.ok:
+                    retry = True
+                    retries += 1
+                    await asyncio.sleep(0.2)
+                    continue
+
+                new_state = CredentialsState(
+                    modified_by=credential.state.modified_by,
+                    modified_date=datetime.now(tz=pytz.timezone("UTC")),
+                    name=credential.state.name,
+                    secret=OauthAzureSecret(
+                        redirect_uri=credential.state.secret.redirect_uri,
+                        arefresh_token=result["refresh_token"],
+                        access_token=result["access_token"],
+                        valid_until=get_plus_delta(
+                            get_minus_delta(get_utc_now(), seconds=60),
+                            seconds=int(result["expires_in"]),
+                        ),
+                    ),
+                    is_pat=credential.state.is_pat,
+                    azure_organization=credential.state.azure_organization,
+                    type=credential.state.type,
+                )
+                await update_credential_state(
+                    current_value=credential.state,
+                    credential_id=credential.id,
+                    organization_id=credential.organization_id,
+                    state=new_state,
+                    force_update_owner=False,
+                )
+                loaders.credentials.clear_all()
+                loaders.organization_credentials.clear(
+                    credential.organization_id
+                )
+
+                return result["access_token"]
 
     return None
