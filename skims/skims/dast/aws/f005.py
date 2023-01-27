7d7a3a8439f7bf90407a1977e275d0236bb091b5
Luis Saavedra <lsaavedra@fluidattacks.com>
2023-01-27 14:42:14 -0500

    skims\feat(back): #7612 add method
    - Add method for F005

--- skims/skims/dast/aws/f005.py
+++ skims/skims/dast/aws/f005.py
@@ -114,7 +114,92 @@ async def allows_priv_escalation_by_policies_versions(
     return vulns
 
 
+async def allows_priv_escalation_by_attach_policy(
+    credentials: AwsCredentials,
+) -> core_model.Vulnerabilities:
+    response: Dict[str, Any] = await run_boto3_fun(
+        credentials,
+        service="iam",
+        function="list_policies",
+        parameters={"Scope": "Local", "OnlyAttached": True},
+    )
+    policies = response.get("Policies", []) if response else []
+    method = core_model.MethodsEnum.AWS_ALLOWS_PRIV_ESCALATION_BY_ATTACH_POLICY
+    vulns: core_model.Vulnerabilities = ()
+    if policies:
+        for policy in policies:
+            locations: List[Location] = []
+            pol_ver: Dict[str, Any] = await run_boto3_fun(
+                credentials,
+                service="iam",
+                function="get_policy_version",
+                parameters={
+                    "PolicyArn": str(policy["Arn"]),
+                    "VersionId": str(policy["DefaultVersionId"]),
+                },
+            )
+            policy_names = pol_ver.get("PolicyVersion", {})
+            pol_access = ast.literal_eval(
+                str(policy_names.get("Document", {}))
+            )
+            policy_statements = ast.literal_eval(
+                str(pol_access.get("Statement", []))
+            )
+
+            if not isinstance(policy_statements, List):
+                policy_statements = [policy_statements]
+
+            for index, stm in enumerate(policy_statements):
+                with suppress(KeyError):
+                    vulnerable = (
+                        stm["Effect"] == "Allow"
+                        and "Resource" in stm
+                        and "iam:AttachUserPolicy" in stm["Action"]
+                    )
+
+                    if vulnerable:
+                        locations = [
+                            *[
+                                Location(
+                                    access_patterns=(
+                                        f"/Document/Statement/{index}/Effect",
+                                        (
+                                            f"/Document/Statement/{index}"
+                                            "/Resource"
+                                        ),
+                                        f"/Document/Statement/{index}/Action",
+                                    ),
+                                    arn=(f"{policy['Arn']}"),
+                                    values=(
+                                        policy_statements[index]["Effect"],
+                                        policy_statements[index]["Resource"],
+                                        policy_statements[index]["Action"],
+                                    ),
+                                    description=t(
+                                        "lib_path.f005."
+                                        "allows_priv_escalation_by_attach_"
+                                        "policy"
+                                    ),
+                                )
+                            ],
+                        ]
+
+            vulns = (
+                *vulns,
+                *build_vulnerabilities(
+                    locations=locations,
+                    method=method,
+                    aws_response=policy_names,
+                ),
+            )
+
+    return vulns
+
+
 CHECKS: Tuple[
     Callable[[AwsCredentials], Coroutine[Any, Any, Tuple[Vulnerability, ...]]],
     ...,
-] = (allows_priv_escalation_by_policies_versions,)
+] = (
+    allows_priv_escalation_by_policies_versions,
+    allows_priv_escalation_by_attach_policy,
+)
