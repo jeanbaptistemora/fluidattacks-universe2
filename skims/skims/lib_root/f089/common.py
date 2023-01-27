ba1e67480fd4d54857c9521af90302f0ef828537
Luis Patino <lpatino@fluidattacks.com>
2023-01-27 11:06:07 -0500

    skims\feat(back): #8567 f089 usage of json.parse unvalidated data pt3
    - Adds method logic for detection of direct case.
    - Updates .csv test output.
    - TODO: check if additional logic has sense in order to extend untouched
    variable case.

--- skims/skims/lib_root/f089/common.py
+++ skims/skims/lib_root/f089/common.py
@@ -12,6 +12,14 @@ from utils import (
 
 def json_parse_unval_data(graph: Graph) -> List[NId]:
     vuln_nodes: List[NId] = []
-    for n_id in g.matching_nodes(graph, label_type="Placeholder"):
-        vuln_nodes.append(n_id)
+    for n_id in g.matching_nodes(
+        graph, label_type="MethodInvocation", expression="JSON.parse"
+    ):
+        for m_id in g.get_nodes_by_path(
+            graph, n_id, [], "ArgumentList", "MethodInvocation"
+        ):
+            if (exp := graph.nodes[m_id].get("expression")) and (
+                exp == "localStorage.getItem"
+            ):
+                vuln_nodes.append(n_id)
     return vuln_nodes
