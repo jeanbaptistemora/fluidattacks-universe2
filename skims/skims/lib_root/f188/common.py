815a7dd1acaa8bc5ca746a72fe3a0beaf4565539
Jhon Romero <jromero@fluidattacks.com>
2023-01-27 11:03:59 -0500

    skims\feat(back): #8668 f188 lack of validation
    - check method invocation

---
+++ skims/skims/lib_root/f188/common.py
@@ -0,0 +1,40 @@
+from model.core_model import (
+    MethodsEnum,
+)
+from model.graph_model import (
+    Graph,
+    NId,
+)
+from symbolic_eval.evaluate import (
+    evaluate,
+)
+from symbolic_eval.utils import (
+    get_backward_paths,
+)
+from typing import (
+    List,
+)
+from utils import (
+    graph as g,
+)
+
+
+def is_insec_invocation(graph: Graph, n_id: NId, method: MethodsEnum) -> bool:
+    for path in get_backward_paths(graph, n_id):
+        evaluation = evaluate(method, graph, path, n_id)
+        if evaluation and evaluation.danger:
+            return True
+    return False
+
+
+def has_dangerous_param(graph: Graph) -> List[NId]:
+    vuln_nodes: List[NId] = []
+    sensitive_methods = {"window.addEventListener"}
+    method = MethodsEnum.TSX_LACK_OF_VALIDATION_EVENT_LISTENER
+    for member in g.matching_nodes(graph, label_type="MethodInvocation"):
+        if graph.nodes[member].get(
+            "expression"
+        ) in sensitive_methods and is_insec_invocation(graph, member, method):
+            vuln_nodes.append(member)
+
+    return vuln_nodes
---
+++ skims/skims/lib_root/f188/typescript.py
@@ -0,0 +1,41 @@
+from lib_root.f188.common import (
+    has_dangerous_param,
+)
+from model.core_model import (
+    MethodsEnum,
+    Vulnerabilities,
+)
+from model.graph_model import (
+    GraphDB,
+    GraphShardMetadataLanguage as GraphLanguage,
+    GraphShardNode,
+)
+from sast.query import (
+    get_vulnerabilities_from_n_ids,
+)
+from typing import (
+    Iterable,
+)
+
+
+def lack_of_validation_dom_window(
+    graph_db: GraphDB,
+) -> Vulnerabilities:
+    method = MethodsEnum.TSX_LACK_OF_VALIDATION_EVENT_LISTENER
+
+    def n_ids() -> Iterable[GraphShardNode]:
+        for shard in graph_db.shards_by_language(
+            GraphLanguage.TYPESCRIPT,
+        ):
+            if shard.syntax_graph is None:
+                continue
+            graph = shard.syntax_graph
+            for n_id in has_dangerous_param(graph):
+                yield shard, n_id
+
+    return get_vulnerabilities_from_n_ids(
+        desc_key="src.lib_root.f280.non_secure_construction_of_cookies",
+        desc_params={},
+        graph_shard_nodes=n_ids(),
+        method=method,
+    )
