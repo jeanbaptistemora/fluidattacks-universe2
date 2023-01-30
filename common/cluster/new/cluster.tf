fef314e07abad81752fe71a4ca42dc62ad6efb02
Daniel Salazar <dsalazar@fluidattacks.com>
2023-01-30 18:07:44 -0500

    common\feat(infra): #8675 update cluster module
    - Update cluster module to latest version

--- common/cluster/new/cluster.tf
+++ common/cluster/new/cluster.tf
@@ -1,6 +1,6 @@
 module "cluster" {
   source                         = "terraform-aws-modules/eks/aws"
-  version                        = "~> 19.5.1"
+  version                        = "19.6.0"
   cluster_name                   = local.cluster_name
   cluster_version                = "1.24"
   cluster_endpoint_public_access = true
