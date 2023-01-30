cac3d38dc9ffd225e6d1acd74504346cd700618b
Jhon Romero <jromero@fluidattacks.com>
2023-01-30 11:33:30 -0500

    skims\feat(back): #8676 replace statement block reader
    - replace statement with execution block readerader in go

--- skims/skims/syntax_graph/syntax_readers/go/block.py
+++ skims/skims/syntax_graph/syntax_readers/go/block.py
@@ -1,15 +1,11 @@
 from model.graph_model import (
     NId,
 )
-from syntax_graph.syntax_nodes.statement_block import (
-    build_statement_block_node,
+from syntax_graph.syntax_nodes.execution_block import (
+    build_execution_block_node,
 )
 from syntax_graph.types import (
     SyntaxGraphArgs,
-)
-from typing import (
-    cast,
-    Iterator,
 )
 from utils.graph import (
     adj_ast,
@@ -27,4 +23,4 @@ def reader(args: SyntaxGraphArgs) -> NId:
         for _id in c_ids
         if args.ast_graph.nodes[_id]["label_type"] not in ignored_labels
     )
-    return build_statement_block_node(args, cast(Iterator[str], filtered_ids))
+    return build_execution_block_node(args, filtered_ids)
--- skims/skims/syntax_graph/syntax_readers/go/expression_list.py
+++ skims/skims/syntax_graph/syntax_readers/go/expression_list.py
@@ -1,8 +1,8 @@
 from model.graph_model import (
     NId,
 )
-from syntax_graph.syntax_nodes.statement_block import (
-    build_statement_block_node,
+from syntax_graph.syntax_nodes.execution_block import (
+    build_execution_block_node,
 )
 from syntax_graph.types import (
     SyntaxGraphArgs,
@@ -16,7 +16,7 @@ def reader(args: SyntaxGraphArgs) -> NId:
     graph = args.ast_graph
     c_ids = adj_ast(graph, args.n_id)
 
-    return build_statement_block_node(
+    return build_execution_block_node(
         args,
         c_ids=(
             _id
