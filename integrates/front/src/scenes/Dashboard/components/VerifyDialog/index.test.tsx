d771c229cdfb184b53e67eb07ca4e0ee30753b16
Laura Naranjo <lnaranjo@fluidattacks.com>
2023-01-31 14:48:07 -0500

    integrates\refac(front): #7060 decrease tech debt
    - wrapping function in useCallback hook
    - Solved 1 smells in 0.5 hrs (2 smells/hr)
    - Remaining: 210 smells, 105 hrs

--- integrates/front/src/scenes/Dashboard/components/VerifyDialog/index.test.tsx
+++ integrates/front/src/scenes/Dashboard/components/VerifyDialog/index.test.tsx
@@ -2,14 +2,75 @@ import type { MockedResponse } from "@apollo/client/testing";
 import { MockedProvider } from "@apollo/client/testing";
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import React from "react";
+import React, { useCallback } from "react";
 
 import { GET_STAKEHOLDER_PHONE, VERIFY_STAKEHOLDER_MUTATION } from "./queries";
+import type { IVerifyFn } from "./types";
 
 import { VerifyDialog } from ".";
 import { Button } from "components/Button";
 
 describe("VerifyDialog", (): void => {
+  const verifyCallback: jest.Mock = jest.fn();
+  const cancelCallback: jest.Mock = jest.fn();
+  const mockQuery: MockedResponse[] = [
+    {
+      request: {
+        query: GET_STAKEHOLDER_PHONE,
+      },
+      result: {
+        data: {
+          me: {
+            __typename: "Me",
+            phone: {
+              callingCountryCode: "1",
+              countryCode: "US",
+              nationalNumber: "1234545",
+            },
+            userEmail: "test@fluidattacks.com",
+          },
+        },
+      },
+    },
+  ];
+  const mocksMutation: readonly MockedResponse[] = [
+    {
+      request: {
+        query: VERIFY_STAKEHOLDER_MUTATION,
+      },
+      result: { data: { verifyStakeholder: { success: true } } },
+    },
+  ];
+  const TestComponent: React.FC = (): JSX.Element => {
+    const handleClick = useCallback(
+      (setVerifyCallbacks: IVerifyFn): (() => void) =>
+        (): void => {
+          setVerifyCallbacks(verifyCallback, cancelCallback);
+        },
+      []
+    );
+
+    return (
+      <MockedProvider
+        addTypename={false}
+        mocks={mockQuery.concat(mocksMutation)}
+      >
+        <VerifyDialog isOpen={true}>
+          {(setVerifyCallbacks): React.ReactNode => {
+            return (
+              <Button
+                onClick={handleClick(setVerifyCallbacks)}
+                variant={"primary"}
+              >
+                {"Button Test"}
+              </Button>
+            );
+          }}
+        </VerifyDialog>
+      </MockedProvider>
+    );
+  };
+
   it("should return a fuction", (): void => {
     expect.hasAssertions();
     expect(typeof VerifyDialog).toBe("function");
@@ -18,57 +79,7 @@ describe("VerifyDialog", (): void => {
   it("should handle stakeholder verification", async (): Promise<void> => {
     expect.hasAssertions();
 
-    const verifyCallback: jest.Mock = jest.fn();
-    const cancelCallback: jest.Mock = jest.fn();
-    const mockQuery: MockedResponse[] = [
-      {
-        request: {
-          query: GET_STAKEHOLDER_PHONE,
-        },
-        result: {
-          data: {
-            me: {
-              __typename: "Me",
-              phone: {
-                callingCountryCode: "1",
-                countryCode: "US",
-                nationalNumber: "1234545",
-              },
-              userEmail: "test@fluidattacks.com",
-            },
-          },
-        },
-      },
-    ];
-    const mocksMutation: readonly MockedResponse[] = [
-      {
-        request: {
-          query: VERIFY_STAKEHOLDER_MUTATION,
-        },
-        result: { data: { verifyStakeholder: { success: true } } },
-      },
-    ];
-
-    render(
-      <MockedProvider
-        addTypename={false}
-        mocks={mockQuery.concat(mocksMutation)}
-      >
-        <VerifyDialog isOpen={true}>
-          {(setVerifyCallbacks): React.ReactNode => {
-            function handleClick(): void {
-              setVerifyCallbacks(verifyCallback, cancelCallback);
-            }
-
-            return (
-              <Button onClick={handleClick} variant={"primary"}>
-                {"Button Test"}
-              </Button>
-            );
-          }}
-        </VerifyDialog>
-      </MockedProvider>
-    );
+    render(<TestComponent />);
     await waitFor((): void => {
       expect(screen.queryByText("verifyDialog.verify")).toBeInTheDocument();
     });
