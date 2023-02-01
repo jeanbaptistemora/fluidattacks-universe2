010342a7b2c4578bee006404b8825990d59495ae
Lewis Contreras <lcontreras@fluidattacks.com>
2023-02-01 08:18:55 -0500

    skims\feat(back): #8104 add method runner sca
    - Add conan runner method for sca

--- skims/skims/lib_path/f011/__init__.py
+++ skims/skims/lib_path/f011/__init__.py
@@ -7,6 +7,7 @@ from lib_path.f011.composer import (
     composer_lock,
 )
 from lib_path.f011.conan import (
+    conan_conanfile_py,
     conan_conanfile_txt,
 )
 from lib_path.f011.gem import (
@@ -58,6 +59,11 @@ def run_composer_lock(content: str, path: str) -> Vulnerabilities:
 @SHIELD_BLOCKING
 def run_conan_conanfile_txt(content: str, path: str) -> Vulnerabilities:
     return conan_conanfile_txt(content, path)
+
+
+@SHIELD_BLOCKING
+def run_conan_conanfile_py(content: str, path: str) -> Vulnerabilities:
+    return conan_conanfile_py(content, path)
 
 
 @SHIELD_BLOCKING
