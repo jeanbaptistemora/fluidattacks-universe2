34de4e7198a6feed7fbff6fdde6e8be1c1778830
Carlos Betancur <cbetancur@fluidattacks.com>
2023-02-02 18:18:47 +0000

    integrates\test(back): #4944 modify mocks
    - modify mocks' scope
    - modify mocked data
    - no-deltas-test

--- integrates/back/test/unit/src/group_access/test_domain.py
+++ integrates/back/test/unit/src/group_access/test_domain.py
@@ -94,25 +94,28 @@ async def test_exists(
     ],
 )
 @patch(
-    get_mocked_path("loaders.group_stakeholders_access.load"),
+    MODULE_AT_TEST + "Dataloaders.group_stakeholders_access",
     new_callable=AsyncMock,
 )
 async def test_get_group_stakeholders_emails(
-    mock_loaders_group_stakeholders_access: AsyncMock,
+    mock_dataloaders_group_stakeholders_access: AsyncMock,
     group_name: str,
     expected_result: list,
 ) -> None:
     assert set_mocks_return_values(
-        mocked_objects=[mock_loaders_group_stakeholders_access],
-        paths_list=["loaders.group_stakeholders_access.load"],
         mocks_args=[[group_name]],
+        mocked_objects=[mock_dataloaders_group_stakeholders_access.load],
+        module_at_test=MODULE_AT_TEST,
+        paths_list=["Dataloaders.group_stakeholders_access"],
     )
     loaders = get_new_context()
     users = await get_group_stakeholders_emails(loaders, group_name)
     for user in expected_result:
         assert user in users
-    assert mock_loaders_group_stakeholders_access.called is True
-    mock_loaders_group_stakeholders_access.assert_called_with(group_name)
+    assert mock_dataloaders_group_stakeholders_access.load.called is True
+    mock_dataloaders_group_stakeholders_access.load.assert_called_with(
+        group_name
+    )
 
 
 async def test_get_managers() -> None:
