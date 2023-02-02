43afb22795656e7122c7f7722223cbf2d9ba3847
Luis Patino <lpatino@fluidattacks.com>
2023-02-02 15:58:29 -0500

    skims\feat(back): #8676 reactivates f344 method
    - Fixes F344 bug causing skims to fail.
    - Updates .csv output.
    - TODO: Change method technique avoiding to send n_ids in triggers.
    - no-deltas-test

--- skims/skims/lib_root/f344/__init__.py
+++ skims/skims/lib_root/f344/__init__.py
@@ -1,7 +1,9 @@
 from lib_root.f344.javascript import (
+    js_local_storage_sens_data_async,
     js_local_storage_with_sensitive_data as js_local_storage_sensitive_data,
 )
 from lib_root.f344.typescript import (
+    ts_local_storage_sens_data_async,
     ts_local_storage_with_sensitive_data as ts_local_storage_sensitive_data,
 )
 from model import (
@@ -13,4 +15,6 @@ FINDING: core_model.FindingEnum = core_model.FindingEnum.F344
 QUERIES: graph_model.Queries = (
     (FINDING, js_local_storage_sensitive_data),
     (FINDING, ts_local_storage_sensitive_data),
+    (FINDING, js_local_storage_sens_data_async),
+    (FINDING, ts_local_storage_sens_data_async),
 )
