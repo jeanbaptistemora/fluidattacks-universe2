b81e385e56499187ecc78da168b674f8acce943f
Jonathan Mesa <jmesa@fluidattacks.com>
2023-02-03 14:20:00 +0000

    integrates\fix(back): #8607 script clear enrolled flag
    - add script for clearing the problematic flag at
    previously removed stakeholders

---
+++ integrates/back/migrations/0358_clear_enrollment_flag_for_inactive_stakeholders.py
@@ -0,0 +1,65 @@
+# pylint: disable=invalid-name
+"""
+Clear flag enrolled in enrollment_metadata facet for stakeholders
+previously removed due to inactivity.
+"""
+from aioextensions import (
+    collect,
+    run,
+)
+import csv
+from dataloaders import (
+    get_new_context,
+)
+from db_model import (
+    enrollment as enrollment_model,
+)
+from db_model.enrollment.types import (
+    EnrollmentMetadataToUpdate,
+)
+import time
+
+
+async def process_email(email: str) -> None:
+    loaders = get_new_context()
+    enrollment = await loaders.enrollment.load(email)
+    if enrollment.enrolled is False:
+        return
+
+    orgs_access = await loaders.stakeholder_organizations_access.load(email)
+    if len(orgs_access) > 0:
+        return
+
+    groups_access = await loaders.stakeholder_groups_access.load(email)
+    if len(groups_access) > 0:
+        return
+
+    await enrollment_model.update_metadata(
+        email=email,
+        metadata=EnrollmentMetadataToUpdate(enrolled=False),
+    )
+    print(f"Updated {email=}")
+
+
+async def main() -> None:
+
+    with open("0358_data.csv", mode="r", encoding="utf8") as f:
+        reader = csv.reader(f)
+        emails = sorted([rows[0] for rows in reader])
+        print(f"{len(emails)=}")
+
+        await collect(
+            tuple(process_email(email) for email in emails),
+            workers=1,
+        )
+
+
+if __name__ == "__main__":
+    execution_time = time.strftime(
+        "Execution Time:    %Y-%m-%d at %H:%M:%S %Z"
+    )
+    run(main())
+    finalization_time = time.strftime(
+        "Finalization Time: %Y-%m-%d at %H:%M:%S %Z"
+    )
+    print(f"{execution_time}\n{finalization_time}")
