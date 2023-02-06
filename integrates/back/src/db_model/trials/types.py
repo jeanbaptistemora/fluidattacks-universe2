99796cd9b0a3d567803e58ad4c82248d24f8361b
David Acevedo <dacevedo@fluidattacks.com>
2023-02-06 18:38:38 +0000

    integrates\feat(back): #8623 trial model
    - added model logic for trial
    - updated sk to enable a pattern for getting all the items

---
+++ integrates/back/src/db_model/trials/types.py
@@ -0,0 +1,15 @@
+from datetime import (
+    datetime,
+)
+from typing import (
+    NamedTuple,
+    Optional,
+)
+
+
+class Trial(NamedTuple):
+    email: str
+    completed: bool
+    extension_date: Optional[datetime]
+    extension_days: int
+    start_date: Optional[datetime]
