6d819f7c39d54b7c62ac528eda3cdd9a0f2ac43c
Lewis Contreras <lcontreras@fluidattacks.com>
2023-02-06 05:35:18 -0500

    skims\feat(back): #8104 add test method
    - Add test for sca method

--- skims/test/test_libpath_sca_methods.py
+++ skims/test/test_libpath_sca_methods.py
@@ -33,6 +33,7 @@ from lib_path.f393.gem import (
 )
 from lib_path.f393.npm import (
     npm_package_json as npm_package_json_dev,
+    npm_pkg_lock_json as npm_pkg_lock_json_dev,
     npm_yarn_lock_dev,
 )
 from lib_path.f393.pub import (
@@ -491,5 +492,27 @@ def test_npm_yarn_lock_dev() -> None:
         assertion = not assertion
     if not (pkg_item == pkg_info[0] and pkg_info[1] == item_ver):
         assertion = not assertion
+
+    assert assertion
+
+
+@pytest.mark.skims_test_group("unittesting")
+def test_npm_pkg_lock_json_dev() -> None:
+    path: str = "skims/test/data/lib_path/f011/package-lock.json"
+    file_contents: str = get_file_info_from_path(path)
+    generator_dep = npm_pkg_lock_json_dev.__wrapped__(  # type: ignore
+        file_contents, path
+    )
+    assertion: bool = True
+    packages = (("@babel/dev", "7.11.0.4"), ("hoek", "5.0.0.3"))
+    for product, version in packages:
+        try:
+            next_dep = next(generator_dep)
+            pkg_item = itemgetter("item")(next_dep[0])
+            item_ver = itemgetter("item")(next_dep[1])
+        except StopIteration:
+            assertion = not assertion
+        if not (pkg_item == product and version == item_ver):
+            assertion = not assertion
 
     assert assertion
