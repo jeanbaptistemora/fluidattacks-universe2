7beed500ba0d3217865224fe39514f8c51b9c420
Lewis Contreras <lcontreras@fluidattacks.com>
2023-02-06 09:05:38 -0500

    skims\feat(back): #8104 add test method
    - Add test for sca method

--- skims/test/test_libpath_sca_methods.py
+++ skims/test/test_libpath_sca_methods.py
@@ -18,6 +18,9 @@ from lib_path.f011.go import (
 from lib_path.f011.maven import (
     maven_pom_xml,
 )
+from lib_path.f011.npm import (
+    npm_package_json,
+)
 from lib_path.f011.pip import (
     pip_requirements_txt,
 )
@@ -505,6 +508,33 @@ def test_npm_pkg_lock_json_dev() -> None:
     )
     assertion: bool = True
     packages = (("@babel/dev", "7.11.0.4"), ("hoek", "5.0.0.3"))
+    for product, version in packages:
+        try:
+            next_dep = next(generator_dep)
+            pkg_item = itemgetter("item")(next_dep[0])
+            item_ver = itemgetter("item")(next_dep[1])
+        except StopIteration:
+            assertion = not assertion
+        if not (pkg_item == product and version == item_ver):
+            assertion = not assertion
+
+    assert assertion
+
+
+@pytest.mark.skims_test_group("unittesting")
+def test_npm_package_json() -> None:
+    path: str = "skims/test/data/lib_path/f011/package.json"
+    file_contents: str = get_file_info_from_path(path)
+    generator_dep = npm_package_json.__wrapped__(  # type: ignore
+        file_contents, path
+    )
+    assertion: bool = True
+    packages = (
+        ("@angular/core", "^13.3.3"),
+        ("cloudron-sysadmin", "1.0.0"),
+        ("script-manager", "0.8.6"),
+        ("slug", "0.9.0"),
+    )
     for product, version in packages:
         try:
             next_dep = next(generator_dep)
