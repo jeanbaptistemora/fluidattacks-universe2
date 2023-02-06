f7d1abaa38d9bb4dc9a3d6038f894addbd73a655
Andres Saldarriaga <asaldarriaga@fluidattacks.com>
2023-02-06 15:19:30 -0500

    airs\refac(front): #7729 new web menu and footer
    - update styles in category on the new menu

--- airs/front/src/scenes/Menu/Categories/Resources/index.tsx
+++ airs/front/src/scenes/Menu/Categories/Resources/index.tsx
@@ -18,78 +18,88 @@ const ResourcesMenu: React.FC<IResourcesProps> = ({
   const { width } = useWindowSize();
 
   return (
-    <Container bgColor={"#ffffff"} display={width > 1200 ? display : "none"}>
-      <Container display={"flex"} height={"310px"} justify={"center"}>
-        <Container maxWidth={"708px"} mr={4}>
-          <Container
-            borderBottomColor={"#b0b0bf"}
-            height={"36px"}
-            mb={3}
-            pb={3}
-          >
-            <Text color={"#8f8fa3"}>
-              {translate.t("menu.resources.learn.title")}
-            </Text>
+    <Container bgColor={"#ffffff"} display={display}>
+      <Container
+        display={width > 960 ? "flex" : "inline"}
+        height={"100%"}
+        justify={"center"}
+        ph={3}
+        scroll={"y"}
+      >
+        <Container maxWidth={width > 960 ? "708px" : "1440px"} mb={3} mr={4}>
+          <Container ph={3}>
+            <Container
+              borderBottomColor={"#b0b0bf"}
+              height={"36px"}
+              mb={3}
+              ph={4}
+            >
+              <Text color={"#8f8fa3"}>
+                {translate.t("menu.resources.learn.title")}
+              </Text>
+            </Container>
+            <Grid columns={width > 1200 ? 2 : 1} gap={"1rem"}>
+              <Container ph={3}>
+                <AirsLink hoverColor={"#bf0b1a"} href={"/solutions/"}>
+                  <Text color={"#121216"} mb={3} weight={"bold"}>
+                    {translate.t("menu.resources.learn.blog.title")}
+                  </Text>
+                </AirsLink>
+                <Text color={"#535365"} mb={3} size={"small"}>
+                  {translate.t("menu.resources.learn.blog.subtitle")}
+                </Text>
+                <AirsLink hoverColor={"#bf0b1a"} href={"/solutions/"}>
+                  <Text color={"#121216"} mb={3} weight={"bold"}>
+                    {translate.t("menu.resources.learn.downloadables.title")}
+                  </Text>
+                </AirsLink>
+                <Text color={"#535365"} size={"small"}>
+                  {translate.t("menu.resources.learn.downloadables.subtitle")}
+                </Text>
+              </Container>
+              <Container ph={3}>
+                <AirsLink hoverColor={"#bf0b1a"} href={"/compliance/"}>
+                  <Text color={"#121216"} mb={3} weight={"bold"}>
+                    {translate.t("menu.resources.learn.clients.title")}
+                  </Text>
+                </AirsLink>
+                <Text color={"#535365"} mb={4} size={"small"}>
+                  {translate.t("menu.resources.learn.clients.subtitle")}
+                </Text>
+              </Container>
+            </Grid>
           </Container>
-          <Grid columns={2} gap={"1rem"}>
-            <Container>
-              <AirsLink hoverColor={"#bf0b1a"} href={"/solutions/"}>
-                <Text color={"#121216"} mb={3} weight={"bold"}>
-                  {translate.t("menu.resources.learn.blog.title")}
-                </Text>
-              </AirsLink>
-              <Text color={"#535365"} mb={3} size={"small"}>
-                {translate.t("menu.resources.learn.blog.subtitle")}
-              </Text>
-              <AirsLink hoverColor={"#bf0b1a"} href={"/solutions/"}>
-                <Text color={"#121216"} mb={3} weight={"bold"}>
-                  {translate.t("menu.resources.learn.downloadables.title")}
-                </Text>
-              </AirsLink>
-              <Text color={"#535365"} size={"small"}>
-                {translate.t("menu.resources.learn.downloadables.subtitle")}
-              </Text>
-            </Container>
-            <Container>
-              <AirsLink hoverColor={"#bf0b1a"} href={"/compliance/"}>
-                <Text color={"#121216"} mb={3} weight={"bold"}>
-                  {translate.t("menu.resources.learn.clients.title")}
-                </Text>
-              </AirsLink>
-              <Text color={"#535365"} mb={4} size={"small"}>
-                {translate.t("menu.resources.learn.clients.subtitle")}
-              </Text>
-            </Container>
-          </Grid>
         </Container>
-        <Container maxWidth={"708px"}>
-          <Container borderBottomColor={"#b0b0bf"} height={"36px"} pb={3}>
-            <Text color={"#8f8fa3"}>
-              {translate.t("menu.resources.help.title")}
-            </Text>
+        <Container maxWidth={width > 960 ? "708px" : "1440px"} scroll={"y"}>
+          <Container ph={3}>
+            <Container borderBottomColor={"#b0b0bf"} pb={3} ph={4}>
+              <Text color={"#8f8fa3"}>
+                {translate.t("menu.resources.help.title")}
+              </Text>
+            </Container>
+            <Grid columns={width > 1200 ? 2 : 1} gap={"1rem"}>
+              <Container ph={3}>
+                <AirsLink hoverColor={"#bf0b1a"} href={"/solutions/"}>
+                  <Text color={"#121216"} mb={3} weight={"bold"}>
+                    {translate.t("menu.resources.help.documentation.title")}
+                  </Text>
+                </AirsLink>
+                <Text color={"#535365"} size={"small"}>
+                  {translate.t("menu.resources.help.documentation.subtitle")}
+                </Text>
+              </Container>
+              <Container ph={3}>
+                <AirsLink hoverColor={"#bf0b1a"} href={"/compliance/"}>
+                  <Text color={"#121216"} mb={3} weight={"bold"}>
+                    {translate.t("menu.resources.help.faq.title")}
+                  </Text>
+                </AirsLink>
+                <Text color={"#535365"} size={"small"}>
+                  {translate.t("menu.resources.help.faq.subtitle")}
+                </Text>
+              </Container>
+            </Grid>
           </Container>
-          <Grid columns={2} gap={"1rem"}>
-            <Container>
-              <AirsLink hoverColor={"#bf0b1a"} href={"/solutions/"}>
-                <Text color={"#121216"} mb={3} weight={"bold"}>
-                  {translate.t("menu.resources.help.documentation.title")}
-                </Text>
-              </AirsLink>
-              <Text color={"#535365"} size={"small"}>
-                {translate.t("menu.resources.help.documentation.subtitle")}
-              </Text>
-            </Container>
-            <Container>
-              <AirsLink hoverColor={"#bf0b1a"} href={"/compliance/"}>
-                <Text color={"#121216"} mb={3} weight={"bold"}>
-                  {translate.t("menu.resources.help.faq.title")}
-                </Text>
-              </AirsLink>
-              <Text color={"#535365"} size={"small"}>
-                {translate.t("menu.resources.help.faq.subtitle")}
-              </Text>
-            </Container>
-          </Grid>
         </Container>
       </Container>
     </Container>
