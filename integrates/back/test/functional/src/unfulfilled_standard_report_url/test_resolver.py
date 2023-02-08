e145f7ca7fb63d98d0caedc89aa93738552ca604
Andres Aguirre <aaguirre@fluidattacks.com>
2023-02-08 15:22:39 +0000

    integrates\test(back): #8720 invalid parameters in report
    - Test the invalid parameters in to get a filtered standard report

--- integrates/back/test/functional/src/unfulfilled_standard_report_url/test_resolver.py
+++ integrates/back/test/functional/src/unfulfilled_standard_report_url/test_resolver.py
@@ -62,3 +62,53 @@ async def test_get_filtered_unfulfilled_standard_report_url(
         unfulfilled_standards=unfulfilled_standards,
     )
     assert "/reports/pdf/" in result["data"]["unfulfilledStandardReportUrl"]
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    ["email", "unfulfilled_standards"],
+    [
+        ["admin@gmail.com", []],
+    ],
+)
+@pytest.mark.resolver_test_group("unfulfilled_standard_report_url")
+async def test_invalid_unfulfilled_standards(
+    populate: bool, email: str, unfulfilled_standards: Optional[list[str]]
+) -> None:
+    assert populate
+    group_name: str = "group1"
+    result: dict[str, Any] = await get_result(
+        user=email,
+        group_name=group_name,
+        unfulfilled_standards=unfulfilled_standards,
+    )
+    assert "errors" in result
+    assert (
+        result["errors"][0]["message"]
+        == "Exception - Field unfulfilledStandards is invalid"
+    )
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    ["email", "unfulfilled_standards"],
+    [
+        ["admin@gmail.com", ["test_standard_error"]],
+    ],
+)
+@pytest.mark.resolver_test_group("unfulfilled_standard_report_url")
+async def test_invalid_standard_id(
+    populate: bool, email: str, unfulfilled_standards: Optional[list[str]]
+) -> None:
+    assert populate
+    group_name: str = "group1"
+    result: dict[str, Any] = await get_result(
+        user=email,
+        group_name=group_name,
+        unfulfilled_standards=unfulfilled_standards,
+    )
+    assert "errors" in result
+    assert (
+        result["errors"][0]["message"]
+        == "Exception - The standard id is invalid"
+    )
