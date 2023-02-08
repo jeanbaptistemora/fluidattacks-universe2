15513a082e92479c435c86cdcd651af239d3ce51
Laura Naranjo <lnaranjo@fluidattacks.com>
2023-02-08 11:24:19 -0500

    integrates\refac(front): #7060 decrease tech debt
    - rever exceptions nosonar

--- integrates/front/src/scenes/Dashboard/containers/Finding-Content/VulnerabilitiesView/HandleAcceptanceModal/ZeroRiskTable/changeZeroRiskFormatter.tsx
+++ integrates/front/src/scenes/Dashboard/containers/Finding-Content/VulnerabilitiesView/HandleAcceptanceModal/ZeroRiskTable/changeZeroRiskFormatter.tsx
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import type { IVulnDataAttr } from "../types";
 import { MixedCheckBoxButton } from "components/MixedCheckBoxButton";
 
-export const changeZeroRiskFormatter = (
-  row: IVulnDataAttr,
-  approveFunction: (arg1?: IVulnDataAttr | undefined) => void,
-  deleteFunction: (arg1?: IVulnDataAttr | undefined) => void
-): JSX.Element => {
-  function handleOnApprove(): void {
+interface IChangeZeroRiskFormatterProps {
+  row: IVulnDataAttr;
+  approveFunction: (arg1?: IVulnDataAttr | undefined) => void;
+  deleteFunction: (arg1?: IVulnDataAttr | undefined) => void;
+}
+const ChangeZeroRiskFormatter: React.FC<IChangeZeroRiskFormatterProps> = ({
+  row,
+  approveFunction,
+  deleteFunction,
+}: IChangeZeroRiskFormatterProps): JSX.Element => {
+  const handleOnApprove = useCallback((): void => {
     approveFunction(row);
-  }
+  }, [approveFunction, row]);
 
-  function handleOnDelete(): void {
+  const handleOnDelete = useCallback((): void => {
     deleteFunction(row);
-  }
+  }, [deleteFunction, row]);
 
   return (
     <div style={{ width: "150px" }}>
@@ -25,12 +30,24 @@ export const changeZeroRiskFormatter = (
         isSelected={row.acceptance !== ""}
         isYesEnabled={row.acceptance !== "REJECTED"}
         noLabel={row.acceptance === "REJECTED" ? "REJECTED" : "REJECT"}
-        // eslint-disable-next-line
-        onApprove={handleOnApprove} // NOSONAR
-        // eslint-disable-next-line
-        onDelete={handleOnDelete} // NOSONAR
+        onApprove={handleOnApprove}
+        onDelete={handleOnDelete}
         yesLabel={row.acceptance === "APPROVED" ? "CONFIRMED" : "CONFIRM"}
       />
     </div>
+  );
+};
+
+export const changeZeroRiskFormatter = (
+  row: IVulnDataAttr,
+  approveFunction: (arg1?: IVulnDataAttr | undefined) => void,
+  deleteFunction: (arg1?: IVulnDataAttr | undefined) => void
+): JSX.Element => {
+  return (
+    <ChangeZeroRiskFormatter
+      approveFunction={approveFunction}
+      deleteFunction={deleteFunction}
+      row={row}
+    />
   );
 };
