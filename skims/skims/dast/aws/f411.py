8c93b10b9cc5563d9ac936da114f4d8489474575
Luis Saavedra <lsaavedra@fluidattacks.com>
2023-02-09 17:38:25 -0500

    skims\feat(back): #7612 add method
    - Add method for F411

--- skims/skims/dast/aws/f411.py
+++ skims/skims/dast/aws/f411.py
@@ -48,7 +48,6 @@ async def ebs_uses_default_kms_key(
                     and alias.get("AliasName") == "alias/aws/ebs"
                 ):
                     locations = [
-                        *locations,
                         Location(
                             arn=(
                                 f"arn:aws:ec2:::VolumeId/{volume['VolumeId']}"
@@ -61,14 +60,85 @@ async def ebs_uses_default_kms_key(
                         ),
                     ]
 
-                vulns = (
-                    *vulns,
-                    *build_vulnerabilities(
-                        locations=locations,
-                        method=(method),
-                        aws_response=alias,
-                    ),
-                )
+                    vulns = (
+                        *vulns,
+                        *build_vulnerabilities(
+                            locations=locations,
+                            method=(method),
+                            aws_response=alias,
+                        ),
+                    )
+
+    return vulns
+
+
+async def get_paginated_items(
+    credentials: AwsCredentials,
+) -> List:
+    """Get all items in paginated API calls."""
+    pools = []
+    args: Dict[str, Any] = {
+        "credentials": credentials,
+        "service": "efs",
+        "function": "describe_file_systems",
+        "parameters": {"MaxItems": 50},
+    }
+    data = await run_boto3_fun(**args)
+    object_name = "FileSystems"
+    pools += data.get(object_name, [])
+
+    next_token = data.get("NextMarker", None)
+    while next_token:
+        args["parameters"]["Marker"] = next_token
+        data = await run_boto3_fun(**args)
+        pools += data.get(object_name, [])
+        next_token = data.get("NextMarker", None)
+
+    return pools
+
+
+async def efs_uses_default_kms_key(
+    credentials: AwsCredentials,
+) -> core_model.Vulnerabilities:
+    filesystems = await get_paginated_items(credentials)
+    vulns: core_model.Vulnerabilities = ()
+    method = core_model.MethodsEnum.AWS_EFS_USES_DEFAULT_KMS_KEY
+    list_aliases: Dict[str, Any] = await run_boto3_fun(
+        credentials, service="kms", function="list_aliases"
+    )
+    kms_aliases = list_aliases.get("Aliases", []) if list_aliases else []
+    for filesystem in filesystems:
+        vol_key = filesystem.get("KmsKeyId", "")
+        if vol_key:
+            locations: List[Location] = []
+            for alias in kms_aliases:
+                if (
+                    alias.get("TargetKeyId", "") == vol_key.split("/")[1]
+                    and str(alias.get("AliasName", ""))
+                    == "alias/aws/elasticfilesystem"
+                ):
+                    locations = [
+                        Location(
+                            arn=(
+                                "arn:aws:ec2:::FileSystemId/"
+                                f"{filesystem['FileSystemId']}"
+                            ),
+                            description=t(
+                                "lib_path.f411.efs_uses_default_kms_key"
+                            ),
+                            values=(),
+                            access_patterns=(),
+                        ),
+                    ]
+
+                    vulns = (
+                        *vulns,
+                        *build_vulnerabilities(
+                            locations=locations,
+                            method=(method),
+                            aws_response=alias,
+                        ),
+                    )
 
     return vulns
 
@@ -76,4 +146,7 @@ async def ebs_uses_default_kms_key(
 CHECKS: Tuple[
     Callable[[AwsCredentials], Coroutine[Any, Any, Tuple[Vulnerability, ...]]],
     ...,
-] = (ebs_uses_default_kms_key,)
+] = (
+    efs_uses_default_kms_key,
+    ebs_uses_default_kms_key,
+)
