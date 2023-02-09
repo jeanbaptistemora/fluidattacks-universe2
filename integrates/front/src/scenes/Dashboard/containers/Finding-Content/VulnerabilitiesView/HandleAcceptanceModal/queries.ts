a701ce5cb25db61593b810416cb5bd92c283c8c4
Andres Aguirre <aaguirre@fluidattacks.com>
2023-02-09 18:49:42 +0000

    integrates\feat(cross): #7782 queries to confirm vulns
    - Add helpers to confirm and reject the vulnerabilities

--- integrates/front/src/scenes/Dashboard/containers/Finding-Content/VulnerabilitiesView/HandleAcceptanceModal/queries.ts
+++ integrates/front/src/scenes/Dashboard/containers/Finding-Content/VulnerabilitiesView/HandleAcceptanceModal/queries.ts
@@ -19,6 +19,20 @@ const HANDLE_VULNS_ACCEPTANCE: DocumentNode = gql`
   }
 `;
 
+const CONFIRM_VULNERABILITIES: DocumentNode = gql`
+  mutation ConfirmVulnerabilities(
+    $findingId: String!
+    $vulnerabilities: [String!]!
+  ) {
+    confirmVulnerabilities(
+      findingId: $findingId
+      vulnerabilities: $vulnerabilities
+    ) {
+      success
+    }
+  }
+`;
+
 const CONFIRM_VULNERABILITIES_ZERO_RISK: DocumentNode = gql`
   mutation ConfirmVulnerabilitiesZeroRisk(
     $findingId: String!
@@ -28,6 +42,24 @@ const CONFIRM_VULNERABILITIES_ZERO_RISK: DocumentNode = gql`
     confirmVulnerabilitiesZeroRisk(
       findingId: $findingId
       justification: $justification
+      vulnerabilities: $vulnerabilities
+    ) {
+      success
+    }
+  }
+`;
+
+const REJECT_VULNERABILITIES: DocumentNode = gql`
+  mutation RejectVulnerabilities(
+    $findingId: String!
+    $reasons: [VulnerabilityRejectionReason!]!
+    $otherReason: String
+    $vulnerabilities: [String!]!
+  ) {
+    rejectVulnerabilities(
+      findingId: $findingId
+      reasons: $reasons
+      otherReason: $otherReason
       vulnerabilities: $vulnerabilities
     ) {
       success
@@ -52,7 +84,9 @@ const REJECT_VULNERABILITIES_ZERO_RISK: DocumentNode = gql`
 `;
 
 export {
+  CONFIRM_VULNERABILITIES,
   CONFIRM_VULNERABILITIES_ZERO_RISK,
   HANDLE_VULNS_ACCEPTANCE,
+  REJECT_VULNERABILITIES,
   REJECT_VULNERABILITIES_ZERO_RISK,
 };
