1583d098fd01ad3a74a5fb5691e8e7d54f860854
Luis Patino <lpatino@fluidattacks.com>
2023-02-09 09:23:06 -0500

    skims\feat(back): #8676 if else evaluators improvement pt2
    - Danger now propagates to else_clause.

--- skims/skims/symbolic_eval/cases/if_statement.py
+++ skims/skims/symbolic_eval/cases/if_statement.py
@@ -14,12 +14,18 @@ FINDING_EVALUATORS: Dict[FindingEnum, Evaluator] = {}
 
 
 def evaluate(args: SymbolicEvalArgs) -> SymbolicEvaluation:
-    child_labels = {"condition_id", "true_id"}
-    for label in child_labels:
-        if n_id := args.graph.nodes[args.n_id].get(label):
-            args.evaluation[args.n_id] = args.generic(
-                args.fork_n_id(n_id)
-            ).danger
+    danger_cond = False
+    danger_true = False
+    danger_false = False
+    node = args.graph.nodes[args.n_id]
+    if _id := node.get("condition_id"):
+        danger_cond = args.generic(args.fork_n_id(_id)).danger
+    if _id := node.get("true_id"):
+        danger_true = args.generic(args.fork_n_id(_id)).danger
+    if (args.n_id not in args.path) and (_id := node.get("false_id")):
+        danger_false = args.generic(args.fork_n_id(_id)).danger
+
+    args.evaluation[args.n_id] = danger_cond or danger_true or danger_false
 
     if finding_evaluator := FINDING_EVALUATORS.get(args.method.value.finding):
         args.evaluation[args.n_id] = finding_evaluator(args).danger
