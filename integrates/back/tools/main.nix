8c732aff5a597cdb774ec184abe1778793cf6723
David Acevedo <dacevedo@fluidattacks.com>
2023-02-10 14:21:22 +0000

    integrates\refac(build): #6063 cleanup
    - remove unused code
    - towards a lighter runtime env
    - no-deltas-test

--- integrates/back/tools/main.nix
+++ integrates/back/tools/main.nix
@@ -7,6 +7,5 @@ makeSearchPaths {
   source = [
     outputs."/integrates/back/tools/asciidoctor-pdf"
     outputs."/integrates/back/tools/concurrent-ruby"
-    outputs."/integrates/back/tools/secure-spreadsheet"
   ];
 }
--- integrates/back/tools/secure-spreadsheet/main.nix
+++
@@ -1,7 +0,0 @@
-{makeNodeJsEnvironment, ...}:
-makeNodeJsEnvironment {
-  name = "integrates-back-tools-secure-spreadsheet";
-  nodeJsVersion = "14";
-  packageJson = ./npm/package.json;
-  packageLockJson = ./npm/package-lock.json;
-}
