477ab8a52e68a4f73b40c82b88dade38b93e6238
Fabio Lagos <flagos@fluidattacks.com>
2023-02-14 10:55:07 -0500

    skims\feat(back): #8627 add python readers
    - add control structure basic readers
    - no-deltas-test

--- skims/skims/syntax_graph/dispatchers/python.py
+++ skims/skims/syntax_graph/dispatchers/python.py
@@ -4,15 +4,22 @@ from syntax_graph.syntax_readers.python import (
     attribute as python_attribute,
     call as python_call,
     class_definition as python_class_definition,
+    except_clause as python_except_clause,
+    execution_block as python_execution_block,
     expression_statement as python_expression_statement,
+    finally_clause as python_finally_clause,
+    for_statement as python_for_statement,
     function_definition as python_function_definition,
     identifier as python_identifier,
+    if_statement as python_if_statement,
     import_statement as python_import_statement,
     list as python_list,
     module as python_module,
     parameters as python_parameters,
     reserved_word as python_reserved_word,
+    try_statement as python_try_statement,
     tuple as python_tuple,
+    while_statement as python_while_statement,
 )
 from syntax_graph.types import (
     Dispatcher,
@@ -52,9 +59,33 @@ PYTHON_DISPATCHERS: Dispatchers = (
     ),
     Dispatcher(
         applicable_types={
+            "except_clause",
+        },
+        syntax_reader=python_except_clause.reader,
+    ),
+    Dispatcher(
+        applicable_types={
+            "block",
+        },
+        syntax_reader=python_execution_block.reader,
+    ),
+    Dispatcher(
+        applicable_types={
             "expression_statement",
         },
         syntax_reader=python_expression_statement.reader,
+    ),
+    Dispatcher(
+        applicable_types={
+            "finally_clause",
+        },
+        syntax_reader=python_finally_clause.reader,
+    ),
+    Dispatcher(
+        applicable_types={
+            "for_statement",
+        },
+        syntax_reader=python_for_statement.reader,
     ),
     Dispatcher(
         applicable_types={
@@ -70,6 +101,13 @@ PYTHON_DISPATCHERS: Dispatchers = (
     ),
     Dispatcher(
         applicable_types={
+            "if_statement",
+        },
+        syntax_reader=python_if_statement.reader,
+    ),
+    Dispatcher(
+        applicable_types={
+            "import_from_statement",
             "import_statement",
         },
         syntax_reader=python_import_statement.reader,
@@ -101,8 +139,20 @@ PYTHON_DISPATCHERS: Dispatchers = (
     ),
     Dispatcher(
         applicable_types={
+            "try_statement",
+        },
+        syntax_reader=python_try_statement.reader,
+    ),
+    Dispatcher(
+        applicable_types={
             "tuple",
         },
         syntax_reader=python_tuple.reader,
+    ),
+    Dispatcher(
+        applicable_types={
+            "while_statement",
+        },
+        syntax_reader=python_while_statement.reader,
     ),
 )
--- skims/skims/syntax_graph/syntax_readers/python/import_statement.py
+++ skims/skims/syntax_graph/syntax_readers/python/import_statement.py
@@ -7,8 +7,8 @@ from syntax_graph.syntax_nodes.import_statement import (
 from syntax_graph.types import (
     SyntaxGraphArgs,
 )
-from typing import (
-    Dict,
+from utils.graph import (
+    match_ast_group_d,
 )
 from utils.graph.text_nodes import (
     node_to_str,
@@ -16,10 +16,24 @@ from utils.graph.text_nodes import (
 
 
 def reader(args: SyntaxGraphArgs) -> NId:
-    import_node = args.ast_graph.nodes[args.n_id]
-    name_id = import_node["label_field_name"]
-    import_text = node_to_str(args.ast_graph, name_id)
-    node_attrs: Dict[str, str] = {
-        "expression": import_text,
-    }
-    return build_import_statement_node(args, node_attrs)
+    graph = args.ast_graph
+
+    name_id = graph.nodes[args.n_id]["label_field_name"]
+    module_name = node_to_str(graph, name_id)
+    nodes: list[dict[str, str]] = [
+        {
+            "expression": module_name,
+            "corrected_n_id": name_id,
+        },
+    ]
+    imported_names = match_ast_group_d(graph, args.n_id, "dotted_name")
+    for _id in imported_names[1:]:
+        nodes.append(
+            {
+                "expression": module_name,
+                "identifier": node_to_str(graph, _id),
+                "corrected_n_id": _id,
+            }
+        )
+
+    return build_import_statement_node(args, *nodes)
