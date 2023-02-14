dcbfeac2797ac8e57a12d99ef5963788064461e8
Lewis Contreras <lcontreras@fluidattacks.com>
2023-02-14 05:10:49 -0500

    skims\feat(back): #8104 add method to model
    - Add conan method to the model

--- skims/skims/model/core_model.py
+++ skims/skims/model/core_model.py
@@ -5452,6 +5452,14 @@ class MethodsEnum(Enum):
         developer=DeveloperEnum.LEWIS_CONTRERAS,
         technique=TechniqueEnum.SCA,
     )
+    CONAN_CONANFILE_TXT_DEV = MethodInfo(
+        file_name="conan",
+        name="conan_conanfile_txt",
+        module="lib_path",
+        finding=FindingEnum.F393,
+        developer=DeveloperEnum.LEWIS_CONTRERAS,
+        technique=TechniqueEnum.SCA,
+    )
     GEM_GEMFILE_DEV = MethodInfo(
         file_name="gem",
         name="gem_gemfile_dev",
