ac702183977d1f47bf8181c9ed5a4b3d045a4ad2
Andres Aguirre <aaguirre@fluidattacks.com>
2023-02-15 11:38:19 -0500

    integrates\feat(back): #7782 mail vuln report
    - Send an email when the vulnerabilities are confirmed

--- integrates/back/src/api/mutations/confirm_vulnerabilities.py
+++ integrates/back/src/api/mutations/confirm_vulnerabilities.py
@@ -1,3 +1,6 @@
+from aioextensions import (
+    schedule,
+)
 from api import (
     APP_EXCEPTIONS,
 )
@@ -7,20 +10,35 @@ from api.mutations import (
 from ariadne.utils import (
     convert_kwargs_to_snake_case,
 )
+from dataloaders import (
+    Dataloaders,
+)
+from decimal import (
+    Decimal,
+)
 from decorators import (
     concurrent_decorators,
     enforce_group_level_auth_async,
     require_login,
     require_report_vulnerabilities,
 )
+from findings import (
+    domain as findings_domain,
+)
 from graphql.type.definition import (
     GraphQLResolveInfo,
+)
+from mailer import (
+    findings as findings_mail,
 )
 from newutils import (
     logs as logs_utils,
 )
 from sessions import (
     domain as sessions_domain,
+)
+from typing import (
+    Any,
 )
 from unreliable_indicators.enums import (
     EntityDependency,
@@ -30,6 +48,9 @@ from unreliable_indicators.operations import (
 )
 from vulnerabilities import (
     domain as vulns_domain,
+)
+from vulnerabilities.domain.core import (
+    get_by_finding_and_vuln_ids,
 )
 
 
@@ -46,10 +67,11 @@ async def mutate(
     vulnerabilities: list[str],
 ) -> SimplePayload:
     try:
+        loaders: Dataloaders = info.context.loaders
         user_data = await sessions_domain.get_jwt_content(info.context)
         stakeholder_email = user_data["user_email"]
         await vulns_domain.confirm_vulnerabilities(
-            loaders=info.context.loaders,
+            loaders=loaders,
             vuln_ids=set(vulnerabilities),
             finding_id=finding_id,
             modified_by=stakeholder_email,
@@ -57,6 +79,37 @@ async def mutate(
         await update_unreliable_indicators_by_deps(
             EntityDependency.confirm_vulnerabilities,
             finding_ids=[finding_id],
+        )
+
+        finding = await findings_domain.get_finding(loaders, finding_id)
+        severity_score: Decimal = findings_domain.get_severity_score(
+            finding.severity
+        )
+        severity_level: str = findings_domain.get_severity_level(
+            severity_score
+        )
+        vulnerabilities_properties: dict[
+            str, Any
+        ] = await findings_domain.vulns_properties(
+            loaders,
+            finding_id,
+            list(
+                await get_by_finding_and_vuln_ids(
+                    loaders, finding_id, set(vulnerabilities)
+                )
+            ),
+        )
+        schedule(
+            findings_mail.send_mail_vulnerability_report(
+                loaders=loaders,
+                group_name=finding.group_name,
+                finding_title=finding.title,
+                finding_id=finding_id,
+                vulnerabilities_properties=vulnerabilities_properties,
+                responsible=finding.hacker_email,
+                severity_score=severity_score,
+                severity_level=severity_level,
+            )
         )
         logs_utils.cloudwatch_log(
             info.context,
