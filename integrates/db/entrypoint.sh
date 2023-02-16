ff934b737d981a84133f3f445ac41bfd67a274ec
Daniel Salazar <dsalazar@fluidattacks.com>
2023-02-16 15:31:52 -0500

    integrates\rever(build): #8675 rm daemonize
    - Remove daemonize function as it will not
    be necessary

--- integrates/db/entrypoint.sh
+++ integrates/db/entrypoint.sh
@@ -1,22 +1,12 @@
 # shellcheck shell=bash
 
-# Make subprocesses orphan when running in CI
-# in order to avoid https://gitlab.com/gitlab-org/gitlab-runner/-/issues/2880
-function daemonize {
-  if test -n "${CI:-}"; then
-    DAEMON=true "${@}" < /dev/null &> /dev/null &
-  else
-    DAEMON=true "${@}" &
-  fi
-}
-
 function main {
   : \
-    && daemonize dynamodb \
-    && daemonize opensearch \
+    && { DAEMON=true dynamodb & } \
+    && { DAEMON=true opensearch & } \
     && wait \
     && if [ "${DAEMON:-}" = "true" ]; then
-      daemonize integrates-streams dev
+      { integrates-streams dev & }
     else
       integrates-streams dev
     fi
