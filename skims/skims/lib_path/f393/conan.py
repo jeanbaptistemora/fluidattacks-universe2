0d2e98455eb58f1def9ec0b324415f12d87548ee
Lewis Contreras <lcontreras@fluidattacks.com>
2023-02-17 07:27:44 -0500

    skims\feat(back): #8104 add method sca
    - Add conan method for sca

--- skims/skims/lib_path/f393/conan.py
+++ skims/skims/lib_path/f393/conan.py
@@ -36,3 +36,21 @@ def conan_conanfile_txt_dev(
             yield format_pkg_dep(
                 pkg_name, pkg_version, line_number, line_number
             )
+
+
+# pylint: disable=unused-argument
+@pkg_deps_to_vulns(Platform.CONAN, MethodsEnum.CONAN_CONANFILE_PY_DEV)
+def conan_conanfile_py_dev(
+    content: str, path: str
+) -> Iterator[DependencyType]:
+    line_deps: bool = False
+    for line_number, line in enumerate(content.splitlines(), 1):
+        if line.startswith("build_requires"):
+            line_deps = True
+        elif line_deps:
+            if not line:
+                break
+            pkg_name, pkg_version = get_dep_info(line)
+            yield format_pkg_dep(
+                pkg_name, pkg_version, line_number, line_number
+            )
