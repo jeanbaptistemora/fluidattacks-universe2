0ec7b79cd4a2b416c4324c63dd9cfa124d73ae3a
Carlos Betancur <cbetancur@fluidattacks.com>
2023-02-21 08:34:40 -0500

    integrates\refac(build): #7323 increase coverage threshold
    - coverage job will fail if back coverage drops under 87.95%

--- integrates/coverage/entrypoint.sh
+++ integrates/coverage/entrypoint.sh
@@ -1,7 +1,7 @@
 # shellcheck shell=bash
 
 function main {
-  local coverage_target=87.85
+  local coverage_target=87.95
   local coverage_args=(
     --omit="back/migrations/*"
     --ignore-errors
