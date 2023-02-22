1b7a8a9b5ab5288dbab250e68f720225b5296fd6
Lewis Contreras <lcontreras@fluidattacks.com>
2023-02-22 13:16:31 -0500

    skims\feat(back): #8104 mod method sca
    - Modify conan deps resolver for sca

--- skims/skims/lib_path/f011/conan.py
+++ skims/skims/lib_path/f011/conan.py
@@ -12,9 +12,11 @@ from model.core_model import (
 )
 import re
 
+CONANFILE_PY_DEP: re.Pattern[str] = re.compile(r'\s+requires\s*=\s*"[^"]*"')
+
 
 def get_dep_info(dep_line: str) -> tuple[str, str]:
-    product, version = dep_line.lstrip().split("@")[0].split("/")
+    product, version = dep_line.strip().split("@")[0].split("/")
     if "[" in version:
         version = re.sub(r"[\[\]]", "", version).split(",")[0]
     return product, version
@@ -41,15 +43,17 @@ def conan_conanfile_txt(content: str, path: str) -> Iterator[DependencyType]:
 def conan_conanfile_py(content: str, path: str) -> Iterator[DependencyType]:
     line_deps: bool = False
     for line_number, line in enumerate(content.splitlines(), 1):
-        if line.startswith("requires"):
+        if CONANFILE_PY_DEP.search(line):
             line_deps = True
         elif line_deps:
             if not line:
                 break
-            pkg_name, pkg_version = line.split("/")
-            yield format_pkg_dep(
-                pkg_name, pkg_version, line_number, line_number
-            )
+            if matched := re.search(r'"(.*)"', line):
+                line = matched.group(1)
+                pkg_name, pkg_version = get_dep_info(line)
+                yield format_pkg_dep(
+                    pkg_name, pkg_version, line_number, line_number
+                )
 
 
 # pylint: disable=unused-argument
