16c0c77ef536c3b64f7ea3f60172585d51295b95
Jonathan Mesa <jmesa@fluidattacks.com>
2023-02-23 15:16:55 +0000

    integrates\fix(back): #7494 streams toe lines keyerror
    - fix keyerror as some items now have some attr nested in the
    state map

--- integrates/streams/src/dynamodb/processors/redshift/toe_lines/utils.py
+++ integrates/streams/src/dynamodb/processors/redshift/toe_lines/utils.py
@@ -10,26 +10,28 @@ import hashlib
 def format_row_metadata(
     item: Item,
 ) -> Item:
+    state_item: Item = item["state"]
+
     return dict(
         id=hashlib.sha256(item["sk"].encode("utf-8")).hexdigest(),
-        attacked_at=datetime.fromisoformat(item["attacked_at"])
-        if item.get("attacked_at")
+        attacked_at=datetime.fromisoformat(state_item["attacked_at"])
+        if state_item.get("attacked_at")
         else None,
-        attacked_by=item["attacked_by"],
-        attacked_lines=int(item["attacked_lines"]),
-        be_present=bool(item["be_present"]),
-        be_present_until=datetime.fromisoformat(item["be_present_until"])
-        if item.get("be_present_until")
+        attacked_by=state_item["attacked_by"],
+        attacked_lines=int(state_item["attacked_lines"]),
+        be_present=bool(state_item["be_present"]),
+        be_present_until=datetime.fromisoformat(state_item["be_present_until"])
+        if state_item.get("be_present_until")
         else None,
-        first_attack_at=datetime.fromisoformat(item["first_attack_at"])
-        if item.get("first_attack_at")
+        first_attack_at=datetime.fromisoformat(state_item["first_attack_at"])
+        if state_item.get("first_attack_at")
         else None,
         group_name=item["group_name"],
-        has_vulnerabilities=bool(item.get("has_vulnerabilities", False)),
-        loc=item["loc"],
+        has_vulnerabilities=bool(state_item.get("has_vulnerabilities", False)),
+        loc=int(state_item["loc"]),
         modified_date=datetime.fromisoformat(item["modified_date"]),
         root_id=item["root_id"],
-        seen_at=datetime.fromisoformat(item["seen_at"]),
+        seen_at=datetime.fromisoformat(state_item["seen_at"]),
         seen_first_time_by=item.get("seen_first_time_by"),
-        sorts_risk_level=int(item["sorts_risk_level"]),
+        sorts_risk_level=int(state_item["sorts_risk_level"]),
     )
