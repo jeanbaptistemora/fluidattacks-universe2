81d1a87a67b1df99cd7f6544f1885502a85d24c0
Julian Gomez <ugomez@fluidattacks.com>
2023-02-27 09:37:48 -0500

    skims\feat(back): #8476 add utilities to cfn
    - add utilities to cfn in lib root module

---
+++ skims/skims/lib_root/utilities/cloudformation.py
@@ -0,0 +1,56 @@
+from collections.abc import (
+    Iterator,
+)
+from model.graph_model import (
+    Graph,
+    NId,
+)
+from utils import (
+    graph as g,
+)
+from utils.graph import (
+    adj_ast,
+)
+
+
+def get_key_value(graph: Graph, nid: NId) -> tuple[str, str]:
+    key_id = graph.nodes[nid]["key_id"]
+    key = graph.nodes[key_id]["value"]
+    value_id = graph.nodes[nid]["value_id"]
+    value = ""
+    if graph.nodes[value_id]["label_type"] == "ArrayInitializer":
+        child_id = adj_ast(graph, value_id, label_type="Literal")
+        if len(child_id) > 0:
+            value = graph.nodes[child_id[0]].get("value", "")
+    else:
+        value = graph.nodes[value_id].get("value", "")
+    return key, value
+
+
+def get_attribute(
+    graph: Graph, object_id: NId, expected_attr: str
+) -> tuple[str | None, str, NId]:
+    if object_id != "":
+        for attr_id in adj_ast(graph, object_id, label_type="Pair"):
+            key, value = get_key_value(graph, attr_id)
+            if key == expected_attr:
+                return key, value, attr_id
+    return None, "", ""
+
+
+def iterate_resource(graph: Graph, expected_type: str) -> Iterator[NId]:
+    for nid in g.matching_nodes(graph, label_type="Object"):
+        resource_type_key, resource_type_val, _ = get_attribute(
+            graph, nid, "Type"
+        )
+        if resource_type_key and resource_type_val == expected_type:
+            yield nid
+
+
+def list_has_string(graph: Graph, nid: NId, value: str) -> bool:
+    child_ids = adj_ast(graph, nid)
+    for c_id in child_ids:
+        curr_value = graph.nodes[c_id].get("value")
+        if curr_value and curr_value == value:
+            return True
+    return False
