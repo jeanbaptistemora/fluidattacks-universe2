9a12b66a32a35643adcc3a8db8749b29737bde28
Fabio Lagos <flagos@fluidattacks.com>
2023-02-27 08:20:23 -0500

    skims\fix(back): #4846 fp in f353
    - solve non deterministic reports in f353 js ts method
    - avoid report if token verify could be performed in another file
    - add test case and adjust csv results

--- skims/test/data/lib_root/f353/js_decode_insecure_jwt_token.js
+++ skims/test/data/lib_root/f353/js_decode_insecure_jwt_token.js
@@ -1,3 +1,4 @@
+import {verifyToken} from "personal_module.js";
 const jwt = require('jsonwebtoken');
 
 function unsafeVerifyJwt() {
@@ -11,5 +12,13 @@ function safeVerifyJwt() {
     let allowed_algos = ['PS384'];
     const verify_config = { expiresIn: 10000, algorithms:  allowed_algos};
     jwt.verify(token_secure, key, verify_config);
+    jwt.decode(token_secure, key, allowed_algos);
+}
+
+function nonDeterministicJwt() {
+    // The jwt may be verified correctly from a function in another file, this vuln is not deterministic
+    if (!verifyToken(token_secure)){
+        return null;
+    };
     jwt.decode(token_secure, key, allowed_algos);
 }
