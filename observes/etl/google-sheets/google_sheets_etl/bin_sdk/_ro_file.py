8802261c359c33d8aafa6d65a26b74cfacc4bec0
Daniel Murcia <dmurcia@fluidattacks.com>
2023-02-28 15:49:22 -0500

    observes\feat(back): #8780 temp file
    - define temp file
    - adjust read only temp file definition

--- observes/etl/google-sheets/google_sheets_etl/bin_sdk/_ro_file.py
+++ observes/etl/google-sheets/google_sheets_etl/bin_sdk/_ro_file.py
@@ -10,114 +10,86 @@ from fa_purity import (
     PureIter,
     Stream,
 )
-from fa_purity.cmd import (
-    unsafe_unwrap,
-)
-from fa_purity.cmd.core import (
-    CmdUnwrapper,
-    new_cmd,
-)
 from fa_purity.pure_iter.factory import (
     unsafe_from_cmd,
 )
 from fa_purity.stream.factory import (
     unsafe_from_cmd as unsafe_build_stream,
 )
-import logging
+from pathlib import (
+    Path,
+)
 from tempfile import (
     NamedTemporaryFile,
 )
 from typing import (
-    Callable,
-    IO,
     Iterable,
-    TypeVar,
 )
 
-LOG = logging.getLogger(__name__)
+
+@dataclass(frozen=True)
+class TempFile:
+    path: Path
+
+    @staticmethod
+    def new() -> Cmd[TempFile]:
+        def _action() -> TempFile:
+            file_object = NamedTemporaryFile("w", delete=False)
+            file_object.close()
+            return TempFile(Path(file_object.name))
+
+        return Cmd.from_cmd(_action)
+
+    def read_lines(self) -> Stream[str]:
+        def _new_iter() -> Iterable[str]:
+            with open(self.path, "r", encoding="UTF-8") as file:
+
+                line = file.readline()
+                while line:
+                    yield line
+                    line = file.readline()
+
+        return unsafe_build_stream(Cmd.from_cmd(_new_iter))
+
+    def write_lines(self, lines: PureIter[str]) -> Cmd[None]:
+        def _new_iter() -> None:
+            with open(self.path, "w", encoding="UTF-8") as file:
+                file.writelines(lines)
+
+        return Cmd.from_cmd(_new_iter)
 
 
 @dataclass(frozen=True)
 class _TempReadOnlyFile:
-    file_path: str
-
-
-_T = TypeVar("_T")
+    file: TempFile
 
 
 @dataclass(frozen=True)
 class TempReadOnlyFile:
+    """
+    Wraps a `TempFile` ensuring read only access.
+    File path is private and should not be exposed.
+    """
+
     _inner: _TempReadOnlyFile
 
-    def over_binary(self, transform: Callable[[IO[bytes]], _T]) -> _T:
-        def _action() -> _T:
-            with open(self._inner.file_path, "rb") as file:
-                return transform(file)
+    def read_lines(self) -> PureIter[str]:
+        # safe since the file is supposed to be read only
+        return unsafe_from_cmd(self._inner.file.read_lines().unsafe_to_iter())
 
-        # even if opening a file is a Cmd
-        # unsafe_unwrap is safe to use since
-        # the file is supposed to be immutable
-        # and therefore return the same output
-        return unsafe_unwrap(Cmd.from_cmd(_action))
-
-    def read(self) -> PureIter[str]:
-        def _new_iter() -> Iterable[str]:
-            with open(self._inner.file_path, "r") as file:
-                line = file.readline()
-                while line:
-                    yield line
-                    line = file.readline()
-
-        return unsafe_from_cmd(Cmd.from_cmd(_new_iter))
+    def extract(self) -> Cmd[TempFile]:
+        """
+        Useful when file path is required a new `TempFile` object
+        will provide it without exposing the inner path
+        """
+        return TempFile.new().bind(
+            lambda f: f.write_lines(self.read_lines()).map(lambda _: f)
+        )
 
     @staticmethod
     def new(content: PureIter[str]) -> Cmd[TempReadOnlyFile]:
-        def _action() -> TempReadOnlyFile:
-            file_object = NamedTemporaryFile("w", delete=False)
-            file_object.writelines(content)
-            file_object.close()
-            return TempReadOnlyFile(_TempReadOnlyFile(file_object.name))
-
-        return Cmd.from_cmd(_action)
-
-    @staticmethod
-    def from_cmd(
-        write_cmd: Callable[[IO[str]], Cmd[None]]
-    ) -> Cmd[TempReadOnlyFile]:
-        """
-        `write_cmd` initializes the file content. It has write-only access to file
-        """
-
-        def _action(act: CmdUnwrapper) -> TempReadOnlyFile:
-            file_object = NamedTemporaryFile("w", delete=False)
-            act.unwrap(write_cmd(file_object))
-            file_object.close()
-            return TempReadOnlyFile(_TempReadOnlyFile(file_object.name))
-
-        return new_cmd(_action)
-
-    @staticmethod
-    def save(content: Stream[str]) -> Cmd[TempReadOnlyFile]:
-        def _action(act: CmdUnwrapper) -> TempReadOnlyFile:
-            file_object = NamedTemporaryFile("w", delete=False)
-            LOG.debug("Saving stream into %s", file_object.name)
-            file_object.writelines(act.unwrap(content.unsafe_to_iter()))
-            file_object.close()
-            return TempReadOnlyFile(_TempReadOnlyFile(file_object.name))
-
-        return new_cmd(_action)
-
-    @classmethod
-    def freeze(cls, file_path: str) -> Cmd[TempReadOnlyFile]:
-        def _action() -> Iterable[str]:
-            with open(file_path, "r") as file:
-                file.seek(0)
-                line = file.readline()
-                while line:
-                    yield line
-                    line = file.readline()
-
-        start = Cmd.from_cmd(lambda: LOG.debug("Freezing file"))
-        end = Cmd.from_cmd(lambda: LOG.debug("Freezing completed!"))
-        stream = unsafe_build_stream(Cmd.from_cmd(_action))
-        return start + cls.save(stream).bind(lambda f: end.map(lambda _: f))
+        return TempFile.new().bind(
+            lambda f: f.write_lines(content).map(
+                lambda _: TempReadOnlyFile(_TempReadOnlyFile(f))
+            )
+        )
