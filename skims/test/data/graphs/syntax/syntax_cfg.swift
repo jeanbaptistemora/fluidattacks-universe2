2c01166a4b43b0ff6c459a69c20bd574697d7d0e
Jhon Romero <jromero@fluidattacks.com>
2023-03-01 10:11:07 -0500

    skims\feat(back): #8805 swift stx graph readers
    - add test cases to the graph generation
    
    - no-deltas-test

--- skims/test/data/graphs/syntax/syntax_cfg.swift
+++ skims/test/data/graphs/syntax/syntax_cfg.swift
@@ -25,3 +25,23 @@ let algorithm = .aes // Compliant
 
 
 let aes = try AES(key: key, iv: iv) // Compliant
+
+
+let cryptor = try Cryptor(operation: .encrypt, algorithm: .des, options: .none, key: key, iv: []) // Noncompliant
+
+let crypt = CkoCrypt2()
+crypt.CryptAlgorithm = "3des" // Noncompliant
+
+
+
+let cryptor = try Cryptor(operation: .encrypt, algorithm: .aes, options: .none, key: key, iv: []) // Compliant
+
+let crypt = CkoCrypt2()
+crypt.CryptAlgorithm = "aes" // Compliant
+
+
+
+
+let digest = input.md5() // Noncompliant
+
+let digest = input.sha256() // Compliant
