394f7814e0b0cf1220689024904773e9f0cb282e
Daniel Salazar <dsalazar@fluidattacks.com>
2023-03-01 15:47:58 -0500

    common\feat(build): #8905 meta tests
    - Add basic meta tests
    - Add maintainers test
    - Active maintainers test still pending
    - lastReviewed test still pending
    - Schema validation with ajv still pending
    - Other minor changes

--- common/compute/schedule/test/src/__init__.py
+++ common/compute/schedule/test/src/__init__.py
@@ -6,40 +6,82 @@ from typing import (
 )
 
 
-def test_meta_keys(*, name: str, values: Any) -> bool:
-    success: bool = False
-    keys: list[str] = [
-        "description",
-        "requiredBy",
-        "responsible",
-        "lastReviewed",
-    ]
+def error(msg: str) -> None:
+    print("[ERROR]", msg)
 
-    if "meta" not in values.keys():
-        print(f"[ERROR] 'meta' option does not exist in schedule '{name}'.")
-    else:
-        success = True
 
-    if success:
-        for key in keys:
-            if key not in values["meta"].keys():
-                print(
-                    f"[ERROR] 'meta.{key}' option"
-                    f" does not exist in schedule '{name}'."
-                )
-                success = False
+def test_schedule_maintainers(*, name: str, values: Any) -> bool:
+    success: bool = True
+
+    if len(values["meta"]["maintainers"]) == 0:
+        success = False
+        error(
+            "'meta.maintainers' option"
+            " must not be empty"
+            f" in schedule '{name}'."
+        )
+
+    return success
+
+
+def test_schedule(*, name: str, values: Any) -> bool:
+    return all(
+        tuple(
+            [
+                test_meta_description(name=name, values=values),
+                test_meta_required_by(name=name, values=values),
+            ]
+        )
+    )
+
+
+def test_meta_description(*, name: str, values: Any) -> bool:
+    success: bool = True
+    min_chars: int = 50
+    description: str = values["meta"]["description"]
+
+    if len(description) < min_chars:
+        success = False
+        error(
+            "'meta.description' option"
+            f" must be at least {min_chars} characters long"
+            f" in schedule '{name}'."
+        )
+
+    return success
+
+
+def test_meta_required_by(*, name: str, values: Any) -> bool:
+    success: bool = True
+    required_by: list[str] = values["meta"]["requiredBy"]
+
+    if len(required_by) == 0:
+        success = False
+        error(
+            "'meta.requiredBy' option"
+            f"must must not be empty in schedule '{name}'."
+        )
 
     return success
 
 
 def main() -> None:
-    success: bool = False
+    success: bool = True
     schedules: dict[str, Any] = json.loads(os.environ["SCHEDULES"])
+    user: str = os.environ["CI_COMMIT_REF_NAME"]
 
-    success = all(
+    success = success and all(
         tuple(
-            test_meta_keys(name=name, values=values)
+            test_schedule_maintainers(name=name, values=values)
             for (name, values) in schedules.items()
+        )
+    )
+
+    success = success and all(
+        tuple(
+            test_schedule(name=name, values=values)
+            for (name, values) in schedules.items()
+            if user in values["meta"]["maintainers"]
         )
     )
 
