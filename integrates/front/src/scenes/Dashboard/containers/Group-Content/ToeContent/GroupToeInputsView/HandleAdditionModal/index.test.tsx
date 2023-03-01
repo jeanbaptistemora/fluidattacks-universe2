2e15bd05e0e45fd2e1023a3048d6294d983386be
Juan Villegas <jvillegas@fluidattacks.com>
2023-03-01 13:31:16 -0500

    integrates\test(front): #7758 input modal
    - test to component

---
+++ integrates/front/src/scenes/Dashboard/containers/Group-Content/ToeContent/GroupToeInputsView/HandleAdditionModal/index.test.tsx
@@ -0,0 +1,8 @@
+import { HandleAdditionModal } from ".";
+
+describe("Handle addition modal", (): void => {
+  it("should return a function", (): void => {
+    expect.hasAssertions();
+    expect(typeof HandleAdditionModal).toBe("function");
+  });
+});
