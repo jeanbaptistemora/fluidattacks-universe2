af5ddb70affc39893f36edc44094996b770fc698
Juan Villegas <jvillegas@fluidattacks.com>
2023-03-02 10:33:08 -0500

    integrates\test(front): #7758 input modal
    - render component test

--- integrates/front/src/scenes/Dashboard/containers/Group-Content/ToeContent/GroupToeInputsView/HandleAdditionModal/index.test.tsx
+++ integrates/front/src/scenes/Dashboard/containers/Group-Content/ToeContent/GroupToeInputsView/HandleAdditionModal/index.test.tsx
@@ -1,8 +1,70 @@
+import type { MockedResponse } from "@apollo/client/testing";
+import { MockedProvider } from "@apollo/client/testing";
+import { render, screen, waitFor } from "@testing-library/react";
+import React from "react";
+
+import { GET_ROOTS } from "./queries";
+
 import { HandleAdditionModal } from ".";
 
 describe("Handle addition modal", (): void => {
+  const refetchDataFn: jest.Mock = jest.fn();
+  const handleCloseModal: jest.Mock = jest.fn();
+  const queryMock: MockedResponse = {
+    request: {
+      query: GET_ROOTS,
+      variables: { groupName: "groupname" },
+    },
+    result: {
+      data: {
+        group: {
+          __typename: "Group",
+          name: "test",
+          roots: [
+            {
+              __typename: "GitRoot",
+              gitEnvironmentUrls: [
+                {
+                  __typename: "GitEnvironmentUrl",
+                  id: "00fbe3579a253b43239954a545dc0536e6c83094",
+                  url: "https://app.fluidattacks.com/test",
+                  urlType: "URL",
+                },
+              ],
+              id: "4039d098-ffc5-4984-8ed3-eb17bca98e19",
+              nickname: "universe",
+              state: "ACTIVE",
+            },
+          ],
+        },
+      },
+    },
+  };
+
   it("should return a function", (): void => {
     expect.hasAssertions();
     expect(typeof HandleAdditionModal).toBe("function");
+  });
+
+  it("should render modal", async (): Promise<void> => {
+    expect.hasAssertions();
+
+    jest.clearAllMocks();
+
+    render(
+      <MockedProvider addTypename={false} mocks={[queryMock]}>
+        <HandleAdditionModal
+          groupName={"groupname"}
+          handleCloseModal={handleCloseModal}
+          refetchData={refetchDataFn}
+        />
+      </MockedProvider>
+    );
+
+    await waitFor((): void => {
+      expect(
+        screen.getByText("group.toe.inputs.addModal.title")
+      ).toBeInTheDocument();
+    });
   });
 });
