b76fed0dc90a94fd277ace062ded5ebf12b3c352
Luis Patino <lpatino@fluidattacks.com>
2023-03-03 14:53:42 -0500

    skims\feat(back): #8898 f153 java header accept any mime type
    - Activates method.
    - Updates .csv output.
    - TODO: Check if detection logic for .headers() usage of
    .newBuilder() could be included in this method.

--- skims/skims/lib_root/f153/__init__.py
+++ skims/skims/lib_root/f153/__init__.py
@@ -1,4 +1,5 @@
 from lib_root.f153.java import (
+    java_accepts_any_mime_type_chain,
     java_http_accepts_any_mime_type,
 )
 from model import (
@@ -7,4 +8,7 @@ from model import (
 )
 
 FINDING: core_model.FindingEnum = core_model.FindingEnum.F153
-QUERIES: graph_model.Queries = ((FINDING, java_http_accepts_any_mime_type),)
+QUERIES: graph_model.Queries = (
+    (FINDING, java_accepts_any_mime_type_chain),
+    (FINDING, java_http_accepts_any_mime_type),
+)
