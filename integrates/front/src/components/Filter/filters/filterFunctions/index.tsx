553446d4edec6972b35ab032e69ca69ac60e7941
Juan Villegas <jvillegas@fluidattacks.com>
2023-03-06 12:09:26 -0500

    integrates\refac(front): #8368 filters
    - handler to fn component

--- integrates/front/src/components/Filter/filters/filterFunctions/index.tsx
+++ integrates/front/src/components/Filter/filters/filterFunctions/index.tsx
@@ -1,4 +1,7 @@
+import type { Dispatch, SetStateAction } from "react";
+
 import type {
+  IFilter,
   IPermanentData,
   IPermanentValuesProps,
 } from "components/Filter/types";
@@ -24,4 +27,42 @@ const setPermanentValues = ({
   );
 };
 
-export { setPermanentValues };
+const rangeValueChangeHandler = <IData extends object>(
+  id: string,
+  filters: IFilter<IData>[],
+  position: 0 | 1,
+  setFilters: Dispatch<SetStateAction<IFilter<IData>[]>>,
+  permaValues?: IPermanentData[],
+  setPermaValues?: Dispatch<SetStateAction<IPermanentData[]>>
+): ((event: React.ChangeEvent<HTMLInputElement>) => void) => {
+  return (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setFilters(
+      filters.map((filter): IFilter<IData> => {
+        const value: [string, string] =
+          position === 0
+            ? [event.target.value, filter.rangeValues?.[1] ?? ""]
+            : [filter.rangeValues?.[0] ?? "", event.target.value];
+
+        if (filter.id === id) {
+          const permaValue: IPermanentData = {
+            checkValues: filter.checkValues,
+            id,
+            rangeValues: value,
+            value: filter.value,
+          };
+          if (permaValues)
+            setPermanentValues({ permaValue, permaValues, setPermaValues });
+
+          return {
+            ...filter,
+            rangeValues: value,
+          };
+        }
+
+        return filter;
+      })
+    );
+  };
+};
+
+export { rangeValueChangeHandler, setPermanentValues };
