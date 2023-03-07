b765da1afed9745bdf8249a927c019f02de939e1
Nicolas Carvajal <ncarvajal@fluidattacks.com>
2023-03-07 12:01:56 -0500

    integrates\refac(front): #6686 migrate editable field at assigned vuln
    - migrate editable field at assigned vuln

--- integrates/front/src/scenes/Dashboard/components/Vulnerabilities/UpdateDescription/AssignedField/index.tsx
+++ integrates/front/src/scenes/Dashboard/components/Vulnerabilities/UpdateDescription/AssignedField/index.tsx
@@ -5,8 +5,8 @@ import React from "react";
 
 import type { IAssignedFieldProps } from "./types";
 
+import { Editable, Select } from "components/Input";
 import { authzPermissionsContext } from "utils/authz/config";
-import { EditableField, FormikDropdown } from "utils/forms/fields";
 import { translate } from "utils/translations/translate";
 
 const AssignedField: React.FC<IAssignedFieldProps> = ({
@@ -26,22 +26,24 @@ const AssignedField: React.FC<IAssignedFieldProps> = ({
       {isInProgressSelected ||
       isAcceptedSelected ||
       isAcceptedUndefinedSelected ? (
-        <EditableField
-          component={FormikDropdown}
+        <Editable
           currentValue={_.get(lastTreatment, "assigned", "")}
+          isEditing={canUpdateVulnsTreatment}
           label={translate.t("searchFindings.tabDescription.assigned")}
-          name={"assigned"}
-          renderAsEditable={canUpdateVulnsTreatment}
-          type={"text"}
         >
-          {userEmails.map(
-            (email: string): JSX.Element => (
-              <option key={email} value={email}>
-                {email}
-              </option>
-            )
-          )}
-        </EditableField>
+          <Select
+            label={translate.t("searchFindings.tabDescription.assigned")}
+            name={"assigned"}
+          >
+            {userEmails.map(
+              (email: string): JSX.Element => (
+                <option key={email} value={email}>
+                  {email}
+                </option>
+              )
+            )}
+          </Select>
+        </Editable>
       ) : undefined}
     </React.StrictMode>
   );
