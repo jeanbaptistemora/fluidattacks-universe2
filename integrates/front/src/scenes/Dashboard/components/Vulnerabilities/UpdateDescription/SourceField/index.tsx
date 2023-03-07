99e75f64f75543632e4728c05b1b5b647ef6b7a5
Nicolas Carvajal <ncarvajal@fluidattacks.com>
2023-03-07 13:37:55 -0500

    integrates\refac(front): #6686 migrate editable field at update desc
    - migrate editable field at update desc

--- integrates/front/src/scenes/Dashboard/components/Vulnerabilities/UpdateDescription/SourceField/index.tsx
+++ integrates/front/src/scenes/Dashboard/components/Vulnerabilities/UpdateDescription/SourceField/index.tsx
@@ -6,8 +6,8 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 
 import type { IUpdateVulnerabilityForm } from "../../types";
+import { Editable, Select } from "components/Input";
 import { authzPermissionsContext } from "utils/authz/config";
-import { EditableField, FormikDropdown } from "utils/forms/fields";
 import { required } from "utils/validations";
 
 const SourceField: React.FC = (): JSX.Element => {
@@ -22,34 +22,36 @@ const SourceField: React.FC = (): JSX.Element => {
   const { initialValues } = useFormikContext<IUpdateVulnerabilityForm>();
 
   return canSeeSource && canUpdateVulnerabilityDescription ? (
-    <EditableField
-      component={FormikDropdown}
+    <Editable
       currentValue={""}
+      isEditing={true}
       label={t("searchFindings.tabVuln.vulnTable.source")}
-      name={"source"}
-      renderAsEditable={true}
-      type={"text"}
-      validate={_.isEmpty(initialValues.source) ? undefined : required}
     >
-      <option value={""} />
-      <option value={"ANALYST"}>
-        {t(`searchFindings.tabVuln.vulnTable.vulnerabilitySource.ANALYST`)}
-      </option>
-      <option value={"CUSTOMER"}>
-        {t(`searchFindings.tabVuln.vulnTable.vulnerabilitySource.CUSTOMER`)}
-      </option>
-      <option value={"DETERMINISTIC"}>
-        {t(
-          `searchFindings.tabVuln.vulnTable.vulnerabilitySource.DETERMINISTIC`
-        )}
-      </option>
-      <option value={"ESCAPE"}>
-        {t(`searchFindings.tabVuln.vulnTable.vulnerabilitySource.ESCAPE`)}
-      </option>
-      <option value={"MACHINE"}>
-        {t(`searchFindings.tabVuln.vulnTable.vulnerabilitySource.MACHINE`)}
-      </option>
-    </EditableField>
+      <Select
+        label={t("searchFindings.tabVuln.vulnTable.source")}
+        name={"source"}
+        validate={_.isEmpty(initialValues.source) ? undefined : required}
+      >
+        <option value={""} />
+        <option value={"ANALYST"}>
+          {t(`searchFindings.tabVuln.vulnTable.vulnerabilitySource.ANALYST`)}
+        </option>
+        <option value={"CUSTOMER"}>
+          {t(`searchFindings.tabVuln.vulnTable.vulnerabilitySource.CUSTOMER`)}
+        </option>
+        <option value={"DETERMINISTIC"}>
+          {t(
+            `searchFindings.tabVuln.vulnTable.vulnerabilitySource.DETERMINISTIC`
+          )}
+        </option>
+        <option value={"ESCAPE"}>
+          {t(`searchFindings.tabVuln.vulnTable.vulnerabilitySource.ESCAPE`)}
+        </option>
+        <option value={"MACHINE"}>
+          {t(`searchFindings.tabVuln.vulnTable.vulnerabilitySource.MACHINE`)}
+        </option>
+      </Select>
+    </Editable>
   ) : (
     <div />
   );
