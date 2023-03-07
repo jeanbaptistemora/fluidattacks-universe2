1e6494870a79c34417b824eaef8d01b8310d1f78
Carlos Betancur <cbetancur@fluidattacks.com>
2023-03-07 21:38:18 +0000

    integrates\test(back): #4944 remove duplicated test
    - remove tests that are already covered in findings/domain

--- integrates/back/test/unit/src/domain/test_group.py
+++ integrates/back/test/unit/src/domain/test_group.py
@@ -5,9 +5,6 @@ from dataloaders import (
 from db_model.events.types import (
     GroupEventsRequest,
 )
-from findings.domain import (
-    get_pending_verification_findings,
-)
 import pytest
 
 pytestmark = [
@@ -30,13 +27,3 @@ async def test_list_events() -> None:
         GroupEventsRequest(group_name=group_name)
     )
     assert expected_output == sorted([event.id for event in events_group])
-
-
-async def test_get_pending_verification_findings() -> None:
-    group_name = "unittesting"
-    loaders = get_new_context()
-    findings = await get_pending_verification_findings(loaders, group_name)
-    assert len(findings) >= 1
-    assert findings[0].title == "038. Business information leak"
-    assert findings[0].id == "436992569"
-    assert findings[0].group_name == "unittesting"
