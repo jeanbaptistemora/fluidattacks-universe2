a4e7bead3649a00e841e15a42ecafb33358da1a4
Lewis Contreras <lcontreras@fluidattacks.com>
2023-03-09 13:01:51 -0500

    skims\feat(back): #8971 add scheduler test
    - Add test to the format_range function

--- skims/test/test_sca_scheduler.py
+++ skims/test/test_sca_scheduler.py
@@ -1,5 +1,6 @@
 import pytest
 from schedulers.update_sca_table.repositories.advisories_community import (
+    format_range,
     RE_RANGES,
 )
 
@@ -17,3 +18,17 @@ from schedulers.update_sca_table.repositories.advisories_community import (
 def test_re_ranges_pattern(ranges: str, expected: list[str]) -> None:
     match_ranges: list[str] = RE_RANGES.findall(ranges)
     assert match_ranges == expected
+
+
+@pytest.mark.skims_test_group("unittesting")
+@pytest.mark.parametrize(
+    "range_str,expected",
+    [
+        ("[5.0.0,6.0.0)", ">=5.0.0 <6.0.0"),
+        ("(,0.3.13]", ">=0 <=0.3.13"),
+        ("[1.1.4]", "=1.1.4"),
+    ],
+)
+def test_advs_format_range(range_str: str, expected: str) -> None:
+    formated_range: str = format_range(range_str)
+    assert formated_range == expected
