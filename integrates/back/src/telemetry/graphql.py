bdb3ee8241538db6d3f37dc98af1df2a017473d2
David Acevedo <dacevedo@fluidattacks.com>
2023-03-10 10:26:17 -0500

    integrates\feat(back): #8854 otel instrumentation
    - graphql instrumentor now traces resolvers
    - properly measuring time in async execution
    - now this is by far the most complete tracer for graphql python
    - out there

--- integrates/back/src/telemetry/graphql.py
+++ integrates/back/src/telemetry/graphql.py
@@ -1,16 +1,34 @@
 import graphql
 from graphql import (
+    default_field_resolver,
     DocumentNode,
+    ExecutionContext,
+    FieldNode,
     get_operation_ast,
     GraphQLError,
+    GraphQLField,
+    GraphQLFieldResolver,
+    GraphQLObjectType,
+    OperationDefinitionNode,
     Source,
+)
+from graphql.execution.execute import (
+    get_field_def,
 )
 from graphql.language.parser import (
     SourceType,
 )
-from inspect import (
-    isawaitable,
-)
+
+try:
+    # Faster, but only available from 3.1.0 onwards
+    from graphql.pyutils import (
+        is_awaitable,
+    )
+except ImportError:
+    from inspect import (  # type: ignore
+        isawaitable as is_awaitable,
+    )
+
 from opentelemetry.instrumentation.instrumentor import (  # type: ignore
     BaseInstrumentor,
 )
@@ -25,6 +43,7 @@ import re
 from typing import (
     Any,
     Callable,
+    cast,
     Collection,
     List,
     Optional,
@@ -65,11 +84,17 @@ class GraphQLCoreInstrumentor(BaseInstrumentor):
             "execute",
             self._patched_execute,
         )
+        wrap_function_wrapper(
+            graphql,
+            "ExecutionContext.execute_field",
+            self._patched_execute_field,
+        )
 
     def _uninstrument(self, **kwargs: Any) -> None:
         unwrap(graphql, "parse")
         unwrap(graphql.validation, "validate")
         unwrap(graphql, "execute")
+        unwrap(graphql, "ExecutionContext.execute_field")
 
     def _patched_parse(
         self,
@@ -109,10 +134,9 @@ class GraphQLCoreInstrumentor(BaseInstrumentor):
         with self._tracer.start_as_current_span("graphql.execute") as span:
             document_arg: DocumentNode = args[1]
             _set_operation_attrs(span, document_arg)
-
             result = original_func(*args, **kwargs)
 
-            if isawaitable(result):
+            if is_awaitable(result):
 
                 async def await_result() -> Any:
                     with self._tracer.start_as_current_span(
@@ -125,6 +149,37 @@ class GraphQLCoreInstrumentor(BaseInstrumentor):
 
                 return await_result()
             _set_errors(span, result.errors)
+            return result
+
+    def _patched_execute_field(
+        self,
+        original_func: Callable[..., Any],
+        instance: ExecutionContext,
+        args: tuple[Any, ...],
+        kwargs: dict[str, Any],
+    ) -> Any:
+        parent_type_arg: GraphQLObjectType = args[0]
+        field_nodes_arg: List[FieldNode] = args[2]
+        field_node = field_nodes_arg[0]
+        field = get_field_def(instance.schema, parent_type_arg, field_node)
+
+        if _should_exclude_field(field, instance.operation):
+            return original_func(*args, **kwargs)
+
+        with self._tracer.start_as_current_span("graphql.resolve") as span:
+            _set_field_attrs(span, field_node)
+            result = original_func(*args, **kwargs)
+
+            if is_awaitable(result):
+
+                async def await_result() -> Any:
+                    with self._tracer.start_as_current_span(
+                        "graphql.resolve.await"
+                    ) as span:
+                        _set_field_attrs(span, field_node)
+                        return await result
+
+                return await_result()
             return result
 
 
@@ -170,3 +225,37 @@ def _set_errors(span: Span, errors: Optional[List[GraphQLError]]) -> None:
     if errors:
         for error in errors:
             span.record_exception(error)
+
+
+def _set_field_attrs(span: Span, field_node: FieldNode) -> None:
+    span.set_attribute("graphql.field.name", field_node.name.value)
+
+
+def _is_default_resolver(resolver: Optional[GraphQLFieldResolver]) -> bool:
+    # pylint: disable=comparison-with-callable
+    return (
+        # graphql-core
+        resolver is None
+        or resolver == default_field_resolver
+        # ariadne
+        or getattr(resolver, "_ariadne_alias_resolver", False)
+        # strawberry
+        or getattr(resolver, "_is_default", False)
+    )
+
+
+def _is_introspection_query(operation: OperationDefinitionNode) -> bool:
+    selections = operation.selection_set.selections
+
+    if selections:
+        root_field = cast(FieldNode, selections[0])
+        return root_field.name.value == "__schema"
+    return False
+
+
+def _should_exclude_field(
+    field: GraphQLField, operation: OperationDefinitionNode
+) -> bool:
+    return _is_default_resolver(field.resolve) or _is_introspection_query(
+        operation
+    )
