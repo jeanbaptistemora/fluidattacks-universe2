9893849bd623704cc9a255464eca7561ecfce2a6
Lewis Contreras <lcontreras@fluidattacks.com>
2023-03-10 07:31:07 -0500

    skims\feat(back): #8971 add scheduler test
    - Add test for scheduler function

--- skims/test/test_sca_scheduler.py
+++ skims/test/test_sca_scheduler.py
@@ -1,5 +1,6 @@
 import pytest
 from schedulers.update_sca_table.repositories.advisories_community import (
+    fix_npm_gem_go_range,
     format_range,
     RE_RANGES,
 )
@@ -31,4 +32,21 @@ def test_re_ranges_pattern(ranges: str, expected: list[str]) -> None:
 )
 def test_advs_format_range(range_str: str, expected: str) -> None:
     formated_range: str = format_range(range_str)
+    assert formated_range == expected
+
+
+@pytest.mark.skims_test_group("unittesting")
+@pytest.mark.parametrize(
+    "range_str,expected",
+    [
+        (">=3.1.0 <3.1.6||>=3.2.0 <3.2.6", ">=3.1.0 <3.1.6 || >=3.2.0 <3.2.6"),
+        ("<5.2.4.4||>=6.0.0.0 <6.0.3.3", "<5.2.4.4 || >=6.0.0.0 <6.0.3.3"),
+        (
+            ">=5.0.0.alpha <5.0.0.beta1.1||>=4.2.0.alpha <4.2.5.1",
+            ">=5.0.0.alpha <5.0.0.beta1.1 || >=4.2.0.alpha <4.2.5.1",
+        ),
+    ],
+)
+def test_fix_npm_gem_go_range(range_str: str, expected: str) -> None:
+    formated_range: str = fix_npm_gem_go_range(range_str)
     assert formated_range == expected
